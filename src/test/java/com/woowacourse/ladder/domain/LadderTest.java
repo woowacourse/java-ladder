504c8e59e5d62c11f5721f9350a6534855007d08
Jinwoo Shin <bluesky0255@gmail.com>
2019-05-14 17:40:42 +0900

    Add test for Ladder

---
+++ src/test/java/com/woowacourse/ladder/domain/LadderTest.java
@@ -0,0 +1,19 @@
+package com.woowacourse.ladder.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LadderTest {
+
+    @Test
+    void testCreate() {
+        Ladder ladder = new Ladder(Arrays.asList("pobi", "crong", "honux", 4));
+        assertThat(ladder.findDestination("pobi")).isEqualTo(0);
+        assertThat(ladder.findDestination("crong")).isEqualTo(2);
+        assertThat(ladder.findDestination("crong")).isEqualTo(1);
+
+    }
+}
