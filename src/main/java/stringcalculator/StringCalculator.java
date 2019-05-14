d8095b2738ab86225daf8a8768fe2d877e1c8662
conatuseus <conatuseus@gmail.com>
2019-05-14 15:16:24 +0900

    [FEATURE] 커스텀 구분자를 포함하는 경우 구현
    문자열에 커스텀 구분자를 포함하는 경우에
    해당 커스텀 구분자로 split해서 List에 저장
    
    Squashed commit of the following:
    
    commit 666f1afbf49aef5874db45ef577329796522db94
    Author: conatuseus <conatuseus@gmail.com>
    Date:   Tue May 14 15:15:53 2019 +0900
    
        커스텀 구분자를 포함한 문자열 확인 및 계산

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -3,7 +3,11 @@ package stringcalculator;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class StringCalculator {
 
@@ -13,9 +17,25 @@ public class StringCalculator {
         if (StringUtils.isBlank(formula)) {
             numbers.add(0);
         }
+        if (isCustomDelimiter(formula)) {
+            numbers = getNumbersByCustomDelimiter(formula);
+        }
+
+        throw new RuntimeException("잘못된 문자열입니다.");
+    }
+
+    private List<Integer> getNumbersByCustomDelimiter(String formula) {
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(formula);
+        m.find();
+        String customDelimiter = m.group(1);
+        return Arrays.stream(m.group(2).split(customDelimiter)).map(Integer::parseInt).collect(Collectors.toList());
     }
 
     public int calculate() {
         return numbers.stream().reduce(0, Integer::sum);
+    }
+
+    private boolean isCustomDelimiter(String formula) {
+        return Pattern.compile("//(.)\n(.*)").matcher(formula).find();
     }
 }
--- src/test/java/stringcalculator/StringCalculatorTest.java
+++ src/test/java/stringcalculator/StringCalculatorTest.java
@@ -3,6 +3,7 @@ package stringcalculator;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class StringCalculatorTest {
     StringCalculator stringCalculator;
@@ -17,6 +18,17 @@ public class StringCalculatorTest {
     void 빈문자열_입력() {
         stringCalculator = new StringCalculator("");
         assertThat(stringCalculator.calculate()).isZero();
+    }
+
+    @Test
+    void 커스텀_구분자_포함_확인() {
+        stringCalculator = new StringCalculator("//;\n1;2;3");
+        assertThat(stringCalculator.calculate()).isEqualTo(6);
+    }
+
+    @Test
+    void 커스텀_구분자가_포함된_잘못된_문자열() {
+        assertThrows(RuntimeException.class, () -> new StringCalculator("//;\n1,2,3"));
     }
 
 }
