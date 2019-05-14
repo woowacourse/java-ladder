05e56a2378043970bce2373260799649494c29b6
kmdngyu <kmdngyu@gmail.com>
2019-05-14 14:17:06 +0900

    add: 구분자 나눠서 저장

--- src/main/java/cal/SeparatorProcessor.java
+++ src/main/java/cal/SeparatorProcessor.java
@@ -1,9 +1,17 @@
 package cal;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class SeparatorProcessor {
     public static String [] inputSeparate(String input){
         input = input.substring(2);
         String [] splitedInput = input.split("₩n");
         return splitedInput;
+    }
+
+    public static List<String> splitSeparator(String separators){
+        return  new ArrayList<String>(Arrays.asList(separators.split("")));
     }
 }
--- src/test/java/cal/SeparatorProcessorTest.java
+++ src/test/java/cal/SeparatorProcessorTest.java
@@ -2,6 +2,8 @@ package cal;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SeparatorProcessorTest {
@@ -11,5 +13,9 @@ public class SeparatorProcessorTest {
         assertThat(SeparatorProcessor.inputSeparate(inputString)).isEqualTo(new String[]{".;[","1;2;3"});
     }
 
-   
+    @Test
+    void 구분자_쪼개서_저장(){
+        String separators = ".;[";
+        assertThat(SeparatorProcessor.splitSeparator(separators)).isEqualTo(Arrays.asList(".",";","["));
+    }
 }
