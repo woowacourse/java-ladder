ca281b8c389cd2a62ecf4085370131071abc81ba
school0bhy <house1217@ajou.ac.kr>
2019-05-14 09:28:07 +0900

    refactor : 메소드 분할 및 정리하여 리팩토링

--- src/main/java/textCalculator/TextCalculator.java
+++ src/main/java/textCalculator/TextCalculator.java
@@ -1,32 +1,41 @@
 package textCalculator;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TextCalculator {
-
     private static final String DEFAULT_DELIMITERS = ",:";
+    private List<String> delimiters;
 
-    public int calculate(String text) {
-        String customDelimiters = "";
-        Matcher m = Pattern.compile("//(.*)\n(.*)").matcher(text);
-        if (m.find()) {
-            customDelimiters += m.group(1);
-            text = m.group(2);
-        }
-        List<String> tokens = Arrays.asList(text.split(getDelimiterSet(customDelimiters)));
-        return getResult(tokens);
+    public TextCalculator() {
+        delimiters = new ArrayList<>(Arrays.asList((DEFAULT_DELIMITERS).split("")));
     }
 
-    private String getDelimiterSet(String customDelimiters) {
-        return Arrays.asList((DEFAULT_DELIMITERS + customDelimiters).split(""))
-                .toString()
-                .replaceAll(", ", "");
+    public int calculate(String text) {
+        return getResult(textTokenizer(text));
     }
 
     private int getResult(List<String> tokens) {
         return tokens.stream().mapToInt(token -> Integer.parseInt(token)).sum();
+    }
+
+    private List<String> textTokenizer(String text) {
+        return Arrays.asList(extractCustomDelimiters(text).split(getSplitDelimiter()));
+    }
+
+    private String extractCustomDelimiters(String text) {
+        Matcher m = Pattern.compile("//(.*)\n(.*)").matcher(text);
+        if (m.find()) {
+            delimiters.addAll(Arrays.asList((m.group(1)).split("")));
+            return m.group(2);
+        }
+        return text;
+    }
+
+    private String getSplitDelimiter() {
+        return delimiters.toString().replaceAll(", ", "");
     }
 }
