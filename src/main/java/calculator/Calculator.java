cfb9a8e78e4d3c5946f0b2c620a5153591379f63
stopsilver13 <stopsilver13>
2019-05-14 15:13:31 +0900

    Get sum of strings

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,18 +1,30 @@
 package calculator;
 
+import java.util.Arrays;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Calculator {
-    public String[] splitString(String input) {
-        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(input);
+    public static String[] splitString(String input) {
+        Matcher m = Pattern.compile("//(.)\\\\n(.*)").matcher(input);
         String regex = ",|:";
 
         if (m.find()) {
+            System.out.println("adf");
             String customDelimiter = m.group(1);
             return m.group(2).split(customDelimiter+"|"+regex);
         }
 
         return input.split(regex);
+    }
+
+    public static int sumString(String[] values) {
+        return Arrays.stream(values).mapToInt(value -> Integer.parseInt(value)).sum();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(sumString(splitString(scanner.next())));
     }
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -15,26 +15,43 @@ public class CalculatorTest {
 
     @Test
     void 쉼표로_문자열_구분하기() {
-        assertThat(calculator.splitString("1,2,3")).isEqualTo(new String[] {"1", "2", "3"});
+        assertThat(calculator.splitString("1,2,3")).isEqualTo(new String[]{"1", "2", "3"});
     }
 
     @Test
     void 콜론으로_문자열_구분하기() {
-        assertThat(calculator.splitString("1:2:3")).isEqualTo(new String[] {"1", "2", "3"});
+        assertThat(calculator.splitString("1:2:3")).isEqualTo(new String[]{"1", "2", "3"});
     }
 
     @Test
     void 쉼표와_콜론으로_문자열_구분하기() {
-        assertThat(calculator.splitString("1:2,3")).isEqualTo(new String[] {"1", "2", "3"});
+        assertThat(calculator.splitString("1:2,3")).isEqualTo(new String[]{"1", "2", "3"});
     }
 
     @Test
     void 커스텀_구분자로_문자열_구분하기() {
-        assertThat(calculator.splitString("//#\n1#2#3")).isEqualTo(new String[] {"1", "2", "3"});
+        assertThat(calculator.splitString("//#\n1#2#3")).isEqualTo(new String[]{"1", "2", "3"});
     }
 
     @Test
     void 쉼표와_콜론과_커스텀_구분자로_문자열_구분하기() {
-        assertThat(calculator.splitString("//#\n1#2,3:4")).isEqualTo(new String[] {"1", "2", "3", "4"});
+        assertThat(calculator.splitString("//#\n1#2,3:4")).isEqualTo(new String[]{"1", "2", "3", "4"});
     }
+
+    @Test
+    void 구분한_문자열의_합_구하기() {
+        assertThat(calculator.sumString(new String[]{"1", "2", "3"})).isEqualTo(6);
+    }
+
+    @Test
+    void 숫자_하나를_입력한_경우_문자열_구분() {
+        assertThat(calculator.splitString("1")).isEqualTo(new String[]{"1"});
+    }
+
+    @Test
+    void 숫자_하나를_입력한_경우_합() {
+        assertThat(calculator.sumString(new String[]{"1"})).isEqualTo(1);
+    }
+
+
 }
