0e4650539c1eb1ad10c06487350efba382e6d26a
yunheehyeon <ok94life@daum.net>
2019-05-14 16:46:46 +0900

    fix/<InputView 변경> 메소드 이름 변경 및 출력문 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -6,15 +6,17 @@ public class InputView {
     private static Scanner scanner = new Scanner(System.in);
 
     public static String inputNames() {
+        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
         return scanner.nextLine();
     }
 
-    public static int inputLadderLength() {
+    public static int inputLadderHeight() {
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
         try {
             return scanner.nextInt();
         } catch (NumberFormatException e) {
             scanner.nextLine();
-            return inputLadderLength();
+            return inputLadderHeight();
         }
     }
 }
