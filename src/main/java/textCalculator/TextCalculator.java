c37447e559d9e277e755a7a0839ba4803072f53a
school0bhy <house1217@ajou.ac.kr>
2019-05-14 09:01:02 +0900

    feat : 콜론(:) 구분자 입력 케이스
    콜론을 기준으로 나눈 숫자에 대한 합을 리턴

--- src/main/java/textCalculator/TextCalculator.java
+++ src/main/java/textCalculator/TextCalculator.java
@@ -5,10 +5,17 @@ import java.util.List;
 
 public class TextCalculator {
 
+    private static final String DEFAULT_DELIMITERS = ",:";
+
     public int calculate(String text) {
-        String delimiter = ",";
-        List<String> tokens = Arrays.asList(text.split(delimiter));
+        List<String> tokens = Arrays.asList(text.split(getDelimiterSet()));
         return getResult(tokens);
+    }
+
+    private String getDelimiterSet() {
+        return Arrays.asList(DEFAULT_DELIMITERS.split(""))
+                .toString()
+                .replaceAll(", ", "");
     }
 
     private int getResult(List<String> tokens) {
--- src/test/java/textCalculator/TextCalculatorTest.java
+++ src/test/java/textCalculator/TextCalculatorTest.java
@@ -14,4 +14,9 @@ public class TextCalculatorTest {
     void 쉼표구분자포함() {
         assertThat(new TextCalculator().calculate("1,2")).isEqualTo(3);
     }
+
+    @Test
+    void 콜론구분자포함() {
+        assertThat(new TextCalculator().calculate("1:2")).isEqualTo(3);
+    }
 }
