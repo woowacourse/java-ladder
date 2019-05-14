67fd53b355afe9c496ebe3dd2a364a5ff1f24619
SunHo Van <vsh123@naver.com>
2019-05-14 12:05:07 +0900

    feat : 숫자 덧셈 기능 구현
     1. List안의 숫자들의 합을 반환하는 테스트코드 작성
     2. 위에 대한 프로덕션 코드 작성

---
+++ src/test/java/stringadder/domain/IntegerAdderTest.java
@@ -0,0 +1,20 @@
+package stringadder.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class IntegerAdderTest {
+    @Test
+    public void List안의_숫자들의_합을_반환하는지_테스트() {
+        List<Integer> integers = new ArrayList<>(Arrays.asList(1, 2, 3));
+        assertThat(IntegerAdder.add(integers)).isEqualTo(6);
+
+        integers = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+        assertThat(IntegerAdder.add(integers)).isEqualTo(10);
+    }
+}
