dd5e5117dab7d12c24c5bb498157695fcea9f34e
pkch93 <star_final@naver.com>
2019-05-15 16:17:40 +0900

    feat Row 줄긋기 기능 구현
    - 해당 위치에서 가로로 이동할 수 있는지 여부를 true / false로 구현

---
+++ src/main/java/ladder/Row.java
@@ -0,0 +1,39 @@
+package ladder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Row {
+    private List<Boolean> lines;
+
+    public Row(int countOfMember) {
+        lines = new ArrayList<>();
+
+        lines.add(booleanGenerator(randomValueGenerator(), false));
+
+        for (int i = 1; i < countOfMember - 1; i++) {
+            lines.add(booleanGenerator(randomValueGenerator(), lines.get(i - 1)));
+        }
+    }
+
+    private boolean booleanGenerator(int random, boolean prevStatus) {
+        return !prevStatus && random == 1;
+    }
+
+    private int randomValueGenerator() {
+        return (int) (Math.random() * 2);
+    }
+
+    public int getLineSize() {
+        return lines.size();
+    }
+
+    public boolean checkDoubleDraw() {
+        for (int i = 0; i < lines.size() - 1; i++) {
+            if (lines.get(i) && lines.get(i+1)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ src/test/java/ladder/RowTest.java
@@ -0,0 +1,26 @@
+package ladder;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RowTest {
+
+    Row row;
+
+    @BeforeEach
+    void setUp() {
+        row = new Row(4);
+    }
+
+    @Test
+    void Row_생성_확인() {
+        assertThat(row.getLineSize()).isEqualTo(3);
+    }
+
+    @Test
+    void 두번_긋기_확인() {
+        assertThat(row.checkDoubleDraw()).isFalse();
+    }
+}
