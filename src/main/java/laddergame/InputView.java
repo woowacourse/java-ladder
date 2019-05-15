a17f7c2dd5183cff214bc567254da8b9c860516e
yuyu154 <38104827+yuyu154@users.noreply.github.com>
2019-05-15 23:17:10 +0900

    [ADD] 입력 기능 추가

---
+++ src/main/java/laddergame/InputView.java
@@ -0,0 +1,21 @@
+package laddergame;
+
+import java.util.Scanner;
+
+public class InputView {
+	public static final Scanner SCANNER = new Scanner(System.in);
+
+	private InputView() {
+
+	}
+
+	public static String inputPlayers() {
+		System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
+		return SCANNER.nextLine();
+	}
+
+	public static String inputLadderHeight() {
+		System.out.println("최대 사다리 높이는 몇 개인가요?");
+		return SCANNER.nextLine();
+	}
+}
---
+++ src/main/java/laddergame/Main.java
@@ -0,0 +1,14 @@
+package laddergame;
+
+import laddergame.domain.Ladder;
+import laddergame.domain.LadderHeight;
+import laddergame.domain.PlayerMaker;
+
+public class Main {
+
+	public static void main(String[] args) {
+		PlayerMaker playerMaker = new PlayerMaker(InputView.inputPlayers());
+		LadderHeight ladderHeight = new LadderHeight(InputView.inputLadderHeight());
+		Ladder ladder = new Ladder(ladderHeight.getLadderHeight(), playerMaker.makePlayers().size());
+	}
+}
