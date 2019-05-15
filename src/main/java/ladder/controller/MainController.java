1158a295fce76795a650dbf11a09f00f23020035
conatuseus <conatuseus@gmail.com>
2019-05-15 13:23:47 +0900

    [FEATURE] 이름 입력 기능 구현
    InputView에서 입력을 받고 split해서 반환
    MainController 에서 해당 메소드 사용해서 이름 가져옴
    
    Squashed commit of the following:
    
    commit 6b317b1a12d3d2e6452eae70a0a4c59775e38f83
    Author: conatuseus <conatuseus@gmail.com>
    Date:   Wed May 15 13:23:21 2019 +0900
    
        Create InutView, MainController

---
+++ src/main/java/ladder/controller/MainController.java
@@ -0,0 +1,10 @@
+package ladder.controller;
+
+import ladder.view.InputView;
+
+public class MainController {
+    public static void main(String[] args) {
+        String[] names= InputView.inputNames();
+
+    }
+}
---
+++ src/main/java/ladder/view/InputView.java
@@ -0,0 +1,15 @@
+package ladder.view;
+
+import java.util.Scanner;
+
+public class InputView {
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    public static String[] inputNames() {
+        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
+        String[] scannedNames=SCANNER.nextLine().split(",");
+        return scannedNames;
+    }
+
+
+}
