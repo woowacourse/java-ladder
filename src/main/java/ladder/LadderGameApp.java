9021bfd02cdb5064a5891c49a0e76f3235c8a30d
Yu-Min Kim <green4469@yonsei.ac.kr>
2019-05-15 13:54:33 +0900

    add: 게임 진행 로직 추가

---
+++ src/main/java/ladder/LadderGameApp.java
@@ -0,0 +1,10 @@
+package ladder;
+
+import ladder.controller.LadderGame;
+
+public class LadderGameApp {
+    public static void main(String[] args) {
+        LadderGame ladderGame = new LadderGame();
+        ladderGame.play();
+    }
+}
--- src/main/java/ladder/controller/LadderGame.java
+++ src/main/java/ladder/controller/LadderGame.java
@@ -2,6 +2,7 @@ package ladder.controller;
 
 import ladder.domain.Ladder;
 import ladder.domain.Player;
+import ladder.domain.Result;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,4 +10,28 @@ import java.util.List;
 public class LadderGame {
     private Ladder ladder;
     private List<Player> players = new ArrayList<>();
+    private List<String> rewards;
+
+    public LadderGame() {
+        List<String> names = InputView.getNames();
+        for (int i = 0; i < names.size(); i++) {
+            players.add(new Player(names.get(i), i));
+        }
+        rewards = InputView.getRewards(players);
+        int height = InputView.getHeight();
+        ladder = new Ladder(players.size(), height);
+    }
+
+    public void play() {
+        printGame();
+        ladder.goDown(players);
+        Result result = new Result(players, rewards);
+        String name;
+        while(!(name = InputView.getName(players)).equals("all")){
+            printResult(name, result);
+        }
+        printResultAll(result);
+    }
+
+
 }
