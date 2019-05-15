fb38c8a5b9511d4ab1c9d0144cc38cb9ada335a9
men7627 <men1219@hanmail.net>
2019-05-15 17:11:12 +0900

    Add : Results Class

---
+++ src/main/java/ladder/domain/Results.java
@@ -0,0 +1,47 @@
+package ladder.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Results {
+    private static final String DELIMITER = ",";
+    private static final String VALID_INPUT_ERROR = "플레이어 이름들 입력 형식 오류";
+    private static final String RESULTS_INPUT_REGEX = "^([^,]+)(,[^,]+)*$";
+
+    private List<ResultName> resultNames;
+
+    public Results(String input) {
+        resultNames = new ArrayList<>();
+        checkValidInput(input);
+        addResultNames(input);
+    }
+
+    public List<ResultName> getResultNames() {
+        return resultNames;
+    }
+
+    public int getResultNumber() {
+        return resultNames.size();
+    }
+
+    private void addResultNames(String input) {
+        for(String name : input.split(DELIMITER)){
+            resultNames.add(new ResultName(name.trim()));
+        }
+    }
+
+    private void checkValidInput(String input) {
+        if(!input.matches(RESULTS_INPUT_REGEX)){
+            throw new IllegalArgumentException(VALID_INPUT_ERROR);
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for(ResultName resultName : resultNames){
+            sb.append(resultName.toString());
+        }
+        return sb.toString();
+    }
+}
---
+++ src/test/java/ladder/domain/ResultsTest.java
@@ -0,0 +1,35 @@
+package ladder.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ResultsTest {
+    @Test
+    void 결과명들_콤마시작_검사() {
+        assertThrows(IllegalArgumentException.class, ()->{
+            new Results(",abc,abc");
+        });
+    }
+
+    @Test
+    void 결과명들_콤마끝_검사() {
+        assertThrows(IllegalArgumentException.class, ()->{
+            new Results("abc,abc,");
+        });
+    }
+
+    @Test
+    void 결과명들_콤마중복_검사() {
+        assertThrows(IllegalArgumentException.class, ()->{
+            new Results("abc,,abc");
+        });
+    }
+
+    @Test
+    void 결과명들_콤마만입력_검사() {
+        assertThrows(IllegalArgumentException.class, ()->{
+            new Results(",");
+        });
+    }
+}
