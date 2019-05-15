87f47b7012dabd16691f1e6c1996d98297235157
yunheehyeon <ok94life@daum.net>
2019-05-15 11:49:54 +0900

    feature/<사다리 게임 실행>

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -1,4 +1,25 @@
 package controller;
 
+import domain.LadderGame;
+import view.InputView;
+import view.OutputView;
+
 public class Main {
+    public static void main(String[] args) {
+        LadderGame ladderGame = createLadderGame();
+        OutputView.outputNames(ladderGame.getUsers());
+        ladderGame.createLadder();
+        OutputView.outputLadder(ladderGame.getLadder());
+    }
+
+    private static LadderGame createLadderGame(){
+        String names = InputView.inputNames();
+        int height = InputView.inputLadderHeight();
+        try {
+            return new LadderGame(names, height);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return  createLadderGame();
+        }
+    }
 }
