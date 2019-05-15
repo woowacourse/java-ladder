cbefb56401f12433b623015d7c174dd240e0a937
yunheehyeon <ok94life@daum.net>
2019-05-15 11:48:40 +0900

    <feature>/<사다리 그리기> 사다리 출력

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,13 +1,38 @@
 package view;
 
 import java.util.List;
+
+import domain.Floor;
 import domain.User;
 
 public class OutputView {
     public static void outputNames(List<User> users) {
         for (User user : users) {
-            System.out.print(user.getName() + "\t");
+            System.out.printf("%-6s", user.getName());
         }
         System.out.println();
+    }
+
+    public static void outputLadder(List<Floor> ladder) {
+        for (Floor floor : ladder) {
+            outputFloor(floor);
+            System.out.println("|");
+        }
+    }
+
+    private static void outputFloor(Floor floor) {
+        for (boolean isStair : floor.getStairs()) {
+            System.out.print("|");
+            outputStair(isStair);
+        }
+    }
+
+    private static void outputStair(boolean isStair) {
+        if (isStair) {
+            System.out.print("-----");
+        }
+        if (!isStair) {
+            System.out.print("     ");
+        }
     }
 }
