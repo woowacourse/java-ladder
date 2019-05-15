2d7b273a8ff18db2ad8771f95a251ad27171b40e
yuyu154 <38104827+yuyu154@users.noreply.github.com>
2019-05-15 15:55:23 +0900

    [ADD] 사다리 높이와 관련된 클래스 구현

---
+++ src/main/java/laddergame/LadderHeight.java
@@ -0,0 +1,43 @@
+package laddergame;
+
+import java.util.Objects;
+
+public class LadderHeight {
+    public final int ladderHeight;
+
+    public LadderHeight(String height) {
+        this.ladderHeight = parse(height);
+        checkValidBound();
+    }
+
+    private void checkValidBound() {
+        if (this.ladderHeight <= 0) {
+            throw new IllegalArgumentException("1 이상의 정수를 입력해주세요");
+        }
+    }
+
+    private int parse(String height) {
+        try {
+            return Integer.parseInt(height);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자를 입력해주세요");
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof LadderHeight)) return false;
+        LadderHeight that = (LadderHeight) o;
+        return Objects.equals(ladderHeight, that.ladderHeight);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ladderHeight);
+    }
+
+    public int getLadderHeight() {
+        return this.ladderHeight;
+    }
+}
