d6cf96f4b5bd5cc5885efe97271fba85e4bcf512
conatuseus <conatuseus@gmail.com>
2019-05-15 17:33:22 +0900

    [FEATURE] 높이만큼 Line을 가지는 사다리 생성 구현
    높이만큼 Line이 만들어 졌는지 TestCode 작성
    해당 기능에 필요한 메소드 구현
    리팩토링
    
    Squashed commit of the following:
    
    commit ed428a9f09f2a6597b480ff61677c4f8c77cc273
    Author: conatuseus <conatuseus@gmail.com>
    Date:   Wed May 15 17:32:46 2019 +0900
    
        높이만큼 Line을 가지는 사다리를 만든다
    
    commit e7c18485f75cc8c12f1c4f0365afccea30e0aef2
    Author: conatuseus <conatuseus@gmail.com>
    Date:   Wed May 15 17:12:34 2019 +0900
    
        오타 수정

--- src/main/java/ladder/model/Ladder.java
+++ src/main/java/ladder/model/Ladder.java
@@ -1,18 +1,32 @@
 package ladder.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Ladder {
     private static final int MIN_LADDER_HEIGHT = 1;
     private final int height;
+    private List<Line> lines = new ArrayList<>();
 
-    public Ladder(int height) {
-        if (this.isValidHeight(height)) {
+    public Ladder(int height, int countOfPlayer) {
+        if (!this.isValidHeight(height)) {
             throw new IllegalArgumentException("높이는 1 이상의 정수이어야 합니다.");
         }
         this.height = height;
+        linesInit(countOfPlayer);
+    }
+
+    private void linesInit(int countOfPlayer) {
+        for (int i = 0; i < height; i++) {
+            lines.add(new Line(countOfPlayer));
+        }
     }
 
     private boolean isValidHeight(int height) {
         return height >= MIN_LADDER_HEIGHT;
     }
 
+    public int linesSize() {
+        return lines.size();
+    }
 }
--- src/test/java/ladder/LadderTest.java
+++ src/test/java/ladder/LadderTest.java
@@ -3,18 +3,24 @@ package ladder;
 import ladder.model.Ladder;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class LadderTest {
 
     @Test
-    void 올바른_사다리_높이일_때_생성자_확인(){
-        assertDoesNotThrow(() -> new Ladder(5));
+    void 올바른_사다리_높이일_때_생성자_확인() {
+        assertDoesNotThrow(() -> new Ladder(5, 1));
     }
 
     @Test
-    void _1보다_작은_사다리_높이일_때_예외발생(){
-        assertThrows(IllegalArgumentException.class, ()-> new Ladder(0));
+    void _1보다_작은_사다리_높이일_때_예외발생() {
+        assertThrows(IllegalArgumentException.class, () -> new Ladder(0, 1));
+    }
+
+    @Test
+    void Ladder의_크기가_Height와_같은_지_검증() {
+        assertThat(new Ladder(5, 4).linesSize()).isEqualTo(5);
     }
 }
