5de629ec760460dc0fc79278da8412a70f6f11e9
men7627 <men1219@hanmail.net>
2019-05-15 14:19:47 +0900

    Refactor : Line Constructor

--- src/main/java/ladder/domain/Line.java
+++ src/main/java/ladder/domain/Line.java
@@ -1,16 +1,32 @@
 package ladder.domain;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Line {
-    List<Boolean> horizon;
+    private List<Boolean> horizon;
 
-    public Line(List<Boolean> horizon) {
-        this.horizon = horizon;
+    public Line(int playerNumber) {
+        this.horizon = makeRandomBooleans(playerNumber);
     }
 
     public List<Boolean> getHorizon() {
         return this.horizon;
+    }
+
+    private List<Boolean> makeRandomBooleans(int playerNumber){
+        List<Boolean> booleans = new ArrayList<>();
+        Random random = new Random();
+        booleans.add(random.nextBoolean());
+        for (int i = 1; i < playerNumber - 1; i++) {
+            if (booleans.get(i - 1)) {
+                booleans.add(false);
+                continue;
+            }
+            booleans.add(random.nextBoolean());
+        }
+        return booleans;
     }
 //
 //    public void settingHorizonLine() {
--- src/test/java/ladder/domain/LineTest.java
+++ src/test/java/ladder/domain/LineTest.java
@@ -2,14 +2,22 @@ package ladder.domain;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LineTest {
+//    @Test
+//    void 가로줄이_있나요() {
+//        Line line = new Line(Arrays.asList(true, false, true, false, true));
+//        assertThat(line.getHorizon()).isEqualTo(Arrays.asList(true, false, true, false, true));
+//    }
+
     @Test
-    void 가로줄이_있나요() {
-        Line line = new Line(Arrays.asList(true, false, true, false, true));
-        assertThat(line.getHorizon()).isEqualTo(Arrays.asList(true, false, true, false, true));
+    void 다음엔_무조건_트루6() {
+        List<Boolean> testLine = (new Line(5)).getHorizon();
+        for (int i = 0; i < testLine.size() - 1; i++) {
+            assertThat(testLine.get(i) && testLine.get(i + 1)).isEqualTo(false);
+        }
     }
 }
