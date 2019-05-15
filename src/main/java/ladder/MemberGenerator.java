495ec7a9249ac84155122e41e4ba82b58af84a90
pkch93 <star_final@naver.com>
2019-05-15 11:20:52 +0900

    feat 입력값을 구분자 쉼표를 기준으로 split

---
+++ src/main/java/ladder/MemberGenerator.java
@@ -0,0 +1,8 @@
+package ladder;
+
+public class MemberGenerator {
+    public static String[] splitByComma(String inputText) {
+        String[] members = inputText.split(",");
+        return members;
+    }
+}
---
+++ src/test/java/ladder/MemberGeneratorTest.java
@@ -0,0 +1,16 @@
+package ladder;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MemberGeneratorTest {
+
+    @Test
+    void 쉼표_구분자_분리() {
+        String inputText = "pobi,crong";
+        String[] result = MemberGenerator.splitByComma(inputText);
+        String[] members = {"pobi", "crong"};
+        assertThat(result).isEqualTo(members);
+    }
+}
