254456d1f3ece178d63d832cdb96330f855f4c6b
pkch93 <star_final@naver.com>
2019-05-15 12:27:34 +0900

    feat 사다리 높이 예외처리 구현
    - 사다리 높이가 0인 경우 예외처리
    - 사다리 높이가 음수인 경우 예외처리

---
+++ src/main/java/ladder/LadderValidator.java
@@ -0,0 +1,12 @@
+package ladder;
+
+public class LadderValidator {
+
+    private static final int MIN_HEIGHT = 1;
+
+    public static void checkHeight(int height) {
+        if (height < MIN_HEIGHT) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
