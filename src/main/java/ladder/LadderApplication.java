93912ad9c28ee61bf73f011bea7ec6dc7b4effb4
kmdngyu <kmdngyu@gmail.com>
2019-05-15 16:09:33 +0900

    add: 입력처리

--- src/main/java/ladder/LadderApplication.java
+++ src/main/java/ladder/LadderApplication.java
@@ -1,8 +1,13 @@
 package ladder;
 
+import ladder.controller.LadderController;
+import ladder.view.InputView;
+
 public class LadderApplication {
+    public static void main(String[] args){
+        LadderController ladderController = new LadderController();
+        ladderController.registParticipant(InputView.inputNames());
+        ladderController.makeLadder(InputView.inputLadderHeight());
 
-
-
-
+    }
 }
--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -1,5 +1,32 @@
 package ladder.view;
 
-public class InputView {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
+public class InputView {
+    private static final Scanner input = new Scanner(System.in);
+
+    public static List<String> inputNames() {
+        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요).");
+        return filterTypos(input.nextLine());
+    }
+
+    private static List<String> filterTypos(String arg) {
+        return new ArrayList<>(
+                Arrays.stream(arg.split(","))
+                        .map(x -> x.trim())
+                        .filter(x -> (!x.equals("")) && (!x.equals(" ")) && (!x.equals(",")))
+                        .collect(Collectors.toList())
+        );
+    }
+
+    public static int inputLadderHeight() throws IllegalArgumentException {
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
+        return Integer.parseInt(input.nextLine().trim());
+    }
 }
+
+
