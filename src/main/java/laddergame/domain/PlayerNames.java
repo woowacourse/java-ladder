5567e3c3ded7826c062026380ff03fe842cd33c7
dasistHYOJIN <hgenie14@gmail.com>
2019-05-15 14:23:37 +0900

    [REFACTOR] Seperate methods from makeNames

--- src/main/java/laddergame/domain/PlayerNames.java
+++ src/main/java/laddergame/domain/PlayerNames.java
@@ -1,12 +1,47 @@
 package laddergame.domain;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class PlayerNames {
     public List<String> makeNames(String input) {
+        input = input.replaceAll(" ", "");
         List<String> names = new ArrayList<>(Arrays.asList(input.split(",")));
+
+        checkConditions(input, names);
+
         return names;
+    }
+
+    private static void checkConditions(String input, List<String> names) {
+        checkNullName(input);
+        checkEmptyName(input);
+        checkNameLength(names);
+        checkDuplicatedName(names);
+    }
+
+    private static void checkDuplicatedName(List<String> names) {
+        Set<String> namesWithoutDuplicates = new HashSet<>(names);
+        if (names.size() != namesWithoutDuplicates.size()) {
+            throw new IllegalArgumentException("중복된 이름이 존재합니다.\n다시 입력해주세요.");
+        }
+    }
+
+    private static void checkNameLength(List<String> names) {
+        for (String name : names) {
+            if (name.isEmpty() || name.length() > 5)
+                throw new IllegalArgumentException("이름의 길이는 1이상이어야 합니다.\n다시 입력해주세요.");
+        }
+    }
+
+    private static void checkNullName(String input) {
+        if (input == null) {
+            throw new IllegalArgumentException("입력하신 이름이 null입니다.\n다시 입력해주세요.");
+        }
+    }
+
+    private static void checkEmptyName(String input) {
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException("입력하신 이름이 없습니다.\n다시 입력해주세요.");
+        }
     }
 }
--- src/test/java/laddergame/domain/PlayerNamesTest.java
+++ src/test/java/laddergame/domain/PlayerNamesTest.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class PlayerNamesTest {
     @Test
@@ -14,4 +15,46 @@ public class PlayerNamesTest {
         List<String> checker = Arrays.asList("a", "b", "c");
         assertThat(new PlayerNames().makeNames(input)).isEqualTo(checker);
     }
+
+    @Test
+    void 널일때예외처리되는지테스트() {
+        assertThrows(IllegalArgumentException.class, ()->
+                new PlayerNames().makeNames(null));
+    }
+
+    @Test
+    void 엠티쓰트링일때테스트() {
+        assertThrows(IllegalArgumentException.class, ()->
+                new PlayerNames().makeNames(""));
+    }
+
+    @Test
+    void 중복하는_이름이_존재할때_예외테스트() {
+        assertThrows(IllegalArgumentException.class, ()->
+                new PlayerNames().makeNames("a,a,b"));
+    }
+
+    @Test
+    void 이름에_빈칸이_있을때_예외테스트() {
+        new PlayerNames().makeNames("a,,,b");
+        /*assertThrows(IllegalArgumentException.class, ()->
+                new PlayerNames().makeNames("a,,,b"));*/
+    }
+
+    @Test
+    void 이름에_공백문자만_있을때_예외테스트() {
+        assertThrows(IllegalArgumentException.class, ()->
+                new PlayerNames().makeNames("a, ,b"));
+        assertThrows(IllegalArgumentException.class, ()->
+                new PlayerNames().makeNames("a,  ,b"));
+    }
+
+    @Test
+    void 이름의길이가적합하지않을때() {
+        assertThrows(IllegalArgumentException.class, ()->
+                new PlayerNames().makeNames("a,b,aadsfsdfsadfadf"));
+        assertThrows(IllegalArgumentException.class, ()->
+                new PlayerNames().makeNames("a,,b"));
+    }
+
 }
