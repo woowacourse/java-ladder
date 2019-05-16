d42089bd26f2be8e13d570c01e54a8f7cd6e08d2
starkim06 <starkim06@naver.com>
2019-05-16 11:08:02 +0900

    refactor: SubLineRandomGenerator.class Stack을 사용하여 subLine 생성 수정

--- src/main/java/ladder/domain/SubLineRandomGenerator.java
+++ src/main/java/ladder/domain/SubLineRandomGenerator.java
@@ -2,6 +2,8 @@ package ladder.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.Stack;
 
 public final class SubLineRandomGenerator implements SubLineGenerator {
     private final int countOfPlayers;
@@ -12,17 +14,22 @@ public final class SubLineRandomGenerator implements SubLineGenerator {
 
     @Override
     public List<Boolean> generate() {
-        List<Boolean> subLines = new ArrayList<>();
+        Stack<Boolean> subLines = new Stack<>();
         for (int i = 0; i < countOfPlayers - 1; i++) {
-            if (Math.random() < 0.5) {
-                subLines.add(true);
-                i++;
-                if(i==countOfPlayers-1){
-                    break;
-                }
-            }
-            subLines.add(false);
+            push(subLines);
         }
         return subLines;
+    }
+
+    private void push(Stack<Boolean> subLines) {
+        if (!subLines.empty() && subLines.peek()) {
+            subLines.push(false);
+            return;
+        }
+        subLines.push(randomBoolean());
+    }
+
+    private Boolean randomBoolean() {
+        return new Random().nextBoolean();
     }
 }
