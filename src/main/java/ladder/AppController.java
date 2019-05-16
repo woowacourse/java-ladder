eebc7d8253496529f5a8aa763d758d15766d1a59
pkch93 <star_final@naver.com>
2019-05-16 14:53:08 +0900

    feat Controller, Main 구현

--- src/main/java/ladder/AppController.java
+++ src/main/java/ladder/AppController.java
@@ -4,16 +4,26 @@ import ladder.model.LadderGame;
 import ladder.model.Member;
 import ladder.model.Members;
 import ladder.view.InputView;
+import ladder.view.OutputView;
 
 import java.util.List;
 
 public class AppController {
 
-    public LadderGame startGame() {
+    public void play(){
+        LadderGame ladderGame = initGame();
+        playGame(ladderGame);
+    }
+
+    private LadderGame initGame() {
         String[] names = InputView.inputNames();
         List<Member> members = Members.generateMembers(names);
         int ladderHeight = InputView.inputLadderHeight();
 
         return new LadderGame(members, ladderHeight);
+    }
+
+    private void playGame(LadderGame ladderGame){
+        OutputView.printLadder(ladderGame);
     }
 }
