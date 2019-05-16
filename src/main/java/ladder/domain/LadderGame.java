5c06cc1222742d569ce7ff92b1f83a3985441688
DoWon Lee <gch01410@ajou.ac.kr>
2019-05-16 16:24:13 +0900

    Feat LadderGame

---
+++ src/main/java/ladder/domain/LadderGame.java
@@ -0,0 +1,41 @@
+package ladder.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LadderGame {
+    private static final int BOUNDARY_CHECK= 1;
+
+    public static List<Integer> play(Ladder ladder) {
+        List<Integer> result = new ArrayList<>();
+        int numberOfPeople = ladder.getNumberOfPeople();
+
+        for (int i = 0; i < numberOfPeople; i++) {
+            result.add(decidePosition(ladder, i));
+        }
+
+        return result;
+    }
+
+    private static int decidePosition(Ladder ladder, int column) {
+        int row = 0;
+
+        while (row != ladder.getHeight()) {
+            column = moveColumn(ladder, row, column);
+            row++;
+        }
+
+        return column;
+    }
+
+    private static int moveColumn(Ladder ladder, int row, int column) {
+        if (ladder.getLine(row).isConnected(column)) {
+            return column + 1;
+        }
+        if (column >= BOUNDARY_CHECK && ladder.getLine(row).isConnected(column - 1)) {
+            return column - 1;
+        }
+
+        return column;
+    }
+}
--- src/test/java/ladder/domain/LadderGameTest.java
+++ src/test/java/ladder/domain/LadderGameTest.java
@@ -2,8 +2,19 @@ package ladder.domain;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class LadderGameTest {
     @Test
-    void name() {
+    public void playTest() {
+        Ladder ladder = LadderGenerator.generate(3, 4);
+
+        List<Integer> result = LadderGame.play(ladder);
+        List<Integer> result2 = Arrays.asList(0, 1, 2, 3);
+
+        assertThat(result).isEqualTo(result2);
     }
 }
