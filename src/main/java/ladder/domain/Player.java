68e55159a763644623ebadedbd2f7765262bb59e
stopsilver13 <stopsilver13>
2019-05-16 23:39:50 +0900

    Refactor: Modify bugs at Player class trymove method

--- src/main/java/ladder/domain/Player.java
+++ src/main/java/ladder/domain/Player.java
@@ -3,48 +3,48 @@ package ladder.domain;
 import java.util.List;
 
 public class Player {
-	private String name;
-	private int position;
+    private String name;
+    private int position;
 
-	public Player(String name, int position) {
-		this.name = name;
-		this.position = position;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public int getPosition() {
-	    return position;
+    public Player(String name, int position) {
+        this.name = name;
+        this.position = position;
     }
 
-	public int trymove(List<Boolean> points) {
-		if (position == 0) {
-			return moveRight();
-		}
-		if (position == points.size() - 1) {
-			return moveLeft();
-		}
-		if (points.get(position - 1)) {
-			return moveLeft();
-		}
-		return moveRight();
-	}
+    public String getName() {
+        return name;
+    }
 
-	private int moveLeft() {
-		position --;
-		return position;
-	}
+    public int getPosition() {
+        return position;
+    }
 
-	private int moveRight() {
-		position++;
-		return position;
-	}
+    public int trymove(List<Boolean> points) {
+        if (position == 0) {
+            return (points.get(position) ? moveRight() : position);
+        }
+        if (position == points.size()) {
+            return (points.get(position - 1) ? moveLeft() : position);
+        }
+        if (points.get(position - 1)) {
+            return moveLeft();
+        }
+        return moveRight();
+    }
 
-	public boolean matchName(String name) {
-		return this.name.equals(name);
-	}
+    private int moveLeft() {
+        position--;
+        return position;
+    }
+
+    private int moveRight() {
+        position++;
+        return position;
+    }
+
+    public boolean matchName(String name) {
+        return this.name.equals(name);
+    }
 
 
 }
