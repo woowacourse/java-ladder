5d892d6e39e1524f32e20a7d89a6297d34a0fd5d
Seongmo Jeon <oeeen3@gmail.com>
2019-05-16 15:37:49 +0900

    test: 사다리 게임 결과 출력 객체 테스트 추가

---
+++ src/main/java/ladder/domain/LadderResult.java
@@ -0,0 +1,48 @@
+package ladder.domain;
+
+import java.util.*;
+
+public class LadderResult {
+    private final LinkedHashMap<String, String> nameToResult = new LinkedHashMap<>();
+
+    public LadderResult(List<Integer> allResult, List<String> names, List<String> resultCandidate) {
+        for (int i = 0; i < allResult.size(); i++) {
+            String name = names.get(i);
+            String result = resultCandidate.get(allResult.get(i));
+            nameToResult.put(name, result);
+        }
+    }
+
+    public String matchResult(String requestedName) {
+        if (requestedName.equals("all")) {
+            return makeAllResult();
+        }
+        return nameToResult.get(requestedName);
+    }
+
+    private String makeAllResult() {
+        StringBuilder sb = new StringBuilder();
+
+        for (String name : nameToResult.keySet()) {
+            sb.append(makeResult(name));
+        }
+        return sb.toString();
+    }
+
+    private String makeResult(String name) {
+        return name + " : " + nameToResult.get(name) + "\n";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LadderResult that = (LadderResult) o;
+        return Objects.equals(nameToResult, that.nameToResult);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nameToResult);
+    }
+}
---
+++ src/test/java/ladder/domain/LadderResultTest.java
@@ -0,0 +1,31 @@
+package ladder.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LadderResultTest {
+    @Test
+    void 생성자확인() {
+        List<Integer> allResult = Arrays.asList(0, 1, 2);
+        List<String> names = Arrays.asList("pobi", "brown", "woni");
+        List<String> resultCandidate = Arrays.asList("꽝", "5000", "꽝");
+        LadderResult ladderResult = new LadderResult(allResult, names, resultCandidate);
+        assertThat(ladderResult).isEqualTo(new LadderResult(allResult, names, resultCandidate));
+    }
+
+    @Test
+    void 결과_출력() {
+        List<Integer> allResult = Arrays.asList(0, 1, 2);
+        List<String> names = Arrays.asList("pobi", "brown", "woni");
+        List<String> resultCandidate = Arrays.asList("꽝", "5000", "꽝");
+        LadderResult ladderResult = new LadderResult(allResult, names, resultCandidate);
+        assertThat(ladderResult.matchResult("woni")).isEqualTo("꽝");
+        assertThat(ladderResult.matchResult("all")).isEqualTo("pobi : 꽝\nbrown : 5000\nwoni : 꽝\n");
+    }
+
+
+}
