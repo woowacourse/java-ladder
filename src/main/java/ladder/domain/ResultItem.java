7ef08598fbc5e15a1730f2c356ec30abdcb19145
SunHo Van <vsh123@naver.com>
2019-05-16 17:12:43 +0900

    feat : 당첨 항목을 저장하는 Resultitem 클래스 생성합니다.

---
+++ src/main/java/ladder/domain/ResultItem.java
@@ -0,0 +1,31 @@
+package ladder.domain;
+
+public class ResultItem {
+    private static final int MAXIMUM_NAME_LENGTH = 5;
+    private static final int MINIMUM_NANE_LENGTH = 1;
+
+    private final String itemName;
+
+    public ResultItem(String itemName) {
+        nullCheck(itemName);
+        this.itemName = getTrimed(itemName);
+    }
+
+    private void nullCheck(String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("당첨 항목은 null이 입력될 수 없습니다.");
+        }
+    }
+
+    private String getTrimed(String name) {
+        name = name.trim();
+        vaildateNameLength(name);
+        return name;
+    }
+
+    private void vaildateNameLength(String name) {
+        if ((name.length() > MAXIMUM_NAME_LENGTH) || (name.length() < MINIMUM_NANE_LENGTH)) {
+            throw new IllegalArgumentException("당첨 항목의 길이는 5자 이하여야 합니다.");
+        }
+    }
+}
