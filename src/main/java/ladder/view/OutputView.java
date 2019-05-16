7f3e5e89cd39d96770bc5fdbcc39c50923f5c690
Seongmo Jeon <oeeen3@gmail.com>
2019-05-16 16:07:58 +0900

    refactor: 사다리 출력 로직 추가

--- src/main/java/ladder/view/OutputView.java
+++ src/main/java/ladder/view/OutputView.java
@@ -3,13 +3,19 @@ package ladder.view;
 import java.util.List;
 
 public class OutputView {
-    public static void printLadder(List<String> ladder) {
+    public static void printLadder(List<String> ladder, String names, String resultCandidate) {
+        printNames(names);
         for (String s : ladder) {
             System.out.println(s);
         }
+        printResultCandidate(resultCandidate);
     }
 
-    public static void printNames(String names) {
+    private static void printNames(String names) {
         System.out.println(names);
+    }
+
+    private static void printResultCandidate(String resultCandidate) {
+        System.out.println(resultCandidate);
     }
 }
