14cff9fd717dba0d02485589db791832e3232876
Seongmo Jeon <oeeen3@gmail.com>
2019-05-16 12:07:57 +0900

    refactor: randomPoint 메소드 분리

--- src/main/java/ladder/domain/LadderMaker.java
+++ src/main/java/ladder/domain/LadderMaker.java
@@ -5,19 +5,41 @@ import java.util.List;
 import java.util.Random;
 
 public class LadderMaker {
-    private static boolean randomPoint(List<Boolean> points, int countOfPerson) {
+
+    public static List<Line> generateLadder(int height, int countOfPerson) {
+        List<Line> ladder = new ArrayList<>();
+        for (int i = 0; i < height; i++) {
+            ladder.add(new Line(generateAllPoints(countOfPerson)));
+        }
+        return ladder;
+    }
+
+    static boolean randomPoint(List<Boolean> points, int countOfPerson) {
         Random random = new Random();
-        if (countOfPerson == 1 || points.size() == 0 ||(points.size() != 0 && points.get(points.size() - 1))) {
+        if (isOnePerson(countOfPerson) || isStartEnd(points, countOfPerson) || isFormerFalse(points)) {
             return false;
         }
         return random.nextBoolean();
     }
 
-    public static List<Boolean> generateAllPoints(int countOfPerson) {
+    private static boolean isOnePerson(int countOfPerson) {
+        return countOfPerson == 1;
+    }
+
+    private static boolean isStartEnd(List<Boolean> points, int countOfPerson) {
+        return (points.size() == 0 || points.size() == countOfPerson);
+    }
+
+    private static boolean isFormerFalse(List<Boolean> points) {
+        return (points.size() != 0 && points.get(points.size() - 1));
+    }
+
+    private static List<Boolean> generateAllPoints(int countOfPerson) {
         List<Boolean> points = new ArrayList<>();
         for (int i = 0; i < countOfPerson; i++) {
             points.add(randomPoint(points, countOfPerson));
         }
+        points.add(false);
         return points;
     }
 }
