2f3c375b02a670d3b9df428bfb081cf486a31af0
yejilee <yjll1019@naver.com>
2019-05-16 11:46:33 +0900

    Refactor: modify test method name-ValidatorTest

--- src/test/java/ladder/ValidatorTest.java
+++ src/test/java/ladder/ValidatorTest.java
@@ -9,22 +9,22 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ValidatorTest {
 	@Test
-	void 글자수_5자_초과인_값이_없을_경우_True_반환() {
+	void 글자수_5자_초과인_값이_없을_경우() {
 		assertDoesNotThrow(() -> Validator.checkNamesLength(Arrays.asList("pobi", "crong")));
 	}
 
 	@Test
-	void 글자수_5자_초과인_값이_있을_경우_False_반환() {
+	void 글자수_5자_초과인_값이_있을_경우_예외_반환() {
 		assertThrows(IllegalArgumentException.class, () -> Validator.checkNamesLength(Arrays.asList("pobicrong", "crong")));
 	}
 
 	@Test
-	void 사다리_높이값이_숫자인_경우_True_반횐() {
+	void 사다리_높이값이_숫자인_경우() {
 		assertDoesNotThrow(() -> Validator.checkLadderHeight("19"));
 	}
 
 	@Test
-	void 사다리_높이값이_숫자가_아닌_경우_False_반환() {
+	void 사다리_높이값이_숫자가_아닌_경우_예외_반환() {
 		assertThrows(NumberFormatException.class, () -> Validator.checkLadderHeight("a"));
 	}
 }
