216567026c2854edd03b94e20f9a58a28deea78d
yejilee <yjll1019@naver.com>
2019-05-16 10:36:59 +0900

    Refactor: generate RandomGeneratorTest

--- src/test/java/ladder/domain/RandomGeneratorTest.java
+++ src/test/java/ladder/domain/RandomGeneratorTest.java
@@ -1,6 +1,5 @@
 package ladder.domain;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -9,17 +8,10 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class LineTest {
-    private Line line;
-
-    @BeforeEach
-    void setUp() {
-        line = new Line(5);
-    }
-
+public class RandomGeneratorTest {
     @Test
     void 이전_값이_True이면_False_반환() {
-        assertFalse(line.generatePoints(true));
+        assertFalse(RandomGenerator.getRandomBoolean(true));
     }
 
     @Test
@@ -27,7 +19,7 @@ public class LineTest {
         List<Boolean> booleans = new ArrayList<>();
 
         for (int i = 0; i < 100; ++i) {
-            booleans.add(line.generatePoints(false));
+            booleans.add(RandomGenerator.getRandomBoolean(false));
         }
 
         assertTrue((booleans.contains(true)) && (booleans.contains(false)));
