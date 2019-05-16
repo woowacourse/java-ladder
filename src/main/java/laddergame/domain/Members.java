d155b0a00deb0480c4b46819642ee1e07bbb9fff
yk1028 <wongeunsong94@gmail.com>
2019-05-16 17:11:02 +0900

    refactor: Person 클래스 삭제
    글자수 검사는 Members에게 위임

--- src/main/java/laddergame/domain/Members.java
+++ src/main/java/laddergame/domain/Members.java
@@ -3,28 +3,34 @@ package laddergame.domain;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 public class Members {
-    private List<Person> people;
+    private static final int NAME_LENGTH_BOUND = 5;
+
+    private List<String> people;
 
     public Members(List<String> names) {
         if (hasDuplicateName(names)) {
             throw new IllegalArgumentException("중복된 이름은 불가능합니다.");
         }
-        this.people = names.stream()
-                .map(Person::new)
-                .collect(Collectors.toList());
+        for (String name : names) {
+            checkNameLengthBound(name);
+        }
+        this.people = names;
     }
 
     public List<String> getNames() {
-        return people.stream()
-                .map(Person::getName)
-                .collect(Collectors.toList());
+        return people;
     }
 
     private boolean hasDuplicateName(List<String> names) {
         Set<String> nonDuplicateNames = new HashSet<>(names);
         return names.size() != nonDuplicateNames.size();
+    }
+
+    private void checkNameLengthBound(String name) {
+        if (name.length() > NAME_LENGTH_BOUND) {
+            throw new IllegalArgumentException("이름은 5자 이내여야 합니다.");
+        }
     }
 }
