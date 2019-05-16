e5f7e15b692b8d2eb37b50f2ebe501906b57c917
je941004 <je941004@naver.com>
2019-05-16 11:14:12 +0900

    Update create line logic

--- src/main/java/ladder/domain/Line.java
+++ src/main/java/ladder/domain/Line.java
@@ -1,39 +1,52 @@
 package ladder.domain;
 
+import java.util.Random;
+
 public class Line {
     private static final int RANDOM_FACTOR = 2;
     private static final int BEFORE = 1;
-    private static final int TRUE = 1;
-    private static final int COUNT_CORRECTION = 1;
-    private static final boolean NON_LINE = false;
+    private static final int NONE = 0;
+    private static final int RIGHT = 1;
+    private static final int LEFT = 2;
 
-    private boolean[] points;
+    private int[] points;
 
     public Line(int countOfPerson) {
-        points = new boolean[countOfPerson - COUNT_CORRECTION];
+        points = new int[countOfPerson];
 
-        points[0] = makeRandomBool();
+        points[0] = getRandomLineComponent();
         for (int i = 1; i < points.length; i++) {
-            points[i] = compareBeforeBool(i);
+            points[i] = compareBeforeComponent(i);
+
         }
     }
 
-    private boolean compareBeforeBool(int index) {
-        if (isFalse(points[index - BEFORE])) {
-            return makeRandomBool();
+    private int compareBeforeComponent(int index) {
+        if (index == points.length - 1) {
+            return compareLastComponent(index);
         }
-        return NON_LINE;
+        return compareNotLastComponent(points[index - BEFORE]);
     }
 
-    private boolean isFalse(boolean beforeValue) {
-        return !beforeValue;
+    private int compareNotLastComponent(int point) {
+        if (point == RIGHT) {
+            return LEFT;
+        }
+        return getRandomLineComponent();
     }
 
-    private boolean makeRandomBool() {
-        return (int) (Math.random() * RANDOM_FACTOR) == TRUE;
+    private int compareLastComponent(int index) {
+        if (points[index - BEFORE] == RIGHT) {
+            return LEFT;
+        }
+        return NONE;
     }
 
-    public boolean[] getPoints() {
+    private int getRandomLineComponent() {
+        return new Random().nextInt(RANDOM_FACTOR);
+    }
+
+    public int[] getPoints() {
         return points;
     }
 }
