103d57058959f7526b4856d47f0cf8886ac96029
ep1stas1s <masloz@konkuk.ac.kr>
2019-05-17 21:56:28 +0900

    Add test case

--- src/test/java/ladder/domain/PlayerTest.java
+++ src/test/java/ladder/domain/PlayerTest.java
@@ -2,14 +2,25 @@ package ladder.domain;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class PlayerTest {
 
     @Test
     void 이름이_5글자_초과_일_때() {
-        assertThrows(IllegalArgumentException.class, () -> {
-            new Player("pobipobi");
-        });
+        assertThatThrownBy(() -> new Player("pobipobi"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 이름이_null_일_경우() {
+        assertThatThrownBy(() -> new Player(null))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 이름이_빈_값일_경우() {
+        assertThatThrownBy(() -> new Player(""))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
