b18c3ba152ab7799128adfe35e83ca9e6ddf3e32
starkim06 <starkim06@naver.com>
2019-05-17 16:27:33 +0900

    feat: GamePlayers.class 이름 중복 체크 추가

--- src/main/java/ladder/domain/GamePlayers.java
+++ src/main/java/ladder/domain/GamePlayers.java
@@ -1,19 +1,33 @@
 package ladder.domain;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public final class GamePlayers {
     private final List<Player> players;
 
     public GamePlayers(final List<Player> players) {
-        validateSize(players);
+        validate(players);
         this.players = new ArrayList<>(players);
+    }
+
+    private void validate(List<Player> players) {
+        validateSize(players);
+        validateDuplication(players);
     }
 
     private void validateSize(List<Player> players) {
         if (players.isEmpty()) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("두 명 이상 입력 해주세요.");
+        }
+    }
+
+    private void validateDuplication(List<Player> players) {
+        Set<Player> set = new HashSet<>(players);
+        if (set.size() != players.size()) {
+            throw new IllegalArgumentException("이름 중복은 안됩니다.");
         }
     }
 
@@ -25,9 +39,11 @@ public final class GamePlayers {
         }
         return -1;
     }
-    public String getPlayerName(int index){
+
+    public String getPlayerName(int index) {
         return players.get(index).getName();
     }
+
     public int size() {
         return players.size();
     }
--- src/test/java/ladder/domain/GamePlayersTest.java
+++ src/test/java/ladder/domain/GamePlayersTest.java
@@ -3,6 +3,7 @@ package ladder.domain;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -12,6 +13,14 @@ public class GamePlayersTest {
     void 사이즈0_체크() {
         List<Player> players = new ArrayList<>();
         assertThrows(IllegalArgumentException.class, () -> {
+            GamePlayers gamePlayers = new GamePlayers(players);
+        });
+    }
+
+    @Test
+    void 이름_중복_체크() {
+        List<Player> players = Arrays.asList(new Player("뚱이"), new Player("뚱이"));
+        assertThrows(IllegalArgumentException.class, ()->{
             GamePlayers gamePlayers = new GamePlayers(players);
         });
     }
