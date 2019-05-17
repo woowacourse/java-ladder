a36d5946c381232fbe5aeb77ca2f71addad49ef6
yejilee <yjll1019@naver.com>
2019-05-17 17:48:44 +0900

    Refactor : modify test method name

--- src/test/java/ladder/util/StringSplitUtilsTest.java
+++ src/test/java/ladder/util/StringSplitUtilsTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class StringSplitUtilsTest {
-
 	@Test
 	void 쉼표로_문자열_구분하기() {
 		assertThat(StringSplitUtils.splitString("pobi,honux,crong,jk")).isEqualTo(Arrays.asList("pobi", "honux", "crong", "jk"));
