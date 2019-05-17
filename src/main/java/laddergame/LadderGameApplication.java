4a23dcf31955ee8192b771ebaec0eb90a083add5
huisam <huisam@naver.com>
2019-05-17 17:27:44 +0900

    refactor: 전체 로직 변경에 따른 application 수정

--- src/main/java/laddergame/LadderGameApplication.java
+++ src/main/java/laddergame/LadderGameApplication.java
@@ -1,23 +1,18 @@
 package laddergame;
 
+import laddergame.controller.LadderGame;
+import laddergame.controller.LadderGameResult;
 import laddergame.controller.Maker;
-import laddergame.domain.*;
-import laddergame.domain.rule.RandomCreateRule;
-import laddergame.domain.rule.Rule;
+import laddergame.controller.rule.RandomCreateRule;
 import laddergame.util.InputView;
 import laddergame.util.OutputView;
 
 public class LadderGameApplication {
     public static void main(String[] args) {
-        Tags members = Maker.makeMembers();
-        Tags prizes = Maker.makePrizes(members.size());
-        int height = InputView.inputHeight();
-        Rule rule = new RandomCreateRule();
-        Ladder ladder = LadderGenerator.generateLadder(height, members.size(), rule);
-        LadderGameResult ladderGameResult = LadderGame.startGame(members, ladder, prizes);
+        LadderGame ladderGame = new LadderGame(Maker.makeMembers(), new RandomCreateRule());
 
-        OutputView.outputLadderGame(members, ladder, prizes);
-        String person = InputView.inputWantResult();
-        OutputView.outputLadderGameResult(person, ladderGameResult);
+        LadderGameResult ladderGameResult = ladderGame.startGame();
+
+        OutputView.outputLadderGameResult(InputView.inputWantResult(), ladderGameResult);
     }
 }
