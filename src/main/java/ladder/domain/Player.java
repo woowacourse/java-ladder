c5ddf09afec76029279470473ee66272862109e7
SunHo Van <vsh123@naver.com>
2019-05-17 13:02:14 +0900

    feat : 각 플레이어가 사다리를 내려간 결과를 반환하는 기능을 구현
     1. 플레이어는 내려간 결과와 본인의 이름을 Map으로 반환합니다.

--- src/main/java/ladder/domain/Player.java
+++ src/main/java/ladder/domain/Player.java
@@ -1,16 +1,23 @@
 package ladder.domain;
 
-import java.util.Objects;
+import java.util.*;
 
 public class Player {
     private static final int MAXIMUM_NAME_LENGTH = 5;
     private static final int MINIMUM_NANE_LENGTH = 1;
     private static final String SHOW_ALL_COMMAND = "all";
     private final String name;
+    private int position;
 
     public Player(final String name) {
         nullCheck(name);
         this.name = getTrimed(name);
+    }
+
+    public Player(String name, int position) {
+        nullCheck(name);
+        this.name = getTrimed(name);
+        this.position = position;
     }
 
     private void nullCheck(String name) {
@@ -38,6 +45,14 @@ public class Player {
         }
     }
 
+    public Map<String, ResultItem> stepDown(Ladder ladder) {
+        Map<String, ResultItem> ladderingResult = new LinkedHashMap<>();
+        ResultItem resultItem = ladder.answerResult(position);
+
+        ladderingResult.put(name, resultItem);
+        return ladderingResult;
+    }
+
     @Override
     public String toString() {
         return this.name;
@@ -55,4 +70,5 @@ public class Player {
     public int hashCode() {
         return Objects.hash(name);
     }
+
 }
