15fd36b3a95317b969f37ef3e53f6fc850a385bc
pkch93 <star_final@naver.com>
2019-05-17 14:53:22 +0900

    feat endResult 구현
    - 사다리타기 게임의 최종 결과를 가지는 endResult 구현

---
+++ src/main/java/ladder/model/EndResult.java
@@ -0,0 +1,27 @@
+package ladder.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EndResult {
+    private final List<Result> results;
+
+    public EndResult(List<Result> results) {
+        this.results = results;
+    }
+
+    public Result getMemberResult(String member) {
+        for (Result result : results) {
+            if (result.getWinner().equals(member)) {
+                return result;
+            }
+        }
+        throw new IllegalArgumentException();
+    }
+
+    public List<Result> getAllResult() {
+        List<Result> allResult = new ArrayList<>();
+        allResult.addAll(results);
+        return allResult;
+    }
+}
