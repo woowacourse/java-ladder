2d510e41a40c2ecd3fccb8bb522e40bec4daf899
yejilee <yjll1019@naver.com>
2019-05-17 17:48:23 +0900

    Refactor : add Test method(LadderTest class, LineTest class)

--- src/test/java/ladder/domain/LadderTest.java
+++ src/test/java/ladder/domain/LadderTest.java
@@ -1,24 +1,39 @@
 package ladder.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 class LadderTest {
+	private Ladder ladder;
+	private int countOfNumber = 5;
 
-	@Test
-	void 사다리_생성자에_올바른_값이_입력된_경우() {
-		assertDoesNotThrow(() -> new Ladder(5,6));
+	@BeforeEach
+	void init() {
+		List<Boolean> points = new ArrayList<>();
+		for (int i = 0; i < countOfNumber; ++i) {
+			points.add((i % 2 == 0) ? true : false);
+		}
+
+		List<Line> lines = new ArrayList<>();
+		for (int i = 0; i < 3; ++i) {
+			lines.add(new Line(points));
+		}
+
+		ladder = new Ladder(lines);
 	}
 
 	@Test
-	void 사다리_생성자에_참가자_수가_음수인_경우() {
-		assertThrows(IllegalArgumentException.class, () -> new Ladder(-1,5));
+	void 참가자의_처음_위치에_따라_마지막_위치_반환() {
+		int[] playerLastPosition = {1, 0, 3, 2, 5};
+
+		for (int i = 0; i < countOfNumber; ++i) {
+			assertEquals(ladder.getLastPosition(i), playerLastPosition[i]);
+		}
 	}
 
-	@Test
-	void 사다리_생성자에_사다리의_높이가_음수인_경우() {
-		assertThrows(IllegalArgumentException.class, () -> new Ladder(5,-1 ));
-	}
 }
--- src/test/java/ladder/domain/LineTest.java
+++ src/test/java/ladder/domain/LineTest.java
@@ -10,13 +10,13 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class LineTest {
 	@Test
-	void 잘못된_생성자_인자가_입력된_경우() {
+	void 생성자에_잘못된_값이_입력된_경우() {
 		List<Boolean> points = Arrays.asList(true, true, false);
 		assertThrows(IllegalArgumentException.class, () -> new Line(points));
 	}
 
 	@Test
-	void 올바른_생성자_인자가_입력된_경우() {
+	void 생성자에_올바른_값이_입력된_경우() {
 		List<Boolean> points = Arrays.asList(false, true, false);
 		assertDoesNotThrow(() -> new Line(points));
 	}
