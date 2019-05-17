b4eb8e0cf6e65274001e003cca2a3660afcd0b13
kangmin <kangmin789@naver.com>
2019-05-17 16:52:13 +0900

    [추가] PartGenerator class,  PartGeneratorTest class 추가
    PartGenerator class는 모든 부품들을 다 생성하는 역할을 담당하는 static 메서드 만 존재하는 클래스 입니다.

---
+++ src/test/java/com/woowacourse/ladder/domain/PartGeneratorTest.java
@@ -0,0 +1,54 @@
+package com.woowacourse.ladder.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PartGeneratorTest {
+    List<Boolean> bools;
+
+    @BeforeEach
+    void setUp() {
+        bools = Arrays.asList(true, false, true, false);
+    }
+
+    @Test
+    void Generate_Point메소드_테스트() {
+        assertThat(PartGenerator.generatePoint(3)).isEqualTo(new Point(3, 1000));
+    }
+
+    @Test
+    void Generate_Points메소드_테스트() {
+        List<Point> points = PartGenerator.generatePoints(bools);
+        for (int i = 0; i < points.size(); i++) {
+            assertThat(points.get(i)).isEqualTo(new Point(i, 1000));
+        }
+    }
+
+    @Test
+    void Generate_Directions메소드_테스트() {
+        List<Direction> directions = PartGenerator.generateDirections(bools);
+        assertThat(directions.get(0)).isEqualTo(Direction.first(bools.get(0)));
+        for (int i = 1; i < directions.size() - 1; i++) {
+            assertThat(directions.get(i)).isEqualTo(Direction.middle(bools.get(i - 1), bools.get(i)));
+        }
+        assertThat(directions.get(directions.size()-1)).isEqualTo(Direction.last(bools.get(bools.size() - 1)));
+    }
+
+    @Test
+    void Generate_Line메소드_테스트() {
+        List<Point> points = PartGenerator.generatePoints(bools);
+        List<Direction> directions = PartGenerator.generateDirections(bools);
+        PartGenerator.generateLine(bools,points,directions);
+    }
+
+    @Test
+    void Generate_BoolList_테스트() {
+        List<Boolean> bools2 = Arrays.asList(true, false, true, false);
+        assertThat(PartGenerator.generateBoolList(5)).isEqualTo(bools2);
+    }
+}
