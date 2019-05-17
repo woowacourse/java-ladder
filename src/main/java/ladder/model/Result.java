86b7b1ed603872243aeedae5206777267706f16e
pkch93 <star_final@naver.com>
2019-05-17 14:55:06 +0900

    feat Result 구현
    - 사다리타기의 결과로 해당 상품과 당첨자를 가지는 객체 Result 구현

---
+++ src/main/java/ladder/model/Result.java
@@ -0,0 +1,36 @@
+package ladder.model;
+
+import java.util.Objects;
+
+public class Result {
+    private final String result;
+    private final String winner;
+
+    public Result(String result, String winner) {
+        this.result = result;
+        this.winner = winner;
+    }
+
+    public String getWinner(){
+        return winner;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Result result1 = (Result) o;
+        return Objects.equals(result, result1.result) &&
+                Objects.equals(winner, result1.winner);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(result, winner);
+    }
+
+    @Override
+    public String toString() {
+        return winner + " : " + result;
+    }
+}
