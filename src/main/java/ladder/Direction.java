643dab13f33aa98816dc0d08f77c2c9feb04d8bf
ddu0422 <ddu0422@naver.com>
2019-05-18 18:17:47 +0900

    각 라인 마다 방향 정하기

---
+++ src/main/java/ladder/Direction.java
@@ -0,0 +1,61 @@
+package ladder;
+
+import java.util.Objects;
+
+public class Direction {
+    private final boolean hasLeft;
+    private final boolean hasRight;
+
+    public Direction(boolean hasLeft, boolean hasRight) {
+        checkDirection(hasLeft, hasRight);
+        this.hasLeft = hasLeft;
+        this.hasRight = hasRight;
+    }
+
+    private void checkDirection(boolean hasLeft, boolean hasRight) {
+        if (hasLeft && hasRight) {
+            throw new IllegalArgumentException("방향을 두 개 이상 가질 수 없습니다.");
+        }
+    }
+
+    public static Direction first(boolean hasRight) {
+        return new Direction(false, hasRight);
+    }
+
+    public Direction next(boolean hasNext) {
+        if (hasRight) {
+            return of(true, false);
+        }
+        return of(false, hasNext);
+    }
+
+    public static Direction of(boolean hasLeft, boolean hasRight) {
+        return new Direction(hasLeft, hasRight);
+    }
+
+    public static Direction last(boolean hasLeft) {
+        return new Direction(hasLeft, false);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Direction direction = (Direction) o;
+        return hasLeft == direction.hasLeft &&
+                hasRight == direction.hasRight;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(hasLeft, hasRight);
+    }
+
+    @Override
+    public String toString() {
+        return "Direction{" +
+                "hasLeft=" + hasLeft +
+                ", hasRight=" + hasRight +
+                '}';
+    }
+}
---
+++ src/test/java/ladder/DirectionTest.java
@@ -0,0 +1,63 @@
+package ladder;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class DirectionTest {
+    private static boolean TRUE = true;
+    private static boolean FALSE = false;
+
+    @Test
+    void 각_포지션에서_방향_확인() {
+        Direction direction = new Direction(FALSE, TRUE);
+        assertThat(direction).isEqualTo(new Direction(FALSE, TRUE));
+    }
+
+    @Test
+    void 방향이_한개가_아닌_경우() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Direction(TRUE, TRUE);
+        });
+    }
+
+    @Test
+    void 첫번째_라인에서_생성되는_경우() {
+        assertThat(Direction.first(TRUE)).isEqualTo(new Direction(FALSE, TRUE));
+        assertThat(Direction.first(FALSE)).isEqualTo(new Direction(FALSE, FALSE));
+    }
+
+    @Test
+    void 마지막_라인에서_생성되는_경우() {
+        assertThat(Direction.last(TRUE)).isEqualTo(new Direction(TRUE, FALSE));
+        assertThat(Direction.last(FALSE)).isEqualTo(new Direction(FALSE, FALSE));
+    }
+
+    @Test
+    void 이전_방향이_오른쪽인_경우_다음_방향_생성() {
+        Direction direction = Direction.of(FALSE, TRUE).next(TRUE);
+        assertThat(direction).isEqualTo(new Direction(TRUE, FALSE));
+
+        direction = Direction.of(FALSE, TRUE).next(FALSE);
+        assertThat(direction).isEqualTo(new Direction(TRUE, FALSE));
+    }
+
+    @Test
+    void 이전_방향이_왼쪽인_경우_다음_방향_생성() {
+        Direction direction = Direction.of(TRUE, FALSE).next(TRUE);
+        assertThat(direction).isEqualTo(new Direction(FALSE, TRUE));
+
+        direction = Direction.of(TRUE, FALSE).next(FALSE);
+        assertThat(direction).isEqualTo(new Direction(FALSE, FALSE));
+    }
+
+    @Test
+    void 이전_방향이_존재하지_않는_경우_다음_방향_생성() {
+        Direction direction = Direction.of(FALSE, FALSE).next(TRUE);
+        assertThat(direction).isEqualTo(new Direction(FALSE, TRUE));
+
+        direction = Direction.of(FALSE, FALSE).next(FALSE);
+        assertThat(direction).isEqualTo(new Direction(FALSE, FALSE));
+    }
+}
