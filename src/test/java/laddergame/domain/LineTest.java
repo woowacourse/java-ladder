c82f8cffdf22fb39b8bf326e862aa10b48404cc9
yk1028 <wongeunsong94@gmail.com>
2019-05-18 16:45:43 +0900

    refactor: 테스트 케이스 세분화

--- src/test/java/laddergame/domain/LineTest.java
+++ src/test/java/laddergame/domain/LineTest.java
@@ -1,5 +1,6 @@
 package laddergame.domain;
 
+import laddergame.domain.rule.AlwaysCreateRule;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -9,42 +10,52 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class LineTest {
     @Test
-    void 발판을_놓을수_있는지_테스트() {
-        Line line = new Line(Arrays.asList(false, false, true, false));
-        Line line2 = new Line(Arrays.asList(false, false, false));
-        Line line3 = new Line(Arrays.asList(false, true, false, false));
-
-        assertThat(line.canAddScaffold(1)).isFalse();
-        assertThat(line2.canAddScaffold(1)).isTrue();
-        assertThat(line3.canAddScaffold(2)).isFalse();
-    }
-
-    @Test
-    void 발판이_제대로_놓아지는지_테스트() {
-        Line line = new Line(Arrays.asList(false, false, false));
-        final int interval = 1;
-
-        line.addScaffold(interval);
+    void 라인이_제대로_생성되는지_테스트() {
+        Line line = new Line(2, new AlwaysCreateRule());
 
         assertThat(line).isEqualTo(new Line(Arrays.asList(false, true, false)));
     }
 
     @Test
-    void 빈_발판이_올바르게_생성되는지_테스트() {
-        Line line = new Line(Arrays.asList(false, false, false));
+    void 왼쪽에_발판이_있는_경우_발판_생성_불가() {
+        Line line = new Line(Arrays.asList(false, true, false, false));
 
-        assertThat(new Line(2)).isEqualTo(line);
+        assertThat(line.canAddScaffold(2)).isFalse();
     }
 
     @Test
-    void 제대로_이동되는지_테스트() {
-        Line line1 = new Line(Arrays.asList(false, true, false));
-        Line line2 = new Line(Arrays.asList(false, false, false));
+    void 오른쪽에_발판이_있는_경우_발판_생성_불가() {
+        Line line = new Line(Arrays.asList(false, false, true, false));
 
-        assertThat(line1.moveNextPoint(0)).isEqualTo(1);
-        assertThat(line1.moveNextPoint(1)).isEqualTo(0);
-        assertThat(line2.moveNextPoint(0)).isEqualTo(0);
-        assertThat(line2.moveNextPoint(1)).isEqualTo(1);
+        assertThat(line.canAddScaffold(1)).isFalse();
+    }
+
+    @Test
+    void 양쪽에_발판이_없는_경우_발판_생성_불가() {
+        Line line = new Line(Arrays.asList(false, false, false, false));
+
+        assertThat(line.canAddScaffold(1)).isTrue();
+    }
+
+    @Test
+    void 왼쪽에서_오른쪽으로_이동하는_경우() {
+        Line line = new Line(Arrays.asList(false, true, false));
+
+        assertThat(line.moveNextPoint(0)).isEqualTo(1);
+    }
+
+    @Test
+    void 오른쪽에서_왼쪽으로_이동하는_경우() {
+        Line line = new Line(Arrays.asList(false, true, false));
+
+        assertThat(line.moveNextPoint(1)).isEqualTo(0);
+    }
+
+    @Test
+    void 직진하는_경우() {
+        Line line = new Line(Arrays.asList(false, false, false));
+
+        assertThat(line.moveNextPoint(0)).isEqualTo(0);
     }
 
     @Test
