752b8e9d454ad1a3230f35b536f6f2871568216d
ep1stas1s <masloz@konkuk.ac.kr>
2019-05-19 01:38:35 +0900

    [ADD] Ladder class

---
+++ src/main/java/ladder/domain/Ladder.java
@@ -0,0 +1,23 @@
+package ladder.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ladder {
+    private List<Line> lines;
+
+    public Ladder(int numberOfPlayers, int height) {
+        lines = new ArrayList<>();
+        addLines(numberOfPlayers, height);
+    }
+
+    private void addLines(int numberOfPlayers, int height) {
+        for (int i = 0; i < height; i++) {
+            lines.add(new Line(numberOfPlayers));
+        }
+    }
+
+    public List<Line> getLines() {
+        return lines;
+    }
+}
---
+++ src/test/java/ladder/domain/LadderTest.java
@@ -0,0 +1,25 @@
+package ladder.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LadderTest {
+    private Ladder ladder;
+    @BeforeEach
+    void setUp() {
+        ladder = new Ladder(3, 3);
+    }
+
+    @Test
+    void 생성되는_line_수() {
+        assertThat(ladder.getLines().size()).isEqualTo(3);
+    }
+
+    @Test
+    void 생성된_line의_Direction수() {
+        Line line = ladder.getLines().get(0);
+        assertThat(line.getDirections().size()).isEqualTo(3);
+    }
+}
