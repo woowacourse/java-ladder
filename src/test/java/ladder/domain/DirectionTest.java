91fe549c6fb7a252cfda9483948360fc0511d405
ep1stas1s <masloz@konkuk.ac.kr>
2019-05-19 01:54:39 +0900

    [ADD] Test case

--- src/test/java/ladder/domain/DirectionTest.java
+++ src/test/java/ladder/domain/DirectionTest.java
@@ -7,12 +7,22 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class DirectionTest {
     @Test
-    void valueOf() {
+    void valueOf_직진() {
         assertThat(Direction.valueOf(0)).isEqualTo(Direction.STRAIGHT);
     }
 
     @Test
-    void 존재하지_않는_value() {
-        assertThatThrownBy(() -> Direction.valueOf(-1)).isInstanceOf(IllegalArgumentException.class);
+    void valueOf_오른쪽() {
+        assertThat(Direction.valueOf(1)).isEqualTo(Direction.RIGHT);
+    }
+
+    @Test
+    void valueOf_왼쪽() {
+        assertThat(Direction.valueOf(-1)).isEqualTo(Direction.LEFT);
+    }
+
+    @Test
+    void 존재하지_않는_valueOf() {
+        assertThatThrownBy(() -> Direction.valueOf(2)).isInstanceOf(IllegalArgumentException.class);
     }
 }
