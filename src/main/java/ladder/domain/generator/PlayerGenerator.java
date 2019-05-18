0f86542781b3bfbcb3ba0ab4df66ba75b26a1179
DaejunBae <dpudpu11@gmail.com>
2019-05-18 21:15:16 +0900

    fix: PlayerGeneartor 상수 접근제어자 때문에 테스트가 안되는 문제 해결

--- src/main/java/ladder/domain/generator/PlayerGenerator.java
+++ src/main/java/ladder/domain/generator/PlayerGenerator.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public final class PlayerGenerator {
-    private static final String DELIMITER = ",";
+    protected static final String DELIMITER = ",";
 
     private final String playerNames;
 
