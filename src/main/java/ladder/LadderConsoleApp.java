1b44dff4a0b4f2e5590a3585ded82858155e0417
DaejunBae <dpudpu11@gmail.com>
2019-05-18 17:58:15 +0900

    refactor: controller 삭제 후 main이 대신 기능하게 수행하게 수정

--- src/main/java/ladder/LadderConsoleApp.java
+++ src/main/java/ladder/LadderConsoleApp.java
@@ -1,10 +1,45 @@
 package ladder;
 
-import ladder.controller.LadderGameController;
+import ladder.domain.*;
+import ladder.domain.generator.PlayerGenerator;
+import ladder.domain.generator.PlayerRewardsGenerator;
+import ladder.view.InputConsoleView;
+import ladder.view.OutputConsoleView;
+
+import java.util.List;
 
 public class LadderConsoleApp {
     public static void main(String[] args) {
-        LadderGameController ladderGameController = new LadderGameController();
-        ladderGameController.run();
+        GamePlayers gamePlayers = new GamePlayers(generatePlayers(inputNames()));
+        PlayerRewards playerRewards = generateRewards(inputRewards());
+        Ladder ladder = new Ladder(inputHeight(), gamePlayers.size());
+        GameResult gameResult = new GameResult(ladder, gamePlayers, playerRewards);
+
+        OutputConsoleView.printLadderGame(ladder, gamePlayers, playerRewards);
+        String name;
+        while (!(name = InputConsoleView.inputResultName()).equals("all")) {
+            OutputConsoleView.printResult(gameResult, name);
+        }
+        OutputConsoleView.printResult(gameResult);
+    }
+
+    private static List<Player> generatePlayers(String names) {
+        return new PlayerGenerator(names).generate();
+    }
+
+    private static String inputNames() {
+        return InputConsoleView.inputNames();
+    }
+
+    private static int inputHeight() {
+        return InputConsoleView.inputHeight();
+    }
+
+    private static String inputRewards() {
+        return InputConsoleView.inputRewards();
+    }
+
+    private static PlayerRewards generateRewards(String results) {
+        return new PlayerRewardsGenerator(results).generate();
     }
 }
