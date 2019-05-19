df1b9d24901b9754dfad44129ab844755062aa28
DaejunBae <dpudpu11@gmail.com>
2019-05-19 15:07:07 +0900

    feat: DirectionGenerator.class 추가

---
+++ src/test/java/ladder/domain/generator/DirectionRandomGeneratorTest.java
@@ -0,0 +1,24 @@
+package ladder.domain.generator;
+
+import ladder.domain.Direction;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class DirectionRandomGeneratorTest {
+    List<Direction> actual;
+
+    @BeforeEach
+    void setUp() {
+         actual = new DirectionRandomGenerator(3).generate();
+    }
+
+    @Test
+    void size_검사() {
+        assertThat(actual.size()).isEqualTo(3);
+    }
+}
