db1efb7e6730eb6d96aa72d35448e4bffb1bb49a
ep1stas1s <masloz@konkuk.ac.kr>
2019-05-19 12:23:37 +0900

    [FIX] 오작동하는 method

--- src/main/java/ladder/domain/Line.java
+++ src/main/java/ladder/domain/Line.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class Line {
     private static final int RANDOM_RANGE = 2;
     private static final int ONE_PLAYER = 1;
+    private static final int BEFORE = 1;
 
     private final List<Direction> directions;
 
@@ -30,18 +31,41 @@ public class Line {
     private void generateRandomDirections(int numberOfPlayers) {
         directions.add(Direction.valueOf(generateRandomNumber()));
         for (int i = 1; i < numberOfPlayers; i++) {
-            Direction prev = directions.get(directions.size() - 1);
-            if (Direction.isRight(prev)) {
-                directions.add(Direction.LEFT);
-                continue;
-            }
-            directions.add(Direction.valueOf(generateRandomNumber()));
+            addDirection(numberOfPlayers);
         }
+    }
+
+    private void addDirection(int numberOfPlayers) {
+        Direction prev = directions.get(directions.size() - BEFORE);
+        if (Direction.isRight(prev)) {
+            directions.add(Direction.LEFT);
+            return;
+        }
+        if (directions.size() == numberOfPlayers - BEFORE){
+            directions.add(Direction.STRAIGHT);
+            return;
+        }
+        directions.add(Direction.valueOf(generateRandomNumber()));
     }
 
     private int generateRandomNumber() {
         return (int) (Math.random() * RANDOM_RANGE);
     }
+
+    /* TODO 할지 말지?
+    public static boolean isValidLine(Line line){
+        List<Direction> directions = line.getDirections();
+        for (int i = directions.size() - 1; i > 0; i--) {
+            Direction now = directions.get(i);
+            Direction prev = directions.get(i - 1);
+            if(now == Direction.LEFT){
+                if(prev != Direction.RIGHT){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }*/
 
     public List<Direction> getDirections() {
         return Collections.unmodifiableList(directions);
