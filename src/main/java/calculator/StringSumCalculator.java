9ad86af60c4bb80c17a2b6bf96d0de5e647a7fe8
huisam <huisam@naver.com>
2019-05-20 01:18:22 +0900

    refactor: 상수에게 의미를 부여하는 리팩토링

--- src/main/java/calculator/StringSumCalculator.java
+++ src/main/java/calculator/StringSumCalculator.java
@@ -1,26 +1,27 @@
 package calculator;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringSumCalculator {
+    private static final int CUSTOM = 1;
+    private static final int CALCULATE_SENTENCE = 2;
+
     public int calculateFormula(String formula) {
-        if (isBlank(formula)) {
+        if (StringUtils.isBlank(formula)) {
             return 0;
         }
         return sum(split(formula));
-    }
-
-    private boolean isBlank(String formula) {
-        return formula == null || formula.isEmpty();
     }
 
     private String[] split(String formula) {
         Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(formula);
 
         if (matcher.find()) {
-            return matcher.group(2).split(matcher.group(1));
+            return matcher.group(CALCULATE_SENTENCE).split(matcher.group(CUSTOM));
         }
         return formula.split("[,:]");
     }
@@ -35,7 +36,7 @@ public class StringSumCalculator {
         int num = Integer.parseInt(number);
 
         if (num < 0) {
-            throw new RuntimeException();
+            throw new RuntimeException("음수는 입력될 수 없습니다!");
         }
         return num;
     }
