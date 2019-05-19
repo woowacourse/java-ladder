14b251fec776f68362ba6a1acfb138e91ba7e898
DaejunBae <dpudpu11@gmail.com>
2019-05-19 16:39:52 +0900

    refactor: GameResult는 값만 가지고 있는 vo 역할로 수정

--- src/main/java/ladder/domain/GameResult.java
+++ src/main/java/ladder/domain/GameResult.java
@@ -6,23 +6,8 @@ import java.util.Map;
 public class GameResult {
     private final Map<String, String> results;
 
-    public GameResult(Ladder ladder, GamePlayers gamePlayers, PlayerRewards playerRewards) {
-        this.results = new HashMap<>();
-        init(ladder, gamePlayers, playerRewards);
-    }
-
-    private void init(Ladder ladder, GamePlayers gamePlayers, PlayerRewards playerRewards) {
-        validate(gamePlayers.size(), playerRewards.size());
-        for (int i = 0; i < gamePlayers.size(); i++) {
-            int result = ladder.moveLadder(i);
-            results.put(gamePlayers.getPlayerName(i), playerRewards.getReward(result));
-        }
-    }
-
-    private void validate(int countOfPlayers, int countOfRewards) {
-        if (countOfPlayers != countOfRewards) {
-            throw new IllegalArgumentException();
-        }
+    public GameResult(final Map<String, String> results) {
+        this.results = results;
     }
 
     public String get(String playerName) {
