4b5ae9a85b159086758cc1086d79e20e6d3ef295
deocksoo <dusol92@snu.ac.kr>
2019-05-20 07:44:40 +0900

    refactor : userSetCrossbarGeneratorTest의 이름 없는 메서드 명칭을 변경합니다

--- src/test/java/ladder/domain/UserSetCrossbarGeneratorTest.java
+++ src/test/java/ladder/domain/UserSetCrossbarGeneratorTest.java
@@ -9,7 +9,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class UserSetCrossbarGeneratorTest {
     @Test
-    void name() {
+    void User가_setting해놓은_사다리를_제대로_생성하는지_테스트() {
         List<Boolean> userSetCrossbars = Arrays.asList(false, true, false, true, false);
         UserSetCrossbarGenerator testGenerator = new UserSetCrossbarGenerator(userSetCrossbars);
 
