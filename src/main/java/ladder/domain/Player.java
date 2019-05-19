eba0a122d013fe66a6df9843fd9cdc84df755709
deocksoo <dusol92@snu.ac.kr>
2019-05-20 07:43:03 +0900

    refactor : Player 생성자의 내부 상수를 필드 상수 변수로 변경합니다.

--- src/main/java/ladder/domain/Player.java
+++ src/main/java/ladder/domain/Player.java
@@ -6,12 +6,13 @@ public class Player {
     private static final int MAXIMUM_NAME_LENGTH = 5;
     private static final int MINIMUM_NANE_LENGTH = 1;
     private static final String FORBIDDEN_NAME = "all";
+    private static final int DEFAULT_POSITION = 0;
 
     private final String name;
     private int position;
 
     Player(final String name) {
-        this(name, 0);
+        this(name, DEFAULT_POSITION);
     }
 
     Player(String name, int position) {
@@ -35,7 +36,7 @@ public class Player {
 
     private void validateCommandName(String name) {
         if (name.equals(FORBIDDEN_NAME)) {
-            throw new IllegalArgumentException("이름에는 all이 입력될 수 없습니다.");
+            throw new IllegalArgumentException("플레이어 이름에는 all이 입력될 수 없습니다.");
         }
     }
 
