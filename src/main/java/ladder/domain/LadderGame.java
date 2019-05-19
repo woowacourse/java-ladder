c420cba5a47dcb58de915c8e42cfdeb2bd07c7f6
ep1stas1s <masloz@konkuk.ac.kr>
2019-05-19 13:23:29 +0900

    [ADD] LadderGame, 사다리 게임을 진행하는 method

---
+++ src/main/java/ladder/domain/LadderGame.java
@@ -0,0 +1,40 @@
+package ladder.domain;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LadderGame {
+    private Ladder ladder;
+
+    public LadderGame(Ladder ladder) {
+        this.ladder = ladder;
+    }
+
+    public LadderGameResult play(Players players, Rewards rewards) {
+        checkNumberOfRewards(players, rewards);
+        return new LadderGameResult(calculateReward(players, rewards));
+    }
+
+    private Map<Player, Reward> calculateReward(Players players, Rewards rewards) {
+        Map<Player, Reward> result = new HashMap<>();
+        for (int startPoint = 0; startPoint < players.size(); startPoint++) {
+            int currentLocation = startPoint;
+            currentLocation = takeLadder(currentLocation);
+            result.put(players.get(startPoint), rewards.get(currentLocation));
+        }
+        return result;
+    }
+
+    private int takeLadder(int currentLocation) {
+        for (int j = 0; j < ladder.getHeight(); j++) {
+            currentLocation += ladder.get(j).get(currentLocation).getDirection();
+        }
+        return currentLocation;
+    }
+
+    private void checkNumberOfRewards(Players players, Rewards rewards) {
+        if (players.size() != rewards.size()) {
+            throw new IllegalArgumentException("Player 수와 reward 의 수가 같아야합니다.");
+        }
+    }
+}
---
+++ src/test/java/ladder/domain/LadderGameTest.java
@@ -0,0 +1,18 @@
+package ladder.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class LadderGameTest {
+    @Test
+    void player수와_reward수가_다른_경우() {
+        Players players = new Players(new String[]{"pobi", "denis"});
+        Rewards rewards = new Rewards(new String[]{"100", "200", "300"});
+        Ladder ladder = new Ladder(players.size(), 5);
+        LadderGame ladderGame = new LadderGame(ladder);
+
+        assertThatThrownBy(() -> ladderGame.play(players, rewards)).isInstanceOf(IllegalArgumentException.class);
+    }
+}
