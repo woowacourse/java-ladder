9d830441f6d9cf59e423ae380fe07d401fde8d13
ep1stas1s <masloz@konkuk.ac.kr>
2019-05-19 12:38:57 +0900

    [REF] method 분할 및 오타 수정

--- src/main/java/ladder/view/OutputView.java
+++ src/main/java/ladder/view/OutputView.java
@@ -10,17 +10,25 @@ public class OutputView {
     private static final String CONNECTED_LINE_COMPONENT = "|-----------";
     private static final String NOT_CONNECTED_LINE_COMPONENT = "|           ";
 
-    public static void printLadder(Ladder ladder){
-        for (Line line : ladder.getLines()){
-            for(Direction direction : line.getDirections()){
-                if(direction == Direction.RIGHT){
-                    System.out.print(CONNECTED_LINE_COMPONENT);
-                    continue;
-                }
-                System.out.print(NOT_CONNECTED_LINE_COMPONENT);
-            }
-            System.out.println();
+    public static void printLadder(Ladder ladder) {
+        for (Line line : ladder.getLines()) {
+            printLine(line);
         }
+    }
+
+    private static void printLine(Line line) {
+        for (Direction direction : line.getDirections()) {
+            printLineComponent(direction);
+        }
+        System.out.println();
+    }
+
+    private static void printLineComponent(Direction direction) {
+        if (Direction.isRight(direction)) {
+            System.out.print(CONNECTED_LINE_COMPONENT);
+            return;
+        }
+        System.out.print(NOT_CONNECTED_LINE_COMPONENT);
     }
 
     // TODO interface 적용해서 중복코드 줄여보기? (Players, Rewards)
@@ -32,9 +40,9 @@ public class OutputView {
     }
 
     public static void printRewards(Rewards rewards) {
-        String joinedPlayer = rewards.getRewards().stream()
+        String joinedReward = rewards.getRewards().stream()
                 .map(reward -> String.format(PRINT_FORMAT, reward.getReward()))
                 .collect(Collectors.joining(CONNECTOR));
-        System.out.println(joinedPlayer);
+        System.out.println(joinedReward);
     }
 }
