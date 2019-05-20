88024f7e6c359a7c7f96647a91dbb4ed5b412d21
Jo Jinsik <kunggom@gmail.com>
2019-05-20 14:12:38 +0900

    수정: Input view 리팩토링
    이름을 고치고, 중복되는 부분을 줄였다.

--- src/main/java/ladder/controller/App.java
+++ src/main/java/ladder/controller/App.java
@@ -1,13 +1,15 @@
 package ladder.controller;
 
 import ladder.model.Game;
-import ladder.view.InputView;
+import ladder.view.Input;
 import ladder.view.OutputView;
+
+// TODO: readme.md 기능목록 작성
 
 public class App {
     public static void main(String[] argc) {
-        Game game = new Game(InputView.inputNames(), InputView.inputRewards(), InputView.inputHeight());
+        Game game = new Game(Input.names(), Input.rewards(), Input.height());
         OutputView.printGame(game);
-        while(OutputView.printResult(game.getResultOf(InputView.inputCandidates())));
+        while(OutputView.printResult(game.getResultOf(Input.candidates())));
     }
 }
