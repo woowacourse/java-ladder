26cbbe9933e297cfdc0a7459a134b111d3d45421
stopsilver13 <stopsilver13>
2019-05-20 15:47:02 +0900

    [Review] Generate Players as a first class collection

---
+++ src/main/java/ladder/domain/Players.java
@@ -0,0 +1,14 @@
+package ladder.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Players {
+    private static final List<Player> players = new ArrayList<>();
+
+    public Players(List<PlayerName> names) {
+        for (int position = 0; position < names.size(); position++) {
+            players.add(new Player(names.get(position), position));
+        }
+    }
+}
