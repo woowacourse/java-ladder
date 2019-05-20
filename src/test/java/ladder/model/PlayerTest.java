b97367e6947d643f70bd51e0c5ad87c6b30c6346
Jo Jinsik <kunggom@gmail.com>
2019-05-20 19:15:38 +0900

    다듬기: PlayerTest
    테스트 프레임워크를 하나로 통일하였다.

--- src/test/java/ladder/model/PlayerTest.java
+++ src/test/java/ladder/model/PlayerTest.java
@@ -3,36 +3,46 @@ package ladder.model;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class PlayerTest {
     @Test
     void initTestA() {
-        assertThat(new Player("ABC", "123").toString()).isEqualTo("ABC : 123");
+        assertThat(new Player("ABC", "123")
+                .toString())
+                .isEqualTo("ABC : 123");
     }
 
     @Test
     void initTestB() {
-        assertThat(new Player("ABC ", "123").toString()).isEqualTo("ABC : 123");
+        assertThat(new Player("ABC ", "123")
+                .toString())
+                .isEqualTo("ABC : 123");
     }
 
     @Test
     void initTestC() {
-        assertThat(new Player("ABC", "12 ").toString()).isEqualTo("ABC : 12");
+        assertThat(new Player("ABC", "12 ")
+                .toString())
+                .isEqualTo("ABC : 12");
     }
 
     @Test
     void initTestD() {
-        assertThat(new Player(" ABC      ", "123 ").toString()).isEqualTo("ABC : 123");
+        assertThat(new Player(" ABC      ", "123 ")
+                .toString())
+                .isEqualTo("ABC : 123");
     }
 
     @Test
     void initTestE() {
-        assertThrows(Exception.class, () -> new Player("", "123 "));
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Player("", "123 "));
     }
 
     @Test
     void initTestF() {
-        assertThrows(Exception.class, () -> new Player("sdgaeGdfhasr", "123 "));
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Player("sdgaeGdfhasr", "123 "));
     }
 }
