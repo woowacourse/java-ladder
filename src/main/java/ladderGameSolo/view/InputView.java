7060e4dd0bbae6c1d864f790028b408de1d7695c
이영현 <leey9494@icloud.com>
2019-05-20 12:44:34 +0900

    [solo] [ADD] 높이 입력

--- src/main/java/ladderGameSolo/view/InputView.java
+++ src/main/java/ladderGameSolo/view/InputView.java
@@ -12,6 +12,7 @@ import java.util.regex.Pattern;
 public class InputView {
     private static final Scanner SCAN = new Scanner(System.in);
     private static final String REGEX_NAME = "^([a-zA-Zㄱ-ㅎ가-힣0-9]{1,5}){1}(,[a-zA-Zㄱ-ㅎ가-힣0-9]{1,5}){1,}$";
+    private static final String REGEX_HEIGHT = "([0-9]){1}";
 
     public static String inputName() {
         System.out.println(MessageContants.INPUT_NAME);
@@ -23,6 +24,18 @@ public class InputView {
         }
 
         return name;
+    }
+
+    public static int inputHeight() {
+        System.out.println(MessageContants.INPUT_HEIGHT);
+        String height = SCAN.nextLine();
+
+        if (!validHeight(height)) {
+            System.err.println(MessageContants.ERROR_INPUT);
+            return inputHeight();
+        }
+
+        return Integer.parseInt(height);
     }
 
     private static boolean validName(String name) {
@@ -42,5 +55,12 @@ public class InputView {
         }
 
         return false;
+    }
+
+    private static boolean validHeight(String height) {
+        Pattern pattern = Pattern.compile(REGEX_HEIGHT);
+        Matcher matcher = pattern.matcher(height);
+
+        return matcher.matches();
     }
 }
