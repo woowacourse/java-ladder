0ba13ed9142ef90b933183cd9dcfcb99f3869a81
Jo Jinsik <kunggom@gmail.com>
2019-05-20 19:07:22 +0900

    다듬기: LevelTest
    줄바꿈만 수정하였다.

--- src/test/java/ladder/model/LevelTest.java
+++ src/test/java/ladder/model/LevelTest.java
@@ -13,21 +13,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 class LevelTest {
     @Test
     void levelCreateTestNeverDraw() {
-        assertThat(new Level(4, new Never()).getLines()).isEqualTo(Arrays.asList());
+        assertThat(new Level(4, new Never()).getLines())
+                .isEqualTo(Arrays.asList());
     }
 
     @Test
     void levelCreateTestAlwaysDraw() {
-        assertThat(new Level(11, new Always()).getLines()).isEqualTo(Arrays.asList(0, 2, 4, 6, 8, 10));
+        assertThat(new Level(11, new Always()).getLines())
+                .isEqualTo(Arrays.asList(0, 2, 4, 6, 8, 10));
     }
 
     @Test
     void levelCreateTestDrawOdd() {
-        assertThat(new Level(13, new OddOnly()).getLines()).isEqualTo(Arrays.asList(0, 3, 6, 9, 12));
+        assertThat(new Level(13, new OddOnly()).getLines())
+                .isEqualTo(Arrays.asList(0, 3, 6, 9, 12));
     }
 
     @Test
     void levelCreateTestDrawEven() {
-        assertThat(new Level(23, new EvenOnly()).getLines()).isEqualTo(Arrays.asList(1, 4, 7, 10, 13, 16, 19, 22));
+        assertThat(new Level(23, new EvenOnly()).getLines())
+                .isEqualTo(Arrays.asList(1, 4, 7, 10, 13, 16, 19, 22));
     }
 }
