d3d840369f172e5df146ca9cd4b36513d4f6f155
Kwanwoo Kang <kkw200@naver.com>
2019-05-20 23:55:17 +0900

    refactor: result class

--- src/main/java/ladder/model/Result.java
+++ src/main/java/ladder/model/Result.java
@@ -5,36 +5,25 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Result implements Iterator {
-    private final List<String> players;
-    private final List<String> rewards;
+    private final Players players;
+    private final Rewards rewards;
+    private final List<Integer> result;
     private int index = 0;
 
     Result(Players players, Rewards rewards, Ladder ladder, List<String> query) {
-        final Rewards applied = ladder.apply(players, rewards);
-        if (query.contains("all")) {
-            this.players = Collections.unmodifiableList(players.getListOfPlayers());
-            this.rewards = Collections.unmodifiableList(applied.getListOfRewards());
-            return;
-        }
-        List<Integer> searchResult = IntStream.range(0, players.number()).boxed()
-                                            .filter(i -> query.contains(players.get(i)))
-                                            .collect(Collectors.toList());
-        this.players = Collections.unmodifiableList(
-                searchResult.stream()
-                            .map(i -> players.get(i))
-                            .collect(Collectors.toList())
-        );
-        this.rewards = Collections.unmodifiableList(
-                searchResult.stream()
-                            .map(i -> applied.get(i))
-                            .collect(Collectors.toList())
+        this.players = players;
+        this.rewards = ladder.apply(players, rewards);
+        this.result = Collections.unmodifiableList(
+                IntStream.range(0, players.number()).boxed()
+                .filter(i -> query.contains("all") || query.contains(players.get(i)))
+                .collect(Collectors.toList())
         );
     }
     public boolean hasNext() {
-        return index < players.size();
+        return index < result.size();
     }
 
     public String next() {
-        return players.get(index) + " : " + rewards.get(index++);
+        return players.get(result.get(index)) + " : " + rewards.get(result.get(index++));
     }
 }
