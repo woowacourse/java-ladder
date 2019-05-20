195f0af7f66d0a16344e06f3eed758fc0c8d2d44
Jo Jinsik <kunggom@gmail.com>
2019-05-20 19:50:37 +0900

    수정: README.md

--- src/main/java/ladder/model/Result.java
+++ src/main/java/ladder/model/Result.java
@@ -7,12 +7,13 @@ import java.util.stream.Collectors;
 public class Result implements Iterator {
     private final List<Player> result;
     private int index = 0;
+    private String ALL_PLAYER = "all";
 
     Result(List<Player> players, Ladder ladder, List<String> query) {
         result = ladder
                 .apply(players)
                 .stream()
-                .filter(x -> query.contains("all") || query.contains(x.getName()))
+                .filter(x -> query.contains(ALL_PLAYER) || query.contains(x.getName()))
                 .collect(Collectors.toList());
     }
     public boolean hasNext() {
