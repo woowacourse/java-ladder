cf52e5f1445aafc29a036cfa89d048902c17755b
SEOKHYEONCHOI <lkamna12@gmail.com>
2019-05-20 15:52:44 +0900

    [REFACTOR] 불필요한 필드변수 제거 및 validation 함수 수정
    저장할 필요가 없는 height를 필드에서 제거
    validation함수 내에서 exception throw 하도록 수정

--- src/main/java/ladder/model/ladder/Ladder.java
+++ src/main/java/ladder/model/ladder/Ladder.java
@@ -10,19 +10,17 @@ import java.util.stream.IntStream;
 public class Ladder {
     private static final int MIN_LADDER_HEIGHT = 1;
     private static final String NEW_LINE = "\n";
-    private final int height;
     private List<Line> lines = new ArrayList<>();
 
     public Ladder(LinePointsGenerator linePointsGenerator, int height) {
-        if (!this.isValidHeight(height)) {
-            throw new IllegalArgumentException("높이는 1 이상의 정수이어야 합니다.");
-        }
-        this.height = height;
+        validateHeight(height);
         IntStream.range(0, height).forEach(i -> lines.add(new Line(linePointsGenerator.generatePoints())));
     }
 
-    private boolean isValidHeight(int height) {
-        return height >= MIN_LADDER_HEIGHT;
+    private void validateHeight(int height) {
+        if(height < MIN_LADDER_HEIGHT){
+            throw new IllegalArgumentException("높이는 1 이상의 정수이어야 합니다.");
+        }
     }
 
     public void move(Players players) {
@@ -32,9 +30,8 @@ public class Ladder {
     @Override
     public String toString() {
         StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < height; i++) {
-            stringBuilder.append(lines.get(i)).append(NEW_LINE);
-        }
+        lines.stream().forEach(line -> stringBuilder.append(line).append(NEW_LINE));
+
         return stringBuilder.toString();
     }
 }
