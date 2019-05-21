300a74b5eef556491a0eab05c143b2d40ba93a5d
Seongmo Jeon <oeeen3@gmail.com>
2019-05-21 20:15:53 +0900

    refactor: "all" 검사 하는 부분 메소드 추출 분리

--- src/main/java/ladder/util/InputHelper.java
+++ src/main/java/ladder/util/InputHelper.java
@@ -5,8 +5,13 @@ import java.util.List;
 
 public class InputHelper {
     private static final String SPLITTER = ",";
+    private static final String ALL = "all";
 
     public static List<String> splitNames(String inputs) {
         return Arrays.asList(inputs.split(SPLITTER));
+    }
+
+    public static boolean isAll(String input) {
+        return input.equals(ALL);
     }
 }
