0ff3a18ca5bfba15b4ce1f443d13d080b7f47d5e
hyperpace <cnk9109@gmail.com>
2019-05-22 23:34:52 +0900

    refactor(Calculator) : remove not used import

--- src/main/java/com/woowacourse/calculator/domain/Calculator.java
+++ src/main/java/com/woowacourse/calculator/domain/Calculator.java
@@ -1,17 +1,10 @@
 package com.woowacourse.calculator.domain;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-
 public class Calculator {
     public static int sum(CalculatorDto calculatorDto) {
-        int sum = 0;
-        for (int number : calculatorDto.getNumbers()) {
-            sum += number;
-        }
-        return sum;
+        return calculatorDto.getNumbers()
+                .stream()
+                .reduce(0, Integer::sum)
+                ;
     }
 }
