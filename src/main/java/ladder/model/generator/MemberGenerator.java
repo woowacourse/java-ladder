a705517a0cf02cad283694b96c6d3ca1c0f5ca68
pkch93 <star_final@naver.com>
2019-05-23 16:06:04 +0900

    refactor: MemberGenerator의 refineNames 제거
    - MemberGenerator는 단일책임원칙에 따라 Member를 생성하는 기능만 담당하기 위해
    refineNames를 InputView의 메서드로 추출

--- src/main/java/ladder/model/generator/MemberGenerator.java
+++ src/main/java/ladder/model/generator/MemberGenerator.java
@@ -7,13 +7,6 @@ import java.util.List;
 
 public class MemberGenerator {
 
-    private static final String COMMA = ",";
-
-    public static String[] refineNames(String inputText) {
-        String[] members = inputText.split(COMMA);
-        return members;
-    }
-
     public static List<Member> generateMembers(String[] names) {
         List<Member> members = new ArrayList<>();
 
--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -1,6 +1,5 @@
 package ladder.view;
 
-import ladder.model.generator.MemberGenerator;
 import ladder.model.validator.MemberValidator;
 
 import java.util.Scanner;
@@ -10,6 +9,8 @@ public class InputView {
     private static final String RESULT_INPUT_MESSAGE = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
     private static final String HEIGHT_INPUT_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
     private static final String GET_MEMBER_RESULT_INPUT_MESSAGE = "결과를 보고 싶은 사람은?";
+    private static final String COMMA = ",";
+
     private static Scanner scanner = new Scanner(System.in);
 
     public static String[] inputNames() {
@@ -17,7 +18,7 @@ public class InputView {
         String inputText = scanner.nextLine().trim();
         try {
             MemberValidator.checkSeparator(inputText);
-            String[] inputs = MemberGenerator.refineNames(inputText);
+            String[] inputs = refineNames(inputText);
             MemberValidator.checkNamesLength(inputs);
             return inputs;
         } catch (IllegalArgumentException e) {
@@ -25,12 +26,16 @@ public class InputView {
         }
     }
 
+    private static String[] refineNames(final String inputText) {
+        return inputText.split(COMMA);
+    }
+
     public static String[] inputResults(int countOfMember){
         System.out.println(RESULT_INPUT_MESSAGE);
         String inputText = scanner.nextLine().trim();
         try {
             MemberValidator.checkSeparator(inputText);
-            String[] inputs = MemberGenerator.refineNames(inputText);
+            String[] inputs = refineNames(inputText);
             MemberValidator.checkNamesLength(inputs);
             MemberValidator.checkMemberCount(inputs.length, countOfMember);
             return inputs;
