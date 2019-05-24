a55345b598a1a7c3c0f124aa34eb7ee505b93228
pkch93 <star_final@naver.com>
2019-05-24 16:48:19 +0900

    refactor: LadderGame.class
    - List<Member>, List<Result>, List<Row>를 모두 일급 컬랙션인
    Members, DefaultResults, Ladder로 변경
    
    - 변경에 따른 executeGame() 변경

--- src/main/java/ladder/model/LadderGame.java
+++ src/main/java/ladder/model/LadderGame.java
@@ -1,71 +1,24 @@
 package ladder.model;
 
-import ladder.model.generator.RandomValueGenerator;
-
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 
 public class LadderGame {
-    private final List<Row> ladder = new ArrayList<>();
-    private final List<Member> members;
-    private final List<String> results;
+    private final Members members;
+    private final Ladder ladder;
+    private final DefaultResults results;
 
-    public LadderGame(List<Member> members, int ladderHeight, List<String> results) {
+    public LadderGame(Members members, Ladder ladder, DefaultResults results) {
         this.members = members;
+        this.ladder = ladder;
         this.results = results;
-
-        for (int i = 0; i < ladderHeight; i++) {
-            ladder.add(new Row(linkedStatusGenerator(members.size())));
-        }
-    }
-
-    public LadderGame(List<Member> members, int[][] linkedStatus, List<String> results) {
-        this.members = members;
-        this.results = results;
-
-        for (int[] status : linkedStatus) {
-            ladder.add(new Row(status));
-        }
-    }
-
-    private int[] linkedStatusGenerator(int countOfMembers) {
-        int[] linkedStatus = new int[countOfMembers - 1];
-
-        for (int i = 0; i < linkedStatus.length; i++) {
-            linkedStatus[i] = RandomValueGenerator.generateRandomValue();
-        }
-
-        return linkedStatus;
     }
 
     public EndResult executeGame() {
-        ladder.forEach(row -> members.forEach(member -> member.move(row.move(member.getPosition()))));
-        return makeResult();
-    }
-
-    private EndResult makeResult() {
-        List<Result> finalResults = new ArrayList<>();
-
-        for (Member member : members) {
-            finalResults.add(new Result(results.get(member.getPosition()), member.getName()));
-        }
-
-        return new EndResult(finalResults);
-    }
-
-    public List<Row> getLadder() {
-        return ladder;
-    }
-
-    public List<Member> getMembers() {
-        return members;
-    }
-
-    public List<String> getResults() {
-        return results;
-    }
-
-    public int getLadderHeight() {
-        return ladder.size();
+        EndResult endResult = new EndResult(new HashMap<>());
+        members.allMembers().forEach(member -> {
+            Result result = ladder.play(member, results);
+            endResult.put(member, result);
+        });
+        return endResult;
     }
 }
