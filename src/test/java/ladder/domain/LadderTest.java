5da546c5e1070dc444423dc1308f544dcf9ca332
yejilee <yjll1019@naver.com>
2019-05-24 15:42:46 +0900

    Refactor : remove function in LadderTest class

--- src/test/java/ladder/domain/LadderTest.java
+++ src/test/java/ladder/domain/LadderTest.java
@@ -1,31 +1,10 @@
 package ladder.domain;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static ladder.domain.PointTest.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LadderTest {
-	private int countOfNumber = 5;
-	private int ladderHeight = 3;
-
-	@BeforeEach
-	void init() {
-		List<Point> points = new ArrayList<>(Arrays.asList(CAN_MOVE_RIGHT_POINT, CAN_MOVE_LEFT_POINT,
-				CAN_MOVE_RIGHT_POINT, CAN_MOVE_LEFT_POINT, STOP_POINT));
-		List<Line> lines = new ArrayList<>();
-		for (int i = 0; i < ladderHeight; ++i) {
-			lines.add(new Line(points));
-		}
-
-		Ladder.createLadder(countOfNumber, ladderHeight);
-	}
-
 	@Test
 	void 참가자의_수가_2미만인_경우_예외_반환() {
 		assertThrows(IllegalArgumentException.class, () -> Ladder.createLadder(1, 2));
