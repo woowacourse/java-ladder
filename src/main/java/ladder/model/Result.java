9d15b4dc5bc7667763dd723da924839d8f683948
pkch93 <star_final@naver.com>
2019-05-24 15:51:32 +0900

    refactor: Result.class
    - 기존에 Result가 가지고 있던 winner 정보를 없애고 해당 위치의 결과값만 가지도록 변경
    - winner 정보를 Result가 가지게 되니 member 정보에 종속되는 문제점이 있어 분리

--- src/main/java/ladder/model/Result.java
+++ src/main/java/ladder/model/Result.java
@@ -4,33 +4,25 @@ import java.util.Objects;
 
 public class Result {
     private final String result;
-    private final String winner;
 
-    public Result(String result, String winner) {
+    public Result(String result) {
         this.result = result;
-        this.winner = winner;
     }
 
-    public String getWinner(){
-        return winner;
+    public String getResult() {
+        return result;
     }
 
     @Override
-    public boolean equals(Object o) {
+    public boolean equals(final Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Result result1 = (Result) o;
-        return Objects.equals(result, result1.result) &&
-                Objects.equals(winner, result1.winner);
+        final Result result1 = (Result) o;
+        return Objects.equals(result, result1.result);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(result, winner);
-    }
-
-    @Override
-    public String toString() {
-        return winner + " : " + result;
+        return Objects.hash(result);
     }
 }
