e929803a34df4c532edf15c2f8042131f271d7a6
hyojaekim <hj00686@naver.com>
2019-05-25 15:39:22 +0900

    [수정] 리팩토링

--- src/main/java/calculator/InputView.java
+++ src/main/java/calculator/InputView.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static String inputFormula(){
+    public static String inputFormula() {
         System.out.println("수식을 입력하세요 : ");
         return scanner.nextLine();
     }
