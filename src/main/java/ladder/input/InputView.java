f2f953b53affc7c41df8c6c8fe95944ed714b669
ddu0422 <ddu0422@naver.com>
2019-05-25 18:30:55 +0900

    사다리 높이 입력

--- src/main/java/ladder/input/InputView.java
+++ src/main/java/ladder/input/InputView.java
@@ -2,6 +2,7 @@ package ladder.input;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InputView {
@@ -10,5 +11,16 @@ public class InputView {
     public static String[] inputParticipants() {
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
         return StringUtils.split(scanner.nextLine(), ",");
+    }
+
+    public static int inputLadderHeight() {
+        try {
+            System.out.println("최대 사다리 높이는 몇 개 인가요?");
+            return scanner.nextInt();
+        } catch (InputMismatchException e) {
+            scanner.nextLine();
+            System.out.println("숫자를 입력해주세요.");
+            return inputLadderHeight();
+        }
     }
 }
