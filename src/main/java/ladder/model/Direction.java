b169faf35e45d82bd6e5077c6d48787f56bb7ee0
pkch93 <star_final@naver.com>
2019-05-26 10:39:29 +0900

    test: DirectionTest
    - Direction 관련 테스트 추가

--- src/main/java/ladder/model/Direction.java
+++ src/main/java/ladder/model/Direction.java
@@ -1,5 +1,7 @@
 package ladder.model;
 
+import java.util.Objects;
+
 public class Direction {
     private final boolean left;
     private final boolean right;
@@ -44,5 +46,19 @@ public class Direction {
 
     boolean isMovable() {
         return right;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Direction direction = (Direction) o;
+        return left == direction.left &&
+                right == direction.right;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(left, right);
     }
 }
