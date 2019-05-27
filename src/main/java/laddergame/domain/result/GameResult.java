958b933209c2b417cb68f08cdee7b1820887aa33
yuyu154 <38104827+yuyu154@users.noreply.github.com>
2019-05-27 22:32:38 +0900

    [MOD] HashMap 중복 호출 수정

--- src/main/java/laddergame/domain/result/GameResult.java
+++ src/main/java/laddergame/domain/result/GameResult.java
@@ -3,42 +3,39 @@ package laddergame.domain.result;
 import laddergame.domain.NameList;
 import laddergame.domain.ladder.Ladder;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class GameResult {
-	private final Map<String, String> gameResults;
+    private final Map<String, String> gameResults;
 
-	private GameResult(NameList players, NameList rewards, Ladder ladder) {
-		this.gameResults = new HashMap<>();
-		init(players, rewards, ladder);
-	}
+    private GameResult(NameList players, NameList rewards, Ladder ladder) {
+        this.gameResults = new HashMap<>();
+        init(players, rewards, ladder);
+    }
 
-	private void init(NameList players, NameList rewards, Ladder ladder) {
-		for (int i = 1; i <= players.getSize(); i++) {
-			int destination = ladder.findDestination(i);
-			gameResults.put(players.getNameOfIndex(i), rewards.getNameOfIndex(destination));
-		}
-	}
+    private void init(NameList players, NameList rewards, Ladder ladder) {
+        for (int i = 1; i <= players.getSize(); i++) {
+            int destination = ladder.findDestination(i);
+            gameResults.put(players.getNameOfIndex(i), rewards.getNameOfIndex(destination));
+        }
+    }
 
-	public static GameResult of(NameList players, NameList rewards, Ladder ladder) {
-		return new GameResult(players, rewards, ladder);
-	}
+    public static GameResult of(NameList players, NameList rewards, Ladder ladder) {
+        return new GameResult(players, rewards, ladder);
+    }
 
-	public List<GameResultFormat> getAllGameResultFormat() {
-		List<GameResultFormat> ret = new ArrayList<>();
-		for (String people : gameResults.keySet()) {
-			ret.add(getGameResultFormat(people));
-		}
-		return ret;
-	}
+    public List<GameResultFormat> getAllGameResultFormat() {
+        List<GameResultFormat> ret = new ArrayList<>();
+        for (String people : gameResults.keySet()) {
+            ret.add(getGameResultFormat(people));
+        }
+        return ret;
+    }
 
-	public GameResultFormat getGameResultFormat(final String name) {
-		if (gameResults.containsKey(name)) {
-			return GameResultFormat.of(name, gameResults.get(name));
-		}
-		throw new IllegalArgumentException("올바른 이름을 입력하세요");
-	}
+    public GameResultFormat getGameResultFormat(final String name) {
+        Optional<String> optResult = Optional.ofNullable(gameResults.get(name));
+        return GameResultFormat.of(name, optResult.orElseThrow(
+                () -> new IllegalArgumentException("올바른 이름을 입력하세요")
+        ));
+    }
 }
