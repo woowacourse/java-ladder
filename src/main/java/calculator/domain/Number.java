e654163fb0a4e78ba2aac1ba6095cfc934a2f806
yuyu154 <38104827+yuyu154@users.noreply.github.com>
2019-05-27 17:51:16 +0900

    [MOD] ZERO를 0값을 가지는 Number 불변 객체로 수정

--- src/main/java/calculator/domain/Number.java
+++ src/main/java/calculator/domain/Number.java
@@ -3,47 +3,45 @@ package calculator.domain;
 import java.util.Objects;
 
 public class Number {
-	private static final String ZERO = "0";
+    public static final Number ZERO = new Number("0");
 
-	private int number;
+    private static final int MIN_NUMBER = 0;
 
-	public Number(final String number) {
-		this.number = parseNumber(number);
-		if (this.number < 0) {
-			throw new IllegalArgumentException("0 이상의 정수를 입력하세요");
-		}
-	}
+    private int number;
 
-	private int parseNumber(String number) {
-		try {
-			return Integer.parseInt(number);
-		} catch (NumberFormatException e) {
-			throw new IllegalArgumentException("문자를 입력하지마세요");
-		}
-	}
+    public Number(final String number) {
+        this.number = parseNumber(number);
+        if (this.number < MIN_NUMBER) {
+            throw new IllegalArgumentException("0 이상의 정수를 입력하세요");
+        }
+    }
 
-	public static Number zeroNumber() {
-		return new Number(ZERO);
-	}
+    private int parseNumber(String number) {
+        try {
+            return Integer.parseInt(number);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("문자를 입력하지마세요");
+        }
+    }
 
-	public Number add(final Number operand) {
-		return new Number(Integer.toString(this.number + operand.number));
-	}
+    public Number add(final Number operand) {
+        return new Number(Integer.toString(this.number + operand.number));
+    }
 
-	public int getNumber() {
-		return this.number;
-	}
+    public int getNumber() {
+        return this.number;
+    }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (!(o instanceof Number)) return false;
-		Number number1 = (Number) o;
-		return number == number1.number;
-	}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Number)) return false;
+        Number number1 = (Number) o;
+        return number == number1.number;
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(number);
-	}
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
 }
