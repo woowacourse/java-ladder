730afc27c7f8ee4c7a8316be8c5071d2086d4e20
yejilee <yjll1019@naver.com>
2019-05-29 15:42:20 +0900

    Refactor : modified method name in PlayersTest, RewardsTest class

--- src/test/java/ladder/domain/PlayersTest.java
+++ src/test/java/ladder/domain/PlayersTest.java
@@ -9,25 +9,25 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PlayersTest {
 	@Test
-	void overlapPlayerNames() {
+	void 참가자_중복_이름_검사() {
 		Player player = new Player("pobi");
 		assertThrows(IllegalArgumentException.class, () -> new Players(Arrays.asList(player, player)));
 	}
 
 	@Test
-	void validateNumberOfPlayersIsLessThanTwo() {
+	void 참가자의_수가_2미만일_때() {
 		List<Player> players = Arrays.asList(new Player("pobi"));
 		assertThrows(IllegalArgumentException.class, () -> new Players(players));
 	}
 
 	@Test
-	void validateNumberOfPlayers() {
+	void 참가자의_수가_2이상일_때() {
 		List<Player> players = Arrays.asList(new Player("pobi"), new Player("jason"));
 		assertDoesNotThrow(() -> new Players(players));
 	}
 
 	@Test
-	void invalidIndex() {
+	void 참가자_리스트_인덱스_검사() {
 		Players players = new Players(Arrays.asList(new Player("jason"), new Player("pobi")));
 		assertThrows(IllegalArgumentException.class, () -> players.getPlayer(-1));
 		assertThrows(IllegalArgumentException.class, () -> players.getPlayer(2));
--- src/test/java/ladder/domain/RewardsTest.java
+++ src/test/java/ladder/domain/RewardsTest.java
@@ -8,17 +8,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class RewardsTest {
 	@Test
-	void create() {
+	void 게임의_보상_리스트_생성() {
 		assertDoesNotThrow(() -> new Rewards(Arrays.asList(new Reward("1000"), new Reward("꽝"))));
 	}
 
 	@Test
-	void invalidNumberOfRewards() {
+	void 게임의_보상_리스트_개수_검사() {
 		assertThrows(IllegalArgumentException.class, () -> new Rewards(Arrays.asList(new Reward("1000"))));
 	}
 
 	@Test
-	void invalidIndex() {
+	void 게임의_보상_리스트_인덱스_검사() {
 		Rewards rewards = new Rewards(Arrays.asList(new Reward("1000"), new Reward("꽝")));
 		assertThrows(IllegalArgumentException.class, () -> rewards.getReward(-1));
 		assertThrows(IllegalArgumentException.class, () -> rewards.getReward(2));
