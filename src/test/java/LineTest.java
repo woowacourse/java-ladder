d2c13af9395fc38ce52bacb7bcc0996b712b7905
ingpyo <juninpyo726@gmail.com>
2023-02-14 20:35:52 +0900

    test: 랜덤 값의 의한 라인 생성 테스트

--- src/test/java/LineTest.java
+++ src/test/java/LineTest.java
@@ -1,34 +1,50 @@
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class LineTest {
     Line line;
 
-    @BeforeEach
-    @Test
-    void createLine() {
-        line = new Line(4);
+    @Nested
+    @DisplayName("랜덤 값의 의한 라인 생성 테스트(첫번째 좌표는 이전 좌표가 없는것을 표시)")
+    class Creat {
+        @DisplayName("들어간대로 잘 생성되었는지 테스트")
+        @Test
+        void createTest() {
+            line = new Line(4, new testTrueAndFalseGenerator(new ArrayList<>(Arrays.asList(false, true, false)) {
+            }));
+            assertThat(line.getPoints()).isEqualTo(List.of(false, false, true, false));
+        }
+
+        @DisplayName("이전 좌표와 현재 좌표가 겹칠때 변환 테스트(처)")
+        @Test
+        void createLine2() {
+            line = new Line(4, new testTrueAndFalseGenerator(new ArrayList<>(Arrays.asList(true, true, false)) {
+            }));
+            assertThat(line.getPoints()).isEqualTo(List.of(false, true, false, false));
+        }
     }
 
-    @DisplayName("랜덤 값의 의한 좌표값 생성")
-    @Test
-    void createPointOfRandomNumber() {
-        int result = line.lineMaker(4).size();
-        assertThat(result).isEqualTo(3);
+
+
+
+    static class testTrueAndFalseGenerator implements TrueAndFalseGenerator {
+        List<Boolean> points;
+
+        testTrueAndFalseGenerator(List<Boolean> points) {
+            this.points = points;
+        }
+
+        @Override
+        public Boolean generate() {
+            return points.remove(0);
+        }
     }
 
-    @DisplayName("좌표가 겹칠 때 판단")
-    @Test
-    void cross() {
-        assertThat(line.validate(true, true)).isFalse();
-    }
-
-    @DisplayName("좌표가 안겹칠 때 판단")
-    @Test
-    void notcross() {
-        assertThat(line.validate(true, false)).isFalse();
-    }
 }
