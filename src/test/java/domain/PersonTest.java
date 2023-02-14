4a24d5670009221e1383d7d8ffb3e134fbc8695a
splitube <voicesplit@daum.net>
2023-02-14 16:43:08 +0900

    refactor: 테스트 코드 수정
    생성자 호출만으로 테스트 가능

--- src/test/java/domain/PersonTest.java
+++ src/test/java/domain/PersonTest.java
@@ -12,7 +12,7 @@ class PersonTest {
     @Test
     void nameSuccess() {
         try {
-            Person person = new Person("abcde");
+            new Person("abcde");
         } catch (IllegalArgumentException exception) {
             Assertions.fail("이름이 조건에 맞을 경우 객체가 제대로 생성되어야 합니다.");
         }
@@ -21,40 +21,35 @@ class PersonTest {
     @DisplayName("사람이름이 5글자보다 많을 경우 오류를 던진다.")
     @Test
     void nameOver5() {
-        Assertions.assertThatThrownBy(() -> {
-            Person person = new Person("abcdef");
-        }).isExactlyInstanceOf(InvalidPersonNameException.class);
+        Assertions.assertThatThrownBy(() -> new Person("abcdef"))
+            .isExactlyInstanceOf(InvalidPersonNameException.class);
     }
 
     @DisplayName("사람이름이 빈문자열일 경우 오류를 던진다.")
     @Test
     void nameNull() {
-        Assertions.assertThatThrownBy(() -> {
-            Person person = new Person(null);
-        }).isExactlyInstanceOf(InvalidPersonNameException.class);
+        Assertions.assertThatThrownBy(() -> new Person(null))
+            .isExactlyInstanceOf(InvalidPersonNameException.class);
     }
 
     @DisplayName("사람이름이 빈문자열일 경우 오류를 던진다.")
     @Test
     void nameEmpty() {
-        Assertions.assertThatThrownBy(() -> {
-            Person person = new Person("");
-        }).isExactlyInstanceOf(InvalidPersonNameException.class);
+        Assertions.assertThatThrownBy(() -> new Person(""))
+            .isExactlyInstanceOf(InvalidPersonNameException.class);
     }
 
     @DisplayName("사람이름이 띄어쓰기로만 이루어진 경우 오류를 던진다.")
     @Test
     void nameBlank() {
-        Assertions.assertThatThrownBy(() -> {
-            Person person = new Person("     ");
-        }).isExactlyInstanceOf(InvalidPersonNameException.class);
+        Assertions.assertThatThrownBy(() -> new Person("     "))
+            .isExactlyInstanceOf(InvalidPersonNameException.class);
     }
 
     @DisplayName("사람이름에 중복 식별자가 들어간 경우 오류를 던진다.")
     @Test
     void nameIdentifier() {
-        Assertions.assertThatThrownBy(() -> {
-            Person person = new Person("ab-cd");
-        }).isExactlyInstanceOf(NameContainsIdentifierException.class);
+        Assertions.assertThatThrownBy(() -> new Person("ab-cd"))
+            .isExactlyInstanceOf(NameContainsIdentifierException.class);
     }
 }
