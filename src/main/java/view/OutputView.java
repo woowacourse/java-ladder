edc6ef1885b43d47e10be88b37eca0392a0546c4
MoonJeWoong <munjin0201@naver.com>
2023-02-14 19:05:23 +0900

    feat: 참여할 사람 이름 입력 메세지 기능 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,4 +1,10 @@
 package view;
 
 public class OutputView {
+
+    private static final String PLAYER_NAME_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+
+    public void printPlayerNamesMessage() {
+        System.out.println(PLAYER_NAME_MESSAGE);
+    }
 }
