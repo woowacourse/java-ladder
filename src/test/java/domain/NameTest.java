7d01180739c5aba0076342e4eb69d98975079d89
hyunseo <ohseo8813@gmail.com>
2023-02-14 17:18:41 +0900

    fix: NameTest에 DisplayName 추가

--- src/test/java/domain/NameTest.java
+++ src/test/java/domain/NameTest.java
@@ -1,5 +1,6 @@
 package domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -9,13 +10,15 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class NameTest {
 
+    @DisplayName("알맞은 이름을 입력하면 정상적으로 생성된다.")
     @Test
     void create_success() {
         assertThatNoException().isThrownBy(() -> new Name("name"));
     }
 
+    @DisplayName("이름의 길이가 5글자를 초과하면 예외를 반환한다.")
     @ParameterizedTest
-    @ValueSource(strings = {"lensix","len7777"})
+    @ValueSource(strings = {"lensix", "len7777"})
     void create_fail(String wrongName) {
         assertThatThrownBy(() -> new Name(wrongName))
                 .isInstanceOf(IllegalArgumentException.class);
