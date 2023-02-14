78bbb922c53b796dd972360e348ca1777db77574
BackFoxx <backfoxxx@gmail.com>
2023-02-14 19:42:57 +0900

    refactor: 이름 객체 검증 메서드 추출 후 매직 넘버 상수화

--- src/main/java/domain/Name.java
+++ src/main/java/domain/Name.java
@@ -2,16 +2,28 @@ package domain;
 
 public class Name {
 
+    public static final int MAX_LENGTH = 5;
     private final String name;
 
     public Name(final String name) {
-        if(name.length() > 5){
+        validateName(name);
+        this.name = name;
+    }
+
+    private void validateName(final String name) {
+        validateBlank(name);
+        validateMaxLength(name);
+    }
+
+    private static void validateMaxLength(final String name) {
+        if (name.length() > MAX_LENGTH) {
             throw new IllegalArgumentException("5글자 이하를 입력해주세요.");
         }
+    }
 
-        if(name.length() == 0){
+    private static void validateBlank(final String name) {
+        if (name.isBlank()) {
             throw new IllegalArgumentException("한글자 이상 입력해주세요.");
         }
-        this.name = name;
     }
 }
