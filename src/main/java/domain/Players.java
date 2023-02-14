0b5d219fe0532725d3a187fd996c70e02b7a6a08
seokjin8678 <rnrn8678@naver.com>
2023-02-14 17:45:59 +0900

    feat(Players): 사용자의 수가 범위를 벗어나는 경우에 대한 예외 발생 기능 추가

--- src/main/java/domain/Players.java
+++ src/main/java/domain/Players.java
@@ -13,10 +13,17 @@ public class Players {
 
     public Players(String input) {
         String[] names = input.split(DELIMITER);
+        validateSize(names);
         validateDuplicate(names);
         this.players = Arrays.stream(names)
                 .map(Player::new)
                 .collect(toList());
+    }
+
+    public void validateSize(String[] names) {
+        if (names.length < 2 || names.length > 13) {
+            throw new IllegalArgumentException("[ERROR] 사용자는 2명에서 13명까지 가능합니다.");
+        }
     }
 
     private void validateDuplicate(String[] names) {
