2c2cc648ece6cab91c4a41f938bd64fe83facf1e
dev_kong <ahdjd5@gmail.com>
2023-02-14 18:51:46 +0900

    test: 정수가 아닌 문자열을 숫자로 변환할때 예외발생 테스트코드 작성

--- src/test/java/utils/StringParserTest.java
+++ src/test/java/utils/StringParserTest.java
@@ -32,11 +32,21 @@ class StringParserTest {
 
     @Test
     @DisplayName("문자열을 int로 변환한다.")
-    void convertToNumber() {
+    void convertToNumberTest() {
         String input = "1";
 
         int number = StringParser.parseToInteger(input);
 
         assertThat(number).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("정수가 아닌 문자열이 들어오면 예외발생")
+    void convertToNumberFailureTest() {
+        String input = "adsf";
+
+        assertThatThrownBy(() -> StringParser.parseToInteger(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("정수만 입력가능합니다.");
     }
 }
