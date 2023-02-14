eef4536139ca93b735c963332460175403c6b70f
MoonJeWoong <munjin0201@naver.com>
2023-02-14 18:03:30 +0900

    feat: 사람 이름이 문자로만 이루어져 있는지 검증기능 추가

--- src/main/java/model/Name.java
+++ src/main/java/model/Name.java
@@ -1,17 +1,29 @@
 package model;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Name {
     private static final int MAXIMUM_NAME_LENGTH = 5;
+    private static final Pattern pattern = Pattern.compile("^[a-zA-Z]+$");
 
     private final String name;
 
     public Name(String name){
         validateNameLength(name);
+        validateNameHasOnlyCharacters(name);
         this.name = name;
     }
 
     private void validateNameLength(String name){
         if(name.length() > MAXIMUM_NAME_LENGTH) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateNameHasOnlyCharacters(String name) {
+        Matcher matcher = pattern.matcher(name);
+        if (!matcher.matches()) {
             throw new IllegalArgumentException();
         }
     }
