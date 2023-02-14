aa474b894c5875822aba8a0d8ad2b25e8d503251
Eunkeee <eunkeeeee@gmail.com>
2023-02-14 17:27:42 +0900

    feat(Floor): 포인트 생성

--- src/main/java/domain/Floor.java
+++ src/main/java/domain/Floor.java
@@ -1,9 +1,22 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import utils.BooleanGenerator;
+
 public class Floor {
-    public Floor(int personCount) {
+    List<Boolean> points = new ArrayList<>();
+
+    public Floor(int personCount, BooleanGenerator generator) {
         if (personCount < 2 || personCount > 100) {
             throw new IllegalArgumentException();
+        }
+        for (int i = 0; i < personCount - 1; i++) {
+            if (i > 0 && points.get(i - 1)) {
+                points.add(false);
+            } else {
+                points.add(generator.generate());
+            }
         }
     }
 }
