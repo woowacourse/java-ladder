a066568eda4ebf6e27cf0bbad5c8555fadc39c1a
LJW25 <dbjwl@g.skku.edu>
2023-02-14 17:59:09 +0900

    test(RawTest): 왼쪽 포인트에 라인이 존재하면 true를 반환하는 테스트 작성

--- src/test/java/ladder/model/RawTest.java
+++ src/test/java/ladder/model/RawTest.java
@@ -12,11 +12,22 @@ class RawTest {
     @DisplayName("특정 인덱스에 가로 라인 생성 테스트")
     void createLineTest() {
         int personCount = 5;
-        int index = 0;
+        int point = 0;
         boolean isCreated = true;
         Raw raw = new Raw(personCount);
-        raw.createLine(index, isCreated);
-        assertThat(raw.isPointHasLine(index)).isTrue();
+        raw.createLineAt(point, isCreated);
+        assertThat(raw.isPointHasLine(point)).isTrue();
+    }
+
+    @Test
+    @DisplayName("왼쪽 포인트에 라인이 존재하면 true를 반환하는 테스트")
+    void checkLeftPointHasLineTest() {
+        int personCount = 5;
+        int point = 1;
+        Raw raw = new Raw(personCount);
+        raw.createLineAt(point - 1, true);
+        assertThat(raw.isLeftPointHasLine(point)).isTrue();
+
     }
 
 
