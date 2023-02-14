540e3cc5cac5ae90af7d08f6c58fa9184626ecfe
yujamint <woawoa0513@gmail.com>
2023-02-14 17:30:57 +0900

    feat: 1자 미만의 이름에 대한 검증

--- src/main/java/domain/Name.java
+++ src/main/java/domain/Name.java
@@ -5,7 +5,7 @@ public class Name {
     private final String name;
 
     public Name(String name) {
-        if (name.length() > 5) {
+        if (name.length() > 5 || name.length() < 1) {
             throw new IllegalArgumentException();
         }
         this.name = name;
