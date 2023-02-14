76e2af019d8dcefb37616e4c739e143e1f41b35b
xxeol2 <jhj01177@naver.com>
2023-02-14 17:10:26 +0900

    feat: 참여자 이름 글자 제한, 빈 입력값 예외 처리

---
+++ src/main/java/domain/Player.java
@@ -0,0 +1,18 @@
+package domain;
+
+public class Player {
+
+    private static final int PLAYER_NAME_MAX_LENGTH = 5;
+    private final String name;
+
+    public Player(final String name) {
+        validate(name);
+        this.name = name;
+    }
+
+    private void validate(final String name) {
+        if (name.isBlank() || name.length() > PLAYER_NAME_MAX_LENGTH) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
---
+++ src/test/java/domain/PlayerTest.java
@@ -0,0 +1,38 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class PlayerTest {
+
+    @DisplayName("참여자 이름은 5글자를 초과할 수 없다.")
+    @Test
+    void playerNameNotMoreThan5() {
+        String playerName = "abcdef";
+        assertThatThrownBy(() -> {
+            new Player(playerName);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("빈문자열을 입력하면 예외가 발생한다.")
+    @Test
+    void playerNameNotNull() {
+        String playerName = "";
+        assertThatThrownBy(() -> {
+            new Player(playerName);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("1글자 이상 5글자 이하이면 정상적으로 객체를 생성한다.")
+    @Test
+    void playerName_정상() {
+        String playerName = "abcde";
+        assertThatNoException()
+                .isThrownBy(() -> {
+                    new Player(playerName);
+                });
+    }
+}
