5ff57b7d76e0e6fe2499ad3c46efefaa5c05f570
DESKTOP-FNVN0HH\USER <beatus96@naver.com>
2023-02-14 22:37:29 +0900

    feat: LineMaker 도메인 생성

--- src/test/java/ladder/domain/LineMakerTest.java
+++ src/test/java/ladder/domain/LineMakerTest.java
@@ -22,14 +22,14 @@ public class LineMakerTest {
     @Test
     @DisplayName("플레이어 숫자대로 리스트가 생성되는지 확인한다.")
     void generateLineSizeTest() {
-        assertThat(lineMaker.generateLine().size()).isEqualTo(playerCount);
+        assertThat(lineMaker.generate().size()).isEqualTo(playerCount);
     }
 
     @Test
     @DisplayName("생성된 Line의 가로가 겹치지 않는지 확인한다.")
     void generateLineTest() {
         // 리스트가 연속된 true값을 갖지 않은지.
-        List<Boolean> bars = lineMaker.generateLine();
+        List<Boolean> bars = lineMaker.generate();
         for (int idx = 0; idx < bars.size() - 1; idx++) {
             assertThat(!bars.get(idx) || !bars.get(idx + 1)).isTrue();
         }
