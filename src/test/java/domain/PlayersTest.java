918c9658a690758277cae17ba0169e53201220b3
seokjin8678 <rnrn8678@naver.com>
2023-02-14 17:31:33 +0900

    test(Players): 사용자의 이름에 중복이 있으면 예외가 발생하는 테스트 추가

--- src/test/java/domain/PlayersTest.java
+++ src/test/java/domain/PlayersTest.java
@@ -1,9 +1,12 @@
 package domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class PlayersTest {
 
@@ -16,5 +19,15 @@ class PlayersTest {
         Players players = new Players(input);
         // then
         assertThat(players.getPlayers()).hasSize(2);
+    }
+
+    @ParameterizedTest
+    @DisplayName("사용자 이름이 중복되면 예외가 발생한다.")
+    @ValueSource(strings = {"glen,glen", "glen,doggy,doggy", "glen,doggy,glen"})
+    void create_duplicateName(String input) {
+        // expect
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            new Players(input);
+        }).withMessage("[ERROR] 중복된 이름이 있습니다.");
     }
 }
