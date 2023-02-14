ac366d15d54fedfae82e7a6d0424024ebf36bb57
thdwoqor <celebarte01@gmail.com>
2023-02-14 17:11:39 +0900

    feat : 유저 이름 입력 유효성 검사 코드 작성

---
+++ src/main/java/User.java
@@ -0,0 +1,10 @@
+public class User {
+    private final String name;
+
+    User(String name) {
+        if (1 > name.length() || 5 < name.length()) {
+            throw new IllegalArgumentException("이름은 1~5 글자 입니다.");
+        }
+        this.name = name;
+    }
+}
