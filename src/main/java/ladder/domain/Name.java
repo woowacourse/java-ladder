aa2d8b3230337b898ff405f05ad13d11cce5a36d
be-student <presentsong@naver.com>
2023-02-14 16:13:15 +0900

    feat: name 클래스 추가

--- src/main/java/ladder/domain/Name.java
+++ src/main/java/ladder/domain/Name.java
@@ -2,4 +2,28 @@ package ladder.domain;
 
 public class Name {
 
+    private static final String OVER_LENGTH_MESSAGE = "글자수가 6글자를 초과했습니다";
+    private static final String BLANK_MESSAGE = "이름이 빈 문자열이 될 수 없습니다";
+    private static final String NULL_MESSAGE = "이름이 null이 되면 안됩니다";
+
+    private final String name;
+
+    public Name(String name) {
+        validateName(name);
+        this.name = name;
+    }
+
+    private static void validateName(String name) {
+        if (name == null) {
+            throw new IllegalArgumentException(NULL_MESSAGE);
+        }
+        if (name.length() >= 6) {
+            throw new IllegalArgumentException(OVER_LENGTH_MESSAGE);
+        }
+        if (name.length() == 0) {
+            throw new IllegalArgumentException(BLANK_MESSAGE);
+        }
+    }
+
+
 }
---
+++ src/test/java/ladder/domain/NameTest.java
@@ -0,0 +1,33 @@
+package ladder.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+class NameTest {
+
+    @Test
+    void 글자5초과는_에러() {
+        String error = assertThrows(IllegalArgumentException.class, () -> new Name("123456")).getMessage();
+        assertThat(error).isEqualTo("글자수가 6글자를 초과했습니다");
+    }
+
+    @Test
+    void 글자5이하는_통과() {
+        assertDoesNotThrow(() -> new Name("12345"));
+    }
+
+    @Test
+    void 글자_0은_에러() {
+        String error = assertThrows(IllegalArgumentException.class, () -> new Name("")).getMessage();
+        assertThat(error).isEqualTo("이름이 빈 문자열이 될 수 없습니다");
+    }
+
+    @Test
+    void 글자_null_에러() {
+        String error = assertThrows(IllegalArgumentException.class, () -> new Name(null)).getMessage();
+        assertThat(error).isEqualTo("이름이 null이 되면 안됩니다");
+    }
+}
