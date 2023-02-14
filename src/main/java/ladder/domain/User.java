9a4ac68fa8e66f6ea7e82a80b6bec12dd2a7e3d2
kevstevie <jianna442@gmail.com>
2023-02-14 17:21:29 +0900

    feat: User 생성 예외 메시지 테스트 케이스 추가

--- src/main/java/ladder/domain/User.java
+++ src/main/java/ladder/domain/User.java
@@ -2,17 +2,24 @@ package ladder.domain;
 
 public class User {
 
-    private String name;
+    private static final int MAX_NAME_LENGTH = 5;
+    private static final int MINIMUM_NAME_LENGTH = 1;
+    private static final String INVALID_LENGTH_MESSAGE = "유저 이름 길이는 공백이거나 6 이상일 수 없습니다.";
+    private final String name;
 
     public User(String name) {
-        if (name.isBlank()) {
-            throw new IllegalArgumentException();
-        }
-
-        if (name.length() > 5) {
-            throw new IllegalArgumentException();
-        }
+        validate(name);
 
         this.name = name;
+    }
+
+    private void validate(String name) {
+        validateLength(name.trim());
+    }
+
+    private void validateLength(String name) {
+        if (name.length() > MAX_NAME_LENGTH || name.length() < MINIMUM_NAME_LENGTH) {
+            throw new IllegalArgumentException(INVALID_LENGTH_MESSAGE);
+        }
     }
 }
--- src/test/java/ladder/domain/UserTest.java
+++ src/test/java/ladder/domain/UserTest.java
@@ -11,11 +11,11 @@ import static org.assertj.core.api.Assertions.*;
 public class UserTest {
 
     @ParameterizedTest
-    @ValueSource(strings = {"a","aaaaa"})
+    @ValueSource(strings = {"a", "aaaaa"})
     @DisplayName("유저 정상 생성 테스트")
     void makeValidUserTest(String value) {
-        assertThatCode(()->
-            new User(value)
+        assertThatCode(() ->
+                new User(value)
         ).doesNotThrowAnyException();
     }
 
@@ -24,6 +24,15 @@ public class UserTest {
     @DisplayName("유저 생성 예외 테스트")
     void makeInvalidUserTest(String value) {
         assertThatThrownBy(() -> new User(value))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "aaaaaa"})
+    @DisplayName("유저 생성 예외 메시지 테스트")
+    void makeInvalidUserNameLengthMessageTest(String value) {
+        assertThatThrownBy(() -> new User(value))
+                .hasMessageContaining("유저 이름 길이는 공백이거나 6 이상일 수 없습니다.")
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
