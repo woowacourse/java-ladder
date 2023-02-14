a7354a1b0c4ff47135e836ea27f88d1997e79a14
This2sho <zx00018@naver.com>
2023-02-14 17:47:41 +0900

    feat(Line): 라인에 대한 검증 조건 구현
    - 연속으로 가로 라인이 2개 나오면 예외처리한다

--- src/main/java/domain/Line.java
+++ src/main/java/domain/Line.java
@@ -4,11 +4,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Line {
-    private final List<Boolean> points = new ArrayList<>();
+
+    private final List<Boolean> points;
 
     public Line(int personCount) {
-        for (int j = 0; j < personCount-1; j++) {
+        this.points = new ArrayList<>();
+        for (int i = 0; i < personCount - 1; i++) {
             points.add(false);
+        }
+    }
+
+    public Line(List<Boolean> points) {
+        validate(points);
+        this.points = points;
+    }
+
+    private void validate(List<Boolean> points) {
+        Boolean flag = points.get(0);
+
+        for (int i = 1; i < points.size(); i++) {
+            validateSequential(points, flag, i);
+            flag = points.get(i);
+        }
+    }
+
+    private void validateSequential(List<Boolean> points, Boolean flag, int index) {
+        if (points.get(index) & flag) {
+            throw new IllegalArgumentException("연속된 true가 나올 수 없습니다.");
         }
     }
 
--- src/test/java/domain/LineTest.java
+++ src/test/java/domain/LineTest.java
@@ -1,15 +1,33 @@
 package domain;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LineTest {
+
     @Test
     @DisplayName("사람수에 따라 라인을 생성한다. (라인수 = 사람수 - 1)")
     void createLine() {
         Line line = new Line(3);
         assertThat(line.size()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("true가 연속으로 두 개 나오면 예외를 발생시킨다.")
+    void exceptionTest() {
+        assertThatThrownBy(() -> new Line(List.of(true, true, false)))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("연속된 true가 나올 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("true가 연속으로 두 개 나오지 않아야 한다.")
+    void succeedTest() {
+        assertDoesNotThrow(() -> new Line(List.of(true, false, false)));
     }
 }
