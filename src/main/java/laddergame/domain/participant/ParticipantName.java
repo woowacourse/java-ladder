d540979873db565d6d5b3552102088e58baad244
jiwon <ljwon77@daum.net>
2023-02-14 17:16:44 +0900

    feat: ParticipantName 클래스의 이름 길이를 검증하는 기능을 구현한다

---
+++ src/main/java/laddergame/domain/participant/ParticipantName.java
@@ -0,0 +1,18 @@
+package laddergame.domain.participant;
+
+public class ParticipantName {
+
+    private static final int MAX_LENGTH = 5;
+    private final String name;
+
+    public ParticipantName(final String name) {
+        if (isOutOfRange(name)) {
+            throw new IllegalArgumentException("[ERROR] 이름은 다섯 글자를 초과할 수 없습니다.");
+        }
+        this.name = name;
+    }
+
+    private boolean isOutOfRange(final String name) {
+        return name.length() > MAX_LENGTH;
+    }
+}
