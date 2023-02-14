3da312cc4e9720385dc800402474ae4da07cad4a
MoonJeWoong <munjin0201@naver.com>
2023-02-14 18:55:22 +0900

    feat: Controller 추가

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,4 +1,18 @@
 package controller;
 
+import view.InputView;
+import view.OutputView;
+
 public class Controller {
+
+    public InputView inputView;
+    public OutputView outputView;
+
+    public Controller(InputView inputView,OutputView outputView){
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+    public void run(){
+
+    }
 }
