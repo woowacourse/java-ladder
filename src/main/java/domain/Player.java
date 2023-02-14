4fb979d37a82b3cdb5938ce64ecaa1707cfa41bd
seokjin8678 <rnrn8678@naver.com>
2023-02-14 16:52:15 +0900

    refactor(Player): 이름 빈 값 검증 로직 메서드로 추출

--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -5,10 +5,14 @@ public class Player {
     private final String name;
 
     public Player(String name) {
+        validateBlank(name);
+        this.name = name;
+    }
+
+    private void validateBlank(String name) {
         if (name.isBlank()) {
             throw new IllegalArgumentException("[ERROR] 이름은 공백이 될 수 없습니다.");
         }
-        this.name = name;
     }
 
     public String getName() {
