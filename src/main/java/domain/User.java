3c62309cfbd3e73f3d9c8f6e826419f09f3a5c8e
dev_kong <ahdjd5@gmail.com>
2023-02-14 18:32:23 +0900

    feat: user 이름 길이 검증코드 작성

--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -4,6 +4,13 @@ public class User {
     private final String name;
 
     public User(String name) {
+        validate(name);
         this.name = name;
+    }
+
+    private void validate(String name) {
+        if (name.isEmpty() || name.length() > 5) {
+            throw new IllegalArgumentException("[ERROR] 이름은 1글자 이상 5글자 이하여야 합니다.");
+        }
     }
 }
