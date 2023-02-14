c864586588ab02c3b740e229ee7226dd08dae8cb
dev_kong <ahdjd5@gmail.com>
2023-02-14 18:44:04 +0900

    feat: split method parameter 수정

--- src/main/java/utils/StringParser.java
+++ src/main/java/utils/StringParser.java
@@ -5,6 +5,6 @@ import java.util.List;
 
 public class StringParser {
     public static List<String> splitByComma(String input) {
-        return Arrays.asList(input.split(","));
+        return Arrays.asList(input.split(",", -1));
     }
 }
