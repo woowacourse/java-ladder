f8928c0f68613f4b0379e73e11e9b091ccf029b4
yujamint <woawoa0513@gmail.com>
2023-02-14 17:30:42 +0900

    test: 이름 1자 미만에 대한 테스트 작성

--- src/test/java/domain/NameTest.java
+++ src/test/java/domain/NameTest.java
@@ -4,18 +4,31 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-
+@DisplayName("이름은 ")
 class NameTest {
-    @DisplayName("이름은 5자 이하이다.")
+    @DisplayName("5자 이하이다.")
     @Test
     public void nameTest_5() {
         Assertions.assertDoesNotThrow(() -> new Name("12345"));
     }
 
-    @DisplayName("이름이 6자 이상이면 에러가 발생한다.")
+    @DisplayName("6자 이상이면 에러가 발생한다.")
     @Test
     public void nameTest_6() {
         Assertions.assertThrows(IllegalArgumentException.class,
                 () -> new Name("123456"));
+    }
+
+    @DisplayName("1자 이상이다.")
+    @Test
+    public void nameTest_1() {
+        Assertions.assertDoesNotThrow(() -> new Name("1"));
+    }
+
+    @DisplayName("1자 미만이면 예외가 발생한다.")
+    @Test
+    public void nameTest_0() {
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> new Name(""));
     }
 }
