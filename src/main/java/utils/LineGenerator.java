8c7f3e5b2b919251c00841d854cb061b056413bf
dev_kong <ahdjd5@gmail.com>
2023-02-14 16:44:59 +0900

    refa: for-> stream, 변수명 변경, convertBoolean 을 통한 메서드 분리

--- src/main/java/utils/LineGenerator.java
+++ src/main/java/utils/LineGenerator.java
@@ -2,19 +2,25 @@ package utils;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import domain.Line;
 
 public class LineGenerator {
 
     public static Line generate(Line previousLine) {
-        List<Boolean> pL = previousLine.getLine();
-        List<Boolean> ret = new ArrayList<>();
-        for (boolean b : pL) {
-            if (b) {
-                ret.add(false);
-            }
+        List<Boolean> lines = previousLine.getLine();
+
+        List<Boolean> newLines = lines.stream()
+                .map(LineGenerator::convertBoolean)
+                .collect(Collectors.toList());
+        return new Line(newLines);
+    }
+
+    private static boolean convertBoolean(boolean comparedBoolean) {
+        if(comparedBoolean) {
+            return false;
         }
-        return new Line(ret);
+        return false;
     }
 }
