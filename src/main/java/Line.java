894a7b657d1a74875bd1907d942143835c690bcb
2014012606 <2014012606@hanyang.ac.kr>
2023-02-14 16:49:20 +0900

    feat: 사다리의 연결 여부를 갖는 Line 클래스 생성
    - 사다리의 라인이 겹치는지 확인하는 기능

--- src/main/java/Line.java
+++ src/main/java/Line.java
@@ -1,9 +1,26 @@
-public class Line {
+import java.util.List;
 
-    public Line() {
+public class Line {
+    private final List<Boolean> lines;
+
+    public Line(List<Boolean> lines) {
+        this.lines = lines;
     }
 
+    //TODO : 네이밍
     public void method() {
-        throw new IllegalArgumentException();
+        boolean state = false;
+        for (boolean line : lines) {
+            state = updateState(state, line);
+        }
+    }
+
+    //TODO : 네이밍
+    private boolean updateState(boolean state, boolean line) {
+        if (line && state) {
+            throw new IllegalArgumentException();
+        }
+        state = line;
+        return state;
     }
 }
--- src/test/java/LineTest.java
+++ src/test/java/LineTest.java
@@ -2,11 +2,21 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 public class LineTest {
     @Test
-    @DisplayName("Ladder의 라인이 겸치는지 확인하는 기능")
+    @DisplayName("Ladder의 라인이 겸치지 않으면 정상 작동")
+    //TODO : 메서드명 고민하기
+    void unContinuousLineTest() {
+        Line line = new Line(List.of(true, false, true));
+        Assertions.assertDoesNotThrow(line::method);
+    }
+
+    @Test
+    @DisplayName("Ladder의 라인이 겸치면 예외 처리")
     void continuousLineTest() {
-        Line line = new Line();
+        Line line = new Line(List.of(true, true, true));
         Assertions.assertThrows(IllegalArgumentException.class, line::method);
     }
 }
