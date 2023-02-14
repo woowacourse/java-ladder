b27ca96140b7479f4ad4cdb38e98bbcbd0ed97e2
dev_kong <ahdjd5@gmail.com>
2023-02-14 18:39:48 +0900

    feat: StringParser class splitByComma 메서드 작성

---
+++ src/main/java/utils/StringParser.java
@@ -0,0 +1,10 @@
+package utils;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StringParser {
+    public static List<String> splitByComma(String input) {
+        return Arrays.asList(input.split(","));
+    }
+}
