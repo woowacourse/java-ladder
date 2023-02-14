187cab7ffd60b4c9d8d8de1ce012b716225226cd
greeng00se <thegreengoose7@gmail.com>
2023-02-14 17:18:58 +0900

    feat: 참가자 생성 기능 구현
    Co-authored-by: wugawuga <dnr3655@naver.com>

---
+++ src/main/java/ladder/domain/Player.java
@@ -0,0 +1,27 @@
+package ladder.domain;
+
+public class Player {
+    private static final String INVALID_NAME_LENGTH_MESSAGE = "이름은 1자 이상, 5자 이하여야 한다.";
+    private static final int NAME_LENGTH_UPPER_BOUND = 5;
+
+    private final String name;
+
+    public Player(final String name) {
+        validate(name);
+        this.name = name;
+    }
+
+    private void validate(final String name) {
+        if (isInvalidNameLength(name)) {
+            throw new IllegalArgumentException(INVALID_NAME_LENGTH_MESSAGE);
+        }
+    }
+
+    private boolean isInvalidNameLength(final String name) {
+        return name.isBlank() || NAME_LENGTH_UPPER_BOUND < name.length();
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/test/java/ladder/domain/PlayerTest.java
@@ -0,0 +1,32 @@
+package ladder.domain;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+@SuppressWarnings({"InnerClassMayBeStatic", "NonAsciiCharacters"})
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+public class PlayerTest {
+
+    @Test
+    void 참여자는_이름을_가진다() {
+        final Player player = new Player("name");
+
+        assertThat(player.getName()).isEqualTo("name");
+    }
+
+    @ParameterizedTest(name = "이름이 비어있거나 6자 이상인 경우 예외를 던진다. 입력값: \"{0}\"")
+    @ValueSource(strings = {"", "우아한형제들"})
+    void 이름이_비어있거나_6자_이상인_경우_예외를_던진다(final String name) {
+        assertThatThrownBy(() -> new Player(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이름은 1자 이상, 5자 이하여야 한다.");
+    }
+}
