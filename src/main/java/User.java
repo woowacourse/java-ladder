a083c735a68c792fb115629062981e329584ac2c
jjongwa <troas96@naver.com>
2023-02-14 17:34:56 +0900

    feat: 이름 형식 검증 기능 추가

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -3,6 +3,7 @@ public class User {
     private static final int MAX_NAME_LENGTH = 5;
     private static final String NAME_LENGTH_ERROR_MESSAGE =
             "[ERROR] 사람 이름은 " + MIN_NAME_LENGTH + "~" + MAX_NAME_LENGTH + "글자로 입력해 주세요.";
+    private static final String NAME_FORMAT_ERROR_MESSAGE = "[ERROR] 사람 이름은 영문자만 가능합니다.";
 
     private String name;
 
@@ -14,5 +15,11 @@ public class User {
 
     private static boolean isValidLength(String name) {
         return name.length() < MIN_NAME_LENGTH || name.length() > MAX_NAME_LENGTH;
+    }
+
+    public static void validateNameFormat(String name) {
+        if (!name.matches("^[a-zA-z]*$")) {
+            throw new IllegalArgumentException(NAME_FORMAT_ERROR_MESSAGE);
+        }
     }
 }
