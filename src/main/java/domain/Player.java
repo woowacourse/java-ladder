00ec83187cc4d8217f71d45889fd9cfd63ff207f
seokjin8678 <rnrn8678@naver.com>
2023-02-14 17:03:35 +0900

    feat(Player): 사용자의 이름에 빈 값이 들어간 경우 예외 발생 기능 추가

--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -5,6 +5,7 @@ public class Player {
     private final String name;
 
     public Player(String name) {
+        validateNull(name);
         validateBlank(name);
         this.name = name;
     }
@@ -12,6 +13,12 @@ public class Player {
     private void validateBlank(String name) {
         if (name.isBlank()) {
             throw new IllegalArgumentException("[ERROR] 이름은 공백이 될 수 없습니다.");
+        }
+    }
+
+    private void validateNull(String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("[ERROR] 이름에 null 값이 들어갈 수 없습니다.");
         }
     }
 
