fefbf68d96c8dffc689acd253c9be13cafb0ec2d
thdwoqor <celebarte01@gmail.com>
2023-02-14 17:43:35 +0900

    feat : 입력된 유저 수 유효성 검사 코드 작성

---
+++ src/main/java/Users.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+public class Users {
+    public Users(List<String> a) {
+        if (a.size() < 2 || a.size() > 10) {
+            throw new IllegalArgumentException("유저는 최소 2명 이상이다.");
+        }
+    }
+}
