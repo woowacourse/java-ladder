64bd5fe75393edec1f24877b6ed9be76c53f432d
dev_kong <ahdjd5@gmail.com>
2023-02-14 18:39:23 +0900

    test: splitByComma 테스트 코드 작성

---
+++ src/test/java/utils/StringParserTest.java
@@ -0,0 +1,23 @@
+package utils;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class StringParserTest {
+    @Test
+    @DisplayName("콤마를 기준으로 사람이름을 분리한다.")
+    void splitByCommaTest() {
+        String input = "pobi,honux,crong,jk";
+
+        List<String> strings = StringParser.splitByComma(input);
+
+        assertThat(strings).containsExactly("pobi", "honux", "crong", "jk");
+    }
+
+}
