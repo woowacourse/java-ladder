e0d49cfb12de22548315f95e1b52d0aa7629588b
yoondgu <doyou2ing@naver.com>
2023-02-14 17:39:20 +0900

    feat: 사다리 라인의 참가자 인원에 대한 예외 추가
    Co-authored-by: choi-jjunho <junho5336@gmail.com>

--- src/main/java/ladder/domain/Line.java
+++ src/main/java/ladder/domain/Line.java
@@ -9,8 +9,15 @@ public class Line {
     private List<Boolean> points;
 
     public Line(int playerCount) {
+        validatePlayerCount(playerCount);
         points = new ArrayList<>();
         initializePoints(playerCount);
+    }
+
+    private void validatePlayerCount(int playerCount) {
+        if (playerCount <= 1) {
+            throw new IllegalArgumentException("참가자는 1명이하일 수 없습니다.");
+        }
     }
 
     private void initializePoints(int count) {
--- src/test/java/ladder/domain/LineTest.java
+++ src/test/java/ladder/domain/LineTest.java
@@ -1,6 +1,8 @@
 package ladder.domain;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import org.junit.jupiter.api.Test;
 
 class LineTest {
@@ -9,7 +11,15 @@ class LineTest {
     void 라인은_주어진_사람보다_하나_적은_좌표값을_가진다() {
         Line line = new Line(5);
 
-        Assertions.assertThat(line.toUnmodifiablePoints())
+        assertThat(line.toUnmodifiablePoints())
                 .hasSize(4);
+    }
+
+    @Test
+    void 주어진_사람이_한명_이하면_예외() {
+        assertThatThrownBy(() -> {
+            new Line(1);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("참가자는 1명이하일 수 없습니다.");
     }
 }
