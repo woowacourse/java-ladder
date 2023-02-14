f822d175b6d1d49d1d28399e66b11f0cb175d3cf
seokjin8678 <rnrn8678@naver.com>
2023-02-14 17:41:15 +0900

    test(Players): 사용자의 수가 범위를 벗어나는 경우에 대한 테스트 코드 추가

--- src/test/java/domain/PlayersTest.java
+++ src/test/java/domain/PlayersTest.java
@@ -30,4 +30,20 @@ class PlayersTest {
             new Players(input);
         }).withMessage("[ERROR] 중복된 이름이 있습니다.");
     }
+
+    @Test
+    @DisplayName("사용자가 13명을 초과하면 예외가 발생한다.")
+    void create_overMaxPlayers() {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            new Players("1,2,3,4,5,6,7,8,9,10,11,12,13,14");
+        }).withMessage("[ERROR] 사용자는 2명에서 13명까지 가능합니다.");
+    }
+
+    @Test
+    @DisplayName("사용자가 2명 미만이면 예외가 발생한다.")
+    void create_underMinPlayers() {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            new Players("1");
+        }).withMessage("[ERROR] 사용자는 2명에서 13명까지 가능합니다.");
+    }
 }
