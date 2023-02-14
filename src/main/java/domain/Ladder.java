52cb05d06117db5071cc9d72ad229500f12132df
seokjin8678 <rnrn8678@naver.com>
2023-02-14 18:19:08 +0900

    feat(Ladder): 입력된 사다리의 높이가 인원 수의 범위를 초과할 때 예외 발생 기능 추가

--- src/main/java/domain/Ladder.java
+++ src/main/java/domain/Ladder.java
@@ -5,14 +5,21 @@ public class Ladder {
 
     private final int height;
 
-    public Ladder(int height) {
+    public Ladder(int height, int playersCount) {
         validatePositive(height);
+        validatePlayersCount(height, playersCount);
         this.height = height;
     }
 
     private void validatePositive(int height) {
         if (height <= MIN_HEIGHT) {
             throw new IllegalArgumentException("[ERROR] 사다리의 높이는 양의 정수여야 합니다.");
+        }
+    }
+
+    private void validatePlayersCount(int height, int playersCount) {
+        if (height < playersCount || height > playersCount * 2) {
+            throw new IllegalArgumentException("[ERROR] 사다리의 높이는 사람 수보다 크거나, 사람 수의 두 배 보다 작아야 합니다.");
         }
     }
 }
