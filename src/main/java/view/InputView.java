903b76dd6ffdb955180abbbca773d0537f7f9cfa
MoonJeWoong <munjin0201@naver.com>
2023-02-14 18:21:48 +0900

    feat: 게임 참여자 이름을 입력하는 기능 추가
    세부사항
    참여자 이름은 쉼표를 기준으로 입력

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,4 +1,19 @@
 package view;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class InputView {
+    private static final String SPLIT_DELIMITER = ",";
+    private Scanner scanner = new Scanner(System.in);
+
+    public List<String> readPlayerNames(){
+        String names = scanner.nextLine();
+        return splitPlayerNames(names);
+    }
+
+    private List<String> splitPlayerNames(String names){
+        return Arrays.asList(names.split(SPLIT_DELIMITER));
+    }
 }
