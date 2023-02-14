2127b9b282c0112f882f0e65cabdc2d8e13afa27
MoonJeWoong <munjin0201@naver.com>
2023-02-14 17:49:37 +0900

    feat: 사람 이름 최대 5글자로 제한하는 기능 추가

---
+++ src/main/java/model/Name.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Name {
+    private static final int MAXIMUM_NAME_LENGTH = 5;
+
+    private final String name;
+
+    public Name(String name){
+        validateNameLength(name);
+        this.name = name;
+    }
+
+    private void validateNameLength(String name){
+        if(name.length() > MAXIMUM_NAME_LENGTH) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+}
