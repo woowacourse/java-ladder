6a5794f49ad6e1118edaa71188dbcef11fd1810c
Cyma-s <ssol0319@naver.com>
2023-02-14 16:39:29 +0900

    test: Name 테스트 코드 추가

---
+++ src/test/java/ladder/domain/NameTest.java
@@ -0,0 +1,37 @@
+package ladder.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class NameTest {
+    @ParameterizedTest
+    @ValueSource(strings = {"\n", "\t", "  ", ""})
+    @DisplayName("이름이 빈 문자열이면 예외가 발생해야 한다.")
+    void name_throwExceptionIfEmptyString(String input) {
+        // expected
+        assertThatThrownBy(() -> new Name(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"aaaaaa", "akdfjqkf"})
+    @DisplayName("이름은 6글자 이상이면 예외가 발생해야 한다.")
+    void name_throwExceptionOver6(String input) {
+        // expected
+        assertThatThrownBy(() -> new Name(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"dochi", "vero", "a"})
+    @DisplayName("이름은 최소 1글자, 최대 5글자이다.")
+    void name_success(String input) {
+        // expected
+        assertThatCode(() -> new Name(input))
+                .doesNotThrowAnyException();
+    }
+}
