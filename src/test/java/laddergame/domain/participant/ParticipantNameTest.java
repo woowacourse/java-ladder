5b893b07ba9193a55f8bba2f5f9b23b4ddc02719
jiwon <ljwon77@daum.net>
2023-02-14 17:37:43 +0900

    test: 참여자 이름에 대한 테스트 코드를 작성한다
    내용 :
    한 글자에서 다섯 글자 범위의 이름에 대한 검증을 테스트한다.
    공백 이름에 대한 검증을 테스트한다.

--- src/test/java/laddergame/domain/participant/ParticipantNameTest.java
+++ src/test/java/laddergame/domain/participant/ParticipantNameTest.java
@@ -1,24 +1,37 @@
 package laddergame.domain.participant;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 public class ParticipantNameTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"orange", "chanmin", "elephant"})
-    @DisplayName("")
+    @DisplayName("다섯 글자 초과한 이름이 입력되면, 예외가 발생한다.")
     void name_error_test(String errorName) {
         assertThatThrownBy(() -> new ParticipantName(errorName))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR]");
     }
-    
 
+    @ParameterizedTest
+    @ValueSource(strings = {"pobi", "i", "JiWon", "우테코", "12345"})
+    @DisplayName("한 글자에서 다섯 글자 범위의 이름이 입력되면, 예외가 발생하지 않는다.")
+    void name_success_test(String validName) {
+        assertDoesNotThrow(() -> new ParticipantName(validName));
+    }
 
-
+    @ParameterizedTest
+    @EmptySource
+    @DisplayName("공백이 입력되면, 예외가 발생한다.")
+    void name_blank_test(String blankName) {
+        assertThatThrownBy(() -> new ParticipantName(blankName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
+    }
 }
