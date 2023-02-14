48dd10828280bfec835e1ee2550b372d324d3f13
yujamint <woawoa0513@gmail.com>
2023-02-14 17:47:58 +0900

    refactor: 공백을 포함하는 이름에 대한 검증 및 수정

--- src/main/java/domain/Name.java
+++ src/main/java/domain/Name.java
@@ -5,9 +5,24 @@ public class Name {
     private final String name;
 
     public Name(String name) {
+        validateName(name);
+        this.name = name;
+    }
+
+    private void validateName(String name) {
+        validateNameLength(name);
+        validateNameFormat(name);
+    }
+
+    private void validateNameLength(String name) {
         if (name.length() > 5 || name.length() < 1) {
             throw new IllegalArgumentException();
         }
-        this.name = name;
+    }
+
+    private void validateNameFormat(String name) {
+        if (name.contains(" ")) {
+            throw new IllegalArgumentException();
+        }
     }
 }
