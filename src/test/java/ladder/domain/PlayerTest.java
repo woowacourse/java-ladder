3eb9db858a10168592468b37aee8696ef10fc19d
greeng00se <thegreengoose7@gmail.com>
2023-02-14 17:25:49 +0900

    remove: 사용하지 않는 SuppressWarning 제거
    Co-authored-by: wugawuga <dnr3655@naver.com>

--- src/test/java/ladder/domain/PlayerTest.java
+++ src/test/java/ladder/domain/PlayerTest.java
@@ -1,17 +1,15 @@
 package ladder.domain;
 
-import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-@SuppressWarnings({"InnerClassMayBeStatic", "NonAsciiCharacters"})
+@SuppressWarnings("NonAsciiCharacters")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class PlayerTest {
 
