8c6b56f7b19842b8250e2e4c3d9edef4229c0d4b
seokjin8678 <rnrn8678@naver.com>
2023-02-14 18:03:06 +0900

    refactor(Ladder): 최소 높이를 상수로 추출

--- src/main/java/domain/Ladder.java
+++ src/main/java/domain/Ladder.java
@@ -1,6 +1,8 @@
 package domain;
 
 public class Ladder {
+    public static final int MIN_HEIGHT = 0;
+
     private final int height;
 
     public Ladder(int height) {
@@ -9,7 +11,7 @@ public class Ladder {
     }
 
     private void validatePositive(int height) {
-        if (height <= 0) {
+        if (height <= MIN_HEIGHT) {
             throw new IllegalArgumentException("[ERROR] 사다리의 높이는 양의 정수여야 합니다.");
         }
     }
