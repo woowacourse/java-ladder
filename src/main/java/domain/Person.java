05a72e732161138194e3c0e457e261f05e29d1cd
This2sho <zx00018@naver.com>
2023-02-14 20:06:30 +0900

    feat(InputView): 입력 값에 대한 검증 로직 구현

---
+++ src/main/java/domain/Person.java
@@ -0,0 +1,19 @@
+package domain;
+
+public class Person {
+
+    public static final int MAX_LENGTH = 5;
+
+    private final String name;
+
+    public Person(String name) {
+        validateNameLength(name);
+        this.name = name;
+    }
+
+    private void validateNameLength(String name) {
+        if (name.isBlank() || name.length() > MAX_LENGTH) {
+            throw new IllegalArgumentException("이름은 1자 이상 5자 이하여야 합니다");
+        }
+    }
+}
---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,45 @@
+package view;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class InputView {
+
+    private static final Scanner scanner = new Scanner(System.in);
+    public static final String DELIMITER = ",";
+    public static final int MIN_PERSON_COUNT = 2;
+
+    public List<String> readNames() {
+        String input = readLine();
+        validateDelimiter(input);
+        List<String> result = Arrays.stream(input.split(DELIMITER))
+            .collect(Collectors.toList());
+        validatePersonCount(result);
+        validateDuplicate(result);
+        return result;
+    }
+
+    private String readLine() {
+        return scanner.nextLine();
+    }
+
+    private void validateDelimiter(String input) {
+        if (input.endsWith(",")) {
+            throw new IllegalArgumentException(",로 끝날 수 없습니다");
+        }
+    }
+
+    private void validatePersonCount(List<String> result) {
+        if (result.size() < MIN_PERSON_COUNT) {
+            throw new IllegalArgumentException("사람은 두 명 이상이어야 합니다.");
+        }
+    }
+
+    private void validateDuplicate(List<String> result) {
+        if (result.size() != result.stream().distinct().count()) {
+            throw new IllegalArgumentException("중복된 이름이 존재합니다");
+        }
+    }
+}
