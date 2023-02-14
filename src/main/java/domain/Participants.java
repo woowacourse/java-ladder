d118d37671c153f9b1ee7904763fd85dd28e66bc
splitube <voicesplit@daum.net>
2023-02-14 20:19:35 +0900

    refactor: 참가자 검증 메서드 분리

--- src/main/java/domain/Participants.java
+++ src/main/java/domain/Participants.java
@@ -7,17 +7,29 @@ import java.util.List;
 public class Participants {
 
     public Participants(String participantNames) {
-        final int maxParticipants = 10;
+        validate(participantNames);
+    }
 
-        if (participantNames == null || participantNames.isBlank()) {
+    private void validate(String participantNames) {
+        if (!isExist(participantNames)) {
             throw new EmpytInputException();
         }
-
-        List<String> names = List.of(participantNames.split(",", -1));
-        if (names.size() > maxParticipants) {
+        if (isValidCount(splitNames(participantNames))) {
             throw new InvalidParticipantsCountException();
         }
     }
 
+    private boolean isExist(String participantNames) {
+        return !(participantNames == null || participantNames.isBlank());
+    }
 
+    private boolean isValidCount(List<String> names) {
+        final int maxParticipantCount = 10;
+        return names.size() > maxParticipantCount;
+    }
+
+    private List<String> splitNames(String participantNames) {
+        final String delimiter = ",";
+        return List.of(participantNames.split(delimiter, -1));
+    }
 }
