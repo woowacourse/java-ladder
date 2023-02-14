68a1bb4bd57ce1715c0c6a765a5d92eb444f3ce4
kevstevie <jianna442@gmail.com>
2023-02-14 18:29:58 +0900

    feat: Line 객체 생성

---
+++ src/main/java/ladder/domain/Line.java
@@ -0,0 +1,12 @@
+package ladder.domain;
+
+public class Line {
+
+    private boolean isExist = false;
+
+    public void a(int number) {
+        if (number == 1) {
+            isExist = true;
+        }
+    }
+}
---
+++ src/test/java/ladder/domain/LineTest.java
@@ -0,0 +1,19 @@
+package ladder.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LineTest {
+
+    @Test
+    @DisplayName("1을 받으면 true가 된다.")
+    void makeTrue() {
+        Line line = new Line();
+        line.a(1);
+
+        Assertions.assertThat(line)
+                .extracting("isExist")
+                .isEqualTo(true);
+    }
+}
