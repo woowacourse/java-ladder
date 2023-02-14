9a222de192ddad94479258b83638a650325da878
MoonJeWoong <munjin0201@naver.com>
2023-02-14 18:39:44 +0900

    feat: LadderHeight가 1 미만일 경우 실패하는 기능 추가

--- src/main/java/model/LadderHeight.java
+++ src/main/java/model/LadderHeight.java
@@ -2,9 +2,17 @@ package model;
 
 public class LadderHeight {
 
+    private static final int MINIMUM_LADDER_HEIGHT = 1;
     private final int height;
 
     public LadderHeight(int height){
+        validateHeight(height);
         this.height = height;
+    }
+
+    private void validateHeight(int height) {
+        if(height < MINIMUM_LADDER_HEIGHT) {
+            throw new IllegalArgumentException();
+        }
     }
 }
