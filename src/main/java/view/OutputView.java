3a2c8b6bd8b945c5d045817a8c735842c9ec3f72
MoonJeWoong <munjin0201@naver.com>
2023-02-14 19:06:58 +0900

    feat: 최대 사다리 높이 입력 메세지 기능 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -3,8 +3,13 @@ package view;
 public class OutputView {
 
     private static final String PLAYER_NAME_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    private static final String LADDER_HEIGHT_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
 
     public void printPlayerNamesMessage() {
         System.out.println(PLAYER_NAME_MESSAGE);
+    }
+
+    public void printLadderHeightMessage() {
+        System.out.println(LADDER_HEIGHT_MESSAGE);
     }
 }
