e0f6c7bf736e66e5ab2e89133f36e4ab19cb293f
Cyma-s <ssol0319@naver.com>
2023-02-14 16:42:40 +0900

    feat: 이름 생성 기능 구현
    - Name의 검증 기능 구현

---
+++ src/main/java/ladder/domain/Name.java
@@ -0,0 +1,24 @@
+package ladder.domain;
+
+public class Name {
+    private static final int MAX_SIZE = 6;
+    private final String name;
+
+    public Name(String name) {
+        validateEmptyName(name);
+        validateOutOfNameLength(name);
+        this.name = name;
+    }
+
+    private void validateEmptyName(String name) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("이름은 빈 문자일 수 없습니다.");
+        }
+    }
+
+    private void validateOutOfNameLength(String name) {
+        if(name.length() >= MAX_SIZE) {
+            throw new IllegalArgumentException("이름은 최소 1글자, 최대 5글자여야 합니다.");
+        }
+    }
+}
