2a96229456b49dfd93032624ed42aa86a6359fad
seokjin8678 <rnrn8678@naver.com>
2023-02-14 18:00:17 +0900

    feat(Ladder): 입력된 사다리의 높이가 양의 정수가 아니면 예외 발생 기능 추가

--- src/main/java/domain/Ladder.java
+++ src/main/java/domain/Ladder.java
@@ -1,4 +1,16 @@
 package domain;
 
 public class Ladder {
+    private final int height;
+
+    public Ladder(int height) {
+        validatePositive(height);
+        this.height = height;
+    }
+
+    private void validatePositive(int height) {
+        if (height <= 0) {
+            throw new IllegalArgumentException("[ERROR] 사다리의 높이는 양의 정수여야 합니다.");
+        }
+    }
 }
