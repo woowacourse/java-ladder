5e687b46e31e9f8a6853b9fe22b9b6e968aa9e1b
thdwoqor <celebarte01@gmail.com>
2023-02-14 17:58:12 +0900

    test : 사다리 높이 유효성 검사 성공 테스트 작성

--- src/test/java/LadderTest.java
+++ src/test/java/LadderTest.java
@@ -3,13 +3,22 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class LadderTest {
 
     @ParameterizedTest
-    @ValueSource(ints = {0,11})
+    @ValueSource(ints = {0, 11})
     @DisplayName("사다리 높이가 1~10을 벗어나면 예외가 발생한다.")
-    void LadderHeightTest(int height){
+    void LadderHeightFailTest(int height) {
         Assertions.assertThrows(IllegalArgumentException.class,
-                ()-> new Ladder(height));
+                () -> new Ladder(height));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 10})
+    @DisplayName("사다리 높이가 1~10 사이면 정상적으로 수행된다.")
+    void LadderHeightSuccessTest(int height) {
+        assertThatCode(() -> new Ladder(height)).doesNotThrowAnyException();
     }
 }
