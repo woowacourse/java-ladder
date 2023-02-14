e84cba041656a761113aa4a0cdff16612ac74185
seokjin8678 <rnrn8678@naver.com>
2023-02-14 16:41:15 +0900

    test(PlayerTest): 사용자의 이름에 빈 값이 들어가면 예외 발생 테스트 추가

--- src/test/java/domain/PlayerTest.java
+++ src/test/java/domain/PlayerTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class PlayerTest {
 
@@ -16,5 +18,15 @@ class PlayerTest {
         // expect
         assertThat(glen.getName())
                 .isEqualTo("glen");
+    }
+
+    @ParameterizedTest
+    @DisplayName("사용자의 이름에 빈 값이 들어가면 예외가 발생한다.")
+    @ValueSource(strings = {"", " ", "\n", "  "})
+    void create_blankName(String input) {
+        // expect
+        assertThatThrownBy(() -> new Player(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 이름은 공백이 될 수 없습니다.");
     }
 }
