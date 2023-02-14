23e4260a393ae61714f87f85c7fb894fbf2c3996
MoonJeWoong <munjin0201@naver.com>
2023-02-14 18:55:11 +0900

    feat: Application 추가

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,13 @@
-public class Application {
-    public static void main(String[] args) {
+import controller.Controller;
+import view.InputView;
+import view.OutputView;
 
+import java.util.Scanner;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Controller controller = new Controller( new InputView(new Scanner(System.in)),new OutputView());
+        controller.run();
     }
 }
