79643efe55b096e3e766568c583904e5ba1868c8
Miseong Kim <miseongk@naver.com>
2023-02-14 17:43:51 +0900

    test: Height 객체 생성 확인 및 예외처리

---
+++ src/test/java/HeightTest.java
@@ -0,0 +1,27 @@
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class HeightTest {
+
+    @ParameterizedTest
+    @DisplayName("높이가 양수가 아닌 경우 예외 확인")
+    @ValueSource(ints = {-100, -5, -1, 0})
+    void errorNegativeNumber(int height) {
+        Assertions.assertThatThrownBy(() -> new Height(height))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 높이는 양수만 가능합니다.");
+    }
+
+
+    @Test
+    @DisplayName("높이가 양수 인 경우 정상적인 객체 생성 확인")
+    void nonErrorPositiveNumber() {
+        int height = 1;
+        assertDoesNotThrow(() -> new Height(height));
+    }
+}
