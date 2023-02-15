7a9a35bf89f5e5317086246be6cb469ee577d53e
Eunkeee <eunkeeeee@gmail.com>
2023-02-15 17:34:56 +0900

    test(Line): 테스트 코드 삭제

--- src/test/java/domain/LineTest.java
+++ src/test/java/domain/LineTest.java
@@ -1,13 +1,10 @@
 package domain;
 
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 import utils.booleanGenerator.BooleanGenerator;
 import utils.booleanGenerator.RandomBooleanGenerator;
 import utils.booleanGenerator.TrueBooleanGenerator;
@@ -15,26 +12,6 @@ import utils.booleanGenerator.TrueBooleanGenerator;
 class LineTest {
     private final BooleanGenerator randomBooleanGenerator = new RandomBooleanGenerator();
     private final BooleanGenerator trueBooleanGenerator = new TrueBooleanGenerator();
-
-    @Nested
-    class personCountTest {
-
-
-        @DisplayName("참가자의 수는 2명 이상 100명 이하이다.")
-        @ParameterizedTest
-        @ValueSource(ints = {2, 100})
-        void validPersonCountTest(int personCount) {
-            assertDoesNotThrow(() -> new Line(personCount, randomBooleanGenerator));
-        }
-
-        @DisplayName("1명 이하, 101명 이상의 참가자는 예외 처리한다.")
-        @ParameterizedTest
-        @ValueSource(ints = {0, 1, 101, 102})
-        void invalidPersonCountTest(int personCount) {
-            assertThatIllegalArgumentException()
-                    .isThrownBy(() -> new Line(personCount, randomBooleanGenerator));
-        }
-    }
 
     @Nested
     class pointsTest {
