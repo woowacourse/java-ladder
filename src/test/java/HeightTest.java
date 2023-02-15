8a3ca33e79d0c861bb1331c1e11a75d7d8581847
ingpyo <juninpyo726@gmail.com>
2023-02-15 16:41:28 +0900

    test: 입력값이 0이하 인지 예외 테스트

--- src/test/java/HeightTest.java
+++ src/test/java/HeightTest.java
@@ -2,6 +2,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatNoException;
 
 class HeightTest {
@@ -21,6 +22,12 @@ class HeightTest {
     @Test
     void nonDigitFail1() {
         Assertions.assertThatThrownBy(() -> new Height("abc")).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("높이가 숫자가 0이하 일 때 에러")
+    @Test
+    void nonDigitFail2() {
+        Assertions.assertThatThrownBy(() -> new Height("0")).isInstanceOf(IllegalArgumentException.class);
     }
 
 
