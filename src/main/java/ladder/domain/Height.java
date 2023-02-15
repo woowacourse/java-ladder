e1daab7d29f9c3107c06b66273b5263afbe8aef7
Cyma-s <ssol0319@naver.com>
2023-02-15 16:27:08 +0900

    feat: 사다리 높이 생성 기능 구현

---
+++ src/main/java/ladder/domain/Height.java
@@ -0,0 +1,24 @@
+package ladder.domain;
+
+public class Height {
+
+    private final int height;
+
+    public Height(int height) {
+        validateNegative(height);
+        validateTooSmall(height);
+        this.height = height;
+    }
+
+    private void validateNegative(int height) {
+        if (height < 0) {
+            throw new IllegalArgumentException("높이는 음수일 수 없습니다.");
+        }
+    }
+
+    private void validateTooSmall(int height) {
+        if(height <= 1) {
+            throw new IllegalArgumentException("높이는 2 이상이어야 합니다.");
+        }
+    }
+}
