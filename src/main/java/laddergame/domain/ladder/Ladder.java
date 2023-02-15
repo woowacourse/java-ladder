c91fd4bf55a58f25d91b6341c0a89f53cf41c398
jiwon <ljwon77@daum.net>
2023-02-15 17:37:40 +0900

    feat: LadderFactory 클래스의 Ladder 생성 기능을 구현한다

---
+++ src/main/java/laddergame/domain/ladder/Ladder.java
@@ -0,0 +1,14 @@
+package laddergame.domain.ladder;
+
+import laddergame.domain.rung.Rungs;
+
+import java.util.List;
+
+public class Ladder {
+
+    private final List<Rungs> ladder;
+
+    public Ladder(final List<Rungs> ladder) {
+        this.ladder = ladder;
+    }
+}
--- src/main/java/laddergame/domain/ladder/LadderFactory.java
+++ src/main/java/laddergame/domain/ladder/LadderFactory.java
@@ -1,13 +1,39 @@
 package laddergame.domain.ladder;
 
+import laddergame.domain.rung.Rungs;
+import laddergame.util.NumberGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LadderFactory {
 
-    private static final int MIN_RANGE = 1;
-    private static final int MAX_RANGE = 10_000;
+    private static final int MIN_HEIGHT = 1;
+    private static final int MAX_HEIGHT = 10_000;
+    private static final int DEFAULT_COUNT = 1;
+    private final NumberGenerator rungNumberGenerator;
 
-    public void createLadder(String height) {
+    public LadderFactory(final NumberGenerator rungNumberGenerator) {
+        this.rungNumberGenerator = rungNumberGenerator;
+    }
+
+    public Ladder createLadder(final String height, final int participantCount) {
+        int ladderHeight = convertLadderHeight(height);
+        List<Rungs> ladder = new ArrayList<>();
+        for (int i = 0; i < ladderHeight; i++) {
+           ladder.add(new Rungs(makeRungCount(participantCount), rungNumberGenerator));
+        }
+        return new Ladder(ladder);
+    }
+
+    private int makeRungCount(final int participantCount) {
+        return participantCount - DEFAULT_COUNT;
+    }
+
+    private int convertLadderHeight(final String height) {
         int ladderHeight = validateHeightType(height);
         validateHeightRange(ladderHeight);
+        return ladderHeight;
     }
 
     private int validateHeightType(final String height) {
@@ -19,7 +45,7 @@ public class LadderFactory {
     }
 
     private void validateHeightRange(final int height) {
-        if (height < MIN_RANGE || height > MAX_RANGE) {
+        if (height < MIN_HEIGHT || height > MAX_HEIGHT) {
             throw new IllegalArgumentException("[ERROR] 사다리 높이는 1~10000 사이의 값만 가질 수 있습니다.");
         }
     }
