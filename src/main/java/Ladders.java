d4c65bdb92135fb63d1ac7837b16ecd58e6deb12
thdwoqor <celebarte01@gmail.com>
2023-02-15 16:18:57 +0900

    feat : 전체 사다리 생성하는 코드 작성

--- src/main/java/Ladders.java
+++ src/main/java/Ladders.java
@@ -1,9 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Ladders {
     private final int height;
-    Ladders(int height){
+    private final List<Ladder> ladders;
+    private final RandomGenerator randomGenerator;
+
+    Ladders(int height, RandomGenerator randomGenerator) {
         if (height < 1 || height > 10) {
             throw new IllegalArgumentException("사다리 높이는 1에서 10");
         }
         this.height = height;
+        this.ladders = new ArrayList<>();
+        this.randomGenerator = randomGenerator;
+    }
+
+    public void make(int width) {
+        for (int index = 0; index < height; index++) {
+            ladders.add(new Ladder(randomGenerator.generateLadder(width)));
+        }
     }
 }
