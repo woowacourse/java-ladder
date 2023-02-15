6ff8fca8adbf417485b8d08f0e5adbddce59baf4
chaewon121 <cw121@naver.com>
2023-02-15 14:29:54 +0900

    refactor: 방향을 정하는 객체 수정

--- src/main/java/ladder/domain/Direction.java
+++ src/main/java/ladder/domain/Direction.java
@@ -1,8 +1,8 @@
 package ladder.domain;
 
 public class Direction {
-    private Bar leftBar;
-    private Bar rightBar;
+    private final Bar leftBar;
+    private final Bar rightBar;
 
     private Direction(Bar leftBar, Bar rightBar) {
         this.leftBar = leftBar;
@@ -13,16 +13,17 @@ public class Direction {
         return new Direction(new Bar(() -> false), new Bar(barGenerator));
     }
 
-    public static Direction createMiddle(Bar leftBar, BarGenerator barGenerator) {
-        if (leftBar.isExistBar()) {
-            return new Direction(leftBar, new Bar(() -> false));
+    public Direction createNext(BarGenerator barGenerator) {
+        Bar newNextLeftBar = rightBar;
+        if (rightBar.isExistBar()) {
+            return new Direction(newNextLeftBar, new Bar(() -> false));
         }
-
-        return new Direction(leftBar, new Bar(barGenerator));
+        return new Direction(newNextLeftBar, new Bar(barGenerator));
     }
 
-    public static Direction createFinal(Bar leftBar) {
-        return new Direction(leftBar, new Bar(() -> false));
+    public Direction createFinal() {
+        Bar newNextLeftBar = rightBar;
+        return new Direction(newNextLeftBar, new Bar(() -> false));
     }
 
     public boolean isExistLeftBar() {
--- src/test/java/ladder/domain/DirectionTest.java
+++ src/test/java/ladder/domain/DirectionTest.java
@@ -1,5 +1,6 @@
 package ladder.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -7,38 +8,47 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class DirectionTest {
+    private Direction firstDirectionFalse;
+    private Direction firstDirectionTrue;
+    private Direction middleDirectionTrue;
+    private Direction middleDirectionFalse;
+
+    @BeforeEach
+    void setUp() {
+        firstDirectionFalse = Direction.createFirst(() -> false);
+        firstDirectionTrue = Direction.createFirst(() -> true);
+        middleDirectionTrue = firstDirectionFalse.createNext(() -> true);
+        middleDirectionFalse = firstDirectionFalse.createNext(() -> false);
+    }
 
     @Test
     @DisplayName("createFirst()는 오른쪽 바가 랜덤값을 받아 생성된다.")
     void test_1() {
-        // given, when
-        Direction direction = Direction.createFirst(() -> true);
-
         // then
         assertAll(
-                () -> assertThat(direction.isExistLeftBar()).isFalse(),
-                () -> assertThat(direction.isExistRightBar()).isTrue()
+                () -> assertThat(firstDirectionFalse.isExistLeftBar()).isFalse(),
+                () -> assertThat(firstDirectionFalse.isExistRightBar()).isFalse()
         );
     }
 
     @Test
-    @DisplayName("createMiddle()는 이전 바의 오른쪽 값으로 false를 받았을때 랜덤값으로 오른쪽 바를 생성한다.")
+    @DisplayName("createNext()는 이전 바의 오른쪽 값으로 false를 받았을때 랜덤값으로 오른쪽 바를 생성한다.")
     void test_2() {
         // given, when
-        Direction direction = Direction.createMiddle(new Bar(() -> false), () -> true);
+        Direction direction = firstDirectionFalse.createNext(() -> false);
 
         // then
         assertAll(
                 () -> assertThat(direction.isExistLeftBar()).isFalse(),
-                () -> assertThat(direction.isExistRightBar()).isTrue()
+                () -> assertThat(direction.isExistRightBar()).isFalse()
         );
     }
 
     @Test
-    @DisplayName("createMiddle()는 이전 바의 오른쪽 값으로 true를 받았을때 오른쪽 값으로 무조건 false를 갖는다.")
+    @DisplayName("createNext()는 이전 바의 오른쪽 값으로 true를 받았을때 오른쪽 값으로 무조건 false를 갖는다.")
     void test_3() {
         // given, when
-        Direction direction = Direction.createMiddle(new Bar(() -> true), () -> true);
+        Direction direction = firstDirectionTrue.createNext(() -> true);
 
         // then
         assertAll(
@@ -48,10 +58,23 @@ public class DirectionTest {
     }
 
     @Test
-    @DisplayName("createMiddle()는 이전 바의 오른쪽 값으로 true를 받았을때 오른쪽 값으로 무조건 false를 갖는다.")
+    @DisplayName("createFinal()는 이전 바의 오른쪽 값으로 false를 받았을때 오른쪽 값으로 무조건 false를 갖는다.")
     void test_4() {
         // given, when
-        Direction direction = Direction.createFinal(new Bar(() -> true));
+        Direction direction = middleDirectionFalse.createFinal();
+
+        // then
+        assertAll(
+                () -> assertThat(direction.isExistLeftBar()).isFalse(),
+                () -> assertThat(direction.isExistRightBar()).isFalse()
+        );
+    }
+
+    @Test
+    @DisplayName("createFinal()는 이전 바의 오른쪽 값으로 true를 받았을때 오른쪽 값으로 무조건 false를 갖는다.")
+    void test_5() {
+        // given, when
+        Direction direction = middleDirectionTrue.createFinal();
 
         // then
         assertAll(
