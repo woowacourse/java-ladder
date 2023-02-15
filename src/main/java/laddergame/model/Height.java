7519c8dfada352aeb89ca5b65cde3c1cd2c13776
ezzanzzan <cjsdmswjd821@naver.com>
2023-02-15 17:37:30 +0900

    feat: 사다리의 높이의 범위를 검증하는 테스트-기능 구현

---
+++ src/main/java/laddergame/model/Height.java
@@ -0,0 +1,18 @@
+package laddergame.model;
+
+public class Height {
+    private static final int MIN_HEIGHT = 1;
+    private static final String ERROR_HEIGHT_LENGTH = "[ERROR] 사다리의 높이는 " + MIN_HEIGHT + "이상이어야 합니다.";
+    private final int height;
+
+    public Height(int height) {
+        validateRange(height);
+        this.height = height;
+    }
+
+    private static void validateRange(int height) {
+        if (height < MIN_HEIGHT) {
+            throw new IllegalArgumentException(ERROR_HEIGHT_LENGTH);
+        }
+    }
+}
