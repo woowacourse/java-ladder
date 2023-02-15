20dcc65ca8a243c30afb31d949109cd77be1789d
drunkenhw <drunkenhw@gmail.com>
2023-02-15 17:17:34 +0900

    feat: 이름을 입력 받는 기능 구현

---
+++ src/main/java/ladder/view/InputView.java
@@ -0,0 +1,17 @@
+package ladder.view;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class InputView {
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    public static List<String> readNames() {
+        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
+        String input = SCANNER.nextLine();
+        return Arrays.stream(input.split(","))
+                .collect(Collectors.toList());
+    }
+}
