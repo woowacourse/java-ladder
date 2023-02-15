df08bd34563bc412617d781d02ff4255ce31cc82
hanueleee <ezzeezz@naver.com>
2023-02-15 16:00:10 +0900

    feat: 양 옆 다리 여부 검증 후 다리 생성

--- src/main/java/domain/Line.java
+++ src/main/java/domain/Line.java
@@ -1,6 +1,7 @@
 package domain;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import util.BooleanGenerator;
 
@@ -16,7 +17,33 @@ public class Line {
 
     public void generateBridge(BooleanGenerator booleanGenerator) {
         for (int bridgeIndex = 0; bridgeIndex < bridges.size(); bridgeIndex++) {
-            bridges.set(bridgeIndex, booleanGenerator.generate());
+            if (!hasSide(bridgeIndex, bridges.size() - 1)) {
+                bridges.set(bridgeIndex, booleanGenerator.generate());
+            }
         }
+    }
+
+    public boolean hasSide(int bridgeIndex, int maxIndex) {
+        if (bridgeIndex == 0) {
+            return isBridgeInRight(bridgeIndex + 1);
+        }
+
+        if (bridgeIndex == maxIndex) {
+            return isBridgeInLeft(bridgeIndex - 1);
+        }
+
+        return isBridgeInLeft(bridgeIndex - 1) || isBridgeInRight(bridgeIndex + 1);
+    }
+
+    private boolean isBridgeInLeft(int leftIndex) {
+        return bridges.get(leftIndex);
+    }
+
+    private boolean isBridgeInRight(int rightIndex) {
+        return bridges.get(rightIndex);
+    }
+
+    public List<Boolean> getBridges() {
+        return Collections.unmodifiableList(bridges);
     }
 }
