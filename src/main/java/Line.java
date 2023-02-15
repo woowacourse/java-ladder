008d7d76cd169cd513ae7d15658aacaa182791e8
ingpyo <juninpyo726@gmail.com>
2023-02-15 14:57:37 +0900

    refactor: validate -> correctOverLapPoints 함수명 변경 및 첫번째 좌표를 생성시 필요한 초기값 제거

--- src/main/java/Line.java
+++ src/main/java/Line.java
@@ -2,11 +2,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Line {
-    private final TrueAndFalseGenerator trueAndFalseGenerator;
+    private final NumberGenerator numberGenerator;
     private final List<Boolean> points;
 
-    public Line(int personCount, TrueAndFalseGenerator trueAndFalseGenerator) {
-        this.trueAndFalseGenerator = trueAndFalseGenerator;
+    public Line(int personCount, NumberGenerator numberGenerator) {
+        this.numberGenerator = numberGenerator;
         this.points = lineMaker(personCount);
     }
 
@@ -14,20 +14,24 @@ public class Line {
         List<Boolean> points = new ArrayList<>();
         points.add(false);
         for (int count = 1; count < personCount; count++) {
-            points.add(validate(points.get(count - 1), trueAndFalseGenerator.generate()));
+            points.add(correctOverLapPoints(points.get(count - 1), changeIntToBoolean(numberGenerator.generate())));
         }
+        points.remove(0);
         return points;
     }
 
-    private boolean validate(Boolean previous, Boolean current) {
+    private Boolean correctOverLapPoints(boolean previous, boolean current) {
         if (!previous || !current) {
             return current;
         }
         return false;
     }
 
-    public List<Boolean> getPoints() {
-        return points;
+    private boolean changeIntToBoolean(int randomNumber) {
+        if (randomNumber == Path.GO.getGoNumberValue()) {
+            return Path.GO.getGoBooleanValue();
+        }
+        return Path.STOP.getGoBooleanValue();
     }
 
 }
