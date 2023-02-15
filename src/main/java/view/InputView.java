d5564b92c0f36dd22aab12a2226feaae9782271b
jaehee329 <jjhs9803@gmail.com>
2023-02-15 17:59:21 +0900

    feat(InputView): add InputView

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,49 @@
+package view;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class InputView {
+
+	private static Scanner scanner = new Scanner(System.in);
+	private static final String DELIMITER = ",";
+	private static final String PARTICIPANTS_NAMES_REQUEST_MSG = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+
+	public static List<String> readParticipantsNames() {
+		// TODO: 공백 제거
+		String line = scanner.nextLine();
+		String[] names = line.split(DELIMITER);
+		validateBlankedName(names);
+		return Arrays.stream(names).collect(Collectors.toList());
+	}
+
+	private static void validateBlankedName(String[] names) {
+		if (names.length == 0) {
+			throw new IllegalArgumentException("[ERROR]: 최소 한 명의 참가자의 이름을 입력해야합니다.");
+		}
+	}
+
+	public static int readHeight() {
+		String line = scanner.nextLine();
+		validateIsNatural(line);
+		return Integer.parseInt(line);
+	}
+
+	private static void validateIsNatural(String line) {
+		int parsedInput;
+		try {
+			parsedInput = Integer.parseInt(line);
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException("[ERROR]: 사다리 높이는 자연수여야합니다.");
+		}
+		isNumberPositive(parsedInput);
+	}
+
+	private static void isNumberPositive(int parsedInput) {
+		if (parsedInput <= 0) {
+			throw new IllegalArgumentException("[ERROR]: 사다리 높이는 자연수여야합니다.");
+		}
+	}
+}
