fecb8713cc060aa4720b5df199b6c613fa2aad47
0chil <0@chll.it>
2023-02-15 16:01:11 +0900

    feat : 이름 중복 방지

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -2,21 +2,38 @@ package view;
 
 import utils.ScannerUtil;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class InputView {
 
     public static List<String> inputName() {
+        try {
+            System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
+            String input = ScannerUtil.nextLine();
 
-        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
-        String input = ScannerUtil.nextLine();
+            List<String> names = List.of(input.split(","));
 
-        return List.of(input.split(","));
+            if (hasDuplicated(names)) {
+                throw new IllegalArgumentException("이름은 중복될 수 없습니다.");
+            }
+
+            return names;
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+            return inputName();
+        }
     }
 
     public static int inputMaxLadderHeight() {
 
         System.out.println("최대 사다리 높이는 몇 개인가요?");
         return ScannerUtil.nextInt();
+    }
+
+    private static boolean hasDuplicated(List<?> target) {
+        Set<?> duplicateRemoval = new HashSet<>(target);
+        return target.size() != duplicateRemoval.size();
     }
 }
