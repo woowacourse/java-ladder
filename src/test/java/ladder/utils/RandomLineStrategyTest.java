a8d6415ed54d3542dd8c17c18b08abd30a2105fa
Cyma-s <ssol0319@naver.com>
2023-02-15 14:28:38 +0900

    refactor: import문 변경으로 인한 접근제어자 수정

--- src/test/java/ladder/utils/RandomLineStrategyTest.java
+++ src/test/java/ladder/utils/RandomLineStrategyTest.java
@@ -1,16 +1,16 @@
 package ladder.utils;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class RandomLineStrategyTest {
+class RandomLineStrategyTest {
     @Test
     @DisplayName("연속으로 이어지지 않는 라인을 생성한다.")
-    public void line_generateNotContinuous() {
+    void line_generateNotContinuous() {
         // given
         RandomLineStrategy randomLineStrategy = new RandomLineStrategy();
 
@@ -18,7 +18,7 @@ public class RandomLineStrategyTest {
         List<Boolean> randomLine = randomLineStrategy.generate(5);
         boolean isSame = false;
         for (int i = 0; i < randomLine.size() - 1; i++) {
-            if((randomLine.get(i).equals(randomLine.get(i+1))) && randomLine.get(i)) {
+            if ((randomLine.get(i).equals(randomLine.get(i + 1))) && randomLine.get(i)) {
                 isSame = true;
                 break;
             }
