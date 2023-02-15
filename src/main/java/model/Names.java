90c51276ea5c08261b7dea128ab732d59f6f9103
이건회 <rg970604@naver.com>
2023-02-15 15:31:29 +0900

    feat : Names 일급컬렉션 사용

---
+++ src/main/java/model/Names.java
@@ -0,0 +1,16 @@
+package model;
+
+import java.util.List;
+
+public class Names {
+
+    private final List<Name> names;
+
+    public Names(List<Name> names) {
+        this.names = names;
+    }
+
+    public List<Name> getNames() {
+        return names;
+    }
+}
