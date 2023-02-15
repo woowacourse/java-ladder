cd07a2af0292455c6dde8e3a8909a3bf49846d6b
This2sho <zx00018@naver.com>
2023-02-15 15:38:56 +0900

    feat(Ladder): 최대 높이만큼 사다리를 생성해주는 기능 구현

---
+++ src/main/java/domain/Ladder.java
@@ -0,0 +1,24 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ladder {
+
+    private final List<Line> lines;
+
+    public Ladder(int personCount, int maxHeight, BridgeGenerator generator) {
+        this.lines = new ArrayList<>(maxHeight);
+        for (int i = 0; i < maxHeight; i++) {
+            lines.add(new Line(generator.generate(personCount)));
+        }
+    }
+
+    public int getHeight() {
+        return lines.size();
+    }
+
+    public int getWidth() {
+        return lines.get(0).size();
+    }
+}
---
+++ src/test/java/domain/LadderTest.java
@@ -0,0 +1,29 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class LadderTest {
+
+    @DisplayName("사다리의 높이만큼 라인을 생성한다")
+    @ParameterizedTest(name = "{index} : 사다리 높이 {0}만큼 라인을 생성한다 ")
+    @ValueSource(ints = {3, 4, 5, 6})
+    void ladder_height_test(int height) {
+        Ladder ladder = new Ladder(4, height, new RandomBridgeGenerator());
+
+        assertThat(ladder.getHeight()).isEqualTo(height);
+
+    }
+
+    @DisplayName("사람 {수 -1} 길이의 사다리의 폭을 생성한다")
+    @ParameterizedTest(name = "{index} : 사다리의 폭의 길이는 {0}-1 이다 ")
+    @ValueSource(ints = {3, 4, 5, 6})
+    void ladder_width_test(int personCount) {
+        Ladder ladder = new Ladder(personCount, 3, new RandomBridgeGenerator());
+        assertThat(ladder.getWidth()).isEqualTo(personCount - 1);
+
+    }
+}
