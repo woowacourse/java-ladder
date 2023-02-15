bfdb3a732ecd476a9bead03c667f22df5eba60e3
Devtist <yt930310@gmail.com>
2023-02-15 13:37:26 +0900

    feat(RandomBooleanGenerator): 랜덤한 boolean을 반환하는 기능 구현

---
+++ src/main/java/ladder/domain/RandomBooleanGenerator.java
@@ -0,0 +1,12 @@
+package ladder.domain;
+
+import java.util.Random;
+
+public class RandomBooleanGenerator implements BooleanGenerator {
+    private static final Random random = new Random();
+
+    @Override
+    public boolean generate() {
+        return random.nextBoolean();
+    }
+}
