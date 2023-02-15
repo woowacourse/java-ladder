e7c4a31cf99bb331794ccd5b40e935fb6429c804
jiwon <ljwon77@daum.net>
2023-02-15 17:36:49 +0900

    test: LadderFactory의 Ladder 객체 생성에 대한 기능을 테스트 한다

--- src/test/java/laddergame/domain/ladder/LadderFactoryTest.java
+++ src/test/java/laddergame/domain/ladder/LadderFactoryTest.java
@@ -1,34 +1,44 @@
 package laddergame.domain.ladder;
 
+import laddergame.domain.rung.RungNumberGenerator;
+import laddergame.util.NumberGenerator;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 public class LadderFactoryTest {
 
+    private LadderFactory ladderFactory;
+    private int participantCount;
+
+    @BeforeEach
+    void init() {
+        NumberGenerator rungNumberGenerator = new RungNumberGenerator();
+        ladderFactory = new LadderFactory(rungNumberGenerator);
+        participantCount = 4;
+    }
+
     @ParameterizedTest
     @ValueSource(strings = {"1", "10000"})
     @DisplayName("1 이상의 정수 범위의 사다리 높이가 들어오면, 예외가 발생하지 않는다.")
-    void ladder_height_success_test(String height){
-        // given
-        LadderFactory ladderFactory = new LadderFactory();
-
+    void ladder_create_test(String height){
         // when & then
-        assertDoesNotThrow(() -> ladderFactory.createLadder(height));
+        assertDoesNotThrow(() -> ladderFactory.createLadder(height, participantCount));
+        assertThat(ladderFactory.createLadder(height, participantCount))
+                .isInstanceOf(Ladder.class);
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"hi", "23fgie", "@#$%$"})
     @DisplayName("정수 값이 아닌 사다리 높이가 들어오면, 예외가 발생한다.")
-    void ladder_height_type_test(String height){
-        // given
-        LadderFactory ladderFactory = new LadderFactory();
-
+    void ladder_height_type_error_test(String height){
         // when & then
-        assertThatThrownBy(() -> ladderFactory.createLadder(height))
+        assertThatThrownBy(() -> ladderFactory.createLadder(height, participantCount))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR]");
     }
@@ -36,12 +46,9 @@ public class LadderFactoryTest {
     @ParameterizedTest
     @ValueSource(strings = {"0", "10001"})
     @DisplayName("1~10000 범위를 벗어난 사다리 높이가 입력되면, 예외가 발생한다.")
-    void ladder_height_range_test(String height){
-        // given
-        LadderFactory ladderFactory = new LadderFactory();
-
+    void ladder_height_range_error_test(String height){
         // when & then
-        assertThatThrownBy(() -> ladderFactory.createLadder(height))
+        assertThatThrownBy(() -> ladderFactory.createLadder(height, participantCount))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR]");
     }
