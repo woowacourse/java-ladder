9db822df245bfef7d7558829a80f48ed5b9b8470
be-student <presentsong@naver.com>
2023-02-15 15:22:15 +0900

    feat: 입력 기능 추가

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -1,5 +1,25 @@
 package ladder.view;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class InputView {
 
+    private static final Pattern pattern = Pattern.compile("^-?\\d+$");
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static String inputPlayerNames() {
+        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)\n");
+        return scanner.nextLine();
+    }
+
+    public static int inputLadderHeight() {
+        System.out.println("최대 사다리 높이는 몇 개인가요?\n");
+        String height = scanner.nextLine();
+        if (!pattern.matcher(height).matches()) {
+            throw new IllegalArgumentException("입력은 숫자여야 합니다");
+        }
+        return Integer.parseInt(height);
+    }
 }
