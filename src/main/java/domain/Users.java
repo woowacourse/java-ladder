1e5d5f49e661f26044a155ca9d4c52e3613b72f4
2014012606 <2014012606@hanyang.ac.kr>
2023-02-15 17:43:08 +0900

    chore: Users에서 변경될 필요 없는 변수들에 final 키워드 추가

--- src/main/java/domain/Users.java
+++ src/main/java/domain/Users.java
@@ -1,17 +1,17 @@
 package domain;
 
+import exception.ErrorMessage;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Users {
     private final List<User> users;
 
-    public Users(List<String> userNames) {
-        if (userNames.size() == 0) {
-            throw new IllegalArgumentException("");
-        }
+    public Users(final List<String> userNames) {
+        validateUserNamesEmpty(userNames);
         users = new ArrayList<>();
-        for (String userName : userNames) {
+        for (final String userName : userNames) {
             users.add(new User(userName));
         }
     }
@@ -21,10 +21,16 @@ public class Users {
     }
 
     public List<String> getUsersName() {
-        List<String> usersName = new ArrayList<>();
-        for(User user : users) {
+        final List<String> usersName = new ArrayList<>();
+        for (final User user : users) {
             usersName.add(user.getName());
         }
         return usersName;
+    }
+
+    private void validateUserNamesEmpty(final List<String> userNames) {
+        if (userNames.isEmpty()) {
+            throw new IllegalArgumentException(ErrorMessage.USERS_NAME_BLANK_EXCEPTION.getMessage());
+        }
     }
 }
