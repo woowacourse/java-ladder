d5f2375248a834e8f525d84d6e6018cb636a1bc3
xxeol2 <jhj01177@naver.com>
2023-02-15 15:18:45 +0900

    refactor: PlayerFactoryTest 클래스 네이밍 변경

--- src/test/java/factory/PlayerFactoryTest.java
+++ src/test/java/factory/PlayerFactoryTest.java
@@ -11,7 +11,7 @@ import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-public class PlayersFactoryTest {
+public class PlayerFactoryTest {
 
     @DisplayName("참여자가 20명을 넘을 수 없다.")
     @Test
