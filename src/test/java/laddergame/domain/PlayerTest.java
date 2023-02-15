037b265f2eff112d9ad458cc8cdbdafb99c2721f
moonchaewon <codnjs8989@gmail.com>
2023-02-15 14:15:58 +0900

    test: implement test method to create player with exception

--- src/test/java/laddergame/domain/PlayerTest.java
+++ src/test/java/laddergame/domain/PlayerTest.java
@@ -2,7 +2,10 @@ package laddergame.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
@@ -12,5 +15,11 @@ class PlayerTest {
     void playerCreateTest() {
         String playerName = "test";
         assertDoesNotThrow(() -> new Player(playerName));
+    }
+
+    @ParameterizedTest(name = "{0} 라는 이름으로 Player를 생성할 경우 예외가 발생한다.")
+    @ValueSource(strings = {"  ", "test11"})
+    void playerCreateExceptionTest(String playerName) {
+        assertThatThrownBy(() -> new Player(playerName)).isInstanceOf(IllegalArgumentException.class);
     }
 }
