372fd1e0d42c6b8d751e89d623b84d5f5f90c26d
donghae <donghae1999@gmail.com>
2023-02-15 22:06:08 +0900

    feat: Line 생성 검증 코드 추가

--- src/main/java/model/Line.java
+++ src/main/java/model/Line.java
@@ -7,24 +7,40 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Line {
+    private static final int MINIMUM_LINE_SIZE = 1;
+
     public Generator generator = new LineGenerator();
     private List<Boolean> points = new ArrayList<>();
 
     public Line (int personCount) {
-        for(int index=0; index<personCount; index++) {
+        for(int column=0; column<personCount-1; column++) {
             points.add(generator.generate());
-            if(checkLine(index)){
-                index--;
+            if(validateLineMake(column)){
+                column--;
             }
         }
     }
 
-    private boolean checkLine(int index){
-        if(index>=1){
-            if(points.get(index)==points.get(index-1)){
-                points.remove(index);
+    private boolean validateLineMake(int column){
+        if(points.size()>=MINIMUM_LINE_SIZE){
+            if(validateContinuousLine(column) && validateTrueLine(column)){
+                points.remove(column);
                 return true;
             }
+        }
+        return false;
+    }
+
+    private boolean validateContinuousLine(int column){
+        if(points.get(column) == points.get(column-1)){
+            return true;
+        }
+        return false;
+    }
+
+    private boolean validateTrueLine(int index){
+        if(points.get(index)==true){
+            return true;
         }
         return false;
     }
