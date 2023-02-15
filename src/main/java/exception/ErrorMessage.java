7182ba780fdb0e8b2a50a2bce476da873ec97168
2014012606 <2014012606@hanyang.ac.kr>
2023-02-15 17:31:05 +0900

    feat: 예외 메시지 Enum으로 관리

---
+++ src/main/java/exception/ErrorMessage.java
@@ -0,0 +1,19 @@
+package exception;
+
+public enum ErrorMessage {
+    LADDER_HEIGHT_EXCEPTION("사다리의 높이는 1이상이어야 합니다."),
+    LADDER_HEIGHT_NON_NUMERIC_EXCEPTION("사다리의 높이는 정수형이어야 합니다."),
+    USER_NAME_LENGTH_EXCEPTION("유저의 이름은 5자 이하여야 합니다."),
+    USER_NAME_BLANK_EXCEPTION("유저의 이름은 공백일 수 없습니다."),
+    USERS_NAME_BLANK_EXCEPTION("유저의 이름으로 쉼표만 입력하면 안 됩니다.");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
