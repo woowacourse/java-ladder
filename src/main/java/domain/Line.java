574196cd3d898e426833d8bc7b15a84f14cee011
seokjin8678 <rnrn8678@naver.com>
2023-02-15 15:13:48 +0900

    feat(Line): 연속되는 Step이 있으면 예외를 발생시키는 기능 추가

--- src/main/java/domain/Line.java
+++ src/main/java/domain/Line.java
@@ -1,4 +1,24 @@
 package domain;
 
+import java.util.List;
+
 public class Line {
+    private final List<Step> steps;
+
+    public Line(List<Step> steps) {
+        validateContinuousStep(steps);
+        this.steps = steps;
+    }
+
+    public int getSize() {
+        return steps.size();
+    }
+
+    private static void validateContinuousStep(List<Step> steps) {
+        for (int i = 1; i < steps.size(); i++) {
+            if (steps.get(i - 1) == steps.get(i)) {
+                throw new IllegalArgumentException("[ERROR] 라인에 Step이 연속될 수 없습니다.");
+            }
+        }
+    }
 }
