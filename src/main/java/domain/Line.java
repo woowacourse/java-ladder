7e6646e08cea5f647163545b2803c4fbd960828b
parkmuhyeun <pjhg410@gmail.com>
2023-02-15 15:22:25 +0900

    feat: 랜덤 다리 생성 기능 추가

--- src/main/java/domain/Line.java
+++ src/main/java/domain/Line.java
@@ -5,19 +5,36 @@ import java.util.List;
 
 public class Line {
 
+    private static final int POSSIBLE_STATE = 1;
+    private static final int IMPOSSIBLE_STATE = 0;
+
     private final List<Boolean> points = new ArrayList<>();
 
-    public Line(int personCount) {
-        addPoints(personCount);
+    public Line(int personCount, RandomGenerator generator) {
+        addPoints(personCount, generator);
     }
 
-    private void addPoints(int personCount) {
+    public List<Boolean> getPoints() {
+        return points;
+    }
+
+    private void addPoints(int personCount, RandomGenerator generator) {
         for (int i = 0; i < personCount - 1; i++) {
+            addPoint(generator.generate());
+        }
+    }
+
+    private void addPoint(int state) {
+        if (state == POSSIBLE_STATE) {
             points.add(true);
+        }
+        if (state == IMPOSSIBLE_STATE) {
+            points.add(false);
         }
     }
 
     public int getPointsSize() {
         return points.size();
     }
+
 }
