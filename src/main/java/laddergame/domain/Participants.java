1f914e2028184c2cac9be5faf257a829a743d9cb
hyunseo <goatsfish@gmail.com>
2023-02-15 14:17:06 +0900

    feat: Participants 생성 및 검증

---
+++ src/main/java/laddergame/domain/Participants.java
@@ -0,0 +1,17 @@
+package laddergame.domain;
+
+import java.util.List;
+
+public class Participants {
+    private final List<Person> people;
+
+    public Participants(List<Person> people) {
+        if (people == null) {
+            throw new IllegalArgumentException();
+        }
+        if (people.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        this.people = people;
+    }
+}
