3bf292b653240d01d8ed884878deb8d18f640100
jiwon <ljwon77@daum.net>
2023-02-15 18:47:35 +0900

    feat: InputView 클래스의 사용자 입력을 받는 기능을 구현한다

---
+++ src/main/java/laddergame/view/InputView.java
@@ -0,0 +1,33 @@
+package laddergame.view;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.function.Supplier;
+
+public class InputView {
+
+    private final BufferedReader bufferedReader;
+
+    public InputView() {
+        this.bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    public String readConsole() {
+        try {
+            return bufferedReader.readLine();
+        } catch (IOException e) {
+            OutputView.print(e.getMessage());
+        }
+        return null;
+    }
+
+    public <T> T getUserInput(Supplier<T> inputReader) {
+        try {
+            return inputReader.get();
+        } catch (IllegalArgumentException e) {
+            OutputView.print(e.getMessage());
+            return getUserInput(inputReader);
+        }
+    }
+}
