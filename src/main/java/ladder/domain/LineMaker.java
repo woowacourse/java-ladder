ef2e84fca1c713f64973a57ca59f06982cc22a9a
DESKTOP-FNVN0HH\USER <beatus96@naver.com>
2023-02-15 14:34:16 +0900

    feat: LineMaker 라인 생성 기능 추가

--- src/main/java/ladder/domain/LineMaker.java
+++ src/main/java/ladder/domain/LineMaker.java
@@ -4,46 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LineMaker {
-    // 생성자에 넣지 말고. generate를 static 메서드로 만들기
-    // 그럼 매개변수로 player숫자(리스트 길이)와 randomGenerator만 넣으면 됨.
-    //변수에 저장xxxxx
-    //테스트 코드도 고치기
 
-    private final int playerCount;
-    private final RandomGenerator<Boolean> randomGenerator;
-
-    public LineMaker(int playerCount, RandomGenerator<Boolean> randomGenerator) {
-        this.playerCount = playerCount;
-        this.randomGenerator = randomGenerator;
-    }
-
-    public List<Boolean> generate() {
+    public static List<Boolean> generate(int playerCount, RandomGenerator<Boolean> randomGenerator) {
         List<Boolean> line = new ArrayList<>();
+        int lineSize = playerCount - 1;
+        boolean before = false;
 
-        line.add(randomGenerator.generate());
-        for (int idx = 1; idx < playerCount; idx++) {
-            if (line.get(idx - 1)) {
-                line.add(false);
-                continue;
-            }
-            line.add(randomGenerator.generate());
-
+        for (int idx = 0; idx < lineSize; idx++) {
+            boolean current = getBar(before, randomGenerator);
+            line.add(current);
+            before = current;
         }
-
         return line;
     }
 
-    //add를 먼저하지 않고.
-    //for문 전에 boolean before = false을 메서드 변수로 두고,
-    //for문 안에 getBar넣어서 playCount만큼 돌리기 int i = 0에서 시작할 수 있음
-
-    public boolean getBar(boolean before) {
+    private static boolean getBar(boolean before, RandomGenerator<Boolean> randomGenerator) {
         if(before) {
             return false;
         }
         return randomGenerator.generate();
     }
-
-
 
 }
