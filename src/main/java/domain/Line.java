51c2f6040c7e6c9b1bdc79ad10e3355634aa24d4
hanueleee <ezzeezz@naver.com>
2023-02-15 15:55:08 +0900

    feat: 각 칸마다 사다리 생성 여부는 랜덤값으로 결정

--- src/main/java/domain/Line.java
+++ src/main/java/domain/Line.java
@@ -2,6 +2,7 @@ package domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import util.BooleanGenerator;
 
 public class Line {
     private final List<Boolean> bridges;
@@ -10,6 +11,12 @@ public class Line {
         this.bridges = new ArrayList<>();
         for (int index = 0; index < personCount - 1; index++) {
             bridges.add(false);
+        }
+    }
+
+    public void generateBridge(BooleanGenerator booleanGenerator) {
+        for (int bridgeIndex = 0; bridgeIndex < bridges.size(); bridgeIndex++) {
+            bridges.set(bridgeIndex, booleanGenerator.generate());
         }
     }
 }
