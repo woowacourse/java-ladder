b02d1bc5c995591f8ff39f32674a456c2884bad1
eunbii0213 <eunbii0213@naver.com>
2023-02-15 20:48:41 +0900

    feat: 사다리의 높이를 입력받는 기능 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class InputView {
     public static final String DELIMITER = ",";
     public static final String PLAYER_NAME_INPUT_GUIDE_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    public static final String LADDER_HEIGHT_INPUT_GUIDE_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
     public static final String PLAYER_NAME_NULL_BLANK_ERROR_MESSAGE = "[ERROR] 플레이어의 이름으로 공백 혹은 빈 칸을 입력할 수 없습니다";
 
     public List<String> readPlayerName() {
@@ -26,5 +27,26 @@ public class InputView {
         if(playerName.isBlank() || playerName.isEmpty()){
             throw new IllegalArgumentException(PLAYER_NAME_NULL_BLANK_ERROR_MESSAGE);
         }
+    }
+
+    public int readLadderHeight(){
+        printLadderHeightGuideMessage();
+        Scanner scanner = new Scanner(System.in);
+        String ladderHeight = scanner.nextLine();
+
+        validateLadderHeightIsNumber(ladderHeight);
+        return Integer.parseInt(ladderHeight);
+    }
+
+    private static void validateLadderHeightIsNumber(String ladderHeight) {
+        try {
+            Integer.parseInt(ladderHeight);
+        }catch(IllegalArgumentException e){
+            throw new IllegalArgumentException("[ERROR] 사다리의 높이는 숫자를 입력해야합니다");
+        }
+    }
+
+    private void printLadderHeightGuideMessage() {
+        System.out.println(LADDER_HEIGHT_INPUT_GUIDE_MESSAGE);
     }
 }
