e90d275bfac480d199a6e5d198f012d243c3d67c
junpakPark <junpak.park@gmail.com>
2023-02-15 14:43:50 +0900

    feat: Line 객체 생성 시 라인이 겹치지 않게 List<Boolean> 생성

--- src/main/java/laddergame/domain/Line.java
+++ src/main/java/laddergame/domain/Line.java
@@ -1,15 +1,56 @@
 package laddergame.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Line {
 
     private static final String MIN_WIDTH_MESSAGE = "너비는 1보다 작을 수 없습니다.";
     private static final int MIN_WIDTH = 1;
 
-    private final int width;
+    private final List<Boolean> statuses;
+    private final PickStrategy strategy;
 
-    public Line(final int width) {
+    private Line(final int width, final PickStrategy pickStrategy) {
+        this.strategy = pickStrategy;
         validateMinWidth(width);
-        this.width = width;
+        this.statuses = createStatuses(width);
+    }
+
+    public static Line from(final int width) {
+        return new Line(width, new RandomBooleanPicker());
+    }
+
+    public static Line of(final int width, final PickStrategy pickStrategy) {
+        return new Line(width, pickStrategy);
+    }
+
+    private List<Boolean> createStatuses(int width) {
+        List<Boolean> statuses = new ArrayList<>();
+
+        for (int i = 0; i < width; i++) {
+            statuses.add(createLineStatus(statuses, i));
+        }
+
+        return statuses;
+    }
+
+    private boolean createLineStatus(final List<Boolean> statuses, final int index) {
+        final boolean pick = strategy.pick();
+
+        if (index == 0) {
+            return pick;
+        }
+
+        if (isOverlap(statuses, pick)) {
+            return false;
+        }
+
+        return pick;
+    }
+
+    private static boolean isOverlap(final List<Boolean> statuses, final boolean pick) {
+        return statuses.get(statuses.size() - 1) && pick;
     }
 
     private void validateMinWidth(final int width) {
