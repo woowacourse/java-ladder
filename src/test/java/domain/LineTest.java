8cf7b68573845472e7fa8d504d2ce2b753f23603
hanueleee <ezzeezz@naver.com>
2023-02-15 15:57:36 +0900

    test: BooleanGenerator 결과에 따른 다리 생성 테스트

--- src/test/java/domain/LineTest.java
+++ src/test/java/domain/LineTest.java
@@ -3,10 +3,29 @@ package domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import util.BooleanGenerator;
 
 class LineTest {
+
+    static class TrueGenerator implements BooleanGenerator {
+
+        @Override
+        public boolean generate() {
+            return true;
+        }
+    }
+
+    static class FalseGenerator implements BooleanGenerator {
+
+        @Override
+        public boolean generate() {
+            return false;
+        }
+    }
+
     @ParameterizedTest
     @ValueSource(ints = {3, 5, 10})
     @DisplayName("한 라인에 사람 수 만큼 칸이 생성되는가")
@@ -17,6 +36,32 @@ class LineTest {
 
         //then
         assertThat(line.getBridges().size()).isEqualTo(personCount - 1);
+    }
+
+    @Test
+    @DisplayName("BooleanGenerator 결과가 항상 true 일 때 다리 생성 테스트")
+    void alwaysTrueCreateBridge() {
+        //given
+        Line line = new Line(5);
+
+        //when
+        line.generateBridge(new TrueGenerator());
+
+        //then
+        assertThat(line.getBridges()).containsExactly(true, false, true, false);
+    }
+
+    @Test
+    @DisplayName("BooleanGenerator 결과가 항상 false 일 때 다리 생성 테스트")
+    void alwaysFalseCreateBridge() {
+        //given
+        Line line = new Line(5);
+
+        //when
+        line.generateBridge(new FalseGenerator());
+
+        //then
+        assertThat(line.getBridges()).containsExactly(false, false, false, false);
     }
 
 }
