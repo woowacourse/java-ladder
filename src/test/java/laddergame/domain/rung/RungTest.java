a6c9307d6ff162b0362416e5d54e020f4abe5423
jiwon <ljwon77@daum.net>
2023-02-15 14:59:38 +0900

    test: 사다리 가로대의 생성자에 대한 테스트 코드를 구현한다

---
+++ src/test/java/laddergame/domain/rung/RungTest.java
@@ -0,0 +1,43 @@
+package laddergame.domain.rung;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+public class RungTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1})
+    @DisplayName("0 혹은 1이 입력되면, 사다리 가로대가 생성된다.")
+    void create_test(int validNumber) {
+        assertDoesNotThrow(() -> new Rung(validNumber));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 2, 3, 4})
+    @DisplayName("0 혹은 1이 아닌 값이 입력되면, 예외가 발생한다.")
+    void create_error_test(int errorNumber) {
+        assertThatThrownBy(() -> new Rung(errorNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "0:false"}, delimiter = ':')
+    @DisplayName("0 혹은 1이 아닌 값이 입력되면, 예외가 발생한다.")
+    void make_rung_test(int material, boolean expectedValue) {
+        // given
+        Rung rung = new Rung(material);
+
+        // when
+        boolean actualValue = rung.isExistence();
+
+        // then
+        assertThat(actualValue).isEqualTo(expectedValue);
+    }
+}
