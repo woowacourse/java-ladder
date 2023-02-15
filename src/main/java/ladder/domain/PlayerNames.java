6b84c478c9b7a29d4ed2a49d4eb20ece74820500
JunDonghyuk <ehdgur4814@naver.com>
2023-02-15 19:45:32 +0900

    feat(PlayerNames): 참여자의 이름 검증 기능 구현

--- src/main/java/ladder/domain/PlayerNames.java
+++ src/main/java/ladder/domain/PlayerNames.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class PlayerNames {
 
     private static final String LENGTH_EXCEPTION_MESSAGE = "[ERROR] 참여자의 이름은 1자 이상 5자 이하여야 합니다.";
+    private static final String BLANK_EXCEPTION_MESSAGE = "[ERROR] 참여자의 이름은 빈 값일 수 없습니다.";
 
     private final List<String> names;
 
@@ -15,11 +16,26 @@ public class PlayerNames {
     }
 
     private void validate(final List<String> names) {
+        validateBlank(names);
         validateLength(names);
     }
 
+
+    private void validateBlank(final List<String> names) {
+        if (isBlank(names)) {
+            throw new IllegalArgumentException(BLANK_EXCEPTION_MESSAGE);
+        }
+    }
+
+    private boolean isBlank(final List<String> names) {
+        long count = names.stream()
+                .filter(name -> name.isBlank())
+                .count();
+        return count != 0;
+    }
+
     private void validateLength(final List<String> names) {
-        if(isWrongLength(names)) {
+        if (isWrongLength(names)) {
             throw new IllegalArgumentException(LENGTH_EXCEPTION_MESSAGE);
         }
     }
