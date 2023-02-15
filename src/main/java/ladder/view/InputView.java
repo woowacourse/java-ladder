c0ed923fcebbe3eb609edc77ed90c81a9b852f02
LJW25 <dbjwl@g.skku.edu>
2023-02-15 15:05:29 +0900

    feat(InputView): 사다리 높이 정수 아니면 예외 처리 메소드 작성

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -3,10 +3,12 @@ package ladder.view;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class InputView {
 
     private static final InputView INSTANCE = new InputView();
+    private static final Pattern NUMBER_REGEX = Pattern.compile("^-?[0-9]+$");
 
     private InputView() {
     }
@@ -33,6 +35,20 @@ public class InputView {
     private void validateDuplicatedNames(List<String> names) {
         if (names.size() != names.stream().distinct().count()) {
             throw new IllegalArgumentException("이름은 중복될 수 없습니다.");
+        }
+    }
+
+    public int readLength() {
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        validateLength(input);
+        return Integer.parseInt(input);
+    }
+
+    private void validateLength(String input) {
+        if (!NUMBER_REGEX.matcher(input).matches()) {
+            throw new IllegalArgumentException("정수가 아닙니다.");
         }
     }
 }
