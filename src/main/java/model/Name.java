34764ffb90edbba779c07a338329c29381206e63
donghae <donghae1999@gmail.com>
2023-02-15 23:51:17 +0900

    feat: Name 출력을 위한 getName 기능 추가

--- src/main/java/model/Name.java
+++ src/main/java/model/Name.java
@@ -6,6 +6,7 @@ import java.util.regex.Pattern;
 public class Name {
     private static final int MAXIMUM_NAME_LENGTH = 5;
     private static final Pattern pattern = Pattern.compile("^[a-zA-Z]+$");
+    private static final String EXCEPTION_NAME_PATTERN = "[ERROR] 이름은 알파벳 대소문자로만 구성 가능합니다.";
 
     private final String name;
 
@@ -24,8 +25,12 @@ public class Name {
     private void validateNameHasOnlyCharacters(String name) {
         Matcher matcher = pattern.matcher(name);
         if (!matcher.matches()) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(EXCEPTION_NAME_PATTERN);
         }
+    }
+
+    public String getName(){
+        return this.name;
     }
 
 }
