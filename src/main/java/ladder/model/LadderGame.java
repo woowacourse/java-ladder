b5ab953cb5a7ddb70bc6220452116669659a104c
LJW25 <dbjwl@g.skku.edu>
2023-02-15 16:35:51 +0900

    refactor(LadderGame): 하드 코딩 삭제

--- src/main/java/ladder/model/LadderGame.java
+++ src/main/java/ladder/model/LadderGame.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class LadderGame {
 
-
+    private static final int MIN_PLAYER_COUNT = 2;
     private Ladder ladder;
 
     public LadderGame(List<String> playerNames) {
@@ -12,8 +12,21 @@ public class LadderGame {
     }
 
     private void validatePlayerCount(List<String> playerNames) {
-        if (playerNames.size() < 2) {
-            throw new IllegalArgumentException("게임을 진행하기 위해서는 두 명 이상의 플레이어가 필요합니다.");
+        if (playerNames.size() < MIN_PLAYER_COUNT) {
+            throw new IllegalArgumentException(ErrorMessage.EXCEPTION_INVALID_PLAYER_COUNT.getMessage());
+        }
+    }
+
+    private enum ErrorMessage {
+        EXCEPTION_INVALID_PLAYER_COUNT("게임을 진행하기 위해서는 두 명 이상의 플레이어가 필요합니다.");
+        private final String message;
+
+        ErrorMessage(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
         }
     }
 }
