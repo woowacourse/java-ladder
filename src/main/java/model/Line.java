53f7e6fab2df185c0fc162854f4dd5d312cf2ae4
MoonJeWoong <munjin0201@naver.com>
2023-02-15 22:05:44 +0900

    feat: 사다리 한 라인에 존재하는 포인트들을 만드는 기능 추가

--- src/main/java/model/Line.java
+++ src/main/java/model/Line.java
@@ -5,6 +5,20 @@ import java.util.List;
 
 public class Line {
 
-    public Line(int personCount) {}
+    private final List<Boolean> points = new ArrayList<>();
 
+    public Line(int personCount) {
+        createPoints(personCount);
+    }
+
+    public List<Boolean> getPoints() {
+        return new ArrayList<>(points);
+    }
+
+    private void createPoints(int personCount) {
+        int pointsCount = personCount - 1;
+        for(int i=0; i<pointsCount; i++){
+            points.add(Boolean.TRUE);
+        }
+    }
 }
