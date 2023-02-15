03c49b4b0dd181132478bd7acc952b4ed394e11f
thdwoqor <celebarte01@gmail.com>
2023-02-15 14:01:59 +0900

    refactor : Ladder클래스 네이밍 수정

--- src/main/java/Ladders.java
+++ src/main/java/Ladders.java
@@ -1,6 +1,6 @@
-public class Ladder {
+public class Ladders {
     private final int height;
-    Ladder(int height){
+    Ladders(int height){
         if (height < 1 || height > 10) {
             throw new IllegalArgumentException("사다리 높이는 1에서 10");
         }
--- src/test/java/LaddersTest.java
+++ src/test/java/LaddersTest.java
@@ -5,20 +5,20 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
 
-public class LadderTest {
+public class LaddersTest {
 
     @ParameterizedTest
     @ValueSource(ints = {0, 11})
     @DisplayName("사다리 높이가 1~10을 벗어나면 예외가 발생한다.")
     void LadderHeightFailTest(int height) {
         Assertions.assertThrows(IllegalArgumentException.class,
-                () -> new Ladder(height));
+                () -> new Ladders(height));
     }
 
     @ParameterizedTest
     @ValueSource(ints = {1, 10})
     @DisplayName("사다리 높이가 1~10 사이면 정상적으로 수행된다.")
     void LadderHeightSuccessTest(int height) {
-        assertThatCode(() -> new Ladder(height)).doesNotThrowAnyException();
+        assertThatCode(() -> new Ladders(height)).doesNotThrowAnyException();
     }
 }
