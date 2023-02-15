627c4a536b472c29965697188259aa81e150058a
이건회 <rg970604@naver.com>
2023-02-15 14:52:44 +0900

    refactor : Name 상수 처리 및 메소드 분리

--- src/main/java/model/Name.java
+++ src/main/java/model/Name.java
@@ -2,13 +2,25 @@ package model;
 
 public class Name {
 
+    public static final int MAXIMUM_NAME_LENGTH = 5;
     private final String name;
 
     public Name(String name) {
-        if (name.isBlank() || name.length() > 5) {
-            throw new IllegalArgumentException();
-        }
+        validateBlank(name);
+        validateLength(name);
         this.name = name;
+    }
+
+    private void validateLength(String name) {
+        if (name.length() > MAXIMUM_NAME_LENGTH) {
+            throw new IllegalArgumentException("이름은 최대 5글자까지만 가능합니다.");
+        }
+    }
+
+    private void validateBlank(String name) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("이름은 공백일 수 없습니다.");
+        }
     }
 
     public String getName() {
