68adb413a04170ffb8c60e78b0fe804103d47069
Devtist <yt930310@gmail.com>
2023-02-15 15:01:47 +0900

    test(Names): 참여자 수를 검증하는 기능 테스트

---
+++ src/test/java/ladder/domain/NamesTest.java
@@ -0,0 +1,41 @@
+package ladder.domain;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class NamesTest {
+
+    // 사람 수는 2명 이상 100명 이하여야 한다.
+    @ParameterizedTest(name = "사람 수는 2명 이상 100명 이하여야 한다.")
+    @ValueSource(ints = {1, 101})
+    void createNamesFailTest(int size) {
+        List<String> userNames = createUserNamesBySize(size);
+
+        assertThatThrownBy(() -> new Names(userNames))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest(name = "사람 수는 2명 이상 100명 이하여야 한다.")
+    @ValueSource(ints = {2, 100})
+    void createNamesSuccessTest(int size) {
+        List<String> userNames = createUserNamesBySize(size);
+
+        assertDoesNotThrow(() -> new Names(userNames));
+    }
+
+    public List<String> createUserNamesBySize(int size) {
+        List<String> userNames = new ArrayList<>();
+
+        for (int i = 0; i < size; i++) {
+            userNames.add(String.valueOf(i));
+        }
+
+        return userNames;
+    }
+}
