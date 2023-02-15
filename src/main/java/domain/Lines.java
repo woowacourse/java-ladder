4a22c99dcfc07115ffee5cf33249c9c20eb543ea
sosow0212 <sosow0212@naver.com>
2023-02-15 15:01:06 +0900

    feat: 사다리 생성 기능 구현

---
+++ src/main/java/domain/Lines.java
@@ -0,0 +1,29 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import utils.RandomFootholdGenerator;
+
+public class Lines {
+
+    private final List<Line> lines;
+
+    public Lines(final int numberOfPlayer, final int height) {
+        List<Line> lines = makeLines(numberOfPlayer, height);
+        this.lines = lines;
+    }
+
+    private List<Line> makeLines(final int numberOfPlayer, final int height) {
+        List<Line> lines = new ArrayList<>();
+
+        for (int i = 0; i < height; i++) {
+            lines.add(new Line(numberOfPlayer, new RandomFootholdGenerator()));
+        }
+
+        return lines;
+    }
+
+    public List<Line> getLines() {
+        return lines;
+    }
+}
---
+++ src/test/java/domain/LinesTest.java
@@ -0,0 +1,24 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LinesTest {
+    @Test
+    @DisplayName("Lines는 4명의 플레이어와 5의 높이가 주어졌을 때, 생성을 확인한다.")
+    void make_lines() {
+        // given
+        int numberOfPlayer = 4;
+        int height = 5;
+        int expectedLadderWidth = numberOfPlayer - 1;
+
+        // when
+        Lines lines = new Lines(numberOfPlayer, height);
+
+        // then
+        assertThat(lines.getLines().get(0).getPoints().size()).isEqualTo(expectedLadderWidth);
+        assertThat(lines.getLines().size()).isEqualTo(height);
+    }
+}
