d01b94b1afccf6a3cf80aaad0ef073c9a540f1bc
kim0914 <kosa0914@naver.com>
2023-02-15 16:46:09 +0900

    feat(BooleanGenerator): boolean을 랜덤으로 반환하는 메서드 추가
    - BooleanGenerator 인터페이스 추가
    - RandomBooleanGenerator 구현체 추가

---
+++ src/main/java/domain/RandomBooleanGenerator.java
@@ -0,0 +1,12 @@
+package domain;
+
+import java.util.Random;
+
+public class RandomBooleanGenerator implements BooleanGenerator {
+
+    private final Random random = new Random();
+    @Override
+    public boolean generate() {
+        return random.nextBoolean();
+    }
+}
