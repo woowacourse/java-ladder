efbdb0a6eb6c2ac8d9b304cc891547f2dfd64504
MoonJeWoong <munjin0201@naver.com>
2023-02-15 22:04:55 +0900

    test: 사다리 한 라인에 존재하는 포인트들을 만드는 기능 테스트 추가

--- src/test/java/model/LineTest.java
+++ src/test/java/model/LineTest.java
@@ -1,9 +1,12 @@
 package model;
 
 import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 public class LineTest {
 
@@ -12,4 +15,18 @@ public class LineTest {
     void createLineTest() {
         assertThatNoException().isThrownBy(()->{Line line = new Line(4);});
     }
+
+    @Test
+    @DisplayName("사다리 한 라인에 존재하는 포인트들을 만드는 기능 테스트")
+    void makePointsInLineTest() {
+        //Given
+        Line line = new Line(4);
+
+        //When
+        List<Boolean> result = line.getPoints();
+
+        //Then
+        assertThat(result.size()).isEqualTo(3);
+    }
+
 }
