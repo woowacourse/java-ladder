1cae8fb7bfb8b4b9a8827fa4af55d5bae3961e3f
LJW25 <dbjwl@g.skku.edu>
2023-02-15 17:19:07 +0900

    feat(PlayerName); 이름 공백 제거 메소드 구현

--- src/main/java/ladder/model/PlayerName.java
+++ src/main/java/ladder/model/PlayerName.java
@@ -4,18 +4,26 @@ public class PlayerName {
 
     private static final int MIN_LENGTH = 1;
     private static final int MAX_LENGTH = 5;
-
     private final String playerName;
 
     public PlayerName(String playerName) {
+        playerName = removeWhiteSpace(playerName);
         validatePlayerNameLength(playerName);
         this.playerName = playerName;
+    }
+
+    private String removeWhiteSpace(String playerName) {
+        return playerName.replaceAll(" ", "");
     }
 
     private void validatePlayerNameLength(String playerName) {
         if (playerName.length() < MIN_LENGTH || playerName.length() > MAX_LENGTH) {
             throw new IllegalArgumentException(ErrorMessage.EXCEPTION_INVALID_LENGTH_NAME.getMessage());
         }
+    }
+
+    public String getPlayerName() {
+        return playerName;
     }
 
     private enum ErrorMessage {
