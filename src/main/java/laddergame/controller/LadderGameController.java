b34cf18b04c95c515cac8b6f11d96dc58587f2c1
jiwon <ljwon77@daum.net>
2023-02-15 18:48:20 +0900

    feat: LadderGame에 대한 Controller를 구현한다

---
+++ src/main/java/laddergame/controller/LadderGameController.java
@@ -0,0 +1,42 @@
+package laddergame.controller;
+
+import laddergame.domain.ladder.Ladder;
+import laddergame.domain.ladder.LadderFactory;
+import laddergame.domain.participant.Participants;
+import laddergame.domain.rung.RungNumberGenerator;
+import laddergame.view.InputView;
+import laddergame.view.OutputView;
+import laddergame.view.message.Message;
+
+public class LadderGameController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public LadderGameController(final InputView inputView, final OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void start() {
+        Participants participants = getParticipants();
+        LadderFactory ladderFactory = new LadderFactory(new RungNumberGenerator());
+        Ladder ladder = getLadder(participants, ladderFactory);
+    }
+
+    private Participants getParticipants() {
+        return inputView.getUserInput(() -> {
+            OutputView.print(Message.INPUT_PARTICIPANT_NAMES_GUIDE.getMessage());
+            String participantNames = inputView.readConsole();
+            return new Participants(participantNames);
+        });
+    }
+
+    private Ladder getLadder(final Participants participants, final LadderFactory ladderFactory) {
+        return inputView.getUserInput(() -> {
+            OutputView.print(Message.INPUT_LADDER_MAX_HEIGHT_GUIDE.getMessage());
+            String maxLadderHeight = inputView.readConsole();
+            return ladderFactory.createLadder(maxLadderHeight, participants.size());
+        });
+    }
+}
