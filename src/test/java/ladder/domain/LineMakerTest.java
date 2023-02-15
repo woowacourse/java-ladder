cc66db64f6b7923e10d258d196fce44af3973a16
DESKTOP-FNVN0HH\USER <beatus96@naver.com>
2023-02-15 14:24:55 +0900

    test: LineMaker 테스트 수정

--- src/test/java/ladder/domain/LineMakerTest.java
+++ src/test/java/ladder/domain/LineMakerTest.java
@@ -1,5 +1,6 @@
 package ladder.domain;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,7 +10,6 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LineMakerTest {
-    private LineMaker lineMaker;
     private int playerCount;
     private RandomGenerator<Boolean> randomGenerator;
 
@@ -17,19 +17,19 @@ public class LineMakerTest {
     void setup() {
         playerCount = 5;
         randomGenerator = new RandomBooleanGenerator();
-        lineMaker = new LineMaker(playerCount, randomGenerator);
     }
     @Test
-    @DisplayName("플레이어 숫자대로 리스트가 생성되는지 확인한다.")
+    @DisplayName("플레이어 수 -1 크기의 리스트가 생성되는지 확인한다.")
     void generateLineSizeTest() {
-        assertThat(lineMaker.generate().size()).isEqualTo(playerCount);
+        assertThat(LineMaker.generate(playerCount, randomGenerator).size()).isEqualTo(playerCount-1);
     }
 
     @Test
     @DisplayName("생성된 Line의 가로가 겹치지 않는지 확인한다.")
     void generateLineTest() {
         // 리스트가 연속된 true값을 갖지 않은지.
-        List<Boolean> bars = lineMaker.generate();
+        List<Boolean> bars = LineMaker.generate(playerCount, randomGenerator);
+
         for (int idx = 0; idx < bars.size() - 1; idx++) {
             assertThat(!bars.get(idx) || !bars.get(idx + 1)).isTrue();
         }
