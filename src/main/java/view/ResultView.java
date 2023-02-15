16794c99f076130bd3997c17b0704441661cde45
DESKTOP-FNVN0HH\USER <beatus96@naver.com>
2023-02-15 17:11:09 +0900

    feat: ResultView 생성 및 출력 기능 추가

---
+++ src/main/java/view/ResultView.java
@@ -0,0 +1,54 @@
+package view;
+
+import ladder.domain.Bar;
+import ladder.domain.Line;
+import ladder.domain.Name;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ResultView {
+
+    private static final String BLANK_BETWEEN_NAMES = " ";
+    private static final String BAR_SYMBOL = "-";
+    private static final String EMPTY_BAR_SYMBOL = " ";
+    private static final String LADDER_VERTICAL_SYMBOL = "|";
+    private static final String OUTPUT_RESULT_MESSAGE = "실행결과";
+
+    public static void printLadder(List<Name> playerNames, int nameLength, List<Line> ladder) {
+        System.out.println(OUTPUT_RESULT_MESSAGE);
+        System.out.println(convertPlayerNames(playerNames, nameLength));
+        System.out.println(convertLadder(ladder, nameLength));
+
+    }
+
+    private static String convertPlayerNames(List<Name> playerNames, int nameLength) {
+        String pattern = "%" + nameLength + "s";
+
+        return playerNames.stream()
+                .map(name -> String.format(pattern, name.getName()))
+                .collect(Collectors.joining(BLANK_BETWEEN_NAMES));
+    }
+
+    private static String convertLadder(List<Line> ladder, int nameLength) {
+        return ladder.stream()
+                .map(line -> convertLine(line, nameLength))
+                .collect(Collectors.joining(System.lineSeparator()));
+    }
+
+    private static String convertLine(Line line, int nameLength) {
+        return line.getLine().stream()
+                .map(bar -> convertBar(bar, nameLength))
+                .collect(Collectors.joining(LADDER_VERTICAL_SYMBOL, String.format("%5s", LADDER_VERTICAL_SYMBOL), LADDER_VERTICAL_SYMBOL));
+    }
+
+    private static String convertBar(Bar bar, int nameLength) {
+        String barSymbol = BAR_SYMBOL.repeat(nameLength);
+        String emptyBarSymbol = EMPTY_BAR_SYMBOL.repeat(nameLength);
+
+        if (bar.getValue()) {
+            return barSymbol;
+        }
+        return emptyBarSymbol;
+    }
+}
