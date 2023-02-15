92c7b08f52d8db2c4f99606877dea8720235fe54
moonchaewon <codnjs8989@gmail.com>
2023-02-15 19:10:42 +0900

    fix: fix output messages

--- src/main/java/laddergame/view/OutputView.java
+++ src/main/java/laddergame/view/OutputView.java
@@ -8,8 +8,8 @@ import java.util.List;
 public class OutputView {
 
     private static final String PLAYER_NAMES_REQUEST_MSG = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요) ";
-    private static final String LADDER_HEIGHT_MSG = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요) ";
-    private static final String RESULT_INFO_MSG = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요) ";
+    private static final String LADDER_HEIGHT_MSG = "최대 사다리 높이는 몇 개인가요?";
+    private static final String RESULT_INFO_MSG = "실행결과";
     private static final String PLAYER_NAME_FORMAT = "%6s";
     private static final String LADDER_FORMAT = "%s|";
 
