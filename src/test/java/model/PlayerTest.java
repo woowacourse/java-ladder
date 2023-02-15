672157002f42741d74691304f23d51ad3c50f6a5
MoonJeWoong <munjin0201@naver.com>
2023-02-16 01:47:36 +0900

    test: 참여자의 이름 정보를 조회할 수 있는 기능 테스트 추가

--- src/test/java/model/PlayerTest.java
+++ src/test/java/model/PlayerTest.java
@@ -1,5 +1,7 @@
 package model;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.assertj.core.api.Assertions;
@@ -9,7 +11,19 @@ public class PlayerTest {
     @Test
     @DisplayName("Player 객체 생성 성공 테스트")
     void createPlayerTest(){
-        Assertions.assertThatNoException().isThrownBy(()->{Player player = new Player("ocean");});
+        Assertions.assertThatNoException().isThrownBy(()->{Player player = new Player(new Name("pobi"));});
     }
 
+    @Test
+    @DisplayName("참여자의 이름 정보를 조회할 수 있는 기능 테스트")
+    void getPlayerNameTest() {
+        //Given
+        Player player = new Player(new Name("pobi"));
+
+        //When
+        Name result = player.getName();
+
+        //Then
+        assertThat(result.getName()).isEqualTo("pobi");
+    }
 }
