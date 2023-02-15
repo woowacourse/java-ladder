f16c2b3c391410794d549971246a3164957b302c
ezzanzzan <cjsdmswjd821@naver.com>
2023-02-15 15:48:38 +0900

    feat: Line 클래스에 연속적으로 true가 포함될 때 예외처리하는 테스트-기능 구현

--- src/main/java/laddergame/model/Line.java
+++ src/main/java/laddergame/model/Line.java
@@ -8,12 +8,26 @@ public class Line {
     static final Random random = new Random();
     private final List<Boolean> line;
 
+    public Line(List<Boolean> line) {
+        for (int i = 0; i < line.size() - 1; i++) {
+            if (line.get(i) && line.get(i + 1)) {
+                throw new IllegalArgumentException("[ERROR]");
+            }
+        }
+        this.line = line;
+    }
+
     public Line(int personCount) {
+        this(getBooleans(personCount));
+    }
+
+    private static List<Boolean> getBooleans(int personCount) {
         List<Boolean> line = new ArrayList<>();
+
         for (int i = 0; i < personCount - 1; i++) {
             line.add(random.nextBoolean());
         }
-        this.line= line;
+        return line;
     }
 
     public int getSize() {
--- src/test/java/laddergame/model/LineTest.java
+++ src/test/java/laddergame/model/LineTest.java
@@ -1,7 +1,9 @@
 package laddergame.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,5 +13,11 @@ class LineTest {
     void Should_Success_When_MakeLine() {
         int personCount = 4;
         assertThat(new Line(personCount).getSize()).isEqualTo(personCount - 1);
+    }
+
+    @Test
+    @DisplayName("리스트에 연속적으로 true가 포함될 경우 예외 발생")
+    void Should_ThrowException_When_ContinueTrue() {
+        assertThatThrownBy(()->new Line(List.of(true,true)));
     }
 }
