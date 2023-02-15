a827cb27b0cdc09a09b16afde43e0041a6e89448
jeonghoon <shb03323@naver.com>
2023-02-15 17:21:49 +0900

    feat: 다리가 겹치지 않는 사다리 생성

--- src/main/java/ladder/domain/LadderMaker.java
+++ src/main/java/ladder/domain/LadderMaker.java
@@ -2,16 +2,30 @@ package ladder.domain;
 
 import ladder.exception.LadderLengthException;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LadderMaker {
 
     public Ladder makeLadder(final int playerNumber, final int height) {
         validateLadderLength(playerNumber, height);
-        return new Ladder(height);
+        List<Line> lines = generateEachLines(playerNumber, height);
+        return new Ladder(lines);
     }
 
-    private static void validateLadderLength(final int playerNumber, final int height) {
+    private void validateLadderLength(final int playerNumber, final int height) {
         if (height <= playerNumber - 1) {
             throw new LadderLengthException();
         }
+    }
+
+    private List<Line> generateEachLines(final int playerNumber, final int height) {
+        List<Line> lines = new ArrayList<>();
+        LineMaker lineMaker = new LineMaker(new RandomBlockGenerator());
+
+        for (int i = 0; i < height; i++) {
+            lines.add(lineMaker.makeLine(playerNumber));
+        }
+        return lines;
     }
 }
