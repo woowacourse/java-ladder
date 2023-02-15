17a9b7ffac0560952ba57c4178df6e33aa7debea
donghae <donghae1999@gmail.com>
2023-02-15 21:43:01 +0900

    refactor: 사용하지 않는 코드 제거

--- src/test/java/model/LadderHeightTest.java
+++ src/test/java/model/LadderHeightTest.java
@@ -11,14 +11,14 @@ public class LadderHeightTest {
     @Test
     @DisplayName("LadderHeight 객체 생성 성공 테스트")
     void createLadderHeightTest(){
-        Assertions.assertThatNoException().isThrownBy(()->{LadderHeight ladderHeight = new LadderHeight(5);});
+        Assertions.assertThatNoException().isThrownBy(()->{new LadderHeight(5);});
     }
 
     @Test
     @DisplayName("LadderHeight가 1 미만일 경우 실패 테스트")
     void validateLadderHeightTest() {
         //When
-        Throwable result = catchThrowable(()->{LadderHeight ladderHeight = new LadderHeight(0);});
+        Throwable result = catchThrowable(()->{new LadderHeight(0);});
 
         //Then
         assertThat(result).isInstanceOf(IllegalArgumentException.class);
