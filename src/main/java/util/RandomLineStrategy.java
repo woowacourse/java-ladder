821ea4fbb90d977270af141319b49bb8fe1c7529
seokjin8678 <rnrn8678@naver.com>
2023-02-15 14:49:13 +0900

    feat(RandomLineStrategy): Line 하나를 랜덤하게 생성하는 기능 추가

--- src/main/java/util/RandomLineStrategy.java
+++ src/main/java/util/RandomLineStrategy.java
@@ -3,17 +3,35 @@ package util;
 import domain.Line;
 import domain.Step;
 import java.util.Random;
+import java.util.Stack;
 
 public class RandomLineStrategy implements LineStrategy {
     private final Random random = new Random();
 
     @Override
     public Line generate(int width) {
-        return null;
+        Stack<Step> steps = new Stack<>();
+        while (true) {
+            if (steps.size() == width - 1) {
+                break;
+            }
+            if (steps.isEmpty()) {
+                steps.add(createRandomStep());
+            }
+            if (steps.peek() == Step.EXIST) {
+                steps.add(Step.EMPTY);
+                continue;
+            }
+            steps.add(createRandomStep());
+        }
+        return new Line(steps);
     }
 
     private Step createRandomStep() {
-        int number = random.nextInt(10);
-        return null;
+        int number = random.nextInt(1);
+        if (number == 0) {
+            return Step.EMPTY;
+        }
+        return Step.EXIST;
     }
 }
