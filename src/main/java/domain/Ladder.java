6f45cb00e070cb2a9b1ee51a9b31e2e6c6762e43
kiarakim <kiarakim413@gmail.com>
2023-02-15 15:36:53 +0900

    feat(Ladder) : Ladder 발판 생성 기능 추가 및 테스트
    Co-authored-by: nuyh99 <hjaehyun25@gmail.com>

--- src/main/java/domain/Ladder.java
+++ src/main/java/domain/Ladder.java
@@ -1,10 +1,12 @@
 package domain;
 
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Ladder {
+	private static final Random random = new Random();
 	private final List<List<Boolean>> level;
 
 	public Ladder(int height, int participantSize) {
@@ -22,6 +24,14 @@ public class Ladder {
 		return level.get(0).size();
 	}
 
+	public List<List<Boolean>> getLadder() {
+		return level;
+	}
+
+	public void makeLadder() {
+		getLadder().forEach(Ladder::makeStools);
+	}
+
 	private void validate(int height) {
 		if (height < 1 || height > 100)
 			throw new IllegalArgumentException("높이는 1부터 100까지만 가능합니다");
@@ -31,5 +41,27 @@ public class Ladder {
 		return IntStream.range(0, participantSize - 1)
 			.mapToObj(o -> false)
 			.collect(Collectors.toList());
+	}
+
+	private static void makeStools(List<Boolean> level) {
+		level.set(0, random.nextBoolean());
+		IntStream.range(1, level.size())
+			.forEach(index -> makeStool(level, index));
+
+		if (isNotValidLevel(level))
+			makeStools(level);
+	}
+
+	private static boolean isNotValidLevel(List<Boolean> level) {
+		return (int)level.stream()
+			.filter(stool -> stool)
+			.count() == 0;
+	}
+
+	private static void makeStool(List<Boolean> level, int index) {
+		if (level.get(index - 1))
+			return;
+
+		level.set(index, random.nextBoolean());
 	}
 }
--- src/test/java/domain/LadderTest.java
+++ src/test/java/domain/LadderTest.java
@@ -2,6 +2,8 @@ package domain;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -13,7 +15,7 @@ class LadderTest {
 	@ParameterizedTest
 	@ValueSource(ints = {1, 50, 100})
 	void height1_100(int height) {
-		Ladder ladder = new Ladder(height, 5);
+		Ladder ladder = initLadder(height, 5);
 
 		assertThat(ladder.getHeight()).isBetween(1, 100);
 	}
@@ -30,10 +32,38 @@ class LadderTest {
 	@DisplayName("참여자수 - 1 만큼의 열을 가진다")
 	@Test
 	void columnSizeParticipantsMinusOne() {
-		int height = 7;
 		int participantSize = 5;
-		Ladder ladder = new Ladder(height, participantSize);
+		Ladder ladder = initLadder(4, participantSize);
 
 		assertThat(ladder.getColumnSize()).isEqualTo(participantSize - 1);
+	}
+
+	@DisplayName("발판은 연속될 수 없다")
+	@Test
+	void stoolNotContinuous() {
+		Ladder ladder = initLadder(4, 5);
+
+		ladder.makeLadder();
+
+		for (List<Boolean> level : ladder.getLadder())
+			for (int i = 0; i < level.size() - 1; i++)
+				if (level.get(i) && level.get(i + 1))
+					fail("발판은 연속될 수 없다");
+	}
+
+	@DisplayName("한 라인에는 반드시 하나의 발판이 있어야 한다")
+	@Test
+	void lineMustHaveStool() {
+		Ladder ladder = initLadder(4, 6);
+		ladder.makeLadder();
+
+		for (List<Boolean> level : ladder.getLadder()) {
+			int count = (int)level.stream().filter(stool -> stool).count();
+			assertThat(count).isGreaterThan(0);
+		}
+	}
+
+	private static Ladder initLadder(int height, int participantSize) {
+		return new Ladder(height, participantSize);
 	}
 }
