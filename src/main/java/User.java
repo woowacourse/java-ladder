8c36f2ec69959c530ad37a2b4c5a4af9c4d1784e
thdwoqor <celebarte01@gmail.com>
2023-02-15 13:59:09 +0900

    feat : 유저 이름 공백 유효성 검사 코드 작성

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -5,6 +5,9 @@ public class User {
         if (1 > name.length() || 5 < name.length()) {
             throw new IllegalArgumentException("이름은 1~5 글자 입니다.");
         }
+        if (name.isBlank()){
+            throw new IllegalArgumentException("이름은 공백으로만 이루어지면 안됩니다.");
+        }
         this.name = name;
     }
 }
