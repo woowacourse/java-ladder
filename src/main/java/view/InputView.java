1061aa52a8971b1332171c017b1eacfe781e1537
0chil <0@chll.it>
2023-02-15 15:35:07 +0900

    feat : 입력 문구 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -8,6 +8,7 @@ public class InputView {
 
     public static List<String> inputName() {
 
+        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
         String input = ScannerUtil.nextLine();
 
         return List.of(input.split(","));
@@ -15,6 +16,7 @@ public class InputView {
 
     public static int inputMaxLadderHeight() {
 
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
         return ScannerUtil.nextInt();
     }
 }
