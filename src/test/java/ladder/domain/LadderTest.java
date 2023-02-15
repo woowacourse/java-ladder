173182570c519f1aa6f8dfeeb82565711870cb45
JunDonghyuk <ehdgur4814@naver.com>
2023-02-15 16:50:48 +0900

    test: 라인 수에 맞는 사다리 생성 테스트

---
+++ src/test/java/ladder/domain/LadderTest.java
@@ -0,0 +1,37 @@
+package ladder.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LadderTest {
+
+
+    @Test
+    @DisplayName("사다리가 라인 수에 맞게 생성된다.")
+    void generateLadderTest() {
+
+        class IntendedBooleanGenerator implements BooleanGenerator {
+
+            @Override
+            public Boolean generate() {
+                return Boolean.TRUE;
+            }
+        }
+
+        List<Line> lines = List.of(
+                new Line(4, new IntendedBooleanGenerator()),
+                new Line(4, new IntendedBooleanGenerator()),
+                new Line(4, new IntendedBooleanGenerator()),
+                new Line(4, new IntendedBooleanGenerator()),
+                new Line(4, new IntendedBooleanGenerator())
+        );
+
+        Ladder ladder = new Ladder(lines);
+        List<Line> copiedLines = ladder.getLines();
+        assertEquals(5, copiedLines.size());
+    }
+}
