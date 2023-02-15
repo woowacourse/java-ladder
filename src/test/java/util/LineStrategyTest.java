2749f4fffd54219b5a7742c166d18e05176315af
seokjin8678 <rnrn8678@naver.com>
2023-02-15 15:05:59 +0900

    test(LineStrategyTest): 연속되는 Step이 있으면 예외가 발생하는 테스트 코드 추가

--- src/test/java/util/LineStrategyTest.java
+++ src/test/java/util/LineStrategyTest.java
@@ -3,6 +3,9 @@ package util;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import domain.Line;
+import domain.Step;
+import java.util.List;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -20,5 +23,17 @@ class LineStrategyTest {
         // then
         assertThat(line.getSize())
                 .isEqualTo(4);
+    }
+    
+    @Test
+    @DisplayName("Line의 Step이 연속되면 안된다.")
+    void create_continuousStep() {
+        // given
+        LineStrategy lineStrategy = new CustomLineStrategy(List.of(Step.EMPTY, Step.EXIST, Step.EXIST));
+
+        // expect
+        Assertions.assertThatIllegalArgumentException().isThrownBy(() -> {
+            lineStrategy.generate(4);
+        }).withMessage("[ERROR] 라인에 Step이 연속될 수 없습니다.");
     }
 }
