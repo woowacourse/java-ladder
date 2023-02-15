cad529c99ab0359255df68d344835ef953387dbd
kiarakim <kiarakim413@gmail.com>
2023-02-15 17:41:32 +0900

    refactor(People) : 정적 팩토리 메서드 패턴 적용
    Co-authored-by: nuyh99 <hjaehyun25@gmail.com>

--- src/main/java/domain/People.java
+++ src/main/java/domain/People.java
@@ -9,21 +9,40 @@ public class People {
 
 	private final List<Person> people;
 
-	public People(List<String> names) {
+	private People(List<Person> people) {
+		this.people = people;
+	}
+
+	public static People from(List<String> names) {
 		validate(names);
-		this.people = names.stream()
+
+		return new People(names.stream()
 			.map(Person::new)
-			.collect(Collectors.toUnmodifiableList());
+			.collect(Collectors.toUnmodifiableList()));
 	}
 
 	public int size() {
 		return people.size();
 	}
 
-	private void validate(List<String> names) {
+	private static void validate(List<String> names) {
+		validateSize(names);
+		validateDuplication(names);
+	}
+
+	private static void validateSize(List<String> names) {
 		if (names.size() < MIN_PEOPLE_SIZE_INCLUSIVE)
 			throw new IllegalArgumentException("사람은 최소 두명 이상이어야 합니다");
 		if (names.size() > MAX_PEOPLE_SIZE_INCLUSIVE)
 			throw new IllegalArgumentException("사람은 최대 10명 이어야 합니다");
+	}
+
+	private static void validateDuplication(List<String> names) {
+		int distinctCount = (int)names.stream()
+			.map(String::trim)
+			.distinct()
+			.count();
+		if (distinctCount != names.size())
+			throw new IllegalArgumentException("사람 이름은 중복되지 않아야 합니다");
 	}
 }
--- src/test/java/domain/PeopleTest.java
+++ src/test/java/domain/PeopleTest.java
@@ -14,7 +14,7 @@ class PeopleTest {
 	@DisplayName("2~10 명이다")
 	@Test
 	void peopleSizeBetween2_10() {
-		People people = new People(List.of("salmn", "kiara"));
+		People people = People.from(List.of("salmn", "kiara"));
 
 		assertThat(people.size()).isBetween(2, 10);
 	}
@@ -22,7 +22,7 @@ class PeopleTest {
 	@DisplayName("2명 미만이면 예외가 발생한다")
 	@Test
 	void peopleSize1() {
-		assertThatThrownBy(() -> new People(List.of("kiara")))
+		assertThatThrownBy(() -> People.from(List.of("kiara")))
 			.isInstanceOf(IllegalArgumentException.class)
 			.hasMessage("사람은 최소 두명 이상이어야 합니다");
 	}
@@ -34,8 +34,16 @@ class PeopleTest {
 			.mapToObj(i -> "kia" + i)
 			.collect(Collectors.toList());
 
-		assertThatThrownBy(() -> new People(names))
+		assertThatThrownBy(() -> People.from(names))
 			.isInstanceOf(IllegalArgumentException.class)
 			.hasMessage("사람은 최대 10명 이어야 합니다");
+	}
+
+	@DisplayName("중복된 이름은 예외가 발생한다")
+	@Test
+	void duplication() {
+		assertThatThrownBy(() -> People.from(List.of("kiara", "kiara")))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessage("사람 이름은 중복되지 않아야 합니다");
 	}
 }
