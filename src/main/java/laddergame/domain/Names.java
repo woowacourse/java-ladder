d6de97b741179ea6dbf4562f382bb89ecbb6fe19
junpakPark <junpak.park@gmail.com>
2023-02-15 16:51:42 +0900

    feat: Names 객체 생성 시 이름 개수 검증

--- src/main/java/laddergame/domain/Names.java
+++ src/main/java/laddergame/domain/Names.java
@@ -8,7 +8,15 @@ public class Names {
     private final List<Name> names;
 
     public Names(final List<String> names) {
+        validateNUmberOfNames(names);
+
         this.names = Collections.unmodifiableList(names.stream().map(Name::new)
                 .collect(Collectors.toList()));
+    }
+
+    private void validateNUmberOfNames(final List<String> names) {
+        if (names.size() < 2) {
+            throw new IllegalArgumentException();
+        }
     }
 }
--- src/test/java/laddergame/domain/NamesTest.java
+++ src/test/java/laddergame/domain/NamesTest.java
@@ -4,13 +4,19 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class NamesTest {
 
     @Test
-    @DisplayName("문자열 리스트로 Names 객체를 생성한다.")
+    @DisplayName("List<String>이 입력되면 Names 객체를 생성한다.")
     void givenStringList_thenCreateNames() {
         //given
         final List<String> names = List.of("에단", "준팍");
@@ -22,5 +28,16 @@ public class NamesTest {
         Assertions.assertThat(result)
                 .extracting("names")
                 .isEqualTo(List.of(new Name("에단"), new Name("준팍")));
+    }
+
+    @Test
+    @DisplayName("List<String>의 길이가 2미만이면 예외를 발생한다.")
+    void givenTwoUnderStringListSize_thenFail() {
+        //given
+        final List<String> wrongSizeList = List.of("에단");
+
+        //then
+        assertThatThrownBy((() -> new Names(wrongSizeList)))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
