202bba5db0576e2ad5b6991aae1f4827d8afd5bd
odo27 <odo27@odo27ui-MacBookAir.local>
2023-02-15 15:17:55 +0900

    feat(RandomNumberGenerator): 사다리 연결구조룰 무작위로 생성하는 NumberGenerator 구현 클래스

---
+++ src/main/java/domain/numbergenerator/RandomNumberGenerator.java
@@ -0,0 +1,13 @@
+package domain.numbergenerator;
+
+import java.util.Random;
+
+public class RandomNumberGenerator implements NumberGenerator {
+
+    private final Random random = new Random();
+
+    @Override
+    public int generate() {
+        return random.nextInt(2);
+    }
+}
