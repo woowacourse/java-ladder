f4bb6d9d1fbdba3f45c998866cad51d758542215
drunkenhw <drunkenhw@gmail.com>
2023-02-15 15:39:39 +0900

    feat: 플레이어 생성

---
+++ src/main/java/ladder/Player.java
@@ -0,0 +1,9 @@
+package ladder;
+
+public class Player {
+    private final Name name;
+
+    public Player(String nameValue) {
+        this.name = new Name(nameValue);
+    }
+}
---
+++ src/test/java/ladder/PlayerTest.java
@@ -0,0 +1,19 @@
+package ladder;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PlayerTest {
+
+    @Test
+    @DisplayName("이름을 입력받아 Player를 생성한다")
+    public void shouldCreatePlayerWhenInputName() {
+        // given
+        // when
+        // then
+        assertDoesNotThrow(() -> new Player("name"));
+    }
+
+}
