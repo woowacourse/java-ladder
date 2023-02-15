978373f61e32ab358bf08df50f86a61f8429747a
Eunkeee <eunkeeeee@gmail.com>
2023-02-15 17:16:43 +0900

    test(Names): 사람 수 테스트 코드 작성

---
+++ src/test/java/domain/NamesTest.java
@@ -0,0 +1,25 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class NamesTest {
+    @DisplayName("사람의 수는 2명 이상 100명 이하이다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"깃짱,제리,이리내,제나", "깃짱,제리"})
+    void validNamesTest(String names) {
+        Assertions.assertDoesNotThrow(() -> new Names(names));
+    }
+
+    @DisplayName("2명 미만 100명 초과의 사람 수는 예외 처리한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"깃짱", "제리"})
+    void invalidNamesTest(String names) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Names(names));
+    }
+}
