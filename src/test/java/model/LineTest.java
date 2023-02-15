32bbd9c5b0767d54e332a8ce53ff5e4c2ab67181
MoonJeWoong <munjin0201@naver.com>
2023-02-15 23:06:18 +0900

    test: 각 포인트들은 랜덤하게 만드는 기능 테스트 추가

--- src/test/java/model/LineTest.java
+++ src/test/java/model/LineTest.java
@@ -6,6 +6,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LineTest {
@@ -28,5 +29,34 @@ public class LineTest {
         //Then
         assertThat(result.size()).isEqualTo(3);
     }
+
+    @Test
+    @DisplayName("각 포인트들은 랜덤하게 만든다.")
+    void makePointsRandomlyTest() {
+        //Given
+        Line line = new Line(new TestPointGenerator(new ArrayList<>(List.of(false,true,false))), 4);
+
+        //When
+        List<Boolean> result = line.getPoints();
+
+        //Then
+        assertThat(result).isEqualTo(List.of(false,true,false));
+    }
+
+    private class TestPointGenerator implements PointGenerator{
+
+        private final List<Boolean> points;
+
+        public TestPointGenerator(List<Boolean> points) {
+            this.points = new ArrayList<>(points);
+        }
+
+        @Override
+        public boolean generate() {
+            return points.remove(0);
+        }
+
+    }
+
 
 }
