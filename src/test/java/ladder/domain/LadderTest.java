cd8afb9ccc5d9a448098a6186d20830cf4d963c3
DESKTOP-FNVN0HH\USER <beatus96@naver.com>
2023-02-15 17:03:00 +0900

    test: Ladder 도메인 테스트

---
+++ src/test/java/ladder/domain/LadderTest.java
@@ -0,0 +1,27 @@
+package ladder.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import view.ResultView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LadderTest {
+    private final int heightOfLadder = 5;
+    private final int playerCount = 5;
+
+    @Test
+    @DisplayName("사다리 생성을 테스트")
+    void ladderInitiatorTest() {
+        List<Line> lines = new ArrayList<>();
+
+        for(int i = 0; i < heightOfLadder; i++) {
+            List<Bar> bars = LineMaker.generate(playerCount, new MockRandomBooleanGenerator());
+            lines.add(new Line(bars));
+        }
+
+        Assertions.assertDoesNotThrow(new Ladder(lines));
+    }
+}
