9ec3aa75d57b4f6149d83f68bdbcad18bb933157
moonchaewon <codnjs8989@gmail.com>
2023-02-15 14:18:36 +0900

    feat: implement method to validate player name

--- src/main/java/laddergame/domain/Player.java
+++ src/main/java/laddergame/domain/Player.java
@@ -5,6 +5,13 @@ public class Player {
     private final String name;
 
     public Player(String name) {
+        validatePlayerName(name);
         this.name = name;
+    }
+
+    private void validatePlayerName(String name) {
+        if (name.isBlank() || name.length() > 5) {
+            throw new IllegalArgumentException("[ERROR] 플레이어 이름은 공백이 아닌 5글자 이하여야 합니다.");
+        }
     }
 }
