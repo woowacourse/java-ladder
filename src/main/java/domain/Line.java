cb445caa9a3aef9ada3ce1762c25eb26f0db3241
BackFoxx <backfoxxx@gmail.com>
2023-02-15 17:07:07 +0900

    refactor: 사용하지 않는 생성자 제거 및 함수 추출

--- src/main/java/domain/Line.java
+++ src/main/java/domain/Line.java
@@ -8,48 +8,40 @@ import java.util.List;
 public class Line {
 
     private final List<FootStep> footSteps = new ArrayList<>();
-    private BooleanGenerator generator;
+    private final BooleanGenerator generator;
 
-    public Line(final int width){
-        for (int i = 0; i < width; i++) {
-            footSteps.add(new FootStep(true));
-        }
-
-        generator = null;
-    }
-
-    public Line(final int width, BooleanGenerator generator){
-        this(width);
+    public Line(final BooleanGenerator generator) {
         this.generator = generator;
     }
 
-    public Line(BooleanGenerator generator){
-        this.generator = generator;
-    }
-
-    public Line(FootStep firstFootstep) {
-        this.footSteps.add(firstFootstep);
-        this.generator = null;
-    }
-
-    public boolean isSteppableAt(final int index){
+    public boolean isSteppableAt(final int index) {
         return this.footSteps
                 .get(index)
                 .isSteppable();
     }
 
-    public int getWidth() {
-        return footSteps.size();
+    public void generateFootStep() {
+        if (!isStartEdge()) {
+            FootStep previous = getPreviousFootStep();
+            addFootStep(previous.isSteppable());
+            return;
+        }
+        addFootStep(false);
     }
 
-    public void generateFootStep() {
-        if (this.footSteps.size() != 0) {
-            FootStep previous = this.footSteps.get(this.footSteps.size() - 1);
-            if (previous.isSteppable()) {
-                this.footSteps.add(new FootStep(false));
-                return;
-            }
+    private FootStep getPreviousFootStep() {
+        return this.footSteps.get(this.footSteps.size() - 1);
+    }
+
+    private void addFootStep(final boolean isPreviousFootStepSteppable) {
+        if (isPreviousFootStepSteppable) {
+            this.footSteps.add(new FootStep(false));
+            return;
         }
         this.footSteps.add(new FootStep(generator.generate()));
+    }
+
+    private boolean isStartEdge() {
+        return this.footSteps.size() == 0;
     }
 }
--- src/test/java/domain/LineTest.java
+++ src/test/java/domain/LineTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import util.BooleanGenerator;
 import util.FixBooleanGenerator;
@@ -12,23 +13,6 @@ import static org.assertj.core.api.Assertions.*;
 
 @DisplayName("사다리 한 층은 ")
 class LineTest {
-
-    @Nested
-    class Generate {
-        @Test
-        @DisplayName(" 생성될 때 사람 수 만큼 다리를 생성한다.")
-        void generateLine() {
-            assertThatCode(() -> new Line(5))
-                    .doesNotThrowAnyException();
-        }
-
-        @Test
-        @DisplayName(" 사람이 다섯 명이라면 다리의 폭이 5다.")
-        void givenFivePerson_thenWidthFive() {
-            assertThat(new Line(5).getWidth())
-                    .isEqualTo(5);
-        }
-    }
 
     @Nested
     @DisplayName(" 이전 다리가 이전 다리가 건널 수")
@@ -57,21 +41,18 @@ class LineTest {
     }
 
     @Nested
+    @DisplayName(" 만약 특정 다리가")
     class SteepableInfomation {
-        @Test
-        @DisplayName(" 만약 특정 다리가 건널 수 있으면 true를 반환하다.")
-        void crossableFootStepCase() {
-            BooleanGenerator trueGenerator = new FixBooleanGenerator(true);
-            assertThat(new Line(5, trueGenerator).isSteppableAt(1))
-                    .isTrue();
-        }
+        @ParameterizedTest(name = " {0}으면 {1}를 반환하다.")
+        @CsvSource(value = {"있,true", "없,false"})
+        void crossableFootStepCase(String condition, boolean value) {
+            BooleanGenerator trueGenerator = new FixBooleanGenerator(value);
 
-        @Test
-        @DisplayName(" 만약 특정 다리가 건널 수 없으면 false를 반환하다.")
-        void uncrossableFootStepCase() {
-            BooleanGenerator falseGenerator = new FixBooleanGenerator(false);
-            assertThat(new Line(5, falseGenerator).isSteppableAt(1))
-                    .isFalse();
+            Line line = new Line(trueGenerator);
+            line.generateFootStep();
+
+            assertThat(line.isSteppableAt(0))
+                    .isEqualTo(value);
         }
     }
 }
