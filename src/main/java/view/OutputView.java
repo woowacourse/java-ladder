3ed2d415a6c876c431a18eab7d4307eaa4f2b001
2014012606 <2014012606@hanyang.ac.kr>
2023-02-15 14:18:55 +0900

    feat: 사람 이름을 형식에 맞게 출력하는 기능 추가
    - "실행결과" 메세지를 출력하는 메소드 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -6,14 +6,35 @@ import domain.Point;
 import java.util.List;
 
 public class OutputView {
+    private static final String RESULT_MESSAGE = "실행결과";
     private static final String BLANK_LINE = "     ";
     private static final String CONNECTED_LINE = "-----";
     private static final String LADDER = "|";
+
+
+    public void printResultMessage() {
+        System.out.println(RESULT_MESSAGE);
+        System.out.println();
+    }
+
+    public void printUserNames(List<String> userNames) {
+        String formattedUserNames = formatUserNames(userNames);
+        System.out.println(formattedUserNames);
+    }
 
     public void printResult(List<Line> lines) {
         for (Line line : lines) {
             System.out.println(renderLine(line));
         }
+    }
+
+    private String formatUserNames(List<String> userNames) {
+        StringBuilder builder = new StringBuilder();
+        for (String userName : userNames) {
+            String formattedUserName = String.format("%6s", userName);
+            builder.append(formattedUserName);
+        }
+        return builder.toString();
     }
 
     private String renderLine(Line line) {
