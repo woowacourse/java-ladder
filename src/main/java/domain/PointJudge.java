f6e391d93627f020c480c9c618b4961d6f482379
kim0914 <kosa0914@naver.com>
2023-02-15 17:22:08 +0900

    feat(PointJudge): 다리를 만들지 결정하는 메서드 추가

---
+++ src/main/java/domain/PointJudge.java
@@ -0,0 +1,20 @@
+package domain;
+
+import java.util.List;
+
+public class PointJudge {
+
+    public static boolean canMake(List<Boolean> points, boolean flag, int index) {
+        if (flag == true) {
+            return isFirstIndexOrLeftEmpty(points, index);
+        }
+        return false;
+    }
+
+    private static boolean isFirstIndexOrLeftEmpty(List<Boolean> points, int index) {
+        if (index == 0 || points.get(index - 1) == false) {
+            return true;
+        }
+        return false;
+    }
+}
