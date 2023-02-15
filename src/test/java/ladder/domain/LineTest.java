86c3b7acc41918ec223998a27d97fc9532d1820c
DESKTOP-FNVN0HH\USER <beatus96@naver.com>
2023-02-15 14:51:37 +0900

    test: LineTest 테스트 수정

--- src/test/java/ladder/domain/LineTest.java
+++ src/test/java/ladder/domain/LineTest.java
@@ -9,31 +9,34 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LineTest {
-    private int personCount = 5;
+
+    private final List<Bar> bars = List.of(new Bar(true), new Bar(false));
     private Line line;
 
     @BeforeEach
     void setup() {
-        line = new Line(personCount);
+        line = new Line(bars);
     }
 
-    @DisplayName("Line 생성 시, Line의 Bar개수가 사람 수보다 1 작은지 확인한다.")
     @Test
-    void lineInitiatorTest() {
-        //int 사람 수 넣으면, 리스트의 크기가 사람수-1인지
-        assertThat(line.getLine().size())
-                .isEqualTo(personCount - 1);
-    }
-
-
     @DisplayName("Line의 가로 라인이 겹치지 않는지 확인한다.")
-    @Test
     void getLineTest() {
-        // 리스트가 연속된 true값을 갖지 않은지.
-        boolean check = false;
-        List<Boolean> bars = line.getLine();
+        List<Bar> bars = line.getLine();
+
         for (int idx = 0; idx < bars.size() - 1; idx++) {
-            assertThat(!bars.get(idx) || !bars.get(idx + 1)).isTrue();
+            Bar currentBar = bars.get(idx);
+            Bar nextBar = bars.get(idx + 1);
+
+            assertThat(!currentBar.getValue() || !nextBar.getValue())
+                    .isTrue();
         }
     }
+
+    @Test
+    @DisplayName("생성된 Line의 변수가 입력한 값과 같은 값을 포함하는지 확인한다")
+    void checkLineTest() {
+        assertThat(line.getLine())
+                .isEqualTo(bars);
+    }
+
 }
