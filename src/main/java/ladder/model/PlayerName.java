1416c127f173a0f8763d1ac32e951cbec106e952
LJW25 <dbjwl@g.skku.edu>
2023-02-15 16:31:41 +0900

    feat(PlayerName): 클래스 생성 및 플레이어 이름 길이 검증 메소드 구현

---
+++ src/main/java/ladder/model/PlayerName.java
@@ -0,0 +1,34 @@
+package ladder.model;
+
+public class PlayerName {
+
+    private static final int MIN_LENGTH = 1;
+    private static final int MAX_LENGTH = 5;
+
+    private final String playerName;
+
+    public PlayerName(String playerName) {
+        validatePlayerNameLength(playerName);
+        this.playerName = playerName;
+    }
+
+    private void validatePlayerNameLength(String playerName) {
+        if (playerName.length() < MIN_LENGTH || playerName.length() > MAX_LENGTH) {
+            throw new IllegalArgumentException(ErrorMessage.EXCEPTION_INVALID_LENGTH_NAME.getMessage());
+        }
+    }
+
+    private enum ErrorMessage {
+        EXCEPTION_INVALID_LENGTH_NAME("플레이어 이름은 1자 이상 5자 이하여야 합니다.");
+        private final String message;
+
+        ErrorMessage(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+    }
+
+}
