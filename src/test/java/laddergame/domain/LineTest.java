cfcd67edb6f073ab904d64e946f32742d730a153
hyunseo <goatsfish@gmail.com>
2023-02-15 16:56:00 +0900

    test: Line 생성 및 검증 테스트 추가

---
+++ src/test/java/laddergame/domain/LineTest.java
@@ -0,0 +1,35 @@
+package laddergame.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+class LineTest {
+    @Test
+    void create() {
+        assertDoesNotThrow(() -> new Line(List.of(true, false, true)));
+    }
+
+    @Test
+    void createWithBothFalse() {
+        assertDoesNotThrow(() -> new Line(List.of(false, false, true)));
+    }
+
+    @Test
+    void throwExceptionWhenPointsIsEmpty() {
+        assertThatThrownBy(()->new Line(List.of()));
+    }
+
+    @Test
+    void throwExceptionWhenEveryPointIsFalse() {
+        assertThatThrownBy(()-> new Line(List.of(false, false)));
+    }
+
+    @Test
+    void throwExceptionWhenBothTrue() {
+        assertThatThrownBy(()-> new Line(List.of(true, true, false)));
+    }
+}
