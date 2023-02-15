46854fa7c1f8d6ec6ce45c06f0a52122a70341af
Devtist <yt930310@gmail.com>
2023-02-15 14:06:58 +0900

    feat(Height): 사다리 높이 검증 기능 구현

--- src/main/java/ladder/error/ErrorMessage.java
+++ src/main/java/ladder/error/ErrorMessage.java
@@ -2,7 +2,8 @@ package ladder.error;
 
 public enum ErrorMessage {
     DUPLICATION("이름은 중복일 수 없습니다."),
-    NON_NUMERIC("사다리 높이는 숫자여야 합니다.");
+    NON_NUMERIC("사다리 높이는 숫자여야 합니다."),
+    INVALID_HEIGHT_RANGE("사다리 높이는 1이상, 10000 이하여야 합니다.");
 
     private static final String ERROR_FORMAT = "[ERROR]: %s";
 
