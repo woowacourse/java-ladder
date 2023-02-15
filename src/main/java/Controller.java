4d78cd7fef4e66f645374243f085f5e88c72113c
thdwoqor <celebarte01@gmail.com>
2023-02-15 16:49:24 +0900

    feat : Controller 구현

---
+++ src/main/java/Controller.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+public class Controller {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public Controller(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run(){
+        List<String> usersName = inputView.inputUsername();
+        Ladders ladders = new Ladders(inputView.inputLadderHeight(),new RandomGenerator());
+        ladders.make(usersName.size()-1);
+    }
+}
