924b9db09d0458176f0c85f28b4302809ce506ae
moonchaewon <codnjs8989@gmail.com>
2023-02-15 14:39:58 +0900

    feat: implement method to validate players

--- src/main/java/laddergame/domain/Players.java
+++ src/main/java/laddergame/domain/Players.java
@@ -1,14 +1,25 @@
 package laddergame.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Players {
 
     private final List<Player> players;
+
     public Players(List<String> playerNames) {
+        validatePlayerNames(playerNames);
         this.players = playerNames.stream()
                 .map(name -> new Player(name))
                 .collect(Collectors.toUnmodifiableList());
+    }
+
+    private void validatePlayerNames(List<String> playerNames) {
+        Set<String> nameSet = new HashSet<>(playerNames);
+        if (nameSet.size() != playerNames.size()) {
+            throw new IllegalArgumentException("[ERROR] 플레이어 이름이 중복되었습니다.");
+        }
     }
 }
--- src/test/java/laddergame/domain/PlayersTest.java
+++ src/test/java/laddergame/domain/PlayersTest.java
@@ -5,15 +5,23 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayersTest {
 
     @Test
     @DisplayName("Players가 정상적으로 생성된다.")
-    void playerCreateTest() {
+    void playersCreateTest() {
         List<String> playerNames = List.of("test1", "test2", "test3");
         assertDoesNotThrow(() -> new Players(playerNames));
+    }
+
+    @Test
+    @DisplayName("Player 이름이 중복될 경우 예외가 발생한다.")
+    void playersCreateExceptionTest() {
+        List<String> playerNames = List.of("test1", "test2", "test1");
+        assertThatThrownBy(() -> new Players(playerNames)).isInstanceOf(IllegalArgumentException.class);
     }
 
 }
