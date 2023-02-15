265b7d5b0a1075a58573ddd1e456e7b837c8609c
Cyma-s <ssol0319@naver.com>
2023-02-15 15:10:41 +0900

    test: 플레이어의 중복된 이름 검증 테스트 구현

--- src/test/java/ladder/domain/PlayersTest.java
+++ src/test/java/ladder/domain/PlayersTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PlayersTest {
 
@@ -20,5 +21,16 @@ class PlayersTest {
                     .isEqualTo(names.get(i));
 
         }
+    }
+
+    @Test
+    @DisplayName("중복된 이름이 입력되면 예외를 던진다.")
+    void players_throwExceptionDuplicatedNames() {
+        // given
+        List<String> duplicateNames = List.of("dochi", "dochi", "vero", "tori");
+
+        // expected
+        assertThatThrownBy(() -> new Players(duplicateNames))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
