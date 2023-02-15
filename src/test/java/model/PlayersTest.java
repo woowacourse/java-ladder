dd12c177bbe4207a952e5458ce4a8383b117f458
donghae <donghae1999@gmail.com>
2023-02-15 23:51:35 +0900

    test: PlayersTest 객체 생성 성공 테스트 추가

--- src/test/java/model/PlayersTest.java
+++ src/test/java/model/PlayersTest.java
@@ -3,12 +3,16 @@ package model;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class PlayersTest {
 
     @Test
     @DisplayName("Players 객체 생성 성공 테스트")
     void createCarsTest() {
-        Players players = new Players("pobi,crong,honux");
+        Players players = new Players(Arrays.asList("pobi,crong,honux"));
 
         assertThat(players.getPlayers().size()).isEqualTo(3);
     }
