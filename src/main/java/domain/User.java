123d019e09499320699bb5dd2bed12cd945da38b
2014012606 <2014012606@hanyang.ac.kr>
2023-02-15 17:39:25 +0900

    chore: user 메서드 파라미터에 final 키워드 추가

--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -1,9 +1,12 @@
 package domain;
 
+import exception.ErrorMessage;
+
 public class User {
+    private static final int MAX_NAME_LENGTH = 5;
     private final String name;
 
-    public User(String name) {
+    public User(final String name) {
         validateLength(name);
         validateBlank(name);
         this.name = name;
@@ -13,15 +16,15 @@ public class User {
         return name;
     }
 
-    private void validateLength(String name) {
-        if (name.length() > 5) {
-            throw new IllegalArgumentException();
+    private void validateLength(final String name) {
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException(ErrorMessage.USER_NAME_LENGTH_EXCEPTION.getMessage());
         }
     }
 
-    private void validateBlank(String name) {
+    private void validateBlank(final String name) {
         if (name.isBlank()) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ErrorMessage.USER_NAME_BLANK_EXCEPTION.getMessage());
         }
     }
 }
