567c27e00fb254e62efa394c3d9c3e4bdf3a92d9
seokjin8678 <rnrn8678@naver.com>
2023-02-15 16:50:57 +0900

    test(LadderTest): Ladder 클래스 구조 변경에 따른 테스트 코드 수정

--- src/test/java/domain/LadderTest.java
+++ src/test/java/domain/LadderTest.java
@@ -3,21 +3,19 @@ package domain;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 class LadderTest {
 
-    @ParameterizedTest
-    @DisplayName("양의 정수 외의 값이 들어오면 예외가 발생한다.")
-    @ValueSource(ints = {Integer.MIN_VALUE, -1, 0})
-    void create_NotPositive(int input) {
-        // expect
-        assertThatIllegalArgumentException().isThrownBy(() -> {
-            new Ladder(input, 5);
-        }).withMessage("[ERROR] 사다리의 높이는 양의 정수여야 합니다.");
+    private List<Line> makeLines() {
+        Line line1 = new Line(List.of(Step.EMPTY));
+        Line line2 = new Line(List.of(Step.EMPTY));
+        Line line3 = new Line(List.of(Step.EMPTY));
+        Line line4 = new Line(List.of(Step.EMPTY));
+        Line line5 = new Line(List.of(Step.EMPTY));
+        return List.of(line1, line2, line3, line4, line5);
     }
 
     @Test
@@ -25,7 +23,7 @@ class LadderTest {
     void create_heightMinPlayersCount() {
         // expect
         assertThatIllegalArgumentException().isThrownBy(() -> {
-            new Ladder(1, 2);
+            new Ladder(makeLines(), 12);
         }).withMessage("[ERROR] 사다리의 높이는 사람 수보다 크거나, 사람 수의 두 배 보다 작아야 합니다.");
     }
 
@@ -34,7 +32,7 @@ class LadderTest {
     void create_heightMaxPlayersCount() {
         // expect
         assertThatIllegalArgumentException().isThrownBy(() -> {
-            new Ladder(5, 2);
+            new Ladder(makeLines(), 2);
         }).withMessage("[ERROR] 사다리의 높이는 사람 수보다 크거나, 사람 수의 두 배 보다 작아야 합니다.");
     }
 
@@ -43,7 +41,7 @@ class LadderTest {
     void create_success() {
         // expect
         assertThatNoException().isThrownBy(() -> {
-            new Ladder(6, 3);
+            new Ladder(makeLines(), 3);
         });
     }
 }
