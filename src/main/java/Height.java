ecc06bed2384b9c645b0e0f14fdcb6c1a73b0ae3
ingpyo <juninpyo726@gmail.com>
2023-02-15 16:37:40 +0900

    feat: 높이를 저장하는 클래스 정의 및 검증 기능 추가 - 입력값이 유효하지 않은 경우     - digit 가 아닐때     - 0 이하일 때

---
+++ src/main/java/Height.java
@@ -0,0 +1,24 @@
+import java.util.regex.Pattern;
+
+public class Height {
+    private int height;
+
+    public Height(String height) {
+        checkNumberMissMatch(height);
+        checkNumberRange(height);
+        this.height = Integer.parseInt(height);
+    }
+
+
+    private void checkNumberMissMatch(String height) {
+        if (!Pattern.matches("^[0-9]*$", height)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void checkNumberRange(String height) {
+        if (Integer.parseInt(height) <= 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
