cec337a0ed595eb735e6bb4667de7d9210a48906
donghae <donghae1999@gmail.com>
2023-02-15 16:52:18 +0900

    feat: ladderLine 가로라인 겹치지 않게 checkLine 기능 구현

--- src/main/java/model/Line.java
+++ src/main/java/model/Line.java
@@ -13,6 +13,19 @@ public class Line {
     public Line (int personCount) {
         for(int index=0; index<personCount; index++) {
             points.add(generator.generate());
+            if(checkLine(index)){
+                index--;
+            }
         }
+    }
+
+    private boolean checkLine(int index){
+        if(index>=1){
+            if(points.get(index)==points.get(index-1)){
+                points.remove(index);
+                return true;
+            }
+        }
+        return false;
     }
 }
