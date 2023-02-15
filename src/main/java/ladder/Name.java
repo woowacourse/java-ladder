b501589cc6fed64c351be40dc646fab1444f42e1
drunkenhw <drunkenhw@gmail.com>
2023-02-15 15:34:54 +0900

    feat: 이름 유효성 검사

--- src/main/java/ladder/Name.java
+++ src/main/java/ladder/Name.java
@@ -1,9 +1,19 @@
 package ladder;
 
 public class Name {
+    private static final int MAXIMUM_LENGTH = 5;
+
     private final String value;
 
     public Name(String value) {
+        validateLength(value);
         this.value = value;
     }
+
+    private void validateLength(String value) {
+        if (value.isBlank() || value.length() > MAXIMUM_LENGTH) {
+            throw new IllegalArgumentException("이름은 1자 이상 5자 이하여야 합니다.");
+        }
+    }
+
 }
--- src/test/java/ladder/NameTest.java
+++ src/test/java/ladder/NameTest.java
@@ -1,9 +1,12 @@
 package ladder;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class NameTest {
 
@@ -13,6 +16,18 @@ class NameTest {
         // given
         // when
         // then
-        assertDoesNotThrow(()-> new Name("abcde"));
+        assertDoesNotThrow(() -> new Name("abcde"));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"abcedf", "", " "})
+    @DisplayName("잘못된 길이의 이름을 넣으면 예외를 발생한다")
+    void shouldThrowExceptionWhenWrongLength(String input) {
+        //given
+        //when
+        //then
+        assertThatThrownBy(() -> new Name(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이름은 1자 이상 5자 이하여야 합니다.");
     }
 }
