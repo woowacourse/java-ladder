df40446ca31372e3bf9ccf04376ac5919dc668a3
watterricecake <fruturum@nate.com>
2023-02-15 16:31:33 +0900

    refactor(RandomBooleanGenerator): random 필드로 이동

--- src/main/java/domain/service/RandomBooleanGenerator.java
+++ src/main/java/domain/service/RandomBooleanGenerator.java
@@ -3,8 +3,10 @@ package domain.service;
 import java.util.Random;
 
 public class RandomBooleanGenerator implements BooleanGenerator {
+    private final Random random = new Random();
+
     @Override
     public Boolean generate() {
-        return new Random().nextBoolean();
+        return random.nextBoolean();
     }
 }
