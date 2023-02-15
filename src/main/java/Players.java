5f35b0e24bc631e8063725441b6801c58133f62c
ingpyo <juninpyo726@gmail.com>
2023-02-15 17:44:10 +0900

    feat: 플레이어 이름 길이 및 중복 검사

--- src/main/java/Players.java
+++ src/main/java/Players.java
@@ -7,10 +7,26 @@ public class Players {
 
     Players(List<String> players) {
         checkPlayerCount(players);
+        checkPlayerNameLength(players);
+        checkDuplicatePlayers(players);
     }
 
     private void checkPlayerCount(List<String> players) {
         if (players.size() < 2 || players.size() > 12) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void checkPlayerNameLength(List<String> players){
+        if (players.stream()
+                .anyMatch(player -> player.length() > 5 || player.replaceAll(" ", "").isEmpty())){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void checkDuplicatePlayers(List<String> players) {
+        if (!players.stream()
+                .distinct().equals(players)) {
             throw new IllegalArgumentException();
         }
     }
