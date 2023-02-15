d5847348708bd760c706c26bfa10c3af593e39a0
seokjin8678 <rnrn8678@naver.com>
2023-02-15 17:17:54 +0900

    feat(LadderView): 사다리 게임의 입출력 기능 추가

---
+++ src/main/java/view/LadderView.java
@@ -0,0 +1,17 @@
+package view;
+
+import java.util.List;
+
+public class LadderView {
+    public String readPlayerNames() {
+        return InputView.readPlayerNames();
+    }
+
+    public int readLadderHeight() {
+        return InputView.readLadderHeight();
+    }
+
+    public void printResult(String names, List<String> lines) {
+        OutputView.printResult(names, lines);
+    }
+}
