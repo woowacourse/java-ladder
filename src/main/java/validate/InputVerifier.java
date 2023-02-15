0d79d4fdf69674560239e2116d9deb69ad9fb0b9
jjongwa <troas96@naver.com>
2023-02-15 17:55:13 +0900

    feat: 사다리 높이 형식 검증 기능 추가

--- src/main/java/validate/InputVerifier.java
+++ src/main/java/validate/InputVerifier.java
@@ -9,6 +9,8 @@ public class InputVerifier {
     public static final String NAME_LENGTH_ERROR_MESSAGE =
             "[ERROR] 사람 이름은 " + MIN_NAME_LENGTH + "~" + MAX_NAME_LENGTH + "글자로 입력해 주세요.";
     public static final String NAME_FORMAT_ERROR_MESSAGE = "[ERROR] 사람 이름은 영문자만 가능합니다.";
+    public static final String HEIGHT_FORMAT_ERROR_MESSAGE = "[ERROR] 사다리 높이는 숫자만 가능합니다.";
+    public static final String INPUT_NOTHING_ERROR_MESSAGE = "[ERROR] 값을 입력하지 않았습니다.";
 
     public List<String> validateNameInput(String nameInput) {
         validateNameLength(nameInput);
@@ -57,5 +59,24 @@ public class InputVerifier {
             nameBuilder.insert(0, " ");
         }
         return nameBuilder.toString();
+    }
+
+    public int validateHeightInput(String heightInput) {
+        validateHeightFormat(heightInput);
+        return convertHeightInput(heightInput);
+    }
+
+    private void validateHeightFormat(String heightInput) {
+        if (!heightInput.matches("^[0-9]*$")) {
+            throw new IllegalArgumentException(HEIGHT_FORMAT_ERROR_MESSAGE);
+        }
+    }
+
+    private int convertHeightInput(String heightInput) {
+        try {
+            return Integer.parseInt(heightInput);
+        } catch (NumberFormatException numberFormatException) {
+            throw new IllegalArgumentException(INPUT_NOTHING_ERROR_MESSAGE);
+        }
     }
 }
