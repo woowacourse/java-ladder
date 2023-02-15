1eeea6688dfa341e775696857d54ec9e026c3b8c
MoonJeWoong <munjin0201@naver.com>
2023-02-15 23:35:23 +0900

    test: 한 라인에서 같은 포인트가 연속으로 등장하지 않는 기능 테스트 추가

--- src/test/java/model/LineTest.java
+++ src/test/java/model/LineTest.java
@@ -31,7 +31,7 @@ public class LineTest {
     }
 
     @Test
-    @DisplayName("각 포인트들은 랜덤하게 만든다.")
+    @DisplayName("각 포인트들은 랜덤하게 만드는 기능 테스트")
     void makePointsRandomlyTest() {
         //Given
         Line line = new Line(new TestPointGenerator(new ArrayList<>(List.of(false,true,false))), 4);
@@ -41,6 +41,23 @@ public class LineTest {
 
         //Then
         assertThat(result).isEqualTo(List.of(false,true,false));
+    }
+
+    @Test
+    @DisplayName("한 라인에서 같은 포인트가 연속으로 등장하지 않는 기능 테스트")
+    void noContinuouslySamePointTest() {
+        //Given
+        Line line = new Line(new TestPointGenerator(new ArrayList<>(List.of(false,true,true,true))), 5);
+        Line line2 = new Line(new TestPointGenerator(new ArrayList<>(List.of(true,true,true,true,true))), 6);
+
+        //When
+        List<Boolean> result = line.getPoints();
+        List<Boolean> result2 = line2.getPoints();
+
+        //Then
+        assertThat(result).isEqualTo(List.of(false,true,false,true));
+        assertThat(result2).isEqualTo(List.of(true,false,true,false,true));
+
     }
 
     private class TestPointGenerator implements PointGenerator{
