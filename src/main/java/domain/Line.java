9ea580382bfc988ab8b1df0e2701e36a4b5fbdda
eunbii0213 <eunbii0213@naver.com>
2023-02-15 20:24:30 +0900

    feat: 한 라인에서의 블록 개수가 플레이어 수보다 하나 적은지 검증하는 기능 구현

---
+++ src/main/java/domain/Line.java
@@ -0,0 +1,20 @@
+package domain;
+
+import java.util.List;
+
+public class Line {
+    public static final String LINE_LENGTH_ERROR_MESSAGE = "[ERROR] 사다리 블록의 수는 플레이어의 수보다 하나 작아야합니다";
+
+    private List<Block> blocks;
+
+    public Line(int playerCount, List<Block> blocks) {
+        validateLineLength(playerCount, blocks);
+        this.blocks = blocks;
+    }
+
+    private void validateLineLength(int playerCount, List<Block> blocks) {
+        if (blocks.size() != playerCount - 1) {
+            throw new IllegalArgumentException(LINE_LENGTH_ERROR_MESSAGE);
+        }
+    }
+}
---
+++ src/test/java/domain/LineTest.java
@@ -0,0 +1,21 @@
+package domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+public class LineTest {
+
+    @Test
+    void 한_column에서의_블록_개수는_플레이어_수보다_하나_적어야한다() {
+        int playerCount = 3;
+        List<Block> blocks = new ArrayList<>(List.of(new Block(true), new Block(false), new Block(true)));
+        Assertions.assertThatThrownBy(() -> new Line(playerCount, blocks))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
