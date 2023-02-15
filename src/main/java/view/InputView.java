edb12275b3f5de206c4f941f993b05680c16d577
Eunkeee <eunkeeeee@gmail.com>
2023-02-15 14:29:42 +0900

    refactor(InputView): 메서드 분리

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -18,7 +18,7 @@ public class InputView {
     }
 
     public List<Name> readNames() {
-        String input = scanner.nextLine();
+        String input = readInput(Message.INPUT_NAMES.message);
         List<Name> names = new ArrayList<>();
         for (String name : input.split(",")) {
             names.add(new Name(name.trim()));
@@ -27,8 +27,24 @@ public class InputView {
     }
 
     public int readLadderSize() {
-        String input = scanner.nextLine();
+        String input = readInput(Message.INPUT_LADDER_SIZE.message);
         LadderSizeValidator.validate(input);
         return Integer.parseInt(input);
+    }
+
+    private static String readInput(String message) {
+        System.out.println(message);
+        return scanner.nextLine();
+    }
+
+    private enum Message {
+        INPUT_NAMES("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)"),
+        INPUT_LADDER_SIZE("최대 사다리 높이는 몇 개인가요?");
+
+        private final String message;
+
+        Message(String message) {
+            this.message = message;
+        }
     }
 }
