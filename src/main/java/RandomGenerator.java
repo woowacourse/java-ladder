efa93492dfb6e1584555a5728c33d453826aa13e
thdwoqor <celebarte01@gmail.com>
2023-02-15 15:43:17 +0900

    feat : 랜덤으로 사다리의 각 층 생성 코드 구현

---
+++ src/main/java/RandomGenerator.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class RandomGenerator {
+    private final Random random;
+
+    RandomGenerator() {
+        this.random = new Random();
+    }
+
+    public List<Boolean> generateLadder(int bridgeCount) {
+        List<Boolean> ladder = new ArrayList<>();
+        while (ladder.size() < bridgeCount) {
+            ladder.add(generateBridge(ladder));
+        }
+        return ladder;
+    }
+
+    private boolean generateBridge(List<Boolean> result) {
+        if (result.isEmpty() || !result.get(result.size() - 1)) {
+            return random.nextBoolean();
+        }
+        return false;
+    }
+}
