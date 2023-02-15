a1c7191a964b8b5c42799429267e7a1540ea9d94
BackFoxx <backfoxxx@gmail.com>
2023-02-15 17:24:51 +0900

    refactor: 사다리 높이 검증 함수 추출

--- src/main/java/domain/Ladder.java
+++ src/main/java/domain/Ladder.java
@@ -2,13 +2,27 @@ package domain;
 
 public class Ladder {
 
-    public Ladder(final int height) {
-        if (height < 2) {
-            throw new IllegalArgumentException("사다리 높이는 2 이상이어야 합니다.");
-        }
+    public static final int MAX_HEIGHT = 10;
+    public static final int MIN_HEIGHT = 2;
 
-        if (10 < height) {
+    public Ladder(final int height) {
+        validateLadderHeight(height);
+    }
+
+    private static void validateLadderHeight(final int height) {
+        validateMinHeight(height);
+        validateMaxHeight(height);
+    }
+
+    private static void validateMaxHeight(final int height) {
+        if (MAX_HEIGHT < height) {
             throw new IllegalArgumentException("사다리 높이는 10 이하여야 합니다.");
+        }
+    }
+
+    private static void validateMinHeight(final int height) {
+        if (height < MIN_HEIGHT) {
+            throw new IllegalArgumentException("사다리 높이는 2 이상이어야 합니다.");
         }
     }
 }
