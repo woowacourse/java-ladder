eb52261dadc0d0bbc617d68e24ec870a65c63744
kim0914 <kosa0914@naver.com>
2023-02-15 14:01:57 +0900

    test(Players): 참가자 수 검증 테스트 메서드 추가

--- src/test/java/domain/PlayersTest.java
+++ src/test/java/domain/PlayersTest.java
@@ -1,6 +1,5 @@
 package domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -22,6 +21,21 @@ public class PlayersTest {
     @DisplayName("중복된 이름을 가진 참가자가 있을 경우 예외가 발생한다.")
     void createPlayers_Fail() {
         List<Player> players = List.of(new Player("gray"), new Player("encho"), new Player("gray"));
+        assertThatThrownBy(() -> new Players(players))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("참가자 수가 2명 이상 50명 이하이면 Players 객체가 생성된다.")
+    void creteNumberOfPlayers_Success() {
+        List<Player> players = List.of(new Player("gray"), new Player("encho"), new Player("pobi"));
+        assertThatNoException().isThrownBy(() -> new Players(players));
+    }
+
+    @Test
+    @DisplayName("참가자 수가 2명미만 이거나 50명 초과이면 예외가 발생한다.")
+    void createNumberOfPlayers_Fail() {
+        List<Player> players = List.of(new Player("gray"));
         assertThatThrownBy(() -> new Players(players))
                 .isInstanceOf(IllegalArgumentException.class);
     }
