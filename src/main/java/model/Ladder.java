58e680d7ab78a8b3217642dacb9c89970310a91d
MoonJeWoong <munjin0201@naver.com>
2023-02-16 01:04:54 +0900

    feat: Ladder 객체 생성 기능 추가

---
+++ src/main/java/model/Ladder.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Ladder {
+
+    private LadderHeight ladderHeight;
+
+    public Ladder(LadderHeight ladderHeight) {
+        this.ladderHeight = ladderHeight;
+    }
+}
