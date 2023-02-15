e00ee735d6cacb4a7392e4cd8a9b0a2ca4260875
chaewon121 <cw121@naver.com>
2023-02-15 20:46:39 +0900

    feat: 입력받은 사람 이름을 콤마를 기준으로 나눠서 반환하는 기능 구현

---
+++ src/main/java/ladder/domain/NamesSplitor.java
@@ -0,0 +1,12 @@
+package ladder.domain;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class NamesSplitor {
+    public static List<String> split(String names){
+        return Arrays.stream(names.split(","))
+                .collect(Collectors.toUnmodifiableList());
+    }
+}
---
+++ src/test/java/ladder/domain/NamesSplitorTest.java
@@ -0,0 +1,20 @@
+package ladder.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class NamesSplitorTest {
+
+    @Test
+    @DisplayName("입력받은 이름들을 ,를 기준으로 나눈다.")
+    void test_() {
+        // given & when
+        List<String> expected = List.of("채채", "아벨");
+
+        // then
+        Assertions.assertThat(NamesSplitor.split("채채,아벨")).isEqualTo(expected);
+    }
+}
