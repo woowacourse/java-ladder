9c81885cc84d31d5e782f31953557e7f236e6c1f
odo27 <odo27@odo27ui-MacBookAir.local>
2023-02-15 15:22:28 +0900

    feat(Ladder): 사다리 생성 메서드 추가
    - 사다리 높이가 자연수인지 검증하는 메서드 추가
    - 사다리 높이가 사다리 게임 참여자 수보다 큰지 검증하는 메서드 추가
    - 사다리 각 라인을 생성하는 메서드 추가

--- src/main/java/domain/Ladder.java
+++ src/main/java/domain/Ladder.java
@@ -1,2 +1,44 @@
-package domain;public class Ladder {
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import domain.numbergenerator.NumberGenerator;
+
+public class Ladder {
+
+    private static final int HEIGHT_LOWER_BOUND = 1;
+    private static final String NOT_NATURAL_NUMBER_MESSAGE = "사다리의 높이는 자연수이어야 합니다.";
+    private static final String HEIGHT_IS_LOWER_THAN_PERSON_COUNT_MESSAGE = "사다리의 높이는 참가자 수 이상이어야 합니다.";
+
+    private final List<Line> ladder;
+
+    public Ladder(int height, int personCount, NumberGenerator numberGenerator) {
+        validate(height, personCount);
+        ladder = new ArrayList<>();
+        generateLadder(height, personCount, numberGenerator);
+    }
+
+    private void generateLadder(int height, int personCount, NumberGenerator numberGenerator) {
+        for (int i = 0; i < height; i++) {
+            ladder.add(new Line(personCount, numberGenerator));
+        }
+    }
+
+    private static void validate(int height, int personCount) {
+        validateHeight(height);
+        validateHeightCompareWithPersonCount(height, personCount);
+    }
+
+    private static void validateHeightCompareWithPersonCount(int height, int personCount) {
+        if (height < personCount) {
+            throw new IllegalArgumentException(HEIGHT_IS_LOWER_THAN_PERSON_COUNT_MESSAGE);
+        }
+    }
+
+    private static void validateHeight(int height) {
+        if (height < HEIGHT_LOWER_BOUND) {
+            throw new IllegalArgumentException(NOT_NATURAL_NUMBER_MESSAGE);
+        }
+    }
 }
--- src/test/java/domain/LadderTest.java
+++ src/test/java/domain/LadderTest.java
@@ -1,2 +1,38 @@
-package domain;public class LadderTest {
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import domain.numbergenerator.RandomNumberGenerator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class LadderTest {
+
+    @DisplayName("사다리의 높이는 자연수로 입력되어야 한다.")
+    @ParameterizedTest
+    @CsvSource({"0", "-1"})
+    void createLadderTest(int height) {
+        assertThatThrownBy(() -> new Ladder(height, 4, new RandomNumberGenerator()))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("사다리의 높이는 자연수이어야 합니다.");
+    }
+
+    @DisplayName("사다리 높이는 참가자 수 이상이어야한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"3:4", "8:14"}, delimiter = ':')
+    void createLadderTest2(int height, int personCount) {
+        assertThatThrownBy(() -> new Ladder(height, personCount, new RandomNumberGenerator()))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("사다리의 높이는 참가자 수 이상이어야 합니다.");
+    }
+
+    @DisplayName("사다리 높이가 정상적으로 입력되면 사다리를 생성한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"4:3", "14:5"}, delimiter = ':')
+    void createLadderTest3(int height, int personCount) {
+        assertThatCode(() -> new Ladder(height, personCount, new RandomNumberGenerator()))
+                .doesNotThrowAnyException();
+    }
 }
