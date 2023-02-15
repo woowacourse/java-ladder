20d6b202dfad37f9a763667548958cf9196786bf
thdwoqor <celebarte01@gmail.com>
2023-02-15 16:02:41 +0900

    feat : 사다리 생성 및 생성시 연속된 다리가 있는지 검사하는 유효성 검사 코드 작성

---
+++ src/main/java/Ladder.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+public class Ladder {
+    private final List<Boolean> ladder;
+
+    Ladder(List<Boolean> ladder) {
+        for (int index = 1; index < ladder.size(); index++) {
+            Boolean currentBridge = ladder.get(index);
+            Boolean previousBridge = ladder.get(index - 1);
+            if (currentBridge && previousBridge) {
+                throw new IllegalArgumentException("다리는 연속으로 생성되면 안됩니다.");
+            }
+        }
+        this.ladder = ladder;
+    }
+}
