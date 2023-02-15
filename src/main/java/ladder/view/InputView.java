15d2fa3df27231d850f6c2fec45f71bdb7c6e094
be-student <presentsong@naver.com>
2023-02-15 16:40:07 +0900

    chore: 줄바꿈 변경

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -10,12 +10,12 @@ public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static String inputPlayerNames() {
-        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)\n");
+        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
         return scanner.nextLine();
     }
 
     public static int inputLadderHeight() {
-        System.out.println("최대 사다리 높이는 몇 개인가요?\n");
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
         String height = scanner.nextLine();
         if (!pattern.matcher(height).matches()) {
             throw new IllegalArgumentException("입력은 숫자여야 합니다");
