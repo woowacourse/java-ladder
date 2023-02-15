54d4ea70dea71ba8d59b59f36c73488e56303321
Cyma-s <ssol0319@naver.com>
2023-02-15 15:11:47 +0900

    feat: 플레이어들의 중복된 이름 검증하기 기능 구현

--- src/main/java/ladder/domain/Players.java
+++ src/main/java/ladder/domain/Players.java
@@ -1,6 +1,7 @@
 package ladder.domain;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.IntStream;
@@ -9,10 +10,19 @@ public class Players {
     private final Map<StartPoint, Player> players;
 
     public Players(List<String> names) {
+        validateDuplicateNames(names);
         players = new HashMap<>();
 
         IntStream.range(0, names.size())
                 .forEach(index -> players.put(new StartPoint(index), new Player(names.get(index))));
+    }
+
+    private void validateDuplicateNames(List<String> names) {
+        int distinctNameSize = new HashSet<>(names).size();
+        int size = names.size();
+        if(size != distinctNameSize) {
+            throw new IllegalArgumentException("이름은 중복될 수 없습니다.");
+        }
     }
 
     public String getPlayerName(StartPoint startPoint) {
