4e3d8e09431226cdb819c4d775678b94630d76b3
dhkim <akdlzmf1123@gmail.com>
2023-02-15 15:46:36 +0900

    feat: Foothold 생성 기능

---
+++ src/main/java/ladder/domain/Foothold.java
@@ -0,0 +1,22 @@
+package ladder.domain;
+
+import java.util.Arrays;
+
+public enum Foothold {
+    Y(true, "-----"), N(false, "     ");
+
+    private final boolean state;
+    private final String mark;
+
+    Foothold(boolean state, String mark) {
+        this.state = state;
+        this.mark = mark;
+    }
+
+    public static Foothold from(boolean state) {
+        return Arrays.stream(Foothold.values())
+                .filter((foothold) -> foothold.state == state)
+                .findAny()
+                .get();
+    }
+}
