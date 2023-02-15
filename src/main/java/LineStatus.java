a117a7cf7e971da8a782bae44c0ca01742a962a2
Miseong Kim <miseongk@naver.com>
2023-02-15 15:09:25 +0900

    feat: Line 상태값을 저장하는 Enum객체 생성 및 테스트

---
+++ src/main/java/LineStatus.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public enum LineStatus {
+    EXIST(true),
+    NON_EXIST(false);
+
+    private final boolean status;
+
+    LineStatus(boolean status) {
+        this.status = status;
+    }
+
+    public static LineStatus findBy(boolean status) {
+        return Arrays.stream(LineStatus.values())
+                .filter(line -> line.status == status)
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] boolean 타입의 입력값이 아닙니다"));
+    }
+
+    public boolean getStatus() {
+        return status;
+    }
+}
---
+++ src/test/java/LineStatusTest.java
@@ -0,0 +1,22 @@
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LineStatusTest {
+
+    @Test
+    @DisplayName("true값을 입력하면 해당하는 다리가 존재하는 enum객체 생성")
+    void existLineTest() {
+        boolean isLine = true;
+        assertThat(LineStatus.findBy(isLine)).isEqualTo(LineStatus.EXIST);
+    }
+
+    @Test
+    @DisplayName("false값을 입력하면 해당하는 다리가 존재하지않는 enum객체 생성")
+    void nonExistLineTest() {
+        boolean isLine = false;
+        assertThat(LineStatus.findBy(isLine)).isEqualTo(LineStatus.NON_EXIST);
+    }
+}
