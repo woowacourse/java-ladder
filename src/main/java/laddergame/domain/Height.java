9fe9c2cf9bade3923992cb363c56ea72e1d57621
hyunseo <goatsfish@gmail.com>
2023-02-15 14:41:58 +0900

    feat: Height 생성 및 검증 기능 추가

---
+++ src/main/java/laddergame/domain/Height.java
@@ -0,0 +1,12 @@
+package laddergame.domain;
+
+public class Height {
+    private final int value;
+
+    public Height(final int value) {
+        if (value <= 0) {
+            throw new IllegalArgumentException();
+        }
+        this.value = value;
+    }
+}
