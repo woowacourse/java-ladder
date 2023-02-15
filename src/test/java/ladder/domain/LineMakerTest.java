5ab174d602494f7112c47bdf5ab55f7c64291cae
DESKTOP-FNVN0HH\USER <beatus96@naver.com>
2023-02-15 14:34:42 +0900

    test: LineMaker 테스트 수정

--- src/test/java/ladder/domain/LineMakerTest.java
+++ src/test/java/ladder/domain/LineMakerTest.java
@@ -1,8 +1,8 @@
 package ladder.domain;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -10,6 +10,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LineMakerTest {
+
     private int playerCount;
     private RandomGenerator<Boolean> randomGenerator;
 
@@ -18,13 +19,14 @@ public class LineMakerTest {
         playerCount = 5;
         randomGenerator = new RandomBooleanGenerator();
     }
+
     @Test
     @DisplayName("플레이어 수 -1 크기의 리스트가 생성되는지 확인한다.")
     void generateLineSizeTest() {
-        assertThat(LineMaker.generate(playerCount, randomGenerator).size()).isEqualTo(playerCount-1);
+        assertThat(LineMaker.generate(playerCount, randomGenerator).size()).isEqualTo(playerCount - 1);
     }
 
-    @Test
+    @RepeatedTest(100)
     @DisplayName("생성된 Line의 가로가 겹치지 않는지 확인한다.")
     void generateLineTest() {
         // 리스트가 연속된 true값을 갖지 않은지.
