422c58ff2c6ff17671cc99badf9dd05575e903c7
Cyma-s <ssol0319@naver.com>
2023-02-15 14:12:49 +0900

    test: 라인을 랜덤하게 겹치지 않게 생성하기 테스트 구현

---
+++ src/test/java/ladder/utils/RandomLineStrategyTest.java
@@ -0,0 +1,30 @@
+package ladder.utils;
+
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RandomLineStrategyTest {
+    @Test
+    @DisplayName("연속으로 이어지지 않는 라인을 생성한다.")
+    public void line_generateNotContinuous() {
+        // given
+        RandomLineStrategy randomLineStrategy = new RandomLineStrategy();
+
+        // when
+        List<Boolean> randomLine = randomLineStrategy.generate(5);
+        boolean isSame = false;
+        for (int i = 0; i < randomLine.size() - 1; i++) {
+            if((randomLine.get(i).equals(randomLine.get(i+1))) && randomLine.get(i)) {
+                isSame = true;
+                break;
+            }
+        }
+
+        // expected
+        assertThat(isSame).isFalse();
+    }
+}
