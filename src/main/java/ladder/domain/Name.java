23f37cff8787b5ca943fefd9cdbde197785f16b2
Devtist <yt930310@gmail.com>
2023-02-15 14:31:04 +0900

    feat(Name): 이름 검증 기능 구현

---
+++ src/main/java/ladder/domain/Name.java
@@ -0,0 +1,28 @@
+package ladder.domain;
+
+import ladder.error.ErrorMessage;
+
+public class Name {
+    private static final int MAX_NAME_LENGTH = 5;
+    private static final String COMMA = ",";
+
+    private final String name;
+
+    public Name(String name) {
+        validate(name);
+        this.name = name;
+    }
+
+    private void validate(String name) {
+        if (name == null) {
+            throw new IllegalArgumentException(ErrorMessage.NAME_IS_NULL.getMessage());
+        }
+        if (name.isBlank() || name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_NAME_LENGTH.getMessage());
+        }
+        if (name.contains(COMMA)) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_NAME_FORMAT.getMessage());
+        }
+    }
+
+}
--- src/main/java/ladder/error/ErrorMessage.java
+++ src/main/java/ladder/error/ErrorMessage.java
@@ -3,7 +3,10 @@ package ladder.error;
 public enum ErrorMessage {
     DUPLICATION("이름은 중복일 수 없습니다."),
     NON_NUMERIC("사다리 높이는 숫자여야 합니다."),
-    INVALID_HEIGHT_RANGE("사다리 높이는 1이상, 10000 이하여야 합니다.");
+    INVALID_HEIGHT_RANGE("사다리 높이는 1이상, 10000 이하여야 합니다."),
+    INVALID_NAME_LENGTH("이름은 1글자 이상, 5글자 이하여야 합니다."),
+    INVALID_NAME_FORMAT("이름에 쉼표(,)를 포함할 수 없습니다."),
+    NAME_IS_NULL("null은 이름으로 설정할 수 없습니다.");
 
     private static final String ERROR_FORMAT = "[ERROR]: %s";
 
