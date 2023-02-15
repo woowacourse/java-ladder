c238e6186fee643f69733e0be8d2f5fa9b1f21af
odo27 <odo27@odo27ui-MacBookAir.local>
2023-02-15 16:50:26 +0900

    feat(Players): 사다리 게임 참여자 일급 컬렉션 클래스 생성

---
+++ src/main/java/domain/Players.java
@@ -0,0 +1,15 @@
+package domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Players {
+
+    private final List<Player> players;
+
+    public Players(List<String> names) {
+        players = names.stream()
+                .map(Player::new)
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/test/java/domain/PlayersTest.java
@@ -0,0 +1,29 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PlayersTest {
+
+    @DisplayName("사다리 게임 참여자의 이름이 하나라도 잘못 입력된 경우 예외를 발생시킨다.")
+    @Test
+    void validateTest() {
+        List<String> names = List.of("odo", "odo27", "kong", "konghana");
+        assertThatThrownBy(() -> new Players(names))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("사람 이름은 1글자에서 5글자 사이이어야 합니다.");
+    }
+
+    @DisplayName("사다리 게임 참여자의 이름이 정상적으로 입력된 경우 참여자들을 생성한다.")
+    @Test
+    void validateTest2() {
+        List<String> names = List.of("odo", "odo27", "kong", "ko ng", "  ko    ");
+        assertThatCode(() -> new Players(names))
+                .doesNotThrowAnyException();
+    }
+}
