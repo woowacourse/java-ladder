f526078ba674940864ae9d08c0327ccf303307fa
2014012606 <2014012606@hanyang.ac.kr>
2023-02-15 17:37:55 +0900

    chore: InputValidator 생성자 파라미터에 final 키워드 추가

--- src/main/java/view/InputValidator.java
+++ src/main/java/view/InputValidator.java
@@ -1,12 +1,14 @@
 package view;
 
+import exception.ErrorMessage;
+
 public class InputValidator {
 
-    public int validateHeight(String height) {
+    public int validateHeight(final String height) {
         try {
             return Integer.parseInt(height);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ErrorMessage.LADDER_HEIGHT_NON_NUMERIC_EXCEPTION.getMessage());
         }
     }
 }
