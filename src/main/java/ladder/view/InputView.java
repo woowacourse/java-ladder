8c620ed8fa7ab2ac918fb42bdd0b093d07d6af74
greeng00se <thegreengoose7@gmail.com>
2023-02-15 15:55:59 +0900

    feat: 입력 기능 구현
    Co-authored-by: wugawuga <dnr3655@naver.com>

---
+++ src/main/java/ladder/view/InputView.java
@@ -0,0 +1,41 @@
+package ladder.view;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class InputView {
+
+    private static final String READ_PLAYER_NAMES_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    private static final String DELIMITER = ",";
+    private static final int SPLIT_LIMIT = -1;
+    private static final String READ_LADDER_HEIGHT_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
+    private static final String INVALID_NUMBER_MESSAGE = "정수만 입력 가능합니다.";
+
+    private final Scanner scanner;
+
+    public InputView(final Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public List<String> readPlayerNames() {
+        System.out.println(READ_PLAYER_NAMES_MESSAGE);
+        final String input = scanner.nextLine();
+
+        return Arrays.stream(input.split(DELIMITER, SPLIT_LIMIT))
+                .map(String::trim)
+                .collect(Collectors.toList());
+    }
+
+    public int readLadderHeight() {
+        System.out.println(READ_LADDER_HEIGHT_MESSAGE);
+        final String input = scanner.nextLine();
+
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INVALID_NUMBER_MESSAGE);
+        }
+    }
+}
