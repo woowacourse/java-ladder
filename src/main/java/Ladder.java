feedb120524b4b3d2ba71bf8614b83e93899b185
shin-mallang <huipulci1@naver.com>
2023-02-15 16:06:36 +0900

    feat: 사다리 생성 구현

---
+++ src/main/java/Ladder.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ladder {
+
+    private final List<Line> lines;
+
+    public Ladder(final List<Line> lines) {
+        validate(lines);
+        this.lines = new ArrayList<>(lines);
+    }
+
+    private static void validate(final List<Line> lines) {
+        validateLineSizeEmpty(lines);
+        validateLinesSameSize(lines);
+    }
+
+    private static void validateLineSizeEmpty(final List<Line> lines) {
+        if (lines.size() == 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validateLinesSameSize(final List<Line> lines) {
+        for (int i = 0; i < lines.size() - 1; i++) {
+            validateLineSameSize(lines, i);
+        }
+    }
+
+    private static void validateLineSameSize(final List<Line> lines, final int i) {
+        if (lines.get(i).size() != lines.get(i + 1).size()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+
+    public int getHeight() {
+        return lines.size();
+    }
+
+    public int getWidth() {
+        return lines.get(0).size();
+    }
+}
---
+++ src/test/java/LadderTest.java
@@ -0,0 +1,108 @@
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+@SuppressWarnings("NonAsciiCharacters")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@DisplayName("Ladder 는")
+public class LadderTest {
+
+    @Test
+    @DisplayName("Line 을 받아 생성된다")
+    void generate_ladder_by_line() {
+        Scaffold scaffold1 = Scaffold.EXIST;
+        Scaffold scaffold2 = Scaffold.NONE;
+
+        Line line = new Line(List.of(scaffold1, scaffold2));
+
+        assertDoesNotThrow(() -> new Ladder(List.of(line)));
+    }
+
+    @ParameterizedTest(name = "Line 개수가 높이이다")
+    @MethodSource("sameLengthLines")
+    void has_lines_by_height(final List<Line> lines) {
+        Ladder ladder = new Ladder(lines);
+
+        assertThat(ladder.getHeight()).isEqualTo(lines.size());
+    }
+
+    @Test
+    void 들어오는_Line_들의_개수가_0인_경우_예외가_발생한다() {
+        assertThatThrownBy(() ->
+                new Ladder(List.of())
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest(name = "사다리의 너비는 Line 의 길이와 동일해야 한다")
+    @MethodSource("sameLengthLines")
+    void 사다리의_너비는_Line_의_길이와_동일해야_한다(final List<Line> lines) {
+        Ladder ladder = new Ladder(lines);
+
+        assertThat(ladder.getWidth()).isEqualTo(lines.get(0).size());
+    }
+
+    private static Stream<Arguments> sameLengthLines() {
+        List<Scaffold> size0Scaffolds = List.of();
+        List<Scaffold> size1Scaffolds = List.of(Scaffold.EXIST);
+        List<Scaffold> size2Scaffolds = List.of(Scaffold.EXIST, Scaffold.NONE);
+        List<Scaffold> size3Scaffolds = List.of(Scaffold.EXIST, Scaffold.NONE, Scaffold.EXIST);
+        List<Scaffold> size4Scaffolds = List.of(Scaffold.EXIST, Scaffold.NONE, Scaffold.EXIST, Scaffold.NONE);
+
+        Line line0 = new Line(size0Scaffolds);
+        Line line1 = new Line(size1Scaffolds);
+        Line line2 = new Line(size2Scaffolds);
+        Line line3 = new Line(size3Scaffolds);
+        Line line4 = new Line(size4Scaffolds);
+
+        return Stream.of(
+                Arguments.of(List.of(line0, line0)),
+                Arguments.of(List.of(line1, line1)),
+                Arguments.of(List.of(line2, line2, line2)),
+                Arguments.of(List.of(line3, line3, line3, line3)),
+                Arguments.of(List.of(line4, line4, line4, line4, line4)),
+                Arguments.of(List.of(line2))
+        );
+    }
+
+
+    @ParameterizedTest(name = "사다리에 속한 Line 은 모두 길이가 같아야 한다")
+    @MethodSource("differentSizeLines")
+    void 사다리에_속한_Line_은_모두_길이가_같아야_한다(final List<Line> lines) {
+        Assertions.assertThatThrownBy(() ->
+                new Ladder(lines)
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private static Stream<Arguments> differentSizeLines() {
+        List<Scaffold> size0Scaffolds = List.of();
+        List<Scaffold> size1Scaffolds = List.of(Scaffold.EXIST);
+        List<Scaffold> size2Scaffolds = List.of(Scaffold.EXIST, Scaffold.NONE);
+        List<Scaffold> size3Scaffolds = List.of(Scaffold.EXIST, Scaffold.NONE, Scaffold.EXIST);
+        List<Scaffold> size4Scaffolds = List.of(Scaffold.EXIST, Scaffold.NONE, Scaffold.EXIST, Scaffold.NONE);
+
+        Line line0 = new Line(size0Scaffolds);
+        Line line1 = new Line(size1Scaffolds);
+        Line line2 = new Line(size2Scaffolds);
+        Line line3 = new Line(size3Scaffolds);
+        Line line4 = new Line(size4Scaffolds);
+
+        return Stream.of(
+                Arguments.of(List.of(line0, line1)),
+                Arguments.of(List.of(line1, line2)),
+                Arguments.of(List.of(line1, line1, line2)),
+                Arguments.of(List.of(line3, line3, line3, line4))
+        );
+    }
+}
