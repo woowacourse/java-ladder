23c1b60fb937249b2ac98ec69ea0077bbb3cadd2
2014012606 <2014012606@hanyang.ac.kr>
2023-02-15 16:35:35 +0900

    refactor: InputView 출력 형식 맞추기

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -10,11 +10,12 @@ public class InputView {
     private static final InputValidator inputValidator = new InputValidator();
     private static final String USER_NAMES_GUIDE_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     private static final String LADDER_HEIGHT_GUIDE_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
-    private static final String DELIMITER = ",";
+    private static final String DELIMITER = "\\s*,\\s*";
 
     public static List<String> readUserNames() {
         System.out.println(USER_NAMES_GUIDE_MESSAGE);
         String names = scanner.nextLine();
+        System.out.println();
         return Arrays.stream(names.split(DELIMITER))
                 .collect(Collectors.toUnmodifiableList());
     }
@@ -22,6 +23,7 @@ public class InputView {
     public static int readLadderHeight() {
         System.out.println(LADDER_HEIGHT_GUIDE_MESSAGE);
         String height = scanner.nextLine();
+        System.out.println();
         return inputValidator.validateHeight(height);
     }
 }
