2a77951ec4ebe7f6d42623ce06927d20d4a5f817
donghae <donghae1999@gmail.com>
2023-02-15 15:42:29 +0900

    feat: Ladder 객체 생성 기능 수정

--- src/main/java/model/Ladder.java
+++ src/main/java/model/Ladder.java
@@ -1,9 +1,17 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Ladder {
+
+    private List<Line> ladderLine = new ArrayList<>();
     private final LadderHeight height;
 
-    public Ladder(LadderHeight height){
+    public Ladder(int playerNumber,LadderHeight height){
+        for(int index = 0; index<height.getLadderHeight(); index++) {
+            this.ladderLine.add(new Line(playerNumber));
+        }
         this.height = height;
     }
 }
