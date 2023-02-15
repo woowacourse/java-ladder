8ab52ba15e3e5d9f7044555313489ce29198e3f9
donghae <donghae1999@gmail.com>
2023-02-15 16:04:12 +0900

    feat: 랜덤 생성 line generator 추가

---
+++ src/main/java/util/LineGenerator.java
@@ -0,0 +1,13 @@
+package util;
+
+import java.util.Random;
+
+public class LineGenerator implements Generator{
+    private Random random = new Random();
+
+    @Override
+    public boolean generate() {
+        return random.nextBoolean();
+    }
+}
+
