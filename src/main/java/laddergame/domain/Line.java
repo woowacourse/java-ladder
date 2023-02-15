ae964a5ced846417ae90bddce9eff10ca70ffb5f
hyunseo <goatsfish@gmail.com>
2023-02-15 16:57:07 +0900

    feat: Line 생성 및 검증 기능 추가

---
+++ src/main/java/laddergame/domain/Line.java
@@ -0,0 +1,23 @@
+package laddergame.domain;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class Line {
+    private final List<Boolean> points;
+
+    public Line(final List<Boolean> points) {
+        if (points.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        if (points.stream().allMatch(point -> point == false)) {
+            throw new IllegalArgumentException();
+        }
+        boolean isBothTrue = IntStream.range(0, points.size() - 1)
+                .anyMatch(pointIndex -> points.get(pointIndex) && points.get(pointIndex + 1));
+        if (isBothTrue) {
+            throw new IllegalArgumentException();
+        }
+        this.points = points;
+    }
+}
