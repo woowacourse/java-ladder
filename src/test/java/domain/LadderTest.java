e632662d1bf653e6b9065b773482233fcd803d1a
hyunseo <ohseo8813@gmail.com>
2023-02-15 16:54:05 +0900

    test: LadderTest 클래스의 중복 제거 및 가독성 개선

--- src/test/java/domain/LadderTest.java
+++ src/test/java/domain/LadderTest.java
@@ -11,24 +11,38 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class LadderTest {
 
-    @DisplayName("다리가 높이와 라인 수를 입력받고, 각 라인들에 높이만큼의 크기를 가진 Boolean 리스트를 생성한다")
+    @DisplayName("다리가 높이와 라인 수를 입력받고, 각 라인들에 높이만큼의 크기를 가진 Point 리스트를 생성한다")
     @Test
     void create_success() {
-        LadderHeight ladderHeight = new LadderHeight(3);
-        int numberOfLine = 3;
-        Ladder ladder = Ladder.create(ladderHeight, numberOfLine, new RandomNumberGenerator());
+        Ladder ladder = createLadder(3,3);
+
         List<Line> lines = ladder.getLines();
-        assertThat(lines.stream().map(Line::getPointSize).collect(Collectors.toSet()).size()).isEqualTo(1);
+        int numberOfPoint = lines.stream()
+                .map(Line::getPointSize)
+                .collect(Collectors.toSet())
+                .size();
+
+        assertThat(numberOfPoint).isEqualTo(1);
     }
 
     @DisplayName("마지막 라인은 모든 point가 false이다")
     @Test
     void last_line_have_false_only() {
-        LadderHeight ladderHeight = new LadderHeight(3);
-        int numberOfLine = 3;
-        Ladder ladder = Ladder.create(ladderHeight, numberOfLine, new RandomNumberGenerator());
+        Ladder ladder = createLadder(3,3);
+
         List<Line> lines = ladder.getLines();
-        assertThat(lines.get(lines.size() - 1).getPoints())
-                .containsExactly(Point.BLOCKED, Point.BLOCKED, Point.BLOCKED);
+        List<Point> points = getLastLinePoints(lines);
+
+        assertThat(points).containsExactly(Point.BLOCKED, Point.BLOCKED, Point.BLOCKED);
+    }
+
+    private Ladder createLadder(int height, int numberOfLine) {
+        LadderHeight ladderHeight = new LadderHeight(height);
+        Ladder ladder = Ladder.create(ladderHeight, numberOfLine, new RandomNumberGenerator());
+        return ladder;
+    }
+
+    private List<Point> getLastLinePoints(List<Line> lines) {
+        return lines.get(lines.size() - 1).getPoints();
     }
 }
