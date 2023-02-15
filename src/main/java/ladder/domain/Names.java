931f42e1e1c8e983080891dbfd197c27bea3231c
Devtist <yt930310@gmail.com>
2023-02-15 15:02:15 +0900

    feat(Names): 참여자 수를 검증하는 기능 구현

---
+++ src/main/java/ladder/domain/Names.java
@@ -0,0 +1,32 @@
+package ladder.domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import ladder.error.ErrorMessage;
+
+public class Names {
+    private static final int MIN_NAMES_COUNT = 2;
+    private static final int MAX_NAMES_COUNT = 100;
+
+    private final List<Name> names;
+
+    public Names(List<String> names) {
+        validate(names);
+
+        this.names = names.stream()
+            .map(Name::new)
+            .collect(Collectors.toList());
+    }
+
+    private void validate(List<String> names) {
+        if (names.size() < MIN_NAMES_COUNT || names.size() > MAX_NAMES_COUNT) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_PEOPLE_NUMBER.getMessage());
+        }
+    }
+
+    public List<Name> getNames() {
+        return names;
+    }
+
+}
--- src/main/java/ladder/error/ErrorMessage.java
+++ src/main/java/ladder/error/ErrorMessage.java
@@ -6,7 +6,8 @@ public enum ErrorMessage {
     INVALID_HEIGHT_RANGE("사다리 높이는 1이상, 10000 이하여야 합니다."),
     INVALID_NAME_LENGTH("이름은 1글자 이상, 5글자 이하여야 합니다."),
     INVALID_NAME_FORMAT("이름에 쉼표(,)를 포함할 수 없습니다."),
-    NAME_IS_NULL("null은 이름으로 설정할 수 없습니다.");
+    NAME_IS_NULL("null은 이름으로 설정할 수 없습니다."),
+    INVALID_PEOPLE_NUMBER("참여자는 2명 이상 100명 이하여야 합니다.");
 
     private static final String ERROR_FORMAT = "[ERROR]: %s";
 
