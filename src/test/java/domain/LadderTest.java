4941a059639cfc0e64b9ba550ef88328da8e5402
kim0914 <kosa0914@naver.com>
2023-02-15 17:30:48 +0900

    test(Ladder): 사다리 높이 검증 테스트 메서드 추가

---
+++ src/test/java/domain/LadderTest.java
@@ -0,0 +1,25 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class LadderTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 10, 50, 100})
+    @DisplayName("높이가 1이상 100이하이면 사다리가 생성된다.")
+    void createLine_Success(int height) {
+        assertThatNoException().isThrownBy(() -> new Ladder(height));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0, 101, 150, 1000})
+    @DisplayName("높이가 1미만 100초과이면 예외가 발생한다.")
+    void createLine_Fail(int height) {
+        assertThatThrownBy(() -> new Ladder(height))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
