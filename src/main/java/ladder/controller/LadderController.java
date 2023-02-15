79ac1ef22e923be2c2c8809cc54ecb0851cade67
be-student <presentsong@naver.com>
2023-02-15 15:21:36 +0900

    feat: controller 작성

--- src/main/java/ladder/controller/LadderController.java
+++ src/main/java/ladder/controller/LadderController.java
@@ -2,14 +2,20 @@ package ladder.controller;
 
 import ladder.domain.Lines;
 import ladder.domain.Names;
+import ladder.domain.RandomGenerator;
+import ladder.view.InputView;
+import ladder.view.ResultView;
 
 public class LadderController {
 
-    private final Names names;
-    private final Lines lines;
+    private Names names;
+    private Lines lines;
 
-    public LadderController(Names names, Lines lines) {
-        this.names = names;
-        this.lines = lines;
+    public void run() {
+        names = new Names(InputView.inputPlayerNames());
+        lines = new Lines(InputView.inputLadderHeight(), names.getCount());
+
+        lines.generateLegsOfLines(new RandomGenerator());
+        ResultView.printResult(names.toDto(), lines.toDto());
     }
 }
