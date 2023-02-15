c7975c14f72da065335ddf1aca018144d36e4089
parkmuhyeun <pjhg410@gmail.com>
2023-02-15 16:41:12 +0900

    test: 사다리 생성 테스트

--- src/test/java/domain/LadderTest.java
+++ src/test/java/domain/LadderTest.java
@@ -1,24 +1,25 @@
 package domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LadderTest {
 
     @Test
     @DisplayName("")
     void ladder() {
-        new Ladder(4);
+        new Ladder(4, 4, new RandomDigitsGenerator());
     }
 
     @ParameterizedTest
     @ValueSource(ints = {0, -1})
     @DisplayName("")
     void invalid_height(int height) {
-        Assertions.assertThatThrownBy(() -> new Ladder(height))
+        assertThatThrownBy(() -> new Ladder(height, 4, new RandomDigitsGenerator()))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
