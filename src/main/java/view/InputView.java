7675b925f32ec9b223d56cdefafcccc3456413ae
parkmuhyeun <pjhg410@gmail.com>
2023-02-16 13:32:18 +0900

    feat: 최대 사다리 높이 입력 기능 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -9,6 +9,7 @@ public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
     private static final String NAME_INPUT_NOTICE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요";
     private static final String DELIMITER = ",";
+    private static final String LADDER_HEIGHT_INPUT_NOTICE = "최대 사다리 높이는 몇 개인가요?";
 
     public List<String> readPersonsName(){
         System.out.println(NAME_INPUT_NOTICE);
@@ -18,5 +19,14 @@ public class InputView {
 
     public String readInput(){
         return scanner.nextLine();
+    }
+
+    public int readLadderHeight(){
+        System.out.println(LADDER_HEIGHT_INPUT_NOTICE);
+        try {
+            return Integer.parseInt(scanner.next());
+        }catch (NumberFormatException e){
+            throw new IllegalArgumentException();
+        }
     }
 }
