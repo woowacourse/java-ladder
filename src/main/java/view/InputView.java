43f5fbd8befe428a339bab6d2591eb1e61b74fce
jaehee329 <jjhs9803@gmail.com>
2023-02-16 13:44:06 +0900

    fix(InputView): add input prompt message printing logic
    Co-authored-by: echo <echo.backend@gmail.com>

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -10,8 +10,10 @@ public class InputView {
 	private static Scanner scanner = new Scanner(System.in);
 	private static final String DELIMITER = ",";
 	private static final String PARTICIPANTS_NAMES_REQUEST_MSG = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+	private static final String LADDER_HEIGHT_REQUEST_MSG = "최대 사다리 높이는 몇 개인가요?";
 
 	public static List<String> readParticipantsNames() {
+		System.out.println(PARTICIPANTS_NAMES_REQUEST_MSG);
 		String line = scanner.nextLine();
 		System.out.println();
 		String[] names = line.split(DELIMITER);
