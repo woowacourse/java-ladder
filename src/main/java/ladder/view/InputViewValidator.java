7e0fd41c27ea11720e5698bd82f4be292bf00cc5
JunDonghyuk <ehdgur4814@naver.com>
2023-02-16 23:10:55 +0900

    fix: 잘못된 메시지 수정

--- src/main/java/ladder/view/InputViewValidator.java
+++ src/main/java/ladder/view/InputViewValidator.java
@@ -5,7 +5,7 @@ public class InputViewValidator {
     private static final int MAX_NAMES_COUNT = 10000;
     private static final int MIN_NAMES_COUNT = 1;
     private static final String NAMES_MAX_COUNT_EXCEPTION_MESSAGE = "[ERROR] 참여자의 수는 10000명 이하여야합니다.";
-    private static final String NAMES_MIN_COUNT_EXCEPTION_MESSAGE = "[ERROR] 참여자의 수는 10000명 이하여야합니다.";
+    private static final String NAMES_MIN_COUNT_EXCEPTION_MESSAGE = "[ERROR] 참여자의 수는 1명 이상이여야합니다.";
     private static final String NONE_NUMERIC_EXCEPTION_MESSAGE = "[ERROR] 숫자만 입력해야합니다.";
 
     public static void validateNamesCount(final int count) {
