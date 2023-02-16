ddf0bd4689ea2d7f511b6ba6ee9ba73da6497ec0
MoonJeWoong <munjin0201@naver.com>
2023-02-16 17:13:30 +0900

    test: 사다리 발판 모양을 생성 기능 테스트 리팩토링
    세부사항
    - LadderStep Enum에서 key를 사용하지 않음

--- src/test/java/model/LadderStepTest.java
+++ src/test/java/model/LadderStepTest.java
@@ -10,8 +10,8 @@ public class LadderStepTest {
     @Test
     @DisplayName("사다리 발판 모양을 생성 기능 테스트")
     void createLadderStepTest() {
-        assertThat(LadderStep.of("first")).isEqualTo(LadderStep.FIRST_STEP);
-        assertThat(LadderStep.of("empty")).isEqualTo(LadderStep.EMPTY_STEP);
-        assertThat(LadderStep.of("exist")).isEqualTo(LadderStep.EXIST_STEP);
+        assertThat(LadderStep.FIRST_STEP.getStep()).isEqualTo(String.format("%5s","    |"));
+        assertThat(LadderStep.EMPTY_STEP.getStep()).isEqualTo(String.format("%6s","     |"));
+        assertThat(LadderStep.EXIST_STEP.getStep()).isEqualTo(String.format("%6s","-----|"));
     }
 }
