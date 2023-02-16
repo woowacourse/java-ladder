55373c24426e4bb1869e3f77081a5f4389d15802
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-02-16 16:43:36 +0900

    refactor: 매직 넘버를 제거합니다. domain.ladder.strategy.RandomGenerateBridgeStrategy

--- src/main/java/domain/ladder/strategy/RandomGenerateBridgeStrategy.java
+++ src/main/java/domain/ladder/strategy/RandomGenerateBridgeStrategy.java
@@ -4,9 +4,14 @@ import util.RandomNumberGenerator;
 
 public class RandomGenerateBridgeStrategy implements GenerateBridgeStrategy {
 
+    private static final int RATE = 40;
+    private static final int LOWER_BOUND_INCLUSIVE = 1;
+    private static final int UPPER_BOUND_INCLUSIVE = 100;
+
     @Override
     public boolean isGeneratedBridge() {
-        return 6 <= RandomNumberGenerator.pickRandomNumberInRange(1, 10);
+        return RandomNumberGenerator.pickRandomNumberInRange(
+                LOWER_BOUND_INCLUSIVE, UPPER_BOUND_INCLUSIVE) <= RATE;
     }
 
 }
