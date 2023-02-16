eb3654d3ce3cc1e4be5af165fa3b65c3701f3a7c
junpakPark <junpak.park@gmail.com>
2023-02-16 13:39:31 +0900

    refactor: Name 클래스 매직넘버 제거 및 trim 메서드 적용

--- src/main/java/laddergame/domain/Name.java
+++ src/main/java/laddergame/domain/Name.java
@@ -5,18 +5,24 @@ import java.util.Objects;
 public class Name {
 
     private static final String NAME_LENGTH_RANGE_MESSAGE = "1이상 5글자 이하의 이름을 입력해 주세요.";
+    private static final int MIN_NAME_LENGTH = 1;
+    private static final int MAX_NAME_LENGTH = 5;
 
     private final String name;
 
     public Name(final String name) {
         validateNameLength(name);
-        this.name = name;
+        this.name = name.trim();
     }
 
     private void validateNameLength(final String name) {
-        if (name.length() < 1 || name.length() > 5) {
+        if (isOutOfRange(name)) {
             throw new IllegalArgumentException(NAME_LENGTH_RANGE_MESSAGE);
         }
+    }
+
+    private static boolean isOutOfRange(final String name) {
+        return name.length() < MIN_NAME_LENGTH || name.length() > MAX_NAME_LENGTH;
     }
 
     @Override
