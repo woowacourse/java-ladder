ed7b47365d4cc87dd2838a65b898023a9e09b42d
LJW25 <dbjwl@g.skku.edu>
2023-02-16 14:01:28 +0900

    test(LadderGame): 사다리 생성 테스트 작성

--- src/test/java/ladder/model/LadderGameTest.java
+++ src/test/java/ladder/model/LadderGameTest.java
@@ -7,23 +7,57 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+import static org.assertj.core.util.Lists.newArrayList;
 
 class LadderGameTest {
 
     @Test
     @DisplayName("플레이어가 2명 미만이면 예외처리 테스트")
     void invalidHeightTest() {
-        List<String> input = new ArrayList<>(List.of("이오"));
-        Assertions.assertThatThrownBy(() -> new LadderGame(input))
+        List<PlayerName> input = new ArrayList<>(List.of(new PlayerName("이오")));
+        Assertions.assertThatThrownBy(() -> new LadderGame(input, new RandomLineCreateDecider()))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("플레이어가 2명 이상이면 통과하는 테스트")
     void validHeightTest() {
-        List<String> input = new ArrayList<>(List.of("이오", "이리내"));
-        assertThatCode(() -> new LadderGame(input)).doesNotThrowAnyException();
+        List<PlayerName> input = new ArrayList<>(List.of(new PlayerName("이오"), new PlayerName("이리내")));
+        assertThatCode(() -> new LadderGame(input, new RandomLineCreateDecider())).doesNotThrowAnyException();
+    }
+
+
+    @Test
+    @DisplayName("사다리 생성 테스트")
+    void generateLadderTest() {
+        List<PlayerName> input = new ArrayList<>(List.of(new PlayerName("a"), new PlayerName("asd"), new PlayerName("qwert")));
+
+        LadderGame ladderGame = new LadderGame(input, new TestLineCreateDecider(newArrayList(true, false, true)));
+        ladderGame.generateLadder(2);
+        List<Raw> raws = ladderGame.getLadder().getRaws();
+
+        assertThat(raws.get(0).isPointHasLine(0)).isTrue();
+        assertThat(raws.get(0).isPointHasLine(1)).isFalse();
+        assertThat(raws.get(1).isPointHasLine(0)).isFalse();
+        assertThat(raws.get(1).isPointHasLine(1)).isTrue();
+    }
+
+
+    static class TestLineCreateDecider implements LineCreateDecider {
+
+        private final List<Boolean> isCreated;
+
+        TestLineCreateDecider(List<Boolean> isCreated) {
+            this.isCreated = isCreated;
+        }
+
+        @Override
+        public boolean decide() {
+            return isCreated.remove(0);
+        }
+
     }
 
 }
