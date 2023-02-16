926d1d49bb354687ef30ce4ddce715618ce7ae7e
jjongwa <troas96@naver.com>
2023-02-16 13:56:39 +0900

    test: 라인 생성 제한 조건 테스트

--- src/test/java/LineTest.java
+++ src/test/java/LineTest.java
@@ -1,3 +1,7 @@
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,5 +20,27 @@ public class LineTest {
     void shouldNotMakePoint() {
         FixedNumberGenerator fixedNumberGenerator = new FixedNumberGenerator(0);
         Assertions.assertThat(fixedNumberGenerator.isPoint()).isEqualTo(false);
+    }
+
+    @DisplayName("라인 생성 확인 테스트")
+    @Test
+    void shouldSuccessLine() {
+        assertDoesNotThrow(() -> new Line(List.of(false, true, false, true)));
+    }
+
+    @DisplayName("라인 생성 시, 포인트 첫 번째 값이 true면 실패한다.")
+    @Test
+    void shouldFailFirstPointIsTrue() {
+        assertThatThrownBy(() -> new Line(List.of(true, false, false, true)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining();
+    }
+
+    @DisplayName("라인 생성 시, 포인트 값이 연속으로 true면 실패한다.")
+    @Test
+    void shouldFailContinuousTrue() {
+        assertThatThrownBy(() -> new Line(List.of(false, true, true, false)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining();
     }
 }
