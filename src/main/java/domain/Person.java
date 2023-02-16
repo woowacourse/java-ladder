897f82ebe049712099432d0de721c6439dc0b096
0chil <0@chll.it>
2023-02-16 15:23:41 +0900

    refactor : 검증 메서드 병합
    - 최대, 최소 한 메서드로 병합

--- src/main/java/domain/Person.java
+++ src/main/java/domain/Person.java
@@ -4,25 +4,17 @@ public class Person {
 
     private static final int MIN_NAME_LENGTH = 1;
     private static final int MAX_NAME_LENGTH = 5;
-    private static final String NAME_LENGTH_EXCEPTION_MESSAGE = "이름은 1글자 이상 5글자 이하여야 합니다";
 
     private final String name;
 
     public Person(final String name) {
-        validateMinNameLength(name);
-        validateMaxNameLength(name);
+        validateNameLength(name);
         this.name = name;
     }
 
-    private void validateMinNameLength(final String name) {
-        if (name.length() < MIN_NAME_LENGTH) {
-            throw new IllegalArgumentException(NAME_LENGTH_EXCEPTION_MESSAGE);
-        }
-    }
-
-    private void validateMaxNameLength(final String name) {
-        if (name.length() > MAX_NAME_LENGTH) {
-            throw new IllegalArgumentException(NAME_LENGTH_EXCEPTION_MESSAGE);
+    private void validateNameLength(final String name) {
+        if (name.length() < MIN_NAME_LENGTH || name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("이름은 1글자 이상 5글자 이하여야 합니다");
         }
     }
 
