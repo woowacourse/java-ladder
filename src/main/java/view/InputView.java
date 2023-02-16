7630e1b139b316644edd98e7dbd3dad0a05baba8
BackFoxx <backfoxxx@gmail.com>
2023-02-16 13:30:50 +0900

    feat: 사용자 입력을 받는 기능 구현

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,55 @@
+package view;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class InputView {
+
+    private static final String SEPARATOR = ",";
+    private static final String BLANK = "";
+    private static final String SPACE = " ";
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public List<String> getNames() {
+        String input = input();
+
+        validateSeparatorShape(input);
+        return getSeparated(input);
+    }
+
+    public int getHeight() {
+        int input;
+
+        try {
+            input = Integer.parseInt(input());
+            validatePositiveInteger(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("정수를 입력해주세요.");
+        }
+        return input;
+    }
+
+    private void validatePositiveInteger(final int input) {
+        if (input < 1) {
+            throw new IllegalArgumentException("양의 정수를 입력해주세요.");
+        }
+    }
+
+    private static List<String> getSeparated(final String input) {
+        return List.of(removeSpace(input).split(SEPARATOR));
+    }
+
+    private static String removeSpace(final String input) {
+        return input.replace(SPACE, BLANK);
+    }
+
+    private String input() {
+        return scanner.nextLine();
+    }
+
+    private void validateSeparatorShape(final String input) {
+        if (input.contains(SEPARATOR)) {
+            throw new IllegalArgumentException(SEPARATOR + "로 이름으 구분해주세요");
+        }
+    }
+}
