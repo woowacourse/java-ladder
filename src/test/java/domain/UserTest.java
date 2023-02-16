f6d2503389bd4e6b07017fcf8cfb8df26b39de36
2014012606 <2014012606@hanyang.ac.kr>
2023-02-16 22:22:44 +0900

    test: User 클래스 테스트코드 수정
    - 변수명을 명확히 수정

--- src/test/java/domain/UserTest.java
+++ src/test/java/domain/UserTest.java
@@ -8,21 +8,21 @@ public class UserTest {
     @Test
     @DisplayName("유저가 제대로 생성되는지 확인")
     void generateUserTest() {
-        String userName = "홍실";
-        Assertions.assertDoesNotThrow(() -> new User(userName));
+        String validUserName = "홍실";
+        Assertions.assertDoesNotThrow(() -> new User(validUserName));
     }
 
     @Test
     @DisplayName("유저의 이름은 5글자 이하여야 한다.")
     void userNameOver5Test() {
-        String userName = "홍실썬샷페어";
-        Assertions.assertThrows(IllegalArgumentException.class, () -> new User(userName));
+        String longUserName = "홍실썬샷페어";
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new User(longUserName));
     }
 
     @Test
     @DisplayName("유저의 이름이 공백인지 확인")
     void userNameBlankTest() {
-        String userName = " ";
-        Assertions.assertThrows(IllegalArgumentException.class, () -> new User(userName));
+        String blankUserName = " ";
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new User(blankUserName));
     }
 }
