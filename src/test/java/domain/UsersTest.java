346335222dceccb02d6bd0ca4cdfbfed1db0a385
thdwoqor <celebarte01@gmail.com>
2023-02-16 15:55:13 +0900

    test : print코드 제거

--- src/test/java/domain/UsersTest.java
+++ src/test/java/domain/UsersTest.java
@@ -1,24 +1,23 @@
 package domain;
 
-import domain.User;
-import domain.Users;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.stream.Stream;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatCode;
 
 public class UsersTest {
     @DisplayName("입력된 유저의 수가 1~10명 사이가 아니면 예외가 발생한다.")
     @ParameterizedTest
     @MethodSource("usersFailParameter")
     void usersSizeFailTest(List<User> input) {
-        System.out.println(input);
         Assertions.assertThrows(IllegalArgumentException.class,
                 () -> new Users(input));
     }
