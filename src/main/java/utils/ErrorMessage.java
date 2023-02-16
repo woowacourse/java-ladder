103afb1a2d420713052569d36d75861200a67b14
kim0914 <kosa0914@naver.com>
2023-02-16 14:56:54 +0900

    refactor(ErrorMessage): 예외 메세지 문구 변경

--- src/main/java/utils/ErrorMessage.java
+++ src/main/java/utils/ErrorMessage.java
@@ -2,8 +2,8 @@ package utils;
 
 public enum ErrorMessage {
 
-    DIGIT_ERROR("숫자만 입력 가능합니다."),
-    HEIGHT_ERROR("사다리 높이는 1이상 100이하의 수만 가능합니다."),
+    DIGIT_ERROR("자연수만 입력 가능합니다."),
+    HEIGHT_ERROR("사다리 높이는 1이상 100이하의 자연수만 가능합니다."),
     NAME_LENGTH_ERROR("참가자 이름의 길이는 1이상 5이하만 가능합니다."),
     NAME_FORMAT_ERROR("참가자 이름에는 공백이 들어갈 수 없습니다."),
     PLAYER_SIZE_ERROR("게임 참여자 수는 최소 2명 최대 50명까지 가능합니다."),
