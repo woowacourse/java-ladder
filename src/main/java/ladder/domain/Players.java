1cb91ed08898885e14bf0e471454cc00f9482c52
Cyma-s <ssol0319@naver.com>
2023-02-16 13:56:21 +0900

    feat: Player들의 최대 이름 길이를 구하는 기능 추가

--- src/main/java/ladder/domain/Players.java
+++ src/main/java/ladder/domain/Players.java
@@ -23,7 +23,16 @@ public class Players {
         }
     }
 
-    public String getPlayerName(StartPoint startPoint) {
-        return players.get(startPoint).getName();
+    public List<String> getPlayerNames() {
+        return players.stream()
+                .map(Player::getName)
+                .collect(Collectors.toList());
+    }
+
+    public int getNameMaxLength() {
+        return this.players.stream()
+                .map(player -> player.getName().length())
+                .max(Integer::compareTo)
+                .orElseThrow(() -> new IllegalStateException("플레이어가 존재하지 않습니다."));
     }
 }
