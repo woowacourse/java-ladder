0d2e21f0092b4e89b0cd49f99aece624fef3eae4
shin-mallang <huipulci1@naver.com>
2023-02-16 14:34:36 +0900

    refactor: 문자열 포장

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -5,7 +5,10 @@ import domain.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
+
+import static java.lang.String.format;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
 public class OutputView {
 
@@ -14,6 +17,7 @@ public class OutputView {
     private static final String NONE_SCAFFOLD = "     ";
     private static final String BLANK = " ";
     private static final String EMPTY = "";
+    private static final String LINE_BLANK = "\n";
     private static final String FIRST_NAME_FORMAT = "%s  ";
     private static final String DEFAULT_NAME_FORMAT = "%5s";
     private static final Map<Scaffold, String> SCAFFOLD_STRING_MAP;
@@ -25,38 +29,42 @@ public class OutputView {
     }
 
     public static void printResult(final Ladder ladder, final Names names) {
-        printName(names);
+        printNames(names);
         printLadder(ladder, names);
     }
 
-    public static void printName(final Names names) {
-        List<String> nameValues = names.getNames().stream().map(Name::getValue).collect(Collectors.toList());
-        String firstName = String.format(FIRST_NAME_FORMAT, nameValues.remove(0));
+    public static void printNames(final Names names) {
+        List<String> nameValues = names.getNames()
+                .stream()
+                .map(Name::getValue)
+                .collect(toList());
+        String firstName = format(FIRST_NAME_FORMAT, nameValues.remove(0));
         System.out.println(makeNameFormat(nameValues, firstName));
     }
 
     private static String makeNameFormat(final List<String> nameValues, final String firstName) {
-        return nameValues.stream().map(it -> String.format(DEFAULT_NAME_FORMAT, it))
-                .collect(Collectors.joining(BLANK, firstName, EMPTY));
+        return nameValues.stream()
+                .map(it -> format(DEFAULT_NAME_FORMAT, it))
+                .collect(joining(BLANK, firstName, EMPTY));
     }
 
     private static void printLadder(final Ladder ladder, final Names names) {
         int length = names.firstNameLength();
         String prefixBlank = BLANK.repeat(length);
-        String ladderFormat = getLadderFormat(ladder, prefixBlank);
+        String ladderFormat = makeLadderFormat(ladder, prefixBlank);
         System.out.println(ladderFormat);
     }
 
-    private static String getLadderFormat(final Ladder ladder, final String prefixBlank) {
+    private static String makeLadderFormat(final Ladder ladder, final String prefixBlank) {
         return ladder.getLines().stream()
-                .map(line -> getLineFormat(prefixBlank, line)
-                ).collect(Collectors.joining("\n"));
+                .map(line -> makeLineFormat(prefixBlank, line)
+                ).collect(joining(LINE_BLANK));
     }
 
-    private static String getLineFormat(final String prefixBlank, final Line line) {
+    private static String makeLineFormat(final String prefixBlank, final Line line) {
         return line.getScaffolds()
                 .stream()
                 .map(SCAFFOLD_STRING_MAP::get)
-                .collect(Collectors.joining(BAR, prefixBlank + BAR, BAR));
+                .collect(joining(BAR, prefixBlank + BAR, BAR));
     }
 }
