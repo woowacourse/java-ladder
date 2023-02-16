2dc2ce8da34e79515da1708e48fa5d8cee897e6b
Cyma-s <ssol0319@naver.com>
2023-02-16 16:04:08 +0900

    refactor: 라인을 랜덤 생성하는 로직 수정하여 인덴트 개선

--- src/main/java/ladder/utils/RandomLineStrategy.java
+++ src/main/java/ladder/utils/RandomLineStrategy.java
@@ -1,21 +1,28 @@
 package ladder.utils;
 
 import java.util.*;
+import java.util.stream.IntStream;
 
 public class RandomLineStrategy implements LineStrategy {
-
     private final Random random = new Random();
 
     @Override
     public List<Boolean> generate(int partCount) {
-        List<Boolean> line = new ArrayList<>();
-        while (line.size() < partCount) {
-            boolean current = random.nextBoolean();
-            line.add(current);
-            if (current && (line.size() <= partCount - 1)) {
-                line.add(false);
+        List<Boolean> line = makeRandomLine(partCount);
+
+        IntStream.range(0, partCount - 1).forEach(i -> {
+            if (line.get(i) && line.get(i + 1)) {
+                line.set(i + 1, false);
             }
+        });
+        return line;
+    }
+
+    private List<Boolean> makeRandomLine(int partCount) {
+        List<Boolean> line = new ArrayList<>();
+        for (int i = 0; i < partCount; i++) {
+            line.add(random.nextBoolean());
         }
-        return Collections.unmodifiableList(line);
+        return line;
     }
 }
