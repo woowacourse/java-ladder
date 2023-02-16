e0f76e3fd82608f373c015bbf15d1ba101c5f7ca
thdwoqor <celebarte01@gmail.com>
2023-02-16 14:21:08 +0900

    refactor : indent를 줄이기 위해 함수 분리

--- src/main/java/domain/Ladder.java
+++ src/main/java/domain/Ladder.java
@@ -4,17 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Ladder {
-    private final List<Bridge> ladder; //enum
+    private final List<Bridge> ladder;
 
     public Ladder(List<Bridge> ladder) {
-        for (int index = 1; index < ladder.size(); index++) { //스트림
-            Bridge currentBridge = ladder.get(index);
-            Bridge previousBridge = ladder.get(index - 1);
-            if (currentBridge.isExist() && previousBridge.isExist()) {
-                throw new IllegalArgumentException("다리는 연속으로 생성되면 안됩니다.");
-            }
+        for (int index = 1; index < ladder.size(); index++) {
+            validateLadder(ladder, index);
         }
         this.ladder = ladder;
+    }
+
+    private void validateLadder(List<Bridge> ladder, int index) {
+        Bridge currentBridge = ladder.get(index);
+        Bridge previousBridge = ladder.get(index - 1);
+        if (currentBridge.isExist() && previousBridge.isExist()) {
+            throw new IllegalArgumentException("다리는 연속으로 생성되면 안됩니다.");
+        }
     }
 
     public List<Bridge> getLadder() {
