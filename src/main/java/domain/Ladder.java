7b1088d3646b38bfa52d4ec539fa22c0b77f7c0d
hanueleee <ezzeezz@naver.com>
2023-02-16 14:15:15 +0900

    feat: 사다리 생성 구현

--- src/main/java/domain/Ladder.java
+++ src/main/java/domain/Ladder.java
@@ -2,18 +2,31 @@ package domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import util.BooleanGenerator;
 
 public class Ladder {
     private final List<Line> lines;
 
-    public Ladder(int ladderHeight, int personCount) {
+    public Ladder(Height height, int personCount) {
         this.lines = new ArrayList<>();
-        for (int index = 0; index < ladderHeight; index++) {
+        for (int index = 0; index < height.getHeight(); index++) {
             lines.add(new Line(personCount));
+        }
+    }
+
+    public void generateLadder(BooleanGenerator booleanGenerator) {
+        for (Line line : lines) {
+            line.generate(booleanGenerator);
         }
     }
 
     public int calculateTotalHeight() {
         return lines.size();
+    }
+    public List<List<Boolean>> getStatus() {
+        return lines.stream()
+                .map(Line::getBridges)
+                .collect(Collectors.toList());
     }
 }
