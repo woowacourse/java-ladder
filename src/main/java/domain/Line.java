61be2a0c9f76de37a6a595605d1ada7e76201101
yujamint <woawoa0513@gmail.com>
2023-02-16 14:44:52 +0900

    refactor: 불필요한 필드 제거 및 메소드 분리

--- src/main/java/domain/Line.java
+++ src/main/java/domain/Line.java
@@ -6,24 +6,24 @@ import utils.LineMaker;
 
 public class Line {
 
-    private final LineMaker lineMaker;
     private final List<Boolean> points;
 
     public Line(LineMaker lineMaker, int userCount) {
-        this.lineMaker = lineMaker;
         this.points = new ArrayList<>();
         addPoint(lineMaker.generateLine(userCount));
     }
 
-    // TODO: 2023/02/15 메소드명 변경
     public void addPoint(List<Boolean> randomPoints) {
         for (Boolean randomPoint : randomPoints) {
-            if (checkFront()) {
-                points.add(false);
-                continue;
-            }
-            points.add(randomPoint);
+            points.add(convertPoint(randomPoint));
         }
+    }
+
+    private boolean convertPoint(Boolean randomPoint) {
+        if (checkFront()) {
+            return false;
+        }
+        return randomPoint;
     }
 
     private boolean checkFront() {
