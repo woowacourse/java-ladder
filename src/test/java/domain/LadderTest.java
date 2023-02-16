3511574fbb86c42635e8951c7144cbd0484cc729
2014012606 <2014012606@hanyang.ac.kr>
2023-02-16 22:18:01 +0900

    test: Ladder 클래스 테스트코드 수정
    - DisplayName 명확히 수정
    - 사다리의 높이로 0이 입력될 때를 테스트하는 경우를 추가

--- src/test/java/domain/LadderTest.java
+++ src/test/java/domain/LadderTest.java
@@ -5,19 +5,25 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LadderTest {
+    private final int validatePersonCount = 5;
     @Test
-    @DisplayName("사다리는 높이를 입력받는다.")
+    @DisplayName("사다리의 높이로 0 이상의 수를 입력받는다.")
     void ladderTest() {
-        int height = 4;
-        int personCount = 5;
-        Assertions.assertDoesNotThrow(() -> new Ladder(height, personCount));
+        int height = 1;
+        Assertions.assertDoesNotThrow(() -> new Ladder(height, validatePersonCount));
+    }
+
+    @Test
+    @DisplayName("사다리의 높이로 0을 입력받는다.")
+    void ladderHeightZeroTest() {
+        int height = 0;
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Ladder(height, validatePersonCount));
     }
 
     @Test
     @DisplayName("사다리의 높이로 음수가 들어오는 경우 예외를 발생시킨다.")
-    void ladderHeightNonPositive() {
+    void ladderHeightNegativeTest() {
         int height = -1;
-        int personCount = 5;
-        Assertions.assertThrows(IllegalArgumentException.class, () -> new Ladder(height, personCount));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Ladder(height, validatePersonCount));
     }
 }
