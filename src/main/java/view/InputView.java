5d0a8417fc7405ec71d362863bb09565b430da3e
xxeol2 <jhj01177@naver.com>
2023-02-16 14:45:37 +0900

    feat: InputView 사용자 입력 요구 메세지 출력 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -10,6 +10,9 @@ import static view.InputViewValidator.validatePlayerNames;
 
 public class InputView {
 
+    public static final String INPUT_PLAYER_NAMES_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    public static final String INPUT_LADDER_HEIGHT_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
+
     private final Scanner scanner;
 
     public InputView(final Scanner scanner) {
@@ -17,6 +20,7 @@ public class InputView {
     }
 
     public List<String> readPlayerNames() {
+        printInputMessage(INPUT_PLAYER_NAMES_MESSAGE);
         String playerNamesInput = readline();
         validatePlayerNames(playerNamesInput);
         return Arrays.stream(playerNamesInput.split(",", -1))
@@ -25,10 +29,14 @@ public class InputView {
     }
 
     public int readLadderHeight() {
-        // TODO : LadderHeigth, size 네이밍 맞추기
+        printInputMessage(INPUT_LADDER_HEIGHT_MESSAGE);
         String ladderHeight = readline();
         validateLadderHeight(ladderHeight);
         return Integer.parseInt(ladderHeight);
+    }
+
+    private void printInputMessage(final String message) {
+        System.out.println(message);
     }
 
     private String readline() {
