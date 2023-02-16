27a946258c2c40b89e11057fa5ffcc98b568f525
jeonghoon <shb03323@naver.com>
2023-02-16 15:10:28 +0900

    test: Line 생성에 대한 테스트를 파라미터 테스트로 변경

--- src/test/java/ladder/domain/LineMakerTest.java
+++ src/test/java/ladder/domain/LineMakerTest.java
@@ -5,6 +5,10 @@ import ladder.domain.builder.LineMaker;
 import ladder.domain.ladder.Block;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,11 +16,12 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.util.Lists.newArrayList;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class LineMakerTest {
 
     @Test
     @DisplayName("라인에 연속해서 블록이 놓인다면 다시 블록을 놓는다.")
-    void 올바른_라인이_생성되는지_테스트() {
+    void 라인이_재생성되는지_테스트() {
         List<Block> blocks = newArrayList(Block.EXIST, Block.EXIST);
         BlockGenerator blockGenerator = new TestBlockGenerator(blocks);
         LineMaker lineMaker = new LineMaker(blockGenerator);
@@ -26,15 +31,21 @@ class LineMakerTest {
                 .isNotEqualTo(blocks);
     }
 
-    @Test
+    @ParameterizedTest
+    @MethodSource("parameterProvider")
     @DisplayName("<플레이어수 - 1>만큼의 블록이 생성된다.")
-    void 올바른_개수의_블록이_생성되는지_확인() {
-        List<Block> blocks = newArrayList(Block.EXIST);
+    void 올바른_개수의_블록이_생성되는지_확인(List<Block> blocks, int playerNumber) {
         BlockGenerator blockGenerator = new TestBlockGenerator(new ArrayList<>(blocks));
         LineMaker lineMaker = new LineMaker(blockGenerator);
-        int playerNumber = 2;
 
-        assertThat(lineMaker.makeLine(playerNumber).getBlocks())
-                .isEqualTo(blocks);
+        assertThat(lineMaker.makeLine(playerNumber).getBlocks().size())
+                .isEqualTo(playerNumber - 1);
+    }
+
+    private List<Arguments> parameterProvider() {
+        return List.of(
+                Arguments.of(List.of(Block.EXIST), 2),
+                Arguments.of(List.of(Block.EXIST, Block.EXIST), 3)
+        );
     }
 }
