ee496ed2610ee3ffce6f632778642c45da75e043
kevstevie <jianna442@gmail.com>
2023-02-16 10:54:47 +0900

    refactor: ladder 생성 방식 수정

--- src/main/java/ladder/view/OutputView.java
+++ src/main/java/ladder/view/OutputView.java
@@ -3,6 +3,7 @@ package ladder.view;
 import ladder.domain.*;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class OutputView {
 
@@ -16,9 +17,27 @@ public class OutputView {
 
     private static void printUsersName(Users users) {
         for (User user : users.getUsers()) {
-            System.out.printf("%-7s", user.getName());
+            System.out.printf(" ".repeat(10 - calculateBlank(user)));
+            System.out.printf("%s", user.getName());
         }
     }
+
+    private static int calculateBlank(User user) {
+        double userNameSpan = 0.0;
+        for(Character name : user.getName().toCharArray()){
+            userNameSpan+=userNameSpanSize(name);
+        }
+        return (int)(userNameSpan+0.5);
+    }
+
+    private static double userNameSpanSize(Character name) {
+        String hi = String.valueOf(name);
+        if(Pattern.matches("[ㄱ-ㅎㅏ-ㅣ가-힣]", hi)){
+            return 1.5;
+        }
+        return 1.0;
+    }
+
 
     private static void printLadder(Ladder ladder) {
         for (Floor floor : ladder.getFloors()) {
@@ -28,7 +47,7 @@ public class OutputView {
     }
 
     private static void printFloor(Floor floor) {
-        System.out.print("    ");
+        System.out.print(" ".repeat(9));
         System.out.print("|");
         for (Line line : floor.getLines()) {
             printLine(line);
@@ -37,11 +56,11 @@ public class OutputView {
 
     private static void printLine(Line line) {
         if (line.isExist()) {
-            System.out.print("-----");
+            System.out.print("-".repeat(9));
             System.out.print("|");
             return;
         }
-        System.out.printf("     |");
+        System.out.printf("         |");
     }
 
 }
