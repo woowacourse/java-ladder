a9b76b46e0d68d85d34efa65ba9d64a3f2716918
donghae <donghae1999@gmail.com>
2023-02-16 10:55:29 +0900

    test: PlayersTest에서 NamesTest로 이름 변경

---
+++ src/test/java/model/NamesTest.java
@@ -0,0 +1,20 @@
+package model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class NamesTest {
+
+    @Test
+    @DisplayName("Names 객체 생성 성공 테스트")
+    void createNamesTest() {
+        Names names = new Names(Arrays.asList("pobi,crong,honux"));
+
+        assertThat(names.getNames().size()).isEqualTo(3);
+    }
+
+}
