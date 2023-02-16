349671b5fb1f6b3661832569ce8aaefc57374d5a
2014012606 <2014012606@hanyang.ac.kr>
2023-02-16 22:03:58 +0900

    test: User 클래스 테스트코드 수정
    - User 생성자에 입력되는 String 분리
    - getter 테스트 삭제

--- src/test/java/domain/UserTest.java
+++ src/test/java/domain/UserTest.java
@@ -4,33 +4,25 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class UserTest {
     @Test
     @DisplayName("유저가 제대로 생성되는지 확인")
-    void userTest() {
-        Assertions.assertDoesNotThrow(() -> new User("홍실"));
+    void generateUserTest() {
+        String userName = "홍실";
+        Assertions.assertDoesNotThrow(() -> new User(userName));
     }
 
     @Test
-    @DisplayName("유저가 이름은 5글자 이하여야한다.")
-    void userName5overTest() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> new User("홍실썬샷페어"));
+    @DisplayName("유저의 이름은 5글자 이하여야 한다.")
+    void userNameOver5Test() {
+        String userName = "홍실썬샷페어";
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new User(userName));
     }
 
     @Test
     @DisplayName("유저의 이름이 공백인지 확인")
     void userNameBlankTest() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> new User(" "));
-    }
-
-    @Test
-    @DisplayName("유저의 이름을 반환하는 메서드 테스트")
-    void getUserNameTest() {
-        String userName = "썬샷";
-        User user = new User(userName);
-        assertThat(user.getName())
-                .isEqualTo(userName);
+        String userName = " ";
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new User(userName));
     }
 }
