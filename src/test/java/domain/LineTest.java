8a7d8a44019a46d2428cad1aee647cd3815d0a54
xxeol2 <jhj01177@naver.com>
2023-02-16 16:21:18 +0900

    fix: 테스트문 속 잘못된 포인트 유효값 수정

--- src/test/java/domain/LineTest.java
+++ src/test/java/domain/LineTest.java
@@ -2,27 +2,23 @@ package domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LineTest {
 
-    @DisplayName("라인의 포인트 개수는 20을 넘을 수 없다.")
+    @DisplayName("라인의 포인트 개수는 19를 넘을 수 없다.")
     @Test
-    void pointNotMoreThan20() {
-        int pointSize = 21;
-        assertThatThrownBy(() -> new Line(pointSize))
+    void pointNotMoreThan19() {
+        assertThatThrownBy(() -> new Line(20))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("라인의 포인트 개수는 0이하일 수 없다.")
-    @ValueSource(ints = {0, -1})
-    @ParameterizedTest
-    void pointNotLessThan1(int pointSize) {
-        assertThatThrownBy(() -> new Line(pointSize))
+    @DisplayName("라인의 포인트 개수는 0보다 작을 수 없다.")
+    @Test
+    void pointNotLessThan0() {
+        assertThatThrownBy(() -> new Line(-1))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
