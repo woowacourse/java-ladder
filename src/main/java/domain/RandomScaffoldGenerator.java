24e6f63456c7543f33e6c75aad81c79705cab370
shin-mallang <huipulci1@naver.com>
2023-02-16 20:35:14 +0900

    refactor: 발판 생성 확률을 증가
    EXIST - EXIST인 경우는 없으므로 50프로라면 상대적으로 사다리가 많이 생성되지 않아 위처럼 조금 더 생성되도록 수정

--- src/main/java/domain/RandomScaffoldGenerator.java
+++ src/main/java/domain/RandomScaffoldGenerator.java
@@ -1,20 +1,17 @@
 package domain;
 
-import java.util.Random;
-
 public class RandomScaffoldGenerator implements ScaffoldGenerator {
-
-    private static final Random random = new Random();
 
     @Override
     public Scaffold generate() {
-        if (fiftyPercent()) {
-            return Scaffold.EXIST;
+        // 25 % 확률로 NONE
+        if (twentyFivePercent()) {
+            return Scaffold.NONE;
         }
-        return Scaffold.NONE;
+        return Scaffold.EXIST;
     }
 
-    private boolean fiftyPercent() {
-        return random.nextBoolean();
+    private boolean twentyFivePercent() {
+        return (int) (Math.random() * 4) == 0;
     }
 }
