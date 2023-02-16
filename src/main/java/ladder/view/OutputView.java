7d48c69722fadbc7f50219356a2dd1a2fbd011d1
greeng00se <thegreengoose7@gmail.com>
2023-02-16 09:35:29 +0900

    refactor: 메소드 분리 및 호출 순서 변경
    Co-authored-by: wugawuga <dnr3655@naver.com>

--- src/main/java/ladder/view/OutputView.java
+++ src/main/java/ladder/view/OutputView.java
@@ -1,13 +1,17 @@
 package ladder.view;
 
+import static java.util.stream.Collectors.joining;
+
 import java.util.List;
-import java.util.stream.Collectors;
 import ladder.domain.Line;
 import ladder.domain.LineStatus;
 import ladder.domain.Player;
 
 public class OutputView {
 
+    private static final int INITIAL_PLAYER_INDEX = 0;
+    private static final int EMPTY_NAME_LENGTH = 0;
+    private static final long SKIP_INITIAL_PLAYER = 1L;
     private static final String GAME_RESULT_MESSAGE = "\n실행결과";
     private static final String CONNECTED_SYMBOL = "-";
     private static final String EMPTY_SYMBOL = " ";
@@ -17,53 +21,63 @@ public class OutputView {
     private static final String NEXT_LINE = "\n";
 
     public void printResult(final List<Player> players, final List<Line> lines) {
-        final int initLength = initPlayerNameLength(players.get(0));
-        final int length = calculateMaxLength(players);
-
         System.out.println(GAME_RESULT_MESSAGE);
-        System.out.print(players.get(0).getName() + EMPTY_SYMBOL);
 
-        players.stream()
-                .map(Player::getName)
-                .skip(1L)
-                .forEach(name -> generateNameMessage(name, length));
+        final int maxNameLength = calculateMaxNameLength(players);
+        System.out.println(generateNameMessages(players, maxNameLength));
 
-        System.out.println(NEXT_LINE + generateLadderMessage(initLength, length, lines));
+        final String initialPlayerName = findInitialPlayerName(players);
+        System.out.println(generateLadderMessage(initialPlayerName.length(), maxNameLength, lines));
     }
 
-    private void generateNameMessage(final String name, int maxNameLength) {
-        System.out.print(String.format(NAME_MESSAGE_FORMAT + maxNameLength + STRING_FORMAT, name));
-    }
-
-    private int initPlayerNameLength(final Player player) {
-        return player.getName().length();
-    }
-
-    private int calculateMaxLength(final List<Player> players) {
+    private int calculateMaxNameLength(final List<Player> players) {
         return players.stream()
-                .mapToInt(player -> player.getName().length())
-                .max()
-                .getAsInt();
+                .map(Player::getName)
+                .map(String::length)
+                .max(Integer::compareTo)
+                .orElse(EMPTY_NAME_LENGTH);
     }
 
-    private String generateLadderMessage(final int initLength, final int length, final List<Line> lines) {
-        return lines.stream()
-                .map(line -> generateLineMessage(initLength, length, line))
-                .collect(Collectors.joining(NEXT_LINE));
+    private String generateNameMessages(final List<Player> players, final int maxNameLength) {
+        final String initialPlayerName = findInitialPlayerName(players) + EMPTY_SYMBOL;
+        final String nameMessage = players.stream()
+                .map(Player::getName)
+                .skip(SKIP_INITIAL_PLAYER)
+                .map(name -> generateNameMessage(name, maxNameLength))
+                .collect(joining());
+        return initialPlayerName + nameMessage;
     }
 
-    private String generateLineMessage(final int initLength, final int length, final Line line) {
-        final String startMessage = String.format(LINE_STATUS_MESSAGE_FORMAT, EMPTY_SYMBOL.repeat(initLength));
+    private String generateNameMessage(final String name, int maxNameLength) {
+        return String.format(NAME_MESSAGE_FORMAT + maxNameLength + STRING_FORMAT, name);
+    }
+
+    private String findInitialPlayerName(final List<Player> players) {
+        return players.get(INITIAL_PLAYER_INDEX).getName();
+    }
+
+    private String generateLadderMessage(final int initialNameLength, final int maxNameLength, final List<Line> lines) {
+        return NEXT_LINE + lines.stream()
+                .map(line -> generateLineMessage(initialNameLength, maxNameLength, line))
+                .collect(joining(NEXT_LINE));
+    }
+
+    private String generateLineMessage(final int initialNameLength, final int maxNameLength, final Line line) {
+        final String initialMessage = generateRepeatedLineStatusMessage(CONNECTED_SYMBOL, initialNameLength);
         final String ladderMessage = line.getLine().stream()
-                .map(lineStatus -> generateLineStatusMessage(length, lineStatus))
-                .collect(Collectors.joining());
-        return startMessage + ladderMessage;
+                .map(lineStatus -> generateLineStatusMessage(maxNameLength, lineStatus))
+                .collect(joining());
+        return initialMessage + ladderMessage;
     }
 
-    private String generateLineStatusMessage(final int length, final LineStatus lineStatus) {
+    private String generateRepeatedLineStatusMessage(final String symbol, final int count) {
+        return String.format(LINE_STATUS_MESSAGE_FORMAT, symbol.repeat(count));
+    }
+
+    private String generateLineStatusMessage(final int maxNameLength, final LineStatus lineStatus) {
         if (LineStatus.GO == lineStatus) {
-            return String.format(LINE_STATUS_MESSAGE_FORMAT, CONNECTED_SYMBOL.repeat(length));
+            return generateRepeatedLineStatusMessage(CONNECTED_SYMBOL, maxNameLength);
         }
-        return String.format(LINE_STATUS_MESSAGE_FORMAT, EMPTY_SYMBOL.repeat(length));
+        return generateRepeatedLineStatusMessage(EMPTY_SYMBOL, maxNameLength);
     }
 }
