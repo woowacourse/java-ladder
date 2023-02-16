8d24cf8788110e51f0343b666b6d583301d12590
chaewon121 <cw121@naver.com>
2023-02-16 16:26:45 +0900

    feat: 뷰와 도메인 객체를 조합하여 사다리 애플리케이션 구현

--- src/main/java/ladder/Application.java
+++ src/main/java/ladder/Application.java
@@ -1,9 +1,10 @@
 package ladder;
 
-import ladder.domain.RandomBasedBarGenerator;
+import ladder.controller.LadderController;
 
 public class Application {
     public static void main(String[] args) {
-
+        LadderController ladderController = new LadderController();
+        ladderController.run();
     }
 }
--- src/main/java/ladder/controller/LadderController.java
+++ src/main/java/ladder/controller/LadderController.java
@@ -1,7 +1,21 @@
 package ladder.controller;
 
+import ladder.domain.Ladder;
+import ladder.domain.NamesSplitor;
+import ladder.domain.RandomBasedBarGenerator;
+import ladder.view.InputView;
+import ladder.view.OutputView;
+
+import java.util.List;
+
 public class LadderController {
     public void run() {
+        List<String> names = InputView.repeat(() -> NamesSplitor.split(InputView.inputPeopleNames()));
+        int ladderHeight = InputView.repeat(InputView::inputLadderHeight);
 
+        Ladder ladder = new Ladder(new RandomBasedBarGenerator(), ladderHeight, names.size());
+
+        OutputView.printNames(names);
+        OutputView.printLadder(ladder, names.get(0).length());
     }
 }
