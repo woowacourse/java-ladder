29214d7cf31f8fda2469cbca30ce115fa15e1777
MoonJeWoong <munjin0201@naver.com>
2023-02-16 17:14:45 +0900

    refactor: 사다리 발판 모양을 생성 기능 수정

--- src/main/java/model/LadderStep.java
+++ src/main/java/model/LadderStep.java
@@ -1,28 +1,19 @@
 package model;
 
-import java.util.Arrays;
-
 public enum LadderStep {
-    FIRST_STEP("first", String.format("%5s","    |")),
-    EMPTY_STEP("empty", String.format("%6s","     |")),
-    EXIST_STEP("exist", String.format("%6s","-----|"));
+    FIRST_STEP(String.format("%5s","    |")),
+    EMPTY_STEP(String.format("%6s","     |")),
+    EXIST_STEP(String.format("%6s","-----|"));
 
-    private final String key;
+    private static final int LIMIT_NAME_LENGTH = 5;
+
     private final String step;
 
-    LadderStep(String key, String step) {
-        this.key = key;
+    LadderStep(String step) {
         this.step = step;
     }
 
     public String getStep() {
         return step;
-    }
-
-    public static LadderStep of(String key) {
-        return Arrays.stream(values())
-                .filter(ladderStep -> ladderStep.key.equals(key))
-                .findFirst()
-                .orElseThrow(()-> new IllegalArgumentException("[ERROR] 존재하지 않는 LadderStep 입니다."));
     }
 }
