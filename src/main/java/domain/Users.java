f7ced5691ab4c0f8671e12255fdaed2f64fa8182
yujamint <woawoa0513@gmail.com>
2023-02-16 11:14:52 +0900

    feat: Users 도메인 생성

---
+++ src/main/java/domain/Users.java
@@ -0,0 +1,15 @@
+package domain;
+
+import java.util.List;
+
+public class Users {
+    private final List<User> users;
+
+    public Users(List<User> users) {
+        this.users = users;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+}
