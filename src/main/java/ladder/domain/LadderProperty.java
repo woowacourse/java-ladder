88b207a2a26421bbfef65a5ac2b8af47f01b77a3
JunDonghyuk <ehdgur4814@naver.com>
2023-02-16 14:36:04 +0900

    feat(LadderProperty): 사다리 높이 검증 기능 구현

--- src/main/java/ladder/domain/LadderProperty.java
+++ src/main/java/ladder/domain/LadderProperty.java
@@ -2,12 +2,23 @@ package ladder.domain;
 
 public class LadderProperty {
 
+    private static final int MIN_HEIGHT = 1;
+    private static final int MAX_HEIGHT = 1000;
+    private static final String HEIGHT_RANGE_EXCEPTION_MESSAGE = "[ERROR] 사다리의 높이는 1 이상 1000 이하여야합니다.";
+
     private final int width;
     private final int height;
 
     public LadderProperty(final int width, final int height) {
         this.width = width;
+        validateHeight(height);
         this.height = height;
+    }
+
+    private void validateHeight(final int height) {
+        if (height < MIN_HEIGHT || height > MAX_HEIGHT) {
+            throw new IllegalArgumentException(HEIGHT_RANGE_EXCEPTION_MESSAGE);
+        }
     }
 
     public final int getWidth() {
