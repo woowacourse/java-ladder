c4f1d08cab70bb5abde1e36394a6967727552b0b
eunbii0213 <eunbii0213@naver.com>
2023-02-16 13:58:25 +0900

    style: 사다리 높이 입력 오류 메세지 상수 선언

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -8,6 +8,11 @@ public class InputView {
     public static final String PLAYER_NAME_INPUT_GUIDE_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     public static final String LADDER_HEIGHT_INPUT_GUIDE_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
     public static final String PLAYER_NAME_NULL_BLANK_ERROR_MESSAGE = "[ERROR] 플레이어의 이름으로 공백 혹은 빈 칸을 입력할 수 없습니다";
+    public static final String LADDER_HEIGHT_ERROR_MESSAGE = "[ERROR] 사다리의 높이는 숫자를 입력해야합니다";
+
+    private static void printPlayerNameInputGuideMessage() {
+        System.out.println(PLAYER_NAME_INPUT_GUIDE_MESSAGE);
+    }
 
     public List<String> readPlayerName() {
         printPlayerNameInputGuideMessage();
@@ -16,20 +21,16 @@ public class InputView {
         String playerName = scanner.nextLine();
 
         validatePlayerNameInput(playerName);
-       return List.of(playerName.split(DELIMITER));
+        return List.of(playerName.split(DELIMITER));
     }
 
-    private static void printPlayerNameInputGuideMessage() {
-        System.out.println(PLAYER_NAME_INPUT_GUIDE_MESSAGE);
-    }
-
-    private void validatePlayerNameInput(String playerName){
-        if(playerName.isBlank() || playerName.isEmpty()){
+    private void validatePlayerNameInput(String playerName) {
+        if (playerName.isBlank() || playerName.isEmpty()) {
             throw new IllegalArgumentException(PLAYER_NAME_NULL_BLANK_ERROR_MESSAGE);
         }
     }
 
-    public int readLadderHeight(){
+    public int readLadderHeight() {
         printLadderHeightGuideMessage();
         Scanner scanner = new Scanner(System.in);
         String ladderHeight = scanner.nextLine();
@@ -41,10 +42,11 @@ public class InputView {
     private static void validateLadderHeightIsNumber(String ladderHeight) {
         try {
             Integer.parseInt(ladderHeight);
-        }catch(IllegalArgumentException e){
-            throw new IllegalArgumentException("[ERROR] 사다리의 높이는 숫자를 입력해야합니다");
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(LADDER_HEIGHT_ERROR_MESSAGE);
         }
     }
+
 
     private void printLadderHeightGuideMessage() {
         System.out.println(LADDER_HEIGHT_INPUT_GUIDE_MESSAGE);
