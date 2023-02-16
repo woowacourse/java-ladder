8453a9abd93b9352d53ccd30bb280a7cc3556e6e
thdwoqor <celebarte01@gmail.com>
2023-02-16 15:39:55 +0900

    refactor : 상수 변수 분리

--- src/main/java/domain/Users.java
+++ src/main/java/domain/Users.java
@@ -4,11 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Users {
+    private static final int USER_COUNT_MIN_RANGE = 1;
+    private static final int USER_COUNT_MAX_RANGE = 10;
+    private static final String INVALID_USER_COUNT_MESSAGE = "유저는 최소 2명 이상이다.";
     private final List<User> users;
 
     public Users(List<User> users) { // 구현
-        if (users.size() < 2 || users.size() > 10) {
-            throw new IllegalArgumentException("유저는 최소 2명 이상이다.");
+        if (users.size() < USER_COUNT_MIN_RANGE || users.size() > USER_COUNT_MAX_RANGE) {
+            throw new IllegalArgumentException(INVALID_USER_COUNT_MESSAGE);
         }
         this.users = users;
     }
