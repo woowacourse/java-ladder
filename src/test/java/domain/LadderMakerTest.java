4ea40eaab8d9b1d7070505e7952c3e9529eb3649
kim0914 <kosa0914@naver.com>
2023-02-16 10:37:44 +0900

    test(LadderMaker): 사다리를 만드는 테스트 객체를 LadderMaker로 변경

--- src/test/java/domain/LadderMakerTest.java
+++ src/test/java/domain/LadderMakerTest.java
@@ -1,26 +1,27 @@
 package domain;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
 
-public class LadderTest {
+public class LadderMakerTest {
+
+    LadderMaker ladderMaker = new LadderMaker();
 
     @ParameterizedTest
     @ValueSource(ints = {1, 10, 50, 100})
     @DisplayName("높이가 1이상 100이하이면 사다리가 생성된다.")
     void createLine_Success(int height) {
-        assertThatNoException().isThrownBy(() -> new Ladder(height));
+        assertThatNoException().isThrownBy(() -> ladderMaker.make(5, height));
     }
 
     @ParameterizedTest
     @ValueSource(ints = {-1, 0, 101, 150, 1000})
     @DisplayName("높이가 1미만 100초과이면 예외가 발생한다.")
     void createLine_Fail(int height) {
-        assertThatThrownBy(() -> new Ladder(height))
+        assertThatThrownBy(() -> ladderMaker.make(5, height))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -28,7 +29,7 @@ public class LadderTest {
     @ValueSource(ints = {2, 10, 100})
     @DisplayName("사다리 높이 만큼 Line을 생성한다.")
     void createLinesWithHeight(int height) {
-        Ladder ladder = new Ladder(height);
-        assertThat(ladder.getLines().size()).isEqaulTo(height);
+        Ladder ladder = new Ladder(ladderMaker.make(5, height));
+        assertThat(ladder.getLines().size()).isEqualTo(height);
     }
 }
