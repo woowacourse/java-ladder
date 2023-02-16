e9aa206cdfd71c3b61bcd684f887d36ce56cd7ab
Woojin Lee <dhkep03@gmail.com>
2023-02-16 14:54:00 +0900

    refactor: 사다리 옆 공백이 첫 플레이어의 이름 길이와 같게 로직 수정

--- src/main/java/ladder/view/OutputView.java
+++ src/main/java/ladder/view/OutputView.java
@@ -5,20 +5,32 @@ import ladder.domain.Bar;
 import ladder.domain.Line;
 
 public class OutputView {
-    private static final String WIDTH_PREFIX = "    ";
+    private static String startSpaces;
+
     private static final String WIDTH_SUFFIX = "|";
     private static final String MOVABLE_BAR = "|-----";
     private static final String IMMOVABLE_BAR = "|     ";
     private static final String BLANK = " ";
-    private static final int DEFAULT_NAME_LENGTH = 7;
+    private static final int DEFAULT_NAME_LENGTH = 6;
+    private static final int MAXIMUM_NAME_LENGTH = 5;
+    private static final String LONGEST_START_SPACE = "    ";
 
     public static void printPlayers(List<String> playerNames) {
+        setStartSpaces(playerNames.get(0).length());
         System.out.println();
         System.out.print(playerNames.get(0));
         for (int i = 1; i < playerNames.size(); i++) {
             printNameOnSquares(playerNames.get(i));
         }
         System.out.println();
+    }
+
+    private static void setStartSpaces(int nameLength) {
+        if (nameLength == MAXIMUM_NAME_LENGTH) {
+            startSpaces = LONGEST_START_SPACE;
+            return;
+        }
+        startSpaces = BLANK.repeat(nameLength);
     }
 
     private static void printNameOnSquares(String playerName) {
@@ -34,7 +46,7 @@ public class OutputView {
     }
 
     private static void printLine(Line line) {
-        System.out.print(WIDTH_PREFIX);
+        System.out.print(startSpaces);
         for (Bar bar : line.getBars()) {
             printBar(bar);
         }
