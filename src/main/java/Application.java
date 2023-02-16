67a4dbce80c5eb31b5849d59b4074e013379abb4
Miseong Kim <miseongk@naver.com>
2023-02-16 15:08:35 +0900

    feat: 사다리 게임 Application

---
+++ src/main/java/Application.java
@@ -0,0 +1,7 @@
+public class Application {
+
+    public static void main(String[] args) {
+        LadderController ladderController = new LadderController();
+        ladderController.run();
+    }
+}
