d3762e8e37c82c17fb6a8dec7a43ae780aa71b24
sosow0212 <sosow0212@naver.com>
2023-02-16 11:56:57 +0900

    refactor: Line 메서드 분리 및 리네이밍 진행

--- src/main/java/domain/Line.java
+++ src/main/java/domain/Line.java
@@ -9,24 +9,47 @@ public class Line {
     private final List<Boolean> footholds;
 
     public Line(final int numberOfPlayer, final FootholdGenerator footholdGenerator) {
-        footholds = makePoint(numberOfPlayer, footholdGenerator);
+        footholds = makeFootholds(numberOfPlayer, footholdGenerator);
     }
 
-    private List<Boolean> makePoint(final int numberOfPlayer, final FootholdGenerator footholdGenerator) {
-        List<Boolean> points = new ArrayList<>();
+    private List<Boolean> makeFootholds(final int numberOfPlayer, final FootholdGenerator footholdGenerator) {
+        List<Boolean> footholds = new ArrayList<>();
+
+        generateFootholds(footholdGenerator, footholds, numberOfPlayer);
+
+        return footholds;
+    }
+
+    private void generateFootholds(final FootholdGenerator footholdGenerator, final List<Boolean> footholds,
+                                   final int numberOfPlayer) {
         int numberOfPoint = numberOfPlayer - 1;
 
+        createFirstFoothold(footholds, footholdGenerator);
+        createOtherFootholds(footholdGenerator, footholds, numberOfPoint);
+    }
+
+    private void createFirstFoothold(final List<Boolean> points, final FootholdGenerator footholdGenerator) {
         points.add(footholdGenerator.generate());
+    }
+
+    private void createOtherFootholds(final FootholdGenerator footholdGenerator, final List<Boolean> footholds,
+                                      final int numberOfPoint) {
         for (int i = 1; i < numberOfPoint; i++) {
             boolean random = footholdGenerator.generate();
-            if (points.get(i - 1) == true && random == true) {
-                points.add(false);
-                continue;
-            }
-            points.add(random);
+            createFootholdWithoutFirst(footholds, i, random);
         }
+    }
 
-        return points;
+    private void createFootholdWithoutFirst(final List<Boolean> points, final int index, final boolean isExisting) {
+        if (isConsecutiveExistingFoothold(points, index, isExisting)) {
+            points.add(false);
+            return;
+        }
+        points.add(isExisting);
+    }
+
+    private boolean isConsecutiveExistingFoothold(final List<Boolean> points, final int index, final boolean random) {
+        return points.get(index - 1) && random;
     }
 
     public List<Boolean> getFootholds() {
