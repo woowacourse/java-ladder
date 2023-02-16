e144077b934355cbbc06fdb7261118a56c11eee1
iamjooon2 <iamjooon2@gmail.com>
2023-02-16 16:42:42 +0900

    refactor: 함수 네이밍에 규칙성 적용하기

--- src/main/java/domain/LadderFactory.java
+++ src/main/java/domain/LadderFactory.java
@@ -13,28 +13,30 @@ public class LadderFactory {
     }
 
     public Ladder createLadder(final int width, final int height) {
-        List<Line> lines = new ArrayList<>();
-        insertLines(width, height, lines);
+        List<Line> lines = generateLines(width, height);
         return new Ladder(lines);
     }
 
-    private void insertLines(int width, int height, List<Line> lines) {
+    private List<Line> generateLines(int width, int height) {
+        List<Line> lines = new ArrayList<>();
         for (int i = 0; i < height; i++) {
             List<Scaffold> scaffolds = insertScaffolds(width);
             lines.add(new Line(scaffolds));
         }
+        return lines;
     }
 
     private List<Scaffold> insertScaffolds(int width) {
-        List<Scaffold> scaffolds = new ArrayList<>();
-        generateScaffolds(width, scaffolds);
+        List<Scaffold> scaffolds = generateScaffolds(width);
         return scaffolds;
     }
 
-    private void generateScaffolds(int width, List<Scaffold> scaffolds) {
+    private List<Scaffold> generateScaffolds(int width) {
+        List<Scaffold> scaffolds = new ArrayList<>();
         IntStream.range(0, width).forEach(value -> {
             insertDiscontinuousScaffold(scaffolds, value);
         });
+        return scaffolds;
     }
 
     private void insertDiscontinuousScaffold(List<Scaffold> scaffolds, int value) {
