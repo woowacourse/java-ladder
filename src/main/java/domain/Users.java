c76cd1bb8bd3b46776bef82cd573c0e9c865e3ac
thdwoqor <celebarte01@gmail.com>
2023-02-16 15:55:39 +0900

    feat : 최소 인원 수 변수 값 수정

--- src/main/java/domain/Users.java
+++ src/main/java/domain/Users.java
@@ -4,16 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Users {
-    private static final int USER_COUNT_MIN_RANGE = 1;
+    private static final int USER_COUNT_MIN_RANGE = 2;
     private static final int USER_COUNT_MAX_RANGE = 10;
     private static final String INVALID_USER_COUNT_MESSAGE = "유저는 최소 2명 이상이다.";
     private final List<User> users;
 
-    public Users(List<User> users) { // 구현
+    public Users(List<User> users) {
+        validateUserCount(users);
+        this.users = users;
+    }
+
+    private void validateUserCount(List<User> users) {
         if (users.size() < USER_COUNT_MIN_RANGE || users.size() > USER_COUNT_MAX_RANGE) {
             throw new IllegalArgumentException(INVALID_USER_COUNT_MESSAGE);
         }
-        this.users = users;
     }
 
     public int getCount() {
