686606aac08815e5cbcbb7c629168583d91ca5b0
jjongwa <troas96@naver.com>
2023-02-16 11:26:57 +0900

    feat: 사다리 높이 입력 기능 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 public class InputView {
     private static final String INPUT_USER_NAME = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    private static final String INPUT_LADDER_HEIGHT = "최대 사다리 높이는 몇 개인가요?";
 
     static Scanner sc = new Scanner(System.in);
 
@@ -15,5 +16,10 @@ public class InputView {
 
     private List<String> splitNameInput(String nameInput) {
         return List.of(nameInput.split(","));
+    }
+
+    public String inputLadderHeight() {
+        System.out.println(INPUT_LADDER_HEIGHT);
+        return sc.next();
     }
 }
