a35f5da77354172bc7913ac8edf6aa51367148dc
MoonJeWoong <munjin0201@naver.com>
2023-02-16 13:23:20 +0900

    test: 사다리 높이에 맞게 라인을 생성하는 기능 테스트 추가

--- src/test/java/model/LadderTest.java
+++ src/test/java/model/LadderTest.java
@@ -6,11 +6,29 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatNoException
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class LadderTest {
 
     @Test
     @DisplayName("Ladder 객체 생성 성공 테스트")
     void createLadderTest() {
         assertThatNoException().isThrownBy(()->{Ladder ladder = new Ladder(new LadderHeight(5));});
+    }
+
+    @Test
+    @DisplayName("사다리 높이에 맞게 라인을 생성하는 기능 테스트 ")
+    void makeLinesByLadderHeightTest() {
+        //Given
+        Names names = new Names("pobi, neo, hiiro");
+        Players players = new Players(names);
+        LadderHeight ladderHeight = new LadderHeight(5);
+        Ladder ladder = new Ladder(players, ladderHeight);
+
+        //Then
+        IntStream.range(0, ladderHeight.getHeight()).forEach(
+                index -> assertThat(ladder.getLine(index)).isInstanceOf(Line.class)
+        );
     }
 }
