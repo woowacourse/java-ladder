bd06362ec0230f8d1d7a0e58657d0fa20167e340
MoonJeWoong <munjin0201@naver.com>
2023-02-16 12:49:34 +0900

    feat: 사다리 게임 전체 참여자 정보 생성 기능 추가

--- src/main/java/model/Players.java
+++ src/main/java/model/Players.java
@@ -9,6 +9,16 @@ public class Players {
     private final List<Player> players = new ArrayList<>();
 
     public Players(Names names) {
+        IntStream.range(0,names.size()).forEach((index)->{
+            players.add(new Player(names.getName(index)));
+        });
+    }
 
+    public int size() {
+        return players.size();
+    }
+
+    public Player getPlayer(int index) {
+        return players.get(index);
     }
 }
