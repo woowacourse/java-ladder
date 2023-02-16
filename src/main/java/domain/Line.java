3b47699feae0f882afb7a5ee7c8d167efe2b7e56
jjongwa <troas96@naver.com>
2023-02-16 14:30:36 +0900

    feat: 라인 객체 생성 기능 추가

---
+++ src/main/java/domain/Line.java
@@ -0,0 +1,30 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import utils.RandomNumberGenerator;
+
+public class Line {
+    private List<Boolean> points;
+
+    public Line(int personCount) {
+        this.points = createLine(personCount);
+    }
+
+    private List<Boolean> createLine(int personCount) {
+        List<Boolean> points = new ArrayList<>();
+        points.add(false);
+        for (int i = 1; i < personCount; i++) {
+            points.add(isContinuousTrue(points, i));
+        }
+        return points;
+    }
+
+    private boolean isContinuousTrue(List<Boolean> points, int index) {
+        RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+        if (points.get(index - 1)) {
+            return false;
+        }
+        return randomNumberGenerator.isPoint();
+    }
+}
