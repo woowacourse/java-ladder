59a1e2064ff4a52b31d7883d874bc2b05212653f
iamjooon2 <iamjooon2@gmail.com>
2023-02-16 15:11:40 +0900

    refactor: LadderFactory 클래스 메서드 분리

--- src/main/java/domain/LadderFactory.java
+++ src/main/java/domain/LadderFactory.java
@@ -2,6 +2,7 @@ package domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class LadderFactory {
 
@@ -13,26 +14,43 @@ public class LadderFactory {
 
     public Ladder createLadder(final int width, final int height) {
         List<Line> lines = new ArrayList<>();
+        insertLines(width, height, lines);
+        return new Ladder(lines);
+    }
+
+    private void insertLines(int width, int height, List<Line> lines) {
         for (int i = 0; i < height; i++) {
-
-            List<Scaffold> scaffolds = new ArrayList<>();
-
-            for (int j = 0; j < width; j++) {
-                Scaffold scaffold = scaffoldGenerator.generate();
-
-                if (scaffolds.isEmpty()) {
-                    scaffolds.add(scaffold);
-                    continue;
-                }
-
-                if (scaffold == Scaffold.EXIST && scaffolds.get(j - 1) == Scaffold.EXIST) {
-                    scaffold = Scaffold.NONE;
-                }
-                scaffolds.add(scaffold);
-            }
-
+            List<Scaffold> scaffolds = insertScaffold(width);
             lines.add(new Line(scaffolds));
         }
-        return new Ladder(lines);
+    }
+
+    private List<Scaffold> insertScaffold(int width) {
+        List<Scaffold> scaffolds = new ArrayList<>();
+        generateScaffold(width, scaffolds);
+        return scaffolds;
+    }
+
+    private void generateScaffold(int width, List<Scaffold> scaffolds) {
+        IntStream.range(0, width).forEach(value -> {
+            createConsecutiveScaffold(scaffolds, value);
+        });
+    }
+
+    private void createConsecutiveScaffold(List<Scaffold> scaffolds, int value) {
+        Scaffold scaffold = scaffoldGenerator.generate();
+        if (scaffolds.isEmpty()) {
+            scaffolds.add(scaffold);
+            return;
+        }
+        scaffold = validateConsecutiveScaffold(scaffolds, value, scaffold);
+        scaffolds.add(scaffold);
+    }
+
+    private static Scaffold validateConsecutiveScaffold(List<Scaffold> scaffolds, int j, Scaffold scaffold) {
+        if (scaffold == Scaffold.EXIST && scaffolds.get(j - 1) == Scaffold.EXIST) {
+            scaffold = Scaffold.NONE;
+        }
+        return scaffold;
     }
 }
