baa34fc434d64572116145cfaa1e1d8774f7a493
BackFoxx <backfoxxx@gmail.com>
2023-02-16 16:24:01 +0900

    feat: 사용자 재입력 요청 구현

--- src/main/java/controller/LadderGameController.java
+++ src/main/java/controller/LadderGameController.java
@@ -4,26 +4,49 @@ import domain.Ladder;
 import domain.Name;
 import domain.Names;
 import dto.GameDto;
+import util.RandomBooleanGenerator;
 import view.InputView;
 import view.OutputView;
 
 public class LadderGameController {
 
-    public void run(){
+    public void run() {
         Names names = getNames();
         int height = getHeight();
-        Ladder ladder = new Ladder();
+
+        Ladder ladder = buildLadder(names, height);
+
+        printResult(names, ladder);
+    }
+
+    private Ladder buildLadder(final Names names, final int height) {
+        Ladder ladder = new Ladder(new RandomBooleanGenerator());
         ladder.build(height, names.count());
+        return ladder;
+    }
+
+    private static void printResult(final Names names, final Ladder ladder) {
         OutputView.printResult(new GameDto(names, ladder));
     }
 
-    private Names getNames(){
-        OutputView.printRequestNames();
-        return new Names(Name.of(InputView.getNames()));
+
+    private Names getNames() {
+        try {
+            OutputView.printRequestNames();
+            return new Names(Name.of(InputView.getNames()));
+        } catch (IllegalArgumentException e) {
+            OutputView.printErrorMessage(e.getMessage());
+            return getNames();
+        }
     }
 
-    private int getHeight(){
-        OutputView.printRequestLadderHeight();
-        return InputView.getHeight();
+    private int getHeight() {
+        try {
+            OutputView.printRequestLadderHeight();
+            return InputView.getHeight();
+        } catch (IllegalArgumentException e) {
+            OutputView.printErrorMessage(e.getMessage());
+            return getHeight();
+        }
     }
 }
