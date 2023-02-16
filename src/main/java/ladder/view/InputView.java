2b813010add080a66c7cfcbed7491b004acc313a
Cyma-s <ssol0319@naver.com>
2023-02-16 14:22:54 +0900

    refactor: InputView 메서드를 static으로 변경, 입력 메시지 추가

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -8,12 +8,14 @@ import java.util.stream.Collectors;
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
-    public List<String> readNames() {
+    public static List<String> readNames() {
+        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
         String names = scanner.reset().nextLine();
         return Arrays.stream(names.split(",")).collect(Collectors.toList());
     }
 
-    public int readLadderHeight() {
+    public static int readLadderHeight() {
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
         return scanner.reset().nextInt();
     }
 }
