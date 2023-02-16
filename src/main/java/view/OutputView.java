54e4e9621b6a32672a44435e42345577845d7062
donghae <donghae1999@gmail.com>
2023-02-16 11:50:10 +0900

    refactor: Mesage enum 타입 기능 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,7 +1,6 @@
 package view;
 
 import model.Ladder;
-import model.LadderHeight;
 import model.Name;
 
 import java.util.List;
@@ -10,11 +9,10 @@ public class OutputView {
 
     private static final String PLAYER_NAME_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     private static final String LADDER_HEIGHT_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
-    private static final String START_LADDER = "   |";
-    private static final String CONNECT_LADDER = "-----|";
-    private static final String NO_CONNECT_LADDER = "     |";
+    private static final int MAXIMUM_PLAYER_NAME_Length = 5;
 
-    private static final int MAXIMUM_PLAYER_NAME = 5;
+    private static StringBuilder stringBuilder = new StringBuilder();
+
     public void printPlayerNamesMessage() {
             System.out.println(PLAYER_NAME_MESSAGE);
     }
@@ -24,32 +22,47 @@ public class OutputView {
     }
 
     public void printPlayerName( List<Name> names){
-        for(int index=0; index<names.size(); index++){
-            System.out.print(String.format("%"+ MAXIMUM_PLAYER_NAME +"s ",
-                    names.get(index).getName()));
-        }
+        names.forEach(name -> System.out.printf("%-"+ MAXIMUM_PLAYER_NAME_Length +"s", name.getName()));
         System.out.println("");
     }
 
     public void printLadder(int playerSize,Ladder ladder, int ladderHeight) {
         for(int row=0; row<ladderHeight;row++) {
-            printStartLadder();
+            stringBuilder.append(printStartLadder());
             printConnectLadder(playerSize, ladder, row);
+            stringBuilder.append(System.lineSeparator());
         }
+        System.out.println(stringBuilder);
     }
 
-    private void printStartLadder(){
-        System.out.print(START_LADDER);
+    private String printStartLadder(){
+        return Message.START_LADDER.getMessage();
     }
 
     private void printConnectLadder(int playerSize, Ladder ladder,int row){
         for (int column = 0; column < playerSize - 1; column++) {
             if (ladder.getLadderLine(row, column)) {
-                System.out.print(CONNECT_LADDER);
+                stringBuilder.append(Message.CONNECT_LADDER.getMessage());
             }
             else
-                System.out.print(NO_CONNECT_LADDER);
+                stringBuilder.append(Message.NO_CONNECT_LADDER.getMessage());
         }
-        System.out.println("");
+    }
+
+    public enum Message {
+        START_LADDER("   |"),
+        CONNECT_LADDER("-----|"),
+        NO_CONNECT_LADDER("     |");
+
+        private final String message;
+
+        Message(String move){
+            this.message = move;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
     }
 }
