2558412c1e4585eb749d3dfbbf2e8129e398704f
MoonJeWoong <munjin0201@naver.com>
2023-02-16 12:30:53 +0900

    feat: Players 객체 생성 기능 추가

---
+++ src/main/java/model/Players.java
@@ -0,0 +1,14 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class Players {
+
+    private final List<Player> players = new ArrayList<>();
+
+    public Players(Names names) {
+
+    }
+}
