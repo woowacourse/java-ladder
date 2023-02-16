bbf563605b3b8c8bf183abb30376caee0b8c039d
MoonJeWoong <munjin0201@naver.com>
2023-02-16 14:59:23 +0900

    feat: 전체 플레이어 이름 출력 기능 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,7 +1,15 @@
 package view;
 
+import model.Name;
+import model.Player;
+import model.Players;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class OutputView {
 
+    private static final int STARTING_INDEX_OF_RIGHT_FORMATTING = 1;
     private static final String PLAYER_NAME_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     private static final String LADDER_HEIGHT_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
 
@@ -11,5 +19,25 @@ public class OutputView {
 
     public void printLadderHeightMessage() {
         System.out.println(LADDER_HEIGHT_MESSAGE);
+    }
+
+    public void printAllPlayerNames(Players players) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(makeLeftFormattingFirstName(players));
+        stringBuilder.append(makeRightFormattingNamesFromSecond(players));
+        System.out.println(stringBuilder);
+    }
+
+    private String makeLeftFormattingFirstName(Players players) {
+        Name firstPlayerName = players.getPlayer(0).getName();
+        return String.format("%-5s ", firstPlayerName.getName());
+    }
+
+    private String makeRightFormattingNamesFromSecond(Players players) {
+        return IntStream.range(STARTING_INDEX_OF_RIGHT_FORMATTING, players.size())
+                .mapToObj(players::getPlayer)
+                .map(Player::getName)
+                .map(name -> String.format("%5s", name.getName()))
+                .collect(Collectors.joining(" "));
     }
 }
