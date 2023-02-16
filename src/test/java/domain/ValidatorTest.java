b93719fc662e6435473f468561527793f267b690
thdwoqor <celebarte01@gmail.com>
2023-02-16 14:22:47 +0900

    refactor : 사다리 높이 유효성 검증 테스트 작성

---
+++ src/test/java/domain/ValidatorTest.java
@@ -0,0 +1,24 @@
+package domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class ValidatorTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 11})
+    @DisplayName("사다리 높이가 1~10을 벗어나면 예외가 발생한다.")
+    void LadderHeightFailTest(int height) {
+        Assertions.assertThatThrownBy(() -> Validator.validateLadderHeight(height))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 10})
+    @DisplayName("사다리 높이가 1~10 사이면 정상적으로 수행된다.")
+    void LadderHeightSuccessTest(int height) {
+        Assertions.assertThatCode(() -> Validator.validateLadderHeight(height)).doesNotThrowAnyException();
+    }
+}
