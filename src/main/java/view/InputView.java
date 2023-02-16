dffdc64bbeeb3d350009624fece04354e318269a
ingpyo <juninpyo726@gmail.com>
2023-02-16 13:55:05 +0900

    refactor: ,로 구분하여 이름 분리 기능 추가 및 유효 검증 기능 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,10 +1,15 @@
 package view;
 
+import laddervalidate.HeightValidator;
+import laddervalidate.PlayerNameValidator;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
+    PlayerNameValidator playerNameValidator = new PlayerNameValidator();
+    HeightValidator heightValidator = new HeightValidator();
     private static final Scanner scanner = new Scanner(System.in);
     private static final String NAME_INPUT_REQUEST = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     private static final String DELIMITER = ",";
@@ -13,12 +18,22 @@ public class InputView {
     public List<String> readUserNames() {
         System.out.println(NAME_INPUT_REQUEST);
         String inputUserNames = scanner.nextLine();
+        List<String> playersNames = splitInputUserNames(inputUserNames);
+        playerNameValidator.checkDuplicatePlayers(playersNames);
+        playerNameValidator.checkPlayerNameLength(playersNames);
+        playerNameValidator.checkPlayerCount(playersNames);
+        return playersNames;
+    }
+
+    private List<String> splitInputUserNames(String inputUserNames) {
         return Arrays.asList(inputUserNames.split(DELIMITER));
     }
 
     public String readHeight() {
         System.out.println(HEIGHT_INPUT_REQUEST);
         String inputHeight = scanner.nextLine();
+        heightValidator.checkNumberMissMatch(inputHeight);
+        heightValidator.checkNumberRange(inputHeight);
         return inputHeight;
     }
 
