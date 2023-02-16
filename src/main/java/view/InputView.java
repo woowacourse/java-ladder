b3f12b0a890da1366287e93ba21c94c506e6bf2b
odo27 <odo27@odo27ui-MacBookAir.local>
2023-02-16 13:15:02 +0900

    refactor(InputView): 매직넘버 제거

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -10,12 +10,13 @@ public class InputView {
     private static final String READ_NAMES_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     private static final String READ_HEIGHT_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
     private static final String NOT_INTEGER_MESSAGE = "사다리의 높이는 숫자여야 합니다.";
+    private static final String NAME_DELIMITER = ",";
     private static final Scanner scanner = new Scanner(System.in);
 
     public static List<String> readNames() {
         System.out.println(READ_NAMES_MESSAGE);
         String line = scanner.next();
-        return Parser.parse(line, ",");
+        return Parser.parse(line, NAME_DELIMITER);
     }
 
     public static int readHeight() {
