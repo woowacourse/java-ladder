540ac65d5bdcd3e2af5fa950bbc02bdbf9323677
chaewon121 <cw121@naver.com>
2023-02-16 16:23:57 +0900

    feat: 입력받은 이름 예외 처리 기능 구현

--- src/main/java/ladder/domain/NamesSplitor.java
+++ src/main/java/ladder/domain/NamesSplitor.java
@@ -2,12 +2,48 @@ package ladder.domain;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class NamesSplitor {
-    public static List<String> split(String names){
+    private static final String NAMES_INPUT_FORM = "([a-zA-Z]{1,5})(,[a-zA-Z]{1,5})*";
 
+    public static List<String> split(String names){
+        validateNamesInputForm(names);
+        List<String> splitedNames = namesSplit(names);
+        validateRange(splitedNames);
+        validateDuplicateNames(splitedNames);
+        return splitedNames;
+    }
+
+    private static void validateRange(List<String> splitedNames) {
+        if (splitedNames.size() < 2 || splitedNames.size() > 100) {
+            throw new IllegalArgumentException("이름의 수가 2이상 100이하여야 합니다.");
+        }
+    }
+
+    private static void validateNamesInputForm(String names) {
+        Matcher matcher = Pattern.compile(NAMES_INPUT_FORM).matcher(names);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException("입력된 플레이어들의 이름 형식이 올바르지 않습니다.");
+        }
+    }
+
+    private static List<String> namesSplit(String names) {
         return Arrays.stream(names.split(","))
                 .collect(Collectors.toUnmodifiableList());
+    }
+
+    private static void validateDuplicateNames(List<String> splitedNames) {
+        if (deleteDuplicateNames(splitedNames) != splitedNames.size()) {
+            throw new IllegalArgumentException("중복된 이름은 입력할 수 없습니다.");
+        }
+    }
+
+    private static int deleteDuplicateNames(List<String> splitedNames) {
+        return (int) splitedNames.stream()
+                .distinct()
+                .count();
     }
 }
--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -12,7 +12,9 @@ public class InputView {
 
     public static String inputPeopleNames() {
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
-        return sc.nextLine();
+        String inputPeopleNames = sc.nextLine();
+        validateBlank(inputPeopleNames);
+        return inputPeopleNames;
     }
 
     public static int inputLadderHeight() {
