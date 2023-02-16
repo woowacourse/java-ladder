74ff550d38074c2259f6cfa75e7323b0239620f7
thdwoqor <celebarte01@gmail.com>
2023-02-16 15:45:25 +0900

    test : Height원시값 포장으로 인한 코드 변경

---
+++ src/test/java/domain/LaddersTest.java
@@ -0,0 +1,27 @@
+package domain;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+public class LaddersTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 11})
+    @DisplayName("사다리 높이가 1~10을 벗어나면 예외가 발생한다.")
+    void LadderHeightFailTest(int height) {
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> new Ladders(new Height(height)));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 10})
+    @DisplayName("사다리 높이가 1~10 사이면 정상적으로 수행된다.")
+    void LadderHeightSuccessTest(int height) {
+        assertThatCode(() -> new Ladders(new Height(height))).doesNotThrowAnyException();
+    }
+}
