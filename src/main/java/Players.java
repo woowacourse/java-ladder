c51aa17bd18764a81286be5a6116c9ec30e8fd2b
Miseong Kim <miseongk@naver.com>
2023-02-16 14:05:39 +0900

    feat: Player 객체를 갖는 Players 객체 생성 및 예외 처리

---
+++ src/main/java/Players.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Players {
+
+    private final List<Player> players;
+
+    public Players(String names) {
+        validateDelimiter(names);
+        List<String> splitNames = getSplitNames(names);
+        validateMoreThanOnePlayer(splitNames);
+        this.players = createPlayer(splitNames);
+    }
+
+    private List<String> getSplitNames(String names) {
+        return List.of(names.split("\\s*,\\s*"));
+    }
+
+    private void validateMoreThanOnePlayer(List<String> splitNames) {
+        if (splitNames.size() < 2) {
+            throw new IllegalArgumentException("[ERROR] 플레이어는 두 명 이상 입력되어야 합니다.");
+        }
+    }
+
+    private void validateDelimiter(String names) {
+        if (!names.contains(",")) {
+            throw new IllegalArgumentException("[ERROR] 구분자는 쉼표여야 합니다.");
+        }
+    }
+
+    private List<Player> createPlayer(List<String> splitNames) {
+        return splitNames.stream()
+                .map(Player::new)
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/test/java/PlayersTest.java
@@ -0,0 +1,37 @@
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PlayersTest {
+
+    @Test
+    @DisplayName("Players객체 생성 테스트")
+    void makePlayers() {
+        String names = "roy,poy,soy,koy";
+
+        assertDoesNotThrow(() -> new Players(names));
+    }
+
+    @Test
+    @DisplayName("구분자가 쉼표가 아닌 경우 예외 발생")
+    void validateDelimiter() {
+        String names = "roy|poy|soy|koy";
+
+        assertThatThrownBy(() -> new Players(names))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 구분자는 쉼표여야 합니다.");
+    }
+
+    @Test
+    @DisplayName("플레이어 수가 한명일 경우 예외 발생")
+    void validateMoreThanOnePlayer() {
+        String names = "roy,";
+
+        assertThatThrownBy(() -> new Players(names))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 플레이어는 두 명 이상 입력되어야 합니다.");
+    }
+}
