84fa42a9c5a0e210443d8936c90ee2550ce53dfc
woo-chang <clllickme@naver.com>
2023-02-16 10:57:44 +0900

    feat: 높이 검증 기능 구현
    Co-authored-by: 70825 <hello70825@gmail.com>

---
+++ src/main/java/ladder/domain/Height.java
@@ -0,0 +1,36 @@
+package ladder.domain;
+
+import java.text.MessageFormat;
+
+public class Height {
+
+    private static final int MINIMUM_HEIGHT = 2;
+    private static final int MAXIMUM_HEIGHT = 10;
+    private static final String INVALID_HEIGHT_ERROR_MESSAGE
+            = "높이는 " + MINIMUM_HEIGHT + "이상, " + MAXIMUM_HEIGHT + "이하여야 합니다. 현재 높이는 {0}입니다.";
+
+    private final int value;
+
+    public Height(final int value) {
+        validate(value);
+        this.value = value;
+    }
+
+    private void validate(final int value) {
+        if (hasShort(value) || hasLong(value)) {
+            throw new IllegalArgumentException(MessageFormat.format(INVALID_HEIGHT_ERROR_MESSAGE, value));
+        }
+    }
+
+    private boolean hasShort(final int value) {
+        return value < MINIMUM_HEIGHT;
+    }
+
+    private boolean hasLong(final int value) {
+        return value > MAXIMUM_HEIGHT;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
---
+++ src/test/java/ladder/domain/HeightTest.java
@@ -0,0 +1,28 @@
+package ladder.domain;
+
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.text.MessageFormat;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class HeightTest {
+
+    @ParameterizedTest(name = "입력: {0}")
+    @ValueSource(ints = {-5, 0, 1, 11, 100})
+    @DisplayName("높이가 2미만, 10초과이면 예외를 던진다.")
+    void checkInvalidHeight(final int value) {
+        assertThatThrownBy(() -> new Height(value))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(MessageFormat.format("높이는 2이상, 10이하여야 합니다. 현재 높이는 {0}입니다.", value));
+    }
+
+    @ParameterizedTest(name = "입력: {0}")
+    @ValueSource(ints = {2, 5, 10})
+    @DisplayName("높이는 2이상, 10이하여야 한다.")
+    void checkValidHeight(final int value) {
+        assertThatNoException().isThrownBy(() -> new Height(value));
+    }
+}
