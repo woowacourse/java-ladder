f006284671ac9efb693fdd3ebd15a472addb1946
yujamint <woawoa0513@gmail.com>
2023-02-16 15:48:06 +0900

    feat: 사다리 출력 형식 enum 형으로 분리

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -2,11 +2,15 @@ package view;
 
 import static domain.Name.BLANK;
 import static domain.Name.MAX_NAME_LENGTH;
+import static utils.LadderFormat.LADDER_COLUMN;
+import static utils.LadderFormat.NON_CONNECTION;
+import static utils.LadderFormat.CONNECTION;
 
 import domain.Ladder;
 import domain.Line;
 import domain.User;
 import domain.Users;
+import utils.LadderFormat;
 
 public class OutputView {
 
@@ -14,10 +18,6 @@ public class OutputView {
     private final String LADDER_HEIGHT_ENTER_NOTICE_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
     private final String FINAL_RESULT = "실행결과";
     private final char NEXT_LINE = '\n';
-
-    private final char LADDER_PRINT_FORMAT = '|';
-    private final String CONNECTION_LINE = "-----";
-    private final String NON_CONNECTION = "     ";
 
     private final int SECOND_USER_INDEX = 1;
 
@@ -51,7 +51,7 @@ public class OutputView {
     private void printLadder(Ladder ladder, int width) {
         StringBuilder result = new StringBuilder();
         for (Line line : ladder.getLines()) {
-            result.append(BLANK.repeat(width)).append(LADDER_PRINT_FORMAT);
+            result.append(BLANK.repeat(width)).append(LADDER_COLUMN);
             appendLine(result, line);
             result.append(NEXT_LINE);
         }
@@ -61,13 +61,13 @@ public class OutputView {
     private void appendLine(StringBuilder result, Line line) {
         for (Boolean point : line.getPoints()) {
             result.append(getConnectionStatus(point));
-            result.append(LADDER_PRINT_FORMAT);
+            result.append(LADDER_COLUMN);
         }
     }
 
-    private String getConnectionStatus(Boolean point) {
+    private LadderFormat getConnectionStatus(Boolean point) {
         if (point) {
-            return CONNECTION_LINE;
+            return CONNECTION;
         }
         return NON_CONNECTION;
     }
