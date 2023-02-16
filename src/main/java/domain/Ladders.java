8dd23f7e7b013517b318e75497e5b669213522fa
thdwoqor <celebarte01@gmail.com>
2023-02-16 11:55:21 +0900

    refactor : 띄어쓰기 추가

--- src/main/java/domain/Ladders.java
+++ src/main/java/domain/Ladders.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Ladders {
     private static final int LADDER_MIN_HEIGHT = 1;
     private static final int LADDER_MAX_HEIGHT = 10;
-    private static final String INVALID_LADDER_HEIGHT_MESSAGE ="사다리 높이는 1~10 사이 정수만 가능합니다.";
+    private static final String INVALID_LADDER_HEIGHT_MESSAGE = "사다리 높이는 1~10 사이 정수만 가능합니다.";
     private final int height;
     private final List<Ladder> ladders;
     private final LadderGenerator ladderGenerator;
