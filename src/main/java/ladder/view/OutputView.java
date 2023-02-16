8a5d0d66644fa3e05ec4954d8ded402db7925e94
kevstevie <jianna442@gmail.com>
2023-02-16 11:02:28 +0900

    refactor: String, 매직넘버 제거

--- src/main/java/ladder/view/OutputView.java
+++ src/main/java/ladder/view/OutputView.java
@@ -2,12 +2,22 @@ package ladder.view;
 
 import ladder.domain.*;
 
-import java.util.List;
 import java.util.regex.Pattern;
 
 public class OutputView {
 
-    private OutputView() {}
+    private static final String BLANK_SPACE = " ";
+    private static final int ONE_BLOCK_SIZE = 10;
+    private static final double INIT_SPAN = 0.0;
+    private static final String KOREAN_MATCH_REGEX = "[ㄱ-ㅎㅏ-ㅣ가-힣]";
+    private static final double KOREAN_SPAN = 1.5;
+    private static final double OTHER_SPAN = 1.0;
+    private static final String DELIMITER = "|";
+    private static final String LINE_COMPONENT = "-";
+    private static final int BLOCK_SIZE_EXCEPT_DELIMITER = 9;
+
+    private OutputView() {
+    }
 
     public static void printResult(Users users, Ladder ladder) {
         printUsersName(users);
@@ -17,25 +27,25 @@ public class OutputView {
 
     private static void printUsersName(Users users) {
         for (User user : users.getUsers()) {
-            System.out.printf(" ".repeat(10 - calculateBlank(user)));
+            System.out.print(BLANK_SPACE.repeat(ONE_BLOCK_SIZE - calculateBlank(user)));
             System.out.printf("%s", user.getName());
         }
     }
 
     private static int calculateBlank(User user) {
-        double userNameSpan = 0.0;
-        for(Character name : user.getName().toCharArray()){
-            userNameSpan+=userNameSpanSize(name);
+        double userNameSpan = INIT_SPAN;
+        for (Character name : user.getName().toCharArray()) {
+            userNameSpan += userNameSpanSize(name);
         }
-        return (int)(userNameSpan+0.5);
+        return (int) Math.round(userNameSpan);
     }
 
     private static double userNameSpanSize(Character name) {
         String hi = String.valueOf(name);
-        if(Pattern.matches("[ㄱ-ㅎㅏ-ㅣ가-힣]", hi)){
-            return 1.5;
+        if (Pattern.matches(KOREAN_MATCH_REGEX, hi)) {
+            return KOREAN_SPAN;
         }
-        return 1.0;
+        return OTHER_SPAN;
     }
 
 
@@ -47,8 +57,8 @@ public class OutputView {
     }
 
     private static void printFloor(Floor floor) {
-        System.out.print(" ".repeat(9));
-        System.out.print("|");
+        System.out.print(BLANK_SPACE.repeat(BLOCK_SIZE_EXCEPT_DELIMITER));
+        System.out.print(DELIMITER);
         for (Line line : floor.getLines()) {
             printLine(line);
         }
@@ -56,11 +66,12 @@ public class OutputView {
 
     private static void printLine(Line line) {
         if (line.isExist()) {
-            System.out.print("-".repeat(9));
-            System.out.print("|");
+            System.out.print(LINE_COMPONENT.repeat(BLOCK_SIZE_EXCEPT_DELIMITER));
+            System.out.print(DELIMITER);
             return;
         }
-        System.out.printf("         |");
+        System.out.print(BLANK_SPACE.repeat(BLOCK_SIZE_EXCEPT_DELIMITER));
+        System.out.print(DELIMITER);
     }
 
 }
