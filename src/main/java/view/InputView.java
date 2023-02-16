814ac878d80bbd415a457a863a2a5923ea357277
odo27 <odo27@odo27ui-MacBookAir.local>
2023-02-16 11:59:18 +0900

    refactor(InputView): 정수 검증 방식 변경

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -2,7 +2,6 @@ package view;
 
 import java.util.List;
 import java.util.Scanner;
-import java.util.regex.Pattern;
 
 import utils.Parser;
 
@@ -10,7 +9,7 @@ public class InputView {
 
     private static final String READ_NAMES_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     private static final String READ_HEIGHT_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
-    private static final Pattern NUMBER = Pattern.compile("^[0-9]+$");
+    private static final String NOT_INTEGER_MESSAGE = "사다리의 높이는 숫자여야 합니다.";
     private static final Scanner scanner = new Scanner(System.in);
 
     public static List<String> readNames() {
@@ -22,13 +21,10 @@ public class InputView {
     public static int readHeight() {
         System.out.println(READ_HEIGHT_MESSAGE);
         String line = scanner.next();
-        validateInteger(line);
-        return Integer.parseInt(line);
-    }
-
-    private static void validateInteger(String line) {
-        if (!NUMBER.matcher(line).matches()) {
-            throw new IllegalArgumentException("사다리의 높이는 자연수이어야 합니다.");
+        try {
+            return Integer.parseInt(line);
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException(NOT_INTEGER_MESSAGE);
         }
     }
 }
