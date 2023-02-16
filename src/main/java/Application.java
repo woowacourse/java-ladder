1f9f0680ccf36fc603b9a289784d7277491b56c9
BackFoxx <backfoxxx@gmail.com>
2023-02-16 15:36:39 +0900

    feat: 메인메서드 생성

---
+++ src/main/java/Application.java
@@ -0,0 +1,8 @@
+import controller.LadderGameController;
+
+public class Application {
+    public static void main(String[] args) {
+        LadderGameController game = new LadderGameController();
+        game.run();
+    }
+}
