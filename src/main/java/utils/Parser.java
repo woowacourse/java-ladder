c13c2c3ee27b195de0125e7b1a77967d3080067a
odo27 <odo27@odo27ui-MacBookAir.local>
2023-02-16 14:21:49 +0900

    refactor(Parser): 매직 넘버 제거

--- src/main/java/utils/Parser.java
+++ src/main/java/utils/Parser.java
@@ -6,8 +6,10 @@ import java.util.stream.Collectors;
 
 public class Parser {
 
+    private static final int SPLIT_LIMIT = -1;
+
     public static List<String> parse(String input, String delimiter) {
-        return Arrays.stream(input.split(delimiter, -1))
+        return Arrays.stream(input.split(delimiter, SPLIT_LIMIT))
                 .collect(Collectors.toList());
     }
 }
