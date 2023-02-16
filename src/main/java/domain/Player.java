eb082521e32a3ce53bea3f1e1bd6cc02a82289ba
eunbii0213 <eunbii0213@naver.com>
2023-02-16 13:49:11 +0900

    refactor: Player null 검증 로직 수정

--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -1,42 +1,37 @@
 package domain;
 
-import java.util.ArrayList;
-
 public class Player {
-    public static final String NAME_LENGTH_ERROR_MESSAGE = "[ERROR] 플레이어의 이름은 최대 5글자까지입니다.";
-    public static final String NAME_NULL_ERROR_MESSAGE = "[ERROR] 플레이어의 이름은 null이면 안됩니다.";
+    public static final String NAME_LENGTH_ERROR_MESSAGE = "[ERROR] 플레이어의 이름은 1이상 5글자 이하입니다.";
     public static final String NAME_BLANK_ERROR_MESSAGE = "[ERROR] 플레이어의 이름은 빈칸이면 안됩니다.";
     public static final int NAME_MAX_LENGTH = 5;
+    public static final int NAME_MIN_LENGTH = 1;
 
-    private String name; //judy 4글자. 5-4 = 1.  Players.add();
+    private String name;
 
     public Player(String name) {
         this.name = validateName(name);
     }
 
     private String validateName(String name) {
-        validateNameIsNull(name);
         validateNameIsBlank(name);
         validateNameLength(name);
         return name;
-    }
-
-    private void validateNameLength(String name) {
-        if (name.length() > NAME_MAX_LENGTH) {
-            throw new IllegalArgumentException(NAME_LENGTH_ERROR_MESSAGE);
-        }
-    }
-
-    private void validateNameIsNull(String name) {
-        if (name.equals(null)) {
-            throw new IllegalArgumentException(NAME_NULL_ERROR_MESSAGE);
-        }
     }
 
     private void validateNameIsBlank(String name) {
         if (name.isBlank()) {
             throw new IllegalArgumentException(NAME_BLANK_ERROR_MESSAGE);
         }
+    }
+
+    private void validateNameLength(String name) {
+        if (isLengthError(name)) {
+            throw new IllegalArgumentException(NAME_LENGTH_ERROR_MESSAGE);
+        }
+    }
+
+    private static boolean isLengthError(String name) {
+        return name.length() > NAME_MAX_LENGTH || name.length() < NAME_MIN_LENGTH;
     }
 
     public String getName() {
