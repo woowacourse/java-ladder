1bbc386fa86ad5c772c000646d2a548804d549ee
yoondgu <doyou2ing@naver.com>
2023-02-16 14:21:18 +0900

    fix: 출력기능에 문구 추가
    Co-authored-by: choi-jjunho <junho5336@gmail.com>

--- src/main/java/ladder/view/OutputView.java
+++ src/main/java/ladder/view/OutputView.java
@@ -19,12 +19,14 @@ public class OutputView {
     private static final String VERTICAL = "|";
     private static final String HORIZON = "-----";
     private static final String NONE = "     ";
+    private static final String MESSAGE_GAME_RESULT = "실행 결과";
 
     public static void showGameResult(List<String> players, List<Line> lines) {
         String result = players.stream()
                 .map(name -> String.format("%5s", name))
                 .collect(Collectors.joining());
 
+        System.out.println(MESSAGE_GAME_RESULT);
         System.out.println(result);
         lines.stream()
                 .map(OutputView::extractLine)
