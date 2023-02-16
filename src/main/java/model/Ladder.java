ada241f8935ae8237d8c87d91f9557b8171b5cc6
MoonJeWoong <munjin0201@naver.com>
2023-02-16 13:27:51 +0900

    feat: 사다리 높이에 맞게 라인 생성 기능 추가

--- src/main/java/model/Ladder.java
+++ src/main/java/model/Ladder.java
@@ -1,10 +1,29 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class Ladder {
 
-    private LadderHeight ladderHeight;
+    private final List<Line> lines = new ArrayList<>();
 
-    public Ladder(LadderHeight ladderHeight) {
+    private final Players players;
+    private final LadderHeight ladderHeight;
+
+    public Ladder(Players players, LadderHeight ladderHeight) {
+        this.players = players;
         this.ladderHeight = ladderHeight;
+        makeLadderLines(players.size(), ladderHeight.getHeight());
+    }
+
+    public Line getLine(int index) {
+        return lines.get(index);
+    }
+
+    private void makeLadderLines(int playerSize, int height) {
+        IntStream.range(0, height).forEach(
+                (index) -> lines.add(new Line(new RandomPointGenerator(), playerSize))
+        );
     }
 }
