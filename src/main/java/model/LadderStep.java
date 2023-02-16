0735ecb9b74ba9ece59e10599a05be90bc5b454a
MoonJeWoong <munjin0201@naver.com>
2023-02-16 17:38:06 +0900

    docs: 사다리의 폭 생성 관련 사항 수정 및 주석 명시
    세부사항
    - 사다리의 폭은 입력될 수 있는 사람 이름의 최대 길이로 고정한다.

--- src/main/java/model/LadderStep.java
+++ src/main/java/model/LadderStep.java
@@ -1,11 +1,11 @@
 package model;
 
 public enum LadderStep {
+
+    //첫 STEP 길이는 플레이어 이름 길이 제한, 나머지 STEP 길이는 플레이어 이름 길이 제한 + 1로 설정한다.
     FIRST_STEP(String.format("%5s","    |")),
     EMPTY_STEP(String.format("%6s","     |")),
     EXIST_STEP(String.format("%6s","-----|"));
-
-    private static final int LIMIT_NAME_LENGTH = 5;
 
     private final String step;
 
