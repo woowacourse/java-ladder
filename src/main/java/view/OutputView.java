e65cac85e3a61e9660d6de5b00a2155e1f8f2057
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-02-16 16:09:23 +0900

    feat: 게임 결과를 출력합니다. - 게임 참가자 이름을 가운데 정렬하여 출력 - 사다리 생성 결과 출력 - view.OutputView

---
+++ src/main/java/view/OutputView.java
@@ -0,0 +1,85 @@
+package view;
+
+import domain.Direction;
+import domain.Name;
+import domain.PlayerNames;
+import domain.Point;
+import domain.ladder.Ladder;
+
+public class OutputView {
+
+    private static final String BLANK = " ";
+    private static final int BRIDGE_LENGTH = 5;
+    private static final String EMPTY_BRIDGE = BLANK.repeat(BRIDGE_LENGTH);
+    private static final String BRIDGE = "-".repeat(BRIDGE_LENGTH);
+    private static final String LINE_COMPONENT = "|";
+    private static final String LADDER_PREFIX = BLANK.repeat(2);
+    private static final int NAME_BOX_SIZE_MAX = 5;
+
+    public void printResultPrefix() {
+        System.out.println("실행결과");
+    }
+
+    public void printPlayerNames(PlayerNames playerNames) {
+        for (Name playerName : playerNames.getNames()) {
+            System.out.print(makeName(playerName) + BLANK);
+        }
+        System.out.println();
+    }
+
+    public String makeName(Name playerName) {
+        String name = playerName.getName();
+        String makeName = name;
+
+        int index = NAME_BOX_SIZE_MAX;
+        while (name.length() != index) {
+            makeName = appendSpace(makeName, index);
+            index -= 1;
+        }
+
+        return makeName;
+    }
+
+    private String appendSpace(String makeName, int i) {
+        if (isFirstAppendable(i)) {
+            return BLANK + makeName;
+        }
+
+        return makeName + BLANK;
+    }
+
+    private boolean isFirstAppendable(int i) {
+        return i % 2 == 0;
+    }
+
+    public void printResult(Ladder ladder) {
+        int lineAmount = ladder.getLines().size();
+        for (int pointAt = 0; pointAt < ladder.getHeightSize(); pointAt++) {
+            printLadderPrefix();
+            printSingleLadder(ladder, lineAmount, pointAt);
+            System.out.println();
+        }
+    }
+
+    private void printSingleLadder(Ladder ladder, int lineAmount, int pointAt) {
+        for (int lineAt = 0; lineAt < lineAmount; lineAt++) {
+            Point point = ladder.getLines().get(lineAt).getPoints().get(pointAt);
+            printPointDirection(point);
+        }
+    }
+
+    private void printPointDirection(Point point) {
+        System.out.print(LINE_COMPONENT);
+        if (point.matchDirection(Direction.RIGHT_DOWN)) {
+            System.out.print(BRIDGE);
+            return;
+        }
+        System.out.print(EMPTY_BRIDGE);
+    }
+
+    private void printLadderPrefix() {
+        System.out.print(LADDER_PREFIX);
+    }
+
+
+}
