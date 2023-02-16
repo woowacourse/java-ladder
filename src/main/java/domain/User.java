f69031a6a6458b9735e14043418d3fa5c625e99b
thdwoqor <celebarte01@gmail.com>
2023-02-16 11:30:30 +0900

    refactor : 검증 함수 분리

--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -1,15 +1,26 @@
 package domain;
 
 public class User {
+    private static final int USER_NAME_MIN_LENGTH = 1;
+    private static final int USER_NAME_MAX_LENGTH = 5;
     private final String name;
 
     public User(String name) {
-        if (1 > name.length() || 5 < name.length()) {
+        validateNameLength(name);
+        validateBlankName(name);
+
+        this.name = name;
+    }
+
+    private void validateNameLength(String name) {
+        if (USER_NAME_MIN_LENGTH > name.length() || USER_NAME_MAX_LENGTH < name.length()) {
             throw new IllegalArgumentException("이름은 1~5 글자 입니다.");
         }
-        if (name.isBlank()){
+    }
+
+    private void validateBlankName(String name) {
+        if (name.isBlank()) {
             throw new IllegalArgumentException("이름은 공백으로만 이루어지면 안됩니다.");
         }
-        this.name = name;
     }
 }
