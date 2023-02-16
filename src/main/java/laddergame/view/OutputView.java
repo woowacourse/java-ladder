e5d363132173df74ccaeca6bc92a82b8c5053be3
jiwon <ljwon77@daum.net>
2023-02-16 16:46:02 +0900

    feat: 사다리 메시지를 만드는 로직을 수정한다

--- src/main/java/laddergame/view/OutputView.java
+++ src/main/java/laddergame/view/OutputView.java
@@ -1,8 +1,10 @@
 package laddergame.view;
 
+import laddergame.domain.rung.Rung;
 import laddergame.domain.rung.Rungs;
 
 import java.util.List;
+import java.util.StringJoiner;
 import java.util.stream.Collectors;
 
 import static laddergame.view.message.LadderMessage.*;
@@ -14,32 +16,45 @@ public class OutputView {
     }
 
     public void printParticipants(final List<String> participantNames) {
-        String paddedParticipantNames = participantNames.stream()
-                .map(this::getPaddedName)
-                .collect(Collectors.joining());
-        System.out.println(paddedParticipantNames.trim());
+        String paddedParticipantNames = makePaddedParticipantNames(participantNames);
+        print(paddedParticipantNames.trim());
     }
 
     public void printLadder(final List<Rungs> ladder, final List<String> participantNames) {
-        int firstNameLength = participantNames.get(0).length();
-        StringBuilder sb = new StringBuilder();
+        int firstNameLength = getLengthOfFirstParticipantName(participantNames);
+        String ladderMessage = makeLadderMessage(ladder, firstNameLength);
+        print(ladderMessage);
+    }
 
-        ladder.stream()
-                .map(Rungs::getRungs)
-                .forEach(rungs ->
-                {
-                    sb.append(LADDER_PADDING.getMessage().repeat(firstNameLength));
-                    String result = rungs.stream()
-                            .map(rung -> makeRungMessage(rung.isExistence()))
-                            .collect(Collectors.joining(LADDER_FRAME.getMessage(), LADDER_FRAME.getMessage(), LADDER_FRAME.getMessage()));
-                    sb.append(result).append("\n");
-                });
-
-        System.out.println(sb);
+    private String makePaddedParticipantNames(final List<String> participantNames) {
+        return participantNames.stream()
+                .map(this::getPaddedName)
+                .collect(Collectors.joining());
     }
 
     private String getPaddedName(final String participantName) {
         return String.format("%6s", participantName);
+    }
+
+    private int getLengthOfFirstParticipantName(final List<String> participantNames) {
+        String firstParticipantName = participantNames.get(0);
+        return firstParticipantName.length();
+    }
+
+    private String makeLadderMessage(final List<Rungs> ladder, final int firstNameLength) {
+        return ladder.stream()
+                .map(Rungs::getRungs)
+                .map(rungs -> {
+                    StringJoiner ladderMessage = new StringJoiner("", LADDER_PADDING.getMessage().repeat(firstNameLength), "\n");
+                    ladderMessage.add(makeRungsMessage(rungs));
+                    return ladderMessage.toString();
+                }).collect(Collectors.joining());
+    }
+
+    private String makeRungsMessage(final List<Rung> rungs) {
+        return rungs.stream()
+                .map(rung -> makeRungMessage(rung.isExistence()))
+                .collect(Collectors.joining(LADDER_FRAME.getMessage(), LADDER_FRAME.getMessage(), LADDER_FRAME.getMessage()));
     }
 
     private String makeRungMessage(final boolean isExistence) {
