56924ebf5b698871a7afb40646df77f20320fdb7
thdwoqor <celebarte01@gmail.com>
2023-02-16 11:38:55 +0900

    refactor : 검증 예외 메시지 분리

--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -3,6 +3,8 @@ package domain;
 public class User {
     private static final int USER_NAME_MIN_LENGTH = 1;
     private static final int USER_NAME_MAX_LENGTH = 5;
+    private static final String INVALID_NAME_LENGTH_MESSAGE = "이름은 1~5 글자만 가능합니다.";
+    private static final String INVALID_NAME_BLANK_MESSAGE = "이름은 공백으로만 이루어지면 안됩니다.";
     private final String name;
 
     public User(String name) {
@@ -14,13 +16,13 @@ public class User {
 
     private void validateNameLength(String name) {
         if (USER_NAME_MIN_LENGTH > name.length() || USER_NAME_MAX_LENGTH < name.length()) {
-            throw new IllegalArgumentException("이름은 1~5 글자 입니다.");
+            throw new IllegalArgumentException(INVALID_NAME_LENGTH_MESSAGE);
         }
     }
 
     private void validateBlankName(String name) {
         if (name.isBlank()) {
-            throw new IllegalArgumentException("이름은 공백으로만 이루어지면 안됩니다.");
+            throw new IllegalArgumentException(INVALID_NAME_BLANK_MESSAGE);
         }
     }
 }
