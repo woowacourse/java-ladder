738c1da9c6992fed11f3462fec309621183854cd
shin-mallang <huipulci1@naver.com>
2023-02-16 13:52:24 +0900

    refactor: 사다리 출력 로직 변경

--- src/main/java/domain/Scaffold.java
+++ src/main/java/domain/Scaffold.java
@@ -2,16 +2,7 @@ package domain;
 
 public enum Scaffold {
 
-    EXIST("-----"),
-    NONE("     ");
-
-    private final String status;
-
-    Scaffold(final String status) {
-        this.status = status;
-    }
-
-    public String getStatus() {
-        return this.status;
-    }
+    EXIST,
+    NONE,
+    ;
 }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,67 +1,65 @@
 package view;
 
 import domain.Ladder;
+import domain.Line;
 import domain.Name;
+import domain.Scaffold;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class OutputView {
 
     private static final String BAR = "|";
+    private static final String EXIST_SCAFFOLD = "-----";
+    private static final String NONE_SCAFFOLD = "     ";
+    private static final String BLANK = " ";
+    private static final String EMPTY = "";
+    private static final String FIRST_NAME_FORMAT = "%-6s";
+    private static final String DEFAULT_NAME_FORMAT = "%5s";
+    private static final Map<Scaffold, String> SCAFFOLD_STRING_MAP;
 
-    private static final StringBuilder stringBuilder = new StringBuilder();
+    static {
+        SCAFFOLD_STRING_MAP = new HashMap<>();
+        SCAFFOLD_STRING_MAP.put(Scaffold.EXIST, EXIST_SCAFFOLD);
+        SCAFFOLD_STRING_MAP.put(Scaffold.NONE, NONE_SCAFFOLD);
+    }
 
-    /**
-     * pobi  honux crong   jk
-     * |-----|     |-----|
-     * |     |-----|     |
-     * |-----|     |     |
-     * |     |-----|     |
-     * |-----|     |-----|
-     */
     public static void printResult(final Ladder ladder, final List<Name> names) {
         printName(names);
-        int length = names.get(0).getValue().length();
-        String blank = " ".repeat(length);
-        for (int i = 0; i < ladder.getHeight(); i++) {
-            stringBuilder.append(blank);
-            stringBuilder.append(BAR);
-            for (int j = 0; j < ladder.getWidth(); j++) {
-                stringBuilder.append(ladder.getLines().get(i).getScaffolds().get(j).getStatus() + BAR);
-                String s = stringBuilder.toString();
-                System.out.print(s);
-                stringBuilder.delete(0, s.length());
-            }
-            System.out.println();
-        }
+        printLadder(ladder, names);
     }
 
     public static void printName(final List<Name> names) {
-        List<String> makeNames = new ArrayList<>();
         List<String> nameValues = names.stream().map(Name::getValue).collect(Collectors.toList());
-        String remove = nameValues.remove(0);
-        int addBlankSize = 5 - remove.length();
-        String blank = " ";
-        String result = "";
-        for (int i = 0; i < addBlankSize; i++) {
-            result += blank;
-        }
-        String firstName = remove + blank;
-        makeNames.add(firstName);
+        String firstName = String.format(FIRST_NAME_FORMAT, nameValues.remove(0));
+        System.out.println(makeNameFormat(nameValues, firstName));
+    }
 
-        for (String name : nameValues) {
-            addBlankSize = 5 - name.length();
-            result = "";
-            for (int i = 0; i < addBlankSize; i++) {
-                result += blank;
-            }
-            makeNames.add(result + name);
-        }
-        for (int i = 0; i < makeNames.size(); i++) {
-            System.out.print(makeNames.get(i) + " ");
-        }
-        System.out.println();
+    private static String makeNameFormat(final List<String> nameValues, final String firstName) {
+        return nameValues.stream().map(it -> String.format(DEFAULT_NAME_FORMAT, it))
+                .collect(Collectors.joining(BLANK, firstName, EMPTY));
+    }
+
+    private static void printLadder(final Ladder ladder, final List<Name> names) {
+        int length = names.get(0).getValue().length();
+        String prefixBlank = BLANK.repeat(length);
+        String ladderFormat = getLadderFormat(ladder, prefixBlank);
+        System.out.println(ladderFormat);
+    }
+
+    private static String getLadderFormat(final Ladder ladder, final String prefixBlank) {
+        return ladder.getLines().stream()
+                .map(line -> getLineFormat(prefixBlank, line)
+                ).collect(Collectors.joining("\n"));
+    }
+
+    private static String getLineFormat(final String prefixBlank, final Line line) {
+        return line.getScaffolds()
+                .stream()
+                .map(SCAFFOLD_STRING_MAP::get)
+                .collect(Collectors.joining(BAR, prefixBlank + BAR, BAR));
     }
 }
