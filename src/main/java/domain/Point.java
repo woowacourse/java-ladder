d5c471b575732e95cbb264b4e17f24b726d4430d
kim0914 <kosa0914@naver.com>
2023-02-16 11:28:01 +0900

    feat(Point): flag 값에 해당하는 포인트 객체를 반환하는 메서드 추가

---
+++ src/main/java/domain/Point.java
@@ -0,0 +1,28 @@
+package domain;
+
+import java.util.Arrays;
+
+public enum Point {
+
+    TRUE(true, "-----"),
+    FALSE(false, "     ");
+
+    private final Boolean flag;
+    private final String message;
+
+    Point(Boolean flag, String message) {
+        this.flag = flag;
+        this.message = message;
+    }
+
+    public static Point getPoint(boolean flag) {
+        return Arrays.stream(values())
+                .filter(point -> point.flag == flag)
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("일치하는 객체가 존재하지 않습니다."));
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
