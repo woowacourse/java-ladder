f17388f74a11f273e345e71a93329f8ac0c7165e
iamjooon2 <iamjooon2@gmail.com>
2023-02-16 19:00:22 +0900

    refactor: inputView 문자열에 매직넘버 적용

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -6,19 +6,22 @@ import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
+    private static final String INPUT_NAMES = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    private static final String INPUT_HEIGHT = "최대 사다리 높이는 몇 개인가요?";
 
     private static final String DELIMITER = ",";
+
     private static final Scanner scanner = new Scanner(System.in);
 
     public static List<String> inputNames() {
-        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
+        System.out.println(INPUT_NAMES);
         String input = scanner.nextLine();
         String[] split = input.split(DELIMITER);
         return new ArrayList<>(Arrays.asList(split));
     }
 
     public static int inputHeight() {
-        System.out.println("최대 사다리 높이는 몇 개인가요?");
+        System.out.println(INPUT_HEIGHT);
         String input = scanner.nextLine();
         validateInt(input);
         return Integer.parseInt(input);
