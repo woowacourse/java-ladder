cc74074a543dbbb94bb0764d33f284d0d0013f27
apptie <appti@naver.com>
2023-02-16 12:56:10 +0900

    test: Names를 LadderMaker에서 관리하면서 발생한 테스트 케이스 수정

--- src/test/java/model/LadderMakerTest.java
+++ src/test/java/model/LadderMakerTest.java
@@ -2,22 +2,20 @@ package model;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 import strategy.FixedPassGenerator;
-
 
 class LadderMakerTest {
 
     private final LadderMaker ladderMaker = new LadderMaker(new FixedPassGenerator(false));
 
     @Test
-    void initLadder_메소드는_Height와_참가자_수를_입력하면_Ladder를_초기화_한다() {
-        assertThatCode(() -> ladderMaker.initLadder(new Height(3), 5)).doesNotThrowAnyException();
+    void initLadder_메소드는_Height와_참가자_수를_입력하면_ladder를_초기화_한다() {
+        assertThatCode(() -> ladderMaker.initLadder(new Height(3), 5))
+                .doesNotThrowAnyException();
     }
 
     @Test
@@ -33,24 +31,18 @@ class LadderMakerTest {
     }
 
     @Test
-    void generateNames_메소드는_컬렉션_내의_이름_수만큼_NAME을_만들어_NAMES를_생성한다() {
-        Names names = ladderMaker.generateNames(List.of("pobi", "crong", "honux", "jk"));
-
-        assertThat(names.getTotalParticipantSize()).isSameAs(4);
+    void initParticipants_메소드는_참가자의_이름을_전달하면_participants를_초기화한다() {
+        assertThatCode(() -> ladderMaker.initParticipants(List.of("pobi", "honux", "crong", "jk")))
+                .doesNotThrowAnyException();
     }
-
+    
     @Test
-    void 컬렉션_내_사람이름이_5글자_초과면_예외가_발생한다() {
-        assertThatThrownBy(() -> ladderMaker.generateNames(List.of("pobiaaa")))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("이름은 최대 5글자까지만 가능합니다.");
-    }
+    void getParticipantsName_메소드는_참가자의_이름을_반환한다() {
+        List<String> participantsName = List.of("pobi", "honux", "crong", "jk");
+        ladderMaker.initParticipants(participantsName);
+        Names names = ladderMaker.getParticipantsName();
 
-    @ParameterizedTest
-    @ValueSource(strings = {""," ","   ","         "})
-    void 컬렉션_내_참가자_이름이_공백이면_예외가_발생한다(String name) {
-        assertThatThrownBy(() -> ladderMaker.generateNames(List.of(name)))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("이름은 공백일 수 없습니다.");
+        assertThat(names.getNames().stream().map(Name::getName).collect(Collectors.toList()))
+                .containsExactly(participantsName.toArray(new String[0]));
     }
 }
