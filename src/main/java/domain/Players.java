600a1032d4e302ecb6b0e494d6b845e17a9eeb84
ingpyo <juninpyo726@gmail.com>
2023-02-16 11:34:38 +0900

    feat(Players): 출력의 필요한 getter 기능 추가

--- src/main/java/domain/Players.java
+++ src/main/java/domain/Players.java
@@ -1,19 +1,29 @@
+package domain;
+
 import java.util.List;
 
 public class Players {
     private final List<String> players;
 
-    Players(List<String> players) {
+    public Players(List<String> players) {
         checkPlayerCount(players);
         checkPlayerNameLength(players);
         checkDuplicatePlayers(players);
+        this.players = players;
     }
 
     public int getMaxPlayerNameLength() {
         return players.stream().mapToInt(String::length).max().orElse(0);
-
-
     }
+
+    public int getPlayersCount() {
+        return players.size();
+    }
+
+    public List<String> getPlayers() {
+        return players;
+    }
+
     private void checkPlayerCount(List<String> players) {
         if (players.size() < 2 || players.size() > 12) {
             throw new IllegalArgumentException();
