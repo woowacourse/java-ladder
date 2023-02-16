0db41ff1c0dbb60ab2e591d5da8d3485809b9210
hyunseo <goatsfish@gmail.com>
2023-02-16 15:17:07 +0900

    feat: InputView 이름, 높이 입력 시 안내문구 출력 기능 추가

--- src/main/java/laddergame/view/InputView.java
+++ src/main/java/laddergame/view/InputView.java
@@ -7,10 +7,14 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static laddergame.messsages.ViewMessages.ANNOUNCE_READ_HEIGHT;
+import static laddergame.messsages.ViewMessages.ANNOUNCE_READ_NAMES;
+
 public class InputView {
     private final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
     public List<String> readNames() {
+        System.out.println(ANNOUNCE_READ_NAMES.getMessage());
         try {
             return Arrays.stream(reader.readLine().split(","))
                     .collect(Collectors.toList());
@@ -20,6 +24,7 @@ public class InputView {
     }
 
     public int readHeight() {
+        System.out.println(ANNOUNCE_READ_HEIGHT.getMessage());
         try {
             return Integer.parseInt(reader.readLine());
         } catch (IOException e) {
