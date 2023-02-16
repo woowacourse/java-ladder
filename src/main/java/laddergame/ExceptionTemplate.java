08ed1206ff1f4d6e098b17d541b60f2af907f130
hyunseo <goatsfish@gmail.com>
2023-02-16 13:32:11 +0900

    feat: 반복 템플릿 구현

---
+++ src/main/java/laddergame/ExceptionTemplate.java
@@ -0,0 +1,18 @@
+package laddergame;
+
+import java.util.function.Supplier;
+
+public class ExceptionTemplate {
+
+    private ExceptionTemplate() {}
+
+    public static  <T> T repeat(Supplier<T> supplier) {
+        while (true) {
+            try {
+                return supplier.get();
+            } catch (RuntimeException ignored) {
+                /* IGNORED */
+            }
+        }
+    }
+}
