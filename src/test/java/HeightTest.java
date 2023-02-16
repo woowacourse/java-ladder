778f1b2bcd0032bd85687cd7d60625f3c1a3cd66
jjongwa <troas96@naver.com>
2023-02-16 11:24:38 +0900

    refactor: 사다리 높이 검증 테스트 리팩터링

--- src/test/java/HeightTest.java
+++ src/test/java/HeightTest.java
@@ -1,7 +1,7 @@
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
-import org.assertj.core.api.Assertions;
+import domain.Height;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -10,8 +10,6 @@ import org.junit.jupiter.params.provider.ValueSource;
 import validate.InputVerifier;
 
 public class HeightTest {
-    InputVerifier inputVerifier = new InputVerifier();
-
     @Nested
     @DisplayName("높이 형식 검증 테스트")
     class HeightFormatTest {
@@ -19,7 +17,7 @@ public class HeightTest {
         @ParameterizedTest
         @ValueSource(strings = {"1#", "12a", "일23", "1 2", " "})
         void shouldFailHeightWithNotNumber(String heightInput) {
-            assertThatThrownBy(() -> inputVerifier.validateHeightInput(heightInput)).isInstanceOf(
+            assertThatThrownBy(() -> new Height(heightInput)).isInstanceOf(
                             IllegalArgumentException.class)
                     .hasMessageContaining(InputVerifier.HEIGHT_FORMAT_ERROR_MESSAGE);
         }
@@ -27,7 +25,7 @@ public class HeightTest {
         @DisplayName("사다리 높이 입력에 아무 값을 입력하지 않으면 실패한다.")
         @Test
         void shouldFailHeightWithNothing() {
-            assertThatThrownBy(() -> inputVerifier.validateHeightInput("")).isInstanceOf(
+            assertThatThrownBy(() -> new Height("")).isInstanceOf(
                             IllegalArgumentException.class)
                     .hasMessageContaining(InputVerifier.INPUT_NOTHING_ERROR_MESSAGE);
         }
@@ -40,7 +38,7 @@ public class HeightTest {
         @ParameterizedTest
         @ValueSource(strings = {"-1", "-5", "-100"})
         void shouldFailHeightWithNegativeNumber(String heightInput) {
-            assertThatThrownBy(() -> inputVerifier.validateHeightInput(heightInput)).isInstanceOf(
+            assertThatThrownBy(() -> new Height(heightInput)).isInstanceOf(
                             IllegalArgumentException.class)
                     .hasMessageContaining(InputVerifier.HEIGHT_FORMAT_ERROR_MESSAGE);
         }
@@ -49,7 +47,7 @@ public class HeightTest {
         @ParameterizedTest
         @ValueSource(strings = {"0", "3", "10"})
         void shouldSuccessHeightCorrectBoundary(String heightInput) {
-            assertDoesNotThrow(() -> inputVerifier.validateHeightInput(heightInput));
+            assertDoesNotThrow(() -> new Height(heightInput));
         }
     }
 }
