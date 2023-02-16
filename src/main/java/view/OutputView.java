da75d17472143bdfaab1bf080ccf194430d32d2c
eunbii0213 <eunbii0213@naver.com>
2023-02-16 14:04:01 +0900

    fix : 플레이어 출력 메소드 오류 수정

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,13 +1,25 @@
 package view;
 
+import domain.BlockType;
+import domain.Line;
 import domain.Players;
 
 import java.util.List;
 
 public class OutputView {
-
-    public static final String BOTH_ENDS_FORMAT = "%-5s";
+    public static final String FIRST_FORMAT = "%-5s";
     public static final String MIDDLE_FORMAT = "%6s";
+    public static final String LAST_FORMAT = "%5s";
+
+    private static void middlePlayersPrint(List<String> names, int index) {
+        if (isMiddlePlayer(names, index)) {
+            System.out.print(String.format(MIDDLE_FORMAT, names.get(index)));
+        }
+    }
+
+    private static boolean isMiddlePlayer(List<String> names, int index) {
+        return 0 < index && index < names.size() - 1;
+    }
 
     public void printNames(Players players) {
         List<String> names = players.getPlayersName();
@@ -18,22 +30,23 @@ public class OutputView {
         }
     }
 
-    private void firstPlayerPrint(List<String> names, int i) {
-        if (i == 0) {
-            System.out.print(String.format(BOTH_ENDS_FORMAT, names.get(i)));
+    private void firstPlayerPrint(List<String> names, int index) {
+        if (isFirstPlayer(index)) {
+            System.out.print(String.format(FIRST_FORMAT, names.get(index)));
         }
     }
 
-    private void lastPlayerPrint(List<String> names, int i) {
-        if (i == names.size() - 1) {
-            System.out.print(String.format(BOTH_ENDS_FORMAT, names.get(i)));
+    private static boolean isFirstPlayer(int i) {
+        return i == 0;
+    }
+
+    private void lastPlayerPrint(List<String> names, int index) {
+        if (isLastPlayer(names, index)) {
+            System.out.print(String.format(LAST_FORMAT, names.get(index)));
         }
     }
 
-    private static void middlePlayersPrint(List<String> names, int i) {
-        if (0 < i && i < names.size() - 1) {
-            System.out.print(String.format(MIDDLE_FORMAT, names.get(i)));
-        }
+    private static boolean isLastPlayer(List<String> names, int index) {
+        return index == names.size() - 1;
     }
-
 }
