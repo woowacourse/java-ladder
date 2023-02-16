39cc8e4c55e6d32876bc59ad300ff6c95c23a9f8
echo <echo.backend@gmail.com>
2023-02-16 15:42:15 +0900

    refactor(LadderHeight): add ladder height validation

--- src/main/java/domain/LadderHeight.java
+++ src/main/java/domain/LadderHeight.java
@@ -1,14 +1,22 @@
 package domain;
 
 public class LadderHeight {
+	private final static String HEIGHT_ERROR_MSG = "사다리의 높이는 1 이상이어야 합니다.";
 
 	private final int height;
 
 	public LadderHeight(final int height) {
+		validateHeight(height);
 		this.height = height;
 	}
 
 	public int getHeight() {
 		return height;
+	}
+
+	private void validateHeight(final int height) {
+		if (height < 1) {
+			throw new IllegalArgumentException(HEIGHT_ERROR_MSG);
+		}
 	}
 }
