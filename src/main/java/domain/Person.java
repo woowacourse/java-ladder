4168bd164ed9e09fc06626f9e86e5a926c6fb892
This2sho <zx00018@naver.com>
2023-02-16 13:58:51 +0900

    refactor(Person): 예외 메시지 수정

--- src/main/java/domain/Person.java
+++ src/main/java/domain/Person.java
@@ -2,6 +2,7 @@ package domain;
 
 public class Person {
 
+    public static final int MIN_LENGTH = 1;
     public static final int MAX_LENGTH = 5;
 
     private final String name;
@@ -13,7 +14,8 @@ public class Person {
 
     private void validateNameLength(String name) {
         if (name.isBlank() || name.length() > MAX_LENGTH) {
-            throw new IllegalArgumentException("이름은 1자 이상 5자 이하여야 합니다");
+            throw new IllegalArgumentException(
+                String.format("이름은 %d자 이상 %d자 이하여야 합니다", MIN_LENGTH, MAX_LENGTH));
         }
     }
 
