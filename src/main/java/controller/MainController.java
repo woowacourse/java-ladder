fd1e211169c1d13d73b856c217fced85f9ddc834
Eunkeee <eunkeeeee@gmail.com>
2023-02-16 11:06:22 +0900

    refactor(MainController): 게임 진행 로직 변경

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -3,6 +3,9 @@ package controller;
 import domain.Height;
 import domain.Lines;
 import domain.Names;
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.function.Supplier;
 import view.InputView;
 import view.OutputView;
 
@@ -10,16 +13,74 @@ public class MainController {
 
     private final InputView inputView;
     private final OutputView outputView;
+    private Map<ApplicationStatus, Supplier<ApplicationStatus>> gameGuide;
+    private Names names;
+    private Height height;
+    private Lines lines;
+
 
     public MainController(InputView inputView, OutputView outputView) {
         this.inputView = inputView;
         this.outputView = outputView;
+        this.gameGuide = initializeGameGuide();
     }
 
     public void start() {
-        Names names = inputView.readNames();
-        Height height = inputView.readHeight();
-        Lines lines = new Lines(names.getPersonNumber(), height.getHeight());
+        ApplicationStatus applicationStatus = progress(ApplicationStatus.INPUT_NAME);
+        while (applicationStatus.playable()) {
+            applicationStatus = progress(applicationStatus);
+        }
+    }
+
+    private ApplicationStatus progress(ApplicationStatus applicationStatus) {
+        try {
+            return gameGuide.get(applicationStatus).get();
+        } catch (NullPointerException exception) {
+            return ApplicationStatus.APPLICATION_EXIT;
+        }
+    }
+
+    private Map<ApplicationStatus, Supplier<ApplicationStatus>> initializeGameGuide() {
+        Map<ApplicationStatus, Supplier<ApplicationStatus>> gameGuide = new EnumMap<>(
+                ApplicationStatus.class);
+        gameGuide.put(ApplicationStatus.INPUT_NAME, this::inputName);
+        gameGuide.put(ApplicationStatus.INPUT_HEIGHT, this::inputHeight);
+        gameGuide.put(ApplicationStatus.CREATE_LINES, this::createLines);
+        gameGuide.put(ApplicationStatus.PRINT_RESULT, this::printResult);
+        return gameGuide;
+    }
+
+    private ApplicationStatus inputName() {
+        names = inputView.readNames();
+        return ApplicationStatus.INPUT_HEIGHT;
+    }
+
+    private ApplicationStatus inputHeight() {
+        height = inputView.readHeight();
+        return ApplicationStatus.CREATE_LINES;
+    }
+
+    private ApplicationStatus createLines() {
+        lines = new Lines(names.getPersonNumber(), height.getHeight());
+        return ApplicationStatus.PRINT_RESULT;
+    }
+
+
+    private ApplicationStatus printResult() {
         outputView.printResult(names, lines);
+        return ApplicationStatus.APPLICATION_EXIT;
+    }
+
+
+    private enum ApplicationStatus {
+        INPUT_NAME,
+        INPUT_HEIGHT,
+        CREATE_LINES,
+        PRINT_RESULT,
+        APPLICATION_EXIT;
+
+        boolean playable() {
+            return this != APPLICATION_EXIT;
+        }
     }
 }
