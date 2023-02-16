a53bed94b2ab296c37d3fff7c01dbc7536362bdb
kim0914 <kosa0914@naver.com>
2023-02-16 13:20:48 +0900

    feat(InputView): 사용자 입력 메서드 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -4,14 +4,18 @@ import utils.Parser;
 
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class InputView {
 
     private static final String NAME_INPUT_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     private static final String HEIGHT_INPUT_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
     private static final String DELIMITER = ",";
+    private static final String DIGIT_REGEX = "^[0-9]+$";
+    private static final int MIN_HEIGHT = 1;
+    private static final int MAX_HEIGHT = 100;
 
-    Scanner scanner = new Scanner(System.in);
+    private final Scanner scanner = new Scanner(System.in);
 
     public List<String> readNames() {
         printMessage(NAME_INPUT_MESSAGE);
@@ -21,11 +25,25 @@ public class InputView {
 
     public int readLadderHeight() {
         printMessage(HEIGHT_INPUT_MESSAGE);
-        return scanner.nextInt();
+        String ladderHeight = scanner.nextLine();
+        validateDigit(ladderHeight);
+        validateHeightRange(Integer.valueOf(ladderHeight));
+        return Integer.valueOf(ladderHeight);
+    }
+
+    private void validateDigit(String ladderHeight) {
+        if (!Pattern.matches(DIGIT_REGEX ,ladderHeight)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateHeightRange(int height) {
+        if (height < MIN_HEIGHT || height > MAX_HEIGHT) {
+            throw new IllegalArgumentException();
+        }
     }
 
     private void printMessage(String message) {
         System.out.println(message);
     }
-
 }
