37de62a784d1012a4699f4c53a6057a048fb4b0d
kokodak <kokodakadokok@gmail.com>
2023-02-16 18:54:55 +0900

    feat: 에러 메세지 추가

--- src/main/java/utils/constants/ErrorMessages.java
+++ src/main/java/utils/constants/ErrorMessages.java
@@ -3,7 +3,8 @@ package utils.constants;
 public enum ErrorMessages {
     NAME_LENGTH("이름은 1글자 이상 5글자 이하여야 합니다."),
     NUMBER_FORMAT("1 이상 10 이하의 정수만 입력가능합니다."),
-    DUPLICATED_INPUT("중복된 값은 입력할 수 없습니다.");
+    DUPLICATED_INPUT("중복된 값은 입력할 수 없습니다."),
+    CONSECUTIVE_LINE("사다리의 라인은 같은 깊이에서 연속적으로 존재할 수 없습니다.");
 
     private final String message;
 
