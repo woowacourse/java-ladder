614f26be4df0bde2509cae1759725ffd5f3727c3
LJW25 <dbjwl@g.skku.edu>
2023-02-16 15:29:29 +0900

    refactor(OutputView): 하드 코딩 제거

--- src/main/java/ladder/view/OutputView.java
+++ src/main/java/ladder/view/OutputView.java
@@ -15,22 +15,35 @@ public class OutputView {
     }
 
     public void printPlayerNames(List<String> playerNames) {
-        System.out.print("  ");
         playerNames.stream().forEach(name -> System.out.print(String.format("%-6s", name)));
     }
 
     public void printRow(List<Boolean> points) {
         System.out.println();
-        System.out.print("  |");
+        System.out.print(LadderMark.COLUMN_LINE.getMark());
         for (boolean point : points) {
-            if (point) {
-                System.out.print("-----|");
-            }
-            if (!point) {
-                System.out.print("     |");
-            }
+            System.out.print(LadderMark.getRowMark(point));
+            System.out.print(LadderMark.COLUMN_LINE.getMark());
         }
     }
 
+    private enum LadderMark {
+        ROW_LINE("-----"),
+        ROW_EMPTY("     "),
+        COLUMN_LINE("|");
+        private final String mark;
 
+        LadderMark(String mark) {
+            this.mark = mark;
+        }
+
+        public String getMark() {
+            return mark;
+        }
+
+        public static String getRowMark(boolean hasLine) {
+            if (hasLine) return ROW_LINE.mark;
+            return ROW_EMPTY.mark;
+        }
+    }
 }
