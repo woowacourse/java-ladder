f980d4a418cabc8aafd69fba9f3ac2ae56ef1929
kim0914 <kosa0914@naver.com>
2023-02-16 14:58:49 +0900

    refactor(Name): 예외 검증 값 상수화

--- src/main/java/domain/player/Name.java
+++ src/main/java/domain/player/Name.java
@@ -4,6 +4,10 @@ import utils.ErrorMessage;
 
 public class Name {
 
+    private static final int MIN_NAME_LENGTH = 1;
+    private static final int MAX_NAME_LENGTH = 5;
+    private static final String SPACE = " ";
+
     private final String name;
 
     public Name(String name) {
@@ -17,13 +21,13 @@ public class Name {
     }
 
     private void validateLength(String name) {
-        if (name.length() < 1 || name.length() > 5) {
+        if (name.length() < MIN_NAME_LENGTH || name.length() > MAX_NAME_LENGTH) {
             throw new IllegalArgumentException(ErrorMessage.NAME_LENGTH_ERROR.getMessage());
         }
     }
 
     private void validateSpace(String name) {
-        if (name.contains(" ")) {
+        if (name.contains(SPACE)) {
             throw new IllegalArgumentException(ErrorMessage.NAME_FORMAT_ERROR.getMessage());
         }
     }
