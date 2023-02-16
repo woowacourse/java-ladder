e7a0dbf3c8e70149e6140e2778619908ac150085
shin-mallang <huipulci1@naver.com>
2023-02-16 12:28:58 +0900

    feat: 사다리게임 Controller 구현

---
+++ src/main/java/controller/LadderController.java
@@ -0,0 +1,26 @@
+package controller;
+
+import domain.Ladder;
+import domain.LadderFactory;
+import domain.Name;
+import view.InputView;
+import view.OutputView;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LadderController {
+    private final LadderFactory ladderFactory;
+
+    public LadderController(final LadderFactory ladderFactory) {
+        this.ladderFactory = ladderFactory;
+    }
+
+    public void run() {
+        List<String> inputNames = InputView.inputNames();
+        List<Name> names = inputNames.stream().map(Name::new).collect(Collectors.toList());
+        int height = InputView.inputHeight();
+        Ladder ladder = ladderFactory.createLadder(names.size() - 1, height);
+        OutputView.printResult(ladder, names);
+    }
+}
