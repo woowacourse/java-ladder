6e75dd0c0d2cfd74e1b39f4c069220521e71eb99
odo27 <odo27@odo27ui-MacBookAir.local>
2023-02-16 14:15:23 +0900

    refactor(Player): final 키워드 추가 및 매직 넘버 제거

--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -4,8 +4,9 @@ public class Player {
 
     private static final int NAME_LENGTH_LOWER_BOUND = 1;
     private static final int NAME_LENGTH_UPPER_BOUND = 5;
+    private static final String INVALID_NAME_LENGTH_MESSAGE = "사람 이름은 1글자에서 5글자 사이이어야 합니다.";
 
-    private String name;
+    private final String name;
 
     public Player(String name) {
         String trimmedName = name.trim();
@@ -15,7 +16,7 @@ public class Player {
 
     private static void validate(String name) {
         if (isInvalidNameLength(name)) {
-            throw new IllegalArgumentException("사람 이름은 1글자에서 5글자 사이이어야 합니다.");
+            throw new IllegalArgumentException(INVALID_NAME_LENGTH_MESSAGE);
         }
     }
 
