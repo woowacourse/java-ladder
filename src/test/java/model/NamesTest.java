3349f2f86fc30eb782ff458cd1a4ddf96dd3a8b6
MoonJeWoong <munjin0201@naver.com>
2023-02-16 11:47:40 +0900

    test: 참여자 이름은 쉼표를 기준으로 입력받는 기능 테스트 리팩토링
    세부사항
    - 테스트 검증 리스트 타입 String -> Name 리팩토링

--- src/test/java/model/NamesTest.java
+++ src/test/java/model/NamesTest.java
@@ -10,15 +10,19 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class NamesTest {
 
     private static Stream<Arguments> provideNamesInputValues() {
         return Stream.of(
-                Arguments.of("hiiro", List.of("hiiro")),
-                Arguments.of("pobi, neo, conan", List.of("pobi", "neo", "conan")),
-                Arguments.of("pobi, neo, conan, ocean, hiiro", List.of("pobi", "neo", "conan", "ocean", "hiiro"))
+                Arguments.of("hiiro", List.of(new Name("hiiro"))),
+                Arguments.of("pobi, neo, conan",
+                        List.of(new Name("pobi"), new Name("neo"), new Name("conan"))),
+                Arguments.of("pobi, neo, conan, ocean, hiiro",
+                        List.of(new Name("pobi"), new Name("neo"), new Name("conan"), new Name("ocean"),
+                                new Name("hiiro")))
         );
     }
 
@@ -32,15 +36,13 @@ public class NamesTest {
     @ParameterizedTest
     @MethodSource("provideNamesInputValues")
     @DisplayName("참여자 이름은 쉼표를 기준으로 입력받는 기능 테스트")
-    void splitNamesByCommas(String inputValue, List<String> expectedResult) {
+    void splitNamesByCommas(String inputValue, List<Name> expectedResult) {
         //Given
         Names names = new Names(inputValue);
 
-        //When
-        List<String> result = names.getNames();
-
         //Then
-        assertThat(result).isEqualTo(expectedResult);
+        IntStream.range(0,names.size())
+                .forEach((index)->{assertThat(names.getName(index)).isEqualTo(expectedResult.get(index));});
     }
 
 }
