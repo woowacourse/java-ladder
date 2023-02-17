11661b4c9b387f88d87459170441083702cb5ce8
hong-sile <gurwns9325@naver.com>
2023-02-17 20:55:25 +0900

    refactor: controller 의 로직을 main메서드가 있는 Application으로 이동

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,8 +1,36 @@
-import controller.LadderController;
+import domain.Ladder;
+import domain.Users;
+import view.InputView;
+import view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        LadderController ladderController = new LadderController();
-        ladderController.run();
+        final Users users = generateUsers();
+        final Ladder ladder = generateLadder(users);
+        printResult(ladder, users);
+    }
+
+    private static Ladder generateLadder(Users users) {
+        try {
+            return Ladder.generateByHeightPersonCount(InputView.readLadderHeight(), users.size());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return generateLadder(users);
+        }
+    }
+
+    private static Users generateUsers() {
+        try {
+            return new Users(InputView.readUserNames());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return generateUsers();
+        }
+    }
+
+    private static void printResult(final Ladder ladder, final Users users) {
+        OutputView.printResultMessage();
+        OutputView.printUserNames(users.getUsersName());
+        OutputView.printResult(ladder.getLines());
     }
 }
