c1761342d63c6f1924d71e8b8e5a05cdf63e0dc2
waterricecake <fruturum@nate.com>
2023-02-17 15:45:06 +0900

    refactor(NameTest): 이름 길이 0일때의 예외처리 test

--- src/test/java/domain/vo/NameTest.java
+++ src/test/java/domain/vo/NameTest.java
@@ -1,18 +1,20 @@
 package domain.vo;
 
+import org.assertj.core.api.AssertionsForClassTypes;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class NameTest {
 
-    @Test
-    @DisplayName("이름은 1~5 글자를 넘지않는다.")
-    void makeNameLengthFailure() {
-        String wrongName = "abcdef";
-        assertThatThrownBy(() -> new Name(wrongName))
+    @ParameterizedTest(name = "이름의 길이는 1 ~ 5 사이여야 합니다.")
+    @ValueSource(strings = {"", "abcdef"})
+    void makeHeightFailure(String provided) {
+        AssertionsForClassTypes.assertThatThrownBy(() -> new Name(provided))
                 .hasMessage("이름 길이는 1 ~ 5 사이여야 합니다.");
     }
 
