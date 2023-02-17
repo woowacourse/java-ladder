e76610355ddde29de69fc8505d5563d70382fe53
sh111-coder <ohk9134@naver.com>
2023-02-18 08:57:12 +0900

    test(LadderGameTest): 테스트 목적을 나타내는 테스트 이름 추가 & Ladder 값 검증 추가 리팩토링

--- src/test/java/ladder/domain/LadderGameTest.java
+++ src/test/java/ladder/domain/LadderGameTest.java
@@ -1,6 +1,7 @@
 package ladder.domain;
 
-import org.junit.jupiter.api.Assertions;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -8,13 +9,16 @@ import java.util.List;
 public class LadderGameTest {
 
     @Test
+    @DisplayName("주어진 플레이어 네임과 최대 사다리 높이대로 LadderGame이 생성된다.")
     void ladderGameTest() {
         List<String> names = List.of("pobi", "crong", "seong", "haddy");
         int height = 5;
 
         LadderGame ladderGame = new LadderGame(names, height);
         List<String> playerNames = ladderGame.getNames();
+        List<Line> lines = ladderGame.getLines();
 
-        Assertions.assertTrue(playerNames.containsAll(names));
+        Assertions.assertThat(playerNames).containsAll(names);
+        Assertions.assertThat(lines.size()).isEqualTo(height);
     }
 }
