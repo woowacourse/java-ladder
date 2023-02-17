86eb6ad99e05f259993c85fec5c7294389cb83f2
iamjooon2 <iamjooon2@gmail.com>
2023-02-18 01:36:33 +0900

    refactor: OutputView 리팩터링

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -2,12 +2,14 @@ package view;
 
 import domain.Ladder;
 import domain.Name;
+import domain.Names;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class OutputView {
-
+    private static final int MAX_NAME_SIZE = 5;
+    private static final int FIRST_NAME_INDEX = 0;
     private static final String BAR = "|";
     private static final String BLANK = " ";
 
@@ -16,17 +18,17 @@ public class OutputView {
     private OutputView() {
     }
 
-    public static void printResult(final Ladder ladder, final List<Name> playerNames) {
-        String initBlank = BLANK.repeat(playerNames.get(0).getValue().length());
+    public static void printResult(final Ladder ladder, final Names playerNames) {
+        String prefixBlank = BLANK.repeat(playerNames.getNames().get(FIRST_NAME_INDEX).getValue().length());
         printNames(playerNames);
-        printLadder(ladder, initBlank);
+        printLadder(ladder, prefixBlank);
     }
 
-    public static void printNames(final List<Name> playerNames) {
+    public static void printNames(final Names playerNames) {
         List<String> names = new ArrayList<>();
-        List<String> nameValues = playerNames.stream().map(Name::getValue).collect(Collectors.toList());
-        String remove = nameValues.remove(0);
-        names.add(remove + BLANK);
+        List<String> nameValues = playerNames.getNames().stream().map(Name::getValue).collect(Collectors.toList());
+        String firstName = nameValues.remove(FIRST_NAME_INDEX);
+        names.add(firstName + BLANK);
         addBlanks(names, nameValues);
         for (int i = 0; i < names.size(); i++) {
             System.out.print(names.get(i) + BLANK);
@@ -36,15 +38,15 @@ public class OutputView {
 
     private static void addBlanks(List<String> makeNames, List<String> nameValues) {
         for (String name : nameValues) {
-            int addBlankSize = 5 - name.length();
+            int addBlankSize = MAX_NAME_SIZE - name.length();
             String result = BLANK.repeat(addBlankSize);
             makeNames.add(result + name);
         }
     }
 
-    private static void printLadder(Ladder ladder, String initBlank) {
+    private static void printLadder(Ladder ladder, String prefixBlank) {
         for (int i = 0; i < ladder.getHeight(); i++) {
-            stringBuilder.append(initBlank);
+            stringBuilder.append(prefixBlank);
             stringBuilder.append(BAR);
             printLinesByStringBuilder(ladder, i);
             System.out.println();
