73bf94c6c38deff8ee78eaa7d9fb1105f30907da
LJW25 <dbjwl@g.skku.edu>
2023-02-17 16:06:09 +0900

    refactor(RandomLineCreateDecider): 변수 명 수정

--- src/main/java/ladder/model/RandomLineCreateDecider.java
+++ src/main/java/ladder/model/RandomLineCreateDecider.java
@@ -5,11 +5,11 @@ import java.security.SecureRandom;
 public class RandomLineCreateDecider implements LineCreateDecider {
 
     private static final int MAX_RANDOM_INT = 2;
-    private static final SecureRandom secureRandom = new SecureRandom();
+    private static final SecureRandom SECURE_RANDOM = new SecureRandom();
 
     @Override
     public boolean decide() {
-        int number = secureRandom.nextInt(MAX_RANDOM_INT);
+        int number = SECURE_RANDOM.nextInt(MAX_RANDOM_INT);
         return number >= MAX_RANDOM_INT / 2;
     }
 }
