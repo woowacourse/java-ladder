d9aa5613145385fcd99c0804200a6e1cac9b3bc1
the9kim <96895686+the9kim@users.noreply.github.com>
2023-02-17 15:26:33 +0900

    refactor: 공백 라인 수정

--- src/main/java/util/RandomLineGenerator.java
+++ src/main/java/util/RandomLineGenerator.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class RandomLineGenerator implements LineGenerator {
 
     private final Random random = new Random();
+
     @Override
     public boolean generate(boolean isLeftLineExist) {
         if (isLeftLineExist) {
