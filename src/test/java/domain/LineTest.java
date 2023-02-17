3fc4745c361eb5fa0240d7b5d0833a77736a602d
java-saeng <gusdn85554@naver.com>
2023-02-17 15:17:18 +0900

    refactor : exception 터지는 경우와 exception 터지지 않는 경우 메서드 extract

--- src/test/java/domain/LineTest.java
+++ src/test/java/domain/LineTest.java
@@ -4,6 +4,8 @@ import helper.AbstractTestFixture;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -13,51 +15,59 @@ class LineTest extends AbstractTestFixture {
     @DisplayName("3개의 브릿지가 연속될 경우 IllegalArgumentException을 반환한다")
     void test_sameHeight1_IllegalArgumentException() {
 
-        assertThatThrownBy(() -> new Line(convert(true, true, false)))
-                .isInstanceOf(IllegalArgumentException.class);
+        testAssertThrowsIllegalArgumentException(convert(true, true, false));
     }
 
     @Test
     @DisplayName("3개의 브릿지가 연속될 경우 IllegalArgumentException을 반환한다")
     void test_sameHeight2_IllegalArgumentException() {
 
-        assertThatThrownBy(() -> new Line(convert(false, true, true)))
-                .isInstanceOf(IllegalArgumentException.class);
+        testAssertThrowsIllegalArgumentException(convert(false, true, true));
     }
 
     @Test
     @DisplayName("3개의 브릿지가 연속될 경우 IllegalArgumentException을 반환한다")
     void test_sameHeight3_IllegalArgumentException() {
 
-        assertThatThrownBy(() -> new Line(convert(true, true, true)))
-                .isInstanceOf(IllegalArgumentException.class);
+        testAssertThrowsIllegalArgumentException(convert(true, true, true));
     }
 
     @Test
     @DisplayName("3개의 브릿지가 연속되지 않으면 성공적으로 사다리가 생성된다")
     void test_notSameHeight1_IllegalArgumentException() {
 
-        assertThatNoException().isThrownBy(() -> new Line(convert(true, false, true)));
+        testAssertNotThrowsException(convert(true, false, true));
     }
 
     @Test
     @DisplayName("3개의 브릿지가 연속되지 않으면 성공적으로 사다리가 생성된다")
     void test_notSameHeight2_IllegalArgumentException() {
 
-        assertThatNoException().isThrownBy(() -> new Line(convert(false, true, false)));
+        testAssertNotThrowsException(convert(false, true, false));
     }
 
     @Test
     @DisplayName("4개의 브릿지가 연속되지 않으면 성공적으로 사다리가 생성된다")
     void test_notSameHeight3_IllegalArgumentException() {
 
-        assertThatNoException().isThrownBy(() -> new Line(convert(true, false, false, true)));
+        testAssertNotThrowsException(convert(true, false, false, true));
     }
 
     @Test
     @DisplayName("1개의 브릿지가 연속되지 않으면 성공적으로 사다리가 생성된다")
     void test_notSameHeight4_IllegalArgumentException() {
 
-        assertThatNoException().isThrownBy(() -> new Line(convert(true)));
+        testAssertNotThrowsException(convert(true));
+    }
+
+    private void testAssertThrowsIllegalArgumentException(List<Bridge> bridges) {
+        assertThatThrownBy(() -> new Line(bridges))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private void testAssertNotThrowsException(List<Bridge> bridges) {
+        assertThatNoException().isThrownBy(
+                () -> new Line(bridges)
+        );
     }
 }
