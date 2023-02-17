d4c80772c2d5b2ced0450521efa80ba2114e9fe0
waterricecake <fruturum@nate.com>
2023-02-17 14:50:38 +0900

    refactor(Name): 변수와 클래스 상수 줄 바꿈

--- src/main/java/domain/vo/Name.java
+++ src/main/java/domain/vo/Name.java
@@ -5,6 +5,7 @@ public class Name {
     private static final int MIN_LENGTH = 1;
     private static final int MAX_LENGTH = 5;
     private static final String LENGTH_ERROR_MESSAGE = "이름 길이는 %d ~ %d 사이여야 합니다.";
+
     private final String value;
 
     public Name(final String value) {
