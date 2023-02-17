3d8b31fd78ef372568348309c73306a0710e2646
jyeost <pig9619@naver.com>
2023-02-17 14:44:38 +0900

    refactor: 필드에 개행 자바컨벤션 적용, static 메소드를 인스턴스 메소드로 수정

--- src/main/java/laddergame/model/Height.java
+++ src/main/java/laddergame/model/Height.java
@@ -3,6 +3,7 @@ package laddergame.model;
 public class Height {
     private static final int MIN_HEIGHT = 1;
     private static final String ERROR_HEIGHT_LENGTH = "사다리의 높이는 " + MIN_HEIGHT + "이상이어야 합니다.";
+
     private final int height;
 
     public Height(int height) {
@@ -10,7 +11,7 @@ public class Height {
         this.height = height;
     }
 
-    private static void validateRange(int height) {
+    private void validateRange(int height) {
         if (height < MIN_HEIGHT) {
             throw new IllegalArgumentException(ERROR_HEIGHT_LENGTH);
         }
