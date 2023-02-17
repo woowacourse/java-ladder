73f0e4fae356194f41baa01f9bb5dba89b6a7122
kevstevie <jianna442@gmail.com>
2023-02-17 22:38:56 +0900

    feat: Point enum 테스트케이스

--- src/main/java/ladder/domain/Point.java
+++ src/main/java/ladder/domain/Point.java
@@ -10,6 +10,13 @@ public enum Point {
         this.isExist = isExist;
     }
 
+    public static Point of(boolean value) {
+        if (value) {
+            return FILLED;
+        }
+        return EMPTY;
+    }
+
     public boolean isExist() {
         return isExist;
     }
