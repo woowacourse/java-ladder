8c260d93d359c344269875123c02ff5d5d689e8b
be-student <presentsong@naver.com>
2023-02-17 20:04:59 +0900

    refactor: 검증 메서드 형식 변경

--- src/main/java/ladder/domain/Name.java
+++ src/main/java/ladder/domain/Name.java
@@ -13,24 +13,29 @@ class Name {
 
     private void validateName(String name) {
         //todo 형식2
+        validateNull(name);
+        validateOverLength(name);
+        validateIsBlank(name);
+    }
+
+    private void validateNull(String name) {
         if (name == null) {
             throw new IllegalArgumentException("이름이 null이 되면 안됩니다");
         }
-        if (isOverLength(name)) {
+    }
+
+    private void validateOverLength(String name) {
+        if (name.length() > MAX_NAME_LENGTH) {
             throw new IllegalArgumentException("글자수가 5글자를 초과했습니다");
         }
-        if (isBlank(name)) {
+    }
+
+    private void validateIsBlank(String name) {
+        if (name.isBlank()) {
             throw new IllegalArgumentException("이름이 빈 문자열이 될 수 없습니다");
         }
     }
 
-    private boolean isBlank(String name) {
-        return name.isBlank();
-    }
-
-    private boolean isOverLength(String name) {
-        return name.length() > MAX_NAME_LENGTH;
-    }
 
     String toDto() {
         return name;
