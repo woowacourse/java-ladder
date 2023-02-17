3ad6fb534ce2dad97ac2c8834c08e9c988063217
parkmuhyeun <pjhg410@gmail.com>
2023-02-17 18:05:57 +0900

    test: PersonsTest DisplayName 작성

--- src/test/java/domain/PersonsTest.java
+++ src/test/java/domain/PersonsTest.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public class PersonsTest {
     @Test
+    @DisplayName("Persons 생성 확인")
     void persons(){
         Persons persons = new Persons(List.of("1", "2", "3"));
 
@@ -15,7 +16,7 @@ public class PersonsTest {
     }
 
     @Test
-    @DisplayName("")
+    @DisplayName("중복된 닉네임을 입력하면 예외 발생")
     void validateDuplicateName() {
         Assertions.assertThatThrownBy(() -> new Persons(List.of("a", "b", "a")))
                 .isInstanceOf(IllegalArgumentException.class);
