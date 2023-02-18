453c443bd077a88da23558bd6c23e94ed0d60a4f
kiarakim <kiarakim413@gmail.com>
2023-02-18 21:06:06 +0900

    refactor(Error) : InputView 에러 메세지 Enum화

--- src/main/java/exception/Error.java
+++ src/main/java/exception/Error.java
@@ -5,7 +5,8 @@ public enum Error {
 	LACK_OF_PEOPLE("사람은 최소 두명 이상이어야 합니다"),
 	TOO_MANY_PEOPLE("사람은 최대 10명 이어야 합니다"),
 	DUPLICATED_NAME("사람 이름은 중복되지 않아야 합니다"),
-	HEIGHT_RANGE_FROM_1_TO_100("높이는 1부터 100까지만 가능합니다");
+	HEIGHT_RANGE_FROM_1_TO_100("높이는 1부터 100까지만 가능합니다"),
+	HEIGHT_ONLY_NUMBER("높이는 숫자를 입력해야 합니다");
 
 	private final String message;
 	private final String PREFIX = "[ERROR] ";
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -3,6 +3,8 @@ package view;
 import java.util.List;
 import java.util.Scanner;
 
+import exception.Error;
+
 public class InputView {
 	public List<String> readNames() {
 		System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
@@ -16,7 +18,7 @@ public class InputView {
 			System.out.println("\n최대 사다리 높이는 몇 개인가요?");
 			return Integer.parseInt(scanner.nextLine());
 		} catch (NumberFormatException e) {
-			throw new IllegalArgumentException("높이는 숫자를 입력해야 합니다");
+			throw new IllegalArgumentException(Error.HEIGHT_ONLY_NUMBER.getMessage());
 		}
 	}
 }
