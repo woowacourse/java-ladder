e5cf620ee6d9af08e14e3711de00925af5e32df2
kevstevie <jianna442@gmail.com>
2023-02-18 18:44:09 +0900

    feat: 미작성된 도메인 테스트 작성

---
+++ src/test/java/ladder/domain/UserNameTest.java
@@ -0,0 +1,28 @@
+package ladder.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class UserNameTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "  ", "여섯글자이름"})
+    @DisplayName("이름은 공백이거나 여섯 글자 이상일 수 없다.")
+    void userNameExceptionTest(String value) {
+        assertThatThrownBy(() -> new UserName(value))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "다섯글자임", "  공백제외5  "})
+    @DisplayName("유효성 검증 경계값을 검증하는 테스트")
+    void validNameTest(String value) {
+        assertThatCode(() -> new UserName(value))
+                .doesNotThrowAnyException();
+    }
+
+}
