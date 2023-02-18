c38cabbab8bd9bd1195a0bef0bd9a05db58d7fb7
Cyma-s <ssol0319@naver.com>
2023-02-18 21:00:15 +0900

    refactor: 메서드 이름을 암묵적인 네이밍 컨벤션으로 통일

--- src/main/java/ladder/domain/Name.java
+++ src/main/java/ladder/domain/Name.java
@@ -5,18 +5,18 @@ public class Name {
     private final String name;
 
     public Name(String name) {
-        validateEmptyName(name);
-        validateOutOfNameLength(name);
+        validateEmpty(name);
+        validateOutOfLengthRange(name);
         this.name = name;
     }
 
-    private void validateEmptyName(String name) {
+    private void validateEmpty(String name) {
         if (name.isBlank()) {
             throw new IllegalArgumentException("이름은 빈 문자일 수 없습니다.");
         }
     }
 
-    private void validateOutOfNameLength(String name) {
+    private void validateOutOfLengthRange(String name) {
         if (name.length() > MAX_SIZE) {
             throw new IllegalArgumentException("이름은 최소 1글자, 최대 5글자여야 합니다.");
         }
