e2d93f7cb846bcf9880a3abbdb06d33828761e8e
ingpyo <juninpyo726@gmail.com>
2023-02-18 21:08:38 +0900

    feat: 높이 값이 숫자가 아닐때 예외 추가

--- src/main/java/view/InputHeightValidator.java
+++ src/main/java/view/InputHeightValidator.java
@@ -1,16 +1,20 @@
 package view;
 
 
+import java.util.regex.Pattern;
+
 public class InputHeightValidator {
 
-    public static final String NOT_POSITIVE_ERROR_MESSAGE = "[ERROR] 양의 정수만 입력해주세요.";
-    public static final String BLANK_MESSAGE = "[ERROR] 입력값이 비어있습니다.";
-    public static final String NULL_MESSAGE = "[ERROR] 아무것도 입력하지 않았습니다.";
+    private static final String NOT_POSITIVE_ERROR_MESSAGE = "[ERROR] 양의 정수만 입력해주세요.";
+    private static final String BLANK_MESSAGE = "[ERROR] 입력값이 비어있습니다.";
+    private static final String NULL_MESSAGE = "[ERROR] 아무것도 입력하지 않았습니다.";
+    private static final String MATCH_NUMBER_MESSAGE = "[ERROR] 숫자만 입력해야 합니다.";
 
     public void checkHeight(String height) {
         checkNegativeNumber(height);
         checkNotEmpty(height);
         checkNull(height);
+        checkMissMatchNumber(height);
     }
 
     private void checkNegativeNumber(String height) {
@@ -18,14 +22,20 @@ public class InputHeightValidator {
             throw new IllegalArgumentException(NOT_POSITIVE_ERROR_MESSAGE);
         }
     }
-    private static void checkNotEmpty(String height) {
+    private void checkNotEmpty(String height) {
         if (height.isBlank()) {
             throw new IllegalArgumentException(BLANK_MESSAGE);
         }
     }
-    private static void checkNull(String height) {
+    private void checkNull(String height) {
         if (height==null) {
             throw new IllegalArgumentException(NULL_MESSAGE);
+        }
+    }
+
+    private void checkMissMatchNumber(String height) {
+        if (!Pattern.matches("^[0-9]*$", height)) {
+            throw new IllegalArgumentException(MATCH_NUMBER_MESSAGE);
         }
     }
 }
