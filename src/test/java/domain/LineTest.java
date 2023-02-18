47dec27f3e15173e19e9a21e80111c52029a2105
ingpyo <juninpyo726@gmail.com>
2023-02-18 22:43:42 +0900

    refactor: 테스트를 별로 Line객체 생성

--- src/test/java/domain/LineTest.java
+++ src/test/java/domain/LineTest.java
@@ -1,3 +1,5 @@
+package domain;
+
 import domain.Line;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -11,7 +13,6 @@ import java.util.List;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class LineTest {
-    Line line;
 
     @Nested
     @DisplayName("랜덤 값의 의한 라인 생성 테스트(첫번째 좌표는 이전 좌표가 없는것을 표시)")
@@ -19,7 +20,7 @@ class LineTest {
         @DisplayName("들어간대로 잘 생성되었는지 테스트")
         @Test
         void createTest() {
-            line = new Line(4, new testTrueOrFalseGenerator(new ArrayList<>(Arrays.asList(false, true, false)) {
+            Line line = new Line(4, new testTrueOrFalseGenerator(new ArrayList<>(Arrays.asList(false, true, false)) {
             }));
             assertThat(line.getPoints()).isEqualTo(List.of(false, true, false));
         }
@@ -27,7 +28,7 @@ class LineTest {
         @DisplayName("이전 좌표와 현재 좌표가 겹칠때 변환 테스트(처)")
         @Test
         void createLine2() {
-            line = new Line(4, new testTrueOrFalseGenerator(new ArrayList<>(Arrays.asList(true, true, false)) {
+            Line line = new Line(4, new testTrueOrFalseGenerator(new ArrayList<>(Arrays.asList(true, true, false)) {
             }));
             assertThat(line.getPoints()).isEqualTo(List.of(true, false, false));
         }
