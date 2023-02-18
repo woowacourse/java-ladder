9ab861e4de2cbd0fccb1a2d53d37b4887d3f4a2c
hyunseo <goatsfish@gmail.com>
2023-02-18 11:22:57 +0900

    refactor: 참여자 수 (0명 -> 2명 미만) 예외 검증 수정

--- src/main/java/laddergame/domain/Participants.java
+++ src/main/java/laddergame/domain/Participants.java
@@ -3,27 +3,24 @@ package laddergame.domain;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static laddergame.messsages.ExceptionMessages.PARTICIPANTS_EMPTY_EXCEPTION;
-import static laddergame.messsages.ExceptionMessages.PARTICIPANTS_NULL_EXCEPTION;
-
 public class Participants {
+    private static final int MIN_PARTICIPANTS_SIZE = 2;
+    private static final String PARTICIPANTS_NULL_EXCEPTION = "참여자 이름 목록은 null이 될 수 없습니다.";
+    private static final String PARTICIPANTS_MIN_SIZE_EXCEPTION = "참여자 이름 목록은 2명 미만이 될 수 없습니다.";
+
     private final List<Name> names;
 
     public Participants(final List<Name> names) {
-        validateNotNull(names);
-        validateNotEmpty(names);
+        validateParticipants(names);
         this.names = names;
     }
 
-    private void validateNotEmpty(final List<Name> names) {
-        if (names.isEmpty()) {
-            throw new IllegalArgumentException(PARTICIPANTS_EMPTY_EXCEPTION.getMessage());
-        }
-    }
-
-    private void validateNotNull(final List<Name> names) {
+    private void validateParticipants(final List<Name> names) {
         if (names == null) {
-            throw new IllegalArgumentException(PARTICIPANTS_NULL_EXCEPTION.getMessage());
+            throw new IllegalArgumentException(PARTICIPANTS_NULL_EXCEPTION);
+        }
+        if (names.size() < MIN_PARTICIPANTS_SIZE) {
+            throw new IllegalArgumentException(PARTICIPANTS_MIN_SIZE_EXCEPTION);
         }
     }
 
