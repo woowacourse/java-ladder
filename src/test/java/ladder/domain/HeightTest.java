4097b54948db18a917389302334a26d232af4cbe
Jaeyoung22 <yt930310@gmail.com>
2023-02-18 10:16:54 +0900

    refactor(HeightTest): 테스트의 목적을 명확히 하기 위해 에러 메세지도 검증하도록 수정

--- src/test/java/ladder/domain/HeightTest.java
+++ src/test/java/ladder/domain/HeightTest.java
@@ -6,13 +6,16 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import ladder.error.ErrorMessage;
+
 class HeightTest {
 
     @ParameterizedTest(name = "높이는 자연수여야 한다.")
     @ValueSource(ints = {-1, 0, 10001})
     void validateHeightFailTest(int height) {
         assertThatThrownBy(() -> new Height(height))
-            .isInstanceOf(IllegalArgumentException.class);
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage(ErrorMessage.INVALID_HEIGHT_RANGE.getMessage());
     }
 
     @ParameterizedTest(name = "높이는 1부터 10000까지의 자연수여야 한다.")
