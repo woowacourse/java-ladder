5e196b205a81b21b9bafe8c7ec49cdeea3583bc1
Wugawuga <dnr3655@naver.com>
2023-02-18 17:22:28 +0900

    feat: 리뷰 반영 이름 null 예외 처리 추가

--- src/main/java/ladder/domain/Player.java
+++ src/main/java/ladder/domain/Player.java
@@ -1,7 +1,7 @@
 package ladder.domain;
 
 public class Player {
-    private static final String INVALID_NAME_LENGTH_MESSAGE = "이름은 1자 이상, 5자 이하여야 한다.";
+    private static final String INVALID_NAME_LENGTH_MESSAGE = "이름은 1자 이상, 5자 이하여야 합니다.";
     private static final int NAME_LENGTH_UPPER_BOUND = 5;
 
     private final String name;
@@ -18,7 +18,7 @@ public class Player {
     }
 
     private boolean isInvalidNameLength(final String name) {
-        return name.isBlank() || NAME_LENGTH_UPPER_BOUND < name.length();
+        return name == null || name.isBlank() || NAME_LENGTH_UPPER_BOUND < name.length();
     }
 
     public String getName() {
--- src/test/java/ladder/domain/PlayerTest.java
+++ src/test/java/ladder/domain/PlayerTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 @SuppressWarnings("NonAsciiCharacters")
@@ -20,11 +21,19 @@ public class PlayerTest {
         assertThat(player.getName()).isEqualTo("name");
     }
 
-    @ParameterizedTest(name = "이름이 비어있거나 6자 이상인 경우 예외를 던진다. 입력값: \"{0}\"")
-    @ValueSource(strings = {"", "우아한형제들"})
-    void 이름이_비어있거나_6자_이상인_경우_예외를_던진다(final String name) {
+    @ParameterizedTest(name = "6자 이상인 경우 예외를 던진다. 입력값: \"{0}\"")
+    @ValueSource(strings = {"우아한형제들", "우아한테크코스"})
+    void 이름이_6자_이상인_경우_예외를_던진다(final String name) {
         assertThatThrownBy(() -> new Player(name))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("이름은 1자 이상, 5자 이하여야 한다.");
+                .hasMessage("이름은 1자 이상, 5자 이하여야 합니다.");
+    }
+
+    @ParameterizedTest(name = "이름이 비어있거나 null인 경우 예외를 던진다. 입력값: \"{0}\"")
+    @NullAndEmptySource
+    void 이름이_비어있거나_null인_경우_예외를_던진다(final String name) {
+        assertThatThrownBy(() -> new Player(null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이름은 1자 이상, 5자 이하여야 합니다.");
     }
 }
