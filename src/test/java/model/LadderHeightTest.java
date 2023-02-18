68f1eb0e9950e5f17d4cecafe40a506381f67df2
MoonJeWoong <munjin0201@naver.com>
2023-02-18 23:17:43 +0900

    test: 예외처리 관련 테스트 코드 리팩토링
    세부사항
    - 예외처리 테스트 시 assertThatThrownBy 메서드 사용하는 것으로 변경
    - 예외처리 테스트 시 예외 메세지도 확인하는 것으로 변경

--- src/test/java/model/LadderHeightTest.java
+++ src/test/java/model/LadderHeightTest.java
@@ -1,32 +1,27 @@
 package model;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.catchThrowable;
-
 public class LadderHeightTest {
+    private static final String MINIMUM_LADDER_HEIGHT_ERROR = "[ERROR] 사다리 높이는 최소 1 이상의 값을 입력해야 합니다.";
+
     @Test
     @DisplayName("LadderHeight 객체 생성 성공 테스트")
     void createLadderHeightTest() {
-        Assertions.assertThatNoException().isThrownBy(() -> {
-            new LadderHeight(5);
-        });
+        Assertions.assertThatNoException().isThrownBy(() -> new LadderHeight(5));
     }
 
     @Test
     @DisplayName("LadderHeight가 1 미만일 경우 실패 테스트")
     void validateLadderHeightTest() {
-        //when
-        Throwable result = catchThrowable(() -> {
-            new LadderHeight(0);
-        });
-
-        //then
-        assertThat(result).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new LadderHeight(0))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(MINIMUM_LADDER_HEIGHT_ERROR);
     }
 
     @Test
--- src/test/java/model/NameTest.java
+++ src/test/java/model/NameTest.java
@@ -1,5 +1,8 @@
 package model;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
@@ -7,10 +10,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.catchThrowable;
-
 public class NameTest {
+    private static final int MAXIMUM_NAME_LENGTH = 5;
+    private static final String MAXIMUM_NAME_LENGTH_ERROR = "[ERROR] 사람 최대 이름 길이는 %d 이하로만 가능합니다.";
+    private static final String NAME_HAS_NON_ALPHABETIC_ERROR = "[ERROR] 사람 이름은 영문자로만 입력되어야 합니다.";
+
     @Test
     @DisplayName("Name 객체 생성 성공 테스트")
     void createNameTest() {
@@ -22,26 +26,18 @@ public class NameTest {
     @Test
     @DisplayName("이름 값 길이 제한으로 인한 Player 객체 생성 실패 테스트")
     void limitPlayerNameLengthTest() {
-        //when
-        Throwable result = catchThrowable(() -> {
-            new Name("woowacourse");
-        });
-
-        //then
-        assertThat(result).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Name("woowacourse"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(String.format(MAXIMUM_NAME_LENGTH_ERROR, MAXIMUM_NAME_LENGTH));
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"1234", " ", "@#$@", "abs@#"})
     @DisplayName("사람 이름은 문자로만 이루어져 있는지 확인하는 기능 테스트")
     void validateNameHasOnlyCharacters(String inputName) {
-        //when
-        Throwable result = catchThrowable(() -> {
-            new Name(inputName);
-        });
-
-        //then
-        assertThat(result).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Name(inputName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(NAME_HAS_NON_ALPHABETIC_ERROR);
     }
 
     @Test
