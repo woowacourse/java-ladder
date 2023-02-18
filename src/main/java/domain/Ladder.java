f0e7b9e883a019d99e081913b01ff2fd54edc04e
xxeol2 <jhj01177@naver.com>
2023-02-18 15:00:45 +0900

    style: 불필요한 개행 삭제

--- src/main/java/domain/Ladder.java
+++ src/main/java/domain/Ladder.java
@@ -8,14 +8,13 @@ import java.util.List;
 
 public class Ladder {
 
-
     private final List<Line> lines;
 
     public Ladder(final List<Line> lines) {
         LadderValidator.validate(lines);
         this.lines = new ArrayList<>(lines);
     }
-    
+
     public List<Line> getLines() {
         return Collections.unmodifiableList(lines);
     }
