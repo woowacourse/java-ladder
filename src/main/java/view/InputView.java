997c625a1e82cd436c6363a7bf81758d146b6183
sosow0212 <sosow0212@naver.com>
2023-02-18 13:31:50 +0900

    feat: 실행 결과를 입력받는 기능 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -14,19 +14,20 @@ public class InputView {
     public List<String> readPlayerNames() {
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
         String playerNames = reader.next();
-        return Arrays.stream(splitNames(playerNames))
-                .collect(Collectors.toList());
-    }
-
-    private String[] splitNames(String playerNames) {
-        return playerNames.split(SPLIT_DELIMITER);
+        return splitInput(playerNames);
     }
 
     public int readHeight() {
         System.out.println("최대 사다리 높이는 몇 개인가요?");
-        String input = reader.next();
-        int ladderHeight = validateIntegerInput(input);
+        String height = reader.next();
+        int ladderHeight = validateIntegerInput(height);
         return ladderHeight;
+    }
+
+    public List<String> readLadderResults() {
+        System.out.println("실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
+        String ladderResults = reader.next();
+        return splitInput(ladderResults);
     }
 
     private int validateIntegerInput(String input) {
@@ -35,5 +36,10 @@ public class InputView {
         } catch (NumberFormatException exception) {
             throw new IllegalArgumentException("사다리 높이의 입력 값은 숫자만 가능합니다.");
         }
+    }
+
+    private List<String> splitInput(String input) {
+        return Arrays.stream(input.split(SPLIT_DELIMITER))
+                .collect(Collectors.toList());
     }
 }
