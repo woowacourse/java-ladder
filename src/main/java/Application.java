65555d8ea0e2e97f0d79ab2eb247bd4b06b688f6
hyunseo <ohseo8813@gmail.com>
2023-02-18 10:11:02 +0900

    refactor: LadderControllerFactory를 제거하여 코드 복잡성 개선

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,10 +1,13 @@
-import config.LadderControllerFactory;
 import controller.LadderController;
+import utils.RandomNumberGenerator;
+import view.InputView;
+import view.OutputView;
 
 public class Application {
 
     public static void main(String[] args) {
-        LadderController ladderController = LadderControllerFactory.generate();
+        LadderController ladderController = new LadderController(new InputView(), new OutputView(),
+                new RandomNumberGenerator());
         ladderController.run();
     }
 }
