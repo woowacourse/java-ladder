df715d55f885f46d4dfd54bf0990c96142bc9710
Cyma-s <ssol0319@naver.com>
2023-02-18 20:58:59 +0900

    refactor: 연속되는 getter를 분리하여 리팩터링

--- src/main/java/ladder/domain/Players.java
+++ src/main/java/ladder/domain/Players.java
@@ -39,7 +39,9 @@ public class Players {
 
     public int getNameMaxLength() {
         return this.players.stream()
-                .map(player -> player.getName().getName().length())
+                .map(Player::getName)
+                .map(Name::getName)
+                .map(String::length)
                 .max(Integer::compareTo)
                 .orElseThrow(() -> new IllegalStateException("플레이어가 존재하지 않습니다."));
     }
