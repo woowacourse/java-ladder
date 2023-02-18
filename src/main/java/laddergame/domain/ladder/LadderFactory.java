03de6133a331afbb96378a4b14ffe7acf12718b0
jiwon <ljwon77@daum.net>
2023-02-18 14:37:44 +0900

    feat: LadderFactory에 캐싱 기능을 추가한다
    - 추가적으로 높이 범위에 대한 조건문에 대해 보기 쉽도록 수정하였습니다.

--- src/main/java/laddergame/domain/ladder/LadderFactory.java
+++ src/main/java/laddergame/domain/ladder/LadderFactory.java
@@ -4,7 +4,9 @@ import laddergame.domain.rung.Rungs;
 import laddergame.util.NumberGenerator;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static laddergame.domain.message.ErrorMessage.INVALID_HEIGHT_RANGE;
 import static laddergame.domain.message.ErrorMessage.INVALID_HEIGHT_TYPE;
@@ -14,6 +16,8 @@ public class LadderFactory {
     private static final int MIN_HEIGHT = 1;
     private static final int MAX_HEIGHT = 10_000;
     private static final int DEFAULT_COUNT = 1;
+    private static final Map<NumberGenerator, LadderFactory> CACHE = new HashMap<>();
+
     private final NumberGenerator rungNumberGenerator;
 
     private LadderFactory(final NumberGenerator rungNumberGenerator) {
@@ -21,7 +25,13 @@ public class LadderFactory {
     }
 
     public static LadderFactory create(final NumberGenerator rungNumberGenerator) {
-        return new LadderFactory(rungNumberGenerator);
+        if (CACHE.containsKey(rungNumberGenerator)) {
+            return CACHE.get(rungNumberGenerator);
+        }
+
+        LadderFactory ladderFactory = new LadderFactory(rungNumberGenerator);
+        CACHE.put(rungNumberGenerator, ladderFactory);
+        return ladderFactory;
     }
 
     public Ladder createLadder(final String height, final int participantCount) {
@@ -52,8 +62,9 @@ public class LadderFactory {
     }
 
     private void validateHeightRange(final int height) {
-        if (height < MIN_HEIGHT || height > MAX_HEIGHT) {
+        if (height < MIN_HEIGHT || MAX_HEIGHT < height) {
             throw new IllegalArgumentException(INVALID_HEIGHT_RANGE.getMessage());
         }
     }
 }
+
