8de591078350b8c64f66695678c77c4f51af40d7
kevstevie <jianna442@gmail.com>
2023-02-18 18:53:05 +0900

    feat: userName 제약 조건 추가
    전체 출력 커맨드 리터럴을 이름으로 사용할 수 없음

--- src/main/java/ladder/domain/UserName.java
+++ src/main/java/ladder/domain/UserName.java
@@ -5,6 +5,8 @@ public class UserName {
     private static final int MAX_NAME_LENGTH = 5;
     private static final int MINIMUM_NAME_LENGTH = 1;
     private static final String INVALID_LENGTH_MESSAGE = "유저 이름 길이는 공백이거나 " + MAX_NAME_LENGTH + " 초과일 수 없습니다.";
+    private static final String COMMAND_ALL = "all";
+    private static final String INVALID_USERNAME_COMMAND_ALL = "유저 이름은 " + COMMAND_ALL + " 일 수 없습니다.";
 
     private final String name;
 
@@ -17,8 +19,19 @@ public class UserName {
 
     private void validate(String name) {
 
+        validateLength(name);
+        validateCommandAll(name);
+    }
+
+    private void validateLength(String name) {
         if (name.length() > MAX_NAME_LENGTH || name.length() < MINIMUM_NAME_LENGTH) {
             throw new IllegalArgumentException(INVALID_LENGTH_MESSAGE);
+        }
+    }
+
+    private void validateCommandAll(String name) {
+        if (name.equals(COMMAND_ALL)) {
+            throw new IllegalArgumentException(INVALID_USERNAME_COMMAND_ALL);
         }
     }
 
--- src/test/java/ladder/domain/UserNameTest.java
+++ src/test/java/ladder/domain/UserNameTest.java
@@ -1,6 +1,7 @@
 package ladder.domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -9,20 +10,30 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class UserNameTest {
 
+    private static final String COMMAND_ALL = "all";
+
     @ParameterizedTest
     @ValueSource(strings = {"", "  ", "여섯글자이름"})
-    @DisplayName("이름은 공백이거나 여섯 글자 이상일 수 없다.")
+    @DisplayName("유저 이름은 공백이거나 여섯 글자 이상일 수 없다.")
     void userNameExceptionTest(String value) {
         assertThatThrownBy(() -> new UserName(value))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("초과");
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"1", "다섯글자임", "  공백제외5  "})
-    @DisplayName("유효성 검증 경계값을 검증하는 테스트")
+    @DisplayName("유저 이름 유효성 검증 경계값을 검증하는 테스트")
     void validNameTest(String value) {
         assertThatCode(() -> new UserName(value))
                 .doesNotThrowAnyException();
     }
 
+    @Test
+    @DisplayName("유저 이름은 all 일 수 없다.")
+    void notAllTest() {
+        assertThatThrownBy(() -> new UserName(COMMAND_ALL))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(COMMAND_ALL);
+    }
 }
