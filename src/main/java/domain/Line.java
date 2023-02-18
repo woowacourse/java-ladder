eb06c12fd779de48e48279552b167f61d8fe0e4f
ingpyo <juninpyo726@gmail.com>
2023-02-18 20:46:10 +0900

    refactor: 매직넘버 제거

--- src/main/java/domain/Line.java
+++ src/main/java/domain/Line.java
@@ -5,6 +5,10 @@ public class Line {
     private final TrueOrFalseGenerator trueOrFalseGenerator;
     private final List<Boolean> points;
 
+    private static final int FIRST_INDEX = 0;
+    private static final int INDEX_SIZE = 0;
+    private static final int BEFORE_INDEX = 1;
+
     public Line(int personCount, TrueOrFalseGenerator trueOrFalseGenerator) {
         this.trueOrFalseGenerator = trueOrFalseGenerator;
         this.points = makeLine(personCount);
@@ -16,16 +20,17 @@ public class Line {
 
     private List<Boolean> makeLine(int personCount) {
         List<Boolean> points = new ArrayList<>();
-        points.add(false);
-        for (int count = 1; count < personCount; count++) {
-            points.add(correctOverLapPoints(points.get(count - 1), trueOrFalseGenerator.generate()));
+        for (int count = FIRST_INDEX; count < personCount-BEFORE_INDEX; count++) {
+            points.add(correctOverLapPoints(points, trueOrFalseGenerator.generate(), count));
         }
-        points.remove(0);
         return points;
     }
 
-    private Boolean correctOverLapPoints(boolean previous, boolean current) {
-        if (!previous || !current) {
+    private Boolean correctOverLapPoints(List<Boolean> points, boolean current,int count) {
+        if (points.size() == INDEX_SIZE) {
+            return current;
+        }
+        if (!points.get(count-BEFORE_INDEX) || !current) {
             return current;
         }
         return false;
