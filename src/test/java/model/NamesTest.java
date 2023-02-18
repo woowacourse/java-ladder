b795bf51b8ccdd1d1191054783f8b79039900dea
donghae <donghae1999@gmail.com>
2023-02-18 12:38:52 +0900

    test: Names 실패 테스트 추가

--- src/test/java/model/NamesTest.java
+++ src/test/java/model/NamesTest.java
@@ -1,9 +1,10 @@
 package model;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.Arrays;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,8 +14,15 @@ public class NamesTest {
     @DisplayName("Names 객체 생성 성공 테스트")
     void createNamesTest() {
         Names names = new Names("pobi,crong,honux");
-
         assertThat(names.getNames().size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest(name = "Names 객체 생성 실패 테스트 name={0}")
+    @ValueSource(strings = {" ","   ","12345,45678","aaaaaa,bbbbbb"})
+    void createNamesTest(String input) {
+        Assertions.assertThatThrownBy(()->{
+            new Names(input);
+        });
     }
 
 }
