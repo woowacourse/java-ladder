f89e4b9b83eb27657608095ad7c3e1ce5306ebf4
thdwoqor <celebarte01@gmail.com>
2023-02-18 23:48:19 +0900

    test : 입력 결과값 유효성 검증 테스트 작성
    (cherry picked from commit 5e3a14f6bdff93bbc31caaecd80dd882e3c4300b)

---
+++ src/test/java/domain/ResultTest.java
@@ -0,0 +1,24 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class ResultTest {
+    @DisplayName("입력된 결과값 개수가 유저수와 다르면 예외가 발생한다.")
+    @ParameterizedTest
+    @MethodSource("resultFailParameter")
+    void usersSizeFailTest(List<String> input) {
+        org.assertj.core.api.Assertions.assertThatThrownBy(() -> new Result(input, 3))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    static Stream<Arguments> resultFailParameter() {
+        return Stream.of(Arguments.of(List.of("aa", "bb")),
+                Arguments.of(List.of("aa", "bb", "cc", "dd")));
+    }
+}
