16247e9fae13e45d320688cc656cc2ac505f4e28
Jaeyoung22 <yt930310@gmail.com>
2023-02-18 10:21:52 +0900

    refactor(NameTest): 테스트의 목적을 명확히 하기 위해 에러 메세지도 검증하도록 수정

--- src/test/java/ladder/domain/NameTest.java
+++ src/test/java/ladder/domain/NameTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import ladder.error.ErrorMessage;
+
 class NameTest {
 
     @ParameterizedTest(name = "이름은 1글자 이상 5글자 이하여야 한다.")
@@ -20,27 +22,31 @@ class NameTest {
     @DisplayName("이름은 5글자 이하여야 한다.")
     void createNameFailByLengthTest() {
         assertThatThrownBy(() -> new Name("ABCDEF")).
-            isInstanceOf(IllegalArgumentException.class);
+            isInstanceOf(IllegalArgumentException.class)
+            .hasMessage(ErrorMessage.INVALID_NAME_LENGTH.getMessage());
     }
 
     @Test
     @DisplayName("이름은 빈 문자열일 수 없다.")
     void createNameFailByEmptyInputTest() {
         assertThatThrownBy(() -> new Name(""))
-            .isInstanceOf(IllegalArgumentException.class);
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage(ErrorMessage.INVALID_NAME_LENGTH.getMessage());
     }
 
     @Test
     @DisplayName("이름은 null일 수 없다.")
     void createNameFailByNullTest() {
         assertThatThrownBy(() -> new Name(null))
-            .isInstanceOf(IllegalArgumentException.class);
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage(ErrorMessage.NAME_IS_NULL.getMessage());
     }
 
     @Test
     @DisplayName("이름에 쉼표(,)가 포함될 수 없다.")
     void createNameFailByIncludingCommaTest() {
         assertThatThrownBy(() -> new Name("yo,me"))
-            .isInstanceOf(IllegalArgumentException.class);
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage(ErrorMessage.INVALID_NAME_FORMAT.getMessage());
     }
 }
