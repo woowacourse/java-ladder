6ebf67f13c1780bf726b1981e6f0b99fabc5dc4d
donghae <donghae1999@gmail.com>
2023-02-18 12:14:44 +0900

    test: LadderHeightTest 코드 수정

--- src/test/java/model/LadderHeightTest.java
+++ src/test/java/model/LadderHeightTest.java
@@ -1,26 +1,23 @@
 package model;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.assertj.core.api.Assertions.*;
 
 public class LadderHeightTest {
-    @Test
-    @DisplayName("LadderHeight 객체 생성 성공 테스트")
-    void createLadderHeightTest() {
-        Assertions.assertThatNoException().isThrownBy(() -> new LadderHeight(5));
+    @ParameterizedTest(name = "LadderHeight가 {0} 일 경우 객체 생성 성공 테스트")
+    @ValueSource(ints = {1,3,5})
+    void createLadderHeightTest(int input) {
+        Assertions.assertThatNoException().isThrownBy(() -> new LadderHeight(input));
     }
 
-    @Test
-    @DisplayName("LadderHeight가 1 미만일 경우 실패 테스트")
-    void validateLadderHeightTest() {
-        //When
-        Throwable result = catchThrowable(() -> new LadderHeight(0));
-
-        //Then
-        assertThat(result).isInstanceOf(IllegalArgumentException.class);
+    @ParameterizedTest(name = "LadderHeight가 {0} 일 경우 실패 테스트")
+    @ValueSource(ints = {0,-1})
+    void validateLadderHeightTest(int input) {
+        assertThatThrownBy(
+                ()->{new LadderHeight(input);}
+        ).isInstanceOf(IllegalArgumentException.class);
     }
 }
