60a95f55ae887d362b401ea57aa4b0af9bf81f9e
Choi-JJunho <junho5336@gmail.com>
2023-02-18 21:57:17 +0900

    fix: 테스트 수정

--- src/test/java/ladder/domain/LadderGameTest.java
+++ src/test/java/ladder/domain/LadderGameTest.java
@@ -11,12 +11,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class LadderGameTest {
 
-    private final List<String> players = List.of("도이", "주노");
+    private final List<Player> players = List.of(
+            new Player("도이"),
+            new Player("주노")
+    );
 
     @Test
     void 사다리는_높이_만큼의_라인을_가진다() {
         int height = 4;
-        LadderGame ladderGame = new LadderGame(players, height);
+        LadderGame ladderGame = new LadderGame(new Players(players), height);
         assertThat(ladderGame.toUnmodifiableLines())
                 .hasSize(height);
     }
@@ -24,7 +27,7 @@ class LadderGameTest {
     @Test
     void 사다리는_주어진_인원_수_만큼의_참가자를_가진다() {
         int height = 4;
-        LadderGame ladderGame = new LadderGame(players, height);
+        LadderGame ladderGame = new LadderGame(new Players(players), height);
         assertThat(ladderGame.toUnmodifiablePlayers())
                 .hasSize(players.size());
     }
