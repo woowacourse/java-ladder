6e1900f346f2a49a034bc4640aa3b42d2f67f8f2
MoonJeWoong <munjin0201@naver.com>
2023-02-18 17:01:14 +0900

    refactor: Exception 처리시 누락된 에러 메세지 추가

--- src/main/java/model/LadderHeight.java
+++ src/main/java/model/LadderHeight.java
@@ -2,6 +2,7 @@ package model;
 
 public class LadderHeight {
 
+    private static final String MINIMUM_LADDER_HEIGHT_ERROR = "[ERROR] 사다리 높이는 최소 1 이상의 값을 입력해야 합니다.";
     private static final int MINIMUM_LADDER_HEIGHT = 1;
     private final int height;
 
@@ -16,7 +17,7 @@ public class LadderHeight {
 
     private void validateHeight(int height) {
         if(height < MINIMUM_LADDER_HEIGHT) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(MINIMUM_LADDER_HEIGHT_ERROR);
         }
     }
 }
--- src/main/java/model/Name.java
+++ src/main/java/model/Name.java
@@ -5,7 +5,11 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Name {
+
     private static final int MAXIMUM_NAME_LENGTH = 5;
+    private static final String MAXIMUM_NAME_LENGTH_ERROR = "[ERROR] 사람 최대 이름 길이는 %d 이하로만 가능합니다.";
+    private static final String NAME_HAS_NON_ALPHABETIC_ERROR = "[ERROR] 사람 이름은 영문자로만 입력되어야 합니다.";
+
     private static final Pattern pattern = Pattern.compile("^[a-zA-Z]+$");
 
     private final String name;
@@ -39,14 +43,14 @@ public class Name {
 
     private void validateNameLength(String name){
         if(name.length() > MAXIMUM_NAME_LENGTH) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(String.format(MAXIMUM_NAME_LENGTH_ERROR, MAXIMUM_NAME_LENGTH));
         }
     }
 
     private void validateNameHasOnlyCharacters(String name) {
         Matcher matcher = pattern.matcher(name);
         if (!matcher.matches()) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(NAME_HAS_NON_ALPHABETIC_ERROR);
         }
     }
 }
