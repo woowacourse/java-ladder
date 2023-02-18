f7347c7c193396871c65227ef4905c156deaf5db
thdwoqor <celebarte01@gmail.com>
2023-02-19 05:22:40 +0900

    feat : Ladder, Generator 변경으로 인한 코드 수정

--- src/main/java/domain/Ladders.java
+++ src/main/java/domain/Ladders.java
@@ -2,28 +2,26 @@ package domain;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Supplier;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
-public class Ladders implements Supplier<List<Ladder>> {
-    private final Height height;
+public class Ladders {
     private final List<Ladder> ladders;
-    private final LadderGenerator ladderGenerator;
 
-    public Ladders(Height height, LadderGenerator ladderGenerator) {
-        this.height = height;
-        this.ladderGenerator = ladderGenerator;
-        this.ladders = generator();
+    public Ladders(int width, Height height, BooleanGenerator booleanGenerator) {
+        this.ladders = new ArrayList<>();
+        generate(width, height, booleanGenerator);
     }
 
-    private List<Ladder> generator() {
-        return IntStream.range(0, height.getHeight())
-                .mapToObj(i->new Ladder(ladderGenerator.generate()))
-                .collect(Collectors.toList());
+    private void generate(int width, Height height, BooleanGenerator booleanGenerator) {
+        for (int i = 0; i < height.getHeight(); i++) {
+            ladders.add(new Ladder(width, booleanGenerator));
+        }
     }
-    @Override
-    public List<Ladder> get() {
-        return new ArrayList<>(ladders);
+
+    public int getHeight() {
+        return ladders.size();
+    }
+
+    public List<Ladder> getLadders() {
+        return ladders;
     }
 }
