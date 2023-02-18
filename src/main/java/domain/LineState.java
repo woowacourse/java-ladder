fc468590f94be75b7e8ab4c7062c15b11aee9fd1
aiaiaiai1 <cvc100326@naver.com>
2023-02-19 00:33:52 +0900

    refactor: for문과 if문대신 stream 사용 + 예외 메시지 추가
    - indent 2 -> 1로 변경

--- src/main/java/domain/LineState.java
+++ src/main/java/domain/LineState.java
@@ -1,5 +1,8 @@
 package domain;
 
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
 public enum LineState {
     MOVABLE_STATE(1, true),
     UNMOVABLE_STATE(0, false);
@@ -17,11 +20,14 @@ public enum LineState {
     }
 
     public static LineState of(int value) {
-        for (LineState lineState : LineState.values()) {
-            if (lineState.value == value) {
-                return lineState;
-            }
+        try {
+            return Arrays.stream(LineState.values())
+                    .filter(s -> s.value == value)
+                    .findAny()
+                    .get();
+        } catch (NoSuchElementException e) {
+            throw new IllegalArgumentException("[ERROR] 잘못된 오류 발생 ");
         }
-        throw new IllegalArgumentException();
     }
+
 }
