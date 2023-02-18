c1ecb43085c07853cc6d004225749a2df01ce8c2
kiarakim <kiarakim413@gmail.com>
2023-02-18 21:35:27 +0900

    refactor(OutputView) : 사다리 출력 문자열 상수화

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -4,6 +4,11 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class OutputView {
+	public static final String FIRST_SECTION = "    |";
+	public static final String SECTION = "|";
+	public static final String ONE_STOOL = "-----";
+	public static final String NO_STOOL = "     ";
+
 	public void printResult(List<String> names, List<List<Boolean>> ladder) {
 		System.out.println("\n실행결과\n");
 		printNames(names);
@@ -11,15 +16,15 @@ public class OutputView {
 	}
 
 	private static String makeLevelView(List<Boolean> level) {
-		return "    |" + level.stream()
+		return FIRST_SECTION + level.stream()
 			.map(OutputView::makeStoolView)
-			.collect(Collectors.joining("|")) + "|";
+			.collect(Collectors.joining(SECTION)) + SECTION;
 	}
 
 	private static String makeStoolView(boolean stool) {
 		if (stool)
-			return "-----";
-		return "     ";
+			return ONE_STOOL;
+		return NO_STOOL;
 	}
 
 	private void printNames(List<String> names) {
