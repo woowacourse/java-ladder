0880f5ce8952d38a44331f96f2c04331e1b07456
thdwoqor <celebarte01@gmail.com>
2023-02-19 04:38:49 +0900

    feat : `Supplier<Boolean>` -> `BooleanGenerator` 변경
    (cherry picked from commit 71c9940ca9780b90ec28a1917a27b99d72e7f507)

--- src/test/java/domain/RandomGeneratorTest.java
+++ src/test/java/domain/RandomGeneratorTest.java
@@ -4,14 +4,13 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.*;
-import java.util.function.Supplier;
 
 public class RandomGeneratorTest {
     @Test
     void generateRandomNumbers() {
         Queue<Boolean> randomNumber = new LinkedList<>();
         Arrays.asList(true, false, false, true, false, false).forEach(randomNumber::add);
-        Supplier<Boolean> customRandomGenerator = new CustomRandomGenerator(randomNumber);
+        BooleanGenerator customRandomGenerator = new CustomRandomGenerator(randomNumber);
 
         List<Boolean> result = new ArrayList<>();
         for (int i = 0; i < randomNumber.size(); i++) {
@@ -19,18 +18,5 @@ public class RandomGeneratorTest {
         }
 
         Assertions.assertThat(result).isEqualTo(randomNumber);
-    }
-
-    public class CustomRandomGenerator implements Supplier<Boolean> {
-        private final Queue<Boolean> randomNumberQueue;
-
-        public CustomRandomGenerator(Queue<Boolean> randomNumberQueue) {
-            this.randomNumberQueue = randomNumberQueue;
-        }
-
-        @Override
-        public Boolean get() {
-            return randomNumberQueue.poll();
-        }
     }
 }
