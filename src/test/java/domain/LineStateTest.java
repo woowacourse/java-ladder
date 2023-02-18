e1d36b3103be0ecd675c46d073256fe292ac255d
aiaiaiai1 <cvc100326@naver.com>
2023-02-19 00:35:27 +0900

    test: LineState에 해당되는 값으로 상태값을 확인하는 테스트 추가

---
+++ src/test/java/domain/LineStateTest.java
@@ -0,0 +1,32 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class LineStateTest {
+    @DisplayName("값이 1인경우 상태값은 true이다.")
+    @Test
+    void value_1_true() {
+        boolean result = LineState.of(1).getState();
+        assertThat(result).isTrue();
+    }
+
+    @DisplayName("값이 0인 경우 상태값은 false이다.")
+    @Test
+    void value_2_false() {
+        boolean result = LineState.of(0).getState();
+        assertThat(result).isFalse();
+    }
+
+    @DisplayName("값이 0과 1이 아닌 경우 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {-1,2})
+    void value_else_Excpetion(int value) {
+        assertThatThrownBy(() -> LineState.of(value).getState())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
