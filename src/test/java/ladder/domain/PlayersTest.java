662e88d329fff7af473006ee6e4ed7903b850e86
Cyma-s <ssol0319@naver.com>
2023-02-18 20:48:36 +0900

    fix: 입력된 플레이어의 명수를 검증하는 생성자 테스트로 수정

--- src/test/java/ladder/domain/PlayersTest.java
+++ src/test/java/ladder/domain/PlayersTest.java
@@ -2,6 +2,8 @@ package ladder.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
@@ -30,15 +32,13 @@ class PlayersTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test
-    @DisplayName("플레이어가 없을 때 이름 최대 길이를 구하면 예외를 던진다.")
-    void players_throwException_WhenPlayersAreEmpty() {
-        // given
-        Players players = new Players(List.of());
-
+    @ParameterizedTest
+    @ValueSource(strings = {"", "vero"})
+    @DisplayName("플레이어가 1명 이하면 예외를 던진다.")
+    void players_throwException_WhenPlayersAreEmpty(String names) {
         // expected
-        assertThatThrownBy(players::getNameMaxLength)
-                .isInstanceOf(IllegalStateException.class);
+        assertThatThrownBy(() -> new Players(List.of(names.split(","))))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
