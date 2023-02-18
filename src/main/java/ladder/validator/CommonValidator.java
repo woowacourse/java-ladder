2c485cf269fb779be7db0c8c0b1194029955bfe9
JunH <tjdtls690@gmail.com>
2023-02-18 18:45:21 +0900

    :art: refactor : 유틸성 유효성 검증 클래스에서 생성자 막기

--- src/main/java/ladder/validator/CommonValidator.java
+++ src/main/java/ladder/validator/CommonValidator.java
@@ -3,6 +3,8 @@ package ladder.validator;
 import java.util.Objects;
 
 public class CommonValidator {
+    private CommonValidator() { }
+    
     public static void validateBlank(String input) {
         if (Objects.isNull(input) || input.isBlank()) {
             throw new IllegalArgumentException("빈 값을 입력할 수 없습니다.");
