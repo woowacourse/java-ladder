78b690ac9ca80fde4cee2531468c58fe22c7f9e5
MoonJeWoong <munjin0201@naver.com>
2023-02-18 18:47:23 +0900

    refactor: Line 클래스 내 불필요한 인스턴스 변수 삭제

--- src/main/java/model/Line.java
+++ src/main/java/model/Line.java
@@ -4,32 +4,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Line {
-
     private final List<Boolean> points = new ArrayList<>();
-    private final PointGenerator pointGenerator;
 
     public Line(PointGenerator pointGenerator, int personCount) {
-        this.pointGenerator = pointGenerator;
-        createPoints(personCount);
+        createPoints(pointGenerator, personCount);
     }
 
     public List<Boolean> getPoints() {
         return new ArrayList<>(points);
     }
 
-    private void createPoints(int personCount) {
+    private void createPoints(PointGenerator pointGenerator, int personCount) {
         int pointsCount = personCount - 1;
         points.add(pointGenerator.generate());
-        for(int index=0; index<pointsCount-1; index++){
-            points.add(makePointAvoidingTrueRepetition(index));
+        for (int index = 0; index < pointsCount - 1; index++) {
+            points.add(makePointAvoidingTrueRepetition(pointGenerator, index));
         }
     }
 
-    private Boolean makePointAvoidingTrueRepetition(int index) {
-        if(points.get(index).equals(true)){
+    private Boolean makePointAvoidingTrueRepetition(PointGenerator pointGenerator, int index) {
+        if (points.get(index).equals(true)) {
             return Boolean.FALSE;
         }
         return pointGenerator.generate();
     }
-
 }
