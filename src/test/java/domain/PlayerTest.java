d6d6f76161bf1dc944051a7fd61b50204d395065
Kong-hana01 <gksqlsl11@khu.ac.kr>
2023-02-18 14:45:52 +0900

    test(Player): 이름의 앞뒤 공백을 제대로 제거하여 플레이어를 생성하는지에 대한 테스트 변경

--- src/test/java/domain/PlayerTest.java
+++ src/test/java/domain/PlayerTest.java
@@ -1,20 +1,21 @@
 package domain;
 
-import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class PlayerTest {
 
-    @DisplayName("사람 이름의 앞 뒤 공백은 무시한다.")
+    @DisplayName("앞 뒤 공백을 제외한 이름의 길이가 1글자부터 5글자 사이인 경우 플레이어가 올바르게 생성된다.")
     @ParameterizedTest
-    @ValueSource(strings = {"  kong", "kong  ", " ko ng ", "    kong   "})
-    void validateTest(String name) {
-        assertThatCode(() -> new Player(name))
-                .doesNotThrowAnyException();
+    @CsvSource(value = {"  kong:4", "kong  :4", "ko ng:5"}, delimiter = ':')
+    void validateTest(String name, int length) {
+        Player player = new Player(name);
+        assertThat(player.getName().length()).isEqualTo(length);
     }
 
     @DisplayName("사람 이름은 1글자에서 5글자 사이이다.")
@@ -26,7 +27,7 @@ public class PlayerTest {
                 .hasMessage("사람 이름은 1글자에서 5글자 사이이어야 합니다.");
     }
 
-    @DisplayName("사람 이름이 정상적으로 입력되는 경우에는 이름이 생성된다.")
+    @DisplayName("사람 이름이 1글자부터 5글자 사이로 입력되는 경우에는 플레이어가 생성된다.")
     @ParameterizedTest
     @ValueSource(strings = {"odo", "kong"})
     void validateTest3(String name) {
