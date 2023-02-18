27b26ab7600f6a90c4a3f12e814b5a2a60d98a2c
Rosie <dev.kayoung@gmail.com>
2023-02-19 02:09:12 +0900

    refactor: Line 생성시 인접한 true 존재 여부를 확인하는 메서드명 수정

--- src/main/java/laddergame/domain/Line.java
+++ src/main/java/laddergame/domain/Line.java
@@ -7,23 +7,22 @@ public class Line {
     private final List<Boolean> points;
 
     public Line(final List<Boolean> points) {
-        validateNotEmpty(points);
-        validateNotBothTrue(points);
+        validate(points);
         this.points = points;
     }
 
-    private void validateNotBothTrue(final List<Boolean> points) {
-        boolean isBothTrue = IntStream.range(0, points.size() - 1)
-                .anyMatch(pointIndex -> points.get(pointIndex) && points.get(pointIndex + 1));
-        if (isBothTrue) {
+    private void validate(final List<Boolean> points) {
+        if (points.isEmpty()) {
             throw new IllegalArgumentException("boolean 리스트는 비어있을 수 없습니다.");
+        }
+        if (hasAdjacentTrue(points)) {
+            throw new IllegalArgumentException("사다리의 가로 라인은 겹칠 수 없습니다.");
         }
     }
 
-    private void validateNotEmpty(final List<Boolean> points) {
-        if (points.isEmpty()) {
-            throw new IllegalArgumentException("사다리의 가로 라인은 겹칠 수 없습니다.");
-        }
+    private static boolean hasAdjacentTrue(List<Boolean> points) {
+        return IntStream.range(0, points.size() - 1)
+                .anyMatch(pointIndex -> points.get(pointIndex) && points.get(pointIndex + 1));
     }
 
     public List<Boolean> getPoints() {
