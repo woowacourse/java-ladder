b8eadb335e4dde819217eb5665b8ff44b85419d2
chanminmaxscaler <jcm04@naver.com>
2023-02-18 20:30:39 +0900

    refactor: RungsTest 의 메서드 이름과 DisplayName의 내용을 수정한다

--- src/test/java/laddergame/domain/rung/RungsTest.java
+++ src/test/java/laddergame/domain/rung/RungsTest.java
@@ -12,7 +12,6 @@ import java.util.List;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 public class RungsTest {
 
@@ -25,31 +24,28 @@ public class RungsTest {
 
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3, 4, 5})
-    @DisplayName("Rungs 객체가 정상적으로 생성되는지 테스트한다.")
-    void create_test(int rungCount) {
-        assertDoesNotThrow(() -> Rungs.create(rungCount, rungNumberGenerator));
-
+    @DisplayName("rungCount와 numberGenerator를 입력하면, Rungs 객체가 생성되는지 확인한다.")
+    void succeeds_creation_if_rung_count_and_number_generator_are_entered(int rungCount) {
         assertThat(Rungs.create(rungCount, rungNumberGenerator))
                 .isInstanceOf(Rungs.class);
     }
 
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3, 4, 5})
-    @DisplayName("Rungs 객체 내부의 리스트의 길이가 rungCount와 같은지 확인한다.")
-    void create_rung_size_test(int rungCount) {
+    @DisplayName("Rungs 객체의 사이즈가 rungCount와 같은지 확인한다.")
+    void is_same_size_as_rung_count(int rungCount) {
         // when
         Rungs rungs = Rungs.create(rungCount, rungNumberGenerator);
         List<Rung> specificRungs = rungs.getRungs();
 
         // then
-        assertThat(specificRungs.size())
-                .isEqualTo(rungCount);
+        assertThat(specificRungs.size()).isEqualTo(rungCount);
     }
 
     @ParameterizedTest
     @MethodSource("getTestRungsWithNumberGenerator")
-    @DisplayName("NumberGenerator에 따라 Rungs의 형태가 달라지는지 확인한다.")
-    void create_rungs_test_by_number_generator(int rungCount, NumberGenerator numberGenerator, List<Rung> expectedRungs) {
+    @DisplayName("Rungs의 형태가 NumberGenerator에 따라 달라지는지 확인한다.")
+    void creates_various_rungs_according_to_number_generator(int rungCount, NumberGenerator numberGenerator, List<Rung> expectedRungs) {
         // given
         Rungs rungs = Rungs.create(rungCount, numberGenerator);
 
