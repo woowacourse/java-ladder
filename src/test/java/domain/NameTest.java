f1c8e4b7d672c5178774a2b1055d2901cbaf8579
yeonkkk <tjddus0302@gmail.com>
2023-02-19 01:06:13 +0900

    test: trimNameTest 추가, 공백 검증 테스트 삭제, ParameterizedTest 로 일괄 수정

--- src/test/java/domain/NameTest.java
+++ src/test/java/domain/NameTest.java
@@ -2,41 +2,33 @@ package domain;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("사용자의 이름은 ")
 class NameTest {
 
-    @DisplayName("5자 이하이다.")
-    @Test
-    public void nameTest_5() {
-        Assertions.assertDoesNotThrow(() -> new Name("12345"));
+    @DisplayName("1자 이상 5자 이하이다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"j", "joy", "pobi", "crong"})
+    public void nameTest_success(String name) {
+        Assertions.assertDoesNotThrow(
+            () -> new Name(name));
     }
 
-    @DisplayName("6자 이상이면 에러가 발생한다.")
-    @Test
-    public void nameTest_6() {
-        Assertions.assertThrows(IllegalArgumentException.class,
-                () -> new Name("123456"));
+    @DisplayName("1자 미만이거나 6자 이상이면 에러가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"", "joy kim"})
+    public void nameTest_fail(String name) {
+        Assertions.assertThrows(
+            IllegalArgumentException.class,
+                () -> new Name(name));
     }
 
-    @DisplayName("1자 이상이다.")
-    @Test
-    public void nameTest_1() {
-        Assertions.assertDoesNotThrow(() -> new Name("1"));
-    }
-
-    @DisplayName("1자 미만이면 예외가 발생한다.")
-    @Test
-    public void nameTest_0() {
-        Assertions.assertThrows(IllegalArgumentException.class,
-                () -> new Name(""));
-    }
-
-    @DisplayName("공백이 포함되면 예외가 발생한다.")
-    @Test
-    public void nameTest_blank() {
-        Assertions.assertThrows(IllegalArgumentException.class,
-                () -> new Name("joy "));
+    @DisplayName("처음이나 끝에 공백이 있으면 공백을 제거한다")
+    @ParameterizedTest
+    @ValueSource(strings = {" joy", " joy ", "joy "})
+    public void trimNameTest(String name) {
+        Assertions.assertEquals(new Name(name).getName(), name.trim());
     }
 }
