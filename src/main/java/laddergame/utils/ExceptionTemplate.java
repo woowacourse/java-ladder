055d24fb329bbcf88cdf18707db93627d99bf3a6
hyunseo <goatsfish@gmail.com>
2023-02-18 20:18:15 +0900

    test: 예외 템플릿 반복 초과 예외 발생 테스트 추가

--- src/main/java/laddergame/utils/ExceptionTemplate.java
+++ src/main/java/laddergame/utils/ExceptionTemplate.java
@@ -3,26 +3,21 @@ package laddergame.utils;
 import java.util.function.Supplier;
 
 public class ExceptionTemplate {
+    private static final String MAX_REPEAT_EXCEPTION = "가능한 최대 반복 횟수를 초과하였습니다.";
+    private static final int MAX_REPEAT_COUNT = 20;
 
     private ExceptionTemplate() {
     }
 
-    public static <T> T repeat(Supplier<T> supplier) {
-        while (true) {
-            try {
-                return supplier.get();
-            } catch (RuntimeException ignored) {
-                /* IGNORED */
-            }
-        }
-    }
     public static <T> T repeatAndPrintCause(Supplier<T> supplier) {
-        while (true) {
+        int repeatCount = 0;
+        while (repeatCount++ != MAX_REPEAT_COUNT) {
             try {
                 return supplier.get();
-            } catch (RuntimeException e) {
+            } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
         }
+        throw new IllegalStateException(MAX_REPEAT_EXCEPTION);
     }
 }
