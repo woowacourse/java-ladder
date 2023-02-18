264725e96d9e3e478ad8e52b46c6afd0e6f5843a
yeonkkk <tjddus0302@gmail.com>
2023-02-19 01:05:00 +0900

    refactor: validateNameFormat 삭제 및 이름의 처음과 끝에 공백이 있을 경우 제거되도록 수정

--- src/main/java/domain/Name.java
+++ src/main/java/domain/Name.java
@@ -1,24 +1,18 @@
 package domain;
 
-import static utils.ErrorMessage.INVALID_USER_NAME_FORMAT;
 import static utils.ErrorMessage.INVALID_USER_NAME_LENGTH;
 
 public class Name {
 
-    public final static String BLANK = " ";
     public final static int MAX_NAME_LENGTH = 5;
 
     private final int MIN_NAME_LENGTH = 1;
     private final String name;
 
     public Name(String name) {
-        validateName(name);
-        this.name = name;
-    }
-
-    private void validateName(String name) {
+        name = name.trim();
         validateNameLength(name);
-        validateNameFormat(name);
+        this.name = name;
     }
 
     private void validateNameLength(String name) {
@@ -26,12 +20,6 @@ public class Name {
             throw new IllegalArgumentException(
                 String.format(INVALID_USER_NAME_LENGTH.getMessage(),
                     MIN_NAME_LENGTH, MAX_NAME_LENGTH));
-        }
-    }
-
-    private void validateNameFormat(String name) {
-        if (name.contains(BLANK)) {
-            throw new IllegalArgumentException(INVALID_USER_NAME_FORMAT.getMessage());
         }
     }
 
--- src/main/java/utils/ErrorMessage.java
+++ src/main/java/utils/ErrorMessage.java
@@ -3,7 +3,6 @@ package utils;
 public enum ErrorMessage {
 
     INVALID_USER_NAME_LENGTH("이름의 길이는 %d 이상 %d 이하여야 합니다."),
-    INVALID_USER_NAME_FORMAT("이름에는 공백이 포함될 수 없습니다."),
     NOT_FOUND_USER("유저가 존재하지 않습니다."),
     INVALID_LADDER_HEIGHT_RANGE("사다리의 높이는 %d 이상입니다."),
     INVALID_LADDER_HEIGHT_INPUT("사다리의 높이는 숫자만 입력 가능합니다.");
