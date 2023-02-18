5ab7376865c1725b1b475c3193bd102dc01cdd5e
thdwoqor <celebarte01@gmail.com>
2023-02-19 04:40:03 +0900

    feat : 위치 정보 저장을 위한 Position enum 클래스 작성

---
+++ src/main/java/domain/Position.java
@@ -0,0 +1,29 @@
+package domain;
+
+public enum Position {
+    LEFT(-1,"     ",true),
+    RIGHT(1,"-----",false),
+    DOWN(0,"     ",true);
+
+    private final int move;
+    private final String format;
+    private final boolean moveAble;
+
+    Position(int move, String format,boolean moveAble) {
+        this.move = move;
+        this.format = format;
+        this.moveAble = moveAble;
+    }
+
+    public int getMove() {
+        return move;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public boolean isMovAble() {
+        return moveAble;
+    }
+}
