95dd1d7d22ab499d5d889bf00b4817e026432686
thdwoqor <celebarte01@gmail.com>
2023-02-19 05:21:05 +0900

    test : Ladders 테스트 코드 작성

--- src/test/java/domain/LaddersTest.java
+++ src/test/java/domain/LaddersTest.java
@@ -1,27 +1,32 @@
 package domain;
 
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThatCode;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.stream.Collectors;
 
 public class LaddersTest {
 
-    @ParameterizedTest
-    @ValueSource(ints = {0, 11})
-    @DisplayName("사다리 높이가 1~10을 벗어나면 예외가 발생한다.")
-    void LadderHeightFailTest(int height) {
-        Assertions.assertThrows(IllegalArgumentException.class,
-                () -> new Ladders(new Height(height), new LadderGenerator(height)));
-    }
+    @Test
+    void LaddersTest() {
+        Queue<Boolean> randomNumber = new LinkedList<>();
+        Arrays.asList(false, true, true).forEach(randomNumber::add);
+        List<List<Position>> result = new Ladders(2, new Height(3), new CustomRandomGenerator(randomNumber))
+                .getLadders()
+                .stream()
+                .map(Ladder::getLadder)
+                .collect(Collectors.toList());
 
-    @ParameterizedTest
-    @ValueSource(ints = {1, 10})
-    @DisplayName("사다리 높이가 1~10 사이면 정상적으로 수행된다.")
-    void LadderHeightSuccessTest(int height) {
-        assertThatCode(() -> new Ladders(new Height(height), new LadderGenerator(height))).doesNotThrowAnyException();
+        Assertions.assertThat(result)
+                .isEqualTo(Arrays.asList(
+                        Arrays.asList(Position.DOWN, Position.DOWN),
+                        Arrays.asList(Position.LEFT, Position.RIGHT),
+                        Arrays.asList(Position.LEFT, Position.RIGHT)
+                ));
     }
 }
