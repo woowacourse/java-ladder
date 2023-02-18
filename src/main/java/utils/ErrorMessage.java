b9d92d5bc7d235596512e6cd777057d0fb0082a4
yeonkkk <tjddus0302@gmail.com>
2023-02-19 01:46:14 +0900

    refactor: 잘못된 에러메세지 수정

--- src/main/java/utils/ErrorMessage.java
+++ src/main/java/utils/ErrorMessage.java
@@ -2,8 +2,8 @@ package utils;
 
 public enum ErrorMessage {
 
-    INVALID_USER_NAME_LENGTH_BY_MINIMUM_LIMIT("이름의 길이는 %d 이하여야 합니다."),
-    INVALID_USER_NAME_LENGTH_BY_MAXIMUM_LIMIT("이름의 길이는 %d 이상이여야 합니다."),
+    INVALID_USER_NAME_LENGTH_BY_MINIMUM_LIMIT("이름의 길이는 %d 이상이여야 합니다."),
+    INVALID_USER_NAME_LENGTH_BY_MAXIMUM_LIMIT("이름의 길이는 %d 이하여야 합니다."),
     NOT_FOUND_USER("유저가 존재하지 않습니다."),
     INVALID_LADDER_HEIGHT_RANGE("사다리의 높이는 %d 이상입니다."),
     INVALID_LADDER_HEIGHT_INPUT("사다리의 높이는 숫자만 입력 가능합니다.");
