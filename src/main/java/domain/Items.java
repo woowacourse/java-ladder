daddc19a5d74e7ca032c69054fdfa24f945281ac
thdwoqor <celebarte01@gmail.com>
2023-02-19 21:10:25 +0900

    refactor : 상수 분리
    (cherry picked from commit 15ea6a8461982801b8fec023c203624b09e9e0c0)

--- src/main/java/domain/Items.java
+++ src/main/java/domain/Items.java
@@ -4,12 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Items {
+    private static final String INVALID_ITEM_COUNT_MESSAGE = "결과 값의 개수는 유저 수와 같아야 합니다.";
     private final List<Item> items;
 
     public Items(List<Item> items, Users users) {
         this.items = items;
         if (items.size() != users.getCount()) {
-            throw new IllegalArgumentException("결과 값의 개수는 유저 수와 같아야 합니다.");
+            throw new IllegalArgumentException(INVALID_ITEM_COUNT_MESSAGE);
         }
     }
 
