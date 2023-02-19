e2bf216ed2563e1d5a5f230417801746673f8f20
thdwoqor <celebarte01@gmail.com>
2023-02-19 21:44:01 +0900

    feat : 네이밍 변경 및 enum을 사용하여 상태 체크
    (cherry picked from commit b8c877327d8b576cfe106443adca39618bae54a9)

--- src/main/java/controller/ResultController.java
+++ src/main/java/controller/ResultController.java
@@ -6,45 +6,35 @@ import domain.User;
 import view.InputView;
 import view.OutputView;
 
-import java.util.EnumMap;
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Map;
-import java.util.function.Supplier;
 
 public class ResultController {
     private final InputView inputView;
     private final OutputView outputView;
-    private final Map<Status, Supplier<Status>> controllerGuide;
     private final Result result;
 
     public ResultController(InputView inputView, OutputView outputView, Result result) {
         this.inputView = inputView;
         this.outputView = outputView;
-        this.controllerGuide = new EnumMap<>(Status.class);
         this.result = result;
-        initializeControllerGuide();
     }
 
-    public void process() {
+    public void run() {
         Status status = Status.CONTINUE;
         do {
             try {
-                status = controllerGuide.get(status).get();
+                status = process();
             } catch (IllegalArgumentException e) {
                 outputView.printExceptionMessage(e.getMessage());
-                status = Status.CONTINUE;
             }
-        } while (status.isEnd());
+        } while (status.isCONTINUE());
     }
 
-    private void initializeControllerGuide() {
-        controllerGuide.put(Status.CONTINUE, this::resultRepeat);
-    }
-
-    private Status resultRepeat() {
+    private Status process() {
         String command = inputView.inputWinner();
 
-        if (command.equals("end")) {
+        if (Status.from(command).equals(Status.END)) {
             return Status.END;
         }
         outputView.printUserResult(resultCommand(command, result));
@@ -53,18 +43,31 @@ public class ResultController {
     }
 
     private HashMap<User, Item> resultCommand(String command, Result result) {
-        if (command.equals("all")) {
+        if (Status.from(command).equals(Status.ALL)) {
             return result.getItemsALL();
         }
         return result.getItem(new User(command));
     }
 
     private enum Status {
-        END,
-        CONTINUE;
+        END("end"),
+        CONTINUE("continue"),
+        ALL("all");
+        private final String command;
 
-        public boolean isEnd() {
-            return this == CONTINUE;
+        Status(String command) {
+            this.command = command;
+        }
+
+        public boolean isCONTINUE() {
+            return this != END;
+        }
+
+        public static Status from(String command) {
+            return Arrays.stream(Status.values())
+                    .filter(option -> option.command.equals(command))
+                    .findAny()
+                    .orElse(Status.CONTINUE);
         }
     }
 }
