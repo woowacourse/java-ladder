7c85fd4309db6d94735cef771bb53db5d151ad4a
thdwoqor <celebarte01@gmail.com>
2023-02-19 17:57:41 +0900

    refactor : 공백 삭제
    (cherry picked from commit 245945fed5f5a77c40902695a08df189eba916f2)

--- src/main/java/domain/Height.java
+++ src/main/java/domain/Height.java
@@ -4,7 +4,6 @@ public class Height {
     private static final int LADDER_MIN_HEIGHT = 1;
     private static final int LADDER_MAX_HEIGHT = 10;
     private static final String INVALID_LADDER_HEIGHT_MESSAGE = "사다리 높이는 1~10 사이 정수만 가능합니다.";
-
     private final int height;
 
     public Height(int height) {
