6aaca1353b908fe6839fef76b785e191a007a1bf
seokjin8678 <seokjin8678@gmail.com>
2023-02-19 21:01:43 +0900

    feat: Height 클래스 생성 시 최대 값 제한 예외 테스트 코드 추가

--- src/test/java/ladder/domain/HeightTest.java
+++ src/test/java/ladder/domain/HeightTest.java
@@ -3,7 +3,6 @@ package ladder.domain;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 
-import ladder.domain.Height;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -12,13 +11,23 @@ import org.junit.jupiter.params.provider.ValueSource;
 class HeightTest {
 
     @ParameterizedTest
-    @DisplayName("양의 정수 외의 값이 들어오면 예외가 발생한다.")
+    @DisplayName("1보다 낮은 값이 들어오면 예외가 발생한다.")
     @ValueSource(ints = {Integer.MIN_VALUE, -1, 0})
-    void create_NotPositive(int input) {
+    void create_underThan1(int input) {
         // expect
         assertThatIllegalArgumentException().isThrownBy(() -> {
             new Height(input);
-        }).withMessage("[ERROR] 사다리의 높이는 양의 정수여야 합니다.");
+        }).withMessage("[ERROR] 사다리의 높이는 1~26 사이여야 합니다.");
+    }
+
+    @ParameterizedTest
+    @DisplayName("26보다 큰 값이 들어오면 예외가 발생한다.")
+    @ValueSource(ints = {27, 28, 29, Integer.MAX_VALUE})
+    void create_overThan26(int input) {
+        // expect
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            new Height(input);
+        }).withMessage("[ERROR] 사다리의 높이는 1~26 사이여야 합니다.");
     }
 
     @Test
@@ -26,7 +35,7 @@ class HeightTest {
     void create_success() {
         // expect
         assertThatNoException().isThrownBy(() -> {
-            new Height(30);
+            new Height(10);
         });
     }
 }
