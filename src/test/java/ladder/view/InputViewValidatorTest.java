56f6029994669e87b89141c8803b6a1e327ec4b3
sh111-coder <ohk9134@naver.com>
2023-02-19 16:27:33 +0900

    test(InputValidatorTest): ParameterizedTest로 테스트 케이스 추가 & AssertJ로 리팩토링

--- src/test/java/ladder/view/InputViewValidatorTest.java
+++ src/test/java/ladder/view/InputViewValidatorTest.java
@@ -1,29 +1,30 @@
 package ladder.view;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class InputViewValidatorTest {
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(ints = {10001, 20000, 30000, 10000000})
     @DisplayName("참여자의 수가 10000명을 넘으면 예외를 던진다.")
-    void exceedMaxPlayerCountTest() {
-        int count = 10001;
+    void exceedMaxPlayerCountTest(int maxPlayerCount) {
 
-        assertThrows(IllegalArgumentException.class,
-                () ->  InputViewValidator.validateNamesCount(count))
-                .getMessage().equals("[ERROR] 참여자의 수는 10000명 이하여야합니다.");
+        assertThatThrownBy(() -> InputViewValidator.validateNamesCount(maxPlayerCount))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage("[ERROR] 참여자의 수는 10000명 이하여야합니다.");
     }
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(strings = {"abc", "##", "#a", "가나다", "a1"})
     @DisplayName("사다리의 높이는 숫자여아한다.")
-    void ladderHeightIsNumericTest() {
-        String ladderHeight = "abc";
+    void ladderHeightIsNumericTest(String ladderHeight) {
 
-        assertThrows(IllegalArgumentException.class,
-                () -> InputViewValidator.validateNumeric(ladderHeight))
-                .getMessage().equals("[ERROR] 숫자만 입력해야합니다.");
+        assertThatThrownBy(() -> InputViewValidator.validateNumeric(ladderHeight))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage("[ERROR] 숫자만 입력해야합니다.");
     }
 }
