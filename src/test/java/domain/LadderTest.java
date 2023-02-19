0044e9ae2ea463f20a0b5c258f42993b6de9d404
aiaiaiai1 <cvc100326@naver.com>
2023-02-19 14:37:39 +0900

    test: DisplayName 작성 + assertDoesNotThrow사용

--- src/test/java/domain/LadderTest.java
+++ src/test/java/domain/LadderTest.java
@@ -1,23 +1,25 @@
 package domain;
 
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 public class LadderTest {
 
+    @DisplayName("사다리를 생성한다.")
     @Test
-    @DisplayName("")
-    void ladder() {
-        new Ladder(4, 4, new RandomDigitsGenerator());
+    void create_ladder() {
+        assertDoesNotThrow(() -> new Ladder(4, 4, new RandomDigitsGenerator()));
     }
 
+    @DisplayName("사다리 높이가 0 이하인 경우 예외가 발생한다.")
     @ParameterizedTest
     @ValueSource(ints = {0, -1})
-    @DisplayName("")
     void invalid_height(int height) {
         assertThatThrownBy(() -> new Ladder(height, 4, new RandomDigitsGenerator()))
                 .isInstanceOf(IllegalArgumentException.class);
