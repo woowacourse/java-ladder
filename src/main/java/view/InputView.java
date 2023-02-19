9e765eb1ce2b6ead5088971478e2dcaff0d8c244
yeonkkk <tjddus0302@gmail.com>
2023-02-19 12:56:00 +0900

    feat: 참여자 수에 대한 검증 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,6 +1,8 @@
 package view;
 
 import static utils.ErrorMessage.INVALID_LADDER_HEIGHT_INPUT;
+import static utils.ErrorMessage.INVALID_USER_NUMBER_NUMBER_BY_MAXIMUM_LIMIT;
+import static utils.ErrorMessage.INVALID_USER_NUMBER_NUMBER_BY_MINIMUM_LIMIT;
 
 import java.util.Arrays;
 import java.util.List;
@@ -8,13 +10,20 @@ import java.util.Scanner;
 
 public class InputView {
     private final String DELIMITER = ",";
+    private final int MINIMUM_USER_NUMBER_LIMIT = 1;
+    private final int MAXIMUM_USER_NUMBER_LIMIT = 100;
 
     private final Scanner scanner = new Scanner(System.in);
 
     public List<String> inputUserNames() {
         String userNames = scanner.nextLine();
-        
-        return Arrays.asList(userNames.split(DELIMITER));
+        List<String> allUserNames = Arrays.asList(
+            userNames.split(DELIMITER));
+
+        validateUserNamesNumberByMinimumLimit(allUserNames);
+        validateUserNamesNumberByMaximumLimit(allUserNames);
+
+        return allUserNames;
     }
 
     public int inputHeight() {
@@ -30,6 +39,24 @@ public class InputView {
             Integer.parseInt(height);
         } catch (Exception e) {
             throw new IllegalArgumentException(INVALID_LADDER_HEIGHT_INPUT.getMessage());
+        }
+    }
+
+    private void validateUserNamesNumberByMinimumLimit(List<String> allUserNames) {
+        if (allUserNames.size() < MINIMUM_USER_NUMBER_LIMIT) {
+            throw new IllegalArgumentException(
+                String.format(
+                INVALID_USER_NUMBER_NUMBER_BY_MINIMUM_LIMIT.getMessage(),
+                    MINIMUM_USER_NUMBER_LIMIT));
+        }
+    }
+
+    private void validateUserNamesNumberByMaximumLimit(List<String> allUserNames) {
+        if (allUserNames.size() > MAXIMUM_USER_NUMBER_LIMIT) {
+            throw new IllegalArgumentException(
+                String.format(
+                INVALID_USER_NUMBER_NUMBER_BY_MAXIMUM_LIMIT.getMessage(),
+                    MAXIMUM_USER_NUMBER_LIMIT));
         }
     }
 
