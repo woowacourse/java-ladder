b3bad9f067f65287e2edf3254bd20243ac1e41fb
woosung1223 <dntjd991223@naver.com>
2023-02-19 13:52:01 +0900

    chore: modifier 순서 컨벤션에 맞게 조정

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -7,9 +7,9 @@ public class InputView {
 
     private static final Scanner scanner = new Scanner(System.in);
 
-    private final static String DELIMITER = ",";
-    private final static String PLAYER_INPUT_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
-    private final static String LADDER_HEIGHT_INPUT_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
+    private static final String DELIMITER = ",";
+    private static final String PLAYER_INPUT_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    private static final String LADDER_HEIGHT_INPUT_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
 
     public static List<String> inputPlayer() {
         System.out.println(PLAYER_INPUT_MESSAGE);
