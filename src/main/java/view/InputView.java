fd1be405d4708cf697067927cb3e8a523abe84ed
aiaiaiai1 <cvc100326@naver.com>
2023-02-19 17:35:36 +0900

    refactor: 예외 메시지 작성

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -11,6 +11,8 @@ public class InputView {
     private static final String DELIMITER = ",";
     private static final String LADDER_HEIGHT_INPUT_NOTICE = "최대 사다리 높이는 몇 개인가요?";
 
+    private static final String NOT_INTEGER_ERROR = "[ERROR] 숫자가 아닙니다.";
+
     public List<String> readPersonsName(){
         System.out.println(NAME_INPUT_NOTICE);
         String[] names = readInput().split(DELIMITER);
@@ -26,7 +28,7 @@ public class InputView {
         try {
             return Integer.parseInt(scanner.next());
         }catch (NumberFormatException e){
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(NOT_INTEGER_ERROR);
         }
     }
 }
