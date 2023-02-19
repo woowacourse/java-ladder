26e79e091f6fb28c02215489b42fef990a568094
kwonyj1022 <kwonyj1022@naver.com>
2023-02-20 02:10:22 +0900

    remove(Line): bridge의 개수 검증 로직 제거

--- src/main/java/domain/ladder/Line.java
+++ src/main/java/domain/ladder/Line.java
@@ -1,28 +1,16 @@
 package domain.ladder;
 
 import domain.generator.BooleanGenerator;
-import utils.ErrorMessage;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Line {
 
-    private static final int BRIDGES_MIN_SIZE = 1;
-    private static final int BRIDGES_MAX_SIZE = 49;
-
     private List<Boolean> bridges = new ArrayList<>();
 
     public Line(int personCount, BooleanGenerator booleanGenerator) {
-        validate(personCount);
         createBridges(personCount, booleanGenerator);
-    }
-
-    private void validate(int personCount) {
-        int bridgeSize = personCount - 1;
-        if (bridgeSize < BRIDGES_MIN_SIZE || bridgeSize > BRIDGES_MAX_SIZE) {
-            throw new IllegalArgumentException(ErrorMessage.PLAYER_SIZE_ERROR.getMessage());
-        }
     }
 
     private void createBridges(int personCount, BooleanGenerator booleanGenerator) {
