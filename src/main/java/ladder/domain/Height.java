76cfc5b7afbbb0cb121de8f494e3881f9105c6ad
DESKTOP-FNVN0HH\USER <beatus96@naver.com>
2023-02-19 16:03:38 +0900

    refactor: Height 최소 높이 검사 기능 추가

--- src/main/java/ladder/domain/Height.java
+++ src/main/java/ladder/domain/Height.java
@@ -4,14 +4,22 @@ import java.util.Objects;
 
 public class Height {
 
+    private static final int MINIMUM_HEIGHT = 1;
     private final int height;
 
-    public Height(int height) {
+    public Height(final int height) {
+        validateHeight(height);
         this.height = height;
     }
 
     public int getHeight() {
         return height;
+    }
+
+    private void validateHeight(int inputHeight) {
+        if(inputHeight < MINIMUM_HEIGHT) {
+            throw new IllegalArgumentException("사다리의 높이는 1 이상이어야 합니다.");
+        }
     }
 
     @Override
