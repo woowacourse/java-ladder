ac26d2141ab2a48f4e34401431a770e1ff338c78
aiaiaiai1 <cvc100326@naver.com>
2023-02-19 18:38:31 +0900

    refactor: 입력메시지 오타 수정

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
-    private static final String NAME_INPUT_NOTICE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요";
+    private static final String NAME_INPUT_NOTICE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     private static final String DELIMITER = ",";
     private static final String LADDER_HEIGHT_INPUT_NOTICE = "최대 사다리 높이는 몇 개인가요?";
     private static final String NOT_INTEGER_ERROR = "[ERROR] 숫자가 아닙니다.";
