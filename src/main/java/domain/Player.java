74d8a3ed52ad72b3175738c335d732140aa6ab63
ingpyo <juninpyo726@gmail.com>
2023-02-19 21:36:28 +0900

    refactor: PlayerName 비교를 위한 equals 재정의

--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -1,6 +1,8 @@
 package domain;
 
 
+import java.util.Objects;
+
 public class Player {
     private final PlayerName name;
 
@@ -10,5 +12,20 @@ public class Player {
 
     public PlayerName getPlayerName() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Player player = (Player) o;
+
+        return Objects.equals(name, player.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
     }
 }
