12b4442c9eea560142b0b7595dfcc19cd0b22120
ChoiYoungHoon <dudgns2865@naver.com>
2023-02-19 17:38:55 +0900

    feat: 이름 입력 시 공백 제거하는 로직 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,13 +1,13 @@
 package view;
 
+import java.util.List;
+import java.util.Scanner;
 import validator.InputValidator;
 import validator.dto.InputValidationRequest;
 import validator.type.ValidateType;
 
-import java.util.List;
-import java.util.Scanner;
-
 public class InputView {
+
     private static final String INPUT_NAMES_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     private static final String INPUT_LADDER_HEIGHT_MESSAGE = "\n최대 사다리 높이는 몇 개인가요?";
     private static final String NAME_DELIMITER = ",";
@@ -20,13 +20,14 @@ public class InputView {
 
     public List<String> inputNames() {
         System.out.println(INPUT_NAMES_MESSAGE);
-        return List.of(scanner.nextLine().trim().split(NAME_DELIMITER));
+        return List.of(scanner.nextLine().replace(" ", "").trim().split(NAME_DELIMITER));
     }
 
     public int inputLadderHeight() {
         System.out.println(INPUT_LADDER_HEIGHT_MESSAGE);
         String input = scanner.nextLine();
-        validator.validate(new InputValidationRequest(List.of(ValidateType.EMPTY_VALUE, ValidateType.INTEGER_VALUE), input));
+        validator.validate(new InputValidationRequest(
+            List.of(ValidateType.EMPTY_VALUE, ValidateType.INTEGER_VALUE), input));
         return Integer.parseInt(input);
     }
 }
