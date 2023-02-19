3006d00bc409ddb107a66adda4f55177b46a4250
hyunseo <goatsfish@gmail.com>
2023-02-19 19:37:51 +0900

    test: Participants 생성자 테스트 수정

--- src/test/java/laddergame/domain/ParticipantsTest.java
+++ src/test/java/laddergame/domain/ParticipantsTest.java
@@ -1,7 +1,6 @@
 package laddergame.domain;
 
 import laddergame.fixture.NameFixture;
-import laddergame.fixture.NamesFixture;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -20,14 +19,14 @@ class ParticipantsTest {
     @DisplayName("생성된다.")
     @ParameterizedTest(name = "names = {0}")
     @MethodSource("namesDummy")
-    void create(final Names names) {
+    void create(final List<String> names) {
         assertDoesNotThrow(() -> new Participants(names));
     }
 
     @DisplayName("이름 목록이 null일 경우 예외가 발생한다.")
     @Test
     void throwExceptionWhenNamesIsNull() {
-        final Names names = null;
+        final List<String> names = null;
 
         assertThatThrownBy(() -> new Participants(names))
                 .isInstanceOf(IllegalArgumentException.class);
@@ -36,9 +35,9 @@ class ParticipantsTest {
     @DisplayName("이름 목록의 크기를 가져온다.")
     @ParameterizedTest(name = "names = {0}")
     @MethodSource("namesDummy")
-    void getSize(final Names names) {
+    void getSize(final List<String> names) {
         final Participants participants = new Participants(names);
-        final int expectedSize = names.getSize();
+        final int expectedSize = names.size();
         final int totalSize = participants.getSize();
 
         assertThat(totalSize).isEqualTo(expectedSize);
@@ -46,33 +45,18 @@ class ParticipantsTest {
 
     @DisplayName("이름 목록을 가져온다.")
     @ParameterizedTest(name = "names = {0}, values = {1}")
-    @MethodSource("namesWithValueDummy")
-    void getNames(final Names names, final List<String> nameValues) {
+    @MethodSource("namesDummy")
+    void getNames(final List<String> names) {
         final Participants participants = new Participants(names);
-        assertThat(participants.getNames()).containsExactlyElementsOf(nameValues);
+        assertThat(participants.getNames()).containsExactlyElementsOf(names);
     }
 
     static Stream<Arguments> namesDummy() {
         return Stream.of(
-                Arguments.arguments(NamesFixture.createNamesSize2()),
-                Arguments.arguments(NamesFixture.createNamesSize3())
-        );
-    }
-
-    static Stream<Arguments> namesWithValueDummy() {
-        return Stream.of(
-                Arguments.arguments(
-                        new Names(List.of(NameFixture.createNameRosie(), NameFixture.createNameHyena())),
-                        List.of(NameFixture.createNameRosie().getValue(), NameFixture.createNameHyena().getValue())),
-                Arguments.arguments(
-                        new Names(List.of(NameFixture.createNameRosie(), NameFixture.createNameJayon())),
-                        List.of(NameFixture.createNameRosie().getValue(), NameFixture.createNameJayon().getValue())),
-                Arguments.arguments(
-                        new Names(List.of(NameFixture.createNameHyena(), NameFixture.createNameJayon())),
-                        List.of(NameFixture.createNameHyena().getValue(), NameFixture.createNameJayon().getValue())),
-                Arguments.arguments(
-                        new Names(List.of(NameFixture.createNameRosie(), NameFixture.createNameHyena(), NameFixture.createNameJayon())),
-                        List.of(NameFixture.createNameRosie().getValue(), NameFixture.createNameHyena().getValue(), NameFixture.createNameJayon().getValue()))
+                Arguments.arguments(List.of(NameFixture.createNameRosie().getValue(), NameFixture.createNameHyena().getValue())),
+                Arguments.arguments(List.of(NameFixture.createNameRosie().getValue(), NameFixture.createNameJayon().getValue())),
+                Arguments.arguments(List.of(NameFixture.createNameHyena().getValue(), NameFixture.createNameJayon().getValue())),
+                Arguments.arguments(List.of(NameFixture.createNameRosie().getValue(), NameFixture.createNameHyena().getValue(), NameFixture.createNameHyena().getValue()))
         );
     }
 }
--- src/test/java/laddergame/fixture/ParticipantsFixture.java
+++ src/test/java/laddergame/fixture/ParticipantsFixture.java
@@ -1,19 +1,17 @@
 package laddergame.fixture;
 
-import laddergame.domain.Name;
-import laddergame.domain.Names;
 import laddergame.domain.Participants;
 
 import java.util.List;
 
 public abstract class ParticipantsFixture {
     public static Participants createParticipantsSize2() {
-        final List<Name> names = List.of(new Name("rosie"), new Name("hyena"));
-        return new Participants(new Names(names));
+        final List<String> names = List.of("rosie", "hyena");
+        return new Participants(names);
     }
 
     public static Participants createParticipantsSize3() {
-        final List<Name> names = List.of(new Name("rosie"), new Name("hyena"), new Name("jayon"));
-        return new Participants(new Names(names));
+        final List<String> names = List.of("rosie", "hyena", "jayon");
+        return new Participants(names);
     }
 }
