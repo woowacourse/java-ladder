daf2fb3b7746ea6c61b7f89f9167a5d92229aa86
thdwoqor <celebarte01@gmail.com>
2023-02-19 15:20:55 +0900

    test : 결과 출력 테스트 코드 작성
    (cherry picked from commit 153c2328ac408461b3c05ef428b34a0a8b142fc4)

---
+++ src/test/java/domain/ResultTest.java
@@ -0,0 +1,32 @@
+package domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class ResultTest {
+    private Result result;
+
+    @BeforeEach
+    void init() {
+        Users users = new Users(List.of(new User("aa"), new User("bb"), new User("cc")));
+
+        Items items = new Items(List.of(new Item("1"), new Item("2"), new Item("3")), users.getCount());
+
+        Queue<Boolean> randomNumber = new LinkedList<>();
+        Arrays.asList(false, true, true, true).forEach(randomNumber::add);
+        Ladders ladders = new Ladders(users.getCount(), new Height(3), new CustomRandomGenerator(randomNumber));
+
+        this.result = new Result(users, items, ladders);
+    }
+
+    @Test
+    void getItem() {
+        Assertions.assertThat(result.getItem(new User("cc"))).isEqualTo("2");
+    }
+}
