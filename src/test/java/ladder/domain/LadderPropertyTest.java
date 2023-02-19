55cdba86f5795593858df1aa6106de3844f32164
sh111-coder <ohk9134@naver.com>
2023-02-19 17:22:57 +0900

    test(LadderPropertyTest): 사다리의 너비와 높이를 받아서 사다라 속성을 생성하는 테스트 추가

--- src/test/java/ladder/domain/LadderPropertyTest.java
+++ src/test/java/ladder/domain/LadderPropertyTest.java
@@ -2,11 +2,20 @@ package ladder.domain;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class LadderPropertyTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {"3, 5", "4, 6", "10, 20"})
+    @DisplayName("사다리의 너비와 높이를 받아서 사다리 속성을 생성한다.")
+    void generateLadderProperty(int width, int height) {
+        Assertions.assertDoesNotThrow(() -> new LadderProperty(width, height));
+    }
 
     @ParameterizedTest
     @ValueSource(ints = {-1, 0, 1001, 10000})
@@ -17,5 +26,4 @@ class LadderPropertyTest {
                         .isInstanceOf(IllegalArgumentException.class)
                         .hasMessage("[ERROR] 사다리의 높이는 1 이상 1000 이하여야합니다.");
     }
-
 }
