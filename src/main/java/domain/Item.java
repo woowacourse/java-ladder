5e7406429f65a025038f2edf7156210f102ddbe8
thdwoqor <celebarte01@gmail.com>
2023-02-19 21:06:38 +0900

    feat : Item 비교를 위해 equals 오버라이딩
    (cherry picked from commit bbace20991f6cf5dd3960d8c549104c29435612a)

--- src/main/java/domain/Item.java
+++ src/main/java/domain/Item.java
@@ -1,5 +1,7 @@
 package domain;
 
+import java.util.Objects;
+
 public class Item {
     private final String item;
 
@@ -9,5 +11,25 @@ public class Item {
 
     public String getItem() {
         return item;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item1 = (Item) o;
+        return Objects.equals(item, item1.item);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(item);
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "item='" + item + '\'' +
+                '}';
     }
 }
