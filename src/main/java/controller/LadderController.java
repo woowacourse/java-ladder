9f3ab16573b94fb9d17f09f79235b1104c6785c9
ChoiYoungHoon <dudgns2865@naver.com>
2023-02-19 17:32:26 +0900

    feat: Controller가 Ladder 객체만 활용하도록 수정

--- src/main/java/controller/LadderController.java
+++ src/main/java/controller/LadderController.java
@@ -1,34 +1,31 @@
 package controller;
 
 import domain.model.Ladder;
-import domain.service.LadderMaker;
 import domain.vo.Height;
 import domain.vo.Name;
 import domain.vo.Width;
+import java.util.List;
+import java.util.stream.Collectors;
 import view.InputView;
 import view.OutputView;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class LadderController {
+
     private final InputView inputView;
     private final OutputView outputView;
-    private final LadderMaker ladderMaker;
 
-    public LadderController(InputView inputView, OutputView outputView, LadderMaker ladderMaker) {
+    public LadderController(InputView inputView, OutputView outputView) {
         this.inputView = inputView;
         this.outputView = outputView;
-        this.ladderMaker = ladderMaker;
     }
 
     public void play() {
         List<Name> names = inputView.inputNames()
-                .stream()
-                .map(Name::new)
-                .collect(Collectors.toList());
-        int height = inputView.inputLadderHeight();
-        Ladder ladder = ladderMaker.make(new Height(height), new Width(names.size() - 1));
+            .stream()
+            .map(Name::new)
+            .collect(Collectors.toList());
+        Ladder ladder = Ladder.makeLadder(new Height(inputView.inputLadderHeight()),
+            new Width(names.size() - 1));
         outputView.printResult(names, ladder);
     }
 }
--- src/main/java/domain/service/BooleanGenerator.java
+++
@@ -1,6 +0,0 @@
-package domain.service;
-
-@FunctionalInterface
-public interface BooleanGenerator {
-    Boolean generate();
-}
