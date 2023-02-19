6c471a1dd41f861a856cc6403da1dbd9b4be2310
DESKTOP-FNVN0HH\USER <beatus96@naver.com>
2023-02-19 20:01:56 +0900

    refactor: Line 객체의 일급 컬렉션을 객체 내에서 직접 생성하는 것으로 수정

--- src/main/java/ladder/domain/Line.java
+++ src/main/java/ladder/domain/Line.java
@@ -8,8 +8,28 @@ public class Line {
 
     private final List<Bar> line;
 
-    public Line(List<Bar> bars) {
-        line = new ArrayList<>(bars);
+    public Line(final int numberOfPlayers, final RandomGenerator randomGenerator) {
+        line = createLine(numberOfPlayers, randomGenerator);
+    }
+
+    private List<Bar> createLine(final int widthOfLadder, final RandomGenerator randomGenerator) {
+        List<Bar> bars = new ArrayList<>();
+
+        for (int idx = 0; idx < widthOfLadder; idx++) {
+            Bar bar = createBar(bars, idx, randomGenerator);
+            bars.add(bar);
+        }
+
+        return bars;
+    }
+
+
+    private Bar createBar(List<Bar> bars, int index, RandomGenerator randomGenerator) {
+        if (bars.isEmpty() || bars.get(index - 1) == Bar.UNMOVABLE_BAR) {
+            return Bar.UNMOVABLE_BAR;
+        }
+
+        return Bar.from(randomGenerator.generateBoolean());
     }
 
     public List<Bar> getLine() {
--- src/test/java/ladder/domain/LineTest.java
+++ src/test/java/ladder/domain/LineTest.java
@@ -2,6 +2,7 @@ package ladder.domain;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -9,18 +10,25 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LineTest {
-
-    private final List<Bar> bars = List.of(Bar.MOVABLE_BAR, Bar.UNMOVABLE_BAR);
+    private int widthOfLadder;
     private Line line;
 
     @BeforeEach
     void setup() {
-        line = new Line(bars);
+        widthOfLadder = 5;
+        line = new Line(widthOfLadder, new MockRandomDataGenerator());
     }
 
     @Test
-    @DisplayName("Line의 가로 라인이 겹치지 않는지 확인한다.")
-    void getLineTest() {
+    @DisplayName("widthOfLadder 크기의 라인이 생성되는지 확인한다.")
+    void generateLineSizeTest() {
+        assertThat(line.getLine().size()).isEqualTo(widthOfLadder);
+    }
+
+    @RepeatedTest(100)
+    @DisplayName("생성된 Line의 가로가 겹치지 않는지 확인한다.")
+    void generateLineTest() {
+
         List<Bar> bars = line.getLine();
 
         for (int idx = 0; idx < bars.size() - 1; idx++) {
@@ -32,11 +40,5 @@ public class LineTest {
         }
     }
 
-    @Test
-    @DisplayName("생성된 Line의 변수가 입력한 값과 같은 값을 포함하는지 확인한다")
-    void checkLineTest() {
-        assertThat(line.getLine())
-                .isEqualTo(bars);
-    }
 
 }
