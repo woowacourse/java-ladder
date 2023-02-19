799a28b5120ea1e6bcfc51cf26ff7afd88c9875a
jiwon <ljwon77@daum.net>
2023-02-19 19:51:41 +0900

    feat: 사다리 결과의 앞뒤 공백을 제거하는 기능을 추가한다

--- src/main/java/laddergame/domain/ladder/LadderResult.java
+++ src/main/java/laddergame/domain/ladder/LadderResult.java
@@ -2,22 +2,29 @@ package laddergame.domain.ladder;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LadderResult {
 
     private static final String DELIMITER = ",";
 
-    private final List<String> ladderResults;
+    private final List<String> results;
 
     private LadderResult(final String results) {
-        ladderResults = splitLadderResults(results);
+        List<String> ladderResults = splitResults(results);
+        this.results = trimResults(ladderResults);
     }
 
     public static LadderResult create(final String results) {
         return new LadderResult(results);
     }
 
-    private List<String> splitLadderResults(final String results) {
+    private List<String> splitResults(final String results) {
         return Arrays.asList(results.split(DELIMITER));
+    }
+
+    private List<String> trimResults(final List<String> results) {
+        return results.stream().map(String::trim)
+                .collect(Collectors.toUnmodifiableList());
     }
 }
