078d64251c5ca70d7d8104e762f37bf0245367ee
yujamint <woawoa0513@gmail.com>
2023-02-19 13:31:28 +0900

    test: ParameterizedTest를 통한 테스트로 변경

--- src/test/java/domain/HeightTest.java
+++ src/test/java/domain/HeightTest.java
@@ -2,22 +2,25 @@ package domain;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("사다리의 높이는 ")
 class HeightTest {
 
     @DisplayName("1 이상입니다.")
-    @Test
-    public void heightRangeTest_1() {
-        Assertions.assertDoesNotThrow(() -> new Height(1));
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 10, 11})
+    public void heightRangeTest_1(int height) {
+        Assertions.assertDoesNotThrow(() -> new Height(height));
     }
 
     @DisplayName("1 미만일 때 예외가 발생합니다.")
-    @Test
-    public void heightRangeTest_0() {
+    @ParameterizedTest
+    @ValueSource(ints = {0, -1, -2, -10, -11})
+    public void heightRangeTest_0(int height) {
         Assertions.assertThrows(IllegalArgumentException.class,
-                () -> new Height(0));
+                () -> new Height(height));
     }
 
 }
--- src/test/java/domain/NameTest.java
+++ src/test/java/domain/NameTest.java
@@ -3,27 +3,32 @@ package domain;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("사용자의 이름은 ")
 class NameTest {
 
     @DisplayName("5자 이하이다.")
-    @Test
-    public void nameTest_5() {
-        Assertions.assertDoesNotThrow(() -> new Name("12345"));
+    @ParameterizedTest
+    @ValueSource(strings = {"5자이하", "1234", "12345"})
+    public void nameTest_5(String name) {
+        Assertions.assertDoesNotThrow(() -> new Name(name));
     }
 
     @DisplayName("6자 이상이면 에러가 발생한다.")
-    @Test
-    public void nameTest_6() {
+    @ParameterizedTest
+    @ValueSource(strings = {"6자이상문자열", "123456", "1234567"})
+    public void nameTest_6(String name) {
         Assertions.assertThrows(IllegalArgumentException.class,
-                () -> new Name("123456"));
+                () -> new Name(name));
     }
 
     @DisplayName("1자 이상이다.")
-    @Test
-    public void nameTest_1() {
-        Assertions.assertDoesNotThrow(() -> new Name("1"));
+    @ParameterizedTest
+    @ValueSource(strings = {"1자이상", "1", "12"})
+    public void nameTest_1(String name) {
+        Assertions.assertDoesNotThrow(() -> new Name(name));
     }
 
     @DisplayName("1자 미만이면 예외가 발생한다.")
@@ -34,9 +39,10 @@ class NameTest {
     }
 
     @DisplayName("공백이 포함되면 예외가 발생한다.")
-    @Test
-    public void nameTest_blank() {
+    @ParameterizedTest
+    @ValueSource(strings = {"중간 공백", " 시작공백", "끝공백 "})
+    public void nameTest_blank(String name) {
         Assertions.assertThrows(IllegalArgumentException.class,
-                () -> new Name("joy "));
+                () -> new Name(name));
     }
 }
