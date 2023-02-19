a56e0acffb0c3ad217b381901102941486dd6686
yujamint <woawoa0513@gmail.com>
2023-02-19 20:48:27 +0900

    refactor: 상수 접근 제어자 수정
    기존에는 사다리 최소 높이를 의미하는 상수를 public으로 두어 ErrorMessage enum에서 import하도록 했지만, String.format을 이용하여 ErrorMessage enum이 import하지 않고도 사다리 최소 높이를 알도록 함

--- src/main/java/utils/ErrorMessage.java
+++ src/main/java/utils/ErrorMessage.java
@@ -1,6 +1,5 @@
 package utils;
 
-import static domain.Height.MIN_HEIGHT;
 import static domain.Name.MAX_NAME_LENGTH;
 import static domain.Name.MIN_NAME_LENGTH;
 
@@ -9,7 +8,7 @@ public enum ErrorMessage {
     INVALID_USER_NAME_LENGTH(String.format("이름의 길이는 %d 이상 %d 이하여야 합니다.", MIN_NAME_LENGTH, MAX_NAME_LENGTH)),
     INVALID_USER_NAME_FORMAT("이름에는 공백이 포함될 수 없습니다."),
     NOT_FOUND_USER("유저가 존재하지 않습니다."),
-    INVALID_LADDER_HEIGHT_RANGE(String.format("사다리의 높이는 %d 이상입니다.", MIN_HEIGHT)),
+    INVALID_LADDER_HEIGHT_RANGE("사다리의 높이는 %d 이상입니다."),
     INVALID_LADDER_HEIGHT_INPUT("사다리의 높이는 숫자만 입력 가능합니다.");
 
     private final String message;
