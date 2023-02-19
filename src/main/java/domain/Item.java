3b45be14ebdf7b09e39469e2088054ef8035cbdf
thdwoqor <celebarte01@gmail.com>
2023-02-20 01:18:32 +0900

    feat : Item에 공백 예외 처리 코드 추가

--- src/main/java/domain/Item.java
+++ src/main/java/domain/Item.java
@@ -3,14 +3,22 @@ package domain;
 import java.util.Objects;
 
 public class Item {
+    private static final String INVALID_NAME_BLANK_MESSAGE = "이름은 공백으로만 이루어지면 안됩니다.";
     private final String item;
 
     public Item(String item) {
+        validateBlankName(item);
         this.item = item;
     }
 
     public String getItem() {
         return item;
+    }
+
+    private void validateBlankName(String name) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException(INVALID_NAME_BLANK_MESSAGE);
+        }
     }
 
     @Override
