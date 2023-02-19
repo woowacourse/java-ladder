0ab86963a31e2f62bfbd21ff550d62027bb2cc64
yoondgu <doyou2ing@naver.com>
2023-02-19 10:51:14 +0900

    fix: 실행 결과 출력 기능 개선, 하드코딩 제거

--- src/main/java/ladder/view/OutputView.java
+++ src/main/java/ladder/view/OutputView.java
@@ -18,37 +18,49 @@ import ladder.domain.StepPoint;
 public class OutputView {
 
     private static final String VERTICAL = "|";
-    private static final String STEP_EXIST = "-----";
-    private static final String STEP_NONE = "     ";
+    private static final String STEP_EXIST = "-";
+    private static final String STEP_NONE = " ";
     private static final String MESSAGE_GAME_RESULT = "실행 결과";
 
     public static void showGameResult(List<String> players, List<Line> lines) {
-        String result = players.stream()
-                .map(name -> String.format("%5s", name))
-                .collect(Collectors.joining());
+        int pointWidth = computeMaxNameLength(players);
 
         System.out.println(MESSAGE_GAME_RESULT);
-        System.out.println(result);
+        System.out.println(" " + extractPlayerNames(players, pointWidth));
         lines.stream()
-                .map(OutputView::extractLine)
+                .map(line -> extractLine(line, pointWidth))
                 .forEach(System.out::println);
     }
 
-    private static String extractLine(Line line) {
-        List<StepPoint> stepPoints = line.toUnmodifiableStepPoints();
-        StringJoiner result = new StringJoiner(VERTICAL, STEP_NONE + VERTICAL, VERTICAL);
-
-        for (StepPoint point : stepPoints) {
-            String stepPointFormat = toStepPointFormat(point);
-            result.add(stepPointFormat);
-        }
-        return result.toString();
+    private static String extractPlayerNames(List<String> players, int pointWidth) {
+        String nameFormat = "%" + pointWidth + "s";
+        return players.stream()
+                .map(name -> String.format(nameFormat, name))
+                .collect(Collectors.joining(" "));
     }
 
-    private static String toStepPointFormat(StepPoint point) {
-        if (point == StepPoint.EXIST) {
-            return STEP_EXIST;
+    private static int computeMaxNameLength(List<String> players) {
+        return players.stream()
+                .map(String::length)
+                .max(Integer::compareTo)
+                .orElseThrow(() -> new IllegalArgumentException("게임 참여자가 존재하지 않습니다."));
+    }
+
+    private static String extractLine(Line line, int pointWidth) {
+        List<StepPoint> stepPoints = line.toUnmodifiableStepPoints();
+        StringJoiner result = new StringJoiner(VERTICAL, VERTICAL, VERTICAL);
+
+        for (StepPoint point : stepPoints) {
+            String stepPointFormat = toStepPointFormat(point, pointWidth);
+            result.add(stepPointFormat);
         }
-        return STEP_NONE;
+        return toStepPointFormat(StepPoint.NONE, pointWidth) + result;
+    }
+
+    private static String toStepPointFormat(StepPoint point, int pointWidth) {
+        if (point == StepPoint.EXIST) {
+            return STEP_EXIST.repeat(pointWidth);
+        }
+        return STEP_NONE.repeat(pointWidth);
     }
 }
