f0633a8693e9536947cea234ab335ea49c070954
ChoiYoungHoon <dudgns2865@naver.com>
2023-02-19 17:30:02 +0900

    feat: Layer 객체가 팩토리 메소드로 Layer를 생성하도록 구현

--- src/main/java/domain/model/Layer.java
+++ src/main/java/domain/model/Layer.java
@@ -3,22 +3,29 @@ package domain.model;
 import domain.type.Line;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.IntStream;
 
 public class Layer {
 
     private final List<Line> lines;
+    private static final Random randomGenerator = new Random();
 
-    public Layer() {
-        this.lines = new ArrayList<>();
+    private Layer(List<Line> lines) {
+        this.lines = lines;
     }
 
-    public void makeLine(final Line line) {
-        if (line.equals(Line.CONNECTED) && lines.isEmpty()) {
-            lines.add(line);
-            return;
-        }
-        if (line.equals(Line.CONNECTED) && !lines.get(lines.size() - 1).equals(Line.UNCONNECTED)) {
-            lines.add(line);
+    public static Layer makeLayerByRandom(final int size) {
+        List<Line> lines = new ArrayList<>();
+        IntStream.range(0, size).mapToObj(index -> lines).forEach(Layer::makeLine);
+        return new Layer(lines);
+    }
+
+    private static void makeLine(final List<Line> lines) {
+        boolean randomBoolean = randomGenerator.nextBoolean();
+        if (randomBoolean &&
+            (lines.isEmpty() || lines.get(lines.size() - 1).equals(Line.UNCONNECTED))) {
+            lines.add(Line.CONNECTED);
             return;
         }
         lines.add(Line.UNCONNECTED);
