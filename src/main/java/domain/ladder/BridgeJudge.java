dd90d48c5491be435e28dca5a3a5e841db46ff3a
kwonyj1022 <kwonyj1022@naver.com>
2023-02-20 00:34:33 +0900

    refactor: isFirstIndexOrLeftEmpty() 메서드가 하는 일 분리 및 조건문 단순하게 수정

--- src/main/java/domain/ladder/BridgeJudge.java
+++ src/main/java/domain/ladder/BridgeJudge.java
@@ -5,16 +5,18 @@ import java.util.List;
 public class BridgeJudge {
 
     public static boolean canMake(List<Boolean> bridges, boolean flag, int index) {
-        if (flag == true) {
-            return isFirstIndexOrLeftEmpty(bridges, index);
+        if (flag) {
+            return isFirstLine(index) || isLeftEmpty(bridges, index);
         }
         return false;
     }
 
-    private static boolean isFirstIndexOrLeftEmpty(List<Boolean> bridges, int index) {
-        if (index == 0 || bridges.get(index - 1) == false) {
-            return true;
-        }
-        return false;
+
+    private static boolean isFirstLine(int index) {
+        return index == 0;
+    }
+
+    private static boolean isLeftEmpty(List<Boolean> bridges, int index) {
+        return !bridges.get(index - 1);
     }
 }
