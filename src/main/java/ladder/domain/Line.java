bc20e0af5e53047ff600518596372a66806101d9
hum02 <ehdgma02@gmail.com>
2023-02-19 23:57:52 +0900

    feat: 시작점에서 라인을 따라 움직인 도착점 계산 기능 구현

--- src/main/java/ladder/domain/Line.java
+++ src/main/java/ladder/domain/Line.java
@@ -6,13 +6,39 @@ import java.util.Collections;
 import java.util.List;
 
 public class Line {
+    private static final boolean LINKED = true;
     private final List<Boolean> sections;
 
     public Line(LineStrategy lineStrategy, int sectionCount) {
         this.sections = lineStrategy.generate(sectionCount);
     }
 
+    public int moveFrom(int startPosition) {
+        validateOutBound(startPosition);
+        if (isRightMoveAble(startPosition) && (sections.get(startPosition) == LINKED)) {
+            return startPosition + 1;
+        }
+        if (isLeftMoveAble(startPosition) && (sections.get(startPosition - 1) == LINKED)) {
+            return startPosition - 1;
+        }
+        return startPosition;
+    }
+
+    private boolean isRightMoveAble(int position) {
+        return position + 1 <= sections.size();
+    }
+
+    private boolean isLeftMoveAble(int position) {
+        return position - 1 >= 0;
+    }
+
     public List<Boolean> getLine() {
         return Collections.unmodifiableList(sections);
+    }
+
+    private void validateOutBound(int startPoint) {
+        if ((startPoint < 0) || (startPoint > sections.size())) {
+            throw new IllegalArgumentException("line 범위 밖의 시작점입니다.");
+        }
     }
 }
