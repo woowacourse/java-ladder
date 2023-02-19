fc53b46d94112bcccd547c7dcd54dd81b57acc11
seokjin8678 <seokjin8678@gmail.com>
2023-02-19 20:47:24 +0900

    refactor: Name 클래스 삭제, Player 클래스에 name 필드 이동
    - 원시값을 포장하려고 Name 클래스를 사용하였으나, Player 클래스가 이미 원시값을 포장하는 클래스이므로 불필요한 클래스 정리

--- src/main/java/ladder/domain/Player.java
+++ src/main/java/ladder/domain/Player.java
@@ -1,13 +1,40 @@
 package ladder.domain;
 
 public class Player {
-    private final Name name;
+    private static final int MAX_NAME_LENGTH = 5;
+
+    private final String name;
 
     public Player(String name) {
-        this.name = new Name(name);
+        validateName(name);
+        this.name = name;
+    }
+
+    private void validateName(String name) {
+        validateNullName(name);
+        validateBlankName(name);
+        validateNameLength(name);
+    }
+
+    private void validateNullName(String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("[ERROR] 이름에 null 값이 들어갈 수 없습니다.");
+        }
+    }
+
+    private void validateBlankName(String name) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("[ERROR] 이름은 공백이 될 수 없습니다.");
+        }
+    }
+
+    private void validateNameLength(String name) {
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("[ERROR] 이름이 5글자를 초과할 수 없습니다.");
+        }
     }
 
     public String getName() {
-        return name.getName();
+        return name;
     }
 }
--- src/test/java/ladder/domain/PlayerTest.java
+++ src/test/java/ladder/domain/PlayerTest.java
@@ -2,7 +2,6 @@ package ladder.domain;
 
 import static org.assertj.core.api.Assertions.*;
 
-import ladder.domain.Player;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
