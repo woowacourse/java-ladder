47ec3c98cf78a0741cbdb65ac6d0ab29d0771768
aiaiaiai1 <cvc100326@naver.com>
2023-02-19 18:35:42 +0900

    refactor: random 변수 필드로 이동

--- src/main/java/domain/RandomDigitsGenerator.java
+++ src/main/java/domain/RandomDigitsGenerator.java
@@ -4,10 +4,10 @@ import java.util.Random;
 
 public class RandomDigitsGenerator implements RandomGenerator {
     private static final int DIGIT_SIZE = 2;
+    private static final Random random = new Random();
 
     @Override
     public int generate() {
-        Random random = new Random();
         return random.nextInt(DIGIT_SIZE);
     }
 
