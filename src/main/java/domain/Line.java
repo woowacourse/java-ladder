073d6175377142951840a79a426d19d7f8513861
ingpyo <juninpyo726@gmail.com>
2023-02-19 22:47:13 +0900

    refactor: trueOrFalseGenerator객체 삭제

--- src/main/java/domain/Line.java
+++ src/main/java/domain/Line.java
@@ -1,36 +1,34 @@
+package domain;
+
+import util.TrueOrFalseGenerator;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Line {
-    private final TrueOrFalseGenerator trueOrFalseGenerator;
     private final List<Boolean> points;
 
-    private static final int FIRST_INDEX = 0;
-    private static final int INDEX_SIZE = 0;
-    private static final int BEFORE_INDEX = 1;
-
     public Line(int personCount, TrueOrFalseGenerator trueOrFalseGenerator) {
-        this.trueOrFalseGenerator = trueOrFalseGenerator;
-        this.points = makeLine(personCount);
+        this.points = makeLine(personCount,trueOrFalseGenerator);
     }
 
     public List<Boolean> getPoints() {
         return points;
     }
 
-    private List<Boolean> makeLine(int personCount) {
+    private List<Boolean> makeLine(int personCount,TrueOrFalseGenerator trueOrFalseGenerator) {
         List<Boolean> points = new ArrayList<>();
-        for (int count = FIRST_INDEX; count < personCount-BEFORE_INDEX; count++) {
+        for (int count = 0; count < personCount-1; count++) {
             points.add(correctOverLapPoints(points, trueOrFalseGenerator.generate(), count));
         }
         return points;
     }
 
     private Boolean correctOverLapPoints(List<Boolean> points, boolean current,int count) {
-        if (points.size() == INDEX_SIZE) {
+        if (points.size() == 0) {
             return current;
         }
-        if (!points.get(count-BEFORE_INDEX) || !current) {
+        if (!points.get(count-1) || !current) {
             return current;
         }
         return false;
