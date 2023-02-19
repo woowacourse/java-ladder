1c81eed0c088a0f29ea84a2cdb8a38ab68897ac2
jiwon <ljwon77@daum.net>
2023-02-19 18:09:30 +0900

    style: 개행 추가

--- src/main/java/laddergame/Application.java
+++ src/main/java/laddergame/Application.java
@@ -1,12 +1,15 @@
 package laddergame;
 
 import laddergame.controller.LadderGameController;
+import laddergame.domain.rung.RungGenerator;
 import laddergame.view.InputView;
 import laddergame.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        LadderGameController gameController = new LadderGameController(new InputView(), new OutputView());
+        LadderGameController gameController = new LadderGameController(
+                new InputView(), new OutputView(), new RungGenerator()
+        );
         gameController.start();
     }
 }
