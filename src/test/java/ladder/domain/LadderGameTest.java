ac8f1f863ae966666efaebf1962efabc608deb10
Wugawuga <dnr3655@naver.com>
2023-02-19 16:08:26 +0900

    refactor: 테스트 코드 분리

--- src/test/java/ladder/domain/LadderGameTest.java
+++ src/test/java/ladder/domain/LadderGameTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 import ladder.util.TestBooleanGenerator;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
@@ -13,28 +12,25 @@ import org.junit.jupiter.api.Test;
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class LadderGameTest {
 
-    private LadderGame ladderGame;
-
-    @BeforeEach
-    void init() {
-        final BooleanGenerator booleanGenerator = new TestBooleanGenerator(List.of(true, true));
-        final Players players = new Players(List.of("name1", "name2"));
-        final int height = 2;
-        ladderGame = new LadderGame(booleanGenerator, players, height);
-    }
-
     @Test
     void 참가자들의_이름을_반환한다() {
+        LadderGame ladderGame = init(new TestBooleanGenerator(List.of(true, true)), 1, "name1", "name2");
         assertThat(ladderGame.getPlayers()).containsExactly("name1", "name2");
     }
 
     @Test
     void 생성된_사다리를_반환한다() {
+        LadderGame ladderGame = init(new TestBooleanGenerator(List.of(true, true)), 2, "name1", "name2");
         assertThat(ladderGame.getLadder())
                 .extracting(Line::getLine)
                 .containsExactly(
                         List.of(LineStatus.CONNECTED),
                         List.of(LineStatus.CONNECTED)
                 );
+    }
+
+    private LadderGame init(BooleanGenerator booleanGenerator, int height, String... name) {
+        final Players players = new Players(List.of(name));
+        return new LadderGame(booleanGenerator, players, height);
     }
 }
--- src/test/java/ladder/domain/LadderTest.java
+++ src/test/java/ladder/domain/LadderTest.java
@@ -3,12 +3,15 @@ package ladder.domain;
 import static ladder.domain.LineStatus.CONNECTED;
 import static ladder.domain.LineStatus.DISCONNECTED;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import ladder.util.TestBooleanGenerator;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @SuppressWarnings("NonAsciiCharacters")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -17,7 +20,7 @@ public class LadderTest {
     @Test
     void 사다리는_라인을_가진다() {
         final BooleanGenerator booleanGenerator = new TestBooleanGenerator(List.of(false, false, true, false));
-        final Ladder ladder = new Ladder(booleanGenerator, 2, 2);
+        final Ladder ladder = getLadder(booleanGenerator, 2, 2);
 
         assertThat(ladder.getLines())
                 .extracting(Line::getLine)
@@ -25,5 +28,17 @@ public class LadderTest {
                         List.of(DISCONNECTED, DISCONNECTED),
                         List.of(CONNECTED, DISCONNECTED)
                 );
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 101})
+    void 사다리의_높이가_1미만_100초과시_예외를_던진다(int height) {
+        assertThatThrownBy(() -> getLadder(() -> true, height, 2))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("사다리 높이는 최소 1, 최대 100까지 가능합니다.");
+    }
+
+    private static Ladder getLadder(final BooleanGenerator booleanGenerator, final int height, final int width) {
+        return new Ladder(booleanGenerator, height, width);
     }
 }
