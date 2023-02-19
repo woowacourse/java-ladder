4dfbc3dc7c9451b287a83e8aca60fc54bdd9f2a6
ChoiYoungHoon <dudgns2865@naver.com>
2023-02-19 17:29:44 +0900

    test: Layer 객체가 팩토리 메소드로 Layer를 생성하는 테스트 코드 작성

--- src/test/java/domain/model/LayerTest.java
+++ src/test/java/domain/model/LayerTest.java
@@ -1,26 +1,46 @@
 package domain.model;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+
+import domain.type.Line;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class LayerTest {
 
     @Test
-    @DisplayName("다리는 연속해서 연결되지 않는다")
-    void makeLineTest() {
-        Layer layer = new Layer();
-        layer.makeLine(true);
-        layer.makeLine(true);
-        assertThat(layer.getLines().get(1)).isFalse();
+    @DisplayName("층이 랜덤으로 생성된다.")
+    public void makeLayerByRandom() {
+        //given
+        int size = 5;
+
+        //when
+        Layer layer = Layer.makeLayerByRandom(size);
+
+        //then
+        assertThat(layer.getLines().size()).isEqualTo(size);
     }
 
     @Test
-    @DisplayName("다리는 연결된다")
-    void makeLineTest2() {
-        Layer layer = new Layer();
-        layer.makeLine(true);
-        assertThat(layer.getLines().get(0)).isTrue();
+    @DisplayName("층이 랜덤으로 생성될 때 연속해서 연결 되지 않는다.")
+    public void makeLayerByRandomNoContinuousConnected() {
+        //given
+        int size = 5;
+
+        //when
+        Layer layer = Layer.makeLayerByRandom(size);
+
+        //then
+        for (int i = 1; i < size; i++) {
+            checkContinuousConnected(layer, i);
+        }
+    }
+
+    private void checkContinuousConnected(Layer layer, int i) {
+        if (layer.getLines().get(i - 1).equals(Line.CONNECTED) && layer.getLines().get(i)
+            .equals(Line.CONNECTED)) {
+            fail("연속해서 연결된 경우 실패");
+        }
     }
 }
