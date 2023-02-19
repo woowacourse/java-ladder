539bebf0dcff7ad66db9730506732491b3fa7909
iamjooon2 <iamjooon2@gmail.com>
2023-02-19 23:11:49 +0900

    refactor: Height 유효성 검사시 한 조건문 내에서 실행하도록 변경

--- src/main/java/domain/Height.java
+++ src/main/java/domain/Height.java
@@ -1,8 +1,7 @@
 package domain;
 
 public class Height {
-    private static final String ERROR_MIN_HEIGHT = "[ERROR] 높이는 2 이상이어야 합니다";
-    private static final String ERROR_MAX_HEIGHT = "[ERROR] 높이는 100 까지만 생성 가능합니다";
+    private static final String ERROR_HEIGHT_RANGE = "[ERROR] 사다리의 높이는 2 ~ 100 사이로 생성 가능합니다";
     private static final int MIN_HEIGHT_INCLUSIVE = 2;
     private static final int MAX_HEIGHT_INCLUSIVE = 100;
 
@@ -14,11 +13,8 @@ public class Height {
     }
 
     private static void validateHeightSize(int value) {
-        if (value < MIN_HEIGHT_INCLUSIVE) {
-            throw new IllegalArgumentException(ERROR_MIN_HEIGHT);
-        }
-        if (value > MAX_HEIGHT_INCLUSIVE) {
-            throw new IllegalArgumentException(ERROR_MAX_HEIGHT);
+        if (value < MIN_HEIGHT_INCLUSIVE || value > MAX_HEIGHT_INCLUSIVE) {
+            throw new IllegalArgumentException(ERROR_HEIGHT_RANGE);
         }
     }
 
