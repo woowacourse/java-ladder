d24b8f1497839906a839c6aa4c4363a772e19b3f
hyunseo <ohseo8813@gmail.com>
2023-02-19 13:28:42 +0900

    refactor: 과도한 메소드 추출 제거

--- src/main/java/domain/Name.java
+++ src/main/java/domain/Name.java
@@ -1,8 +1,5 @@
 package domain;
 
-
-import java.util.Objects;
-
 public class Name {
 
     private static final int MAX_NAME_LENGTH = 5;
@@ -15,17 +12,10 @@ public class Name {
     }
 
     private void validate(String name) {
-        validateBlank(name);
-        validateLength(name);
-    }
-
-    private void validateBlank(String name) {
         if (name.isBlank()) {
             throw new IllegalArgumentException(String.format("이름은 공백이거나 비어있을 수 없습니다. 입력값 : %s", name));
         }
-    }
 
-    private void validateLength(String name) {
         if (name.length() > MAX_NAME_LENGTH) {
             throw new IllegalArgumentException(String.format("이름은 1글자 이상, 5글자 이하여야합니다. 입력값 : %s", name));
         }
@@ -33,22 +23,5 @@ public class Name {
 
     public String getName() {
         return name;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        if (this == object) {
-            return true;
-        }
-        if (object == null || getClass() != object.getClass()) {
-            return false;
-        }
-        Name otherName = (Name) object;
-        return name.equals(otherName.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name);
     }
 }
