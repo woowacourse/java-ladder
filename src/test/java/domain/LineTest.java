8ed2d395ebcbc0d0cf9ef3ff651225f1eb816251
Hyeonjae-K <kwonhyeonjae01@gmail.com>
2023-02-20 00:02:04 +0900

    test: 연속 true값 테스트를 위한 BooleanGenerator 수정

--- src/test/java/domain/LineTest.java
+++ src/test/java/domain/LineTest.java
@@ -5,18 +5,17 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import utils.booleanGenerator.BooleanGenerator;
-import utils.booleanGenerator.RandomBooleanGenerator;
+import utils.booleanGenerator.TrueBooleanGenerator;
 
 class LineTest {
-    private final BooleanGenerator randomBooleanGenerator = new RandomBooleanGenerator();
+    private final TrueBooleanGenerator trueBooleanGenerator = new TrueBooleanGenerator();
 
     @Nested
     class pointsTest {
         @DisplayName("연속적으로 true가 발생해도 사다리는 정상적으로 생성된다.")
         @Test
         void trueSequenceTest() {
-            assertDoesNotThrow(() -> new Line(3, randomBooleanGenerator));
+            assertDoesNotThrow(() -> new Line(3, trueBooleanGenerator));
         }
     }
 }
