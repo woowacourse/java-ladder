4caf42e64f3bdb3638d914fd334562af0139171e
kwonyj1022 <kwonyj1022@naver.com>
2023-02-20 01:46:09 +0900

    refactor: domain에 있는 enum 클래스인 Bridge 클래스 삭제하고 출력 형태와 관련된 로직을 OutputView에 할당

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -3,13 +3,14 @@ package view;
 import domain.ladder.Ladder;
 import domain.ladder.Line;
 import domain.player.Player;
-import domain.ladder.Bridge;
 
 import java.util.List;
 
 public class OutputView {
 
     private static final String RESULT_SHOW_MESSAGE = "실행결과";
+    private static final String BRIDGE_TRUE = "-----";
+    private static final String BRIDGE_FALSE = "     ";
     private static final String LINE_BAR_MESSAGE = "|";
     private static final String LINE_SPACE_MESSAGE = "    ";
     private static final String PLAYER_SHOW_FORMAT = "%5s ";
@@ -37,11 +38,18 @@ public class OutputView {
     private void showBridges(List<Boolean> bridges) {
         System.out.printf(LINE_SPACE_MESSAGE + LINE_BAR_MESSAGE);
         for (Boolean bridge : bridges) {
-            printMessageFormat(Bridge.getBridge(bridge).getMessage() + LINE_BAR_MESSAGE, BRIDGE_SHOW_FORMAT);
+            printMessageFormat(draw(bridge) + LINE_BAR_MESSAGE, BRIDGE_SHOW_FORMAT);
         }
     }
 
     private void printMessageFormat(String playerName, String format) {
         System.out.printf(format, playerName);
+    }
+
+    private String draw(Boolean bridge) {
+        if (bridge) {
+            return BRIDGE_TRUE;
+        }
+        return BRIDGE_FALSE;
     }
 }
