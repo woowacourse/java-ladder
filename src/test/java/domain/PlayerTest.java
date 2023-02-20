bda0355ff2b531d3bac32d6c369f17f8bf477405
parkmuhyeun <pjhg410@gmail.com>
2023-02-20 22:26:47 +0900

    test: 개인 결과 계산 기능 테스트

--- src/test/java/domain/PlayerTest.java
+++ src/test/java/domain/PlayerTest.java
@@ -1,9 +1,11 @@
 package domain;
 
+import helper.StubPossiblePointGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -24,5 +26,17 @@ public class PlayerTest {
         assertThatThrownBy(() -> new Player(blankName, 0))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 빈 문자열 입니다.");
+    }
+
+    @Test
+    @DisplayName("결과가 잘 나오는지 확인")
+    void calculateResult() {
+        int startIndex = 0;
+        String result = "2";
+        Player player = new Player("pobi", startIndex);
+        Ladder ladder = new Ladder(5, 4, new StubPossiblePointGenerator());
+
+        assertThat(player.calculateResult(ladder, new Results(4, List.of("1", "2", "3", "4"))))
+                .isEqualTo(result);
     }
 }
