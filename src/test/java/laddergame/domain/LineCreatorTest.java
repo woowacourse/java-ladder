7f25671bdabf6a527068da70afcd9f311db78ae5
hyunseo <goatsfish@gmail.com>
2023-02-20 15:10:59 +0900

    test: LineCreator 파라미터 타입 변경 테스트 수정

--- src/test/java/laddergame/domain/LineCreatorTest.java
+++ src/test/java/laddergame/domain/LineCreatorTest.java
@@ -1,11 +1,16 @@
 package laddergame.domain;
 
+import laddergame.fixture.BooleanGeneratorFixture;
+import laddergame.fixture.HeightFixture;
+import laddergame.fixture.WidthFixture;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static laddergame.fixture.LineCreatorFixture.TEST_LINE_CREATOR;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -21,27 +26,43 @@ class LineCreatorTest {
         assertDoesNotThrow(() -> new LineCreator(generator));
     }
 
-    @DisplayName("가로 길이가 0 이하이면 예외가 발생한다.")
-    @ParameterizedTest(name = "heightValue = {0}")
-    @ValueSource(ints = {-4, -3, -2, -1, 0})
-    void throwExceptionWhenWidthIsNotPositive(final int heightValue) {
-        assertThatThrownBy(() -> TEST_LINE_CREATOR.createLines(0, heightValue))
+    @DisplayName("createLines 메서드의 인자인 Width가 null일 경우 예외가 발생한다.")
+    @Test
+    void throwExceptionWhenWidthIsNull() {
+        final LineCreator lineCreator = new LineCreator(BooleanGeneratorFixture.TEST_BOOLEAN_GENERATOR);
+        final Width width = null;
+        final Height height = HeightFixture.createHeightValue3();
+        assertThatThrownBy(() -> lineCreator.createLines(width, height))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("세로 길이가 0 이하이면 예외가 발생한다.")
-    @ParameterizedTest(name = "widthValue = {0}")
-    @ValueSource(ints = {-4, -3, -2, -1, 0})
-    void throwExceptionWhenHeightIsNotPositive(final int widthValue) {
-        assertThatThrownBy(() -> TEST_LINE_CREATOR.createLines(widthValue, 0))
+    @DisplayName("createLines 메서드의 인자인 Height가 null일 경우 예외가 발생한다.")
+    @Test
+    void throwExceptionWhenHeightIsNull() {
+        final LineCreator lineCreator = new LineCreator(BooleanGeneratorFixture.TEST_BOOLEAN_GENERATOR);
+        final Width width = WidthFixture.createWidthValue3();
+        final Height height = null;
+        assertThatThrownBy(() -> lineCreator.createLines(width, height))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("라인 사이즈는 세로 길이와 같다.")
     @ParameterizedTest(name = "heightValue = {0}")
-    @ValueSource(ints = {1, 2, 3, 4, 5})
-    void returnsLinesSizeIsHeight(final int heightValue) {
-        final List<Line> lines = TEST_LINE_CREATOR.createLines(3, heightValue);
-        assertThat(lines).hasSize(heightValue);
+    @MethodSource("createLinesParameterDummy")
+    void returnsLinesSizeIsHeight(final Width width, final Height height) {
+        final Lines lines = TEST_LINE_CREATOR.createLines(width, height);
+        final List<Line> findLines = lines.getLines();
+
+        assertThat(findLines).hasSize(height.getValue());
+    }
+
+    static Stream<Arguments> createLinesParameterDummy() {
+        return Stream.of(
+                Arguments.arguments(WidthFixture.createWidthValue1(), HeightFixture.createHeightValue1()),
+                Arguments.arguments(WidthFixture.createWidthValue2(), HeightFixture.createHeightValue1()),
+                Arguments.arguments(WidthFixture.createWidthValue3(), HeightFixture.createHeightValue1()),
+                Arguments.arguments(WidthFixture.createWidthValue1(), HeightFixture.createHeightValue2()),
+                Arguments.arguments(WidthFixture.createWidthValue1(), HeightFixture.createHeightValue3())
+        );
     }
 }
