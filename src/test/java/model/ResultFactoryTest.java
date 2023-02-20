89289b27b6ec66faf1dd981ea774f90c0b114af6
MoonJeWoong <munjin0201@naver.com>
2023-02-20 23:31:07 +0900

    test: 기존 Results 클래스를 팩토리화한 기능 테스트 코드로 수정

--- src/test/java/model/ResultFactoryTest.java
+++ src/test/java/model/ResultFactoryTest.java
@@ -10,22 +10,23 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class ResultsTest {
+public class ResultFactoryTest {
     private static final int MAXIMUM_RESULT_LENGTH = 5;
     private static final String MAXIMUM_RESULT_LENGTH_ERROR = "[ERROR] 게임 결과 입력값 길이는 %d 이하로만 가능합니다.";
     private static final String WRONG_SIZE_RESULTS_ERROR = "[ERROR] 사다리 게임 결과 값의 개수는 전체 사람의 수와 동일해야 합니다.";
 
     @Test
-    @DisplayName("Results 객체 생성 성공 테스트")
+    @DisplayName("Result 객체 리스트 생성 성공 테스트")
     void createResultsTest() {
-        assertThatNoException().isThrownBy(() -> new Results(4, "꽝, 5000, 꽝, 3000"));
+        assertThatNoException().isThrownBy(() -> ResultFactory.create(4, "꽝, 5000, 꽝, 3000"));
     }
 
     @Test
     @DisplayName("결과값 길이 제한으로 인한 Result 객체 생성 실패 테스트")
     void limitResultLengthTest() {
-        assertThatThrownBy(() -> new Results(2,"abcdef, gehijk"))
+        assertThatThrownBy(() -> ResultFactory.create(2,"abcdef, gehijk"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(String.format(MAXIMUM_RESULT_LENGTH_ERROR, MAXIMUM_RESULT_LENGTH));
     }
@@ -38,7 +39,7 @@ public class ResultsTest {
         Players players = new Players(new Names(inputNames));
 
         //then
-        assertThatThrownBy(() -> new Results(players.size(), "꽝, 5000, 꽝, 3000"))
+        assertThatThrownBy(() -> ResultFactory.create(players.size(), "꽝, 5000, 꽝, 3000"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(WRONG_SIZE_RESULTS_ERROR);
     }
@@ -47,10 +48,12 @@ public class ResultsTest {
     @DisplayName("사다리 게임 결과는 쉼표를 기준으로 입력받는 기능 테스트")
     void splitResultsByCommaTest() {
         //given
-        Results givenResults = new Results(4, "꽝 , 5000 , 꽝 , 3000");
+        List<Result> createdResults = ResultFactory.create(4, "꽝 , 5000 , 꽝 , 3000");
 
         //when
-        List<String> thenResult = givenResults.getValues();
+        List<String> thenResult = createdResults.stream()
+                .map(Result::getValue)
+                .collect(Collectors.toList());
 
         //then
         assertThat(thenResult).isEqualTo(List.of("꽝", "5000", "꽝", "3000"));
