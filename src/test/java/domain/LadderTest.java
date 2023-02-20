5dceb73c913ff55734c1cb0a49846504dbf9ed5a
HubCreator <chocomilk8604@gmail.com>
2023-02-20 16:31:10 +0900

    refactor: 쓰지 않는 import문 정리 및 변수 수정

--- src/test/java/domain/LadderTest.java
+++ src/test/java/domain/LadderTest.java
@@ -17,7 +17,7 @@ class LadderTest {
             "2, 10",
             "3, 5",
     })
-    void hello(int personCount, int maxHeight) {
+    void ladder_test(int personCount, int maxHeight) {
         StringBuilder names = new StringBuilder();
         for (int i = 0; i < personCount; i++) {
             names.append("hi").append(i).append(",");
@@ -30,10 +30,9 @@ class LadderTest {
     @DisplayName("사다리의 높이가 1 이상 10 이하가 아니면 예외가 발생한다.")
     @ParameterizedTest(name = "{index} : 현재 사다리 높이 = {0}")
     @ValueSource(ints = {-1, 0, 11})
-    void ladder_invalid_test(int height) {
+    void ladder_height_test(int height) {
         assertThatThrownBy(() -> new Ladder(new People("hello,world"), height, new RandomGenerateStrategy()))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("사다리 높이는 1 이상 10 이하여야 합니다");
     }
-
 }
--- src/test/java/domain/ResultsTest.java
+++ src/test/java/domain/ResultsTest.java
@@ -1,11 +1,10 @@
 package domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 public class ResultsTest {
