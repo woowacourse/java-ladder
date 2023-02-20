8099cfdd4432fc47bd87870a2f47fc2fd84b9a38
MoonJeWoong <munjin0201@naver.com>
2023-02-20 23:34:12 +0900

    refactor: 기존 Results 클래스를 팩토리화

--- src/main/java/model/ResultFactory.java
+++ src/main/java/model/ResultFactory.java
@@ -4,33 +4,25 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class Results {
+public class ResultFactory {
     private static final String WRONG_SIZE_RESULTS_ERROR = "[ERROR] 사다리 게임 결과 값의 개수는 전체 사람의 수와 동일해야 합니다.";
     private static final String RESULTS_DELIMITER = ",";
 
-    private final List<Result> results;
-
-    public Results(int playersSize, String inputResults) {
+    public static List<Result> create(int playersSize, String inputResults) {
         List<String> results = splitInputResults(inputResults);
         validateRightResultsSize(playersSize, results);
-        this.results = results.stream()
+        return results.stream()
                 .map(Result::new)
                 .collect(Collectors.toList());
     }
 
-    public List<String> getValues() {
-        return results.stream()
-                .map(Result::getValue)
-                .collect(Collectors.toList());
-    }
-
-    private void validateRightResultsSize(int playersSize, List<String> results) {
+    private static void validateRightResultsSize(int playersSize, List<String> results) {
         if(results.size() != playersSize) {
             throw new IllegalArgumentException(WRONG_SIZE_RESULTS_ERROR);
         }
     }
 
-    private List<String> splitInputResults(String inputResults) {
+    private static List<String> splitInputResults(String inputResults) {
         return Arrays.stream(inputResults.split(RESULTS_DELIMITER))
                 .map(String::trim)
                 .collect(Collectors.toList());
