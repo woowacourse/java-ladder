ad98f90f8471c2c10d92f20d5f1a1ee9406ac321
DESKTOP-FNVN0HH\USER <beatus96@naver.com>
2023-02-20 21:01:11 +0900

    refactor: Player equals 오버라이딩

--- src/main/java/ladder/domain/Player.java
+++ src/main/java/ladder/domain/Player.java
@@ -1,5 +1,7 @@
 package ladder.domain;
 
+import java.util.Objects;
+
 public class Player {
 
     private final Name name;
@@ -12,4 +14,16 @@ public class Player {
         return name.getName();
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return Objects.equals(name, player.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 }
