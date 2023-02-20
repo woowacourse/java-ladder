7b74d2b32eeb15bef89e1da4114b71e84ff7d4cb
odo27 <odo27@odo27ui-MacBookAir.local>
2023-02-20 17:34:45 +0900

    refactor: 테스트 메서드 이름 보완

--- src/test/java/domain/StringParserTest.java
+++ src/test/java/domain/StringParserTest.java
@@ -6,9 +6,9 @@ import org.junit.jupiter.api.Test;
 
 class StringParserTest {
 
-    @DisplayName("구분자에 따라 문자열을 나눠주는 테스트 케이스")
+    @DisplayName("구분자에 따라 공백을 포함하여 문자열을 나눈다.")
     @Test
-    void splitByCommaTest() {
+    void splitByCommaIncludingBlank() {
         String input = " ,, as";
         Assertions.assertThat(StringParser.splitByComma(input, ",")).containsExactly(" ", "", " as");
     }
