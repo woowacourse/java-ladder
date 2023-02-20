fa7a56a0cf45f1502b9c7d151babd7ed481d047f
Hyeonjae-K <kwonhyeonjae01@gmail.com>
2023-02-20 15:07:53 +0900

    feat: 보상들 객체 구현

---
+++ src/main/java/domain/Rewards.java
@@ -0,0 +1,23 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Rewards {
+    private static final String EXCEPTION_COUNT_MESSAGE = "[ERROR] 보상의 개수는 사용자의 수와 동일해야 합니다.";
+    private static final String REWARD_DELIMITER = ",";
+
+    private final List<Reward> rewards;
+
+    public Rewards(String rewards, int personCount) {
+        this.rewards = new ArrayList<>();
+        String[] splitRewards = rewards.split(REWARD_DELIMITER);
+        validate(splitRewards.length, personCount);
+    }
+
+    private static void validate(int rewardCount, int personCount) {
+        if (rewardCount != personCount) {
+            throw new IllegalArgumentException(EXCEPTION_COUNT_MESSAGE);
+        }
+    }
+}
---
+++ src/test/java/domain/RewardsTest.java
@@ -0,0 +1,26 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class RewardsTest {
+    @DisplayName("보상의 입력 개수는 사다리 게임 참가자 수와 동일하다.")
+    @ParameterizedTest
+    @CsvSource(value = {"a,b:2", "a,b,c:3", "a,b,c,d,e:5"}, delimiter = ':')
+    void validRewardsTest(String rewards, int personCount) {
+        assertDoesNotThrow(() -> new Rewards(rewards, personCount));
+    }
+
+    @DisplayName("보상의 입력 개수와 사다리 게임 참가자 수가 다르면 예외가 발생한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"a,b:3", "a,b,c:2", "a,b,c,d,e:4"}, delimiter = ':')
+    void invalidRewardsTest(String rewards, int personCount) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Rewards(rewards, personCount))
+                .withMessageContaining("[ERROR] 보상의 개수는 사용자의 수와 동일해야 합니다.");
+    }
+}
