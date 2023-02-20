8e041437171fd9b63222ffeae8b1375b386fcf61
parkmuhyeun <pjhg410@gmail.com>
2023-02-20 21:43:12 +0900

    test: 결과값 사이즈 확인 테스트

---
+++ src/test/java/domain/ResultsTest.java
@@ -0,0 +1,18 @@
+package domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+
+class ResultsTest {
+
+    @Test
+    @DisplayName("참여자 수 만큼 입력되지 않으면 예외 처리")
+    void test() {
+        Assertions.assertThatThrownBy(() -> new Results(3, List.of("꽝,꽝")))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 결과값을 참여자 수 만큼 입력해야합니다.");
+    }
+
+}
