98ef0ccc927507476dc1e5d7a79fd82293ad0276
jaehee329 <jjhs9803@gmail.com>
2023-02-20 16:06:10 +0900

    test(UserTest): ParameterizedTest로 테스트 범주 증가

--- src/test/java/domain/UserTest.java
+++ src/test/java/domain/UserTest.java
@@ -5,19 +5,23 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class UserTest {
-	@Test
-	@DisplayName("이름이 네 글자인 참가자는 정상적으로 생성되어야 한다.")
-	void userNameLengthSuccessTest() {
-		String name = "echo";
+	@ParameterizedTest
+	@DisplayName("이름이 한 글자 이상, 다섯 글자 이하인 참가자는 정상적으로 생성되어야 한다.")
+	@ValueSource(ints = {1, 2, 3, 4, 5})
+	void userNameLengthSuccessTest(int nameLength) {
+		String name = "1".repeat(nameLength);
 		Assertions.assertDoesNotThrow(() -> new User(name));
 	}
 
-	@Test
-	@DisplayName("이름이 여섯 글자인 참가자는 예외를 발생시켜야 한다.")
-	void userNameLengthFailTest() {
-		String name = "echo11";
+	@ParameterizedTest
+	@DisplayName("0 글자 또는 6 글자인 참가자 이름은 예외를 발생시켜야 한다.")
+	@ValueSource(ints = {0, 6})
+	void userNameLengthFailTest(int nameLength) {
+		String name = "1".repeat(nameLength);
 		assertThatThrownBy(() -> new User(name)).isInstanceOf(IllegalArgumentException.class);
 	}
 
@@ -29,7 +33,7 @@ class UserTest {
 	}
 
 	@Test
-	@DisplayName("이름이 'all'인 참가자는 예외를 발생시켜야 한다.")
+	@DisplayName("'all' 이라는 이름의 참가자를 생성하면 예외가 발생해야 한다.")
 	void userNameAllTest() {
 		String name = "all";
 		assertThatThrownBy(() -> new User(name)).isInstanceOf(IllegalArgumentException.class);
