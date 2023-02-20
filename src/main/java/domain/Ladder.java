dce4b4b39b6636d2a708f979c2774485f8c8bcef
aiaiaiai1 <cvc100326@naver.com>
2023-02-20 16:34:11 +0900

    feat: 사다리 높이가 50보다 큰 경우 예외 발생 기능 추가

--- src/main/java/domain/Ladder.java
+++ src/main/java/domain/Ladder.java
@@ -5,7 +5,8 @@ import java.util.List;
 
 public class Ladder {
     private static final int MIN_HEIGHT = 1;
-    private static final String INVALID_HEIGHT_MESSAGE = "사다리 높이는 " + MIN_HEIGHT + "이상이어야 합니다.";
+    private static final int MAX_HEIGHT = 50;
+    private static final String INVALID_HEIGHT_MESSAGE = "사다리 높이는 %d부터 %d까지 입니다.";
 
     private final List<Line> lines = new ArrayList<>();
 
@@ -25,8 +26,9 @@ public class Ladder {
     }
 
     private void validateHeight(int height) {
-        if (height < MIN_HEIGHT) {
-            throw new IllegalArgumentException(INVALID_HEIGHT_MESSAGE);
+        if (height < MIN_HEIGHT || height > MAX_HEIGHT) {
+            throw new IllegalArgumentException(
+                    String.format(INVALID_HEIGHT_MESSAGE, MIN_HEIGHT, MAX_HEIGHT));
         }
     }
 
