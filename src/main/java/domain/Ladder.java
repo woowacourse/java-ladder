8779acd390affec9c20d58de81ca3cd24d1e85f3
the9kim <96895686+the9kim@users.noreply.github.com>
2023-02-21 07:06:27 +0900

    refactor: 사용하지 않는 필드 제거

--- src/main/java/domain/Ladder.java
+++ src/main/java/domain/Ladder.java
@@ -5,12 +5,8 @@ import util.LineGenerator;
 public class Ladder {
 
     private final Lines lines;
-    private final int numberOfWalls;
-    private final Height height;
 
     public Ladder(int numberOfWalls, Height height, LineGenerator lineGenerator) {
-        this.numberOfWalls = numberOfWalls;
-        this.height = height;
         this.lines = new Lines(numberOfWalls, height, lineGenerator);
     }
 
