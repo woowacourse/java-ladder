9551c86098eb750da9f11b887bb9c374ea90221d
apptie <appti@naver.com>
2023-02-20 18:07:15 +0900

    test: Ladder 기능 추가로 인한 테스트 케이스 추가

--- src/test/java/model/LadderMakerTest.java
+++ src/test/java/model/LadderMakerTest.java
@@ -1,31 +1,55 @@
 package model;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 import org.junit.jupiter.api.Test;
-import helper.StubPassGenerator;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullSource;
 
+@DisplayNameGeneration(ReplaceUnderscores.class)
 class LadderMakerTest {
 
-    private final LadderMaker ladderMaker = new LadderMaker(new StubPassGenerator(false));
+    @ParameterizedTest
+    @NullSource
+    void 생성자는_null을_전달하면_예외가_발생한다(List<Line> lines) {
+        assertThatThrownBy(() -> new Ladder(lines))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("사다리 줄이 정상적으로 입력되지 않았습니다.");
+    }
 
-    @Test
-    void initLadder_메소드는_Height와_참가자_수를_입력하면_ladder를_초기화_한다() {
-        assertThatCode(() -> ladderMaker.initLadder(new Height(3), 5))
-                .doesNotThrowAnyException();
+    @ParameterizedTest
+    @EmptySource
+    void 생성자는_비어_있는_컬렉션을_전달하면_예외가_발생한다(List<Line> lines) {
+        assertThatThrownBy(() -> new Ladder(lines))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("1 이상의 사다리 줄을 입력해주세요.");
     }
 
     @Test
-    void getLadder_메소드는_ladder를_반환한다() {
-        Height height = new Height(3);
-        ladderMaker.initLadder(height, 5);
+    void 생성자는_비어있지_않은_컬렉션을_전달하면_Ladder를_생성한다() {
+        assertThatCode(() -> new Ladder(List.of(new Line(List.of(Path.PASSABLE)))))
+                .doesNotThrowAnyException();
+    }
 
-        Ladder ladder = ladderMaker.getLadder();
-        List<Line> lines = ladder.getLines();
+    @ParameterizedTest
+    @MethodSource("provideNonEqualSizeLine")
+    void 생성자는_Path의_크기가_다른_Line이_주어지면_예외가_발생한다(Line firstLine, Line secondLine) {
+        assertThatThrownBy(() -> new Ladder(List.of(firstLine, secondLine)))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
 
-        assertThat(lines.size()).isSameAs(3);
-        assertThat(lines.get(0).getLine().size()).isSameAs(4);
+    private static Stream<Arguments> provideNonEqualSizeLine() {
+        return Stream.of(
+                Arguments.of(new Line(List.of(Path.PASSABLE)), new Line(List.of(Path.PASSABLE, Path.UN_PASSABLE))),
+                Arguments.of(new Line(List.of(Path.PASSABLE, Path.UN_PASSABLE)), new Line(List.of(Path.PASSABLE)))
+        );
     }
 }
