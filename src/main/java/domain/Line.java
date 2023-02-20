575416322ae24fb75359a22018640cfb7bd40716
odo27 <odo27@odo27ui-MacBookAir.local>
2023-02-20 23:50:35 +0900

    feat: 이동하는 기능 구현

--- src/main/java/domain/Line.java
+++ src/main/java/domain/Line.java
@@ -41,6 +41,36 @@ public class Line {
         return Point.DISCONNECTED;
     }
 
+    public int move(int index) {
+        Point leftPoint = getLeftPoint(index);
+        if (leftPoint != null && leftPoint.isConnected()) {
+            return index - 1;
+        }
+
+        Point rightPoint = getRightPoint(index);
+        if (rightPoint != null && rightPoint.isConnected()) {
+            return index + 1;
+        }
+
+        return index;
+    }
+
+    private Point getLeftPoint(int index) {
+        if (index == 0) {
+            return null;
+        }
+
+        return points.get(index - 1);
+    }
+
+    private Point getRightPoint(int index) {
+        if (index == points.size()) {
+            return null;
+        }
+
+        return points.get(index);
+    }
+
     public List<Point> getPoints() {
         return Collections.unmodifiableList(points);
     }
