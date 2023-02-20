49a2dbde9c69293892b19d1d57334760a7968cf7
swonny <wonny921@gmail.com>
2023-02-20 20:04:24 +0900

    refactor: 유효성 검사를 객체에서 진행하도록 수정
    - Validator가 분리되어 있을 때 발생하는 혼동을 막기 위해 도메인 객체에서 유효성 검사를 진행하도록 수정

--- src/main/java/domain/Line.java
+++ src/main/java/domain/Line.java
@@ -1,19 +1,24 @@
 package domain;
 
-import domain.validator.LineValidator;
-
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 
 public class Line {
+
+    public static final int POINT_MIN_SIZE = 0;
+    public static final int POINT_MAX_SIZE = 19;
 
     private final List<Point> points;
 
     public Line(final List<Point> points) {
-        LineValidator.validate(points.size());
+        validate(points.size());
         this.points = points;
+    }
+
+    public void validate(final int pointSize) {
+        if (pointSize < POINT_MIN_SIZE || pointSize > POINT_MAX_SIZE) {
+            throw new IllegalArgumentException("포인트 범위는 0부터 19까지입니다.");
+        }
     }
 
     public Point getPointAt(final int index) {
