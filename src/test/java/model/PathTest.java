a99cfaa2867edccf7f5272095a2aa1e37fe10030
apptie <appti@naver.com>
2023-02-20 17:49:24 +0900

    test: Path 기능 추가로 인한 테스트 케이스 추가

--- src/test/java/model/PathTest.java
+++ src/test/java/model/PathTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -26,5 +27,22 @@ class PathTest {
         Path actual = Path.calculatePath(passable);
 
         assertThat(actual).isSameAs(expected);
+    }
+
+    @Test
+    void isMultiplePassable_메소드는_자신과_주어진_Path를_이어서_건널_수_있다면_true를_반환한다() {
+        boolean actual = Path.PASSABLE.isMultiplePassable(Path.PASSABLE);
+
+        assertThat(actual).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"PASSABLE:UN_PASSABLE", "UN_PASSABLE:PASSABLE",
+            "UN_PASSABLE:UN_PASSABLE"}, delimiter = ':')
+    void isMultiplePassable_메소드는_자신과_주어진_Path를_이어서_건널_수_없다면_false를_반환한다(Path leftPath,
+            Path rightPath) {
+        boolean actual = leftPath.isMultiplePassable(rightPath);
+
+        assertThat(actual).isFalse();
     }
 }
