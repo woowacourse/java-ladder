9efbb094561e16efc712fe5668f88007fbdd741b
DESKTOP-FNVN0HH\USER <beatus96@naver.com>
2023-02-20 20:58:52 +0900

    style: LadderOutputSymbol 상수명 변경

--- src/main/java/ladder/view/constant/LadderOutputSymbol.java
+++ src/main/java/ladder/view/constant/LadderOutputSymbol.java
@@ -1,9 +1,11 @@
 package ladder.view.constant;
 
+import ladder.domain.Bar;
+
 public enum LadderOutputSymbol {
 
-    BAR_SYMBOL("-"),
-    EMPTY_BAR_SYMBOL(" "),
+    CONNECTED_BAR_SYMBOL("-"),
+    UNCONNECTED_BAR_SYMBOL(" "),
     LADDER_VERTICAL_SYMBOL("|");
 
     private final String symbol;
@@ -12,12 +14,12 @@ public enum LadderOutputSymbol {
         this.symbol = symbolValue;
     }
 
-    public static String decideLadderSymbol(boolean input) {
-        if (input) {
-            return BAR_SYMBOL.symbol;
+    public static String decideLadderSymbol(Bar input) {
+        if (input.getValue()) {
+            return CONNECTED_BAR_SYMBOL.symbol;
         }
 
-        return EMPTY_BAR_SYMBOL.symbol;
+        return UNCONNECTED_BAR_SYMBOL.symbol;
     }
 
     public String getSymbol() {
