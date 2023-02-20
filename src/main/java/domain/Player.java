91970678af5d0f5817969c8a19e1dfb681773591
parkmuhyeun <pjhg410@gmail.com>
2023-02-20 16:38:34 +0900

    feat: Position Wrapper 생성

--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -3,11 +3,11 @@ package domain;
 public class Player {
 
     private final Name name;
-    private final int position;
+    private final Position position;
 
     public Player(String name, int position){
         this.name = new Name(name);
-        this.position = position;
+        this.position = new Position(position);
     }
 
     public String getName() {
---
+++ src/main/java/domain/Position.java
@@ -0,0 +1,10 @@
+package domain;
+
+public class Position {
+
+    private final int position;
+
+    public Position(int position) {
+        this.position = position;
+    }
+}
