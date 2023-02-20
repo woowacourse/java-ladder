8dd80ceecb4316e3321db809cecc5c62446d9980
Cyma-s <ssol0319@naver.com>
2023-02-20 15:49:15 +0900

    feat: 없는 플레이어의 item을 가져올 때 예외를 던지는 메서드 추가

--- src/main/java/ladder/domain/LadderResult.java
+++ src/main/java/ladder/domain/LadderResult.java
@@ -10,6 +10,15 @@ public class LadderResult {
     }
 
     public Item getItemOfPlayer(Player player) {
+        validateNotExistPlayer(player);
         return result.get(player);
     }
+
+    private void validateNotExistPlayer(Player player) {
+        if(!result.containsKey(player)) {
+            throw new IllegalStateException("플레이어가 존재하지 않습니다.");
+        }
+    }
+
+
 }
