32a0a55a0aadfb6db3c3c793872f24b5c72e19fa
ChoiYoungHoon <dudgns2865@naver.com>
2023-02-20 13:59:51 +0900

    fix: 이름의 최대 길이에 따라 사다리 라인 길이가 달라지도록 수정

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -4,34 +4,37 @@ import domain.model.Ladder;
 import domain.model.Layer;
 import domain.type.Line;
 import domain.vo.Name;
+import java.util.Comparator;
 import java.util.List;
 
 public class OutputView {
 
     private static final String RESULT_ANNOUNCEMENT = "\n실행결과";
-    private static final int INTERVAL_UNIT = 6;
+    private static final int INTERVAL_UNIT = 5;
     private static final String FRONT_SPACE = "    ";
     private static final String LINE_DELIMITER = "|";
-    private static final String CONNECTED_LINE = "-----";
-    private static final String UNCONNECTED_LINE = "     ";
-    private static final String NAME_SPACE = " ";
+    private static final String CONNECTED_LINE = "-";
+    private static final String SPACE = " ";
 
     public void printResult(List<Name> names, Ladder ladder) {
         System.out.println(RESULT_ANNOUNCEMENT);
         printNames(names);
-        printLadder(ladder);
+        printLadder(ladder,
+            names.stream().map(name -> name.getValue().length()).max(Comparator.naturalOrder())
+                .orElse(INTERVAL_UNIT));
     }
 
-    private void printLadder(Ladder ladder) {
-        ladder.getLayers().forEach(this::printLayer);
+    private void printLadder(Ladder ladder, int lineLength) {
+        ladder.getLayers().forEach(layer -> printLayer(layer, lineLength));
     }
 
-    private void printLayer(Layer layer) {
+    private void printLayer(Layer layer, int lineLength) {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append(FRONT_SPACE).append(LINE_DELIMITER);
         List<Line> lines = layer.getLines();
         lines.forEach(
-            line -> stringBuilder.append(selectLine(line)).append(LINE_DELIMITER));
+            line -> stringBuilder.append(selectLine(line).repeat(lineLength))
+                .append(LINE_DELIMITER));
         System.out.println(stringBuilder);
     }
 
@@ -39,7 +42,7 @@ public class OutputView {
         if (line.equals(Line.CONNECTED)) {
             return CONNECTED_LINE;
         }
-        return UNCONNECTED_LINE;
+        return SPACE;
     }
 
     private void printNames(final List<Name> names) {
@@ -47,7 +50,7 @@ public class OutputView {
         stringBuilder.append("\n");
         names.forEach(name -> {
             int difference = INTERVAL_UNIT - name.getValue().length();
-            stringBuilder.append(name.getValue()).append(NAME_SPACE.repeat(difference));
+            stringBuilder.append(SPACE.repeat(difference)).append(name.getValue());
         });
         System.out.println(stringBuilder);
     }
