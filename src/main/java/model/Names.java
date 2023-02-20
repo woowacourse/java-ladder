7e71016bccccd59f0c338c5c8e2520a8b7dbe358
apptie <appti@naver.com>
2023-02-20 17:44:49 +0900

    feat: Names 검증 로직 추가 및 이름을 통한 참가자 순서 조회 기능 추가

--- src/main/java/model/Names.java
+++ src/main/java/model/Names.java
@@ -2,6 +2,8 @@ package model;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.IntStream;
 
 public class Names {
 
@@ -10,15 +12,25 @@ public class Names {
     private final List<Name> names;
 
     public Names(List<Name> names) {
-        validateSize(names);
+        validateNames(names);
 
         this.names = names;
     }
 
-    private void validateSize(List<Name> names) {
+    private void validateNames(List<Name> names) {
+        if (Objects.isNull(names)) {
+            throw new IllegalStateException("참가자 이름이 정상적으로 입력되지 않았습니다.");
+        }
         if (names.size() < MINIMUM_PARTICIPANTS_SIZE) {
             throw new IllegalArgumentException("최소 2명의 이름을 입력해주세요.");
         }
+        if (names.size() != calculateDistinctNameSize(names)) {
+            throw new IllegalArgumentException("참가자의 이름은 중복될 수 없습니다.");
+        }
+    }
+
+    private long calculateDistinctNameSize(List<Name> names) {
+        return names.stream().distinct().count();
     }
 
     public List<Name> getNames() {
@@ -29,6 +41,13 @@ public class Names {
         Name target = names.get(index);
 
         return target.getName();
+    }
+
+    public int getIndexOfName(String name) {
+        return IntStream.range(0, names.size())
+                .filter(i -> names.get(i).matchesByName(name))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("입력한 이름의 참가자가 없습니다."));
     }
 
     public int getTotalParticipantSize() {
