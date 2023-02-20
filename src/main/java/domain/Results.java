d3f70144a70d7bdb8a0908c77e89fb71b35f36b9
kiarakim <kiarakim413@gmail.com>
2023-02-20 16:42:48 +0900

    feat(Results) : Results 도메인 검증 및 테스트 코드 작성

--- src/main/java/domain/Results.java
+++ src/main/java/domain/Results.java
@@ -3,7 +3,12 @@ package domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import exception.Error;
+
 public class Results {
+	private static final int MIN_RESULTS_SIZE_INCLUSIVE = 2;
+	private static final int MAX_RESULTS_SIZE_INCLUSIVE = 10;
+
 	private final List<Result> results;
 
 	private Results(List<Result> results) {
@@ -11,11 +16,19 @@ public class Results {
 	}
 
 	public static Results from(List<String> sequences) {
+		validateSize(sequences);
 		List<Result> resultList = new ArrayList<>();
 		for (String sequence : sequences) {
 			resultList.add(new Result(sequence));
 		}
 		return new Results(resultList);
+	}
+
+	private static void validateSize(List<String> sequences) {
+		if (sequences.size() < MIN_RESULTS_SIZE_INCLUSIVE)
+			throw new IllegalArgumentException(Error.RESULTS_FROM_2_TO_10.getMessage());
+		if (sequences.size() > MAX_RESULTS_SIZE_INCLUSIVE)
+			throw new IllegalArgumentException(Error.RESULTS_FROM_2_TO_10.getMessage());
 	}
 
 	public int size() {
--- src/test/java/domain/ResultsTest.java
+++ src/test/java/domain/ResultsTest.java
@@ -3,6 +3,8 @@ package domain;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,5 +18,25 @@ class ResultsTest {
 		Results results = Results.from(List.of("꽝", "5000"));
 
 		assertThat(results.size()).isEqualTo(people.size());
+	}
+
+	@DisplayName("2개 미만이면 예외가 발생한다")
+	@Test
+	void resultsSize1() {
+		assertThatThrownBy(() -> Results.from(List.of("3000")))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessage("[ERROR] 예상 결과 개수는 2 ~ 10개여야 합니다증");
+	}
+
+	@DisplayName("10개 초과면 예외가 발생한다")
+	@Test
+	void resultsSize11() {
+		List<String> sequences = IntStream.range(0, 11)
+			.mapToObj(i -> "당첨" + i)
+			.collect(Collectors.toList());
+
+		assertThatThrownBy(() -> Results.from(sequences))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessage("[ERROR] 예상 결과 개수는 2 ~ 10개여야 합니다");
 	}
 }
