675bec2c629b81adcc6f89edb0bb7f14b2adb0e7
yeonkkk <tjddus0302@gmail.com>
2023-02-20 16:36:51 +0900

    test: Results 생성 테스트

---
+++ src/test/java/domain/ResultsTest.java
@@ -0,0 +1,28 @@
+package domain;
+
+
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("입력된 실행 결과의 수는 ")
+class ResultsTest {
+
+    private final List<String> results = List.of("꽝", "5000", "3000", "꽝");
+
+    @DisplayName("참여자의 수와 같아야 한다.")
+    @Test
+    void resultSizeTest_success() {
+        Assertions.assertDoesNotThrow(
+            () -> new Results(results, results.size()));
+    }
+
+    @DisplayName("참여자의 수와 다르면 예외가 발생한다.")
+    @Test
+    void resultSizeTest_fail() {
+        Assertions.assertThrows(
+            IllegalArgumentException.class,
+            () -> new Results(results, results.size() + 1));
+    }
+}
