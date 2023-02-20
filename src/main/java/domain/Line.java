b103c98fc272bba9bbc812f5969e81cb01c8d368
the9kim <96895686+the9kim@users.noreply.github.com>
2023-02-20 20:40:59 +0900

    refactor: Line객체의 ArrayList를 필드에서 초기화 하는 방식에서 생성자에서 생성하는 방식으로 변경

--- src/main/java/domain/Line.java
+++ src/main/java/domain/Line.java
@@ -8,23 +8,25 @@ public class Line {
 
 
     private final int numberOfLine;
-    private final List<LineStatus> line = new ArrayList<>();
+    private final List<LineStatus> line;
 
     public Line(int numberOfLineStatus, LineGenerator lineGenerator) {
         this.numberOfLine = numberOfLineStatus;
-        makeLine(lineGenerator);
+        this.line = makeLine(lineGenerator);
     }
 
-    private void makeLine(LineGenerator lineGenerator) {
-        makeFirstLineStatus(lineGenerator);
-        makeElseLineStatus(lineGenerator);
+    private List<LineStatus> makeLine(LineGenerator lineGenerator) {
+        List<LineStatus> line = new ArrayList<>();
+        makeFirstLineStatus(lineGenerator, line);
+        makeElseLineStatus(lineGenerator, line);
+        return line;
     }
 
-    private void makeFirstLineStatus(LineGenerator lineGenerator) {
+    private void makeFirstLineStatus(LineGenerator lineGenerator, List<LineStatus> line) {
         line.add(LineStatus.findBy(lineGenerator.generate(false)));
     }
 
-    private void makeElseLineStatus(LineGenerator lineGenerator) {
+    private void makeElseLineStatus(LineGenerator lineGenerator, List<LineStatus> line) {
         for (int i = 1; i < this.numberOfLine; i++) {
             int leftIndex = i - 1;
             line.add(LineStatus.findBy(lineGenerator.generate(line.get(leftIndex).getStatus())));
