9f18e1fcb710a24e6f58e96b0db54b84e0877047
dooboocookie <bkh3030@gmail.com>
2023-02-20 23:29:14 +0900

    test: Line 특정 위치에서 방향 정하는 기능 테스트

--- src/test/java/ladder/domain/ladder/LineTest.java
+++ src/test/java/ladder/domain/ladder/LineTest.java
@@ -10,6 +10,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class LineTest {
 
+    private static final int DIRECTION_RIGHT = 1;
+    private static final int DIRECTION_LEFT = -1;
+    private static final int DIRECTION_DOWN = 0;
+
+
     private final List<Bar> bars = List.of(Bar.MOVABLE_BAR, Bar.UNMOVABLE_BAR);
     private Line line;
 
@@ -37,6 +42,17 @@ public class LineTest {
     void checkLineTest() {
         assertThat(line.getLine())
                 .isEqualTo(bars);
+    }
+
+    @Test
+    @DisplayName("n번째 위치에서 어디로갈지 알려주는 테스트")
+    void decideDirectionTest() {
+        List<Bar> bars =List.of(Bar.MOVABLE_BAR, Bar.UNMOVABLE_BAR, Bar.UNMOVABLE_BAR);
+        Line line =newLine(bars);
+
+        assertThat(line.decideDirection(0)).isEqualTo(DIRECTION_RIGHT;
+        assertThat(line.decideDirection(1)).isEqualTo(DIRECTION_LEFT);
+        assertThat(line.decideDirection(2)).isEqualTo(DIRECTION_DOWN);
     }
 
 }
