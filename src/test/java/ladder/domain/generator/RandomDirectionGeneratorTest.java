ace4ab16584b27c421f147a7ade6dfd308430ca6
70825 <hello70825@gmail.com>
2023-02-20 15:13:05 +0900

    test: RandomDirectionGenerator 테스트 추가

---
+++ src/test/java/ladder/domain/generator/RandomDirectionGeneratorTest.java
@@ -0,0 +1,27 @@
+package ladder.domain.generator;
+
+import ladder.domain.Direction;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RandomDirectionGeneratorTest {
+
+    @Test
+    @DisplayName("랜덤한 범위의 값을 제대로 생성하는지 확인한다")
+    void validRandomDirection() {
+        // given
+        DirectionGenerator directionGenerator = new RandomDirectionGenerator();
+
+        // when
+        Direction actual = directionGenerator.generate();
+
+        // then
+        Assertions.assertThat(actual.getMove())
+                .isGreaterThanOrEqualTo(0)
+                .isLessThanOrEqualTo(1);
+    }
+
+}
