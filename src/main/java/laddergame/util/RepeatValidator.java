be319801369ad1c2ed95369e1242497052b08424
pilyang <30036534+pilyang@users.noreply.github.com>
2023-02-20 17:54:08 +0900

    refactor: remove unused method

--- src/main/java/laddergame/util/RepeatValidator.java
+++ src/main/java/laddergame/util/RepeatValidator.java
@@ -24,20 +24,4 @@ public class RepeatValidator {
         }
     }
 
-    public static void runUntilValidate(InputFunction inputFunction) {
-        boolean isSuccess = false;
-        while (!isSuccess) {
-            isSuccess = tryInputFunction(inputFunction);
-        }
-    }
-
-    private static boolean tryInputFunction(InputFunction inputFunction) {
-        try {
-            inputFunction.run();
-            return true;
-        } catch (RuntimeException exception) {
-            OutputView.printErrorMsg(exception);
-            return false;
-        }
-    }
 }
