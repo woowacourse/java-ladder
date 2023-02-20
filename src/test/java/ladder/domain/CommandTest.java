3a3ccb3e4a390247580d01f55aeaf918cbb0d5db
hum02 <ehdgma02@gmail.com>
2023-02-20 09:23:16 +0900

    test: 입력 실행결과 개수가 참여자 개수보다 적을 떄 꽝으로 채우는 테스트 구현

--- src/test/java/ladder/domain/CommandTest.java
+++ src/test/java/ladder/domain/CommandTest.java
@@ -22,4 +22,19 @@ class CommandTest {
         }).isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("실행 결과의 개수는 참여자의 개수 이하이어야 합니다.");
     }
+
+    @Test
+    @DisplayName("실행결과 개수가 참여자 개수보다 적으면, 나머지는 꽝으로 채운다.")
+    void result_fill_꽝_if_below_names_count() {
+        //given
+        List<String> names = List.of("dochi", "vero", "dogi", "ddd");
+        List<String> results = List.of("lucky", "dadad");
+        int height = 5;
+
+        //when
+        Command command = new Command(names, results, height);
+
+        //then
+        assertThat(command.getResults()).containsExactly("lucky", "dadad", "꽝", "꽝");
+    }
 }
