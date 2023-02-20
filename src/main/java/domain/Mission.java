e0313134ce9731b0d46b8a82556bca5ec60017e8
Eunkeee <eunkeeeee@gmail.com>
2023-02-20 19:21:32 +0900

    feat(Mission): 미션 생성

---
+++ src/main/java/domain/Mission.java
@@ -0,0 +1,14 @@
+package domain;
+
+public class Mission {
+
+    private final String mission;
+
+    public Mission(String mission) {
+        this.mission = mission;
+    }
+
+    public String getMission() {
+        return mission;
+    }
+}
