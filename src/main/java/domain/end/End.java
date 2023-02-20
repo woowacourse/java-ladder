51fe021ed26a4f4847bc578027a163a5af29a939
jaehee329 <jjhs9803@gmail.com>
2023-02-20 16:35:17 +0900

    test(EndTest): 결과의 이름에 대한 예외 테스트 추가

---
+++ src/main/java/domain/end/End.java
@@ -0,0 +1,5 @@
+package domain.end;
+
+public class End {
+
+}
---
+++ src/test/java/domain/ladder/EndTest.java
@@ -0,0 +1,28 @@
+package domain.ladder;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import domain.end.End;
+
+class EndTest {
+	@ParameterizedTest
+	@DisplayName("한 글자 이상, 다섯 글자 이하인 결과는 정상적으로 생성되어야 한다.")
+	@ValueSource(ints = {1, 2, 3, 4, 5})
+	void endLengthSuccessTest(int endLength) {
+		String end = "9".repeat(endLength);
+		Assertions.assertDoesNotThrow(() -> new End(end));
+	}
+
+	@ParameterizedTest
+	@DisplayName("0 글자 또는 6 글자인 결과는 예외를 발생시켜야 한다.")
+	@ValueSource(ints = {0, 6})
+	void endLengthFailTest(int endLength) {
+		String end = "9".repeat(endLength);
+		assertThatThrownBy(() -> new End(end)).isInstanceOf(IllegalArgumentException.class);
+	}
+}
