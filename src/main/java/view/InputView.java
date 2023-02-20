48b4cd2e879b09ffe9ef5be80391261020120869
odo27 <odo27@odo27ui-MacBookAir.local>
2023-02-20 22:45:05 +0900

    refactor: StringParser 패키지 변경

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -3,8 +3,6 @@ package view;
 import java.util.List;
 import java.util.Scanner;
 
-import domain.StringParser;
-
 public class InputView {
 
     private static final String READ_NAMES_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
