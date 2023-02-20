3331de0d9eba6cdbcc3258bf48fb3c8615875d90
LJW25 <dbjwl@g.skku.edu>
2023-02-20 17:02:48 +0900

    feat(PlayerName): 플레이어 이름이 all이면 예외처리 구현

--- src/main/java/ladder/model/ErrorMessage.java
+++ src/main/java/ladder/model/ErrorMessage.java
@@ -5,6 +5,7 @@ public enum ErrorMessage {
     EXCEPTION_PLAYER_NAME_DUPLICATE("이름은 중복될 수 없습니다."),
     EXCEPTION_PLAYER_COUNT_MINIMUM("게임을 진행하기 위해서는 두 명 이상의 플레이어가 필요합니다."),
     EXCEPTION_PLAYER_NAME_LENGTH("플레이어 이름은 1자 이상 5자 이하여야 합니다."),
+    EXCEPTION_PLAYER_NAME_RESTRICTED("플레이어 이름은 all 일 수 없습니다."),
     EXCEPTION_HEIGHT_INVALID_TYPE("정수가 아닙니다."),
     EXCEPTION_HEIGHT_MINIMUM("사다리 높이는 2 이상이어야 합니다.");
 
--- src/main/java/ladder/model/PlayerName.java
+++ src/main/java/ladder/model/PlayerName.java
@@ -1,16 +1,18 @@
 package ladder.model;
 
 import static ladder.model.ErrorMessage.EXCEPTION_PLAYER_NAME_LENGTH;
+import static ladder.model.ErrorMessage.EXCEPTION_PLAYER_NAME_RESTRICTED;
 
 public class PlayerName {
 
     private static final int MIN_LENGTH = 1;
     private static final int MAX_LENGTH = 5;
+    private static final String RESTRICTED_NAME = "all";
     private final String playerName;
 
     public PlayerName(String playerName) {
         playerName = removeWhiteSpace(playerName);
-        validatePlayerNameLength(playerName);
+        validatePlayerName(playerName);
         this.playerName = playerName;
     }
 
@@ -18,9 +20,20 @@ public class PlayerName {
         return playerName.replaceAll(" ", "");
     }
 
+    private void validatePlayerName(String playerName) {
+        validatePlayerNameLength(playerName);
+        validatePlayerNameRestricted(playerName);
+    }
+
     private void validatePlayerNameLength(String playerName) {
         if (playerName.length() < MIN_LENGTH || playerName.length() > MAX_LENGTH) {
             throw new IllegalArgumentException(EXCEPTION_PLAYER_NAME_LENGTH.getMessage());
+        }
+    }
+
+    private void validatePlayerNameRestricted(String playerName) {
+        if (playerName == RESTRICTED_NAME) {
+            throw new IllegalArgumentException(EXCEPTION_PLAYER_NAME_RESTRICTED.getMessage());
         }
     }
 
