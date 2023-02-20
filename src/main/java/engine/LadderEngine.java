af5b67a9dd455cbe9aa4ca09cc6d4d7a03e9ca39
0chil <0@chll.it>
2023-02-20 16:55:22 +0900

    feat: 결과를 보고싶은 사람의 경품을 출력한다
    - all은 모든 이의 경품을 출력하고 게임을 종료한다

--- src/main/java/engine/LadderEngine.java
+++ src/main/java/engine/LadderEngine.java
@@ -9,6 +9,7 @@ import domain.Ladder;
 import domain.Line;
 import domain.Participant;
 import domain.Participants;
+import dto.Result;
 import generator.LineGenerator;
 import generator.RandomBridgeGenerator;
 import view.InputView;
@@ -19,11 +20,35 @@ public class LadderEngine {
     public void start() {
         Ladder ladder = IllegalArgumentExceptionHandler.handleExceptionByRepeating(() -> {
             Participants participants = gatherParticipants();
+            List<String> prizes = InputView.inputPrizes();
             int height = InputView.inputMaxLadderHeight();
             List<Line> lines = makeLines(participants.count(), height);
-            return new Ladder(participants, lines, List.of("sadf"));
+            return new Ladder(participants, lines, prizes);
         });
+
         OutputView.printLadder(ladder);
+        do {
+            List<Result> results = IllegalArgumentExceptionHandler.handleExceptionByRepeating(() -> {
+                String participantName = InputView.inputParticipantNameToFind();
+                List<String> participantNames;
+                if (participantName.equals("all")) {
+                    participantNames = ladder.getParticipants().getNames();
+                } else {
+                    participantNames = List.of(participantName);
+                }
+
+                List<Result> results2 = new ArrayList<>();
+                for (String name : participantNames) {
+                    String prize = ladder.findPrizeFor(name);
+                    results2.add(new Result(name, prize));
+                }
+                return results2;
+            });
+            OutputView.printResults(results);
+            if (results.size() > 1) {
+                break;
+            }
+        } while (true);
     }
 
     private Participants gatherParticipants() {
