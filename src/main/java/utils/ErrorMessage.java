fe06df8d1793795cd9b37223c16fc40b7cc7c35b
yeonkkk <tjddus0302@gmail.com>
2023-02-20 16:37:17 +0900

    feat: 실행 결과 도메인 생성

--- src/main/java/utils/ErrorMessage.java
+++ src/main/java/utils/ErrorMessage.java
@@ -9,7 +9,8 @@ public enum ErrorMessage {
     NOT_FOUND_USER("유저가 존재하지 않습니다."),
     INVALID_LADDER_HEIGHT_BY_MINIMUM_LIMIT("사다리의 높이는 %d 이상입니다."),
     INVALID_LADDER_HEIGHT_BY_MAXIMUM_LIMIT("사다리의 높이는 %d 이하입니다."),
-    INVALID_LADDER_HEIGHT_INPUT("사다리의 높이는 숫자만 입력 가능합니다.");
+    INVALID_LADDER_HEIGHT_INPUT("사다리의 높이는 숫자만 입력 가능합니다."),
+    INVALID_RESULT_SIZE("실행 결과는 %d개 입력해야합니다.");
 
     private final String message;
 
