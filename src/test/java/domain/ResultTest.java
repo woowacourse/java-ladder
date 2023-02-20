452714527724bc5bcd343e91af11134759c1091d
jjongwa <troas96@naver.com>
2023-02-20 22:10:24 +0900

    test: 실행 결과 확인 유저 입력 검증 테스트

--- src/test/java/domain/ResultTest.java
+++ src/test/java/domain/ResultTest.java
@@ -2,6 +2,7 @@ package domain;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import controller.LadderGameController;
 import domain.user.User;
 import domain.user.Users;
 import java.util.List;
@@ -15,5 +16,13 @@ public class ResultTest {
         Users users = new Users(List.of(new User("dino"), new User("mango")));
         List<String> results = List.of("꽝", "1000", "꽝", "5000");
         assertThatThrownBy(() -> new Results(results, users.getPersonCount()));
+    }
+
+    @DisplayName("입력받은 값과 같은 이름의 유저가 없으면 실패한다.")
+    @Test
+    void shouldFailInputIsNotUser() {
+        Users users = new Users(List.of(new User("dino"), new User("mango")));
+        String input = "dinos";
+        assertThatThrownBy(() -> LadderGameController.checkNameInUsers(input, users));
     }
 }
