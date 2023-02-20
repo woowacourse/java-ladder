a51e662f2487ba9c7dda2432f37b3710bf011785
hyunseo <goatsfish@gmail.com>
2023-02-20 15:13:55 +0900

    refactor: Ladder 필드 수정

--- src/main/java/laddergame/domain/Ladder.java
+++ src/main/java/laddergame/domain/Ladder.java
@@ -1,25 +1,18 @@
 package laddergame.domain;
 
-import java.util.List;
 import java.util.Optional;
 
 public class Ladder {
-    private static final String LADDER_PARTICIPANTS_NULL_EXCEPTION = "참여자는 null이 될 수 없습니다.";
-    private static final String LADDER_HEIGHT_NULL_EXCEPTION = "높이는 null이 될 수 없습니다.";
+    private static final String LADDER_LINES_NULL_EXCEPTION = "Lines는 null이 될 수 없습니다.";
 
-    private final Participants participants;
-    private final Height height;
+    private final Lines lines;
 
-    public Ladder(final Participants participants, final Height height) {
-        this.participants = Optional.ofNullable(participants).orElseThrow(() ->
-                new IllegalArgumentException(LADDER_PARTICIPANTS_NULL_EXCEPTION));
-        
-        this.height = Optional.ofNullable(height).orElseThrow(() ->
-                new IllegalArgumentException(LADDER_HEIGHT_NULL_EXCEPTION));
+    public Ladder(final Lines inputLines) {
+        this.lines = Optional.ofNullable(inputLines)
+                .orElseThrow(() -> new IllegalArgumentException(LADDER_LINES_NULL_EXCEPTION));
     }
 
-    public List<Line> createLines(final BooleanGenerator booleanGenerator) {
-        final LineCreator lineCreator = new LineCreator(booleanGenerator);
-        return lineCreator.createLines(participants.getSize() - 1, height.getValue());
+    public Lines getLines() {
+        return lines;
     }
 }
