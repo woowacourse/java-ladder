078170d1a9e01a1668925ecd6be593e6e63639cc
2014012606 <2014012606@hanyang.ac.kr>
2023-02-20 16:45:36 +0900

    test: LineGeneratorTest 수정
    - DisplayName 네이밍 명확히 변경
    - 다른 클래스의 기능을 확인하는 테스트코드 삭제

--- src/test/java/domain/LineGeneratorTest.java
+++ src/test/java/domain/LineGeneratorTest.java
@@ -2,29 +2,19 @@ package domain;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 public class LineGeneratorTest {
     private final LineGenerator lineGenerator = new LineGenerator();
 
     @ParameterizedTest
-    @DisplayName("랜덤으로 생성된 Line의 길이가 맞는지 테스트")
+    @DisplayName("사람 수를 입력받으면 사람 수보다 하나 적은 라인이 생성된다")
     @ValueSource(ints = {1, 2, 3, 4, 5})
     void randomLineLengthTest(int personCount) {
         Line line = lineGenerator.generate(personCount);
         int lineNumber = line.getLine().size();
         int expectedLineNumber = personCount - 1;
         Assertions.assertThat(lineNumber).isEqualTo(expectedLineNumber);
-    }
-
-    @RepeatedTest(100)
-    @DisplayName("랜덤으로 생성된 Line이 유효한지 반복 테스트")
-    void randomLineValidateTest() {
-        int validatePersonCount = 4;
-        assertDoesNotThrow(() -> lineGenerator.generate(validatePersonCount));
     }
 }
