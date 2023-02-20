6e1b2fb3ef3f0d2bed2da9a8dc4b9c0e82c48467
hectick <chyeon1223@cau.ac.kr>
2023-02-20 16:19:28 +0900

    test(PlayersTest): 플레이어가 26명 이하, 26명 초과인 경우를 테스트하는 코드 작성

--- src/test/java/ladder/model/PlayersTest.java
+++ src/test/java/ladder/model/PlayersTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 
@@ -13,17 +14,33 @@ class PlayersTest {
 
     @Test
     @DisplayName("플레이어가 2명 미만이면 예외처리 테스트")
-    void invalidHeightTest() {
+    void invalidSmallPlayerCountTest() {
         List<Player> input = new ArrayList<>(List.of(new Player("이오")));
         Assertions.assertThatThrownBy(() -> new Players(input))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
 
+    @Test
+    @DisplayName("플레이어가 26명 초과면 통과하는 테스트")
+    void invalidLargePlayerCountTest() {
+        List<String> players = new ArrayList<>(List.of("a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,zzz".split(",")));
+        List<Player> input = players.stream().map(Player::new).collect(Collectors.toList());
+        Assertions.assertThatThrownBy(() -> new Players(input))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     @DisplayName("플레이어가 2명 이상이면 통과하는 테스트")
-    void validHeightTest() {
+    void validMinPlayerCountTest() {
         List<Player> input = new ArrayList<>(List.of(new Player("이오"), new Player("이리내")));
+        assertThatCode(() -> new Players(input)).doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("플레이어가 26명 이하면 통과하는 테스트")
+    void validMaxPlayerCountTest() {
+        List<String> players = new ArrayList<>(List.of("a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z".split(",")));
+        List<Player> input = players.stream().map(Player::new).collect(Collectors.toList());
         assertThatCode(() -> new Players(input)).doesNotThrowAnyException();
     }
 
