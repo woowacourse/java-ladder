980f4a6cd51ac68d6447f4e32cbdc68bb5184e39
This2sho <zx00018@naver.com>
2023-02-20 17:10:23 +0900

    refactor(LadderGenerator, RandomLadderGenerator): 클래스명 변경 및 최종 반환값 변경
    - BridgeGenerator.java -> LadderGenerator.java 로 변경
    - 기존 Line 을 생성하던 것에서 Ladder를 생성하도록 변경

--- src/main/java/domain/BridgeGenerator.java
+++
@@ -1,9 +0,0 @@
-package domain;
-
-import java.util.List;
-
-@FunctionalInterface
-public interface BridgeGenerator {
-
-    List<Boolean> generate(int count);
-}
--- src/main/java/domain/RandomLadderGenerator.java
+++ src/main/java/domain/RandomLadderGenerator.java
@@ -4,24 +4,31 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class RandomBridgeGenerator implements BridgeGenerator {
+public class RandomLadderGenerator implements LadderGenerator {
 
     private static final Random random = new Random();
+
+    @Override
+    public List<Line> generate(int width, int height) {
+        List<Line> lines = new ArrayList<>(height);
+        for (int i = 0; i < height; i++) {
+            List<Boolean> movements = generate(width);
+            lines.add(new Line(movements));
+        }
+        return lines;
+    }
 
     /**
      * 이 생성전략은 (사람수 - 1) 길이, 랜덤한 true와 false값을 리스트로 반환한다.
      *
      * <p>
-     *     랜덤을 생성하면서, 연속된 true값은 나올 수 없다.
-     *     이전 요소값이 true이면 현재 요소값은 무조건 false를 삽입한다.
-     *     그렇지 않다면 랜덤값을 삽입한다.
+     * 랜덤을 생성하면서, 연속된 true값은 나올 수 없다. 이전 요소값이 true이면 현재 요소값은 무조건 false를 삽입한다. 그렇지 않다면 랜덤값을 삽입한다.
      * </p>
      *
      * @param count 사람수; 0 이상이어야 한다.
      * @return (사람수 - 1) 길이의 true와 false 요소값을 가지는 리스트를 반환한다.
      */
-    @Override
-    public List<Boolean> generate(int count) {
+    private List<Boolean> generate(int count) {
         List<Boolean> movements = new ArrayList<>(count);
         movements.add(random.nextBoolean());
         for (int i = 1; i < count - 1; i++) {
