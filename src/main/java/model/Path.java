12110d89fe33f83f9b63d1240235bf5047490840
apptie <appti@naver.com>
2023-02-20 17:48:57 +0900

    feat: 주어진 Path와 자기 자신이 모두 건널 수 있는 경로인지 확인하는 기능 추가

--- src/main/java/model/Path.java
+++ src/main/java/model/Path.java
@@ -21,17 +21,21 @@ public enum Path {
 
     public static Path calculatePath(Path otherPath, boolean passable){
         return Arrays.stream(Path.values())
-                .filter(path -> path.isSamePassable(calculateMovable(otherPath, passable)))
+                .filter(path -> path.isSamePassable(calculatePassable(otherPath, passable)))
                 .findAny()
                 .orElseThrow(() -> new IllegalStateException("유효한 Path를 찾지 못했습니다."));
+    }
+
+    private static boolean calculatePassable(Path otherPath, boolean passable) {
+        return !otherPath.passable && passable;
     }
 
     private boolean isSamePassable(boolean passable) {
         return this.passable == passable;
     }
 
-    private static boolean calculateMovable(Path otherPath, boolean passable) {
-        return !otherPath.passable && passable;
+    public boolean isMultiplePassable(Path otherPath) {
+        return this.passable && otherPath.passable;
     }
 
     public boolean isPassable() {
