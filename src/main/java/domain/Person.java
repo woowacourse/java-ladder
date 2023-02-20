a28b45dc29d66f5b7eacb05e235589b2e9bfd796
nuyh <hjaehyun25@gmail.com>
2023-02-20 17:05:32 +0900

    refactor(Person): 이름 최대 길이 및 최소 길이 상수화

--- src/main/java/domain/Person.java
+++ src/main/java/domain/Person.java
@@ -3,6 +3,9 @@ package domain;
 import exception.Error;
 
 public class Person {
+	private static final int MIN_NAME_LENGTH_INCLUSIVE = 1;
+	private static final int MAX_NAME_LENGTH_INCLUSIVE = 5;
+
 	private final String name;
 
 	public Person(String name) {
@@ -18,7 +21,7 @@ public class Person {
 	private void validate(String name) {
 		name = name.trim();
 
-		if (name.length() < 1 || name.length() > 5) {
+		if (name.length() < MIN_NAME_LENGTH_INCLUSIVE || name.length() > MAX_NAME_LENGTH_INCLUSIVE) {
 			throw new IllegalArgumentException(Error.INVALID_NAME_LENGTH.getMessage());
 		}
 	}
