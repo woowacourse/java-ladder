4e21fab27fd2583ffc0f37a1addc3c6d7f8acee2
hectick <chyeon1223@cau.ac.kr>
2023-02-20 19:25:08 +0900

    feat(Ladder): 사다리 입구에 따른 출구를 판단하는 메소드 구현

--- src/main/java/ladder/model/Ladder.java
+++ src/main/java/ladder/model/Ladder.java
@@ -10,6 +10,43 @@ public class Ladder {
         this.rows = rows;
     }
 
+    public int findExitFrom(int entrance) {
+        int position = entrance;
+        for (Row row : rows) {
+            position = findNextPosition(row, position);
+        }
+        return position;
+    }
+
+    private int findNextPosition(Row row, int position) {
+        if(canGoLeft(row, position)){
+            position--;
+            return position;
+        }
+        if(canGoRight(row, position)){
+            position++;
+            return position;
+        }
+        return position;
+    }
+
+    private boolean canGoLeft(Row row, int position){
+        if(row.isLeftPointHasLine(position)){
+            return true;
+        }
+        return false;
+    }
+
+    private boolean canGoRight(Row row, int position){
+        if(position == row.getSize()){
+            return false;
+        }
+        if(row.isCurrentPointHasLine(position)){
+            return true;
+        }
+        return false;
+    }
+
     public List<Row> getRows() {
         return rows;
     }
