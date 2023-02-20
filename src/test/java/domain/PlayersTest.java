bd132c5dc7a388b5db5d6e7a243ae83f12b6c065
aiaiaiai1 <cvc100326@naver.com>
2023-02-20 14:31:09 +0900

    test: 클래스 이름 Persons 에서 Players 로 변경
    - 해당되는 메서도도 같이 변경

--- src/test/java/domain/PlayersTest.java
+++ src/test/java/domain/PlayersTest.java
@@ -6,20 +6,20 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class PersonsTest {
+public class PlayersTest {
     @Test
     @DisplayName("참여자들의 이름을 받아 참여자그룹 생성한다.")
     void persons() {
-        Persons persons = new Persons(List.of("1", "2", "3"));
+        Players players = new Players(List.of("1", "2", "3"));
 
-        Assertions.assertThat(persons.getPersonsName())
+        Assertions.assertThat(players.getPlayersName())
                 .containsExactly("1", "2", "3");
     }
 
     @Test
     @DisplayName("참여자들중 중복된 이름이 존재하면 예외가 발생한다.")
     void validateDuplicateName() {
-        Assertions.assertThatThrownBy(() -> new Persons(List.of("a", "b", "a")))
+        Assertions.assertThatThrownBy(() -> new Players(List.of("a", "b", "a")))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("중복된 이름입니다.");
     }
