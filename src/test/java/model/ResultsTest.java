d343d02b77bdcb0b7c69024b315c1a18df0c8803
MoonJeWoong <munjin0201@naver.com>
2023-02-20 22:19:12 +0900

    test: Results 일급 컬렉션 요소 값 Result 테스트 항목 추가

--- src/test/java/model/ResultsTest.java
+++ src/test/java/model/ResultsTest.java
@@ -12,12 +12,22 @@ import org.junit.jupiter.params.provider.ValueSource;
 import java.util.List;
 
 public class ResultsTest {
+    private static final int MAXIMUM_RESULT_LENGTH = 5;
+    private static final String MAXIMUM_RESULT_LENGTH_ERROR = "[ERROR] 게임 결과 입력값 길이는 %d 이하로만 가능합니다.";
     private static final String WRONG_SIZE_RESULTS_ERROR = "[ERROR] 사다리 게임 결과 값의 개수는 전체 사람의 수와 동일해야 합니다.";
 
     @Test
     @DisplayName("Results 객체 생성 성공 테스트")
     void createResultsTest() {
         assertThatNoException().isThrownBy(() -> new Results(4, "꽝, 5000, 꽝, 3000"));
+    }
+
+    @Test
+    @DisplayName("결과값 길이 제한으로 인한 Result 객체 생성 실패 테스트")
+    void limitResultLengthTest() {
+        assertThatThrownBy(() -> new Results(2,"abcdef, gehijk"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(String.format(MAXIMUM_RESULT_LENGTH_ERROR, MAXIMUM_RESULT_LENGTH));
     }
 
     @ParameterizedTest
