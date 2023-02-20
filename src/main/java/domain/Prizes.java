0e2e64da30b76ab14a6421938695cd76e54023d9
iamjooon2 <iamjooon2@gmail.com>
2023-02-20 21:28:14 +0900

    feat: 당첨 결과의 일급 컬렉션인 Prizes 구현

---
+++ src/main/java/domain/Prizes.java
@@ -0,0 +1,26 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Prizes {
+
+    private static final String ITEM_SIZE_NOT_MATCH = "[ERROR] 결과의 개수는 사용자의 개수와 일치해야합니다.";
+
+    private final List<Prize> prizes;
+
+    public Prizes(final List<Prize> prizes, final Names names) {
+        validateItemSize(prizes, names);
+        this.prizes = prizes;
+    }
+
+    private static void validateItemSize(final List<Prize> prizes, final Names names) {
+        if (prizes.size() != names.size()) {
+            throw new IllegalArgumentException(ITEM_SIZE_NOT_MATCH);
+        }
+    }
+
+    public List<Prize> getPrizes(){
+        return new ArrayList<>(prizes);
+    }
+}
