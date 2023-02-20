086208593fe91e8eaf199a393a729304fd26acda
xxeol2 <jhj01177@naver.com>
2023-02-20 14:39:05 +0900

    feat: 발판 선택 메서드 테스트

---
+++ src/test/java/domain/ladder/PointTest.java
@@ -0,0 +1,23 @@
+package domain.ladder;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PointTest {
+
+    @DisplayName("이전 Point가 발판이 존재하면, 현재 Point에는 발판이 존재하지 않는다.")
+    @Test
+    void previousPointExistThenNotExist() {
+        Point currentPoint = Point.choosePoint(Point.EXIST);
+        assertThat(currentPoint).isEqualTo(Point.NOT_EXIST);
+    }
+
+    @DisplayName("이전 Point가 발판이 존재하지 않으면, 현재 Point에는 발판이 존재하거나 존재하지 않는다.")
+    @Test
+    void previousPointNotExistThenExistOrNotExist() {
+        Point currentPoint = Point.choosePoint(Point.NOT_EXIST);
+        assertThat(currentPoint).isIn(Point.NOT_EXIST, Point.EXIST);
+    }
+}
