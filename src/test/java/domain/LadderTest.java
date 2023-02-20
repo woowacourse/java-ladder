865a8096514de64d6ea43a62c28186ab471d58ec
2014012606 <2014012606@hanyang.ac.kr>
2023-02-20 15:33:42 +0900

    test: 입력된 사다리의 높이가 동일한 높이를 가지는 사다리가 생성됐는지 확인하는 테스트 코드 추가

--- src/test/java/domain/LadderTest.java
+++ src/test/java/domain/LadderTest.java
@@ -3,14 +3,25 @@ package domain;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class LadderTest {
     private final int validatePersonCount = 5;
 
     @Test
-    @DisplayName("사다리의 높이로 1~100 사이의 수를 입력받는다.")
-    void ladderTest() {
+    @DisplayName("사다리의 높이로 1~100 사이의 수(1)를 입력받는다.")
+    void ladderMinHeightTest() {
         int height = 1;
+        Assertions.assertDoesNotThrow(() -> new Ladder(height, validatePersonCount));
+    }
+
+    @Test
+    @DisplayName("사다리의 높이로 1~100 사이의 수(100)를 입력받는다.")
+    void ladderMaxHeightTest() {
+        int height = 100;
         Assertions.assertDoesNotThrow(() -> new Ladder(height, validatePersonCount));
     }
 
@@ -33,5 +44,14 @@ class LadderTest {
     void ladderHeightOver100Test() {
         int height = 101;
         Assertions.assertThrows(IllegalArgumentException.class, () -> new Ladder(height, validatePersonCount));
+    }
+
+    @ParameterizedTest(name = "입력된 사다리의 높이 = {0}")
+    @DisplayName("입력된 사다리의 높이에 맞는 사다리가 생성되었는지 확인한다.")
+    @ValueSource(ints = {1,50,100})
+    void ladderHeightTest(int height) {
+        Ladder ladder = new Ladder(height, validatePersonCount);
+        int ladderHeight = ladder.getLadder().size();
+        assertThat(ladderHeight).isEqualTo(height);
     }
 }
