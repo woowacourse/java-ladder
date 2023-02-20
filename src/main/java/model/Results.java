4a016269d06f0e0d711185cfab68b8865619e5d2
MoonJeWoong <munjin0201@naver.com>
2023-02-20 22:21:15 +0900

    refactor: Results 일급 컬렉션 요소 값 Result로 변경

--- src/main/java/model/Results.java
+++ src/main/java/model/Results.java
@@ -8,16 +8,20 @@ public class Results {
     private static final String WRONG_SIZE_RESULTS_ERROR = "[ERROR] 사다리 게임 결과 값의 개수는 전체 사람의 수와 동일해야 합니다.";
     private static final String RESULTS_DELIMITER = ",";
 
-    private final List<String> results;
+    private final List<Result> results;
 
     public Results(int playersSize, String inputResults) {
         List<String> results = splitInputResults(inputResults);
         validateRightResultsSize(playersSize, results);
-        this.results = results;
+        this.results = results.stream()
+                .map(Result::new)
+                .collect(Collectors.toList());
     }
 
     public List<String> getValues() {
-        return results;
+        return results.stream()
+                .map(Result::getValue)
+                .collect(Collectors.toList());
     }
 
     private void validateRightResultsSize(int playersSize, List<String> results) {
