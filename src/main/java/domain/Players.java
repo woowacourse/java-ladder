30f3ba0ae76b81bc44883c39b6cc757189328485
parkmuhyeun <pjhg410@gmail.com>
2023-02-20 15:27:51 +0900

    feat: 플레이어 크기 예외처리 기능 추가

--- src/main/java/domain/Players.java
+++ src/main/java/domain/Players.java
@@ -6,12 +6,21 @@ import java.util.List;
 public class Players {
 
     private static final String DUPLICATE_NAME_ERROR = "[ERROR] 중복된 이름입니다.";
+    private static final String SIZE_ERROR = "[ERROR] 2명 이상 입력해야 합니다.";
+    private static final int MIN_SIZE = 2;
 
     private final List<Player> players = new ArrayList<>();
 
     public Players(List<String> names) {
         validateDuplicateName(names);
+        validatePlayersSize(names);
         addPlayer(names);
+    }
+
+    private static void validatePlayersSize(List<String> names) {
+        if (names.size() < MIN_SIZE) {
+            throw new IllegalArgumentException(SIZE_ERROR);
+        }
     }
 
     public List<String> getPlayersName() {
