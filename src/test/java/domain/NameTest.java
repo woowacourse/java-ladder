a323bbdc3dd508c7ece92654a36d4ea8455fe78a
odo27 <odo27@odo27ui-MacBookAir.local>
2023-02-20 17:26:05 +0900

    refactor: 테스트 메서드 이름 변경

--- src/test/java/domain/NameTest.java
+++ src/test/java/domain/NameTest.java
@@ -1,28 +1,26 @@
 package domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 public class NameTest {
 
     @DisplayName("사람 이름의 앞 뒤 공백은 무시한다.")
     @ParameterizedTest
     @CsvSource({"'  kong',kong", "'kong  ',kong", "' ko ng ',ko ng", "'    kong   ',kong"})
-    void validateTest(String name, String expected) {
-        Name player = new Name(name);
-        Assertions.assertThat(player.getName()).isEqualTo(expected);
+    void ignoreBlankOfHeadAndTail(String name, String expected) {
+        assertThat(new Name(name).getName()).isEqualTo(expected);
     }
 
     @DisplayName("사람 이름은 1글자에서 5글자 사이이다.")
     @ParameterizedTest
-    @ValueSource(strings = {"", "kongha", "  ", "ko   ng"})
-    void validateTest2(String name) {
+    @CsvSource({"''", "kongha", "'  '", "ko   ng"})
+    void throwExceptionWhenNameIsInvalid(String name) {
         assertThatThrownBy(() -> new Name(name))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("이름은 1글자에서 5글자 사이이어야 합니다.");
@@ -30,8 +28,8 @@ public class NameTest {
 
     @DisplayName("사람 이름이 정상적으로 입력되는 경우에는 이름이 생성된다.")
     @ParameterizedTest
-    @ValueSource(strings = {"odo", "kong"})
-    void validateTest3(String name) {
+    @CsvSource({"odo", "kong"})
+    void doesNotThrowExceptionWhenNameIsValid(String name) {
         assertThatCode(() -> new Name(name))
                 .doesNotThrowAnyException();
     }
