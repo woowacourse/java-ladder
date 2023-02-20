e7c03a8b1ea9b37d241f182930639002003de70d
Cyma-s <ssol0319@naver.com>
2023-02-20 17:53:56 +0900

    feat: 같은 위치에 있는지 확인하는 메서드 추가

--- src/main/java/ladder/domain/Item.java
+++ src/main/java/ladder/domain/Item.java
@@ -2,8 +2,18 @@ package ladder.domain;
 
 public class Item {
     private final String item;
+    private final Position position;
 
-    public Item(String item) {
+    public Item(String item, int position) {
         this.item = item;
+        this.position = new Position(position);
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public boolean isSamePosition(Position position) {
+        return this.position.equals(position);
     }
 }
--- src/main/java/ladder/domain/Position.java
+++ src/main/java/ladder/domain/Position.java
@@ -1,5 +1,7 @@
 package ladder.domain;
 
+import java.util.Objects;
+
 public class Position {
     private final int value;
 
@@ -16,5 +18,18 @@ public class Position {
 
     public int getValue() {
         return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return value == position.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
     }
 }
