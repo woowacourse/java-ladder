fd80eb1e11b7f4337421dba377c91adca753e1f4
hectick <chyeon1223@cau.ac.kr>
2023-02-21 00:03:24 +0900

    test(RewardsTest): 테스트 코드 오류 수정

--- src/test/java/ladder/model/RewardsTest.java
+++ src/test/java/ladder/model/RewardsTest.java
@@ -7,12 +7,14 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+
 class RewardsTest {
 
     @Test
     @DisplayName("플레이어 수와 실행 결과 수가 일치하지 않는 경우 예외처리 테스트")
     void invalidResultCountTest(){
-        List<String> rewards = new ArrayList<>(List.of("꽝", "꽝", "3000", "5000"));
+        List<Reward> rewards = new ArrayList<>(List.of(new Reward("꽝"), new Reward("꽝"), new Reward("3000"), new Reward("5000")));
         Assertions.assertThatThrownBy(() -> new Rewards(rewards, 2))
                 .isInstanceOf(IllegalArgumentException.class);
     }
@@ -20,9 +22,8 @@ class RewardsTest {
     @Test
     @DisplayName("플레이어 수와 실행 결과 수가 일치하면 통과 테스트")
     void validResultCountTest(){
-        List<String> rewards = new ArrayList<>(List.of("꽝", "꽝", "3000", "5000"));
-        Assertions.assertThatThrownBy(() -> new Rewards(rewards, 4))
-                .isInstanceOf(IllegalArgumentException.class);
+        List<Reward> rewards = new ArrayList<>(List.of(new Reward("꽝"), new Reward("꽝"), new Reward("3000"), new Reward("5000")));
+        assertThatCode(() -> new Rewards(rewards, 4)).doesNotThrowAnyException();
     }
 
 }
