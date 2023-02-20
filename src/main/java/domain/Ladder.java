4aeb3e5523008640ddd6761072c6a7e7cd84cd84
Miseong Kim <miseongk@naver.com>
2023-02-20 16:37:59 +0900

    refactor: 주석 제거

--- src/main/java/domain/Ladder.java
+++ src/main/java/domain/Ladder.java
@@ -12,17 +12,7 @@ public class Ladder {
         this.numberOfWalls = numberOfWalls;
         this.height = height;
         this.lines = new Lines(numberOfWalls, height, lineGenerator);
-        //makeLines(lineGenerator);
     }
-
-//    public void makeLines(LineGenerator lineGenerator) {
-//        //this.lines = new ArrayList<>();
-//
-//        for (int i = 0; i < height.getHeight(); i++) {
-//            lines.add(new Line(this.numberOfWalls - 1, lineGenerator));
-//        }
-//
-//    }
 
     public Lines getLines() {
         return this.lines;
