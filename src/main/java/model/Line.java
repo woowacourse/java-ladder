a195854bf16cdb60174e54b3e7ff7bdc383c6c24
apptie <appti@naver.com>
2023-02-20 17:55:51 +0900

    feat: Line 검증 로직 추가 및 paths 크기 비교 로직 추가

--- src/main/java/model/Line.java
+++ src/main/java/model/Line.java
@@ -2,16 +2,45 @@ package model;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.IntStream;
 
 public class Line {
 
-    private final List<Path> line;
+    private final List<Path> paths;
 
-    public Line(List<Path> line) {
-        this.line = line;
+    public Line(List<Path> paths) {
+        validatePaths(paths);
+
+        this.paths = paths;
+    }
+
+    private void validatePaths(List<Path> paths) {
+        if (Objects.isNull(paths)) {
+            throw new IllegalStateException("사다리 경로가 정상적으로 입력되지 않았습니다.");
+        }
+        if (paths.isEmpty()) {
+            throw new IllegalArgumentException("1 이상의 경로를 입력해주세요.");
+        }
+        if (isMultiplePassable(paths)) {
+            throw new IllegalArgumentException("사다리는 연속적으로 건널 수 없습니다.");
+        }
+    }
+
+    private boolean isMultiplePassable(List<Path> paths) {
+        return IntStream.range(0, paths.size() - 1)
+                .anyMatch(i -> isMultiplePassable(paths.get(i), paths.get(i + 1)));
+    }
+
+    private boolean isMultiplePassable(Path leftPath, Path rightPath) {
+        return leftPath.isMultiplePassable(rightPath);
+    }
+
+    public boolean isSamePathSize(Line otherLine) {
+        return this.paths.size() == otherLine.paths.size();
     }
 
     public List<Path> getLine() {
-        return Collections.unmodifiableList(line);
+        return Collections.unmodifiableList(paths);
     }
 }
