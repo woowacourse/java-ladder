0525abf525d5919877e4c7132489a0ab4fdf0dac
xxeol2 <jhj01177@naver.com>
2023-02-20 14:19:47 +0900

    refactor: 테스트 클래스 내 인스턴스 변수 접근제어자 private으로 변경

--- src/test/java/domain/LadderGameTest.java
+++ src/test/java/domain/LadderGameTest.java
@@ -11,7 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class LadderGameTest {
 
-    LadderGame ladderGame;
+    private LadderGame ladderGame;
 
     @BeforeEach
     void before() {
