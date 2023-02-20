44e1f595bc6eddbc32d4b8683b5bf52e1446d55d
hyunseo <goatsfish@gmail.com>
2023-02-20 09:42:46 +0900

    refactor: null 검증 Optional 활용 수정

--- src/main/java/laddergame/domain/Ladder.java
+++ src/main/java/laddergame/domain/Ladder.java
@@ -1,7 +1,7 @@
 package laddergame.domain;
 
 import java.util.List;
-import java.util.Objects;
+import java.util.Optional;
 
 public class Ladder {
     private static final String LADDER_PARTICIPANTS_NULL_EXCEPTION = "참여자는 null이 될 수 없습니다.";
@@ -11,18 +11,11 @@ public class Ladder {
     private final Height height;
 
     public Ladder(final Participants participants, final Height height) {
-        validateNotNull(participants, height);
-        this.participants = participants;
-        this.height = height;
-    }
-
-    private void validateNotNull(final Participants participants, final Height height) {
-        if (Objects.isNull(participants)) {
-            throw new IllegalArgumentException(LADDER_PARTICIPANTS_NULL_EXCEPTION);
-        }
-        if (Objects.isNull(height)) {
-            throw new IllegalArgumentException(LADDER_HEIGHT_NULL_EXCEPTION);
-        }
+        this.participants = Optional.ofNullable(participants).orElseThrow(() ->
+                new IllegalArgumentException(LADDER_PARTICIPANTS_NULL_EXCEPTION));
+        
+        this.height = Optional.ofNullable(height).orElseThrow(() ->
+                new IllegalArgumentException(LADDER_HEIGHT_NULL_EXCEPTION));
     }
 
     public List<Line> createLines(final BooleanGenerator booleanGenerator) {
