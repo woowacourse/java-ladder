8bd61dfc16d76bafff9c3d94f0d4dbfb1f9b5572
dooboocookie <bkh3030@gmail.com>
2023-02-20 14:42:19 +0900

    test: MockValueGenerator 순차적으로 값 생성하도록 변경

--- src/test/java/ladder/domain/valueGenerator/MockValueGenerator.java
+++ src/test/java/ladder/domain/valueGenerator/MockValueGenerator.java
@@ -1,14 +1,33 @@
 package ladder.domain.valueGenerator;
 
+import java.util.List;
+
 public class MockValueGenerator implements ValueGenerator {
 
-    @Override
-    public boolean generateBoolean() {
-        return false;
+    private final List<Integer> generatedNumber;
+    private final List<Boolean> generatedBoolean;
+
+    private int orderOfNumber;
+    private int orderOfBoolean;
+
+    public MockValueGenerator(List<Integer> generatedNumber, List<Boolean> generatedBoolean) {
+        this.generatedNumber = generatedNumber;
+        this.generatedBoolean = generatedBoolean;
     }
 
     @Override
-    public int generateNumber(int min, int targetValue) {
-        return targetValue;
+    public boolean generateBoolean() {
+        if (generatedBoolean.size() == orderOfBoolean) {
+            orderOfBoolean = 0;
+        }
+        return generatedBoolean.get(orderOfBoolean++);
+    }
+
+    @Override
+    public int generateNumber(int min, int max) {
+        if (generatedNumber.size() == orderOfNumber) {
+            orderOfNumber = 0;
+        }
+        return generatedNumber.get(orderOfNumber++);
     }
 }
