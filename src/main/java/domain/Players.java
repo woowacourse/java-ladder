f20c67d07c481008c68e476efc435ed90bfd8d7e
aiaiaiai1 <cvc100326@naver.com>
2023-02-20 15:27:04 +0900

    feat: 참여자 수를 2이상 100이하로 제한하는 기능 추가

--- src/main/java/domain/Players.java
+++ src/main/java/domain/Players.java
@@ -5,12 +5,16 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Players {
+    private static final int MIN_NUMBER_OF_PLAYERS = 2;
+    private static final int MAX_NUMBER_OF_PLAYERS = 100;
     private static final String DUPLICATE_NAME_MESSAGE = "중복된 이름입니다.";
+    private static final String INVALID_NUMBER_OF_PLAYER_MESSAGE = "참여자 수는 %d부터 %d까지 입니다.";
 
     private final List<Person> players = new ArrayList<>();
 
     public Players(List<String> names) {
         validateDuplicateNames(names);
+        validateNumberOfPlayer(names);
         initiatePlayers(names);
     }
 
@@ -27,6 +31,13 @@ public class Players {
     private void validateDuplicateNames(List<String> names) {
         if (isDuplicated(names)) {
             throw new IllegalArgumentException(DUPLICATE_NAME_MESSAGE);
+        }
+    }
+
+    private void validateNumberOfPlayer(List<String> names) {
+        if (names.size() < MIN_NUMBER_OF_PLAYERS || names.size() > MAX_NUMBER_OF_PLAYERS) {
+            throw new IllegalArgumentException(
+                    String.format(INVALID_NUMBER_OF_PLAYER_MESSAGE, MIN_NUMBER_OF_PLAYERS, MAX_NUMBER_OF_PLAYERS));
         }
     }
 
