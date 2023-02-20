55a8aed260e5351abebcd17a14a5508292879259
hectick <chyeon1223@cau.ac.kr>
2023-02-20 23:42:31 +0900

    test(RewardTest): 실행 결과에서 공백을 제거하는 테스트 작성

--- src/test/java/ladder/model/RewardTest.java
+++ src/test/java/ladder/model/RewardTest.java
@@ -3,8 +3,10 @@ package ladder.model;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 
 class RewardTest {
@@ -22,6 +24,15 @@ class RewardTest {
     @ValueSource(strings = {"꽝", "3000", "50000"})
     void validNameLengthTest(String input) {
         assertThatCode(() -> new Reward(input)).doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest
+    @DisplayName("실행 결과 공백을 제거 테스트")
+    @CsvSource(value = {" 꽝   :꽝", "꽝 꽝:꽝꽝"}, delimiter = ':', ignoreLeadingAndTrailingWhitespace = false)
+    void removeWhiteSpaceTest(String input, String expected) {
+        System.out.println(input);
+        Reward reward = new Reward(input);
+        assertThat(reward.getReward()).isEqualTo(expected);
     }
 
 }
