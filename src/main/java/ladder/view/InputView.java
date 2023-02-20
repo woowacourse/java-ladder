3fca9ea3d742af8b8f7a412c770961082535c830
hum02 <ehdgma02@gmail.com>
2023-02-20 09:58:06 +0900

    feat: 실행결과 쉼표로 구분해 입력받기 기능 구현

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -18,4 +18,10 @@ public class InputView {
         System.out.println("최대 사다리 높이는 몇 개인가요?");
         return scanner.reset().nextInt();
     }
+
+    public static List<String> readResults() {
+        System.out.println("실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
+        String  results = scanner.reset().nextLine();
+        return Arrays.stream(results.split(",")).collect(Collectors.toList());
+    }
 }
