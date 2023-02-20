40f153fb583055a536ce928a96ddb6e7e131d091
LJW25 <dbjwl@g.skku.edu>
2023-02-20 17:55:41 +0900

    feat(Reward): 보상 길이 제한 기능 구현

---
+++ src/main/java/ladder/model/Reward.java
@@ -0,0 +1,26 @@
+package ladder.model;
+
+import static ladder.model.ErrorMessage.EXCEPTION_REWARD_LENGTH;
+
+public class Reward {
+
+
+    private static final int MIN_LENGTH = 1;
+    private static final int MAX_LENGTH = 5;
+    private final String reward;
+
+    public Reward(String reward) {
+        validateReward(reward);
+        this.reward = reward;
+    }
+
+    private void validateReward(String reward) {
+        if (reward.length() < MIN_LENGTH || reward.length() > MAX_LENGTH) {
+            throw new IllegalArgumentException(EXCEPTION_REWARD_LENGTH.getMessage());
+        }
+    }
+
+    public String getReward() {
+        return reward;
+    }
+}
