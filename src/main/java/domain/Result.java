f3e984a0447a3d7165e52a5e66645a99a558f9c3
kokodak <kokodakadokok@gmail.com>
2023-02-21 15:41:41 +0900

    feat: 실행 결과 이름을 포장할 Result 생성

---
+++ src/main/java/domain/Result.java
@@ -0,0 +1,14 @@
+package domain;
+
+public class Result {
+
+    private final String resultName;
+
+    public Result(final String resultName) {
+        this.resultName = resultName;
+    }
+
+    public String getResultName() {
+        return resultName;
+    }
+}
