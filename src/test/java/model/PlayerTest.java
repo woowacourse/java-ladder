36a096bb64febdd7a10068e81df425e567860b2a
MoonJeWoong <munjin0201@naver.com>
2023-02-21 22:09:04 +0900

    test: 참여자의 현재 위치와 주어진 위치 값의 동일 여부 확인 기능 테스트 추가
    세부사항
    - Player의 생성자 시그니처 변경으로 인한 리팩토링

--- src/test/java/model/PlayerTest.java
+++ src/test/java/model/PlayerTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 /**
  * 사다리 게임 참여자 정보를 가지는 클래스.
@@ -14,14 +16,14 @@ public class PlayerTest {
     @Test
     @DisplayName("Player 객체 생성 성공 테스트")
     void createPlayerTest() {
-        Assertions.assertThatNoException().isThrownBy(() -> new Player(new Name("pobi")));
+        Assertions.assertThatNoException().isThrownBy(() -> new Player(new Name("pobi"), new Position(0)));
     }
 
     @Test
     @DisplayName("참여자의 이름 정보가 일치하는지 확인할 수 있는 기능 테스트")
     void isPlayerNameTest() {
         //given
-        Player player = new Player(new Name("pobi"));
+        Player player = new Player(new Name("pobi"), new Position(0));
 
         //when
         boolean result = player.isPlayerName(new Name("pobi"));
@@ -34,7 +36,7 @@ public class PlayerTest {
     @DisplayName("사다리 게임의 개인 결과를 저장하는 기능 테스트")
     void savePlayerResultTest() {
         //given
-        Player player = new Player(new Name("pobi"));
+        Player player = new Player(new Name("pobi"), new Position(0));
         Result result = new Result("5000");
 
         //when
@@ -42,5 +44,19 @@ public class PlayerTest {
 
         //then
         assertThat(player.getResult()).isEqualTo("5000");
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"0:0:true", "0:1:false"}, delimiter = ':')
+    @DisplayName("참여자의 현재 위치와 주어진 위치 값의 동일 여부 확인 기능 테스트")
+    void savePlayerPositionTest(int playerPosition, int inputNumber, boolean expected) {
+        //given
+        Player player = new Player(new Name("hiiro"), new Position(playerPosition));
+
+        //when
+        boolean result = player.isPlayerPosition(new Position(inputNumber));
+
+        //then
+        assertThat(result).isEqualTo(expected);
     }
 }
