aef830e163cfa951d2cff0e777898ea28f361187
이건회 <rg970604@naver.com>
2023-02-21 13:34:38 +0900

    refactor : 메소드 네이밍 변경

--- src/main/java/model/Pass.java
+++ src/main/java/model/Pass.java
@@ -4,11 +4,4 @@ public enum Pass {
     LEFT, // "--ㅣ  "
     RIGHT,// "  ㅣ--"
     NOTHING // "  ㅣ  "
-    ;
-
-
-    @Override
-    public String toString() {
-        return super.toString();
-    }
 }
