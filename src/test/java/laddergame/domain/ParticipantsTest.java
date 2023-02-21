83e609a55106b45df4c439816fd6996ef2279381
hyunseo <goatsfish@gmail.com>
2023-02-21 19:50:53 +0900

    test: 참여자 위치 조회, 참여자 이름 조회 기능 테스트 추가

--- src/test/java/laddergame/domain/ParticipantsTest.java
+++ src/test/java/laddergame/domain/ParticipantsTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -43,12 +44,45 @@ class ParticipantsTest {
         assertThat(totalSize).isEqualTo(expectedSize);
     }
 
-    @DisplayName("이름 목록을 가져온다.")
+    @DisplayName("이름들을 가져온다.")
     @ParameterizedTest(name = "names = {0}, values = {1}")
     @MethodSource("namesDummy")
     void getNames(final List<String> names) {
         final Participants participants = new Participants(names);
-        assertThat(participants.getNames()).containsExactlyElementsOf(names);
+        final Names findNames = participants.getNames();
+        final List<String> findNameValues = findNames.getNames().stream().map(Name::getValue).collect(Collectors.toList());
+        assertThat(findNameValues).containsExactlyElementsOf(names);
+    }
+
+    @DisplayName("이름 목록을 가져온다.")
+    @ParameterizedTest(name = "names = {0}, values = {1}")
+    @MethodSource("namesDummy")
+    void getNameValues(final List<String> names) {
+        final Participants participants = new Participants(names);
+        final List<String> nameValues = participants.getNameValues();
+        assertThat(nameValues).containsExactlyElementsOf(names);
+    }
+
+    @DisplayName("참여자 위치를 이름을 통해서 가져온다.")
+    @Test
+    void findPositionByName() {
+        final Participants participants = new Participants(
+                List.of(NameFixture.createNameHyena().getValue(), NameFixture.createNameJayon().getValue()));
+        final Position findPosition = participants.findPositionByName(NameFixture.createNameHyena().getValue());
+
+        assertThat(findPosition.getValue()).isZero();
+    }
+
+    @DisplayName("위치를 통해서 이름을 가져온다.")
+    @Test
+    void findNameByPosition() {
+        final String hyena = NameFixture.createNameHyena().getValue();
+        final String jayon = NameFixture.createNameJayon().getValue();
+        final Participants participants = new Participants(List.of(hyena, jayon));
+        final Position position = new Position(0);
+        final Name findName = participants.findNameByPosition(position);
+
+        assertThat(findName.getValue()).isEqualTo(hyena);
     }
 
     static Stream<Arguments> namesDummy() {
