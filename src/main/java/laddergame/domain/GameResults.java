ee8d35609bee2e87c0e04e48715c4ead7333c7db
yujamint <woawoa0513@gmail.com>
2023-02-22 01:57:01 +0900

    refactor: 생성자에서 List<GameResult> 받도록 수정

--- src/main/java/laddergame/domain/GameResults.java
+++ src/main/java/laddergame/domain/GameResults.java
@@ -7,23 +7,21 @@ public class GameResults {
 
     private final List<GameResult> results;
 
-    public GameResults(List<String> resultInput, int userCount) {
-        validateGameResults(resultInput, userCount);
+    public GameResults(List<GameResult> gameResults, int userCount) {
+        validateGameResults(gameResults, userCount);
         
         results = new ArrayList<>();
-        addResult(resultInput);
+        addResult(gameResults);
     }
 
-    private void validateGameResults(List<String> results, int userCount) {
+    private void validateGameResults(List<GameResult> results, int userCount) {
         if (results.size() != userCount) {
             throw new IllegalArgumentException();
         }
     }
 
-    private void addResult(List<String> resultInput) {
-        for (String result : resultInput) {
-            results.add(new GameResult(result));
-        }
+    private void addResult(List<GameResult> gameResults) {
+        results.addAll(gameResults);
     }
 
     public List<GameResult> getResults() {
