4053fc6451be79e2bc2a4e1280630c506f610cc9
SproutMJ <mjk54565@gmail.com>
2023-02-21 17:24:51 +0900

    refactor(domain): 진행자 수 검증을 도메인에서 하도록 변경

--- src/main/java/domain/Person.java
+++ src/main/java/domain/Person.java
@@ -14,7 +14,13 @@ public class Person {
     private final List<People> person;
 
     public Person(List<People> person) {
+        validatePeopleNumber(person);
         this.person = person;
+    }
+
+    private static void validatePeopleNumber(List<People> person){
+        if(person.size() <= 1)
+        throw new IllegalArgumentException("진행자 수는 2명 이상이어야 합니다.");
     }
 
     public List<People> getPerson() {
