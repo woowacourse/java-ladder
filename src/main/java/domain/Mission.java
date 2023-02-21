eaef3653bfe14f8d52e556e714269d73ab3ae312
Eunkeee <eunkeeeee@gmail.com>
2023-02-21 14:04:17 +0900

    feat(Mission): 미션 글자수가 5글자를 초과하면 예외 처리

--- src/main/java/domain/Mission.java
+++ src/main/java/domain/Mission.java
@@ -5,7 +5,14 @@ public class Mission {
     private final String mission;
 
     public Mission(String mission) {
-        this.mission = mission;
+        this.mission = getMission(mission);
+    }
+
+    private static String getMission(String mission) {
+        if (mission.length() > 5) {
+            throw new IllegalArgumentException("미션 글자수는 5글자를 초과할 수 없습니다.");
+        }
+        return mission;
     }
 
     public String getMission() {
