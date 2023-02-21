20be3d3c35d14e95b6a23ec12e2166eee0211740
Eunkeee <eunkeeeee@gmail.com>
2023-02-21 14:19:08 +0900

    refactor(Mission): 미션 생성 리팩터링

--- src/main/java/domain/Mission.java
+++ src/main/java/domain/Mission.java
@@ -2,6 +2,7 @@ package domain;
 
 public class Mission {
 
+    private static final int MAX_LENGTH_EXCLUSIVE = 5;
     private final String mission;
 
     public Mission(String mission) {
@@ -12,7 +13,7 @@ public class Mission {
         if (mission.isBlank()) {
             throw new IllegalArgumentException("공백 또는 null을 입력할 수 없습니다.");
         }
-        if (mission.length() > 5) {
+        if (mission.length() > MAX_LENGTH_EXCLUSIVE) {
             throw new IllegalArgumentException("미션 글자수는 5글자를 초과할 수 없습니다.");
         }
         return mission;
--- src/test/java/domain/MissionTest.java
+++ src/test/java/domain/MissionTest.java
@@ -3,36 +3,39 @@ package domain;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @SuppressWarnings("NonAsciiCharacters")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class MissionTest {
 
-    @Test
-    void 미션_생성_값_입력() {
-        Mission mission1 = new Mission("꽝");
-        Mission mission2 = new Mission("5000");
-        Mission mission3 = new Mission("3000");
+    @Nested
+    @DisplayName("미션 생성 테스트")
+    class createMissionTest {
+        @ParameterizedTest
+        @ValueSource(strings = {"꽝", "5000", "3000"})
+        void 미션_생성_값_입력(String input) {
+            Mission mission = new Mission(input);
+            Assertions.assertEquals(mission.getMission(), input);
+        }
 
-        Assertions.assertEquals(mission1.getMission(), "꽝");
-        Assertions.assertEquals(mission2.getMission(), "5000");
-        Assertions.assertEquals(mission3.getMission(), "3000");
-    }
+        @Test
+        void 미션_글자수가_5글자_초과이면_예외_처리() {
+            assertThatIllegalArgumentException()
+                    .isThrownBy(() -> new Mission("다섯글자이상"));
+        }
 
-    @Test
-    void 미션_글자수가_5글자_초과이면_예외_처리() {
-        assertThatIllegalArgumentException()
-                .isThrownBy(() -> new Mission("다섯글자이상"));
-    }
-
-    @Test
-    void 공백으로_생성시_예외_처리() {
-        assertThatIllegalArgumentException()
-                .isThrownBy(() -> new Mission(""));
-        assertThatIllegalArgumentException()
-                .isThrownBy(() -> new Mission(" "));
+        @ParameterizedTest
+        @ValueSource(strings = {"", " "})
+        void 공백으로_생성시_예외_처리(String input) {
+            assertThatIllegalArgumentException()
+                    .isThrownBy(() -> new Mission(input));
+        }
     }
 }
