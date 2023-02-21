643efbc2e66fd053a637cab3114879cd87dbbd9b
Eunkeee <eunkeeeee@gmail.com>
2023-02-21 22:00:44 +0900

    refactor(MainController): 메서드 분리

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -4,6 +4,7 @@ import domain.Height;
 import domain.Lines;
 import domain.Mission;
 import domain.Missions;
+import domain.Name;
 import domain.Names;
 import domain.Player;
 import domain.Players;
@@ -30,18 +31,17 @@ public class MainController {
             Names names = inputView.readNames();
             Missions missions = inputView.readMissions();
             Height height = inputView.readHeight();
-
+            Players players = new Players(names);
             // TODO: 이름 수와 미션 수가 일치하지 않는 경우
 
             int lineNumber = names.getPersonNumber() - 1;
             Lines lines = new Lines(lineNumber, height.getHeight(), booleanGenerator);
             outputView.printResult(names, lines, missions);
 
-            Players players = new Players();
             for (int index = 0; index < names.size(); index++) {
-                Player player = new Player(names.getNameByIndex(index), new Position(index));
+                Player player = createPlayer(names.getNameByIndex(index), index);
                 players.addPlayer(player);
-                Mission mission = missions.getMissionByIndex(player.getExitIndex(lines));
+                Mission mission = missions.getMissionByIndex(player.calculateExitIndex(lines));
                 player.setMission(mission);
             }
 
@@ -51,5 +51,9 @@ public class MainController {
         } catch (Exception exception) {
             outputView.printExceptionMessage(exception);
         }
+    }
+
+    private static Player createPlayer(Name name, int index) {
+        return new Player(name, new Position(index));
     }
 }
---
+++ src/test/java/domain/PlayersTest.java
@@ -0,0 +1,20 @@
+package domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class PlayersTest {
+
+    @Test
+    void 생성_테스트() {
+        Names names = new Names("깃짱,이리내");
+        Players players = new Players(names);
+
+        Player gitJjang = new Player(new Name("깃짱"), new Position(0));
+        Player irene = new Player(new Name("이리내"), new Position(1));
+
+        Assertions.assertThat(players.getPlayers())
+                .containsExactly(gitJjang, irene);
+    }
+
+}
