4c5332455d3cffa22e5afe81dd74f75c5cd986ce
BGuga <tmdwns9913@gmail.com>
2023-02-21 15:21:18 +0900

    test: Floor 도메인의 시작 위치 입력에 따른 반환 위치 기능 테스트 작성

--- src/test/java/ladder/domain/FloorTest.java
+++ src/test/java/ladder/domain/FloorTest.java
@@ -9,8 +9,8 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FloorTest {
-    private final LineSource make = LineSource.MAKE_LINE;
-    private final LineSource blank = LineSource.MAKE_BLANK;
+    private final LineSource MAKE = LineSource.MAKE_LINE;
+    private final LineSource BLANK = LineSource.MAKE_BLANK;
 
     @Test
     @DisplayName("사이즈를 입력 받는 층 생성 테스트")
@@ -23,7 +23,7 @@ public class FloorTest {
     @DisplayName("Floor에 있는 라인 전부에게 생성 명령 테스트")
     void makeFloorTest() {
         Floor floor = new Floor(3);
-        List<LineSource> list = new ArrayList<>(List.of(make, blank, make));
+        List<LineSource> list = new ArrayList<>(List.of(MAKE, BLANK, MAKE));
         floor.makeFloor(list);
         assertThat(floor.getLines().get(0)).extracting("isExist").isEqualTo(true);
         assertThat(floor.getLines().get(1)).extracting("isExist").isEqualTo(false);
@@ -35,13 +35,21 @@ public class FloorTest {
     @DisplayName("다리가 연속해서 생기지 않는 것을 테스트 한다")
     void makeNonContinuousLadderTest() {
         Floor floor = new Floor(3);
-        List<LineSource> list = new ArrayList<>(List.of(make, make, make));
+        List<LineSource> list = List.of(MAKE, MAKE, MAKE);
         floor.makeFloor(list);
 
         assertThat(floor.getLines().get(0)).extracting("isExist").isEqualTo(true);
         assertThat(floor.getLines().get(1)).extracting("isExist").isEqualTo(false);
         assertThat(floor.getLines().get(2)).extracting("isExist").isEqualTo(true);
 
+    }
+
+    @Test
+    @DisplayName("Floor에 출발 위치를 입력하면 반환 위치를 반환한다.")
+    void floorMoveTest() {
+        final Floor floor = new Floor(2);
+        floor.makeFloor(List.of(MAKE,MAKE));
+        assertThat(floor.getResultPosition(0)).isEqualTo(1);
     }
 
 }
