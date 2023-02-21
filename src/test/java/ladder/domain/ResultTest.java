d5378006c8eb04a86ddeedd827ba81c683b55e03
woo-chang <clllickme@naver.com>
2023-02-21 14:00:29 +0900

    feat: 실행 결과 길이 검증 기능 구현

--- src/test/java/ladder/domain/ResultTest.java
+++ src/test/java/ladder/domain/ResultTest.java
@@ -3,6 +3,7 @@ package ladder.domain;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -14,7 +15,17 @@ class ResultTest {
     void resultNotBlank(final String value) {
         assertThatThrownBy(() -> new Result(value))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("결과는 공백일 수 없습니다. 현재 입력한 값은 [\"" + value + "\"] 입니다.");
+                .hasMessage("결과는 공백일 수 없습니다. 현재 입력한 값은 " + value + " 입니다.");
+    }
+
+    @Test
+    @DisplayName("실행 결과가 5글자 초과라면 예외를 던진다.")
+    void resultOverLength() {
+        final String value = "abcedf";
+
+        assertThatThrownBy(() -> new Result(value))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("결과는 최대 5글자까지 가능합니다. 현재 입력한 값은 " + value + " 입니다.");
     }
 }
 
@@ -29,7 +40,10 @@ class Result {
 
     private void validate(final String value) {
         if (value.isBlank()) {
-            throw new IllegalArgumentException("결과는 공백일 수 없습니다. 현재 입력한 값은 [\"" + value + "\"] 입니다.");
+            throw new IllegalArgumentException("결과는 공백일 수 없습니다. 현재 입력한 값은 " + value + " 입니다.");
+        }
+        if (value.length() > 5) {
+            throw new IllegalArgumentException("결과는 최대 5글자까지 가능합니다. 현재 입력한 값은 " + value + " 입니다.");
         }
     }
 }
