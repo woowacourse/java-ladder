0262489522928d8f600c7cb068650777b6e76304
ChoiYoungHoon <dudgns2865@naver.com>
2023-02-21 19:57:20 +0900

    test: Layer에서 Line 생성 테스트 코드 작성

--- src/test/java/domain/model/LayerTest.java
+++ src/test/java/domain/model/LayerTest.java
@@ -1,46 +1,73 @@
 package domain.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.fail;
 
 import domain.type.Line;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class LayerTest {
 
+    private static class TrueTestPassGenerator implements PassGenerator {
+
+        @Override
+        public boolean generate() {
+            return true;
+        }
+    }
+
+    private static class FalseTestPassGenerator implements PassGenerator {
+
+        @Override
+        public boolean generate() {
+            return false;
+        }
+    }
+
+
     @Test
-    @DisplayName("층이 랜덤으로 생성된다.")
-    public void makeLayerByRandom() {
+    @DisplayName("언제나 PassGenerator가 true를 반환할 때 Line 생성을 테스트한다.")
+    public void makeLineWithAlwaysTrue() {
         //given
-        int size = 5;
+        Layer layer = new Layer(new ArrayList<>(), new TrueTestPassGenerator());
 
         //when
-        Layer layer = Layer.makeLayerByRandom(size);
+        IntStream.range(0, 10).forEach(i -> layer.makeLine());
+
+        //then
+        List<Line> lines = layer.getLines();
+        IntStream.range(0, lines.size()).filter(i -> i % 2 == 0)
+            .forEach(i -> assertThat(lines.get(i)).isEqualTo(Line.CONNECTED));
+    }
+
+    @Test
+    @DisplayName("언제나 PassGenerator가 false를 반환할 때 Line 생성을 테스트한다.")
+    public void makeLineWithAlwaysFalse() {
+        //given
+        Layer layer = new Layer(new ArrayList<>(), new FalseTestPassGenerator());
+
+        //when
+        IntStream.range(0, 10).forEach(i -> layer.makeLine());
+
+        //then
+        List<Line> lines = layer.getLines();
+        lines.forEach(line -> assertThat(line).isEqualTo(Line.UNCONNECTED));
+    }
+
+    @Test
+    @DisplayName("Layer가 Line을 생성한다.")
+    public void makeLineSuccessTest() {
+        //given
+        Layer layer = new Layer(new ArrayList<>(), new RandomPassGenerator());
+
+        //when
+        int size = 10;
+        IntStream.range(0, size).forEach(i -> layer.makeLine());
 
         //then
         assertThat(layer.getLines().size()).isEqualTo(size);
-    }
-
-    @Test
-    @DisplayName("층이 랜덤으로 생성될 때 연속해서 연결 되지 않는다.")
-    public void makeLayerByRandomNoContinuousConnected() {
-        //given
-        int size = 5;
-
-        //when
-        Layer layer = Layer.makeLayerByRandom(size);
-
-        //then
-        for (int i = 1; i < size; i++) {
-            checkContinuousConnected(layer, i);
-        }
-    }
-
-    private void checkContinuousConnected(Layer layer, int i) {
-        if (layer.getLines().get(i - 1).equals(Line.CONNECTED) && layer.getLines().get(i)
-            .equals(Line.CONNECTED)) {
-            fail("연속해서 연결된 경우 실패");
-        }
     }
 }
