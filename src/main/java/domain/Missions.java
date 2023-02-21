799678ca07adceb46c810c460950d550c557b713
Eunkeee <eunkeeeee@gmail.com>
2023-02-21 16:13:13 +0900

    feat(Missions): 미션 index로 가져오기

---
+++ src/main/java/domain/Missions.java
@@ -0,0 +1,20 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Missions {
+    private final List<Mission> missions;
+    private static final String SPLIT_STANDARD = ",";
+
+    public Missions(String missions) {
+        this.missions = new ArrayList<>();
+        for (String mission : missions.split(SPLIT_STANDARD)) {
+            this.missions.add(new Mission(mission.trim()));
+        }
+    }
+
+    public Mission getMissionByIndex(int index) {
+        return missions.get(index);
+    }
+}
