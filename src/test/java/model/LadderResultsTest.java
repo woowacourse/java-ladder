e7dae6127ae7d2932b5c454d429f19752fedee25
apptie <appti@naver.com>
2023-02-21 12:26:14 +0900

    test: LadderResultsTest 중첩 구조를 통한 가독성 개선

--- src/test/java/model/LadderResultsTest.java
+++ src/test/java/model/LadderResultsTest.java
@@ -6,6 +6,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import java.util.List;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -14,21 +15,24 @@ import org.junit.jupiter.params.provider.NullSource;
 @DisplayNameGeneration(ReplaceUnderscores.class)
 class LadderResultsTest {
 
-    @ParameterizedTest
-    @NullSource
-    void of_메소드는_null이_주어지면_예외가_발생한다(List<LadderResult> results) {
-        assertThatThrownBy(() -> LadderResults.of(results, 3))
-                .isInstanceOf(IllegalStateException.class)
-                .hasMessageContaining("실행 결과가 정상적으로 입력되지 않았습니다.");
-    }
+    @Nested
+    class of_메소드_테스트 {
+        @ParameterizedTest
+        @NullSource
+        void 인자로_null이_주어지면_예외가_발생한다(List<LadderResult> results) {
+            assertThatThrownBy(() -> LadderResults.of(results, 3))
+                    .isInstanceOf(IllegalStateException.class)
+                    .hasMessageContaining("실행 결과가 정상적으로 입력되지 않았습니다.");
+        }
 
-    @Test
-    void of_메소드는_results의_크기와_참가자의_크기가_일치하지_않으면_예외가_발생한다() {
-        List<LadderResult> results = List.of(new LadderResult("abc"), new LadderResult("bcd"));
+        @Test
+        void 인자로_results의_크기와_참가자의_크기가_일치하지_않으면_예외가_발생한다() {
+            List<LadderResult> results = List.of(new LadderResult("abc"), new LadderResult("bcd"));
 
-        assertThatThrownBy(() -> LadderResults.of(results, 3))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("사다리 게임 결과는 참가자 수와 일치해야 합니다.");
+            assertThatThrownBy(() -> LadderResults.of(results, 3))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("사다리 게임 결과는 참가자 수와 일치해야 합니다.");
+        }
     }
 
     @ParameterizedTest
