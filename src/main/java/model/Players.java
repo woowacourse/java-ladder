ede4a91f49da6df0fa3a16fcba50b6b42b38d98a
MoonJeWoong <munjin0201@naver.com>
2023-02-21 17:48:33 +0900

    feat: 존재하지 않는 이름을 검색한 경우 예외 처리하는 기능 추가

--- src/main/java/model/Players.java
+++ src/main/java/model/Players.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Players {
+    private static final String NO_PLAYER_NAME_ERROR = "[ERROR] 해당 이름의 플레이어는 존재하지 않습니다.";
     private final List<Player> players = new ArrayList<>();
 
     public Players(List<Name> names) {
@@ -27,6 +28,6 @@ public class Players {
         return players.stream()
                 .filter(player -> player.isPlayerName(name))
                 .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException());
+                .orElseThrow(() -> new IllegalArgumentException(NO_PLAYER_NAME_ERROR));
     }
 }
