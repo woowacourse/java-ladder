4bf69a234770b80ac2ddeb5325567a27c5712c4e
hong-sile <gurwns9325@naver.com>
2023-02-21 21:52:40 +0900

    feat: 전체 유저의 이름과 상을 모두 출력하는 기능 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -3,7 +3,9 @@ package view;
 import domain.Line;
 import domain.Prize;
 import java.util.List;
+import java.util.Map;
 
+//TODO: static 키워드 제거
 public class OutputView {
     private static final String RESULT_MESSAGE = "실행결과";
 
@@ -39,6 +41,17 @@ public class OutputView {
     public static void printPrizes(final List<Prize> prizes) {
         for (final Prize prize : prizes) {
             System.out.println(String.format("%6s", prize.getName()));
+        }
+    }
+
+    public static void printPrize(final Prize prize) {
+        System.out.println(prize.getName());
+        System.out.println();
+    }
+
+    public static void printAllUsersAndPrizes(final Map<String, String> allUsersAndPrizes) {
+        for (final Map.Entry<String, String> entry : allUsersAndPrizes.entrySet()) {
+            System.out.printf("%s: %s\n", entry.getKey(), entry.getValue());
         }
     }
 }
