20f904ec2e9a7f604d6fc470637f94db62c2c181
ChoiYoungHoon <dudgns2865@naver.com>
2023-02-21 20:26:47 +0900

    feat: Ladder에 Line들 생성한다

--- src/main/java/domain/model/Ladder.java
+++ src/main/java/domain/model/Ladder.java
@@ -2,9 +2,7 @@ package domain.model;
 
 import domain.vo.Height;
 import domain.vo.Width;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Ladder {
@@ -13,27 +11,21 @@ public class Ladder {
     private final Width width;
     private final List<Layer> layers;
 
-    private Ladder(final Height height, final Width width, final List<Layer> layers) {
+    public Ladder(final Height height, final Width width, final List<Layer> layers) {
         this.height = height;
         this.width = width;
         this.layers = layers;
     }
 
-    public static Ladder makeLadder(final Height height, final Width width) {
-        List<Layer> layers = IntStream.range(0, height.getValue())
-            .mapToObj(index -> Layer.makeLayerByRandom(width.getValue())).collect(Collectors.toList());
-        return new Ladder(height, width, layers);
+    public void makeLineInLayers() {
+        IntStream.range(0, height.getValue())
+            .mapToObj(layers::get)
+            .forEach(
+                layer -> IntStream.range(0, width.getValue())
+                    .forEach(j -> layer.makeLine()));
     }
 
     public List<Layer> getLayers() {
-        return new ArrayList<>(layers);
-    }
-
-    public Height getHeight() {
-        return this.height;
-    }
-
-    public Width getWidth() {
-        return width;
+        return List.copyOf(layers);
     }
 }
