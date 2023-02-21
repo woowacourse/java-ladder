450f779e781c14443ea28029f7c3e65ba34c6942
hyunseo <goatsfish@gmail.com>
2023-02-21 19:51:06 +0900

    feat: 참여자 위치 조회, 참여자 이름 조회 기능 추가

--- src/main/java/laddergame/domain/Participants.java
+++ src/main/java/laddergame/domain/Participants.java
@@ -10,25 +10,41 @@ public class Participants {
     private final Names names;
 
     public Participants(final List<String> nameValues) {
-        final List<String> names = Optional.ofNullable(nameValues).orElse(List.of());
+        final List<String> names = getNames(nameValues);
         validateParticipants(names);
         this.names = new Names(names);
+    }
+
+    public Position findPositionByName(final String name) {
+        return names.findPositionByName(name);
+    }
+
+    public Name findNameByPosition(final Position position) {
+        return names.findNameByPosition(position);
+    }
+
+    public int getSize() {
+        return names.getNames().size();
+    }
+
+    public Names getNames() {
+        return names;
+    }
+
+    public List<String> getNameValues() {
+        return names.getNames()
+                .stream()
+                .map(Name::getValue)
+                .collect(Collectors.toList());
+    }
+
+    private  List<String> getNames(final List<String> nameValues) {
+        return Optional.ofNullable(nameValues).orElse(List.of());
     }
 
     private void validateParticipants(final List<String> names) {
         if (names.isEmpty()) {
             throw new IllegalArgumentException(PARTICIPANTS_EMPTY_EXCEPTION);
         }
-    }
-
-    public int getSize() {
-        return names.getSize();
-    }
-
-    public List<String> getNames() {
-        return names.getNames()
-                .stream()
-                .map(Name::getValue)
-                .collect(Collectors.toList());
     }
 }
