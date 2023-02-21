0211cb8ea3a30b0730bec7745da4d170af9016c2
apptie <appti@naver.com>
2023-02-21 11:58:53 +0900

    test: HeightTest 중첩 구조를 통한 가독성 개선 및 테스트 메소드 이름 변경

--- src/test/java/model/HeightTest.java
+++ src/test/java/model/HeightTest.java
@@ -7,38 +7,45 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import helper.FakeHeight;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 @DisplayNameGeneration(ReplaceUnderscores.class)
 class HeightTest {
 
-    @Test
-    void 생성자는_0_이하의_값이_주어지면_Exception이_발생한다() {
-        assertThatThrownBy(() -> new Height(0))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("사다리 높이는 최소 1 이상이어야 합니다.");
+    @Nested
+    class 생성자_테스트 {
+        @Test
+        void 인자로_0_이하의_값이_주어지면_예외가_발생한다() {
+            assertThatThrownBy(() -> new Height(0))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("사다리 높이는 최소 1 이상이어야 합니다.");
+        }
+
+        @Test
+        void 인자로_1_이상의_값이_주어지면_Height를_생성한다() {
+            assertThatCode(() -> new Height(1)).doesNotThrowAnyException();
+        }
     }
 
-    @Test
-    void 생성자는_1_이상의_값이_주어지면_Height를_생성한다() {
-        assertThatCode(() -> new Height(1)).doesNotThrowAnyException();
-    }
+    @Nested
+    class isContinueMakerLadder_메소드_테스트 {
+        @Test
+        void 원시_값_height가_1_이상이면_true를_반환한다() {
+            Height height = new Height(1);
 
-    @Test
-    void isContinueMakerLadder_메소드는_height가_1_이상이면_true를_반환한다() {
-        Height height = new Height(1);
+            boolean actual = height.isContinueMakeLadder();
 
-        boolean actual = height.isContinueMakeLadder();
+            assertThat(actual).isSameAs(true);
+        }
 
-        assertThat(actual).isSameAs(true);
-    }
+        @Test
+        void 원시_값_height가_0이면_false를_반환한다() {
+            Height height = new FakeHeight();
 
-    @Test
-    void isContinueMakerLadder_메소드는_height가_0이면_false를_반환한다() {
-        Height height = new FakeHeight();
+            boolean actual = height.isContinueMakeLadder();
 
-        boolean actual = height.isContinueMakeLadder();
-
-        assertThat(actual).isSameAs(false);
+            assertThat(actual).isSameAs(false);
+        }
     }
 }
