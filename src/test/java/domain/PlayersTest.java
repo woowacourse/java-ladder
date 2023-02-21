fb36d9ccb90e3e44d2bf97e15468ee4ed9961b08
Eunkeee <eunkeeeee@gmail.com>
2023-02-21 22:00:44 +0900

    refactor(MainController): 메서드 분리

--- src/test/java/domain/PlayersTest.java
+++ src/test/java/domain/PlayersTest.java
@@ -1,10 +1,5 @@
 package domain;
 
-import domain.player.Name;
-import domain.player.Names;
-import domain.player.Player;
-import domain.player.Players;
-import domain.player.Position;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -18,7 +13,8 @@ class PlayersTest {
         Player gitJjang = new Player(new Name("깃짱"), new Position(0));
         Player irene = new Player(new Name("이리내"), new Position(1));
 
-        Assertions.assertThat(players.getPlayersSortedByPosition())
+        Assertions.assertThat(players.getPlayers())
                 .containsExactly(gitJjang, irene);
     }
+
 }
