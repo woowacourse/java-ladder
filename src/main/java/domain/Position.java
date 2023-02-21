0a74dea22fc73d93fcfdad14b01cfa6fe7696722
Eunkeee <eunkeeeee@gmail.com>
2023-02-21 14:35:02 +0900

    feat(Position): 포지션 생성

---
+++ src/main/java/domain/Position.java
@@ -0,0 +1,14 @@
+package domain;
+
+public class Position {
+
+    private final int position;
+
+    public Position(int position) {
+        this.position = position;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
