dcd5c49efe0ef29b3bb319064ab7ca03bb5852b0
Eunkeee <eunkeeeee@gmail.com>
2023-02-21 13:59:29 +0900

    test(Mission): 미션 글자수가 5글자를 초과하면 예외 처리

--- src/test/java/domain/MissionTest.java
+++ src/test/java/domain/MissionTest.java
@@ -1,5 +1,7 @@
 package domain;
 
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
@@ -10,7 +12,7 @@ import org.junit.jupiter.api.Test;
 class MissionTest {
 
     @Test
-    void 미션_생성() {
+    void 미션_생성_값_입력() {
         Mission mission1 = new Mission("꽝");
         Mission mission2 = new Mission("5000");
         Mission mission3 = new Mission("3000");
@@ -18,5 +20,11 @@ class MissionTest {
         Assertions.assertEquals(mission1.getMission(), "꽝");
         Assertions.assertEquals(mission2.getMission(), "5000");
         Assertions.assertEquals(mission3.getMission(), "3000");
+    }
+
+    @Test
+    void 미션_글자수가_5글자_초과이면_예외_처리() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Mission("다섯글자이상"));
     }
 }
