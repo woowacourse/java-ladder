36b0a068b71e47a198b9796458c43caf7e3a34a2
kwonyj1022 <kwonyj1022@naver.com>
2023-02-21 20:58:34 +0900

    feat(Items): Items 생성 시 참가자 수와 실행 결과의 수가 일치하는지 검증하는 메서드 추가

---
+++ src/main/java/domain/Items.java
@@ -0,0 +1,23 @@
+package domain;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Items {
+
+    List<String> items;
+
+    public Items(int count, List<String> items) {
+        validate(count, items);
+        this.items = items;
+    }
+
+    private void validate(int count, List<String> items) {
+        if (count != items.size())
+            throw new IllegalArgumentException("참가자 수와 실행 결과의 수가 일치해야 합니다.");
+    }
+
+    public List<String> getItems() {
+        return Collections.unmodifiableList(items);
+    }
+}
