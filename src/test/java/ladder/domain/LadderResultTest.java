5034f9159217e6e61fff5cb349afe65fb4b717ff
sh111-coder <ohk9134@naver.com>
2023-02-21 17:48:47 +0900

    test(LadderResultTest): "꽝" 이외의 실행 결과가 정수가 아니면 예외를 던지는 테스트 추가

--- src/test/java/ladder/domain/LadderResultTest.java
+++ src/test/java/ladder/domain/LadderResultTest.java
@@ -1,7 +1,10 @@
 package ladder.domain;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -12,5 +15,21 @@ public class LadderResultTest {
     @DisplayName("실행 결과를 받아서 LadderResult를 생성한다.")
     void generateTest(String result) {
         Assertions.assertDoesNotThrow(() -> new LadderResult(result));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"가", "#", "a", "a1", "1.1"})
+    @DisplayName("\"꽝\" 이외의 실행 결과가 정수가 아니면 예외를 던진다.")
+    void throwsExceptionWhenRunResultIsNotIntegerExceptNoLuck(String runResult) {
+        assertThatThrownBy(() -> new LadderResult(runResult))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] \"꽝\" 이외의 실행 결과는 정수여야합니다.");
+    }
+
+    @Test
+    @DisplayName("실행 결과가 \"꽝\"이면 정수가 아니어도 예외가 발생하지 않는다.")
+    void doesNotThrowsExceptionWhenRunResultIsNoLuck() {
+        String runResult = "꽝";
+        Assertions.assertDoesNotThrow(() -> new LadderResult(runResult));
     }
 }
