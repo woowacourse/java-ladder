381f2248d8af6b1bf68640d39ca94d00155f2eaa
Choi-JJunho <junho5336@gmail.com>
2023-02-21 17:16:05 +0900

    feat: 참여자 이동 메소드 추가

--- src/main/java/ladder/domain/Player.java
+++ src/main/java/ladder/domain/Player.java
@@ -2,13 +2,29 @@ package ladder.domain;
 
 public class Player {
 
+    private int position;
     private final Name name;
 
     public Player(final String name) {
         this.name = new Name(name);
+        this.position = 0;
     }
 
     public String getName() {
         return name.getValue();
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void move(boolean left, boolean right) {
+        if (left) {
+            this.position--;
+        }
+
+        if (right) {
+            this.position++;
+        }
     }
 }
--- src/test/java/ladder/domain/PlayerTest.java
+++ src/test/java/ladder/domain/PlayerTest.java
@@ -1,10 +1,10 @@
 package ladder.domain;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class PlayerTest {
@@ -13,5 +13,14 @@ class PlayerTest {
     void 참여자는_이름을_가질_수_있다() {
         Player player = new Player("이름");
         assertThat(player.getName()).isEqualTo("이름");
+    }
+
+    @Test
+    void move_메소드는_참여자의_위치를_이동한다() {
+        Player player = new Player("주노");
+
+        player.move(false, true);
+
+        assertThat(player.getPosition()).isEqualTo(1);
     }
 }
