917f8a8133362e6bb512b0eabdc80d3c7f60b0f2
ChoiYoungHoon <dudgns2865@naver.com>
2023-02-21 20:13:23 +0900

    test: Layer에서 Line 생성 구현

--- src/main/java/domain/model/Layer.java
+++ src/main/java/domain/model/Layer.java
@@ -1,30 +1,21 @@
 package domain.model;
 
 import domain.type.Line;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
-import java.util.stream.IntStream;
 
 public class Layer {
 
     private final List<Line> lines;
-    private static final Random randomGenerator = new Random();
+    private final PassGenerator passGenerator;
 
-    private Layer(List<Line> lines) {
+    public Layer(final List<Line> lines, final PassGenerator passGenerator) {
         this.lines = lines;
+        this.passGenerator = passGenerator;
     }
 
-    public static Layer makeLayerByRandom(final int size) {
-        List<Line> lines = new ArrayList<>();
-        IntStream.range(0, size).mapToObj(index -> lines).forEach(Layer::makeLine);
-        return new Layer(lines);
-    }
-
-    private static void makeLine(final List<Line> lines) {
-        boolean randomBoolean = randomGenerator.nextBoolean();
-        if (randomBoolean &&
-            (lines.isEmpty() || lines.get(lines.size() - 1).equals(Line.UNCONNECTED))) {
+    public void makeLine() {
+        if (passGenerator.generate()
+            && (lines.isEmpty() || lines.get(lines.size() - 1).equals(Line.UNCONNECTED))) {
             lines.add(Line.CONNECTED);
             return;
         }
@@ -32,6 +23,6 @@ public class Layer {
     }
 
     public List<Line> getLines() {
-        return new ArrayList<>(lines);
+        return List.copyOf(lines);
     }
 }
