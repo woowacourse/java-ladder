bda37875f13513c470c58e6dc126e207274ca810
hectick <chyeon1223@cau.ac.kr>
2023-02-21 15:02:26 +0900

    feat(SimpleArrayList): add(String) 메소드 구현

---
+++ src/main/java/techcourse/jcf/mission/SimpleArrayList.java
@@ -0,0 +1,82 @@
+package techcourse.jcf.mission;
+
+public class SimpleArrayList implements SimpleList {
+
+    /*
+    미션 1
+    SimpleList 인터페이스의 ArrayList 구현체인 SimpleArrayList 를 구현해본다.
+    내부적으로 동적으로 배열의 크기를 조절하는 동작을 이해하는 것을 목표로 한다.
+
+    힌트
+    머리로 생각하여 구현하는 것은 매우 어렵다. 툴을 사용하거나 그림을 그리며 개념을 잡고 구현해본다.
+    자바에서 제공하는 java.util.List 구현체로 먼저 성공하는 테스트 코드를 작성하고, 직접 구현한 List 구현체로 변경한다.
+    처음부터 좋은 코드를 작성할 생각을 버려라. 구현 후 리팩토링 한다.
+    직접 구현하기 힘들다면 java.util.List 의 구현체 코드를 참고한다.
+     */
+
+    private static int DEFAULT_CAPACITY = 10;
+
+    private String[] array;
+    private int size;
+
+    public SimpleArrayList(){
+        this.array = new String[DEFAULT_CAPACITY];
+        this.size = 0;
+    }
+
+    @Override
+    public boolean add(String value) {
+        array[size++] = value;
+        return true;
+    }
+
+    @Override
+    public void add(int index, String value) {
+    }
+
+    @Override
+    public String set(int index, String value) {
+        return null;
+    }
+
+    @Override
+    public String get(int index) {
+        return array[index];
+    }
+
+    @Override
+    public boolean contains(String value) {
+        return false;
+    }
+
+    @Override
+    public int indexOf(String value) {
+        return 0;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean remove(String value) {
+        return false;
+    }
+
+    @Override
+    public String remove(int index) {
+        return null;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+}
