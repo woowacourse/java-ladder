d67c5c7a5b0dd859f4edd4cfb89d82e58cd6a07d
jaeyeon kim <jakind@naver.com>
2023-02-21 15:09:35 +0900

    feat: SimpleArrayList 를 구현합니다.

---
+++ src/test/java/techcourse/jcf/mission/SimpleArrayList.java
@@ -0,0 +1,131 @@
+package techcourse.jcf.mission;
+
+public class SimpleArrayList implements SimpleList {
+
+    private String[] array;
+    private int pointer;
+
+    public SimpleArrayList() {
+        array = new String[1];
+        pointer = 0;
+    }
+
+    private void extendSize() {
+        if (pointer == array.length) {
+            String[] copyArray = array.clone();
+            array = new String[array.length * 2];
+            System.arraycopy(copyArray, 0, array, 0, array.length / 2);
+        }
+    }
+
+    @Override
+    public boolean add(String value) {
+        array[pointer++] = value;
+        extendSize();
+        return true;
+    }
+
+    @Override
+    public void add(int index, String value) {
+        validateIndex(index);
+        for (int i = pointer - 1; i >= index; i--) {
+            array[i + 1] = array[i];
+        }
+
+        array[index] = value;
+        pointer++;
+        extendSize();
+    }
+
+    @Override
+    public String set(int index, String value) {
+        validateIndex(index);
+        array[index] = value;
+        return value;
+    }
+
+    @Override
+    public String get(int index) {
+        validateIndex(index);
+        return array[index];
+    }
+
+    @Override
+    public boolean contains(String value) {
+        for (int i = 0; i < pointer; i++) {
+            if (array[i].equals(value)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public int indexOf(String value) {
+        for (int i = 0; i < pointer; i++) {
+            if (array[i].equals(value)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    @Override
+    public int size() {
+        return pointer;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return pointer == 0;
+    }
+
+    @Override
+    public boolean remove(String value) {
+        int targetIndex = -1;
+
+        for (int i = 0; i < pointer; i++) {
+            if (array[i].equals(value)) {
+                targetIndex = i;
+            }
+        }
+
+        if (targetIndex == -1) {
+            return false;
+        }
+
+        for (int i = targetIndex + 1; i < pointer; i++) {
+            array[i - 1] = array[i];
+        }
+
+        array[--pointer] = "";
+        return true;
+    }
+
+    @Override
+    public String remove(int index) {
+        validateIndex(index);
+        String removeValue = array[index];
+
+        for (int i = index + 1; i < pointer; i++) {
+            array[i - 1] = array[i];
+        }
+
+        array[--pointer] = "";
+        return removeValue;
+    }
+
+    private void validateIndex(int index) {
+        if (!(0 <= index && index < pointer)) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    @Override
+    public void clear() {
+        array = new String[1];
+        pointer = 0;
+    }
+}
