a25d689e646f3e9db9f3f4d2a6b8be182f4c5e3a
JunH <tjdtls690@gmail.com>
2023-02-21 15:58:27 +0900

    :sparkles: feat : 중간 Direction의 Bar 생성 및 포지션 조정 기능 구현

--- src/test/java/ladder/domain/DirectionTest.java
+++ src/test/java/ladder/domain/DirectionTest.java
@@ -7,25 +7,50 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 
 class DirectionTest {
-    private Direction firstDirection;
+    private Direction firstDirectionTrue;
+    private Direction firstDirectionFalse;
     
     @BeforeEach
     void setUp() {
-        firstDirection = Direction.createFirst(() -> true);
+        firstDirectionTrue = Direction.createFirst(() -> true);
+        firstDirectionFalse = Direction.createFirst(() -> false);
     }
     
     @Test
-    @DisplayName("각 Line의 첫 Direction의 Bar는 왼쪽 false, 오른쪽 true이기 때문에, 포지션을 조정할 때 1 플러스한다.")
-    void createFirst() {
-        int resultPosition = firstDirection.getAdjustedPosition(3);
+    @DisplayName("각 Line의 첫 Direction의 Bar가 왼쪽 false, 오른쪽 true일 때, 포지션을 +1 한다.")
+    void createFirstTrue() {
+        int resultPosition = firstDirectionTrue.getAdjustedPosition(3);
         assertThat(resultPosition).isEqualTo(4);
     }
     
     @Test
-    @DisplayName("각 Line의 중간 Direction의 Bar는 왼쪽이 true인 경우 오른쪽은 무조건 false이기때문에, 포지션을 조정할 때 1 마이너스한다.")
-    void createNext() {
-        Direction nextDirection = firstDirection.createNext(() -> false);
+    @DisplayName("각 Line의 첫 Direction의 Bar가 왼쪽 false, 오른쪽 false일 때, 포지션을 그대로 반환한다.")
+    void createFirstFalse() {
+        int resultPosition = firstDirectionFalse.getAdjustedPosition(3);
+        assertThat(resultPosition).isEqualTo(3);
+    }
+    
+    @Test
+    @DisplayName("각 Line의 중간 Direction의 Bar가 왼쪽이 true인 경우 오른쪽은 무조건 false이기 때문에, 포지션을 -1 한다.")
+    void createNext_left() {
+        Direction nextDirection = firstDirectionTrue.createNext(() -> false);
         int resultPosition = nextDirection.getAdjustedPosition(3);
         assertThat(resultPosition).isEqualTo(2);
+    }
+    
+    @Test
+    @DisplayName("각 Line의 중간 Direction의 Bar가 왼쪽이 false, 오른쪽 true인 경우, 포지션을 +1 한다.")
+    void createNext_right() {
+        Direction nextDirection = firstDirectionFalse.createNext(() -> true);
+        int resultPosition = nextDirection.getAdjustedPosition(3);
+        assertThat(resultPosition).isEqualTo(4);
+    }
+    
+    @Test
+    @DisplayName("각 Line의 중간 Direction의 Bar가 왼쪽이 false, 오른쪽 false인 경우, 포지션을 그대로 반환한다.")
+    void createNext_middle() {
+        Direction nextDirection = firstDirectionFalse.createNext(() -> false);
+        int resultPosition = nextDirection.getAdjustedPosition(3);
+        assertThat(resultPosition).isEqualTo(3);
     }
 }
