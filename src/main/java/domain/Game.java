8f68628ad1b5cc757d34a20a7c98875a4532e922
jjongwa <troas96@naver.com>
2023-02-21 14:15:41 +0900

    feat: 라인 유무에 따른 유저 위치 교환 기능 추가

---
+++ src/main/java/domain/Game.java
@@ -0,0 +1,29 @@
+package domain;
+
+import domain.ladder.Line;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Game {
+    private ArrayList<String> names;
+    private List<Line> lines;
+
+    public Game(List<String> names, List<Line> lines) {
+        this.names = new ArrayList<>(names);
+        this.lines = lines;
+    }
+
+    public static List<String> compareLineAndUsers(ArrayList<String> names, List<Boolean> points) {
+        for (int index = 1; index < names.size(); index++) {
+            swapUser(index, names, points);
+        }
+        return names;
+    }
+
+    private static void swapUser(int index, ArrayList<String> names, List<Boolean> points) {
+        if (points.get(index)) {
+            Collections.swap(names, index, index - 1);
+        }
+    }
+}
