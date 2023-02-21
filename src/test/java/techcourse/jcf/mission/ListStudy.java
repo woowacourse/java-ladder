5a4dac01909f468155ece416f73480ac27e37514
Eun-chan Cho <eunchan1001@gmail.com>
2023-02-21 16:52:24 +0900

    chore: 리스트 미션 템플릿 추가

---
+++ src/test/java/techcourse/jcf/mission/ListStudy.java
@@ -0,0 +1,27 @@
+package techcourse.jcf.mission;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class ListStudy {
+    @Test
+    public void arrayList() {
+        ArrayList<String> values = new ArrayList<>();
+        values.add("first");
+        values.add("second");
+
+        assertThat([TODO]).isTrue(); // 세 번째 값을 추가한다.
+        assertThat([TODO]).isEqualTo(3); // list의 크기를 구한다.
+        assertThat([TODO]).isEqualTo("first"); // 첫 번째 값을 찾는다.
+        assertThat([TODO]).isTrue(); // "first" 값이 포함되어 있는지를 확인한다.
+        assertThat([TODO]).isEqualTo("first"); // 첫 번째 값을 삭제한다.
+        assertThat([TODO]).isEqualTo(2); // 값이 삭제 됐는지 확인한다.
+
+        // TODO values에 담긴 모든 값을 출력한다.
+    }
+}
+
