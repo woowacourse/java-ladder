6e6c392fb80df288b43b3320f38909faf99dae05
sh111-coder <ohk9134@naver.com>
2023-02-21 20:33:52 +0900

    feat(LadderResult): 정수인 실행 결과 범위 검증 기능 구현

--- src/main/java/ladder/domain/LadderResult.java
+++ src/main/java/ladder/domain/LadderResult.java
@@ -4,6 +4,9 @@ public class LadderResult {
 
     private static final String NO_LUCK = "꽝";
     private static final String RESULT_EXCEPT_NO_LUCK_IS_NOT_NUMERIC_ERROR_MESSAGE = "[ERROR] \"꽝\" 이외의 실행 결과는 정수여야합니다.";
+    private static final int MIN_RUN_RESULT_VALUE = 1;
+    private static final int MAX_RUN_RESULT_VALUE = 10000;
+    private static final String RESULT_EXCEPT_NO_LUCK_IS_WRONG_RANGE_ERROR_MESSAGE = "[ERROR] \"꽝\" 이외의 실행 결과는 1 이상 10000 이하의 정수여야합니다.";
 
     private final String runResult;
 
@@ -15,6 +18,7 @@ public class LadderResult {
     private void validate(String runResult) {
         if (isNotNoLuck(runResult)) {
             validateNumeric(runResult);
+            validateRange(runResult);
         }
     }
 
@@ -28,5 +32,16 @@ public class LadderResult {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(RESULT_EXCEPT_NO_LUCK_IS_NOT_NUMERIC_ERROR_MESSAGE);
         }
+    }
+
+    private void validateRange(String runResult) {
+        int parsedRunResult = Integer.parseInt(runResult);
+        if (isRunResultWrongRange(parsedRunResult)) {
+            throw new IllegalArgumentException(RESULT_EXCEPT_NO_LUCK_IS_WRONG_RANGE_ERROR_MESSAGE);
+        }
+    }
+
+    private boolean isRunResultWrongRange(int runResult) {
+        return runResult < MIN_RUN_RESULT_VALUE || runResult > MAX_RUN_RESULT_VALUE;
     }
 }
