d202412777221d4cea0b3220098fe66751959c15
woosung1223 <dntjd991223@naver.com>
2023-02-21 19:11:18 +0900

    feat: 사다리 게임 실행 결과 입력 기능 생성

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -10,6 +10,7 @@ public class InputView {
     private static final String DELIMITER = ",";
     private static final String PLAYER_INPUT_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     private static final String LADDER_HEIGHT_INPUT_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
+    private static final String LADDER_GAME_RESULT_INPUT_MESSAGE = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
 
     public static List<String> inputPlayer() {
         System.out.println(PLAYER_INPUT_MESSAGE);
@@ -28,5 +29,12 @@ public class InputView {
         String input = scanner.nextLine();
         InputValidator.validateInteger(input);
         return Integer.parseInt(input);
+    }
+
+    public static List<String> inputLadderGameResult() {
+        System.out.println(LADDER_GAME_RESULT_INPUT_MESSAGE);
+
+        String gameResult = scanner.nextLine();
+        return splitInputByDelimiter(gameResult);
     }
 }
