f1b36efc079450bc8efe4be8aac6eb7f91ff054e
hyunseo <goatsfish@gmail.com>
2023-02-21 19:50:15 +0900

    feat: Lines 포인트 위치 조회, 높이 조회 기능 추가

--- src/main/java/laddergame/domain/Lines.java
+++ src/main/java/laddergame/domain/Lines.java
@@ -10,16 +10,28 @@ public class Lines {
     private final List<Line> lines;
 
     public Lines(final List<Line> inputLines) {
-        final List<Line> lines = Optional.ofNullable(inputLines).orElse(List.of());
+        final List<Line> lines = getLines(inputLines);
         validateLines(lines);
         this.lines = lines;
+    }
+
+    public boolean getPointByColumnAndRow(final Position column, final Position row) {
+        return lines.get(column.getValue()).getPointByPosition(row);
+    }
+
+    public Height getHeight() {
+        return new Height(lines.size());
     }
 
     public List<Line> getLines() {
         return new ArrayList<>(lines);
     }
 
-    private void validateLines(List<Line> lines) {
+    private List<Line> getLines(final List<Line> inputLines) {
+        return Optional.ofNullable(inputLines).orElse(List.of());
+    }
+
+    private void validateLines(final List<Line> lines) {
         if (lines.isEmpty()) {
             throw new IllegalArgumentException(LINES_EMPTY_EXCEPTION);
         }
