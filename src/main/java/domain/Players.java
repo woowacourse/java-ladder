78b5b3f8ca6793d0d5af3489c584e352fd72db56
Eunkeee <eunkeeeee@gmail.com>
2023-02-21 22:01:28 +0900

    feat(Players): Players 비교

--- src/main/java/domain/Players.java
+++ src/main/java/domain/Players.java
@@ -1,11 +1,22 @@
 package domain;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class Players {
-    private List<Player> players = new ArrayList<>();
+    private final List<Player> players;
+
+    public Players(Names names) {
+        this.players = names.getNames().stream()
+                .map(name -> new Player(name, createPosition(names, name)))
+                .collect(Collectors.toList());
+    }
+
+    private Position createPosition(Names names, Name name) {
+        return new Position(names.getNames().indexOf(name));
+    }
 
     public void addPlayer(Player player) {
         players.add(player);
@@ -18,4 +29,7 @@ public class Players {
                 .orElseThrow(() -> new IllegalArgumentException("해당하는 플레이어가 없습니다."));
     }
 
+    public List<Player> getPlayers() {
+        return Collections.unmodifiableList(players);
+    }
 }
