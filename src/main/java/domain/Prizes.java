be53a16de4f475fd507b478bbb48f651a7468eb3
hong-sile <gurwns9325@naver.com>
2023-02-21 16:44:14 +0900

    feat: Prizes를 생성할 때 Users의 수와 다르면 예외를 반환한다.

--- src/main/java/domain/Prizes.java
+++ src/main/java/domain/Prizes.java
@@ -1,5 +1,6 @@
 package domain;
 
+import exception.ErrorMessage;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,9 +8,17 @@ public class Prizes {
 
     private final List<Prize> prizes = new ArrayList<>();
 
-    public Prizes(final List<String> prizeNames) {
+    public Prizes(final List<String> prizeNames, final Users users) {
+        validateSize(prizeNames, users);
         for (final String prizeName : prizeNames) {
             prizes.add(new Prize(prizeName));
+        }
+    }
+
+    private static void validateSize(final List<String> prizeNames, final Users users) {
+        if (users.size() != prizeNames.size()) {
+            throw new IllegalArgumentException(
+                    ErrorMessage.PRIZES_COUNT_IS_NOT_EQUAL_USERS_SIZE_EXCEPTION.getMessage());
         }
     }
 
--- src/main/java/exception/ErrorMessage.java
+++ src/main/java/exception/ErrorMessage.java
@@ -6,7 +6,8 @@ public enum ErrorMessage {
     USER_NAME_LENGTH_EXCEPTION("유저의 이름은 5자 이하여야 합니다."),
     USER_NAME_BLANK_EXCEPTION("유저의 이름은 공백일 수 없습니다."),
     USER_NAME_NON_ASCII_CHARACTER_EXCEPTION("유저의 이름에 아스키 코드 이외의 문자는 입력하면 안 됩니다."),
-    USERS_NAME_BLANK_EXCEPTION("유저의 이름으로 쉼표만 입력하면 안 됩니다.");
+    USERS_NAME_BLANK_EXCEPTION("유저의 이름으로 쉼표만 입력하면 안 됩니다."),
+    PRIZES_COUNT_IS_NOT_EQUAL_USERS_SIZE_EXCEPTION("상품의 개수는 참여자의 수와 동일해야 합니다.");
 
     private final String message;
 
