a537faca27736b9da5812f5d6be1b4741b9ac274
BackFoxx <backfoxxx@gmail.com>
2023-02-21 16:04:49 +0900

    refactor: 바뀐 객체 특성을 반영하도록 테스트 메소드 이름 변경

--- src/test/java/domain/ladder/LineTest.java
+++ src/test/java/domain/ladder/LineTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import util.BooleanGenerator;
 import util.FixBooleanGenerator;
@@ -15,11 +14,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 class LineTest {
 
     @Nested
-    @DisplayName("이전 다리가 이전 다리가 건널 수 ")
+    @DisplayName("이전 다리가 오른쪽으로 뻗어 ")
     class GenerateFootsteps {
         @Test
-        @DisplayName("있는 디딤돌이면 지금 다리는 디딤돌이 생성되는 것이 허용되지 않는다.")
-        void givenCrossablePreviousFootstep_thenUncrossableCurrentFootStep() {
+        @DisplayName("있는 디딤돌이면 지금 다리는 완쪽으로 뻗었다는 정보를 담고 있다.")
+        void givenPreviousFootstepConnected_thenConnectedCurrentStep() {
             Line line = Line.of(new FixBooleanGenerator(true));
 
             line.generateStep();
@@ -28,9 +27,9 @@ class LineTest {
             assertThat(line.isConnectedToRight(1)).isFalse();
         }
 
-        @ParameterizedTest(name = "없는 디딤돌이면 지금 다리는 디딤돌이 생성되는 것이 허용된다.")
+        @ParameterizedTest(name = "있지 않은 디딤돌이면 지금 다리는 오른쪽으로 연결되거나, 아무 쪽으로도 연결되지 않는다.")
         @ValueSource(booleans = {true, false})
-        void givenUnCrossablePreviousFootstep_thenNotUncrossableCurrentFootStep(boolean value) {
+        void givenPreviousFootstepUnConnected_thenRandomCurrentStep(boolean value) {
             Line line = Line.of(new FixBooleanGenerator(false, value));
 
             line.generateStep();
@@ -40,19 +39,13 @@ class LineTest {
         }
     }
 
-    @Nested
-    @DisplayName("만약 특정 다리가 ")
-    class SteepableInfomation {
-        @ParameterizedTest(name = "{0}으면 {1}를 반환하다.")
-        @CsvSource(value = {"있,true", "없,false"})
-        void crossableFootStepCase(String condition, boolean value) {
-            BooleanGenerator trueGenerator = new FixBooleanGenerator(value);
-
-            Line line = Line.of(trueGenerator);
-            line.generateStep();
-
-            assertThat(line.isConnectedToRight(0))
-                    .isEqualTo(value);
-        }
+    @ParameterizedTest(name = "특정 발판이 오른쪽으로 뻗어있는 지 여부를 반환하다.")
+    @ValueSource(booleans = {false, true})
+    void StepConnectedToRightCase(boolean value) {
+        BooleanGenerator trueGenerator = new FixBooleanGenerator(value);
+        Line line = Line.of(trueGenerator);
+        line.generateStep();
+        assertThat(line.isConnectedToRight(0))
+                .isEqualTo(value);
     }
 }
