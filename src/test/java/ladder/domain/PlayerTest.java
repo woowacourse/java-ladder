a25320c8b0f0da602edfc9d77541711f661cce7a
sh111-coder <ohk9134@naver.com>
2023-02-21 16:34:21 +0900

    test(PlayerTest): Player의 이름 포장 클래스 리팩토링으로 인한 테스트 수정

--- src/test/java/ladder/domain/PlayerTest.java
+++ src/test/java/ladder/domain/PlayerTest.java
@@ -3,14 +3,15 @@ package ladder.domain;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class PlayerTest {
 
-    @Test
-    @DisplayName("이름과 순서로 참여자가 생성된다.")
-    void generatePlayerTest() {
-        String name = "seongha";
-        int order = 0;
-        Assertions.assertDoesNotThrow(() -> new Player(name, order));
+    @ParameterizedTest
+    @CsvSource(value = {"seong,0", "io,1", "odo,2", "jay,3"})
+    @DisplayName("이름과 시작 인덱스로 참여자가 생성된다.")
+    void generatePlayerTest(String name, int startIndex) {
+        Assertions.assertDoesNotThrow(() -> new Player(new Name(name), startIndex));
     }
 }
