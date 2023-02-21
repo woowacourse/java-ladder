97db3895bed9cfe8c05de992894b151315a4b6b7
jiwon <ljwon77@daum.net>
2023-02-21 09:51:18 +0900

    refactor: LadderTest의 파라미터 네이밍을 수정한다

--- src/test/java/laddergame/domain/ladder/LadderTest.java
+++ src/test/java/laddergame/domain/ladder/LadderTest.java
@@ -32,21 +32,21 @@ public class LadderTest {
     @ParameterizedTest
     @ValueSource(strings = {"1", "10000"})
     @DisplayName("1 이상의 정수 범위의 사다리 높이가 들어오면, 예외가 발생하지 않는다.")
-    void create_thenSuccess(final String ladderHeight) {
+    void create_thenSuccess(final String validHeight) {
         // when & then
-        assertThatCode(() -> Ladder.create(ladderHeight, participantCount, rungGenerator))
+        assertThatCode(() -> Ladder.create(validHeight, participantCount, rungGenerator))
                 .doesNotThrowAnyException();
 
-        assertThat(Ladder.create(ladderHeight, participantCount, rungGenerator))
+        assertThat(Ladder.create(validHeight, participantCount, rungGenerator))
                 .isInstanceOf(Ladder.class);
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"hi", "23fgie", "@#$%$"})
     @DisplayName("정수 값이 아닌 사다리 높이가 들어오면, 예외가 발생한다.")
-    void create_givenInvalidTypeLadderHeight_thenFail(final String ladderHeight) {
+    void create_givenInvalidTypeLadderHeight_thenFail(final String invalidHeight) {
         // when & then
-        assertThatThrownBy(() -> Ladder.create(ladderHeight, participantCount, rungGenerator))
+        assertThatThrownBy(() -> Ladder.create(invalidHeight, participantCount, rungGenerator))
                 .isInstanceOf(IllegalArgumentException.class)
                 .isExactlyInstanceOf(TypeException.class)
                 .hasMessage(TypeException.errorMessage);
@@ -55,9 +55,9 @@ public class LadderTest {
     @ParameterizedTest
     @ValueSource(strings = {"0", "10001"})
     @DisplayName("1~10000 범위를 벗어난 사다리 높이가 입력되면, 예외가 발생한다.")
-    void create_givenInvalidRangeLadderHeight_thenFail(final String ladderHeight) {
+    void create_givenInvalidRangeLadderHeight_thenFail(final String invalidHeight) {
         // when & then
-        assertThatThrownBy(() -> Ladder.create(ladderHeight, participantCount, rungGenerator))
+        assertThatThrownBy(() -> Ladder.create(invalidHeight, participantCount, rungGenerator))
                 .isInstanceOf(IllegalArgumentException.class)
                 .isExactlyInstanceOf(RangeException.class)
                 .hasMessage(String.format(RangeException.errorMessage, 1, 10_000));
