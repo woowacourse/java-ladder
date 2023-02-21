0b0aad59eebb8a55df47cfca8e9e28b067c20345
kdkdhoho <hkim4410@naver.com>
2023-02-21 15:55:20 +0900

    feat(Result): Result 이름에 null 검증 기능 추가

--- src/main/java/ladder/domain/strategy/linestrategy/Result.java
+++ src/main/java/ladder/domain/strategy/linestrategy/Result.java
@@ -4,6 +4,7 @@ public class Result {
     private final String result;
 
     public Result(String result) {
+        validateNull(result);
         validateEmpty(result);
         this.result = result;
     }
@@ -11,6 +12,12 @@ public class Result {
     private void validateEmpty(String result) {
         if (result.isBlank()) {
             throw new IllegalArgumentException("[ERROR] 결과는 공백일 수 없습니다.");
+        }
+    }
+
+    private void validateNull(String result) {
+        if (result == null) {
+            throw new IllegalArgumentException("[ERROR] 이름에 null이 들어갈 수 없습니다.");
         }
     }
 }
--- src/test/java/ladder/domain/strategy/linestrategy/ResultTest.java
+++ src/test/java/ladder/domain/strategy/linestrategy/ResultTest.java
@@ -3,6 +3,7 @@ package ladder.domain.strategy.linestrategy;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -17,4 +18,16 @@ class ResultTest {
                 new Result(input)
         ).withMessage("[ERROR] 결과는 공백일 수 없습니다.");
      }
+
+     @Test
+     @DisplayName("이름에 null이 들어가면 IllegalArgumentException 예외가 발생한다.")
+     void create_null() {
+         // given
+         String input = null;
+
+         // expect
+         assertThatIllegalArgumentException().isThrownBy(() ->
+                 new Result(input)
+         ).withMessage("[ERROR] 이름에 null이 들어갈 수 없습니다.");
+      }
 }
