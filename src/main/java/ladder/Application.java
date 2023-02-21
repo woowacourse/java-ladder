bcf3e43c139583e02926226aeea5b08d808f2b26
kdkdhoho <hkim4410@naver.com>
2023-02-21 20:24:28 +0900

    feat: 입출력 요구사항 모두 구현

--- src/main/java/ladder/Application.java
+++ src/main/java/ladder/Application.java
@@ -4,6 +4,7 @@ import ladder.domain.Height;
 import ladder.domain.Ladder;
 import ladder.domain.LadderFactory;
 import ladder.domain.Players;
+import ladder.domain.Results;
 import ladder.domain.strategy.linestrategy.LineStrategy;
 import ladder.domain.strategy.linestrategy.RandomLineStrategy;
 import ladder.view.InputView;
@@ -12,11 +13,25 @@ import ladder.view.OutputView;
 public class Application {
     public static void main(String[] args) {
         Players players = new Players(InputView.readPlayerNames());
+        Results results = new Results(InputView.readResults(), players.getPlayersCount());
         Height height = new Height(InputView.readLadderHeight());
         LineStrategy lineStrategy = new RandomLineStrategy();
 
         LadderFactory ladderFactory = new LadderFactory(height, players, lineStrategy);
         Ladder ladder = ladderFactory.makeLadder();
-        OutputView.printResult(players.getPlayers(), ladder.getLines());
+
+        OutputView.printLadder(players.getPlayers(), ladder.getLines(), results.getResults());
+
+        while (true) {
+            String targetName = InputView.readTargetName();
+            if (targetName.equals("q")) {
+                break;
+            }
+            if (targetName.equals("all")) {
+                OutputView.printAllResult(players.getPlayerNames(), results.findAllResult(ladder.getLines()));
+                break;
+            }
+            OutputView.printSingleResult(results.findResult(ladder.getLines(), players.findPosition(targetName)));
+        }
     }
 }
--- src/main/java/ladder/view/OutputView.java
+++ src/main/java/ladder/view/OutputView.java
@@ -1,24 +1,27 @@
 package ladder.view;
 
+import static java.util.stream.Collectors.joining;
+
 import ladder.domain.Line;
 import ladder.domain.Player;
 import ladder.domain.Step;
+import ladder.domain.strategy.linestrategy.Result;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class OutputView {
 
-    public static void printResult(List<Player> players, List<Line> lines) {
-        System.out.println("실행결과");
+    public static void printLadder(List<Player> players, List<Line> lines, List<Result> results) {
+        System.out.println(System.lineSeparator() + "사다리 결과" + System.lineSeparator());
         printNames(players);
         printLadder(lines);
+        printResults(results);
     }
 
     private static void printNames(List<Player> players) {
         String result = players.stream()
                 .map(Player::getName)
                 .map(name -> String.format("%-5s", name))
-                .collect(Collectors.joining(" "));
+                .collect(joining(" "));
         System.out.println(result);
     }
 
@@ -26,8 +29,29 @@ public class OutputView {
         String result = lines.stream()
                 .map(line -> "|" + line.getSteps().stream()
                         .map(Step::getShape)
-                        .collect(Collectors.joining("|")) + "|")
-                .collect(Collectors.joining(System.lineSeparator()));
+                        .collect(joining("|")) + "|")
+                .collect(joining(System.lineSeparator()));
         System.out.println(result);
+    }
+
+    private static void printResults(List<Result> results) {
+        String message = results.stream()
+                .map(Result::getResult)
+                .map(result -> String.format("%-5s", result))
+                .collect(joining(" "));
+        System.out.println(message);
+    }
+
+    public static void printSingleResult(String result) {
+        System.out.println(System.lineSeparator() + "실행 결과");
+        System.out.println(result);
+    }
+
+    public static void printAllResult(List<String> players, List<String> allResult) {
+        StringBuilder stringBuilder = new StringBuilder(System.lineSeparator() + "실행 결과" + System.lineSeparator());
+        for (int i = 0; i < players.size(); i++) {
+            stringBuilder.append(players.get(i) + " : " + allResult.get(i) + System.lineSeparator());
+        }
+        System.out.println(stringBuilder);
     }
 }
