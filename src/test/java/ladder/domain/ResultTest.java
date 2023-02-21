248f8cf5b0fd74d5928ca220439052a1f166941a
woo-chang <clllickme@naver.com>
2023-02-21 13:55:10 +0900

    feat: 실행 결과 공백 검증 기능 구현

---
+++ src/test/java/ladder/domain/ResultTest.java
@@ -0,0 +1,35 @@
+package ladder.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class ResultTest {
+
+    @ParameterizedTest(name = "입력: {0}")
+    @ValueSource(strings = {"", " ", "  ", "   ", "    "})
+    @DisplayName("실행 결과가 공백이라면 예외를 던진다.")
+    void resultNotBlank(final String value) {
+        assertThatThrownBy(() -> new Result(value))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("결과는 공백일 수 없습니다. 현재 입력한 값은 [\"" + value + "\"] 입니다.");
+    }
+}
+
+class Result {
+
+    private final String value;
+
+    public Result(final String value) {
+        validate(value);
+        this.value = value;
+    }
+
+    private void validate(final String value) {
+        if (value.isBlank()) {
+            throw new IllegalArgumentException("결과는 공백일 수 없습니다. 현재 입력한 값은 [\"" + value + "\"] 입니다.");
+        }
+    }
+}
