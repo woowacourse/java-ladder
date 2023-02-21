17ab1ddecdd45de959f3bfbb3a1673158db6ca0a
Eunkeee <eunkeeeee@gmail.com>
2023-02-21 14:41:56 +0900

    feat(Player): 사용자 생성

--- src/test/java/domain/PlayerTest.java
+++ src/test/java/domain/PlayerTest.java
@@ -1,19 +1,16 @@
 package domain;
 
-import domain.player.Name;
-import domain.player.Player;
-import domain.player.Position;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class PlayerTest {
-    private final Name name = new Name("깃짱");
-    private final Position initialPosition = new Position(0);
-    private final Player gitJjang = new Player(name, initialPosition);
-
     @Test
     void 사용자_생성() {
-        Assertions.assertEquals(gitJjang.getName(), name);
-        Assertions.assertEquals(gitJjang.getPosition(), initialPosition);
+        Name gitJjang = new Name("깃짱");
+        Position initialPosition = new Position(0);
+        Player player = new Player(gitJjang, initialPosition);
+
+        Assertions.assertEquals(player.getName(), gitJjang);
+        Assertions.assertEquals(player.getPosition(), initialPosition);
     }
 }
