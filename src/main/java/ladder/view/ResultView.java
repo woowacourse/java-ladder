0e2c82681eadb189e2494ee7a88b2b049a1366d1
amaran-th <songsy405@gmail.com>
2023-02-21 14:08:46 +0900

    fix : 요구사항에 맞게 출력 로직 수정

--- src/main/java/ladder/view/ResultView.java
+++ src/main/java/ladder/view/ResultView.java
@@ -7,51 +7,51 @@ import ladder.dto.NamesDto;
 
 public class ResultView {
 
-  private ResultView() {
-  }
-
-  public static void printResult(NamesDto namesDto, RowsDto rowsDto) {
-    printResultTitle();
-    printNames(namesDto);
-    printLines(rowsDto);
-  }
-
-  private static void printResultTitle() {
-    System.out.println("실행 결과");
-    System.out.println();
-  }
-
-  private static void printNames(NamesDto namesDto) {
-    namesDto.getNames().forEach(ResultView::printName);
-  }
-
-  private static void printName(String name) {
-    System.out.printf("%-6s", name);
-  }
-
-  private static void printLines(RowsDto rowsDto) {
-    System.out.println();
-    List<List<Step>> lines = rowsDto.getLines();
-    lines.forEach(ResultView::printLine);
-  }
-
-  private static void printLine(List<Step> line) {
-    System.out.print("|");
-    for (int i = 0; i < line.size(); i++) {
-      printLeg(line, i);
+    private ResultView() {
     }
-    System.out.println();
-  }
 
-  private static void printLeg(List<Step> line, int index) {
-    if (line.get(index) == Step.CONNECTED) {
-      System.out.print("-----|");
-      return;
+    public static void printResult(NamesDto namesDto, RowsDto rowsDto) {
+        printResultTitle();
+        printNames(namesDto);
+        printRows(rowsDto);
     }
-    System.out.print("     |");
-  }
 
-  public static void printErrorMessage(Exception e) {
-    System.out.println(e.getMessage());
-  }
+    private static void printResultTitle() {
+        System.out.println("실행 결과");
+        System.out.println();
+    }
+
+    private static void printNames(NamesDto namesDto) {
+        namesDto.getNames().forEach(ResultView::printName);
+    }
+
+    private static void printName(String name) {
+        System.out.printf("%6s", name);
+    }
+
+    private static void printRows(RowsDto rowsDto) {
+        System.out.println();
+        List<List<Step>> lines = rowsDto.getLines();
+        lines.forEach(ResultView::printRow);
+    }
+
+    private static void printRow(List<Step> steps) {
+        System.out.print("     |");
+        for (int i = 0; i < steps.size(); i++) {
+            printLeg(steps, i);
+        }
+        System.out.println();
+    }
+
+    private static void printLeg(List<Step> steps, int index) {
+        if (steps.get(index) == Step.CONNECTED) {
+            System.out.print("-----|");
+            return;
+        }
+        System.out.print("     |");
+    }
+
+    public static void printErrorMessage(Exception e) {
+        System.out.println(e.getMessage());
+    }
 }
