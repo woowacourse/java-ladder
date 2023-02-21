607cc9dd041b9bb9d2d85436758003a805439d29
apptie <appti@naver.com>
2023-02-21 12:49:49 +0900

    test: NameTest 중첩 구조를 통한 가독성 개선

--- src/test/java/model/NameTest.java
+++ src/test/java/model/NameTest.java
@@ -4,50 +4,60 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class NameTest {
 
-    @ParameterizedTest
-    @ValueSource(strings = {"a", "aaaaa"})
-    void 생성자는_전달받은_name의_길이가_1_이상_5_이하라면_Name을_생성한다(String name) {
-        assertThatCode(() -> new Name(name)).doesNotThrowAnyException();
+    @Nested
+    class 생성자_테스트 {
+
+        @ParameterizedTest
+        @ValueSource(strings = {"a", "aaaaa"})
+        void 인자로_전달받은_name의_길이가_1_이상_5_이하라면_Name을_생성한다(String name) {
+            assertThatCode(() -> new Name(name)).doesNotThrowAnyException();
+        }
+
+        @Test
+        void 인자로_전달받은_name의_길이가_5를_초과하면_예외가_발생한다() {
+            assertThatThrownBy(() -> new Name("aaaaaa"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("이름은 최대 5글자까지만 가능합니다.");
+        }
+
+        @ParameterizedTest
+        @EmptySource
+        void 인자로_전달받은_name이_공백이면_예외가_발생한다(String name) {
+            assertThatThrownBy(() -> new Name(name))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("이름은 공백일 수 없습니다.");
+        }
     }
 
-    @Test
-    void 생성자는_전달받은_name의_길이가_5를_초과하면_예외가_발생한다() {
-        assertThatThrownBy(() -> new Name("aaaaaa"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("이름은 최대 5글자까지만 가능합니다.");
-    }
+    @Nested
+    class equals_메소드_테스트 {
 
-    @ParameterizedTest
-    @ValueSource(strings = {"", "           "})
-    void 생성자는_전달받은_name이_공백이면_예외가_발생한다(String name) {
-        assertThatThrownBy(() -> new Name(name))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("이름은 공백일 수 없습니다.");
-    }
+        @ParameterizedTest
+        @CsvSource(value = {"a:a", "b:b", "c:c"}, delimiter = ':')
+        void equals_메소드는_같은_이름을_가진_Name을_전달하면_true를_반환한다(String firstName, String secondName) {
+            Name nameA = new Name(firstName);
+            Name nameB = new Name(secondName);
 
-    @ParameterizedTest
-    @CsvSource(value = {"a:a", "b:b", "c:c"}, delimiter = ':')
-    void equals_메소드는_같은_이름을_가진_Name을_전달하면_true를_반환한다(String firstName, String secondName) {
-        Name nameA = new Name(firstName);
-        Name nameB = new Name(secondName);
+            assertThat(nameA.equals(nameB)).isTrue();
+        }
 
-        assertThat(nameA.equals(nameB)).isTrue();
-    }
+        @ParameterizedTest
+        @CsvSource(value = {"a:b", "b:c", "c:a"}, delimiter = ':')
+        void equals_메소드는_다른_이름을_가진_Name을_전달하면_false를_반환한다(String firstName, String secondName) {
+            Name nameA = new Name(firstName);
+            Name nameB = new Name(secondName);
 
-    @ParameterizedTest
-    @CsvSource(value = {"a:b", "b:c", "c:a"}, delimiter = ':')
-    void equals_메소드는_다른_이름을_가진_Name을_전달하면_false를_반환한다(String firstName, String secondName) {
-        Name nameA = new Name(firstName);
-        Name nameB = new Name(secondName);
-
-        assertThat(nameA.equals(nameB)).isFalse();
+            assertThat(nameA.equals(nameB)).isFalse();
+        }
     }
 
     @ParameterizedTest
