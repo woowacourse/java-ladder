2609de6346fb53bd0d5bb6c15928310f30186b3b
Eunkeee <eunkeeeee@gmail.com>
2023-02-21 14:34:47 +0900

    test(Position): 포지션 생성 테스트 작성

---
+++ src/test/java/domain/PositionTest.java
@@ -0,0 +1,21 @@
+package domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class PositionTest {
+
+    @Nested
+    @DisplayName("포지션 생성 테스트")
+    class createPositionTest {
+        @ParameterizedTest
+        @ValueSource(ints = {0, 1, 2, 3})
+        void 포지션_생성_값_입력(int input) {
+            Position position = new Position(input);
+            Assertions.assertEquals(position.getPosition(), input);
+        }
+    }
+}
