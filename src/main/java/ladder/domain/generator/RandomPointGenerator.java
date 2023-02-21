6e5d13d83c04b7b4b2de9e7fdb2a8e7721ca1acc
kevstevie <jianna442@gmail.com>
2023-02-21 23:34:14 +0900

    refactor: secureRandom 적용

--- src/main/java/ladder/domain/generator/RandomPointGenerator.java
+++ src/main/java/ladder/domain/generator/RandomPointGenerator.java
@@ -1,13 +1,13 @@
 package ladder.domain.generator;
 
-import java.util.Random;
+import java.security.SecureRandom;
 
 public class RandomPointGenerator implements PointGenerator {
 
-    private static final Random random = new Random();
+    private static final SecureRandom secureRandom = new SecureRandom();
 
     @Override
     public boolean generate() {
-        return random.nextBoolean();
+        return secureRandom.nextBoolean();
     }
 }
