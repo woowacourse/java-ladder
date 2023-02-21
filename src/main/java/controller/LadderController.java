6ecb7bd49a6615d44cf27fb724d61b838ace75fc
jaehee329 <jjhs9803@gmail.com>
2023-02-21 17:30:42 +0900

    feat(LadderController): 사다리 결과 처리 기능 구현

--- src/main/java/controller/LadderController.java
+++ src/main/java/controller/LadderController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import domain.end.Ends;
 import domain.ladder.Ladder;
 import domain.ladder.LadderBuilder;
 import domain.ladder.LadderHeight;
@@ -10,10 +11,13 @@ import view.InputView;
 import view.OutputView;
 
 public class LadderController {
+	private static final String END_NAMES_COUNT_INVALID_ERROR_MSG = "결과의 수가 참여자의 수와 같아야 합니다.";
+
 	public void makeLadder() {
 		Users users = retrieveUsers();
+		Ends ends = retrieveEnds(users.getUsersCount());
 		Ladder ladder = buildLadder(users);
-		OutputView.printResult(users, ladder);
+		OutputView.printLadder(users, ladder, ends);
 	}
 
 	private Users retrieveUsers() {
@@ -22,6 +26,23 @@ public class LadderController {
 		} catch (IllegalArgumentException e) {
 			OutputView.printError(e.getMessage());
 			return retrieveUsers();
+		}
+	}
+
+	private Ends retrieveEnds(final int userCount) {
+		try {
+			Ends ends = new Ends(InputView.readEndNames());
+			validateEndsCount(ends, userCount);
+			return ends;
+		} catch (IllegalArgumentException e) {
+			OutputView.printError(e.getMessage());
+			return retrieveEnds(userCount);
+		}
+	}
+
+	private static void validateEndsCount(final Ends ends, final int expected) {
+		if (ends.getEndsCount() != expected) {
+			throw new IllegalArgumentException(END_NAMES_COUNT_INVALID_ERROR_MSG);
 		}
 	}
 
