3f7a17afef4c3592684568944f82013f9e489c92
hum02 <ehdgma02@gmail.com>
2023-02-21 17:38:23 +0900

    refactor: Line이 생성시 연속적 다리 존재 검증하도록 수정

--- src/main/java/ladder/domain/Line.java
+++ src/main/java/ladder/domain/Line.java
@@ -4,6 +4,7 @@ import ladder.utils.LineStrategy;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Line {
     private static final boolean LINKED = true;
@@ -11,6 +12,7 @@ public class Line {
 
     public Line(LineStrategy lineStrategy, int sectionCount) {
         this.bridges = lineStrategy.generate(sectionCount);
+        validate();
     }
 
     public int moveFrom(int startPosition) {
@@ -34,6 +36,21 @@ public class Line {
 
     private boolean isLeftMoveAble(int position) {
         return position - 1 >= 0;
+    }
+
+    private void validate() {
+        if (isContinuousLine(bridges)) {
+            throw new IllegalArgumentException("라인에 연속으로 연결된 다리는 생성될 수 없습니다.");
+        }
+    }
+
+    private boolean isContinuousLine(List<Boolean> line) {
+        return IntStream.range(0, line.size() - 1)
+                .anyMatch(idx -> isContinuous(line, idx));
+    }
+
+    private boolean isContinuous(List<Boolean> line, int idx) {
+        return line.get(idx) && line.get(idx + 1);
     }
 
     private void validateOutBound(int startPoint) {
