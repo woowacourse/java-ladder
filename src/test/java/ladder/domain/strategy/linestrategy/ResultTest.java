87bc29ec70e4af10e58bb59aa714cf3f978b95d8
kdkdhoho <hkim4410@naver.com>
2023-02-21 15:56:57 +0900

    style(ResultTest): Result 클래스 내, 검증 순서에 따른 메서드 위치 리팩터링

--- src/test/java/ladder/domain/strategy/linestrategy/ResultTest.java
+++ src/test/java/ladder/domain/strategy/linestrategy/ResultTest.java
@@ -9,6 +9,18 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 class ResultTest {
 
+    @Test
+    @DisplayName("이름에 null이 들어가면 IllegalArgumentException 예외가 발생한다.")
+    void create_null() {
+        // given
+        String input = null;
+
+        // expect
+        assertThatIllegalArgumentException().isThrownBy(() ->
+                new Result(input)
+        ).withMessage("[ERROR] 이름에 null이 들어갈 수 없습니다.");
+    }
+
     @ParameterizedTest
     @DisplayName("이름이 공백이면 IllegalArgumentException 예외가 발생한다.")
     @ValueSource(strings = {"", " ", "\n", "    "})
@@ -18,16 +30,4 @@ class ResultTest {
                 new Result(input)
         ).withMessage("[ERROR] 결과는 공백일 수 없습니다.");
      }
-
-     @Test
-     @DisplayName("이름에 null이 들어가면 IllegalArgumentException 예외가 발생한다.")
-     void create_null() {
-         // given
-         String input = null;
-
-         // expect
-         assertThatIllegalArgumentException().isThrownBy(() ->
-                 new Result(input)
-         ).withMessage("[ERROR] 이름에 null이 들어갈 수 없습니다.");
-      }
 }
