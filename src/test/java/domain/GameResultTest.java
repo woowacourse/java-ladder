ecf9a07c65bc29e32757399c795c14cecaedc714
splitube <voicesplit@daum.net>
2023-02-21 23:34:18 +0900

    feat: 테스트를 위한 BooleanGenerator 생성 및 테스트 간소화

--- src/test/java/domain/GameResultTest.java
+++ src/test/java/domain/GameResultTest.java
@@ -1,44 +1,32 @@
 package domain;
 
+import java.util.List;
 import java.util.Map;
-import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.DynamicTest;
-import org.junit.jupiter.api.TestFactory;
+import org.junit.jupiter.api.Test;
 
 class GameResultTest {
 
-    @DisplayName("게임 실행 결과를 ")
-    @TestFactory
-    Stream<DynamicTest> makeGameResult() {
+    @DisplayName("게임 실행 결과를 생성한다.")
+    @Test
+    void getGameResult() {
+        //given
         Participants participants = new Participants("ash,split,ako,mako,heero,bever");
         LadderResults ladderResults = new LadderResults("1000,2000,3000,4000,5000,6000");
-        return Stream.of(
-            DynamicTest.dynamicTest("true만 반환하는 제네레이터를 사용할 때", () -> {
-                Ladder ladder = new Ladder("3", 5, () -> true);
-                GameResult gameResult = new GameResult(ladder, participants, ladderResults);
-                Assertions.assertThat(gameResult.getResult()).containsAllEntriesOf(Map.of(
-                    "ash", "2000",
-                    "split", "1000",
-                    "ako", "4000",
-                    "mako", "3000",
-                    "heero", "6000",
-                    "bever", "5000"
-                ));
-            }),
-            DynamicTest.dynamicTest("false만 반환하는 제네레이터를 사용할 때", () -> {
-                Ladder ladder = new Ladder("3", 5, () -> false);
-                GameResult gameResult = new GameResult(ladder, participants, ladderResults);
-                Assertions.assertThat(gameResult.getResult()).containsAllEntriesOf(Map.of(
-                    "ash", "1000",
-                    "split", "2000",
-                    "ako", "3000",
-                    "mako", "4000",
-                    "heero", "5000",
-                    "bever", "6000"
-                ));
-            })
-        );
+        TestBooleanGenerator testBooleanGenerator = new TestBooleanGenerator();
+        testBooleanGenerator.addAllValues(List.of(true, false, false, true, false));
+        Ladder ladder = new Ladder("1", 5, testBooleanGenerator);
+        //when
+        GameResult gameResult = new GameResult(ladder, participants, ladderResults);
+        //then
+        Assertions.assertThat(gameResult.getResult()).containsAllEntriesOf(Map.of(
+            "ash", "2000",
+            "split", "1000",
+            "ako", "3000",
+            "mako", "4000",
+            "heero", "6000",
+            "bever", "5000"
+        ));
     }
 }
