3ef852a66a2afb937b78c3026d4c70448521eddc
hyunseo <goatsfish@gmail.com>
2023-02-21 19:52:12 +0900

    feat: Ladder 시작점에 맞는 도착점 조회 기능 추가

--- src/main/java/laddergame/domain/Ladder.java
+++ src/main/java/laddergame/domain/Ladder.java
@@ -8,11 +8,50 @@ public class Ladder {
     private final Lines lines;
 
     public Ladder(final Lines inputLines) {
-        this.lines = Optional.ofNullable(inputLines)
-                .orElseThrow(() -> new IllegalArgumentException(LADDER_LINES_NULL_EXCEPTION));
+        this.lines = getLines(inputLines);
+    }
+
+    public Position findLastDestination(final Position row) {
+        final Height height = lines.getHeight();
+        Position participantRow = row;
+        for (int column = 0; column < height.getValue(); column++) {
+            final Position currentColumn = new Position(column);
+            participantRow = findNextPosition(currentColumn, participantRow);
+        }
+        return participantRow;
     }
 
     public Lines getLines() {
         return lines;
+    }
+
+    private Position findNextPosition(final Position column, final Position row) {
+        final Width width = lines.getLines().get(column.getValue()).getWidth();
+        final Position maxPosition = new Position(width.getValue());
+        final Position leftPosition = row.move(-1);
+        if (checkRange(leftPosition, maxPosition) && checkFilled(column, leftPosition)) {
+            return row.move(-1);
+        }
+        if (checkRange(row, maxPosition) && checkFilled(column, row)) {
+            return row.move(1);
+        }
+        return row;
+    }
+
+    private boolean checkRange(final Position position, final Position maxPosition) {
+        return 0 <= position.getValue() && position.getValue() < maxPosition.getValue();
+    }
+
+    private boolean checkFilled(final Position column, final Position row) {
+        final int height = lines.getHeight().getValue();
+        if (height <= column.getValue()) {
+            return false;
+        }
+        return lines.getPointByColumnAndRow(column, row);
+    }
+
+    private Lines getLines(final Lines inputLines) {
+        return Optional.ofNullable(inputLines)
+                .orElseThrow(() -> new IllegalArgumentException(LADDER_LINES_NULL_EXCEPTION));
     }
 }
