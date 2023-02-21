83b81610ce227cb2a7c523a1099d3bd27fe85f1a
hyunseo <goatsfish@gmail.com>
2023-02-21 19:57:03 +0900

    feat: 사다리 매칭 기능 추가

--- src/main/java/laddergame/controller/LadderController.java
+++ src/main/java/laddergame/controller/LadderController.java
@@ -3,13 +3,15 @@ package laddergame.controller;
 import laddergame.domain.*;
 import laddergame.view.InputView;
 import laddergame.view.LadderForm;
+import laddergame.view.LadderMatchForm;
 import laddergame.view.OutputView;
 
-import java.util.List;
+import java.util.Map;
 
 import static laddergame.utils.ExceptionTemplate.repeatAndPrintCause;
 
 public class LadderController {
+    private static final String FIND_ALL_MATCH_RESULTS_COMMAND = "all";
     private final InputView inputView;
     private final OutputView outputView;
     private final BooleanGenerator booleanGenerator;
@@ -25,11 +27,24 @@ public class LadderController {
 
     public void run() {
         final Participants participants = repeatAndPrintCause(() -> new Participants(inputView.readNames()));
+        final Results results = repeatAndPrintCause(() -> new Results(inputView.readResults(), participants.getNames()));
         final Height height = repeatAndPrintCause(() -> new Height(inputView.readHeight()));
+        final Width width = new Width(participants.getSize() - 1);
+        final LineCreator lineCreator = new LineCreator(booleanGenerator);
+        final Ladder ladder = new Ladder(lineCreator.createLines(width, height));
+        final Lines findLines = ladder.getLines();
 
-        final Ladder ladder = new Ladder(participants, height);
+        outputView.printResult(LadderForm.joinUnitsFrom(participants.getNameValues(), findLines.getLines(), results.getResults()));
+        runLadderMatch(ladder, participants, results);
+    }
 
-        final List<Line> lines = ladder.createLines(booleanGenerator);
-        outputView.printResult(LadderForm.joinUnitsFrom(participants.getNames(), lines));
+    private void runLadderMatch(final Ladder ladder, final Participants participants, final Results results) {
+        final LadderMatch ladderMatch = new LadderMatch(ladder, participants, results);
+        String command = "";
+        while (!command.equals(FIND_ALL_MATCH_RESULTS_COMMAND)) {
+            command = inputView.readName();
+            final Map<Name, Result> matchResults = ladderMatch.getLadderMatchResults(command);
+            outputView.printMatchResult(LadderMatchForm.joinUnitsFrom(matchResults));
+        }
     }
 }
