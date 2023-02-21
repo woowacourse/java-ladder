902bc355b7b5f56f38b1445185f560e1a1778c44
kwonyj1022 <kwonyj1022@naver.com>
2023-02-21 21:14:57 +0900

    feat(Ladder): LadderMaker가 하는 일 Ladder로 이동 및 LadderMaker 삭제

--- src/main/java/domain/ladder/Ladder.java
+++ src/main/java/domain/ladder/Ladder.java
@@ -1,14 +1,21 @@
 package domain.ladder;
 
+import domain.generator.BooleanGenerator;
+
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 public class Ladder {
 
-    private final List<Line> lines;
+    private final List<Line> lines = new ArrayList<>();
+    private final BooleanGenerator randomBridgeGenerator = () -> new Random().nextBoolean();
 
-    public Ladder(List<Line> lines) {
-        this.lines = lines;
+    public Ladder(int personCount, Height height) {
+        for (int i = 0; i < height.getHeight(); i++) {
+            lines.add(new Line(personCount, randomBridgeGenerator));
+        }
     }
 
     public List<Line> getLines() {
