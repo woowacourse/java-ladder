714cd04a136a8251ff7d8bca6d1f24d6e9f8ac12
Wugawuga <dnr3655@naver.com>
2023-02-21 13:57:56 +0900

    refactor: 테스트하지 않는 클래스 생성 메소드 분리

--- src/test/java/ladder/domain/LadderGameTest.java
+++ src/test/java/ladder/domain/LadderGameTest.java
@@ -14,15 +14,15 @@ public class LadderGameTest {
 
     @Test
     void 참가자들의_이름을_반환한다() {
-        final Players players = new Players(List.of("name1", "name2"));
-        LadderGame ladderGame = getLadderGame(new TestBooleanGenerator(List.of(true, true)), players, new Height(1));
+        final Players players = getPlayers("name1", "name2");
+        LadderGame ladderGame = new LadderGame(new TestBooleanGenerator(List.of(true, true)), players, new Height(1));
         assertThat(ladderGame.getPlayerNames()).containsExactly("name1", "name2");
     }
 
     @Test
     void 생성된_사다리를_반환한다() {
-        final Players players = new Players(List.of("name1", "name2"));
-        LadderGame ladderGame = getLadderGame(new TestBooleanGenerator(List.of(true, true)), players, new Height(2));
+        final Players players = getPlayers("name1", "name2");
+        LadderGame ladderGame = new LadderGame(new TestBooleanGenerator(List.of(true, true)), players, new Height(2));
         assertThat(ladderGame.getLadder())
                 .extracting(Line::getLine)
                 .containsExactly(
@@ -31,7 +31,7 @@ public class LadderGameTest {
                 );
     }
 
-    private LadderGame getLadderGame(BooleanGenerator booleanGenerator, Players players, Height height) {
-        return new LadderGame(booleanGenerator, players, height);
+    private static Players getPlayers(String... playerNames) {
+        return new Players(List.of(playerNames));
     }
 }
