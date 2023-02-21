1b709fcf83bcdf4dc5191c41d0d34df3da67ef6a
kokodak <kokodakadokok@gmail.com>
2023-02-21 16:05:28 +0900

    feat: 실행 결과를 입력받는 기능 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -7,6 +7,7 @@ import utils.StringParser;
 public class InputView {
 
     private static final String USER_NAMES_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    private static final String RESULT_NAMES_MESSAGE = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
     private static final String LADDER_HEIGHT_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
     private static final Scanner scanner = new Scanner(System.in);
 
@@ -14,6 +15,12 @@ public class InputView {
         System.out.println(USER_NAMES_MESSAGE);
         String userNames = scanner.nextLine();
         return StringParser.splitByDelimiter(userNames);
+    }
+
+    public static List<String> readResultNames() {
+        System.out.println(RESULT_NAMES_MESSAGE);
+        String resultNames = scanner.nextLine();
+        return StringParser.splitByDelimiter(resultNames);
     }
 
     public static int readLadderHeight() {
