a604ee5ae7dae90b741d4c431c09f0a0584189a3
kokodak <kokodakadokok@gmail.com>
2023-02-21 16:34:50 +0900

    feat: 이름으로 몇번째 인덱스에 있는 사람인지 찾는 기능 구현

--- src/main/java/domain/Users.java
+++ src/main/java/domain/Users.java
@@ -9,6 +9,7 @@ import utils.StringParser;
 public class Users {
 
     private static final String DUPLICATED_USER_ERROR = "[ERROR] 사람의 이름은 중복될 수 없습니다.";
+    private static final String NOT_EXIST_USER_ERROR = "[ERROR] 해당 이름을 가진 사람은 존재하지 않습니다.";
 
     private final List<User> users;
 
@@ -22,6 +23,16 @@ public class Users {
         if (users.size() != notDuplicatedUsers.size()) {
             throw new IllegalArgumentException(DUPLICATED_USER_ERROR);
         }
+    }
+
+    public int findByName(String name) {
+        for (int i = 0; i < users.size(); i++) {
+            User user = users.get(i);
+            if (user.getName().equals(name)) {
+                return i;
+            }
+        }
+        throw new IllegalArgumentException(NOT_EXIST_USER_ERROR);
     }
 
     public int getSize() {
--- src/test/java/domain/UsersTest.java
+++ src/test/java/domain/UsersTest.java
@@ -1,11 +1,14 @@
 package domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class UsersTest {
 
@@ -34,5 +37,31 @@ public class UsersTest {
 
         assertThatCode(() -> new Users(users))
                 .doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest(name = "이름으로 User를 찾을 수 있다.")
+    @CsvSource({"kodak,0", "polo,1", "poz,2"})
+    void findByNameTest1(String userName, int expectedIndex) {
+        User kodak = new User("kodak");
+        User polo = new User("polo");
+        User poz = new User("poz");
+
+        Users users = new Users(List.of(kodak, polo, poz));
+        int userIndex = users.findByName(userName);
+
+        assertThat(userIndex).isEqualTo(expectedIndex);
+    }
+
+    @Test
+    @DisplayName("User를 찾을 수 없다면 예외를 던진다.")
+    void findByNameTest2() {
+        User kodak = new User("kodak");
+        User polo = new User("polo");
+        User poz = new User("poz");
+
+        Users users = new Users(List.of(kodak, polo, poz));
+
+        assertThatThrownBy(() -> users.findByName("kadak"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
