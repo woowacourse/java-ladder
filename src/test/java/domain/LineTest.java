ebf5619d35f2f59f181bca24d63a574e8e21aaac
Eunkeee <eunkeeeee@gmail.com>
2023-02-21 14:55:58 +0900

    test(Line): 사다리 발판 생성 테스트 추가

--- src/test/java/domain/LineTest.java
+++ src/test/java/domain/LineTest.java
@@ -1,13 +1,10 @@
 package domain;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import domain.generator.BooleanGenerator;
 import domain.generator.RandomBooleanGenerator;
 import domain.generator.TrueBooleanGenerator;
-import domain.ladder.LadderStep;
-import domain.ladder.Line;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -16,7 +13,6 @@ import org.junit.jupiter.api.Test;
 class LineTest {
     private final BooleanGenerator randomBooleanGenerator = new RandomBooleanGenerator();
     private final BooleanGenerator trueBooleanGenerator = new TrueBooleanGenerator();
-    private Line nonRandomLine = new Line(3, trueBooleanGenerator);
 
     @Nested
     @DisplayName("사다리 발판 생성 테스트")
@@ -28,16 +24,9 @@ class LineTest {
 
         @Test
         void createNonRandomLadderStepsTest() {
-            Assertions.assertThat(nonRandomLine.getLadderSteps())
+            Line line = new Line(3, trueBooleanGenerator);
+            Assertions.assertThat(line.getLadderSteps())
                     .containsExactly(LadderStep.EXISTS, LadderStep.NONE, LadderStep.EXISTS);
         }
-    }
-
-    @Test
-    void 사다리를_타고_한칸_이동한다() {
-        assertEquals(nonRandomLine.getNextStepIndex(0), 1);
-        assertEquals(nonRandomLine.getNextStepIndex(1), 0);
-        assertEquals(nonRandomLine.getNextStepIndex(2), 3);
-        assertEquals(nonRandomLine.getNextStepIndex(3), 2);
     }
 }
