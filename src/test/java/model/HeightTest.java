670284fcf28bdfbdc13bbdc7821fbfb382146735
apptie <appti@naver.com>
2023-02-21 14:16:18 +0900

    test: Height 기능 변경으로 인한 테스트 케이스 수정

--- src/test/java/model/HeightTest.java
+++ src/test/java/model/HeightTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import helper.FakeHeight;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 import org.junit.jupiter.api.Nested;
@@ -15,6 +14,7 @@ class HeightTest {
 
     @Nested
     class 생성자_테스트 {
+
         @Test
         void 인자로_0_이하의_값이_주어지면_예외가_발생한다() {
             assertThatThrownBy(() -> new Height(0))
@@ -30,20 +30,19 @@ class HeightTest {
 
     @Nested
     class isContinueMakerLadder_메소드_테스트 {
-        @Test
-        void 원시_값_height가_1_이상이면_true를_반환한다() {
-            Height height = new Height(1);
 
-            boolean actual = height.isContinueMakeLadder();
+        private final Height height = new Height(5);
+
+        @Test
+        void 원시_값_height가_전달한_매개변수보다_크면_true를_반환한다() {
+            boolean actual = height.isContinueMakeLadder(4);
 
             assertThat(actual).isSameAs(true);
         }
 
         @Test
-        void 원시_값_height가_0이면_false를_반환한다() {
-            Height height = new FakeHeight();
-
-            boolean actual = height.isContinueMakeLadder();
+        void 원시_값_height가_전달한_매개변수보다_작으면_false를_반환한다() {
+            boolean actual = height.isContinueMakeLadder(5);
 
             assertThat(actual).isSameAs(false);
         }
