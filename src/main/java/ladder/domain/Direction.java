852748fb4e94055f570db147695dd9b06180db11
drunkenhw <drunkenhw@gmail.com>
2023-02-21 15:31:38 +0900

    feat: bar의 유무에 따른 위치 반환하는 기능 추가

--- src/main/java/ladder/domain/Direction.java
+++ src/main/java/ladder/domain/Direction.java
@@ -1,14 +1,27 @@
 package ladder.domain;
 
+import java.util.Arrays;
+
 public enum Direction {
-    LEFT(-1),
-    RIGHT(1),
-    NONE(0);
+    LEFT(-1, true, false),
+    RIGHT(1, false, true),
+    NONE(0, false, false);
 
     private int moveValue;
+    private boolean isLeftExist;
+    private boolean isRightExist;
 
-    Direction(int moveValue) {
+    Direction(int moveValue, boolean isLeftExist, boolean isRightExist) {
         this.moveValue = moveValue;
+        this.isLeftExist = isLeftExist;
+        this.isRightExist = isRightExist;
+    }
+
+    public static Direction of(boolean isLeftExist, boolean isRightExist) {
+        return Arrays.stream(Direction.values())
+                .filter(direction -> direction.isLeftExist == isLeftExist && direction.isRightExist == isRightExist)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("해당 방향은 움직일 수 없습니다."));
     }
 
     public int getMoveValue() {
--- src/test/java/ladder/domain/DirectionTest.java
+++ src/test/java/ladder/domain/DirectionTest.java
@@ -2,6 +2,7 @@ package ladder.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -13,6 +14,17 @@ class DirectionTest {
     @DisplayName("방향에 따라 다른 값을 반환한다")
     void shouldReturnValueWhenGetDirection(Direction direction, int expect) {
         assertThat(direction.getMoveValue()).isEqualTo(expect);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"true:false:LEFT", "false:true:RIGHT", "false:false:NONE"}, delimiter = ':')
+    @DisplayName("bar의 유무에 따라 방향을 반환한다")
+    void shouldReturnDirectionWhenBarExist(boolean isLeftExist, boolean isRightExist, Direction expect) {
+        //given
+        //when
+        Direction direction = Direction.of(isLeftExist, isRightExist);
+        //then
+        Assertions.assertThat(direction).isEqualTo(expect);
     }
 
 }
