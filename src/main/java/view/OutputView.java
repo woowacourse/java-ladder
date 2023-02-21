acd896c66b2a3e7e89e2044b683ad6010545494a
kiarakim <kiarakim413@gmail.com>
2023-02-21 23:13:40 +0900

    feat(OutputView) : 보상 출력 기능 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,6 +1,7 @@
 package view;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class OutputView {
@@ -8,12 +9,6 @@ public class OutputView {
 	public static final String SECTION = "|";
 	public static final String ONE_STOOL = "-----";
 	public static final String NO_STOOL = "     ";
-
-	public void printResult(List<String> names, List<List<Boolean>> ladder) {
-		System.out.println("\n실행결과\n");
-		printNames(names);
-		printLadder(ladder);
-	}
 
 	private static String makeLevelView(List<Boolean> level) {
 		return FIRST_SECTION + level.stream()
@@ -27,6 +22,13 @@ public class OutputView {
 		return NO_STOOL;
 	}
 
+	public void printResult(List<String> names, List<List<Boolean>> ladder, List<String> sequences) {
+		System.out.println("\n실행결과\n");
+		printNames(names);
+		printLadder(ladder);
+		printSequences(sequences);
+	}
+
 	private void printNames(List<String> names) {
 		names.stream()
 			.map(name -> String.format("%6s", name))
@@ -38,5 +40,24 @@ public class OutputView {
 		ladder.stream()
 			.map(OutputView::makeLevelView)
 			.forEach(System.out::println);
+	}
+
+	private void printSequences(List<String> sequences) {
+		sequences.stream()
+			.map(sequence -> String.format("%6s", sequence))
+			.forEach(System.out::print);
+		System.out.println();
+	}
+
+	public void printWanted(String name, Map<String, String> temp) {
+		System.out.println("\n실행결과");
+		System.out.println(temp.get(name));
+	}
+
+	public void printAll(Map<String, String> temp) {
+		System.out.println("\n실행결과");
+		for (String key : temp.keySet()) {
+			System.out.println(key + " : " + temp.get(key));
+		}
 	}
 }
