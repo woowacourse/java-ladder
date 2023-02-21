823e8a44810fde2f9c02b8a89cfc13e12e4b7097
hgo641 <hgo641@gmail.com>
2023-02-21 15:52:47 +0900

    test: 발판 생성 가능 여부 판단

--- src/test/java/ladder/domain/RowTest.java
+++ src/test/java/ladder/domain/RowTest.java
@@ -1,14 +1,17 @@
 package ladder.domain;
 
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
-
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThatNoException;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class RowTest {
     @Test
@@ -32,5 +35,19 @@ class RowTest {
     public void 생성_success(Step first, Step second) {
         assertThatNoException()
                 .isThrownBy(() -> Row.of(List.of(first, second), 2));
+    }
+
+    @ParameterizedTest(name = "양옆 index에 발판이 없는 경우에만 발판을 생성할 수 있다")
+    @MethodSource("발판_생성_가능_여부_판단_데이터")
+    public void 발판_생성_가능_여부_판단(Row row, int targetIndex, boolean expected) {
+        assertThat(row.isStepPossible(targetIndex)).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> 발판_생성_가능_여부_판단_데이터() {
+        return Stream.of(
+                Arguments.of(Row.of(List.of(Step.Y, Step.N), 2), 1, false),
+                Arguments.of(Row.of(List.of(Step.N, Step.N, Step.Y), 3), 0, true),
+                Arguments.of(Row.of(List.of(Step.N, Step.N, Step.Y), 3), 1, false)
+        );
     }
 }
