b19a0187d8626b787d94efba71dfc633a18a19ee
sh111-coder <ohk9134@naver.com>
2023-02-21 16:07:27 +0900

    refactor(LadderGame): LadderGame이 참여자들과 사다리 필드만을 가지도록 리팩토링

--- src/main/java/ladder/domain/LadderGame.java
+++ src/main/java/ladder/domain/LadderGame.java
@@ -1,24 +1,30 @@
 package ladder.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LadderGame {
 
-    private final PlayerNames playerNames;
+    private final Players players;
     private final Ladder ladder;
-    private final LadderProperty ladderProperty;
 
     public LadderGame(List<String> names, int height) {
-        this.playerNames = new PlayerNames(names);
-        this.ladderProperty = new LadderProperty(names.size() - 1, height);
-
-        LadderMaker ladderMaker = new LadderMaker(ladderProperty);
+        this.players = new Players(generatePlayers(names));
+        LadderMaker ladderMaker = new LadderMaker(new LadderProperty(names.size() - 1, height));
 
         this.ladder = ladderMaker.generate();
     }
 
+    private List<Player> generatePlayers(List<String> names) {
+        List<Player> gamePlayers = new ArrayList<>();
+        for (int startIndex = 0; startIndex < names.size(); startIndex++) {
+            gamePlayers.add(new Player(names.get(startIndex), startIndex));
+        }
+        return gamePlayers;
+    }
+
     public List<String> getNames () {
-        return playerNames.getNames();
+        return players.getNames();
     }
 
     public List<Line> getLines() {
