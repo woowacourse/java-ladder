302cf1017262a67345fffd492a28ef66a5ae3996
hyunseo <goatsfish@gmail.com>
2023-02-21 19:56:46 +0900

    feat: 사다리 매칭 결과 출력 기능 추가

--- src/main/java/laddergame/view/OutputView.java
+++ src/main/java/laddergame/view/OutputView.java
@@ -1,10 +1,16 @@
 package laddergame.view;
 
 public class OutputView {
-    private static final String ANNOUNCE_RESULT = "실행결과";
+    private static final String ANNOUNCE_LADDER_RESULT = "사다리 결과";
+    private static final String ANNOUNCE_MATCH_RESULTS = "실행 결과";
 
     public void printResult(final String ladderFrom) {
-        System.out.println(ANNOUNCE_RESULT);
+        System.out.println(ANNOUNCE_LADDER_RESULT);
         System.out.println(ladderFrom);
+    }
+
+    public void printMatchResult(final String matchForm) {
+        System.out.println(ANNOUNCE_MATCH_RESULTS);
+        System.out.println(matchForm);
     }
 }
