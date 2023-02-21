870a02ffcb64a5f7f2c2b397eb6d0abb4d1482cb
BGuga <tmdwns9913@gmail.com>
2023-02-21 17:16:36 +0900

    test: floor의 크기에 맞지 않는 위치에서 위치 반환 함수를 호출하면 예외를 발생시키도록 테스트 작성

--- src/test/java/ladder/domain/FloorTest.java
+++ src/test/java/ladder/domain/FloorTest.java
@@ -2,11 +2,14 @@ package ladder.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class FloorTest {
@@ -49,23 +52,33 @@ public class FloorTest {
     @DisplayName("Floor에 출발 위치를 입력하면 반환 위치를 반환한다.")
     void floorMoveTest() {
         final Floor floor = new Floor(2);
-        floor.makeFloor(List.of(MAKE,MAKE));
+        floor.makeFloor(List.of(MAKE, MAKE));
         assertThat(floor.getResultPosition(2)).isEqualTo(2);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 3, 4, 5, 6})
+    @DisplayName("Floor의 크기에 맞지 않는 위치의 index값을 구하려 한다면 예외가 발생한다.")
+    void resultPositionExceptionTest(int index) {
+        final Floor floor = new Floor(3);
+        assertThatThrownBy(()-> floor.getResultPosition(index))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("해당 위치는 존재하지 않는 값입니다.");
     }
 
     @Test
     @DisplayName("Floor의 반환 결과 다중 테스트")
     void floorMoveMultipleTest() {
         final Floor floor = new Floor(2);
-        floor.makeFloor(List.of(MAKE,MAKE));
+        floor.makeFloor(List.of(MAKE, MAKE));
         assertAll(
-                ()->{
+                () -> {
                     assertThat(floor.getResultPosition(0)).isEqualTo(1);
                 },
-                ()->{
+                () -> {
                     assertThat(floor.getResultPosition(1)).isEqualTo(0);
                 },
-                ()->{
+                () -> {
                     assertThat(floor.getResultPosition(2)).isEqualTo(2);
                 }
 
