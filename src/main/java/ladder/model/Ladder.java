36a88fd32102ca9063ad9c7a9face2bff0f408de
LJW25 <dbjwl@g.skku.edu>
2023-02-21 20:26:24 +0900

    feat(Ladder): 사다리 타기 기능 구현

--- src/main/java/ladder/model/Ladder.java
+++ src/main/java/ladder/model/Ladder.java
@@ -10,6 +10,25 @@ public class Ladder {
         this.rows = rows;
     }
 
+
+    public int getResult(int playerNumber) {
+        int currentPosition = playerNumber;
+        for (Row row : rows) {
+            currentPosition = changePositionIfHasLine(row, currentPosition);
+        }
+        return currentPosition;
+    }
+
+    private int changePositionIfHasLine(Row row, int currentPosition) {
+        if (row.isLeftPointHasLine(currentPosition)) {
+            return currentPosition - 1;
+        }
+        if (row.isRightPointHasLine(currentPosition)) {
+            return currentPosition + 1;
+        }
+        return currentPosition;
+    }
+
     public List<Row> getRows() {
         return rows;
     }
