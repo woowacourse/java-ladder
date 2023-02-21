4905f7d81f422578c90ff92a26ec3293eeb26f35
sh111-coder <ohk9134@naver.com>
2023-02-21 17:50:15 +0900

    feat(LadderResult): "꽝" 이외의 실행 결과 정수 검증 기능 구현

--- src/main/java/ladder/domain/LadderResult.java
+++ src/main/java/ladder/domain/LadderResult.java
@@ -2,9 +2,31 @@ package ladder.domain;
 
 public class LadderResult {
 
-    private final String result;
+    private static final String NO_LUCK = "꽝";
+    private static final String RESULT_EXCEPT_NO_LUCK_IS_NOT_NUMERIC_ERROR_MESSAGE = "[ERROR] \"꽝\" 이외의 실행 결과는 정수여야합니다.";
 
-    public LadderResult(final String result) {
-        this.result = result;
+    private final String runResult;
+
+    public LadderResult(final String runResult) {
+        validate(runResult);
+        this.runResult = runResult;
+    }
+
+    private void validate(String runResult) {
+        if (isNotNoLuck(runResult)) {
+            validateNumeric(runResult);
+        }
+    }
+
+    private boolean isNotNoLuck(String runResult) {
+        return !NO_LUCK.equals(runResult);
+    }
+
+    private void validateNumeric(String runResult) {
+        try {
+            Integer.parseInt(runResult);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(RESULT_EXCEPT_NO_LUCK_IS_NOT_NUMERIC_ERROR_MESSAGE);
+        }
     }
 }
