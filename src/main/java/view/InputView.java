513ea709ba8d0f17cdd66907806345e3a3b1f449
iamjooon2 <iamjooon2@gmail.com>
2023-02-22 19:49:06 +0900

    feat: 실행 결과(상품)을 입력받는 기능

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 public class InputView {
     private static final String DELIMITER = ",";
     private static final String INPUT_NAMES = "참여할 사람 이름을 입력하세요. (이름은 쉼표(" + DELIMITER + ")로 구분하세요)";
+    private static final String INPUT_PRIZES = "실행 결과를 입력하세요. (결과는 쉼표(" + DELIMITER + ")로 구분하세요)";
     private static final String INPUT_HEIGHT = "최대 사다리 높이는 몇 개인가요?";
     private static final String ERROR_NOT_NUMBER = "[ERROR] 사다리의 높이는 2 이상의 수를 입력해주세요";
 
@@ -36,5 +37,12 @@ public class InputView {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(ERROR_NOT_NUMBER);
         }
+    }
+
+    public static List<String> inputPrizes() {
+        System.out.println(INPUT_PRIZES);
+        String input = scanner.nextLine();
+        String[] split = input.split(DELIMITER);
+        return new ArrayList<>(Arrays.asList(split));
     }
 }
