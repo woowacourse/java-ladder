c86348679c79b4677c5aad894e971e2d881fa06f
DESKTOP-FNVN0HH\USER <beatus96@naver.com>
2023-02-22 17:46:07 +0900

    refactor: Rewards 도메인 생성 기능 수정

--- src/main/java/ladder/domain/Rewards.java
+++ src/main/java/ladder/domain/Rewards.java
@@ -1,28 +1,27 @@
 package ladder.domain;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Rewards {
 
-    private final List<String> rewards;
+    private final List<Reward> rewards;
 
     public Rewards(int numberOfPlayers, List<String> rewards) {
         validateNumberOfRewards(numberOfPlayers, rewards);
-        validateBlankRewards(rewards);
-        this.rewards = rewards;
+
+        this.rewards = createRewardsBy(rewards);
+    }
+
+    private List<Reward> createRewardsBy(List<String> rewards) {
+        return rewards.stream()
+                .map(Reward::new)
+                .collect(Collectors.toList());
     }
 
     private void validateNumberOfRewards(int numberOfPlayers, List<String> rewards) {
         if (numberOfPlayers != rewards.size()) {
             throw new IllegalArgumentException("보상의 개수는 플레이어 수와 일치해야 합니다.");
-        }
-    }
-
-    private void validateBlankRewards(List<String> rewards) {
-        for (String reward : rewards) {
-            if (reward.isBlank()) {
-                throw new IllegalArgumentException("보상을 입력하지 않았습니다.");
-            }
         }
     }
 }
