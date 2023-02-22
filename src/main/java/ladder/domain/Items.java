95ed76c44ef4a76d30f1930f4826ff6f9947778e
Cyma-s <ssol0319@naver.com>
2023-02-22 14:55:04 +0900

    feat: Item의 일급 컬렉션 클래스 추가

---
+++ src/main/java/ladder/domain/Items.java
@@ -0,0 +1,22 @@
+package ladder.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class Items {
+    private final List<Item> items;
+
+    public Items(List<String> items) {
+        this.items = new ArrayList<>();
+        IntStream.range(0, items.size())
+                .forEach(position -> this.items.add(new Item(items.get(position), position)));
+    }
+
+    public Item findItem(Position position) {
+        return items.stream()
+                .filter(item -> item.isSamePosition(position))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("플레이어의 실행 결과가 존재하지 않습니다."));
+    }
+}
