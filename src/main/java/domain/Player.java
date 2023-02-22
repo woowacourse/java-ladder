8c6c173e40ffcc83aa3fd222004e6402bc4d343a
Miseong Kim <miseongk@naver.com>
2023-02-22 14:07:52 +0900

    test: 입력받은 플레이어 문자열로 Players 객체 생성 확인

--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -1,5 +1,7 @@
 package domain;
 
+import java.util.Objects;
+
 public class Player {
     private static final int MAXIMUM_LENGTH = 5;
 
@@ -18,5 +20,22 @@ public class Player {
 
     public String getName() {
         return this.name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Player player = (Player) o;
+        return Objects.equals(name, player.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
---
+++ src/test/java/util/PlayersMakerTest.java
@@ -0,0 +1,24 @@
+package util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import domain.Player;
+import domain.Players;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PlayersMakerTest {
+
+    @Test
+    @DisplayName("입력받은 플레이어 이름 문자열로 Players 객체 생성 확인")
+    void makePlayers() {
+        Players players = PlayersMaker.makePlayers("roy,soy,coy");
+        Assertions.assertAll(
+                () -> assertThat(players.getPlayers().get(0)).isEqualTo(new Player("roy")),
+                () -> assertThat(players.getPlayers().get(1)).isEqualTo(new Player("soy")),
+                () -> assertThat(players.getPlayers().get(2)).isEqualTo(new Player("coy"))
+        );
+    }
+
+}
