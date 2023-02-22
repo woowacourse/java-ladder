3d821699aca60d539ab719f0f7915f524a5f83cb
kdkdhoho <hkim4410@naver.com>
2023-02-22 18:43:51 +0900

    refactor(Application): 메서드 분리

--- src/main/java/ladder/Application.java
+++ src/main/java/ladder/Application.java
@@ -1,5 +1,12 @@
 package ladder;
 
+import static ladder.view.InputView.readLadderHeight;
+import static ladder.view.InputView.readPlayerNames;
+import static ladder.view.InputView.readResults;
+import static ladder.view.InputView.readTargetName;
+import static ladder.view.OutputView.printLadder;
+import static ladder.view.OutputView.printSingleResult;
+
 import ladder.domain.Height;
 import ladder.domain.Ladder;
 import ladder.domain.LadderFactory;
@@ -7,31 +14,61 @@ import ladder.domain.Players;
 import ladder.domain.Results;
 import ladder.domain.strategy.linestrategy.LineStrategy;
 import ladder.domain.strategy.linestrategy.RandomLineStrategy;
-import ladder.view.InputView;
 import ladder.view.OutputView;
 
 public class Application {
+    private static final String QUIT = "q";
+    private static final String ALL = "all";
+    private static final Boolean END = false;
+    private static final Boolean GO = true;
+
+    private static Players players;
+    private static Results results;
+    private static Height height;
+    private static final LineStrategy lineStrategy = new RandomLineStrategy();
+    private static LadderFactory ladderFactory;
+    private static Ladder ladder;
+
     public static void main(String[] args) {
-        Players players = new Players(InputView.readPlayerNames());
-        Results results = new Results(InputView.readResults(), players.getPlayersCount());
-        Height height = new Height(InputView.readLadderHeight());
-        LineStrategy lineStrategy = new RandomLineStrategy();
+        players = new Players(readPlayerNames());
+        results = new Results(readResults(), players.getPlayersCount());
+        height = new Height(readLadderHeight());
 
-        LadderFactory ladderFactory = new LadderFactory(height, players, lineStrategy);
-        Ladder ladder = ladderFactory.makeLadder();
+        ladderFactory = new LadderFactory(height, players, lineStrategy);
+        ladder = ladderFactory.makeLadder();
+        printLadder(players.getPlayers(), ladder.getLines(), results.getResults());
 
-        OutputView.printLadder(players.getPlayers(), ladder.getLines(), results.getResults());
+        checkResult();
+    }
 
-        while (true) {
-            String targetName = InputView.readTargetName();
-            if (targetName.equals("q")) {
-                break;
-            }
-            if (targetName.equals("all")) {
-                OutputView.printAllResult(players.getPlayerNames(), results.findAllResult(ladder.getLines()));
-                break;
-            }
-            OutputView.printSingleResult(results.findResult(ladder.getLines(), players.findPosition(targetName)));
+    private static void checkResult() {
+        boolean proceed = true;
+        while (proceed) {
+            String input = readTargetName();
+            proceed = isQuit(input);
+            proceed = isAll(proceed, input);
+            isSingleResult(proceed, input);
+        }
+    }
+
+    private static boolean isQuit(String targetName) {
+        if (targetName.equals(QUIT)) {
+            return END;
+        }
+        return GO;
+    }
+
+    private static boolean isAll(boolean flag, String input) {
+        if (flag && input.equals(ALL)) {
+            OutputView.printAllResult(players.getPlayersName(), results.findAllResult(ladder.getLines()));
+            return END;
+        }
+        return GO;
+    }
+
+    private static void isSingleResult(boolean flag, String input) {
+        if (flag) {
+            printSingleResult(results.findResult(ladder.getLines(), players.findPosition(input)));
         }
     }
 }
