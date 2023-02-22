4c5f3b1a1e9c03f4239648a2bfebd0b304a0f51c
Youngho <cyh6099@gmail.com>
2023-02-23 02:09:09 +0900

    feat : 전체 게임 출력 후 게임 종료 메시지 출력

--- src/main/java/view/ResultView.java
+++ src/main/java/view/ResultView.java
@@ -6,6 +6,7 @@ public class ResultView {
     private static final String RESULT_PRINT_GUIDE_MESSAGE = "\n실행결과";
     private static final String NON_EXIST_USER_GUIDE_MESSAGE = "존재하지 않는 유저이름 입니다.";
     private static final String PRINT_ALL_RESULT_DELIMITER = " : ";
+    private static final String GAME_EXIT_MESSAGE = "모든 결과를 출력하고 종료합니다.";
     public void printUserResult(String result) {
         System.out.println(RESULT_PRINT_GUIDE_MESSAGE);
         System.out.println(result);
@@ -16,6 +17,7 @@ public class ResultView {
         for (String user : result.keySet()) {
             System.out.println(user + PRINT_ALL_RESULT_DELIMITER + result.get(user));
         }
+        System.out.println(GAME_EXIT_MESSAGE);
     }
 
     public void printNonExistUser() {
