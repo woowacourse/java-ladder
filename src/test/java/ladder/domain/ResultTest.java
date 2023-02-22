e6c8288349814a46de0d6cd19670114cf6ea9031
cpot5620 <=cpot5620@naver.com>
2023-02-22 21:26:43 +0900

    test(ResultTest): @BeforeAll 적용

--- src/test/java/ladder/domain/ResultTest.java
+++ src/test/java/ladder/domain/ResultTest.java
@@ -1,40 +1,36 @@
 package ladder.domain;
 
 import ladder.util.TrueGenerator;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
 import java.util.List;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ResultTest {
     private Names names;
+    private List<String> bets;
 
-    @BeforeEach
+    @BeforeAll
     void setup() {
         names = new Names(
                 Arrays.asList("AAAA", "BBBB", "CCCC", "DDDD", "EEEE")
         );
+        bets = Arrays.asList("A", "B", "C", "D", "E");
     }
 
     @DisplayName("참여자 수와 내기 목록의 수가 같아야 한다.")
     @Test
     void createResultFailTestByDifferentCountOfNamesAndBet() {
-        List<String> bets = Arrays.asList("꽝", "꽝", "당첨");
-
         Assertions.assertThrows(IllegalArgumentException.class,
-                () -> new Result(names, bets));
+                () -> new Result(names, Arrays.asList("꽝", "꽝", "당첨")));
     }
 
     @DisplayName("참여자 수와 내기 목록의 수가 같아야 한다.")
     @Test
     void createResultSuccessTestByCountOfNamesAndBet() {
-        List<String> bets = Arrays.asList("꽝", "당첨", "꽝", "꽝", "당첨");
-
         Assertions.assertDoesNotThrow(() -> new Result(names, bets));
     }
 
@@ -42,7 +38,6 @@ public class ResultTest {
     @ValueSource(ints = {1, 3, 5, 7, 9, 11, 13})
     void performResultSuccessTest1(int height) {
         Ladder ladder = new Ladder(names.size(), new Height(height), new TrueGenerator());
-        List<String> bets = Arrays.asList("A", "B", "C", "D", "E"); // 초기 내기 목록 입력
         Result result = new Result(names, bets);
 
         result.perform(ladder); // ladder를 기반으로 내기 목록 위치 변경
@@ -58,7 +53,6 @@ public class ResultTest {
     @ValueSource(ints = {2, 4, 6, 8, 10, 12, 14})
     void performResultSuccessTest2(int height) {
         Ladder ladder = new Ladder(names.size(), new Height(height), new TrueGenerator());
-        List<String> bets = Arrays.asList("A", "B", "C", "D", "E"); // 초기 내기 목록 입력
         Result result = new Result(names, bets);
 
         result.perform(ladder); // ladder를 기반으로 내기 목록 위치 변경
