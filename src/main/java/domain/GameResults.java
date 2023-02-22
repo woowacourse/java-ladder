b7f68ffa0c6e7d7a64232ca210b481d80becfa7c
swonny <wonny921@gmail.com>
2023-02-22 15:32:22 +0900

    feat: 실행 결과를 일급컬렉션에 저장

--- src/main/java/domain/GameResults.java
+++ src/main/java/domain/GameResults.java
@@ -1,24 +1,32 @@
 package domain;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
 public class GameResults {
 
-    public GameResults(int playerSize, List<String> results) {
-        validateNullAndEmpty(results);
-        validateSize(playerSize, results);
+    private final List<GameResult> gameResults;
+
+    public GameResults(int playerSize, List<GameResult> gameResults) {
+        validateNullAndEmpty(gameResults);
+        validateSize(playerSize, gameResults);
+        this.gameResults = gameResults;
     }
 
-    private void validateNullAndEmpty(List<String> results) {
+    private void validateNullAndEmpty(List<GameResult> results) {
         if (Objects.isNull(results) || results.isEmpty()) {
             throw new IllegalArgumentException("실행 결과를 입력하세요.");
         }
     }
 
-    private static void validateSize(int playerSize, List<String> results) {
+    private static void validateSize(int playerSize, List<GameResult> results) {
         if (results.size() != playerSize) {
             throw new IllegalArgumentException("실행 결과 개수는 플레이어 수와 일치해야 합니다.");
         }
+    }
+
+    public List<GameResult> getGameResults() {
+        return Collections.unmodifiableList(gameResults);
     }
 }
--- src/test/java/domain/GameResultsTest.java
+++ src/test/java/domain/GameResultsTest.java
@@ -2,13 +2,13 @@ package domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.*;
 
 public class GameResultsTest {
 
@@ -17,9 +17,9 @@ public class GameResultsTest {
     @ParameterizedTest
     @CsvSource(value = {"1:2", "10:11", "11:10", "19:20", "20:21"}, delimiter = ':')
     void validateResultSize(String playerSize, String resultsSize) {
-        List<String> results = new ArrayList<>();
+        List<GameResult> results = new ArrayList<>();
         for (int resultIndex = 0; resultIndex < Integer.parseInt(resultsSize); resultIndex++) {
-            results.add(String.valueOf(resultIndex));
+            results.add(new GameResult(String.valueOf(resultIndex)));
         }
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> new GameResults(Integer.parseInt(playerSize), results));
@@ -31,18 +31,45 @@ public class GameResultsTest {
     void validateNullAndEmpty(List<String> results) {
         int playerSize = 3;
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> new GameResults(playerSize, results));
+                .isThrownBy(() -> new GameResults(
+                        playerSize,
+                        results.stream().map(GameResult::new).collect(Collectors.toList()))
+                );
     }
 
-//    @DisplayName("실행 결과를 일급컬렉션에 저장한다.")
-//    @Test
-//    void validateResultSize(String playerSize, String resultsSize) {
-//        List<String> results = new ArrayList<>();
-//        for (int resultIndex = 0; resultIndex < Integer.parseInt(resultsSize); resultIndex++) {
-//            results.add(String.valueOf(resultIndex));
-//        }
-//        assertThatIllegalArgumentException()
-//                .isThrownBy(() -> new GameResults(Integer.parseInt(playerSize), results));
+    @DisplayName("실행 결과를 일급컬렉션에 저장한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 10, 20})
+    void generateGameResults(int playerSize) {
+        List<GameResult> gameResults = new ArrayList<>();
+        for (int resultIndex = 0; resultIndex < playerSize; resultIndex++) {
+            gameResults.add(new GameResult("꽝"));
+        }
+        assertThatNoException()
+                .isThrownBy(() -> new GameResults(playerSize, gameResults));
+    }
+
+    @DisplayName("저장한 실행 결과명을 가져올 수 있다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 10, 20})
+    void getGameResults(int playerSize) {
+        List<GameResult> gameResultList = new ArrayList<>();
+        for (int resultIndex = 0; resultIndex < playerSize; resultIndex++) {
+            gameResultList.add(new GameResult("꽝"));
+        }
+        GameResults gameResults = new GameResults(playerSize, gameResultList);
+        assertThat(gameResults.getGameResults().get(0))
+                .isInstanceOf(GameResult.class);
+    }
+
+//    private static Stream<Arguments> provideStringsForIsBlank() {
+//        return Stream.of(
+//                Arguments.of(1, List.of(new GameResult("꽝"))),
+//                Arguments.of(10, List.of(new GameResult("꽝"), new GameResult("2000"), new GameResult("3000")),
+//                Arguments.of(3, List.of(new GameResult("꽝"), new GameResult("2000"), new GameResult("3000")),
+//                Arguments.of("  ", true),
+//                Arguments.of("not blank", false)
+//        );
 //    }
 
 }
