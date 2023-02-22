3eb3247deaa1f5d89036561d4a6be0e39a4c2016
ChoiYoungHoon <dudgns2865@naver.com>
2023-02-22 15:57:41 +0900

    test: 사다리에서 현위치가 들어왔을 때 위치를 움직이는 경우 테스트 코드 구현

--- src/test/java/domain/model/LayerTest.java
+++ src/test/java/domain/model/LayerTest.java
@@ -3,6 +3,7 @@ package domain.model;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import domain.type.Line;
+import domain.vo.Location;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
@@ -11,28 +12,11 @@ import org.junit.jupiter.api.Test;
 
 public class LayerTest {
 
-    private static class TrueTestPassGenerator implements PassGenerator {
-
-        @Override
-        public boolean generate() {
-            return true;
-        }
-    }
-
-    private static class FalseTestPassGenerator implements PassGenerator {
-
-        @Override
-        public boolean generate() {
-            return false;
-        }
-    }
-
-
     @Test
     @DisplayName("언제나 PassGenerator가 true를 반환할 때 Line 생성을 테스트한다.")
     public void makeLineWithAlwaysTrue() {
         //given
-        Layer layer = new Layer(new ArrayList<>(), new TrueTestPassGenerator());
+        Layer layer = new Layer(new ArrayList<>(), () -> true);
 
         //when
         IntStream.range(0, 10).forEach(i -> layer.makeLine());
@@ -47,7 +31,7 @@ public class LayerTest {
     @DisplayName("언제나 PassGenerator가 false를 반환할 때 Line 생성을 테스트한다.")
     public void makeLineWithAlwaysFalse() {
         //given
-        Layer layer = new Layer(new ArrayList<>(), new FalseTestPassGenerator());
+        Layer layer = new Layer(new ArrayList<>(), () -> false);
 
         //when
         IntStream.range(0, 10).forEach(i -> layer.makeLine());
@@ -69,5 +53,43 @@ public class LayerTest {
 
         //then
         assertThat(layer.getLines().size()).isEqualTo(size);
+    }
+
+    @Test
+    @DisplayName("사다리에서 현위치가 들어왔을 때 위치를 움직이는 경우1")
+    public void moveSuccessCase1() {
+        //given
+        Layer layer = new Layer(new ArrayList<>(), () -> true);
+        int size = 10;
+        IntStream.range(0, size).forEach(i -> layer.makeLine());
+        int horizon = 0;
+        int vertical = 0;
+        Location location = new Location(horizon, vertical);
+
+        //when
+        layer.move(location);
+
+        //then
+        assertThat(location.getHorizon()).isEqualTo(++horizon);
+        assertThat(location.getVertical()).isEqualTo(++vertical);
+    }
+
+    @Test
+    @DisplayName("사다리에서 현위치가 들어왔을 때 위치를 움직이는 경우2")
+    public void moveSuccessCase2() {
+        //given
+        Layer layer = new Layer(new ArrayList<>(), () -> false);
+        int size = 10;
+        IntStream.range(0, size).forEach(i -> layer.makeLine());
+        int horizon = 0;
+        int vertical = 0;
+        Location location = new Location(horizon, vertical);
+
+        //when
+        layer.move(location);
+
+        //then
+        assertThat(location.getHorizon()).isEqualTo(horizon);
+        assertThat(location.getVertical()).isEqualTo(++vertical);
     }
 }
