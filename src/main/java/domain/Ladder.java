53f5e33fadefd80abdf719749008701d9e79bb31
Youngho <cyh6099@gmail.com>
2023-02-23 00:11:35 +0900

    feat : 전체 사다리 게임 진행 후 map에 결과를 저장하는 로직 구현

--- src/main/java/domain/Ladder.java
+++ src/main/java/domain/Ladder.java
@@ -1,7 +1,9 @@
 package domain;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Ladder {
     private final Height height;
@@ -12,6 +14,25 @@ public class Ladder {
         this.height = height;
         this.width = width;
         this.ladder = randomLadderGenerator.generate(width, height);
+    }
+
+    public Map<String, Integer> play(Users users) {
+        int startPosition = 0;
+        Map<String, Integer> userResult = new HashMap<>();
+        for (User user : users.getUsers()) {
+            int finalPosition = startPosition;
+            finalPosition = calculateFinalPosition(finalPosition);
+            userResult.put(user.getName(), finalPosition);
+            startPosition++;
+        }
+        return userResult;
+    }
+
+    private int calculateFinalPosition(int finalPosition) {
+        for (Line line : ladder) {
+            finalPosition = line.calculateNextPosition(finalPosition);
+        }
+        return finalPosition;
     }
 
     public List<Line> getLadder() {
