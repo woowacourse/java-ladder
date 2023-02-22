67e9b0c2a6190387c9b72cf9eff5b57b466eee6d
MoonJeWoong <munjin0201@naver.com>
2023-02-22 09:35:33 +0900

    test: 기존 기능 변경으로 인한 테스트 코드 삭제 및 추가
    세부사항
    - 참여자의 이름을 통해 해당 참여자 정보를 검색하는 기능 테스트 코드 삭제
    - 사다리 라인에 따라 참여자 현재 위치를 바꾸는 기능 테스트 코드 추가

--- src/test/java/model/PlayersTest.java
+++ src/test/java/model/PlayersTest.java
@@ -2,15 +2,13 @@ package model;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatNoException;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 import java.util.List;
 
 public class PlayersTest {
-    private static final String NO_PLAYER_NAME_ERROR = "[ERROR] 해당 이름의 플레이어는 존재하지 않습니다.";
-
     @Test
     @DisplayName("Players 객체 생성 성공 테스트")
     void createPlayersTest() {
@@ -31,27 +29,22 @@ public class PlayersTest {
     }
 
     @Test
-    @DisplayName("참여자의 이름을 통해 해당 참여자의 정보를 검색하는 기능 테스트")
-    void findPlayerByNameTest() {
+    @DisplayName("사다리 라인에 따라 참여자 현재 위치를 바꾸는 기능 테스트")
+    void moveAllPlayersByLinePointsTest() {
         //given
-        Players players = new Players(NameFactory.create("pobi, neo, hiiro"));
+        Players players = new Players(NameFactory.create("pobi, neo, hiiro, ocean"));
+        List<Boolean> firstPoints = List.of(true, false, true);
+        List<Boolean> secondPoints = List.of(false, true, false);
+        List<Boolean> thirdPoints = List.of(true, false, false);
+        List<Boolean> fourthPoints = List.of(true, false, true);
 
         //when
-        Player result = players.findPlayerByName(new Name("hiiro"));
+        players.moveAllPlayersByLinePoints(firstPoints);
+        players.moveAllPlayersByLinePoints(secondPoints);
+        players.moveAllPlayersByLinePoints(thirdPoints);
+        players.moveAllPlayersByLinePoints(fourthPoints);
 
         //then
-        assertThat(result.getName()).isEqualTo("hiiro");
-    }
-
-    @Test
-    @DisplayName("존재하지 않는 이름을 검색한 경우 예외 처리하는 기능 테스트")
-    void throwExceptionWhenFindWithNoNameTest() {
-        //given
-        Players players = new Players(NameFactory.create("pobi, neo, hiiro"));
-
-        //then
-        assertThatThrownBy(() -> players.findPlayerByName(new Name("abcde")))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(NO_PLAYER_NAME_ERROR);
+        assertThat(players.getAllNamesOrderedByPosition()).isEqualTo(List.of("neo", "ocean", "hiiro", "pobi"));
     }
 }
