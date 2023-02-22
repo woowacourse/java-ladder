8454c951efb70e2b5fd006e4efc883e8a95a6109
echo <echo.backend@gmail.com>
2023-02-22 16:46:50 +0900

    test(Result): Result 테스트 추가

--- src/test/java/domain/ResultTest.java
+++ src/test/java/domain/ResultTest.java
@@ -1,10 +1,10 @@
 package domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class ResultTest {
     @Test
@@ -12,5 +12,21 @@ public class ResultTest {
     void generateValidResult() {
         Result result = Result.from("1000");
         assertThat(result.getValue()).isEqualTo("1000");
+    }
+
+    @Test
+    @DisplayName("결과가 1자 미만이면 예외가 발생한다.")
+    void generateInvalidResult1() {
+        assertThatThrownBy(() -> {
+            Result.from("");
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("결과가 5자 이상이면 예외가 발생한다.")
+    void generateInvalidResult2() {
+        assertThatThrownBy(() -> {
+            Result.from("500000");
+        }).isInstanceOf(IllegalArgumentException.class);
     }
 }
