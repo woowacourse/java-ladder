a0fdb52d9bbaafb454623da34abffd5bc0ccba6f
swonny <wonny921@gmail.com>
2023-02-22 14:50:31 +0900

    test: 경계값 테스트 추가

--- src/test/java/domain/GameResultsTest.java
+++ src/test/java/domain/GameResultsTest.java
@@ -2,7 +2,7 @@ package domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,14 +14,13 @@ public class GameResultsTest {
     // TODO : 실행 결과 개수와 플레이어 수를 비교하는 것이 Result 객체의 책임인지 생각해보기
     @DisplayName("실행 결과의 개수가 플레이어 수보다 적으면 예외 처리 한다.")
     @ParameterizedTest
-    @ValueSource(ints = {1, 10, 20})
-    void validateResultSize(int playerSize) {
+    @CsvSource(value = {"1:2", "10:11", "11:10", "19:20", "20:21"}, delimiter = ':')
+    void validateResultSize(String playerSize, String resultsSize) {
         List<String> results = new ArrayList<>();
-        int sizeDifference = 5;
-        for (int playerIndex = 0; playerIndex < playerSize - sizeDifference; playerIndex++) {
-            results.add(String.valueOf(playerIndex));
+        for (int resultIndex = 0; resultIndex < Integer.parseInt(resultsSize); resultIndex++) {
+            results.add(String.valueOf(resultIndex));
         }
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> new GameResults(playerSize, results));
+                .isThrownBy(() -> new GameResults(Integer.parseInt(playerSize), results));
     }
 }
