c99ac8403ac993e04bacf2b5670b3231fd6c035c
ingpyo <juninpyo726@gmail.com>
2023-02-22 16:55:34 +0900

    feat: 사다리 좌표의 따른 플레이어 이동 기능

--- src/main/java/domain/LadderGame.java
+++ src/main/java/domain/LadderGame.java
@@ -1,17 +1,36 @@
 package domain;
 
+import domain.ladder.Height;
 import domain.ladder.Ladder;
+import domain.player.Player;
 import domain.player.Players;
+import domain.product.Product;
 import domain.product.Products;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LadderGame {
     Products products;
     Players players;
     Ladder ladder;
+    Height height;
 
-    public LadderGame(Players players, Products products, Ladder ladder) {
+    public LadderGame(Players players, Products products, Ladder ladder, Height height) {
+        this.height = height;
         this.players = players;
         this.products = products;
         this.ladder = ladder;
     }
+
+    public int climbTheLadderPlayerPosition(int playerPosition,int floor) {
+        if (playerPosition == 0) {
+            return ladder.sideDecideWhereToGo(playerPosition, floor);
+        }
+        if (playerPosition == players.getPlayersCount() - 1) {
+            return ladder.sideDecideWhereToGo(playerPosition,floor);
+        }
+        return ladder.decideWhereToGo(playerPosition, floor);
+    }
+
 }
--- src/test/java/domain/LadderGameTest.java
+++ src/test/java/domain/LadderGameTest.java
@@ -1,27 +1,52 @@
 package domain;
 
+import domain.ladder.Height;
 import domain.ladder.Ladder;
 import domain.ladder.Line;
 import domain.player.Player;
 import domain.player.Players;
 import domain.product.Product;
 import domain.product.Products;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LadderGameTest {
+
+    LadderGame ladderGame;
+
+    @Test
+    @BeforeEach
+    @DisplayName("사다리 게임의 필요한 객체 저장")
+    void setUpLadderGame() {
+        Products products = new Products(List.of(new Product("사과"), new Product("꽝"), new Product("1000")));
+        Players players = new Players(List.of(new Player("a"),
+                new Player("b"), new Player("c")));
+        Ladder ladder = new Ladder(List.of(new Line(List.of(new Point(false), new Point(true), new Point(false)))));
+        Height height = new Height(1);
+        ladderGame = new LadderGame(players, products, ladder, height);
+    }
+
     @Test
     @DisplayName("사다리 게임의 필요한 객체 저장")
     void createLadderGame() {
         Products products = new Products(List.of(new Product("사과"), new Product("꽝")));
         Players players = new Players(List.of(new Player("a"), new Player("b")));
         Ladder ladder = new Ladder(List.of(new Line(List.of(new Point(false), new Point(true), new Point(false)))));
+        Height height = new Height(1);
         assertDoesNotThrow(() -> {
-            LadderGame ladderGame = new LadderGame(players, products, ladder);
+            LadderGame ladderGame = new LadderGame(players, products, ladder, height);
         });
+    }
+
+    @Test
+    @DisplayName("사다리 좌표의 따른 플레이어 이동")
+    void climbTheLadderPlayerPosition() {
+        assertThat(ladderGame.climbTheLadderPlayerPosition(0, 0)).isEqualTo(0);
     }
 }
