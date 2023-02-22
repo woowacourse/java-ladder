acaba162edf5f5b27229255d33dcef1cee06ea3e
aiaiaiai1 <cvc100326@naver.com>
2023-02-22 13:49:14 +0900

    test: 좌표를 한칸씩 옮기는 기능 테스트

---
+++ src/test/java/domain/PositionTest.java
@@ -0,0 +1,47 @@
+package domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PositionTest {
+    @DisplayName("위치값 생성하기")
+    @Test
+    void create_position() {
+        assertDoesNotThrow(() -> new Position(0, 0));
+    }
+
+    @DisplayName("아래로 내려가기")
+    @Test
+    void move_down() {
+        Position position = new Position(0, 0);
+        position.moveDown();
+        Position result = new Position(0, 1);
+
+        assertThat(position).usingRecursiveComparison().isEqualTo(result);
+    }
+
+    @DisplayName("왼쪽으로 가기")
+    @Test
+    void move_left() {
+        Position position = new Position(0, 0);
+        position.moveLeft();
+        Position result = new Position(-1, 0);
+
+        assertThat(position).usingRecursiveComparison().isEqualTo(result);
+    }
+
+    @DisplayName("오른쪽으로 가기")
+    @Test
+    void move_right() {
+        Position position = new Position(0, 0);
+        position.moveRight();
+        Position result = new Position(1, 0);
+
+        assertThat(position).usingRecursiveComparison().isEqualTo(result);
+    }
+
+
+}
