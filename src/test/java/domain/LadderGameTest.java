9683b8083f6a5a2eecfec9cf177501dd11ce609b
somsom13 <somin1113@naver.com>
2023-02-22 11:18:23 +0900

    test: 게임 참여자 별 당첨 결과 계산 테스트

--- src/test/java/domain/LadderGameTest.java
+++ src/test/java/domain/LadderGameTest.java
@@ -3,6 +3,7 @@ package domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -14,6 +15,7 @@ class LadderGameTest {
     private LadderGame ladderGame;
     private static Persons persons;
     private static Height height;
+    private static WinningEntry winningEntry;
 
     @BeforeAll
     static void setUp() {
@@ -22,31 +24,44 @@ class LadderGameTest {
                 .map(Person::new)
                 .collect(Collectors.toList()));
         height = new Height(5);
+        winningEntry = new WinningEntry(List.of("0", "1", "2", "3", "4"), 5);
     }
 
     @Test
     @DisplayName("항상 true 인 값이 생성될 때, 사다리 이동을 테스트한다")
     void moveWhenLadderAlwaysTrue() {
         //given
-        ladderGame = new LadderGame(persons, height, new TrueGenerator());
+        ladderGame = new LadderGame(persons, height, winningEntry, new TrueGenerator());
+        Map<String, String> expectedResult = Map.of("pobi", "1",
+                "crong", "0",
+                "honux", "3",
+                "jk", "2",
+                "abcd", "4");
 
         //when
-        List<Integer> afterMovePersonIndex = ladderGame.play();
+        Map<String, String> result = ladderGame.play();
 
         //then
-        assertThat(afterMovePersonIndex).containsExactly(1, 0, 3, 2, 4);
+        assertThat(result).containsExactlyInAnyOrderEntriesOf(expectedResult);
+        assertThat(result.values()).containsExactly("1", "0", "3", "2", "4");
     }
 
     @Test
     @DisplayName("항상 false 인 값이 생성될 때, 사다리 이동을 테스트한다")
     void moveWhenLadderAlwaysFalse() {
         //given
-        ladderGame = new LadderGame(persons, height, new FalseGenerator());
+        ladderGame = new LadderGame(persons, height, winningEntry, new FalseGenerator());
+        Map<String, String> expectedResult = Map.of("pobi", "0",
+                "crong", "1",
+                "honux", "2",
+                "jk", "3",
+                "abcd", "4");
 
         //when
-        List<Integer> afterMovePersonIndex = ladderGame.play();
+        Map<String, String> result = ladderGame.play();
 
         //then
-        assertThat(afterMovePersonIndex).containsExactly(0, 1, 2, 3, 4);
+        assertThat(result).containsExactlyInAnyOrderEntriesOf(expectedResult);
+        assertThat(result.values()).containsExactly("0", "1", "2", "3", "4");
     }
 }
