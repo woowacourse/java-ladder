b888fa24ad26df7db1486c7c682396e759c488f0
eunbii0213 <eunbii0213@naver.com>
2023-02-22 14:40:51 +0900

    feat: Reward 값 객체 생성

---
+++ src/main/java/domain/Reward.java
@@ -0,0 +1,13 @@
+package domain;
+
+public class Reward {
+    private String reward;
+
+    public Reward(String reward) {
+        this.reward = reward;
+    }
+
+    public String getReward() {
+        return reward;
+    }
+}
