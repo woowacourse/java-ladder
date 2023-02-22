d402d76f4811cfc983c720e4fee670cc1493f4f2
yeonkkk <tjddus0302@gmail.com>
2023-02-22 21:53:16 +0900

    refactor: 클래스명 수정에 따른 변수명, 메소드명 변경

--- src/test/java/domain/RewardsTest.java
+++ src/test/java/domain/RewardsTest.java
@@ -9,20 +9,20 @@ import org.junit.jupiter.api.Test;
 @DisplayName("입력된 실행 결과의 수는 ")
 class RewardsTest {
 
-    private final List<String> results = List.of("꽝", "5000", "3000", "꽝");
+    private final List<String> rewards = List.of("꽝", "5000", "3000", "꽝");
 
     @DisplayName("참여자의 수와 같아야 한다.")
     @Test
-    void resultSizeTest_success() {
-        Assertions.assertDoesNotThrow(
-            () -> Rewards.of(results, results.size()));
+    void rewardSizeTest_success() {
+        assertDoesNotThrow(
+            () -> Rewards.of(rewards, rewards.size()));
     }
 
     @DisplayName("참여자의 수와 다르면 예외가 발생한다.")
     @Test
-    void resultSizeTest_fail() {
-        Assertions.assertThrows(
+    void rewardSizeTest_fail() {
+        assertThrows(
             IllegalArgumentException.class,
-            () -> Rewards.of(results, results.size() + 1));
+            () -> Rewards.of(rewards, rewards.size() + 1));
     }
 }
