f493a16d44d937107f194b03085bd882db952690
ChoiYoungHoon <dudgns2865@naver.com>
2023-02-22 16:02:22 +0900

    test: 사다리에서 타기 구현

--- src/main/java/domain/model/Ladder.java
+++ src/main/java/domain/model/Ladder.java
@@ -1,12 +1,14 @@
 package domain.model;
 
 import domain.vo.Height;
+import domain.vo.Location;
 import domain.vo.Width;
 import java.util.List;
 import java.util.stream.IntStream;
 
 public class Ladder {
 
+    private static final String START_LOCATION_NOT_EXIST = "사다리에 존재하지 않는 시작 시점입니다.";
     private final Height height;
     private final Width width;
     private final List<Layer> layers;
@@ -26,7 +28,20 @@ public class Ladder {
                     .forEach(j -> layer.makeLine()));
     }
 
+    public int ride(final Location location) {
+        checkStartLocation(location);
+        IntStream.range(0, height.getValue())
+            .forEach(i -> layers.get(location.getVertical()).move(location));
+        return location.getHorizon();
+    }
+
     public List<Layer> getLayers() {
         return List.copyOf(layers);
+    }
+
+    private void checkStartLocation(final Location location) {
+        if (location.getHorizon() > width.getValue() + 1) {
+            throw new IllegalArgumentException(START_LOCATION_NOT_EXIST);
+        }
     }
 }
