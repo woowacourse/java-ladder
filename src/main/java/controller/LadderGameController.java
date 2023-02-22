1b76f30be2531d800664564bb469c6b6f8a6d8ee
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-02-23 07:37:19 +0900

    refactor: 메서드 명으로 변경
    map -> ladder

--- src/main/java/controller/LadderGameController.java
+++ src/main/java/controller/LadderGameController.java
@@ -8,49 +8,45 @@ import view.output.OutputView;
 public class LadderGameController {
 
     public void play(InputView inputView, OutputView outputView, BooleanGenerator booleanGenerator) {
-        Participants participants = makeParticipants(inputView);
-        Results results = makeResults(inputView, participants);
-        Ladder ladder = generateMap(inputView, participants, booleanGenerator);
-        showMap(outputView, participants, ladder, results);
+        Participants participants = setParticipants(inputView);
+        Results results = setResults(inputView, participants);
+        Ladder ladder = generateLadder(inputView, participants, booleanGenerator);
+        printLadder(outputView, participants, ladder, results);
     }
 
-    private Participants makeParticipants(InputView inputView) {
+    private Participants setParticipants(InputView inputView) {
         try {
             String participantNames = inputView.enterParticipantNames();
             return new Participants(participantNames);
         } catch (IllegalArgumentException exception) {
             inputView.printErrorMessage(exception);
-            return makeParticipants(inputView);
+            return setParticipants(inputView);
         }
     }
 
-    private Results makeResults(InputView inputView, Participants participants) {
+    private Results setResults(InputView inputView, Participants participants) {
         try {
             String results = inputView.enterResults();
             return new Results(results, participants.getParticipantCount());
         } catch (IllegalArgumentException exception) {
             inputView.printErrorMessage(exception);
-            return makeResults(inputView, participants);
+            return setResults(inputView, participants);
         }
     }
 
-    private Ladder generateMap(InputView inputView, Participants participants, BooleanGenerator booleanGenerator) {
+    private Ladder generateLadder(InputView inputView, Participants participants, BooleanGenerator booleanGenerator) {
         try {
             Height height = new Height(inputView.enterHeight());
             Weight weight = new Weight(participants.getParticipantCount());
             return new Ladder(height, weight, booleanGenerator);
         } catch (IllegalArgumentException exception) {
             inputView.printErrorMessage(exception);
-            return generateMap(inputView, participants, booleanGenerator);
+            return generateLadder(inputView, participants, booleanGenerator);
         }
     }
 
-    private void showMap(OutputView outputView, Participants participants, Ladder ladder,
-                         Results results) {
+    private void printLadder(OutputView outputView, Participants participants, Ladder ladder,
+                             Results results) {
         outputView.printMap(participants, ladder, results.get());
-    }
-
-    private void getResult(InputView inputView, OutputView outputView) {
-
     }
 }
