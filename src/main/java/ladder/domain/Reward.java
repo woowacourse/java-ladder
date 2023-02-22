80464b690fdcf1298f966ee7cc874774dc9d3a3c
DESKTOP-FNVN0HH\USER <beatus96@naver.com>
2023-02-22 17:26:48 +0900

    feat: Reward 도메인 생성

---
+++ src/main/java/ladder/domain/Reward.java
@@ -0,0 +1,27 @@
+package ladder.domain;
+
+import static ladder.domain.PlayerName.NAME_MAXIMUM_LENGTH;
+
+public class Reward {
+
+    public static final String ERROR_LENGTH_OF_NAME = String.format(
+            "보상은 %d자 이하여야 합니다.", NAME_MAXIMUM_LENGTH);
+    private final String reward;
+
+    public Reward(String reward) {
+        validateNameLength(reward);
+        this.reward = reward;
+    }
+
+    public String getName() {
+        return reward;
+    }
+
+    private void validateNameLength(String name) {
+        if (name.isBlank() || name.length() > NAME_MAXIMUM_LENGTH) {
+            throw new IllegalArgumentException(ERROR_LENGTH_OF_NAME);
+        }
+    }
+
+
+}
