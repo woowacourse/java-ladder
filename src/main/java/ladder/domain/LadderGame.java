8a643ff048d79bd69433a7ae9d8df336f21a78db
BGuga <tmdwns9913@gmail.com>
2023-02-22 16:17:06 +0900

    feat: 사다리 게임에 유저 이름을 입력하면 해당 유저의 보상을 알려주는 기능 구현

--- src/main/java/ladder/domain/LadderGame.java
+++ src/main/java/ladder/domain/LadderGame.java
@@ -3,9 +3,16 @@ package ladder.domain;
 public class LadderGame {
     private final Ladder ladder;
     private final Users users;
+    private final Reward reward;
 
-    public LadderGame(Ladder ladder, Users users) {
+    public LadderGame(Ladder ladder, Users users, Reward reward) {
         this.ladder = ladder;
         this.users = users;
+        this.reward = reward;
+    }
+
+    public String getRewardOf(final String userName) {
+        final int userOrder = users.getOrderOf(userName);
+        return reward.getRewardOf(ladder.resultPositionOf(userOrder));
     }
 }
--- src/test/java/ladder/domain/LadderGameTest.java
+++ src/test/java/ladder/domain/LadderGameTest.java
@@ -6,22 +6,32 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class LadderGameTest {
 
     private Users DEFAULT_USERS;
     private Ladder DEFAULT_LADDER;
+    private Reward DEFAULT_REWARD;
 
     @BeforeEach
     void setting() {
         DEFAULT_USERS = new Users(List.of("a", "b", "c"));
         DEFAULT_LADDER = new Ladder(3, DEFAULT_USERS);
+        DEFAULT_REWARD = new Reward(List.of("1", "2", "3"));
     }
 
     @Test
     @DisplayName("사다리 게임은 사다리와 유저들로 이루어진 게임이다.")
     void validLadderGameTest() {
-        assertDoesNotThrow(() -> new LadderGame(DEFAULT_LADDER, DEFAULT_USERS));
+        assertDoesNotThrow(() -> new LadderGame(DEFAULT_LADDER, DEFAULT_USERS, DEFAULT_REWARD));
+    }
+
+    @Test
+    @DisplayName("사다리 게임에 유저의 이름을 입력하면 해당 유저의 보상을 알려준다.")
+    void getRewardOfUserByBridgeGameTest() {
+        final LadderGame ladderGame = new LadderGame(DEFAULT_LADDER, DEFAULT_USERS, DEFAULT_REWARD);
+        assertThat(ladderGame.getRewardOf("a")).isEqualTo("1");
     }
 }
