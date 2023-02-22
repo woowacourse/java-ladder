ba5bf8334924cbe0d85952e1c4de059c1f93af84
dooboocookie <bkh3030@gmail.com>
2023-02-22 14:24:12 +0900

    test: getContinue 테스트 수정

--- src/test/java/ladder/domain/reward/ContinueTest.java
+++ src/test/java/ladder/domain/reward/ContinueTest.java
@@ -1,35 +1,45 @@
 package ladder.domain.reward;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 public class ContinueTest {
 
-    @ParameterizedTest(name = "input={0}")
+    @Test
     @DisplayName("getContinue 테스트")
-    @ValueSource(strings = {"y","n","Y","N"})
-    void createContinueTest(String inputContinue) {
-        assertDoesNotThrow(() -> Continue.getContinue(inputContinue));
+    void getContinueTest() {
+        assertThat(Continue.getContinue("y")).isEqualTo(Continue.CONTINUE);
+        assertThat(Continue.getContinue("Y")).isEqualTo(Continue.CONTINUE);
+        assertThat(Continue.getContinue("n")).isEqualTo(Continue.STOP);
+        assertThat(Continue.getContinue("N")).isEqualTo(Continue.STOP);
     }
 
     @ParameterizedTest(name = "input={0}")
-    @DisplayName("getContinue 생성 잘못된 문자 예외 테스트")
+    @DisplayName("getContinue 잘못된 문자 예외 테스트")
     @ValueSource(strings = {"x","1","o","no"})
-    void createContinueWrongValueExceptionTest(String inputContinue) {
+    void getContinueWrongValueExceptionTest(String inputContinue) {
         assertThatThrownBy(() -> Continue.getContinue(inputContinue))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @ParameterizedTest(name = "input={0}")
-    @DisplayName("Continue 생성 공백 문자 예외 테스트")
+    @DisplayName("getContinue 공백 문자 예외 테스트")
     @ValueSource(strings = {" ","     ",""})
-    void createContinueBlankExceptionTest(String inputContinue) {
+    void getContinueBlankExceptionTest(String inputContinue) {
         assertThatThrownBy(() -> Continue.getContinue(inputContinue))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("Continue getValue 테스트")
+    void getValueTest() {
+        assertThat(Continue.CONTINUE.getValue()).isEqualTo("Y");
+        assertThat(Continue.STOP.getValue()).isEqualTo("N");
     }
 
 }
