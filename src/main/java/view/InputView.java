bcd8a8631f0d55b65ac18d307d10efae28de6a63
2014012606 <2014012606@hanyang.ac.kr>
2023-02-22 14:14:07 +0900

    feat: 실행 결과를 입력받는 기능 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -9,6 +9,7 @@ public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
     private static final InputValidator inputValidator = new InputValidator();
     private static final String USER_NAMES_GUIDE_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    private static final String RESULT_NAMES_GUIDE_MESSAGE = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
     private static final String LADDER_HEIGHT_GUIDE_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
     private static final String DELIMITER = "\\s*,\\s*";
 
@@ -17,6 +18,14 @@ public class InputView {
         String names = scanner.nextLine();
         System.out.println();
         inputValidator.validateAscii(names);
+        return Arrays.stream(names.split(DELIMITER))
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    public static List<String> readResultNames() {
+        System.out.println(RESULT_NAMES_GUIDE_MESSAGE);
+        String names = scanner.nextLine();
+        System.out.println();
         return Arrays.stream(names.split(DELIMITER))
                 .collect(Collectors.toUnmodifiableList());
     }
