03120b396400bef74b08f3126c32722bc4aa4dff
yeonkkk <tjddus0302@gmail.com>
2023-02-22 22:04:38 +0900

    feat: 입력된 실행 결과의 공백을 제거하고 인덱스를 통해 특정 실행 결과를 조회하는 메서드 생성

--- src/main/java/domain/Rewards.java
+++ src/main/java/domain/Rewards.java
@@ -3,13 +3,17 @@ package domain;
 import static utils.ErrorMessage.INVALID_REWARDS_SIZE;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Rewards {
 
+    private final int FIRST_INDEX = 0;
     private final List<String> rewards;
 
     private Rewards(List<String> rewards) {
-        this.rewards = rewards;
+        this.rewards = rewards.stream()
+            .map(String::strip)
+            .collect(Collectors.toList());
     }
 
     public static Rewards of(List<String> rewards, int userCount) {
@@ -23,5 +27,12 @@ public class Rewards {
                 String.format(INVALID_REWARDS_SIZE.getMessage(),
                     userCount));
         }
+    }
+
+    public String getReward(int index) {
+        if (index < FIRST_INDEX || index >= rewards.size()) {
+            throw new IllegalArgumentException();
+        }
+        return rewards.get(index);
     }
 }
