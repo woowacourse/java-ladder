b6d47b6fdc258ae2c34e0d50d84fb8b7fd954f92
2014012606 <2014012606@hanyang.ac.kr>
2023-02-22 14:40:32 +0900

    test: 실행 결과 수가 참여자 수와 다르면 예외처리 확인

--- src/test/java/domain/ResultsTest.java
+++ src/test/java/domain/ResultsTest.java
@@ -1,5 +1,6 @@
 package domain;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -14,8 +15,8 @@ class ResultsTest {
 
     @ParameterizedTest(name = "입력된 참여자 = {0}, 입력된 실행 결과 = {1}")
     @DisplayName("입력된 실행 결과들의 수가 참여자의 수와 일치하는지 확인한다.")
-    @MethodSource("resultsAndUsersNumberDummy")
-    void resultsNumberSameUsersNumber(List<String> usersName, List<String> resultsName) {
+    @MethodSource("resultsAndUsersNumberSameDummy")
+    void resultsNumberSameWithUsersNumber(List<String> usersName, List<String> resultsName) {
         Users users = new Users(usersName);
         int usersNumber = users.getUsersName().size();
 
@@ -24,10 +25,27 @@ class ResultsTest {
         assertThat(results.getResultsName().size()).isEqualTo(usersNumber);
     }
 
-    static Stream<Arguments> resultsAndUsersNumberDummy() {
+    @ParameterizedTest(name = "입력된 참여자 = {0}, 입력된 실행 결과 = {1}")
+    @DisplayName("입력된 실행 결과들의 수가 참여자의 수와 일치하지 않으면 예외처리한다")
+    @MethodSource("resultsAndUsersNumberNotSameDummy")
+    void resultsNumberNotSameWithUsersNumber(List<String> usersName, List<String> resultsName) {
+        Users users = new Users(usersName);
+        int usersNumber = users.getUsersName().size();
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Results(resultsName, usersNumber));
+    }
+
+    static Stream<Arguments> resultsAndUsersNumberSameDummy() {
         return Stream.of(
                 Arguments.arguments(List.of("pobi"), List.of("꽝")),
                 Arguments.arguments(List.of("pobi", "crong", "honux", "jk"), List.of("꽝", "5000", "3000", "꽝"))
+        );
+    }
+
+    static Stream<Arguments> resultsAndUsersNumberNotSameDummy() {
+        return Stream.of(
+                Arguments.arguments(List.of("pobi", "crong", "honux"), List.of("꽝", "3000")),
+                Arguments.arguments(List.of("pobi"), List.of("꽝", "3000"))
         );
     }
 }
