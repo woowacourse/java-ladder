c3da7413823ec8957d85ec31ee394724e9d957ec
swonny <wonny921@gmail.com>
2023-02-22 14:21:07 +0900

    feat: 실행 결과 개수와 플레이어 수가 다른 경우 예외 처리

--- src/main/java/domain/GameResult.java
+++ src/main/java/domain/GameResult.java
@@ -1,11 +1,22 @@
 package domain;
 
+import java.util.List;
 import java.util.Objects;
 
 public class GameResult {
 
     public GameResult(String resultInput) {
         validateNullAndEmpty(resultInput);
+    }
+
+    public GameResult(int playerSize, List<String> results) {
+        validateResultSize(playerSize, results);
+    }
+
+    private void validateResultSize(int playerSize, List<String> results) {
+        if (results.size() != playerSize) {
+            throw new IllegalArgumentException("플레이어의 수와 실행 결과 수는 일치해야 합니다.");
+        }
     }
 
     private void validateNullAndEmpty(String resultInput) {
--- src/test/java/domain/GameResultTest.java
+++ src/test/java/domain/GameResultTest.java
@@ -3,12 +3,16 @@ package domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class GameResultTest {
 
-    @DisplayName("실행 결과과 공백일 예외처리 한다.")
+    @DisplayName("실행 결과과 공백일 예외 처리 한다.")
     @ParameterizedTest
     @NullAndEmptySource
     void validateNullAndEmpty(String resultInput) {
@@ -16,5 +20,17 @@ public class GameResultTest {
                 .isThrownBy(() -> new GameResult(resultInput));
     }
 
-
+    // TODO : 실행 결과 개수와 플레이어 수를 비교하는 것이 Result 객체의 책임인지 생각해보기
+    @DisplayName("실행 결과의 개수가 플레이어 수보다 적으면 예외 처리 한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 10, 20})
+    void validateResultSize(int playerSize) {
+        List<String> results = new ArrayList<>();
+        int sizeDifference = 5;
+        for (int playerIndex = 0; playerIndex < playerSize - sizeDifference; playerIndex++) {
+            results.add(String.valueOf(playerIndex));
+        }
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new GameResult(playerSize, results));
+    }
 }
