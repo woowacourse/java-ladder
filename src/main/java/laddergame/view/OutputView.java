927548accd3a6f8f8594d0efd4a431e73b917aa3
Rosie <dev.kayoung@gmail.com>
2023-02-22 22:11:14 +0900

    feat: 실행결과 출력 기능

--- src/main/java/laddergame/view/OutputView.java
+++ src/main/java/laddergame/view/OutputView.java
@@ -1,10 +1,28 @@
 package laddergame.view;
 
 
+import java.util.Map;
+import laddergame.domain.LadderResultItem;
+import laddergame.domain.NamesWithMatchedResult;
+import laddergame.domain.PersonalName;
+
 public class OutputView {
-    public void printResult(final String ladderFrom) {
-        System.out.println("실행결과");
+    public void printLadderForm(final String ladderFrom) {
+        System.out.println("사다리결과");
         System.out.println(ladderFrom);
+    }
+
+    public void printResult(final LadderResultItem item) {
+        System.out.println("실행결과");
+        System.out.println(item.getName());
+    }
+
+    public void printTotalResult(final NamesWithMatchedResult namesWithMatchedResult) {
+        System.out.println("실행결과");
+        final Map<PersonalName, LadderResultItem> nameToItem = namesWithMatchedResult.getNameToItem();
+        for (PersonalName name : nameToItem.keySet()) {
+            System.out.println(String.format("%s : %s", name.getValue(), nameToItem.get(name).getName()));
+        }
     }
 
     public static void printExceptionMessage(final String message) {
