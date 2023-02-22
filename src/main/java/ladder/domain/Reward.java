84e2c3d1e8333c039c7837c90133690d526be4f3
BGuga <tmdwns9913@gmail.com>
2023-02-22 15:53:40 +0900

    feat: reward의 범위를 넘는 값을 얻으려 한다면 예외가 발생한다.

--- src/main/java/ladder/domain/Reward.java
+++ src/main/java/ladder/domain/Reward.java
@@ -3,6 +3,7 @@ package ladder.domain;
 import java.util.List;
 
 public class Reward {
+    private static final String OUT_OF_INDEX_ITEM_ERROR_MESSAGE = "해당 위치의 아이템은 존재하지 않습니다.";
     private final List<String> items;
 
     public Reward(final List<String> items) {
@@ -10,6 +11,10 @@ public class Reward {
     }
 
     public String getRewardOf(final int i) {
-        return items.get(i);
+        try {
+            return items.get(i);
+        } catch (IndexOutOfBoundsException e) {
+            throw new IllegalArgumentException(OUT_OF_INDEX_ITEM_ERROR_MESSAGE, e);
+        }
     }
 }
