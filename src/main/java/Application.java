7b1064a0e74c15312c78fe7771d9eb4857ebb252
Youngho <cyh6099@gmail.com>
2023-02-23 02:10:22 +0900

    feat : ResultView추가에 따른 코드 변경

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,10 +1,11 @@
 import controller.Controller;
 import view.InputView;
 import view.OutputView;
+import view.ResultView;
 
 public class Application {
     public static void main(String[] args) {
-        Controller controller = new Controller(new InputView(), new OutputView());
+        Controller controller = new Controller(new InputView(), new OutputView(), new ResultView());
         controller.run();
     }
 }
