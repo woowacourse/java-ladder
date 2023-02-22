2fc8fe1f400394514d911595b88b39f7b168fd30
sh111-coder <ohk9134@naver.com>
2023-02-22 15:44:47 +0900

    test(LadderTest): 시작 위치를 받아서 최종 위치를 반환하는 테스트 추가

--- src/test/java/ladder/domain/LadderTest.java
+++ src/test/java/ladder/domain/LadderTest.java
@@ -1,7 +1,10 @@
 package ladder.domain;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.List;
 
@@ -9,25 +12,35 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class LadderTest {
 
+    private TrueGenerator trueGenerator = new TrueGenerator();
+    private FalseGenerator falseGenerator = new FalseGenerator();
+
+    class TrueGenerator implements BooleanGenerator {
+
+        @Override
+        public Boolean generate() {
+            return Boolean.TRUE;
+        }
+    }
+
+    class FalseGenerator implements BooleanGenerator {
+
+        @Override
+        public Boolean generate() {
+            return Boolean.FALSE;
+        }
+    }
 
     @Test
     @DisplayName("사다리가 TRUE 라인 수에 맞게 생성된다.")
     void generateTrueLadderTest() {
 
-        class IntendedBooleanGenerator implements BooleanGenerator {
-
-            @Override
-            public Boolean generate() {
-                return Boolean.TRUE;
-            }
-        }
-
         List<Line> lines = List.of(
-                new Line(4, new IntendedBooleanGenerator()),
-                new Line(4, new IntendedBooleanGenerator()),
-                new Line(4, new IntendedBooleanGenerator()),
-                new Line(4, new IntendedBooleanGenerator()),
-                new Line(4, new IntendedBooleanGenerator())
+                new Line(3, trueGenerator),
+                new Line(3, trueGenerator),
+                new Line(3, trueGenerator),
+                new Line(3, trueGenerator),
+                new Line(3, trueGenerator)
         );
 
         Ladder ladder = new Ladder(lines);
@@ -40,25 +53,49 @@ public class LadderTest {
     @DisplayName("사다리가 FALSE 라인 수에 맞게 생성된다.")
     void generateFalseLadderTest() {
 
-        class IntendedBooleanGenerator implements BooleanGenerator {
-
-            @Override
-            public Boolean generate() {
-                return Boolean.FALSE;
-            }
-        }
-
         List<Line> lines = List.of(
-                new Line(4, new IntendedBooleanGenerator()),
-                new Line(4, new IntendedBooleanGenerator()),
-                new Line(4, new IntendedBooleanGenerator()),
-                new Line(4, new IntendedBooleanGenerator()),
-                new Line(4, new IntendedBooleanGenerator())
+                new Line(3, falseGenerator),
+                new Line(3, falseGenerator),
+                new Line(3, falseGenerator),
+                new Line(3, falseGenerator),
+                new Line(3, falseGenerator)
         );
 
         Ladder ladder = new Ladder(lines);
         List<Line> copiedLines = ladder.getLines();
 
         assertThat(copiedLines.size()).isEqualTo(5);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"0,1", "1,0", "2,3", "3,2"})
+    @DisplayName("시작 위치를 받아서 사다리 최종 위치를 반환한다. (TrueGenerator)")
+    void moveToEndTrueGeneratorTest(int startIndex, int resultIndex) {
+        List<Line> lines = List.of(
+                new Line(3, trueGenerator),
+                new Line(3, trueGenerator),
+                new Line(3, trueGenerator),
+                new Line(3, trueGenerator),
+                new Line(3, trueGenerator)
+        );
+        Ladder ladder = new Ladder(lines);
+
+        Assertions.assertThat(ladder.moveToEnd(startIndex)).isEqualTo(resultIndex);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"0,0", "1,1", "2,2", "3,3"})
+    @DisplayName("시작 위치를 받아서 사다리 최종 위치를 반환한다. (FalseGenerator)")
+    void moveToEndFalseGeneratorTest(int startIndex, int resultIndex) {
+        List<Line> lines = List.of(
+                new Line(3, falseGenerator),
+                new Line(3, falseGenerator),
+                new Line(3, falseGenerator),
+                new Line(3, falseGenerator),
+                new Line(3, falseGenerator)
+        );
+        Ladder ladder = new Ladder(lines);
+
+        Assertions.assertThat(ladder.moveToEnd(startIndex)).isEqualTo(resultIndex);
     }
 }
