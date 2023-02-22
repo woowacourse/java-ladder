2c94a1609a5969806fe2152413198dfd15abc42d
jaeyeon kim <jakind@naver.com>
2023-02-22 19:32:03 +0900

    feat: 상품 입력을 받습니다. - domain.InputView requestRewards()

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -9,18 +9,19 @@ public class InputView {
 
     private static final int SPLIT_LIMIT_REMOVER = -1;
     private static final String PLAYER_NAME_INPUT_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    private static final String REWARDS_INPUT_MESSAGE = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
     private static final String LADDER_HEIGHT_INPUT_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
-    private static final String PLAYER_NAME_DELIMITER = ",";
+    private static final String DELIMITER = ",";
     private static final String NON_INTEGER_ERROR_MESSAGE = "높이는 정수만 입력 가능합니다.";
+    g
     private static final Scanner SCANNER = new Scanner(System.in);
 
     public List<String> requestPlayerNames() {
         String playerNamesInput = requestUserInput(PLAYER_NAME_INPUT_MESSAGE);
 
-        return Arrays.stream(playerNamesInput.split(PLAYER_NAME_DELIMITER, SPLIT_LIMIT_REMOVER))
-                .map(String::trim)
-                .collect(Collectors.toList());
+        return getSplitInput(playerNamesInput);
     }
+
 
     public int requestLadderHeight() {
         String heightInput = requestUserInput(LADDER_HEIGHT_INPUT_MESSAGE);
@@ -30,6 +31,18 @@ public class InputView {
         } catch (NumberFormatException exception) {
             throw new IllegalArgumentException(NON_INTEGER_ERROR_MESSAGE);
         }
+    }
+
+    public List<String> requestRewards() {
+        String rewardsInput = requestUserInput(REWARDS_INPUT_MESSAGE);
+
+        return getSplitInput(rewardsInput);
+    }
+
+    private List<String> getSplitInput(String inputToSplit) {
+        return Arrays.stream(inputToSplit.split(DELIMITER, SPLIT_LIMIT_REMOVER))
+                .map(String::trim)
+                .collect(Collectors.toList());
     }
 
     private String requestUserInput(String requestMessage) {
