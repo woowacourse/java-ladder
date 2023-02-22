fe5797b711f105ed2c626832f1ab4030fce12774
woo-chang <clllickme@naver.com>
2023-02-22 14:31:28 +0900

    feat: 사다리 결과 확인 대상 검증 기능 구현

---
+++ src/main/java/ladder/domain/Result.java
@@ -0,0 +1,41 @@
+package ladder.domain;
+
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Result {
+
+    private final Map<String, String> value;
+
+    public Result(final Map<String, String> value) {
+        this.value = value;
+    }
+
+    public static Result of(final Players players, final Ladder ladder, final Prizes prizes) {
+        final Map<String, String> result = new LinkedHashMap<>();
+        final List<String> playerNames = players.getPlayerNames();
+
+        for (int playerPosition = 0; playerPosition < playerNames.size(); playerPosition++) {
+            final int prizePosition = ladder.climb(playerPosition);
+            result.put(playerNames.get(playerPosition), prizes.check(prizePosition));
+        }
+        return new Result(result);
+    }
+
+    public String extract(final String key) {
+        validate(key);
+        return value.get(key);
+    }
+
+    private void validate(final String key) {
+        if (!value.containsKey(key)) {
+            throw new IllegalArgumentException("결과를 확인할 수 없는 대상입니다. 존재하는 대상 :" + value.keySet());
+        }
+    }
+
+    public Map<String, String> getValue() {
+        return Collections.unmodifiableMap(value);
+    }
+}
--- src/test/java/ladder/domain/ResultTest.java
+++ src/test/java/ladder/domain/ResultTest.java
@@ -3,62 +3,47 @@ package ladder.domain;
 import static ladder.domain.Direction.RIGHT;
 import static ladder.domain.Direction.STAY;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import java.util.Collections;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 import ladder.domain.generator.DirectionGenerator;
 import ladder.domain.generator.TestDirectionGenerator;
 import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ResultTest {
 
+    private Players players;
+    private Ladder ladder;
+    private Prizes prizes;
+
+    @BeforeEach
+    void setUp() {
+        players = Players.from(List.of("pobi", "crong"));
+        final Height height = new Height(2);
+        final DirectionGenerator directionGenerator = new TestDirectionGenerator(Lists.newArrayList(RIGHT, STAY));
+        ladder = Ladder.of(directionGenerator, players, height);
+        prizes = Prizes.from(List.of("꽝", "3000"));
+    }
+
     @Test
     @DisplayName("사다리 결과를 생성한다.")
     void generateResult() {
-        //given
-        final Players players = Players.from(List.of("pobi", "crong"));
-        final Height height = new Height(2);
-        final Prizes prizes = Prizes.from(List.of("꽝", "3000"));
-        final DirectionGenerator directionGenerator = new TestDirectionGenerator(Lists.newArrayList(RIGHT, STAY));
-        final Ladder ladder = Ladder.of(directionGenerator, players, height);
-
-        //when
         final Result result = Result.of(players, ladder, prizes);
 
-        //then
         assertThat(result.extract("pobi")).isEqualTo("3000");
         assertThat(result.extract("crong")).isEqualTo("꽝");
     }
-}
 
-class Result {
+    @Test
+    @DisplayName("존재하지 않는 대상이라면 예외를 던진다.")
+    void throwExceptionWhenNotExistTarget() {
+        final String target = "honux";
+        final Result result = Result.of(players, ladder, prizes);
 
-    private final Map<String, String> value;
-
-    public Result(final Map<String, String> value) {
-        this.value = value;
-    }
-
-    public static Result of(final Players players, final Ladder ladder, final Prizes prizes) {
-        final Map<String, String> result = new LinkedHashMap<>();
-        final List<String> playerNames = players.getPlayerNames();
-
-        for (int position = 0; position < playerNames.size(); position++) {
-            final int prizePosition = ladder.climb(position);
-            result.put(playerNames.get(position), prizes.check(prizePosition));
-        }
-        return new Result(result);
-    }
-
-    public String extract(final String key) {
-        return value.get(key);
-    }
-
-    public Map<String, String> getValue() {
-        return Collections.unmodifiableMap(value);
+        assertThatThrownBy(() -> result.extract(target))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
