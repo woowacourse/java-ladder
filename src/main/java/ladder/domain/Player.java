fbabad14f28953d45676b25df0cc137e379ab397
sh111-coder <ohk9134@naver.com>
2023-02-22 17:19:01 +0900

    refactor(Player): 시작 위치 원시 값 포장 클래스 StartIndex로 포장

--- src/main/java/ladder/domain/Player.java
+++ src/main/java/ladder/domain/Player.java
@@ -3,14 +3,18 @@ package ladder.domain;
 public class Player {
 
     private final Name name;
-    private final int startIndex;
+    private final StartIndex startIndex;
 
-    public Player(final Name name, final int startIndex) {
+    public Player(final Name name, final StartIndex startIndex) {
         this.name = name;
         this.startIndex = startIndex;
     }
 
-    public Name getName() {
-        return name;
+    public String getName() {
+        return name.getRawName();
+    }
+
+    public int getStartIndex() {
+        return startIndex.getRawStartIndex();
     }
 }
--- src/test/java/ladder/domain/PlayerTest.java
+++ src/test/java/ladder/domain/PlayerTest.java
@@ -12,6 +12,6 @@ public class PlayerTest {
     @CsvSource(value = {"seong,0", "io,1", "odo,2", "jay,3"})
     @DisplayName("이름과 시작 인덱스로 참여자가 생성된다.")
     void generatePlayerTest(String name, int startIndex) {
-        Assertions.assertDoesNotThrow(() -> new Player(new Name(name), startIndex));
+        Assertions.assertDoesNotThrow(() -> new Player(new Name(name), new StartIndex(startIndex)));
     }
 }
