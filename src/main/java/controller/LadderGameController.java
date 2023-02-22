ee0899f8a8daa05fc809ab6bdf0c6b7c1dcb6415
MoonJeWoong <munjin0201@naver.com>
2023-02-22 21:19:36 +0900

    feat: 컨트롤러에 사다리 게임 진행을 완료하는 기능 추가

--- src/main/java/controller/LadderGameController.java
+++ src/main/java/controller/LadderGameController.java
@@ -2,6 +2,7 @@ package controller;
 
 import model.Ladder;
 import model.LadderHeight;
+import model.Line;
 import model.Name;
 import model.Players;
 import model.Result;
@@ -9,6 +10,7 @@ import view.InputView;
 import view.OutputView;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class LadderGameController {
     public InputView inputView;
@@ -26,6 +28,16 @@ public class LadderGameController {
         outputView.printAllPlayerNames(players.getAllPlayerNames());
         outputView.printLadder(ladder);
         outputView.printAllResults(results);
+        playGame(players, ladder);
+    }
+
+    private void playGame(Players players, Ladder ladder) {
+        IntStream.range(0, ladder.getHeight())
+                .forEach(index -> playOneLine(players, ladder.getLine(index)));
+    }
+
+    private void playOneLine(Players players, Line line) {
+        players.moveAllPlayersByLinePoints(line.getPoints());
     }
 
     private List<Result> setResults(int playerCount) {
