6086e058c12c437f2379241d491b574e815c336d
yeonkkk <tjddus0302@gmail.com>
2023-02-22 22:03:31 +0900

    test: 실행 결과 목록의 사이즈 이상의 값이나 0 미만의 인덱스로 조회 시 예외를 발생시키는 테스트

--- src/test/java/domain/RewardsTest.java
+++ src/test/java/domain/RewardsTest.java
@@ -8,20 +8,21 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-@DisplayName("입력된 실행 결과의 수는 ")
+@DisplayName("실행 결과는 ")
 class RewardsTest {
 
     private final List<String> rewards = List.of("꽝", "5000", "3000", "꽝");
 
-    @DisplayName("참여자의 수와 같아야 한다.")
+    @DisplayName("전체 수가 참여자의 수와 같아야 한다.")
     @Test
     void rewardSizeTest_success() {
         assertDoesNotThrow(
             () -> Rewards.of(rewards, rewards.size()));
     }
 
-    @DisplayName("참여자의 수와 다르면 예외가 발생한다.")
+    @DisplayName("전체 수가 참여자의 수와 다르면 예외가 발생한다.")
     @Test
     void rewardSizeTest_fail() {
         assertThrows(
@@ -41,10 +42,19 @@ class RewardsTest {
     @DisplayName("인덱스를 통해 조회할 수 있다.")
     @ParameterizedTest
     @CsvSource(value = {"0:꽝", "1:5000", "2:3000", "3:꽝"}, delimiter = ':')
-    void getRewardsTest(int index, String expected) {
+    void getRewardsTest_success(int index, String expected) {
         Rewards allRewards = Rewards.of(rewards, rewards.size());
 
         assertEquals(expected, allRewards.getReward(index));
+    }
+
+    @DisplayName("사이즈 이상의 값이나 0 미만의 인덱스로 조회하면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 4, 5})
+    void getRewardsTest_fail(int index) {
+        Rewards allRewards = Rewards.of(rewards, rewards.size());
+        assertThrows(IllegalArgumentException.class,
+            () -> allRewards.getReward(index));
     }
 
 }
