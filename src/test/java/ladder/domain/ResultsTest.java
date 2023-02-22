dc47cdc617eaf2877fb546309100b9bd52185cc3
sh111-coder <ohk9134@naver.com>
2023-02-22 17:49:25 +0900

    test(ResultsTest): 실행 결과들을 생성하는 테스트 추가

---
+++ src/test/java/ladder/domain/ResultsTest.java
@@ -0,0 +1,21 @@
+package ladder.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ResultsTest {
+
+    @Test
+    @DisplayName("실행 결과들을 생성한다.")
+    void generateTest() {
+        Result result1 = new Result("꽝");
+        Result result2 = new Result("5000");
+
+        Assertions.assertDoesNotThrow(() -> new Results(List.of(result1, result2)));
+    }
+}
