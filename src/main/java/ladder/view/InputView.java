52144356d733d3b460d6f74ccb52b73d0a7e15a9
Cyma-s <ssol0319@naver.com>
2023-02-22 15:38:17 +0900

    feat: 실행 결과 입력 메서드 추가

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -18,4 +18,10 @@ public class InputView {
         System.out.println("최대 사다리 높이는 몇 개인가요?");
         return scanner.reset().nextInt();
     }
+
+    public static List<String> readItems() {
+        System.out.println("실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
+        String items = scanner.reset().nextLine();
+        return Arrays.stream(items.split(",")).collect(Collectors.toList());
+    }
 }
