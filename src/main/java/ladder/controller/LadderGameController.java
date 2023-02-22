d47d8edd2fa10061c21a4c27748d831da7facda6
Cyma-s <ssol0319@naver.com>
2023-02-22 19:20:41 +0900

    feat: 컨트롤러 추가, 플레이어 결과 입출력 로직 추가

---
+++ src/main/java/ladder/controller/LadderGameController.java
@@ -0,0 +1,37 @@
+package ladder.controller;
+
+import ladder.domain.LadderGame;
+import ladder.domain.LineStrategy;
+import ladder.view.InputView;
+import ladder.view.OutputView;
+
+import java.util.List;
+
+public class LadderGameController {
+    private LadderGame ladderGame;
+    private static final String ALL_RESULT_MESSAGE = "all";
+
+    public void playGame(LineStrategy lineStrategy) {
+        makeLadderGame(lineStrategy);
+        findPlayerResult();
+    }
+
+    private void makeLadderGame(LineStrategy lineStrategy) {
+        List<String> names = InputView.readNames();
+        List<String> items = InputView.readItems();
+        int height = InputView.readLadderHeight();
+        this.ladderGame = new LadderGame(names, items, height, lineStrategy);
+        OutputView.printLadder(ladderGame.getPlayerNames(), ladderGame.getLines(), ladderGame.getNameMaxLength());
+    }
+
+    private void findPlayerResult() {
+        String playerName = InputView.readResultPlayerName();
+        if(playerName.equals(ALL_RESULT_MESSAGE)) {
+            OutputView.printPlayerResultAll(ladderGame.getGameResult());
+            return;
+        }
+        String playerResult = ladderGame.getPlayerResult(playerName);
+        OutputView.printPlayerResult(playerResult);
+        findPlayerResult();
+    }
+}
