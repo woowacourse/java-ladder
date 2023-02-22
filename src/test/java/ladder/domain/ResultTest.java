e30a07c55ecbef5db20437bd4405a1674ddf763c
cpot5620 <=cpot5620@naver.com>
2023-02-22 17:14:48 +0900

    test(Result): 사다리 게임 수행 로직 테스트 추가

--- src/test/java/ladder/domain/ResultTest.java
+++ src/test/java/ladder/domain/ResultTest.java
@@ -5,20 +5,20 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class ResultTest {
     private Names names;
-    private Ladder ladder;
 
     @BeforeEach
     void setup() {
         names = new Names(
                 Arrays.asList("AAAA", "BBBB", "CCCC", "DDDD", "EEEE")
         );
-        ladder = new Ladder(names.size(), new Height(3), new TrueGenerator());
     }
 
     @DisplayName("참여자 수와 내기 목록의 수가 같아야 한다.")
@@ -38,15 +38,32 @@ public class ResultTest {
         Assertions.assertDoesNotThrow(() -> new Result(names, bets));
     }
 
-    @DisplayName("주어진 사다리(ladder)를 기반으로 결과의 순서를 바꾼다.")
-    @Test
-    void performResultSuccessTest() {
+    @ParameterizedTest(name = "주어진 사다리(ladder)를 기반으로 결과의 순서를 바꾼다.")
+    @ValueSource(ints = {1, 3, 5, 7, 9, 11, 13})
+    void performResultSuccessTest1(int height) {
+        Ladder ladder = new Ladder(names.size(), new Height(height), new TrueGenerator());
         List<String> bets = Arrays.asList("A", "B", "C", "D", "E"); // 초기 내기 목록 입력
         Result result = new Result(names, bets);
 
         result.perform(ladder); // ladder를 기반으로 내기 목록 위치 변경
 
         List<String> performedResult = Arrays.asList("B", "A", "D", "C", "E"); // 예상 결과
+
+        for (int i = 0; i < performedResult.size(); i++) {
+            Assertions.assertEquals(performedResult.get(i), result.getResult().get(i).getBet());
+        }
+    }
+
+    @ParameterizedTest(name = "주어진 사다리(ladder)를 기반으로 결과의 순서를 바꾼다.")
+    @ValueSource(ints = {2, 4, 6, 8, 10, 12, 14})
+    void performResultSuccessTest2(int height) {
+        Ladder ladder = new Ladder(names.size(), new Height(height), new TrueGenerator());
+        List<String> bets = Arrays.asList("A", "B", "C", "D", "E"); // 초기 내기 목록 입력
+        Result result = new Result(names, bets);
+
+        result.perform(ladder); // ladder를 기반으로 내기 목록 위치 변경
+
+        List<String> performedResult = Arrays.asList("A", "B", "C", "D", "E"); // 예상 결과
 
         for (int i = 0; i < performedResult.size(); i++) {
             Assertions.assertEquals(performedResult.get(i), result.getResult().get(i).getBet());
