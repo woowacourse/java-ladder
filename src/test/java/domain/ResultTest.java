830207dfe1d05c4b270cd4a8dd7135fcd771984d
Youngho <cyh6099@gmail.com>
2023-02-23 00:42:56 +0900

    refactor : 메서드 이름, displayname수정

--- src/test/java/domain/ResultTest.java
+++ src/test/java/domain/ResultTest.java
@@ -1,6 +1,7 @@
 package domain;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -16,17 +17,19 @@ public class ResultTest {
      * jk : 5000
      */
     @Test
-    void a() {
+    @DisplayName("유저 이름으로 게임 결과를 조회하면 그에 맞는 결과값을 반환한다.")
+    void userLadderGameResultTest() {
         Map<String, Integer> ladderGameResult = new LinkedHashMap<>();
         ladderGameResult.put("pobi", 0);
         ladderGameResult.put("honux", 3);
         ladderGameResult.put("crong", 2);
         ladderGameResult.put("jk", 1);
+
         Result result = new Result(new ArrayList<>(List.of("꽝","5000","꽝","3000")),ladderGameResult);
+
         Assertions.assertThat(result.getUserResult("pobi")).isEqualTo("꽝");
         Assertions.assertThat(result.getUserResult("honux")).isEqualTo("3000");
         Assertions.assertThat(result.getUserResult("crong")).isEqualTo("꽝");
         Assertions.assertThat(result.getUserResult("jk")).isEqualTo("5000");
     }
-
 }
