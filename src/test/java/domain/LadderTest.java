d5d09418b1c21e3ef03a3711624fcc30fb63ec47
Youngho <cyh6099@gmail.com>
2023-02-22 23:05:30 +0900

    test : 사다리 게임이 정상적으로 진행됐는지 검증하는 테스트 작성

--- src/test/java/domain/LadderTest.java
+++ src/test/java/domain/LadderTest.java
@@ -3,9 +3,14 @@ package domain;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
 public class LadderTest {
@@ -24,5 +29,31 @@ public class LadderTest {
     void LadderHeightSuccessTest(int height) {
         assertThatCode(() -> new Ladder(new Height(height), new Width(5), new RandomLadderGenerator()))
                 .doesNotThrowAnyException();
+    }
+
+    @DisplayName("사다리 게임을 정상적으로 진행후 (유저 이름 : 도착 index)데이터가 반환된다.")
+    @Test
+    void ladderResultTest() {
+        Users users = new Users(List.of(
+                new User("pobi"),
+                new User("honux"),
+                new User("crong"),
+                new User("jk"))
+        );
+        Ladder ladder = new Ladder(
+                new Height(4),
+                new Width(users.getCount() - 1),
+                ((width, height) -> List.of(
+                        new Line(List.of(Bridge.EXIST, Bridge.NON_EXIST, Bridge.EXIST)),
+                        new Line(List.of(Bridge.NON_EXIST, Bridge.EXIST, Bridge.NON_EXIST)),
+                        new Line(List.of(Bridge.EXIST, Bridge.NON_EXIST, Bridge.NON_EXIST)),
+                        new Line(List.of(Bridge.EXIST, Bridge.NON_EXIST, Bridge.EXIST))
+                ))
+        );
+        Map<String, Integer> result = ladder.play(users);
+        assertThat(result.get("pobi")).isEqualTo(3);
+        assertThat(result.get("honux")).isEqualTo(0);
+        assertThat(result.get("crong")).isEqualTo(2);
+        assertThat(result.get("jk")).isEqualTo(1);
     }
 }
