1862b8be5f4d4b1c45f0380e201011a3bd4deb39
pilyang <30036534+pilyang@users.noreply.github.com>
2023-02-22 15:45:08 +0900

    feat: implement method to get next position in line

--- src/main/java/laddergame/domain/Line.java
+++ src/main/java/laddergame/domain/Line.java
@@ -22,6 +22,32 @@ public class Line {
         return convertedPoints;
     }
 
+    public Position getConnectedPosition(Position position) {
+        if (isLeftMovable(position)) {
+            return position.getLeftPosition();
+        }
+        if (isRightMovable(position)) {
+            return position.getRightPosition();
+        }
+        return position;
+    }
+
+    private boolean isLeftMovable(Position position) {
+        int pointIndex = position.getValue() - 2;
+        if (pointIndex < 0) {
+            return false;
+        }
+        return points.get(pointIndex).isConnected();
+    }
+
+    private boolean isRightMovable(Position position) {
+        int pointIndex = position.getValue() - 1;
+        if (points.size() <= pointIndex) {
+            return false;
+        }
+        return points.get(pointIndex).isConnected();
+    }
+
     public List<Point> getLine() {
         return points;
     }
