449512610bc3b838e2248f4a7fb74d80b0a19734
ingpyo <juninpyo726@gmail.com>
2023-02-22 14:43:17 +0900

    feat: Products의 정적 팩토리 메소드

--- src/main/java/domain/product/Products.java
+++ src/main/java/domain/product/Products.java
@@ -1,5 +1,6 @@
 package domain.product;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Products {
@@ -7,5 +8,13 @@ public class Products {
 
     public Products(List<Product> products) {
         this.products = products;
+    }
+
+    public static Products generate(List<String> inputProducts) {
+        List<Product> products = new ArrayList<>();
+        for (String product : inputProducts) {
+            products.add(new Product(product));
+        }
+        return new Products(products);
     }
 }
