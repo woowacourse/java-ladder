ba4ae46e67aa33b31ee70c6aa2b39190afa3b6ce
sh111-coder <ohk9134@naver.com>
2023-02-22 16:17:04 +0900

    test(LadderGameTest): LadderGame 생성 시 실행 결과도 받도록 리팩토링하여 테스트

--- src/test/java/ladder/domain/LadderGameTest.java
+++ src/test/java/ladder/domain/LadderGameTest.java
@@ -9,11 +9,12 @@ import java.util.List;
 public class LadderGameTest {
 
     @Test
-    @DisplayName("주어진 플레이어 이름들과 최대 사다리 높이대로 참여자와 사다리를 생성하여 필드로 가지는 LadderGame이 생성된다.")
+    @DisplayName("주어진 플레이어 이름들과 실행 결과, 최대 사다리 높이대로 참여자와 사다리를 생성하여 필드로 가지는 LadderGame이 생성된다.")
     void ladderGameTest() {
         List<String> names = List.of("pobi", "crong", "seong", "haddy");
+        List<String> results = List.of("꽝", "5000", "꽝", "3000");
         int height = 5;
 
-        Assertions.assertDoesNotThrow(() -> new LadderGame(names, height));
+        Assertions.assertDoesNotThrow(() -> new LadderGame(names, results, height));
     }
 }
