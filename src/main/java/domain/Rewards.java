bb422b325e719a48c3f594de58ab3d67c56403ad
jaeyeon kim <jakind@naver.com>
2023-02-22 15:58:53 +0900

    feat: 상품 목록을 생성합니다. - domain.Reward from()

---
+++ src/main/java/domain/Rewards.java
@@ -0,0 +1,31 @@
+package domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Rewards {
+
+    private static final String REWARDS_SIZE_ERROR_MESSAGE = "상품 개수는 참여자 수와 동일하게 입력해야합니다.";
+    private final List<Reward> rewards;
+
+    private Rewards(List<Reward> rewards) {
+        this.rewards = rewards;
+    }
+
+    public static Rewards from(int numberOfPlayer, List<String> inputRewards) {
+        validate(numberOfPlayer, inputRewards);
+        List<Reward> rewards = inputRewards.stream().map(Reward::new).collect(Collectors.toList());
+        return new Rewards(rewards);
+    }
+
+    private static void validate(int numberOfPlayer, List<String> inputRewards) {
+        if (numberOfPlayer != inputRewards.size()) {
+            throw new IllegalArgumentException(REWARDS_SIZE_ERROR_MESSAGE);
+        }
+    }
+
+    public List<Reward> getRewards() {
+        return this.rewards;
+    }
+
+}
---
+++ src/test/java/domain/RewardsTest.java
@@ -0,0 +1,34 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.as;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class RewardsTest {
+
+    @Test
+    @DisplayName("상품 목록과 참여자의 수가 맞지 않으면 예외를 반환합니다.")
+    void createRewardsFail() {
+        PlayerNames playerNames = PlayerNames.from(List.of("pobi", "hell"));
+
+        assertThatThrownBy(() -> Rewards.from(playerNames.getSize(), List.of("fail", "5000", "5000")))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("상품 개수는 참여자 수와 동일하게 입력해야합니다.");
+    }
+
+    @Test
+    @DisplayName("상품 목록을 생성합니다.")
+    void createRewardsSuccess() {
+        PlayerNames playerNames = PlayerNames.from(List.of("pobi", "hell"));
+        Rewards rewards = Rewards.from(playerNames.getSize(), List.of("fail", "5000"));
+
+        assertThat(rewards.getRewards()).map(Reward::getReward)
+                .containsExactly("fail", "5000");
+    }
+
+}
