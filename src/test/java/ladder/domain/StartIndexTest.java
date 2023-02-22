a9fe57012063c89c57a6614c69dfb64e22fc4498
sh111-coder <ohk9134@naver.com>
2023-02-22 17:26:21 +0900

    test(StartIndexTest): 시작 위치 반환 테스트

--- src/test/java/ladder/domain/StartIndexTest.java
+++ src/test/java/ladder/domain/StartIndexTest.java
@@ -1,5 +1,7 @@
 package ladder.domain;
 
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,5 +12,14 @@ public class StartIndexTest {
     @DisplayName("시작 위치를 받아서 객체가 생성된다.")
     void generateTest() {
         Assertions.assertDoesNotThrow(() -> new StartIndex(1));
+    }
+
+    @Test
+    @DisplayName("시작 위치를 반환한다.")
+    void getterTest() {
+        int initStartIndex = 1;
+        StartIndex startIndex = new StartIndex(initStartIndex);
+
+        assertThat(startIndex.getRawStartIndex()).isEqualTo(initStartIndex);
     }
 }
