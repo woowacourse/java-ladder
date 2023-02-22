7911eee003056d54394f9d7b1ba4dfe406bbe00d
iamjooon2 <iamjooon2@gmail.com>
2023-02-22 19:49:51 +0900

    feat: 결과를 보고싶은 사람을 입력받는 기능

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -10,6 +10,7 @@ public class InputView {
     private static final String INPUT_NAMES = "참여할 사람 이름을 입력하세요. (이름은 쉼표(" + DELIMITER + ")로 구분하세요)";
     private static final String INPUT_PRIZES = "실행 결과를 입력하세요. (결과는 쉼표(" + DELIMITER + ")로 구분하세요)";
     private static final String INPUT_HEIGHT = "최대 사다리 높이는 몇 개인가요?";
+    private static final String INPUT_TARGET_NAME = "결과를 보고 싶은 사람은?";
     private static final String ERROR_NOT_NUMBER = "[ERROR] 사다리의 높이는 2 이상의 수를 입력해주세요";
 
     private static final Scanner scanner = new Scanner(System.in);
@@ -44,5 +45,10 @@ public class InputView {
         String input = scanner.nextLine();
         String[] split = input.split(DELIMITER);
         return new ArrayList<>(Arrays.asList(split));
+    }
+
+    public static String inputTargetName() {
+        System.out.println(INPUT_TARGET_NAME);
+        return scanner.nextLine();
     }
 }
