027dc52ec759780b4ce8871752418196062cb7e6
Youngho <cyh6099@gmail.com>
2023-02-23 01:37:19 +0900

    feat : 게임 결과 출력 view구현

---
+++ src/main/java/view/ResultView.java
@@ -0,0 +1,15 @@
+package view;
+
+public class ResultView {
+    public void printUserResult(String result) {
+        System.out.println(result);
+    }
+
+    public void printAllResult() {
+        System.out.println("다 출력");
+    }
+
+    public void printNonExistUser() {
+        System.out.println("존재하지 않는 유저이름 입니다.");
+    }
+}
