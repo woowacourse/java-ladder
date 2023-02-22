41778f3639db8342ab26d19364408813e09f4b19
yeonkkk <tjddus0302@gmail.com>
2023-02-22 22:17:12 +0900

    feat: User 필드 값에 Position 추가, position을 변경하는 메서드 생성

--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -1,13 +1,30 @@
 package domain;
 
+import java.util.List;
+
 public class User {
     private final Name name;
+    private final Position position;
 
-    public User(Name name) {
+    public User(Name name, Position position) {
         this.name = name;
+        this.position = position;
+    }
+
+    public void movePosition(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (position.checkConnection(number)) {
+                position.move(number);
+                break;
+            }
+        }
     }
 
     public String getName() {
         return name.getName();
+    }
+
+    public int getPosition() {
+        return position.getPosition();
     }
 }
