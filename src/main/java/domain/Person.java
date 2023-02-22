ca0ab876c3d082d2847259efee8decbc77c6d4cf
java-saeng <gusdn85554@naver.com>
2023-02-22 16:17:22 +0900

    feat : name이 같으면 같은 Person 메서드 추가

--- src/main/java/domain/Person.java
+++ src/main/java/domain/Person.java
@@ -32,6 +32,10 @@ public class Person {
         return name;
     }
 
+    public boolean isSamePerson(String name) {
+        return name.equals(this.name);
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
--- src/test/java/domain/PersonTest.java
+++ src/test/java/domain/PersonTest.java
@@ -1,11 +1,15 @@
 package domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class PersonTest {
 
@@ -20,5 +24,25 @@ class PersonTest {
     @EmptySource
     void test_person_name_length_fail(String name) {
         assertThatIllegalArgumentException().isThrownBy(() -> new Person(name));
+    }
+
+    @Test
+    @DisplayName("isSamePerson() : 이름이 다를 경우에는 같은 Person이라고 할 수 없다.")
+    void test_isSamePerson_fail() throws Exception {
+        //given
+        Person person = new Person("aa");
+
+        //when & then
+        assertFalse(person.isSamePerson("bb"));
+    }
+
+    @Test
+    @DisplayName("isSamePerson() : 이름이 같을 경우에는 같은 Person이라고 할 수 있다.")
+    void test_isSamePerson_success() throws Exception {
+        //given
+        Person person = new Person("aa");
+
+        //when & then
+        assertTrue(person.isSamePerson("aa"));
     }
 }
