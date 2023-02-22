723a281de172319adbde284e9be7f8f2f55063cd
chanminmaxscaler <jcm04@naver.com>
2023-02-22 13:35:01 +0900

    refactor: 출력 메시지 오타를 수정한다

--- src/main/java/laddergame/view/message/Message.java
+++ src/main/java/laddergame/view/message/Message.java
@@ -1,7 +1,7 @@
 package laddergame.view.message;
 
 public enum Message {
-    INPUT_PARTICIPANT_NAMES_GUIDE("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 분하세요)", "%s"),
+    INPUT_PARTICIPANT_NAMES_GUIDE("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 분리하세요)", "%s"),
     INPUT_LADDER_MAX_HEIGHT_GUIDE("최대 사다리 높이는 몇 개인가요?", "%n%s"),
     RESULT_GUIDE("실행결과", "%n%s%n");
 
