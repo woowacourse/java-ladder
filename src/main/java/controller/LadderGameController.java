5e459a3af1d7eeb4f8eaa5c78595f5b83e7223cf
MoonJeWoong <munjin0201@naver.com>
2023-02-22 19:55:27 +0900

    feat: controller에 실행결과 입력 기능 추가

--- src/main/java/controller/LadderGameController.java
+++ src/main/java/controller/LadderGameController.java
@@ -4,6 +4,7 @@ import model.Ladder;
 import model.LadderHeight;
 import model.Name;
 import model.Players;
+import model.Result;
 import view.InputView;
 import view.OutputView;
 
@@ -20,9 +21,15 @@ public class LadderGameController {
 
     public void run() {
         Players players = new Players(setPlayerNames());
+        List<Result> results = setResults(players.size());
         Ladder ladder = new Ladder(players, setLadderHeight());
         outputView.printAllPlayerNames(players.getAllPlayerNames());
         outputView.printLadder(ladder);
+    }
+
+    private List<Result> setResults(int playerCount) {
+        outputView.printResultsMessage();
+        return inputView.readResults(playerCount);
     }
 
     private List<Name> setPlayerNames() {
