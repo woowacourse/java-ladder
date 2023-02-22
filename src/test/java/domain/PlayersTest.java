12193e44dc61eebe23899de319d8b2cd755fef12
Miseong Kim <miseongk@naver.com>
2023-02-22 14:09:22 +0900

    test: Players 객체 생성 및 예외 발생 확인

--- src/test/java/domain/PlayersTest.java
+++ src/test/java/domain/PlayersTest.java
@@ -1,38 +1,46 @@
 package domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class PlayersTest {
 
-    @Test
-    @DisplayName("Players객체 생성 테스트")
-    void makePlayers() {
-        String names = "roy,poy,soy,koy";
+    private List<Player> players = new ArrayList<>();
 
-        assertDoesNotThrow(() -> new Players(names));
+    @BeforeEach
+    void init() {
+        players.add(new Player("roy"));
     }
 
     @Test
-    @DisplayName("구분자가 쉼표가 아닌 경우 예외 발생")
-    void validateDelimiter() {
-        String names = "roy|poy|soy|koy";
+    @DisplayName("Players 객체 생성 테스트")
+    void makePlayers() {
+        players.add(new Player("soy"));
 
-        assertThatThrownBy(() -> new Players(names))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 구분자 ,를 이용하여 두 명 이상 입력해야 합니다.");
+        assertDoesNotThrow(() -> new Players(players));
     }
 
     @Test
     @DisplayName("플레이어 수가 한명일 경우 예외 발생")
     void validateMoreThanOnePlayer() {
-        String names = "roy";
-
-        assertThatThrownBy(() -> new Players(names))
+        assertThatThrownBy(() -> new Players(players))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 구분자 ,를 이용하여 두 명 이상 입력해야 합니다.");
+                .hasMessage("[ERROR] 플레이어는 두 명 이상 입력해야 합니다.");
+    }
+
+    @Test
+    @DisplayName("플레이어 수 확인")
+    void getNumberOfPlayers() {
+        players.add(new Player("soy"));
+
+        assertThat(new Players(players).getNumberOfPlayers()).isEqualTo(2);
     }
 }
+
