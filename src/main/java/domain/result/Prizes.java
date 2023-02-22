157d031a4710e7ad36d107c57d6e6788258fc180
kim0914 <kosa0914@naver.com>
2023-02-23 01:57:28 +0900

    feat(Prizes): 결과를 가지고 있는 객체 추가

---
+++ src/main/java/domain/result/Prizes.java
@@ -0,0 +1,40 @@
+package domain.result;
+
+import java.util.List;
+
+public class Prizes {
+
+    private static final String PRIZE_SIZE_ERROR_MESSAGE = "최소 2개 이상의 결과가 존재해야 합니다.";
+    private static final String PRIZE_BLANK_ERROR_MESSAGE = "결과명에는 빈 문자열과 공백이 허용되지 않습니다.";
+    private static final int PRIZE_MIN_SIZE = 2;
+
+    private final List<String> prizes;
+
+    public Prizes(List<String> prizes) {
+        validate(prizes);
+        this.prizes = prizes;
+    }
+
+    private void validate(List<String> prizes) {
+        if (prizes.size() < PRIZE_MIN_SIZE) {
+            throw new IllegalArgumentException(PRIZE_SIZE_ERROR_MESSAGE);
+        }
+
+        if (hasBlank(prizes)) {
+            throw new IllegalArgumentException(PRIZE_BLANK_ERROR_MESSAGE);
+        }
+    }
+
+    private boolean hasBlank(List<String> prizes) {
+        return prizes.stream()
+                .anyMatch(String::isBlank);
+    }
+
+    public String findByIndex(int index) {
+        return prizes.get(index);
+    }
+
+    public List<String> getPrizes() {
+        return prizes;
+    }
+}
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -9,8 +9,11 @@ import java.util.stream.Collectors;
 public class InputView {
 
     private static final String NAME_INPUT_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    private static final String RESULT_INPUT_MESSAGE = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
     private static final String HEIGHT_INPUT_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
+    private static final String RESULT_NAME_MESSAGE = "결과를 보고 싶은 사람은?";
     private static final String DIGIT_ERROR_MESSAGE = "자연수만 입력 가능합니다.";
+    private static final String NAME_ERROR_MESSAGE = "이름은 1자 이상 5자 이하만 가능합니다.";
     private static final String DELIMITER = ",";
     private static final String DIGIT_REGEX = "^[0-9]+$";
 
@@ -23,11 +26,27 @@ public class InputView {
                 .collect(Collectors.toList());
     }
 
+    public static List<String> readPrizes() {
+        printMessage(RESULT_INPUT_MESSAGE);
+        String inputResults = scanner.nextLine();
+        return Arrays.stream(inputResults.split(DELIMITER))
+                .collect(Collectors.toList());
+    }
+
     public static int readLadderHeight() {
         printMessage(HEIGHT_INPUT_MESSAGE);
         String ladderHeight = scanner.nextLine();
         validateDigit(ladderHeight);
         return Integer.parseInt(ladderHeight);
+    }
+
+    public static String readResultName() {
+        printMessage(RESULT_NAME_MESSAGE);
+        String resultName = scanner.nextLine();
+        if (resultName.isBlank()) {
+            throw new IllegalArgumentException(NAME_ERROR_MESSAGE);
+        }
+        return resultName;
     }
 
     private static void validateDigit(String ladderHeight) {
