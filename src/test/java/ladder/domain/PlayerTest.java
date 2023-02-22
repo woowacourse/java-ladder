55aa0ff512d3f5a9ae38ba953af537052a5effa8
DESKTOP-FNVN0HH\USER <beatus96@naver.com>
2023-02-22 22:20:21 +0900

    test: Player 보상 결정하기 기능 테스트 추가

--- src/test/java/ladder/domain/PlayerTest.java
+++ src/test/java/ladder/domain/PlayerTest.java
@@ -7,19 +7,30 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PlayerTest {
-    private Name name;
     private Player player;
+    private PlayerName name;
+    private Reward reward;
 
     @BeforeEach
     void setup() {
-        name = new Name("포비");
+        name = new PlayerName("포비");
         player = new Player(name);
+        reward = new Reward("통과");
     }
 
     @Test
     @DisplayName("Player 객체 getName 테스트")
     void getNameTest() {
-        assertThat(player.getName()).isEqualTo(name.getName());
+        assertThat(player.getName())
+                .isEqualTo(name.getName());
     }
 
+    @Test
+    @DisplayName("Player 객체 determineReward 테스트")
+    void determineRewardTest() {
+        player.determineReward(reward);
+
+        assertThat(player.getReward())
+                .isEqualTo(reward.getName());
+    }
 }
