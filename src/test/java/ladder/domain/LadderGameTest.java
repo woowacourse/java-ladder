0456933c4b25493a3ae6a4311d995a65273c496e
BGuga <tmdwns9913@gmail.com>
2023-02-22 16:29:09 +0900

    test: 사다리 게임 테스트 케이스 추가

--- src/test/java/ladder/domain/LadderGameTest.java
+++ src/test/java/ladder/domain/LadderGameTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class LadderGameTest {
@@ -14,6 +15,9 @@ class LadderGameTest {
     private Users DEFAULT_USERS;
     private Ladder DEFAULT_LADDER;
     private Reward DEFAULT_REWARD;
+
+    private final LineSource I_____I = LineSource.MAKE_LINE;
+    private final LineSource IxxxxxI = LineSource.MAKE_BLANK;
 
     @BeforeEach
     void setting() {
@@ -33,5 +37,44 @@ class LadderGameTest {
     void getRewardOfUserByBridgeGameTest() {
         final LadderGame ladderGame = new LadderGame(DEFAULT_LADDER, DEFAULT_USERS, DEFAULT_REWARD);
         assertThat(ladderGame.getRewardOf("a")).isEqualTo("1");
+    }
+
+    @Test
+    @DisplayName("직선 사디레에서 전체 유저들의 보상을 테스트한다.")
+    void getRewardOfAllUserTest1() {
+        final LadderGame ladderGame = new LadderGame(DEFAULT_LADDER, DEFAULT_USERS, DEFAULT_REWARD);
+        assertAll(() -> {
+                    assertThat(ladderGame.getRewardOf("a")).isEqualTo("1");
+                },
+                () -> {
+                    assertThat(ladderGame.getRewardOf("b")).isEqualTo("2");
+                },
+                () -> {
+                    assertThat(ladderGame.getRewardOf("c")).isEqualTo("3");
+                }
+        );
+    }
+
+    @Test
+    @DisplayName("전체 유저들의 보상을 테스트한다.")
+    void getRewardOfAllUserTest2() {
+        final Ladder ladder = new Ladder(3, DEFAULT_USERS);
+        ladder.makeFloors(new TestLineSourceGenerator(List.of(
+                I_____I, IxxxxxI,
+                IxxxxxI, I_____I,
+                IxxxxxI, IxxxxxI
+        )));
+
+        final LadderGame ladderGame = new LadderGame(ladder, DEFAULT_USERS, DEFAULT_REWARD);
+        assertAll(() -> {
+            assertThat(ladderGame.getRewardOf("a")).isEqualTo("3");
+                },
+                () -> {
+            assertThat(ladderGame.getRewardOf("b")).isEqualTo("1");
+                },
+                () -> {
+            assertThat(ladderGame.getRewardOf("c")).isEqualTo("2");
+                }
+        );
     }
 }
