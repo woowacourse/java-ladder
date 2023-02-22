47640f9ab0d5d9c417b1c8b45e5f6cc5f7d9f7d6
MoonJeWoong <munjin0201@naver.com>
2023-02-22 15:44:09 +0900

    feat: 참여자 이름 중복 여부 검사 기능 추가

--- src/main/java/model/NameFactory.java
+++ src/main/java/model/NameFactory.java
@@ -1,21 +1,38 @@
 package model;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class NameFactory {
+    private static final String DUPLICATED_NAME_ERROR = "[ERROR] 참가자 이름은 중복될 수 없습니다.";
     private static final String NAMES_DELIMITER = ",";
 
     public static List<Name> create(String inputNames) {
-        return splitInputNames(inputNames).stream()
-                .map(String::strip)
+        List<String> names = splitInputNames(inputNames);
+        validateDuplicatedNames(names);
+        return names.stream()
                 .map(Name::new)
                 .collect(Collectors.toList());
     }
 
     private static List<String> splitInputNames(String inputNames) {
-        return Arrays.asList(inputNames.split(NAMES_DELIMITER));
+        return Arrays.stream(inputNames.split(NAMES_DELIMITER))
+                .map(String::strip)
+                .collect(Collectors.toList());
     }
+
+    private static void validateDuplicatedNames(List<String> names) {
+        if(names.size() != getDistinctCount(names)) {
+            throw new IllegalArgumentException(DUPLICATED_NAME_ERROR);
+        }
+    }
+
+    private static int getDistinctCount(List<String> names) {
+        return (int) names.stream()
+                .distinct()
+                .count();
+    }
+
+
 }
