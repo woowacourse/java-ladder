222d553770aff3bcfdc40e3221dda5e3e56912ce
BGuga <tmdwns9913@gmail.com>
2023-02-22 19:37:57 +0900

    feat: 출력 커맨드에 따른 컨트롤 제어 기능 작성

---
+++ src/test/java/ladder/domain/PrintCommandTest.java
@@ -0,0 +1,25 @@
+package ladder.domain;
+
+import ladder.controller.PrintCommand;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PrintCommandTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"a", "123", "ffff", "", " "})
+    @DisplayName("PRINT_ONE 반환 테스트")
+    void printOneTest(String value) {
+        assertThat(PrintCommand.of(value)).isEqualTo(PrintCommand.PRINT_ONE);
+    }
+
+    @Test
+    @DisplayName("PRINT_ALL 반환 테스트")
+    void printAllTest() {
+        assertThat(PrintCommand.of("all")).isEqualTo(PrintCommand.PRINT_ALL);
+    }
+}
