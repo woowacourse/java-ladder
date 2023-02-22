731a32a55ad75a6fde2e33acd59d92b5ba206558
Cyma-s <ssol0319@naver.com>
2023-02-22 15:09:46 +0900

    test: 같은 값을 갖는 위치는 동일한 위치임을 확인하는 테스트 추가

--- src/test/java/ladder/domain/PositionTest.java
+++ src/test/java/ladder/domain/PositionTest.java
@@ -3,6 +3,7 @@ package ladder.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PositionTest {
@@ -12,5 +13,16 @@ class PositionTest {
         // expected
         assertThatThrownBy(() -> new Position(-1))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("같은 값을 갖는 위치는 동일한 위치이다.")
+    void position_sameValuePositionIsSamePosition() {
+        // given
+        Position firstPosition = new Position(1);
+        Position secondPosition = new Position(1);
+
+        // expected
+        assertThat(firstPosition.equals(secondPosition)).isTrue();
     }
 }
