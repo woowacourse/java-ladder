8616a85d37c11be0da7f6c0499451670b192982f
ChoiYoungHoon <dudgns2865@naver.com>
2023-02-23 14:29:04 +0900

    test: Result validate 테스트 코드 추가

---
+++ src/test/java/domain/vo/ResultTest.java
@@ -0,0 +1,38 @@
+package domain.vo;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ResultTest {
+
+    @Test
+    @DisplayName("글자 제한 실패 테스트")
+    public void validateFailureTest() {
+        //given
+        String value1 = "";
+        String value2 = "123456";
+
+        //when
+        //then
+        assertThatThrownBy(() -> new Result(value1))
+            .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Result(value2))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("글자 제한 성공 테스트")
+    public void validateSuccessTest() {
+        //given
+        String value1 = "1";
+        String value2 = "12345";
+
+        //when
+        //then
+        assertDoesNotThrow(() -> new Result(value1));
+        assertDoesNotThrow(() -> new Result(value2));
+    }
+}
