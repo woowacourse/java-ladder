7ab88fc1122734534a6c80930c2d82448c2772be
kwonyj1022 <kwonyj1022@naver.com>
2023-02-23 16:29:20 +0900

    refactor(Items): Items의 구성요소를 List<String>에서 List<Item>으로 변경

--- src/main/java/domain/item/Items.java
+++ src/main/java/domain/item/Items.java
@@ -2,14 +2,17 @@ package domain.item;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Items {
 
-    List<String> items;
+    private final List<Item> items;
 
     public Items(int count, List<String> items) {
         validate(count, items);
-        this.items = items;
+        this.items = items.stream()
+                .map(Item::new)
+                .collect(Collectors.toList());
     }
 
     private void validate(int count, List<String> items) {
@@ -17,7 +20,7 @@ public class Items {
             throw new IllegalArgumentException("참가자 수와 실행 결과의 수가 일치해야 합니다.");
     }
 
-    public List<String> getItems() {
+    public List<Item> getItems() {
         return Collections.unmodifiableList(items);
     }
 }
