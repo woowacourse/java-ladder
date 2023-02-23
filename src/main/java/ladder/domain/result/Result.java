317e1e9c7672d083abe48cdca54759d291351ab5
jeonghoon <shb03323@naver.com>
2023-02-23 13:32:10 +0900

    feat: 모든 플레이어 매칭 결과 조회 기능 구현

--- src/main/java/ladder/domain/result/Result.java
+++ src/main/java/ladder/domain/result/Result.java
@@ -1,12 +1,65 @@
 package ladder.domain.result;
 
+import ladder.domain.ladder.Block;
 import ladder.domain.ladder.Ladder;
+import ladder.domain.ladder.Line;
+import ladder.domain.player.PlayerName;
 import ladder.domain.player.Players;
+import ladder.domain.prize.Prize;
 import ladder.domain.prize.Prizes;
+
+import java.util.*;
 
 public class Result {
 
-    public Result(Players players, Prizes prizes, Ladder ladder) {
+    private final Map<PlayerName, Prize> results;
 
+    public Result(final Players players, final Prizes prizes, final Ladder ladder) {
+        this.results = makeResult(players, prizes, ladder);
+    }
+
+    private Map<PlayerName, Prize> makeResult(Players players, Prizes prizes, Ladder ladder) {
+        List<PlayerName> playerNames = new ArrayList<>(players.getPlayers());
+        for (Line line : ladder.getLines()) {
+            moveOneLine(playerNames, line);
+        }
+        Map<PlayerName, Prize> unSortedResults = connectPlayerAndPrize(playerNames, prizes.getPrizes());
+        return sort(unSortedResults, players.getPlayers());
+    }
+
+    private void moveOneLine(List<PlayerName> playerNames, Line line) {
+        for (int i = 0; i < line.getBlocks().size(); i++) {
+            Block block = line.getBlocks().get(i);
+            moveEachPlayer(playerNames, i, block);
+        }
+    }
+
+    private void moveEachPlayer(List<PlayerName> playerNames, int blockIndex, Block block) {
+        if (block.isExistBlock()) {
+            int leftPlayerIndex = blockIndex;
+            int rightPlayerIndex = blockIndex + 1;
+            Collections.swap(playerNames, leftPlayerIndex, rightPlayerIndex);
+        }
+    }
+
+    private Map<PlayerName, Prize> connectPlayerAndPrize(List<PlayerName> playerNames, List<Prize> prizes) {
+        Map<PlayerName, Prize> connection = new HashMap<>();
+        for (int i = 0; i < playerNames.size(); i++) {
+            connection.put(playerNames.get(i), prizes.get(i));
+        }
+        return connection;
+    }
+
+    private Map<PlayerName, Prize> sort(Map<PlayerName, Prize> unSortedResults, List<PlayerName> playerNames) {
+        Map<PlayerName, Prize> sortedResults = new LinkedHashMap<>();
+        for (PlayerName playerName : playerNames) {
+            Prize prize = unSortedResults.get(playerName);
+            sortedResults.put(playerName, prize);
+        }
+        return sortedResults;
+    }
+
+    public Map<PlayerName, Prize> getAllResult() {
+        return results;
     }
 }
--- src/test/java/ladder/domain/ResultTest.java
+++ src/test/java/ladder/domain/ResultTest.java
@@ -3,18 +3,33 @@ package ladder.domain;
 import ladder.domain.ladder.Block;
 import ladder.domain.ladder.Ladder;
 import ladder.domain.ladder.generator.BlockGenerator;
+import ladder.domain.player.PlayerName;
 import ladder.domain.player.Players;
+import ladder.domain.prize.Prize;
 import ladder.domain.prize.Prizes;
 import ladder.domain.result.Result;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ResultTest {
 
     private Result result;
 
+    /**
+     * pobi honux crong  jk
+     * |-----|     |-----|
+     * |     |-----|     |
+     * |-----|     |     |
+     * |     |-----|     |
+     * |-----|     |-----|
+     * 꽝   5000   꽝   3000
+     */
     @BeforeEach
     void setup() {
         Players players = new Players(List.of("pobi", "honux", "crong", "jk"));
@@ -25,13 +40,6 @@ public class ResultTest {
         result = new Result(players, prizes, ladder);
     }
 
-    /**
-     * |-----|     |-----|
-     * |     |-----|     |
-     * |-----|     |     |
-     * |     |-----|     |
-     * |-----|     |-----|
-     */
     Ladder setLadder(int playerNumber, int height) {
         BlockGenerator blockGenerator = new TestBlockGenerator(
                 List.of(Block.EXIST, Block.EXIST,
@@ -40,5 +48,15 @@ public class ResultTest {
                         Block.EMPTY, Block.EXIST,
                         Block.EXIST, Block.EXIST));
         return new Ladder(blockGenerator, playerNumber, height);
+    }
+
+    @Test
+    void 모든_플레이어의_결과가_정확한지_확인() {
+        Map<PlayerName, Prize> allResult = result.getAllResult();
+        List<String> playerNames = allResult.keySet().stream()
+                .map(playerName -> allResult.get(playerName).getName())
+                .collect(Collectors.toUnmodifiableList());
+
+        assertThat(playerNames).isEqualTo(List.of("꽝", "3000", "꽝", "5000"));
     }
 }
