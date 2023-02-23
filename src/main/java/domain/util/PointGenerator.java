a38539325fd95b7ccd3ea39866aaf6868decbe09
echo <echo.backend@gmail.com>
2023-02-23 22:12:01 +0900

    refactor: PointGenerator 테스트를 위한 정적펙토리메서드 제거

--- src/main/java/domain/util/PointGenerator.java
+++ src/main/java/domain/util/PointGenerator.java
@@ -1,12 +1,6 @@
 package domain.util;
 
 public interface PointGenerator {
-	static PointGenerator getInstance(boolean isRandom) {
-		if (isRandom) {
-			return new RandomPointGenerator();
-		}
-		return new FixedPresencePointGenerator();
-	}
 
 	Point generate();
 }
