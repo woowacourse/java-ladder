b203d7c0bce2437e96f5a66f62163d282377a6c5
Kong-hana01 <gksqlsl11@khu.ac.kr>
2023-02-23 16:45:34 +0900

    feat: 입력받은 실행 결과를 사다리 폭에 맞춰서 출력하도록 메서드 추가

--- src/main/java/controller/LadderController.java
+++ src/main/java/controller/LadderController.java
@@ -1,9 +1,12 @@
 package controller;
 
 import java.util.List;
+import java.util.Map;
 
 import domain.Ladder;
+import domain.LadderGame;
 import domain.Players;
+import domain.Prize;
 import domain.numbergenerator.NumberGenerator;
 import utils.Command;
 import utils.LogType;
@@ -16,7 +19,6 @@ public class LadderController {
     private final OutputView outputView;
     private final NumberGenerator numberGenerator;
 
-
     public LadderController(InputView inputView, OutputView outputView, NumberGenerator numberGenerator) {
         this.inputView = inputView;
         this.outputView = outputView;
@@ -25,9 +27,15 @@ public class LadderController {
 
     public void run() {
         Players players = generatePlayers();
-        Ladder ladder = generateLadder(players.getPlayerSize());
+        int playerCount = players.getPlayerSize();
+        Ladder ladder = generateLadder(playerCount);
+        Prize prize = generatePrize(playerCount);
+        LadderGame ladderGame = new LadderGame(ladder, players, prize);
+        Map<String, String> result = ladderGame.run();
         outputView.printNames(players);
         outputView.printLadder(ladder);
+        outputView.printPrizes(prize);
+
     }
 
     private Players generatePlayers() {
@@ -41,6 +49,12 @@ public class LadderController {
         }
     }
 
+    private void validateCommandNames(List<String> names) {
+        if (Command.isIn(names)) {
+            throw new IllegalArgumentException("사다리 게임 참여자의 이름은 결과 검색 명령어와는 달라야합니다.");
+        }
+    }
+
     private Ladder generateLadder(int personCount) {
         try {
             int height = inputView.readHeight();
@@ -51,9 +65,13 @@ public class LadderController {
         }
     }
 
-    private void validateCommandNames(List<String> names) {
-        if (Command.isIn(names)) {
-            throw new IllegalArgumentException("사다리 게임 참여자의 이름은 결과 검색 명령어와는 달라야합니다.");
+    private Prize generatePrize(int playerCount) {
+        try {
+            List<String> prizes = inputView.readPrizes();
+            return new Prize(prizes, playerCount);
+        } catch (IllegalArgumentException exception) {
+            LogType.ERROR_MESSAGE.log(exception.getMessage());
+            return generatePrize(playerCount);
         }
     }
 }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -2,11 +2,7 @@ package view;
 
 import java.util.List;
 
-import domain.Ladder;
-import domain.Line;
-import domain.Player;
-import domain.Players;
-import domain.Point;
+import domain.*;
 
 public class OutputView {
 
@@ -15,6 +11,7 @@ public class OutputView {
     private final String BLANK = " ";
     private final String LINE_START_FORMAT = "    |";
     private final String NAME_START_FORMAT = "  ";
+    private final String PRIZE_START_FORMAT = "  ";
     private final int DIVISOR = 2;
     private final int DEFAULT_PADDING = 2;
     private final int FLAG = 1;
@@ -27,20 +24,20 @@ public class OutputView {
         System.out.print(NAME_START_FORMAT);
         players.getPlayers().stream()
                 .map(Player::getName)
-                .forEach(outputView::printName);
+                .forEach(outputView::printObject);
         System.out.println();
     }
 
-    private void printName(String name) {
-        String formattedName = generateCentralFormattedName(name);
+    private void printObject(String name) {
+        String formattedName = generateCentralFormattedObject(name);
         System.out.print(formattedName + BLANK);
     }
 
-    private String generateCentralFormattedName(String name) {
-        int length = name.length();
+    private String generateCentralFormattedObject(String object) {
+        int length = object.length();
         int leftPadding = DEFAULT_PADDING - length / DIVISOR;
         int rightPadding = DEFAULT_PADDING - (length - FLAG) / DIVISOR;
-        return BLANK.repeat(leftPadding) + name + BLANK.repeat(rightPadding);
+        return BLANK.repeat(leftPadding) + object + BLANK.repeat(rightPadding);
     }
 
     public void printLadder(Ladder ladder) {
@@ -52,5 +49,12 @@ public class OutputView {
         StringBuilder result = new StringBuilder(LINE_START_FORMAT);
         points.forEach(point -> result.append(point.toFormattedStatus()));
         System.out.println(result);
+    }
+
+    public void printPrizes(Prize prizes) {
+        System.out.print(PRIZE_START_FORMAT);
+        prizes.getPrizes()
+                .forEach(outputView::printObject);
+        System.out.println();
     }
 }
