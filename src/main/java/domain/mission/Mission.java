9c0b67e97c65898bbd24936300a22bc56ab60ced
Eunkeee <eunkeeeee@gmail.com>
2023-02-23 15:53:30 +0900

    refactor(Mission): 검증 방식 변경

--- src/main/java/domain/mission/Mission.java
+++ src/main/java/domain/mission/Mission.java
@@ -8,17 +8,17 @@ public class Mission {
     private final String mission;
 
     public Mission(String mission) {
-        this.mission = getValidatedMission(mission);
+        validateMission(mission);
+        this.mission = mission;
     }
 
-    private static String getValidatedMission(String mission) {
+    private void validateMission(String mission) {
         if (mission.isBlank()) {
             throw new IllegalArgumentException("공백 또는 null을 입력할 수 없습니다.");
         }
         if (mission.length() > MAX_LENGTH_EXCLUSIVE) {
             throw new IllegalArgumentException("미션 글자수는 5글자를 초과할 수 없습니다.");
         }
-        return mission;
     }
 
     public String getMission() {
