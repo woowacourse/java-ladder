eb4cb2563f553398a909c78f394e56eefea61e65
drunkenhw <drunkenhw@gmail.com>
2023-02-23 13:47:52 +0900

    feat: LadderGame 생성

---
+++ src/test/java/ladder/domain/LadderGameTest.java
@@ -0,0 +1,31 @@
+package ladder.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LadderGameTest {
+
+    /*
+    a   b   c
+    |---|   |
+    |   |---|
+    b   c   a
+   */
+    @Test
+    @DisplayName("사람 이름을 입력하면 해당 item를 반환한다")
+    void shouldReturnResultItemWhenInputName() {
+        Players players = Players.generate(List.of("a", "b", "c"));
+        List<Boolean> determinedBars = new ArrayList<>(List.of(true, false, true));
+        Ladder ladder = Ladder.generate(2, 2, new DeterminedBooleanGenerator(determinedBars));
+        Items items = Items.generate(List.of("1000", "2000", "0"), players.getSize());
+
+        LadderGame ladderGame = new LadderGame(players, ladder, items);
+        GameResult gameResult = ladderGame.play();
+
+        assertThat(gameResult.findResult(new Name("a"))).containsEntry("a", "0");
+    }
+}
