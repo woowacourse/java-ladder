caadf457335f4a0a3b2bd1fe75eac0b39aa954e1
ChoiYoungHoon <dudgns2865@naver.com>
2023-02-23 14:37:56 +0900

    feat: 실행 결과, 결과를 보고싶은 사람 입력 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -10,7 +10,9 @@ public class InputView {
 
     private static final String INPUT_NAMES_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     private static final String INPUT_LADDER_HEIGHT_MESSAGE = "\n최대 사다리 높이는 몇 개인가요?";
-    private static final String NAME_DELIMITER = ",";
+    private static final String DELIMITER = ",";
+    public static final String INPUT_RESULT_MESSAGE = "\n실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
+    public static final String INPUT_RESULT_TARGET_MESSAGE = "\n결과를 보고 싶은 사람은?";
     private final InputValidatorChain validator;
     private final Scanner scanner = new Scanner(System.in);
 
@@ -19,8 +21,7 @@ public class InputView {
     }
 
     public List<String> inputNames() {
-        System.out.println(INPUT_NAMES_MESSAGE);
-        return List.of(scanner.nextLine().replace(" ", "").trim().split(NAME_DELIMITER));
+        return getInputs(INPUT_NAMES_MESSAGE, List.of(ValidateType.EMPTY_VALUE));
     }
 
     public int inputLadderHeight() {
@@ -29,5 +30,23 @@ public class InputView {
         validator.validate(new InputValidationRequest(
             List.of(ValidateType.EMPTY_VALUE, ValidateType.INTEGER_VALUE), input));
         return Integer.parseInt(input);
+    }
+
+    public List<String> inputResults() {
+        return getInputs(INPUT_RESULT_MESSAGE, List.of(ValidateType.EMPTY_VALUE));
+    }
+
+    public String inputResultTarget() {
+        System.out.println(INPUT_RESULT_TARGET_MESSAGE);
+        String input = scanner.nextLine();
+        validator.validate(new InputValidationRequest(List.of(ValidateType.EMPTY_VALUE), input));
+        return input;
+    }
+
+    private List<String> getInputs(String inputResultMessage, List<ValidateType> validateTypes) {
+        System.out.println(inputResultMessage);
+        String input = scanner.nextLine();
+        validator.validate(new InputValidationRequest(validateTypes, input));
+        return List.of(input.replace(" ", "").trim().split(DELIMITER));
     }
 }
