c3165a96beee5b1ab4b8649065961057565160da
echo <echo.backend@gmail.com>
2023-02-23 20:48:08 +0900

    refactor(Participant): 찾는 사람 이름이 있는지 확인하는 메서드 추가

--- src/main/java/domain/Participant.java
+++ src/main/java/domain/Participant.java
@@ -12,6 +12,7 @@ public class Participant implements Display {
 	private static final int MAX_NAME_LENGTH = 5;
 	private static final String BLANK_PARTICIPANT_NAME_ERROR_MSG = "참가자의 이름은 공백일 수 없습니다.";
 	private static final String NAME_LENGTH_ERROR_MSG = "참가자의 이름은 1글자 이상 5글자 이하여야 한다.";
+	private static final String CANNOT_FIND_PARTICIPANT_MESSAGE = "해당 이름의 참가자는 없습니다.";
 	private final static Map<String, Participant> cachedParticipant = new HashMap<>();
 
 	private final String name;
@@ -38,6 +39,12 @@ public class Participant implements Display {
 	private static void validateNameLength(final String name) {
 		if (MIN_NAME_LENGTH > name.length() || name.length() > MAX_NAME_LENGTH) {
 			throw new IllegalArgumentException(NAME_LENGTH_ERROR_MSG);
+		}
+	}
+
+	public static void contains(String name) {
+		if (!cachedParticipant.containsKey(name)) {
+			throw new IllegalArgumentException(CANNOT_FIND_PARTICIPANT_MESSAGE);
 		}
 	}
 
