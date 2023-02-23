017a8698b884a4d53cc46dc174bfbc3e0d9ab2cc
echo <echo.backend@gmail.com>
2023-02-23 13:33:20 +0900

    feat(Participants): 특정 인덱스 참여자 구하는 메서드 구현

--- src/main/java/domain/SequenceSwapper.java
+++ src/main/java/domain/SequenceSwapper.java
@@ -1,17 +1,28 @@
 package domain;
 
-import javax.sound.midi.Sequence;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SequenceSwapper {
+
+    private final static String INDEX_OUT_OF_BOUND_ERROR_MESSAGE = "교환 가능한 인덱스는 순서보다 1 작아야합니다.";
     private final List<Integer> sequence;
 
-    private SequenceSwapper(List<Integer> sequence){
-        this.sequence = sequence;
+    private SequenceSwapper(List<Integer> sequence) {
+        this.sequence = new ArrayList<>(sequence);
     }
 
-    public static SequenceSwapper of(List<Integer> sequence){
+    public static SequenceSwapper of(List<Integer> sequence) {
         return new SequenceSwapper(sequence);
+    }
+
+    public void swap(int index) {
+        if (index >= sequence.size() - 1) {
+            throw new IndexOutOfBoundsException(INDEX_OUT_OF_BOUND_ERROR_MESSAGE);
+        }
+        int temporary = sequence.get(index);
+        sequence.set(index, sequence.get(index + 1));
+        sequence.set(index + 1, temporary);
     }
 
     public List<Integer> getSequence() {
--- src/test/java/domain/SequenceSwapperTest.java
+++ src/test/java/domain/SequenceSwapperTest.java
@@ -1,12 +1,11 @@
 package domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class SequenceSwapperTest {
     @Test
