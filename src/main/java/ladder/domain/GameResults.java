2d4e27ec659410bb8b4be361211e1296a9bec7f7
chaewon121 <cw121@naver.com>
2023-02-23 16:50:30 +0900

    feat: 사다리 타고 내려가 결과를 저장하는 기능 구현

---
+++ src/main/java/ladder/domain/GameResults.java
@@ -0,0 +1,40 @@
+package ladder.domain;
+
+import ladder.domain.ladder.Bar;
+import ladder.domain.ladder.MoveDirection;
+import ladder.domain.ladder.Ladder;
+import ladder.domain.ladder.Line;
+import ladder.domain.people.Position;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class GameResults {
+    private final List<String> names;
+    private final List<String> results;
+    private final Ladder ladder;
+
+    public GameResults(List<String> names, List<String> results, Ladder ladder) {
+        this.names = names;
+        this.results = results;
+        this.ladder = ladder;
+    }
+
+    public HashMap<String, String> calculateGameResults() {
+        HashMap<String, String> gameResults = new HashMap<>();
+        for (int i = 0; i < names.size(); i++) {
+            Position position = new Position(i);
+            goDownLadder(ladder, position);
+            gameResults.put(names.get(i), results.get(position.getPosition()));
+        }
+        return gameResults;
+    }
+
+    public void goDownLadder(Ladder ladder, Position position) {
+        for (Line line : ladder.getLines()) {
+            Bar leftBar = line.getIndexBar(position.getPosition());
+            Bar rightBar = line.getIndexBar(position.getPosition() + 1);
+            position.movePosition(MoveDirection.directMove(leftBar, rightBar));
+        }
+    }
+}
