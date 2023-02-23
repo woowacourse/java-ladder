7811ffb02ffa1482f756eede065004d345fcce70
echo <echo.backend@gmail.com>
2023-02-23 20:34:41 +0900

    feat(InputView): 높이와 찾을 이름을 입력받는 메서드들 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -9,6 +9,7 @@ public class InputView {
 	private static final String PARTICIPANTS_NAMES_REQUEST_MSG = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
 	private static final String RESULTS_REQUEST_MSG = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
 	private static final String LADDER_HEIGHT_REQUEST_MSG = "최대 사다리 높이는 몇 개인가요?";
+	private static final String NAME_TO_FIND_REQUEST_MSG = "결과를 보고 싶은 사람은?";
 	private static final String NAME_NUMBER_ERROR_MSG = "최소 두 명 이상의 참가자 이름을 입력해야합니다.";
 	private static final String LAST_COMMA_ERROR_MSG = "마지막 참가자 이름이 입력되지 않았습니다.";
 	private static final String HEIGHT_NATURAL_NUMBER_ERROR_MSG = "사다리 높이는 자연수여야합니다.";
@@ -22,6 +23,21 @@ public class InputView {
 	public static String[] readNames() {
 		System.out.println(PARTICIPANTS_NAMES_REQUEST_MSG);
 		return parseListFromInput();
+	}
+
+	public static int readHeight() {
+		System.out.println(LADDER_HEIGHT_REQUEST_MSG);
+		String line = scanner.nextLine();
+		System.out.println();
+		validateIsNatural(line);
+		return Integer.parseInt(line);
+	}
+
+	public static String readNameToFind() {
+		System.out.println(NAME_TO_FIND_REQUEST_MSG);
+		String input = scanner.nextLine();
+		System.out.println();
+		return input;
 	}
 
 	private static String[] parseListFromInput() {
@@ -43,14 +59,6 @@ public class InputView {
 		if (items.length <= 1) {
 			throw new IllegalArgumentException(NAME_NUMBER_ERROR_MSG);
 		}
-	}
-
-	public static int readHeight() {
-		System.out.println(LADDER_HEIGHT_REQUEST_MSG);
-		String line = scanner.nextLine();
-		System.out.println();
-		validateIsNatural(line);
-		return Integer.parseInt(line);
 	}
 
 	private static void validateIsNatural(String line) {
