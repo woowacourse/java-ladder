38a2f4398f292754e9c93bb804cd381b91df6977
aiaiaiai1 <cvc100326@naver.com>
2023-02-23 17:35:08 +0900

    feat: Prizes 클래스 추가

---
+++ src/main/java/domain/Prizes.java
@@ -0,0 +1,31 @@
+package domain;
+
+import java.util.List;
+
+public class Prizes {
+    private final List<String> names;
+
+    public Prizes(List<String> names) {
+        validate(names);
+        this.names = names;
+    }
+
+    private void validate(List<String> names) {
+        for (String name : names) {
+            new Name(name);
+        }
+    }
+
+    public String getName(int index) {
+        return names.get(index);
+    }
+
+    public List<String> getNames() {
+        return names;
+    }
+
+    public boolean isSame(int count) {
+        return names.size() == count;
+    }
+
+}
