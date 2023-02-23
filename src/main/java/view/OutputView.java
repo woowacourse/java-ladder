3063382b9995baa725e478caed20651873642a64
kim0914 <kosa0914@naver.com>
2023-02-23 11:34:43 +0900

    feat(OutputView): 사다리 게임을 결과를 보여주는 메서드 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -3,12 +3,15 @@ package view;
 import domain.ladder.Ladder;
 import domain.ladder.Line;
 import domain.player.Player;
+import domain.player.Players;
 
 import java.util.List;
+import java.util.Map;
 
 public class OutputView {
 
-    private static final String RESULT_SHOW_MESSAGE = "실행결과";
+    private static final String RESULT_SHOW_MESSAGE = "사다리결과";
+    private static final String PLAYERS_RESULT_MESSAGE = "실행결과";
     private static final String LINE_BAR_MESSAGE = "|";
     private static final String LINE_SPACE_MESSAGE = "    ";
     private static final String PLAYER_SHOW_FORMAT = "%5s ";
@@ -32,11 +35,32 @@ public class OutputView {
         }
     }
 
+    public static void showPrizes(List<String> prizes) {
+        prizes.forEach(prize -> printMessageFormat(prize, PLAYER_SHOW_FORMAT));
+        System.out.println();
+    }
+
+    public static void showPlayerOutcome(String name, String prize) {
+        printMessage(PLAYERS_RESULT_MESSAGE);
+        printMessage(name + " : " + prize);
+    }
+
+    public static void showAllPlayersOutcome(Map<String, String> allResults) {
+        printMessage(PLAYERS_RESULT_MESSAGE);
+        for (Map.Entry<String, String> entry : allResults.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+    }
+
     private static void showPoints(List<Boolean> points) {
         System.out.print(LINE_SPACE_MESSAGE + LINE_BAR_MESSAGE);
         for (Boolean point : points) {
             printMessageFormat(Point.getPoint(point).getMessage() + LINE_BAR_MESSAGE, POINT_SHOW_FORMAT);
         }
+    }
+
+    private static void printMessage(String message) {
+        System.out.println(message);
     }
 
     private static void printMessageFormat(String playerName, String format) {
