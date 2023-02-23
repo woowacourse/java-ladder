0f2b1563f760476f56f65716e14b7d4c7f9b7c8d
moonchaewon <codnjs8989@gmail.com>
2023-02-23 18:06:04 +0900

    feat: implement method to validate blank prize

--- src/main/java/laddergame/domain/Prize.java
+++ src/main/java/laddergame/domain/Prize.java
@@ -4,6 +4,13 @@ public class Prize {
 
     private final String prize;
     public Prize(String prize) {
+        validatePrize(prize);
         this.prize = prize;
+    }
+
+    private void validatePrize(String prize) {
+        if (prize.isBlank()) {
+            throw new IllegalArgumentException("[ERROR] 사다리 상품은 공백이 아니여야 합니다.");
+        }
     }
 }
--- src/test/java/laddergame/domain/PrizeTest.java
+++ src/test/java/laddergame/domain/PrizeTest.java
@@ -3,6 +3,7 @@ package laddergame.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PrizeTest {
@@ -12,5 +13,13 @@ class PrizeTest {
     void prizeCreateTest() {
         String prize = "prize";
         assertDoesNotThrow(() -> new Prize(prize));
+    }
+
+    @Test
+    @DisplayName("공백으로 사다리 상품을 생성할 경우 예외가 발생한다.")
+    void prizeCreateExceptionTest() {
+        String prize = "  ";
+        assertThatThrownBy(() -> new Prize(prize))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
