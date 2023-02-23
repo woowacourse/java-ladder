07ef8b2ff94bc5ba95a10b0d1e26a633f5184388
song-wooseok <sws981202@gmail.com>
2023-02-23 17:21:18 +0900

    test: 플레이어가 움직이는 기능 테스트

--- src/test/java/domain/PlayerTest.java
+++ src/test/java/domain/PlayerTest.java
@@ -29,4 +29,24 @@ class PlayerTest {
                     .isEqualTo(startPosition);
         }
     }
+
+    @Nested
+    @DisplayName("움직일 때 ")
+    class MoveCase {
+
+        @Test
+        @DisplayName(" 받은 값 만큼 위치를 옮긴다.")
+        void givenPositionValue_thenMovePosition() {
+            //given
+            Player player = new Player(new Name("Pobi"), 0);
+
+            //then
+            player.move(1);
+
+            //then
+            assertThat(player)
+                    .extracting("currentColumnPosition")
+                    .isEqualTo(1);
+        }
+    }
 }
