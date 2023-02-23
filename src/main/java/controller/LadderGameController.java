0898bc4f5d2cf0c78838dfcdad2989a67d3c32e1
ingpyo <juninpyo726@gmail.com>
2023-02-23 22:51:26 +0900

    feat: 플레이어를 검색하기위한 반복 기능

--- src/main/java/controller/LadderGameController.java
+++ src/main/java/controller/LadderGameController.java
@@ -1,5 +1,7 @@
 package controller;
 
+import domain.LadderGame;
+import domain.LadderGameResult;
 import domain.ladder.Height;
 import domain.ladder.Ladder;
 import domain.player.Players;
@@ -9,9 +11,11 @@ import view.InputView;
 import view.OutputView;
 
 public class LadderGameController {
+    public static final String TOTAL_PLAYER_SEARCH = "all";
     private final InputView inputView;
     private final OutputView outputView;
     private final TrueOrFalseGenerator trueOrFalseGenerator;
+
 
     public LadderGameController(InputView inputView, OutputView outputView, TrueOrFalseGenerator trueOrFalseGenerator) {
         this.inputView = inputView;
@@ -24,6 +28,19 @@ public class LadderGameController {
         Products products = Products.generate(inputView.readProducts());
         Height height = new Height(inputView.readHeight());
         Ladder ladder = Ladder.generate(players, height, trueOrFalseGenerator);
-        outputView.printResult(players, ladder);
+        outputView.printResult(players, ladder, products);
+        LadderGame ladderGame = new LadderGame(players, products, ladder);
+        LadderGameResult ladderGameResult = new LadderGameResult(ladderGame.play());
+        ladderGameTotalResult(ladderGameResult);
+    }
+
+    private void ladderGameTotalResult(LadderGameResult ladderGameResult) {
+        String searchByPlayerName;
+        do {
+            searchByPlayerName = inputView.readResult();
+            outputView.printResultOfPlayerName(searchByPlayerName, ladderGameResult);
+        }
+        while (!searchByPlayerName.equals(TOTAL_PLAYER_SEARCH));
+
     }
 }
