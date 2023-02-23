fa48f4611e5ac0cf6a089b7793957854256b78b4
yenawee <yaena0319@naver.com>
2023-02-24 00:25:57 +0900

    style : 오타 수정

--- src/test/java/laddergame/domain/LineTest.java
+++ src/test/java/laddergame/domain/LineTest.java
@@ -15,7 +15,7 @@ class LineTest {
 
     @Nested
     @DisplayName("랜덤 값의 의한 라인 생성 테스트(첫번째 좌표는 이전 좌표가 없는것을 표시)")
-    class Creat {
+    class Create {
         @DisplayName("들어간대로 잘 생성되었는지 테스트")
         @Test
         void createTest() {
