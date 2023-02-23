7f0f38fce69e40fe3a660dd2d81cb238db657dea
Youngho <cyh6099@gmail.com>
2023-02-23 11:29:41 +0900

    test : 사다리 생성 테스트 작성

---
+++ src/test/java/domain/RandomLadderGeneratorTest.java
@@ -0,0 +1,24 @@
+package domain;
+
+import dto.LineDTO;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RandomLadderGeneratorTest {
+
+    @DisplayName("생성된 사다리의 높이 너비는 인자로 넘어간 수와 같아야한다.")
+    @Test
+    void generateTest() {
+        final RandomLadderGenerator randomLadderGenerator = new RandomLadderGenerator();
+        final List<Line> ladder = randomLadderGenerator.generate(new Width(3), new Height(4));
+        assertThat(ladder.size()).isEqualTo(4);
+
+        final Line line = ladder.get(0);
+        final LineDTO lineDTO = line.getLineDTO();
+        assertThat(lineDTO.getLine().size()).isEqualTo(3);
+    }
+}
