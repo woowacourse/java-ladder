1107fff91d0d5d82b0b2a93ed75e6b71c384ef50
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-02-23 11:08:05 +0900

    refactor: private 생성자 추가
    유틸 클래스 생성 방지를 위해

--- src/main/java/util/StringUtil.java
+++ src/main/java/util/StringUtil.java
@@ -1,6 +1,9 @@
 package util;
 
 public class StringUtil {
+
+    private StringUtil() {}
+
     public static boolean isNullOrBlank(String str) {
         return str == null || str.isBlank();
     }
