26a514596e392382fc1a6c013d74d767f1a8f33b
hanueleee <ezzeezz@naver.com>
2023-02-23 14:26:38 +0900

    test: 사다리를 타고난 후의 위치값을 제대로 반환하는지 테스트

--- src/test/java/domain/LadderTest.java
+++ src/test/java/domain/LadderTest.java
@@ -1,40 +1,73 @@
 package domain;
 
+import static org.assertj.core.api.Assertions.*;
+
 import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import util.TestGenerator;
 import util.TrueGenerator;
 
 public class LadderTest {
-    @Test
-    @DisplayName("하나의 다리에 사용자가 입력한 높이 만큼의 Line 이 생긴다")
-    void createLadderWithEmptyLines() {
-        //given
-        int personCount = 5;
-        int ladderHeight = 3;
-        Height height = new Height(ladderHeight);
-        Ladder ladder = new Ladder(height, personCount);
+    @Nested
+    @DisplayName("사다리 생성")
+    class CreateLadder {
+        @Test
+        @DisplayName("하나의 다리에 사용자가 입력한 높이 만큼의 Line 이 생긴다")
+        void createLadderWithEmptyLines() {
+            //given
+            int personCount = 5;
+            int ladderHeight = 3;
+            Height height = new Height(ladderHeight);
+            Ladder ladder = Ladder.of(height, personCount);
 
-        //when
-        //then
-        Assertions.assertThat(ladder.calculateTotalHeight()).isEqualTo(ladderHeight);
+            //when
+            //then
+            assertThat(ladder.calculateTotalHeight()).isEqualTo(ladderHeight);
+        }
+
+        @Test
+        @DisplayName("사다리 생성 테스트")
+        void generateLadder() {
+            //given
+            int personCount = 5;
+            Height height = new Height(2);
+            Ladder ladder = Ladder.of(height, personCount);
+
+            //when
+            ladder.generate(new TestGenerator(
+                    List.of(true, false, true, false,
+                            true, false, true, false)));
+
+            //then
+            assertThat(ladder.getStatus())
+                    .containsExactly(List.of(Bridge.EXIST, Bridge.EMPTY, Bridge.EXIST, Bridge.EMPTY),
+                            List.of(Bridge.EXIST, Bridge.EMPTY, Bridge.EXIST, Bridge.EMPTY));
+        }
     }
 
-    @Test
-    @DisplayName("사다리 생성 테스트")
-    void generateLadder() {
-        //given
-        int personCount = 5;
-        Height height = new Height(2);
-        Ladder ladder = new Ladder(height, personCount);
+    @Nested
+    @DisplayName("사다리 게임")
+    class climbLadder {
+        @Test
+        @DisplayName("사다리를 타고난 후의 위치값을 제대로 반환하는지 테스")
+        void climbTest() {
+            //given
+            int personCount = 5;
+            Height height = new Height(3);
+            Ladder ladder = Ladder.of(height, personCount);
 
-        //when
-        ladder.generate(new TrueGenerator());
+            //when
+            ladder.generate(new TestGenerator(
+                    List.of(true, false, true, false, false, true, false, true, false, false, true, false)));
 
-        //then
-        Assertions.assertThat(ladder.getStatus())
-                .containsExactly(List.of(Bridge.EXIST, Bridge.EMPTY, Bridge.EXIST, Bridge.EMPTY),
-                        List.of(Bridge.EXIST, Bridge.EMPTY, Bridge.EXIST, Bridge.EMPTY));
+            //then
+            assertThat(ladder.findFinalPosition(0))
+                    .isEqualTo(3);
+            assertThat(ladder.findFinalPosition(1))
+                    .isEqualTo(0);
+        }
     }
 }
