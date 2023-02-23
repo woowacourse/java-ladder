16210c3ce28213a96593e61c90202b54dde37f7c
iamjooon2 <iamjooon2@gmail.com>
2023-02-23 16:29:08 +0900

    feat: 모든 사용자의 결과를 출력하는 명령어인 all을 이름으로 금지하는 기능 구현

--- src/main/java/domain/Name.java
+++ src/main/java/domain/Name.java
@@ -3,17 +3,31 @@ package domain;
 public class Name {
     private static final int NAME_MAX_LENGTH_INCLUSIVE = 5;
     private static final int NAME_MIN_LENGTH_INCLUSIVE = 1;
+    private static final String BANNED_NAME = "all";
     private static final String ERROR_NAME_LENGTH =
             "[ERROR] 이름의 길이는 " + NAME_MIN_LENGTH_INCLUSIVE + "~" + NAME_MAX_LENGTH_INCLUSIVE + "자 내로 입력 가능합니다";
+    private static final String ERROR_NAME_IN_BAN_LIST =
+            "[ERROR] " + BANNED_NAME + "은 이름으로 사용하실 수 없습니다";
 
     private final String value;
 
     public Name(final String name) {
-        validateNameSize(name);
+        validateName(name);
         this.value = name;
     }
 
-    private static void validateNameSize(String name) {
+    private void validateName(String name) {
+        validateBannedName(name);
+        validateNameSize(name);
+    }
+
+    private void validateBannedName(String name) {
+        if (name.equals(BANNED_NAME)) {
+            throw new IllegalArgumentException(ERROR_NAME_IN_BAN_LIST);
+        }
+    }
+
+    private void validateNameSize(String name) {
         if (name.length() < NAME_MIN_LENGTH_INCLUSIVE || name.length() > NAME_MAX_LENGTH_INCLUSIVE) {
             throw new IllegalArgumentException(ERROR_NAME_LENGTH);
         }
--- src/test/java/domain/NameTest.java
+++ src/test/java/domain/NameTest.java
@@ -7,6 +7,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -33,5 +34,12 @@ public class NameTest {
     void 생성_시_파라미터로_받은_이름을_값으로_가진다(final String input) {
         Name name = new Name(input);
         Assertions.assertThat(name.getValue()).isEqualTo(input);
+    }
+
+    @Test
+    void all_을_원시값으로_가질_수_없다() {
+        String bannedName = "all";
+        assertThatThrownBy(() -> new Name(bannedName))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
