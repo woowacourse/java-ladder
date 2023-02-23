cdebb497735cb88daa0883b73abf8da5ce22a9d1
hanueleee <ezzeezz@naver.com>
2023-02-23 14:55:57 +0900

    feat: 결과를 보고 싶은 사람의 이름을 입력하는 기능 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,11 +1,13 @@
 package view;
 
 import java.util.Scanner;
+현import java.util.Stack;
 
 public class InputView {
     private static final String REQUEST_NAME = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
-    private static final String REQUEST_LADDER_HEIGHT = "최대 사다리 높이는 몇 개인가요?";
-    private static final String REQUEST_LADDER_RESULT = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
+    private static final String REQUEST_LADDER_HEIGHT = "\n최대 사다리 높이는 몇 개인가요?";
+    private static final String REQUEST_LADDER_PRIZE = "\n실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
+    private static final String REQUEST_SHOWING_NAME = "\n결과를 보고 싶은 사람은?";
     private static final Scanner scanner = new Scanner(System.in);
 
     public InputView() {
@@ -21,8 +23,13 @@ public class InputView {
         return scanner.nextLine();
     }
 
-    public String requestResult() {
-        System.out.println(REQUEST_LADDER_RESULT);
+    public String requestPrize() {
+        System.out.println(REQUEST_LADDER_PRIZE);
+        return scanner.nextLine();
+    }
+
+    public String requestShowingName() {
+        System.out.println(REQUEST_SHOWING_NAME);
         return scanner.nextLine();
     }
 }
