d2fdc3eb73a62585df82686549cad6a86d69c9fd
hanueleee <ezzeezz@naver.com>
2023-02-23 14:55:10 +0900

    feat: 이름과 결과를 매칭하는 기능 구현

--- src/main/java/domain/LadderGame.java
+++ src/main/java/domain/LadderGame.java
@@ -1,30 +1,44 @@
 package domain;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import util.BridgeGenerator;
 import util.RandomBridgeGenerator;
 
 public class LadderGame {
     private final Persons persons;
     private final Ladder ladder;
+    private final List<String> prizes;
 
     private final BridgeGenerator bridgeGenerator;
 
-    public LadderGame(Persons persons, Height height) {
-        this.persons = persons;
-        this.ladder = new Ladder(height, persons.getTotalPersonCount());
+    public LadderGame(List<String> names, Height height, List<String> prizes) {
+        this.persons = Persons.from(names);
+        this.ladder = Ladder.of(height, persons.getTotalPersonCount());
         this.bridgeGenerator = new RandomBridgeGenerator();
+        this.prizes = prizes;
     }
 
     public void run() {
         ladder.generate(bridgeGenerator);
     }
 
+    public Map<String, String> getGameResults() {
+        Map<String, String> mappingResult = new HashMap<>();
+        for (int i = 0; i < prizes.size(); i++) {
+            String name = persons.findPersonNameInPosition(i);
+            String prize = prizes.get(i);
+            mappingResult.put(name, prize);
+        }
+        return mappingResult;
+    }
+    
     public List<List<Bridge>> getLadderStatus() {
         return ladder.getStatus();
     }
 
-    public List<String> getAllPlayers() {
-        return persons.getAllPersonNames();
+    public List<String> getAllPlayerNames() {
+        return persons.getAllPersonName();
     }
 }
