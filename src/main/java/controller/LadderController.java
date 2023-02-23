ddda6be5b9f18e0215ba9384019b93148cdf6a13
ChoiYoungHoon <dudgns2865@naver.com>
2023-02-23 14:36:30 +0900

    feat: controller 흐름 구현

--- src/main/java/controller/LadderController.java
+++ src/main/java/controller/LadderController.java
@@ -2,38 +2,62 @@ package controller;
 
 import common.ExecuteContext;
 import domain.model.Ladder;
-import domain.model.Layer;
-import domain.model.PassGenerator;
+import domain.service.LadderGameSupport;
 import domain.vo.Height;
 import domain.vo.Name;
+import domain.vo.Result;
 import domain.vo.Width;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import view.InputView;
 import view.OutputView;
 
 public class LadderController {
 
+    public static final String NAME_RESULT_COUNT_NOT_MATCH_MESSAGE = "결과 개수와 이름의 개수가 일치하지 않습니다.";
     private static final int NAMES_WIDTH_DIFFERENCE = 1;
+    private static final String ALL = "all";
+    public static final String NAME_NOT_FOUND = "해당 이름이 존재하지 않습니다.";
     private final InputView inputView;
     private final OutputView outputView;
-    private final PassGenerator passGenerator;
+    private final LadderGameSupport ladderGameSupport;
 
     public LadderController(InputView inputView, OutputView outputView,
-        PassGenerator passGenerator) {
+        LadderGameSupport ladderGameSupport) {
         this.inputView = inputView;
         this.outputView = outputView;
-        this.passGenerator = passGenerator;
+        this.ladderGameSupport = ladderGameSupport;
     }
 
     public void play() {
         List<Name> names = getNames();
+        List<Result> results = getResults(names.size());
         Height height = getHeight();
-        List<Layer> layers = makeEmptyLayers(height);
-        Ladder ladder = new Ladder(height, getWidth(names.size() - NAMES_WIDTH_DIFFERENCE), layers);
-        outputView.printResult(names, ladder);
+        Ladder ladder = ladderGameSupport.makeLadder(height,
+            new Width(names.size() - NAMES_WIDTH_DIFFERENCE));
+        outputView.printLadder(names, ladder, results);
+        Map<Name, Result> resultBoard = ladderGameSupport.makeResultBoard(ladder, names, results);
+        printResult(resultBoard);
+    }
+
+    private void printResult(Map<Name, Result> resultBoard) {
+        Name name = ExecuteContext.workWithExecuteStrategy(
+            () -> new Name(inputView.inputResultTarget()));
+        if (name.getValue().equals(ALL)) {
+            outputView.printAllResult(resultBoard);
+            return;
+        }
+        printOneResult(resultBoard, name);
+        printResult(resultBoard);
+    }
+
+    private void printOneResult(Map<Name, Result> resultBoard, Name name) {
+        if (resultBoard.containsKey(name)) {
+            outputView.printResult(resultBoard.get(name));
+            return;
+        }
+        throw new IllegalArgumentException(NAME_NOT_FOUND);
     }
 
     private List<Name> getNames() {
@@ -48,14 +72,21 @@ public class LadderController {
             () -> new Height(inputView.inputLadderHeight()));
     }
 
-    private List<Layer> makeEmptyLayers(Height height) {
-        return IntStream.range(0, height.getValue())
-            .mapToObj(index -> new Layer(new ArrayList<>(), passGenerator))
-            .collect(Collectors.toList());
+    private List<Result> getResults(final int namesSize) {
+        return ExecuteContext.workWithExecuteStrategy(() -> {
+                List<Result> results = inputView.inputResults()
+                    .stream()
+                    .map(Result::new)
+                    .collect(Collectors.toList());
+                checkNamesAndResultsSize(namesSize, results);
+                return results;
+            }
+        );
     }
 
-    private Width getWidth(final int size) {
-        return ExecuteContext.workWithExecuteStrategy(() -> new Width(size));
+    private static void checkNamesAndResultsSize(int namesSize, List<Result> results) {
+        if (results.size() != namesSize) {
+            throw new IllegalArgumentException(NAME_RESULT_COUNT_NOT_MATCH_MESSAGE);
+        }
     }
-
 }
