0a19e8189d981b01de00a9d9674d59a30091d936
eunbii0213 <eunbii0213@naver.com>
2023-02-24 02:16:00 +0900

    feat: Reward 클래스 검증 기능 추가

--- src/main/java/domain/Reward.java
+++ src/main/java/domain/Reward.java
@@ -1,10 +1,31 @@
 package domain;
 
 public class Reward {
+    public static final String BLANK_ERROR_GUIDE_MESSAGE = "[ERROR] 실행 결과는 빈 칸이 올 수 없습니다";
+    private static final String REWARDS_LENGTH_ERROR_MESSAGE = "[ERROR] 실행 결과는 5자 이하로 입력해주세요.";
+    public static final int MAX_NAME_LENGTH = 5;
     private final String reward;
 
     public Reward(String reward) {
+        validateReward(reward);
         this.reward = reward;
+    }
+
+    private void validateReward(String reward) {
+        validateBlank(reward);
+        validateLength(reward);
+    }
+
+    private void validateBlank(String reward) {
+        if (reward.isBlank() || reward.isEmpty()) {
+            throw new IllegalArgumentException(BLANK_ERROR_GUIDE_MESSAGE);
+        }
+    }
+
+    private void validateLength(String reward) {
+        if (reward.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException(REWARDS_LENGTH_ERROR_MESSAGE);
+        }
     }
 
     public String getReward() {
