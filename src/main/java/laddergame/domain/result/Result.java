a89ea7302df058b624b3a5c24397930425488353
chanminmaxscaler <jcm04@naver.com>
2023-02-23 16:15:40 +0900

    feat: 실행 결과에 공백 포함 여부를 검증하는 로직을 구현한다

--- src/main/java/laddergame/domain/result/Result.java
+++ src/main/java/laddergame/domain/result/Result.java
@@ -2,16 +2,24 @@ package laddergame.domain.result;
 
 public class Result {
 
+    public static final String INVALID_INCLUSION = " ";
     private final String result;
 
     public Result(final String result) {
         validateNullOrEmpty(result);
+        validateBlank(result);
         this.result = result;
     }
 
     private void validateNullOrEmpty(final String result) {
         if (result == null || result.isBlank()) {
             throw new IllegalArgumentException("[ERROR] 실행 결과는 null 이거나 빈 값일 수 없습니다.");
+        }
+    }
+
+    private void validateBlank(final String result) {
+        if (result.contains(INVALID_INCLUSION)) {
+            throw new IllegalArgumentException("[ERROR] 실행 결과에 공백이 포함될 수 없습니다.");
         }
     }
 }
--- src/test/java/laddergame/domain/result/ResultTest.java
+++ src/test/java/laddergame/domain/result/ResultTest.java
@@ -3,10 +3,13 @@ package laddergame.domain.result;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ResultTest {
+
+    private static final String ERROR_MESSAGE_HEAD = "[ERROR]";
 
     @ParameterizedTest
     @NullAndEmptySource
@@ -14,6 +17,15 @@ class ResultTest {
     void throws_exception_if_result_is_null_or_empty(String invalidResult) {
         assertThatThrownBy(() -> new Result(invalidResult))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR]");
+                .hasMessageContaining(ERROR_MESSAGE_HEAD);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {" 꽝", "꽝 ", " 꽝 "})
+    @DisplayName("실행 결과에 공백이 포함되면, 예외가 발생한다.")
+    void throws_exception_if_result_has_blank(String invalidResult) {
+        assertThatThrownBy(() -> new Result(invalidResult))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE_HEAD);
     }
 }
