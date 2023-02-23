542e904303b289f092afebc21dd96911dd742454
seokjin8678 <seokjin8678@gmail.com>
2023-02-23 22:38:02 +0900

    fix: 누락된 문자 추가

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -11,7 +11,7 @@ public class InputView {
     }
 
     public static String readPrizes() {
-        System.out.println("실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요");
+        System.out.println("실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
         return scanner.nextLine();
     }
 
