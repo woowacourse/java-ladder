c0dca93d17e754f05fcc9f37fbbeb7a3bcdbeea7
Kong-hana01 <gksqlsl11@khu.ac.kr>
2023-02-23 16:02:40 +0900

    feat: InputView 내에서 실행 결과를 입력받는 메서드 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -11,8 +11,10 @@ public class InputView {
 
     private final String READ_NAMES_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     private final String READ_HEIGHT_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
+    private final String READ_PRIZES_MESSAGE = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
     private final String NOT_INTEGER_ERROR_MESSAGE = "사다리의 높이는 숫자여야 합니다.";
     private final String NAME_DELIMITER = ",";
+    private final String PRIZE_DELIMITER = ",";
     private final Scanner scanner = new Scanner(System.in);
 
     private InputView() { }
@@ -35,5 +37,11 @@ public class InputView {
         } catch (NumberFormatException exception) {
             throw new IllegalArgumentException(NOT_INTEGER_ERROR_MESSAGE);
         }
+    }
+
+    public List<String> readPrizes() {
+        System.out.println(READ_PRIZES_MESSAGE);
+        String line = scanner.nextLine();
+        return Parser.parse(line, PRIZE_DELIMITER);
     }
 }
