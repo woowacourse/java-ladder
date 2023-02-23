b6cc3c4531d620474e6c89ba0f406d4406fee129
cpot5620 <=cpot5620@naver.com>
2023-02-23 15:57:48 +0900

    refactor(Result): 결과 저장 변수 자료구조 수정

--- src/main/java/ladder/domain/Result.java
+++ src/main/java/ladder/domain/Result.java
@@ -2,50 +2,70 @@ package ladder.domain;
 
 import ladder.error.ErrorMessage;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.*;
 
 public class Result {
-    private final List<Bet> result;
+    private final Map<Name, Bet> result = new LinkedHashMap<>();
 
-    public Result(Names names, List<String> bets) {
+    public Result(Names names, Bets bets) {
         validate(names, bets);
 
-        this.result = bets.stream()
-                .map(Bet::new)
-                .collect(Collectors.toList());
+        for (int i = 0; i < names.size(); i++) {
+            result.put(names.getNames().get(i), bets.getBets().get(i));
+        }
     }
 
-    private void validate(Names names, List<String> bets) {
+    private void validate(Names names, Bets bets) {
         if (names.size() != bets.size())
             throw new IllegalArgumentException(ErrorMessage.DIFFERENT_PARTICIPANTS_AND_BETS_COUNT.getMessage());
     }
 
     public void perform(Ladder ladder) {
+        List<Name> names = List.copyOf(result.keySet());
+        List<Bet> bets = new ArrayList<>(result.values());
+
         for (int i = ladder.getLadder().size() - 1; i >= 0; i--) {
-            changeBetIndex(ladder.getLadder().get(i));
+            Line line = ladder.getLadder().get(i);
+            performByLine(line, bets);
         }
+
+        update(names, bets);
     }
 
-    private void changeBetIndex(Line line) {
+    private void performByLine(Line line, List<Bet> bets) {
         for (int i = 0; i < line.size(); i++) {
-            trySwapBetAtNextIndex(i, line.isExistLineAtCell(i));
+            swapBetIfExistLine(bets, i, line.existLineAtCell(i));
         }
     }
 
-    private void trySwapBetAtNextIndex(int index, boolean exist) {
-        if (exist) {
-            Collections.swap(result, index, index + 1);
+    private void swapBetIfExistLine(List<Bet> bets, int index, boolean exist) {
+        if (!exist) {
+            return;
+        }
+
+        Collections.swap(bets, index, index + 1);
+    }
+
+    private void update(List<Name> names, List<Bet> bets) {
+        for (int i = 0; i < names.size(); i++) {
+            result.put(names.get(i), bets.get(i));
         }
     }
 
-    public List<Bet> getResult() {
-        return Collections.unmodifiableList(result);
+    public Bet getBetByName(Name name) {
+        Bet bet = result.get(name);
+
+        if (bet == null)
+            throw new IllegalArgumentException(ErrorMessage.NAME_IS_NOT_EXIST.getMessage());
+
+        return bet;
     }
 
     public int size() {
         return result.size();
     }
 
+    public Map<Name, Bet> getResult() {
+        return Collections.unmodifiableMap(result);
+    }
 }
