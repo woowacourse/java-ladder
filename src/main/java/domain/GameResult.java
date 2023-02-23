3d54bd90437890b36208978caf26d7345c18b77d
splitube <voicesplit@daum.net>
2023-02-23 21:07:28 +0900

    fix: 출력 순서 보장을 위해 LinkedHashMap 사용

--- src/main/java/domain/GameResult.java
+++ src/main/java/domain/GameResult.java
@@ -1,12 +1,12 @@
 package domain;
 
 import domain.ladder.Block;
-import domain.ladder.LadderResult;
+import domain.ladder.LadderPrize;
 import domain.ladder.Line;
 import domain.participants.Participant;
 import exception.ladder.GameEndException;
 import exception.participants.NullNameException;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -19,20 +19,20 @@ public class GameResult {
     private static final int LEFT = -1;
     private static final int STAY = 0;
     private static final int RIGHT = 1;
-    private final Map<Participant, LadderResult> results;
+    private final Map<Participant, LadderPrize> results;
 
-    private GameResult(Map<Participant, LadderResult> results) {
+    private GameResult(Map<Participant, LadderPrize> results) {
         this.results = results;
     }
 
     public static GameResult from(LadderGame ladderGame) {
-        Map<Participant, LadderResult> result = makeResults(ladderGame);
+        Map<Participant, LadderPrize> result = makeResults(ladderGame);
         return new GameResult(result);
     }
 
-    private static Map<Participant, LadderResult> makeResults(LadderGame ladderGame) {
+    private static Map<Participant, LadderPrize> makeResults(LadderGame ladderGame) {
         List<Participant> participants = ladderGame.getParticipants();
-        Map<Participant, LadderResult> results = new HashMap<>();
+        Map<Participant, LadderPrize> results = new LinkedHashMap<>();
         participants.forEach((participant) -> {
             final int order = participants.indexOf(participant);
             results.put(participant, getGameResultByOrder(ladderGame, order));
@@ -40,7 +40,7 @@ public class GameResult {
         return results;
     }
 
-    private static LadderResult getGameResultByOrder(LadderGame ladderGame, int order) {
+    private static LadderPrize getGameResultByOrder(LadderGame ladderGame, int order) {
         for (Line line : ladderGame.getLines()) {
             int move = getMove(order, line.getBlocks());
             order += move;
@@ -71,12 +71,11 @@ public class GameResult {
         return STAY;
     }
 
-    public LadderResult getResultByName(final String name) {
+    public LadderPrize getResultByName(final String name) {
         if (name.equals("exit")) {
             throw new GameEndException();
         }
-        Optional<Participant> findParticipant = results.keySet()
-            .stream()
+        Optional<Participant> findParticipant = results.keySet().stream()
             .filter((participant) -> participant.getName().equals(name))
             .findFirst();
         if (findParticipant.isEmpty()) {
@@ -86,7 +85,7 @@ public class GameResult {
     }
 
 
-    public Map<Participant, LadderResult> getResults() {
+    public Map<Participant, LadderPrize> getResults() {
         return results;
     }
 }
