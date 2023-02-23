605cb229dbb8a8285210d08c074f91063b5e983d
odo27 <odo27@odo27ui-MacBookAir.local>
2023-02-23 21:57:06 +0900

    feat: 사용할 수 없는 이름 추가

--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -2,10 +2,24 @@ package domain;
 
 public class Player {
 
+    private static final String ALL = "all";
+    private static final String FORBIDDEN_NAME_MESSAGE = "all은 사용할 수 없는 이름입니다.";
+
     private final Name name;
 
     public Player(final String name) {
+        validate(name);
         this.name = new Name(name);
+    }
+
+    private void validate(final String name) {
+        validateForbidden(name);
+    }
+
+    private void validateForbidden(final String name) {
+        if (name.equals(ALL)) {
+            throw new IllegalArgumentException(FORBIDDEN_NAME_MESSAGE);
+        }
     }
 
     public boolean hasSameName(final Name name) {
