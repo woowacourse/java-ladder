c1a7745b10f117f1457d24e99489a8e621f0bb67
chaewon121 <cw121@naver.com>
2023-02-23 17:53:04 +0900

    feat: 결과 보고싶은 사람 입력받는 기능 구현

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -2,6 +2,7 @@ package ladder.view;
 
 import ladder.validator.CommonValidator;
 
+import java.util.List;
 import java.util.Scanner;
 import java.util.function.Supplier;
 import java.util.regex.Matcher;
@@ -11,7 +12,8 @@ public class InputView {
     private static final Scanner sc = new Scanner(System.in);
     private static final Pattern notNumberPattern = Pattern.compile("\\D");
 
-    private InputView(){}
+    private InputView() {
+    }
 
     public static String inputPeopleNames() {
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
@@ -20,11 +22,32 @@ public class InputView {
         return inputPeopleNames;
     }
 
+    public static String inputResults() {
+        System.out.println("실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
+        String inputResults = sc.nextLine();
+        validateBlank(inputResults);
+        return inputResults;
+    }
+
     public static int inputLadderHeight() {
         System.out.println("최대 사다리 높이는 몇 개인가요?");
         String ladderHeight = sc.nextLine();
         validateLadderHeight(ladderHeight);
         return Integer.parseInt(ladderHeight);
+    }
+
+    public static String inputWantGameResults(List<String> namesList) {
+        System.out.println("결과를 보고 싶은 사람은?");
+        String inputWantGameResults = sc.nextLine();
+        validateBlank(inputWantGameResults);
+        validateContainName(namesList, inputWantGameResults);
+        return inputWantGameResults;
+    }
+
+    private static void validateContainName(List<String> namesList, String inputWantGameResults) {
+        if (!namesList.contains(inputWantGameResults) && !inputWantGameResults.equals("all")) {
+            throw new IllegalArgumentException("해당하는 이름이 존재하지 않습니다.");
+        }
     }
 
     private static void validateLadderHeight(String ladderHeight) {
