07154adabf1510a40cdab2d3444b283c8f1b5bee
2014012606 <2014012606@hanyang.ac.kr>
2023-02-23 13:09:42 +0900

    chore: 변경될 필요 없는 파라미터에 final 접근제한자 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -16,30 +16,30 @@ public class OutputView {
         System.out.println();
     }
 
-    public static void printUserNames(List<String> userNames) {
+    public static void printUserNames(final List<String> userNames) {
         String formattedUserNames = formatUserNames(userNames);
         System.out.println(formattedUserNames);
     }
 
-    public static void printLadder(List<List<Boolean>> lines) {
+    public static void printLadder(final List<List<Boolean>> lines) {
         for (List<Boolean> line : lines) {
             System.out.println(renderLine(line));
         }
     }
 
-    public static void printResultNames(List<String> resultNames) {
+    public static void printResultNames(final List<String> resultNames) {
         String formattedResultNames = formatResultNames(resultNames);
         System.out.println(formattedResultNames);
         System.out.println();
     }
 
-    public static void printUserResult(String result) {
+    public static void printUserResult(final String result) {
         System.out.println(RESULT_MESSAGE);
         System.out.println(result);
         System.out.println();
     }
 
-    public static void printAllResult(List<String> userNames, List<String> resultNames) {
+    public static void printAllResult(final List<String> userNames, final List<String> resultNames) {
         System.out.println(RESULT_MESSAGE);
         for (int index = 0; index < userNames.size(); index++) {
             System.out.printf("%s : %s", userNames.get(index), resultNames.get(index));
@@ -47,7 +47,7 @@ public class OutputView {
         }
     }
 
-    private static String formatUserNames(List<String> userNames) {
+    private static String formatUserNames(final List<String> userNames) {
         StringBuilder builder = new StringBuilder();
         for (String userName : userNames) {
             String formattedUserName = String.format("%9s", userName);
@@ -56,7 +56,7 @@ public class OutputView {
         return builder.toString();
     }
 
-    private static String renderLine(List<Boolean> line) {
+    private static String renderLine(final List<Boolean> line) {
         StringBuilder builder = new StringBuilder();
         builder.append(BLANK_LINE);
         builder.append(LADDER);
@@ -67,7 +67,7 @@ public class OutputView {
         return builder.toString();
     }
 
-    private static void renderLink(StringBuilder builder, boolean connected) {
+    private static void renderLink(final StringBuilder builder, final boolean connected) {
         if (connected) {
             builder.append(CONNECTED_LINE);
             return;
@@ -75,7 +75,7 @@ public class OutputView {
         builder.append(BLANK_LINE);
     }
 
-    private static String formatResultNames(List<String> resultNames) {
+    private static String formatResultNames(final List<String> resultNames) {
         StringBuilder builder = new StringBuilder();
         for (String resultName : resultNames) {
             String formattedResultName = String.format("%9s", resultName);
