ee0f1bfa94a2f8629268ed1a654abae47ce2d07b
jeonghoon <shb03323@naver.com>
2023-02-23 14:15:10 +0900

    feat: 플레이어 이름이 all인 경우에 예외 처리 기능 추가

--- src/main/java/ladder/domain/player/PlayerName.java
+++ src/main/java/ladder/domain/player/PlayerName.java
@@ -1,22 +1,31 @@
 package ladder.domain.player;
 
+import ladder.domain.player.exception.ForbiddenPlayerNameException;
 import ladder.domain.player.exception.PlayerNameLengthException;
 
 public class PlayerName {
 
     private static final int MINIMUM_NAME_LENGTH = 1;
     private static final int MAXIMUM_NAME_LENGTH = 5;
+    private static final String FORBIDDEN_NAME = "all";
 
     private final String name;
 
     public PlayerName(final String name) {
         validatePlayerName(name);
+        validateForbiddenName(name);
         this.name = name;
     }
 
     private void validatePlayerName(final String name) {
         if (name.length() < MINIMUM_NAME_LENGTH || name.length() > MAXIMUM_NAME_LENGTH) {
             throw new PlayerNameLengthException();
+        }
+    }
+
+    private void validateForbiddenName(final String name) {
+        if (name.equals(FORBIDDEN_NAME)) {
+            throw new ForbiddenPlayerNameException();
         }
     }
 
--- src/test/java/ladder/domain/PlayerNameTest.java
+++ src/test/java/ladder/domain/PlayerNameTest.java
@@ -1,5 +1,6 @@
 package ladder.domain;
 
+import ladder.common.CustomException;
 import ladder.domain.player.PlayerName;
 import ladder.domain.player.exception.PlayerNameLengthException;
 import org.junit.jupiter.api.Test;
@@ -25,5 +26,11 @@ class PlayerNameTest {
     void 플레이어_이름이_1자_미만이면_예외_발생() {
         assertThatThrownBy(() -> new PlayerName(""))
                 .isInstanceOf(PlayerNameLengthException.class);
+    }
+
+    @Test
+    void 플레이어_이름이_all_이면_예외_발생() {
+        assertThatThrownBy(() -> new PlayerName("all"))
+                .isInstanceOf(CustomException.class);
     }
 }
