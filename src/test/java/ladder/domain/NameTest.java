e0100586673dc4b95a55566329a95bd65f41b4ed
drunkenhw <drunkenhw@gmail.com>
2023-02-23 15:28:05 +0900

    refactor: import 수정

--- src/test/java/ladder/domain/NameTest.java
+++ src/test/java/ladder/domain/NameTest.java
@@ -1,7 +1,7 @@
 package ladder.domain;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
