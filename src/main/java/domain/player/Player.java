418637c0fd3e833f5eed31ed545c0967fc30c1f5
kwonyj1022 <kwonyj1022@naver.com>
2023-02-23 18:50:36 +0900

    feat(Player): Player가 사다리를 타고 내려가서 당첨 결과를 얻는 메서드 추가

--- src/main/java/domain/player/Player.java
+++ src/main/java/domain/player/Player.java
@@ -1,13 +1,17 @@
 package domain.player;
 
 import domain.Position;
+import domain.item.Item;
+import domain.ladder.Bridge;
+import domain.ladder.Line;
 
+import java.util.List;
 import java.util.Objects;
 
 public class Player {
 
     private final Name name;
-    private Position position;
+    private final Position position;
     private String result;
 
     public Player(String name, int index) {
@@ -15,6 +19,45 @@ public class Player {
         this.position = new Position(index);
     }
 
+    public void goToResult(List<Line> ladder, List<Item> items) {
+        int ladderHeight = ladder.size();
+        for (int i = 0; i < ladderHeight; i++) {
+            Line targetLine = ladder.get(position.getY());
+            findPath(targetLine.getBridges());
+        }
+        int itemIndex = position.getX();
+        this.result = items.get(itemIndex).getItem();
+    }
+
+    private void findPath(List<Bridge> targetLine) {
+        if (isLeftPassable(targetLine)) {
+            position.moveLeft();
+            return;
+        }
+        if (isRightPassable(targetLine)) {
+            position.moveRight();
+            return;
+        }
+        position.moveDown();
+    }
+
+    private boolean isLeftPassable(List<Bridge> targetLine) {
+        if (position.getX() == 0)   // 가장 왼쪽에 있다면 왼쪽으로 못 감
+            return false;
+
+        // 왼쪽에 다리가 있는지는 targetLine의 x-1번째 요소가 Passable인지 확인
+        Bridge left = targetLine.get(position.getX() - 1);
+        return left.isPassable();
+    }
+
+    private boolean isRightPassable(List<Bridge> targetLine) {
+        if (position.getX() == targetLine.size())   // 가장 오른쪽에 있다면 오른쪽으로 못 감
+            return false;
+
+        // 오른쪽에 다리가 있는지는 targetLine의 x번째 요소가 Passable인지 확인
+        Bridge right = targetLine.get(position.getX());
+        return right.isPassable();
+    }
 
     public String getName() {
         return name.getName();
