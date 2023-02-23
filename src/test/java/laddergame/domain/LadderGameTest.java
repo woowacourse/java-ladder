2995f548de4d9692a1f39bae7aa5bbcd7ac56c86
yoondgu <doyou2ing@naver.com>
2023-02-23 17:00:07 +0900

    test: 사다리 미생성 시 결과 확인 예외 테스트 추가

--- src/test/java/laddergame/domain/LadderGameTest.java
+++ src/test/java/laddergame/domain/LadderGameTest.java
@@ -9,10 +9,11 @@ import org.junit.jupiter.api.Test;
 class LadderGameTest {
 
     @Test
-    @DisplayName("참여자 수가 2 미만이면 예외를 던진다.")
-    void should_ThrowException_When_PlayersCountLessThan2() {
-        assertThatThrownBy(() -> new LadderGame(List.of("name")))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("참여자는 2명 이상이어야 합니다.");
+    @DisplayName("사다리가 생성되지 않은 게임의 결과를 요청하면 예외를 던진다.")
+    void should_ThrowException_When_ComputeResultWithoutLadder() {
+        LadderGame ladderGame = new LadderGame(List.of("name1", "name2"));
+        assertThatThrownBy(ladderGame::computeResult)
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("아직 사다리가 생성되지 않은 게임입니다.");
     }
 }
