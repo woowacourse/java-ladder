db01219f12c397b5c63cf090b718cee1b9d214ea
kokodak <kokodakadokok@gmail.com>
2023-02-23 15:35:06 +0900

    test: 누락된 LadderGame 테스트 추가

--- src/test/java/domain/LadderGameTest.java
+++ src/test/java/domain/LadderGameTest.java
@@ -2,10 +2,13 @@ package domain;
 
 import domain.LadderTest.TestLadderRowGenerator;
 import java.util.List;
+import java.util.Map;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import utils.LadderRowGenerator;
 
 public class LadderGameTest {
@@ -30,5 +33,26 @@ public class LadderGameTest {
     void ladderGameTest() {
         Assertions.assertThatCode(() -> new LadderGame(ladder, users, results))
                 .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("play 메서드는 게임 진행 상태 값을 변경시킨다.")
+    void should_changeGameProgressStatus_when_play() {
+        LadderGame ladderGame = new LadderGame(ladder, users, results);
+
+        ladderGame.play();
+
+        Assertions.assertThat(ladderGame.inProgress()).isTrue();
+    }
+
+    @ParameterizedTest(name = "getLadderGameResultByName 메서드는 이름을 통해 결과를 찾을 수 있다.")
+    @CsvSource({"userA,1", "userB,4", "userC,3", "userD,2"})
+    void should_findLadderGameResultByName_when_getLadderGameResultByName(String userName, String resultName) {
+        LadderGame ladderGame = new LadderGame(ladder, users, results);
+
+        LadderGameResult ladderGameResult = ladderGame.getLadderGameResultByName(userName);
+        Result result = ladderGameResult.findByUser(new User(userName));
+
+        Assertions.assertThat(result.getResultName()).isEqualTo(resultName);
     }
 }
