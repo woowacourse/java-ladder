f382a6f7c57eaddc116b512f9a1341cf748fbc53
DESKTOP-FNVN0HH\USER <beatus96@naver.com>
2023-02-23 14:12:10 +0900

    test: Ladder 출발점에 배당된 도착점 반환 기능 테스트

--- src/test/java/ladder/domain/LadderTest.java
+++ src/test/java/ladder/domain/LadderTest.java
@@ -3,6 +3,8 @@ package ladder.domain;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.List;
 
@@ -11,22 +13,39 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class LadderTest {
     private int heightOfLadder;
     private int playerCount;
+    private Ladder ladder;
 
     @BeforeEach
     void setup() {
         heightOfLadder = 5;
         playerCount = 5;
+        ladder = Ladder.of(playerCount, heightOfLadder, new MockRandomDataGenerator());
     }
 
+    /**
+     * true,false,true,false,
+     * true,false,true,false,
+     * true,false,true,false,
+     * true,false,true,false,
+     * true,false,true,false,
+     */
     @Test
     @DisplayName("4*5 사이즈의 사다리가 생성되는지 확인한다")
     void ladderInitiatorTest() {
-        Ladder ladder = Ladder.of(playerCount, heightOfLadder, new MockRandomDataGenerator());
         List<Line> lines = ladder.getLinesOfLadder();
 
         int rowSize = lines.size();
         int columnSize = lines.get(0).getLine().size();
-        assertThat(rowSize == heightOfLadder && columnSize == playerCount - 1).isTrue();
 
+        assertThat(rowSize == heightOfLadder && columnSize == playerCount - 1).isTrue();
     }
+
+    @ParameterizedTest
+    @CsvSource(value = {"0,1", "1,0", "2,3", "3,2", "4,4"})
+    @DisplayName("사다리에 출발점을 넣으면 예상된 도착점을 반환한다")
+    void traceThePathTest(int start, int end) {
+        assertThat(ladder.findEndPositionFrom(start))
+                .isEqualTo(end);
+    }
+
 }
