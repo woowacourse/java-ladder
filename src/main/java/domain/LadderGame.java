2e6f06b9714238da9ae73bd1246f1569334e09ae
Kong-hana01 <gksqlsl11@khu.ac.kr>
2023-02-23 13:42:18 +0900

    feat: 사다리 게임을 진행하는 클래스 생성
    - TDD 사용

--- src/main/java/domain/LadderGame.java
+++ src/main/java/domain/LadderGame.java
@@ -1,2 +1,31 @@
-package domain;public class LadderGame {
+package domain;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LadderGame {
+
+    private final Ladder ladder;
+    private final Players players;
+
+    private final Map<String, Integer> result;
+
+    public LadderGame(Ladder ladder, Players players) {
+        this.ladder = ladder;
+        this.players = players;
+        result = new HashMap<>();
+    }
+
+    public Map<String, Integer> run() {
+        if (!result.isEmpty()) {
+            return result;
+        }
+        for (Line line: ladder.getLadder()) {
+            players.move(line);
+        }
+        for (Player player: players.getPlayers()) {
+            result.put(player.getName(), player.getPosition());
+        }
+        return result;
+    }
 }
--- src/test/java/domain/LadderGameTest.java
+++ src/test/java/domain/LadderGameTest.java
@@ -1,2 +1,32 @@
-package domain;public class LadderGameTest {
+package domain;
+
+import domain.numbergenerator.TestNumberGenerator;
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LadderGameTest {
+
+    @DisplayName("사다리가 주어졌을 때 연결구조에 맞게 사다리 게임 참가자의 위치가 변경된다.")
+    @Test
+    void runTest() {
+        String kong = "kong";
+        String odo = "odo";
+        String gray = "gray";
+        String kiara = "kiara";
+
+        Ladder ladder = new Ladder(4, 4, new TestNumberGenerator(Lists.newArrayList(1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0)));
+        Players players = new Players(List.of(kong, odo, gray, kiara));
+        LadderGame ladderGame = new LadderGame(ladder, players);
+        Map<String, Integer> result = ladderGame.run();
+        assertThat(result.get(kong)).isEqualTo(3);
+        assertThat(result.get(odo)).isEqualTo(1);
+        assertThat(result.get(gray)).isEqualTo(0);
+        assertThat(result.get(kiara)).isEqualTo(2);
+    }
 }
