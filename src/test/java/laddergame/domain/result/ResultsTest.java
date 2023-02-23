e8ee37f6570d4638c101215bafbfebaa1c56e0a2
chanminmaxscaler <jcm04@naver.com>
2023-02-23 16:41:41 +0900

    test: Results 객체를 생성할 때 예외가 발생하지 않는 테스트를 구현한다

--- src/test/java/laddergame/domain/result/ResultsTest.java
+++ src/test/java/laddergame/domain/result/ResultsTest.java
@@ -6,8 +6,16 @@ import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class ResultsTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {"꽝,당첨:2", "꽝,당첨,꽝:3", "꽝,꽝,꽝,당첨:4", "당첨,꽝,꽝,꽝,꽝:5"}, delimiter = ':')
+    @DisplayName("모든 실행 결과가 동일하지 않고 실행 결과 수가 참여자 수와 같으면, 예외가 발생하지 않는다.")
+    void does_not_throw_exception_if_results_contain_unique_value_and_results_are_the_same_size_as_participants(String resultNames, int participantsCount) {
+        assertDoesNotThrow(() -> new Results(resultNames, participantsCount));
+    }
 
     @ParameterizedTest
     @ValueSource(strings = {"꽝,꽝,당첨", "꽝,꽝,꽝,당첨", "꽝,꽝,꽝,꽝,꽝,당첨", "꽝,꽝,꽝,꽝,꽝,꽝,당첨"})
