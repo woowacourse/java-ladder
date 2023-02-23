47ce80a8024e727249e7373d4e4c40f3a1ec100b
Rosie <dev.kayoung@gmail.com>
2023-02-23 13:35:39 +0900

    feat: Point 클래스 구현
    * test: Point 객체가 비어있는지 확인하는 기능 테스트
    
    * test: 비어있음 여부로 Point 인스턴스를 생성하기

---
+++ src/main/java/laddergame/domain/Point.java
@@ -0,0 +1,23 @@
+package laddergame.domain;
+
+public enum Point {
+    FILLED(true),
+    EMPTY(false);
+
+    private final boolean isFilled;
+
+    Point(boolean isFilled) {
+        this.isFilled = isFilled;
+    }
+
+    public static Point of(boolean isFilled) {
+        if (isFilled) {
+            return FILLED;
+        }
+        return EMPTY;
+    }
+
+    public boolean isFilled() {
+        return isFilled;
+    }
+}
