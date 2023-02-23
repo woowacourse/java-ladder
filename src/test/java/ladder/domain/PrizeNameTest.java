a6d96e85e9e0d4b723c6ef773c7ce1fbe4baa86e
kevstevie <jianna442@gmail.com>
2023-02-23 16:03:46 +0900

    test: 테스트 케이스 보강

--- src/test/java/ladder/domain/PrizeNameTest.java
+++ src/test/java/ladder/domain/PrizeNameTest.java
@@ -13,15 +13,16 @@ class PrizeNameTest {
     @ValueSource(strings = {"", "  ", "여섯글자이름"})
     @DisplayName("상품 이름은 공백이거나 여섯 글자 이상일 수 없다.")
     void userNameExceptionTest(String value) {
-        assertThatThrownBy(() -> new UserName(value))
-                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new PrizeName(value))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("상품 이름은");
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"1", "다섯글자임", "  공백제외5  "})
     @DisplayName("상품 이름 유효성 검증 경계값을 검증하는 테스트")
     void validNameTest(String value) {
-        assertThatCode(() -> new UserName(value))
+        assertThatCode(() -> new PrizeName(value))
                 .doesNotThrowAnyException();
     }
 }
--- src/test/java/ladder/domain/UsersTest.java
+++ src/test/java/ladder/domain/UsersTest.java
@@ -2,10 +2,13 @@ package ladder.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 
 public class UsersTest {
@@ -42,4 +45,23 @@ public class UsersTest {
                 .hasMessageContaining("명 미만");
     }
 
+    @Test
+    @DisplayName("users 일급 컬렉션의 contain 테스트")
+    void containTest() {
+        var users = new Users(List.of("nameA", "nameB", "nameC"));
+
+        assertThat(users.contain("nameB")).isTrue();
+        assertThat(users.contain("nameD")).isFalse();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"0:user1", "1:user2", "2:user3"}, delimiter = ':')
+    @DisplayName("파라미터 인덱스에 해당하는 유저 이름을 반환한다.")
+    void getUserNameByIndex(int index, String name) {
+        var users = new Users(List.of("user1", "user2", "user3"));
+
+        String userNameByIndex = users.getUserNameByIndex(index);
+
+        assertThat(userNameByIndex).isEqualTo(name);
+    }
 }
