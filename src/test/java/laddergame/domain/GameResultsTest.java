399ffc18bebf91d1f827cc416efa7f456c0f8a20
hyunseo <goatsfish@gmail.com>
2023-02-23 10:50:09 +0900

    test: 최대 결과 길이 조회 기능 테스트 추가

--- src/test/java/laddergame/domain/GameResultsTest.java
+++ src/test/java/laddergame/domain/GameResultsTest.java
@@ -9,18 +9,19 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class ResultsTest {
+class GameResultsTest {
     @DisplayName("결과 목록이 null일 경우 예외가 발생한다.")
     @Test
     void throwExceptionWhenResultsIsNull() {
         final List<String> results = null;
 
-        assertThatThrownBy(() -> new Results(results, NamesFixture.getNamesSize3()));
+        assertThatThrownBy(() -> new GameResults(results, NamesFixture.getNamesSize3()));
     }
 
     @DisplayName("결과 목록이 비어있을 경우 예외가 발생한다.")
@@ -28,32 +29,43 @@ class ResultsTest {
     void throwExceptionWhenResultsIsEmpty() {
         final List<String> results = List.of();
 
-        assertThatThrownBy(() -> new Results(results, NamesFixture.getNamesSize3()));
+        assertThatThrownBy(() -> new GameResults(results, NamesFixture.getNamesSize3()));
     }
 
     @DisplayName("결과 목록이 2개 미만일 경우 예외가 발생한다.")
     @ParameterizedTest(name = "results = {0}")
     @MethodSource("wrongResultsParameterDummy")
     void throwExceptionWhenResultsHasSizeLessThan2(final List<String> inputResults) {
-        assertThatThrownBy(() -> new Results(inputResults, NamesFixture.getNamesSize3()))
+        assertThatThrownBy(() -> new GameResults(inputResults, NamesFixture.getNamesSize3()))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("결과 값 목록을 가져온다.")
+    @ParameterizedTest(name = "results = {0}, names = {1}")
+    @MethodSource("resultParametersDummy")
+    void getResultValues(final List<String> inputResults, final Names names) {
+        final GameResults gameResults = new GameResults(inputResults, names);
+        final List<String> findResults = gameResults.getResultValues();
+
+        assertThat(findResults).containsExactlyElementsOf(inputResults);
     }
 
     @DisplayName("결과 목록을 가져온다.")
     @ParameterizedTest(name = "results = {0}, names = {1}")
     @MethodSource("resultParametersDummy")
     void getResults(final List<String> inputResults, final Names names) {
-        final Results results = new Results(inputResults, names);
-        final List<String> findResults = results.getResults();
+        final GameResults gameResults = new GameResults(inputResults, names);
+        final List<Result> findResults = gameResults.getResults();
+        final List<String> findResultValues = findResults.stream().map(Result::getValue).collect(Collectors.toList());
 
-        assertThat(findResults).containsExactlyElementsOf(inputResults);
+        assertThat(findResultValues).containsExactlyElementsOf(inputResults);
     }
 
     @DisplayName("결과 목록에서 위치를 통해서 결과를 가져올 수 있다.")
     @Test
     void findResultByPosition() {
-        final Results results = new Results(List.of("hello", "helo", "heeeee"), NamesFixture.getNamesSize3());
-        final Result findResult = results.findResultByPosition(PositionFixture.createPositionZero());
+        final GameResults gameResults = new GameResults(List.of("hello", "helo", "heeeee"), NamesFixture.getNamesSize3());
+        final Result findResult = gameResults.findResultByPosition(PositionFixture.createPositionZero());
 
         assertThat(findResult.getValue()).isEqualTo("hello");
     }
@@ -61,7 +73,7 @@ class ResultsTest {
     @DisplayName("결과 목록 개수가 이름 목록 개수와 같지 않을 경우 예외가 발생한다.")
     @Test
     void throwExceptionWhenResultsAndNamesHasNotSameSize() {
-        assertThatThrownBy(() -> new Results(List.of("hello", "helo"), NamesFixture.getNamesSize3()));
+        assertThatThrownBy(() -> new GameResults(List.of("hello", "helo"), NamesFixture.getNamesSize3()));
     }
 
     static Stream<Arguments> resultParametersDummy() {
