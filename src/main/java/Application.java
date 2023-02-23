1ff891cec6255207d88121983d5f5d16de198c56
echo <echo.backend@gmail.com>
2023-02-23 20:49:21 +0900

    feat(LadderGameController): 결과 찾고 출력하는 기능 추가

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,6 +3,6 @@ import controller.LadderGameController;
 public class Application {
 	public static void main(String[] args) {
 		LadderGameController ladderGameController = new LadderGameController();
-		ladderGameController.run();
+		ladderGameController.runGame();
 	}
 }
--- src/main/java/controller/LadderGameController.java
+++ src/main/java/controller/LadderGameController.java
@@ -1,60 +1,86 @@
 package controller;
 
-import domain.LadderGame;
-import domain.LadderHeight;
-import domain.Participant;
-import domain.Participants;
+import domain.*;
 import domain.util.PointGenerator;
 import view.InputView;
 import view.OutputView;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class LadderGameController {
 
-	public void run() {
-		LadderGame ladderGame = setUpLadderGame();
-		OutputView.printLadder(
-				ladderGame.getParticipants(),
-				ladderGame.getLadder()
-		);
+	private final LadderGame ladderGame;
+
+	public LadderGameController() {
+		Participants participants = retrieveParticipants();
+		Results results = retrieveResults();
+		LadderHeight ladderHeight = retrieveHeight();
+		LadderWidth ladderWidth = new LadderWidth(participants.getParticipantsNum() - 1);
+		Ladder ladder = Ladder.create(ladderHeight, ladderWidth, PointGenerator.getInstance(true));
+		this.ladderGame = new LadderGame(participants, results, ladder);
 	}
 
-	// TODO: extract this method to LadderGame.setup()
-	public LadderGame setUpLadderGame() {
-		Participants participants = retrieveParticipants();
-		LadderHeight ladderHeight = new LadderHeight(retrieveLadderHeight());
-		return new LadderGame(participants, ladderHeight, PointGenerator.getInstance(true));
+	public void runGame() {
+		OutputView.printLadder(ladderGame.getParticipants(), ladderGame.getLadder(), ladderGame.getResults());
+		ladderGame.run();
+		String name = retrieveNameToFind();
+		while (!name.equals("all")) {
+			printResult(name);
+			name = retrieveNameToFind();
+		}
+		printAllResult();
 	}
 
 	private Participants retrieveParticipants() {
-		List<Participant> names = retrieveParticipantsNames();
-		Participants participants = new Participants();
-		for (Participant name : names) {
-			participants.add(name);
-		}
-		return participants;
-	}
-
-	private List<Participant> retrieveParticipantsNames() {
 		try {
-			List<String> names = InputView.readParticipantsNames();
-			return names.stream().map(Participant::new).collect(Collectors.toList());
+			String[] names = InputView.readNames();
+			return Participants.of(names);
 		} catch (IllegalArgumentException e) {
 			OutputView.printError(e.getMessage());
-			return retrieveParticipantsNames();
+			return retrieveParticipants();
 		}
 	}
 
-	// TODO: makes method to return LadderHeight
-	private int retrieveLadderHeight() {
-		try{
-			return InputView.readHeight();
-		} catch (IllegalArgumentException e){
+	private Results retrieveResults() {
+		try {
+			String[] results = InputView.readResults();
+			return Results.of(results);
+		} catch (IllegalArgumentException e) {
 			OutputView.printError(e.getMessage());
-			return retrieveLadderHeight();
+			return retrieveResults();
 		}
 	}
 
+	// TODO: LadderHeight, LadderWidth -> Singleton
+	private LadderHeight retrieveHeight() {
+		try {
+			int height = InputView.readHeight();
+			return new LadderHeight(height);
+		} catch (IllegalArgumentException e) {
+			OutputView.printError(e.getMessage());
+			return retrieveHeight();
+		}
+	}
+
+	private String retrieveNameToFind() {
+		try {
+			String name = InputView.readNameToFind();
+			if (name.equals("all")) {
+				return name;
+			}
+			Participant.contains(name);
+			return name;
+		} catch (IllegalArgumentException e) {
+			OutputView.printError(e.getMessage());
+			return retrieveNameToFind();
+		}
+	}
+
+	private void printAllResult() {
+		OutputView.printAllResult(ladderGame.getParticipants(), ladderGame.getAllGameResult());
+	}
+
+	private void printResult(String name) {
+		Participant participant = Participant.from(name);
+		Result result = ladderGame.getResultFrom(participant);
+		OutputView.printParticipantResult(result);
+	}
 }
