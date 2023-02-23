8083d396672fb2479fd5add34675758c4ce9fb56
ChoiYoungHoon <dudgns2865@naver.com>
2023-02-23 14:40:27 +0900

    feat: 실행 결과, 결과를 보고싶은 사람 출력 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -4,24 +4,41 @@ import domain.model.Ladder;
 import domain.model.Layer;
 import domain.type.Line;
 import domain.vo.Name;
+import domain.vo.Result;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class OutputView {
 
     private static final String RESULT_ANNOUNCEMENT = "\n실행결과";
-    private static final int INTERVAL_UNIT = 5;
+    private static final int INTERVAL_UNIT = 6;
     private static final String FRONT_SPACE = "    ";
     private static final String LINE_DELIMITER = "|";
     private static final String CONNECTED_LINE = "-";
     private static final String SPACE = " ";
+    public static final String RESULT_DELIMITER = " : ";
 
-    public void printResult(List<Name> names, Ladder ladder) {
+    public void printLadder(List<Name> names, Ladder ladder, List<Result> results) {
         System.out.println(RESULT_ANNOUNCEMENT);
-        printNames(names);
+        printValues(names.stream().map(Name::getValue).collect(Collectors.toList()));
         printLadder(ladder,
             names.stream().map(name -> name.getValue().length()).max(Comparator.naturalOrder())
                 .orElse(INTERVAL_UNIT));
+        printValues(results.stream().map(Result::getValue).collect(Collectors.toList()));
+    }
+
+    public void printResult(Result result) {
+        System.out.println(RESULT_ANNOUNCEMENT);
+        System.out.println(result.getValue());
+    }
+
+    public void printAllResult(Map<Name, Result> result) {
+        System.out.println(RESULT_ANNOUNCEMENT);
+        for (Name name : result.keySet()) {
+            System.out.println(name.getValue() + RESULT_DELIMITER + result.get(name).getValue());
+        }
     }
 
     private void printLadder(Ladder ladder, int lineLength) {
@@ -45,12 +62,12 @@ public class OutputView {
         return SPACE;
     }
 
-    private void printNames(final List<Name> names) {
+    private void printValues(final List<String> values) {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("\n");
-        names.forEach(name -> {
-            int difference = INTERVAL_UNIT - name.getValue().length();
-            stringBuilder.append(SPACE.repeat(difference)).append(name.getValue());
+        values.forEach(value -> {
+            int difference = INTERVAL_UNIT - value.length();
+            stringBuilder.append(SPACE.repeat(difference)).append(value);
         });
         System.out.println(stringBuilder);
     }
