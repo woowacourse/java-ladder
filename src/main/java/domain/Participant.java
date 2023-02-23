560fe71a1fa3c3cb14c030075ee8773e3d5b4f46
echo <echo.backend@gmail.com>
2023-02-23 15:26:20 +0900

    refactor(Participant): 정적팩토리 메서드를 사용하여 인스턴스 캐싱 사용

--- src/main/java/domain/Participant.java
+++ src/main/java/domain/Participant.java
@@ -2,6 +2,9 @@ package domain;
 
 import domain.util.Display;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Participant implements Display {
 
 	private static final String RIGHT_ALIGN_PLACEHOLDER = "%6s";
@@ -9,28 +12,33 @@ public class Participant implements Display {
 	private static final int MAX_NAME_LENGTH = 5;
 	private static final String BLANK_PARTICIPANT_NAME_ERROR_MSG = "참가자의 이름은 공백일 수 없습니다.";
 	private static final String NAME_LENGTH_ERROR_MSG = "참가자의 이름은 1글자 이상 5글자 이하여야 한다.";
+	private final static Map<String, Participant> cachedParticipant = new HashMap<>();
+
 	private final String name;
 
-	public Participant(final String name) {
-		validateIsBlank(name);
-		validateNameLength(name);
+	private Participant(final String name) {
 		this.name = name;
 	}
 
-	private void validateIsBlank(final String name) {
+	public static Participant from(final String name) {
+		validateIsBlank(name);
+		validateNameLength(name);
+		if (!cachedParticipant.containsKey(name)) {
+			cachedParticipant.put(name, new Participant(name));
+		}
+		return cachedParticipant.get(name);
+	}
+
+	private static void validateIsBlank(final String name) {
 		if (name.isBlank()) {
 			throw new IllegalArgumentException(BLANK_PARTICIPANT_NAME_ERROR_MSG);
 		}
 	}
 
-	private void validateNameLength(final String name) {
+	private static void validateNameLength(final String name) {
 		if (MIN_NAME_LENGTH > name.length() || name.length() > MAX_NAME_LENGTH) {
 			throw new IllegalArgumentException(NAME_LENGTH_ERROR_MSG);
 		}
-	}
-
-	public String getName(){
-		return this.name;
 	}
 
 	@Override
