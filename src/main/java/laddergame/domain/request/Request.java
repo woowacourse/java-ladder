01482ef8d3f52153253746a477792b3facadee05
chanminmaxscaler <jcm04@naver.com>
2023-02-24 00:45:33 +0900

    refactor: 요청 내용을 검증하는 기능을 수정한다
    변경 전:
    Request 객체의 생성자에서 요청 내용을 검증한다.
    
    변경 후 :
    Request 객체를 생성한 후 호출하는 메서드 내부에서 검증을 수행하도록 수정한다.

--- src/main/java/laddergame/domain/request/Request.java
+++ src/main/java/laddergame/domain/request/Request.java
@@ -1,18 +1,32 @@
 package laddergame.domain.request;
 
+import laddergame.domain.ladder.Ladder;
 import laddergame.domain.participant.Participants;
+import laddergame.domain.result.Results;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Request {
 
     public static final String ALL_PARTICIPANTS_REQUEST_KEY = "all";
-    private final String requestContent;
 
-    public Request(final String requestContent, Participants participants) {
-        validateRequestExistence(requestContent, participants);
-        this.requestContent = requestContent;
+    private final Participants participants;
+    private final Ladder ladder;
+    private final Results results;
+
+    public Request(final Participants participants, final Ladder ladder, final Results results) {
+        this.participants = participants;
+        this.ladder = ladder;
+        this.results = results;
     }
 
-    private void validateRequestExistence(final String requestContent, final Participants participants) {
+    public Map<String, String> getResultByRequestContent(final String requestContent) {
+        validateRequestExistence(requestContent);
+        return new HashMap<>();
+    }
+
+    private void validateRequestExistence(final String requestContent) {
         if (!requestContent.equals(ALL_PARTICIPANTS_REQUEST_KEY) || !participants.contains(requestContent)) {
             throw new IllegalArgumentException(String.format("[ERROR] %s은 존재하지 않는 이름입니다. 참여자 한 명의 이름 혹은 \"all\"을 입력하세요.", requestContent));
         }
--- src/test/java/laddergame/domain/request/RequestTest.java
+++ src/test/java/laddergame/domain/request/RequestTest.java
@@ -1,6 +1,8 @@
 package laddergame.domain.request;
 
+import laddergame.domain.ladder.Ladder;
 import laddergame.domain.participant.Participants;
+import laddergame.domain.result.Results;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -11,19 +13,28 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class RequestTest {
 
+    private Request request;
     private Participants participants;
+    private Ladder ladder;
+    private Results results;
 
     @BeforeEach
     void setUp() {
         final String participantNames = "pobi,honux,crong,jk";
+        final String resultNames = "꽝,5000,꽝,3000";
+
         participants = Participants.create(participantNames);
+        ladder = Ladder.create(() -> true, "2", 4);
+        results = new Results(resultNames, participants.size());
+
+        request = new Request(participants, ladder, results);
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"tomy", "토미", "jamie", "제이미"})
     @DisplayName("요청된 내용이 참여자나 요청 키에 포함되지 않으면, 예외가 발생한다.")
     void throws_exception_if_request_content_is_not_included_in_participants_or_request_key(String requestContent) {
-        assertThatThrownBy(() -> new Request(requestContent, participants))
+        assertThatThrownBy(() -> request.getResultByRequestContent(requestContent))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ERROR_MESSAGE_HEAD);
     }
