981c56b9fcdef0aa632c0ccae430bc1f939fe048
yoondgu <doyou2ing@naver.com>
2023-02-23 17:42:19 +0900

    refactor: 사다리 라인 생성 로직 메서드 분리

--- src/main/java/laddergame/domain/ladder/line/Line.java
+++ src/main/java/laddergame/domain/ladder/line/Line.java
@@ -17,14 +17,11 @@ public class Line {
         return new Line(generateDirections(stepPointGenerator, width.get()));
     }
 
-    // TODO 메소드 길이 줄이기
     private static List<Direction> generateDirections(StepPointGenerator stepPointGenerator, int width) {
         List<Direction> directions = new ArrayList<>();
-
         StepPoint left = StepPoint.NONE;
-        for (int i = 0; i < width - 1; i++) {
-            StepPoint generated = stepPointGenerator.generate(left);
-            validateStepPoint(left, generated);
+        for (int i = 0; i < (width - 1); i++) {
+            StepPoint generated = generateStepPoint(stepPointGenerator, left);
             Direction direction = Direction.findDirection(left, generated);
             directions.add(direction);
             left = generated;
@@ -33,12 +30,19 @@ public class Line {
         return directions;
     }
 
+    private static StepPoint generateStepPoint(StepPointGenerator stepPointGenerator, StepPoint left) {
+        StepPoint generated = stepPointGenerator.generate(left);
+        validateStepPoint(left, generated);
+        return generated;
+    }
+
     private static void validateStepPoint(StepPoint previous, StepPoint toAdd) {
         if (previous.isContinuous(toAdd)) {
             throw new IllegalArgumentException("디딤대는 연속적으로 존재할 수 없습니다.");
         }
     }
 
+    // TODO 바로 인덱스 구해서 주기
     public Direction getDirectionToMove(int index) {
         IndexValidator.validateBounds(index, directions.size(), "주어진 위치가 사다리 폭보다 큽니다.");
         return directions.get(index);
