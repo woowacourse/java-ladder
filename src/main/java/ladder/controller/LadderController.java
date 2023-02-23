a6c04899a098f354da0e9adbbdbe734edc035fc3
seokjin8678 <seokjin8678@gmail.com>
2023-02-23 15:38:42 +0900

    feat: 사용자 이름을 입력 받아 게임의 결과가 나오게 하는 기능 구현

--- src/main/java/ladder/controller/LadderController.java
+++ src/main/java/ladder/controller/LadderController.java
@@ -2,9 +2,13 @@ package ladder.controller;
 
 import ladder.domain.Height;
 import ladder.domain.Ladder;
+import ladder.domain.Player;
+import ladder.domain.PlayerResult;
+import ladder.domain.PlayerResults;
 import ladder.domain.Players;
 import ladder.domain.Results;
 import ladder.dto.LadderResponse;
+import ladder.dto.PlayerResultsResponse;
 import ladder.dto.PlayersResponse;
 import ladder.dto.ResultsResponse;
 import ladder.service.LadderService;
@@ -27,7 +31,18 @@ public class LadderController {
         Height height = new Height(inputHeight);
         Results results = ladderService.createResults(resultsInput, players.getPlayersCount());
         Ladder ladder = ladderService.createLadder(height, players.getPlayersCount());
-        ladderView.printResult(PlayersResponse.ofPlayers(players), LadderResponse.ofLadder(ladder),
+        ladderView.printLadderResult(PlayersResponse.ofPlayers(players), LadderResponse.ofLadder(ladder),
                 ResultsResponse.ofResults(results));
+        PlayerResults playerResults = ladderService.createPlayerResults(players, ladder, results);
+        while (true) {
+            String playerName = ladderView.readPlayerName();
+            if (playerName.equals("all")) {
+                break;
+            }
+            Player player = players.findByPlayerName(playerName);
+            PlayerResult playerResult = playerResults.findByPlayer(player);
+            ladderView.printResult(playerResult.getResult());
+        }
+        ladderView.printAllPlayerResults(PlayerResultsResponse.of(playerResults));
     }
 }
