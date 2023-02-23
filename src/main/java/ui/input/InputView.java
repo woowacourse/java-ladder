5b9d560ba24185126184579dabf4778b2e8dbff3
SproutMJ <mjk54565@gmail.com>
2023-02-23 17:54:04 +0900

    refactor(ui): 결과 입력 메시지를 양식에 맞추고 값을 상수화

--- src/main/java/ui/input/InputView.java
+++ src/main/java/ui/input/InputView.java
@@ -14,6 +14,7 @@ public class InputView {
 
     private static final Scanner scanner = new Scanner(System.in);
     private static final String INPUT_PEOPLES_NAME = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    private static final String INPUT_REWARDS = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
     private static final String INPUT_LADDER_HEIGHT = "최대 사다리 높이는 몇 개인가요?";
 
     public static List<String> inputPersonName() {
@@ -27,8 +28,8 @@ public class InputView {
         }
     }
 
-    public static List<String> inputRewards(int peopleNum){
-        System.out.println("실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
+    public static List<String> inputRewards(int peopleNum) {
+        System.out.println("\n" + INPUT_REWARDS);
         while (true) {
             try {
                 List<String> rewards = Arrays.asList(scanner.nextLine().split(","));
