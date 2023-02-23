bd16615a474c236acfd62c99f00c409d2b7136de
yenawee <yaena0319@naver.com>
2023-02-23 16:11:18 +0900

    test(Target) : 결과 받고 싶은 사람 이름 입력 유효성 검증 테스트

---
+++ src/test/java/laddergame/domain/TargetTest.java
@@ -0,0 +1,45 @@
+package laddergame.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+class TargetTest {
+    @DisplayName("입력이 null 일 때 에러 확인")
+    @Test
+    void checknull() {
+        Assertions.assertThatThrownBy(() -> new Target(null)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("입력이 공백일 때 에러 확인")
+    @Test
+    void checkblank() {
+        Assertions.assertThatThrownBy(() -> new Target("")).isInstanceOf(IllegalArgumentException.class);
+        Assertions.assertThatThrownBy(() -> new Target(" ")).isInstanceOf(IllegalArgumentException.class);
+        Assertions.assertThatThrownBy(() -> new Target("  ")).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("입력이 유저 네임이 아니고 all 도 아닐 때 에러 확인")
+    @Test
+    void checkContent() {
+        Target target = new Target("extra");
+        Players player = new Players(List.of("pobi", "crong", "jena"));
+        List<String> playerNames = player.getPlayers().stream().map(Player::getName).collect(Collectors.toList());
+        Assertions.assertThatThrownBy(() -> target.checkNotPlayerNameOrNotAll(playerNames))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("입력이 유저 네임이거나 all 일 때 성공")
+    @Test
+    void checkContent2() {
+        Players player = new Players(List.of("pobi", "crong", "jena"));
+        List<String> playerNames = player.getPlayers().stream().map(Player::getName).collect(Collectors.toList());
+        assertDoesNotThrow(() -> new Target("jena").checkNotPlayerNameOrNotAll(playerNames));
+        assertDoesNotThrow(() -> new Target("all").checkNotPlayerNameOrNotAll(playerNames));
+    }
+}
