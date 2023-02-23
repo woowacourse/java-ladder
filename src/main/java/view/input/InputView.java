52031568169ad9a9e9e6cff9a53225b2a7d5991b
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-02-23 09:49:05 +0900

    feat: 보고 싶은 사람 입력 기능 추가

--- src/main/java/view/input/InputView.java
+++ src/main/java/view/input/InputView.java
@@ -7,6 +7,7 @@ public class InputView {
     private static final String ENTER_PARTICIPANTS_NAME = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     private static final String ENTER_HEIGHT = System.lineSeparator() + "최대 사다리 높이는 몇 개인가요?";
     private static final String ENTER_RESULTS = System.lineSeparator() + "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
+    private static final String ENTER_GET_RESULT = System.lineSeparator() + "결과를 보고 싶은 사람은?";
     private final Scanner scanner = new Scanner(System.in);
 
     public String enterParticipantNames() {
@@ -25,6 +26,11 @@ public class InputView {
 
     public String enterResults() {
         System.out.println(ENTER_RESULTS);
+        return readLine();
+    }
+
+    public String enterGetResult() {
+        System.out.println(ENTER_GET_RESULT);
         return readLine();
     }
 
