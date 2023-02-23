0429ab06cd7243d81992ee3ee6b0d7005c7ac35e
donghae <donghae1999@gmail.com>
2023-02-23 23:55:20 +0900

    refactor: checkColumn 메소드 수정
    checkColumn -> makeLine

--- src/main/java/model/Line.java
+++ src/main/java/model/Line.java
@@ -8,38 +8,23 @@ import java.util.List;
 
 public class Line {
     private static final int MINIMUM_LINE_SIZE = 2;
-    private static final int NUMBER_FOR_RETRY = 1;
     private final List<Status> points = new ArrayList<>();
 
     public Line(int personCount, Generator generator) {
         for (int column = 0; column < personCount - 1; column++) {
-            points.add(Status.findStatus(generator.generate()));
-            column = checkColumn(column);
+            makeLine(column, generator);
         }
     }
-
-    private int checkColumn(int column) {
-        if (points.size() >= MINIMUM_LINE_SIZE && validateRetryLineMake(column)) {
-            return column - NUMBER_FOR_RETRY;
+    private void makeLine(int column, Generator generator){
+        if(points.size() >= MINIMUM_LINE_SIZE && validateContinuousLine(column)){
+            points.add(Status.BRIDGE_DOES_NOT_EXIST);
+            return;
         }
-        return column;
+        points.add(Status.findStatus(generator.generate()));
     }
 
-    private boolean validateRetryLineMake(int column) {
-        if (validateConnectLine(column) && validateTrueLine(column)) {
-            points.remove(column);
-            return true;
-        }
-        return false;
-    }
-
-    private boolean validateConnectLine(int column) {
-        int leftLadderLine = column - 1;
-        return points.get(column).equals(points.get(leftLadderLine));
-    }
-
-    private boolean validateTrueLine(int index) {
-        return points.get(index).getStatus();
+    private boolean validateContinuousLine(int index) {
+        return points.get(index-1).getStatus();
     }
 
     public boolean getLine(int column) {
