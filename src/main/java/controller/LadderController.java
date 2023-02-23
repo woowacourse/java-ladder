33b1c5f486baf6691f732897f9970060a1d8acc7
Miseong Kim <miseongk@naver.com>
2023-02-24 03:33:56 +0900

    feat: 실행 결과 입력 및 결과 객체 생성

--- src/main/java/controller/LadderController.java
+++ src/main/java/controller/LadderController.java
@@ -2,9 +2,11 @@ package controller;
 
 import domain.Height;
 import domain.Ladder;
+import domain.LadderGame;
 import domain.Line;
 import domain.Lines;
 import domain.Players;
+import domain.Results;
 import java.util.ArrayList;
 import java.util.List;
 import util.LineGenerator;
@@ -15,11 +17,16 @@ import view.OutputView;
 
 public class LadderController {
 
+    private static final String DELIMITER_WITH_BLANK = "\\s*,\\s*";
+
     public void run() {
         Players players = makePlayers();
-        int height = inputHeight();
-        Ladder ladder = makeLadder(new Height(height), players.getNumberOfPlayers());
-        printLadder(players, ladder);
+        Results results = makeResult(players.getNumberOfPlayers());
+        Height height = new Height(inputHeight());
+        Ladder ladder = makeLadder(height, players.getNumberOfPlayers());
+        LadderGame ladderGame = new LadderGame(players, ladder, results);
+        ladderGame.printLadder();
+        ladderGame.playGame();
     }
 
     private Players makePlayers() {
@@ -41,6 +48,25 @@ public class LadderController {
         }
     }
 
+    private Results makeResult(int playerCount) {
+        try{
+            String results = inputResults();
+            return new Results(makeResults(results), playerCount);
+        } catch (IllegalArgumentException e) {
+            OutputView.printErrorMessage(e);
+            return makeResult(playerCount);
+        }
+    }
+
+    private String inputResults() {
+        try{
+            return InputView.receiveResults();
+        } catch (IllegalArgumentException e) {
+            OutputView.printErrorMessage(e);
+            return inputResults();
+        }
+    }
+
     private int inputHeight() {
         try {
             return InputView.receiveHeight();
@@ -48,6 +74,10 @@ public class LadderController {
             OutputView.printErrorMessage(e);
             return inputHeight();
         }
+    }
+
+    private List<String> makeResults(String results) {
+        return List.of(results.split(DELIMITER_WITH_BLANK));
     }
 
     private Ladder makeLadder(Height height, int numberOfWalls) {
@@ -66,11 +96,5 @@ public class LadderController {
         }
 
         return new Lines(lines);
-    }
-
-    private void printLadder(Players players, Ladder ladder) {
-        OutputView.printResultMessage();
-        OutputView.printPlayers(players);
-        OutputView.printLadder(ladder);
     }
 }
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -17,6 +17,16 @@ public class InputView {
         return playerNames;
     }
 
+    public static String receiveResults() {
+        System.out.println("\n실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
+
+        String results = input();
+        validateBlank(results);
+        validateDelimiter(results);
+
+        return results;
+    }
+
     public static int receiveHeight() {
         System.out.println("\n최대 사다리 높이는 몇 개인가요?");
 
