3d97090e9b5ea8c33b1aeba1c6c73bb8d98b7849
hyunseo <goatsfish@gmail.com>
2023-02-23 16:42:50 +0900

    refactor: 사다리 다음 위치 확인 기능 수정

--- src/main/java/laddergame/domain/Ladder.java
+++ src/main/java/laddergame/domain/Ladder.java
@@ -3,8 +3,6 @@ package laddergame.domain;
 import static laddergame.utils.OptionalUtils.getValueAfterNullCheck;
 
 public class Ladder {
-    private static final String LADDER_LINES_NULL_EXCEPTION = "Lines는 null이 될 수 없습니다.";
-
     private final Lines lines;
 
     public Ladder(final Lines inputLines) {
@@ -29,24 +27,14 @@ public class Ladder {
         final Width width = lines.getLines().get(column.getValue()).getWidth();
         final Position maxPosition = new Position(width.getValue());
         final Position leftPosition = row.move(-1);
-        if (checkRange(leftPosition, maxPosition) && checkFilled(column, leftPosition)) {
+        final Position rightPosition = row.move(0);
+
+        if (leftPosition.checkBetweenZeroAnd(maxPosition) && lines.getPointByColumnAndRow(column, leftPosition)) {
             return row.move(-1);
         }
-        if (checkRange(row, maxPosition) && checkFilled(column, row)) {
+        if (rightPosition.checkBetweenZeroAnd(maxPosition) && lines.getPointByColumnAndRow(column, rightPosition)) {
             return row.move(1);
         }
         return row;
-    }
-
-    private boolean checkRange(final Position position, final Position maxPosition) {
-        return 0 <= position.getValue() && position.getValue() < maxPosition.getValue();
-    }
-
-    private boolean checkFilled(final Position column, final Position row) {
-        final int height = lines.getHeight().getValue();
-        if (height <= column.getValue()) {
-            return false;
-        }
-        return lines.getPointByColumnAndRow(column, row);
     }
 }
