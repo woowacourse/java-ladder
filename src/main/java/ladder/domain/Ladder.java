3188926dfaf63130410e8a244583d313abd0ab2d
DESKTOP-FNVN0HH\USER <beatus96@naver.com>
2023-02-23 14:13:27 +0900

    feat: Ladder 출발점에 배당된 도착점 반환 기능 추가

--- src/main/java/ladder/domain/Ladder.java
+++ src/main/java/ladder/domain/Ladder.java
@@ -26,5 +26,23 @@ public class Ladder {
         return Collections.unmodifiableList(lines);
     }
 
+    public int findEndPositionFrom(int startIndex) {
+        int current = startIndex;
+
+        for (Line line : lines) {
+            int left = current - 1;
+            int right = current;
+
+            if (left >= 0 && line.getLine().get(left).getValue()) {
+                //왼쪽이 참이면
+                current--;
+            }
+            if (right < line.getLine().size() && line.getLine().get(right).getValue()) {
+                //오른쪽이 참이면
+                current++;
+            }
+        }
+        return current;
+    }
 
 }
