9b993747b7600d09ef77b8c041cdda72e0653d19
Kong-hana01 <gksqlsl11@khu.ac.kr>
2023-02-23 12:00:38 +0900

    feat: 주어진 라인에 대해 player가 움직이는 메서드 구현
    - TDD 사용
    - Player들 간에 이름 중복여부 체크

--- src/main/java/domain/Players.java
+++ src/main/java/domain/Players.java
@@ -2,16 +2,43 @@ package domain;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Players {
 
     private final List<Player> players;
 
     public Players(List<String> names) {
+        validate(names);
         players = new ArrayList<>();
         for (int i = 0; i < names.size(); i++) {
             players.add(new Player(names.get(i), i));
+        }
+    }
+
+    private void validate(List<String> names) {
+        validateDuplicatedNames(names);
+    }
+
+    private void validateDuplicatedNames(List<String> names) {
+        if (hasDuplicatedName(names)) {
+            throw new IllegalArgumentException("사다리 게임 참여자의 이름은 중복이 없어야합니다.");
+        }
+    }
+
+    private boolean hasDuplicatedName(List<String> names) {
+        return getDistinctNameCount(names) != names.size();
+    }
+
+    private long getDistinctNameCount(List<String> names) {
+        return names.stream()
+                .distinct()
+                .count();
+    }
+
+    public void move(Line line) {
+        for (Player player: players) {
+            MoveType moveType = MoveType.getMoveTypeByPosition(player, line);
+            player.move(moveType);
         }
     }
 
--- src/test/java/domain/PlayersTest.java
+++ src/test/java/domain/PlayersTest.java
@@ -1,18 +1,21 @@
 package domain;
 
-import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
 import java.util.List;
 
+import domain.numbergenerator.TestNumberGenerator;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class PlayersTest {
 
     @DisplayName("사다리 게임 참여자의 이름이 하나라도 잘못 입력된 경우 예외를 발생시킨다.")
     @Test
-    void validateTest() {
+    void createPlayersTest1() {
         List<String> names = List.of("odo", "odo27", "kong", "konghana");
         assertThatThrownBy(() -> new Players(names))
                 .isInstanceOf(IllegalArgumentException.class)
@@ -21,9 +24,33 @@ public class PlayersTest {
 
     @DisplayName("사다리 게임 참여자의 이름이 정상적으로 입력된 경우 참여자들을 생성한다.")
     @Test
-    void validateTest2() {
+    void createPlayersTest2() {
         List<String> names = List.of("odo", "odo27", "kong", "ko ng", "  ko    ");
         assertThatCode(() -> new Players(names))
                 .doesNotThrowAnyException();
+    }
+
+    @DisplayName("사다리 게임 참여자의 이름에 중복이 있는 경우에는 예외를 발생시킨다.")
+    @Test
+    void createPlayersTest3() {
+        List<String> names = List.of("kong", "odo", "gray", "kong");
+        assertThatThrownBy(() -> new Players(names))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("사다리 게임 참여자의 이름은 중복이 없어야합니다.");
+    }
+
+    @DisplayName("사다리 한 줄에 대한 정보가 주어지면 사다리 게임 참여자의 위치를 옮길 수 있다.")
+    @ParameterizedTest
+    @CsvSource(value = {"1:1:0", "0:0:1"}, delimiter = ':')
+    void playerMoveTest(int generateNumber, int expectedOdoPosition, int expectedKongPosition) {
+        List<String> names = List.of("odo", "kong");
+        Players players = new Players(names);
+        Line line = new Line(2, new TestNumberGenerator(Lists.newArrayList(generateNumber)));
+        players.move(line);
+        Player odo = players.getPlayers().get(0);
+        Player kong = players.getPlayers().get(1);
+
+        assertThat(odo.getPosition()).isEqualTo(expectedOdoPosition);
+        assertThat(kong.getPosition()).isEqualTo(expectedKongPosition);
     }
 }
