863fd7ed1169026a2a9842c9d159c8ea3fe1244d
song-wooseok <sws981202@gmail.com>
2023-02-24 00:19:15 +0900

    test: Line의 오른쪽만 뻗은 step을 가져오는 테스트

--- src/test/java/domain/LineTest.java
+++ src/test/java/domain/LineTest.java
@@ -18,14 +18,15 @@ class LineTest {
     @DisplayName(" 이전 다리가 이전 다리가 건널 수")
     class GenerateFootsteps {
         @Test
-        @DisplayName(" 있는 디딤돌이면 지금 다리는 디딤돌이 생성되는 것이 허용되지 않는다.")
+        @DisplayName(" 있는 디딤돌이면 지금 다리는 오른쪽으로 건널 수 있는 다리가 생성된다.")
         void givenCrossablePreviousFootstep_thenUncrossableCurrentFootStep() {
-            Line line = new Line(new FixBooleanGenerator(true));
+            Line line = new Line(new FixBooleanGenerator(true, true));
 
-            line.generateFootStep();
-            line.generateFootStep();
+            line.generateStep();
+            line.generateStep();
+            line.generateStep();
 
-            assertThat(line.isSteppableAt(1)).isFalse();
+            assertThat(line.isSteppableAt(2)).isTrue();
         }
 
         @ParameterizedTest(name = " 없는 디딤돌이면 지금 다리는 디딤돌이 생성되는 것이 허용된다.")
@@ -33,8 +34,8 @@ class LineTest {
         void givenUnCrossablePreviousFootstep_thenNotUncrossableCurrentFootStep(boolean value) {
             Line line = new Line(new FixBooleanGenerator(false, value));
 
-            line.generateFootStep();
-            line.generateFootStep();
+            line.generateStep();
+            line.generateStep();
 
             assertThat(line.isSteppableAt(1)).isEqualTo(value);
         }
@@ -49,10 +50,28 @@ class LineTest {
             BooleanGenerator trueGenerator = new FixBooleanGenerator(value);
 
             Line line = new Line(trueGenerator);
-            line.generateFootStep();
+            line.generateStep();
 
             assertThat(line.isSteppableAt(0))
                     .isEqualTo(value);
+        }
+    }
+
+    @Nested
+    @DisplayName(" 한 step 이 연결되어 있다면, 왼쪽에서 오른쪽으로 연결된 정보만 보여준다.")
+    class LineInformation{
+
+        @Test
+        void generateLineTest(){
+            BooleanGenerator generator = new FixBooleanGenerator(true, false, true);
+
+            Line line = new Line(generator);
+
+            for(int generateCount = 0; generateCount<5; generateCount++){
+                line.generateStep();
+            }
+
+            assertThat(line.getRightConnectionCondition()).containsExactly(true, false, false, true, false);
         }
     }
 }
