ee05280d89808be2cb48d9a5ab1da46e54b1c058
yoondgu <doyou2ing@naver.com>
2023-02-23 11:08:18 +0900

    refactor: 상수명 단어 오기 수정

--- src/test/java/laddergame/domain/ladder/LadderTest.java
+++ src/test/java/laddergame/domain/ladder/LadderTest.java
@@ -14,14 +14,14 @@ import org.junit.jupiter.api.Test;
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class LadderTest {
 
-    private static final LineWidth DEFAULT_WEIGHT = new LineWidth(3);
+    private static final LineWidth DEFAULT_WIDTH = new LineWidth(3);
     private static final LadderHeight DEFAULT_HEIGHT = new LadderHeight(3);
 
     @Test
     @DisplayName("결과의 개수가 라인의 폭이 같지 않으면 예외를 던진다.")
     void should_ThrowException_When_DestinationSizeNotEqualWithPlayersCount() {
         List<String> wrongResults = List.of("실패");
-        assertThatThrownBy(() -> Ladder.of(DEFAULT_WEIGHT, DEFAULT_HEIGHT, wrongResults))
+        assertThatThrownBy(() -> Ladder.of(DEFAULT_WIDTH, DEFAULT_HEIGHT, wrongResults))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("결과의 개수와 라인의 폭(3)은 일치해야 합니다.");
     }
@@ -30,10 +30,10 @@ class LadderTest {
     @DisplayName("출발지에 따른 종착지 위치의 결과를 반환한다.")
     void should_ReturnResult_When_GivenStartIndex() {
         List<String> results = List.of("a", "b", "c");
-        Ladder ladder = Ladder.of(DEFAULT_WEIGHT, DEFAULT_HEIGHT, results);
+        Ladder ladder = Ladder.of(DEFAULT_WIDTH, DEFAULT_HEIGHT, results);
 
         List<String> foundResults = new ArrayList<>();
-        for (int i = 0; i < DEFAULT_WEIGHT.get(); i++) {
+        for (int i = 0; i < DEFAULT_WIDTH.get(); i++) {
             foundResults.add(ladder.findResultByStartIndex(i));
         }
 
