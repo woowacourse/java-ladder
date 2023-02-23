ac9484bf93f8f517136eb6055aa68d1075decf23
Cyma-s <ssol0319@naver.com>
2023-02-23 16:03:41 +0900

    test: 이름이 all인 경우 예외를 던지는 테스트 추가

--- src/test/java/ladder/domain/PlayerTest.java
+++ src/test/java/ladder/domain/PlayerTest.java
@@ -1,6 +1,7 @@
 package ladder.domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -32,5 +33,13 @@ class PlayerTest {
     void name_success(String input) {
         // expected
         assertDoesNotThrow(() -> new Player(input, 0));
+    }
+
+    @Test
+    @DisplayName("이름이 all인 경우 예외를 던진다.")
+    void name_throwException_valueIsGameCommand() {
+        // expected
+        assertThatThrownBy(() -> new Player("all", 0))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
