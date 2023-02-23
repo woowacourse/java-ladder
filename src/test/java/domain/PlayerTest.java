f8cf7e50240e259fdde53b22976bd038b273a528
parkmuhyeun <pjhg410@gmail.com>
2023-02-23 23:54:47 +0900

    test: TargetPlayer인지 테스트

--- src/test/java/domain/PlayerTest.java
+++ src/test/java/domain/PlayerTest.java
@@ -1,23 +1,19 @@
 package domain;
 
-import helper.StubPossiblePointGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class PlayerTest {
 
     @Test
-    @DisplayName("플레이어에 따른 원하는 결과값이 나오는지 확인")
-    void calculateResult() {
-        int startIndex = 0;
-        String result = "2";
-        Player player = new Player("pobi", startIndex);
-        Ladder ladder = new Ladder(5, 4, new StubPossiblePointGenerator());
+    @DisplayName("TargetPlayer이면 True 반환")
+    void isTargetPlayer() {
+        String name = "pobi";
+        Player player = new Player(name);
 
-        assertThat(player.calculateResult(ladder, new Results(4, List.of("1", "2", "3", "4"))))
-                .isEqualTo(result);
+        assertThat(player.isTargetPlayer(name)).isTrue();
     }
+
 }
