bb9784f1be14567de935680275b34ef4c97c4232
Rosie <dev.kayoung@gmail.com>
2023-02-23 16:06:21 +0900

    refactor: bufferedReader readLine 사용 시의 예외처리 로직을 분리

--- src/main/java/laddergame/view/InputView.java
+++ src/main/java/laddergame/view/InputView.java
@@ -12,35 +12,25 @@ public class InputView {
 
     public List<String> readNames() {
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
-        try {
-            return Arrays.stream(reader.readLine().split("\\s*,\\s*"))
-                    .collect(Collectors.toList());
-        } catch (IOException e) {
-            throw new IllegalStateException("문자열을 읽을 때 예외가 발생했습니다.", e);
-        }
+        return Arrays.stream(readLine().split("\\s*,\\s*")).collect(Collectors.toList());
     }
 
     public int readHeight() {
         System.out.println("최대 사다리 높이는 몇 개인가요?");
-        try {
-            return Integer.parseInt(reader.readLine());
-        } catch (IOException e) {
-            throw new IllegalStateException("문자열을 읽을 때 예외가 발생했습니다.", e);
-        }
+        return Integer.parseInt(readLine());
     }
 
     public List<String> readResults() {
         System.out.println("실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
-        try {
-            return Arrays.stream(reader.readLine().split("\\s*,\\s*"))
-                    .collect(Collectors.toList());
-        } catch (IOException e) {
-            throw new IllegalStateException("문자열을 읽을 때 예외가 발생했습니다.", e);
-        }
+        return Arrays.stream(readLine().split("\\s*,\\s*")).collect(Collectors.toList());
     }
 
     public String readNameToCheckResult() {
         System.out.println("결과를 보고 싶은 사람은?");
+        return readLine();
+    }
+
+    private String readLine() {
         try {
             return reader.readLine();
         } catch (IOException e) {
