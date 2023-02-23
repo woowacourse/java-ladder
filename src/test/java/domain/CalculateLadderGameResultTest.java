745b54f017d45bea4ad22e09c832239eebb7a23d
2014012606 <2014012606@hanyang.ac.kr>
2023-02-23 13:46:20 +0900

    chore: 상수일 필요 없는 필드 변수 해제

--- src/test/java/domain/CalculateLadderGameResultTest.java
+++ src/test/java/domain/CalculateLadderGameResultTest.java
@@ -13,19 +13,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class CalculateLadderGameResultTest {
 
-    private static final CalculateLadderGameResult CALCULATOR = new CalculateLadderGameResult();
     private static final List<Link> LENGTH_THREE_LINE_CASE1 = List.of(Link.LINKED, Link.UNLINKED, Link.LINKED);
     private static final List<Link> LENGTH_THREE_LINE_CASE2 = List.of(Link.LINKED, Link.UNLINKED, Link.UNLINKED);
     private static final List<Link> LENGTH_THREE_LINE_CASE3 = List.of(Link.UNLINKED, Link.LINKED, Link.UNLINKED);
     private static final List<Link> LENGTH_THREE_LINE_CASE4 = List.of(Link.UNLINKED, Link.UNLINKED, Link.LINKED);
-    private static final List<String> PREV_USER_NAMES = List.of("pobi", "honux", "crong", "jk");
+    private final CalculateLadderGameResult calculator = new CalculateLadderGameResult();
+    private final List<String> prevUserNames = List.of("pobi", "honux", "crong", "jk");
 
 
     @ParameterizedTest
     @DisplayName("참여자들이 사다리의 한 라인을 통과 후 올바른 위치에 있는지 확인한다.")
     @MethodSource("lineAndUserPositionsDummy")
     void passLineTest(List<Line> testLine, List<String> postUserNames) {
-        assertThat(CALCULATOR.passLadder(testLine, PREV_USER_NAMES)).isEqualTo(postUserNames);
+        assertThat(calculator.passLadder(testLine, prevUserNames)).isEqualTo(postUserNames);
     }
 
     @Test
@@ -35,7 +35,7 @@ class CalculateLadderGameResultTest {
                 new Line(LENGTH_THREE_LINE_CASE3), new Line(LENGTH_THREE_LINE_CASE4));
         List<String> postUserNames = List.of("pobi", "jk", "crong","honux");
 
-        assertThat(CALCULATOR.passLadder(testLadder, PREV_USER_NAMES)).isEqualTo(postUserNames);
+        assertThat(calculator.passLadder(testLadder, prevUserNames)).isEqualTo(postUserNames);
 
     }
 
