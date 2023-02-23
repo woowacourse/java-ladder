25dcf8e19c90f3da33220ec4cb36ae411574cbcb
echo <echo.backend@gmail.com>
2023-02-23 16:18:00 +0900

    feat(InputView): 실행 결과 입력 받는 기능 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -10,11 +10,17 @@ public class InputView {
 	private static final Scanner scanner = new Scanner(System.in);
 	private static final String DELIMITER = ",";
 	private static final String PARTICIPANTS_NAMES_REQUEST_MSG = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+	private static final String RESULTS_REQUEST_MSG = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
 	private static final String LADDER_HEIGHT_REQUEST_MSG = "최대 사다리 높이는 몇 개인가요?";
 	private static final String NAME_NUMBER_ERROR_MSG = "최소 두 명 이상의 참가자 이름을 입력해야합니다.";
 	private static final String LAST_COMMA_ERROR_MSG = "마지막 참가자 이름이 입력되지 않았습니다.";
 	private static final String HEIGHT_NATURAL_NUMBER_ERROR_MSG = "사다리 높이는 자연수여야합니다.";
 
+
+	public static List<String> readResults() {
+		System.out.println(RESULTS_REQUEST_MSG);
+		return parseListFromInput();
+	}
 
 	public static List<String> readParticipantsNames() {
 		System.out.println(PARTICIPANTS_NAMES_REQUEST_MSG);
