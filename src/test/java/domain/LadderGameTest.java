228f021a9fc3c7e68567cc37a1eb2fdadaec80f3
yeonkkk <tjddus0302@gmail.com>
2023-02-23 15:22:13 +0900

    test: 인덱스를 통한 실행 결과 조회 테스트

---
+++ src/test/java/domain/LadderGameTest.java
@@ -0,0 +1,34 @@
+package domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class LadderGameTest {
+
+    private LadderGame ladderGame;
+
+    @BeforeEach
+    void setUp() {
+        Ladder ladder = new Ladder(new Height(5), 3);
+        Users users = new Users(List.of(
+            new User(new Name("pobi"), new Position(0)),
+            new User(new Name("joy"), new Position(1)),
+            new User(new Name("crong"), new Position(2))));
+        Rewards rewards = Rewards.of(List.of("꽝", "3000", "2000"), 3);
+
+        ladderGame = new LadderGame(ladder, users, rewards);
+    }
+
+    @DisplayName("인덱스를 통해 실행 결과를 조회한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"0:꽝", "1:3000", "2:2000"}, delimiter = ':')
+    void findUserRewardByIndexTest(int index, String expected) {
+        assertEquals(expected, ladderGame.findUserReward(index));
+    }
+
+}
