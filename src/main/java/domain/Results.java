25acac7bf360ab60b9ffa276be3a8de846a3a5d8
echo <echo.backend@gmail.com>
2023-02-23 15:43:36 +0900

    refactor(Results): 정적 팩토리매서드를 통한 Results 생성 로직 변경

--- src/main/java/domain/Results.java
+++ src/main/java/domain/Results.java
@@ -1,20 +1,25 @@
 package domain;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Results {
 
     private final List<Result> results;
 
-    private Results(List<Result> results){
+    private Results(List<Result> results) {
         this.results = results;
     }
 
-    public static Results of(List<Result> results){
-        return new Results(results);
+    public static Results of(String... results) {
+        List<Result> resultList = Arrays.stream(results)
+                .map(Result::from)
+                .collect(Collectors.toList());
+        return new Results(resultList);
     }
 
-    public Result get(int index){
+    public Result get(int index) {
         return results.get(index);
     }
 
--- src/test/java/domain/ResultsTest.java
+++ src/test/java/domain/ResultsTest.java
@@ -3,21 +3,16 @@ package domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ResultsTest {
 
     @Test
     @DisplayName("결과의 리스트로 Results 객체를 생성한다.")
     void createValidResultsTest(){
-        List<Result> collectedList = Stream.of("꽝", "5000", "꽝", "3000")
-                .map(Result::from)
-                .collect(Collectors.toList());
-        Results results = Results.of(collectedList);
+        Results results = Results.of("꽝", "5000", "꽝", "3000");
         assertThat(results.getResults().stream().map(Result::getValue).collect(Collectors.toList())).containsExactly("꽝", "5000", "꽝", "3000");
     }
 
@@ -25,10 +20,7 @@ public class ResultsTest {
     @DisplayName("인덱스를 통해 Results에서 결과값을 찾을 수 있다.")
     void getResultAtIndexTest(){
         int idx = 3;
-        List<Result> collectedList = Stream.of("꽝", "5000", "꽝", "3000")
-                .map(Result::from)
-                .collect(Collectors.toList());
-        Results results = Results.of(collectedList);
+        Results results = Results.of("꽝", "5000", "꽝", "3000");
         assertThat(results.get(idx).getValue()).isEqualTo("3000");
     }
 }
