72b9dadc1a341d7814a5935f3e054aa98a36bef9
HubCreator <chocomilk8604@gmail.com>
2023-02-23 16:25:14 +0900

    refactor(LadderGenerator): 애너테이션 추가

--- src/main/java/domain/LadderGame.java
+++ src/main/java/domain/LadderGame.java
@@ -1,7 +1,6 @@
 package domain;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 public class LadderGame {
--- src/main/java/domain/LadderGenerator.java
+++ src/main/java/domain/LadderGenerator.java
@@ -1,5 +1,6 @@
 package domain;
 
+@FunctionalInterface
 public interface LadderGenerator {
 
     Ladder generate(People people, int height);
