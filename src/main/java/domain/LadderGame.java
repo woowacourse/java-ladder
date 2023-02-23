b70230fa0f109172429ed3f5ad8e463f50a08e5c
Kong-hana01 <gksqlsl11@khu.ac.kr>
2023-02-23 23:10:01 +0900

    refactor: LadderGame 내 run 함수를 재호출할 때 이전 결과를 리턴하도록 변경

--- src/main/java/domain/LadderGame.java
+++ src/main/java/domain/LadderGame.java
@@ -8,20 +8,29 @@ public class LadderGame {
     private final Ladder ladder;
     private final Players players;
     private final Prize prize;
+    private final Map<String, String> result;
 
     public LadderGame(Ladder ladder, Players players, Prize prize) {
         this.ladder = ladder;
         this.players = players;
         this.prize = prize;
+        this.result = new HashMap<>();
     }
 
     public Map<String, String> run() {
+        if (isDone()) {
+            return result;
+        }
         play();
-        return finish();
+        finish();
+        return result;
+    }
+
+    private boolean isDone() {
+        return !result.isEmpty();
     }
 
     private Map<String, String> finish() {
-        HashMap<String, String> result = new HashMap<>();
         for (Player player : players.getPlayers()) {
             int position = player.getPosition();
             result.put(player.getName(), prize.getOnePrizeByIndex(position));
