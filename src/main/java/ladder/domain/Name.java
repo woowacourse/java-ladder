12f86cc0959b7a9b3c74d23746dbd75d5ac02fc9
Cyma-s <ssol0319@naver.com>
2023-02-23 16:03:50 +0900

    feat: 이름이 all인 경우 예외를 던지는 기능 추가

--- src/main/java/ladder/domain/Name.java
+++ src/main/java/ladder/domain/Name.java
@@ -2,12 +2,18 @@ package ladder.domain;
 
 public class Name {
     private static final int MAX_SIZE = 5;
+    private static final String GAME_COMMAND = "all";
     private final String name;
 
     public Name(String name) {
+        validate(name);
+        this.name = name;
+    }
+
+    private void validate(String name) {
         validateEmpty(name);
         validateOutOfLengthRange(name);
-        this.name = name;
+        validateGameCommand(name);
     }
 
     private void validateEmpty(String name) {
@@ -19,6 +25,12 @@ public class Name {
     private void validateOutOfLengthRange(String name) {
         if (name.length() > MAX_SIZE) {
             throw new IllegalArgumentException("이름은 최소 1글자, 최대 5글자여야 합니다.");
+        }
+    }
+
+    private void validateGameCommand(String name) {
+        if(name.equals(GAME_COMMAND)) {
+            throw new IllegalArgumentException("all이라는 이름을 쓸 수 없습니다.");
         }
     }
 
