dd3cc095762b8d42d6edbeb5dd84fe77e196fa8d
BGuga <tmdwns9913@gmail.com>
2023-02-24 19:53:32 +0900

    style: 도메인 패키지 구조에 맞게끔 위치 변경

--- src/test/java/ladder/controller/PrintCommandTest.java
+++ src/test/java/ladder/controller/PrintCommandTest.java
@@ -1,6 +1,5 @@
-package ladder.domain;
+package ladder.controller;
 
-import ladder.controller.PrintCommand;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
