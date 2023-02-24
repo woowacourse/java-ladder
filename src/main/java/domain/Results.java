2d2d8354c52165694baced341b582be26f7d6a0d
parkmuhyeun <pjhg410@gmail.com>
2023-02-25 01:52:35 +0900

    feat: 최종 결과를 반환하는 기능 추가

--- src/main/java/domain/Results.java
+++ src/main/java/domain/Results.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 public class Results {
 
+    private static final String ALL_PLAYER = "all";
+
     static final String SIZE_ERROR = "[ERROR] 결과값을 참여자 수 만큼 입력해야합니다.";
 
     private final List<Result> results = new ArrayList<>();
@@ -26,7 +28,7 @@ public class Results {
         }
     }
 
-    public List<String> getResults() {
+    public List<String> getResult() {
         List<String> results = new ArrayList<>();
 
         for (Result result : this.results) {
@@ -39,4 +41,31 @@ public class Results {
         return results.get(position)
                 .getName();
     }
+
+    public List<String> getFinalResults(List<String> playersName, List<Integer> lastPositions, String targetPlayer) {
+        List<String> results = new ArrayList<>();
+
+        addResult(playersName, lastPositions, targetPlayer, results);
+        return results;
+    }
+
+    private void addResult(List<String> playersName, List<Integer> lastPositions,
+                           String targetPlayer, List<String> results) {
+        if (targetPlayer.equals(ALL_PLAYER)) {
+            addPlayersResult(playersName, lastPositions, results);
+            return;
+        }
+        addPlayerResult(results, lastPositions.get(playersName.indexOf(targetPlayer)));
+    }
+
+    private void addPlayersResult(List<String> playersName, List<Integer> lastPositions, List<String> results) {
+        for (int index = 0; index < playersName.size(); index++) {
+            results.add(getResult(lastPositions.get(index)));
+        }
+    }
+
+    private void addPlayerResult(List<String> results, int lastPosition) {
+        results.add(getResult(lastPosition));
+    }
+
 }
