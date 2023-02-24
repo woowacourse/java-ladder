6457ea783c651dffecf7fe988a9f79af41a82c18
greeng00se <thegreengoose7@gmail.com>
2023-02-24 14:07:50 +0900

    remove: 사용하지 않는 equals & hashcode 제거

--- src/main/java/ladder/domain/player/PlayerName.java
+++ src/main/java/ladder/domain/player/PlayerName.java
@@ -1,7 +1,5 @@
 package ladder.domain.player;
 
-import java.util.Objects;
-
 public class PlayerName {
     private static final int NAME_LENGTH_UPPER_BOUND = 5;
     private static final String INVALID_NAME_LENGTH_MESSAGE =
@@ -39,23 +37,6 @@ public class PlayerName {
 
     private boolean isReservedName(final String name) {
         return RESERVED_NAME.equals(name);
-    }
-
-    @Override
-    public boolean equals(final Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        final PlayerName that = (PlayerName) o;
-        return Objects.equals(value, that.value);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(value);
     }
 
     public String getValue() {
