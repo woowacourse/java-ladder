4cc465a365c2988ee62e2f58f8c81d7c88f84e8a
kwonyj1022 <kwonyj1022@naver.com>
2023-02-24 13:47:58 +0900

    feat(LadderGame): 모든 참가자를 사다리를 타게 하고, 각 참가자의 당첨 결과를 모아 GameResult를 반환하는 메서드 추가

---
+++ src/main/java/domain/ladderGame/LadderGame.java
@@ -0,0 +1,35 @@
+package domain.ladderGame;
+
+import domain.item.Items;
+import domain.ladder.Ladder;
+import domain.player.Player;
+import domain.player.Players;
+
+import java.util.LinkedHashMap;
+
+public class LadderGame {
+
+    private final Players players;
+    private final Ladder ladder;
+    private final Items items;
+
+    public LadderGame(GameInit gameInit) {
+        this.players = gameInit.getPlayers();
+        this.ladder = gameInit.getLadder();
+        this.items = gameInit.getItems();
+    }
+
+    public void play() {
+        for (Player player : players.getPlayers()) {
+            player.goToResult(ladder.getLines(), items.getItems());
+        }
+    }
+
+    public GameResult getGameResult() {
+        LinkedHashMap<String, String> gameResult = new LinkedHashMap<>();
+        for (Player player : players.getPlayers()) {
+            gameResult.put(player.getName(), player.getResult());
+        }
+        return new GameResult(gameResult);
+    }
+}
