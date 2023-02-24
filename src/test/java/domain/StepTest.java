2c95894fd77113c5cd2bf4d898738f85fefa5ec4
song-wooseok <sws981202@gmail.com>
2023-02-24 16:34:45 +0900

    test: 디딤대의 방향에 따라서 옮겨진 위치를 반환하는 기능 테스트

--- src/test/java/domain/StepTest.java
+++ src/test/java/domain/StepTest.java
@@ -20,4 +20,25 @@ class StepTest {
         assertThat(Step.RIGHT.isSteppable())
                 .isTrue();
     }
+
+    @Test
+    @DisplayName(" 오른쪽 발판일 때 사용자 위치를 오른쪽으로 보낸다.")
+    void whenConnectedToRight_thenMoveRight() {
+        assertThat(Step.RIGHT.move(2))
+                .isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName(" 왼쪽 발판일 때 사용자 위치를 오른쪽으로 보낸다.")
+    void whenConnectedToLeft_thenMoveLeft() {
+        assertThat(Step.LEFT.move(2))
+                .isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName(" 없을 때 아래로 보내기 때문에 값이 변하지 않는다.")
+    void givenCurrentPosition_thenGiveNextLinePosition() {
+        assertThat(Step.NONE.move(2))
+                .isEqualTo(2);
+    }
 }
