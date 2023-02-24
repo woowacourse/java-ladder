b0c49754f4a67b71746a834301169e62eb67506e
iamjooon2 <iamjooon2@gmail.com>
2023-02-24 17:35:25 +0900

    fix: Ladder 클래스에서 필요없는 유효성검사 코드 제거

--- src/main/java/domain/Ladder.java
+++ src/main/java/domain/Ladder.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Ladder {
     private static final String ERROR_SAME_LINE_SIZE = "[ERROR] 다른 길이의 가로줄이 생성되었습니다";
@@ -16,28 +17,6 @@ public class Ladder {
         this.lines = IntStream.range(0, height.getValue())
                 .mapToObj(it -> new Line(width, scaffoldGenerator))
                 .collect(Collectors.toUnmodifiableList());
-        validateLines(lines);
-    }
-
-    private void validateLines(final List<Line> lines) {
-        validateLineSizeEmpty(lines);
-        validateLinesSameSize(lines);
-    }
-
-    private void validateLineSizeEmpty(final List<Line> lines) {
-        if (lines.isEmpty()) {
-            throw new IllegalArgumentException(ERROR_LINE_SIZE_EMPTY);
-        }
-    }
-
-    private void validateLinesSameSize(final List<Line> lines) {
-        IntStream.range(0, lines.size() - 1).forEach(i -> validateLineSameSize(lines, i));
-    }
-
-    private void validateLineSameSize(final List<Line> lines, final int index) {
-        if (lines.get(index).size() != lines.get(index + 1).size()) {
-            throw new IllegalArgumentException(ERROR_SAME_LINE_SIZE);
-        }
     }
 
     public Map<String, String> calculateResult(final Names names, final Prizes prizes) {
