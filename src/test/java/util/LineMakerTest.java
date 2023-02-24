309bdc387becc094fb04552a410e666cb1b5fb83
Miseong Kim <miseongk@naver.com>
2023-02-24 11:29:01 +0900

    test: 라인의 상태 값 추가 및 랜덤 생성 검증

--- src/test/java/util/LineMakerTest.java
+++ src/test/java/util/LineMakerTest.java
@@ -1,24 +1,50 @@
 package util;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import domain.LineStatus;
+import java.util.ArrayList;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LineMakerTest {
 
+    private List<LineStatus> line;
+
+    @BeforeEach
+    void init() {
+        line = new ArrayList<>();
+    }
+
     @Test
-    @DisplayName("라인이 겹치는 경우가 있는지 확인")
+    @DisplayName("라인의 첫번째 Status 추가")
+    void makeFirstLineStatus() {
+        LineMaker.makeFirstLineStatus(new ExistLineGenerator(), line);
+
+        assertThat(line.get(0)).isEqualTo(LineStatus.EXIST);
+    }
+
+    @Test
+    @DisplayName("라인의 첫번째 이후의 Status 추가")
+    void makeElseLineStatus() {
+        LineMaker.makeFirstLineStatus(new ExistLineGenerator(), line);
+        LineMaker.makeElseLineStatus(new ExistLineGenerator(), line, 3);
+
+        assertAll(
+                () -> assertThat(line.get(1)).isEqualTo(LineStatus.EXIST),
+                () -> assertThat(line.get(2)).isEqualTo(LineStatus.EXIST)
+        );
+    }
+
+    @Test
+    @DisplayName("이전 Status가 true이면 이후 Status는 무조건 false이다.")
     void makeRandomLine() {
-        int numberOfLine = 1000;
-        List<LineStatus> line = LineMaker.makeLine(new RandomLineGenerator(), numberOfLine);
-        for (int i = 0; i < numberOfLine - 1; i++) {
-            int rightLine = i+1;
-            if (line.get(i) == LineStatus.EXIST) {
-                assertThat(line.get(rightLine).getStatus()).isFalse();
-            }
-        }
+        line.add(LineStatus.EXIST);
+
+        LineGenerator lineGenerator = new RandomLineGenerator();
+        assertThat(lineGenerator.generate(line.get(0).getStatus())).isFalse();
     }
 }
