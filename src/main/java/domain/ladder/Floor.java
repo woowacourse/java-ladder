0a2ddd5b3cf38c1eb2e7a4d2617772d9ab9e070b
Hyeonjae-K <kwonhyeonjae01@gmail.com>
2023-02-24 14:29:31 +0900

    refactor: Floor 리팩터링

--- src/main/java/domain/ladder/Floor.java
+++ src/main/java/domain/ladder/Floor.java
@@ -10,28 +10,33 @@ public class Floor {
     private final List<Boolean> points;
 
     public Floor(final int namesSize, final BooleanGenerator booleanGenerator) {
-        this.points = new ArrayList<>();
-        addPoints(calculatePointsSize(namesSize), booleanGenerator);
+        this.points = generatePoints(calculatePointsSize(namesSize), booleanGenerator);
     }
 
     private static int calculatePointsSize(final int namesSize) {
         return namesSize - DIFFERENCE_NAME_SIZE_AND_FLOOR_SIZE;
     }
 
-    private void addPoints(final int pointsSize, final BooleanGenerator booleanGenerator) {
+    private static List<Boolean> generatePoints(final int pointsSize, final BooleanGenerator booleanGenerator) {
+        List<Boolean> points = new ArrayList<>();
+
         for (int i = 0; i < pointsSize; i++) {
-            points.add(generatePoint(i, booleanGenerator));
+            points.add(generatePoint(i, points, booleanGenerator));
         }
+
+        return points;
     }
 
-    private boolean generatePoint(final int index, final BooleanGenerator booleanGenerator) {
-        if (isUnableTrue(index)) {
+    private static boolean generatePoint(final int index,
+                                         final List<Boolean> points,
+                                         final BooleanGenerator booleanGenerator) {
+        if (isUnableTrue(index, points)) {
             return false;
         }
         return booleanGenerator.generate();
     }
 
-    private boolean isUnableTrue(final int index) {
+    private static boolean isUnableTrue(final int index, final List<Boolean> points) {
         return index > 0 && points.get(index - 1);
     }
 
