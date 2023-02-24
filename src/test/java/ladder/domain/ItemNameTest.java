7f6615957827ec438a7e29b0c4c3c412737f140e
greeng00se <thegreengoose7@gmail.com>
2023-02-24 11:42:39 +0900

    feat: Item 이름에 대한 원시값 포장 클래스 추가

---
+++ src/test/java/ladder/domain/ItemNameTest.java
@@ -0,0 +1,28 @@
+package ladder.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class ItemNameTest {
+
+    @ParameterizedTest(name = "이름이 비어있거나 6자 이상인 경우 예외를 던진다. 입력값: \"{0}\"")
+    @NullAndEmptySource
+    @ValueSource(strings = {"우아한상품들"})
+    void 이름이_비어있거나_6자_이상인_경우_예외를_던진다(final String name) {
+        assertThatThrownBy(() -> new ItemName(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("실행 결과명은 1자 이상, 5자 이하여야 합니다.");
+    }
+
+    @Test
+    void 실행결과는_이름을_가진다() {
+        final ItemName name = new ItemName("name");
+
+        assertThat(name.getValue()).isEqualTo("name");
+    }
+}
