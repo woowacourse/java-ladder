93dcdba00f113923d15e7db63f7bba69e6a844ab
70825 <hello70825@gmail.com>
2023-02-24 22:06:42 +0900

    refactor: 에러메시지 수정 및 상수로 변경

--- src/main/java/ladder/domain/Direction.java
+++ src/main/java/ladder/domain/Direction.java
@@ -9,6 +9,8 @@ public enum Direction {
     RIGHT(1),
     ;
 
+    private static final String INVALID_VALUE_MESSAGE = "해당 값을 가지는 방향이 존재하지 않습니다.";
+
     private final int move;
 
     Direction(final int move) {
@@ -19,7 +21,7 @@ public enum Direction {
         return Arrays.stream(Direction.values())
                 .filter(direction -> direction.getMove() == value)
                 .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("유효하지 않은 값입니다."));
+                .orElseThrow(() -> new IllegalArgumentException(INVALID_VALUE_MESSAGE));
     }
 
     public int getMove() {
