cfcf44d9e87abba5d695b9290c0b6e8e8dd62f54
Kong-hana01 <gksqlsl11@khu.ac.kr>
2023-02-26 02:38:17 +0900

    refactor: Prize 클래스 일급 컬렉션으로 변경

--- src/main/java/domain/Prizes.java
+++ src/main/java/domain/Prizes.java
@@ -6,23 +6,17 @@ import java.util.stream.Collectors;
 
 public class Prizes {
 
-    private static final String INVALID_PRIZE_LENGTH_ERROR_MESSAGE = "사다리 게임의 실행 결과는 1글자에서 5글자 사이로 입력해야합니다.";
     private static final String INVALID_PRIZE_COUNT_ERROR_MESSAGE = "사다리 게임의 실행 결과는 사람 수와 동일하게 입력해야합니다.";
-    private static final int PRIZE_LENGTH_LOWER_BOUND = 1;
-    private static final int PRIZE_LENGTH_UPPER_BOUND = 5;
 
-
-    private final List<String> prizes;
+    private final List<Prize> prizes;
 
     public Prizes(List<String> prizes, int personCount) {
-        List<String> timedPrizes = trimPrizes(prizes);
-        validate(timedPrizes, personCount);
-        this.prizes = timedPrizes;
+        validate(prizes, personCount);
+        this.prizes = generatePrizes(prizes);
     }
 
     private void validate(List<String> prizes, int personCount) {
         validatePrizesCount(prizes, personCount);
-        validatePrizesLength(prizes);
     }
 
     private void validatePrizesCount(List<String> prizes, int personCount) {
@@ -35,34 +29,17 @@ public class Prizes {
         return prizes.size() == personCount;
     }
 
-    private void validatePrizesLength(List<String> prizes) {
-        if (hasInvalidPrizeLength(prizes)) {
-            throw new IllegalArgumentException(INVALID_PRIZE_LENGTH_ERROR_MESSAGE);
-        }
-    }
-
-    private boolean hasInvalidPrizeLength(List<String> prizes) {
-        return prizes.stream()
-                .filter(prize -> isInvalidNameLength(prize))
-                .count() != 0;
-    }
-
-    private boolean isInvalidNameLength(String prize) {
-        int length = prize.length();
-        return length < PRIZE_LENGTH_LOWER_BOUND || length > PRIZE_LENGTH_UPPER_BOUND;
-    }
-
-    private List<String> trimPrizes(List<String> prizes) {
-        return prizes.stream()
-                .map(prize -> prize.trim())
-                .collect(Collectors.toList());
-    }
-
-    public String getOnePrizeByIndex(int index) {
+    public Prize getOnePrizeByIndex(int index) {
         return prizes.get(index);
     }
 
-    public List<String> getPrizes() {
+    private List<Prize> generatePrizes(List<String> prizes) {
+        return prizes.stream()
+                .map(Prize::new)
+                .collect(Collectors.toList());
+    }
+
+    public List<Prize> getPrizes() {
         return Collections.unmodifiableList(prizes);
     }
 }
