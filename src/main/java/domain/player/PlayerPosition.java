87729ec1579c773368b9a7a801fb565e91e4f10c
ingpyo <juninpyo726@gmail.com>
2023-02-25 15:36:52 +0900

    feat: 포지션 예외처리

--- src/main/java/domain/player/PlayerPosition.java
+++ src/main/java/domain/player/PlayerPosition.java
@@ -3,7 +3,11 @@ package domain.player;
 public class PlayerPosition {
     private int position;
 
+    private static final String UNABLE_MOVE_MESSAGE = "[ERROR] 사다리 밖으로 이동이 불가능 합니다.";
+    private static final int MIN_LADDER_INDEX = 0;
+
     public PlayerPosition(int position) {
+        checkPosition(position);
         this.position = position;
     }
 
@@ -17,5 +21,11 @@ public class PlayerPosition {
 
     public int getPosition() {
         return position;
+    }
+
+    private void checkPosition(int position) {
+        if (position < MIN_LADDER_INDEX) {
+            throw new IllegalArgumentException(UNABLE_MOVE_MESSAGE);
+        }
     }
 }
