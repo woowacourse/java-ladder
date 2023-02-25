b105298aa129749c5fe54aa915faabc40e72488c
SproutMJ <mjk54565@gmail.com>
2023-02-25 22:44:02 +0900

    리스트 이해하기 ArrayList 학습 테스트

---
+++ src/test/java/techcourse/jcf/mission/ListStudy.java
@@ -0,0 +1,26 @@
+package techcourse.jcf.mission;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class ListStudy {
+    @Test
+    public void arrayList() {
+        ArrayList<String> values = new ArrayList<>();
+        values.add("first");
+        values.add("second");
+
+        assertThat(values.add("third")).isTrue();
+        assertThat(values.size()).isEqualTo(3);
+        assertThat(values.get(0)).isEqualTo("first");
+        assertThat(values.contains("first")).isTrue();
+        assertThat(values.remove(0)).isEqualTo("first"); // 첫 번째 값을 삭제한다.
+        assertThat(values.size()).isEqualTo(2);
+
+        values.forEach(System.out::println);
+    }
+}
