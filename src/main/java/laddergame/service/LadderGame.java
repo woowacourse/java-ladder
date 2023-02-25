760190e1869d518147ee00dd049ee1156ce12b90
yujamint <woawoa0513@gmail.com>
2023-02-25 20:22:50 +0900

    refactor: Point 도메인 클래스 생성에 따른 서비스 코드 수정

--- src/main/java/laddergame/service/LadderGame.java
+++ src/main/java/laddergame/service/LadderGame.java
@@ -1,20 +1,16 @@
 package laddergame.service;
 
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 import laddergame.domain.GameResult;
+import laddergame.domain.Position;
 import laddergame.domain.Results;
 import laddergame.domain.Ladder;
 import laddergame.domain.Line;
-import laddergame.domain.Point;
 import laddergame.domain.User;
 import laddergame.domain.Users;
 
 public class LadderGame {
-
-    private static final int MINIMUM_POSITION = 0;
-
     private final Ladder ladder;
     private final Users users;
     private final Results results;
@@ -28,41 +24,19 @@ public class LadderGame {
     public Map<String, String> play() {
         Map<String, String> gameResultByUserName = new LinkedHashMap<>();
         for (User user : users.getUsers()) {
-            int userPosition = users.getUsers().indexOf(user);
+            Position userPosition = new Position(users.getUsers().indexOf(user), users.count());
 
-            userPosition = progressLines(userPosition);
+            progressLines(userPosition);
 
-            GameResult gameResult = results.getResults().get(userPosition);
+            GameResult gameResult = results.getResultByPosition(userPosition);
             gameResultByUserName.put(user.getName(), gameResult.getResult());
         }
         return gameResultByUserName;
     }
 
-    private int progressLines(int userPosition) {
+    private void progressLines(Position userPosition) {
         for (Line line : ladder.getLines()) {
-            userPosition = moveUserByConnectionStatus(userPosition, line);
+            userPosition.moveByConnectionStatus(line);
         }
-        return userPosition;
-    }
-
-    private int moveUserByConnectionStatus(int userPosition, Line line) {
-        List<Point> points = line.getPoints();
-        
-        if (isMovableToRight(userPosition, points)) {
-            return ++userPosition;
-        }
-        if (isMovableToLeft(userPosition, points)) {
-            return --userPosition;
-        }
-
-        return userPosition;
-    }
-
-    private boolean isMovableToRight(int userPosition, List<Point> points) {
-        return userPosition < points.size() && points.get(userPosition).isConnected();
-    }
-
-    private boolean isMovableToLeft(int userPosition, List<Point> points) {
-        return userPosition > MINIMUM_POSITION && points.get(userPosition - 1).isConnected();
     }
 }
