8df07f46617dba5433139dc4bd5a37ac73a37f9b
Kong-hana01 <gksqlsl11@khu.ac.kr>
2023-02-26 03:19:07 +0900

    refactor: 사용하지 않는 import 제거

--- src/test/java/domain/PrizeTest.java
+++ src/test/java/domain/PrizeTest.java
@@ -4,11 +4,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
-import utils.Parser;
 
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
