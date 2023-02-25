a1ce7c09972844a7e2ce7c6560a1ae9c8ace2859
hong-sile <gurwns9325@naver.com>
2023-02-25 19:59:42 +0900

    feat: prize에 equals와 hashcode 재정의 name이 같으면 같은 객체로 판단

--- src/main/java/domain/prize/Prize.java
+++ src/main/java/domain/prize/Prize.java
@@ -1,5 +1,7 @@
 package domain.prize;
 
+import java.util.Objects;
+
 public class Prize {
 
     private final String name;
@@ -10,5 +12,22 @@ public class Prize {
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final Prize prize = (Prize) o;
+        return Objects.equals(name, prize.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
