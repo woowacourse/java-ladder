c902cb7be8461bc211156b292edcad17ccbd7a3c
ingpyo <juninpyo726@gmail.com>
2023-02-25 15:29:32 +0900

    refactor: 예외 검사 삭제

--- src/main/java/domain/player/Player.java
+++ src/main/java/domain/player/Player.java
@@ -4,32 +4,12 @@ package domain.player;
 import domain.Direction;
 
 public class Player {
-    private static final String BLANK_MESSAGE = "[ERROR] 입력값이 비어있습니다.";
-    private static final String NULL_MESSAGE = "[ERROR] 아무것도 입력하지 않았습니다.";
     private final PlayerName name;
     private final PlayerPosition position;
 
     public Player(String playerName, int playerPosition) {
-        checkNotEmpty(playerName);
         this.name = new PlayerName(playerName);
         this.position = new PlayerPosition(playerPosition);
-    }
-
-    private void checkNotEmpty(String playerName) {
-        checkNull(playerName);
-        checkBlank(playerName);
-    }
-
-    private static void checkBlank(String playerName) {
-        if (playerName.isBlank()) {
-            throw new IllegalArgumentException(BLANK_MESSAGE);
-        }
-    }
-
-    private static void checkNull(String playerName) {
-        if (playerName == null) {
-            throw new IllegalArgumentException(NULL_MESSAGE);
-        }
     }
 
     public void playerMove(Direction direction) {
