e757fb759ff315a4d4a030a4741a2145fb2d2b5b
chanminmaxscaler <jcm04@naver.com>
2023-02-25 20:55:03 +0900

    refactor: Line 클래스를 리팩토링한다

--- src/main/java/laddergame/domain/ladder/Line.java
+++ src/main/java/laddergame/domain/ladder/Line.java
@@ -9,6 +9,7 @@ public class Line {
 
     private static final boolean DOES_NOT_EXIST = false;
     private static final int MINIMUM_INDEX = 0;
+    private static final int ORDER_UNIT = 1;
 
     private final List<Rung> rungs;
     private final BooleanGenerator rungBooleanGenerator;
@@ -36,18 +37,18 @@ public class Line {
         rungs.add(firstRung);
 
         for (int order = 1; order < rungCount; order++) {
-            Rung previousRung = rungs.get(order - 1);
-            Rung newRung = createRung(previousRung);
+            Rung previousRung = rungs.get(order - ORDER_UNIT);
+            Rung newRung = Rung.create(generateWith(previousRung));
             rungs.add(newRung);
         }
         return rungs;
     }
 
-    private Rung createRung(final Rung previousRung) {
+    private boolean generateWith(final Rung previousRung) {
         if (previousRung.exists()) {
-            return Rung.create(false);
+            return DOES_NOT_EXIST;
         }
-        return Rung.create(rungBooleanGenerator.generate());
+        return rungBooleanGenerator.generate();
     }
 
     public List<Rung> getRungs() {
