29c276729463157703ee23945e9cf1415781c3f6
bjk1649 <qhwns1649@naver.com>
2023-02-25 18:14:50 +0900

    test: 모든 사다리 게임 결과를 가져오는 메소드의 테스트코드 추가

--- src/test/java/domain/LadderGameTest.java
+++ src/test/java/domain/LadderGameTest.java
@@ -3,6 +3,9 @@ package domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -10,12 +13,25 @@ class LadderGameTest {
 
     @ParameterizedTest(name = "이름이 입력되면 알맞은 결과를 반환해야한다. name: {0}, expectedResult: {1}")
     @CsvSource(value = {"pobi:포비결과", "glen:글렌결과", "vero:베로결과"}, delimiter = ':')
-    void get_appropriate_position(String name, String expectedResult) {
+    void get_appropriate_name_result(String name, String expectedResult) {
         LadderGame ladderGame = createLadderGame();
 
         Result result = ladderGame.getResultOf(name);
 
         assertThat(result).isEqualTo(new Result(expectedResult));
+    }
+
+    @DisplayName("게임의 모든 결과를 정확하게 가져와야 한다.")
+    @Test
+    void get_all_result() {
+        LadderGame ladderGame = createLadderGame();
+        Results expected = new Results(List.of(new Result("포비결과"), new Result("글렌결과"), new Result("베로결과")),
+                new Names(List.of(new Name("pobi"), new Name("glen"), new Name("vero"))));
+        AtomicInteger index = new AtomicInteger();
+
+        Results results = ladderGame.getAllResults();
+
+        results.forEach(result -> assertThat(result).isEqualTo(expected.get(index.getAndIncrement())));
     }
 
 
