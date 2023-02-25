be9121bc9fd4a5d88bcb0fedbcd1422812281953
aiaiaiai1 <cvc100326@naver.com>
2023-02-25 17:43:44 +0900

    test: 플레이어의 이름이 all인 경우 예외 발생기능 테스트 + Name -> PlayerName 클래스이름 변경으로 인한 수정

--- src/test/java/domain/PlayerNameTest.java
+++ src/test/java/domain/PlayerNameTest.java
@@ -8,29 +8,37 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
-public class NameTest {
+public class PlayerNameTest {
     @ParameterizedTest
     @DisplayName("이름이 5자 이하고 공백이 아닌 경우 예외가 발생하지 않는다.")
     @ValueSource(strings = {"무", "12345", "루 쿠  "})
     void create_player_Name(String name) {
-        assertDoesNotThrow(() -> new Name(name));
+        assertDoesNotThrow(() -> new PlayerName(name));
     }
 
     @Test
     @DisplayName("이름이 5자를 초과하면 예외가 발생한다.")
     void name_6() {
-        assertThatThrownBy(() -> new Name("123456"))
+        assertThatThrownBy(() -> new PlayerName("123456"))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("글자 길이는 5자를 넘길 수 없습니다.");
+                .hasMessage("이름 길이는 5자를 넘길 수 없습니다.");
     }
 
     @DisplayName("이름이 공백이면 에외가 발생한다.")
     @ParameterizedTest
     @ValueSource(strings = {"", " ", "   "})
     void name_blank(String blankName) {
-        assertThatThrownBy(() -> new Name(blankName))
+        assertThatThrownBy(() -> new PlayerName(blankName))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("빈 문자열 입니다.");
+    }
+
+    @DisplayName("이름이 all인 경우 예외가 발생한다")
+    @Test
+    void name_reserved(){
+        assertThatThrownBy(() -> new PlayerName("all"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("불가능한 이름입니다.");
     }
 
 }
