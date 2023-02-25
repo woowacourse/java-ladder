f5677aff9d1a58c2c291ad794a9d5484a1654175
yujamint <woawoa0513@gmail.com>
2023-02-25 20:11:02 +0900

    feat: 사용자의 위치를 감싸는 Position 클래스 생성

---
+++ src/main/java/laddergame/domain/Position.java
@@ -0,0 +1,43 @@
+package laddergame.domain;
+
+import java.util.List;
+
+public class Position {
+
+    private int position;
+
+    public Position(int position, int userCount) {
+        validateInitialPosition(position, userCount);
+        this.position = position;
+    }
+
+    public int moveByConnectionStatus(Line line) {
+        List<Point> points = line.getPoints();
+
+        if (isMovableToRight(points)) {
+            return ++position;
+        }
+        if (isMovableToLeft(points)) {
+            return --position;
+        }
+        return position;
+    }
+
+    private void validateInitialPosition(int position, int userCount) {
+        if (position < 0 || position >= userCount) {
+            throw new IllegalArgumentException("유저의 위치는 0 이상, 총 유저 수 미만이어야 합니다.");
+        }
+    }
+
+    private boolean isMovableToRight(List<Point> points) {
+        return position < points.size() && points.get(position).isConnected();
+    }
+
+    private boolean isMovableToLeft(List<Point> points) {
+        return position > 0 && points.get(position - 1).isConnected();
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
