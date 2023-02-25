10bdc1074fc132b50291f101d247f0172924f271
swonny <wonny921@gmail.com>
2023-02-25 19:16:27 +0900

    feat: Point 위치에 따라 움직여야할 위치를 갖는 enum 생성

---
+++ src/main/java/domain/PointSide.java
@@ -0,0 +1,18 @@
+package domain;
+
+public enum PointSide {
+
+    LEFT(-1),
+    RIGHT(1),
+    NONE(0);
+
+    private final int movingPosition;
+
+    PointSide(int movingPosition) {
+        this.movingPosition = movingPosition;
+    }
+
+    public int getMovingPosition() {
+        return this.movingPosition;
+    }
+}
