2171f9068b5017870f8a828e939fb603de8aeec6
kdkdhoho <hkim4410@naver.com>
2023-02-25 14:05:39 +0900

    refactor(OutputView): 도메인 데이터가 View로 나가지 않도록 리팩터링
    - Main 함수에서 데이터를 정제하여 View에게 전달하는 방식으로 변경

--- src/main/java/ladder/Application.java
+++ src/main/java/ladder/Application.java
@@ -1,5 +1,6 @@
 package ladder;
 
+import static java.util.stream.Collectors.joining;
 import static ladder.view.InputView.readLadderHeight;
 import static ladder.view.InputView.readPlayerNames;
 import static ladder.view.InputView.readResults;
@@ -11,10 +12,15 @@ import static ladder.view.OutputView.printSingleResult;
 import ladder.domain.Height;
 import ladder.domain.Ladder;
 import ladder.domain.LadderFactory;
+import ladder.domain.Line;
+import ladder.domain.Player;
 import ladder.domain.Players;
+import ladder.domain.Result;
 import ladder.domain.Results;
+import ladder.domain.Step;
 import ladder.domain.strategy.linestrategy.LineStrategy;
 import ladder.domain.strategy.linestrategy.RandomLineStrategy;
+import java.util.List;
 
 public class Application {
     private static final String QUIT = "q";
@@ -33,12 +39,35 @@ public class Application {
         players = new Players(readPlayerNames());
         results = new Results(readResults(), players.getPlayersCount());
         height = new Height(readLadderHeight());
-
         ladderFactory = new LadderFactory(height, players, lineStrategy);
         ladder = ladderFactory.makeLadder();
-        printLadder(players.getPlayers(), ladder.getLines(), results.getResults());
 
+        printLadder(playersToNames(players.getPlayers()),
+                ladderToString(ladder.getLines()),
+                resultsToString(results.getResults()));
         askResult();
+    }
+
+    private static String playersToNames(List<Player> players) {
+        return players.stream()
+                .map(Player::getName)
+                .map(name -> String.format("%-5s", name))
+                .collect(joining(" "));
+    }
+
+    private static String ladderToString(List<Line> lines) {
+        return lines.stream()
+                .map(line -> "|" + line.getSteps().stream()
+                        .map(Step::getShape)
+                        .collect(joining("|")) + "|")
+                .collect(joining(System.lineSeparator()));
+    }
+
+    private static String resultsToString(List<Result> results) {
+        return results.stream()
+                .map(Result::getResult)
+                .map(result -> String.format("%-5s", result))
+                .collect(joining(" "));
     }
 
     private static void askResult() {
@@ -60,10 +89,18 @@ public class Application {
 
     private static boolean isAll(boolean proceed, String input) {
         if (proceed && input.equals(ALL)) {
-            printAllResult(players.getPlayerNames(), results.findAllResult(ladder));
+            printAllResult(toAllResult(players.getPlayerNames(), results.findAllResult(ladder)));
             return FINISH;
         }
         return PROCEED;
+    }
+
+    private static String toAllResult(List<String> players, List<String> allResult) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < players.size(); i++) {
+            stringBuilder.append(players.get(i) + " : " + allResult.get(i) + System.lineSeparator());
+        }
+        return stringBuilder.toString();
     }
 
     private static void isSingleResult(boolean proceed, String playerName) {
--- src/main/java/ladder/view/OutputView.java
+++ src/main/java/ladder/view/OutputView.java
@@ -1,45 +1,12 @@
 package ladder.view;
 
-import static java.util.stream.Collectors.joining;
-
-import ladder.domain.Line;
-import ladder.domain.Player;
-import ladder.domain.Result;
-import ladder.domain.Step;
-import java.util.List;
-
 public class OutputView {
 
-    public static void printLadder(List<Player> players, List<Line> lines, List<Result> results) {
+    public static void printLadder(String names, String ladder, String results) {
         System.out.println(System.lineSeparator() + "사다리 결과" + System.lineSeparator());
-        printNames(players);
-        printLadder(lines);
-        printResults(results);
-    }
-
-    private static void printNames(List<Player> players) {
-        String result = players.stream()
-                .map(Player::getName)
-                .map(name -> String.format("%-5s", name))
-                .collect(joining(" "));
-        System.out.println(result);
-    }
-
-    private static void printLadder(List<Line> lines) {
-        String result = lines.stream()
-                .map(line -> "|" + line.getSteps().stream()
-                        .map(Step::getShape)
-                        .collect(joining("|")) + "|")
-                .collect(joining(System.lineSeparator()));
-        System.out.println(result);
-    }
-
-    private static void printResults(List<Result> results) {
-        String message = results.stream()
-                .map(Result::getResult)
-                .map(result -> String.format("%-5s", result))
-                .collect(joining(" "));
-        System.out.println(message);
+        System.out.println(names);
+        System.out.println(ladder);
+        System.out.println(results);
     }
 
     public static void printSingleResult(String result) {
@@ -47,11 +14,8 @@ public class OutputView {
         System.out.println(result);
     }
 
-    public static void printAllResult(List<String> players, List<String> allResult) {
-        StringBuilder stringBuilder = new StringBuilder(System.lineSeparator() + "실행 결과" + System.lineSeparator());
-        for (int i = 0; i < players.size(); i++) {
-            stringBuilder.append(players.get(i) + " : " + allResult.get(i) + System.lineSeparator());
-        }
-        System.out.print(stringBuilder);
+    public static void printAllResult(String allResult) {
+        System.out.print(System.lineSeparator() + "실행 결과" + System.lineSeparator());
+        System.out.print(allResult);
     }
 }
