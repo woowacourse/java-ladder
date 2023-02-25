d56e6d9e978c6da22984549241b93d726466e496
Eunkeee <eunkeeeee@gmail.com>
2023-02-25 15:34:04 +0900

    test(Line): 사다리 이동 테스트 코드 작성

--- src/test/java/domain/LineTest.java
+++ src/test/java/domain/LineTest.java
@@ -1,5 +1,6 @@
 package domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import domain.generator.BooleanGenerator;
@@ -7,36 +8,69 @@ import domain.generator.RandomBooleanGenerator;
 import domain.generator.TrueBooleanGenerator;
 import domain.ladder.LadderStep;
 import domain.ladder.Line;
-import org.assertj.core.api.Assertions;
+import domain.player.Position;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class LineTest {
     private final BooleanGenerator randomLadderGenerator = new RandomBooleanGenerator();
     private final BooleanGenerator nonRandomLadderGenerator = new TrueBooleanGenerator();
-    private final Line nonRandomLine = new Line(3, nonRandomLadderGenerator);
 
+    @DisplayName("사다리 값에 따라서 사다리를 생성할 수 있다.")
     @Nested
-    @DisplayName("사다리 발판 생성 테스트")
-    class createLadderStepsTest {
+    class createLineTest {
+        @DisplayName("랜덤으로 사다리를 생성할 수 있다.")
         @Test
-        void createRandomLadderStepsTest() {
+        void createRandomLineTest() {
             assertDoesNotThrow(() -> new Line(3, randomLadderGenerator));
         }
 
+        @DisplayName("같은 값을 입력하면 항상 같은 사다리가 생성된다.")
         @Test
         void createNonRandomLadderStepsTest() {
-            Assertions.assertThat(nonRandomLine.getLadderSteps())
+            assertThat(new Line(3, nonRandomLadderGenerator).getLadderSteps())
                     .containsExactly(LadderStep.EXISTS, LadderStep.NONE, LadderStep.EXISTS);
         }
     }
 
- /*   @Test
-    void 사다리를_타고_한칸_이동한다() {
-        assertEquals(nonRandomLine.getNextStepIndex2(0), 1);
-        assertEquals(nonRandomLine.getNextStepIndex2(1), 0);
-        assertEquals(nonRandomLine.getNextStepIndex2(2), 3);
-        assertEquals(nonRandomLine.getNextStepIndex2(3), 2);
-    }*/
+    @DisplayName("각 방향으로 움직일 수 있는지 확인할 수 있다.")
+    @Nested
+    class canMoveTest {
+        private final Line line = new Line(5, nonRandomLadderGenerator);
+
+        /*
+        |-----|     |-----|     |-----|
+        */
+
+        @DisplayName("왼쪽으로 움질일 수 있는지 확인할 수 있다.")
+        @ParameterizedTest
+        @CsvSource({
+                "0,false",
+                "1,true",
+                "2,false",
+                "3,true",
+                "4,false",
+                "5,true"}
+        )
+        void canMoveLeftTest(int index, boolean expected) {
+            assertThat(line.canMoveLeft(new Position(index))).isEqualTo(expected);
+        }
+
+        @DisplayName("오른쪽으로 움질일 수 있는지 확인할 수 있다.")
+        @ParameterizedTest
+        @CsvSource({
+                "0,true",
+                "1,false",
+                "2,true",
+                "3,false",
+                "4,true",
+                "5,false"}
+        )
+        void canMoveRightTest(int index, boolean expected) {
+            assertThat(line.canMoveRight(new Position(index))).isEqualTo(expected);
+        }
+    }
 }
