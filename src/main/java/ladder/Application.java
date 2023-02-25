fc7e17fc3abe6d9e20f3f94c366309c56e63e96d
Jaeyoung22 <yt930310@gmail.com>
2023-02-25 23:37:00 +0900

    style: todo 삭제

--- src/main/java/ladder/Application.java
+++ src/main/java/ladder/Application.java
@@ -4,7 +4,6 @@ import ladder.controller.LadderController;
 import ladder.util.RandomBooleanGenerator;
 
 public class Application {
-    // TODO: 2023/02/25 결과 입력할 때 검증 순서 변경
     public static void main(String[] args) {
         new LadderController(new RandomBooleanGenerator()).execute();
     }
