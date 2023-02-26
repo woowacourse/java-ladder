0cf55c1509a0280ac7e7f037ede090ed7a1b7e48
jjongwa <troas96@naver.com>
2023-02-27 01:57:05 +0900

    test: 성공 테스트 케이스 추가

--- src/test/java/domain/ResultTest.java
+++ src/test/java/domain/ResultTest.java
@@ -6,7 +6,6 @@ import static view.InputView.RESULT_LENGTH_ERROR;
 import static domain.user.Users.NOT_CONTAIN_NAME_ERROR;
 
 import domain.game.Results;
-import domain.user.User;
 import domain.user.Users;
 import java.util.List;
 import org.junit.jupiter.api.*;
@@ -21,6 +20,14 @@ public class ResultTest {
                 .hasMessageContaining(RESULT_LENGTH_ERROR);
     }
 
+    @DisplayName("입력받은 결과의 수가 유저 수와 같으면 성공한다.")
+    @Test
+    void shouldSuccessResultEqualUserLength() {
+        Users users = new Users(List.of("dino", "mango","study","java"));
+        List<String> results = List.of("꽝", "1000", "꽝", "5000");
+        assertDoesNotThrow(() -> new Results(results, users.getPersonCount()));
+    }
+
     @DisplayName("입력받은 값과 같은 이름의 유저가 없으면 실패한다.")
     @Test
     void shouldFailInputIsNotUser() {
@@ -28,6 +35,14 @@ public class ResultTest {
         String input = "dinos";
         assertThatThrownBy(() -> users.checkNameInUsers(input))
                 .hasMessageContaining(NOT_CONTAIN_NAME_ERROR);
+    }
+
+    @DisplayName("입력받은 값과 같은 이름의 유저가 있으면 성공한다.")
+    @Test
+    void shouldSuccessInputIsNotUser() {
+        Users users = new Users(List.of("dino", "mango"));
+        String input = "dino";
+        assertDoesNotThrow(() -> users.checkNameInUsers(input));
     }
 
     @DisplayName("결과를 보고싶은 유저로 'all'을 입력받으면 성공한다.")
