0fafc261f3e0e1881bf113da4be6031325dc3d8a
Be-student <presentsong@naver.com>
2023-02-26 11:23:35 +0900

    feat: 아이템에서 일치하는 것만 찾아오는 기능 추가

--- src/main/java/ladder/domain/item/Result.java
+++ src/main/java/ladder/domain/item/Result.java
@@ -3,6 +3,7 @@ package ladder.domain.item;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import ladder.domain.player.Position;
 
 /**
  * Result 는 결과의 이름, 위치들을 가지고 있는 클래스입니다
@@ -29,5 +30,13 @@ public class Result {
         return resultItems.stream()
                 .map(Item::getName)
                 .collect(Collectors.toList());
+    }
+
+    public String findByPosition(Position position) {
+        return resultItems.stream()
+                .filter(item -> item.isSamePosition(position))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new)
+                .getName();
     }
 }
