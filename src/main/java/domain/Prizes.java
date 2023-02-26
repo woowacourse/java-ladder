bb0b1b3c3a8e4071f103de7b2bbe0a8edb923c8f
aiaiaiai1 <cvc100326@naver.com>
2023-02-26 12:29:22 +0900

    refactor: Prizes클래스 수정
    - 생성자 파라미터 Players 추가
    - 참여자수랑 일치하는지 검증 추가
    - 일급컬렉션 적용

--- src/main/java/domain/Prizes.java
+++ src/main/java/domain/Prizes.java
@@ -1,31 +1,38 @@
 package domain;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Prizes {
-    private final List<String> names;
+    private static final String INVALID_COUNT_MESSAGE = "실행결과 수는 참여자 수와 같아야합니다.";
+    private final List<Prize> prizes = new ArrayList<>();
 
-    public Prizes(List<String> names) {
-        validate(names);
-        this.names = names;
+    public Prizes(List<String> names, Players players) {
+        validate(names, players);
+        initiate(names);
     }
 
-    private void validate(List<String> names) {
+    private void initiate(List<String> names) {
         for (String name : names) {
-            new PrizeName(name);
+            prizes.add(new Prize(name));
+        }
+    }
+
+    private void validate(List<String> names, Players players) {
+        if (names.size() != players.getCount()) {
+            throw new IllegalArgumentException(INVALID_COUNT_MESSAGE);
         }
     }
 
     public String getName(int index) {
-        return names.get(index);
+        return prizes.get(index).getName();
     }
 
     public List<String> getNames() {
-        return names;
-    }
-
-    public boolean isSame(int count) {
-        return names.size() == count;
+        return prizes.stream()
+                .map(Prize::getName)
+                .collect(Collectors.toList());
     }
 
 }
