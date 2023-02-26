9db8d70b933697922da6faf60cd1b415a286a1e9
woo-chang <clllickme@naver.com>
2023-02-26 11:38:40 +0900

    refactor: 입력 예시 알려주기 위한 주석 작성

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -13,6 +13,10 @@ public class InputView {
         this.scanner = scanner;
     }
 
+    /**
+     * <example>
+     * pobi,honux,crong,jk
+     */
     public List<String> readPlayerNames() {
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
         final String input = scanner.nextLine();
@@ -24,6 +28,10 @@ public class InputView {
                 .collect(Collectors.toUnmodifiableList());
     }
 
+    /**
+     * <example>
+     * 5
+     */
     public int readHeight() {
         System.out.println("최대 사다리 높이는 몇 개인가요?");
         final String input = scanner.nextLine();
@@ -38,12 +46,20 @@ public class InputView {
         }
     }
 
+    /**
+     * <example>
+     * 꽝,5000,꽝,3000
+     */
     public List<String> readPrizeNames() {
         System.out.println("실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
         final String input = scanner.nextLine();
         return parseNames(input);
     }
 
+    /**
+     * <example>
+     * pobi
+     */
     public String readTarget() {
         System.out.println("결과를 보고 싶은 사람은?");
         return scanner.nextLine();
