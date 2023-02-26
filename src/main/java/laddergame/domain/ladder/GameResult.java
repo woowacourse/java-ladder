37cdd56c34bcf45b9894f2bc7e5158549978e199
yoondgu <doyou2ing@naver.com>
2023-02-27 00:32:43 +0900

    feat: 게임 결과 조회 기능에 참여자 이름 검증 로직 추가

--- src/main/java/laddergame/domain/ladder/GameResult.java
+++ src/main/java/laddergame/domain/ladder/GameResult.java
@@ -2,6 +2,7 @@ package laddergame.domain.ladder;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Optional;
 import laddergame.domain.ladder.destination.Item;
 import laddergame.domain.players.Name;
 
@@ -9,12 +10,13 @@ public class GameResult {
 
     private final Map<Name, Item> result;
 
-    public GameResult(Map<Name, Item> result) {
+    public GameResult(final Map<Name, Item> result) {
         this.result = result;
     }
 
-    public Item findByPlayerName(String name) {
-        return result.get(new Name(name));
+    public Item findByPlayerName(final String name) {
+        return Optional.ofNullable(result.get(new Name(name)))
+                .orElseThrow(() -> new IllegalArgumentException("참여자 정보가 존재하지 않습니다."));
     }
 
     public Map<Name, Item> result() {
