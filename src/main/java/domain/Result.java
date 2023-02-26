4689e6ca55346b1b4504c2906ed8182bc36dc577
parkmuhyeun <pjhg410@gmail.com>
2023-02-26 23:50:58 +0900

    refactor: Result equals, hashCode 재정의

--- src/main/java/domain/Result.java
+++ src/main/java/domain/Result.java
@@ -1,11 +1,26 @@
 package domain;
 
+import java.util.Objects;
+
 public class Result {
 
     private final String name;
 
     public Result(String name) {
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Result result = (Result) o;
+        return Objects.equals(name, result.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 
     public String getName() {
