9cd953659711e2d4cb256898fb09b7f938e0fc70
Cyma-s <ssol0319@naver.com>
2023-02-26 16:51:15 +0900

    test: 이름과 실행 결과의 개수가 같지 않은 예외 테스트 추가

---
+++ src/test/java/ladder/domain/LadderGameTest.java
@@ -0,0 +1,23 @@
+package ladder.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class LadderGameTest {
+    @Test
+    @DisplayName("이름과 실행 결과의 개수가 같지 않은 경우 예외를 던진다.")
+    void ladderGame_throwException_NotSameNameAndResultCount() {
+        // given
+        List<String> names = new ArrayList<>(List.of("vero", "poi", "glen"));
+        List<String> results = new ArrayList<>(List.of("꽝", "당첨"));
+
+        // expected
+        assertThatThrownBy(() -> new LadderGame(names, results, 4, new NonContinuousRandomLineStrategy()))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
