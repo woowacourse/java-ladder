0aeb40547ef4e46e9de01f63113e8f508e278551
yeonkkk <tjddus0302@gmail.com>
2023-02-27 03:34:27 +0900

    refactor: 검증 메서드를 InputView에서 Users로 이동

--- src/main/java/domain/Users.java
+++ src/main/java/domain/Users.java
@@ -1,12 +1,15 @@
 package domain;
 
 
-import static utils.ErrorMessage.NOT_FOUND_USER;
-
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static utils.ErrorMessage.*;
 
 public class Users {
+    private static final int MINIMUM_USER_NUMBER = 1;
+    private static final int MAXIMUM_USER_NUMBER = 20;
 
     private final List<User> users;
 
@@ -14,14 +17,14 @@ public class Users {
         this.users = users;
     }
 
-    public static Users from(List<String> userNames) {
-        List<User> users = new ArrayList<>();
-        for (int i = 0; i < userNames.size(); i++) {
-            users.add(User.of(
-                    Name.from(userNames.get(i)),
-                    Position.from(i)));
-        }
+    public static Users from(List<String> usernames) {
+        validateUsernameDuplication(usernames);
+        validateUsersNumber(usernames);
 
+        List<User> users = IntStream.range(0, usernames.size())
+                .mapToObj(i -> User.of(
+                Name.from(usernames.get(i)), Position.from(i)))
+                .collect(Collectors.toList());
         return new Users(users);
     }
 
@@ -42,6 +45,28 @@ public class Users {
         for (List<Integer> numbers : allNumbers) {
             user.movePosition(numbers);
         }
+    }
+
+    private static void validateUsersNumber(List<String> allUsernames) {
+        if (allUsernames.size() < MINIMUM_USER_NUMBER || allUsernames.size() > MAXIMUM_USER_NUMBER) {
+            throw new IllegalArgumentException(
+                    String.format(
+                            INVALID_USER_NUMBER.getMessage(),
+                            MINIMUM_USER_NUMBER, MAXIMUM_USER_NUMBER));
+        }
+    }
+
+    private static void validateUsernameDuplication(List<String> allUsernames) {
+        if (allUsernames.size() != checkDistinctSize(allUsernames)) {
+            throw new IllegalArgumentException(
+                    DUPLICATE_USERNAME.getMessage());
+        }
+    }
+
+    private static long checkDistinctSize(List<String> allUsernames) {
+        return allUsernames.stream()
+                .distinct()
+                .count();
     }
 
     public int size() {
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -8,21 +8,14 @@ import java.util.stream.Collectors;
 import static utils.ErrorMessage.*;
 
 public class InputView {
-    private static final int MINIMUM_USER_NUMBER = 1;
-    private static final int MAXIMUM_USER_NUMBER = 20;
+
     private static final String DELIMITER = ",";
 
     private final Scanner scanner = new Scanner(System.in);
 
     public List<String> inputUsernames() {
         String usernames = scanner.nextLine();
-        List<String> allUsernames = Arrays.asList(
-                usernames.split(DELIMITER));
-
-        validateUsernameDuplication(allUsernames);
-        validateUsernamesNumber(allUsernames);
-
-        return allUsernames;
+        return Arrays.asList(usernames.split(DELIMITER));
     }
 
     public int inputHeight() {
@@ -50,28 +43,6 @@ public class InputView {
         } catch (Exception e) {
             throw new IllegalArgumentException(INVALID_LADDER_HEIGHT_INPUT.getMessage());
         }
-    }
-
-    private void validateUsernamesNumber(List<String> allUsernames) {
-        if (allUsernames.size() < MINIMUM_USER_NUMBER || allUsernames.size() > MAXIMUM_USER_NUMBER) {
-            throw new IllegalArgumentException(
-                    String.format(
-                            INVALID_USER_NUMBER.getMessage(),
-                            MINIMUM_USER_NUMBER, MAXIMUM_USER_NUMBER));
-        }
-    }
-
-    private void validateUsernameDuplication(List<String> allUsernames) {
-        if (allUsernames.size() != checkDistinctSize(allUsernames)) {
-            throw new IllegalArgumentException(
-                    DUPLICATE_USERNAME.getMessage());
-        }
-    }
-
-    private long checkDistinctSize(List<String> allUsernames) {
-        return allUsernames.stream()
-                .distinct()
-                .count();
     }
 
 }
