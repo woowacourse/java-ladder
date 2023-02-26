60767a013cf033ea10771a4d7dd0c4043244c599
Go-Jaecheol <gojaech@naver.com>
2023-02-26 14:30:51 +0900

    feat: Prizes 객체 생성 후 저장

---
+++ src/main/java/domain/prize/Prizes.java
@@ -0,0 +1,11 @@
+package domain.prize;
+
+import java.util.List;
+
+public class Prizes {
+    private final List<Prize> prizes;
+
+    public Prizes(List<Prize> prizes) {
+        this.prizes = prizes;
+    }
+}
