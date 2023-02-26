d507a146e73aaffd38de8f7d0f1397b43aba4a32
hgo641 <hgo641@gmail.com>
2023-02-27 02:01:32 +0900

    refactor: 메시지 내용 수정

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -11,7 +11,7 @@ public class InputView {
     private static final String HEIGHT_INPUT_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
     private static final String HEIGHT_ERROR_MESSAGE = "사다리 높이는 자연수만 입력할 수 있습니다.";
     private static final String PRIZE_NAMES_INPUT_MESSAGE = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
-    private static final String RESULT_COMMAND_INPUT_MESSAGE = "결과를 보고 싶은 사람은?";
+    private static final String RESULT_COMMAND_INPUT_MESSAGE = "결과를 보고 싶은 사람은? (참여자 이름 또는 all을 입력하세요. end를 입력하면 프로그램을 종료합니다.)";
 
     public List<String> inputPlayerNames() {
         System.out.println(PLAYER_NAMES_INPUT_MESSAGE);
@@ -19,7 +19,7 @@ public class InputView {
         return splitInputSource(playerNames);
     }
 
-    private List<String> splitInputSource(String inputSource){
+    private List<String> splitInputSource(String inputSource) {
         return List.of(inputSource.split(INPUT_SOURCE_DELIMITER));
     }
 
