86e068fcc95cba29365d9be51bdf729d8ac927b3
song-wooseok <sws981202@gmail.com>
2023-02-27 08:18:22 +0900

    test: 사다리 한층을 타는 테스트

--- src/test/java/domain/LineTest.java
+++ src/test/java/domain/LineTest.java
@@ -2,9 +2,14 @@ package domain;
 
 import domain.ladder.Line;
 import domain.ladder.LineGenerator;
+import domain.player.Name;
+import domain.player.Player;
+import domain.player.Position;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import util.BooleanGenerator;
 import util.FixBooleanGenerator;
 
@@ -25,6 +30,27 @@ class LineTest {
 
             //then
             assertThat(line.getRightConnectionCondition()).containsExactly(true, false, false, true, false);
+        }
+    }
+
+    @Nested
+    @DisplayName("밟을 때")
+    class RideLineCase{
+
+        @ParameterizedTest
+        @DisplayName("플레이어를 받아, 해당 플레이어의 위치를 업데이트한다.")
+        @CsvSource(value = {"0:1", "1:0", "2:2", "3:4", "4:3"}, delimiter = ':')
+        void rideLineTest(final int startPosition, final int targetPosition){
+            //given
+            BooleanGenerator generator = new FixBooleanGenerator(true, true, false, true, true);
+            Player player = new Player(new Name("pobi"), new Position(startPosition));
+
+            //when
+            Line line = new LineGenerator(generator).build(5);
+            line.ride(player);
+
+            //then
+            assertThat(player.getPosition()).isEqualTo(targetPosition);
         }
     }
 }
