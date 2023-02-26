b5a7b322ea9eb50abd5ff7df817f4754653e6d59
song-wooseok <sws981202@gmail.com>
2023-02-27 08:31:28 +0900

    test: 사다리를 타는 기능 테스트

--- src/test/java/domain/LadderTest.java
+++ src/test/java/domain/LadderTest.java
@@ -2,10 +2,16 @@ package domain;
 
 import domain.ladder.Ladder;
 import domain.ladder.LadderGenerator;
+import domain.player.Name;
+import domain.player.Player;
+import domain.player.Position;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import util.FixBooleanGenerator;
 import util.RandomBooleanGenerator;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -46,6 +52,33 @@ class LadderTest {
             private int getLadderArea() {
                 return ladder.getLineHeight() * ladder.getWidth();
             }
+        }
+    }
+
+    @Nested
+    @DisplayName("탈 때 ")
+    class rideLadderCase {
+
+        private Ladder ladder;
+
+        @BeforeEach
+        void setUp() {
+            ladder = new LadderGenerator(new FixBooleanGenerator(true, true, false, true, true, false)).build(2, 3);
+        }
+
+
+        @ParameterizedTest
+        @DisplayName("플레이어를 받아 사다리를 다 내려온 뒤의 플레이어 위치를 업데이트한다.")
+        @CsvSource(value = {"0:2", "1:0", "2:1"}, delimiter = ':')
+        void rideLadderTest(final int startPosition, final int targetPosition){
+            //given
+            Player player = new Player(new Name("pobi"), new Position(startPosition));
+
+            //when
+            ladder.ride(player);
+
+            //then
+            assertThat(player.getPosition()).isEqualTo(targetPosition);
         }
     }
 }
