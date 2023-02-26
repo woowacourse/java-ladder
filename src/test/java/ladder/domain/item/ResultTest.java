dc11f1fe4e5a3c50ab38e1706903479ab0ed05bc
Be-student <presentsong@naver.com>
2023-02-26 11:26:20 +0900

    test: 아이템을 포지션 기준으로 찾는 검증 추가

--- src/test/java/ladder/domain/item/ResultTest.java
+++ src/test/java/ladder/domain/item/ResultTest.java
@@ -2,8 +2,10 @@ package ladder.domain.item;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import java.util.List;
+import ladder.domain.player.Position;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 import org.junit.jupiter.api.Test;
@@ -23,5 +25,16 @@ class ResultTest {
         assertThatThrownBy(() -> new Result(List.of("꽝", "5000", "꽝", "3000"), 3))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("플레이어의 수와 결과의 수가 다릅니다. 플레이어 : 3, 결과 : 4");
+    }
+
+    @Test
+    void 포지션을_바탕으로_아이템을_가져오는_기능_검증() {
+        Result result = new Result(List.of("꽝", "5000", "꽝", "3000"), 4);
+        assertAll(
+                () -> assertThat(result.findByPosition(Position.valueOf(0))).isEqualTo("꽝"),
+                () -> assertThat(result.findByPosition(Position.valueOf(1))).isEqualTo("5000"),
+                () -> assertThat(result.findByPosition(Position.valueOf(2))).isEqualTo("꽝"),
+                () -> assertThat(result.findByPosition(Position.valueOf(3))).isEqualTo("3000")
+        );
     }
 }
