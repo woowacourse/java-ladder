cc7d54f3e16b3aa6f296a08eb75bcd0f755d2ed2
Youngho <cyh6099@gmail.com>
2023-02-26 22:59:42 +0900

    refactor : 예외메시지 상수화

--- src/main/java/domain/WinningResult.java
+++ src/main/java/domain/WinningResult.java
@@ -1,6 +1,9 @@
 package domain;
 
 public class WinningResult {
+
+    private static final String INVALID_RESULT_LENGTH_MESSAGE = "실행결과는 1~5자리까지 입력 가능합니다.";
+    private static final String INVALID_RESULT_BLANK_MESSAGE = "실행결과는 공백으로만 이루어지면 안됩니다.";
     private final String winningResult;
 
     public WinningResult(final String winningResult) {
@@ -11,13 +14,13 @@ public class WinningResult {
 
     private void validate(final String winningResult) {
         if (winningResult.length() < 1 || winningResult.length() > 5) {
-            throw new IllegalArgumentException("실행결과는 1~5자리까지 입력 가능합니다.");
+            throw new IllegalArgumentException(INVALID_RESULT_LENGTH_MESSAGE);
         }
     }
 
     private void validateIsBlank(final String winningResult) {
         if (winningResult.isBlank()) {
-            throw new IllegalArgumentException("실행결과는 공백으로만 이루어지면 안됩니다.");
+            throw new IllegalArgumentException(INVALID_RESULT_BLANK_MESSAGE);
         }
     }
 
