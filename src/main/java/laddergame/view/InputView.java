ad2ba08b6e6a36ec54a6053c0f9e85b0e5c49fc4
yoondgu <doyou2ing@naver.com>
2023-02-26 19:17:52 +0900

    feat: 참여자의 이름이 all, q인 경우 검증 로직 추가

--- src/main/java/laddergame/view/InputView.java
+++ src/main/java/laddergame/view/InputView.java
@@ -1,13 +1,30 @@
 package laddergame.view;
 
 import java.util.List;
+import laddergame.util.ExceptionMessageFormatter;
 import laddergame.view.util.ConsoleReader;
 
 public class InputView {
 
+    private static final List<String> RESERVED_WORDS = List.of("all", "q");
+
     public static List<String> askPlayerNames() {
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
-        return ConsoleReader.readLineByDelimiter(",");
+        List<String> playerNames = ConsoleReader.readLineByDelimiter(",");
+        playerNames.forEach(InputView::validateReserved);
+        return playerNames;
+    }
+
+    private static void validateReserved(String name) {
+        if (isReservedWord(name)) {
+            String message = "참여자의 이름은 예약어와 일치할 수 없습니다.";
+            throw new IllegalArgumentException(ExceptionMessageFormatter.format(message, name));
+        }
+    }
+
+    private static boolean isReservedWord(String name) {
+        return RESERVED_WORDS.stream()
+                .anyMatch(invalidName -> invalidName.equalsIgnoreCase(name));
     }
 
     public static List<String> askDestinationItems() {
