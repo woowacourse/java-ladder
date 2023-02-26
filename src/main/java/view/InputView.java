99ab463e7775b7cd31822263234d914fb255cbf2
Go-Jaecheol <gojaech@naver.com>
2023-02-26 14:18:19 +0900

    refactor: runResult에서 prize로 네이밍 변경

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class InputView {
     private static final String INPUT_USER_NAME = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     private static final String INPUT_LADDER_HEIGHT = "\n최대 사다리 높이는 몇 개인가요?";
-    private static final String INPUT_RUN_RESULT = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
+    private static final String INPUT_PRIZE_NAME = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
 
     static Scanner sc = new Scanner(System.in);
 
@@ -24,7 +24,7 @@ public class InputView {
         return sc.next();
     }
 
-    public void inputRunResult() {
-        System.out.println(INPUT_RUN_RESULT);
+    public void inputPrizeName() {
+        System.out.println(INPUT_PRIZE_NAME);
     }
 }
