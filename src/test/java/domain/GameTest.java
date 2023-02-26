75bd4e1a96659e48a51a6f56e5888a4e76641735
jjongwa <troas96@naver.com>
2023-02-27 02:26:03 +0900

    test: 성공 테스트 케이스 추가

--- src/test/java/domain/GameTest.java
+++ src/test/java/domain/GameTest.java
@@ -3,17 +3,27 @@ package domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import domain.game.Game;
+import domain.game.Results;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class GameTest {
-    @DisplayName("라인이 존재할 때 두 유저의 위치가 바뀌어야 한다.")
+    @DisplayName("라인이 존재할 때 두 유저의 위치가 바뀌지 않으면 실패한다.")
     @Test
     void shouldFailUserMaintain() {
         List<Boolean> points = List.of(false, true, false, false);
         ArrayList<String> userNames = new ArrayList<>(List.of("dino", "mango", "study"));
         assertThat(Game.compareLineAndUsers(userNames, points)).isNotEqualTo(List.of("dino", "mango", "study"));
+    }
+
+    @DisplayName("라인이 존재할 때 두 유저의 위치가 바뀌면 성공한다.")
+    @Test
+    void shouldSuccessUserSwap() {
+        List<Boolean> points = List.of(false, true, false, false);
+        ArrayList<String> userNames = new ArrayList<>(List.of("dino", "mango", "study"));
+        assertThat(Game.compareLineAndUsers(userNames, points)).isEqualTo(List.of("mango", "dino", "study"));
     }
 }
