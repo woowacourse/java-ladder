b44d896d68cbc02ff3c0ccb4c02f019b4452a833
yoondgu <doyou2ing@naver.com>
2023-02-26 21:48:47 +0900

    test: 사다리 게임 예외 테스트 추가

--- src/test/java/laddergame/domain/LadderGameTest.java
+++ src/test/java/laddergame/domain/LadderGameTest.java
@@ -15,7 +15,21 @@ class LadderGameTest {
     @DisplayName("사다리가 생성되지 않은 게임의 결과를 요청하면 예외를 던진다.")
     void should_ThrowException_When_ComputeResultWithoutLadder() {
         LadderGame ladderGame = new LadderGame(List.of("name1", "name2"));
+
+        assertThatThrownBy(ladderGame::ladder)
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("아직 사다리가 생성되지 않은 게임입니다.");
         assertThatThrownBy(ladderGame::computeResult)
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("아직 사다리가 생성되지 않은 게임입니다.");
+    }
+
+    @Test
+    @DisplayName("사다리가 생성되지 않은 게임의 사다리 결과를 조회하면 예외를 던진다.")
+    void should_ThrowException_When_GetLadderWithoutLadder() {
+        LadderGame ladderGame = new LadderGame(List.of("name1", "name2"));
+
+        assertThatThrownBy(ladderGame::ladder)
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessageContaining("아직 사다리가 생성되지 않은 게임입니다.");
     }
