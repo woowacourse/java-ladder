022d1a9afffb2ac7cc99570746c434b1c2a19834
song-wooseok <sws981202@gmail.com>
2023-02-27 02:33:14 +0900

    feat: 위치 값 객체 포장

---
+++ src/main/java/domain/Position.java
@@ -0,0 +1,26 @@
+package domain;
+
+public class Position {
+
+    private static final int MIN_POSITION = 0;
+
+    private static final String UNDER_MIN_POSITION_MESSAGE = "위치는 " + MIN_POSITION + " 이하 일 수 없습니다.";
+
+    private int position;
+
+    public Position(final int position) {
+        validatePosition(position);
+        this.position = position;
+    }
+
+    public void move(int direction) {
+        validatePosition(position+direction);
+        this.position += direction;
+    }
+
+    private void validatePosition(final int position) {
+        if (position < MIN_POSITION) {
+            throw new IllegalArgumentException(UNDER_MIN_POSITION_MESSAGE);
+        }
+    }
+}
