eb8d14e08a9e95264b3f0fe9c6e2b2caae86e28f
DESKTOP-FNVN0HH\USER <beatus96@naver.com>
2023-02-27 06:01:27 +0900

    refactor: Reward 메서드 분리 및 예외 메시지 상수화

--- src/main/java/ladder/domain/Reward.java
+++ src/main/java/ladder/domain/Reward.java
@@ -4,12 +4,14 @@ import static ladder.domain.PlayerName.NAME_MAXIMUM_LENGTH;
 
 public class Reward {
 
-    public static final String ERROR_LENGTH_OF_NAME = String.format(
+    public static final String ERROR_OVER_LENGTH_REWARD = String.format(
             "보상은 %d자 이하여야 합니다.", NAME_MAXIMUM_LENGTH);
+    public static final String ERROR_BLANK_REWARD = "보상을 입력하지 않았습니다.";
     private final String reward;
 
     public Reward(String reward) {
-        validateNameLength(reward);
+        validateRewardLength(reward);
+        validateBlankReward(reward);
         this.reward = reward;
     }
 
@@ -17,14 +19,18 @@ public class Reward {
         return reward;
     }
 
-    private void validateNameLength(String name) {
-        if (name.length() > NAME_MAXIMUM_LENGTH) {
-            throw new IllegalArgumentException(ERROR_LENGTH_OF_NAME);
+    private void validateRewardLength(String input) {
+        if (input.length() > NAME_MAXIMUM_LENGTH) {
+            throw new IllegalArgumentException(ERROR_OVER_LENGTH_REWARD);
+        }
+    }
+
+    private void validateBlankReward(String input) {
+
+        if (input.isBlank()) {
+            throw new IllegalArgumentException(ERROR_BLANK_REWARD);
         }
 
-        if (name.isBlank()) {
-            throw new IllegalArgumentException("보상을 입력하지 않았습니다.");
-        }
     }
 
 
