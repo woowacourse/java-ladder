47544e1d51b05b0fec0465a470dd0ceb0aecde95
JunDonghyuk <ehdgur4814@naver.com>
2023-02-27 16:31:55 +0900

    test: Players 생성 테스트

--- src/test/java/ladder/domain/PlayersTest.java
+++ src/test/java/ladder/domain/PlayersTest.java
@@ -1,4 +1,28 @@
+package ladder.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class PlayersTest {
-  
+
+    @Test
+    @DisplayName("Players에 플레이어 이름이 정확히 들어갔는지 테스트")
+    void containsTest() {
+        List<Player> rawPlayers = List.of(
+                new Player("pobi", 0),
+                new Player("crong", 1),
+                new Player("hardy", 2)
+        );
+
+        Players players = new Players(rawPlayers);
+        assertTrue(players.contains("pobi"));
+        assertTrue(players.contains("crong"));
+        assertTrue(players.contains("hardy"));
+
+        assertFalse(players.contains("hero"));
+    }
 }
