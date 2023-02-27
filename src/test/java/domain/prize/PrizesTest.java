de7a10c19b0212c061d40f74b5c2d5716e0d094b
Go-Jaecheol <gojaech@naver.com>
2023-02-27 15:57:59 +0900

    test: 입력받은 Prize 수와 User 수가 다르면 Prizes 생성에 실패하는 테스트 추가

--- src/test/java/domain/prize/PrizesTest.java
+++ src/test/java/domain/prize/PrizesTest.java
@@ -1,5 +1,6 @@
 package domain.prize;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
@@ -11,7 +12,7 @@ import org.junit.jupiter.api.Test;
 class PrizesTest {
     @Nested
     @DisplayName("Prizes 생성 테스트")
-    class MakeUserTest {
+    class MakePrizesTest {
         @DisplayName("입력값을 통해 올바르게 당첨 항목 리스트가 생성되는지 확인한다.")
         @Test
         void shouldSuccessMakePrizes() {
@@ -19,7 +20,19 @@ class PrizesTest {
             for (String prizeName : List.of("꽝", "3000", "0", "mango")) {
                 prizes.add(new Prize(prizeName));
             }
-            assertDoesNotThrow(() -> new Prizes(prizes));
+            assertDoesNotThrow(() -> new Prizes(prizes, 4));
+        }
+
+        @DisplayName("입력받은 Prize 수와 User 수가 다르면 Prizes 생성에 실패한다.")
+        @Test
+        void shouldFailMakePrizesIncorrectSize() {
+            List<Prize> prizes = new ArrayList<>();
+            for (String prizeName : List.of("꽝", "3000", "0", "mango")) {
+                prizes.add(new Prize(prizeName));
+            }
+            assertThatThrownBy(() -> new Prizes(prizes, 5))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(Prizes.PRIZE_SIZE_ERROR_MESSAGE);
         }
     }
 }
