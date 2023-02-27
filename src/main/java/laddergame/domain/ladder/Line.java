1aa7722c3621419a529bb63c85ed8fc78b449105
chanminmaxscaler <jcm04@naver.com>
2023-02-27 17:16:16 +0900

    refactor: Line 클래스에서 rungCount를 활용하는 로직을 수정한다
    rungCount은 participantCount - ADJUSTMENT_VALUE 이다.
    이러한 이유는 README 사다리 관련 참고사항에 기록해두었다.

--- src/main/java/laddergame/domain/ladder/Line.java
+++ src/main/java/laddergame/domain/ladder/Line.java
@@ -8,28 +8,25 @@ import java.util.List;
 public class Line {
 
     private static final boolean DOES_NOT_EXIST = false;
-    private static final int MINIMUM_INDEX = 0;
+    private static final int ADJUSTMENT_VALUE = 1;
     private static final int ORDER_UNIT = 1;
-    private static final int DEFAULT_RUNG_COUNT = 1;
+    private static final int MINIMUM_INDEX = 0;
 
     private final List<Rung> rungs;
     private final BooleanGenerator randomBooleanGenerator;
 
-    private Line(final int rungCount, final BooleanGenerator randomBooleanGenerator) {
+    private Line(final int participantCount, final BooleanGenerator randomBooleanGenerator) {
         this.randomBooleanGenerator = randomBooleanGenerator;
+        final int rungCount = makeRungCount(participantCount);
         rungs = makeRungs(rungCount);
     }
 
-    public static Line create(final int rungCount, final BooleanGenerator rungBooleanGenerator) {
-        return new Line(rungCount, rungBooleanGenerator);
+    public static Line create(final int participantCount, final BooleanGenerator rungBooleanGenerator) {
+        return new Line(participantCount, rungBooleanGenerator);
     }
 
-    public boolean hasRung(final int index) {
-        if (index < MINIMUM_INDEX || index >= rungs.size()) {
-            return DOES_NOT_EXIST;
-        }
-        Rung rung = rungs.get(index);
-        return rung.exists();
+    private static int makeRungCount(final int participantCount) {
+        return participantCount - ADJUSTMENT_VALUE;
     }
 
     private List<Rung> makeRungs(final int rungCount) {
@@ -37,7 +34,7 @@ public class Line {
         Rung firstRung = Rung.create(randomBooleanGenerator.generate());
         rungs.add(firstRung);
 
-        for (int order = 1; order < rungCount - DEFAULT_RUNG_COUNT; order++) {
+        for (int order = 1; order < rungCount; order++) {
             Rung previousRung = rungs.get(order - ORDER_UNIT);
             Rung newRung = Rung.create(generateWith(previousRung));
             rungs.add(newRung);
@@ -50,6 +47,14 @@ public class Line {
             return DOES_NOT_EXIST;
         }
         return randomBooleanGenerator.generate();
+    }
+
+    public boolean hasRung(final int index) {
+        if (index < MINIMUM_INDEX || index >= rungs.size()) {
+            return DOES_NOT_EXIST;
+        }
+        Rung rung = rungs.get(index);
+        return rung.exists();
     }
 
     public List<Rung> getRungs() {
