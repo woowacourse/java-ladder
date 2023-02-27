b7e29b00a59d60874bdd1cde84c87e2ddafbd7cb
Go-Jaecheol <gojaech@naver.com>
2023-02-27 09:47:55 +0900

    feat: 결과 보고 싶은 사람 입력 메세지 출력

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -7,6 +7,7 @@ public class InputView {
     private static final String INPUT_USER_NAME = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     private static final String INPUT_LADDER_HEIGHT = "\n최대 사다리 높이는 몇 개인가요?";
     private static final String INPUT_PRIZE_NAME = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
+    private static final String INPUT_SEARCH_NAME = "결과를 보고 싶은 사람은?";
 
     static Scanner sc = new Scanner(System.in);
 
@@ -27,5 +28,10 @@ public class InputView {
     public List<String> inputPrizeName() {
         System.out.println(INPUT_PRIZE_NAME);
         return splitNameInput(sc.next());
+    }
+
+    public String inputSearchName() {
+        System.out.println(INPUT_SEARCH_NAME);
+        return "";
     }
 }
