e796d5e50db3b2dfae96ff5c94f9ccac8d855ac8
cpot5620 <=cpot5620@naver.com>
2023-02-27 17:08:06 +0900

    test(Ladder): 사다리 게임 실행 로직 테스트 구현

--- src/test/java/ladder/domain/LadderTest.java
+++ src/test/java/ladder/domain/LadderTest.java
@@ -1,4 +1,63 @@
-import static org.junit.jupiter.api.Assertions.*;
+package ladder.domain;
+
+import ladder.util.TrueGenerator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class LadderTest {
-  
+
+    private Names names;
+    private Bets bets;
+
+    @BeforeEach
+    void setup() {
+        names = new Names(
+                Arrays.asList("AAAA", "BBBB", "CCCC", "DDDD", "EEEE")
+        );
+
+        bets = new Bets(
+                Arrays.asList("A", "B", "C", "D", "E"), names.size()
+        );
+    }
+
+    @ParameterizedTest(name = "주어진 사다리(ladder)를 기반으로 결과의 순서를 바꾼다.")
+    @ValueSource(ints = {1, 3, 5, 7, 9, 11, 13})
+    void createResultSuccessTest1(int height) {
+        //given
+        Ladder ladder = new Ladder(names.size(), new Height(height), new TrueGenerator());
+
+        //when
+        Result result = ladder.perform(names, bets);
+
+        //then
+        List<Bet> actualBets = List.copyOf(result.getResult().values());
+        assertThat(actualBets)
+                .containsExactly(
+                        new Bet("B"), new Bet("A"), new Bet("D"), new Bet("C"), new Bet("E")
+                );
+    }
+
+    @ParameterizedTest(name = "주어진 사다리(ladder)를 기반으로 결과의 순서를 바꾼다.")
+    @ValueSource(ints = {2, 4, 6, 8, 10, 12, 14})
+    void createResultSuccessTest2(int height) {
+        //given
+        Ladder ladder = new Ladder(names.size(), new Height(height), new TrueGenerator());
+
+        //when
+        Result result = ladder.perform(names, bets);
+
+        //then
+        List<Bet> actualBets = List.copyOf(result.getResult().values());
+        assertThat(actualBets)
+                .containsExactly(
+                        new Bet("A"), new Bet("B"), new Bet("C"), new Bet("D"), new Bet("E")
+                );
+    }
+
 }
