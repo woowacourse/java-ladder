0bb851e01196c9be06f9ab5d13092c53e6c77f37
song-wooseok <sws981202@gmail.com>
2023-02-27 09:44:31 +0900

    test: 상품 목록 조회 기능 테스트

--- src/test/java/domain/result/PrizesTest.java
+++ src/test/java/domain/result/PrizesTest.java
@@ -1,16 +1,27 @@
 package domain.result;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
 import java.util.List;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 
 @DisplayName("상품들은")
 class PrizesTest {
+
+    private List<String> prizeNames;
+
+    @BeforeEach
+    void setUp(){
+        prizeNames = new ArrayList<>(List.of("꽝", "5000", "1500"));
+    }
 
     @Nested
     @DisplayName("생성될 때")
@@ -19,7 +30,27 @@ class PrizesTest {
         @Test
         @DisplayName("상품의 이름들을 받아 생성된다.")
         void generatePrizesTest(){
-            assertDoesNotThrow(()-> new Prizes(List.of("꽝", "5000", "1500")));
+            assertDoesNotThrow(()-> new Prizes(prizeNames));
+        }
+    }
+
+    @Nested
+    @DisplayName("조회할 때")
+    class QueryTest{
+
+        @ParameterizedTest
+        @DisplayName("번호를 통해서 조회할 수 있다.")
+        @CsvSource(value = {"0:꽝", "1:5000", "2:1500"}, delimiter = ':')
+        void queryByIndex(final int index, final String prizeName){
+            //given
+            Prizes prizes = new Prizes(prizeNames);
+
+            //when
+            Prize prize = prizes.query(index);
+
+            //then
+            assertThat(prize.equals(new Prize(prizeName)))
+                    .isTrue();
         }
     }
 
