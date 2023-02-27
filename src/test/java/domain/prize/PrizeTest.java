90fa20d84c63881e4c589efcf689ce19cf0891e5
Go-Jaecheol <gojaech@naver.com>
2023-02-27 16:14:44 +0900

    refactor: PrizeTest 테스트 클래스명 변경

--- src/test/java/domain/prize/PrizeTest.java
+++ src/test/java/domain/prize/PrizeTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 class PrizeTest {
     @Nested
     @DisplayName("당첨 항목 생성 테스트")
-    class MakeUserTest {
+    class MakePrizeTest {
         @DisplayName("입력값을 통해 올바르게 당첨 항목이 생성되는지 확인한다.")
         @ParameterizedTest
         @ValueSource(strings = {"꽝", "5000", "꽝", "3000", "mango"})
