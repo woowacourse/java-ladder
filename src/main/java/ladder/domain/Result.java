9cf0057d27cb2daf4c3f50e6a6b50e1529ed5849
JunDonghyuk <ehdgur4814@naver.com>
2023-02-27 15:21:47 +0900

    refactor: List 2개로 게임 결과를 보관하던 것을 LinkedHashMap으로 구현

--- src/main/java/ladder/domain/Result.java
+++ src/main/java/ladder/domain/Result.java
@@ -1,29 +1,30 @@
 package ladder.domain;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
 
-public class ResultStorage {
-    private final Map<String, String> result;
+public class Result {
 
-    public ResultStorage() {
-        result = new LinkedHashMap<>();
+    private final String NOT_FOUND_VALUE_BY_KEY = "[ERROR] 해당 key는 존재하지 않습니다.";
+
+    private final HashMap<String, String> result;
+
+    public Result() {
+        this.result = new LinkedHashMap<>();
     }
 
-    public void add(final String result) {
-        resultStorage.add(result);
+    public void add(final String key, final String value) {
+        result.put(key, value);
     }
 
-    public String get(final int index) {
-        if (resultStorage.size() <= index) {
-            throw new IllegalArgumentException();
+    public String get(final String key) {
+        if(!result.containsKey(key)) {
+            throw new IllegalArgumentException(NOT_FOUND_VALUE_BY_KEY);
         }
-        return resultStorage.get(index);
+        return result.get(key);
     }
 
-    public List<String> getAll() {
-        return List.copyOf(resultStorage);
+    public HashMap<String, String> result() {
+        return (LinkedHashMap<String, String>)result.clone();
     }
 }
