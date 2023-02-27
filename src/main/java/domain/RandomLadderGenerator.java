58226deb555cf79dec1fe6edeb577cf07dbf70ea
HubCreator <chocomilk8604@gmail.com>
2023-02-27 10:53:53 +0900

    refactor(RandomLadderGenerator): 메서드 및 변수 명 수정

--- src/main/java/domain/RandomLadderGenerator.java
+++ src/main/java/domain/RandomLadderGenerator.java
@@ -12,23 +12,23 @@ public class RandomLadderGenerator implements LadderGenerator {
     public Lines generate(Players players, int height) {
         List<Line> result = new ArrayList<>(height);
         for (int i = 0; i < height; i++) {
-            result.add(new Line(getHorizontalLineMovements(players.getCount())));
+            result.add(new Line(getRandomMovements(players.getCount())));
         }
         return new Lines(result);
     }
 
-    private List<Boolean> getHorizontalLineMovements(int width) {
+    private List<Boolean> getRandomMovements(int width) {
         List<Boolean> movements = new ArrayList<>(width);
         movements.add(random.nextBoolean());
         int inBetweenCount = width - 1;
         for (int index = 1; index < inBetweenCount; index++) {
             int previousElement = index - 1;
-            updateResult(movements, movements.get(previousElement));
+            updateMovements(movements, movements.get(previousElement));
         }
         return movements;
     }
 
-    private void updateResult(List<Boolean> result, Boolean previousElement) {
+    private void updateMovements(List<Boolean> result, Boolean previousElement) {
         if (previousElement) {
             result.add(Boolean.FALSE);
             return;
