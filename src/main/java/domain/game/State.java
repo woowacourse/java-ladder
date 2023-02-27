8db135b71dbf4db65c26934abe2c19381b478893
song-wooseok <sws981202@gmail.com>
2023-02-27 10:34:16 +0900

    feat: 게임 플레이 여부를 나타내는 객체 구현

---
+++ src/main/java/domain/game/State.java
@@ -0,0 +1,18 @@
+package domain.game;
+
+public class State {
+
+    private boolean isPlayed;
+
+    public State(){
+        this.isPlayed = false;
+    }
+
+    public void flipState(){
+        this.isPlayed = !isPlayed;
+    }
+
+    public boolean isPlayed(){
+        return isPlayed;
+    }
+}
