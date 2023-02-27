2a8e6986c0eaf4a8c8afbc5c9410cc8671ed0c9a
song-wooseok <sws981202@gmail.com>
2023-02-27 15:50:18 +0900

    feat: 예약어 enum 정의

---
+++ src/main/java/view/constant/Command.java
@@ -0,0 +1,27 @@
+package view.constant;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public enum Command {
+    ALL("all"),
+    END("end");
+
+    String command;
+
+    Command(final String command){
+        this.command = command;
+    }
+
+    public String getCommand(){
+        return this.command;
+    }
+
+    public static boolean isCommand(final String doubtWord){
+        return Arrays.stream(Command
+                .values())
+                .filter(command -> command.getCommand().equals(doubtWord))
+                .collect(Collectors.toList())
+                .isEmpty();
+    }
+}
