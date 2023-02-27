d9e0d44d4f5c549d0646dd5f47de160ddc1c3874
JunDonghyuk <ehdgur4814@naver.com>
2023-02-27 15:32:16 +0900

    refactor: strip을 사용하여 공백제거

--- src/main/java/ladder/domain/PlayerNames.java
+++ src/main/java/ladder/domain/PlayerNames.java
@@ -7,7 +7,6 @@ public class PlayerNames {
 
     private static final String LENGTH_EXCEPTION_MESSAGE = "[ERROR] 참여자의 이름은 1자 이상 5자 이하여야 합니다.";
     private static final String BLANK_EXCEPTION_MESSAGE = "[ERROR] 참여자의 이름은 빈 값일 수 없습니다.";
-
     private static final int MIN_NAME_LENGTH = 1;
     private static final int MAX_NAME_LENGTH = 5;
 
@@ -31,6 +30,7 @@ public class PlayerNames {
 
     private boolean isBlank(final List<String> names) {
         long count = names.stream()
+                .map(String::strip)
                 .filter(String::isBlank)
                 .count();
         return count != 0;
