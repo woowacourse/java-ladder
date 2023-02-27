ff4ee44b2a10f6cfa520db012f7a2131f5d68317
chaewon121 <cw121@naver.com>
2023-02-27 15:54:15 +0900

    refactor: application -> controller에 포함

--- src/main/java/ladder/Application.java
+++ src/main/java/ladder/Application.java
@@ -1,23 +1,10 @@
 package ladder;
 
 import ladder.controller.LadderController;
-import ladder.domain.ladder.Results;
-import ladder.domain.people.Names;
-import ladder.view.InputView;
-
-import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        Names names = InputView.repeat(() -> new Names(InputView.inputPeopleNames()));
-        List<String> namesList = names.getNames();
-
-        Results results = InputView.repeat(() -> new Results(InputView.inputResults(), namesList.size()));
-        List<String> resultsList = results.getResults();
-
-        int ladderHeight = InputView.repeat(InputView::inputLadderHeight);
-
         LadderController ladderController = new LadderController();
-        ladderController.run(namesList, resultsList, ladderHeight);
+        ladderController.run();
     }
 }
--- src/main/java/ladder/controller/LadderController.java
+++ src/main/java/ladder/controller/LadderController.java
@@ -3,6 +3,8 @@ package ladder.controller;
 import ladder.domain.GameResults;
 import ladder.domain.ladder.Ladder;
 import ladder.domain.ladder.RandomBarGenerator;
+import ladder.domain.ladder.Results;
+import ladder.domain.people.Names;
 import ladder.view.InputView;
 import ladder.view.OutputView;
 
@@ -11,7 +13,14 @@ import java.util.List;
 
 public class LadderController {
 
-    public void run(List<String> namesList, List<String> resultsList, int ladderHeight) {
+    public void run() {
+        Names names = InputView.repeat(() -> new Names(InputView.inputPeopleNames()));
+        List<String> namesList = names.getNames();
+
+        Results results = InputView.repeat(() -> new Results(InputView.inputResults(), namesList.size()));
+        List<String> resultsList = results.getResults();
+
+        int ladderHeight = InputView.repeat(InputView::inputLadderHeight);
         Ladder ladder = new Ladder(new RandomBarGenerator(), ladderHeight, namesList.size());
         GameResults gameResults = new GameResults(namesList, resultsList, ladder);
 
@@ -28,7 +37,7 @@ public class LadderController {
 
     private void viewGameResults(HashMap<String, String> gameResults, List<String> namesList) {
         String name = InputView.repeat(() -> InputView.inputWantGameResults(namesList));
-        if (name.equals("all")) {
+        if ("all" .equals(name)) {
             printGameResultsAll(gameResults);
             return;
         }
