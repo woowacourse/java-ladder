9558b339fe777abb001d4e286a6d719fbdb261ea
song-wooseok <sws981202@gmail.com>
2023-02-27 15:50:34 +0900

    feat: 사다리 게임 게임 컨트롤러 구현

--- src/main/java/controller/LadderGameController.java
+++ src/main/java/controller/LadderGameController.java
@@ -1,35 +1,95 @@
 package controller;
 
 import domain.ladder.Ladder;
+import domain.ladder.LadderGame;
 import domain.ladder.LadderGenerator;
 import domain.player.Names;
+import domain.player.Players;
+import domain.result.Prizes;
+import domain.result.Result;
 import util.RandomBooleanGenerator;
 import view.InputView;
 import view.OutputView;
+import view.constant.Command;
 
 public class LadderGameController {
 
-    public void run() {
-        Names names = getNames();
-        int height = getHeight();
+    private final Names names;
+    private final Prizes prizes;
+    private final Ladder ladder;
 
-        Ladder ladder = buildLadder(names, height);
-
-        printResult(names, ladder);
+    public LadderGameController() {
+        this.names = getNames();
+        this.prizes = getPrizes();
+        this.ladder = buildLadder();
     }
 
-    private Ladder buildLadder(final Names names, final int height) {
-        return new LadderGenerator(new RandomBooleanGenerator()).build(height, names.count());
+    public void start() {
+        printLadderResult();
+        final Result result = playGame();
+        printGameResult(result);
     }
 
-    private static void printResult(final Names names, final Ladder ladder) {
-        OutputView.printResult(names, ladder);
+    private void printGameResult(Result result) {
+        try {
+            printGameResultUntilEnd(result);
+        } catch (IllegalArgumentException e) {
+            OutputView.printErrorMessage(e.getMessage());
+            printGameResult(result);
+        }
+
+    }
+
+    private Result playGame() {
+        Players players = new Players(names);
+        LadderGame game = new LadderGame(ladder, players, prizes);
+        game.play();
+
+        return game.getResult();
+    }
+
+    private Ladder buildLadder() {
+        try {
+            int height = getHeight();
+            return new LadderGenerator(new RandomBooleanGenerator()).build(height, names.count());
+        } catch (IllegalArgumentException e) {
+            OutputView.printErrorMessage(e.getMessage());
+            return buildLadder();
+        }
+    }
+
+    private void printLadderResult() {
+        OutputView.printResult(names, ladder, prizes);
+    }
+
+    private void printGameResultUntilEnd(final Result result) {
+        String query = getResultQuery();
+        while (!query.equals(Command.END.getCommand())) {
+            printGameResultByQuery(result, query);
+            query = getResultQuery();
+        }
+    }
+
+    private void printGameResultByQuery(final Result result, final String query) {
+        if (query.equals(Command.ALL.getCommand())) {
+            OutputView.printPlayerResult(result.toString());
+            return;
+        }
+        OutputView.printPlayerResult(result.queryByPlayer(query));
+    }
+
+    private String getResultQuery() {
+        try {
+            return InputView.getPlayerResult();
+        } catch (IllegalArgumentException e) {
+            OutputView.printErrorMessage(e.getMessage());
+            return getResultQuery();
+        }
     }
 
 
     private Names getNames() {
         try {
-            OutputView.printRequestNames();
             return new Names(InputView.getNames());
         } catch (IllegalArgumentException e) {
             OutputView.printErrorMessage(e.getMessage());
@@ -39,11 +99,19 @@ public class LadderGameController {
 
     private int getHeight() {
         try {
-            OutputView.printRequestLadderHeight();
             return InputView.getHeight();
         } catch (IllegalArgumentException e) {
             OutputView.printErrorMessage(e.getMessage());
             return getHeight();
+        }
+    }
+
+    private Prizes getPrizes() {
+        try {
+            return new Prizes(InputView.getPrizes(this.names.getNames().size()));
+        } catch (IllegalArgumentException e) {
+            OutputView.printErrorMessage(e.getMessage());
+            return getPrizes();
         }
     }
 }
