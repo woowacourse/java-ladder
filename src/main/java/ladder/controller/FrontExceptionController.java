579279d89b10a7874ad8008194d702860713dcd1
woosung1223 <dntjd991223@naver.com>
2023-02-27 22:36:58 +0900

    refactor: 메소드 분리 및 PREFIX 삽입 자동화

--- src/main/java/ladder/controller/FrontExceptionController.java
+++ src/main/java/ladder/controller/FrontExceptionController.java
@@ -1,6 +1,5 @@
 package ladder.controller;
 
-import ladder.domain.exception.CustomException;
 import ladder.domain.exception.LadderLengthException;
 import ladder.domain.exception.NoSuchPlayerException;
 import ladder.domain.exception.PlayerNameLengthException;
@@ -24,11 +23,28 @@ public class FrontExceptionController {
     }
 
     private void init() {
-        messageSelector.put(LadderLengthException.class, MESSAGE_PREFIX + "사다리의 길이는 <플레이어 수 - 1> 이상이어야 합니다.");
-        messageSelector.put(PlayerNameLengthException.class, MESSAGE_PREFIX + "플레이어 이름의 길이는 1이상 5이하 입니다.");
-        messageSelector.put(NumberFormatException.class, MESSAGE_PREFIX + "입력된 값은 정수가 아닙니다.");
-        messageSelector.put(PlayerNumberException.class, MESSAGE_PREFIX + "플레이어 수는 두 명 이상이어야 합니다.");
-        messageSelector.put(NoSuchPlayerException.class, MESSAGE_PREFIX + "해당하는 플레이어를 찾을 수 없습니다.");
+        addMessage();
+        addPrefix();
+    }
+
+    private void addMessage() {
+        addMessageWithDomain();
+        addMessageWithView();
+    }
+
+    private void addMessageWithDomain() {
+        messageSelector.put(LadderLengthException.class, "사다리의 길이는 <플레이어 수 - 1> 이상이어야 합니다.");
+        messageSelector.put(PlayerNameLengthException.class, "플레이어 이름의 길이는 1이상 5이하 입니다.");
+        messageSelector.put(PlayerNumberException.class, "플레이어 수는 두 명 이상이어야 합니다.");
+        messageSelector.put(NoSuchPlayerException.class, "해당하는 플레이어를 찾을 수 없습니다.");
+    }
+
+    private void addMessageWithView() {
+        messageSelector.put(NumberFormatException.class, "입력된 값은 정수가 아닙니다.");
+    }
+
+    private void addPrefix() {
+        messageSelector.forEach((k, v) -> messageSelector.put(k, MESSAGE_PREFIX + v));
     }
 
     public void handle(RuntimeException e) {
