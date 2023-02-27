c87753e4fa161966ef54cfbdda13be91ebf3da8d
cpot5620 <=cpot5620@naver.com>
2023-02-27 17:08:46 +0900

    refactor(Ladder): 사다리 게임 실행 로직 구현

--- src/main/java/ladder/domain/Ladder.java
+++ src/main/java/ladder/domain/Ladder.java
@@ -2,8 +2,7 @@ package ladder.domain;
 
 import ladder.util.BooleanGenerator;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Ladder {
     private final List<Line> ladder = new ArrayList<>();
@@ -12,6 +11,40 @@ public class Ladder {
         for (int i = 0; i < height.getHeight(); i++) {
             ladder.add(new Line(countOfParticipants, generator));
         }
+    }
+
+    public Result perform(Names names, Bets bets) {
+        List<Bet> performedBets = new ArrayList<>(bets.getBets());
+
+        for (int i = ladder.size() - 1; i >= 0; i--) {
+            performByLine(ladder.get(i), performedBets);
+        }
+
+        return makeResult(names, performedBets);
+    }
+
+    private void performByLine(Line line, List<Bet> bets) {
+        for (int i = 0; i < line.size(); i++) {
+            swapBetIfExistLine(bets, i, line.existLineAtCell(i));
+        }
+    }
+
+    private void swapBetIfExistLine(List<Bet> bets, int index, boolean exist) {
+        if (!exist) {
+            return;
+        }
+
+        Collections.swap(bets, index, index + 1);
+    }
+
+    private Result makeResult(Names names, List<Bet> performedBets) {
+        Map<Name, Bet> result = new LinkedHashMap<>();
+
+        for (int i = 0; i < names.size(); i++) {
+            result.put(names.getNames().get(i), performedBets.get(i));
+        }
+
+        return new Result(result);
     }
 
     public List<Line> getLadder() {
--- src/main/java/ladder/domain/Result.java
+++ src/main/java/ladder/domain/Result.java
@@ -2,54 +2,15 @@ package ladder.domain;
 
 import ladder.error.ErrorMessage;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class Result {
     private final Map<Name, Bet> result = new LinkedHashMap<>();
 
-    public Result(Names names, Bets bets) {
-        validate(names, bets);
-
-        for (int i = 0; i < names.size(); i++) {
-            result.put(names.getNames().get(i), bets.getBets().get(i));
-        }
-    }
-
-    private void validate(Names names, Bets bets) {
-        if (names.size() != bets.size())
-            throw new IllegalArgumentException(ErrorMessage.DIFFERENT_PARTICIPANTS_AND_BETS_COUNT.getMessage());
-    }
-
-    public void perform(Ladder ladder) {
-        List<Name> names = List.copyOf(result.keySet());
-        List<Bet> bets = new ArrayList<>(result.values());
-
-        for (int i = ladder.getLadder().size() - 1; i >= 0; i--) {
-            Line line = ladder.getLadder().get(i);
-            performByLine(line, bets);
-        }
-
-        update(names, bets);
-    }
-
-    private void performByLine(Line line, List<Bet> bets) {
-        for (int i = 0; i < line.size(); i++) {
-            swapBetIfExistLine(bets, i, line.existLineAtCell(i));
-        }
-    }
-
-    private void swapBetIfExistLine(List<Bet> bets, int index, boolean exist) {
-        if (!exist) {
-            return;
-        }
-
-        Collections.swap(bets, index, index + 1);
-    }
-
-    private void update(List<Name> names, List<Bet> bets) {
-        for (int i = 0; i < names.size(); i++) {
-            result.put(names.get(i), bets.get(i));
-        }
+    public Result(Map<Name, Bet> result) {
+        this.result.putAll(result);
     }
 
     public Bet getBetByName(Name name) {
