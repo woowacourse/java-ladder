cb5c2ef5448111d923ecb04f42439ee440e7651a
Rosie <dev.kayoung@gmail.com>
2023-02-27 19:07:48 +0900

    refactor: 재시도 방법 통일
    * 재시도할 횟수를 인자로 받아 실행

--- src/main/java/laddergame/controller/LadderController.java
+++ src/main/java/laddergame/controller/LadderController.java
@@ -54,7 +54,7 @@ public class LadderController {
     private void playGame(PersonalNames names, LadderResult ladderResult, Ladder ladder) {
         final LadderGame ladderGame = new LadderGame(names, ladderResult);
         final GameResult result = ladderGame.moveAndGetResult(ladder);
-        printSearchedResultFrom(result);
+        printSearchedResultFrom(result, RETRY_LIMIT);
     }
 
     private void printLadder(PersonalNames names, LadderResult ladderResult, Ladder ladder) {
@@ -85,9 +85,8 @@ public class LadderController {
         throw new IllegalStateException("재시도 횟수를 넘었습니다.");
     }
 
-    private void printSearchedResultFrom(GameResult gameResult) {
-        int count = 0;
-        while (count++ < RETRY_LIMIT) {
+    private void printSearchedResultFrom(GameResult gameResult, int tryCount) {
+        while (tryCount-- > 0) {
             String name = inputView.readNameToCheckResult();
             if (isAll(name)) {
                 outputView.printTotalResult(gameResult);
