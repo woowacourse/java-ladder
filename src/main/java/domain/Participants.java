90eb49c0a6ac5e6db099b617f9acbd51e1db2174
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-02-27 10:16:13 +0900

    refactor: 사용하지 않는 메서드 제거

--- src/main/java/domain/Participants.java
+++ src/main/java/domain/Participants.java
@@ -63,12 +63,4 @@ public class Participants {
     public Person getByIndex(int index) {
         return people.get(index);
     }
-
-    public Person getByName(String name) {
-        Person findPerson = new Person(name);
-        return people.stream()
-                     .filter(person -> person.equals(findPerson))
-                     .findFirst()
-                     .orElseThrow(NotFindPersonException::new);
-    }
 }
