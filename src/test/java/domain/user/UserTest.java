9cdc303cd15b8c3909ec9521170b24a99925b66a
Go-Jaecheol <gojaech@naver.com>
2023-02-27 14:52:25 +0900

    refactor: movePosition이 값을 반환하지 않도록 변경

--- src/test/java/domain/user/UserTest.java
+++ src/test/java/domain/user/UserTest.java
@@ -85,27 +85,31 @@ class UserTest {
     @Nested
     @DisplayName("유저 이동 테스트")
     class MoveUserTest {
-        User user = new User("test", 0);
-
         @DisplayName("현재 position에 해당하는 다음 라인 값이 true인 경우, 왼쪽 밑으로 이동한다.")
         @Test
         void shouldSuccessMoveLowerLeft() {
+            User user = new User("test", 1);
             Line nextLine = new Line(4, new FixedBooleanGenerator(true));
-            assertThat(user.movePosition(1, nextLine)).isEqualTo(0);
+            user.movePosition(nextLine);
+            assertThat(user.getPosition()).isEqualTo(0);
         }
 
         @DisplayName("현재 position + 1에 해당하는 다음 라인 값이 true인 경우, 오른쪽 밑으로 이동한다.")
         @Test
         void shouldSuccessMoveLowerRight() {
+            User user = new User("test", 0);
             Line nextLine = new Line(4, new FixedBooleanGenerator(true));
-            assertThat(user.movePosition(0, nextLine)).isEqualTo(1);
+            user.movePosition(nextLine);
+            assertThat(user.getPosition()).isEqualTo(1);
         }
 
         @DisplayName("현재 position과 position + 1에 해당하는 다음 라인 값이 둘 다 false인 경우, 그대로 밑으로 이동한다.")
         @Test
         void shouldSuccessMoveDown() {
+            User user = new User("test", 0);
             Line nextLine = new Line(4, new FixedBooleanGenerator(false));
-            assertThat(user.movePosition(0, nextLine)).isEqualTo(0);
+            user.movePosition(nextLine);
+            assertThat(user.getPosition()).isEqualTo(0);
         }
     }
 }
