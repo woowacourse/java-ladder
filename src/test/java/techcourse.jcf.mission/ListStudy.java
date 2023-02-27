cb16ba590b49cf462ab41115fc4eb1b98a4caeb5
Eunkeee <eunkeeeee@gmail.com>
2023-02-27 16:30:28 +0900

    style(ListStudy): 주석 제거

--- src/test/java/techcourse.jcf.mission/ListStudy.java
+++ src/test/java/techcourse.jcf.mission/ListStudy.java
@@ -17,17 +17,17 @@ public class ListStudy {
         assertThatThrownBy(() -> values.indexOf("none"))
                 .isInstanceOf(NoSuchElementException.class);
 
-        assertThat(values.add("third")).isTrue(); // 세 번째 값을 추가한다.
+        assertThat(values.add("third")).isTrue();
 
-        assertThat(values.size()).isEqualTo(3); // list의 크기를 구한다.
-        assertThat(values.get(0)).isEqualTo("first"); // 첫 번째 값을 찾는다.
-        assertThat(values.contains("first")).isTrue(); // "first" 값이 포함되어 있는지를 확인한다.
+        assertThat(values.size()).isEqualTo(3);
+        assertThat(values.get(0)).isEqualTo("first");
+        assertThat(values.contains("first")).isTrue();
 
-        assertThat(values.remove(0)).isEqualTo("first"); // 첫 번째 값을 삭제한다.
+        assertThat(values.remove(0)).isEqualTo("first");
         assertThat(values.remove("second")).isTrue();
         assertThat(values.remove("none")).isFalse();
 
-        assertThat(values.size()).isEqualTo(1); // 값이 삭제 됐는지 확인한다
+        assertThat(values.size()).isEqualTo(1);
 
         SimpleList emptyValues = new SimpleArrayList();
         assertThat(emptyValues.isEmpty()).isTrue();
