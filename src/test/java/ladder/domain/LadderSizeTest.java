f0f310f8bb352a75cd3616e74fec647a973f2d4e
JunDonghyuk <ehdgur4814@naver.com>
2023-02-27 15:12:51 +0900

    refactor: final keyword 추가

--- src/test/java/ladder/domain/LadderSizeTest.java
+++ src/test/java/ladder/domain/LadderSizeTest.java
@@ -1,17 +1,18 @@
 package ladder.domain;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class LadderSizeTest {
 
     @ParameterizedTest
     @ValueSource(ints = {-1, 0, 1001, 10000})
     @DisplayName("높이가 1 미만이거나 1000 초과이면 예외를 던진다.")
-    void heightRangeTest(int height) {
-        Assertions.assertThrows(IllegalArgumentException.class,
+    void heightRangeTest(final int height) {
+        assertThrows(IllegalArgumentException.class,
                 () ->  new LadderSize(3, height));
     }
 }
