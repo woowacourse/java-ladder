d63c5ee5a616911d566de4d3e97cd8bcfb7326f1
JunDonghyuk <ehdgur4814@naver.com>
2023-02-27 15:07:57 +0900

    test: LadderGameTest 테스트 추가

--- src/test/java/ladder/domain/LadderGameTest.java
+++ src/test/java/ladder/domain/LadderGameTest.java
@@ -1,17 +1,18 @@
 package ladder.domain;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class LadderGameTest {
 
     BooleanGenerator booleanGenerator;
+    LadderGame ladderGame;
 
     @BeforeEach
     void setting() {
@@ -24,53 +25,72 @@ public class LadderGameTest {
             }
         }
 
+        /** pobi crong seong
+         *     |-----|     |
+         *     |-----|     |
+         *     |-----|     |
+         *     |-----|     |
+         *     |-----|     |
+         *     꽝 4000    당첨
+         *
+         *     결과 = pobi : 4000
+         *           crong : 꽝
+         *           seong : 당첨
+         */
+
         booleanGenerator = new IntendedBooleanGenerator();
+
+        List<String> names = List.of("pobi", "crong", "seong");
+        int height = 5;
+        List<String> prizes = List.of("꽝", "4000", "당첨");
+        ladderGame = new LadderGame(names, height, prizes, booleanGenerator);
     }
 
     @Test
     @DisplayName("사다리 게임 내 플레이어 이름들은 처음 전달했을 때와 같아야 한다.")
     void ladderGamePlayerNamesTest() {
-        List<String> names = List.of("pobi", "crong", "seong", "haddy");
-        int height = 5;
-        List<String> prizes = List.of("꽝", "4000", "당첨", "3000");
-
-        LadderGame ladderGame = new LadderGame(names, height, prizes, booleanGenerator);
         List<String> playerNames = ladderGame.getNames();
-
-        Assertions.assertTrue(playerNames.containsAll(names));
+        assertTrue(playerNames.containsAll(List.of("pobi", "crong", "seong")));
     }
 
     @Test
-    @DisplayName("사다리 게임 내 게임결과들은 처음 전달했을 때와 같아야 한다.")
+    @DisplayName("사다리 게임 내 당첨결과들은 처음 전달했을 때와 같아야 한다.")
     void ladderGamePrizesTest() {
-        List<String> names = List.of("pobi", "crong", "seong");
-        int height = 5;
-        List<String> prizes = List.of("꽝", "4000", "당첨");
-
-        LadderGame ladderGame = new LadderGame(names, height, prizes, booleanGenerator);
         List<String> results = ladderGame.getPrizes();
-
-        Assertions.assertTrue(results.containsAll(prizes));
+        assertTrue(results.containsAll(List.of("꽝", "4000", "당첨")));
     }
 
     @Test
-    @DisplayName("사다리 게임 실행 결과 후 비교")
-    void ladderGameStartTest() {
-        List<String> names = List.of("pobi", "crong", "seong");
-        int height = 3;
-        List<String> prizes = List.of("꽝", "4000", "당첨");
-
-        /**pobi crong seong
-         *  |-----|     |
-         *  |-----|     |
-         *  |-----|     |
-         *  꽝   4000   당첨
-         */
-
-        LadderGame ladderGame = new LadderGame(names, height, prizes, booleanGenerator);
-        ladderGame.start();
-        assertThat(ladderGame.getGameResult("pobi").getGameResult().get(0)).isEqualTo("4000");
-        assertThat(ladderGame.getGameResult("crong").getGameResult().get(0)).isEqualTo("꽝");
-        assertThat(ladderGame.getGameResult("seong").getGameResult().get(0)).isEqualTo("당첨");
+    @DisplayName("플레이어 이름이 all이면 사다리게임이 끝나야한다.")
+    void gameContinueTest() {
+        String playerName = "all";
+        assertFalse(ladderGame.continueGame(playerName));
     }
+
+    @Test
+    @DisplayName("플레이어 한 명의 게임 결과의 반환 Map의 사이즈는 1이어야한다.")
+    void getOnePlayerGameResultTest() {
+        ladderGame.start();
+        HashMap<String, String> gameResult = ladderGame.getGameResult("pobi");
+        assertEquals(1, gameResult.size());
+    }
+
+    @Test
+    @DisplayName("플레이어 전체의 게임 결과의 반환 Map의 사이즈는 플레이어의 수와 같아야한다.")
+    void getGameAllPlayerResultTest() {
+        ladderGame.start();
+        HashMap<String, String> gameResult = ladderGame.getGameResult("all");
+        assertEquals(3, gameResult.size());
+    }
+
+    @Test
+    @DisplayName("플레이어 한 명의 게임 결과값 일치 테스트")
+    void matchEachPlayerToPrizeTest() {
+        ladderGame.start();
+        HashMap<String, String> gameResult = ladderGame.getGameResult("all");
+        assertEquals("4000", gameResult.get("pobi"));
+        assertEquals("꽝", gameResult.get("crong"));
+        assertEquals("당첨", gameResult.get("seong"));
+    }
+
 }
