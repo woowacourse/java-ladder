2c5137a608b6b0e52e9c16a956f169c7055e2f37
song-wooseok <sws981202@gmail.com>
2023-02-27 15:46:44 +0900

    feat: 게임 플레이 기능 구현

--- src/main/java/domain/ladder/LadderGame.java
+++ src/main/java/domain/ladder/LadderGame.java
@@ -1,16 +1,23 @@
 package domain.ladder;
 
-import domain.Prizes;
+import domain.game.State;
+import domain.result.Prizes;
 import domain.player.Player;
 import domain.player.Players;
+import domain.result.Result;
 
 
 public class LadderGame {
+    private static final String GAME_NOT_PLAYED_MESSAGE = "게임이 아직 플레이 되지 않았습니다.";
     private final Ladder ladder;
     private final Players players;
     private final Prizes prizes;
 
+    private final State gameState;
+
     public LadderGame(Ladder ladder, Players players, Prizes prizes){
+        this.gameState = new State();
+
         this.ladder = ladder;
         this.players = players;
         this.prizes = prizes;
@@ -19,6 +26,18 @@ public class LadderGame {
     public void play() {
         for(Player player: players.getPlayers()){
             ladder.ride(player);
+        }
+        gameState.flipState();
+    }
+
+    public Result getResult(){
+        validateGamePlayedState();
+        return new Result(players, prizes);
+    }
+
+    private void validateGamePlayedState(){
+        if(!this.gameState.isPlayed()){
+            throw new IllegalStateException(GAME_NOT_PLAYED_MESSAGE);
         }
     }
 }
