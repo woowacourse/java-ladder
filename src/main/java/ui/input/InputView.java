c8c4e59c559be069f104bff2e3f90a4e44e790d4
SproutMJ <mjk54565@gmail.com>
2023-02-27 17:11:43 +0900

    refactor(ui): 잘못된 이름 regex 수정

--- src/main/java/ui/input/InputView.java
+++ src/main/java/ui/input/InputView.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
+import javax.print.attribute.standard.MediaSize.NA;
 
 /**
  * @author 최원용
@@ -17,7 +18,7 @@ public class InputView {
     private static final String INPUT_REWARDS = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
     private static final String INPUT_LADDER_HEIGHT = "최대 사다리 높이는 몇 개인가요?";
     private static final String INPUT_WANTS_SEE_PERSON = "결과를 보고 싶은 사람은?";
-    private static final String REGEX = ",";
+    private static final String NAME_DELIMITER = ",";
 
     public static List<String> inputPersonName() {
         System.out.println(INPUT_PEOPLES_NAME);
@@ -34,7 +35,7 @@ public class InputView {
         System.out.println("\n" + INPUT_REWARDS);
         while (true) {
             try {
-                List<String> rewards = Arrays.asList(scanner.nextLine().split(REGEX));
+                List<String> rewards = Arrays.asList(scanner.nextLine().split(NAME_DELIMITER));
                 InputVerifier.validateRewardsNum(rewards, peopleNum);
                 return rewards;
             } catch (IllegalArgumentException e) {
