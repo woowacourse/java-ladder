50a596f749e2ffaf8004e0e6e482efc49a5de35a
0chil <0@chll.it>
2023-03-02 00:23:48 +0900

    refactor(LadderGameTest): 변수 정리 및 DisplayName 적용

--- src/test/java/domain/LadderGameTest.java
+++ src/test/java/domain/LadderGameTest.java
@@ -22,11 +22,14 @@ class LadderGameTest extends AbstractTestFixture {
                 new Line(convert(true, false, true)),
                 new Line(convert(false, true, false)),
                 new Line(convert(true, false, true)));
+        Participants participants = new Participants(createParticipantsFrom("a", "b", "c", "d"));
+        final var prizes = createPrizesFrom("1", "2", "3", "4");
+
         assertThatNoException().isThrownBy(
-                () -> new LadderGame(createDefaultParticipants(), ladder, createPrizesFrom("1", "2", "3", "4")));
+                () -> new LadderGame(participants, ladder, prizes));
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "특정 참가자의 결과를 알 수 있다")
     @CsvSource({"a,3", "b,4", "c,1", "d,2"})
     void 특정_참가자의_결과를_알_수_있다(String participantName, String expectedPrize) {
         Participants participants = new Participants(createParticipantsFrom("a", "b", "c", "d"));
@@ -37,12 +40,13 @@ class LadderGameTest extends AbstractTestFixture {
                 new Line(convert(false, true, false)),
                 new Line(convert(true, false, true)));
         final var prizes = createPrizesFrom("1", "2", "3", "4");
-        LadderGame ladder = new LadderGame(participants, lines, prizes);
+        LadderGame ladderGame = new LadderGame(participants, lines, prizes);
 
-        assertThat(ladder.findPrizeFor(participantName)).isEqualTo(expectedPrize);
+        assertThat(ladderGame.findPrizeFor(participantName)).isEqualTo(expectedPrize);
     }
 
     @Test
+    @DisplayName("Line들로 Ladder를 생성한다")
     void 참가자_수_경품_수가_같아야한다() {
         Participants participants = new Participants(createParticipantsFrom("a", "b", "c", "d"));
         final var lines = createLadderWith(
