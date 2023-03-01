8f1514f213b5efe02e4a6457ae77333725f7140b
bjk1649 <qhwns1649@naver.com>
2023-03-01 15:33:36 +0900

    feat: Player에 equals와 hashcode 재정의

--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -1,5 +1,7 @@
 package domain;
 
+import java.util.Objects;
+
 final public class Player {
 
     private final Name name;
@@ -16,5 +18,22 @@ final public class Player {
 
     public Position position() {
         return new Position(position.value());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Player player = (Player) o;
+        return Objects.equals(name, player.name) && Objects.equals(position, player.position);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, position);
     }
 }
