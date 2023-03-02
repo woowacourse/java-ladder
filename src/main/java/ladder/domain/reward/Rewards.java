9995319e644b8c6deb9d822978d84af7a9e37046
DESKTOP-FNVN0HH\USER <beatus96@naver.com>
2023-03-02 23:32:25 +0900

    refactor: RewardName 도메인 수정 및 테스트

--- src/main/java/ladder/domain/reward/Rewards.java
+++ src/main/java/ladder/domain/reward/Rewards.java
@@ -1,16 +1,21 @@
 package ladder.domain.reward;
 
+import ladder.domain.laddergame.Position;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Map;
 
 public class Rewards {
 
-    private final List<Reward> rewards;
+
+    private final Map<Position, RewardName> rewards;
 
     public Rewards(int numberOfPlayers, List<String> rewards) {
         validateNumberOfRewards(numberOfPlayers, rewards);
 
-        this.rewards = createRewardsBy(rewards);
+        this.rewards = createRewardsBy(numberOfPlayers, rewards);
     }
 
     private void validateNumberOfRewards(int numberOfPlayers, List<String> rewards) {
@@ -19,20 +24,35 @@ public class Rewards {
         }
     }
 
-    private List<Reward> createRewardsBy(List<String> rewards) {
-        return rewards.stream()
-                .map(Reward::new)
-                .collect(Collectors.toList());
+    private Map<Position, RewardName> createRewardsBy(int numberOfPlayers, List<String> rewardNames) {
+        Map<Position, RewardName> rewards = new LinkedHashMap<>();
+
+        for (int i = 0; i < numberOfPlayers; i++) {
+            rewards.put(new Position(i), new RewardName(rewardNames.get(i)));
+        }
+
+        return rewards;
     }
 
-    public Reward findRewardBy(int index) {
-        return rewards.get(index);
+//    private List<SecondReward> createRewardsBy(int numberOfPlayers, List<String> rewards) {
+//        return IntStream.range(0, numberOfPlayers)
+//                .mapToObj(i -> new SecondReward(new RewardName(rewards.get(i)), new SecondPosition(i)))
+//                .collect(Collectors.toList());
+//    }
+
+    public RewardName findRewardBy(Position playerPosition) {
+        if(!rewards.containsKey(playerPosition)) {
+            throw new IllegalArgumentException("해당하는 보상이 없습니다.");
+        }
+
+        return rewards.get(playerPosition);
     }
 
-    public List<String> findRewards() {
-        return rewards.stream()
-                .map(Reward::getName)
-                .collect(Collectors.toList());
-    }
+    public List<String> findRewardNames() {
+        List<String> rewardNames = new ArrayList<>();
+        rewards.forEach(((position, rewardName) ->
+                rewardNames.add(rewardName.getName())));
 
+        return rewardNames;
+    }
 }
--- src/test/java/ladder/domain/reward/RewardNameTest.java
+++ src/test/java/ladder/domain/reward/RewardNameTest.java
@@ -1,6 +1,5 @@
-package ladder.domain;
+package ladder.domain.reward;
 
-import ladder.domain.reward.Reward;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,13 +8,13 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class RewardTest {
+class RewardNameTest {
 
     @ParameterizedTest(name = "reward: {0}")
     @ValueSource(strings = {"123456789", "123456"})
     @DisplayName("보상 길이가 5자를 초과하면 예외가 발생한다")
-    void validateRewardIncorrectLengthTest(String inputReward) {
-        assertThatThrownBy(() -> new Reward(inputReward))
+    void validateRewardIncorrectLengthTest(final String inputReward) {
+        assertThatThrownBy(() -> new RewardName(inputReward))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("보상은 5자 이하여야 합니다.");
     }
@@ -23,9 +22,9 @@ class RewardTest {
     @Test
     @DisplayName("보상 길이가 공백이면 예외가 발생한다")
     void validateRewardBlankTest() {
-        String inputReward = "";
+        final String inputReward = "";
 
-        assertThatThrownBy(() -> new Reward(inputReward))
+        assertThatThrownBy(() -> new RewardName(inputReward))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("보상을 입력하지 않았습니다.");
     }
@@ -33,8 +32,9 @@ class RewardTest {
     @ParameterizedTest(name = "reward: {0}")
     @ValueSource(strings = {"1", "12", "12345"})
     @DisplayName("보상 길이가 1자 이상 5자 이하면 정상적으로 생성된다")
-    void validateRewardCorrectLengthTest(String inputReward) {
+    void validateRewardCorrectLengthTest(final String inputReward) {
 
-        Assertions.assertDoesNotThrow(() -> new Reward(inputReward));
+        Assertions.assertDoesNotThrow(() -> new RewardName(inputReward));
     }
+
 }
