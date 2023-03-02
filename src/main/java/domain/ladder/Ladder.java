fbee1adf2078ce552e2750b933867accd5632b92
Go-Jaecheol <gojaech@naver.com>
2023-03-03 02:33:45 +0900

    refactor: Ladder 클래스에 필요 없는 필드 삭제

--- src/main/java/domain/ladder/Ladder.java
+++ src/main/java/domain/ladder/Ladder.java
@@ -7,14 +7,12 @@ import utils.BooleanGenerator;
 
 public class Ladder {
     private final List<LineDto> lines;
-    private final Height height;
 
     public Ladder(int width, Height height, BooleanGenerator booleanGenerator) {
         this.lines = new ArrayList<>();
         for (int i = 0; i < height.getHeight(); i++) {
             this.lines.add(LineDto.from(new Line(width, booleanGenerator)));
         }
-        this.height = height;
     }
 
     public List<LineDto> getLines() {
