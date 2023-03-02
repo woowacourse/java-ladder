fabfeb1dbddd00c8ee0550270ec6ff7305c90372
songusika <sws981202@gmail.com>
2023-03-02 10:25:57 +0900

    refactor: 사용하지 않는 필드 및 메서드 삭제

--- src/main/java/domain/ladder/Ladder.java
+++ src/main/java/domain/ladder/Ladder.java
@@ -3,30 +3,13 @@ package domain.ladder;
 import domain.player.Player;
 import java.util.List;
 import java.util.stream.Collectors;
-import util.BooleanGenerator;
 
 public class Ladder {
 
     private final List<Line> lines;
-    private final BooleanGenerator booleanGenerator;
 
     public Ladder(final List<Line> liens) {
         this.lines = liens;
-        this.booleanGenerator = null;
-    }
-
-    public int getLineHeight() {
-        return this.lines.size();
-    }
-
-    public int getWidth() {
-        if (this.lines.isEmpty()) {
-            return 0;
-        }
-
-        return this.lines
-                .get(0)
-                .getWidth();
     }
 
     public List<List<Boolean>> getLadderShape() {
