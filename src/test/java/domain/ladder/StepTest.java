219cd7edfb97c66a394cd1911157e92204caf5e3
songusika <sws981202@gmail.com>
2023-03-06 03:21:40 +0900

    refactor: 로직 변경으로 인한 디댐대 테스트 코드 리팩터링

--- src/test/java/domain/ladder/StepTest.java
+++ src/test/java/domain/ladder/StepTest.java
@@ -22,7 +22,7 @@ class StepTest {
     @DisplayName(" 오른쪽 발판일 때 사용자 위치를 오른쪽으로 보낸다.")
     void whenConnectedToRight_thenMoveRight() {
         //when
-        Step.RIGHT.step(position);
+        position = Step.RIGHT.step(position);
 
         //then
         assertThat(position)
@@ -34,7 +34,7 @@ class StepTest {
     @DisplayName(" 왼쪽 발판일 때 사용자 위치를 오른쪽으로 보낸다.")
     void whenConnectedToLeft_thenMoveLeft() {
         //when
-        Step.LEFT.step(position);
+        position = Step.LEFT.step(position);
 
         //then
         assertThat(position)
@@ -46,7 +46,7 @@ class StepTest {
     @DisplayName(" 없을 때 아래로 보내기 때문에 값이 변하지 않는다.")
     void givenCurrentPosition_thenGiveNextLinePosition() {
         //when
-        Step.NONE.step(position);
+        position = Step.NONE.step(position);
 
         //then
         assertThat(position)
