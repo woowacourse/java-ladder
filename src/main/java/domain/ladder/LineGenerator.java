31778b183460bf0379ede242042bf25526db5f0c
songusika <sws981202@gmail.com>
2023-03-06 03:34:07 +0900

    refactor: lines 필드 제거

--- src/main/java/domain/ladder/LineGenerator.java
+++ src/main/java/domain/ladder/LineGenerator.java
@@ -7,43 +7,34 @@ import util.BooleanGenerator;
 public class LineGenerator {
     private final BooleanGenerator generator;
 
-    private final List<Step> steps = new ArrayList<>();
-    private int targetWidth;
-
     public LineGenerator(final BooleanGenerator generator) {
         this.generator = generator;
     }
 
     public Line build(final int targetWidth) {
-        this.steps.clear();
-        this.targetWidth = targetWidth;
-
-        return buildLine();
-    }
-
-    private Line buildLine() {
-        buildFirstStep();
-        buildMiddleStep();
-        buildLastStep();
+        final List<Step> steps = new ArrayList<>();
+        buildFirstStep(steps);
+        buildMiddleStep(steps, targetWidth);
+        buildLastStep(steps);
         return new Line(new ArrayList<>(steps));
     }
 
-    private void buildMiddleStep() {
+    private void buildMiddleStep(final List<Step> steps, final int targetWidth) {
         for (int currentMiddleStep = 0; currentMiddleStep < targetWidth - 2; currentMiddleStep++) {
-            addStep(getValidConditionStep(generator.generate()));
+            addStep(steps, getValidConditionStep(generator.generate()));
         }
     }
 
-    private boolean isPreviousRightConnected() {
+    private boolean isPreviousRightConnected(final List<Step> steps) {
         return (steps.get(steps.size() - 1) == Step.RIGHT);
     }
 
-    private void buildLastStep() {
-        addStep(Step.NONE);
+    private void buildLastStep(final List<Step> steps) {
+        addStep(steps, Step.NONE);
     }
 
-    private void addStep(final Step step) {
-        if (isPreviousRightConnected()) {
+    private void addStep(final List<Step> steps, final Step step) {
+        if (isPreviousRightConnected(steps)) {
             steps.add(Step.LEFT);
             return;
         }
@@ -51,7 +42,7 @@ public class LineGenerator {
         steps.add(step);
     }
 
-    private void buildFirstStep() {
+    private void buildFirstStep(final List<Step> steps) {
         steps.add(getValidConditionStep(generator.generate()));
     }
 
