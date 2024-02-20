436ffc69d49e32a7fe3b5bc03180a91add9e65e0
jhon3242 <rldotnr2@gmail.com>
2024-02-20 15:38:35 +0900

    test(green) : Ladder 생성자가 사람 수를 전달 받도록 변경
    Co-authored-by: zangsu <zangsu_@naver.com>

--- src/main/java/domain/Ladder.java
+++ src/main/java/domain/Ladder.java
@@ -2,13 +2,17 @@ package domain;
 
 public class Ladder {
 
-    public Ladder(int height) {
-        validate(height);
+    public Ladder(int personCount, int height) {
+        validate(personCount, height);
     }
 
-    private void validate(int height) {
+    private void validate(int personCount, int height) {
         if (height <= 0) {
             throw new IllegalArgumentException(); // TODO 예외 메시지
         }
+        if (personCount <= 0) {
+            throw new IllegalArgumentException(); // TODO 예외 메시지
+        }
+
     }
 }
