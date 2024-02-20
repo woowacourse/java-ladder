39507c4c142835200bcdb2e592ef6842a5ac8d31
ehtjsv2 <ehtjsv2@naver.com>
2024-02-20 17:17:15 +0900

    feat: 사다리 높이를 입력받는 기능 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -16,4 +16,9 @@ public class InputView {
                 .map(Player::new)
                 .toList();
     }
+
+    public static int readLadderHeight() {
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
+        return scanner.nextInt();
+    }
 }
