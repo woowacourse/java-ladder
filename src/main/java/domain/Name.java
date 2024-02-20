50bb66ef9769ddf01f3994972b5a5022e60efa86
SCY <helenason@naver.com>
2024-02-20 15:23:55 +0900

    feat: 참여자 이름 도메인 생성
    Co-authored-by: takoyakimchi <c4nyou결@gmail.com>

---
+++ src/main/java/domain/Name.java
@@ -0,0 +1,40 @@
+package domain;
+
+public class Name {
+
+    private static final int MIN_NAME_LENGTH = 1;
+    private static final int MAX_NAME_LENGTH = 5;
+    private static final String FORMAT_NAME = "^[A-Za-z0-9]+$";
+
+
+    private final String name;
+
+    public Name(String name) {
+        validate(name);
+        this.name = name;
+    }
+
+    private void validate(String name) {
+        validateNull(name);
+        validateLength(name);
+        validatePattern(name);
+    }
+
+    private void validateNull(String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("이름에 null을 입력할 수 없습니다.");
+        }
+    }
+
+    private void validateLength(String name) {
+        if (name.length() < MIN_NAME_LENGTH || name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException(MIN_NAME_LENGTH + "~" + MAX_NAME_LENGTH + "자의 이름만 허용합니다.");
+        }
+    }
+
+    private void validatePattern(String name) {
+        if (!name.matches(FORMAT_NAME)) {
+            throw new IllegalArgumentException("이름은 알파벳과 숫자만 허용합니다.");
+        }
+    }
+}
