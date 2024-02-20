cdeb4faf13104cbdc13937d0f7514b78a0a04aaa
JUHA <84626225+khabh@users.noreply.github.com>
2024-02-20 15:27:04 +0900

    test: 입력된 문자열로 Participants 생성 및 검증 테스트

---
+++ src/test/java/ladder/domain/ParticipantsTest.java
@@ -0,0 +1,41 @@
+package ladder.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+public class ParticipantsTest {
+
+    @Test
+    @DisplayName("참가자 이름을 쉼표 기준으로 구분한다.")
+    void splitNamesTest() {
+        // given
+        String names = "a, b, c, d";
+        List<String> expectNames = List.of("a", "b", "c", "d");
+
+        // when
+        Participants participants = new Participants(names);
+
+        // then
+        List<Participant> actualParticipants = participants.getValues();
+        assertThat(actualParticipants)
+                .hasSize(4)
+                .extracting(Participant::getName)
+                .containsAll(expectNames);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {",mia, pota", ",,", "mia,"})
+    @DisplayName("문자열 시작 혹은 끝이 쉼표일 경우 예외가 발생한다.")
+    void checkNamesSideTest(String names) {
+        // when & then
+        assertThatThrownBy(() -> new Participants(names))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
