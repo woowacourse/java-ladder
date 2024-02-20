e2002e758f7b69a482cca308424c0bd1cd5f8406
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-02-20 16:33:37 +0900

    test(LineTest) : 연속으로 True를 가질 수 없다 (Test Fails)

--- src/test/java/ladderGame/model/LineTest.java
+++ src/test/java/ladderGame/model/LineTest.java
@@ -1,7 +1,10 @@
 package ladderGame.model;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,5 +14,23 @@ class LineTest {
     @DisplayName("인원 수를 인자로 받아서 Line을 생성한다.")
     void createLine() {
         assertThatCode(() -> new Line(3));
+    }
+
+    @RepeatedTest(100)
+    @DisplayName("연속으로 True를 가질 수 없다.")
+    void notConsecutiveDraw() {
+        Line line = new Line(5);
+
+        List<Boolean> isDrawn = line.getIsDrawn();
+
+        boolean isConsecutive = false;
+        for(int i = 1; i < isDrawn.size(); i++) {
+            if(isDrawn.get(i) && isDrawn.get(i - 1)) {
+                isConsecutive = true;
+                break;
+            }
+        }
+
+        assertFalse(isConsecutive);
     }
 }
