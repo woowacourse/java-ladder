233b95914a0643f629610ceeede6a1677dbf4c11
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-02-20 19:48:25 +0900

    feat(Ladder): Ladder 검증 로직 작성
    Co-authored-by: HaiSeong <wjdgotjd9908@gmail.com>

--- src/main/java/model/Ladder.java
+++ src/main/java/model/Ladder.java
@@ -1,7 +1,25 @@
 package model;
 
+import java.util.List;
+
 public class Ladder {
+    private final List<Line> lines;
+
+    private Ladder(List<Line> lines) {
+        this.lines = lines;
+    }
+
     public static Ladder from(int height) {
-        return null;
+        validateHeight(height);
+        // TODO:
+//        IntStream.range(0, height)
+//                .();
+        return new Ladder(null);
+    }
+
+    private static void validateHeight(int height) {
+        if (height < 1) {
+            throw new IllegalArgumentException("사다리의 높이는 1 이상이어야 합니다.");
+        }
     }
 }
