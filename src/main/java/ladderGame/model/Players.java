f1cab8c62761857edf5080b74292e2aeaf44e7b0
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-02-20 15:05:17 +0900

    feat(Players) : 중복된 이름이 있을 시 예외처리 된다 (Test Passes)

--- src/main/java/ladderGame/model/Players.java
+++ src/main/java/ladderGame/model/Players.java
@@ -1,15 +1,25 @@
 package ladderGame.model;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Players {
     private final List<Player> players;
 
     public Players(List<String> names) {
+        validateDuplicationName(names);
+
         players = new ArrayList<>();
         for(String name : names) {
             players.add(new Player(name));
+        }
+    }
+
+    private void validateDuplicationName(List<String> name) {
+        if(new HashSet<>(name).size() != name.size()) {
+            throw new IllegalArgumentException("참여자들의 이름은 중복될 수 없습니다.");
         }
     }
 }
