027d03af0ad9a6e90545e649abbed811986319d4
ay-eonii <naayen@naver.com>
2024-02-20 17:09:32 +0900

    test(NamesTest): 이름 글자 수 검증

--- src/test/java/ladder/domain/NamesTest.java
+++ src/test/java/ladder/domain/NamesTest.java
@@ -4,14 +4,22 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class NamesTest {
 
     @Test
-    @DisplayName("사다리 게임에 참여하는 사람에 이름을 최대 5글자까지 부여할 수 있다.")
+    @DisplayName("사람 이름은 쉼표(,)를 기준으로 구분한다.")
     void createNames() {
         assertThatCode(() -> new Names("pobi,honux,crong,jk"))
                 .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("이름이 5글자를 초과하면 예외가 발생한다.")
+    void createInvalidNames() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Names("pobipobi,honux,crong,jk"));
     }
 
 }
