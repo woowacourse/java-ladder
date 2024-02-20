b310a7c3957046b825499dd742d4f7c71979d8f7
leetaehoon <xogns1514@gmail.com>
2024-02-20 16:32:28 +0900

    feat(Ladder): 사다리를 생성한다.

--- src/main/java/domain/Ladder.java
+++ src/main/java/domain/Ladder.java
@@ -1,6 +1,33 @@
 package domain;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Ladder {
+    private List<Line> ladder = new ArrayList<>();
+    private int personCount;
+    private int maxHeight;
+
+    public Ladder() {
+    }
+
+    public Ladder(int personCount, int maxHeight) {
+        this.personCount = personCount;
+        this.maxHeight = maxHeight;
+    }
+
+
+    public void generate() {
+        IntStream.range(0, maxHeight)
+                .forEach(iterator -> ladder.add(new Line(personCount)));
+    }
+
+    public int getPersonCount() {
+        return personCount;
+    }
+
+    public int getMaxHeight() {
+        return maxHeight;
+    }
 }
--- src/test/java/LadderTest.java
+++ src/test/java/LadderTest.java
@@ -1,9 +1,11 @@
 import domain.Ladder;
+import domain.Line;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class LadderTest {
     @Test
@@ -16,5 +18,31 @@ public class LadderTest {
         //then
         assertThatCode(() -> new Ladder()).doesNotThrowAnyException();
     }
+    
+    @Test
+    @DisplayName("사람 수와 높이로 Ladder 생성")
+    void createLadderWithPersonCountAndMaxHeight() throws Exception {
+        //given
+        int maxHeight = 4;
+        int personCount = 4;
+        //when
+        //then
+        assertThatCode(()->new Ladder(personCount,maxHeight)).doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("사다리를 생성한다")
+    void generate() {
+        //given
+        int maxHeight = 4;
+        int personCount = 4;
+        //when
+        Ladder ladder = new Ladder(maxHeight, personCount);
+        ladder.generate();
+        //then
+        assertThat(maxHeight).isEqualTo(ladder.getMaxHeight());
+    }
+
+    
 
 }
