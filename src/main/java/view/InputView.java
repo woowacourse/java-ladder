1d06b58831d0bd53eb09955b2a95e52b64c7ca35
Choo <choo000407@naver.com>
2024-02-20 22:05:36 +0900

    feat: 최대 사다리 높이는 숫자로 입력한다

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -8,6 +8,8 @@ public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
     private static final String SPLIT_DELIMITER = ",";
     private static final String ASK_PLAYER_NAMES = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    private static final String ASK_LADDER_HEIGHT = "최대 사다리 높이는 몇 개인가요?";
+    private static final String LADDER_HEIGHT_NOT_INTEGER = "최대 사다리 높이는 숫자로 입력한다.";
 
 
     private InputView() {
@@ -23,5 +25,19 @@ public class InputView {
         return Arrays.stream(input.split(SPLIT_DELIMITER))
                 .map(String::strip)
                 .toList();
+    }
+
+    public static int askLadderHeight() {
+        System.out.println(ASK_LADDER_HEIGHT);
+        String input = scanner.nextLine();
+        return parseLadderHeight(input);
+    }
+
+    private static int parseLadderHeight(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(LADDER_HEIGHT_NOT_INTEGER);
+        }
     }
 }
