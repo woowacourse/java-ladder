e34834f5aed140a7407f7aaadbe88ac2ebe0119d
eunjungL <dldmswjd322@gmail.com>
2024-02-20 15:28:53 +0900

    feat: Ladder height 검증 기능 추가

--- src/main/java/domain/Ladder.java
+++ src/main/java/domain/Ladder.java
@@ -1,2 +1,20 @@
-package domain;public class Ladder {
+package domain;
+
+import java.util.regex.Pattern;
+
+public class Ladder {
+
+    private static final Pattern HEIGHT_FORMAT_REGEX = Pattern.compile("^[1-9][0-9]*$");
+    private final int height;
+
+    public Ladder(String height) {
+        validateHeight(height);
+        this.height = Integer.parseInt(height);
+    }
+
+    private void validateHeight(String height) {
+        if (height == null || !HEIGHT_FORMAT_REGEX.matcher(height).matches()) {
+            throw new IllegalArgumentException("사다리의 최대 높이는 자연수여야 합니다.");
+        }
+    }
 }
