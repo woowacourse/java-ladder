5e88cfe30c67638d9a9149db450301c16a36e8e0
hoyeonyy <highoption@naver.com>
2024-02-20 15:22:47 +0900

    feat (PlayerName) : 5글자를 넘을 때 에러 발생 (Pass 단계)

--- src/main/java/ladder/PlayerName.java
+++ src/main/java/ladder/PlayerName.java
@@ -3,5 +3,8 @@ package ladder;
 public class PlayerName {
 
     public PlayerName(String name) {
+        if (name.length() > 5) {
+           throw new IllegalArgumentException("이름은 5글자를 넘을 수 없습니다");
+        }
     }
 }
