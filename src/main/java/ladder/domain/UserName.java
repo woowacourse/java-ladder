d1878da2c2b7768834a77b32eccdb76a4473d685
kelly6bf <kelly6bf@gmail.com>
2024-02-20 16:07:52 +0900

    refactor: UserName 검증 로직 메서드 분리 및 필드 추가

--- src/main/java/ladder/domain/UserName.java
+++ src/main/java/ladder/domain/UserName.java
@@ -1,7 +1,14 @@
 package ladder.domain;
 
 public class UserName {
+    private final String name;
+
     public UserName(final String name) {
+        validateNameLength(name);
+        this.name = name;
+    }
+
+    private void validateNameLength(final String name) {
         if (name.length() > 5) {
             throw new IllegalArgumentException("이름의 길이는 5 이하여야 합니다.");
         }
