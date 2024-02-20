5a839b45261a9a140fed56da899e6ce1db0630a2
SungKyum Kim <sungkyum1@naver.com>
2024-02-20 15:20:57 +0900

    feat: 겹치지 않는 가로 라인 생성 기능 구현

--- src/main/java/domain/Line.java
+++ src/main/java/domain/Line.java
@@ -1,2 +1,29 @@
-package domain;public class Line {
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Line {
+    private final List<Boolean> points = new ArrayList<>();
+
+    public Line(int personCount) {
+        Random random = new Random();
+        for (int i = 0; i < personCount-1; i++) {
+            boolean isTrue = random.nextBoolean();
+            if (i != 0 && points.get(i - 1) == true) {
+                if (isTrue) {
+                    points.add(false);
+                } else {
+                    points.add(false);
+                }
+            } else {
+                points.add(isTrue);
+            }
+        }
+    }
+
+    public List<Boolean> getPoints() {
+        return points;
+    }
 }
