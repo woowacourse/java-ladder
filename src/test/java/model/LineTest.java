310bb392ccc675cd2125acb6180b7746fc1ccd48
Choo <choo000407@naver.com>
2024-02-20 21:36:25 +0900

    test: 한 라인에서 다리가 겹치지 않게 랜덤으로 놓는다

--- src/test/java/model/LineTest.java
+++ src/test/java/model/LineTest.java
@@ -1,5 +1,23 @@
 package model;
 
-public class LineTest {
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LineTest {
+    @DisplayName("라인 생성 시 다리가 겹치면 예외가 발생한다")
+    @Test
+    void testInvalidLine() {
+        assertThatThrownBy(() -> new Line(List.of(true, true, false)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("라인 생성 시 다리가 겹치지 않으면 예외가 발생하지 않는다")
+    @Test
+    void testValidLine() {
+        assertDoesNotThrow(() -> new Line(List.of(true, false, true)));
+    }
 }
