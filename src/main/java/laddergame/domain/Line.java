a08b8165dbf3b753e94f1af354ee6b96b81283ae
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-02-20 18:51:28 +0900

    feat: 특정 좌표에서 랜덤값에 따라 다리 건설 구현
    Co-Authored-By: lemone <55480011+jihyeonl@users.noreply.github.com>

--- src/main/java/laddergame/domain/Line.java
+++ src/main/java/laddergame/domain/Line.java
@@ -3,6 +3,7 @@ package laddergame.domain;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import laddergame.util.NumberGenerator;
 
 public class Line {
     private final List<Boolean> points;
@@ -11,7 +12,18 @@ public class Line {
         this.points = new ArrayList<>(Collections.nCopies(playerCount, Boolean.FALSE));
     }
 
+    public void buildBridge(final int position, final NumberGenerator numberGenerator) {
+        if (numberGenerator.generate() == 1) {
+            points.set(position, Boolean.TRUE);
+        }
+    }
+
+    public boolean isBuilt(final int position) {
+        return points.get(position);
+    }
+
     public List<Boolean> getPoints() {
         return points;
     }
+
 }
