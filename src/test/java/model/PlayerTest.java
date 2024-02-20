ce66670c179c5788091732ff3d5af7ff02042064
Choo <choo000407@naver.com>
2024-02-20 15:37:25 +0900

    test: 참여자 이름은 최대 5글자이다

---
+++ src/test/java/model/PlayerTest.java
@@ -0,0 +1,26 @@
+package model;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class PlayerTest {
+    @DisplayName("참여자 이름이 5자 초과면 예외가 발생한다")
+    @ParameterizedTest
+    @CsvSource({"pobii", "doraa", "jojojo"})
+    void testInvalidLengthOfPlayerName(String name) {
+        assertThatThrownBy(() -> new Player(name))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("참여자 이름이 5자 이하면 예외가 발생하지 않는다")
+    @ParameterizedTest
+    @CsvSource({"p", "dora", "joj"})
+    void testValidLengthOfPlayerName(String name) {
+        assertDoesNotThrow(() -> new Player(name))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
