fc9fbb3bef36d29b0e321860c30da7b90c1fca43
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-02-20 15:00:56 +0900

    feat(Person): 이름 검증 로직 작성

--- src/main/java/model/Person.java
+++ src/main/java/model/Person.java
@@ -1,11 +1,24 @@
 package model;
 
 public class Person {
+    private static final int MIN_NAME_LENGTH = 1;
+    private static final int MAX_NAME_LENGTH = 5;
+
+    private final String name;
 
     public Person(String name) {
+        validateNameLength(name);
+        this.name = name;
+    }
+
+    private void validateNameLength(String name) {
+        int length = name.length();
+        if (length < MIN_NAME_LENGTH || length > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("이름은 최소 1글자 최대 5글자여야 합니다.");
+        }
     }
 
     public String getName() {
-        return "moly";
+        return name;
     }
 }
