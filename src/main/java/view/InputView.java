a23d9e4dd9d615768d969a9cb7e02a456ea28685
HyunJoong Kim <hjk0761@naver.com>
2024-02-20 16:26:28 +0900

    feat: readNames에서 null 혹은 빈 문자열을 입력받으면 예외를 발생하는 기능 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -8,7 +8,14 @@ public class InputView {
 
     public static List<String> readNames(Supplier<String> input) {
         String inputString = input.get();
+        validateEmpty(inputString);
         return Arrays.stream(inputString.split(","))
                 .toList();
+    }
+
+    private static void validateEmpty(String inputString) {
+        if (inputString == null || inputString.isBlank()) {
+            throw new IllegalArgumentException("null 혹은 비 문자열을 입력할 수 없습니다.");
+        }
     }
 }
--- src/test/java/view/InputViewTest.java
+++ src/test/java/view/InputViewTest.java
@@ -3,6 +3,9 @@ package view;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
@@ -10,8 +13,18 @@ class InputViewTest {
 
     @DisplayName("interface Reader로부터 받은 String을 List<String>으로 반환한다.")
     @Test
-    void interface_Reader로부터_받은_String을_List으로_반환한다() {
+    void stringToListTest() {
         Assertions.assertThat(InputView.readNames(() -> "a,b,c"))
                 .isEqualTo(List.of("a", "b", "c"));
+    }
+
+    @DisplayName("null 혹은 빈 문자열을 받으면 예외를 발생한다.")
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {" ", "\t", "\n"})
+    void emptyInputTest(String input) {
+        Assertions.assertThatThrownBy(() -> InputView.readNames(() -> input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("null 혹은 비 문자열을 입력할 수 없습니다.");
     }
 }
