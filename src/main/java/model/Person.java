c3611ac5acb6ddc5c7518e7c973e1d272463e302
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-02-20 14:48:09 +0900

    test(PersonTest): 이름 검증 로직 테스트 작성

---
+++ src/main/java/model/Person.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Person {
+
+    public Person(String name) {
+    }
+
+    public String getName() {
+        return "moly";
+    }
+}
---
+++ src/test/java/model/PersonTest.java
@@ -0,0 +1,28 @@
+package model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class PersonTest {
+
+    @DisplayName("사람은 이름을 갖는다.")
+    @Test
+    void createPerson() {
+        String name = "moly";
+        Person person = new Person(name);
+        assertThat(person.getName()).isEqualTo(name);
+    }
+
+    @ParameterizedTest(name = "이름은 최소 1글자 최대 5글자다.")
+    @ValueSource(strings = {"", "mollly"})
+    void createPersonThrowException(String name) {
+        assertThatThrownBy(()-> new Person(name))
+                .isExactlyInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이름은 최소 1글자 최대 5글자여야 합니다.");
+    }
+}
