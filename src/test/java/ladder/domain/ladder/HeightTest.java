ad521836925110fc05ddc7efaa14c11bb4495743
김민수 MINSOO KIM <alstn113@gmail.com>
2024-02-20 14:39:28 +0900

    feat(Height): 사다리 높이 구현

---
+++ src/test/java/ladder/domain/ladder/HeightTest.java
@@ -0,0 +1,25 @@
+package ladder.domain.ladder;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class HeightTest {
+
+    @Test
+    @DisplayName("사다리는 높이를 가진다.")
+    void testConstruct() {
+        int value = 1;
+        Height height = new Height(value);
+        assertThat(value).isEqualTo(height.getHeight());
+    }
+
+    @Test
+    @DisplayName("사다리의 높이가 범위를 벗어나면 예외가 발생한다.")
+    void testInvalidHeightRange() {
+        assertThatThrownBy(() -> new Height(0))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
