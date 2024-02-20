1b8f5f5a6d45e41d9ee29aeb5c6b55872da57619
hoyeonyy <highoption@naver.com>
2024-02-20 18:03:19 +0900

    feat (Line) : 가로 라인이 이어지면 예외 발생

--- src/main/java/ladder/domain/Line.java
+++ src/main/java/ladder/domain/Line.java
@@ -1,6 +1,7 @@
 package ladder.domain;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Line {
     public Line(List<Boolean> line) {
@@ -8,8 +9,24 @@ public class Line {
     }
 
     private void validate(List<Boolean> line) {
+        validateIsNotEmpty(line);
+        validateIsNotOverlapped(line);
+    }
+
+    private void validateIsNotEmpty(List<Boolean> line) {
         if (line.isEmpty()) {
             throw new IllegalArgumentException("적어도 가로 라인이 하나이상 있어야 한다.");
         }
+    }
+
+    private void validateIsNotOverlapped(List<Boolean> line) {
+        if (isRowLineOverlapped(line)) {
+            throw new IllegalArgumentException("가로 라인이 이어지면 안된다.");
+        }
+    }
+
+    private boolean isRowLineOverlapped(List<Boolean> line) {
+        return IntStream.range(1, line.size())
+                .anyMatch(i -> line.get(i) && line.get(i - 1));
     }
 }
--- src/test/java/ladder/domain/LineTest.java
+++ src/test/java/ladder/domain/LineTest.java
@@ -1,6 +1,7 @@
 package ladder.domain;
 
 import java.util.Collections;
+import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,4 +14,16 @@ class LineTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("적어도 가로 라인이 하나이상 있어야 한다.");
     }
+
+    @Test
+    @DisplayName("가로 라인이 겹칠 경우, 예외 발생")
+    void validateOverlappedRowLine() {
+        List<Boolean> rowLines = List.of(true, true, false);
+
+        Assertions.assertThatThrownBy(() -> new Line(rowLines))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("가로 라인이 이어지면 안된다.");
+    }
+
+
 }
