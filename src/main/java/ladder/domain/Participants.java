3e3e05a8175269169c460eb93161281b696e7d64
jongmee <ra123221@gmail.com>
2024-02-20 15:38:24 +0900

    feat: Participants 생성 및 구분자 검증 기능 구현

---
+++ src/main/java/ladder/domain/Participants.java
@@ -0,0 +1,33 @@
+package ladder.domain;
+
+import ladder.exception.DelimiterBoundaryException;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Participants {
+    private static final String DELIMITER = ",";
+
+    private final List<Participant> participants;
+
+    public Participants(String names) {
+        validate(names);
+        this.participants = convertToParticipants(names);
+    }
+
+    private void validate(final String names) {
+        if (names.startsWith(DELIMITER) || names.endsWith(DELIMITER)) {
+            throw new DelimiterBoundaryException();
+        }
+    }
+
+    private List<Participant> convertToParticipants(final String names) {
+        return Arrays.stream(names.split(DELIMITER))
+                .map(Participant::new)
+                .toList();
+    }
+
+    public List<Participant> getValues() {
+        return participants;
+    }
+}
---
+++ src/main/java/ladder/exception/DelimiterBoundaryException.java
@@ -0,0 +1,9 @@
+package ladder.exception;
+
+public class DelimiterBoundaryException extends IllegalArgumentException {
+    private static final String MESSAGE = "구분자는 양 끝에 입력할 수 없습니다.";
+
+    public DelimiterBoundaryException() {
+        super(MESSAGE);
+    }
+}
