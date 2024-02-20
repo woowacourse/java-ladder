d35943722a089310990c3e8c0c44ec30de44458f
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-02-20 18:43:30 +0900

    test: Boolean값이 true이면 특정 위치에서 다리를 건설하는지 검증
    Co-Authored-By: lemone <55480011+jihyeonl@users.noreply.github.com>

--- src/test/java/laddergame/domain/LineTest.java
+++ src/test/java/laddergame/domain/LineTest.java
@@ -1,7 +1,10 @@
 package laddergame.domain;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import laddergame.util.NumberGenerator;
+import laddergame.util.RandomBooleanGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -19,4 +22,22 @@ public class LineTest {
         //then
         assertEquals(line.getPoints().size(), personCount);
     }
+
+    @Test
+    @DisplayName("랜덤값이 true일 경우 특정 위치에서 다리를 건설한다.")
+    public void buildBridgeTest() {
+        //given
+        final int personCount = 4;
+        final int position = 1;
+        Line line = new Line(personCount);
+
+        NumberGenerator alwaysTrueGenerator = () -> 1;
+
+        //when
+        line.buildBridge(position, alwaysTrueGenerator);
+
+        //then
+        assertTrue(line.isBuilt(position));
+    }
+
 }
