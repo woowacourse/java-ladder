1207123b99f781b79a4486a3763d8bd13465d05c
kelly6bf <kelly6bf@gmail.com>
2024-02-20 20:05:46 +0900

    refactor: 인원수 - 1 개수 만큼의 포인트를 가진 Line을 만드는 기능 메소드로 분리

--- src/main/java/ladder/domain/Line.java
+++ src/main/java/ladder/domain/Line.java
@@ -10,9 +10,14 @@ public class Line {
     private final List<Boolean> points;
 
     public Line(final int userCount) {
-        this.points = new ArrayList<>(userCount - 1);
+        this.points = initPoints(userCount);
+    }
+
+    private List<Boolean> initPoints(final int userCount) {
+        final List<Boolean> points = new ArrayList<>(userCount - 1);
         IntStream.rangeClosed(1, userCount - 1)
                 .forEach(i -> points.add(false));
+        return points;
     }
 
     public List<Boolean> getPoints() {
