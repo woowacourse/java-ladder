944c24c9af1ad6efe326db733e90081f36b267d2
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-02-20 14:39:07 +0900

    test: 사용자 이름 최대 길이 예외 처리 테스트
    Co-Authored-By: lemone <55480011+jihyeonl@users.noreply.github.com>

--- src/test/java/laddergame/domain/PlayerTest.java
+++ src/test/java/laddergame/domain/PlayerTest.java
@@ -1,6 +1,7 @@
 package laddergame.domain;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,15 +10,25 @@ import org.junit.jupiter.api.Test;
 public class PlayerTest {
     @Test
     @DisplayName("플레이어 이름이 제대로 생성되었는지 테스트한다.")
-    void playerNameCreateTest() {
+    void nameCreateTest() {
         //given
-        String actualName = "name";
+        final String actualName = "name";
 
         //when
-        Player player = new Player(actualName);
-        String expectName = player.getName();
+        final Player player = new Player(actualName);
+        final String expectName = player.getName();
 
         //then
         assertEquals(actualName, expectName);
+    }
+
+    @Test
+    @DisplayName("플레이어 이름이 5자 초과일 경우 예외가 발생한다.")
+    public void nameExceedMaxLengthException() {
+        //given
+        final String actualName = "nameTest";
+
+        //when&then
+        assertThrows(IllegalArgumentException.class, () -> {new Player(actualName);});
     }
 }
