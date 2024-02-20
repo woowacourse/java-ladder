aa4c203d706d201f5dd89a12313601d89f44d887
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-02-20 16:46:04 +0900

    feat(Line) : 연속으로 True를 가질 수 없다 (Test Passes)

--- src/main/java/ladderGame/model/Line.java
+++ src/main/java/ladderGame/model/Line.java
@@ -2,13 +2,28 @@ package ladderGame.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Line {
     private final List<Boolean> isDrawn;
     public Line(int number) {
         isDrawn = new ArrayList<>();
-        for(int i = 0; i < number; i++) {
-            isDrawn.add(false);
+        draw(number);
+    }
+
+    private void draw(int number) {
+        isDrawn.add(new Random().nextBoolean());
+        for(int i = 1; i < number-1; i++) {
+            if(isDrawn.get(i - 1)) {
+                isDrawn.add(false);
+                continue;
+            }
+            isDrawn.add(new Random().nextBoolean());
         }
     }
+
+    public List<Boolean> getIsDrawn() {
+        return new ArrayList<>(isDrawn);
+    }
+
 }
