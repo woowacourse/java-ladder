46cbea1d1b79c3570550d526740f61abb4359b22
nayonsoso <panda0329@naver.com>
2024-02-20 19:13:07 +0900

    feat: 0 입력 시 EMPTY 반환 기능 구현
    - green 단계

--- src/main/java/HorizonLineGenerator.java
+++ src/main/java/HorizonLineGenerator.java
@@ -1,6 +1,9 @@
 public class HorizonLineGenerator {
 
     public HorizonLine generate(int i) {
+        if (i == 0) {
+            return HorizonLine.EMPTY;
+        }
         return null;
     }
 }
