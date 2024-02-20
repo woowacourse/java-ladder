106682798727c47fe69b5d31e555429ec8d69903
DobbyMac <kimdobby@g.skku.edu>
2024-02-20 18:03:27 +0900

    feat(LadderPiece): LadderPiece 클래스가 오른쪽 방향을 가지고 있는지 여부를 반환하는 기능 구현

--- src/main/java/LadderLeg.java
+++ src/main/java/LadderLeg.java
@@ -3,8 +3,12 @@ import java.util.List;
 public class LadderLeg {
     private final List<LadderPiece> ladderPieces;
 
-    LadderLeg(final List<LadderPiece> ladderPieces){
+    LadderLeg(final List<LadderPiece> ladderPieces) {
         this.ladderPieces = ladderPieces;
     }
 
+    public boolean hasRightDirectionAtIndex(int index) {
+        return ladderPieces.get(index)
+                           .isRightDirection();
+    }
 }
--- src/test/java/LadderLegTest.java
+++ src/test/java/LadderLegTest.java
@@ -8,15 +8,30 @@ import static org.junit.jupiter.api.Assertions.*;
 public class LadderLegTest {
     @Test
     @DisplayName("사다리 조각을 통해 사다리 다리를 만든다.")
-    public void createLadderLeg(){
-        List<Direction> directions = List.of(Direction.LEFT,Direction.RIGHT);
+    public void createLadderLeg() {
+        List<Direction> directions = List.of(Direction.LEFT, Direction.RIGHT);
         List<LadderPiece> ladderPieces = 사다리_조각들_생성(directions);
 
         LadderLeg ladderLeg = new LadderLeg(ladderPieces);
 
-        assertInstanceOf(LadderLeg.class,ladderLeg);
+        assertInstanceOf(LadderLeg.class, ladderLeg);
     }
-    private List<LadderPiece> 사다리_조각들_생성(List<Direction> directions){
-        return directions.stream().map(LadderPiece::new).toList();
+
+    @Test
+    @DisplayName("가지고 있는 특정 사다리 조각의 방향이 오른쪽인지 확인한다.")
+    public void checkLadderPieceDirection() {
+        List<Direction> directions = List.of(Direction.RIGHT, Direction.LEFT);
+        List<LadderPiece> ladderPieces = 사다리_조각들_생성(directions);
+
+        LadderLeg ladderLeg = new LadderLeg(ladderPieces);
+
+        assertTrue(ladderLeg.hasRightDirectionAtIndex(0));
+        assertFalse(ladderLeg.hasRightDirectionAtIndex(1));
+    }
+
+    private List<LadderPiece> 사다리_조각들_생성(List<Direction> directions) {
+        return directions.stream()
+                         .map(LadderPiece::new)
+                         .toList();
     }
 }
