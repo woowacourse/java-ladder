4d607984e380dcd5b69b8ca6027c91afd557a7bc
hangillee <skfcb10@naver.com>
2024-02-20 14:13:39 +0900

    test: 이름 길이 검증 테스트 작성

---
+++ src/test/java/NameTest.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class NameTest {
+    @Test
+    @DisplayName("사람 이름은 5글자를 넘을 수 없다.")
+    void isNameOverMaxLengthLimit() {
+        String name = "pobipobi";
+        assertThrows(IllegalArgumentException.class, () -> new Name(name));
+    }
+}
