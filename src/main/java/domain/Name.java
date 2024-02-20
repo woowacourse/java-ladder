d91261c65a81107f85ee3272e9ecab7b6420ca99
hyxrxn <gpfls981220@gmail.com>
2024-02-20 16:56:24 +0900

    feat: Name 클래스의 이름 존재 검증
    Co-authored-by: shin-jisong <songjin1127@naver.com>

--- src/main/java/domain/Name.java
+++ src/main/java/domain/Name.java
@@ -5,10 +5,15 @@ public class Name {
     String name;
 
     public Name(String name) {
+
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("[ERROR] 이름이 없습니다.");
+        }
         if (name.length() > 5) {
             throw new IllegalArgumentException("[ERROR] 이름의 길이는 5글자를 초과할 수 없습니다.");
         }
-    }
 
+        this.name = name;
+    }
 
 }
