f35800520f4a70ba408578037c674d9f42b02072
eunhee <dmsgml9903@gmail.com>
2024-02-20 14:24:57 +0900

    feat(Ladder): 최대 사다리의 높이는 양수가 되어야 한다.

--- src/main/java/Ladder.java
+++ src/main/java/Ladder.java
@@ -3,7 +3,14 @@ public class Ladder {
     private final int height;
 
     public Ladder(int height) {
+        validateHeightIsPositive(height);
         this.height = height;
+    }
+
+    private void validateHeightIsPositive(int height) {
+        if (height < 0) {
+            throw new IllegalArgumentException("최대 사다리의 높이는 양수가 되어야 합니다");
+        }
     }
 
     public int height() {
