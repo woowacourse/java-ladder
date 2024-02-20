319b08bd3042f310e61d3deb1b38c116381ee25c
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-02-20 14:37:22 +0900

    test(PlayerTest) : Name을 인자로 받아서 객체를 생성할 수 있다 (Test Fails)

---
+++ src/test/java/ladderGame/model/PlayerTest.java
@@ -0,0 +1,17 @@
+package ladderGame.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    @Test
+    @DisplayName("Name을 인자로 받아서 객체를 생성할 수 있다.")
+    void createPlayer() {
+        Name name = new Name("이름");
+        assertThatCode(() -> new Player(name));
+    }
+}
