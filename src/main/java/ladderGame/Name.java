72ccd215a504c60d4ae0f8177a3f9a67ff3a2142
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-02-20 14:28:59 +0900

    feat(Name) : 이름은 최대 5글자까지 가능하다 (Test Passes)

--- src/main/java/ladderGame/Name.java
+++ src/main/java/ladderGame/Name.java
@@ -4,6 +4,13 @@ public class Name {
     private final String name;
 
     public Name(String name) {
+        validateNameLength(name);
         this.name = name;
+    }
+
+    private void validateNameLength(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("이름은 최대 5글자까지 가능합니다.");
+        }
     }
 }
