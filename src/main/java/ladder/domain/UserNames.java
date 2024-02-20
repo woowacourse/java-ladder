f7be44b922477e3aabc78798f884d3efa5218ffd
kelly6bf <kelly6bf@gmail.com>
2024-02-20 17:01:58 +0900

    refactor: 사용자 수 7명 초과 시 예외 발생시키는 코드 메소드로 분리

--- src/main/java/ladder/domain/UserNames.java
+++ src/main/java/ladder/domain/UserNames.java
@@ -4,6 +4,10 @@ import java.util.List;
 
 public class UserNames {
     public UserNames(List<UserName> userNames) {
+        validateSize(userNames);
+    }
+
+    private void validateSize(final List<UserName> userNames) {
         if (userNames.size() > 7) {
             throw new IllegalArgumentException("사용자는 7명 이하여야 합니다");
         }
