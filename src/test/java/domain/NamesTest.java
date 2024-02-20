c91014b7c555b6f05a17063ea2e494e21dab89af
J-I-H-O <jeongjiho0731@gmail.com>
2024-02-20 18:32:58 +0900

    test: 이름 중복 검증 테스트
    Co-authored-by: J-I-H-O <jeongjiho0731@gmail.com>
    Co-authored-by: Nam Gi Beom <rlqja0523@naver.com>

---
+++ src/test/java/domain/NamesTest.java
@@ -0,0 +1,34 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class NamesTest {
+    @Test
+    void 중복된_이름이_존재하면_예외가_발생한다() {
+        // given
+        final Name duplicateName1 = new Name("prin");
+        final Name duplicateName2 = new Name("prin");
+        final Name uniqueName = new Name("ddang");
+        final List<Name> names = List.of(duplicateName1, duplicateName2, uniqueName);
+
+        // when & then
+        assertThatThrownBy(() -> new Names(names))
+                .isExactlyInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 중복된_이름이_없으면_예외가_발생하지_않는다() {
+        // given
+        final Name uniqueName1 = new Name("prin");
+        final Name uniqueName2 = new Name("ddang");
+        final Name uniqueName3 = new Name("pobi");
+        final List<Name> names = List.of(uniqueName1, uniqueName2, uniqueName3);
+
+        // when & then
+        assertDoesNotThrow(() -> new Names(names));
+    }
+}
