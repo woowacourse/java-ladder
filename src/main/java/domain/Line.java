37dadaf4285ff1c4a3ea0374156dcff2da790271
leetaehoon <xogns1514@gmail.com>
2024-02-20 15:26:54 +0900

    feat(Line): 라인을 생성한다.

--- src/main/java/domain/Line.java
+++ src/main/java/domain/Line.java
@@ -1,6 +1,16 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Line {
 
+    List<Boolean> points;
 
+    public Line() {
+    }
+
+    public Line(int personCount) {
+        points = new ArrayList<>(personCount - 1);
+    }
 }
--- src/test/java/LineTest.java
+++ src/test/java/LineTest.java
@@ -3,12 +3,25 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class LineTest {
 
     @Test
     @DisplayName("라인을 생성한다.")
     void createLine() {
-        Assertions.assertThatCode(() -> new Line()).doesNotThrowAnyException();
+        assertThatCode(() -> new Line()).doesNotThrowAnyException();
     }
+    
+    @Test
+    @DisplayName("가로 길이는 사용자 수 -1 이다.")
+    void methodName() throws Exception {
+        //given
+        int personCount = 4;
+        //when
+        //then
+        assertThatCode(() -> new Line(personCount)).doesNotThrowAnyException();
+    }
+    
 
 }
