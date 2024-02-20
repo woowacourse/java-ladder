21ceed33b8ef00b9e77cc58f62ea3a3a6b7438f1
KIMGYUTAE <kiimwf0819@gmail.com>
2024-02-20 17:34:01 +0900

    feat: Ladder의 Line 수 검증 기능 구현

---
+++ src/main/java/ladder/domain/Ladder.java
@@ -0,0 +1,21 @@
+package ladder.domain;
+
+import java.util.List;
+
+public class Ladder {
+
+    private final List<Line> lines;
+
+    public Ladder(List<Line> lines) {
+        validateLinesSize(lines);
+        this.lines = lines;
+    }
+
+    private void validateLinesSize(List<Line> lines) {
+        if (lines.size() > 50)  {
+            throw new IllegalArgumentException("[ERROR] 사다리의 최대 높이는 50이하만 가능합니다.");
+        }
+    }
+
+
+}
---
+++ src/test/java/ladder/domain/LadderTest.java
@@ -0,0 +1,28 @@
+package ladder.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LadderTest {
+
+    @DisplayName("사다리의 라인의 개수가 50개를 초과할 경우 예외가 발생한다.")
+    @Test
+    void newLadderTestByLineCount() {
+        //given
+        int lineCount = 51;
+        List<Line> lines = new ArrayList<>();
+        for (int i = 0; i < lineCount; i++) {
+             lines.add(new Line());
+        }
+
+        //when, then
+        assertThatThrownBy(() -> new Ladder(lines))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 사다리의 최대 높이는 50이하만 가능합니다.");
+    }
+
+}
