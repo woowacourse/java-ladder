c88da7f2fa3a2a92a0c7d85f133b210be967f854
kelly6bf <kelly6bf@gmail.com>
2024-02-20 16:17:25 +0900

    test: 입력된 사용자 이름에 유효하지 않은 특수문자 검증 테스트 작성

--- src/test/java/ladder/domain/UserNameTest.java
+++ src/test/java/ladder/domain/UserNameTest.java
@@ -1,7 +1,12 @@
 package ladder.domain;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
@@ -12,5 +17,14 @@ public class UserNameTest {
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> new UserName("abcdef"))
                 .withMessage("이름의 길이는 5 이하여야 합니다.");
+    }
+
+    @DisplayName("유효하지 않은 특수문자가 포함된 이름이 입력되면 예외가 발생한다.")
+    @ValueSource(strings = {"kel!", "liv#", "liv,k"})
+    @ParameterizedTest()
+    void createNameByInvalidCharacter(final String name) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new UserName(name))
+                .withMessage("이름에는 한글, 영문, 숫자, `-`, `_`, `&`만 포함될 수 있습니다.");
     }
 }
