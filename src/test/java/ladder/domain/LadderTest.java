32e308c263a7b1e55598955d8fdbaba599daaf60
JUHA <84626225+khabh@users.noreply.github.com>
2024-02-20 17:48:38 +0900

    test: Ladder 생성 테스트

---
+++ src/test/java/ladder/domain/LadderTest.java
@@ -0,0 +1,23 @@
+package ladder.domain;
+
+import ladder.domain.generator.LadderStepGenerator;
+import ladder.domain.generator.RandomLadderStepGenerator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+
+public class LadderTest {
+    @Test
+    @DisplayName("주어진 높이만큼 사다리를 생성한다.")
+    void createLadderTest() {
+        // given
+        int stepWidth = 3;
+        int stepCount = 4;
+        LadderStepGenerator ladderStepGenerator = new RandomLadderStepGenerator(stepWidth);
+
+        // when & then
+        assertThatCode(() -> new Ladder(ladderStepGenerator, stepCount))
+                .doesNotThrowAnyException();
+    }
+}
