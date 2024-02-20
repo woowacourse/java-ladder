e960f1bd8bbb5266f8e2baff597977a0447d0b04
ay-eonii <naayen@naver.com>
2024-02-20 17:09:58 +0900

    feat(Names): 이름 글자 수 검증

--- src/main/java/ladder/domain/Names.java
+++ src/main/java/ladder/domain/Names.java
@@ -1,14 +1,29 @@
 package ladder.domain;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Names {
+
+    private static final int MAX_NAME_LENGTH = 5;
 
     private final List<String> names;
 
     public Names(String rawNames) {
         String[] names = rawNames.split(",");
+        validate(names);
         this.names = new ArrayList<>(List.of(names));
     }
+
+    private static void validate(String[] names) {
+        long count = Arrays.stream(names)
+                .filter(name -> name.length() > MAX_NAME_LENGTH)
+                .count();
+
+        if (count > 0) {
+            throw new IllegalArgumentException("이름은 최대 5글자까지 부여할 수 있습니다.");
+        }
+    }
+
 }
