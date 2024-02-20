21acc840f56a73045112ef1fce4d9e16a7b869c7
김민수 MINSOO KIM <alstn113@gmail.com>
2024-02-20 17:07:45 +0900

    feat(Ladder): 참여자 수와 높이를 가지는 사다리 구현

--- src/main/java/ladder/domain/ladder/Ladder.java
+++ src/main/java/ladder/domain/ladder/Ladder.java
@@ -1,5 +1,15 @@
 package ladder.domain.ladder;
 
 public class Ladder {
+    private final int playerCount;
+    private final Height height;
 
+    public Ladder(int playerCount, int height) {
+        this.playerCount = playerCount;
+        this.height = new Height(height);
+    }
+
+    public int getHeight() {
+        return height.getHeight();
+    }
 }
--- src/test/java/ladder/domain/ladder/LadderTest.java
+++ src/test/java/ladder/domain/ladder/LadderTest.java
@@ -1,10 +1,22 @@
 package ladder.domain.ladder;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LadderTest {
 
     @Test
-    @DisplayName("사다리는 가로 라인들을 가진다.")
+    @DisplayName("사다리는 참여자 수와 높이를 가진다.")
+    void testConstruct() {
+        int playerCount = 4;
+        int height = 5;
+        Ladder ladder = new Ladder(playerCount, height);
+
+        assertThat(ladder).extracting("playerCount")
+                .isEqualTo(4);
+        assertThat(ladder).extracting("height")
+                .isEqualTo(5);
+    }
 }
