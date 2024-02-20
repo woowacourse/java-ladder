74785cdc0090a06f633cf4289d5fd41cea9feacb
hoyeonyy <highoption@naver.com>
2024-02-20 16:43:47 +0900

    feat (Height) : 사다리 높이 검증 (Test Pass)

--- src/main/java/ladder/domain/Height.java
+++ src/main/java/ladder/domain/Height.java
@@ -3,5 +3,8 @@ package ladder.domain;
 public class Height {
 
     public Height(int value) {
+        if (value < 1) {
+            throw new IllegalArgumentException("사다리 높이는 1 이상이어야 합니다.");
+        }
     }
 }
