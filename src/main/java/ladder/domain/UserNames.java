005445660065720e4cbe2ca9edb9f1c2ff55b414
kelly6bf <kelly6bf@gmail.com>
2024-02-20 17:00:47 +0900

    feat: 사용자 수 7명 초과시 예외 발생 기능 구현

--- src/main/java/ladder/domain/UserNames.java
+++ src/main/java/ladder/domain/UserNames.java
@@ -3,6 +3,9 @@ package ladder.domain;
 import java.util.List;
 
 public class UserNames {
-    public UserNames(List<UserName> names) {
+    public UserNames(List<UserName> userNames) {
+        if (userNames.size() > 7) {
+            throw new IllegalArgumentException("사용자는 7명 이하여야 합니다");
+        }
     }
 }
