f53eff45a3622f7f25889ed2d527fba3049163e5
Arachneee <jhg2819@naver.com>
2024-02-20 18:03:52 +0900

    feat: 사다리 높이 양수 아닐시 예외처리

---
+++ src/main/java/laddergame/LadderHeight.java
@@ -0,0 +1,20 @@
+package laddergame;
+
+public class LadderHeight {
+    private final int value;
+
+    public LadderHeight(final int value) {
+        validatePositive(value);
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    private void validatePositive(final int value) {
+        if (value <= 0) {
+            throw new IllegalArgumentException("[ERROR] 입력값은 양수만 가능합니다.");
+        }
+    }
+}
---
+++ src/test/java/laddergame/LadderHeightTest.java
@@ -0,0 +1,30 @@
+package laddergame;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class LadderHeightTest {
+
+    @DisplayName("사다리 높이가 양수가 아닐시 예외를 발생시킨다.")
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0})
+    void notPositive(int value) {
+        Assertions.assertThatThrownBy(() -> new LadderHeight(value))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 입력값은 양수만 가능합니다.");
+    }
+
+    @DisplayName("사다리 높이가 양수면 생성된다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 10, 100})
+    void create(int value) {
+        // given & when
+        final LadderHeight ladderHeight = new LadderHeight(value);
+
+        // then
+        Assertions.assertThat(ladderHeight.getValue()).isEqualTo(value);
+
+    }
+}
