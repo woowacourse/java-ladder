19b913dd9a723d3a4be142c32c4ac10f0b08fd65
kelly6bf <kelly6bf@gmail.com>
2024-02-20 16:52:51 +0900

    refactor: 사다리 크기 검증 로직 메서드 분리

--- src/main/java/ladder/domain/LadderHeight.java
+++ src/main/java/ladder/domain/LadderHeight.java
@@ -2,6 +2,10 @@ package ladder.domain;
 
 public class LadderHeight {
     public LadderHeight(final int ladderHeight) {
+        validateRange(ladderHeight);
+    }
+
+    private void validateRange(final int ladderHeight) {
         if (ladderHeight < 2 || ladderHeight > 10) {
             throw new IllegalArgumentException("사다리 크기는 2 ~ 10 범위만 가능합니다.");
         }
