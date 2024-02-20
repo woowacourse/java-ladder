8d0d6ebd7724052010604c20fefad2013a51d911
DobbyMac <kimdobby@g.skku.edu>
2024-02-20 16:16:05 +0900

    refactor(Names): Names 일급컬렉션 생성 시 2 명이상 입력되었는지 검증 기능 구현

--- src/main/java/Names.java
+++ src/main/java/Names.java
@@ -7,13 +7,13 @@ import java.util.stream.Collectors;
 public class Names {
     private final List<Name> value;
 
-    public Names(List<String> value){
-        validate(value);
-        this.value = from(value);
+    public Names(List<String> names) {
+        validate(names);
+        this.names = from(names);
     }
 
-    private List<Name> from(List<String> value){
-        return value.stream()
+    private List<Name> from(List<String> names) {
+        return names.stream()
                     .map(Name::new)
                     .toList();
     }
@@ -22,14 +22,14 @@ public class Names {
         validateDuplicated(value);
     }
 
-    private void validateDuplicated(List<String> value) {
-        Set<String> duplicateSize = new HashSet<>(value);
-        if (duplicateSize.size() != value.size()) {
+    private void validateDuplicated(List<String> names) {
+        Set<String> duplicateSize = new HashSet<>(names);
+        if (duplicateSize.size() != names.size()) {
             throw new IllegalArgumentException("중복된 이름이 포함되어 있습니다.");
         }
     }
 
     public List<Name> getValue() {
-        return value;
+        return names;
     }
 }
--- src/test/java/NamesTest.java
+++ src/test/java/NamesTest.java
@@ -8,25 +8,32 @@ import static org.junit.jupiter.api.Assertions.*;
 class NamesTest {
     @Test
     @DisplayName("이름 목록을 포함한 일급 컬렉션을 만든다.")
-    public void createNames(){
+    public void createNames() {
         //Given
-        List<String> value = List.of("도비","조이썬");
+        List<String> value = List.of("도비", "조이썬");
 
         //When
         Names names = new Names(value);
 
         //Then
-        assertInstanceOf(Names.class,names);
+        assertInstanceOf(Names.class, names);
     }
 
     @Test
     @DisplayName("중복된 플레이어의 이름이 포함된 목록은 예외를 발생한다.")
-    public void throwExceptionWhenDuplicatedNameInput(){
+    public void throwExceptionWhenDuplicatedNameInput() {
 
         List<String> value = List.of("도비", "도비", "조이썬");
 
-        assertThrows(IllegalArgumentException.class, () ->{
+        assertThrows(IllegalArgumentException.class, () -> {
             new Names(value);
         });
+    }
+
+    @Test
+    @DisplayName("2명 미만의 이름이 포함된 목록은 예외를 발생한다.")
+    public void throwExceptionWhenNameInputLessThanTwo() {
+        List<String> value = List.of("도비");
+        assertThrows(IllegalArgumentException.class, () -> new Names(value));
     }
 }
