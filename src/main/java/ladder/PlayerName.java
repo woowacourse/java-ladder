d943e316e845c4b7392d26a34813668220871e68
hoyeonyy <highoption@naver.com>
2024-02-20 16:02:04 +0900

    refactor (PlayerName) : 이름에 공백이나 null 이 오면 예외발생 (refactor 단계)

--- src/main/java/ladder/PlayerName.java
+++ src/main/java/ladder/PlayerName.java
@@ -6,10 +6,18 @@ public class PlayerName {
         validate(name);
     }
 
-    private static void validate(String name) {
+    private void validate(String name) {
+        validateNotEmpty(name);
+        validateMaxLength(name);
+    }
+
+    private void validateNotEmpty(String name) {
         if (name == null || name.isBlank()) {
             throw new IllegalArgumentException("이름은 한글자 이상이어야 합니다.");
         }
+    }
+
+    private void validateMaxLength(String name) {
         if (name.length() > 5) {
             throw new IllegalArgumentException("이름은 5글자를 넘을 수 없습니다.");
         }
