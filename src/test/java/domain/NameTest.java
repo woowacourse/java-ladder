2bc90c7a75bb1d1670d90bc9d4d6151722ad9a01
jhon3242 <rldotnr2@gmail.com>
2024-02-20 14:11:56 +0900

    test(red) : Name 생성 테스트 작성
    Co-authored-by: zangsu <zangsu_@naver.com>

---
+++ src/test/java/domain/NameTest.java
@@ -0,0 +1,31 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class NameTest {
+
+    @Test
+    void constructSuccessTest(){
+        assertThatNoException()
+                .isThrownBy(() -> new Name("takan"));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    void constructFailWithBlankName(String name) {
+        assertThatThrownBy(() -> new Name(name))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void constructFailBecauseNameLength(){
+        //TODO : 테스트 이름 변경
+        assertThatThrownBy(() -> new Name("zangsu"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
