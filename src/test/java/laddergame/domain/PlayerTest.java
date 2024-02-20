f6121248b3039a0c48316fe46f84115553df9aad
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-02-20 14:50:20 +0900

    test: 플레이어 이름 공백 예외 테스트
    Co-Authored-By: lemone <55480011+jihyeonl@users.noreply.github.com>

--- src/test/java/laddergame/domain/PlayerTest.java
+++ src/test/java/laddergame/domain/PlayerTest.java
@@ -5,12 +5,14 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("플레이어")
 public class PlayerTest {
     @Test
     @DisplayName("플레이어 이름이 제대로 생성되었는지 테스트한다.")
-    void nameCreateTest() {
+    public void nameCreateTest() {
         //given
         final String actualName = "name";
 
@@ -28,9 +30,17 @@ public class PlayerTest {
         //given
         final String actualName = "nameTest";
 
-        //when&then
+        //when & then
         assertThrows(IllegalArgumentException.class, () -> {
             new Player(actualName);
         });
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "  "})
+    @DisplayName("플레이어 이름이 공백일 경우 예외가 발생한다.")
+    public void nameBlankException(final String name) {
+        //given & when & then
+        assertThrows(IllegalArgumentException.class, () -> new Player(name));
     }
 }
