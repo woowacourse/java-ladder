7815cdf1e3a30c78f1b22318f6d366f804d39c16
hangillee <skfcb10@naver.com>
2024-02-20 17:38:29 +0900

    refactor: 라인 모양을 출력하기 위한 문자열을 관리하는 열거형 구현

---
+++ src/main/java/view/ResultMessage.java
@@ -0,0 +1,35 @@
+package view;
+
+import domain.Line;
+import domain.Names;
+
+public enum ResultMessage {
+    MOVABLE_LINE("-----|"),
+    UNMOVABLE_LINE("     |");
+
+    private final String message;
+
+    ResultMessage(String message) {
+        this.message = message;
+    }
+
+    public static String of(Line line) {
+        StringBuilder result = new StringBuilder();
+        for (Boolean point : line.getPoints()) {
+            result.append(currentPoint(point));
+        }
+        result.append("\n");
+        return result.toString();
+    }
+
+    private static String currentPoint(final boolean point) {
+        if (point) {
+            return ResultMessage.MOVABLE_LINE.message;
+        }
+        return ResultMessage.UNMOVABLE_LINE.message;
+    }
+
+    public static String ladderPadding(final Names names) {
+        return " ".repeat(names.getFirstName().getLength()) + "|";
+    }
+}
