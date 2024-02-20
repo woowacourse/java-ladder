8c86f9b475303b8418f3b564a7926d3ddfaf67e9
devhoya97 <diajbh123@gmail.com>
2024-02-20 15:07:52 +0900

    feat : 가로 라인 생성 기능

--- src/main/java/domain/Line.java
+++ src/main/java/domain/Line.java
@@ -1,13 +1,16 @@
 package domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Line {
 
-    List<Boolean> points;
+    List<Boolean> points = new ArrayList<>();
 
     public Line(final int size) {
-
+        for (int i = 0; i < size; i++) {
+            points.add(false);
+        }
     }
 
     public List<Boolean> getPoints() {
