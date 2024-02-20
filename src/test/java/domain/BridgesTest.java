4e3c316575e37bef7b21ed333a5919b879f9a6b0
jhon3242 <rldotnr2@gmail.com>
2024-02-20 17:49:32 +0900

    test(red) : Bridge 조회 테스트 추가
    Co-authored-by: zangsu <zangsu_@naver.com>

--- src/test/java/domain/BridgesTest.java
+++ src/test/java/domain/BridgesTest.java
@@ -4,9 +4,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.List;
+import java.util.stream.Stream;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class BridgesTest {
@@ -40,5 +45,21 @@ public class BridgesTest {
 
         //then
         assertThat(bridges.getBridges()).hasSize(length);
+    }
+
+    @MethodSource("provier")
+    @ParameterizedTest
+    void getBridgeTest(List<Boolean> expectedBridges) {
+        Bridges bridges = new Bridges(4);
+
+        Assertions.assertThat(bridges.getBridges())
+                .isEqualTo(expectedBridges);
+    }
+
+
+    static Stream<Arguments> provier() {
+        return Stream.of(
+                Arguments.of(List.of(true, false, false, true))
+        );
     }
 }
