8f7923f5b64d7c94f15cc41565bd2f804160d5d8
hoyeonyy <highoption@naver.com>
2024-02-20 15:24:41 +0900

    refactor (PlayerName) : 5글자를 넘을 때 에러 발생 (Refactor 단계)

--- src/main/java/ladder/PlayerName.java
+++ src/main/java/ladder/PlayerName.java
@@ -3,6 +3,10 @@ package ladder;
 public class PlayerName {
 
     public PlayerName(String name) {
+        validate(name);
+    }
+
+    private static void validate(String name) {
         if (name.length() > 5) {
            throw new IllegalArgumentException("이름은 5글자를 넘을 수 없습니다");
         }
