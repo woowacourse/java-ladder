289f310dcddcdc509e323f4f172bd78cf89090f9
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-02-20 18:39:42 +0900

    feat: 랜덤으로 boolean값을 반환하는 인터페이스 추가
    Co-Authored-By: lemone <55480011+jihyeonl@users.noreply.github.com>

---
+++ src/main/java/laddergame/util/NumberGenerator.java
@@ -0,0 +1,5 @@
+package laddergame.util;
+
+public interface NumberGenerator {
+    Integer generate();
+}
--- src/main/java/laddergame/util/RandomBooleanGenerator.java
+++ src/main/java/laddergame/util/RandomBooleanGenerator.java
@@ -2,7 +2,7 @@ package laddergame.util;
 
 import java.util.Random;
 
-public class RandomBooleanGenerator {
+public class RandomBooleanGenerator implements NumberGenerator {
     private static final RandomBooleanGenerator generator = new RandomBooleanGenerator();
     private final Random random;
 
@@ -10,6 +10,7 @@ public class RandomBooleanGenerator {
         this.random = new Random();
     }
 
+    @Override
     public Integer generate() {
         return random.nextInt(2);
     }
