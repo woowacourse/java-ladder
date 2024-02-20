2276c9e809e1a0e069401992142a7e6b0693f4d9
KIMGYUTAE <kiimwf0819@gmail.com>
2024-02-20 16:51:40 +0900

    refactor: 상수 하드코딩

--- src/main/java/ladder/domain/User.java
+++ src/main/java/ladder/domain/User.java
@@ -4,6 +4,10 @@ import java.util.regex.Pattern;
 
 public class User {
 
+    private final String ALPHABET_FORMAT = "^[a-z A-Z]+$";
+    private final String BLANK = " ";
+    private final int MAX_NAME_LENGTH = 5;
+
     private final String userName;
 
     public User(String userName) {
@@ -18,19 +22,19 @@ public class User {
     }
 
     private void validateNameLength(String name) {
-        if (name.isEmpty() || name.length() > 5) {
+        if (name.isEmpty() || name.length() > MAX_NAME_LENGTH) {
             throw new IllegalArgumentException("[ERROR] 사용자 이름의 길이는 1~5글자여야 합니다.");
         }
     }
 
     private void validateNameEngFormat(String name) {
-        if (!Pattern.matches("^[a-z A-Z]+$", name)) {
+        if (!Pattern.matches(ALPHABET_FORMAT, name)) {
             throw new IllegalArgumentException("[ERROR] 사용자 이름은 영문 대소문자만 허용합니다.");
         }
     }
 
     private void validateBlankInName(String name) {
-        if (name.contains(" ")) {
+        if (name.contains(BLANK)) {
             throw new IllegalArgumentException("[ERROR] 사용자 이름 내에는 공백을 허용하지 않습니다.");
         }
     }
