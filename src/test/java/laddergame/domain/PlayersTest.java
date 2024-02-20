a5faa445af241097e8e7d454824d1888a1bdf248
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-02-20 15:04:52 +0900

    test: 플레이어 이름 중복 예외 테스트
    Co-Authored-By: lemone <55480011+jihyeonl@users.noreply.github.com>

---
+++ src/test/java/laddergame/domain/PlayersTest.java
@@ -0,0 +1,21 @@
+package laddergame.domain;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("플레이어들")
+public class PlayersTest {
+    @Test
+    @DisplayName("플레이어 이름에 중복이 있을 경우 예외가 발생한다.")
+    public void nameDuplicateException() {
+        //given
+        final String name = "name";
+
+        // when & then
+        assertThrows(IllegalArgumentException.class, () -> new Players(List.of(name, name)));
+
+    }
+}
