01e57ce0c4bacdeb3ea296b0a9650a9d92b24d07
hyxrxn <gpfls981220@gmail.com>
2024-02-20 17:52:20 +0900

    style: ParticipantsTest 클래스의 메서드 이름 수정
    Co-authored-by: shin-jisong <songjin1127@naver.com>

--- src/test/java/domain/ParticipantsTest.java
+++ src/test/java/domain/ParticipantsTest.java
@@ -18,7 +18,7 @@ class ParticipantsTest {
 
     @Test
     @DisplayName("참가자가 50명 초과면 예외가 발생한다.")
-    void moreThanTwoExceptionTest() {
+    void moreThanFiftyExceptionTest() {
         String[] names = {
                 "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
                 "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
