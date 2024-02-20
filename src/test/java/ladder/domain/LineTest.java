590966c0abf60e37c78eeeac6546e1d04ef739e3
kelly6bf <kelly6bf@gmail.com>
2024-02-20 22:26:33 +0900

    test: 같은층에 연속된 EXIST가 존재하지 않는걸 검증하는 테스트 로직 작성

--- src/test/java/ladder/domain/LineTest.java
+++ src/test/java/ladder/domain/LineTest.java
@@ -3,6 +3,7 @@ package ladder.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static ladder.domain.StepStatus.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LineTest {
@@ -10,9 +11,18 @@ public class LineTest {
     @DisplayName("인원수 -1 개수 만큼의 포인트를 가진 Line 인스턴스를 생성한다.")
     @Test
     void createLine() {
-        final Line line = new Line(4);
+        final Line line = new Line(() -> true, 4);
 
-        assertThat(line.getPoints().size())
+        assertThat(line.getStepStatuses().size())
                 .isEqualTo(4 - 1);
+    }
+
+    @DisplayName("같은층에 연속된 EXIST가 존재하지 않는다.")
+    @Test
+    void notExistBetweenNextAndCurrent() {
+        final Line line = new Line(() -> true, 4);
+
+        assertThat(line.getStepStatuses().get(1))
+                .isEqualTo(NONE);
     }
 }
