c2e129d57e39cd6caf84253604df39ab4475e649
eunhee <dmsgml9903@gmail.com>
2024-02-20 18:13:12 +0900

    test(LadderTest): 사다리의 행 내부의 라인은 랜덤하게 결정한다.

--- src/test/java/LadderTest.java
+++ src/test/java/LadderTest.java
@@ -1,8 +1,9 @@
-
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 public class LadderTest {
@@ -11,7 +12,7 @@ public class LadderTest {
     @Test
     void ladderHeight() {
         int maximumHeight = 5;
-        Ladder ladder = new Ladder(maximumHeight);
+        Ladder ladder = new Ladder(maximumHeight, List.of());
         assertThat(ladder.height()).isEqualTo(maximumHeight);
     }
 
@@ -19,8 +20,17 @@ public class LadderTest {
     @Test
     void ladderHeightIsPositive() {
         int maximumHeight = -1;
-        assertThatThrownBy(() -> new Ladder(maximumHeight))
+        assertThatThrownBy(() -> new Ladder(maximumHeight, List.of()))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("최대 사다리의 높이는 양수가 되어야 합니다");
     }
+
+    @DisplayName("사다리의 행 내부의 라인은 랜덤하게 결정한다.")
+    @RepeatedTest(100)
+    void makeLines() {
+        Ladder ladder = new Ladder(1, List.of(new Line(5)));
+        ladder.makeLine();
+        assertThat(ladder.getLines().get(0).size()).isEqualTo(4);
+    }
+
 }
