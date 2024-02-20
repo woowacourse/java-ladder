f61fab1956f2145ddf3bf5b88be90fb71d6dafd2
HyunJoong Kim <hjk0761@naver.com>
2024-02-20 21:03:38 +0900

    feat: 라인의 상태 결정하는 기능 구현

--- src/main/java/domain/Line.java
+++ src/main/java/domain/Line.java
@@ -1,20 +1,28 @@
 package domain;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Line {
     private final List<Boolean> line;
 
-    public Line (int height) {
-        validateRange(height);
-        this.line = new ArrayList<>(Collections.nCopies(height, false));
+    public Line (List<Integer> numbers) {
+        validateRange(numbers.size());
+        this.line = numbers.stream()
+                .map(this::hasConnection)
+                .toList();
+    }
+
+    public boolean getValue(int index) {
+        return line.get(index);
     }
 
     private void validateRange(int height) {
         if (height < 1 || height > 50) {
             throw new IllegalArgumentException("숫자는 1 이상 50 이하여야 합니다.");
         }
+    }
+
+    private boolean hasConnection(int threshold) {
+        return threshold >= 5;
     }
 }
--- src/test/java/domain/LineTest.java
+++ src/test/java/domain/LineTest.java
@@ -2,17 +2,42 @@ package domain;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class LineTest {
 
     @DisplayName("입력받은 숫자가 1 이상 50 이하가 아닌 경우 예외를 발생한다.")
     @ParameterizedTest
-    @ValueSource(ints = {-1, 0, 51})
-    void heightRangeTest(int input) {
+    @MethodSource("heightRangeTestMethod")
+    void heightRangeTest(List<Integer> input) {
         Assertions.assertThatThrownBy(() -> new Line(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("숫자는 1 이상 50 이하여야 합니다.");
+    }
+
+    private static Stream<Arguments> heightRangeTestMethod() {
+        return Stream.of(
+                Arguments.arguments(List.of()),
+                Arguments.arguments(Collections.nCopies(51, 1))
+        );
+    }
+
+    @DisplayName("받은 Integer List 값에 따라 내부 상태를 결정한다.")
+    @Test
+    void stateDecisionTest() {
+        Line line = new Line(List.of(1,9,2,8,4,5));
+        Assertions.assertThat(
+                IntStream.rangeClosed(0,5)
+                .mapToObj(line::getValue)
+                .toList()
+        ).isEqualTo(List.of(false, true, false, true, false, true));
     }
 }
