e65380a17743e1fdd53b5821bc640a289858f754
jhon3242 <rldotnr2@gmail.com>
2024-02-20 16:47:04 +0900

    feat(green) : Ladder 필드 추가
    Co-authored-by: zangsu <zangsu_@naver.com>

--- src/main/java/domain/Ladder.java
+++ src/main/java/domain/Ladder.java
@@ -1,9 +1,17 @@
 package domain;
 
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class Ladder {
+
+    private final List<Bridges> bridges;
 
     public Ladder(int personCount, int height) {
         validate(personCount, height);
+        bridges = IntStream.range(0, personCount - 1)
+                .mapToObj((index) -> new Bridges(height))
+                .toList();
     }
 
     private void validate(int personCount, int height) {
@@ -14,5 +22,9 @@ public class Ladder {
             throw new IllegalArgumentException(); // TODO 예외 메시지
         }
 
+    }
+
+    public List<Bridges> getBridge() {
+        return bridges;
     }
 }
