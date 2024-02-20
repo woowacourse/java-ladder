c98c671facfd79f9b53381f454bf23c2158ce514
kelly6bf <kelly6bf@gmail.com>
2024-02-20 16:51:47 +0900

    feat: 사다리 크기 검증 로직 구현

--- src/main/java/ladder/domain/LadderHeight.java
+++ src/main/java/ladder/domain/LadderHeight.java
@@ -2,5 +2,8 @@ package ladder.domain;
 
 public class LadderHeight {
     public LadderHeight(final int ladderHeight) {
+        if (ladderHeight < 2 || ladderHeight > 10) {
+            throw new IllegalArgumentException("사다리 크기는 2 ~ 10 범위만 가능합니다.");
+        }
     }
 }
