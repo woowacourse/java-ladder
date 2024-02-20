25706842e62ee8f3705b090c487243f315f932ab
이혜린 <hyerin@ihyelin-ui-noteubug.local>
2024-02-20 15:51:44 +0900

    test: Name 클래스의 이름 길이 검증

---
+++ src/test/java/NameTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class NameTest {
+
+    @Test
+    @DisplayName("이름 길이가 5글자 초과일 때 예외가 발생한다.")
+    void longNameExceptionTest() {
+        assertThatThrownBy(() -> new Name("zangsu"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 이름의 길이는 5글자를 초과할 수 없습니다.");
+    }
+
+}
