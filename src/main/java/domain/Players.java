4e8557494e27671d05b3b72fbfbf7bdde79607f4
kunsanglee <gosmdochee2@gmail.com>
2024-02-20 18:59:22 +0900

    feat: Players 생성자 및 검증로직 구현

---
+++ src/main/java/domain/Players.java
@@ -0,0 +1,22 @@
+package domain;
+
+import java.util.List;
+
+public class Players {
+
+    private static final int MINIMUM_PLAYER_SIZE = 2;
+    private static final int MAXIMUM_PLAYER_SIZE = 10;
+
+    private final List<Player> players;
+
+    public Players(List<Player> players) {
+        validate(players);
+        this.players = players;
+    }
+
+    private void validate(List<Player> players) {
+        if (players.size() < MINIMUM_PLAYER_SIZE || players.size() > MAXIMUM_PLAYER_SIZE) {
+            throw new IllegalArgumentException(String.format("2명 이상, 10명 이하인 인원만 입력해주세요. 입력한 인원 : %d", players.size()));
+        }
+    }
+}
