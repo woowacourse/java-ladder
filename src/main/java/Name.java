28ceab6edbb5b3d891742d9c5f54deeeffbeb5e7
nayonsoso <panda0329@naver.com>
2024-02-20 14:22:16 +0900

    feat: 이름 길이 검증 테스트
    - green 단계

--- src/main/java/Name.java
+++ src/main/java/Name.java
@@ -2,6 +2,9 @@ public class Name {
     private final String name;
 
     public Name(String name) {
+        if (name.length() < 1 || name.length() > 5) {
+            throw new IllegalArgumentException();
+        }
         this.name = name;
     }
 }
