71862b79b66b964606874593dd6122b73b999802
hyxrxn <gpfls981220@gmail.com>
2024-02-20 17:28:41 +0900

    test: Participants 클래스의 참가자 수 검증
    Co-authored-by: shin-jisong <songjin1127@naver.com>

---
+++ src/main/java/domain/Person.java
@@ -0,0 +1,9 @@
+package domain;
+
+public class Person {
+    Name name;
+
+    public Person(String name) {
+        this.name = new Name(name);
+    }
+}
---
+++ src/test/java/domain/ParticipantsTest.java
@@ -0,0 +1,34 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class ParticipantsTest {
+
+    @Test
+    @DisplayName("참가자가 2명 미만이면 예외가 발생한다.")
+    void lessThanTwoExceptionTest() {
+        String[] names = {"a"};
+        assertThatThrownBy(() -> new Participants(names))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 참가자는 2명 이상이어야 합니다.");
+    }
+
+    @Test
+    @DisplayName("참가자가 50명 초과면 예외가 발생한다.")
+    void lessThanTwoExceptionTest() {
+        String[] names = {
+                "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+                "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+                "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+                "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y"};
+
+        assertThatThrownBy(() -> new Participants(names))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 참가자는 50명 이하여야 합니다.");
+    }
+}
