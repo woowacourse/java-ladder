c8dd1fbbbbf6a54d06bc07e8e711404abac1524f
hoyeonyy <highoption@naver.com>
2024-02-20 15:48:57 +0900

    test (PlayerName) : 이름에 공백이나 null 이 오면 예외발생 (TestFail 단계)

--- src/test/java/ladder/PlayerNameTest.java
+++ src/test/java/ladder/PlayerNameTest.java
@@ -5,16 +5,30 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class PlayerNameTest {
+    private static final String nullString = null;
 
     @DisplayName("이름은 5글자를 넘을 수 없다")
     @ParameterizedTest
     @CsvSource({"steve!", "123 45", "123456"})
-    void validateTest(String name) {
+    void validateTest_WhenLengthIsOver5(String name) {
 
         assertThatThrownBy(() -> new PlayerName(name))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("이름은 5글자를 넘을 수 없습니다");
+                .hasMessage("이름은 5글자를 넘을 수 없습니다.");
     }
+
+    @DisplayName("이름에 공백 또는 Null 이 오면 안된다.")
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = " ")
+    void validateTest_WhenNameIsEmpty(String name){
+        assertThatThrownBy(() -> new PlayerName(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이름은 한글자 이상이어야 합니다.");
+    }
+
 }
