c188d75a39fef9bcf938e4f8dd65ba719b432fb6
leetaehoon <xogns1514@gmail.com>
2024-02-20 15:51:51 +0900

    feat(Line): 무작위 가로 라인을 생성한다.

--- src/main/java/domain/Line.java
+++ src/main/java/domain/Line.java
@@ -1,16 +1,23 @@
 package domain;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.IntStream;
 
 public class Line {
 
-    List<Boolean> points;
+    List<Boolean> points = new ArrayList<>();
 
     public Line() {
     }
 
     public Line(int personCount) {
-        points = new ArrayList<>(personCount - 1);
+        for (int i = 0; i < personCount-1; i++) {
+            points.add(false);
+        }
+    }
+
+    public List<Boolean> generateRandomPoint() {
+        Collections.fill(points, true);
+        return points;
     }
 }
--- src/test/java/LineTest.java
+++ src/test/java/LineTest.java
@@ -3,6 +3,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class LineTest {
@@ -12,16 +14,30 @@ public class LineTest {
     void createLine() {
         assertThatCode(() -> new Line()).doesNotThrowAnyException();
     }
-    
+
     @Test
     @DisplayName("가로 길이는 사용자 수 -1 이다.")
-    void methodName() throws Exception {
+    void createLineWithPersonCount() {
         //given
         int personCount = 4;
         //when
         //then
         assertThatCode(() -> new Line(personCount)).doesNotThrowAnyException();
     }
-    
+
+    @Test
+    @DisplayName("무작위로 가로 라인을 생성한다.")
+    void createRandomLine() {
+        //given
+        int personCount = 4;
+        List<Boolean> expectedPoint = List.of(true, true, true);
+        //when
+        Line line = new Line(personCount);
+
+        List<Boolean> line2 = line.generateRandomPoint();
+        //then
+        assertThat(line2).isEqualTo(expectedPoint);
+    }
+
 
 }
