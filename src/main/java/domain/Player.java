4ccf67914989f994391692abfbeb888a36261588
ehtjsv2 <ehtjsv2@naver.com>
2024-02-20 16:42:51 +0900

    feat: 플레이어의 이름이 영어가 아니면 예외 처리

--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -1,5 +1,7 @@
 package domain;
 
+import java.util.regex.Pattern;
+
 public class Player {
 
     private final String name;
@@ -10,6 +12,17 @@ public class Player {
     }
 
     private void validate(String name) {
+        validateNameSize(name);
+        validateNameLanguage(name);
+    }
+
+    private void validateNameLanguage(String name) {
+        if(!name.matches("^[A-Za-z]*$")){
+            throw new IllegalArgumentException("[Error] 플레이어 이름은 영어여야 합니다.");
+        }
+    }
+
+    private void validateNameSize(String name) {
         if(name.length() > 5) {
             throw new IllegalArgumentException("[Error] 플레이어 이름은 5자 이하여야 합니다.");
         }
--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import domain.Player;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class PlayerTest {
 
@@ -20,5 +22,13 @@ public class PlayerTest {
         assertThatThrownBy(() -> new Player("dodododo"))
                 .isInstanceOf(IllegalArgumentException.class);
 
+    }
+
+    @DisplayName("플레이어의 이름이 영어가 아닌 경우 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"도도","c!apy","123"})
+    void createPlayerWithNotEnglish(String invalidName) {
+        assertThatThrownBy(()-> new Player(invalidName))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
