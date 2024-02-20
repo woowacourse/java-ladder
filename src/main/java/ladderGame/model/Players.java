07e38938de80ee68ef350ee6e451f7e411143739
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-02-20 14:49:26 +0900

    feat(Players) : 문자열 리스트를 인자로 받아서 참여자들을 생성한다 (Test Passes)

---
+++ src/main/java/ladderGame/model/Players.java
@@ -0,0 +1,15 @@
+package ladderGame.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Players {
+    private final List<Player> players;
+
+    public Players(List<String> names) {
+        players = new ArrayList<>();
+        for(String name : names) {
+            players.add(new Player(new Name(name)));
+        }
+    }
+}
