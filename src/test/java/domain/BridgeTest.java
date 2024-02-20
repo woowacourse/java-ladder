39525f463435adfa510bf55cbefcf9d594a0eef4
jhon3242 <rldotnr2@gmail.com>
2024-02-20 14:46:30 +0900

    refactor(blue) : 테스트코드 리팩터링
    Co-authored-by: zangsu <zangsu_@naver.com>

--- src/test/java/domain/BridgeTest.java
+++ src/test/java/domain/BridgeTest.java
@@ -3,6 +3,7 @@ package domain;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -10,13 +11,15 @@ import org.junit.jupiter.params.provider.ValueSource;
 public class BridgeTest {
     //TODO 더 적합한 클래스 이름 고민하기
 
+    @DisplayName("객체가 정상적으로 생성된다.")
     @Test
     void constructSuccessTest() {
         assertThatNoException()
                 .isThrownBy(() -> new Bridge(5));
     }
 
-    @ParameterizedTest
+    @DisplayName("높이가 자연수가 아니라면 예외가 발생한다.")
+    @ParameterizedTest(name = "높이가 {0}인 경우 예외가 발생한다.")
     @ValueSource(ints = {-10, 0})
     void constructFailWithNotPositive(int height){
         //TODO 더 적합한 메서드 이름 고민
