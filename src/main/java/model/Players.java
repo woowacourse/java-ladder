a5b47dff638fed9a1c695912daeda7ef05fa0647
Choo <choo000407@naver.com>
2024-02-20 16:11:52 +0900

    feat: 참여자 수는 최소 2명이다

---
+++ src/main/java/model/Players.java
@@ -0,0 +1,30 @@
+package model;
+
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toList;
+
+import java.util.List;
+
+public class Players {
+    private static final int MIN_SIZE_OF_PLAYERS = 2;
+    private static final String INVALID_SIZE_OF_PLAYERS = "참여자 수는 최소 2명입니다.";
+
+    private final List<Player> players;
+
+    private Players(List<Player> players) {
+        validateSizeOfPlayers(players);
+        this.players = players;
+    }
+
+    private void validateSizeOfPlayers(List<Player> players) {
+        if (players.size() < MIN_SIZE_OF_PLAYERS) {
+            throw new IllegalArgumentException(INVALID_SIZE_OF_PLAYERS);
+        }
+    }
+
+    public static Players fromNames(List<String> names) {
+        return names.stream()
+                .map(Player::new)
+                .collect(collectingAndThen(toList(), Players::new));
+    }
+}
