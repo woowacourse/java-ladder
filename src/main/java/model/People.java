eeb57ad66a1116a59b60fe6fed3ec0ad476b5144
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-02-20 17:37:15 +0900

    feat(People): People 검증 로직 작성
    Co-authored-by: HaiSeong <wjdgotjd9908@gmail.com>

--- src/main/java/model/People.java
+++ src/main/java/model/People.java
@@ -3,9 +3,26 @@ package model;
 import java.util.List;
 
 public class People {
+    private static final int MIN_PERSON_COUNT = 2;
+
+    private final List<Person> personGroup;
+
+    private People(List<Person> personGroup) {
+        validatePersonCount(personGroup.size());
+        this.personGroup = personGroup;
+    }
+
+    private void validatePersonCount(int personCount) {
+        if (personCount < MIN_PERSON_COUNT) {
+            throw new IllegalArgumentException("사람은 최소 2명 참가해야 합니다.");
+        }
+    }
 
     public static People from(List<String> names) {
-        return null;
+        List<Person> personGroup = names.stream()
+                .map(Person::new)
+                .toList();
+        return new People(personGroup);
     }
 
 
