8d585d9f966a4aa0527342782b487e5e5a89a99d
nayonsoso <panda0329@naver.com>
2024-02-20 14:02:49 +0900

    test: 이름 생성 테스트
    - red 단계

---
+++ src/test/java/NameTest.java
@@ -0,0 +1,12 @@
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class NameTest {
+    @DisplayName("이름 생성 테스트")
+    @Test
+    void createName(){
+        Assertions.assertThatCode(new Name("이름"))
+                .doesNotThrowAnyException();
+    }
+}
