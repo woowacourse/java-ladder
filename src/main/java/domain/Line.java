e81dfae9f3b9c7e1eb6e03b30f2b9c921e9bbdf4
SungKyum Kim <sungkyum1@naver.com>
2024-02-20 15:31:07 +0900

    refactor: 겹치지 않는 라인 생성 로직 개선

--- src/main/java/domain/Line.java
+++ src/main/java/domain/Line.java
@@ -8,19 +8,30 @@ public class Line {
     private final List<Boolean> points = new ArrayList<>();
 
     public Line(int personCount) {
+        createLine(personCount);
+    }
+
+    private void createLine(int personCount) {
         Random random = new Random();
-        for (int i = 0; i < personCount-1; i++) {
-            boolean isTrue = random.nextBoolean();
-            if (i != 0 && points.get(i - 1) == true) {
-                if (isTrue) {
-                    points.add(false);
-                } else {
-                    points.add(false);
-                }
-            } else {
-                points.add(isTrue);
-            }
+        for (int i = 0; i < personCount -1; i++) {
+            boolean nextBoolean = random.nextBoolean();
+            boolean nextPoint = getNextPoint(nextBoolean);
+            points.add(nextPoint);
         }
+    }
+
+    private boolean getNextPoint(boolean nextBoolean) {
+        if (points.isEmpty()) {
+            return nextBoolean;
+        }
+        if (isLastPointTrue()) {
+            return false;
+        }
+        return nextBoolean;
+    }
+
+    private Boolean isLastPointTrue() {
+        return points.get(points.size() - 1);
     }
 
     public List<Boolean> getPoints() {
