0eb906d70bcf6ce3bfdec0a876b0400199f3ee3e
JUHA <84626225+khabh@users.noreply.github.com>
2024-02-20 17:17:42 +0900

    test: RandomLadderStepGenerator에서 스텝 생성 테스트

---
+++ src/test/java/ladder/domain/generator/RandomLadderStepGeneratorTest.java
@@ -0,0 +1,24 @@
+package ladder.domain.generator;
+
+import ladder.domain.RandomLadderStepGenerator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+
+public class RandomLadderStepGeneratorTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {3, 4, 5})
+    @DisplayName("올바르게 랜덤한 사다리 스텝을 생성한다.")
+    void createRandomLadderStepTest(int participantCount) {
+        // given
+        LadderStepGenerator randomLadderStepGenerator = new RandomLadderStepGenerator();
+
+        // when & then
+        assertThatCode(() -> randomLadderStepGenerator.generate(participantCount))
+                .doesNotThrowAnyException();
+    }
+}
