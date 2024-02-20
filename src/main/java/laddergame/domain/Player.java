e0a83e5e2e4f71f0a94aeb0307f52fc2b147ab4f
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-02-20 14:40:51 +0900

    feat: 사용자 이름 최대 길이 검증
    Co-Authored-By: lemone <55480011+jihyeonl@users.noreply.github.com>

--- src/main/java/laddergame/domain/Player.java
+++ src/main/java/laddergame/domain/Player.java
@@ -3,8 +3,15 @@ package laddergame.domain;
 public class Player {
     private final String name;
 
-    public Player(String name) {
+    public Player(final String name) {
+        validate(name);
         this.name = name;
+    }
+
+    private void validate(final String name){
+        if(name.length() > 5){
+            throw new IllegalArgumentException();
+        }
     }
 
     public String getName() {
