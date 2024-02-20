7b264e16e55e299c20cdf2844c16c882ad409fd5
eunhee <dmsgml9903@gmail.com>
2024-02-20 14:42:34 +0900

    feat(Line): 사다리의 열은 참가자의 수보다 하나 적다.

--- src/main/java/Line.java
+++ src/main/java/Line.java
@@ -1,13 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Line {
 
-
-
-
+    private final List<Boolean> points;
     public Line(int participantSize) {
-
+        this.points = new ArrayList<>();
+        for (int i = 0; i < participantSize-1; i++) {
+            points.add(false);
+        }
     }
 
     public int size() {
-        return 0;
+        return points.size();
     }
 }
