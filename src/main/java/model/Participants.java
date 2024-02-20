b6f50220a6d0f52cd5b88e3d73ce182abaf97c9d
hyun <osh123chm@gmail.com>
2024-02-20 15:39:27 +0900

    refactor: 최소 참여자 이름수 상수 포장

--- src/main/java/model/Participants.java
+++ src/main/java/model/Participants.java
@@ -3,16 +3,19 @@ package model;
 import java.util.List;
 
 public class Participants {
+    private static final int MIN_PARTICIPANT_NAME = 2;
     private final List<Participant> participants;
 
     public Participants(List<String> names) {
         validatorNameSize(names);
-        List<Participant> participants = names.stream().map(Participant::new).toList();
+        List<Participant> participants = names.stream()
+                .map(Participant::new)
+                .toList();
         this.participants = participants;
     }
 
     private void validatorNameSize(List<String> names) {
-        if (names.size() < 2) {
+        if (names.size() < MIN_PARTICIPANT_NAME) {
             throw new IllegalArgumentException("참여할 사람은 두명 이상이어야한다.");
         }
     }
