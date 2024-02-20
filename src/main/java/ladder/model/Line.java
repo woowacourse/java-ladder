acdc850f50353e0b7e57ef18ed08243a2396f8e4
BurningFalls <sherlocky31415@gmail.com>
2024-02-20 20:03:32 +0900

    feat(line): 특정 길이의 한 행을 생성하는 기능 구현
    Co-authored-by: Hyunguk Ryu <hw0603@naver.com>

--- src/main/java/ladder/model/Line.java
+++ src/main/java/ladder/model/Line.java
@@ -1,18 +1,34 @@
 package ladder.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Line {
-    private final List<Boolean> rows;
+    private final List<Boolean> row;
 
-    public Line(List<Boolean> rows) {
-        this.rows = rows;
+    private Line(List<Boolean> row) {
+        this.row = row;
+    }
+
+    public static Line of(int playerCount) {
+        List<Boolean> randomRow = new ArrayList<>();
+
+        // TODO: Random 선택을 기반으로 생성해야 함.
+        for (int i = 0; i < playerCount; i++) {
+            randomRow.add(true);
+        }
+
+        return new Line(randomRow);
     }
 
     public boolean isLeftLadderExist(int position) {
         if (position == 0) {
             return false;
         }
-        return rows.get(position - 1);
+        return row.get(position - 1);
+    }
+
+    public int size() {
+        return row.size();
     }
 }
--- src/test/java/ladder/model/LineTest.java
+++ src/test/java/ladder/model/LineTest.java
@@ -1,42 +1,18 @@
 package ladder.model;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LineTest {
-    private final Line line = new Line(List.of(true, false, true, false, true));
-
     @Test
-    @DisplayName("왼쪽에 사다리가 있다면 true를 반환한다.")
-    public void leftLadderExistTest() {
-        int position = 1;
-        boolean actual = line.isLeftLadderExist(position);
-        boolean expected = true;
+    @DisplayName("지정된 수 만큼의 길이를 갖는 한 행이 생성된다.")
+    void createLineTest() {
+        Line line = Line.of(5);
 
-        assertThat(actual).isEqualTo(expected);
-    }
-
-    @Test
-    @DisplayName("왼쪽에 사다리가 없다면 false를 반환한다.")
-    public void leftLadderNotExistTest() {
-        int position = 2;
-        boolean actual = line.isLeftLadderExist(position);
-        boolean expected = false;
-
-        assertThat(actual).isEqualTo(expected);
-    }
-
-    @Test
-    @DisplayName("첫번째 사다리 왼쪽은 사다리가 없는 것으로 간주한다.")
-    public void firstLadderTest() {
-        int position = 0;
-        boolean actual = line.isLeftLadderExist(position);
-        boolean expected = false;
+        int actual = line.size();
+        int expected = 5;
 
         assertThat(actual).isEqualTo(expected);
     }
