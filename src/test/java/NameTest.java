d8a7c007edbfcb356ee865d381169d93d1b63364
hyxrxn <gpfls981220@gmail.com>
2024-02-20 16:31:40 +0900

    test: Name 클래스의 이름 존재 검증
    Co-authored-by: shin-jisong <songjin1127@naver.com>

--- src/test/java/NameTest.java
+++ src/test/java/NameTest.java
@@ -1,6 +1,8 @@
 import domain.Name;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -12,6 +14,16 @@ public class NameTest {
         assertThatThrownBy(() -> new Name("zangsu"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 이름의 길이는 5글자를 초과할 수 없습니다.");
+    }
+
+    @ParameterizedTest
+    @DisplayName("이름이 없을 때 예외가 발생한다.")
+    @NullAndEmptySource
+    void noNameException(String name) {
+        assertThatThrownBy(() -> new Name(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 이름이 없습니다.");
+
     }
 
 }
