71481460ede7543480547c754b8c800f0a45f69c
eunjungL <dldmswjd322@gmail.com>
2024-02-20 17:39:24 +0900

    feat: Line에서 다리를 놓을 수 있는가에 대한 기능 추가

--- src/main/java/domain/Direction.java
+++ src/main/java/domain/Direction.java
@@ -1,2 +1,22 @@
-package domain;public class Direction {
+package domain;
+
+import java.util.Arrays;
+
+public enum Direction {
+    LEFT(-1),
+    RIGHT(1),
+    EMPTY(0); // TODO: 더 적절한 네이밍 생각해보기
+
+    private final int value;
+
+    Direction(int value) {
+        this.value = value;
+    }
+
+    public static Direction getDirectionByValue(int value) {
+        return Arrays.stream(Direction.values())
+                .filter((direction) -> direction.value == value)
+                .findAny()
+                .orElseThrow();
+    }
 }
--- src/main/java/domain/Line.java
+++ src/main/java/domain/Line.java
@@ -1,2 +1,21 @@
-package domain;public class Line {
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Line {
+    private final List<Direction> points;
+
+    public Line(int participantCount) {
+        this.points = new ArrayList<>();
+    }
+
+    public boolean checkIsPossibleAddBridge(int position) {
+        // TODO: 간소화 시킬 수 있을지 고민해보기
+        return !points.get(position - 1).equals(Direction.RIGHT);
+    }
+
+    public List<Direction> getPoints() {
+        return points;
+    }
 }
