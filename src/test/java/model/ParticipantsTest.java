fa854cfe28f033d4678551b464cdac1164f0cac7
hyun <osh123chm@gmail.com>
2024-02-20 15:41:51 +0900

    test: 중복 참여자 이름 검증

--- src/test/java/model/ParticipantsTest.java
+++ src/test/java/model/ParticipantsTest.java
@@ -10,4 +10,10 @@ public class ParticipantsTest {
         List<String> names = List.of("엘라");
         Assertions.assertThrows(IllegalArgumentException.class, () -> new Participants(names));
     }
+
+    @Test
+    void 중복된_참여자_이름이_있으면_예외가_발생한다() {
+        List<String> names = List.of("릴리", "릴리");
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Participants(names));
+    }
 }
