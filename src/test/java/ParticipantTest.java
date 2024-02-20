ba1552b835d1907f06d402ff6d4c0e3f9151bd68
eunhee <dmsgml9903@gmail.com>
2024-02-20 13:44:12 +0900

    test(ParticipantTest): 참여자의 이름이 5글자 이하일 시 생성에 성공한다.

--- src/test/java/ParticipantTest.java
+++ src/test/java/ParticipantTest.java
@@ -6,10 +6,17 @@ public class ParticipantTest {
 
     @DisplayName("참여자의 이름은 최대 5글자까지 부여할 수 있다.")
     @Test
-    void participantNameTest() {
+    void participantNameThrowExceptionTest() {
         Assertions.assertThatThrownBy(() -> new Participant("asdfsda"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("참여자의 이름은 최대 5글자입니다.");
+    }
+
+    @DisplayName("참여자의 이름이 5글자 이하일 시 생성에 성공한다.")
+    @Test
+    void participantNameTest() {
+        Assertions.assertThatCode(() -> new Participant("pobi"))
+                .doesNotThrowAnyException();
     }
 
 }
