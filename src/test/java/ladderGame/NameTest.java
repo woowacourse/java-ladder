47319695792ae9c7ec5ce0b3d4224dabbc4bf054
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-02-20 14:25:07 +0900

    test(NameTest) : 이름은 6글자 이상일 시 예외처리 된다 (Test Fails)

--- src/test/java/ladderGame/NameTest.java
+++ src/test/java/ladderGame/NameTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class NameTest {
@@ -12,5 +13,13 @@ class NameTest {
     @DisplayName("문자열을 인자로 넘겨서 Name 객체를 생성할 수 있다.")
     void createName() {
         assertThatCode(() -> new Name("이름"));
+    }
+
+    @Test
+    @DisplayName("이름은 6글자 이상일 시 예외처리 된다.")
+    void validateNameLength() {
+        assertThatThrownBy(() -> new Name("aaaaaa"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이름은 최대 5글자까지 가능합니다.");
     }
 }
