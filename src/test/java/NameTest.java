149e4e021eab0dd4f04e83b8d09242606b9135cc
nayonsoso <panda0329@naver.com>
2024-02-20 14:20:02 +0900

    test: 이름 길이 검증 테스트
    - red 단계

--- src/test/java/NameTest.java
+++ src/test/java/NameTest.java
@@ -1,6 +1,9 @@
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class NameTest {
     @DisplayName("이름 생성 테스트")
@@ -8,5 +11,25 @@ public class NameTest {
     void createName() {
         Assertions.assertThatCode(() -> new Name("이름"))
                 .doesNotThrowAnyException();
+    }
+
+    @Nested
+    @DisplayName("이름은 최대 5자여야 한다.")
+    class NameLength {
+
+        @DisplayName("이름이 1~5자이이면 성공한다.")
+        @ParameterizedTest
+        @ValueSource(strings = {"1", "12345"})
+        void nameLengthValidateSuccess(String source) {
+            Assertions.assertThatCode(() -> new Name(source))
+                    .doesNotThrowAnyException();
+        }
+
+        @DisplayName("이름이 6자 이상이면 실패한다.")
+        @Test
+        void nameLengthValidateFail() {
+            Assertions.assertThatThrownBy(() -> new Name("123456"))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
     }
 }
