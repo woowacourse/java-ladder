d4d5fc0e0faf3d6ad5b2d02db2ab1fe6596c301c
kelly6bf <kelly6bf@gmail.com>
2024-02-20 20:15:49 +0900

    test: 사다리 높이 만큼의 Line생성을 검증하는 테스트 로직 작성

---
+++ src/test/java/ladder/domain/LinesTest.java
@@ -0,0 +1,15 @@
+package ladder.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LinesTest {
+    @DisplayName("사다리 높이 만큼의 Line을 생성한다.")
+    @Test
+    void createLines() {
+        assertThat(new Lines(3, 4).getLines().size())
+                .isEqualTo(3);
+    }
+}
