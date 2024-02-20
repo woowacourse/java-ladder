be75b6e88677261dd9fc9bd98158e40c7ea33892
ikjo39 <wer04129@gmail.com>
2024-02-20 14:35:15 +0900

    feat: 참여할 사람 객체 생성

---
+++ src/main/java/Participant.java
@@ -0,0 +1,17 @@
+public class Participant {
+    private final String name;
+
+    public Participant(final String name) {
+        validateNameLength(name);
+        this.name = name;
+    }
+
+    private void validateNameLength(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException();
+        }
+        if (name.length() > 5) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
---
+++ src/test/java/ParticipantTest.java
@@ -0,0 +1,36 @@
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class ParticipantTest {
+
+    @DisplayName("String 값을 받아 Participant 객체를 생성한다.")
+    @Test
+    void createParticipant() {
+        String given = "daon";
+        assertThatCode(() -> new Participant(given))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("참여할 사람의 이름은 5자를 초과할 수 없다.")
+    @Test
+    void validateNameLength() {
+        String given = "123456";
+        assertThatThrownBy(() -> new Participant(given))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("참여할 사람의 이름은 null이거나 공백이면 안된다.")
+    @ParameterizedTest
+    @NullSource
+    @ValueSource(strings = {""})
+    void validateNameNotNullAndNotBlank(String given) {
+        assertThatThrownBy(() -> new Participant(given))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
