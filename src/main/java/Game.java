dc21648c6266a9a4e6e4d0b3aa232d63c7ade862
nayonsoso <panda0329@naver.com>
2024-02-20 17:13:17 +0900

    test: 이름 분리 기능 검증 테스트
    - red 단계

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,2 +1,8 @@
+import java.util.List;
+
 public class Game {
+    public List<Person> parsePersonName(String input) {
+
+        return null;
+    }
 }
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -2,6 +2,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 public class GameTest {
 
     @DisplayName("생성 테스트")
@@ -9,5 +11,16 @@ public class GameTest {
     void creatGame() {
         Assertions.assertThatCode(() -> new Game())
                 .doesNotThrowAnyException();
+    }
+
+    @DisplayName("사람들의 이름을 받아 사람 리스트로 반환한다.")
+    @Test
+    void parsePersonName() {
+        Game game = new Game();
+
+        List<Person> result = game.parsePersonName("아톰,산초");
+
+        Assertions.assertThat(result)
+                .containsExactly(new Person("아톰"), new Person("산초"));
     }
 }
