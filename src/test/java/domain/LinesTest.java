40023864755ae1dfc6ec16dfb32767986bd4e3c4
hangillee <skfcb10@naver.com>
2024-02-20 15:37:10 +0900

    test: 라인 생성 로직 검증 테스트 작성

---
+++ src/test/java/domain/LinesTest.java
@@ -0,0 +1,17 @@
+package domain;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LinesTest {
+    @Test
+    @DisplayName("높이 값 만큼 Line을 생성한다.")
+    void createLines() {
+        int height = 5;
+        int personCount = 4;
+        Lines lines = new Lines(height, personCount);
+        assertEquals(height, lines.getLines().size());
+    }
+}
