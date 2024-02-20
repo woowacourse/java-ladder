6807748db80652ce0bb0f3b4ee8042fa399df9a7
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-02-20 17:03:39 +0900

    feat(Ladder) : 최대 사다리 높이를 인자로 받아서 사다리를 생성한다 (Test Passes)

---
+++ src/main/java/ladderGame/model/Ladder.java
@@ -0,0 +1,15 @@
+package ladderGame.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ladder {
+    private final List<Line> lines;
+
+    public Ladder(int maxHeight, int personNumber) {
+        lines = new ArrayList<>();
+        for (int i = 0; i < maxHeight; i++) {
+            lines.add(new Line(personNumber));
+        }
+    }
+}
