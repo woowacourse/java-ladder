f455c6dacde460be177013cc77b2d30a409ec5fa
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-02-20 15:13:23 +0900

    feat(Players) : 이름이 두 명보다 적으면 예외처리 된다 (Test Passes)

--- src/main/java/ladderGame/model/Players.java
+++ src/main/java/ladderGame/model/Players.java
@@ -10,6 +10,7 @@ public class Players {
 
     public Players(List<String> names) {
         validateDuplicationName(names);
+        validateLessThanMinimum(names);
 
         players = new ArrayList<>();
         for(String name : names) {
@@ -20,6 +21,12 @@ public class Players {
     private void validateDuplicationName(List<String> name) {
         if(new HashSet<>(name).size() != name.size()) {
             throw new IllegalArgumentException("참여자들의 이름은 중복될 수 없습니다.");
+        }
+    }
+
+    private void validateLessThanMinimum(List<String> names) {
+        if(names.size() < 2) {
+            throw new IllegalArgumentException("참여자의 이름은 두 개 이상이어야 한다.");
         }
     }
 }
