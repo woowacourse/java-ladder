041b679321d9c97f60360d835a7d0a84ef677a5d
hoyeonyy <highoption@naver.com>
2024-02-20 15:54:16 +0900

    feat (PlayerName) : 이름에 공백이나 null 이 오면 예외발생 (TestPass 단계)

--- src/main/java/ladder/PlayerName.java
+++ src/main/java/ladder/PlayerName.java
@@ -7,8 +7,11 @@ public class PlayerName {
     }
 
     private static void validate(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("이름은 한글자 이상이어야 합니다.");
+        }
         if (name.length() > 5) {
-           throw new IllegalArgumentException("이름은 5글자를 넘을 수 없습니다");
+            throw new IllegalArgumentException("이름은 5글자를 넘을 수 없습니다.");
         }
     }
 }
