38aaaf6d7a835a2342331185ccfda280aa114bf1
김민수 MINSOO KIM <alstn113@gmail.com>
2024-02-20 17:12:27 +0900

    refactor(Ladder): 사다리 생성자에 발판 생성기 추가

--- src/main/java/ladder/domain/ladder/Ladder.java
+++ src/main/java/ladder/domain/ladder/Ladder.java
@@ -1,12 +1,17 @@
 package ladder.domain.ladder;
 
+import ladder.domain.generator.RandomRungGenerator;
+import ladder.domain.generator.RungGenerator;
+
 public class Ladder {
     private final int playerCount;
     private final Height height;
+    private final RungGenerator rungGenerator;
 
-    public Ladder(int playerCount, int height) {
+    public Ladder(int playerCount, int height, RungGenerator rungGenerator) {
         this.playerCount = playerCount;
         this.height = new Height(height);
+        this.rungGenerator = rungGenerator;
     }
 
     public int getHeight() {
--- src/test/java/ladder/domain/ladder/LadderTest.java
+++ src/test/java/ladder/domain/ladder/LadderTest.java
@@ -2,21 +2,26 @@ package ladder.domain.ladder;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import ladder.domain.generator.RandomRungGenerator;
+import ladder.domain.generator.RungGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LadderTest {
 
     @Test
-    @DisplayName("사다리는 참여자 수와 높이를 가진다.")
+    @DisplayName("참여자 수, 높이와 발판 생성기를 가진다.")
     void testConstruct() {
         int playerCount = 4;
         int height = 5;
-        Ladder ladder = new Ladder(playerCount, height);
+        RungGenerator rungGenerator = new RandomRungGenerator();
+        Ladder ladder = new Ladder(playerCount, height, rungGenerator);
 
         assertThat(ladder).extracting("playerCount")
                 .isEqualTo(4);
         assertThat(ladder).extracting("height")
                 .isEqualTo(5);
+        assertThat(ladder).extracting("rungGenerator")
+                .isInstanceOf(RandomRungGenerator.class);
     }
 }
