747fe2766dd1d536c0beeead952e653983416866
hyun <osh123chm@gmail.com>
2024-02-20 15:03:22 +0900

    test: 참여자 이름 길이 예외 검증

---
+++ src/test/java/ParticipantTest.java
@@ -0,0 +1,11 @@
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ParticipantTest {
+    @Test
+    void 참여자_이름이_다섯글자를_넘으면_예외가_발생한다() {
+        String name = "엘라릴리애쉬";
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Participant(name));
+    }
+}
