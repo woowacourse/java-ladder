73dee05cab6c2ba53d738adbb2665b0913b74eba
eunhee <dmsgml9903@gmail.com>
2024-02-20 13:47:01 +0900

    refactor(Participant): 참여자의 이름이 5글자 이하일 시 생성에 성공한다.

--- src/main/java/Participant.java
+++ src/main/java/Participant.java
@@ -1,9 +1,16 @@
 public class Participant {
+    private static final int MAX_NAME_LENGTH = 5;
 
     private final String name;
 
     public Participant(String name) {
+        validateNameLength(name);
         this.name = name;
-        throw new IllegalArgumentException("참여자의 이름은 최대 5글자입니다.");
+    }
+
+    private void validateNameLength(String name) {
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("참여자의 이름은 최대 5글자입니다.");
+        }
     }
 }
