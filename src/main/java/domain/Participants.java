942d1d8891fe6b1d919cd5ca2f3e48ef5cd90243
hyxrxn <gpfls981220@gmail.com>
2024-02-20 17:34:12 +0900

    feat: Participants 클래스의 참가자 수 검증
    Co-authored-by: shin-jisong <songjin1127@naver.com>

--- src/main/java/domain/Participants.java
+++ src/main/java/domain/Participants.java
@@ -1,4 +1,13 @@
 package domain;
 
 public class Participants {
+
+    public Participants(String[] names) {
+        if (names.length < 2) {
+            throw new IllegalArgumentException("[ERROR] 참가자는 2명 이상이어야 합니다.");
+        }
+        if (names.length > 50) {
+            throw new IllegalArgumentException("[ERROR] 참가자는 50명 이하여야 합니다.");
+        }
+    }
 }
