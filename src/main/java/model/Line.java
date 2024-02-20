667464537126ac97d5e18c1f5afdabbd3d7f6f29
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-02-20 15:22:14 +0900

    feat(Line): 라인 검증 로직 작성

--- src/main/java/model/Line.java
+++ src/main/java/model/Line.java
@@ -4,13 +4,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Line {
-    private List<Boolean> points = new ArrayList<>();
+    private static final int MIN_PERSON_COUNT = 2;
+
+    private final List<Boolean> points;
 
     public Line(int personCount) {
-        // 라인의 좌표 값에 선이 있는지 유무를 판단하는 로직 추가
+        // TODO 라인의 좌표 값에 선이 있는지 유무를 판단하는 로직 추가 (???)
+        validatePersonCount(personCount);
+        // TODO: 우리 코드의 의미를 이해 가능??
+        points = new ArrayList<>(personCount - 1);
+        for (int i= 0; i < personCount - 1; i++) {
+            points.add(true);
+        }
+    }
+
+    private void validatePersonCount(int personCount) {
+        if (personCount < MIN_PERSON_COUNT) {
+            throw new IllegalArgumentException("참여할 사람은 최소 2명이어야 합니다.");
+        }
     }
 
     public int getSize() {
-        return 0;
+        return points.size();
     }
 }
