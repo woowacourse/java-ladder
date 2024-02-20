995fa4375760f533f679faad8f7e7faa8cf0908f
eunjungL <dldmswjd322@gmail.com>
2024-02-20 15:00:46 +0900

    feat: Participant 이름 글자 수 예외 검증 기능 추가

--- src/main/java/domain/Participant.java
+++ src/main/java/domain/Participant.java
@@ -1,2 +1,18 @@
-package domain;public class Participant {
+package domain;
+
+public class Participant {
+
+    private static final Integer MAX_NAME_LENGTH = 5;
+    private final String name;
+
+    public Participant(String name) {
+        validateNameLength(name);
+        this.name = name;
+    }
+
+    private void validateNameLength(String name) {
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("이름은 5글자를 초과할 수 없습니다.");
+        }
+    }
 }
