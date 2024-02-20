40fde4e47cd0cdb398383ddbacad7f87c14ac69d
Arachneee <jhg2819@naver.com>
2024-02-20 14:27:52 +0900

    feat: 사다리 생성

---
+++ src/main/java/ladder/Ladder.java
@@ -0,0 +1,23 @@
+package ladder;
+
+import java.util.ArrayList;
+import java.util.List;
+import ladder.Line;
+
+public class Ladder {
+
+    private final List<Line> lines;
+
+    public Ladder(final int height, final int personSize, final BooleanGenerator booleanGenerator) {
+        List<Line> lines = new ArrayList<>();
+        for(int i=0; i<height; i++) {
+            lines.add(new Line(personSize, booleanGenerator));
+        }
+
+        this.lines = lines;
+    }
+
+    public int getHeight() {
+        return lines.size();
+    }
+}
---
+++ src/test/java/laddergame/LadderTest.java
@@ -0,0 +1,26 @@
+package laddergame;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import ladder.Ladder;
+import ladder.RandomBooleanGenerator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LadderTest {
+
+    @DisplayName("사다리를 생성한다.")
+    @Test
+    void test() {
+        // given
+        int height = 5;
+        int personSize = 4;
+
+        // when
+        Ladder ladder = new Ladder(height, personSize, new RandomBooleanGenerator());
+
+        // then
+        assertThat(ladder.getHeight()).isEqualTo(height);
+    }
+
+}
