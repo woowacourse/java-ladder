eaae4db0e3b57d978486cf113d38d8d857f9a070
MingyeomKim <mingyum119@naver.com>
2024-02-21 15:28:44 +0900

    feat: 이름은 최대 5글자이다.

---
+++ src/main/java/model/Player.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Player {
+    private String name;
+
+    public Player(String name) {
+        validate(name);
+        this.name = name;
+    }
+
+    private void validate(String name) {
+        if(name.length() > 5) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
---
+++ src/test/java/model/PlayerTest.java
@@ -0,0 +1,23 @@
+package model;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PlayerTest {
+    @Test
+    @DisplayName("플레이어 생성 테스트")
+    void createPlayer() {
+        String name = "pobi";
+        Assertions.assertThatCode(() -> new Player(name))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("플레이어의 이름이 5자 초과여서 오류가 발생한다.")
+    void invalidNameLengthTest() {
+        String name = "pobipobi";
+        Assertions.assertThatThrownBy(() -> new Player(name))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
