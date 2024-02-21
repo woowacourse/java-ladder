9b2b29fe5235cec559b95d3b1cd3c30b587c2605
J-I-H-O <jeongjiho0731@gmail.com>
2024-02-21 15:02:59 +0900

    test: 가로대 연결 검증 테스트
    Co-authored-by: J-I-H-O <jeongjiho0731@gmail.com>
    Co-authored-by: Nam Gi Beom <rlqja0523@naver.com>

---
+++ src/test/java/domain/LadderRungTest.java
@@ -0,0 +1,18 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class LadderRungTest {
+    @ParameterizedTest
+    @ValueSource(booleans = {true, false})
+    void 가로대가_연결되면_true를_반환하고_연결되지_않으면_false를_반환한다(boolean isConnected) {
+        // given
+        LadderRung ladderRung = LadderRung.create(isConnected);
+
+        // when & then
+        assertThat(ladderRung.isConnected()).isEqualTo(isConnected);
+    }
+}
