2759f7ce2176c7d335d944323f9fab6586b2e549
hoyeonyy <highoption@naver.com>
2024-02-21 19:12:06 +0900

    feat (OutputView) : 결과 출력 메서드 생성

--- src/main/java/ladder/view/OutputView.java
+++ src/main/java/ladder/view/OutputView.java
@@ -1,11 +1,32 @@
 package ladder.view;
 
 import ladder.dto.LadderDto;
+import ladder.dto.LineDto;
 import ladder.dto.PlayerNamesDto;
 
 public class OutputView {
 
+
     public void printResult(LadderDto ladderDto, PlayerNamesDto playerNamesDto) {
-        throw new UnsupportedOperationException();
+        System.out.println("실행결과");
+        System.out.println();
+
+        for (String playerName : playerNamesDto.playerNames()) {
+            System.out.printf("%5s ", playerName);
+        }
+        System.out.println();
+
+        for (LineDto line : ladderDto.lines()) {
+            System.out.printf("    |");
+            for (Boolean isExist : line.sticks()) {
+                if (isExist) {
+                    System.out.print("-----|");
+                }
+                if (!isExist) {
+                    System.out.print("     |");
+                }
+            }
+            System.out.println();
+        }
     }
 }
