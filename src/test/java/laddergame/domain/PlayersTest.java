3905df06ab716bb3364929a3c04117d35b53858b
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-02-21 19:05:00 +0900

    test: 이름 입력이 null이거나 공백일 경우 예외 검증
    Co-Authored-By: lemone <55480011+JiHyeonL@users.noreply.github.com>

--- src/test/java/laddergame/domain/PlayersTest.java
+++ src/test/java/laddergame/domain/PlayersTest.java
@@ -5,6 +5,9 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("플레이어들")
 public class PlayersTest {
@@ -17,5 +20,14 @@ public class PlayersTest {
         // when & then
         assertThrows(IllegalArgumentException.class, () -> Players.from(List.of(name, name)));
 
+    }
+
+    @ParameterizedTest
+    @NullSource
+    @ValueSource(strings = {"", "  "})
+    @DisplayName("입력이 없을 경우 예외가 발생한다.")
+    public void nameIsNullException(final String name) {
+        //given & when & then
+        assertThrows(IllegalArgumentException.class, () -> Players.from(List.of(name)));
     }
 }
