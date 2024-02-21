628a0623373caf4c455066d1a73dd0f2052d010f
coli-geonwoo <kkwoo001021@naver.com>
2024-02-21 17:22:50 +0900

    refactor(domain): 참여자 수 검증기능 메소드 분리

--- src/main/java/ladder/domain/People.java
+++ src/main/java/ladder/domain/People.java
@@ -11,25 +11,27 @@ public class People {
     private final List<String> names;
 
     public People(List<String> names) {
-        //TODO Validate로 분리하기
-        if (names.size() < MIN_PEOPLE_NUMBERS || names.size() > MAX_PEOPLE_NUMBERS) {
-            throw new IllegalArgumentException("참여자 수는 1이상 100이하만 가능합니다.");
+        validatePeopleNumber(names);
 
-        }
         for (String name : names) {
-            if (name == null) {
-                throw new IllegalArgumentException("이름은 5자 이내의 영숫자로 구성되어야 합니다.");
-            }
-            if (!name.matches(NAME_RULE)) {
-                throw new IllegalArgumentException("이름은 5자 이내의 영숫자로 구성되어야 합니다.");
-            }
+            validateNameRule(name);
         }
-        this.names = new ArrayList<>() {{
-            addAll(names);
-        }};
+        this.names.addAll(names);
     }
 
     public List<String> getNames() {
         return Collections.unmodifiableList(names);
+    }
+
+    private void validatePeopleNumber(List<String> names) {
+        if (names.size() < MIN_PEOPLE_NUMBERS || names.size() > MAX_PEOPLE_NUMBERS) {
+            throw new IllegalArgumentException("참여자 수는 1이상 100이하만 가능합니다.");
+        }
+    }
+
+    private void validateNameRule(String name) {
+        if (name == null || !name.matches(NAME_RULE)) {
+            throw new IllegalArgumentException("이름은 5자 이내의 영숫자로 구성되어야 합니다.");
+        }
     }
 }
