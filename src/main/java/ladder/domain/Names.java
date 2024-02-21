017702f193a962add410682cadcc2757120c89f0
ay-eonii <naayen@naver.com>
2024-02-21 17:27:00 +0900

    refactor(Names): parse 메서드 추출
    이름을 쉼표 단위로 분리 및 공백 제거 후 리스트로 변환

--- src/main/java/ladder/domain/Names.java
+++ src/main/java/ladder/domain/Names.java
@@ -12,13 +12,20 @@ public class Names {
     private final List<String> names;
 
     public Names(String rawNames) {
-        String[] names = rawNames.split(",");
+        List<String> names = parse(rawNames);
         validate(names);
-        this.names = new ArrayList<>(List.of(names));
+        this.names = new ArrayList<>(names);
     }
 
-    private static void validate(String[] names) {
-        long count = Arrays.stream(names)
+    private List<String> parse(String rawNames) {
+        String[] names = rawNames.split(",");
+        return Arrays.stream(names)
+                .map(String::trim)
+                .toList();
+    }
+
+    private void validate(List<String> names) {
+        long count = names.stream()
                 .filter(name -> name.length() > MAX_NAME_LENGTH)
                 .count();
 
@@ -27,12 +34,16 @@ public class Names {
         }
     }
 
-    public int getSize() {
+    public int count() {
         return names.size();
     }
 
+    //todo : 가장 긴 이름의 길아 반환하는 메서드
+
     @Override
     public String toString() {
-        return names.stream().map(name -> String.format("%-7s", name)).collect(Collectors.joining());
+        return names.stream()
+                .map(name -> String.format("%-7s", name))
+                .collect(Collectors.joining());
     }
 }
