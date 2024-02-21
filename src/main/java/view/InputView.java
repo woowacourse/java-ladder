b361893cfe8d7db6dd5bdf1ab7559a7ed5e9cc3d
ehtjsv2 <ehtjsv2@naver.com>
2024-02-21 15:31:37 +0900

    refactor: InputView 멤버 순서 변경

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -7,9 +7,9 @@ import java.util.Scanner;
 
 public class InputView {
 
-    private static final Scanner scanner = new Scanner(System.in);
     private static final String MESSAGE_PARTICIPATED_PLAYERS = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     private static final String MESSAGE_LADDER_HEIGHT = "\n최대 사다리 높이는 몇 개인가요?";
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static List<Player> readPlayerNames() {
         System.out.println(MESSAGE_PARTICIPATED_PLAYERS);
