a041f803a6b77baf124ad58d90818719672530d1
kelly6bf <kelly6bf@gmail.com>
2024-02-21 15:48:12 +0900

    feat: InputView의 메시지 추가

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -11,12 +11,15 @@ public class InputView {
     }
 
     public static List<String> readNames(Supplier<String> reader) {
+        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
         String input = reader.get();
         validateEmpty(input);
+        System.out.println();
         return parseNames(input);
     }
 
     public static int readLadderHeight(Supplier<String> reader) {
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
         String input = reader.get();
         validateEmpty(input);
         return parseLadderHeight(input);
