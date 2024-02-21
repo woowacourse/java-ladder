672a6c2c5a4d2752eb62a77d6b7944327952d725
김민수 MINSOO KIM <alstn113@gmail.com>
2024-02-21 17:07:09 +0900

    feat(OutputView): 참가자들의 이름을 출력

--- src/main/java/ladder/view/OutputView.java
+++ src/main/java/ladder/view/OutputView.java
@@ -1,7 +1,35 @@
 package ladder.view;
 
+import java.util.List;
+import ladder.dto.LadderDto;
+import ladder.dto.PlayersDto;
+
 public class OutputView {
+
+    public void printLadderResultMessage() {
+        System.out.println();
+        System.out.println("실행결과");
+    }
+
+    public void printPlayerNames(PlayersDto playersDto) {
+        List<String> playerNames = playersDto.playerNames();
+
+        System.out.println();
+        for (String playerName: playerNames) {
+            System.out.printf("%5s ", playerName);
+        }
+        System.out.println();
+    }
+
+
+    public void printLadder(LadderDto ladderDto) {
+
+    }
+
+
     public void printErrorMessage(String message) {
         System.out.println(message);
     }
+
+
 }
