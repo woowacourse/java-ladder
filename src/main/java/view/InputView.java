91dad6f1d8b5908f6c0d962f98a2249270076353
nhlee98 <e5slnh9876@gmail.com>
2024-02-21 22:57:34 +0900

    feat: 최대 사다리 높이를 입력받는다

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,12 +5,18 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
     private static final String PEOPLE_NAMES_INPUT_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    private static final String LADDER_HEIGHT_INPUT_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
 
     private InputView() {
     }
 
     public static String readPeopleNames() {
         OutputView.printMessage(PEOPLE_NAMES_INPUT_MESSAGE);
+        return scanner.nextLine();
+    }
+
+    public static String readLadderHeight() {
+        OutputView.printMessage(LADDER_HEIGHT_INPUT_MESSAGE);
         return scanner.nextLine();
     }
 }
