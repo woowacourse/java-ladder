381e78b8efe108aedab10dc86c3bb7e545bc4462
J-I-H-O <jeongjiho0731@gmail.com>
2024-02-21 17:20:56 +0900

    test: 주어진 높이에 맞는 사다리 생성 테스트
    Co-authored-by: J-I-H-O <jeongjiho0731@gmail.com>
    Co-authored-by: Nam Gi Beom <rlqja0523@naver.com>

---
+++ src/test/java/domain/ladder/LadderTest.java
@@ -0,0 +1,24 @@
+package domain.ladder;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import domain.BooleanGenerator;
+import org.junit.jupiter.api.Test;
+import support.TrueGenerator;
+
+public class LadderTest {
+    private final BooleanGenerator trueGenerator = new TrueGenerator();
+
+    @Test
+    void 주어진_높이에_맞게_사다리가_생성된다() {
+        // given
+        int height = 5;
+        int playerSize = 5;
+
+        // when
+        Ladder ladder = Ladder.create(height, playerSize, trueGenerator);
+
+        // then
+        assertThat(ladder.getHeight()).isEqualTo(height);
+    }
+}
