227109df1c44061407cc13aa80b8f3684a359b81
hangillee <skfcb10@naver.com>
2024-02-21 15:32:23 +0900

    test: 이름 리스트가 비어있을 때, 전체 사람 수로 0을 반환하는 기능 테스트 작성

--- src/test/java/domain/NamesTest.java
+++ src/test/java/domain/NamesTest.java
@@ -16,10 +16,18 @@ class NamesTest {
 
     @Test
     @DisplayName("전체 사람 수를 반환한다.")
-    void getTotalPersonNumber() {
+    void getTotalPersonCount() {
         List<String> rawNames = List.of("pobi", "crong", "honux");
         Names names = new Names(rawNames);
 
         Assertions.assertEquals(3, names.size());
+    }
+
+    @Test
+    @DisplayName("전체 사람 수는 이름 리스트가 비어있다면 0을 반환한다.")
+    void isNameListEmptyReturnZero() {
+        Names names = new Names(List.of());
+
+        Assertions.assertEquals(0, names.size());
     }
 }
