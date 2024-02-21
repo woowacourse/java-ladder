da8f93ca1e5f16a292baa3a2a30190476daf8c60
hangillee <skfcb10@naver.com>
2024-02-21 15:21:58 +0900

    chore: 가독성 증진을 위한 개행 추가

--- src/main/java/controller/LadderController.java
+++ src/main/java/controller/LadderController.java
@@ -3,19 +3,19 @@ package controller;
 import domain.Height;
 import domain.Lines;
 import domain.Names;
+import java.util.List;
 import view.InputView;
 import view.ResultView;
-
-import java.util.List;
 
 public class LadderController {
     public void start() {
         List<String> rawNames = InputView.getNames();
         int rawHeight = InputView.getHeight();
+
         Names names = new Names(rawNames);
         Height height = new Height(rawHeight);
-
         Lines lines = new Lines(height.getValue(), names.getNames().size());
-        ResultView.printResult(names,lines);
+        
+        ResultView.printResult(names, lines);
     }
 }
