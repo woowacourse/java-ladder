389608d81dbb8cc4ba1f42541da8e850a8b4d74f
le2sky <leehaneul990623@gmail.com>
2024-02-21 18:50:26 +0900

    test: 라인이 인원 수 만큼의 막대를 갖는지 검증 테스트
    - red 단계

--- src/test/java/domain/LineTest.java
+++ src/test/java/domain/LineTest.java
@@ -1,14 +1,31 @@
 package domain;
 
+import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class LineTest {
+class LineTest {
+
     @DisplayName("생성 테스트")
     @Test
     void createLine() {
-        Assertions.assertThatCode(() -> new Line())
+        Assertions.assertThatCode(() -> new Line(filledStickGenerator(), 3))
                 .doesNotThrowAnyException();
+    }
+
+    @DisplayName("라인은 인원 수 만큼의 막대를 갖는다.")
+    @Test
+    void lineHasStick() {
+        int playerSize = 3;
+        Line line = new Line(filledStickGenerator(), playerSize);
+
+        List<Stick> sticks = line.getSticks();
+
+        Assertions.assertThat(sticks).hasSize(playerSize);
+    }
+
+    private StickGenerator filledStickGenerator() {
+        return () -> Stick.FILLED;
     }
 }
