48ebee27788ad2d259e0777113fb5a3682fce6ab
le2sky <leehaneul990623@gmail.com>
2024-02-21 20:27:38 +0900

    feat: Radder 높이 검증 기능 구현
    - green 단계

--- src/main/java/domain/Ladder.java
+++ src/main/java/domain/Ladder.java
@@ -1,7 +1,10 @@
 package domain;
 
 class Ladder {
-    public Ladder(int i, int i1) {
 
+    public Ladder(int height, int playerSize) {
+        if (height < 1) {
+            throw new IllegalArgumentException("사다리의 높이는 최소 1 이어야 합니다.");
+        }
     }
 }
