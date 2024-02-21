c1b82a4e09fec62ceaf0947b25acd279d2e9c751
tackyu <tack970913@gmail.com>
2024-02-21 19:42:42 +0900

    test: 높이 값 검증 테스트 작성

---
+++ src/test/java/domain/model/HeightTest.java
@@ -0,0 +1,26 @@
+package domain.model;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class HeightTest {
+
+    @Test
+    @DisplayName("사다리는 높이를 가진다.")
+    void heightTest() {
+        //given
+        Height height = new Height("1");
+        //when & then
+        Assertions.assertThat(height.getHeight()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("사다리의 높이가 음수일 경우 예외를 발생시킨다.")
+    void heightNegativeInputTest() {
+        Assertions.assertThatThrownBy(() -> new Height("0"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+    }
+
+}
