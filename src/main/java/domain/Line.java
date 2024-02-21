9bca8972404dcabc4696b5cf1f3e8519bfd62d1a
le2sky <leehaneul990623@gmail.com>
2024-02-21 18:51:51 +0900

    feat: 라인이 인원 수 만큼의 막대를 갖는 기능 구현
    - green 단계

--- src/main/java/domain/Line.java
+++ src/main/java/domain/Line.java
@@ -1,4 +1,23 @@
 package domain;
 
-public class Line {
+import java.util.ArrayList;
+import java.util.List;
+
+class Line {
+
+    private final List<Stick> sticks;
+
+    public Line(StickGenerator stickGenerator, int playerSize) {
+        List<Stick> sticks = new ArrayList<>();
+
+        for (int i = 0; i < playerSize; i++) {
+            sticks.add(stickGenerator.generateOne());
+        }
+
+        this.sticks = sticks;
+    }
+
+    public List<Stick> getSticks() {
+        return sticks;
+    }
 }
