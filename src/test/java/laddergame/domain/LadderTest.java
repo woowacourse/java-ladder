3ab899d945802d1065305308258f3bf800edf4d4
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-02-21 14:56:35 +0900

    test: 사다리 생성 검증
    Co-Authored-By: lemone <55480011+JiHyeonL@users.noreply.github.com>

---
+++ src/test/java/laddergame/domain/LadderTest.java
@@ -0,0 +1,25 @@
+package laddergame.domain;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("사다리")
+public class LadderTest {
+    @Test
+    @DisplayName("생성에 성공한다.")
+    public void createLadderTest() {
+        //given
+        final int height = 4;
+        final int playerCount = 4;
+
+        //when
+        Ladder ladder = new Ladder(playerCount, height);
+
+        //then
+        assertEquals(ladder.getLines().size(), height);
+        assertEquals(ladder.getLines().getPoints().size(), playerCount - 1);
+    }
+}
