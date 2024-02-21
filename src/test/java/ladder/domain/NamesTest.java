ff36438f241ea6de66785f768963eab58a441416
ay-eonii <naayen@naver.com>
2024-02-21 17:32:51 +0900

    test(NamesTest): 이름 공백 제거 검증

--- src/test/java/ladder/domain/NamesTest.java
+++ src/test/java/ladder/domain/NamesTest.java
@@ -3,16 +3,21 @@ package ladder.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.*;
 
 class NamesTest {
 
     @Test
     @DisplayName("사람 이름은 쉼표(,)를 기준으로 구분한다.")
     void createNames() {
-        assertThatCode(() -> new Names("pobi,honux,crong,jk"))
-                .doesNotThrowAnyException();
+        // given
+        Names names = new Names("pobi,honux,crong,jk");
+
+        // when
+        int count = names.count();
+
+        // then
+        assertThat(count).isEqualTo(4);
     }
 
     @Test
@@ -22,4 +27,14 @@ class NamesTest {
                 .isThrownBy(() -> new Names("pobipobi,honux,crong,jk"));
     }
 
+    @Test
+    @DisplayName("이름에 공백은 포함하지 않는다.")
+    void createValidNames() {
+        // given
+        String name = "  jk  ";
+
+        // when & then
+        assertThatCode(() -> new Names(name))
+                .doesNotThrowAnyException();
+    }
 }
