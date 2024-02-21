bb94793a9675452f32f24dfe28fc00c1e0d9482b
coli-geonwoo <kkwoo001021@naver.com>
2024-02-21 14:23:29 +0900

    style: 컨벤션 reformat

--- src/main/java/RandomLineGenerator.java
+++ src/main/java/RandomLineGenerator.java
@@ -2,13 +2,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RandomLineGenerator implements LineGenerator {
-
     @Override
     public List<Boolean> getLine(int peopleNumber) {
 
         List<Boolean> line = new ArrayList<>();
         Boolean before = Boolean.FALSE;
-
+        //TODO depth 줄이기
         for (int i = 0; i < peopleNumber - 1; i++) {
             if (before) {
                 line.add(false);
@@ -18,7 +17,6 @@ public class RandomLineGenerator implements LineGenerator {
             }
             before = line.get(i);
         }
-
         return line;
     }
 }
--- src/main/java/SuccessiveLineGenerator.java
+++ src/main/java/SuccessiveLineGenerator.java
@@ -2,8 +2,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SuccessiveLineGenerator implements LineGenerator {
-
-
     @Override
     public List<Boolean> getLine(int peopleNumber) {
         List<Boolean> line = new ArrayList<>();
