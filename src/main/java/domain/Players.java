86d47209d864ede0a52a2ea92187fcac4e518224
ehtjsv2 <ehtjsv2@naver.com>
2024-02-21 15:23:48 +0900

    refactor: Player의 수 검증 함수 분리

--- src/main/java/domain/Players.java
+++ src/main/java/domain/Players.java
@@ -4,13 +4,18 @@ import java.util.List;
 
 public class Players {
 
+    private static final int MINIMUM_PLAYER_COUNT = 2;
     private final List<Player> players;
 
     public Players(List<Player> players) {
-        if(players.size() <= 1) {
+        validatePlayerSize(players);
+        this.players = players;
+    }
+
+    private static void validatePlayerSize(List<Player> players) {
+        if(players.size() < MINIMUM_PLAYER_COUNT) {
             throw new IllegalArgumentException("[Error] 플레이어의 숫자는 2명 이상이어야 합니다.");
         }
-        this.players = players;
     }
 
     public List<Player> getPlayers() {
