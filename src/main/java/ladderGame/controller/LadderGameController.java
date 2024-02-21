a3443cb5063e85ee754edba74a2ef0553cdac578
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-02-21 16:03:25 +0900

    feat(LadderGameController) : 컨트롤러를 통해 뷰, 도메인 연결

--- src/main/java/ladderGame/controller/LadderGameController.java
+++ src/main/java/ladderGame/controller/LadderGameController.java
@@ -3,16 +3,23 @@ package ladderGame.controller;
 import ladderGame.model.Ladder;
 import ladderGame.model.Players;
 import ladderGame.view.InputView;
+import ladderGame.view.ResultView;
 
 public class LadderGameController {
-    private InputView inputView;
+    private final InputView inputView;
+    private final ResultView resultView;
 
     public LadderGameController() {
         this.inputView = new InputView();
+        this.resultView = new ResultView();
     }
 
     public void run() {
         Players players = new Players(inputView.inputPlayerNames());
         Ladder ladder = new Ladder(inputView.inputMaxLadderHeight(), players.getPlayerSize());
+
+        resultView.printResultPrompt();
+        resultView.printPlayerNames(players.getPlayers());
+        resultView.printLadder(ladder.getLines());
     }
 }
