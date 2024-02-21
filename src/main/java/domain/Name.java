3104b29bbdb229f3c340fe0f5c5687f693abb677
seunghye218 <seunghye@student.42seoul.kr>
2024-02-21 20:50:47 +0900

    feat: OutputView 클래스 구현
    Co-authored-by: 3juhwan <13selfesteem91@naver.com>

--- src/main/java/domain/Name.java
+++ src/main/java/domain/Name.java
@@ -13,11 +13,15 @@ public class Name {
             throw new IllegalArgumentException("이름의 길이는 1 ~ 5 자 이어야 합니다.");
         }
 
-        if (!Pattern.matches("[a-zA-Z0-9]", name)) {
+        if (!Pattern.matches("^[a-zA-Z0-9]+", name)) {
             throw new IllegalArgumentException("이름이 영어와 숫자가 아니라면 예외가 발생한다");
         }
 
         this.name = name;
+    }
+
+    public String getName() {
+        return name;
     }
 
     @Override
---
+++ src/main/java/view/OutputView.java
@@ -0,0 +1,31 @@
+package view;
+
+import domain.Bridge;
+import domain.Name;
+import java.util.List;
+
+public class OutputView {
+
+    public static void printLadder(List<Name> names, List<Bridge> bridges, int height) {
+        System.out.println("실행 결과\n");
+
+        for (Name name : names) {
+            System.out.printf("%6s", name.getName());
+        }
+        System.out.println();
+
+        for (int y = 0; y < height; y++) {
+            System.out.print("     |");
+            for (int x = 0; x < names.size() - 1; x++) {
+                Bridge bridge = new Bridge(x, x + 1, y);
+                if (bridges.contains(bridge)) {
+                    System.out.print("-----|");
+                } else {
+                    System.out.print("     |");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+}
