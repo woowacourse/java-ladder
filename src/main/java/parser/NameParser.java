86115408ecf49ec2fcb9b47b918b66d2d86752e8
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-02-21 15:00:24 +0900

    feat(NameParser): 입력받은 이름을 파싱하는 로직 작성
    - InputView 에 입력받은 텍스트를 파싱해 전달하는 메서드 작성
    
    Co-authored-by: HaiSeong <wjdgotjd9908@gmail.com>

--- src/main/java/parser/NameParser.java
+++ src/main/java/parser/NameParser.java
@@ -1,10 +1,13 @@
 package parser;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class NameParser {
 
     public static List<String> parse(String text) {
-        return null;
+        return Arrays.stream(text.split(","))
+                .map(String::trim)
+                .toList();
     }
 }
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,7 +1,14 @@
 package view;
 
+import java.util.List;
+import java.util.Scanner;
+import parser.NameParser;
+
 public class InputView {
-    public String inputNames() {
-        return null;
+    private final Scanner scanner = new Scanner(System.in);
+
+    public List<String> inputNames() {
+        String text = scanner.next();
+        return NameParser.parse(text);
     }
 }
