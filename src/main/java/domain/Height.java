6fac1f23ea04a3c6432be8700300c261e4538c23
J-I-H-O <jeongjiho0731@gmail.com>
2024-02-21 17:32:34 +0900

    feat: 사다리 높이 범위 검증 기능 구현
    Co-authored-by: J-I-H-O <jeongjiho0731@gmail.com>
    Co-authored-by: Nam Gi Beom <rlqja0523@naver.com>

---
+++ src/main/java/domain/Height.java
@@ -0,0 +1,24 @@
+package domain;
+
+public class Height {
+    private static final String RANGE_EXCEPTION_MESSAGE = "[ERROR] %d이상 %d이하의 숫자만 입력 가능합니다.";
+    private static final int MINIMUM_HEIGHT = 1;
+    private static final int MAXIMUM_HEIGHT = 100;
+
+    private final int height;
+
+    public Height(final int height) {
+        validateRange(height);
+        this.height = height;
+    }
+
+    private void validateRange(int height) {
+        if (height < MINIMUM_HEIGHT || height > MAXIMUM_HEIGHT) {
+            throw new IllegalArgumentException(String.format(RANGE_EXCEPTION_MESSAGE, MINIMUM_HEIGHT, MAXIMUM_HEIGHT));
+        }
+    }
+
+    public int getHeight() {
+        return this.height;
+    }
+}
