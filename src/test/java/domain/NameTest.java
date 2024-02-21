951f3dd3bf03167c6d6c0b6277d01b96d5605229
SungKyum Kim <sungkyum1@naver.com>
2024-02-21 16:28:02 +0900

    test: 이름 길이 검증 기능 테스트 작성

--- src/test/java/domain/NameTest.java
+++ src/test/java/domain/NameTest.java
@@ -10,7 +10,15 @@ class NameTest {
     @DisplayName("사람 이름은 5글자를 넘을 수 없다.")
     void isNameOverMaxLengthLimit() {
         String name = "pobipobi";
-        
+
+        assertThrows(IllegalArgumentException.class, () -> new Name(name));
+    }
+
+    @Test
+    @DisplayName("이름은 2글자 이상이어야 한다.")
+    void isValidName() {
+        String name = "p";
+
         assertThrows(IllegalArgumentException.class, () -> new Name(name));
     }
 }
