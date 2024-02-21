5a1746869fbeb9ad10d60c7d838bb62f33140c8d
robinjoon <robin980108@naver.com>
2024-02-21 16:49:16 +0900

    feat: 예외처리 기능 및 main 메서드 추가

---
+++ src/main/java/Main.java
@@ -0,0 +1,38 @@
+import domain.LadderGame;
+import java.util.function.Supplier;
+import view.InputView;
+import view.OutPutView;
+
+public class Main {
+    public static void main(String[] args) {
+        LadderGame ladderGame = RetryHelper.retry(() -> {
+            OutPutView.print("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
+            String nameInput = InputView.getInput();
+            OutPutView.print("최대 사다리 높이는 몇 개인가요?");
+            int ladderHeight = Integer.parseInt(InputView.getInput());
+            return new LadderGame(nameInput, ladderHeight);
+        });
+        OutPutView.print(ladderGame.getNamesString());
+        OutPutView.print(ladderGame.getLadderString());
+    }
+
+    static final class RetryHelper {
+
+        public static <E> E retry(Supplier<E> supplier) {
+            E result = null;
+            while (result == null) {
+                result = useSupplier(supplier);
+            }
+            return result;
+        }
+
+        private static <E> E useSupplier(Supplier<E> supplier) {
+            try {
+                return supplier.get();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                return null;
+            }
+        }
+    }
+}
