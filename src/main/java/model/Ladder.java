8c89ecc1a6e187014684ee1e67eb9157cfc0da51
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-02-21 18:26:00 +0900

    feat(Ladder, LadderTest): 사다리의 정적 팩토리 메서드에 인원수 인자 추가
    Co-authored-by: HaiSeong <wjdgotjd9908@gmail.com>

--- src/main/java/model/Ladder.java
+++ src/main/java/model/Ladder.java
@@ -1,6 +1,8 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Ladder {
     private final List<Line> lines;
@@ -9,17 +11,21 @@ public class Ladder {
         this.lines = lines;
     }
 
-    public static Ladder from(int height) {
+    public static Ladder from(int height, int personCount) {
         validateHeight(height);
-        // TODO:
-//        IntStream.range(0, height)
-//                .();
-        return new Ladder(null);
+        List<Line> lines = new ArrayList<>();
+        IntStream.range(0, height)
+                .forEach(i -> lines.add(new Line(personCount)));
+        return new Ladder(lines);
     }
 
     private static void validateHeight(int height) {
         if (height < 1) {
             throw new IllegalArgumentException("사다리의 높이는 1 이상이어야 합니다.");
         }
+    }
+
+    public List<Line> getLines() {
+        return lines;
     }
 }
--- src/test/java/model/LadderTest.java
+++ src/test/java/model/LadderTest.java
@@ -1,9 +1,7 @@
 package model;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,14 +13,14 @@ class LadderTest {
     @ParameterizedTest(name = "사다리의 높이는 1 이상이다.")
     @ValueSource(ints = {1, 5})
     void createLadder(int height) {
-        assertThatCode(() -> Ladder.from(height));
+        assertThatCode(() -> Ladder.from(height, 4));
     }
 
     @Test
     @DisplayName("사다리의 높이는 1 미만은 불가능이다.")
     void createLadderThrowException() {
         int height = 0;
-        assertThatThrownBy(() -> Ladder.from(height))
+        assertThatThrownBy(() -> Ladder.from(height, 4))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("사다리의 높이는 1 이상이어야 합니다.");
     }
