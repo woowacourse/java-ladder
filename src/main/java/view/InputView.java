bce37e558e60d642d123b6204eef26a760412cb2
leetaehoon <xogns1514@gmail.com>
2024-02-21 20:36:42 +0900

    feat: 최대 높이 숫자 검증 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -27,7 +27,9 @@ public class InputView {
 
     public static int inputHeight() {
         System.out.println("최대 사다리 높이는 몇 개인가요?");
-        return Integer.parseInt(SCANNER.nextLine());
+        String rawHeight = SCANNER.nextLine();
+        validateHeight(rawHeight);
+        return Integer.parseInt(rawHeight);
     }
 
     private static void validateDelimiter(final String userNames) {
@@ -38,5 +40,15 @@ public class InputView {
         if (userNames.startsWith(",")) {
             throw new IllegalArgumentException(String.format("입력된 값: %s, 구분자로 끝날 수 없습니다.", userNames));
         }
+    }
+
+    private static void validateHeight(String height) {
+        if (isNumeric(height)) {
+            throw new IllegalArgumentException(String.format("입력된 값: %s, 숫자를 입력해주세요.", height));
+        }
+    }
+
+    private static boolean isNumeric(String input) {
+        return !input.matches("-?\\d+");
     }
 }
