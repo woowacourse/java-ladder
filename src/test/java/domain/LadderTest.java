f8d28838bf55c95baffc419e8119e70610449910
le2sky <leehaneul990623@gmail.com>
2024-02-21 19:06:44 +0900

    test: 사다리의 높이 검증 테스트
    - red 단계

--- src/test/java/domain/LadderTest.java
+++ src/test/java/domain/LadderTest.java
@@ -1,6 +1,7 @@
 package domain;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,5 +13,12 @@ class LadderTest {
     void createLadder() {
         assertThatCode(() -> new Ladder(3, 2))
                 .doesNotThrowAnyException();
+    }
+
+    @DisplayName("사다리의 높이는 1 이상만 허용한다.")
+    @Test
+    void checkLadderHeight() {
+        assertThatThrownBy(() -> new Ladder(0, 2))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
