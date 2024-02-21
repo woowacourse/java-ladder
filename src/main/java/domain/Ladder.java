00d90294773332a568f0eeb2e1df12728be1540c
le2sky <leehaneul990623@gmail.com>
2024-02-21 20:34:18 +0900

    test: 사다리가 높이 만큼의 라인을 갖는지 검증 테스트
    - red 단계

--- src/main/java/domain/Ladder.java
+++ src/main/java/domain/Ladder.java
@@ -1,10 +1,16 @@
 package domain;
 
+import java.util.List;
+
 class Ladder {
 
     public Ladder(int height, int playerSize) {
         if (height < 1) {
             throw new IllegalArgumentException("사다리의 높이는 최소 1 이어야 합니다.");
         }
+    }
+
+    public List<Line> getLine() {
+        return null;
     }
 }
--- src/test/java/domain/LadderTest.java
+++ src/test/java/domain/LadderTest.java
@@ -1,8 +1,10 @@
 package domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -20,5 +22,15 @@ class LadderTest {
     void checkLadderHeight() {
         assertThatThrownBy(() -> new Ladder(0, 2))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("사다리는 높이 만큼의 라인을 갖는다.")
+    @Test
+    void ladderHasLines() {
+        int height = 3;
+        Ladder ladder = new Ladder(height, 9);
+        List<Line> lines = ladder.getLine();
+
+        assertThat(lines).hasSize(height);
     }
 }
