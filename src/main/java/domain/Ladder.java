bb9858e1bb4eb48875044b0c553fc7d9133a700d
eunjungL <dldmswjd322@gmail.com>
2024-02-21 17:13:46 +0900

    feat: 사다리 생성 기능 추가

--- src/main/java/domain/Ladder.java
+++ src/main/java/domain/Ladder.java
@@ -1,20 +1,38 @@
 package domain;
 
+import util.BooleanGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class Ladder {
 
     private static final Pattern HEIGHT_FORMAT_REGEX = Pattern.compile("^[1-9][0-9]*$");
+
     private final int height;
+    private final List<Line> ladder;
 
     public Ladder(String height) {
         validateHeight(height);
         this.height = Integer.parseInt(height);
+        this.ladder = new ArrayList<>();
     }
 
     private void validateHeight(String height) {
         if (height == null || !HEIGHT_FORMAT_REGEX.matcher(height).matches()) {
             throw new IllegalArgumentException("사다리의 최대 높이는 자연수여야 합니다.");
         }
+    }
+
+    public List<Line> makeLadder(int columnLength, BooleanGenerator booleanGenerator) {
+        for (int i = 0; i < height; i++) {
+            Line line = new Line(columnLength);
+            line.makeLine(booleanGenerator);
+
+            ladder.add(line);
+        }
+
+        return ladder;
     }
 }
