443890323d84bd564d7dc709c4083daa1508d212
Zeus6768 <akaps6768@gmail.com>
2024-02-21 16:44:28 +0900

    test(domain): 테스트 예외케이스 추가

--- src/test/java/ladder/domain/LineTest.java
+++ src/test/java/ladder/domain/LineTest.java
@@ -1,8 +1,10 @@
 package ladder.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import static ladder.domain.Direction.LEFT;
+import static ladder.domain.Direction.NONE;
 import static ladder.domain.Direction.RIGHT;
 
 import org.junit.jupiter.api.DisplayName;
@@ -13,13 +15,19 @@ class LineTest {
     @DisplayName("라인에서 움직일 방향을 표현한다.")
     @Test
     void lineTest() {
-        LineGenerator lineGenerator = () -> RIGHT;
+        Line line1 = new Line(4, () -> RIGHT);
+        Line line2 = new Line(4, () -> NONE);
 
-        Line line = new Line(4, lineGenerator);
+        assertAll(
+                () -> assertThat(line1.getDirectionAt(0)).isEqualTo(RIGHT),
+                () -> assertThat(line1.getDirectionAt(1)).isEqualTo(LEFT),
+                () -> assertThat(line1.getDirectionAt(2)).isEqualTo(RIGHT),
+                () -> assertThat(line1.getDirectionAt(3)).isEqualTo(LEFT),
 
-        assertThat(line.getDirectionAt(0)).isEqualTo(RIGHT);
-        assertThat(line.getDirectionAt(1)).isEqualTo(LEFT);
-        assertThat(line.getDirectionAt(2)).isEqualTo(RIGHT);
-        assertThat(line.getDirectionAt(3)).isEqualTo(LEFT);
+                () -> assertThat(line2.getDirectionAt(0)).isEqualTo(NONE),
+                () -> assertThat(line2.getDirectionAt(1)).isEqualTo(NONE),
+                () -> assertThat(line2.getDirectionAt(2)).isEqualTo(NONE),
+                () -> assertThat(line2.getDirectionAt(3)).isEqualTo(NONE)
+        );
     }
 }
