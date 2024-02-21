a8c01db098cde8ff9ef4d6a471e3d7b37276dd3f
devhoya97 <diajbh123@gmail.com>
2024-02-21 15:42:37 +0900

    feat : 높이만큼 사다리 생성 기능

--- src/main/java/service/LadderGame.java
+++ src/main/java/service/LadderGame.java
@@ -1,10 +1,18 @@
 package service;
 
 import domain.Ladder;
+import domain.Line;
+import domain.RandomPointGenerator;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LadderGame {
 
     public Ladder createLadder(final int personCount, final int height) {
-        return null;
+        final List<Line> lines = new ArrayList<>();
+        for (int i = 0; i < height; i++) {
+            lines.add(Line.create(personCount, RandomPointGenerator.getInstance()));
+        }
+        return new Ladder(lines);
     }
 }
