348c65fac5901b89c301484eb738e88434521171
devhoya97 <diajbh123@gmail.com>
2024-02-21 18:23:21 +0900

    feat : 사다리 출력 기능

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,5 +1,6 @@
 package view;
 
+import domain.Line;
 import java.util.List;
 import java.util.StringJoiner;
 
@@ -12,5 +13,21 @@ public class OutputView {
             nameJoiner.add(String.format("%5s", name));
         }
         System.out.println(nameJoiner);
+    }
+
+    public void printLadder(List<Line> lines){
+        StringJoiner lineJoiner = new StringJoiner(System.lineSeparator());
+        for (final Line line : lines) {
+            StringJoiner pointJoiner = new StringJoiner("|","    |","|");
+            for (boolean points : line.getPoints()){
+                if (points) {
+                    pointJoiner.add("-----");
+                    continue;
+                }
+                pointJoiner.add("     ");
+            }
+            lineJoiner.add(pointJoiner.toString());
+        }
+        System.out.println(lineJoiner);
     }
 }
