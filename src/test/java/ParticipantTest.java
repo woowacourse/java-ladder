62ee636fc6a56c8dbf1f830809f2fc22cebe902d
yoonjuho <dbswn990@naver.com>
2024-02-21 15:05:45 +0900

    test(ParticipantTest): 참가자의 이름은 null 일 수 없다.

--- src/test/java/ParticipantTest.java
+++ src/test/java/ParticipantTest.java
@@ -2,6 +2,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class ParticipantTest {
 
     @DisplayName("참여자의 이름은 최대 5글자까지 부여할 수 있다.")
@@ -17,6 +19,14 @@ public class ParticipantTest {
     void participantName() {
         Assertions.assertThatCode(() -> new Participant("pobi"))
                 .doesNotThrowAnyException();
+    }
+
+    @DisplayName("참가자의 이름은 null 일 수 없다")
+    @Test
+    void participantNameIsNull() {
+        assertThatThrownBy(() -> new Participant(null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("참가자의 이름은 null 이거나 공백일 수 없습니다.");
     }
 
 }
