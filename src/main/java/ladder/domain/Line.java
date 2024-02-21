cbccf1b49070845b559589c67b53c76373d22b17
ay-eonii <naayen@naver.com>
2024-02-21 14:35:34 +0900

    feat(Line): 연속된 두 번째 true는 false로 변경

--- src/main/java/ladder/domain/Line.java
+++ src/main/java/ladder/domain/Line.java
@@ -7,9 +7,29 @@ import java.util.List;
 public class Line {
 
     private final List<Boolean> points;
+    private final BooleanListGenerator booleanListGenerator;
 
     public Line(int personCount, BooleanListGenerator booleanListGenerator) {
-        this.points = booleanListGenerator.generate(personCount - 1);
+        List<Boolean> points = booleanListGenerator.generate(personCount - 1);
+        this.booleanListGenerator = booleanListGenerator;
+        this.points = temp(points);
+
+    }
+
+    private List<Boolean> temp(List<Boolean> points) {
+        if (!points.contains(true)) {
+            return temp(booleanListGenerator.generate(points.size()));
+        }
+        for (int i = 1; i < points.size(); i++) {
+            if (points.get(i - 1)) {
+                points.set(i, false);
+            }
+        }
+        return points;
+    }
+
+    public void generate(BooleanListGenerator booleanListGenerator) {
+
     }
 
     public List<Boolean> getPoints() {
--- src/main/java/ladder/util/RandomBooleanListGenerator.java
+++ src/main/java/ladder/util/RandomBooleanListGenerator.java
@@ -2,14 +2,16 @@ package ladder.util;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class RandomBooleanListGenerator implements BooleanListGenerator {
 
     @Override
     public List<Boolean> generate(int size) {
-        ArrayList<Boolean> list = new ArrayList<>();
+        List<Boolean> list = new ArrayList<>();
         for (int i = 0; i < size; i++) {
-            list.add(false);
+            boolean value = new Random().nextBoolean();
+            list.add(value);
         }
         return list;
     }
