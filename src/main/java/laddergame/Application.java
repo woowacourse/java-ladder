ce596aeae67125739be47b839d9b281aacdc9976
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-02-21 17:03:00 +0900

    feat: 컨트롤러 생성 후 사다리 건설 구현
    Co-Authored-By: lemone <55480011+JiHyeonL@users.noreply.github.com>

--- src/main/java/laddergame/Application.java
+++ src/main/java/laddergame/Application.java
@@ -1,17 +1,7 @@
 package laddergame;
 
-import java.io.IOException;
-import laddergame.view.InputView;
-
 public class Application {
     public static void main(String[] args) {
-        try {
-            InputView inputView = new InputView();
-            System.out.println(inputView.readPlayersName());
-            System.out.println(inputView.readLadderHeight());
-        } catch (IOException exception) {
-
-        }
-
+        new LadderGame().run();
     }
 }
---
+++ src/main/java/laddergame/LadderGame.java
@@ -0,0 +1,31 @@
+package laddergame;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.IntStream;
+import laddergame.domain.Ladder;
+import laddergame.domain.strategy.CanBuildStrategy;
+import laddergame.domain.strategy.RandomBuildStrategy;
+import laddergame.view.InputView;
+
+public class LadderGame {
+
+    public void run() {
+        try {
+            InputView inputView = new InputView();
+            List<String> players = inputView.readPlayersName();
+            System.out.println();
+            int height = inputView.readLadderHeight();
+
+            Ladder ladder = new Ladder(players.size(), height);
+
+            CanBuildStrategy randomBuildStrategy = new RandomBuildStrategy();
+            List<List<Boolean>> randomResult = IntStream.range(0, height).mapToObj(i ->
+                    randomBuildStrategy.canBuildBridges(players.size() - 1)).toList();
+
+            ladder.build(randomResult);
+        } catch (IOException exception) {
+
+        }
+    }
+}
