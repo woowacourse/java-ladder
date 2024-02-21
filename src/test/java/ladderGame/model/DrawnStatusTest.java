53f0fd80f7cd32328b66e2f95a5fd421d7d23d58
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-02-21 18:00:33 +0900

    test(DrawnStatusTest) : DrawnStatus에 따라 true 또는 false를 반환한다(Test Fails)

---
+++ src/test/java/ladderGame/model/DrawnStatusTest.java
@@ -0,0 +1,26 @@
+package ladderGame.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DrawnStatusTest {
+
+    @Test
+    @DisplayName("DrawnStatus가 DRAWN이면 true를 반환한다.")
+    void checkDrawnIsTrue() {
+        DrawnStatus drawnStatus = DrawnStatus.DRAWN;
+
+        assertTrue(drawnStatus.checkDrawn());
+    }
+
+    @Test
+    @DisplayName("DrawnStatus가 NON_DRAWN이면 false를 반환한다.")
+    void checkNonDrawnIsFalse() {
+        DrawnStatus drawnStatus = DrawnStatus.NON_DRAWN;
+
+        assertFalse(drawnStatus.checkDrawn());
+    }
+
+}
