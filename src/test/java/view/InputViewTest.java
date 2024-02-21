5fc756aad85326cd360fbd0b4870a253101d48e0
kelly6bf <kelly6bf@gmail.com>
2024-02-21 15:04:23 +0900

    test: 사용자 이름에 공백을 넣을시 예외를 발생시키는 테스트 로직 작성

--- src/test/java/view/InputViewTest.java
+++ src/test/java/view/InputViewTest.java
@@ -1,8 +1,13 @@
 package view;
 
 import ladder.view.InputView;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
@@ -17,4 +22,13 @@ public class InputViewTest {
         assertThat(names).containsExactly("a", "b", "c");
     }
 
+    @DisplayName("사용자 이름으로 공백이 입력되면 예외를 던진다.")
+    @NullAndEmptySource
+    @ParameterizedTest
+    @ValueSource(strings = {" ", "  ", "\b", "\n"})
+    void validateNamesWithNullOrEmpty(final String input) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> InputView.readNames(() -> input))
+                .withMessage("사용자 이름으로 공백을 넣을 수 없습니다.");
+    }
 }
