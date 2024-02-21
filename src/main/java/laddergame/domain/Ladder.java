3cb2795300623240d8570709d156cb886402820f
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-02-21 15:09:55 +0900

    refactor: 사다리 생성자 반복문을 stream으로 수정
    Co-Authored-By: lemone <55480011+JiHyeonL@users.noreply.github.com>

--- src/main/java/laddergame/domain/Ladder.java
+++ src/main/java/laddergame/domain/Ladder.java
@@ -2,16 +2,16 @@ package laddergame.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Ladder {
     private final List<Line> lines;
 
     public Ladder(final int playerCount, final int height) {
-        ArrayList<Line> list = new ArrayList<>();
-        IntStream.range(0, height)
-                .forEach(i -> list.add(new Line(playerCount)));
-        this.lines = list;
+        this.lines = IntStream.range(0, height)
+                .mapToObj(i -> new Line(playerCount))
+                .collect(Collectors.toList());
     }
 
     public List<Line> getLines() {
