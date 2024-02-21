a3b6846cec73c7695c3c567088c81ebeac3df454
devhoya97 <diajbh123@gmail.com>
2024-02-21 18:57:45 +0900

    refactor : 높이 원시값 포장

---
+++ src/main/java/domain/Height.java
@@ -0,0 +1,13 @@
+package domain;
+
+public class Height {
+    private final int height;
+
+    public Height(final int height) {
+        this.height = height;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
