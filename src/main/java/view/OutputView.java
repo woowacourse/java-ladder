fa68d69faec6baa533852e857e425402f1d095d8
hyxrxn <gpfls981220@gmail.com>
2024-02-21 16:15:01 +0900

    refactor: OutputView 클래스 메서드 분리
    Co-authored-by: shin-jisong <songjin1127@naver.com>

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -9,23 +9,38 @@ public class OutputView {
 
     public void printResult(List<String> participantsName, Ladder ladder) {
         System.out.println("\n실행결과\n");
+        printNames(participantsName);
+        printLadder(ladder);
+    }
+
+    private void printNames(List<String> participantsName) {
         for (String name : participantsName) {
             System.out.printf("%5s ", name);
         }
         System.out.println();
+    }
 
+    private void printLadder(Ladder ladder) {
         List<Line> lines = ladder.getLines();
         for (Line line : lines) {
-            List<Boolean> points = line.getPoints();
-            System.out.print("    |");
-            for (Boolean point : points) {
-                if (point) {
-                    System.out.print("-----|");
-                    continue;
-                }
-                System.out.print("     |");
-            }
-            System.out.println();
+            printOneLine(line);
         }
+    }
+
+    private void printOneLine(Line line) {
+        List<Boolean> points = line.getPoints();
+        System.out.print("    |");
+        for (Boolean point : points) {
+            printOnePoint(point);
+        }
+        System.out.println();
+    }
+
+    private void printOnePoint(Boolean point) {
+        if (point) {
+            System.out.print("-----|");
+            return;
+        }
+        System.out.print("     |");
     }
 }
