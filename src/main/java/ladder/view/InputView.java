6383b810b766ef2d79f754d67ae4f13ebe83af46
jongmee <ra123221@gmail.com>
2024-02-21 17:47:59 +0900

    refactor: 참가자 이름 구분자 상수 이름 변경

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
-    private static final String DELIMITER = ",";
+    private static final String NAME_DELIMITER = ",";
     private static final String REQUEST_PARTICIPANTS_NAME = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     private static final String REQUEST_LADDER_HEIGHT = "최대 사다리 높이는 몇 개인가요?";
 
@@ -17,18 +17,18 @@ public class InputView {
     public List<String> readParticipantsName() {
         System.out.println(REQUEST_PARTICIPANTS_NAME);
         String names = scanner.nextLine();
-        validateDelimiterPosition(names);
+        validateNameDelimiterPosition(names);
         return splitNames(names);
     }
 
-    private void validateDelimiterPosition(final String names) {
-        if (names.startsWith(DELIMITER) || names.endsWith(DELIMITER)) {
+    private void validateNameDelimiterPosition(final String names) {
+        if (names.startsWith(NAME_DELIMITER) || names.endsWith(NAME_DELIMITER)) {
             throw new DelimiterBoundaryException();
         }
     }
 
     private List<String> splitNames(final String names) {
-        return Arrays.stream(names.split(DELIMITER))
+        return Arrays.stream(names.split(NAME_DELIMITER))
                 .toList();
     }
 
