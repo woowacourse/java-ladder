9a3d915a53ddfc5eae97808aa25696a9ba0845ef
Zeus6768 <akaps6768@gmail.com>
2024-02-21 17:25:25 +0900

    refactor(view): 문자열 포매팅

--- src/main/java/ladder/view/ResultView.java
+++ src/main/java/ladder/view/ResultView.java
@@ -2,11 +2,17 @@ package ladder.view;
 
 import static ladder.domain.Direction.RIGHT;
 
+import java.util.StringJoiner;
+
 import ladder.domain.Direction;
 import ladder.domain.Ladder;
 import ladder.domain.People;
 
 public class ResultView {
+
+    private static final String NAME_FORMAT = "%5s";
+    private static final String LINE = "|-----";
+    private static final String EMPTY_LINE = "|     ";
 
     public static void printResult(People people, Ladder ladder) {
         printPeople(people);
@@ -14,29 +20,30 @@ public class ResultView {
     }
 
     private static void printPeople(People people) {
-        people.stream().forEach(name -> System.out.print(name + "\t"));
-        System.out.println();
+        StringJoiner stringJoiner = new StringJoiner(" ");
+        people.stream().forEach(name -> stringJoiner.add(NAME_FORMAT.formatted(name)));
+        System.out.println(stringJoiner);
     }
 
     private static void printLadder(Ladder ladder) {
         for (int currentHeight = 0; currentHeight < ladder.getHeight(); currentHeight++) {
-            printLine(ladder, currentHeight);
+            printLadderLevel(ladder, currentHeight);
             System.out.println();
         }
     }
 
-    private static void printLine(Ladder ladder, int currentHeight) {
+    private static void printLadderLevel(Ladder ladder, int currentHeight) {
         System.out.print("\t");
         for (int personIndex = 0; personIndex < ladder.getPeopleCount(); personIndex++) {
-            printDirections(ladder.getDirection(currentHeight, personIndex));
+            printLineIfPresent(ladder.getDirection(currentHeight, personIndex));
         }
     }
 
-    private static void printDirections(Direction direction) {
+    private static void printLineIfPresent(Direction direction) {
         if (direction == RIGHT) {
-            System.out.print("|-----");
+            System.out.print(LINE);
             return;
         }
-        System.out.print("|     ");
+        System.out.print(EMPTY_LINE);
     }
 }
