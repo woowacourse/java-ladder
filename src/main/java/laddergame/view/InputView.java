87cad3771c91efcd514e45b7521c98a73438cb42
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-02-21 18:47:17 +0900

    feat: 플레이어 이름 앞 뒤 공백 제거
    Co-Authored-By: lemone <55480011+JiHyeonL@users.noreply.github.com>

--- src/main/java/laddergame/view/InputView.java
+++ src/main/java/laddergame/view/InputView.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class InputView {
@@ -14,7 +13,10 @@ public class InputView {
     public List<String> readPlayersName() throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
-        return List.of(bufferedReader.readLine().split(","));
+
+        return Arrays.stream(bufferedReader.readLine().split(","))
+                .map(String::trim)
+                .toList();
     }
 
     public int readLadderHeight() throws IOException {
