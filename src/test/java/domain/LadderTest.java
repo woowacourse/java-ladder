84da65753dec691bc68f043152e05f6b22c4b901
nhlee98 <e5slnh9876@gmail.com>
2024-02-21 17:49:53 +0900

    test: 입력받은 높이의 사다리를 생성한다

---
+++ src/test/java/domain/LadderTest.java
@@ -0,0 +1,36 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("사다리")
+public class LadderTest {
+    @DisplayName("생성한다.")
+    @Test
+    void createTest() {
+        // given
+        LadderHeight ladderHeight = LadderHeight.from("3");
+        People people = People.from("pobi,nak,seya");
+        List<Line> expected = List.of(
+                new Line(List.of(true, false, true)),
+                new Line(List.of(false, true, false)),
+                new Line(List.of(true, false, false)));
+
+        // when
+        Ladder ladder = Ladder.create((width, height) ->
+                List.of(
+                        new Line(List.of(true, false, true)),
+                        new Line(List.of(false, true, false)),
+                        new Line(List.of(true, false, false))), people, ladderHeight);
+
+        // then
+        assertThat(ladder)
+                .extracting("lines")
+                .usingRecursiveComparison()
+                .isEqualTo(expected);
+    }
+}
