50b2af0bc4cdfd45865209a8b70cb1ba56c12bf0
nayonsoso <panda0329@naver.com>
2024-02-21 15:05:14 +0900

    style: 구글 자바 컨벤션에 맞도록 EOF 변경

--- src/test/java/domain/GameTest.java
+++ src/test/java/domain/GameTest.java
@@ -15,7 +15,7 @@ class GameTest {
 
     @DisplayName("게임에 사람들이 참여한다.")
     @Test
-    void playersParticipate(){
+    void playersParticipate() {
         Game game = new Game("아톰, 산초");
         Assertions.assertThat(game.getPlayers())
                 .containsExactly(new Player("아톰"), new Player("산초"));
