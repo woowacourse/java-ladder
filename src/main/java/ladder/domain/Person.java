2be6b9a42f8f335a9c71aa31d1a22238a28320ca
nhlee98 <e5slnh9876@gmail.com>
2024-02-21 23:34:27 +0900

    refactor: 안쓰는 메소드 제거

--- src/main/java/ladder/domain/Person.java
+++ src/main/java/ladder/domain/Person.java
@@ -1,7 +1,5 @@
 package ladder.domain;
 
-import java.util.Objects;
-
 public class Person {
     private static final int MAX_LENGTH = 5;
 
@@ -16,22 +14,5 @@ public class Person {
         if (name.length() > MAX_LENGTH) {
             throw new IllegalArgumentException("[ERROR] 사람 이름의 길이는 5자를 넘을 수 없습니다.");
         }
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || !(o instanceof Person)) {
-            return false;
-        }
-
-        return ((Person) o).name.equals(this.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name);
     }
 }
