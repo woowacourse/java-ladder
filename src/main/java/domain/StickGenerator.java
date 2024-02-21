a8bd0bb17320656ec0b1d409b56a3a0cd662b86a
le2sky <leehaneul990623@gmail.com>
2024-02-21 18:44:15 +0900

    refactor: StickGenerator 인터페이스 추출
    - blue 단계

--- src/main/java/domain/StickGenerator.java
+++ src/main/java/domain/StickGenerator.java
@@ -1,18 +1,6 @@
 package domain;
 
-class StickGenerator {
-    
-    public Stick generate(int i) {
-        validateCommand(i);
-        if (i == 0) {
-            return Stick.EMPTY;
-        }
-        return Stick.FILLED;
-    }
+interface StickGenerator {
 
-    private void validateCommand(int i) {
-        if (i != 0 && i != 1) {
-            throw new IllegalArgumentException("0과 1이 아닌 값은 허용하지 않습니다.");
-        }
-    }
+    Stick generateOne();
 }
