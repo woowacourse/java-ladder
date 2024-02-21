0c96f1f7863a3525e3051ae38d8f8922cc4fd645
hangillee <skfcb10@naver.com>
2024-02-21 17:01:26 +0900

    refactor: 구현 변경에 의한 테스트 코드 수정

--- src/test/java/domain/NamesTest.java
+++ src/test/java/domain/NamesTest.java
@@ -1,14 +1,13 @@
 package domain;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import java.util.List;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class NamesTest {
     @Test
@@ -29,11 +28,9 @@ class NamesTest {
     }
 
     @Test
-    @DisplayName("전체 사람 수는 이름 리스트가 비어있다면 0을 반환한다.")
+    @DisplayName("전체 사람 수는 이름 리스트가 비어있다면 예외가 발생한다.")
     void isNameListEmptyReturnZero() {
-        Names names = new Names(List.of());
-
-        assertEquals(0, names.size());
+        assertThrows(IllegalArgumentException.class, () -> new Names(List.of()));
     }
 
     @Test
