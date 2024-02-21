f7fdfd1778bfe78546953c3981e8eeeb8b54caa0
libienz <leekh10195@gmail.com>
2024-02-21 15:42:53 +0900

    feat: Name 속성 정의 및 검증 구현

---
+++ src/main/java/domain/Name.java
@@ -0,0 +1,27 @@
+package domain;
+
+public class Name {
+    private static final Integer MIN_LENGTH = 1;
+    private static final Integer MAX_LENGTH = 5;
+
+    private final String name;
+
+    public Name(String name) {
+        validateLength(name);
+        this.name = name;
+    }
+
+    private void validateLength(String name) {
+        if (isInvalidLengthName(name)) {
+            throw new IllegalArgumentException("[ERROR] 이름의 길이는 " + MIN_LENGTH + "자에서 " + MAX_LENGTH + "자 사이여야 합니다");
+        }
+    }
+
+    private boolean isInvalidLengthName(String name) {
+        return name.length() < MIN_LENGTH || name.length() > MAX_LENGTH;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
