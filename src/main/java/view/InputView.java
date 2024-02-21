ef5a04a2f20652067a2d032b7028be9a266bd4d7
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-02-21 18:23:20 +0900

    feat(InputView): 입력 문구 출력 추가
    Co-authored-by: HaiSeong <wjdgotjd9908@gmail.com>

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -8,11 +8,14 @@ public class InputView {
     private final Scanner scanner = new Scanner(System.in);
 
     public List<String> inputNames() {
+        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
         String text = scanner.next();
         return NameParser.parse(text);
     }
 
     public int inputHeight() {
+        System.out.println();
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
         return scanner.nextInt();
     }
 }
