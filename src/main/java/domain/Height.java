de2561ee7fef5b62827d3f87e2e198dd74178aa9
hyxrxn <gpfls981220@gmail.com>
2024-02-21 15:42:20 +0900

    refactor: Height 클래스 메서드 분리
    Co-authored-by: shin-jisong <songjin1127@naver.com>

--- src/main/java/domain/Height.java
+++ src/main/java/domain/Height.java
@@ -2,12 +2,16 @@ package domain;
 
 public class Height {
 
+    public static final int MIN_OF_HEIGHT = 1;
+    public static final int MAX_OF_HEIGHT = 100;
+
     public Height(int height) {
-        if (height < 1) {
-            throw new IllegalArgumentException("[ERROR] 높이는 1개 이상이어야 합니다.");
-        }
-        if (height > 100) {
-            throw new IllegalArgumentException("[ERROR] 높이는 100개 이하여야 합니다.");
+        validate(height);
+    }
+
+    private void validate(int height) {
+        if (height < MIN_OF_HEIGHT || height > MAX_OF_HEIGHT) {
+            throw new IllegalArgumentException("[ERROR] 높이는 1개 이상 100개 이하여야 합니다.");
         }
     }
 }
--- src/test/java/domain/HeightTest.java
+++ src/test/java/domain/HeightTest.java
@@ -1,8 +1,9 @@
+package domain;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-import domain.Height;
 
 public class HeightTest {
 
@@ -11,7 +12,7 @@ public class HeightTest {
     void lessThanOneExceptionTest() {
         assertThatThrownBy(() -> new Height(0))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 높이는 1개 이상이어야 합니다.");
+                .hasMessage("[ERROR] 높이는 1개 이상 100개 이하여야 합니다.");
     }
 
     @Test
@@ -19,7 +20,7 @@ public class HeightTest {
     void moreThanHundredExceptionTest() {
         assertThatThrownBy(() -> new Height(101))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 높이는 100개 이하여야 합니다.");
+                .hasMessage("[ERROR] 높이는 1개 이상 100개 이하여야 합니다.");
     }
 
 }
