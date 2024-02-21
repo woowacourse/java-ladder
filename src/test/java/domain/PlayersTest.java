b345f56a1f1a426d269ac5745c80ae46e53bd5d1
le2sky <leehaneul990623@gmail.com>
2024-02-21 18:32:08 +0900

    style: import문 최적화

--- src/test/java/domain/PlayersTest.java
+++ src/test/java/domain/PlayersTest.java
@@ -1,10 +1,9 @@
 package domain;
 
+import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
 
 class PlayersTest {
 
