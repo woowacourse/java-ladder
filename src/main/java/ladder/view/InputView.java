37e19216f2bf8f4c845ab606793f4d23abfcd00a
masonkimseoul <masonkimseoul@gmail.com>
2024-02-21 17:36:42 +0900

    feat: InputView의 scanner 종료 기능 구현

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class InputView {
 
-    private  final Scanner scanner = new Scanner(System.in);
+    private final Scanner scanner = new Scanner(System.in);
 
     public List<String> readUserNames() {
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
@@ -49,4 +49,7 @@ public class InputView {
         }
     }
 
+    private void closeScanner() {
+        scanner.close();
+    }
 }
