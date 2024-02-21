ac99ed96dc8a996effebdd8dd8c034eba25c176d
ehtjsv2 <ehtjsv2@naver.com>
2024-02-21 16:45:26 +0900

    refactor: 사다리의 높이를 클래스로 포장

---
+++ src/main/java/domain/Height.java
@@ -0,0 +1,23 @@
+package domain;
+
+import static message.ErrorMessage.INVALID_LADDER_HEIGHT_EXCEPTION;
+
+public class Height {
+
+    private final int height;
+
+    public Height(int height) {
+        validate(height);
+        this.height = height;
+    }
+
+    private void validate(int height) {
+        if (height <= 0) {
+            throw new IllegalArgumentException(INVALID_LADDER_HEIGHT_EXCEPTION.getMessage());
+        }
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
--- src/main/java/domain/Ladder.java
+++ src/main/java/domain/Ladder.java
@@ -7,28 +7,22 @@ import java.util.List;
 
 public class Ladder {
 
-    private final int height;
+    private final Height height;
     private final List<Line> lines;
 
     public Ladder(int height) {
-        validate(height);
-        this.height = height;
+        this.height = new Height(height);
         this.lines = new ArrayList<>();
     }
 
     public void makeLines(int width) {
-        for (int i = 0; i < height; i++) {
+        for (int i = 0; i < height.getHeight(); i++) {
             Line line = new Line();
             line.makeLeg(width);
             lines.add(line);
         }
     }
 
-    private void validate(int height) {
-        if (height <= 0) {
-            throw new IllegalArgumentException(INVALID_LADDER_HEIGHT_EXCEPTION.getMessage());
-        }
-    }
     public List<Line> getLines() {
         return lines;
     }
