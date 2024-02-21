0e2a0407b9d2b4b0fb9bac2eace6fafc6e17e072
coli-geonwoo <kkwoo001021@naver.com>
2024-02-21 17:11:39 +0900

    refactor(domain): RowLine refactoring
    검증 기능 분리
    경계값 상수화
    connection deepcopy로 안전성 강화

--- src/main/java/ladder/domain/RowLine.java
+++ src/main/java/ladder/domain/RowLine.java
@@ -4,18 +4,21 @@ import java.util.Collections;
 import java.util.List;
 //TODO package 이동
 public class RowLine {
-    private final List<Boolean> connection;
+    private static final int MIN_PEOPLE_NUMBER=1;
+    private static final int MAX_PEOPLE_NUMBER=100;
+    private final List<Boolean> connection = new ArrayList<>();
 
     public RowLine(int peopleNumber, LineGenerator generator) {
-        // TODO Validate로 빽;
-        // TODO MIN, MAX값 상수화
-        // TODO  deepcopy로 안전성 높이기
-        if (peopleNumber < 1 || peopleNumber > 100) {
+        validatePeopleNumber(peopleNumber);
+        List<Boolean> generatedConnection = generator.getLine(peopleNumber);
+        validateSuccessiveLine(generatedConnection);
+        Collections.copy( this.connection,generatedConnection);
+    }
+
+    private static void validatePeopleNumber(int peopleNumber) {
+        if (peopleNumber < MIN_PEOPLE_NUMBER || peopleNumber > MAX_PEOPLE_NUMBER) {
             throw new IllegalArgumentException("참여자 수는 1이상 100이하의 사람 수 입니다.");
         }
-        List<Boolean> line = generator.getLine(peopleNumber);
-        validateSuccessiveLine(line);
-        this.connection = line;
     }
 
     public List<Boolean> getConnection() {
