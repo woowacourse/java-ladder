c398f03da1beb82c403835b3baf6098374fbbb55
hangillee <skfcb10@naver.com>
2024-02-21 14:23:24 +0900

    refactor: 겹치지 않는 가로 라인 생성 검증 테스트 수정

--- src/test/java/domain/LineTest.java
+++ src/test/java/domain/LineTest.java
@@ -1,29 +1,21 @@
 package domain;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Collections;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class LineTest {
+class LineTest {
     @Test
     @DisplayName("가로 라인은 겹치지 않아야 한다.")
     void isLineCannotNextToLine() {
         int personCount = 5;
         Line line = new Line(personCount, new RandomBooleanGenerator());
-
-        boolean isNotNextToLine = true;
         List<Boolean> points = line.getPoints();
-        Boolean prevPoint = points.get(0);
+        int isInvalidLine = Collections.indexOfSubList(points, List.of(true, true));
 
-        for (Boolean point : points.subList(1, points.size())) {
-            if (prevPoint && point) {
-                isNotNextToLine = false;
-            }
-            prevPoint = point;
-        }
-
-        assertTrue(isNotNextToLine);
+        assertEquals(-1, isInvalidLine);
     }
 }
