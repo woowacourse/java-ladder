752e7cc26c2c923afbd4776b83314caa0b2361e1
nayonsoso <panda0329@naver.com>
2024-02-21 17:17:29 +0900

    refactor: 0과 1이 아닌 값이 오면 예외 발생 메서드 추출
    - blue 단계

--- src/main/java/domain/StickGenerator.java
+++ src/main/java/domain/StickGenerator.java
@@ -1,14 +1,18 @@
 package domain;
 
 class StickGenerator {
-
+    
     public Stick generate(int i) {
+        validateCommand(i);
         if (i == 0) {
             return Stick.EMPTY;
         }
-        if (i == 1) {
-            return Stick.FILLED;
+        return Stick.FILLED;
+    }
+
+    private void validateCommand(int i) {
+        if (i != 0 && i != 1) {
+            throw new IllegalArgumentException("0과 1이 아닌 값은 허용하지 않습니다.");
         }
-        throw new IllegalArgumentException("0과 1이 아닌 값은 허용하지 않습니다.");
     }
 }
