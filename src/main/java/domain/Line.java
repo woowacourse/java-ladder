09fed86e382c2922f0b845dd27511ff84b06f564
nhlee98 <e5slnh9876@gmail.com>
2024-02-21 16:07:14 +0900

    feat: 좌우 연속해서 발판이 존재할 수 없다

--- src/main/java/domain/Line.java
+++ src/main/java/domain/Line.java
@@ -3,12 +3,21 @@ package domain;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.IntFunction;
+import java.util.stream.IntStream;
 
 public class Line {
     private final List<Boolean> scaffold;
 
     public Line(List<Boolean> scaffold) {
         this.scaffold = new ArrayList<>(scaffold);
+        validateContinue(this.scaffold);
+    }
+
+    private static void validateContinue(List<Boolean> scaffold) {
+        if (IntStream.range(0, scaffold.size() - 1)
+                .anyMatch(i -> scaffold.get(i) && scaffold.get(i + 1))) {
+            throw new IllegalArgumentException("[ERROR] 좌우 연속해서 발판이 존재할 수 없습니다.");
+        }
     }
 
     public static Line create(IntFunction<List<Boolean>> generator, int count) {
