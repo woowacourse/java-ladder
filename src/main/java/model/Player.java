95c2eafabd3538929fb7f48c8ff8c8723fc71d78
Choo <choo000407@naver.com>
2024-02-21 17:36:16 +0900

    feat: 참여자 이름은 중복되지 않는다

--- src/main/java/model/Player.java
+++ src/main/java/model/Player.java
@@ -1,23 +1,16 @@
 package model;
 
-public class Player {
+public record Player(String name) {
     public static final int MAX_LENGTH_OF_NAME = 5;
     private static final String INVALID_LENGTH_OF_NAME = "참여자 이름은 최대 5글지입니다.";
 
-    private final String name;
-
-    public Player(String name) {
+    public Player {
         validateLengthOfName(name);
-        this.name = name;
     }
 
     private void validateLengthOfName(String name) {
         if (name.length() > MAX_LENGTH_OF_NAME) {
             throw new IllegalArgumentException(INVALID_LENGTH_OF_NAME);
         }
-    }
-
-    public String getName() {
-        return name;
     }
 }
--- src/main/java/model/Players.java
+++ src/main/java/model/Players.java
@@ -3,22 +3,37 @@ package model;
 import static java.util.stream.Collectors.collectingAndThen;
 import static java.util.stream.Collectors.toList;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Players {
     private static final int MIN_SIZE_OF_PLAYERS = 2;
     private static final String INVALID_SIZE_OF_PLAYERS = "참여자 수는 최소 2명입니다.";
+    private static final String INVALID_PLAYER_NAMES_UNIQUE = "참여자 이름은 중복될 수 없습니다.";
 
     private final List<Player> players;
 
     private Players(List<Player> players) {
-        validateSizeOfPlayers(players);
+        validate(players);
         this.players = players;
+    }
+
+    private void validate(List<Player> players) {
+        validateSizeOfPlayers(players);
+        validatePlayerNamesUnique(players);
     }
 
     private void validateSizeOfPlayers(List<Player> players) {
         if (players.size() < MIN_SIZE_OF_PLAYERS) {
             throw new IllegalArgumentException(INVALID_SIZE_OF_PLAYERS);
+        }
+    }
+
+    private void validatePlayerNamesUnique(List<Player> players) {
+        Set<Player> uniquePlayers = new HashSet<>(players);
+        if (uniquePlayers.size() < players.size()) {
+            throw new IllegalArgumentException(INVALID_PLAYER_NAMES_UNIQUE);
         }
     }
 
@@ -30,7 +45,7 @@ public class Players {
 
     public List<String> getPlayerNames() {
         return players.stream()
-                .map(Player::getName)
+                .map(Player::name)
                 .toList();
     }
 
