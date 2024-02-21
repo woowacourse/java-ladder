b0cea09efa055284d202b7ca793ed7500f49eb2b
jhon3242 <rldotnr2@gmail.com>
2024-02-21 15:42:34 +0900

    feat(green) : 사람 이름 입력받는 기능 구현
    Co-authored-by: zangsu <zangsu_@naver.com>

---
+++ src/main/java/LadderGame.java
@@ -0,0 +1,14 @@
+import domain.Name;
+import java.util.List;
+import view.InputView;
+
+public class LadderGame {
+
+    public static void run() {
+        List<Name> names = InputView.readPersonNames()
+                .stream()
+                .map(Name::new)
+                .toList();
+
+    }
+}
---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,17 @@
+package view;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class InputView {
+
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    public static List<String> readPersonNames() {
+        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
+
+        return Arrays.stream(SCANNER.nextLine().split(","))
+                .toList();
+    }
+}
