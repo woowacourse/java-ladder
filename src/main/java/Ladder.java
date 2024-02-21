ebe5d0250219e4329253ae8073d840f3b711af57
ikjo39 <wer04129@gmail.com>
2024-02-21 15:40:54 +0900

    feat: 사다리 생성 기능 추가

---
+++ src/main/java/Ladder.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ladder {
+
+    private final List<Line> lines = new ArrayList<>();
+
+    public void generateLine(int height, int peopleCount, RandomGenerator randomGenerator) {
+        for (int i = 0; i < height; i++) {
+            List<Boolean> generatedResult = randomGenerator.generate(peopleCount);
+            lines.add(new Line(peopleCount, generatedResult));
+        }
+    }
+
+    public List<Line> getLines() {
+        return lines;
+    }
+}
---
+++ src/test/java/LadderTest.java
@@ -0,0 +1,29 @@
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LadderTest {
+
+    @DisplayName("Ladder 객체를 생성한다.")
+    @Test
+    void createLadder() {
+        assertThatCode(Ladder::new)
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("사다리 높이와 사람수, RandomGenerator을 입력받아 사다리 줄을 설정한다.")
+    @Test
+    void addLadderLine() {
+        int height = 2;
+        int peopleCount = 3;
+        RandomGenerator randomGenerator = new RandomGenerator();
+        Ladder ladder = new Ladder();
+        ladder.generateLine(height, peopleCount, randomGenerator);
+        List<Line> result = ladder.getLines();
+
+        assertThat(result).hasSize(height);
+    }
+}
