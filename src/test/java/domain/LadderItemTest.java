f36cb17e97b4e0a1099ef0573a5aa85618388338
eunjungL <dldmswjd322@gmail.com>
2024-02-21 18:51:16 +0900

    test: LadderItemTest 추가

---
+++ src/test/java/domain/LadderItemTest.java
@@ -0,0 +1,17 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LadderItemTest {
+
+    @DisplayName("사다리 연결 여부로 사다리 형태 가져오기")
+    @ParameterizedTest
+    @CsvSource(value = {"true,-----|", "false,'     |'"}, delimiter = ',')
+    void getShapeByIsConnected(Boolean isConnected, String expected) {
+        assertThat(LadderItem.getShapeByIsConnected(isConnected)).isEqualTo(expected);
+    }
+}
