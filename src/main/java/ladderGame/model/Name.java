bff3fd983b66a0c5e1391ecfa09abf38d90291e2
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-02-21 16:35:43 +0900

    feat(Name) : 이름은 공백일 시 예외처리 된다(Test Passes)

--- src/main/java/ladderGame/model/Name.java
+++ src/main/java/ladderGame/model/Name.java
@@ -2,17 +2,25 @@ package ladderGame.model;
 
 public class Name {
     private static final String EXCEPTION_MESSAGE_OVER_THAN_MAXIMUM = "이름은 최대 5글자까지 가능합니다.";
+    private static final String EXCEPTION_MESSAGE_NOT_BLANK = "이름은 공백일 수 없습니다.";
     private static final int MAXIMUM_NAME_LENGTH = 5;
     private final String name;
 
     public Name(String name) {
         validateNameLength(name);
+        validateNotBlank(name);
         this.name = name;
     }
 
     private void validateNameLength(String name) {
-        if (name.isBlank() || name.length() > MAXIMUM_NAME_LENGTH) {
+        if (name.length() > MAXIMUM_NAME_LENGTH) {
             throw new IllegalArgumentException(EXCEPTION_MESSAGE_OVER_THAN_MAXIMUM);
+        }
+    }
+
+    private void validateNotBlank(String name) {
+        if(name.isBlank()) {
+            throw new IllegalArgumentException(EXCEPTION_MESSAGE_NOT_BLANK);
         }
     }
 
