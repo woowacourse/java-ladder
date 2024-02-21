fa875c7a068a33c59752bef3e8a025dc029e953f
kelly6bf <kelly6bf@gmail.com>
2024-02-21 16:54:34 +0900

    feat: controller 사다리 높이 입력 받는 기능 구현

--- src/main/java/ladder/controller/LadderGameMachine.java
+++ src/main/java/ladder/controller/LadderGameMachine.java
@@ -1,5 +1,6 @@
 package ladder.controller;
 
+import ladder.domain.LadderHeight;
 import ladder.domain.UserNames;
 import ladder.util.ConsoleReader;
 import ladder.view.InputView;
@@ -7,18 +8,30 @@ import ladder.view.InputView;
 import java.util.List;
 
 public class LadderGameMachine {
+    private static final ConsoleReader CONSOLE = new ConsoleReader();
 
     public void run() {
         initNames();
+        initLadderHeight();
     }
 
     private UserNames initNames() {
         try {
-            List<String> input = InputView.readNames(new ConsoleReader());
+            List<String> input = InputView.readNames(CONSOLE);
             return UserNames.of(input);
         } catch (IllegalArgumentException e) {
             // TODO: 예외 메시지 출력
             return initNames();
+        }
+    }
+
+    private LadderHeight initLadderHeight() {
+        try {
+            int input = InputView.readLadderHeight(CONSOLE);
+            return new LadderHeight(input);
+        } catch (IllegalArgumentException e) {
+            // TODO: 예외 메시지 출력
+            return initLadderHeight();
         }
     }
 }
--- src/main/java/ladder/domain/LadderHeight.java
+++ src/main/java/ladder/domain/LadderHeight.java
@@ -1,13 +1,20 @@
 package ladder.domain;
 
 public class LadderHeight {
+    private final int ladderHeight;
+
     public LadderHeight(final int ladderHeight) {
         validateRange(ladderHeight);
+        this.ladderHeight = ladderHeight;
     }
 
     private void validateRange(final int ladderHeight) {
         if (ladderHeight < 2 || ladderHeight > 10) {
             throw new IllegalArgumentException("사다리 크기는 2 ~ 10 범위만 가능합니다.");
         }
+    }
+
+    public int getLadderHeight() {
+        return ladderHeight;
     }
 }
