3c51209567ae34d8e3557aaf9570d9fb243c73b0
libienz <leekh10195@gmail.com>
2024-02-21 18:13:21 +0900

    feat: 상수 리스트를 반환하는 기능 추가

--- src/main/java/domain/ConnectionStatus.java
+++ src/main/java/domain/ConnectionStatus.java
@@ -1,5 +1,9 @@
 package domain;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public enum ConnectionStatus {
 
     CONNECTED(Boolean.TRUE),
@@ -9,6 +13,10 @@ public enum ConnectionStatus {
 
     ConnectionStatus(boolean connect) {
         this.connect = connect;
+    }
+
+    public static List<ConnectionStatus> getAllStatus() {
+        return Arrays.stream(ConnectionStatus.values()).collect(Collectors.toList());
     }
 
     public boolean isConnect() {
