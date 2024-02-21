c2b0db6a9e1f41ce487eb407e28e4505d02f81ab
nhlee98 <e5slnh9876@gmail.com>
2024-02-21 23:35:19 +0900

    style: 클래스 내부 순서에 대한 컨벤션 적용

--- src/main/java/ladder/domain/LadderHeight.java
+++ src/main/java/ladder/domain/LadderHeight.java
@@ -11,14 +11,14 @@ public class LadderHeight {
         validateMin(value);
     }
 
+    public static LadderHeight from(String height) {
+        return new LadderHeight(Integer.valueOf(height));
+    }
+
     private static void validateMin(int value) {
         if (value < MIN_HEIGHT) {
             throw new IllegalArgumentException("[ERROR] 사다리의 높이는 1 이상의 정수이어야 합니다.");
         }
-    }
-
-    public static LadderHeight from(String height) {
-        return new LadderHeight(Integer.valueOf(height));
     }
 
     public int getValue() {
--- src/main/java/ladder/domain/People.java
+++ src/main/java/ladder/domain/People.java
@@ -15,19 +15,23 @@ public class People {
         validateCount(this.values);
     }
 
-    private static void validateCount(List<Person> values) {
-        if (values.size() < MIN_COUNT) {
-            throw new IllegalArgumentException("[ERROR] 사다리 게임에 참여하는 사람의 수는 2명 이상 이여야 합니다.");
-        }
-    }
-
     public static People from(String names) {
         return new People(Arrays.stream(names.split(DELIMITER))
                 .map(Person::new)
                 .toList());
     }
 
+    private static void validateCount(List<Person> values) {
+        if (values.size() < MIN_COUNT) {
+            throw new IllegalArgumentException("[ERROR] 사다리 게임에 참여하는 사람의 수는 2명 이상 이여야 합니다.");
+        }
+    }
+
     public int getCount() {
         return values.size();
+    }
+
+    public List<Person> getValues() {
+        return values;
     }
 }
