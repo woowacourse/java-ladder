da9702ef91ea2d286df67e0cd394cf44cddbe04d
ikjo39 <wer04129@gmail.com>
2024-02-21 16:25:59 +0900

    feat: Controller Layer 추가

---
+++ src/main/java/controller/LadderGameController.java
@@ -0,0 +1,19 @@
+package controller;
+
+import view.InputView;
+import view.OutputView;
+
+public class LadderGameController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public LadderGameController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+
+    }
+}
