363ac67afab07d46a88d02312f786cae8a2e3e20
DobbyMac <kimdobby@g.skku.edu>
2024-02-22 14:48:10 +0900

    feat(출력 기능): OutputView 클래스의 결과 출력 기능 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -4,16 +4,44 @@ import domain.ladder.common.Direction;
 
 public class OutputView {
 
-    public static final void printDirection(Direction direction) {
-        if (direction.equals(Direction.RIGHT)) {
-            System.out.print("|---");
-        }
-        if(direction.equals(Direction.LEFT)){
-            System.out.print("---|      ");
-        }
-        if (direction.equals(Direction.DOWN)) {
-            System.out.print("|      ");
-        }
+    private static final String RIGHT_DIRECTION_SYMBOL = "|---";
+    private static final String LEFT_DIRECTION_SYMBOL = "---|      ";
+    private static final String DOWN_DIRECTION_SYMBOL = "|      ";
+    private static final String BLANK_SPACE = "      ";
+    private static final int NAME_SPACE_SIZE = 7;
+    private static final String NAME_SPACE_UNIT = " ";
+
+    public static void printPlayerNames(List<Name> playerNames) {
+        playerNames.stream()
+                   .map(Name::getValue)
+                   .map(OutputView::padString)
+                   .forEach(System.out::print);
+        printNewLine();
+    }
+    private static String padString(String name) {
+        return NAME_SPACE_UNIT.repeat(NAME_SPACE_SIZE - name.length()) + name;
+    }
+    private static void printNewLine() {
+        System.out.print(System.lineSeparator());
+    }
+    public static void printDirections(List<Direction> directions) {
+
+        StringBuilder resultStringBuilder = new StringBuilder();
+        resultStringBuilder.append(BLANK_SPACE);
+
+        directions.forEach(direction -> {
+            String symbol = directionSymbols.get(direction);
+            resultStringBuilder.append(symbol);
+        });
+
+        System.out.println(resultStringBuilder);
     }
 
+    private static EnumMap<Direction, String> initializedDirectionSymbol() {
+        final EnumMap<Direction, String> directionSymbols = new EnumMap<>(Direction.class);
+        directionSymbols.put(Direction.RIGHT, RIGHT_DIRECTION_SYMBOL);
+        directionSymbols.put(Direction.LEFT, LEFT_DIRECTION_SYMBOL);
+        directionSymbols.put(Direction.DOWN, DOWN_DIRECTION_SYMBOL);
+        return directionSymbols;
+    }
 }
