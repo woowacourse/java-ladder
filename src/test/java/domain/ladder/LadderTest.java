c60f047f19777ab01efddf125b84ac8ff37b6877
Nam Gi Beom <rlqja0523@naver.com>
2024-02-22 17:11:40 +0900

    refactor: 테스트 메서드명, 변수명, assert문 변경

--- src/test/java/domain/ladder/LadderTest.java
+++ src/test/java/domain/ladder/LadderTest.java
@@ -7,18 +7,18 @@ import org.junit.jupiter.api.Test;
 import support.ConnectedLadderRungGenerator;
 
 public class LadderTest {
-    private final LadderRungGenerator trueGenerator = new ConnectedLadderRungGenerator();
+    private final LadderRungGenerator ladderRungGenerator = new ConnectedLadderRungGenerator();
 
     @Test
-    void 주어진_높이에_맞게_사다리가_생성된다() {
+    void 주어진_높이에_맞게_사다리를_생성한다() {
         // given
         Height height = new Height(5);
-        int playerSize = 5;
+        int playerSize = 6;
 
         // when
-        Ladder ladder = Ladder.create(height, playerSize, trueGenerator);
+        Ladder ladder = Ladder.create(height, playerSize, ladderRungGenerator);
 
         // then
-        assertThat(ladder.getHeight()).isEqualTo(height.getValue());
+        assertThat(ladder.getRows()).hasSize(height.getValue());
     }
 }
