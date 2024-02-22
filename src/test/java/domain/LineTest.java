6f309ff978d71282c2f0c2a0ab170c67ad3731ce
SCY <helenason@naver.com>
2024-02-22 13:41:04 +0900

    test: 가로줄 테스트 도메인에 맞게 수정
    Co-authored-by: takoyakimchi <c4nyou@gmail.com>

--- src/test/java/domain/LineTest.java
+++ src/test/java/domain/LineTest.java
@@ -2,15 +2,14 @@ package domain;
 
 import static org.assertj.core.api.Assertions.*;
 
-import java.util.List;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+import strategy.RandomPointStrategy;
 
 public class LineTest {
 
-    /* TODO
+    /* TODO: true 연속 2개 나오는 테스트 케이스 어떻게 만들지 생각해보기
     @Test
     @DisplayName("true-true 쌍이 발견될 경우 에러를 반환한다.")
     void test_exception_findTrueTrue() {
@@ -20,7 +19,7 @@ public class LineTest {
     @RepeatedTest(1000)
     @DisplayName("랜덤으로 만든 값이 true 또는 false이다.")
     void test() {
-        Line line = new Line();
+        Line line = new Line(4, new RandomPointStrategy());
         line.getPoints().forEach(
                 point -> assertThat(point).isInstanceOf(Boolean.class));
     }
@@ -28,12 +27,7 @@ public class LineTest {
     @Test
     @DisplayName("true가 나오면 그 다음은 무조건 false가 된다.")
     void test_ok_trueNextFalse() {
-        Line line = new Line() {
-            @Override
-            boolean makeRandomBoolean() {
-                return true;
-            }
-        };
+        Line line = new Line(3, () -> true);
         assertThat(line.getPoints().get(1)).isFalse();
     }
 }
