d89e46171c9c6869649d6983ca6c6c62ee194f0b
3juhwan <13selfesteem91@naver.com>
2024-02-23 00:28:54 +0900

    test: width가 0일 때 Line 테스트

--- src/test/java/domain/LineTest.java
+++ src/test/java/domain/LineTest.java
@@ -11,7 +11,7 @@ public class LineTest {
 
     @Test
     @DisplayName("가로줄이 겹치지 않는 라인이 만들어지는가")
-    void test() {
+    void create_non_adjacent_line_test() {
         // given
         int width = 5;
 
@@ -25,5 +25,19 @@ public class LineTest {
             Boolean nextBridge = bridges.get(x + 1);
             assertThat(currentBridge && nextBridge).isFalse();
         }
+    }
+
+    @Test
+    @DisplayName("너비가 0일 때 라인이 안 만들어지는가")
+    void create_non_adjacent_line_with_zero_width() {
+        // given
+        int width = 0;
+
+        // when
+        Line line = new Line(width);
+        List<Boolean> bridges = line.getBridges();
+
+        // then
+        assertThat(bridges).isEqualTo(List.of());
     }
 }
