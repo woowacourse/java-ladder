3b4ab82fbde489c84c65d918fe9acbed7d130b3f
le2sky <leehaneul990623@gmail.com>
2024-02-22 10:30:24 +0900

    feat: 사용자가 최소 2명인지 검증 하는 기능 구현
    - green 단계

--- src/main/java/domain/Players.java
+++ src/main/java/domain/Players.java
@@ -7,6 +7,7 @@ class Players {
     private final List<Player> players;
 
     public Players(List<String> names) {
+        validatePlayerSize(names);
         this.players = mapToPlayer(names);
     }
 
@@ -14,6 +15,12 @@ class Players {
         return names.stream()
                 .map(name -> new Player(name.trim()))
                 .toList();
+    }
+
+    private void validatePlayerSize(List<String> names) {
+        if (names.size() < 2) {
+            throw new IllegalArgumentException("사용자는 최소 2명이여야 한다.");
+        }
     }
 
     public List<Player> getPlayers() {
