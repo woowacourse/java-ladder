df8a74abc52057e2475bccb0094b68bb8359ae5c
hyun <osh123chm@gmail.com>
2024-02-22 15:00:42 +0900

    test: Ladder 테스트 케이스 추가

--- src/test/java/model/LadderTest.java
+++ src/test/java/model/LadderTest.java
@@ -1,13 +1,21 @@
 package model;
 
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.Test;
 
 public class LadderTest {
     @Test
     void 사다리_높이는_1_미만이면_예외가_발생한다() {
-        int height = 0;
+        int invalidHeight = 0;
+        int validHeight = 1;
         int numberOfParticipants = 5;
-        Assertions.assertThrows(IllegalArgumentException.class, () -> new Ladder(height, numberOfParticipants));
+        assertAll(
+                () -> assertThrows(IllegalArgumentException.class,
+                        () -> new Ladder(invalidHeight, numberOfParticipants)),
+                () -> assertDoesNotThrow(() -> new Ladder(validHeight, numberOfParticipants))
+        );
     }
 }
