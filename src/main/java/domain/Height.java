63aff0e94ab3c90667991c80e3fd354d367d6648
SCY <helenason@naver.com>
2024-02-22 14:12:17 +0900

    feat: 사다리 높이 도메인 생성
    Co-authored-by: takoyakimchi <c4nyou@gmail.com>

---
+++ src/main/java/domain/Height.java
@@ -0,0 +1,29 @@
+package domain;
+
+public class Height {
+
+    private static final int MIN_HEIGHT = 1;
+    private static final int MAX_HEIGHT = 20;
+
+    private final int value;
+
+    public Height(String rawValue) {
+        this.value = validateNonNumeric(rawValue);
+        validateRange(value);
+    }
+
+    // TODO: 변환과 검증 분리
+    private int validateNonNumeric(String rawValue) {
+        try {
+            return Integer.parseInt(rawValue); // TODO: 얘를 분리하고 싶다
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자를 입력해 주세요.");
+        }
+    }
+
+    private void validateRange(int value) {
+        if (value < MIN_HEIGHT || value > MAX_HEIGHT) {
+            throw new IllegalArgumentException(MIN_HEIGHT + " 이상 " + MAX_HEIGHT + " 이하의 숫자를 입력해 주세요.");
+        }
+    }
+}
