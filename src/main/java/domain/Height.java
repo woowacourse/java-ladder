50376b6ec1005196f64b1dd839f34afc94ff268f
devhoya97 <diajbh123@gmail.com>
2024-02-22 16:06:50 +0900

    feat : 사다리 높이 예외 처리 기능

--- src/main/java/domain/Height.java
+++ src/main/java/domain/Height.java
@@ -4,7 +4,14 @@ public class Height {
     private final int height;
 
     public Height(final int height) {
+        validateRange(height);
         this.height = height;
+    }
+
+    private void validateRange(final int height) {
+        if (height < 1 || height > 10) {
+            throw new IllegalArgumentException("사다리의 높이는 1이상 10이하 이어야 합니다.");
+        }
     }
 
     public int getHeight() {
