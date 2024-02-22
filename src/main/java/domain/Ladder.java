4c3fa05d70638e47395c0b3ff45159bd28092152
libienz <leekh10195@gmail.com>
2024-02-22 13:36:31 +0900

    feat: 사다리 생성 전략을 전달받아 사다리를 생성하는 기능 구현

--- src/main/java/domain/Ladder.java
+++ src/main/java/domain/Ladder.java
@@ -1,19 +1,24 @@
 package domain;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Ladder {
-
-    private final Height height;
     private final List<RowLine> lines;
 
-    public Ladder(Height height, List<RowLine> lines) {
-        this.height = height;
+    private Ladder(List<RowLine> lines) {
         this.lines = lines;
     }
 
+    public static Ladder createFrom(RowLineGenerator rowLineGenerator, Height height) {
+        List<RowLine> lines = IntStream.range(0, height.getHeight())
+                .mapToObj(i -> rowLineGenerator.generate(height.getHeight()))
+                .toList();
+        return new Ladder(lines);
+    }
+
     public Integer getHeight() {
-        return height.getHeight();
+        return lines.size();
     }
 
     public Integer getWidth() {
