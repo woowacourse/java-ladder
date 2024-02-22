02d3add040031c9b2a7bbba4162ffec6c87e535e
nhlee98 <e5slnh9876@gmail.com>
2024-02-22 13:28:50 +0900

    feat: 사용자가 잘못된 입력을 했을 때 재입력을 받는다

---
+++ src/main/java/ladder/util/ExceptionRetryHandler.java
@@ -0,0 +1,24 @@
+package ladder.util;
+
+import java.util.Optional;
+import java.util.function.Supplier;
+
+public class ExceptionRetryHandler {
+    public static <T> T handle(Supplier<T> supplier) {
+        Optional<T> result = Optional.empty();
+        while (result.isEmpty()) {
+            result = tryGet(supplier);
+        }
+
+        return result.get();
+    }
+
+    private static <T> Optional<T> tryGet(Supplier<T> supplier) {
+        try {
+            return Optional.of(supplier.get());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return Optional.empty();
+        }
+    }
+}
