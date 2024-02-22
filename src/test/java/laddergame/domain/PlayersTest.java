cc8953436a1543a055cb90aa51505669adffac09
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-02-22 14:22:59 +0900

    test: 플레이어 생성 검증
    Co-Authored-By: lemone <55480011+JiHyeonL@users.noreply.github.com>

--- src/test/java/laddergame/domain/PlayersTest.java
+++ src/test/java/laddergame/domain/PlayersTest.java
@@ -1,5 +1,6 @@
 package laddergame.domain;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.List;
@@ -10,6 +11,24 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("플레이어들")
 public class PlayersTest {
+    @Test
+    @DisplayName("플레이어가 제대로 생성되는지 테스트한다.")
+    public void createPlayersTest() {
+        //given
+        final String playerName1 = "choco";
+        final String playerName2 = "lemon";
+        final Player player1 = new Player(playerName1);
+        final Player player2 = new Player(playerName2);
+
+        //when
+        final Players players = Players.from(List.of(player1, player2));
+
+        //then
+        assertEquals(players.getPlayers().size(), 2);
+        assertEquals(players.getPlayers().get(0).getName(), playerName1);
+        assertEquals(players.getPlayers().get(1).getName(), playerName2);
+    }
+
     @Test
     @DisplayName("플레이어 이름에 중복이 있을 경우 예외가 발생한다.")
     public void nameDuplicateException() {
