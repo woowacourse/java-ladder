eab30921ea3cd8c685b07c9955b36a1063cfef77
DobbyMac <kimdobby@g.skku.edu>
2024-02-22 15:17:22 +0900

    feat(Players): Players 일급 컬렉션 구현

---
+++ src/test/java/domain/player/PlayersTest.java
@@ -0,0 +1,21 @@
+package domain.player;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayersTest {
+    @Test
+    @DisplayName("이름 목록을 통해 플레이어 목록을 생성한다.")
+    public void createPlayers(){
+        Names names = new Names(List.of("조이썬","도비"));
+
+        Players players = new Players(names);
+
+        assertInstanceOf(Players.class,players);
+        assertIterableEquals(players.getPlayerNames(),names.getValue());
+    }
+}
