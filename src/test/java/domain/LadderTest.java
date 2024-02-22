342b37bc5917ef3e3653f84fc417bcecafe41c37
Hyeon0208 <dmsgktn0208@gmail.com>
2024-02-22 17:22:06 +0900

    refactor : Ladder의 높이와 다리 길이 테스트를 위해 RandomBridgeGenerator 구현체 전달하도록 수정

--- src/test/java/domain/LadderTest.java
+++ src/test/java/domain/LadderTest.java
@@ -2,7 +2,7 @@ package domain;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 
-import domain.bridge.strategy.BridgeGeneratorStub;
+import domain.bridge.strategy.RandomBridgeGenerator;
 import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -12,14 +12,12 @@ public class LadderTest {
     @Test
     void createLadderSuccessWithHeightAndPointCount() {
         LadderHeight height = new LadderHeight("3");
-        int pointCount = 3;
-        BridgeGeneratorStub bridgeGeneratorStub = new BridgeGeneratorStub();
-        bridgeGeneratorStub.setBridges(List.of(LadderBridge.BRIDGE, LadderBridge.NONE, LadderBridge.BRIDGE));
-        Ladder ladder = Ladder.of(height, pointCount, bridgeGeneratorStub);
+        int pointCount = 5;
+        Ladder ladder = Ladder.of(height, pointCount, new RandomBridgeGenerator());
         List<Floor> floors = ladder.getFloors();
         assertAll(
                 () -> Assertions.assertThat(floors.size()).isEqualTo(3),
-                () -> Assertions.assertThat(floors.get(0).getBridges().size()).isEqualTo(3)
+                () -> Assertions.assertThat(floors.get(0).getBridges().size()).isEqualTo(5)
         );
     }
 }
