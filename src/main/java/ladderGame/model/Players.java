b117df54ba5e23276aa1855e8b197230d8b29825
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-02-22 13:45:22 +0900

    refactor(Players) : 변수명 변경

--- src/main/java/ladderGame/model/Players.java
+++ src/main/java/ladderGame/model/Players.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class Players {
     private static final String EXCEPTION_MESSAGE_DUPLICATION_NAME = "참여자들의 이름은 중복될 수 없습니다.";
     private static final String EXCEPTION_MESSAGE_LESS_THAN_MINIMUM = "참여자의 이름은 두 개 이상이어야 합니다.";
-    private static final int MINIMUM_NAMES = 2;
+    private static final int MINIMUM_NAME_COUNT = 2;
     private final List<Player> players;
 
     public Players(List<String> names) {
@@ -31,7 +31,7 @@ public class Players {
     }
 
     private void validatePlayerCount(List<String> names) {
-        if (names.size() < MINIMUM_NAMES) {
+        if (names.size() < MINIMUM_NAME_COUNT) {
             throw new IllegalArgumentException(EXCEPTION_MESSAGE_LESS_THAN_MINIMUM);
         }
     }
