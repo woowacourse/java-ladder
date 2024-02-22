e46536a249af9b7c980f9d94a3e0514352630b90
devhoya97 <diajbh123@gmail.com>
2024-02-22 12:35:13 +0900

    test : 불필요한 상수처리 제거

--- src/test/java/domain/PlayerTest.java
+++ src/test/java/domain/PlayerTest.java
@@ -5,13 +5,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class PlayerTest {
-    private static final String INVALID_NAME = "crrong";
 
     @DisplayName("사람의 이름이 5글자를 초과하면 예외를 발생시킨다.")
     @Test
     void invalidNameLength() {
         //given
-        final String playerName = INVALID_NAME;
+        final String playerName = "crrong";
         //when & then
         Assertions.assertThatThrownBy(() -> new Player(playerName)).isInstanceOf(IllegalArgumentException.class);
     }
--- src/test/java/service/LadderMakerTest.java
+++ src/test/java/service/LadderMakerTest.java
@@ -6,15 +6,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LadderMakerTest {
-    private static final int PERSON_COUNT = 4;
-    private static final int HEIGHT = 5;
 
     @DisplayName("사다리의 높이만큼 Line을 만들어서 사다리를 완성한다")
     @Test
     void createLadder() {
         //given
-        final int personCount = PERSON_COUNT;
-        final int height = HEIGHT;
+        final int personCount = 4;
+        final int height = 5;
         final LadderMaker ladderMaker = new LadderMaker();
         //when
         Ladder ladder = ladderMaker.createLadder(personCount, height);
