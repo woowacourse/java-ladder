ac613aeade9c43fc733523e42277f6066cf0eff3
jongmee <ra123221@gmail.com>
2024-02-22 14:26:20 +0900

    test: Participants 생성시 참가자 수 검증 테스트

--- src/test/java/ladder/domain/participant/ParticipantsTest.java
+++ src/test/java/ladder/domain/participant/ParticipantsTest.java
@@ -2,9 +2,13 @@ package ladder.domain.participant;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ParticipantsTest {
@@ -20,5 +24,18 @@ public class ParticipantsTest {
 
         // then
         assertEquals(3, count);
+    }
+
+    @ParameterizedTest
+    @MethodSource("getInvalidParticipantsNames")
+    @DisplayName("참가자 수가 1명 이하일 경우 예외가 발생한다.")
+    void checkParticipantsCountTest(List<String> names) {
+        // given & when
+        assertThatThrownBy(() -> new Participants(names))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    static Stream<List<String>> getInvalidParticipantsNames() {
+        return Stream.of(List.of("mia"), List.of());
     }
 }
