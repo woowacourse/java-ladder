3cb3797d88bebe7a0377350c7aa36be29bc1fc07
Minjoo <m0522inju@gmail.com>
2024-02-22 10:33:27 +0900

    style: Line 생성 관련 테스트 코드 DisplayName 변경

--- src/test/java/ladder/domain/LineTest.java
+++ src/test/java/ladder/domain/LineTest.java
@@ -1,15 +1,15 @@
 package ladder.domain;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
 import static ladder.domain.StepStatus.EXIST;
 import static ladder.domain.StepStatus.NONE;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class LineTest {
 
-    @DisplayName("인원수 -1 개수 만큼의 포인트를 가진 Line 인스턴스를 생성한다.")
+    @DisplayName("인원수 -1 개수 만큼의 StepStatus들을 가진 Line 인스턴스를 생성한다.")
     @Test
     void createLine() {
         final Line line = new Line(() -> true, 4);
