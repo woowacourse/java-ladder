2a346a0727794213c7cd7c028e520ce4a289a51b
reddevilmidzy <midzy0228@gmail.com>
2024-02-22 14:52:51 +0900

    refactor: 문자열을 상수로 변경

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,6 +5,11 @@ import java.util.Scanner;
 
 public class InputView {
 
+    private static final String PLAYERS_REQUEST_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    private static final String HEIGHT_REQUEST_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
+    private static final String NAME_SEPARATOR = ",";
+
+
     private final Scanner scanner;
 
     public InputView(Scanner scanner) {
@@ -12,33 +17,35 @@ public class InputView {
     }
 
     public List<String> readPlayersName() {
-        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
+        System.out.println(PLAYERS_REQUEST_MESSAGE);
         String rawNames = scanner.nextLine();
-        if (rawNames == null || rawNames.trim().isEmpty()) {
-            // TODO: 예외 메시지
-            throw new IllegalArgumentException();
-        }
-
+        validateBlank(rawNames);
         validateSeparators(rawNames);
-        List<String> names = List.of(rawNames.split(",", -1));
-
+        List<String> names = List.of(rawNames.split(NAME_SEPARATOR, -1));
+        System.out.println();
         return names;
     }
 
+    private void validateBlank(String rawNames) {
+        if (rawNames == null || rawNames.trim().isEmpty()) {// TODO: 예외 메시지
+            throw new IllegalArgumentException();
+        }
+    }
+
     private void validateSeparators(String rawNames) {
-        if (rawNames.startsWith(",") || rawNames.endsWith(",") || rawNames.contains(",,")) {
+        if (rawNames.startsWith(NAME_SEPARATOR) || rawNames.endsWith(NAME_SEPARATOR) || rawNames.contains(
+                NAME_SEPARATOR.repeat(2))) {
             throw new IllegalArgumentException();
         }
     }
 
     public int readHeight() {
-        System.out.println();
-        System.out.println("최대 사다리 높이는 몇 개인가요?");
+        System.out.println(HEIGHT_REQUEST_MESSAGE);
         String rawHeight = scanner.nextLine();
-        if (rawHeight == null || rawHeight.trim().isEmpty()) {
-            throw new IllegalArgumentException();
-        }
-        return convert(rawHeight);
+        validateBlank(rawHeight);
+        int height = convert(rawHeight);
+        System.out.println();
+        return height;
     }
 
     private int convert(String rawHeight) {
