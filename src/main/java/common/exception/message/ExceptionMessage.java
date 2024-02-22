c4fc871ed63fd4d89374bf340e5dec27c41fa32b
PgmJun <chltmdwns96@gmail.com>
2024-02-22 14:05:52 +0900

    refactor : 예외 메시지 예외 별로 주석 분리

--- src/main/java/common/exception/message/ExceptionMessage.java
+++ src/main/java/common/exception/message/ExceptionMessage.java
@@ -7,15 +7,26 @@ import view.InputView;
 
 public class ExceptionMessage {
 
+    /**
+     * ValidationException
+     */
+    public static final String INTEGER_FORMAT = "정수 형태만 입력 가능합니다";
     public static final String PLAYER_NAMES_RANGE = String.format("참가자의 수는 %d 이상, %d 이하여야 합니다",
             PlayerNames.PLAYER_NAMES_MIN_RANGE, PlayerNames.PLAYER_NAMES_MAX_RANGE);
     public static final String PLAYER_NAMES_DUPLICATION = "참가자 이름은 중복될 수 없습니다";
-    public static final String LADDER_HEIGHT_RANGE = String.format("사다리 높이의 범위는 %d 이상, %d 이하여야 합니다",
-            LadderHeight.HEIGHT_MIN_RANGE, LadderHeight.HEIGHT_MAX_RANGE);
-    public static final String INTEGER_FORMAT = "정수 형태만 입력 가능합니다";
     public static final String PLAYER_NAME_BLANK = "참가자 이름으로 공백을 사용할 수 없습니다";
     public static final String PLAYER_NAMES_INPUT_FORMAT = String.format("참가자 이름은 %s로 구분하여 입력해야합니다",
             InputView.PLAYER_NAMES_INPUT_DELIMITER);
-    public static final String READ_LIMIT_OVER = String.format("입력 횟수 제한(%d)를 초과하였습니다", Controller.READ_LIMIT);
+    public static final String LADDER_HEIGHT_RANGE = String.format("사다리 높이의 범위는 %d 이상, %d 이하여야 합니다",
+            LadderHeight.HEIGHT_MIN_RANGE, LadderHeight.HEIGHT_MAX_RANGE);
+
+    /**
+     * NotFoundException
+     */
     public static final String NOT_FOUND_BRIDGE = "존재하지 않는 Bridge 입니다";
+
+    /**
+     * IOException
+     */
+    public static final String READ_LIMIT_OVER = String.format("입력 횟수 제한(%d)를 초과하였습니다", Controller.READ_LIMIT);
 }
