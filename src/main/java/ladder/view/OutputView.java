440f70b9a702f03622ae6872ac11d2bb969387f7
masonkimseoul <masonkimseoul@gmail.com>
2024-02-22 15:57:00 +0900

    refactor: OutputView에서 메서드 분리 및 상수 하드코딩

--- src/main/java/ladder/view/OutputView.java
+++ src/main/java/ladder/view/OutputView.java
@@ -9,6 +9,11 @@ import ladder.domain.user.Users;
 import java.util.List;
 
 public class OutputView {
+    private static final String BLANK = " ";
+    private static final String LINE_VERTICAL = "|";
+    private static final String LINE_HORIZONTAL = "-----";
+    private static final int MAX_NAME_LENGTH = 5;
+
     public void printLadderGameResult(Users users, Ladder ladder) {
         System.out.println("\n실행결과\n");
         printUserNames(users);
@@ -19,39 +24,41 @@ public class OutputView {
         StringBuilder sb = new StringBuilder();
         for (User user : users.getUsers()) {
             String userName = user.getUserName();
-            appendNameFormat(userName, sb);
+            sb.append(appendNameFormat(userName));
         }
         System.out.println(sb);
+    }
+
+    private String appendNameFormat(String userName) {
+        if (userName.length() < MAX_NAME_LENGTH) {
+            String blankFormats = BLANK.repeat(4 - userName.length());
+            return blankFormats + userName + BLANK.repeat(2);
+        }
+        return userName + BLANK;
     }
 
     private void printLadder(Ladder ladder) {
         List<Line> lines = ladder.getLines();
         StringBuilder sb = new StringBuilder();
         for (Line line : lines) {
-            sb.append("    ");
-            List<Direction> directionsInfo = line.getDirectionsInfo();
-            for (Direction direction : directionsInfo) {
-                sb.append("|");
-                if (direction == Direction.RIGHT) {
-                    sb.append("-----");
-                }
-                if (direction != Direction.RIGHT) {
-                    sb.append("     ");
-                }
-            }
-            sb.append("\n");
+            printLine(sb, line.getDirectionsInfo());
         }
         System.out.println(sb);
     }
 
-    private void appendNameFormat(String userName, StringBuilder sb) {
-        if (userName.length() == 5) {
-            sb.append(userName).append(" ");
+    private void printLine(StringBuilder sb, List<Direction> directionsInfo) {
+        sb.append(BLANK.repeat(4));
+        for(int i = 0; i < directionsInfo.size() - 1; i++) {
+            sb.append(LINE_VERTICAL)
+                    .append(printDirections(directionsInfo.get(i)));
         }
-        if (userName.length() < 5) {
-            String blank = " ";
-            String blankFormats = blank.repeat(4 - userName.length());
-            sb.append(blankFormats).append(userName).append(blank).append(blank);
+        sb.append(LINE_VERTICAL).append("\n");
+    }
+
+    private String printDirections(Direction direction) {
+        if (direction == Direction.RIGHT) {
+            return LINE_HORIZONTAL;
         }
+        return BLANK.repeat(5);
     }
 }
