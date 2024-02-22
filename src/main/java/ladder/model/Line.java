989beb3639973280af3663ae17a943c59cb9ac96
BurningFalls <sherlocky31415@gmail.com>
2024-02-22 15:19:24 +0900

    refactor(line): LadderPath 패턴 검증 로직의 순회 구조 변경
    - for loop -> Stream
    
    Co-authored-by: Hyunguk Ryu <hw0603@naver.com>

--- src/main/java/ladder/model/Line.java
+++ src/main/java/ladder/model/Line.java
@@ -3,6 +3,9 @@ package ladder.model;
 import ladder.constant.LadderPath;
 
 import java.util.List;
+import java.util.stream.IntStream;
+
+import static ladder.constant.LadderPath.*;
 
 public class Line {
     private final List<LadderPath> row;
@@ -13,38 +16,34 @@ public class Line {
     }
 
     private void validate(List<LadderPath> row) {
-        if (!isRLPatternAllMatched(row) || !isLRPatternAllMatched(row)) {
-            throw new IllegalArgumentException("유효한 가로줄이 아닙니다.");
-        }
         if (isLeftOnFirst(row) || isRightOnEnd(row)) {
             throw new IllegalArgumentException("유효한 가로줄이 아닙니다.");
         }
-    }
-
-    private boolean isRLPatternAllMatched(List<LadderPath> row) {
-        for (int i = 0; i < row.size() - 1; i++) {
-            if (row.get(i) == LadderPath.RIGHT && row.get(i + 1) != LadderPath.LEFT) {
-                return false;
-            }
+        if (!isRLPatternAllMatched(row) || !isLRPatternAllMatched(row)) {
+            throw new IllegalArgumentException("유효한 가로줄이 아닙니다.");
         }
-        return true;
-    }
-
-    private boolean isLRPatternAllMatched(List<LadderPath> row) {
-        for (int i = row.size() - 1; i > 0; i--) {
-            if (row.get(i) == LadderPath.LEFT && row.get(i - 1) != LadderPath.RIGHT) {
-                return false;
-            }
-        }
-        return true;
     }
 
     private boolean isLeftOnFirst(List<LadderPath> row) {
-        return row.get(0) == LadderPath.LEFT;
+        return row.get(0) == LEFT;
     }
 
     private boolean isRightOnEnd(List<LadderPath> row) {
-        return row.get(row.size() - 1) == LadderPath.RIGHT;
+        return row.get(row.size() - 1) == RIGHT;
+    }
+
+    private boolean isRLPatternAllMatched(List<LadderPath> row) {
+        return IntStream.range(0, row.size())
+                .filter(i -> row.get(i).equals(RIGHT))
+                .map(idx -> idx + 1)
+                .allMatch(idx -> row.get(idx).equals(LEFT));
+    }
+
+    private boolean isLRPatternAllMatched(List<LadderPath> row) {
+        return IntStream.range(0, row.size())
+                .filter(i -> row.get(i).equals(LEFT))
+                .map(idx -> idx - 1)
+                .allMatch(idx -> row.get(idx).equals(RIGHT));
     }
 
     public int size() {
