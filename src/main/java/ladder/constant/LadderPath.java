7fc46bf4062da2fe208161f9c0b6a16020da3397
BurningFalls <sherlocky31415@gmail.com>
2024-02-22 22:44:03 +0900

    refactor(ladderPath): RIGHT인지 확인하는 메서드를 추가하여 가시성 향상

--- src/main/java/ladder/constant/LadderPath.java
+++ src/main/java/ladder/constant/LadderPath.java
@@ -2,4 +2,8 @@ package ladder.constant;
 
 public enum LadderPath {
     STAY, LEFT, RIGHT;
+
+    public boolean isRightPath() {
+        return this == RIGHT;
+    }
 }
--- src/main/java/ladder/dto/LineDto.java
+++ src/main/java/ladder/dto/LineDto.java
@@ -16,7 +16,7 @@ public class LineDto {
     public static LineDto from(Line line) {
         List<LadderPath> row = line.getRow();
         List<Boolean> connected = IntStream.range(0, row.size() - 1)
-                .mapToObj(idx -> row.get(idx).equals(LadderPath.RIGHT))
+                .mapToObj(idx -> row.get(idx).isRightPath())
                 .toList();
 
         return new LineDto(connected);
