940e61ad3ce9f87de0899388362b1047c1aaf1dd
libienz <leekh10195@gmail.com>
2024-02-22 14:22:52 +0900

    feat: InputMapper 구현

---
+++ src/main/java/controller/InputMapper.java
@@ -0,0 +1,27 @@
+package controller;
+
+import domain.Height;
+import domain.Names;
+import java.util.Arrays;
+import java.util.List;
+
+public class InputMapper {
+
+    public Names mapToNames(String target) {
+        List<String> names = Arrays.asList(target.split(","));
+        return Names.from(names);
+    }
+
+    public Height mapToHeight(String target) {
+        validateInputNumeric(target);
+        return new Height(Integer.parseInt(target));
+    }
+
+    public void validateInputNumeric(String target) {
+        try {
+            Integer.parseInt(target);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자 값이 아닙니다");
+        }
+    }
+}
