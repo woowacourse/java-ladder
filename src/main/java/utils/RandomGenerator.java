a9eff5cb0e3ae8d85850d3c9bbab6d2ed6bc9495
tackyu <tack970913@gmail.com>
2024-02-22 16:29:13 +0900

    refactor: 임계값 지정하여 사용할 수 있도록 수정

--- src/main/java/utils/RandomGenerator.java
+++
@@ -1,7 +0,0 @@
-package utils;
-
-
-public interface RandomGenerator {
-    int generate();
-
-}
---
+++ src/main/java/utils/RuleGeneratorImpl.java
@@ -0,0 +1,15 @@
+package utils;
+
+import java.util.Random;
+
+public class RuleGeneratorImpl implements RuleGenerator {
+
+    private static final int THRESHOLD = 3;
+
+    @Override
+    public boolean generate() {
+        int randomNumber = new Random().nextInt(10);
+
+        return randomNumber >= THRESHOLD;
+    }
+}
