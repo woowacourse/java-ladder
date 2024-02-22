b09e7b972ede2f907ed70711e6a13f975ecc45fc
김민수 MINSOO KIM <alstn113@gmail.com>
2024-02-23 07:17:01 +0900

    refactor: rung 테스트 변경

--- src/test/java/ladder/domain/ladder/RungTest.java
+++ src/test/java/ladder/domain/ladder/RungTest.java
@@ -3,16 +3,21 @@ package ladder.domain.ladder;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class RungTest {
     @ParameterizedTest
-    @EnumSource(Rung.class)
+    @CsvSource(value = {"true:EXIST", "false:EMPTY"}, delimiter = ':')
     @DisplayName("발판은 상태를 가진다.")
-    void testRungStatus(Rung rung) {
-        Rung[] values = Rung.values();
-        assertThat(values).contains(rung);
+    void testCreateRung(boolean exist, Rung rung) {
+        assertThat(Rung.of(exist)).isEqualTo(rung);
     }
 
+    @Test
+    @DisplayName("발판은 존재한다.")
+    void testRungExist() {
+        assertThat(Rung.of(true).isExist()).isTrue();
+    }
 }
