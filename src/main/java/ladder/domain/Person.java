29b20e951d4b6766d6a9d7be13dd762c0531ca4d
nhlee98 <e5slnh9876@gmail.com>
2024-02-22 13:33:13 +0900

    feat: 사용자 이름은 비어있거나 공백일 수 없다

--- src/main/java/ladder/domain/Person.java
+++ src/main/java/ladder/domain/Person.java
@@ -6,13 +6,24 @@ public class Person {
     private final String name;
 
     public Person(String name) {
-        validateLength(name);
+        validate(name);
         this.name = name;
+    }
+
+    private void validate(String name) {
+        validateLength(name);
+        validateBlank(name);
     }
 
     private void validateLength(String name) {
         if (name.length() > MAX_LENGTH) {
             throw new IllegalArgumentException("[ERROR] 사람 이름의 길이는 5자를 넘을 수 없습니다.");
+        }
+    }
+
+    private void validateBlank(String name) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("[ERROR] 사람 이름의 비어있거나 공백일 수 없습니다.");
         }
     }
 
