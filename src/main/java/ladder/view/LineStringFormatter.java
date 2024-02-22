9229cc54bb5cd83e1a5da38b71c4e7a2f8744ad8
BurningFalls <sherlocky31415@gmail.com>
2024-02-22 15:32:15 +0900

    refactor(lineStringFormatter): 상수 포장
    Co-authored-by: Hyunguk Ryu <hw0603@naver.com>

--- src/main/java/ladder/view/LineStringFormatter.java
+++ src/main/java/ladder/view/LineStringFormatter.java
@@ -3,19 +3,23 @@ package ladder.view;
 import ladder.dto.LineDto;
 
 public class LineStringFormatter {
+    private static final String VERTICAL_CHAR = "|";
+    private static final String HORIZONTAL_CHAR = "-";
+    private static final String EMPTY_SPACE = " ";
+    private static final int PATH_WIDTH = 5;
+
     public static String create(LineDto lineDto) {
         StringBuilder sb = new StringBuilder();
         lineDto.getConnected()
                 .forEach(val -> sb.append(convertLine(val)));
-        sb.append("|\n");
+        sb.append(VERTICAL_CHAR + "\n");
         return sb.toString();
     }
 
-    // TODO: 상수 포장하기
-    private static String convertLine(boolean value) {
-        if (value) {
-            return "|-----";
+    private static String convertLine(boolean connected) {
+        if (connected) {
+            return VERTICAL_CHAR + HORIZONTAL_CHAR.repeat(PATH_WIDTH);
         }
-        return "|     ";
+        return VERTICAL_CHAR + EMPTY_SPACE.repeat(PATH_WIDTH);
     }
 }
