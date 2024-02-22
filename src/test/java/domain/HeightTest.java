4525bd56a6cf98f88b3113a74a3f5a6e677af8c5
devhoya97 <diajbh123@gmail.com>
2024-02-22 16:04:46 +0900

    test : 사다리 높이 예외 처리 테스트

---
+++ src/test/java/domain/HeightTest.java
@@ -0,0 +1,28 @@
+package domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class HeightTest {
+
+    @DisplayName("사다리의 높이가 0이상 10 이하이면 높이가 잘 생성된다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 10})
+    void validHeight(final int rawHeight) {
+        //when
+        final Height height = new Height(rawHeight);
+        // then
+        Assertions.assertThat(height.getHeight()).isEqualTo(rawHeight);
+    }
+
+    @DisplayName("사다리의 높이가 0미만 10 초과이면 예외를 발생시킨다.")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 11})
+    void invalidHeight(final int rawHeight) {
+        //when & then
+        Assertions.assertThatThrownBy(() -> new Height(rawHeight))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
