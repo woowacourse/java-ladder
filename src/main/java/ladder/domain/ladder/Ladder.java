ed3e70ee2b220a43717584861108a85a2fd1f87b
JUHA <84626225+khabh@users.noreply.github.com>
2024-02-22 14:42:52 +0900

    refactor: ladderSteps 조회 시 변경 불가능하도록 수정

--- src/main/java/ladder/domain/ladder/Ladder.java
+++ src/main/java/ladder/domain/ladder/Ladder.java
@@ -3,15 +3,15 @@ package ladder.domain.ladder;
 import ladder.domain.generator.LadderStepGenerator;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+
+import static java.util.Collections.unmodifiableList;
 
 public class Ladder {
     private final List<LadderStep> ladderSteps;
 
     public Ladder(final LadderStepGenerator stepGenerator, final Height height) {
-        final List<LadderStep> generatedLadderSteps = generateLadderSteps(stepGenerator, height);
-        this.ladderSteps = Collections.unmodifiableList(generatedLadderSteps);
+        this.ladderSteps = generateLadderSteps(stepGenerator, height);
     }
 
     private List<LadderStep> generateLadderSteps(final LadderStepGenerator stepGenerator, final Height height) {
@@ -24,6 +24,6 @@ public class Ladder {
     }
 
     public List<LadderStep> getLadderSteps() {
-        return ladderSteps;
+        return unmodifiableList(ladderSteps);
     }
 }
