67842fec8ecde3c0dd39158789325b4b80a0ac7b
nayonsoso <panda0329@naver.com>
2024-02-22 13:22:35 +0900

    feat: 중복 사용자 검증 기능 구현
    - green 단계

--- src/main/java/domain/Players.java
+++ src/main/java/domain/Players.java
@@ -10,12 +10,20 @@ class Players {
 
     public Players(List<String> names) {
         validatePlayerSize(names);
+        validateDuplicated(names);
         this.players = mapToPlayer(names);
     }
 
     private void validatePlayerSize(List<String> names) {
         if (names.size() < MIN_PLAYER_SIZE) {
             throw new IllegalArgumentException("사용자는 최소 2명이여야 한다.");
+        }
+    }
+
+    private void validateDuplicated(List<String> names) {
+        long count = names.stream().distinct().count();
+        if (count != names.size()) {
+            throw new IllegalArgumentException("사용자는 중복될 수 없다.");
         }
     }
 
