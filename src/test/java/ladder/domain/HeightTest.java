13cd567878ac10cce87b286285aabed77508acb1
ay-eonii <naayen@naver.com>
2024-02-22 16:32:10 +0900

    test(HeightTest): 올바르지 않은 입력 예외 처리

--- src/test/java/ladder/domain/HeightTest.java
+++ src/test/java/ladder/domain/HeightTest.java
@@ -2,8 +2,6 @@ package ladder.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -14,16 +12,23 @@ class HeightTest {
     @DisplayName("높이는 1 이상의 숫자이다.")
     void createHeight() {
         // when & then
-        assertThatCode(() -> new Height(1))
+        assertThatCode(() -> new Height("5"))
                 .doesNotThrowAnyException();
     }
 
-    @ParameterizedTest
-    @ValueSource(ints = {0, -1})
+    @Test
     @DisplayName("입력된 높이가 0 이하라면 예외가 발생한다.")
-    void outOfBoundsHeightExceptionTest(int outOfBounds) {
+    void createNegativeHeight() {
         // when & then
-        assertThatThrownBy(() -> new Height(outOfBounds))
+        assertThatThrownBy(() -> new Height("-5"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("입력된 높이가 숫자가 아니라면 예외가 발생한다.")
+    void createNotNumber() {
+        // when & then
+        assertThatThrownBy(() -> new Height("pobi"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
