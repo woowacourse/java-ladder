5fe50232bd2b8b852beb81e2194a026740282e23
ay-eonii <naayen@naver.com>
2024-02-22 15:11:26 +0900

    refactor(Line): 가독성을 위해 메서드 정리

--- src/main/java/ladder/domain/Line.java
+++ src/main/java/ladder/domain/Line.java
@@ -2,53 +2,42 @@ package ladder.domain;
 
 import ladder.util.BooleanListGenerator;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Line {
 
-    private final List<Point> points;
     private final BooleanListGenerator booleanListGenerator;
+    private final List<Point> points;
 
     public Line(int personCount, BooleanListGenerator booleanListGenerator) {
         this.booleanListGenerator = booleanListGenerator;
-        this.points = createValidPoints(personCount - 1);
+        this.points = createPoints(personCount - 1);
+        ensurePoints();
     }
 
-    private List<Point> createValidPoints(int size) {
-        List<Point> rawPoints = booleanListGenerator.generate(size);
-        if (!rawPoints.contains(Point.ON)) {
-            return createValidPoints(size);
-        }
-
-        return makePoints(size, rawPoints);
-    }
-
-    private List<Point> makePoints(int size, List<Point> rawPoints) {
-        List<Point> points = init(rawPoints.get(0));
-        for (int i = 1; i < size; i++) {
-            Point previous = points.get(i - 1);
-            Point current = rawPoints.get(i);
-            Point point = replaceValidPoint(previous, current);
-            points.add(point);
+    private List<Point> createPoints(int size) {
+        List<Point> points = booleanListGenerator.generate(size);
+        if (!points.contains(Point.ON)) {
+            return createPoints(size);
         }
         return points;
     }
 
-    private List<Point> init(Point point) {
-        List<Point> points = new ArrayList<>();
-        points.add(point);
-        return points;
+    private void ensurePoints() {
+        for (int i = 1; i < points.size(); i++) {
+            makePointEnsure(i);
+        }
     }
 
-    private Point replaceValidPoint(Point previous, Point current) {
+    private void makePointEnsure(int currentIndex) {
+        Point previous = points.get(currentIndex - 1);
         if (previous == Point.ON) {
-            return Point.OFF;
+            points.set(currentIndex, Point.OFF);
         }
-        return current;
     }
 
     public List<Point> getPoints() {
-        return points;
+        return Collections.unmodifiableList(points);
     }
 }
