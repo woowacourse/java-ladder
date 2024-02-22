912bca2de73253187e3139a963e6b58a1b218525
seokmyungham <hellotjraud@gmail.com>
2024-02-22 17:34:40 +0900

    refactor(OutputView): OutputView 리팩토링
    Co-authored-by: jinchiim <kyunellroll@gmail.com>

--- src/main/java/ladder/view/OutputView.java
+++ src/main/java/ladder/view/OutputView.java
@@ -7,15 +7,32 @@ import ladder.domain.dto.ResultLadderDto;
 
 public class OutputView {
 
+    private static final String LADDERS_PREFIX = "    |";
+    private static final String LADDER_STICK_SYMBOL = "|";
+
     public void printResult(ResultLadderDto resultLadderDto, List<Name> names) {
         List<BuiltLadderDto> builtLadderDtos = resultLadderDto.resultLadder();
 
+        printParticipantsNames(names);
+        printBuiltLadders(builtLadderDtos);
+    }
+
+    private void printParticipantsNames(List<Name> names) {
         for (Name name : names) {
             System.out.printf("%6s", name.getName());
         }
         System.out.println();
+    }
+
+    private void printBuiltLadders(List<BuiltLadderDto> builtLadderDtos) {
         for (BuiltLadderDto builtLadderDto : builtLadderDtos) {
-            System.out.println("    |" + String.join("|", builtLadderDto.builtLadder()));
+            System.out.println(LADDERS_PREFIX + String.join(LADDER_STICK_SYMBOL, builtLadderDto.builtLadder()));
         }
     }
+
+    public void printError(IllegalArgumentException e) {
+        System.out.println(e.getMessage());
+    }
+
+
 }
