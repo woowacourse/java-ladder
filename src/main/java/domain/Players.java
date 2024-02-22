6f766adf4334c1b695488643a5f622177cde5b2b
nayonsoso <panda0329@naver.com>
2024-02-22 13:12:14 +0900

    refactor: 메서드 추출, 매직 넘버 상수화
    - blue 단계

--- src/main/java/domain/Players.java
+++ src/main/java/domain/Players.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 class Players {
 
+    private static final int MIN_PLAYER_SIZE = 2;
+
     private final List<Player> players;
 
     public Players(List<String> names) {
@@ -11,16 +13,16 @@ class Players {
         this.players = mapToPlayer(names);
     }
 
+    private void validatePlayerSize(List<String> names) {
+        if (names.size() < MIN_PLAYER_SIZE) {
+            throw new IllegalArgumentException("사용자는 최소 2명이여야 한다.");
+        }
+    }
+
     public List<Player> mapToPlayer(List<String> names) {
         return names.stream()
                 .map(name -> new Player(name.trim()))
                 .toList();
-    }
-
-    private void validatePlayerSize(List<String> names) {
-        if (names.size() < 2) {
-            throw new IllegalArgumentException("사용자는 최소 2명이여야 한다.");
-        }
     }
 
     public List<Player> getPlayers() {
