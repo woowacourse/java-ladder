49c108982c4228d21a5b3c09d5bbb3307da9a1bd
ay-eonii <naayen@naver.com>
2024-02-22 15:17:50 +0900

    refactor(Height): 변수명 수정

--- src/main/java/ladder/domain/Height.java
+++ src/main/java/ladder/domain/Height.java
@@ -2,14 +2,13 @@ package ladder.domain;
 
 public class Height {
 
-    //todo: 변수명 & 메서드명 변경
     private final int value;
 
     public Height(int value) {
         this.value = value;
     }
 
-    public boolean isSame(int size) {
-        return size == value;
+    public boolean isSame(int value) {
+        return this.value == value;
     }
 }
