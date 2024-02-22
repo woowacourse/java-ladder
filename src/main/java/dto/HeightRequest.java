3572cb04f1f8f8f394e1f991d9f96cfd59300619
unifolio0 <boho82ha@gmail.com>
2024-02-22 15:43:01 +0900

    refactor : dto를 record로 변환

--- src/main/java/dto/HeightRequest.java
+++ src/main/java/dto/HeightRequest.java
@@ -2,12 +2,7 @@ package dto;
 
 import domain.Height;
 
-public class HeightRequest {
-    private final int height;
-
-    public HeightRequest(int height) {
-        this.height = height;
-    }
+public record HeightRequest(int height) {
 
     public Height toHeigth() {
         return new Height(height);
