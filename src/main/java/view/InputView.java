d52452b2f5e58c01a2d46515da829f9cbd389707
3juhwan <13selfesteem91@naver.com>
2024-02-22 15:27:51 +0900

    feat: 사용자 안내 메시지 출력
    Co-authored-by: seunghye218 <seunghye@student.42seoul.kr>

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -9,10 +9,12 @@ public class InputView {
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public static String[] readNames() throws IOException {
+        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
         return br.readLine().split(",");
     }
 
     public static int readHeight() throws IOException {
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
         return Integer.parseInt(br.readLine());
     }
 }
