978b5a5968b71c63d1d6fd80c096560615192580
Nam Gi Beom <rlqja0523@naver.com>
2024-02-22 16:38:24 +0900

    refactor: 이름 최소 길이와 에러 메시지 포맷 변경

--- src/main/java/domain/player/Name.java
+++ src/main/java/domain/player/Name.java
@@ -1,9 +1,9 @@
 package domain.player;
 
 public class Name {
-    private static final int MINIMUM_NAME_LENGTH = 1;
+    private static final int MINIMUM_NAME_LENGTH = 2;
     private static final int MAXIMUM_NAME_LENGTH = 5;
-    private static final String LENGTH_EXCEPTION_MESSAGE = "[ERROR] 이름의 길이는 5글자보다 작아야합니다.";
+    private static final String LENGTH_EXCEPTION_MESSAGE = "[ERROR] 잘못된 이름: %s - 이름의 길이는 %d ~ %d 글자여야 합니다.";
 
     private final String name;
 
@@ -14,7 +14,9 @@ public class Name {
 
     private void validateLength(final String name) {
         if (name.length() < MINIMUM_NAME_LENGTH || name.length() > MAXIMUM_NAME_LENGTH) {
-            throw new IllegalArgumentException(LENGTH_EXCEPTION_MESSAGE);
+            throw new IllegalArgumentException(
+                    String.format(LENGTH_EXCEPTION_MESSAGE, name, MINIMUM_NAME_LENGTH, MAXIMUM_NAME_LENGTH)
+            );
         }
     }
 
--- src/test/java/domain/player/NameTest.java
+++ src/test/java/domain/player/NameTest.java
@@ -7,8 +7,8 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 public class NameTest {
     @ParameterizedTest
-    @ValueSource(strings = {"123456", ""})
-    void 이름의_길이가_1보다_작거나_5보다_크면_예외가_발생한다(String name) {
+    @ValueSource(strings = {"1", "123456"})
+    void 이름의_길이가_2보다_작거나_5보다_크면_예외가_발생한다(String name) {
         assertThatThrownBy(() -> new Name(name))
                 .isExactlyInstanceOf(IllegalArgumentException.class);
     }
