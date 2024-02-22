b8e32bfa175948fb69df168bbd4b92547f9a4f89
nhlee98 <e5slnh9876@gmail.com>
2024-02-22 13:31:28 +0900

    test: 사용자 이름은 비어있거나 공백일 수 없다

--- src/test/java/ladder/domain/PersonTest.java
+++ src/test/java/ladder/domain/PersonTest.java
@@ -2,6 +2,8 @@ package ladder.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -22,10 +24,19 @@ public class PersonTest {
                 .isEqualTo(name);
     }
 
-    @Test
     @DisplayName("이름이 5글자를 넘으면 예외가 발생한다.")
+    @Test
     void nameLengthExceptionTest() {
         assertThatThrownBy(() -> new Person("naknak"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @DisplayName("이름이 공백이거나 비어있는 경우 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "   "})
+    void nameBlankExceptionTest(String name) {
+        assertThatThrownBy(() -> new Person(name))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
 }
