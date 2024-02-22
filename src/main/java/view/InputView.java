3384ba687e4639b0e66872ab8314460789a121d8
Nam Gi Beom <rlqja0523@naver.com>
2024-02-23 00:37:19 +0900

    refactor: 개행 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -16,6 +16,7 @@ public class InputView {
     public static List<String> inputPlayerNames() {
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
         String rawNames = scanner.nextLine();
+        System.out.println();
         PlayersInputValidator.validate(rawNames);
         return Arrays.asList(rawNames.split(DEFAULT_DELIMITER));
     }
@@ -23,6 +24,7 @@ public class InputView {
     public static int inputHeight() {
         System.out.println("최대 사다리 높이는 몇 개인가요?");
         String rawHeight = scanner.nextLine();
+        System.out.println();
         HeightInputValidator.validate(rawHeight);
         return Integer.parseInt(rawHeight);
     }
