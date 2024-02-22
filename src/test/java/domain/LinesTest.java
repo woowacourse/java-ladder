8fd944983b6a4c12721beb1d827793eeed378b1b
SCY <helenason@naver.com>
2024-02-22 14:19:26 +0900

    test: 사다리 전체 도메인에 맞게 수정
    Co-authored-by: takoyakimchi <c4nyou@gmail.com>

--- src/test/java/domain/LinesTest.java
+++ src/test/java/domain/LinesTest.java
@@ -2,7 +2,6 @@ package domain;
 
 import static org.assertj.core.api.Assertions.*;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import strategy.RandomPointStrategy;
@@ -12,7 +11,7 @@ public class LinesTest {
     @Test
     @DisplayName("사디리 높이에 맞게 Line 리스트 생성 성공")
     void test_ok_createListLine() {
-        Lines lines = new Lines(new Height("5"), new RandomPointStrategy());
+        Lines lines = new Lines(4, new Height("5"), new RandomPointStrategy());
         assertThat(lines.getLines().size()).isEqualTo(5);
     }
 }
