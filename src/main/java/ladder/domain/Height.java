06fd3cebc3f905c201f146a98fbafa26a5de8a7c
Zeus6768 <akaps6768@gmail.com>
2024-02-22 15:51:49 +0900

    refactor(domain): 메서드 통합

--- src/main/java/ladder/domain/Height.java
+++ src/main/java/ladder/domain/Height.java
@@ -4,11 +4,7 @@ public record Height(int value) {
     private static final int MIN_HEIGHT = 1;
 
     public Height {
-        validate(value);
-    }
-
-    private void validate(int height) {
-        if (height < MIN_HEIGHT) {
+        if (value < MIN_HEIGHT) {
             throw new IllegalArgumentException("높이는 %d이상입니다.".formatted(MIN_HEIGHT));
         }
     }
