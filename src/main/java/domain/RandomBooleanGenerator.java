2d6fc763e8d9afaddc4f7adba3198f9d95790f55
Nam Gi Beom <rlqja0523@naver.com>
2024-02-22 15:29:06 +0900

    feat: 랜덤으로 boolean을 생성하는 기능 구현
    Co-authored-by: J-I-H-O <jeongjiho0731@gmail.com>
    Co-authored-by: Nam Gi Beom <rlqja0523@naver.com>

---
+++ src/main/java/domain/RandomBooleanGenerator.java
@@ -0,0 +1,10 @@
+package domain;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class RandomBooleanGenerator implements BooleanGenerator {
+    @Override
+    public boolean generate() {
+        return ThreadLocalRandom.current().nextBoolean();
+    }
+}
