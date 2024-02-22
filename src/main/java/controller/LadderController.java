46e2d0c94bda3d2192c8667c19bac06cc300178a
tackyu <tack970913@gmail.com>
2024-02-22 14:52:36 +0900

    feat: 재입력 구현

--- src/main/java/controller/LadderController.java
+++ src/main/java/controller/LadderController.java
@@ -2,24 +2,32 @@ package controller;
 
 import domain.model.Ladder;
 import domain.model.People;
+import handler.ExceptionHandler;
 import view.InputView;
 import view.ResultView;
+
+import java.util.function.Supplier;
 
 public class LadderController {
 
     private final ResultView resultView;
     private final InputView inputView;
-
+    private final ExceptionHandler handler;
     public LadderController(ResultView resultView, InputView inputView) {
         this.resultView = resultView;
         this.inputView = inputView;
+        this.handler = new ExceptionHandler();
     }
 
     public void make() {
-        People people = new People(inputView.askParticipants());
-        Ladder ladder = new Ladder(inputView.askLadderHeight(), people.numberOfParticipant());
+        People people = runWithHandler(() -> new People(inputView.askParticipants()));
+        Ladder ladder = runWithHandler(() -> new Ladder(inputView.askLadderHeight(), people.numberOfParticipants()));
 
         resultView.printResult(people, ladder);
     }
 
+
+    public <T> T runWithHandler(Supplier<T> callback){
+        return handler.handle(callback);
+    }
 }
