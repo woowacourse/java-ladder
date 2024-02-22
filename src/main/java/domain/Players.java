bb2728b05fc28b3bacf07ceb7493888d7256a903
devhoya97 <diajbh123@gmail.com>
2024-02-22 15:57:12 +0900

    feat : 참가자 수 예외 처리 기능

--- src/main/java/domain/Players.java
+++ src/main/java/domain/Players.java
@@ -7,8 +7,15 @@ public class Players {
     private final List<Player> players = new ArrayList<>();
 
     public Players(final List<String> names) {
+        validateNameCount(names.size());
         for (final String name : names) {
             players.add(new Player(name));
+        }
+    }
+
+    private void validateNameCount(final int count) {
+        if (count < 2 || count > 10) {
+            throw new IllegalArgumentException("참가자 수는 2명 이상 10명 이하 이어야 합니다.");
         }
     }
 
