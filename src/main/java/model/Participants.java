1233ba1b8aace0d21bde25e81a773f611371fcc3
yoonjuho <dbswn990@naver.com>
2024-02-22 15:04:49 +0900

    fix(Participants): 참가자의 수는 한명 이상이어야 한다.

--- src/main/java/model/Participants.java
+++ src/main/java/model/Participants.java
@@ -9,15 +9,20 @@ public class Participants {
 
     public Participants(List<String> participantsName) {
         validateNotDuplicateName(participantsName);
+        validateParticipantSize(participantsName);
         this.participants = create(participantsName);
     }
-
-
 
     private void validateNotDuplicateName(List<String> participantsName){
         Set<String> distinctNames = new HashSet<>(participantsName);
         if(distinctNames.size() != participantsName.size()){
             throw new IllegalArgumentException("중복된 참가자들은 존재할 수 없습니다.");
+        }
+    }
+
+    private void validateParticipantSize(List<String> participantsName) {
+        if (participantsName.size() <= 1) {
+            throw new IllegalArgumentException("참가자가 1명 이하인 경우는 존재할 수 없습니다.");
         }
     }
 
--- src/test/java/model/ParticipantsTest.java
+++ src/test/java/model/ParticipantsTest.java
@@ -17,4 +17,12 @@ class ParticipantsTest {
                 .hasMessage("중복된 참가자들은 존재할 수 없습니다.");
     }
 
+    @Test
+    @DisplayName("참가자의 수가 1명 이하일 경우 예외가 발생한다.")
+    void moreThanOneParticipants() {
+        assertThatThrownBy(() -> new Participants(List.of("pobi")))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("참가자가 1명 이하인 경우는 존재할 수 없습니다.");
+    }
+
 }
