d5e1f20029fb72b967db152179855b94e7bf7957
KimSangHyeok <tkdgur0906@naver.com>
2024-02-23 17:02:49 +0900

    test : ladder가 너비에 맞게 라인을 생성하는지 테스트

--- src/test/java/LadderTest.java
+++ src/test/java/LadderTest.java
@@ -1,18 +1,29 @@
-import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
 import domain.Ladder;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class LadderTest {
+
+    private static final int HEIGHT = 5;
+    private static final int WIDTH = 4;
 
     @DisplayName("사다리 객체를 정상적으로 생성한다.")
     @Test
     void createLadder() {
-        assertThatCode(()->Ladder.from(5,4))
+        assertThatCode(()->Ladder.from(HEIGHT,WIDTH))
                 .doesNotThrowAnyException();
+    }
+
+    @DisplayName("주어진 너비에 맞게 사다리의 다리의 개수를 생성한다.")
+    @Test
+    void makeLinesWithWidth() {
+        Ladder ladder = Ladder.from(HEIGHT, WIDTH);
+
+        ladder.getLines().stream()
+                        .forEach(line -> assertThat(line.getLegs().size()).isEqualTo(WIDTH));
     }
 }
