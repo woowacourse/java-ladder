9dcd4fdc3d349531669ecef36262d5566b83f3ac
HyunJoong Kim <hjk0761@naver.com>
2024-02-23 17:55:11 +0900

    refactor: Name 에서 주석 삭제

--- src/main/java/domain/Name.java
+++ src/main/java/domain/Name.java
@@ -18,13 +18,6 @@ public class Name {
     }
 
     private void validateStyle(String inputName) {
-//        Pattern pattern = Pattern.compile("^a-zA-Z0-9_-]+$");
-//        if (!pattern.matcher(inputName).find()) {
-//            throw new IllegalArgumentException("이름은 영어, 숫자, '_', '-'로만 이루어져야 합니다.");
-//        }
-//        위의 코드와 아래의 코드가 같은 기능을 하는데, 우리가 생각하는 차이점은 Pattern 객체를 만드는 것 외에는 없는 것 같다.
-//        그 외의 차이점이 있거나, 위의 코드를 사용해야 할 일이 있는지 궁금합니다. 물어보기
-
         if (!inputName.matches(NAME_STYLE)){
             throw new IllegalArgumentException("이름은 영어, 숫자, '_', '-'로만 이루어져야 합니다.");
         }
