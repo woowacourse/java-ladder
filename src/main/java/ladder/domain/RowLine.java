c19898819076bd01ca96a2fc23277f9c2af5d770
coli-geonwoo <kkwoo001021@naver.com>
2024-02-24 03:40:58 +0900

    refactor(domain): 사람 수 검증책임을 RowLine에서 분리

--- src/main/java/ladder/domain/RowLine.java
+++ src/main/java/ladder/domain/RowLine.java
@@ -10,29 +10,19 @@ import static ladder.domain.Connection.CONNECTED;
 import static ladder.domain.Connection.NOTCONNECTED;
 
 public class RowLine {
-    private static final int MIN_PEOPLE_NUMBER = 1;
-    private static final int MAX_PEOPLE_NUMBER = 100;
     private final List<Connection> connections = new ArrayList<>();
 
     public RowLine(int peopleNumber, ConnectionGenerator generator) {
-        validatePeopleNumber(peopleNumber);
         List<Connection> generatedConnection = generator.getConnections(peopleNumber);
         validateConnection(generatedConnection);
         this.connections.addAll(generatedConnection);
     }
 
-    private void validatePeopleNumber(int peopleNumber) {
-        if (peopleNumber < MIN_PEOPLE_NUMBER || peopleNumber > MAX_PEOPLE_NUMBER) {
-            throw new IllegalArgumentException("참여자 수는 1이상 100이하의 사람 수 입니다.");
-        }
-    }
-
-    //TODO 가독성 강화하기 - 네이밍
     private void validateConnection(List<Connection> connections) {
         Connection beforeConnection = NOTCONNECTED;
 
         for (Connection currentConnection : connections) {
-            if (beforeConnection==CONNECTED && currentConnection==CONNECTED) {
+            if (beforeConnection == CONNECTED && currentConnection == CONNECTED) {
                 throw new IllegalArgumentException("사다리 가로선이 연속되었습니다.");
             }
             beforeConnection = currentConnection;
@@ -42,6 +32,4 @@ public class RowLine {
     public List<Connection> getConnections() {
         return Collections.unmodifiableList(connections);
     }
-
-
 }
