9a22261ab72517853f2f08b9271eca59ca8ab54d
KimSangHyeok <tkdgur0906@naver.com>
2024-02-23 15:03:32 +0900

    refactor: 이름 정규표현식 검증 할 때 Pattern 객체를 캐싱하여 사용하도록 수정

--- src/main/java/domain/Name.java
+++ src/main/java/domain/Name.java
@@ -1,12 +1,14 @@
 package domain;
 
+import java.util.regex.Pattern;
+
 import static message.ErrorMessage.INVALID_PLAYER_NAME_LANGUAGE_EXCEPTION;
 import static message.ErrorMessage.INVALID_PLAYER_NAME_SIZE_EXCEPTION;
 import static message.ErrorMessage.NO_PLAYER_NAME_EXCEPTION;
 
 public class Name {
 
-    private static final String NAME_LANGUAGE_FORMAT = "^[A-Za-z]*$";
+    private static final Pattern NAME_LANGUAGE_FORMAT = Pattern.compile("^[A-Za-z]*$");
     private static final int MAXIMUM_NAME_SIZE = 5;
     private final String name;
 
@@ -28,7 +30,7 @@ public class Name {
     }
 
     private void validateNameLanguage(String name) {
-        if (!name.matches(NAME_LANGUAGE_FORMAT)) {
+        if (!NAME_LANGUAGE_FORMAT.matcher(name).matches()) {
             throw new IllegalArgumentException(INVALID_PLAYER_NAME_LANGUAGE_EXCEPTION.getMessage());
         }
     }
