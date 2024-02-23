a4631be41dbac54b5813004dd9b5ecabb2011eb7
KB Kim <28584160+jimi567@users.noreply.github.com>
2024-02-23 16:35:30 +0900

    refactor : Players의 생성자에 주입하는 인자 변경
    기존에는 Controller에서 List<Player>를 만들고 주입했으나, List<String>을 인자로 받아 Players 내부에서 List<Player> 를 생성하도록 변경

--- src/main/java/domain/Players.java
+++ src/main/java/domain/Players.java
@@ -9,18 +9,29 @@ public class Players {
 
     private final List<Player> players;
 
-    public Players(List<Player> players) {
-        validate(players);
-        this.players = players;
+    public Players(List<String> playerNames) {
+        validate(playerNames);
+        this.players = generatePlayers(playerNames);
     }
 
-    private void validate(List<Player> players) {
-        if (players.size() < MINIMUM_PLAYER_SIZE || players.size() > MAXIMUM_PLAYER_SIZE) {
-            throw new IllegalArgumentException(String.format("2명 이상, 10명 이하인 인원만 입력해주세요. 입력한 인원 : %d", players.size()));
+    private void validate(List<String> playerNames) {
+        if (playerNames.size() < MINIMUM_PLAYER_SIZE || playerNames.size() > MAXIMUM_PLAYER_SIZE) {
+            throw new IllegalArgumentException(
+                    String.format("2명 이상, 10명 이하인 인원만 입력해주세요. 입력한 인원 : %d", playerNames.size()));
         }
+    }
+
+    private List<Player> generatePlayers(List<String> names) {
+        return names.stream()
+                .map(Player::new)
+                .toList();
     }
 
     public int getSize() {
         return this.players.size();
+    }
+
+    public List<String> getNames() {
+        return players.stream().map(Player::toString).toList();
     }
 }
