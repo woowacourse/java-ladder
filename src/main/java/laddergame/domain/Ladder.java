466f619c7fba0a6b7e95d47fe3f4e2796d4967ee
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-02-23 14:15:03 +0900

    refactor: 불필요 필드 제거

--- src/main/java/laddergame/domain/Ladder.java
+++ src/main/java/laddergame/domain/Ladder.java
@@ -8,13 +8,10 @@ import laddergame.domain.strategy.PointBuildStrategy;
 
 public class Ladder {
     private final List<Line> lines;
-    private final Height height;
     private final BuildStrategy pointBuildStrategy = new PointBuildStrategy();
 
     public Ladder(final int playerCount, final Height height) {
-
-        this.height = height;
-        this.lines = IntStream.range(0, this.height.getHeight())
+        this.lines = IntStream.range(0, height.getHeight())
                 .mapToObj(i -> new Line(playerCount, pointBuildStrategy))
                 .collect(Collectors.toList());
     }
