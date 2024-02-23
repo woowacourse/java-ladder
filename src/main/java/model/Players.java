226ae3101ddb6aa5ac68d18f448716f992a2c71c
reddevilmidzy <midzy0228@gmail.com>
2024-02-23 10:59:02 +0900

    refactor: 메서드 분리하였던 것을 다시 병합

--- src/main/java/model/Players.java
+++ src/main/java/model/Players.java
@@ -12,7 +12,9 @@ public class Players {
 
     public Players(List<String> players) {
         validate(players);
-        this.players = convert(players);
+        this.players = players.stream()
+                .map(Player::new)
+                .toList();
     }
 
     private void validate(List<String> players) {
@@ -34,12 +36,6 @@ public class Players {
 
     private boolean isDuplicated(List<String> players) {
         return Set.copyOf(players).size() != players.size();
-    }
-
-    private List<Player> convert(List<String> players) {
-        return players.stream()
-                .map(Player::new)
-                .toList();
     }
 
     public List<String> getNames() {
