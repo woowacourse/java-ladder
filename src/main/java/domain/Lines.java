14be13e2bdc267ae6e2d0ed1263c3f9d657c2d8c
SungKyum Kim <sungkyum1@naver.com>
2024-02-23 20:47:07 +0900

    refactor: 생성자 개선 및 불필요한 인스턴스 삭제

--- src/main/java/domain/Lines.java
+++ src/main/java/domain/Lines.java
@@ -1,4 +1,4 @@
-package domain.lines;
+package domain;
 
 import domain.BooleanGenerator;
 import domain.RandomBooleanGenerator;
@@ -9,7 +9,6 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 public class Lines {
-    private final List<Line> lines;
     final int height;
     final int personCount;
     private final BooleanGenerator generator = new RandomBooleanGenerator();
@@ -17,11 +16,10 @@ public class Lines {
     public Lines(final int height, final int personCount) {
         this.height = height;
         this.personCount = personCount;
-        lines = new ArrayList<>();
     }
 
     public List<Line> getLines() {
-        return lines;
+        return createLines();
     }
 
     private List<Line> createLines() {
