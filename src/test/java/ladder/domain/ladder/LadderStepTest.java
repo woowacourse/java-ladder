99e9044ccf535897ad6987b55dcbd9ef12d13773
jongmee <ra123221@gmail.com>
2024-02-23 17:12:57 +0900

    test: LadderStep 보정 ParameterizedTest로 테스트

--- src/test/java/ladder/domain/ladder/LadderStepTest.java
+++ src/test/java/ladder/domain/ladder/LadderStepTest.java
@@ -2,8 +2,12 @@ package ladder.domain.ladder;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static ladder.domain.ladder.Path.EMPTY;
 import static ladder.domain.ladder.Path.EXIST;
@@ -22,17 +26,23 @@ public class LadderStepTest {
                 .doesNotThrowAnyException();
     }
 
-    @Test
+    @ParameterizedTest
+    @MethodSource("getContinuousAndExpectedResult")
     @DisplayName("연속된 사다리 발판이 생성자의 매개변수로 넘어올 경우 보정된다.")
-    void checkContinuousPathTest() {
-        // given
-        final List<Path> continuousLadderStep = List.of(EXIST, EXIST, EXIST, EMPTY);
-
+    void checkContinuousPathTest(List<Path> continuousLadderStep, List<Path> expectedResult) {
         // when
         final LadderStep ladderStep = new LadderStep(continuousLadderStep);
 
         // then
         final List<Path> correctedLadderStep = ladderStep.getLadderPaths();
-        assertThat(correctedLadderStep).isEqualTo(List.of(EXIST, EMPTY, EXIST, EMPTY));
+        assertThat(correctedLadderStep).isEqualTo(expectedResult);
+    }
+
+    static Stream<Arguments> getContinuousAndExpectedResult() {
+        return Stream.of(
+                Arguments.arguments(List.of(EXIST, EXIST), List.of(EXIST, EMPTY)),
+                Arguments.arguments(List.of(EXIST, EXIST, EXIST, EMPTY), List.of(EXIST, EMPTY, EXIST, EMPTY)),
+                Arguments.arguments(List.of(EXIST, EMPTY, EXIST, EXIST), List.of(EXIST, EMPTY, EXIST, EMPTY))
+        );
     }
 }
