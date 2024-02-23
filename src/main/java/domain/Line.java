926381872ba79bfb3adb9808d3fb50998cc86eb7
KB Kim <28584160+jimi567@users.noreply.github.com>
2024-02-23 20:49:31 +0900

    refactor : Line의 Bridge생성 로직 Bridge 도메인에 위임

--- src/main/java/domain/Line.java
+++ src/main/java/domain/Line.java
@@ -10,33 +10,21 @@ public class Line {
 
     public Line(int playerCount, BooleanGenerator booleanGenerator) {
         bridges = new ArrayList<>();
-        generate(playerCount, booleanGenerator);
-    }
-
-    private void generate(int playerCount, BooleanGenerator booleanGenerator) {
         for (int position = 0; position < playerCount - 1; position++) {
-            makePoint(booleanGenerator);
+            addBridge(booleanGenerator);
         }
     }
 
-    private void makePoint(BooleanGenerator booleanGenerator) {
+    private void addBridge(BooleanGenerator booleanGenerator) {
         if (bridges.isEmpty() || checkPreviousBlank()) {
-            makeBridge(booleanGenerator);
+            bridges.add(Bridge.of(booleanGenerator));
             return;
         }
         bridges.add(Bridge.BLANK);
     }
 
     private boolean checkPreviousBlank() {
-        return bridges.get(bridges.size() - 1) == Bridge.BLANK;
-    }
-
-    private void makeBridge(BooleanGenerator booleanGenerator) {
-        if (booleanGenerator.generate()) {
-            bridges.add(Bridge.EXIST);
-            return;
-        }
-        bridges.add(Bridge.BLANK);
+        return bridges.get(getBridgeCount() - 1) == Bridge.BLANK;
     }
 
     public int getBridgeCount() {
