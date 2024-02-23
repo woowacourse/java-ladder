fdc5939ef4a580eac73c8134d688b44e1737a1ee
KB Kim <28584160+jimi567@users.noreply.github.com>
2024-02-23 17:18:57 +0900

    refactor : 코드의 가독성 향상
    nCopies 을 사용해 코드의 가독성 향상

--- src/test/java/domain/PlayersTest.java
+++ src/test/java/domain/PlayersTest.java
@@ -2,6 +2,8 @@ package domain;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
@@ -14,38 +16,24 @@ public class PlayersTest {
     static Stream<Arguments> createFailArguments() {
         return Stream.of(
                 Arguments.arguments(
-                        List.of(new Player("pobi")),
-                        List.of(
-                                new Player("asd"),
-                                new Player("asd"),
-                                new Player("asd"),
-                                new Player("asd"),
-                                new Player("asd"),
-                                new Player("asd"),
-                                new Player("asd"),
-                                new Player("asd"),
-                                new Player("asd"),
-                                new Player("asd"),
-                                new Player("asd")
-                        ))
+                        List.of("pobi"),
+                        new ArrayList<>(Collections.nCopies(11, "asd")))
         );
+    }
+
+    @DisplayName("2명 미만, 10명 초과하면 예외를 발생시킨다.")
+    @ParameterizedTest
+    @MethodSource("createFailArguments")
+    public void createFail(List<String> players) {
+        assertThatCode(() -> new Players(players))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("2명 이상 10명 이하인 경우 예외가 발생하지않는다,")
     @Test
     public void createSuccess() {
         assertThatCode(() -> new Players(List.of(
-                new Player("pobi"),
-                new Player("abc"),
-                new Player("wiib"))))
+                "pobi", "wiib", "lee")))
                 .doesNotThrowAnyException();
-    }
-
-    @DisplayName("2명 미만, 10명 초과하면 예외를 발생시킨다.")
-    @ParameterizedTest
-    @MethodSource("createFailArguments")
-    public void createFail(List<Player> players) {
-        assertThatCode(() -> new Players(players))
-                .isInstanceOf(IllegalArgumentException.class);
     }
 }
