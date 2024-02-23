b6209b7394cf2c893a0c6991a3fd2071b34b8803
haiseong <wjdgotjd9908@gmail.com>
2024-02-23 17:11:20 +0900

    feat(Position): Position 클래스 작성

--- src/main/java/model/Point.java
+++
@@ -1,4 +0,0 @@
-package model;
-
-public record Point(int depth, int column) {
-}
---
+++ src/main/java/model/Position.java
@@ -0,0 +1,13 @@
+package model;
+
+public record Position(int depth, int column) {
+    public Position {
+        validateNegativePosition(depth, column);
+    }
+
+    private void validateNegativePosition(final int depth, final int column) {
+        if (depth < 0 || column < 0) {
+            throw new IllegalArgumentException("위치는 음수일 수 없습니다.");
+        }
+    }
+}
