30ae94e21c2e8a27b88eff970a562770c867de59
JUHA <84626225+khabh@users.noreply.github.com>
2024-02-23 16:49:34 +0900

    test: LadderStep 테스트 케이스 추가

---
+++ src/test/java/ladder/domain/ladder/LadderStepTest.java
@@ -0,0 +1,29 @@
+package ladder.domain.ladder;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static ladder.domain.ladder.Path.EMPTY;
+import static ladder.domain.ladder.Path.EXIST;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LadderStepTest {
+
+    @Test
+    @DisplayName("연속된 Path가 존재하지 않도록 수정한다.")
+    void discontinuousLadderPathsTest() {
+        // given
+        List<Path> continuousLadderPaths = new ArrayList<>(List.of(EXIST, EXIST, EMPTY));
+        List<Path> expectedLadderPaths = List.of(EXIST, EMPTY, EMPTY);
+
+        // when
+        List<Path> actualLadderPaths = new LadderStep(continuousLadderPaths).ladderPaths();
+
+        // then
+        assertThat(actualLadderPaths)
+                .containsExactlyElementsOf(expectedLadderPaths);
+    }
+}
