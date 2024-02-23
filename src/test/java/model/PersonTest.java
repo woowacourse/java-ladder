0d1d282be8f7725c781744f2e2aedef619e394ed
haiseong <wjdgotjd9908@gmail.com>
2024-02-23 17:18:19 +0900

    test(PersonTest): Person 클래스 필드에 위치 추가 테스트 작성

--- src/test/java/model/PersonTest.java
+++ src/test/java/model/PersonTest.java
@@ -10,18 +10,20 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 class PersonTest {
 
-    @DisplayName("사람은 이름을 갖는다.")
+    @DisplayName("사람은 이름과 위치를 갖는다.")
     @Test
     void createPerson() {
         String name = "moly";
-        Person person = Person.from(name);
+        Person person = Person.from(name, 0);
         assertThat(person.getName()).isEqualTo(name);
+        assertThat(person.getDepth()).isZero();
+        assertThat(person.getColumn()).isZero();
     }
 
     @ParameterizedTest(name = "이름은 최소 1글자 최대 5글자다.")
     @ValueSource(strings = {"", "mollly"})
     void createPersonThrowExceptionWhenInvalidNameLength(String name) {
-        assertThatThrownBy(() -> Person.from(name))
+        assertThatThrownBy(() -> Person.from(name, 0))
                 .isExactlyInstanceOf(IllegalArgumentException.class)
                 .hasMessage("이름은 최소 1글자 최대 5글자여야 합니다.");
     }
