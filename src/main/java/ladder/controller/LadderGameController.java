2b563a9785565880ebb3d0aba0add7afc1fe81b3
coli-geonwoo <kkwoo001021@naver.com>
2024-02-23 21:21:41 +0900

    refactor(controller): 컨트롤러에서 예외처리 책임 분리

--- src/main/java/ladder/controller/LadderGameController.java
+++ src/main/java/ladder/controller/LadderGameController.java
@@ -2,53 +2,29 @@ package ladder.controller;
 
 import ladder.domain.Ladder;
 import ladder.domain.People;
+import ladder.util.ExceptionHandledReader;
 import ladder.view.InputView;
 import ladder.view.OutputView;
 
 public class LadderGameController {
     private LadderGameController() {
     }
-    //TODO 예외처리가 Controller의 역할인지 생각하기
+
     public static void run() {
-        People people = getPeople();
-        Ladder ladder = getLadder(people);
+        People people = ExceptionHandledReader.readUntilNoError(LadderGameController::getPeople);
+        Ladder ladder = ExceptionHandledReader.readUntilNoError(() -> getLadder(people));
 
         OutputView.printNames(people);
         OutputView.printLadder(ladder);
     }
 
     private static People getPeople() {
-        People people = null;
-        while (people == null) {
-            people = makePeople();
-        }
-        return people;
+        return new People(InputView.readNames());
     }
 
     private static Ladder getLadder(People people) {
-        Ladder ladder = null;
-        while (ladder == null) {
-            ladder = makeLadder(people);
-        }
-        return ladder;
+        int peopleNumber = people.getNames().size();
+        return new Ladder(InputView.readHeight(), peopleNumber);
     }
 
-    private static People makePeople() {
-        try {
-            return new People(InputView.readNames());
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
-        return null;
-    }
-
-    private static Ladder makeLadder(People people) {
-        try {
-            int peopleNumber = people.getNames().size();
-            return new Ladder(InputView.readHeight(), peopleNumber);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
-        return null;
-    }
 }
