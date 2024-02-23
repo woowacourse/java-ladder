22d617f61986e933eafecb6e9cc0c370a9eedc74
takoyakimchi <c4nyou@gmail.com>
2024-02-23 14:05:42 +0900

    refactor: Point Enum에서 view 로직 제거

--- src/main/java/domain/Point.java
+++ src/main/java/domain/Point.java
@@ -2,18 +2,12 @@ package domain;
 
 public enum Point {
 
-    CONNECTED(true, "-"),
-    DISCONNECTED(false, " ");
+    CONNECTED(true),
+    DISCONNECTED(false);
 
     private final Boolean isConnected;
-    private final String displayCharacter;
 
-    Point(Boolean isConnected, String displayCharacter) {
+    Point(Boolean isConnected) {
         this.isConnected = isConnected;
-        this.displayCharacter = displayCharacter;
-    }
-
-    public String getDisplayCharacter() {
-        return displayCharacter;
     }
 }
