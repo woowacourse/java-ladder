b7684cc37869e23c5d1bfc8b8f5d06bc5d94e9a1
kelly6bf <kelly6bf@gmail.com>
2024-02-23 15:27:43 +0900

    refactor: LadderHeight 사다리 최소 & 최대 허용 크기값을 상수로 분리

--- src/main/java/ladder/domain/LadderHeight.java
+++ src/main/java/ladder/domain/LadderHeight.java
@@ -1,13 +1,16 @@
 package ladder.domain;
 
 public record LadderHeight(int ladderHeight) {
+    private static final int MINIMUM_LADDER_HEIGHT_LIMIT = 2;
+    private static final int MAXIMUM_LADDER_HEIGHT_LIMIT = 10;
+
     public LadderHeight {
         validateRange(ladderHeight);
     }
 
     private void validateRange(final int ladderHeight) {
-        if (ladderHeight < 2 || ladderHeight > 10) {
-            throw new IllegalArgumentException("사다리 크기는 2 ~ 10 범위만 가능합니다.");
+        if (ladderHeight < MINIMUM_LADDER_HEIGHT_LIMIT || ladderHeight > MAXIMUM_LADDER_HEIGHT_LIMIT) {
+            throw new IllegalArgumentException("사다리 크기는 " + MINIMUM_LADDER_HEIGHT_LIMIT + " ~ " + MAXIMUM_LADDER_HEIGHT_LIMIT + " 범위만 가능합니다.");
         }
     }
 }
