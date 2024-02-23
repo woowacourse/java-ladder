ee3f115f69fa818e31cfd41418954e17556c6ffd
3juhwan <13selfesteem91@naver.com>
2024-02-24 00:30:28 +0900

    refactor: 검증 메서드 분리

--- src/main/java/domain/Name.java
+++ src/main/java/domain/Name.java
@@ -5,13 +5,22 @@ import java.util.regex.Pattern;
 public record Name(String name) {
     private static final int NAME_LENGTH_MIN = 1;
     private static final int NAME_LENGTH_MAX = 5;
+    private static final String NAME_REGEX_POLICY = "^[A-Za-z0-9]+";
 
     public Name {
+        validateNameLength();
+        validateAlphanumericName();
+    }
+
+    private void validateNameLength() {
+        String errorMessage = String.format("이름의 길이는 %d ~ %d 자 이어야 합니다.", NAME_LENGTH_MIN, NAME_LENGTH_MAX);
         if (NAME_LENGTH_MIN > name.length() || name.length() > NAME_LENGTH_MAX) {
-            throw new IllegalArgumentException("이름의 길이는 " + NAME_LENGTH_MIN + " ~ " +
-                    NAME_LENGTH_MAX + " 자 이어야 합니다.");
+            throw new IllegalArgumentException(errorMessage);
         }
-        if (!Pattern.matches("^[A-Za-z0-9]+", name)) {
+    }
+
+    private void validateAlphanumericName() {
+        if (!Pattern.matches(NAME_REGEX_POLICY, name)) {
             throw new IllegalArgumentException("이름이 영어와 숫자가 아니라면 예외가 발생한다");
         }
     }
