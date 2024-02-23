0802962af43139e3243045761330c10c59ee7ebf
DobbyMac <kimdobby@g.skku.edu>
2024-02-23 11:02:34 +0900

    refactor: 코드 컨벤션 적용

--- src/main/java/domain/player/Name.java
+++ src/main/java/domain/player/Name.java
@@ -2,7 +2,8 @@ package domain.player;
 
 public class Name {
     private final String value;
-    public Name(String value){
+
+    public Name(String value) {
         validate(value);
         this.value = value;
     }
@@ -10,23 +11,27 @@ public class Name {
     public String getValue() {
         return value;
     }
-    private void validate(String inputName){
+
+    private void validate(String inputName) {
         validateAvailableLength(inputName);
         validateBlank(inputName);
         validateContainBlankInName(inputName);
     }
+
     private void validateAvailableLength(String inputName) {
         if (inputName.length() > 5) {
             throw new IllegalArgumentException();
         }
     }
+
     private void validateBlank(String initialInput) {
-        if(initialInput.isBlank()) {
+        if (initialInput.isBlank()) {
             throw new IllegalArgumentException("공백으로 이루어진 이름은 사용할 수 없습니다.");
         }
     }
+
     private void validateContainBlankInName(String inputName) {
-        if(inputName.contains(" ")) {
+        if (inputName.contains(" ")) {
             throw new IllegalArgumentException();
         }
     }
