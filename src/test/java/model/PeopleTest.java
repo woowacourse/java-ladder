730702c943663c1274e325465245595869ae6ef4
minjong kim <godminjong@naver.com>
2024-02-23 15:56:39 +0900

    test: 2명 이상의 이름이 입력되었을 경우의 테스트 작성.
    사람 이름이 2명 이상일 경우 어떤 예외 없이 성공적으로 People이 생성되는 것을 테스트하는 코드 작성함.

--- src/test/java/model/PeopleTest.java
+++ src/test/java/model/PeopleTest.java
@@ -1,18 +1,27 @@
 package model;
 
-import model.People;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class PeopleTest {
 
     @Test
-    @DisplayName("최소 2명 이상의 참가자들이 필요하다")
+    @DisplayName("최소 2명 미만의 이름이 입력될 경위 예외를 발생시킨다.")
     void leastParticipantTest() {
         assertThatThrownBy(() -> new People("a"))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("참겨자는 2명 이상은 허용한다.")
+    void greatParticipantTest() {
+        assertThatCode(() -> new People("a,aa"))
+                .doesNotThrowAnyException();
     }
 
     @Test
