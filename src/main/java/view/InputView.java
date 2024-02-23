0cb881aca1ebf77609149ca64f8bce9ad8aabbcf
3juhwan <13selfesteem91@naver.com>
2024-02-23 16:49:15 +0900

    fix: 이름에 대한 요구사항 변경

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,16 +5,29 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class InputView {
+    public static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private static final String USER_NAME_DELIMITER = ",";
 
-    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public static String[] readNames() throws IOException {
-        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
-        return br.readLine().split(",");
+        printNameInputMessage();
+        return removeBlank(reader.readLine()).split(USER_NAME_DELIMITER);
     }
 
     public static int readHeight() throws IOException {
+        printLadderHeightInputMessage();
+        return Integer.parseInt(reader.readLine());
+    }
+
+    private static void printNameInputMessage() {
+        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
+    }
+
+    private static String removeBlank(final String text) {
+        return text.replace(" ", "");
+    }
+
+    private static void printLadderHeightInputMessage() {
         System.out.println("최대 사다리 높이는 몇 개인가요?");
-        return Integer.parseInt(br.readLine());
     }
 }
