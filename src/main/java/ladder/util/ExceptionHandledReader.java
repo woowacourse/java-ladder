d57f9e9a35dc5b85eb1fe1e31c37172a47dd4483
coli-geonwoo <kkwoo001021@naver.com>
2024-02-23 21:20:52 +0900

    feat(util): 예외 발생 시 재입력 받는 로직 추가

---
+++ src/main/java/ladder/util/ExceptionHandledReader.java
@@ -0,0 +1,27 @@
+package ladder.util;
+
+import java.util.function.Supplier;
+import static java.lang.System.*;
+
+public class ExceptionHandledReader {
+    private ExceptionHandledReader() {
+    }
+
+    public static <T> T readUntilNoError(Supplier<T> supplier) {
+        T readObj = null;
+        while (readObj == null) {
+            readObj = readOnce(supplier);
+        }
+        return readObj;
+    }
+
+    private static <T> T readOnce(Supplier<T> supplier) {
+        try {
+            return supplier.get();
+        } catch (IllegalArgumentException e) {
+            out.println(e.getMessage());
+        }
+        return null;
+    }
+
+}
