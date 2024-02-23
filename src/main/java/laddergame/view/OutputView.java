083db8c1294b35631ac3b95862b1c9ea08254fe7
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-02-23 14:00:18 +0900

    refactor: OS를 고려한 개행 적용

--- src/main/java/laddergame/view/OutputView.java
+++ src/main/java/laddergame/view/OutputView.java
@@ -35,9 +35,7 @@ public class OutputView {
     }
 
     public void writeResultTitle() {
-        System.out.println();
-        System.out.println("실행결과");
-        System.out.println();
+        System.out.println(System.lineSeparator() + "실행결과" + System.lineSeparator());
     }
 
     public static void writeErrorMessage(final String message) {
