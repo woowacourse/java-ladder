ee70e762bc516e28d100c8051fdfa734c707f160
haiseong <wjdgotjd9908@gmail.com>
2024-02-23 16:17:19 +0900

    test(PersonNameTest, PersonTest): PersonName 분리 테스트 작성

--- src/test/java/model/PersonNameTest.java
+++ src/test/java/model/PersonNameTest.java
@@ -8,21 +8,20 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-class PersonTest {
+class PersonNameTest {
 
     @DisplayName("사람은 이름을 갖는다.")
     @Test
-    void createPerson() {
+    void createPersonName() {
         String name = "moly";
         PersonName personName = new PersonName(name);
-        Person person = new Person(personName);
-        assertThat(person.getName()).isEqualTo(name);
+        assertThat(personName.name()).isEqualTo(name);
     }
 
     @ParameterizedTest(name = "이름은 최소 1글자 최대 5글자다.")
     @ValueSource(strings = {"", "mollly"})
-    void createPersonThrowExceptionWhenInvalidNameLength(String name) {
-        assertThatThrownBy(() -> new Person(new PersonName(name)))
+    void createPersonNameThrowExceptionWhenInvalidNameLength(String name) {
+        assertThatThrownBy(() -> new PersonName(name))
                 .isExactlyInstanceOf(IllegalArgumentException.class)
                 .hasMessage("이름은 최소 1글자 최대 5글자여야 합니다.");
     }
