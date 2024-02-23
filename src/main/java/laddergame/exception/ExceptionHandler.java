1d25a745387e3b79f305039fa87c5765057cb265
Arachneee <jhg2819@naver.com>
2024-02-23 16:41:50 +0900

    feat: 스택오버플로우 방지 기능 추가

--- src/main/java/laddergame/exception/ExceptionHandler.java
+++ src/main/java/laddergame/exception/ExceptionHandler.java
@@ -5,12 +5,22 @@ import laddergame.view.OutputView;
 
 public class ExceptionHandler {
 
+    private static final int MAX_RETRY_COUNT = 1000;
+
+    private static int retryCount = 0;
     public static <T> T retryUntilInputIsValid(final Supplier<T> function, final OutputView outputView) {
         try {
             return function.get();
-        } catch (IllegalArgumentException exception) {
+        } catch (final IllegalArgumentException exception) {
             outputView.printExceptionMessage(exception.getMessage());
+            validateRetryCount();
             return retryUntilInputIsValid(function, outputView);
+        }
+    }
+
+    private static void validateRetryCount() {
+        if (retryCount++ >= MAX_RETRY_COUNT) {
+            throw new IllegalArgumentException("[ERROR] 재시도 횟수를 초과했습니다.");
         }
     }
 
