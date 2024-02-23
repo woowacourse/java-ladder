ab456a42c8c66eb25c514e23a3bd1e469ee8068b
slimsha2dy <chy2581@naver.com>
2024-02-23 19:26:48 +0900

    refactor: LadderGameTest의 입력값 크기 수정

--- src/test/java/domain/LadderGameTest.java
+++ src/test/java/domain/LadderGameTest.java
@@ -15,17 +15,13 @@ public class LadderGameTest {
     @DisplayName("입력받은 이름과 사다리 높이에 따른 결과를 String으로 반환한다.")
     @Test
     void getResultTest() {
-        List<String> names = List.of("1", "2", "3", "4", "5");
-        CustomGenerator customGenerator = new CustomGenerator(List.of(false, true, false, true, false));
-        LadderGame ladderGame = new LadderGame(names, new Height(5), customGenerator);
+        List<String> names = List.of("1", "2");
+        CustomGenerator customGenerator = new CustomGenerator(List.of(false, true));
+        LadderGame ladderGame = new LadderGame(names, new Height(1), customGenerator);
         Assertions.assertThat(ladderGame.getResult())
                 .isEqualTo(List.of(
-                        "     1     2     3     4     5",
-                        UNCONNECTED.getBridge() + CONNECTED.getBridge() + UNCONNECTED.getBridge() + CONNECTED.getBridge() + UNCONNECTED.getBridge(),
-                        UNCONNECTED.getBridge() + CONNECTED.getBridge() + UNCONNECTED.getBridge() + CONNECTED.getBridge() + UNCONNECTED.getBridge(),
-                        UNCONNECTED.getBridge() + CONNECTED.getBridge() + UNCONNECTED.getBridge() + CONNECTED.getBridge() + UNCONNECTED.getBridge(),
-                        UNCONNECTED.getBridge() + CONNECTED.getBridge() + UNCONNECTED.getBridge() + CONNECTED.getBridge() + UNCONNECTED.getBridge(),
-                        UNCONNECTED.getBridge() + CONNECTED.getBridge() + UNCONNECTED.getBridge() + CONNECTED.getBridge() + UNCONNECTED.getBridge()
+                        "     1     2",
+                        UNCONNECTED.getBridge() + CONNECTED.getBridge()
                 ));
     }
 }
