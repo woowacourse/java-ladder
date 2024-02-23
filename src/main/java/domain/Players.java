957079e7b9486f6a29bc812e5e42ce345ecac497
kunsanglee <gosmdochee2@gmail.com>
2024-02-23 17:09:22 +0900

    refactor: Players 및 PlayersTest 리팩토링
    - Players 생성시 매개변수로 List<Player>를 받던 것에서 List<String> 타입을 받도록 수정하여
    Players 내부에서 List<Player>를 생성하여 할당하게 했다.
    
    - Ladder 생성시 Players의 총 인원을 중요한 로직으로 사용하기 때문에 List<String> 타입의 이름의 사이즈를 넘겨주는 대신
    이름을 받아 생성한 Players 객체에 사다리게임에 참여하는 총 인원을 반환하는 메서드를 만들어 넘겨주는 것으로 변경했다.
    
    - PlayersTest에 Players를 생성시 2명 이상 10명 이하인 경우 예외가 발생하지 않는 테스트 코드를 경계값 테스트하도록 수정하고
    Players 생성자의 매개변수로 List<String>을 받도록 코드를 수정했다.

--- src/main/java/domain/Players.java
+++ src/main/java/domain/Players.java
@@ -1,6 +1,7 @@
 package domain;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Players {
 
@@ -9,18 +10,31 @@ public class Players {
 
     private final List<Player> players;
 
-    public Players(List<Player> players) {
-        validate(players);
-        this.players = players;
+    public Players(List<String> names) {
+        validate(names);
+        this.players = convertToPlayer(names);
     }
 
-    private void validate(List<Player> players) {
-        if (players.size() < MINIMUM_PLAYER_SIZE || players.size() > MAXIMUM_PLAYER_SIZE) {
-            throw new IllegalArgumentException(String.format("2명 이상, 10명 이하인 인원만 입력해주세요. 입력한 인원 : %d", players.size()));
+    private void validate(List<String> names) {
+        int size = names.size();
+        if (size < MINIMUM_PLAYER_SIZE || size > MAXIMUM_PLAYER_SIZE) {
+            throw new IllegalArgumentException(String.format("2명 이상, 10명 이하인 인원만 입력해주세요. 입력한 인원 : %d", size));
         }
     }
 
-    public int getSize() {
-        return this.players.size();
+    private List<Player> convertToPlayer(List<String> names) {
+        return names.stream()
+                .map(Player::new)
+                .collect(Collectors.toList());
+    }
+
+    public int getTotalPlayerSize() {
+        return players.size();
+    }
+
+    public List<String> getNames() {
+        return players.stream()
+                .map(Player::getName)
+                .collect(Collectors.toList());
     }
 }
--- src/test/java/domain/PlayersTest.java
+++ src/test/java/domain/PlayersTest.java
@@ -14,51 +14,51 @@ public class PlayersTest {
     static Stream<Arguments> createSuccessArguments() {
         return Stream.of(
                 Arguments.arguments(
-                        List.of(new Player("pobi"), new Player("tommy")),
-                        List.of(new Player("pobi"),
-                                new Player("pobi"),
-                                new Player("pobi"),
-                                new Player("pobi"),
-                                new Player("pobi"),
-                                new Player("pobi"),
-                                new Player("pobi"),
-                                new Player("pobi"),
-                                new Player("pobi"),
-                                new Player("pobi")))
+                        List.of("pobi", "tommy"),
+                        List.of("pobi",
+                                "pobi",
+                                "pobi",
+                                "pobi",
+                                "pobi",
+                                "pobi",
+                                "pobi",
+                                "pobi",
+                                "pobi",
+                                "pobi"))
         );
     }
 
     static Stream<Arguments> createFailArguments() {
         return Stream.of(
                 Arguments.arguments(
-                        List.of(new Player("pobi")),
-                        List.of(new Player("asd"),
-                                new Player("asd"),
-                                new Player("asd"),
-                                new Player("asd"),
-                                new Player("asd"),
-                                new Player("asd"),
-                                new Player("asd"),
-                                new Player("asd"),
-                                new Player("asd"),
-                                new Player("asd"),
-                                new Player("asd")))
+                        List.of("pobi"),
+                        List.of("pobi",
+                                "pobi",
+                                "pobi",
+                                "pobi",
+                                "pobi",
+                                "pobi",
+                                "pobi",
+                                "pobi",
+                                "pobi",
+                                "pobi",
+                                "pobi"))
         );
     }
 
     @DisplayName("2명 이상 10명 이하인 경우 예외가 발생하지않는다,")
     @ParameterizedTest
     @MethodSource("createSuccessArguments")
-    public void createSuccess(List<Player> players) {
-        assertThatCode(() -> new Players(players))
+    public void createSuccess(List<String> names) {
+        assertThatCode(() -> new Players(names))
                 .doesNotThrowAnyException();
     }
 
     @DisplayName("2명 미만, 10명 초과하면 예외를 발생시킨다.")
     @ParameterizedTest
     @MethodSource("createFailArguments")
-    public void createFail(List<Player> players) {
-        assertThatCode(() -> new Players(players))
+    public void createFail(List<String> names) {
+        assertThatCode(() -> new Players(names))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
