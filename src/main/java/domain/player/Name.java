a33e2856c66dc659cc7119716545169f3ecc5ca0
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-02-23 11:06:20 +0900

    refactor : 매직 넘버 상수화 , 예외 메시지 추가
    - 5 -> NAME_MAX_SIZE 로 분리
    - 예외 메시지 NAME_MAX_SIZE 포함하여 구체적 출력하게 구현
    
    [관련 피드백 링크](https://github.com/woowacourse/java-ladder/pull/291#discussion_r1499182571)

--- src/main/java/domain/player/Name.java
+++ src/main/java/domain/player/Name.java
@@ -1,8 +1,10 @@
 package domain.player;
 
 public class Name {
+    private static final Integer NAME_MAX_SIZE = 5;
     private final String value;
-    public Name(String value){
+
+    public Name(String value) {
         validate(value);
         this.value = value;
     }
@@ -10,23 +12,27 @@ public class Name {
     public String getValue() {
         return value;
     }
-    private void validate(String inputName){
+
+    private void validate(String inputName) {
         validateAvailableLength(inputName);
         validateBlank(inputName);
         validateContainBlankInName(inputName);
     }
+
     private void validateAvailableLength(String inputName) {
-        if (inputName.length() > 5) {
-            throw new IllegalArgumentException();
+        if (inputName.length() > NAME_MAX_SIZE) {
+            throw new IllegalArgumentException(String.format("이름은 최대 %d글자 입니다!", NAME_MAX_SIZE));
         }
     }
+
     private void validateBlank(String initialInput) {
-        if(initialInput.isBlank()) {
+        if (initialInput.isBlank()) {
             throw new IllegalArgumentException("공백으로 이루어진 이름은 사용할 수 없습니다.");
         }
     }
+
     private void validateContainBlankInName(String inputName) {
-        if(inputName.contains(" ")) {
+        if (inputName.contains(" ")) {
             throw new IllegalArgumentException();
         }
     }
