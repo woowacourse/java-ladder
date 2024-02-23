2ca79ec534a93bde726420b93b20696dbcd75a76
libienz <leekh10195@gmail.com>
2024-02-23 15:32:43 +0900

    refactor: 의도가 드러나지 않는 Config 클래스 삭제 개선

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,8 +1,21 @@
-import config.Config;
+import controller.InputMapper;
+import controller.LadderGameController;
+import domain.NonContinuousLineGenerator;
+import domain.RowLineGenerator;
+import view.InputView;
+import view.MessageResolver;
+import view.ResultView;
 
 public class Application {
     public static void main(String[] args) {
-        Config config = new Config();
-        config.ladderGameController().run();
+        InputView inputView = new InputView();
+        ResultView resultView = new ResultView(new MessageResolver());
+        RowLineGenerator nonContinuousLineGenerator = new NonContinuousLineGenerator();
+        InputMapper inputMapper = new InputMapper();
+
+        LadderGameController ladderGameController = new LadderGameController(inputView, inputMapper, resultView,
+                nonContinuousLineGenerator);
+
+        ladderGameController.run();
     }
 }
