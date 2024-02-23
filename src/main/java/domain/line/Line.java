bfb92d4ef9983e1abcf2b018b542cf1536d9edb8
SungKyum Kim <sungkyum1@naver.com>
2024-02-23 20:23:33 +0900

    remove: 불필요 함수 삭제

--- src/main/java/domain/line/Line.java
+++ src/main/java/domain/line/Line.java
@@ -6,20 +6,12 @@ import java.util.List;
 import java.util.stream.Stream;
 
 public class Line {
-    private final LineValue points;
+    private final Points points;
     private final BooleanGenerator generator;
 
     public Line(final int personCount, final BooleanGenerator generator) {
         this.generator = generator;
-        points = new CreateLine(personCount);
-    }
-
-    private List<Point> createLine(int personCount) {
-        Point firstPoint = Point.from(generator.generate());
-        return Stream.iterate(firstPoint, prevPoint -> {
-            if (firstPoint.isConnected()) return Point.DISCONNECTED;
-            return Point.from(generator.generate());
-        }).limit(personCount - 1).toList();
+        points = new CountToPoints(personCount);
     }
 
     public List<Point> getPoints() {
