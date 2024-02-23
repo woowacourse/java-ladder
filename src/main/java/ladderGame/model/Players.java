7774047e3d9cb96bc45e051eaba7b55a893153d3
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-02-23 12:51:52 +0900

    refactor(Players) : 불필요한 메서드 분리 제거

--- src/main/java/ladderGame/model/Players.java
+++ src/main/java/ladderGame/model/Players.java
@@ -3,6 +3,7 @@ package ladderGame.model;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Players {
     private static final String EXCEPTION_MESSAGE_DUPLICATION_NAME = "참여자들의 이름은 중복될 수 없습니다.";
@@ -20,17 +21,9 @@ public class Players {
     }
 
     private void validate(List<String> names) {
-        validateNotDuplicationName(names);
-        validatePlayerCount(names);
-    }
-
-    private void validateNotDuplicationName(List<String> names) {
         if (new HashSet<>(names).size() != names.size()) {
             throw new IllegalArgumentException(EXCEPTION_MESSAGE_DUPLICATION_NAME);
         }
-    }
-
-    private void validatePlayerCount(List<String> names) {
         if (names.size() < MINIMUM_NAME_COUNT) {
             throw new IllegalArgumentException(EXCEPTION_MESSAGE_LESS_THAN_MINIMUM);
         }
