09033233e1ba5987ebd47e05435dddaece3d0ac0
JUHA <84626225+khabh@users.noreply.github.com>
2024-02-23 13:21:27 +0900

    refactor: LadderStep 생성자 재정의

--- src/main/java/ladder/domain/ladder/LadderStep.java
+++ src/main/java/ladder/domain/ladder/LadderStep.java
@@ -5,8 +5,7 @@ import java.util.List;
 import static java.util.Collections.unmodifiableList;
 
 public record LadderStep(List<Path> ladderPaths) {
-    @Override
-    public List<Path> ladderPaths() {
-        return unmodifiableList(ladderPaths);
+    public LadderStep {
+        ladderPaths = unmodifiableList(ladderPaths);
     }
 }
