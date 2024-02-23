5d9f30f8eb7acef46bc29658bc8802243b0d5cb8
donghoony <aru0504@naver.com>
2024-02-23 11:17:16 +0900

    refactor(Reader): 불필요한 ConsoleReader 클래스 제거

--- src/main/java/LadderMain.java
+++ src/main/java/LadderMain.java
@@ -1,16 +1,17 @@
 import game.LadderGame;
 import generator.LadderFloorGenerator;
 import java.util.Random;
-import view.ConsoleReader;
+import java.util.Scanner;
 import view.InputView;
 import view.OutputView;
 
 public class LadderMain {
 
     public static void main(String[] args) {
-        ConsoleReader consoleReader = new ConsoleReader(System.in);
-        InputView inputView = new InputView(consoleReader);
+        Scanner scanner = new Scanner(System.in);
+        InputView inputView = new InputView(scanner::nextLine);
         OutputView outputView = new OutputView();
+
         Random random = new Random();
         LadderFloorGenerator floorGenerator = new LadderFloorGenerator(random::nextBoolean);
 
