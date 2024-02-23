4ca55966586e3232a9742d795780135bee09d198
devhoya97 <diajbh123@gmail.com>
2024-02-23 16:27:42 +0900

    test : 참가자 정상 생성 테스트 추가

--- src/test/java/domain/PlayerTest.java
+++ src/test/java/domain/PlayerTest.java
@@ -2,24 +2,24 @@ package domain;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class PlayerTest {
 
-    @DisplayName("참가자 이름이 5글자를 초과하면 예외를 발생시킨다.")
-    @Test
-    void overMaximumNameLength() {
-        //given
-        final String playerName = "crrong";
+    @DisplayName("참가자 이름이 1글자 이상 5글자 이하이면 참가자 객체가 잘 생성된다..")
+    @ParameterizedTest
+    @ValueSource(strings = {"a", "aaa12"})
+    void validNameLength(String playerName) {
         //when & then
-        Assertions.assertThatThrownBy(() -> new Player(playerName)).isInstanceOf(IllegalArgumentException.class);
+        Assertions.assertThatCode(() -> new Player(playerName))
+                        .doesNotThrowAnyException();
     }
 
-    @DisplayName("참가자 이름이 1글자 미만이면 예외를 발생시킨다.")
-    @Test
-    void underMinimumNameLength() {
-        //given
-        final String playerName = "";
+    @DisplayName("참가자 이름이 1글자 미만 5글자 초과이면 예외를 발생시킨다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"abc123", ""})
+    void invalidNameLength(String playerName) {
         //when & then
         Assertions.assertThatThrownBy(() -> new Player(playerName)).isInstanceOf(IllegalArgumentException.class);
     }
