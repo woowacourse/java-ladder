2b6f49074f592ebd8b1ce5f927f0f34630a61d7f
Hyeon0208 <dmsgktn0208@gmail.com>
2024-02-23 21:47:07 +0900

    feat : Floor 생성시 연속되는 다리가 있는지 검증하는 기능 추가

--- src/main/java/domain/Floor.java
+++ src/main/java/domain/Floor.java
@@ -1,5 +1,7 @@
 package domain;
 
+import common.exception.message.ExceptionMessage;
+import common.exception.model.ValidationException;
 import java.util.Collections;
 import java.util.List;
 
@@ -7,7 +9,23 @@ public class Floor {
     private final List<LadderBridge> bridges;
 
     public Floor(final List<LadderBridge> bridges) {
+        validateSerialBridge(bridges);
         this.bridges = bridges;
+    }
+
+    private void validateSerialBridge(final List<LadderBridge> bridges) {
+        LadderBridge beforeBridge = bridges.get(0);
+        for (int next = 1; next < bridges.size(); next++) {
+            LadderBridge nextBridge = bridges.get(next);
+            isSerialBridge(beforeBridge, nextBridge);
+            beforeBridge = nextBridge;
+        }
+    }
+
+    private void isSerialBridge(LadderBridge beforeBridge, LadderBridge nextBridge) {
+        if (beforeBridge.equals(LadderBridge.BRIDGE) && beforeBridge == nextBridge) {
+            throw new ValidationException(ExceptionMessage.SERIAL_BRIDGE);
+        }
     }
 
     public List<LadderBridge> getBridges() {
--- src/test/java/domain/FloorTest.java
+++ src/test/java/domain/FloorTest.java
@@ -1,5 +1,7 @@
 package domain;
 
+import common.exception.message.ExceptionMessage;
+import common.exception.model.ValidationException;
 import domain.bridge.strategy.BridgeGeneratorStub;
 import java.util.List;
 import org.assertj.core.api.Assertions;
@@ -7,16 +9,17 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class FloorTest {
-    @Test
-    @DisplayName("가로 라인이 연속되는 포인트는 생성되지 않는다")
-    void createFloorSuccessWithNoneSerialPoints() {
-        int playerCount = 4;
 
-        List<LadderBridge> points = List.of(LadderBridge.BRIDGE, LadderBridge.NONE, LadderBridge.BRIDGE);
+    @Test
+    @DisplayName("가로 라인에서 다리가 연속되어 있다면 예외가 발생한다")
+    void createFloorFailByNoneSerialBridge() {
+        List<LadderBridge> points = List.of(LadderBridge.BRIDGE, LadderBridge.BRIDGE, LadderBridge.NONE);
         BridgeGeneratorStub pointGenerator = new BridgeGeneratorStub();
         pointGenerator.setBridges(points);
+        int pointCount = points.size();
 
-        Floor floor = new Floor(pointGenerator.generate(playerCount - 1));
-        Assertions.assertThat(floor.getBridges()).containsExactlyElementsOf(points);
+        Assertions.assertThatThrownBy(() -> new Floor(pointGenerator.generate(pointCount)))
+                .isInstanceOf(ValidationException.class)
+                .hasMessage(ExceptionMessage.SERIAL_BRIDGE);
     }
 }
