a9126332c0b983320d3ec6799700922e26b1c285
Minjoo <m0522inju@gmail.com>
2024-02-23 16:19:04 +0900

    refactor: Destination의 최대값 관련 숫자 값 상수로 추출

--- src/main/java/ladder/domain/Destination.java
+++ src/main/java/ladder/domain/Destination.java
@@ -1,9 +1,10 @@
 package ladder.domain;
 
 public record Destination(String value) {
+    private static final int MAX_LENGTH = 5;
 
     public Destination {
-        if (value.length() > 5) {
+        if (value.length() > MAX_LENGTH) {
             throw new IllegalArgumentException("실행 결과 수는 사용자 수와 같아야 합니다.");
         }
     }
