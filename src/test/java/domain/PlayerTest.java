72835433cd59332d46f46e85bd24dea5c33243a8
kunsanglee <gosmdochee2@gmail.com>
2024-02-23 16:13:45 +0900

    refactor: PlayerTest 리팩토링
    - 이름의 길이가 1~5 사이가 아니면 에러를 발생하는지 확인하는 실패 케이스 테스트코드에 정확히 에러 메세지를 검증하도록 수정.
    - 입력한 이름이 영어가 아니면 에러를 반환하는 테스트 코드에 메세지까지 검증하도록 수정.

--- src/test/java/domain/PlayerTest.java
+++ src/test/java/domain/PlayerTest.java
@@ -9,16 +9,25 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 public class PlayerTest {
 
-    @DisplayName("입력의 길이가 1~5 사이가 아니면 예외를 발생 시킨다.")
+    @DisplayName("입력의 길이가 1~5 사이가 아니면 예외를 발생 시킨다")
     @ParameterizedTest
     @ValueSource(strings = {"", "abcdef"})
     public void createByLessThanOneLength(String inputName) {
-        assertThatCode(() ->
-                new Player(inputName))
-                .isInstanceOf(IllegalArgumentException.class);
+        assertThatCode(() -> new Player(inputName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(String.format("1글자 이상 5글자 이하의 이름만 입력하세요. 입력한 이름: %s", inputName));
     }
 
-    @DisplayName("정상적인 이름을 입력하면 에러가 발생하지 않는다.")
+    @DisplayName("입력한 이름이 영어가 아니면 에러를 반환한다")
+    @ParameterizedTest
+    @ValueSource(strings = {"123", "우테코", "!@#"})
+    public void createFailByNonAlphabet(String inputName) {
+        assertThatCode(() -> new Player(inputName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(String.format("이름은 영어만 가능합니다. 입력한 이름:%s", inputName));
+    }
+
+    @DisplayName("정상적인 이름을 입력하면 에러가 발생하지 않는다")
     @Test
     public void createSuccess() {
         assertThatCode(() -> new Player("asd"))
