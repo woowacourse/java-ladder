d3a5999d5f3de249b7cff511f2a6b6514dc1c988
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-02-24 18:28:51 +0900

    feat: 결과 이름 빈칸 구현

--- src/main/java/laddergame/domain/Result.java
+++ src/main/java/laddergame/domain/Result.java
@@ -1,10 +1,19 @@
 package laddergame.domain;
 
+import static laddergame.domain.Player.NAME_BLANK_ERROR;
+
 public class Result {
     private final String name;
 
     public Result(final String name) {
+        validate(name);
         this.name = name;
+    }
+
+    private void validate(final String name) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException(NAME_BLANK_ERROR);
+        }
     }
 
     public String getName() {
