09864d7ed83778ab93b31b792ffd47491a38ea6a
eunhee <dmsgml9903@gmail.com>
2024-02-24 09:22:49 +0900

    fix(Height): 높이가 양수임을 검증할 때 0이 검증되지 않는 버그 수정

--- src/main/java/model/Height.java
+++ src/main/java/model/Height.java
@@ -5,7 +5,7 @@ public record Height(int value) {
     private static final String NOT_POSITIVE_HEIGHT = "최대 사다리의 높이는 양수가 되어야 합니다.";
 
     public Height {
-        if (value < 0) {
+        if (value <= 0) {
             throw new IllegalArgumentException(NOT_POSITIVE_HEIGHT);
         }
     }
