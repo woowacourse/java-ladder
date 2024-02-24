553c3bfdeb94682582a4313534d7e472cfc0e6f5
reddevilmidzy <midzy0228@gmail.com>
2024-02-24 14:12:37 +0900

    feat: 실행 결과를 담을 객체 생성

---
+++ src/main/java/model/Result.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.List;
+
+public class Result {
+
+    private final List<String> values;
+
+    public Result(final List<String> values) {
+        this.values = values;
+    }
+
+}
---
+++ src/test/java/model/ResultTest.java
@@ -0,0 +1,17 @@
+package model;
+
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ResultTest {
+
+    @Test
+    @DisplayName("정상적으로 실행 결과 객체를 생성한다.")
+    void createResult() {
+        List<String> values = List.of("꽝", "5000", "꽝", "1000");
+        Assertions.assertThatCode(() -> new Result(values))
+                .doesNotThrowAnyException();
+    }
+}
