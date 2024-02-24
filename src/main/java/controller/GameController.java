160410f223a8361362dd83d1bde6ecc3d16fb008
takoyakimchi <c4nyou@gmail.com>
2024-02-24 18:05:38 +0900

    refactor: 컨트롤러가 구현체에 의존하지 않는 방식으로 변경

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -5,7 +5,7 @@ import domain.Height;
 import domain.Lines;
 import domain.Members;
 import error.ErrorHandler;
-import strategy.RandomPointStrategy;
+import strategy.PointStrategy;
 import view.InputView;
 import view.OutputView;
 
@@ -14,20 +14,27 @@ public class GameController {
     private final InputView inputView;
     private final OutputView outputView;
     private final ErrorHandler errorHandler;
+    private final PointStrategy pointStrategy;
 
-    public GameController(InputView inputView, OutputView outputView) {
+    public GameController(
+        InputView inputView,
+        OutputView outputView,
+        ErrorHandler errorHandler,
+        PointStrategy pointStrategy) {
+
         this.inputView = inputView;
         this.outputView = outputView;
-        this.errorHandler = new ErrorHandler();
+        this.errorHandler = errorHandler;
+        this.pointStrategy = pointStrategy;
     }
 
-    public void run() {
+    public void start() {
         String rawNames = errorHandler.readUntilNoError(inputView::readNames);
         String rawHeight = errorHandler.readUntilNoError(inputView::readHeight);
 
         Members members = Members.from(rawNames);
         Lines lines = Lines.of(
-            members.getCount(), Height.from(rawHeight), new RandomPointStrategy());
+            members.getCount(), Height.from(rawHeight), pointStrategy);
 
         Game game = Game.of(members, lines);
         outputView.printResult(game);
