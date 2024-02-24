3bf30bb3e33533cc288a2a4424a4e86b9637265a
unifolio0 <boho82ha@gmail.com>
2024-02-24 17:33:05 +0900

    test : Ladder 테스트 수정

--- src/test/java/LadderTest.java
+++ src/test/java/LadderTest.java
@@ -1,18 +1,18 @@
 import domain.Height;
+import domain.Ladder;
 import domain.PlayerCount;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class LadderMakerTest {
-
+public class LadderTest {
     @Test
     @DisplayName("높이와 참가자수, 번호생성기가 주어지면 번호에 따른 랜덤 사다리를 생성한다.")
     void makeLadder() {
-        final LadderMaker ladderMaker = LadderMaker.of(new Height(4), PlayerCount.from(3));
+        final Ladder ladder = Ladder.create(new Height(4), PlayerCount.from(3));
 
-        assertThat(ladderMaker.makeLadder()).extracting("lines")
+        assertThat(ladder).extracting("lines")
                 .asList()
                 .hasSize(4);
     }
