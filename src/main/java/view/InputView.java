bd7cc6a8b0a6301e5e34d5d19b49a1d595760e6e
eun-byeol <joeunbyeol98@gmail.com>
2024-02-24 12:42:47 +0900

    refactor: InputView nextStep Console util로 수정

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,11 +1,10 @@
 package view;
 
+import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
 
 public class InputView {
-    private static final Scanner scanner = new Scanner(System.in);
     private static final String SPLIT_DELIMITER = ",";
     private static final String ASK_PLAYER_NAMES = "\n참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     private static final String ASK_LADDER_HEIGHT = "\n최대 사다리 높이는 몇 개인가요?";
@@ -16,7 +15,7 @@ public class InputView {
 
     public static List<String> askPlayerNames() {
         System.out.println(ASK_PLAYER_NAMES);
-        String input = scanner.nextLine();
+        String input = Console.readLine();
         return parsePlayerNames(input);
     }
 
@@ -28,7 +27,7 @@ public class InputView {
 
     public static int askLadderHeight() {
         System.out.println(ASK_LADDER_HEIGHT);
-        String input = scanner.nextLine();
+        String input = Console.readLine();
         return parseLadderHeight(input);
     }
 
