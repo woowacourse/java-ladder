975506e5da738f46bcb115236f88ba73c05f9a66
seokmyungham <hellotjraud@gmail.com>
2024-02-24 20:54:02 +0900

    test(ParticipantsTest): inviteParticipants 메서드 테스트 추가

--- src/test/java/ladder/domain/paticipant/ParticipantsTest.java
+++ src/test/java/ladder/domain/paticipant/ParticipantsTest.java
@@ -1,9 +1,12 @@
-package ladder.domain;
+package ladder.domain.paticipant;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.ArrayList;
 import java.util.List;
+import ladder.domain.participant.Name;
+import ladder.domain.participant.Participants;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -28,5 +31,18 @@ public class ParticipantsTest {
         assertThatThrownBy(() -> new Participants(names))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("참여자 이름은 중복을 허용하지 않습니다");
+    }
+
+    @Test
+    @DisplayName("문자열 리스트를 매개변수로 받아서 Name 리스트를 반환한다")
+    void inviteParticipantsTest() {
+        List<String> names = new ArrayList<>(List.of("aru", "pola", "jazz"));
+
+        Participants participants = new Participants(names);
+
+        List<Name> participantsNames = participants.getNames();
+        assertThat(participantsNames.get(0)).isEqualTo(new Name("aru"));
+        assertThat(participantsNames.get(1)).isEqualTo(new Name("pola"));
+        assertThat(participantsNames.get(2)).isEqualTo(new Name("jazz"));
     }
 }
