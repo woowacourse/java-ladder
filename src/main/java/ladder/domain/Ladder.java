c94eec6b54c5178bd2d03b0a6c5b7feae8aa802b
seokmyungham <hellotjraud@gmail.com>
2024-02-25 02:15:03 +0900

    refactor(Ladder): 원시값 래핑 객체 Height를 제거하고 Ladder로 책임을 위임

--- src/main/java/ladder/domain/Ladder.java
+++ src/main/java/ladder/domain/Ladder.java
@@ -8,14 +8,40 @@ import ladder.domain.randomGenerator.RungGenerator;
 
 public class Ladder {
 
+    private static final int MIN_HEIGHT_RANGE = 1;
+    private static final int MAX_HEIGHT_RANGE = 100;
+
     private final List<Floor> floors;
 
-    public Ladder(Height height, int personCount, RungGenerator rungGenerator) {
+    public Ladder(String inputHeight, int personCount, RungGenerator rungGenerator) {
+        int height = parseAndValidateHeight(inputHeight);
         floors = makeFloors(height, personCount, rungGenerator);
     }
 
-    private List<Floor> makeFloors(Height height, int personCount, RungGenerator rungGenerator) {
-        return IntStream.range(0, height.getHeight())
+    private int parseAndValidateHeight(String inputHeight) {
+        int height = changeInputToInteger(inputHeight);
+        validateHeightRange(height);
+        return height;
+    }
+
+    private int changeInputToInteger(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException("숫자로 입력을 변환할 수 없습니다.");
+        }
+    }
+
+    private void validateHeightRange(int number) {
+        if (number < MIN_HEIGHT_RANGE || MAX_HEIGHT_RANGE < number) {
+            throw new IllegalArgumentException(
+                    "입력된 높이는 " + MIN_HEIGHT_RANGE + " 이상,"
+                            + " " + MAX_HEIGHT_RANGE + " 이하여야 합니다. 입력값 : " + number);
+        }
+    }
+
+    private List<Floor> makeFloors(int height, int personCount, RungGenerator rungGenerator) {
+        return IntStream.range(0, height)
                 .mapToObj(currentHeight -> new Floor(rungGenerator, personCount))
                 .toList();
     }
--- src/test/java/ladder/domain/LadderTest.java
+++ src/test/java/ladder/domain/LadderTest.java
@@ -1,10 +1,10 @@
 package ladder.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import ladder.domain.dto.LadderResponseDto;
 import ladder.domain.dto.FloorResponseDto;
-import org.junit.jupiter.api.BeforeEach;
+import ladder.domain.dto.LadderResponseDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -12,17 +12,10 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 public class LadderTest {
 
-    private Height height;
-
-    @BeforeEach
-    void setUp() {
-        height = new Height("3");
-    }
-
     @Test
     @DisplayName("매개변수 height와 생성되는 사다리와 사다리의 높이는 일치해야 한다.")
     void ladderHeightTest() {
-        Ladder ladder = new Ladder(height, 5, () -> Rung.EXIST);
+        Ladder ladder = new Ladder("3", 5, () -> Rung.EXIST);
         LadderResponseDto resultLadders = ladder.getResultLadders();
         int ladderHeight = resultLadders.ladderResult().size();
 
@@ -33,11 +26,31 @@ public class LadderTest {
     @ValueSource(ints = {0, 1, 2})
     @DisplayName("생성되는 사다리의 가로 공간은 사람 수 보다 1적어야 한다.")
     void ladderHorizontalLengthTest(int heightPosition) {
-        Ladder ladder = new Ladder(height, 5, () -> Rung.EXIST);
+        Ladder ladder = new Ladder("3", 5, () -> Rung.EXIST);
         LadderResponseDto resultLadders = ladder.getResultLadders();
         FloorResponseDto floorResponseDto = resultLadders.ladderResult().get(heightPosition);
         int maxRungsCount = floorResponseDto.buildStatusList().size();
 
         assertThat(maxRungsCount).isEqualTo(4);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"-1", "0",})
+    @DisplayName("높이는 양의 정수만 가능하다.")
+    void isPositiveIntegerTest(String number) {
+
+        assertThatThrownBy(() -> new Height(number))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("입력된 높이는 1 이상, 100 이하여야 합니다. 입력값 : " + number);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"&", "op%la",})
+    @DisplayName("높이는 숫자로 변환이 가능해야 함을 테스트")
+    void isCanTranslateToIntegerTest(String input) {
+
+        assertThatThrownBy(() -> new Height(input))
+                .isInstanceOf(NumberFormatException.class)
+                .hasMessage("숫자로 입력을 변환할 수 없습니다.");
     }
 }
