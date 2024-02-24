1e977c2d179f9f8a19d45c840673ccab68f14b98
seokmyungham <hellotjraud@gmail.com>
2024-02-24 22:55:16 +0900

    refactor(MockBooleanGenerator): 반복 루프를 iterator로 개선

--- src/test/java/ladder/MockBooleanGenerator.java
+++ src/test/java/ladder/MockBooleanGenerator.java
@@ -1,23 +1,24 @@
 package ladder;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 import ladder.domain.randomGenerator.RungGenerator;
 
 public class MockBooleanGenerator implements RungGenerator {
 
-    private final List<Boolean> mockStatus;
-    public int currentIdx = 0;
+    private final Iterable<Boolean> mockStatus;
+    private final Iterator<Boolean> iterator;
 
-    public MockBooleanGenerator(List<Boolean> mockStatus) {
-        this.mockStatus = new ArrayList<>(mockStatus);
+    public MockBooleanGenerator(Iterable<Boolean> mockStatus) {
+        this.mockStatus = mockStatus;
+        this.iterator = mockStatus.iterator();
     }
 
     @Override
     public boolean getRandomBooleanStatus() {
-        if (currentIdx >= mockStatus.size()) {
-            throw new IllegalArgumentException("mock 현재 인덱스가 최대 범위를 벗어났습니다.");
+        if (!iterator.hasNext()) {
+            throw new NoSuchElementException("mock 인덱스를 넘어섰습니다.");
         }
-        return mockStatus.get(currentIdx++);
+        return iterator.next();
     }
 }
