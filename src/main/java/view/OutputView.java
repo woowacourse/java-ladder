db76610efd2e72fa3ae64938c0a6b375137706ca
cutehumanS2 <cherrycoke@sookmyung.ac.kr>
2024-02-24 21:10:00 +0900

    feat: 실행결과 문구 출력

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -4,6 +4,10 @@ import java.util.List;
 
 public class OutputView {
 
+    public void printResultMessage() {
+        System.out.print("\n실행결과\n");
+    }
+
     public void printLadder(List<String> ladder) {
         System.out.println(String.join("\n", ladder));
     }
