31a70f9eeccb4fc36b3cd1051dde3ca4a5961e37
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-02-24 18:08:26 +0900

    test: 결과 이름 빈칸 테스트

--- src/test/java/laddergame/domain/ResultsTest.java
+++ src/test/java/laddergame/domain/ResultsTest.java
@@ -1,10 +1,13 @@
 package laddergame.domain;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("결과")
 class ResultsTest {
@@ -19,5 +22,16 @@ class ResultsTest {
 
         //then
         assertEquals(results.getResults().get(0).name(), resultNames.get(0));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "    "})
+    @DisplayName("결과 이름에 빈칸을 허용하지 않는다.")
+    public void resultsBlankException(final String name) {
+        //given
+        List<String> resultNames = List.of(name);
+
+        //when & then
+        assertThrows(IllegalArgumentException.class, () -> Results.from(resultNames));
     }
 }
