b4e842ce3643d8e395c5683e57e27820d0304a50
le2sky <leehaneul990623@gmail.com>
2024-02-24 13:27:52 +0900

    test: 누락된 이름 검증 테스트 추가 작성

--- src/test/java/domain/PlayerTest.java
+++ src/test/java/domain/PlayerTest.java
@@ -1,9 +1,12 @@
 package domain;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class PlayerTest {
 
@@ -12,5 +15,20 @@ class PlayerTest {
     void createTest() {
         assertThatCode(() -> new Player("산초"))
                 .doesNotThrowAnyException();
+    }
+
+    @DisplayName("사용자의 이름은 양 옆 공백을 제외한 1자 이상이다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "\n", "\r\n", "\r"})
+    void checkPlayerNameLengthMin(String source) {
+        assertThatThrownBy(() -> new Player(source))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("사용자의 이름은 양 옆 공백을 제외한 5자 이하이다.")
+    @Test
+    void checkPlayerNameLengthMax() {
+        assertThatThrownBy(() -> new Player("abcdef"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
