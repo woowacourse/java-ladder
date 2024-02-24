d09fd51762b568c2d6cd8df9ade2011c7b21f9d6
zangsu <zangsu_@naver.com>
2024-02-24 14:41:26 +0900

    feat : 일급 컬렉션 `Results` 생성

---
+++ src/main/java/domain/result/Results.java
@@ -0,0 +1,22 @@
+package domain.result;
+
+import java.util.List;
+
+public class Results {
+    private static final int MIN_RESULT_SIZE = 2;
+
+    private final List<Result> results;
+
+    public Results(List<String> resultNames) {
+        validate(resultNames);
+        this.results = resultNames.stream()
+                .map(Result::new)
+                .toList();
+    }
+
+    private void validate(List<String> resultNames) {
+        if (resultNames.size() < MIN_RESULT_SIZE) {
+            throw new IllegalArgumentException("결과는 " + MIN_RESULT_SIZE + " 개 이상으로 구성되어야 합니다.");
+        }
+    }
+}
