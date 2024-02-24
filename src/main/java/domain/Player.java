5d6995978d41893fc0b1b302422276fb3c25b619
le2sky <leehaneul990623@gmail.com>
2024-02-24 13:28:19 +0900

    refactor: 사용자 이름 검증시 공백을 제외하도록 개선

--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -14,7 +14,9 @@ public class Player {
     }
 
     private void validateNameLength(String name) {
-        if (name.length() < MIN_NAME_LENGTH || name.length() > MAX_NAME_LENGTH) {
+        int nameLength = name.trim().length();
+
+        if (nameLength < MIN_NAME_LENGTH || nameLength > MAX_NAME_LENGTH) {
             String message = String.format("이름은 %d~%d자 사이여야 합니다.", MIN_NAME_LENGTH, MAX_NAME_LENGTH);
 
             throw new IllegalArgumentException(message);
