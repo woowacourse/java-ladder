fc7ba3b6019d1639c24a707b1fce4aa6b7cc7a85
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-02-24 19:45:23 +0900

    refactor(RandomBooleanGenerator) : 생성자에서 랜덤 객체 생성하여 생성 비용 절감

--- src/main/java/ladderGame/model/RandomBooleanGenerator.java
+++ src/main/java/ladderGame/model/RandomBooleanGenerator.java
@@ -3,8 +3,15 @@ package ladderGame.model;
 import java.util.Random;
 
 public class RandomBooleanGenerator implements BooleanGenerator {
+
+    private final Random random;
+
+    public RandomBooleanGenerator() {
+        this.random = new Random();
+    }
+
     @Override
     public boolean generate() {
-        return new Random().nextBoolean();
+        return random.nextBoolean();
     }
 }
