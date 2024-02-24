096952fb6bd61114fc0753d5f8f225b3686adb3b
takoyakimchi <c4nyou@gmail.com>
2024-02-24 18:00:55 +0900

    refactor: 불필요한 import 제거

--- src/test/java/domain/MembersTest.java
+++ src/test/java/domain/MembersTest.java
@@ -4,8 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import java.util.ArrayList;
-import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
