9dd66ca5439f923f9dc5446d0949a030da0afea8
SCY <helenason@naver.com>
2024-02-24 21:33:09 +0900

    refactor: String 덧셈 연산 StringBuilder 사용하여 불필요한 객체 생성 과정 제거

--- src/main/java/view/MessageResolver.java
+++ src/main/java/view/MessageResolver.java
@@ -4,36 +4,39 @@ import domain.Line;
 import domain.Lines;
 import domain.Members;
 import domain.Point;
+import java.util.List;
 
 public class MessageResolver {
 
     public static final int MAX_NAME_LENGTH = 5;
 
     public String resolveMembers(Members members) {
-        String result = "";
+        StringBuilder stringBuilder = new StringBuilder();
         for (String name : members.getNames()) {
-            result += String.format("%" + MAX_NAME_LENGTH + "s ", name);
+            stringBuilder.append(String.format("%" + MAX_NAME_LENGTH + "s ", name));
         }
-        return result;
+        return stringBuilder.toString();
     }
 
-    public String resolveLines(Lines lines) {
-        String result = "";
-        for (int i = 0; i < lines.getLines().size(); i++) {
-            result += resolveLine(lines.getLines().get(i));
-            result += "\n";
+    public String resolveLines(Lines ladder) {
+        StringBuilder stringBuilder = new StringBuilder();
+        List<Line> lines = ladder.getLines();
+        for (Line line : lines) {
+            stringBuilder.append(resolveLine(line));
+            stringBuilder.append("\n");
         }
-        return result;
+        return stringBuilder.toString();
     }
 
     private String resolveLine(Line line) {
-        String result = " ".repeat(MAX_NAME_LENGTH - 1);
-        for (Point point : line.getPoints()) {
-            result += "|";
-            result += resolvePoint(point);
+        List<Point> points = line.getPoints();
+        StringBuilder stringBuilder = new StringBuilder(" ".repeat(MAX_NAME_LENGTH - 1));
+        for (Point point : points) {
+            stringBuilder.append("|");
+            stringBuilder.append(resolvePoint(point));
         }
-        result += "|";
-        return result;
+        stringBuilder.append("|");
+        return stringBuilder.toString();
     }
 
     private String resolvePoint(Point point) {
