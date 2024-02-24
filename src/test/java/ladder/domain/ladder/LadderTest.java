d88b8b1c39084632c098d9ce8f74b02e17fc2583
김민수 MINSOO KIM <alstn113@gmail.com>
2024-02-24 15:42:04 +0900

    test: 사다리의 높이와 시작 인덱스를 테스트

--- src/test/java/ladder/domain/ladder/LadderTest.java
+++ src/test/java/ladder/domain/ladder/LadderTest.java
@@ -1,6 +1,7 @@
 package ladder.domain.ladder;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
 import java.util.List;
@@ -42,5 +43,23 @@ class LadderTest {
             softly.assertThat(lines.get(2).getRungs())
                     .containsExactly(Rung.EXIST, Rung.EMPTY, Rung.EMPTY);
         });
+    }
+
+    @Test
+    @DisplayName("사다리의 높이가 범위를 벗어날 경우 예외를 발생한다")
+    void testValidateHeightRange() {
+        int height = 0;
+        RungGenerator rungGenerator = new RandomRungGenerator();
+        assertThatThrownBy(() -> Ladder.of(height, 4, rungGenerator))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("시작 인덱스가 범위를 벗어날 경우 예외를 발생한다")
+    void testValidateIndexRange() {
+        RungGenerator rungGenerator = new RandomRungGenerator();
+        Ladder ladder = Ladder.of(3, 4, rungGenerator);
+        assertThatThrownBy(() -> ladder.findEndIndex(4))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
