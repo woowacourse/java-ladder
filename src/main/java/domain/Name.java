7a7a3601eb9793ae3c037d38b147fef4a857ac3d
seunghye218 <seunghye@student.42seoul.kr>
2024-02-24 15:08:36 +0900

    refactor: 문자열 연결 -> 문자열 포맷팅

--- src/main/java/domain/Name.java
+++ src/main/java/domain/Name.java
@@ -3,13 +3,14 @@ package domain;
 import java.util.regex.Pattern;
 
 public record Name(String name) {
+
     private static final int NAME_LENGTH_MIN = 1;
     private static final int NAME_LENGTH_MAX = 5;
 
     public Name {
         if (name.length() < NAME_LENGTH_MIN || name.length() > NAME_LENGTH_MAX) {
-            throw new IllegalArgumentException("이름의 길이는 " + NAME_LENGTH_MIN + " ~ " +
-                    NAME_LENGTH_MAX + " 자 이어야 합니다.");
+            throw new IllegalArgumentException(
+                    "이름의 길이는 %d ~ %d 자 이어야 합니다.".formatted(NAME_LENGTH_MIN, NAME_LENGTH_MAX));
         }
         if (!Pattern.matches("^[A-Za-z0-9]+", name)) {
             throw new IllegalArgumentException("이름이 영어와 숫자가 아니라면 예외가 발생한다");
