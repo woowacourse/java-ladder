d399addba40896a26b41518dfdbc12c9405a48af
takoyakimchi <c4nyou@gmail.com>
2024-02-24 18:36:13 +0900

    refactor: Point enum에서 불필요한 값 제거

--- src/main/java/domain/Point.java
+++ src/main/java/domain/Point.java
@@ -2,12 +2,5 @@ package domain;
 
 public enum Point {
 
-    CONNECTED(true),
-    DISCONNECTED(false);
-
-    private final Boolean isConnected;
-
-    Point(Boolean isConnected) {
-        this.isConnected = isConnected;
-    }
+    CONNECTED, DISCONNECTED
 }
