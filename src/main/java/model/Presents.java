87a3a93dfba7b3d50a01b2470970e05bcec687f9
haiseong <wjdgotjd9908@gmail.com>
2024-02-24 22:52:24 +0900

    feat(PresentsTest): 상품 지급 로직 테스트 작성

--- src/main/java/model/Presents.java
+++ src/main/java/model/Presents.java
@@ -11,12 +11,19 @@ public class Presents {
 
     public static Presents from(List<String> presentNames, int personCount) {
         validatePresentCount(presentNames.size(), personCount);
-        return null;
+        final List<Present> presentGroup = presentNames.stream()
+                .map(Present::new)
+                .toList();
+        return new Presents(presentGroup);
     }
 
     private static void validatePresentCount(int presentCount, int personCount) {
         if (presentCount != personCount) {
             throw new IllegalArgumentException("상품의 갯수는 사람 이름수와 같아야 합니다.");
         }
+    }
+
+    public Present getPresent(int column) {
+        return presentGroup.get(column);
     }
 }
