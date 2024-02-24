8f9c97bc4683e0a4cd36e6e06bcca4dbe9239d5f
fromitive <fromitive@gmail.com>
2024-02-24 16:45:31 +0900

    test: 이름 테스트 커버리지 추가

--- src/test/java/domain/NameTest.java
+++ src/test/java/domain/NameTest.java
@@ -2,8 +2,11 @@ package domain;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class NameTest {
@@ -24,5 +27,26 @@ class NameTest {
         assertThatThrownBy(() -> new Name(name))
                 .isInstanceOf(LadderGameException.class)
                 .hasMessage(ExceptionType.INVALID_NAME_CHARACTER.getMessage());
+    }
+
+    @ParameterizedTest
+    @CsvSource({"aaa,aaa,true",
+            "aaa,bbb,false"})
+    @DisplayName("이름 문자열이 같으면 Name.equals는 true 다르면 false")
+    void validateCreateName(String leftName, String rightName, Boolean expected) {
+        Name name1 = new Name(leftName);
+        Name name2 = new Name(rightName);
+        Boolean actual = name1.equals(name2);
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
+
+    @Test
+    @DisplayName("이름 객체는 이름 문자열을 반환할 수 있음")
+    void validateCreateName() {
+        String expected = "abcd";
+        Name name = new Name(expected);
+        String actual = name.getName();
+        Assertions.assertThat(actual).isEqualTo(expected);
     }
 }
