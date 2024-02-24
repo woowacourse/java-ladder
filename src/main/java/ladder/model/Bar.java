a2c81303e1725cce5ab8ba19d451715ec5c1f98b
BurningFalls <sherlocky31415@gmail.com>
2024-02-24 13:55:41 +0900

    refactor(bar): 사용하지 않는 변수 삭제

--- src/main/java/ladder/model/Bar.java
+++ src/main/java/ladder/model/Bar.java
@@ -2,11 +2,9 @@ package ladder.model;
 
 public class Bar {
     private final int leftPosition;
-    private final int rightPosition;
 
     public Bar(int leftPosition) {
         this.leftPosition = leftPosition;
-        this.rightPosition = leftPosition + 1;
     }
 
     public int getLeftPosition() {
