f2178c8e8f9c956326acd57a955a265436a8cb16
masonkimseoul <masonkimseoul@gmail.com>
2024-02-24 17:13:21 +0900

    refactor: 가독성 확보를 위해 상수화 및 상수, 변수 이름 변경

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -6,7 +6,8 @@ import java.util.Scanner;
 
 public class InputView {
 
-    private static final String COMMA = ",";
+    private static final String SEPARATOR = ",";
+    private static final int MIN_LADDER_HEIGHT = 1;
 
     private final Scanner scanner = new Scanner(System.in);
 
@@ -14,7 +15,7 @@ public class InputView {
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
         String input = scanner.nextLine();
         validateInput(input);
-        return Arrays.stream(input.split(COMMA))
+        return Arrays.stream(input.split(SEPARATOR))
                 .toList();
     }
 
@@ -29,15 +30,15 @@ public class InputView {
     }
 
     private void validateInput(String input) {
-        if (input.isEmpty() || input.endsWith(COMMA)) {
+        if (input.isEmpty() || input.endsWith(SEPARATOR)) {
             throw new IllegalArgumentException("[ERROR] 올바르지 않은 사용자 이름입니다.");
         }
     }
 
     private int convertInteger(String input) {
-        int value = parseInt(input);
-        validatePositive(value);
-        return value;
+        int ladderHeight = parseInt(input);
+        validatePositive(ladderHeight);
+        return ladderHeight;
     }
 
     private int parseInt(String input) {
@@ -48,8 +49,8 @@ public class InputView {
         }
     }
 
-    private void validatePositive(int value) {
-        if (value < 1) {
+    private void validatePositive(int ladderHeight) {
+        if (ladderHeight < MIN_LADDER_HEIGHT) {
             throw new IllegalArgumentException("[ERROR] 최대 사다리 높이는 양의 정수여야 합니다.");
         }
     }
