6887a248deb796d949961ab0f3a569807ed8ffc9
haiseong <wjdgotjd9908@gmail.com>
2024-02-24 22:49:07 +0900

    test(PresentsTest): 상품 지급 로직 테스트 작성

--- src/test/java/model/PresentsTest.java
+++ src/test/java/model/PresentsTest.java
@@ -1,5 +1,6 @@
 package model;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -7,6 +8,7 @@ import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class PresentsTest {
@@ -26,9 +28,17 @@ class PresentsTest {
     @ValueSource(ints = {3, 5})
     void createPresentsThrowExceptionWhenInvalidPresentNamesCount(int personCount) {
         List<String> presentNames = List.of("꽝", "5000", "꽝", "3000");
-        assertThatThrownBy(() ->  Presents.from(presentNames, personCount))
+        assertThatThrownBy(() -> Presents.from(presentNames, personCount))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("상품의 갯수는 사람 이름수와 같아야 합니다.");
     }
 
+    @ParameterizedTest(name = "위치에 따라 상품을 지급합니다.")
+    @CsvSource({"0, '꽝'", "1, '5000'", "2, '꽝'", "3, '3000'"})
+    void getPresent(int column, String expected) {
+        Presents presents = Presents.from(List.of("꽝", "5000", "꽝", "3000"), 4);
+        Present present = presents.getPresent(column);
+        String presentName = present.name();
+        assertThat(presentName).isEqualTo(expected);
+    }
 }
