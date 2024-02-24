1838eab4d63b0d3071d50309decd738a9aa48ba5
haiseong <wjdgotjd9908@gmail.com>
2024-02-24 14:21:10 +0900

    test(LineTest): Line의 column에서 좌우에 경로가 있는지 확인하는 테스트 추가

--- src/test/java/model/LineTest.java
+++ src/test/java/model/LineTest.java
@@ -1,11 +1,14 @@
 package model;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import model.path.Path;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class LineTest {
 
@@ -24,5 +27,23 @@ class LineTest {
         assertThatThrownBy(() -> new Line(paths))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("사다리의 경로는 연달아 있을 수 없습니다.");
+    }
+
+    @ParameterizedTest(name = "왼쪽으로 길이 있는지 확인한다.")
+    @CsvSource({"0, false", "1, true", "2, false", "3, false", "4, true"})
+    void hasLeftPath(int column, boolean expected) {
+        List<Path> paths = List.of(Path.EXIST, Path.NOT_EXIST, Path.NOT_EXIST, Path.EXIST);
+        Line line = new Line(paths);
+        boolean hasLeftPath = line.hasLeftPath(column);
+        assertThat(hasLeftPath).isEqualTo(expected);
+    }
+
+    @ParameterizedTest(name = "오른쪽으로 길이 있는지 확인한다.")
+    @CsvSource({"0, true", "1, false", "2, false", "3, true", "4, false"})
+    void hasRightPath(int column, boolean expected) {
+        List<Path> paths = List.of(Path.EXIST, Path.NOT_EXIST, Path.NOT_EXIST, Path.EXIST);
+        Line line = new Line(paths);
+        boolean hasRightPath = line.hasRightPath(column);
+        assertThat(hasRightPath).isEqualTo(expected);
     }
 }
