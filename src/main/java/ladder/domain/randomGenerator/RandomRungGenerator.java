6f430e3aaf7c734093cec391b3a688f4f51661a7
seokmyungham <hellotjraud@gmail.com>
2024-02-25 00:01:39 +0900

    refactor(RandomRungGenerator): getRandomStatusRung이 Rung을 반환하도록 리팩토링

--- src/main/java/ladder/domain/randomGenerator/RandomRungGenerator.java
+++ src/main/java/ladder/domain/randomGenerator/RandomRungGenerator.java
@@ -1,13 +1,17 @@
 package ladder.domain.randomGenerator;
 
 import java.util.Random;
+import ladder.domain.Rung;
 
 public class RandomRungGenerator implements RungGenerator {
 
-    private static final Random random = new Random();
+    private static final Random RANDOM = new Random();
 
     @Override
-    public boolean getRandomBooleanStatus() {
-        return random.nextBoolean();
+    public Rung getRandomStatusRung() {
+        if (RANDOM.nextBoolean()) {
+            return Rung.EXIST;
+        }
+        return Rung.NOT_EXIST;
     }
 }
