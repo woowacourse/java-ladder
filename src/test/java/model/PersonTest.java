9d892458bbe4e430328431c1f377e8d490d9df27
haiseong <wjdgotjd9908@gmail.com>
2024-02-24 18:42:32 +0900

    test(PersonTest): 사람이 사다리를 타고 내려가는 기능의 테스트 작성

--- src/test/java/model/PersonTest.java
+++ src/test/java/model/PersonTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import model.path.Path;
+import model.path.PathGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -39,5 +40,35 @@ class PersonTest {
         person.moveHorizontally(line);
         int after = person.getColumn();
         assertThat(after).isEqualTo(expected);
+    }
+
+    @ParameterizedTest(name = "사다리를 타고 올바른 경로로 내려가야 합니다.")
+    @CsvSource({"0, 2", "1, 1", "2, 0", "3, 3", "4, 4"})
+    void climbDown(int before, int expected) {
+        List<List<Path>> lines = List.of(
+                List.of(Path.EXIST, Path.NOT_EXIST, Path.NOT_EXIST, Path.EXIST),
+                List.of(Path.NOT_EXIST, Path.EXIST, Path.NOT_EXIST, Path.EXIST),
+                List.of(Path.EXIST, Path.NOT_EXIST, Path.EXIST, Path.NOT_EXIST),
+                List.of(Path.NOT_EXIST, Path.NOT_EXIST, Path.EXIST, Path.NOT_EXIST)
+        );
+        Ladder ladder = Ladder.from(4, 5, new TestPathGenerator(lines));
+        Person person = Person.from("loky", before);
+        person.climbDown(ladder);
+        int after = person.getColumn();
+        assertThat(after).isEqualTo(expected);
+    }
+
+    private class TestPathGenerator implements PathGenerator {
+        private final List<List<Path>> lines;
+        private int lineNumber = 0;
+
+        private TestPathGenerator(List<List<Path>> lines) {
+            this.lines = lines;
+        }
+
+        @Override
+        public List<Path> generate(int count) {
+            return lines.get(lineNumber++);
+        }
     }
 }
