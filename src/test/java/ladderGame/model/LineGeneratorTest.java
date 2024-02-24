a31556ccde270349abc6b6e20a18a8a7e4f5f458
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-02-24 21:20:04 +0900

    test(makeLine) : 라인은 연속적으로 연결될 수 없다.

---
+++ src/test/java/ladderGame/model/LineGeneratorTest.java
@@ -0,0 +1,18 @@
+package ladderGame.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LineGeneratorTest {
+
+    @Test
+    @DisplayName("라인은 연속적으로 연결될 수 없다.")
+    void makeLine() {
+        List<ConnectionStatus> isConnections = new LineGenerator(() -> true).makeLine(3);
+        assertEquals(isConnections, List.of(ConnectionStatus.CONNECTION, ConnectionStatus.DISCONNECTION, ConnectionStatus.CONNECTION));
+    }
+}
