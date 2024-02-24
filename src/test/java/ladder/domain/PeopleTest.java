b5be01018a9b702ea4bf200778017fc7ae8e2604
mzeong <mjeong@sookmyung.ac.kr>
2024-02-24 11:43:35 +0900

    test(PeopleTest): 이름이 1글자 미만이면 예외 발생

--- src/test/java/ladder/domain/PeopleTest.java
+++ src/test/java/ladder/domain/PeopleTest.java
@@ -9,7 +9,7 @@ class PeopleTest {
 
     @Test
     @DisplayName("사람 이름은 쉼표(,)를 기준으로 구분한다.")
-    void createNames() {
+    void separateByCommas() {
         // given
         People people = new People("pobi,honux,crong,jk");
 
@@ -22,14 +22,23 @@ class PeopleTest {
 
     @Test
     @DisplayName("이름이 5글자를 초과하면 예외가 발생한다.")
-    void createInvalidNames() {
+    void createExceedLengthName() {
+        // when & then
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> new People("pobipobi,honux,crong,jk"));
+                .isThrownBy(() -> new People("pobipobi"));
     }
 
     @Test
-    @DisplayName("이름에 공백은 포함하지 않는다.")
-    void createValidNames() {
+    @DisplayName("이름이 1글자 미만이면 예외가 발생한다.")
+    void createEmptyName() {
+        // when & then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new People(""));
+    }
+
+    @Test
+    @DisplayName("이름의 글자 수를 셀 때 앞뒤 공백은 포함하지 않는다.")
+    void countByRemovingSpaces() {
         // given
         String name = "  jk  ";
 
