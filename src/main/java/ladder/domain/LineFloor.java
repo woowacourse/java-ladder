7bf35c7dbc95ddaef01b5e4493bc6f2d36b4fbc2
kelly6bf <kelly6bf@gmail.com>
2024-02-25 13:14:26 +0900

    feat: 유효하지 않은 라인 층 번호 검증 로직 구현

--- src/main/java/ladder/domain/LineFloor.java
+++ src/main/java/ladder/domain/LineFloor.java
@@ -5,6 +5,9 @@ public class LineFloor {
     private final int lineFloor;
 
     public LineFloor(final int lineFloor) {
+        if (lineFloor < 1) {
+            throw new IllegalArgumentException("유효하지 않은 라인 층 번호입니다.");
+        }
         this.lineFloor = lineFloor;
     }
 }
