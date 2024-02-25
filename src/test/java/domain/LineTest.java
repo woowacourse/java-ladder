046b06429b007a35def01ffe1e6dade244f7d915
eunjungL <dldmswjd322@gmail.com>
2024-02-26 00:31:22 +0900

    test: 사다리 다음 행에서 이동 결과 탐색 테스트 추가

--- src/test/java/domain/LineTest.java
+++ src/test/java/domain/LineTest.java
@@ -3,6 +3,7 @@ package domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 import util.TestBooleanGenerator;
 
@@ -28,5 +29,16 @@ class LineTest {
         Line line = new Line(4);
 
         assertThat(line.makeLine(testDirectionGenerator)).isEqualTo(expected);
+    }
+
+    @DisplayName("라인 상에서 이동 결과 탐색")
+    @ParameterizedTest
+    @CsvSource(value = {"0,1", "1,0", "2,3", "3,2"}, delimiter = ',')
+    void getLineResultTest(int actual, int expected) {
+        TestBooleanGenerator testtestBooleanGenerator = new TestBooleanGenerator(true);
+        Line line = new Line(4);
+        line.makeLine(testtestBooleanGenerator);
+
+        assertThat(line.decideNextPosition(actual)).isEqualTo(expected);
     }
 }
