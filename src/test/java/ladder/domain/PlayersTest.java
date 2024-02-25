0690d203859468f2cd412ac396769227adfae032
kelly6bf <kelly6bf@gmail.com>
2024-02-25 13:33:45 +0900

    test: Players 인스턴스 생성 테스트 로직 작성

---
+++ src/test/java/ladder/domain/PlayersTest.java
@@ -0,0 +1,26 @@
+package ladder.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class PlayersTest {
+
+    @DisplayName("Player 인스턴스들을 입력하면 인스턴스를 생성한다.")
+    @Test
+    void Players_인스턴스_생성() {
+        // Given
+        final List<String> userNames = List.of("kelly", "pobi", "con", "hi");
+        final int maxLadderHeight = 5;
+
+        // When
+        Players players = Player.of(userNames, maxLadderHeight);
+
+        // Then
+        assertThat(players).isNotNull();
+    }
+}
