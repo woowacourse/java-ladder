b718ecdff855c433ace163af4a563fa6439ec466
3juhwan <13selfesteem91@naver.com>
2024-02-25 14:10:06 +0900

    feat: Prizes 클래스 추가

---
+++ src/main/java/domain/Prizes.java
@@ -0,0 +1,10 @@
+package domain;
+
+import java.util.Arrays;
+import java.util.List;
+
+public record Prizes(List<Prize> prizes) {
+    public Prizes(String[] names) {
+        this(Arrays.stream(names).map(Prize::new).toList());
+    }
+}
---
+++ src/test/java/domain/PrizesTest.java
@@ -0,0 +1,19 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+class PrizesTest {
+
+    @Test
+    @DisplayName("상들이 정상적으로 생성되는가")
+    void do_prizes_creat_correctly() {
+        String[] prizeNames = {"꽝", "5000", "꽝", "3000"};
+
+        assertThatCode(() -> new Prizes(prizeNames))
+                .doesNotThrowAnyException();
+    }
+
+}
