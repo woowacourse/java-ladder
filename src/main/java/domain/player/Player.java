576eeee5718f3021be6fead712e93ae00c14c38c
DobbyMac <kimdobby@g.skku.edu>
2024-02-25 14:28:43 +0900

    refactor(Player): Player를 record 클래스로 변환

--- src/main/java/domain/player/Player.java
+++ src/main/java/domain/player/Player.java
@@ -1,13 +1,4 @@
 package domain.player;
 
-public class Player {
-    private final Name name;
-
-    public Player(final Name name) {
-        this.name = name;
-    }
-
-    public Name getName() {
-        return name;
-    }
+public record Player(Name name) {
 }
