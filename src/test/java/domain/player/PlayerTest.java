672ce62c4624bdac1793687074fe90ed7de2a308
DobbyMac <kimdobby@g.skku.edu>
2024-02-25 14:30:31 +0900

    refactor(PlayerTest): Player를 record 클래스 변환점 적용

--- src/test/java/domain/player/PlayerTest.java
+++ src/test/java/domain/player/PlayerTest.java
@@ -1,18 +1,17 @@
 package domain.player;
 
-import domain.player.Name;
-import domain.player.Player;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-public class PlayerTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PlayerTest {
     @Test
     @DisplayName("이름을 통해 사용자를 생성한다.")
-    public void createPlayer(){
+    void createPlayer() {
         Name name = new Name("포비");
 
         Player player = new Player(name);
-        assertEquals(player.getName(),name);
+        assertEquals(player.name(), name);
     }
 }
