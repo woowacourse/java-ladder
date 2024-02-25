609b1273aa6ee62d612fd632a360aa066f7b7598
ay-eonii <naayen@naver.com>
2024-02-25 12:50:46 +0900

    refactor(View): `\n`을 `lineSeparator()`로 대체

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -2,17 +2,19 @@ package ladder.view;
 
 import java.util.Scanner;
 
+import static java.lang.System.*;
+
 public class InputView {
 
-    private final Scanner scanner = new Scanner(System.in);
+    private final Scanner scanner = new Scanner(in);
 
     public String readNames() {
-        System.out.println("\n참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
+        out.println(lineSeparator() + "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
         return scanner.nextLine();
     }
 
     public String readHeight() {
-        System.out.println("\n최대 사다리 높이는 몇 개인가요?");
+        out.println(lineSeparator() + "최대 사다리 높이는 몇 개인가요?");
         return scanner.nextLine();
     }
 }
