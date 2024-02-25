b22030216326eb4af49b8af8d1b488582a538d1b
devhoya97 <diajbh123@gmail.com>
2024-02-25 13:26:44 +0900

    refactor : 예외를 trace할 수 있도록 try-catch 제거

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -4,10 +4,6 @@ public class Application {
 
     public static void main(String[] args) {
         final LadderGame ladderGame = new LadderGame();
-        try {
-            ladderGame.start();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+        ladderGame.start();
     }
 }
