c92d07226baf85ad10d80e0c6320b71df70c7a50
Zeus6768 <akaps6768@gmail.com>
2024-02-25 18:26:19 +0900

    refactor(domain): 코드리뷰 반영 - 예외처리 로직 위치 변경

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -6,20 +6,25 @@ import java.util.Scanner;
 
 import ladder.domain.Height;
 import ladder.domain.Players;
+import ladder.exception.ExceptionHandler;
 
 public class InputView {
 
     private static final Scanner SCANNER = new Scanner(System.in);
 
     public static Players inputNames() {
-        System.out.println("\n참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
-        List<String> names = Arrays.asList(readLine().split(","));
-        return new Players(names);
+        return ExceptionHandler.run(() -> {
+            System.out.println("\n참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
+            List<String> names = Arrays.asList(readLine().split(","));
+            return new Players(names);
+        });
     }
 
     public static Height inputHeight() {
-        System.out.println("\n최대 사다리 높이는 몇 개인가요?");
-        return new Height(Integer.parseInt(readLine()));
+        return ExceptionHandler.run(() -> {
+            System.out.println("\n최대 사다리 높이는 몇 개인가요?");
+            return new Height(Integer.parseInt(readLine()));
+        });
     }
 
     private static String readLine() {
