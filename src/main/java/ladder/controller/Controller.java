864c2557c5febbc4c525be6f9aa8d4f0d1c20f3b
Zeus6768 <akaps6768@gmail.com>
2024-02-25 18:40:44 +0900

    refactor(controller): 불필요한 코드 삭제

--- src/main/java/ladder/controller/Controller.java
+++ src/main/java/ladder/controller/Controller.java
@@ -4,24 +4,15 @@ import ladder.domain.DefaultLadderDirectionSelector;
 import ladder.domain.Height;
 import ladder.domain.Ladder;
 import ladder.domain.Players;
-import ladder.exception.ExceptionHandler;
 import ladder.view.InputView;
 import ladder.view.ResultView;
 
 public class Controller {
 
     public void run() {
-        Players players = createPlayers();
-        Height height = createHeight();
+        Players players = InputView.inputNames();
+        Height height = InputView.inputHeight();
         Ladder ladder = Ladder.of(players, height, new DefaultLadderDirectionSelector());
         ResultView.printResult(players, ladder);
-    }
-
-    private Players createPlayers() {
-        return ExceptionHandler.run(InputView::inputNames);
-    }
-
-    private Height createHeight() {
-        return ExceptionHandler.run(InputView::inputHeight);
     }
 }
