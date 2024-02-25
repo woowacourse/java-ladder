ea2ba4012f21d370e8a7d55777eaee9cf1874b11
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2024-02-25 13:40:31 +0900

    test (PlayerNameTest) : 객체가 정상적으로 생성되는지 체크하는 테스트 케이스 추가

--- src/test/java/ladder/domain/PlayerNameTest.java
+++ src/test/java/ladder/domain/PlayerNameTest.java
@@ -1,5 +1,6 @@
 package ladder.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
@@ -25,8 +26,18 @@ class PlayerNameTest {
     @NullAndEmptySource
     @ValueSource(strings = " ")
     void validateTest_WhenNameIsEmpty(String name) {
+
         assertThatThrownBy(() -> new PlayerName(name))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("이름은 한글자 이상이어야 합니다.");
+    }
+
+    @DisplayName("이름이 1글자 이상 5글자 이하인 경우, 객체가 정상적으로 생성된다")
+    @ParameterizedTest
+    @ValueSource(strings = {"steve", "lucy", "bri"})
+    void validateTest_WhenNameSizeIsInRange(String name) {
+        PlayerName playerName = new PlayerName(name);
+
+        assertThat(playerName.getName()).isEqualTo(name);
     }
 }
