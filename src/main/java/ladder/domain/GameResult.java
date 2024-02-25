4ec78b660d2b15dfc4d7149a4c0c2f3c1170eb6d
Minjoo <m0522inju@gmail.com>
2024-02-25 23:10:14 +0900

    refactor: 결과를 보고 싶은 사람의 이름이 all이나 입력했던 사용자 중에 없으면 예외를 던지는 기능 메소드로 추출

--- src/main/java/ladder/domain/GameResult.java
+++ src/main/java/ladder/domain/GameResult.java
@@ -2,7 +2,13 @@ package ladder.domain;
 
 public class GameResult {
     public GameResult(UserNames userNames, final String requestUser) {
-        if (!requestUser.equals("all") || !userNames.isExist(requestUser)) {
+        validateRequestUser(userNames, requestUser);
+    }
+
+    private static void validateRequestUser(UserNames userNames, final String requestUser) {
+        boolean isNotAll = !requestUser.equals("all");
+        boolean isNotExist = !userNames.isExist(requestUser);
+        if (isNotAll || isNotExist) {
             throw new IllegalArgumentException("존재하지 않는 참여자입니다.");
         }
     }
