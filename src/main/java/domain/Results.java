738a34d4e5623c3d3f09916bc0eefb9622c52938
hangillee <skfcb10@naver.com>
2024-02-26 01:14:47 +0900

    feat: 실행 결과들을 관리하는 일급 컬렉션 구현

---
+++ src/main/java/domain/Results.java
@@ -0,0 +1,19 @@
+package domain;
+
+import java.util.List;
+
+public class Results {
+    private final List<Result> results;
+
+    public Results(final List<String> values) {
+        this.results = values.stream()
+                .map(Result::new)
+                .toList();
+    }
+
+    public List<String> getAll() {
+        return results.stream()
+                .map(Result::getValue)
+                .toList();
+    }
+}
