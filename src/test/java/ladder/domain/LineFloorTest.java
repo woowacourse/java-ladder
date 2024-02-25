4156e9f0e2c81df56c260bb65f9fa1415a648765
kelly6bf <kelly6bf@gmail.com>
2024-02-25 13:13:42 +0900

    test: 유효하지 않은 라인 층 번호 검증 테스트 로직 작성

--- src/test/java/ladder/domain/LineFloorTest.java
+++ src/test/java/ladder/domain/LineFloorTest.java
@@ -1,10 +1,12 @@
 package ladder.domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LineFloorTest {
 
@@ -12,12 +14,22 @@ public class LineFloorTest {
     @Test
     void 라인_층_번호_인스턴스_생성() {
         // Given
-        final int inputLineFloorNumber = 5;
+        final int inputLineFloor = 5;
 
         // When
-        LineFloor lineFloor = new LineFloor(inputLineFloorNumber);
+        LineFloor lineFloor = new LineFloor(inputLineFloor);
 
         // Then
         assertThat(lineFloor).isNotNull();
+    }
+
+    @DisplayName("유효하지 않은 층 번호가 입력되면 예외를 발생시킨다.")
+    @ValueSource(ints = {-1, 0})
+    @ParameterizedTest
+    void 유효하지_않은_층_번호_예외_발생(final int inputLineFloor) {
+        // When & Then
+        assertThatThrownBy(() -> new LineFloor(inputLineFloor))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("유효하지 않은 라인 층 번호입니다.");
     }
 }
