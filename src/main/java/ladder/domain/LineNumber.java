2f59c96c40bf8175d1f72fa97e37639490185723
kelly6bf <kelly6bf@gmail.com>
2024-02-25 13:06:46 +0900

    refactor: 유효하지 않은 라인 번호 검증 로직 메서드 분리 및 매직 넘버 제거

--- src/main/java/ladder/domain/LineNumber.java
+++ src/main/java/ladder/domain/LineNumber.java
@@ -1,13 +1,18 @@
 package ladder.domain;
 
 public class LineNumber {
+    private static final int MINIMUM_LINE_NUMBER = 1;
 
     private final int lineNumber;
 
     public LineNumber(final int lineNumber) {
-        if (lineNumber < 1) {
+        validateInvalidLineNumber(lineNumber);
+        this.lineNumber = lineNumber;
+    }
+
+    private void validateInvalidLineNumber(final int lineNumber) {
+        if (lineNumber < MINIMUM_LINE_NUMBER) {
             throw new IllegalArgumentException("유효하지 않은 라인 번호입니다.");
         }
-        this.lineNumber = lineNumber;
     }
 }
