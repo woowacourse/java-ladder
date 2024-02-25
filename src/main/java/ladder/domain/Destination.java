9ee4bfdee079c9fea8744589713fed5bb39dafc5
Minjoo <m0522inju@gmail.com>
2024-02-25 23:42:00 +0900

    fix: Destination 유효성 검사 예외 메시지 오류 수정

--- src/main/java/ladder/domain/Destination.java
+++ src/main/java/ladder/domain/Destination.java
@@ -5,7 +5,7 @@ public record Destination(String value) {
 
     public Destination {
         if (value.length() > MAX_LENGTH) {
-            throw new IllegalArgumentException("실행 결과 수는 사용자 수와 같아야 합니다.");
+            throw new IllegalArgumentException("실행 결과는 5자 이하여야 합니다.");
         }
     }
 }
