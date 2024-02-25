bae1470f5933ef41869d1868d92a0ed30bf913e1
Zeus6768 <akaps6768@gmail.com>
2024-02-25 15:49:15 +0900

    fix: 프로젝트 전체에서 단어 변환 실수 교정

--- gradlew.bat
+++ gradlew.bat
@@ -66,7 +66,7 @@ echo location of your Java installation.
 goto fail
 
 :execute
-@rem Setup the command ladderLevel
+@rem Setup the command line
 
 set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 
