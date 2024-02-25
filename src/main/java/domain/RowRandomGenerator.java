5acc964567a9bf5a0b457219d59e8bd467a5ce2a
fromitive <fromitive@gmail.com>
2024-02-25 21:43:02 +0900

    refactor: set 을 immutable 방식으로 변경

--- src/main/java/domain/RowRandomGenerator.java
+++ src/main/java/domain/RowRandomGenerator.java
@@ -8,25 +8,26 @@ import java.util.stream.IntStream;
 class RowRandomGenerator implements RowGenerator {
     private final Random random = new Random();
 
+    private Boolean previousBridge = false;
+
     public Row generate(int width) {
         return new Row(generateBridges(width, random));
     }
 
     private Bridges generateBridges(int width, Random random) {
         List<Boolean> rawBridges = IntStream.range(0, width)
-                .mapToObj(value -> random.nextBoolean())
+                .mapToObj(value -> nextBridge())
                 .collect(Collectors.toList());
-        fixRawBridges(rawBridges);
         return new Bridges(rawBridges);
     }
 
-    private void fixRawBridges(List<Boolean> rawBridges) {
-        IntStream.range(1, rawBridges.size()).forEach(
-                index -> {
-                    if (rawBridges.get(index) && rawBridges.get(index - 1)) {
-                        rawBridges.set(index, false);
-                    }
-                }
-        );
+    private Boolean nextBridge() {
+        Boolean newBridge = random.nextBoolean();
+        if (newBridge && previousBridge) {
+            previousBridge = false;
+            return false;
+        }
+        previousBridge = newBridge;
+        return newBridge;
     }
 }
