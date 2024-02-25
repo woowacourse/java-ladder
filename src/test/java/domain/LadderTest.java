3ea837d3876fd7875a20463ceefbb4e276c60b90
Hyeon0208 <dmsgktn0208@gmail.com>
2024-02-25 11:57:32 +0900

    refactor : 가독성을 위해 given-when-then 주석 추가

--- src/test/java/domain/LadderTest.java
+++ src/test/java/domain/LadderTest.java
@@ -2,7 +2,7 @@ package domain;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 
-import domain.bridge.strategy.RandomBridgeGenerator;
+import domain.bridge.strategy.BridgeGeneratorStub;
 import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -11,13 +11,21 @@ public class LadderTest {
 
     @Test
     void createLadderSuccessWithHeightAndPointCount() {
+        // given
         LadderHeight height = new LadderHeight(3);
-        int pointCount = 5;
-        Ladder ladder = Ladder.of(height, pointCount, new RandomBridgeGenerator());
+        List<LadderBridge> bridges = List.of(LadderBridge.BRIDGE, LadderBridge.NONE, LadderBridge.NONE);
+        BridgeGeneratorStub bridgeGeneratorStub = new BridgeGeneratorStub();
+        List<PlayerName> playerNames = List.of(new PlayerName("kaki"), new PlayerName("eden"), new PlayerName("solar"));
+
+        // when
+        bridgeGeneratorStub.setBridges(bridges);
+        Ladder ladder = Ladder.of(height, new PlayerNames(playerNames), bridgeGeneratorStub);
         List<Floor> floors = ladder.getFloors();
+
+        // then
         assertAll(
                 () -> Assertions.assertThat(floors.size()).isEqualTo(3),
-                () -> Assertions.assertThat(floors.get(0).getBridges().size()).isEqualTo(5)
+                () -> Assertions.assertThat(floors.get(0).getBridges().size()).isEqualTo(3)
         );
     }
 }
