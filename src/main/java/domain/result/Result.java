ddbada7e03b2135b56cda49e490d2ab0e7a5c0e2
donghoony <aru0504@naver.com>
2024-02-25 19:19:27 +0900

    feat(Result): 사다리 실행 결과에 대한 검증 로직 구현

--- src/main/java/domain/result/Result.java
+++ src/main/java/domain/result/Result.java
@@ -1,7 +1,14 @@
 package domain.result;
 
-public class Result {
+import java.util.regex.Pattern;
 
-    public Result(String result) {
+public record Result(String result) {
+
+    private static final Pattern RESULT_REGEX = Pattern.compile("^[a-zA-Z0-9가-힣]*$");
+
+    public Result {
+        if (result == null || !RESULT_REGEX.matcher(result).matches() || result.length() > 5 || result.isEmpty()) {
+            throw new IllegalArgumentException("결과는 1글자에서 5글자 사이여야 합니다.");
+        }
     }
 }
