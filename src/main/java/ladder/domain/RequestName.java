cf72a5b11026c8f873a4600a2214247d25dfee54
Minjoo <m0522inju@gmail.com>
2024-02-26 02:43:42 +0900

    refactor: RequestName 유효성 검사 기능 메소드로 추출

--- src/main/java/ladder/domain/RequestName.java
+++ src/main/java/ladder/domain/RequestName.java
@@ -6,10 +6,16 @@ public class RequestName {
     private final String requestName;
 
     public RequestName(String requestName, UserNames userNames) {
-        if (!requestName.equals("all") && !userNames.isExist(requestName)) {
+        validateAvailable(requestName, userNames);
+        this.requestName = requestName;
+    }
+
+    public void validateAvailable(String requestName, UserNames userNames) {
+        boolean isNotAll = !requestName.equals("all");
+        boolean isNotExist = !userNames.isExist(requestName);
+        if (isNotAll && isNotExist) {
             throw new IllegalArgumentException("존재하지 않는 참여자입니다.");
         }
-        this.requestName = requestName;
     }
 
     public boolean isAll() {
