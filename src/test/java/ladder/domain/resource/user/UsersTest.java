c0628e34790aaf4eee0bd63d42761bf06e994afe
KIMGYUTAE <kiimwf0819@gmail.com>
2024-02-25 23:37:10 +0900

    refactor(UsersTest): 공통 기능 분리

--- src/test/java/ladder/domain/resource/user/UsersTest.java
+++ src/test/java/ladder/domain/resource/user/UsersTest.java
@@ -2,6 +2,7 @@ package ladder.domain.resource.user;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,30 +13,19 @@ class UsersTest {
     @Test
     void newUsersTestByCountUnderSize() {
         //given
-        List<User> users = List.of(new User("ted"));
+        List<User> users = generateUsers(1);
 
         // when, then
         assertThatThrownBy(() -> new Users(users))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 사용자는 2~10명 까지만 등록 가능합니다.");
     }
-    
-    //TODO 초과할 -> 초과인
-    @DisplayName("사용자의 이름의 개수가 10명 초과할 경우 예외가 발생한다.")
+
+    @DisplayName("사용자의 이름의 개수가 10명 초과인 경우 예외가 발생한다.")
     @Test
     void newUsersTestByCountOverSize() {
         //given
-        List<User> users = List.of(new User("ted"),
-                new User("ash"),
-                new User("dora"),
-                new User("ella"),
-                new User("daon"),
-                new User("lily"),
-                new User("mia"),
-                new User("zozo"),
-                new User("gamza"),
-                new User("mason"),
-                new User("woni"));
+        List<User> users = generateUsers(11);
 
         // when, then
         assertThatThrownBy(() -> new Users(users))
@@ -56,5 +46,14 @@ class UsersTest {
         assertThatThrownBy(() -> new Users(users))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 중복된 사용자명이 존재합니다.");
+    }
+
+    private List<User> generateUsers(int count) {
+        List<User> users = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+             users.add(new User("u" + i));
+        }
+
+        return users;
     }
 }
