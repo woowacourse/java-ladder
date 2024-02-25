02ed4f53b99492d0f9ed4c39ee7511380e2926b7
Minjoo <m0522inju@gmail.com>
2024-02-26 02:09:26 +0900

    feat: 입력 받은 결과 보고 싶은 사람이 "all"인지 확인하는 기능 구현

--- src/main/java/ladder/domain/RequestName.java
+++ src/main/java/ladder/domain/RequestName.java
@@ -1,12 +1,22 @@
 package ladder.domain;
 
+import java.util.Objects;
+
 public class RequestName {
     private final String requestName;
 
     public RequestName(String requestName, UserNames userNames) {
-        if (!requestName.equals("all") || !userNames.isExist(requestName)) {
+        if (!requestName.equals("all") && !userNames.isExist(requestName)) {
             throw new IllegalArgumentException("존재하지 않는 참여자입니다.");
         }
         this.requestName = requestName;
+    }
+
+    public boolean isAll() {
+        return Objects.equals(requestName, "all");
+    }
+
+    public String getRequestName() {
+        return requestName;
     }
 }
--- src/test/java/ladder/domain/RequestNameTest.java
+++ src/test/java/ladder/domain/RequestNameTest.java
@@ -1,9 +1,13 @@
 package ladder.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
 import java.util.List;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class RequestNameTest {
     @DisplayName("입력 받은 결과를 보고 싶은 사람이 all 혹은 참여자가 아니면 예외를 던진다.")
@@ -11,8 +15,18 @@ public class RequestNameTest {
     void createRequestNameByInvalidMessage() {
         UserNames userNames = UserNames.from(List.of("a", "b"));
 
-        Assertions.assertThatIllegalArgumentException()
+        assertThatIllegalArgumentException()
                 .isThrownBy(() -> new RequestName("c", userNames))
                 .withMessage("존재하지 않는 참여자입니다.");
+    }
+
+    @DisplayName("입력 받은 이름이 `all`이면 true를 아니면 false를 반환한다.")
+    @CsvSource(value = {"all:true", "a:false"}, delimiter = ':')
+    @ParameterizedTest
+    void isAll(String inputName, boolean expected) {
+        UserNames userNames = UserNames.from(List.of("a", "b"));
+        RequestName requestName = new RequestName(inputName, userNames);
+
+        assertThat(requestName.isAll()).isEqualTo(expected);
     }
 }
