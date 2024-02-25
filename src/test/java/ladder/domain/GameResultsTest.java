3208e7dcd0a6bb4406fc99b752c8e2efd77e63b5
Minjoo <m0522inju@gmail.com>
2024-02-26 01:02:27 +0900

    feat: 결과를 먼저 생성 후 결과 보고 싶은 사람 입력시 findByUserName 메서드를 호출하여 해당하는 이름이 없으면 예외를 반환하도록 구현

--- src/test/java/ladder/domain/GameResultsTest.java
+++ src/test/java/ladder/domain/GameResultsTest.java
@@ -10,9 +10,13 @@ public class GameResultsTest {
     @DisplayName("결과를 보고 싶은 사람의 이름이 all이나 입력했던 사용자 중에 없는 이름이면 예외를 던진다.")
     @Test
     void requestResultByUndefined() {
-        UserNames userNames = UserNames.from(List.of("liv1", "liv2"));
+        UserNames userNames = UserNames.from(List.of("a", "b"));
+        List<Integer> stepPositions = List.of(0, 0);
+        List<Destination> destinations = List.of(new Destination("꽝"), new Destination("3000"));
+        GameResults gameResults = new GameResults(userNames, stepPositions, destinations);
+
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> new GameResults(userNames, "liv3"))
+                .isThrownBy(() -> gameResults.findByUserName("c"))
                 .withMessage("존재하지 않는 참여자입니다.");
     }
 }
