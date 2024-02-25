c621022952b5fe1adb827f7e009ca9ce3e9e8d4a
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2024-02-25 14:17:55 +0900

    test (Height) : 정상 범위 내의 value에서 객체가 정상적으로 생성되는지에 대한 테스트 추가

--- src/test/java/ladder/domain/HeightTest.java
+++ src/test/java/ladder/domain/HeightTest.java
@@ -1,5 +1,6 @@
 package ladder.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
@@ -12,8 +13,18 @@ class HeightTest {
     @ParameterizedTest
     @ValueSource(ints = {0, -1})
     void validateTest_WhenHeightIsNotPositive(int value) {
+
         assertThatThrownBy(() -> new Height(value))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("사다리 높이는 1 이상이어야 합니다.");
+    }
+
+    @DisplayName("사다리의 높이가 1 이상일 경우 정상적으로 생성된다")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 10})
+    void validateTest_WhenHeightIsPositive(int value) {
+        Height height = new Height(value);
+
+        assertThat(height.getValue()).isEqualTo(value);
     }
 }
