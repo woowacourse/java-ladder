37d107f2d2769c6555d407291e1cd52e59cba5f6
HoeSeong123 <ghltjd3@gmail.com>
2024-02-25 19:43:34 +0900

    feat(Results) : 실행 결과를 입력받는다

--- src/main/java/ladderGame/controller/LadderGameController.java
+++ src/main/java/ladderGame/controller/LadderGameController.java
@@ -2,6 +2,7 @@ package ladderGame.controller;
 
 import ladderGame.model.Ladder;
 import ladderGame.model.Players;
+import ladderGame.model.Results;
 import ladderGame.view.InputView;
 import ladderGame.view.ResultView;
 
@@ -16,6 +17,7 @@ public class LadderGameController {
 
     public void run() {
         Players players = new Players(inputView.inputPlayerNames());
+        Results results = new Results(inputView.inputResults(), players.getPlayerSize());
         Ladder ladder = new Ladder(inputView.inputMaxLadderHeight(), players.getPlayerSize());
 
         resultView.printLadder(players.getPlayers(), ladder.getLines());
