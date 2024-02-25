61db2862e427348870a48333523e8805999bb87e
Gyeongho Yang <chchch1213@naver.com>
2024-02-25 23:15:52 +0900

    refactor: 예외 처리 유틸 제거 후 컨트롤러로 병합하여 단순화

--- src/main/java/ladder/controller/LadderController.java
+++ src/main/java/ladder/controller/LadderController.java
@@ -3,37 +3,38 @@ package ladder.controller;
 import ladder.domain.Ladder;
 import ladder.domain.LadderHeight;
 import ladder.domain.People;
-import ladder.util.ExceptionRetryHandler;
 import ladder.util.RandomLinesGenerator;
 import ladder.view.InputView;
 import ladder.view.OutputView;
+
+import java.util.Optional;
+import java.util.function.Supplier;
 
 public class LadderController {
     private LadderController() {
     }
 
     public static void start() {
-        People people = requestPeopleUntilValid();
-        LadderHeight ladderHeight = requestLadderHeightUntilValid();
+        People people = requestUntilValid(() -> People.from(InputView.readPeopleNames()));
+        LadderHeight ladderHeight = requestUntilValid(() -> LadderHeight.from(InputView.readLadderHeight()));
 
         Ladder ladder = Ladder.create(RandomLinesGenerator::generate, people, ladderHeight);
-
         OutputView.printResult(people, ladder);
     }
 
-    private static People requestPeopleUntilValid() {
-        return ExceptionRetryHandler.handle(LadderController::requestPeople);
+    private static <T> T requestUntilValid(Supplier<T> supplier) {
+        Optional<T> result = Optional.empty();
+        while (result.isEmpty()) {
+            result = tryGet(supplier);
+        }
+        return result.get();
     }
-
-    private static People requestPeople() {
-        return People.from(InputView.readPeopleNames());
-    }
-
-    private static LadderHeight requestLadderHeightUntilValid() {
-        return ExceptionRetryHandler.handle(LadderController::requestLadderHeight);
-    }
-
-    private static LadderHeight requestLadderHeight() {
-        return LadderHeight.from(InputView.readLadderHeight());
+    private static <T> Optional<T> tryGet(Supplier<T> supplier) {
+        try {
+            return Optional.of(supplier.get());
+        } catch (IllegalArgumentException e) {
+            OutputView.printMessage(e.getMessage());
+            return Optional.empty();
+        }
     }
 }
