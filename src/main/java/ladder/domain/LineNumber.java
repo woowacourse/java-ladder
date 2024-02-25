98f0d30cdc74e1fadf4d73987e1d96df77c802aa
kelly6bf <kelly6bf@gmail.com>
2024-02-25 13:05:50 +0900

    feat: 유효하지 않은 라인 번호 검증 로직 구현

--- src/main/java/ladder/domain/LineNumber.java
+++ src/main/java/ladder/domain/LineNumber.java
@@ -5,6 +5,9 @@ public class LineNumber {
     private final int lineNumber;
 
     public LineNumber(final int lineNumber) {
+        if (lineNumber < 1) {
+            throw new IllegalArgumentException("유효하지 않은 라인 번호입니다.");
+        }
         this.lineNumber = lineNumber;
     }
 }
