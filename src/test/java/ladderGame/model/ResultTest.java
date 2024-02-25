9cef30752715d48c99aa94d0814f8f47fa1fdaf1
HoeSeong123 <ghltjd3@gmail.com>
2024-02-25 15:01:35 +0900

    test(ResultTest) : 실행 결과는 6글자 이상일 시 예외처리 된다 (Test Fails)

---
+++ src/test/java/ladderGame/model/ResultTest.java
@@ -0,0 +1,19 @@
+package ladderGame.model;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ResultTest {
+
+    @Test
+    @DisplayName("실행 결과는 6글자 이상일 시 예외처리 된다.")
+    void validateNameLength() {
+        assertThatThrownBy(() -> new Result("123456"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("실행 결과는 최대 5글자까지 가능합니다.");
+    }
+
+}
