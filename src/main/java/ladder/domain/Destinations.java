f52f97528bbc9d002b2025f7ad62ecdb5314a7c1
Minjoo <m0522inju@gmail.com>
2024-02-25 23:54:30 +0900

    refactor: Destinations의 개수가 게임 참여자의 수와 일치하는지 검증하는 코드 메서드로 분리

--- src/main/java/ladder/domain/Destinations.java
+++ src/main/java/ladder/domain/Destinations.java
@@ -1,14 +1,16 @@
 package ladder.domain;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Destinations {
     public static Destinations of(final List<String> destinations, final int userCount) {
-        final int destinationsCount = new ArrayList<>(destinations).size();
-        if (destinationsCount != userCount) {
+        validateDestinationsCount(destinations, userCount);
+        return new Destinations();
+    }
+
+    private static void validateDestinationsCount(final List<String> destinations, final int userCount) {
+        if (destinations.size() != userCount) {
             throw new IllegalArgumentException("실행 결과 수는 사용자 수와 같아야 합니다.");
         }
-        return new Destinations();
     }
 }
