9aa1cc50cb231553fe5d4429ec3956463c63e185
tsulocalize <tsulocalize@gmail.com>
2024-02-25 20:35:32 +0900

    feat(domain): 보상 클래스 생성

--- src/main/java/ladder/controller/LadderGameController.java
+++ src/main/java/ladder/controller/LadderGameController.java
@@ -1,5 +1,6 @@
 package ladder.controller;
 
+import ladder.domain.Compensation;
 import ladder.domain.Ladder;
 import ladder.domain.People;
 import ladder.view.InputView;
@@ -13,6 +14,7 @@ public class LadderGameController {
 
     public static void run() {
         People people = getPeople();
+        Compensation compensation = getCompensation(people);
         Ladder ladder = getLadder(people);
 
         OutputView.printNames(people);
@@ -25,6 +27,10 @@ public class LadderGameController {
 
     private static Ladder getLadder(People people) {
         return retryUntilGet(LadderGameController::makeLadder, people);
+    }
+
+    private static Compensation getCompensation(People people) {
+        return retryUntilGet(LadderGameController::makeCompensation, people);
     }
 
     private static People makePeople() {
@@ -42,6 +48,15 @@ public class LadderGameController {
             return new Ladder(InputView.readHeight(), peopleNumber);
         } catch (NumberFormatException e) {
             OutputView.printMessage("사다리의 높이는 1이상 100이하의 자연수여야 합니다");
+        } catch (IllegalArgumentException e) {
+            OutputView.printMessage(e.getMessage());
+        }
+        return null;
+    }
+
+    private static Compensation makeCompensation(People people) {
+        try {
+            return new Compensation(InputView.readCompensation(), people.getNames().size());
         } catch (IllegalArgumentException e) {
             OutputView.printMessage(e.getMessage());
         }
