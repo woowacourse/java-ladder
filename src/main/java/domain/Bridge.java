0b821404fcaacbb1a2c85fa4cc077c34aaa52054
devhoya97 <diajbh123@gmail.com>
2024-02-25 17:40:44 +0900

    style : 람다식에서 사용하는 변수명 직관적으로 변경 및 미사용 import 제거

--- src/main/java/domain/Bridge.java
+++ src/main/java/domain/Bridge.java
@@ -3,7 +3,6 @@ package domain;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public enum Bridge {
 
@@ -19,8 +18,8 @@ public enum Bridge {
 
     public static Bridge getBy(final boolean exist) {
         final List<Bridge> matchedBridge = Arrays.stream(values())
-                .filter(value -> value.exist == exist)
-                .collect(Collectors.toList());
+                .filter(bridge -> bridge.exist == exist)
+                .toList();
 
         if (matchedBridge.size() != MATCHED_BRIDGE_COUNT) {
             throw new IllegalStateException("Bridge를 찾을 수 없거나 중복되는 값이 존재합니다.");
