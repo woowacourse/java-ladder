9d5616654d2328752768148a20a52cccdf989e87
fromitive <fromitive@gmail.com>
2024-02-25 16:34:44 +0900

    refactor: 정규 표현식 미리 컴파일 하도록 변경

--- src/main/java/domain/Name.java
+++ src/main/java/domain/Name.java
@@ -1,9 +1,12 @@
 package domain;
 
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Name {
     public static final int MAX_NAME_LENGTH = 5;
+    private static final Pattern ALPHABET_PATTERN = Pattern.compile("^[^a-zA-Z]+$");
     private final String name;
 
     Name(String name) {
@@ -29,7 +32,8 @@ public class Name {
     }
 
     private void validateNameCharacters(String name) {
-        if (name.matches("^[^a-zA-Z]+$")) {
+        Matcher matcher = ALPHABET_PATTERN.matcher(name);
+        if (matcher.matches()) {
             throw new LadderGameException(ExceptionType.INVALID_NAME_CHARACTER);
         }
     }
