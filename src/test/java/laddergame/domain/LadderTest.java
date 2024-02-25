4b6fd7bd90ae468422ce3035eb157761ae9dcd62
Kwoun Ki Ho <fingercut3822@gmail.com>
2024-02-25 15:49:37 +0900

    test: 플레이어 전체 결과 순서 검증

--- src/test/java/laddergame/domain/LadderTest.java
+++ src/test/java/laddergame/domain/LadderTest.java
@@ -2,7 +2,10 @@ package laddergame.domain;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import laddergame.domain.strategy.BuildStrategy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -68,5 +71,51 @@ public class LadderTest {
         assertEquals(ladder.find("lemon"), results.getResults().get(0));
         assertEquals(ladder.find("clova"), results.getResults().get(3));
         assertEquals(ladder.find("nyang"), results.getResults().get(2));
+    }
+
+    @Test
+    @DisplayName("플레이어 전체 결과 반환에 성공한다.")
+    public void getPlayersResultsTest() {
+        //given
+        final Height height = new Height("5");
+        final Players players = Players.from(List.of("choco", "lemon", "clova", "nyang"));
+        final Results results = Results.from(List.of("꽝1", "꽝2", "꽝3", "꽝4"), players.getPlayers().size());
+
+        BuildStrategy buildStrategy = count -> new Points(List.of(falsePoint, falsePoint, falsePoint));
+
+        //when
+        Ladder ladder = new Ladder(players, height, results, buildStrategy);
+        final Map<Player, Result> playersResults = ladder.getPlayersResults();
+
+        //then
+        assertEquals(playersResults.get(players.getPlayerByName("choco")).name(), "꽝1");
+        assertEquals(playersResults.get(players.getPlayerByName("lemon")).name(), "꽝2");
+        assertEquals(playersResults.get(players.getPlayerByName("clova")).name(), "꽝3");
+        assertEquals(playersResults.get(players.getPlayerByName("nyang")).name(), "꽝4");
+    }
+
+    @Test
+    @DisplayName("플레이어 전체 결과는 순서대로 반환된다.")
+    public void getPlayerResultsInOrder() {
+        //given
+        final Height height = new Height("5");
+        final Players players = Players.from(List.of("choco", "lemon", "clova", "nyang"));
+        final Results results = Results.from(List.of("꽝1", "꽝2", "꽝3", "꽝4"), players.getPlayers().size());
+
+        BuildStrategy buildStrategy = count -> new Points(List.of(falsePoint, falsePoint, falsePoint));
+
+        //when
+        Ladder ladder = new Ladder(players, height, results, buildStrategy);
+        final Map<Player, Result> playersResults = ladder.getPlayersResults();
+        List<Player> playersToTest = new ArrayList<>();
+        for (Entry<Player, Result> entry : playersResults.entrySet()) {
+            playersToTest.add(entry.getKey());
+        }
+
+        //then
+        assertEquals(playersToTest.get(0).getName(), "choco");
+        assertEquals(playersToTest.get(1).getName(), "lemon");
+        assertEquals(playersToTest.get(2).getName(), "clova");
+        assertEquals(playersToTest.get(3).getName(), "nyang");
     }
 }
