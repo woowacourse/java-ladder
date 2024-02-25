238e730e20634e5a42f10db15f4089005d9f8cb3
masonkimseoul <masonkimseoul@gmail.com>
2024-02-25 19:31:42 +0900

    refactor: LadderTest에서 내부 구현을 은닉

--- src/test/java/ladder/domain/ladder/LadderTest.java
+++ src/test/java/ladder/domain/ladder/LadderTest.java
@@ -2,11 +2,6 @@ package ladder.domain.ladder;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import java.util.ArrayList;
-import java.util.List;
-import ladder.domain.direction.DirectionGeneratorImpl;
-import ladder.domain.line.Line;
-import ladder.domain.line.LineGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -17,14 +12,9 @@ class LadderTest {
         //given
         int lineCount = 51;
         int width = 5;
-        List<Line> lines = new ArrayList<>();
-        LineGenerator lineGenerator = new LineGenerator(new DirectionGeneratorImpl());
-        for (int i = 0; i < lineCount; i++) {
-            lines.add(lineGenerator.generate(width));
-        }
 
         //when, then
-        assertThatThrownBy(() -> new Ladder(lines))
+        assertThatThrownBy(() -> new Ladder(lineCount, width))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 사다리의 최대 높이는 50이하만 가능합니다.");
     }
