7cbc4f16b5e6e0fb43ff162b0bfb2fca56942350
kelly6bf <kelly6bf@gmail.com>
2024-02-25 13:05:33 +0900

    test: 유효하지 않은 라인 번호 검증 테스트 로직 작성

--- src/test/java/ladder/domain/LineNumberTest.java
+++ src/test/java/ladder/domain/LineNumberTest.java
@@ -3,8 +3,11 @@ package ladder.domain;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class LineNumberTest {
 
@@ -19,5 +22,15 @@ public class LineNumberTest {
 
         // Then
         assertThat(lineNumber).isNotNull();
+    }
+
+    @DisplayName("유효하지 않은 라인 번호를 입력하면 예외를 발생시킨다.")
+    @ValueSource(ints = {-1, 0})
+    @ParameterizedTest
+    void 유효하지_않은_라인_번호_예외_발생(final int inputLineNumber) {
+        // When & Then
+        assertThatThrownBy(() -> new LineNumber(inputLineNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("유효하지 않은 라인 번호입니다.");
     }
 }
