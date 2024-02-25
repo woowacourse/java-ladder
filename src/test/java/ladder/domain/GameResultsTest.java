f29fa3d866472f495433b370777c5e8f01f0ee26
Minjoo <m0522inju@gmail.com>
2024-02-25 23:28:05 +0900

    rename: GameResult -> GameResults로 변경 - 각 UserName에 해당하는 Destination GameResult에 함께 저장하고, GameResults에서는 각 결과를 모아 두기 위해 변경

--- src/test/java/ladder/domain/GameResultsTest.java
+++ src/test/java/ladder/domain/GameResultsTest.java
@@ -6,13 +6,13 @@ import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class GameResultTest {
+public class GameResultsTest {
     @DisplayName("결과를 보고 싶은 사람의 이름이 all이나 입력했던 사용자 중에 없는 이름이면 예외를 던진다.")
     @Test
     void requestResultByUndefined() {
         UserNames userNames = UserNames.from(List.of("liv1", "liv2"));
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> new GameResult(userNames, "liv3"))
+                .isThrownBy(() -> new GameResults(userNames, "liv3"))
                 .withMessage("존재하지 않는 참여자입니다.");
     }
 }
