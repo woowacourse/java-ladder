5390d203388cefd2948a985fdd0fda7f464f5838
HoeSeong123 <ghltjd3@gmail.com>
2024-02-25 19:34:18 +0900

    refactor(ResultTest) : 테스트명 변경

--- src/test/java/ladderGame/model/ResultTest.java
+++ src/test/java/ladderGame/model/ResultTest.java
@@ -10,7 +10,7 @@ class ResultTest {
 
     @Test
     @DisplayName("실행 결과는 6글자 이상일 시 예외처리 된다.")
-    void validateNameLength() {
+    void validateResultLength() {
         assertThatThrownBy(() -> new Result("123456"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("실행 결과는 최대 5글자까지 가능합니다.");
