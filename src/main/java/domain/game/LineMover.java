20a2972c749b4cdd225bdb3443cfcd711030eceb
3juhwan <13selfesteem91@naver.com>
2024-02-25 13:22:41 +0900

    feat: LineMover 클래스 추가

---
+++ src/main/java/domain/game/LineMover.java
@@ -0,0 +1,35 @@
+package domain.game;
+
+import domain.Bridge;
+import domain.Line;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LineMover {
+    private final List<Integer> result = new ArrayList<>();
+
+    public LineMover(final Line line, final List<Integer> input) {
+        move(line, input);
+    }
+
+    public List<Integer> getResult() {
+        return Collections.unmodifiableList(result);
+    }
+
+    private void move(final Line line, final List<Integer> input) {
+        this.result.add(input.get(0));
+        for (Bridge bridge : line.getBridges()) {
+            result.add(input.get(result.size()));
+            swapIfBridgeExist(bridge);
+        }
+    }
+
+    private void swapIfBridgeExist(final Bridge bridge) {
+        if (bridge == Bridge.EXIST) {
+            int lastIndex = this.result.size() - 1;
+            Collections.swap(result, lastIndex - 1, lastIndex);
+        }
+    }
+}
---
+++ src/test/java/domain/game/LineMoverTest.java
@@ -0,0 +1,37 @@
+package domain.game;
+
+import domain.Bridge;
+import domain.Line;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LineMoverTest {
+
+    @Test
+    @DisplayName("한 줄을 정상적으로 이동시키는가")
+    void move_single_line_correctly() {
+        Line line = new Line(3, () -> Bridge.EXIST);
+        List<Integer> players = List.of(1, 2, 3, 4);
+
+        LineMover lineMover = new LineMover(line, players);
+
+        List<Integer> expected = List.of(2, 1, 4, 3);
+        assertThat(lineMover.getResult()).containsExactlyElementsOf(expected);
+    }
+
+    @Test
+    @DisplayName("다리가 없는 경우에 이동이 없는가")
+    void no_movement_when_any_bridge_exist() {
+        Line line = new Line(3, () -> Bridge.EMPTY);
+        List<Integer> players = List.of(1, 2, 3, 4);
+
+        LineMover lineMover = new LineMover(line, players);
+
+        assertThat(lineMover.getResult()).containsExactlyElementsOf(players);
+    }
+
+}
