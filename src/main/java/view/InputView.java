1ff8e0bfcf8f732fe7a705856fa51b176be66047
3juhwan <13selfesteem91@naver.com>
2024-02-25 15:25:43 +0900

    feat: 상금 입력 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,11 +5,17 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner reader = new Scanner(System.in);
     private static final String USER_NAME_DELIMITER = ",";
+    private static final String PRIZE_NAME_DELIMITER = ",";
 
 
     public static String[] readNames() {
-        printNameInputMessage();
+        printNamesInputMessage();
         return removeBlank(reader.nextLine()).split(USER_NAME_DELIMITER);
+    }
+
+    public static String[] readPrizes() {
+        printPrizesInputMessage();
+        return removeBlank(reader.nextLine()).split(PRIZE_NAME_DELIMITER);
     }
 
     public static int readHeight() {
@@ -21,12 +27,16 @@ public class InputView {
         }
     }
 
-    private static void printNameInputMessage() {
+    private static String removeBlank(final String text) {
+        return text.replace(" ", "");
+    }
+
+    private static void printNamesInputMessage() {
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
     }
 
-    private static String removeBlank(final String text) {
-        return text.replace(" ", "");
+    private static void printPrizesInputMessage() {
+        System.out.println("실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
     }
 
     private static void printLadderHeightInputMessage() {
