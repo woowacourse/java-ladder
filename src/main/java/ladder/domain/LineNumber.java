0843ceabc3560f1ce9b8780017fc2059f94435c3
kelly6bf <kelly6bf@gmail.com>
2024-02-25 13:03:04 +0900

    feat: LineNumber 인스턴스 생성 로직 구현

---
+++ src/main/java/ladder/domain/LineNumber.java
@@ -0,0 +1,10 @@
+package ladder.domain;
+
+public class LineNumber {
+
+    private final int lineNumber;
+
+    public LineNumber(final int lineNumber) {
+        this.lineNumber = lineNumber;
+    }
+}
