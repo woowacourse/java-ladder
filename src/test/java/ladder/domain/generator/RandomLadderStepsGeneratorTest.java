ab7602f38f8e7d2a1d534bb873e60c87c3756a91
JUHA <84626225+khabh@users.noreply.github.com>
2024-02-25 16:14:51 +0900

    test: LadderStepsGenerator가 올바른 크기로 laddderSteps를 생성하는지 테스트

---
+++ src/test/java/ladder/domain/generator/RandomLadderStepsGeneratorTest.java
@@ -0,0 +1,33 @@
+package ladder.domain.generator;
+
+import ladder.domain.ladder.LadderStep;
+import ladder.domain.ladder.size.LadderSize;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class RandomLadderStepsGeneratorTest {
+    @Test
+    @DisplayName("올바른 크기의 LadderSteps를 생성한다.")
+    void generateTest() {
+        // given
+        int expectedWidth = 7;
+        int expectedHeight = 5;
+        LadderSize ladderSize = new LadderSize(expectedWidth, expectedHeight);
+        RandomLadderStepsGenerator generator = new RandomLadderStepsGenerator(ladderSize);
+
+        // when
+        List<LadderStep> ladderSteps = generator.generate();
+
+        // then
+        assertThat(ladderSteps.size())
+                .isEqualTo(expectedHeight);
+        for (LadderStep ladderStep : ladderSteps) {
+            int ladderStepWidth = ladderStep.ladderPaths().size();
+            assertThat(ladderStepWidth).isEqualTo(expectedWidth);
+        }
+    }
+}
