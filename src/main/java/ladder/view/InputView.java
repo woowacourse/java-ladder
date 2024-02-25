9dc1b67ad1dc6fb5616b2595f4a80fde9beeeb24
JUHA <84626225+khabh@users.noreply.github.com>
2024-02-25 14:20:45 +0900

    feat: 실행 결과 입력받는 기능 구현

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -10,25 +10,33 @@ import java.util.Scanner;
 public class InputView {
     private static final String NAME_DELIMITER = ",";
     private static final String REQUEST_PARTICIPANTS_NAME = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
-    private static final String REQUEST_LADDER_HEIGHT = "최대 사다리 높이는 몇 개인가요?";
+    private static final String REQUEST_LADDER_HEIGHT = "\n최대 사다리 높이는 몇 개인가요?";
+    private static final String REQUEST_OUTCOME = "\n실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
 
     private final Scanner scanner = new Scanner(System.in);
 
     public List<String> readParticipantsName() {
         System.out.println(REQUEST_PARTICIPANTS_NAME);
         final String names = scanner.nextLine();
-        validateNameDelimiterPosition(names);
-        return splitNames(names);
+        validateDelimiterPosition(names);
+        return splitByDelimiter(names);
     }
 
-    private void validateNameDelimiterPosition(final String names) {
-        if (names.startsWith(NAME_DELIMITER) || names.endsWith(NAME_DELIMITER)) {
+    public List<String> readOutcomes() {
+        System.out.println(REQUEST_OUTCOME);
+        final String outcomes = scanner.nextLine();
+        validateDelimiterPosition(outcomes);
+        return splitByDelimiter(outcomes);
+    }
+
+    private void validateDelimiterPosition(final String input) {
+        if (input.startsWith(NAME_DELIMITER) || input.endsWith(NAME_DELIMITER)) {
             throw new DelimiterBoundaryException();
         }
     }
 
-    private List<String> splitNames(final String names) {
-        return Arrays.stream(names.split(NAME_DELIMITER))
+    private List<String> splitByDelimiter(final String input) {
+        return Arrays.stream(input.split(NAME_DELIMITER))
                 .toList();
     }
 
