f82afbedf9cded712d566dbb85decddec4ac8414
donghoony <aru0504@naver.com>
2024-02-25 21:02:26 +0900

    refactor(Players): 메서드 순서 조정

--- src/main/java/domain/player/Players.java
+++ src/main/java/domain/player/Players.java
@@ -12,18 +12,10 @@ public class Players {
     public Players(List<String> names) {
         validateSize(names);
         validateUniqueNames(names);
+
         this.names = names.stream()
                 .map(Name::new)
                 .toList();
-    }
-
-    private void validateUniqueNames(List<String> names) {
-        int uniqueNameCount = (int) names.stream()
-                .distinct()
-                .count();
-        if (names.size() != uniqueNameCount) {
-            throw new IllegalArgumentException("중복된 이름이 존재합니다.");
-        }
     }
 
     public List<String> getNames() {
@@ -39,6 +31,15 @@ public class Players {
     private void validateSize(List<String> names) {
         if (names.size() < MIN_PLAYERS_SIZE || names.size() > MAX_PLAYERS_SIZE) {
             throw new IllegalArgumentException("참여하는 사람의 수는 2명 이상 10명 이하여야 합니다.");
+        }
+    }
+
+    private void validateUniqueNames(List<String> names) {
+        int uniqueNameCount = (int) names.stream()
+                .distinct()
+                .count();
+        if (names.size() != uniqueNameCount) {
+            throw new IllegalArgumentException("중복된 이름이 존재합니다.");
         }
     }
 }
