78365c02c5fd45690160704f9b7c5014e07770d8
JUHA <84626225+khabh@users.noreply.github.com>
2024-02-25 16:13:08 +0900

    refactor: Height 클래스 패키지 이동 및 레코드로 수정

--- src/main/java/ladder/domain/ladder/size/Height.java
+++ src/main/java/ladder/domain/ladder/size/Height.java
@@ -1,26 +1,17 @@
-package ladder.domain.ladder;
+package ladder.domain.ladder.size;
 
 import ladder.exception.ladder.InvalidHeightRangeException;
 
-import java.util.List;
-
-public class Height {
+public record Height(int value) {
     private static final int MIN_HEIGHT = 1;
 
-    private final int value;
-
-    public Height(final int value) {
+    public Height {
         validateHeightRange(value);
-        this.value = value;
     }
 
     private void validateHeightRange(final int value) {
         if (value < MIN_HEIGHT) {
             throw new InvalidHeightRangeException();
         }
-    }
-
-    public boolean isGreaterThan(final List<LadderStep> ladderSteps) {
-        return value > ladderSteps.size();
     }
 }
