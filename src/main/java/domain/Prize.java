4ad9aa89399dc9a4c81c7b6e21d645c1b19e0053
jhon3242 <rldotnr2@gmail.com>
2024-02-26 19:36:48 +0900

    test : Prize 에 대한 테스트 추가

--- src/main/java/domain/Prize.java
+++ src/main/java/domain/Prize.java
@@ -5,7 +5,14 @@ public class Prize {
     private final String value;
 
     public Prize(String value) {
+        validate(value);
         this.value = value;
+    }
+
+    private void validate(String value) {
+        if (value == null || value.isBlank()) {
+            throw new IllegalArgumentException("빈 값을 입력할 수 없습니다.");
+        }
     }
 
     public String getValue() {
