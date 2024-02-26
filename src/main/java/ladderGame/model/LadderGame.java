ebc47381a2a1dabe8b5d0fa04acfe7705b144627
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-02-26 18:50:53 +0900

    test(LadderGameTest) : 참여자 이름을 통해 사다리 결과를 알려준다 (Test Fails)

---
+++ src/main/java/ladderGame/model/LadderGame.java
@@ -0,0 +1,12 @@
+package ladderGame.model;
+
+public class LadderGame {
+
+    public LadderGame(Players players, LadderResults ladderResults, Ladder ladder) {
+
+    }
+
+    public LadderResult findLadderGameResult(String name) {
+        return new LadderResult("");
+    }
+}
---
+++ src/test/java/ladderGame/model/LadderGameTest.java
@@ -0,0 +1,29 @@
+package ladderGame.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LadderGameTest {
+
+    @Test
+    @DisplayName("참여자 이름을 통해 사다리 결과를 알려준다.")
+    void findLadderGameResult() {
+        LineGenerator lineGenerator = new LineGenerator(() -> true);
+        List<Line> lines = Stream.generate(() -> new Line(lineGenerator.makeLine(3)))
+                .limit(4)
+                .toList();
+
+        Ladder ladder = new Ladder(new ArrayList<>(lines));
+
+        LadderGame ladderGame = new LadderGame(new Players(List.of("포비", "왼손", "준")), new LadderResults(List.of("꽝", "5000", "3000")), ladder);
+
+        assertEquals(ladderGame.findLadderGameResult("포비"), new LadderResult("꽝"));
+    }
+
+}
