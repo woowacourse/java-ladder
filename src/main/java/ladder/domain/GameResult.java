bbbd5f4a9e22dc6de2a8a87201fb5adbb25f5439
kelly6bf <kelly6bf@gmail.com>
2024-02-26 17:28:30 +0900

    feat: GameResult 인스턴스 생성 로직 구현

---
+++ src/main/java/ladder/domain/GameResult.java
@@ -0,0 +1,18 @@
+package ladder.domain;
+
+public class GameResult {
+    private final GameResultDescription gameResultDescription;
+    private final LineNumber lineNumber;
+
+    public GameResult(final GameResultDescription gameResultDescription, final LineNumber lineNumber) {
+        this.gameResultDescription = gameResultDescription;
+        this.lineNumber = lineNumber;
+    }
+
+    public static GameResult of(final String gameResultDescription, final int lineNumber) {
+        return new GameResult(
+                new GameResultDescription(gameResultDescription),
+                new LineNumber(lineNumber)
+        );
+    }
+}
---
+++ src/main/java/ladder/domain/GameResultDescription.java
@@ -0,0 +1,9 @@
+package ladder.domain;
+
+public class GameResultDescription {
+    private final String value;
+
+    public GameResultDescription(final String value) {
+        this.value = value;
+    }
+}
