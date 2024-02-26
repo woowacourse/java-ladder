db18017aad23bfa609b6f81e316b5045271537e4
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2024-02-26 12:52:58 +0900

    refactor (InputView) : Scanner를 매번 생성하지 않고, 만든 Scanner를 재사용하도록 수정

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -7,13 +7,15 @@ import java.util.Scanner;
 
 public class InputView {
 
+    private static final Scanner SCANNER = new Scanner(System.in);
     private static final String NAME_DELIMITER = ",";
 
     public List<String> inputPlayerNames() {
         System.out.println();
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
 
-        String names = new Scanner(System.in).nextLine();
+        String names = SCANNER.nextLine();
+        SCANNER.reset();
         return Arrays.asList(names.split(NAME_DELIMITER));
     }
 
@@ -25,6 +27,8 @@ public class InputView {
             return new Scanner(System.in).nextInt();
         } catch (InputMismatchException e) {
             throw new IllegalArgumentException("사다리 높이는 숫자이여야 합니다.", e);
+        } finally {
+            SCANNER.reset();
         }
     }
 }
