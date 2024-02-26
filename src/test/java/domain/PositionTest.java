bb43fbd0bbe93d42e0765a6c09fe14d5fbdb0b60
SungKyum Kim <sungkyum1@naver.com>
2024-02-26 20:39:13 +0900

    test: 위치를 검증하고 방향에 따라 이동하는 기능 테스트 작성

--- src/test/java/domain/PositionTest.java
+++ src/test/java/domain/PositionTest.java
@@ -1,2 +1,26 @@
-package domain;public class Position {
+package domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PositionTest {
+    @Test
+    @DisplayName("순서는 0부터 시작한다.")
+    void testInitializePosition() {
+        int testPosition = -1;
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Position(testPosition));
+    }
+
+    @Test
+    @DisplayName("주어진 위치에 따라 위치를 이동한다.")
+    void testMove() {
+        int testPosition = 1;
+        Position position = new Position(testPosition);
+        Direction direction = Direction.LEFT;
+        position.move(direction);
+
+        Assertions.assertEquals(0, position.getPosition());
+    }
 }
