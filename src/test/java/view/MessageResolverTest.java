41987a2d67bf376d50ac1be0b4f16b0f0576bdb2
libienz <leekh10195@gmail.com>
2024-02-27 00:20:28 +0900

    test: MessageResolver 결과 출력 테스트 추가

--- src/test/java/view/MessageResolverTest.java
+++ src/test/java/view/MessageResolverTest.java
@@ -5,6 +5,8 @@ import static domain.ConnectionStatus.DISCONNECTED;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import domain.Ladder;
+import domain.LadderResult;
+import domain.LadderResults;
 import domain.Name;
 import domain.Names;
 import domain.Prize;
@@ -25,6 +27,7 @@ class MessageResolverTest {
         messageResolver = new MessageResolver();
     }
 
+    @DisplayName("사다리 정보 메시지 테스트")
     @Test
     void testResolveLadderMessage() {
         RowLine line1 = new RowLine(List.of(CONNECTED, DISCONNECTED));
@@ -44,5 +47,34 @@ class MessageResolverTest {
                         + "    |-----|     |" + System.lineSeparator()
                         + "    |     |     |" + System.lineSeparator()
                         + "1     2     3    ");
+    }
+
+    @DisplayName("단일 결과 메시지 테스트")
+    @Test
+    void testResultMessage() {
+        LadderResult ladderResult = new LadderResult("1등", "리비");
+        assertThat(messageResolver.resolveResultMessage(ladderResult)).isEqualTo(
+                System.lineSeparator() + "실행 결과" + System.lineSeparator() + "1등");
+    }
+
+    @DisplayName("총 결과 메시지 테스트")
+    @Test
+    void testResultsMessage() {
+        LadderResult ladderResult1 = new LadderResult("1등", "리비");
+        LadderResult ladderResult2 = new LadderResult("2등", "잉크");
+        LadderResult ladderResult3 = new LadderResult("3등", "테니");
+        LadderResult ladderResult4 = new LadderResult("4등", "에버");
+        LadderResult ladderResult5 = new LadderResult("5등", "제리");
+
+        LadderResults ladderResults = new LadderResults(
+                List.of(ladderResult1, ladderResult2, ladderResult3, ladderResult4, ladderResult5));
+
+        assertThat(messageResolver.resolveResultsMessage(ladderResults)).isEqualTo(
+                System.lineSeparator() + "실행 결과" + System.lineSeparator()
+                        + "리비 : 1등" + System.lineSeparator()
+                        + "잉크 : 2등" + System.lineSeparator()
+                        + "테니 : 3등" + System.lineSeparator()
+                        + "에버 : 4등" + System.lineSeparator()
+                        + "제리 : 5등");
     }
 }
