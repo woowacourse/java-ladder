ed221fdaf56c37be1dabfd004fc6ae18abf1e908
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-02-26 18:34:51 +0900

    feat(LadderResult) : 사다리 결과를 가지고 있는 클래스 구현

---
+++ src/main/java/ladderGame/model/LadderResult.java
@@ -0,0 +1,9 @@
+package ladderGame.model;
+
+public class LadderResult {
+    private final String LadderResult;
+
+    public LadderResult(String ladderResult) {
+        LadderResult = ladderResult;
+    }
+}
