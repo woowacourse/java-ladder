a4a3c79ccf6edb74ac896b66c28cb17308a727b7
eun-byeol <joeunbyeol98@gmail.com>
2024-02-26 21:46:01 +0900

    test: 실행 결과 수가 참여자 등록 수와 같으면 객체 생성 성공

---
+++ src/test/java/model/prize/PrizesTest.java
@@ -0,0 +1,27 @@
+package model.prize;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+import java.util.List;
+import java.util.stream.Stream;
+import model.player.Players;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class PrizesTest {
+    @DisplayName("실행 결과 수가 참여자 등록 수와 같으면 객체 생성 성공")
+    @ParameterizedTest
+    @MethodSource("provideValidPrizeNamesAndPlayers")
+    void testValidPrizesSize(List<String> prizeNames, Players players) {
+        assertThatCode(() -> Prizes.from(prizeNames, players)).doesNotThrowAnyException();
+    }
+
+    private static Stream<Arguments> provideValidPrizeNamesAndPlayers() {
+        return Stream.of(
+                Arguments.of(List.of("꽝", "식권"), Players.of(List.of("pobi", "dora"))),
+                Arguments.of(List.of("배민상품권", "꽝", "꽝"), Players.of(List.of("pobi", "doraa", "jojo")))
+        );
+    }
+}
