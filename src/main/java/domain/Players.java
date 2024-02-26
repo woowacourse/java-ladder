0bb34a8ba98116b5c1379251f7cb838f5823dda2
KB Kim <28584160+jimi567@users.noreply.github.com>
2024-02-26 13:58:21 +0900

    feat : Players 중복 검증 로직 추가

--- src/main/java/domain/Players.java
+++ src/main/java/domain/Players.java
@@ -15,10 +15,21 @@ public class Players {
     }
 
     private void validate(List<String> playerNames) {
-        if (playerNames.size() < MINIMUM_PLAYER_SIZE || playerNames.size() > MAXIMUM_PLAYER_SIZE) {
+        if (validateRange(playerNames)) {
             throw new IllegalArgumentException(
                     String.format("2명 이상, 10명 이하인 인원만 입력해주세요. 입력한 인원 : %d", playerNames.size()));
+        } else if (hasDuplication(playerNames)) {
+            throw new IllegalArgumentException(
+                    String.format("이름은 중복될 수 없습니다."));
         }
+    }
+
+    private boolean validateRange(List<String> playerNames) {
+        return playerNames.size() < MINIMUM_PLAYER_SIZE || playerNames.size() > MAXIMUM_PLAYER_SIZE;
+    }
+
+    private boolean hasDuplication(List<String> playerNames) {
+        return playerNames.size() > playerNames.stream().distinct().count();
     }
 
     private List<Player> generatePlayers(List<String> names) {
