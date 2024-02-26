1aa1104823c62c998ed7e6fb002d1922f1849771
MingyeomKim <mingyum119@naver.com>
2024-02-26 16:35:03 +0900

    refactor(Step): 사다리의 다리 하나를 의미하는 Step 값 객체 생성

--- src/main/java/model/ladder/Step.java
+++ src/main/java/model/ladder/Step.java
@@ -1,9 +1,44 @@
 package model.ladder;
 
+import java.util.Objects;
+
 public class Step {
-    private StepStatus status;
+    private final StepStatus status;
 
-    public static void generate() {
+    private Step(final StepStatus status) {
+        this.status = status;
+    }
 
+    public static Step from(StepStatus status) {
+        return new Step(status);
+    }
+
+    public static Step of(Step previous, StepStatusGenerator stepStatusGenerator) {
+        return new Step(stepStatusGenerator.generate(previous));
+    }
+
+    public boolean isEmpty() {
+        return status.equals(StepStatus.EMPTY);
+    }
+
+    public boolean isDisconnected() {
+        return status.equals(StepStatus.DISCONNECTED);
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final Step step = (Step) o;
+        return status == step.status;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(status);
     }
 }
