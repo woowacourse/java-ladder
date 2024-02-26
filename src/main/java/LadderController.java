8eafabe3b84d51f0e2c3d0cd32949fda28c3193b
3juhwan <13selfesteem91@naver.com>
2024-02-26 21:31:48 +0900

    feat: Controller에 사다리 게임 결과 로직 추가

--- src/main/java/LadderController.java
+++ src/main/java/LadderController.java
@@ -1,29 +1,63 @@
+import domain.db.Name;
+import domain.db.Names;
+import domain.db.Prizes;
+import domain.game.Judge;
+import domain.game.LadderGame;
+import domain.game.Result;
+import domain.ladder.Bridge;
 import domain.ladder.Height;
 import domain.ladder.Ladder;
-import domain.game.Names;
 import view.InputView;
 import view.OutputView;
 
+import java.util.Random;
 import java.util.function.Supplier;
+
+import static view.OutputView.printResult;
 
 public class LadderController {
 
     public static void main(String[] args) {
         final Names names = retryOnException(LadderController::getNames);
+        final Prizes prizes = retryOnException(LadderController::getPrizes);
         final Height height = retryOnException(LadderController::getHeight);
 
-        final int width = names.names().size() - 1;
-        final Ladder ladder = new Ladder(width, height.height());
+        final Ladder ladder = getLadder(names, height);
+        final Result result = LadderGame.play(ladder);
+        final Judge judge = new Judge(names, prizes, result);
 
-        OutputView.printResult(names, ladder);
+        printResult(names, ladder, prizes);
+        searchGameResult(judge);
+    }
+
+    private static void searchGameResult(final Judge judge) {
+        while (true) {
+            Name nameToSearch = getNameToSearch();
+            System.out.println(judge.getPrize(nameToSearch.name()).name()
+                    + System.lineSeparator());
+        }
+    }
+
+    private static Ladder getLadder(final Names names, final Height height) {
+        final Random random = new Random();
+        final int width = names.names().size() - 1;
+        return new Ladder(width, height.height(), () -> Bridge.getOne(random.nextBoolean()));
     }
 
     private static Names getNames() {
         return new Names(InputView.readNames());
     }
 
+    private static Prizes getPrizes() {
+        return new Prizes(InputView.readPrizes());
+    }
+
     private static Height getHeight() {
         return new Height(InputView.readHeight());
+    }
+
+    private static Name getNameToSearch() {
+        return new Name(InputView.readNameToSearch());
     }
 
     private static <T> T retryOnException(Supplier<T> supplier) {
