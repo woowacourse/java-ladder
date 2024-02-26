e77f8d7ff7e61bed33b45047043da2d835ce6e97
Minjoo <m0522inju@gmail.com>
2024-02-26 16:07:49 +0900

    test: RequestName 정상 생성 테스트 코드 추가

--- src/test/java/ladder/domain/RequestNameTest.java
+++ src/test/java/ladder/domain/RequestNameTest.java
@@ -4,17 +4,32 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class RequestNameTest {
+    private UserNames userNames;
+    @BeforeEach
+    void init() {
+        userNames = UserNames.from(List.of("a", "b"));
+    }
+
+    @DisplayName("입력 받은 결과가 all이나 참여자이면 인스턴스가 생성된다.")
+    @ValueSource(strings = {"all", "a"})
+    @ParameterizedTest
+    void createRequestName(String name) {
+        assertThat(new RequestName(name, userNames).getRequestName())
+                .isEqualTo(name);
+    }
+
+
     @DisplayName("입력 받은 결과를 보고 싶은 사람이 all 혹은 참여자가 아니면 예외를 던진다.")
     @Test
     void createRequestNameByInvalidMessage() {
-        UserNames userNames = UserNames.from(List.of("a", "b"));
-
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> new RequestName("c", userNames))
                 .withMessage("존재하지 않는 참여자입니다.");
@@ -24,7 +39,6 @@ public class RequestNameTest {
     @CsvSource(value = {"all:false", "a:true"}, delimiter = ':')
     @ParameterizedTest
     void isAll(String inputName, boolean expected) {
-        UserNames userNames = UserNames.from(List.of("a", "b"));
         RequestName requestName = new RequestName(inputName, userNames);
 
         assertThat(requestName.isNotAll()).isEqualTo(expected);
