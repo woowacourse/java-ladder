5629b652c092c679792c71c525be56317221f49b
seunghye218 <seunghye@student.42seoul.kr>
2024-02-26 16:15:57 +0900

    refactor: Pattern 객체 상수화 및 생성자 기능 분리

--- src/main/java/domain/Name.java
+++ src/main/java/domain/Name.java
@@ -4,15 +4,24 @@ import java.util.regex.Pattern;
 
 public record Name(String name) {
 
+    public static final Pattern NAME_PATTERN = Pattern.compile("^[A-Za-z0-9]+");
     private static final int NAME_LENGTH_MIN = 1;
     private static final int NAME_LENGTH_MAX = 5;
 
     public Name {
+        validationNameLength(name);
+        isAlphaNumber(name);
+    }
+
+    private void validationNameLength(String name) {
         if (name.length() < NAME_LENGTH_MIN || name.length() > NAME_LENGTH_MAX) {
             throw new IllegalArgumentException(
                     "이름의 길이는 %d ~ %d 자 이어야 합니다.".formatted(NAME_LENGTH_MIN, NAME_LENGTH_MAX));
         }
-        if (!Pattern.matches("^[A-Za-z0-9]+", name)) {
+    }
+
+    private void isAlphaNumber(String name) {
+        if (!NAME_PATTERN.matcher(name).matches()) {
             throw new IllegalArgumentException("이름이 영어와 숫자가 아니라면 예외가 발생한다");
         }
     }
