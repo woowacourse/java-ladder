674b4cdda5caf0ea4cff8ae54fe6f54f37596424
hyeonjilee <skylar12200@gmail.com>
2024-02-26 21:03:51 +0900

    feat : 참여자 이름의 all 입력 검증 구현

--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -16,6 +16,9 @@ public class Player {
         if ((name.isBlank() || name.length() > MAX_NAME_LENGTH)) {
             throw new IllegalArgumentException("참가자 이름은 1~%d자 이내로 입력해야합니다.".formatted(MAX_NAME_LENGTH));
         }
+        if (name.equals("all")) {
+            throw new IllegalArgumentException("참가자 이름에 all을 입력할 수 없습니다.");
+        }
     }
 
     public String getName() {
