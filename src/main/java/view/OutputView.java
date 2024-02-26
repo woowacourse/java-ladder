ae3bf11252e20ca7ab6bdf4151bb0207013d9d63
reddevilmidzy <midzy0228@gmail.com>
2024-02-26 16:59:30 +0900

    refactor: 실행결과 출력문을 사다리 결과로 수정

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -5,7 +5,7 @@ import java.util.stream.Collectors;
 
 public class OutputView {
     private static final String FINAL_RESULT_FORMAT = "%s%n%n";
-    private static final String FINAL_RESULT_MESSAGE = "실행결과";
+    private static final String FINAL_RESULT_MESSAGE = "사다리 결과";
 
     public void printResult(List<String> names, List<String> lines) {
         System.out.printf(FINAL_RESULT_FORMAT, FINAL_RESULT_MESSAGE);
