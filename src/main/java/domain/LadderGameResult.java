d3bf4e20b4823d96b585fef561846c59f0db1780
fromitive <fromitive@gmail.com>
2024-02-26 23:14:43 +0900

    feat: 참여자 이름에 대응하는 사다리 결과 반환 기능 구현

--- src/main/java/domain/LadderGameResult.java
+++ src/main/java/domain/LadderGameResult.java
@@ -1,13 +1,33 @@
 package domain;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class LadderGameResult {
+
+    Names names;
+    LadderResults ladderResults;
+
     public LadderGameResult(Names names, LadderResults ladderResults) {
         validateNamesLadderResultLength(names, ladderResults);
+        this.names = names;
+        this.ladderResults = ladderResults;
     }
 
     private void validateNamesLadderResultLength(Names names, LadderResults ladderResults) {
         if (names.getNameCount() != ladderResults.getLadderResultCount()) {
             throw new LadderGameException(ExceptionType.NOT_ALLOW_DIFFERENT_NAMES_LADDER_RESULTS_LENGTH);
         }
+    }
+
+    public Map<Name, LadderResult> getLadderGameResult() {
+        List<Name> swappedNames = names.getSwappedNames();
+        List<LadderResult> ladderResults = this.ladderResults.getLadderResults();
+
+        return IntStream.range(0, swappedNames.size())
+                .boxed()
+                .collect(Collectors.toMap(swappedNames::get, ladderResults::get));
     }
 }
--- src/test/java/domain/LadderGameResultTest.java
+++ src/test/java/domain/LadderGameResultTest.java
@@ -1,6 +1,7 @@
 package domain;
 
 import java.util.List;
+import java.util.Map;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,4 +19,21 @@ class LadderGameResultTest {
                 .hasMessage(ExceptionType.NOT_ALLOW_DIFFERENT_NAMES_LADDER_RESULTS_LENGTH.getMessage());
     }
 
+
+    @Test
+    @DisplayName("참여자 이름에 대응하는 사다리 결과 결과 반환")
+    void testGetLadderResult() {
+        Names names = new Names(List.of(new Name("name1"), new Name("name2")));
+        LadderResults ladderResults = new LadderResults(
+                List.of(new LadderResult("상품1"), new LadderResult("상품2")));
+
+        names.swapNamePosition(1);
+        LadderGameResult ladderGameResult = new LadderGameResult(names, ladderResults);
+
+        Map<Name, LadderResult> result = ladderGameResult.getLadderGameResult();
+        LadderResult actual = result.get(names.getNames().get(0));
+        LadderResult expected = ladderResults.getLadderResults().get(1);
+
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
 }
