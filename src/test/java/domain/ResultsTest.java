ca0655d0d25223db6c08c53d6999ca3054f280a8
hangillee <skfcb10@naver.com>
2024-02-26 16:40:10 +0900

    test: 실행 결과와 사람 이름 수가 같은지 검증하는 로직 테스트 작성

--- src/test/java/domain/ResultsTest.java
+++ src/test/java/domain/ResultsTest.java
@@ -1,6 +1,7 @@
 package domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.Arrays;
 import java.util.List;
@@ -9,9 +10,21 @@ import org.junit.jupiter.api.Test;
 
 public class ResultsTest {
     @Test
+    @DisplayName("실행 결과는 사람 이름 수와 정확하게 일치해야 한다.")
+    void isResultCountMatchNamesCount() {
+        List<String> rawNames = Arrays.asList("pobi", "crong", "honux");
+        Names names = new Names(rawNames);
+        List<String> results = Arrays.asList("꽝", "5000", "꽝", "3000");
+
+        assertThatThrownBy(() -> new Results(results, names.size()))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("실행 결과의 개수가 사람 이름의 개수와 일치하지 않습니다.");
+    }
+    
+    @Test
     @DisplayName("전체 실행 결과를 반환한다.")
     void getAllResults() {
-        Results results = new Results(Arrays.asList("꽝", "5000", "꽝", "3000"));
+        Results results = new Results(Arrays.asList("꽝", "5000", "꽝", "3000"), 4);
         List<String> allResults = results.getAll();
 
         assertThat(allResults).containsExactly("꽝", "5000", "꽝", "3000");
@@ -20,7 +33,8 @@ public class ResultsTest {
     @Test
     @DisplayName("특정 위치의 실행 결과를 반환한다.")
     void getResultByIndex() {
-        Results results = new Results(Arrays.asList("꽝", "5000", "꽝", "3000"));
+        Names names = new Names(Arrays.asList("pobi", "crong", "honux", "jk"));
+        Results results = new Results(Arrays.asList("꽝", "5000", "꽝", "3000"), 4);
         String result = results.resultOf(1);
 
         assertThat(result).isEqualTo("5000");
