1c5426db5b7152a3cbfa89973d76a98b715739c0
SungKyum Kim <sungkyum1@naver.com>
2024-02-26 20:52:22 +0900

    test: 주어진 방향으로 참가자의 위치를 이동시키는 기능 테스트 작성

--- src/test/java/domain/PlayerTest.java
+++ src/test/java/domain/PlayerTest.java
@@ -1,2 +1,21 @@
-package domain;public class Player {
+package domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PlayerTest {
+    @Test
+    @DisplayName("주어진 방향으로 위치를 이동시킨다")
+    void testMovePosition() {
+        int testPosition = 1;
+        String testName= "pobi";
+        Name name = new Name(testName);
+        Player player = new Player(name, testPosition);
+        Direction direction = Direction.LEFT;
+        player.move(direction);
+
+        Assertions.assertEquals(0, player.getPosition());
+
+    }
 }
