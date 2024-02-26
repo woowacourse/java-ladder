968969435c6c14a37ad5ee4320252796bff0418c
fromitive <fromitive@gmail.com>
2024-02-26 21:03:41 +0900

    refactor: 사다리 결과 개수 계산 방식 변경

--- src/main/java/domain/LadderGameResult.java
+++ src/main/java/domain/LadderGameResult.java
@@ -3,11 +3,10 @@ package domain;
 public class LadderGameResult {
     public LadderGameResult(Names names, LadderResults ladderResults) {
         validateNamesLadderResultLength(names, ladderResults);
-
     }
 
     private void validateNamesLadderResultLength(Names names, LadderResults ladderResults) {
-        if (names.getNameCount() != ladderResults.getLadderResults().size()) {
+        if (names.getNameCount() != ladderResults.getLadderResultCount()) {
             throw new LadderGameException(ExceptionType.NOT_ALLOW_DIFFERENT_NAMES_LADDER_RESULTS_LENGTH);
         }
     }
