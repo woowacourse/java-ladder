32d38613bba6882d520d9c99f205f5a9531e99cd
hoyeonyy <highoption@naver.com>
2024-02-26 14:30:58 +0900

    refactor (PlayerNameTest)  : 생성자와 getter 테스트 추가

--- src/test/java/ladder/domain/PlayerNameTest.java
+++ src/test/java/ladder/domain/PlayerNameTest.java
@@ -1,8 +1,10 @@
 package ladder.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
@@ -10,6 +12,14 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 class PlayerNameTest {
     private static final String nullString = null;
+
+    @DisplayName("이름을 생성하여 반환한다.")
+    @Test
+    void createName() {
+        String hogee = "호기";
+        PlayerName playerName = new PlayerName(hogee);
+        assertThat(playerName.getName()).isEqualTo(hogee);
+    }
 
     @DisplayName("이름은 5글자를 넘을 수 없다")
     @ParameterizedTest
