51b287f881af652151cadc23fcd71433be22a8ce
KB Kim <28584160+jimi567@users.noreply.github.com>
2024-02-26 13:58:21 +0900

    feat : Players 중복 검증 로직 추가

--- src/test/java/domain/PlayersTest.java
+++ src/test/java/domain/PlayersTest.java
@@ -24,12 +24,20 @@ public class PlayersTest {
     @DisplayName("2명 미만, 10명 초과하면 예외를 발생시킨다.")
     @ParameterizedTest
     @MethodSource("createFailArguments")
-    public void createFail(List<String> players) {
+    public void createFailRange(List<String> players) {
         assertThatCode(() -> new Players(players))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("2명 이상 10명 이하인 경우 예외가 발생하지않는다,")
+    @DisplayName("중복이 존재하면 예외를 발생시킨다.")
+    @Test
+    public void createFailHasDuplication() {
+        assertThatCode(() -> new Players(List.of(
+                "wiib", "wiib", "lee")))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("2명 이상 10명 이하인 경우, 중복이 없을 경우 예외가 발생하지않는다,")
     @Test
     public void createSuccess() {
         assertThatCode(() -> new Players(List.of(
