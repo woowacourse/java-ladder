943b9bbac8235f83add83ffd70a4940f04a0a03f
ashsty <qkralsdk991@naver.com>
2024-02-26 13:54:29 +0900

    refactor : RandomGeneratorTest 수정
    DisplayName 수정
    - '랜덤' 관련 문구 삭제

--- src/test/java/model/RandomGeneratorTest.java
+++ src/test/java/model/RandomGeneratorTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 
 public class RandomGeneratorTest {
 
-    @DisplayName("수량을 입력받아 랜덤으로 생성된 불린값 리스트를 반환한다.")
+    @DisplayName("입력받은 수량과 같은 크기의 불린값 리스트를 반환한다.")
     @Test
     void generate() {
         int given = 3;
