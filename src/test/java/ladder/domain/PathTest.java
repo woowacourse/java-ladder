f5ce9fc9ffb1846d57d76414a8a00a9e4ae0393c
kelly6bf <kelly6bf@gmail.com>
2024-02-26 20:48:34 +0900

    test: Path 인스턴스 생성 테스트 로직 작성

---
+++ src/test/java/ladder/domain/PathTest.java
@@ -0,0 +1,22 @@
+package ladder.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PathTest {
+
+    @DisplayName("시작 라인 번호, 끝 라인 번호를 입력하면 Path 인스턴스를 생성한다.")
+    @Test
+    void Path_인스턴스_생성() {
+        // Given
+        final int startLineNumber = 1;
+        final int endLineNumber = 2;
+
+        // When
+        Path path = new Path.of(startLineNumber, endLineNumber);
+
+        // Then
+        Assertions.assertThat(path).isNotNull();
+    }
+}
