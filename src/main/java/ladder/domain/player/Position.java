ddd37789f7a259193174cd47f91959fee07b4a72
gyungchan Jo <namejgc@naver.com>
2024-02-26 21:09:49 +0900

    feat(Position): 참여자의 위치를 검증하는 로직 구현

---
+++ src/main/java/ladder/domain/player/Position.java
@@ -0,0 +1,34 @@
+package ladder.domain.player;
+
+import java.util.Objects;
+
+public class Position {
+    private static final int MINIMUM_POSITION = 1;
+
+    private final int position;
+
+    public Position(final int position) {
+        validatePosition(position);
+        this.position = position;
+    }
+
+    private void validatePosition(final int position) {
+        if (position < MINIMUM_POSITION) {
+            throw new IllegalArgumentException(
+                    String.format("참여자의 위치는 %d보다 작을 수 없습니다.", MINIMUM_POSITION));
+        }
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Position position1 = (Position) o;
+        return position == position1.position;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position);
+    }
+}
---
+++ src/test/java/ladder/domain/player/PositionTest.java
@@ -0,0 +1,24 @@
+package ladder.domain.player;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class PositionTest {
+
+    @Test
+    @DisplayName("참여자는 자신의 위치 정보를 가진다.")
+    void testConstruct() {
+        Position position = new Position(1);
+        assertThat(position).isEqualTo(new Position(1));
+    }
+
+    @Test
+    @DisplayName("참여자의 위치가 1보다 작다면 예외가 발생한다.")
+    void invalidPosition() {
+        assertThatThrownBy(() -> new Position(0))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
