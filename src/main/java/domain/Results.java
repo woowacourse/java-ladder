6fe04707e1187b318a00d77f43ca229a71932f02
hangillee <skfcb10@naver.com>
2024-02-26 16:40:44 +0900

    feat: 실행 결과가 사람 이름과 개수가 같은지 검증하는 로직 추가

--- src/main/java/domain/Results.java
+++ src/main/java/domain/Results.java
@@ -3,12 +3,21 @@ package domain;
 import java.util.List;
 
 public class Results {
+    private static final String INVALID_RESULT_COUNT = "실행 결과의 개수가 사람 이름의 개수와 일치하지 않습니다.";
+
     private final List<Result> results;
 
-    public Results(final List<String> values) {
+    public Results(final List<String> values, final int nameCount) {
+        validateResultSize(values, nameCount);
         this.results = values.stream()
                 .map(Result::new)
                 .toList();
+    }
+
+    private void validateResultSize(final List<String> values, final int nameCount) {
+        if (values.size() != nameCount) {
+            throw new IllegalArgumentException(INVALID_RESULT_COUNT);
+        }
     }
 
     public List<String> getAll() {
