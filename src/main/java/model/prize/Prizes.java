7f1c1a5f31ba7f06b1d8e26039e0300c566dff8c
eun-byeol <joeunbyeol98@gmail.com>
2024-02-26 21:56:53 +0900

    feat: 실행 결과 수가 참여자 등록 수와 다르면 예외 발생

--- src/main/java/model/prize/Prizes.java
+++ src/main/java/model/prize/Prizes.java
@@ -7,6 +7,8 @@ import java.util.List;
 import model.player.Players;
 
 public class Prizes {
+    private static final String INVALID_PRIZES_SIZE = "실행 결과는 참여자 등록 수와 같아야 합니다.";
+
     private final List<Prize> prizes;
 
     private Prizes(List<Prize> prizes) {
@@ -14,8 +16,15 @@ public class Prizes {
     }
 
     public static Prizes from(List<String> prizeNames, Players players) {
+        validatePrizeNamesSize(prizeNames, players);
         return prizeNames.stream()
                 .map(Prize::new)
                 .collect(collectingAndThen(toList(), Prizes::new));
+    }
+
+    private static void validatePrizeNamesSize(List<String> prizeNames, Players players) {
+        if (prizeNames.size() != players.getSize()) {
+            throw new IllegalArgumentException(INVALID_PRIZES_SIZE);
+        }
     }
 }
