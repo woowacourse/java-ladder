1a61d8061b021225dadac6384e38730be5a3b564
unifolio0 <boho82ha@gmail.com>
2024-02-26 10:19:37 +0900

    test : 경계값만 테스트 하도록 변경

--- src/test/java/HeightTest.java
+++ src/test/java/HeightTest.java
@@ -1,15 +1,13 @@
 import domain.Height;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class HeightTest {
-    @ParameterizedTest
-    @ValueSource(ints = {-10, -5, 0})
     @DisplayName("다리 높이에 0이하의 수를 입력하면 예외를 발생시킨다.")
-    void invalidHeight(int height) {
-        assertThatThrownBy(() -> new Height(height)).isInstanceOf(IllegalArgumentException.class);
+    @Test
+    void invalidHeight() {
+        assertThatThrownBy(() -> new Height(0)).isInstanceOf(IllegalArgumentException.class);
     }
 }
