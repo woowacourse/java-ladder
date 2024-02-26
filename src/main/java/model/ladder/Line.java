b5cfc8bfaf77b52e44cfc0a9a3be3f9395c16691
MingyeomKim <mingyum119@naver.com>
2024-02-26 16:34:12 +0900

    refactor(Line): 정적 팩토리 메서드에서 사다리를 생성

--- src/main/java/model/ladder/Line.java
+++ src/main/java/model/ladder/Line.java
@@ -2,47 +2,26 @@ package model.ladder;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 public class Line {
-
-    private static final Random random = new Random();
-
     private final List<Step> steps;
 
-    /**
-     * @param width
-     */
-    public Line(Width width) {
+    private Line(List<Step> steps) {
+        this.steps = steps;
+    }
+
+    public static Line from(final Width width) {
         List<Step> steps = new ArrayList<>();
-        for (int i = 0; i < width.size() - 1; i++) {
-            steps.add(s)
-            steps.add(generate(steps))
-            result.add(generate(result, i));
+        Step previous = Step.from(StepStatus.EMPTY);
+        while (steps.size() < width.size()) {
+            Step step = Step.of(previous, new RandomStepGenerator());
+            steps.add(step);
+            previous = step;
         }
-        this.steps = result;
-    }
-
-    private Boolean generate(List<Boolean> points, int index) {
-        if (cannotConnect(points, index)) {
-            return false;
-        }
-        return random.nextBoolean();
-    }
-
-    private boolean cannotConnect(List<Boolean> points, int index) {
-        return index >= 1 && points.get(index - 1);
+        return new Line(steps);
     }
 
     public int size() {
         return steps.size();
-    }
-
-    public boolean isConnected(int index) {
-        return steps.get(index);
-    }
-
-    public List<Boolean> getSteps() {
-        return steps;
     }
 }
