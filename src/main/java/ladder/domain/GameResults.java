6ec993bcf09e8616ac81c0d88c6cd839c24092ef
kelly6bf <kelly6bf@gmail.com>
2024-02-26 17:30:05 +0900

    test: GameResults 인스턴스 생성 테스트 로직 작성

---
+++ src/main/java/ladder/domain/GameResults.java
@@ -0,0 +1,15 @@
+//package ladder.domain;
+//
+//import java.util.List;
+//
+//public class GameResults {
+//    private final List<GameResult> gameResults;
+//
+//    public GameResults(final List<GameResult> gameResults) {
+//        this.gameResults = gameResults;
+//    }
+//
+//    public static GameResults of(final List<String> gameResults) {
+//        return
+//    }
+//}
