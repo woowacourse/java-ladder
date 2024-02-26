3495cdce3febdc315aeea37db9837e2b79f2eab3
ay-eonii <naayen@naver.com>
2024-02-26 15:32:43 +0900

    refactor(LadderGame): View 의존성 내부에서 주입

--- src/main/java/ladder/Application.java
+++ src/main/java/ladder/Application.java
@@ -1,12 +1,10 @@
 package ladder;
 
-import ladder.controller.LadderCreator;
-import ladder.view.InputView;
-import ladder.view.OutputView;
+import ladder.controller.LadderGame;
 
 public class Application {
     public static void main(String[] args) {
-        LadderCreator ladderCreator = new LadderCreator(new InputView(), new OutputView());
-        ladderCreator.run();
+        LadderGame ladderGame = new LadderGame();
+        ladderGame.run();
     }
 }
