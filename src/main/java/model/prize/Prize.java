e669e6040ca8357d79990426b0a4dbc4aff6ad5d
eun-byeol <joeunbyeol98@gmail.com>
2024-02-26 17:49:13 +0900

    feat: 실행 결과가 5자 초과면 예외 발생

--- src/main/java/model/prize/Prize.java
+++ src/main/java/model/prize/Prize.java
@@ -1,9 +1,19 @@
 package model.prize;
 
 public class Prize {
+    private static final int MAX_NAME_LENGTH = 5;
+    private static final String INVALID_NAME_LENGTH = "실행 결과는 최대 5글자입니다.";
+
     private final String name;
 
     public Prize(String name) {
+        validateNameLength(name);
         this.name = name;
+    }
+
+    private void validateNameLength(String name) {
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException(INVALID_NAME_LENGTH);
+        }
     }
 }
