7d1cb43b86ac574f494c5b3b682bd6ca76a49ad5
libienz <leekh10195@gmail.com>
2024-02-26 15:41:34 +0900

    feat: 사다리 결과 클래스 구현

---
+++ src/main/java/domain/LadderResult.java
@@ -0,0 +1,25 @@
+package domain;
+
+public class LadderResult {
+
+    private static final int MIN_LENGTH = 1;
+    private static final int MAX_LENGTH = 5;
+
+    private final String result;
+
+    public LadderResult(String result) {
+        validateLength(result);
+        this.result = result;
+    }
+
+    private void validateLength(String result) {
+        if (isInvalidLengthName(result)) {
+            throw new IllegalArgumentException("[ERROR] 결과의 길이는 " + MIN_LENGTH + "자에서 " + MAX_LENGTH + "자 사이여야 합니다");
+        }
+    }
+
+    private boolean isInvalidLengthName(String result) {
+        return result.length() < MIN_LENGTH || result.length() > MAX_LENGTH;
+    }
+}
+
