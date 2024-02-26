bdc74df79f5d2d03dcdc2783f0068d374971f572
JUHA <84626225+khabh@users.noreply.github.com>
2024-02-26 09:34:00 +0900

    test: Ladder에서 참가자가 도착한 마지막 위치를 반환하는 기능 테스트

---
+++ src/test/java/ladder/domain/ladder/LadderTest.java
@@ -0,0 +1,48 @@
+package ladder.domain.ladder;
+
+import ladder.domain.Position;
+import ladder.domain.generator.LadderStepsGenerator;
+import ladder.domain.participant.Participants;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static ladder.domain.ladder.Path.EMPTY;
+import static ladder.domain.ladder.Path.EXIST;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LadderTest {
+    @Test
+    @DisplayName("참가자가 도착한 사다리 마지막 위치를 반환한다.")
+    void determineFinalPositionOfParticipantTest() {
+        // given
+        Participants participants = new Participants(List.of("a", "b", "c"));
+        Ladder ladder = generateLadder();
+        List<Position> expectedPositions = List.of(new Position(2), new Position(0), new Position(1));
+
+        // when
+        List<Position> actualPositions = participants.getValues().stream()
+                .map(ladder::determineFinalPositionOf)
+                .toList();
+
+        // then
+        assertThat(actualPositions).containsExactlyElementsOf(expectedPositions);
+    }
+
+    private static Ladder generateLadder() {
+        List<LadderStep> ladderSteps = List.of(
+                new LadderStep(List.of(EXIST, EMPTY)),
+                new LadderStep(List.of(EMPTY, EXIST))
+        );
+        LadderStepsGenerator ladderStepsGenerator = new TestLadderStepsGenerator(ladderSteps);
+        return new Ladder(ladderStepsGenerator);
+    }
+
+    private record TestLadderStepsGenerator(List<LadderStep> ladderSteps) implements LadderStepsGenerator {
+        @Override
+            public List<LadderStep> generate() {
+                return ladderSteps;
+            }
+        }
+}
