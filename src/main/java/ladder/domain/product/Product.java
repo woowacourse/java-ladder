c503be7db1dc6f2b5763a6f1ae7019691c13b956
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2024-02-26 16:37:32 +0900

    feat (Product) : 상품 이름이 비어있을 경우 예외를 던진다

--- src/main/java/ladder/domain/product/Product.java
+++ src/main/java/ladder/domain/product/Product.java
@@ -2,6 +2,8 @@ package ladder.domain.product;
 
 public class Product {
 
+    private static final int MIN_NAME_LENGTH = 5;
+
     private final String name;
 
     public Product(String name) {
@@ -10,7 +12,18 @@ public class Product {
     }
 
     private void validate(String name) {
-        if (name.length() > 5) {
+        validateIsNotEmpty(name);
+        validateLengthIsInRange(name);
+    }
+
+    private void validateIsNotEmpty(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("상품 이름은 적어도 1글자 이상이어야 합니다.");
+        }
+    }
+
+    private void validateLengthIsInRange(String name) {
+        if (name.length() > MIN_NAME_LENGTH) {
             throw new IllegalArgumentException("상품 이름은 5글자를 넘을 수 없습니다.");
         }
     }
--- src/test/java/ladder/domain/product/ProductTest.java
+++ src/test/java/ladder/domain/product/ProductTest.java
@@ -1,9 +1,11 @@
 package ladder.domain.product;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class ProductTest {
@@ -16,5 +18,25 @@ class ProductTest {
         assertThatThrownBy(() -> new Product(name))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("상품 이름은 5글자를 넘을 수 없습니다.");
+    }
+
+    @DisplayName("이름은 적어도 1글자 이상 있어야 한다")
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {" ", "\n"})
+    void validateTest_WhenNameIsEmpty_ThrowException(String name) {
+
+        assertThatThrownBy(() -> new Product(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("상품 이름은 적어도 1글자 이상이어야 합니다.");
+    }
+
+    @DisplayName("이름이 1글자 이상 5글자 이하일 때, 정상적으로 객체를 생성한다")
+    @ParameterizedTest
+    @ValueSource(strings = {"pobi", "steve", "j"})
+    void validateTest(String name) {
+
+        assertThatCode(() -> new Product(name))
+                .doesNotThrowAnyException();
     }
 }
