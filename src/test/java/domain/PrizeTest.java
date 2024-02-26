530db6ffc573726597fd009484b9a7192ba73048
hyxrxn <gpfls981220@gmail.com>
2024-02-26 15:07:39 +0900

    test: Prize 클래스의 해당 위치의 결과 반환

--- src/test/java/domain/PrizeTest.java
+++ src/test/java/domain/PrizeTest.java
@@ -14,9 +14,22 @@ public class PrizeTest {
     @DisplayName("결과의 개수가 참여자의 인원수와 다르면 예외가 발생한다.")
     void numberOfPrizeExceptionTest() {
         List<String> prizes = List.of("꽝", "5000", "꽝");
+
         assertThatThrownBy(() -> new Prize(prizes, 4))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("결과의 개수가 참여자의 인원수와 다릅니다.");
+    }
+
+    @Test
+    @DisplayName("원하는 위치의 결과를 가져올 수 있다.")
+    void getPrizeAtTest() {
+        List<String> prizes = List.of("1", "100", "1000", "10000");
+        Prize prize = new Prize(prizes, 4);
+
+        assertThat(prize.getPrizeOf(0)).isEqualTo("1");
+        assertThat(prize.getPrizeOf(1)).isEqualTo("10");
+        assertThat(prize.getPrizeOf(2)).isEqualTo("100");
+        assertThat(prize.getPrizeOf(3)).isEqualTo("1000");
     }
 
 }
