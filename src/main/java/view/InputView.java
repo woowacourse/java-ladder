9b376e1176d6b2102c1ed9011a65d14683e804ab
ehtjsv2 <ehtjsv2@naver.com>
2024-02-26 17:35:15 +0900

    refactor: 이름들 구분자 매직넘버 상수화

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -13,13 +13,14 @@ public class InputView {
     private static final String MESSAGE_PARTICIPATED_PLAYERS = lineSeparator()+"참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     private static final String MESSAGE_LADDER_HEIGHT = lineSeparator()+"최대 사다리 높이는 몇 개인가요?";
     private static final Pattern EMPTY_SPACE_PATTERN = Pattern.compile(" ");
+    private static final String NAMES_SEPARATOR = ",";
 
     private static final Scanner scanner = new Scanner(in);
 
     public static List<Player> readPlayerNames() {
         out.println(MESSAGE_PARTICIPATED_PLAYERS);
         String input = EMPTY_SPACE_PATTERN.matcher(scanner.nextLine()).replaceAll("");
-        return Arrays.stream(input.split(","))
+        return Arrays.stream(input.split(NAMES_SEPARATOR))
                 .map(Player::new)
                 .toList();
     }
