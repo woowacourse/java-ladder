97415b013d17ffdda23260c768e82489dff5e2ae
nayonsoso <panda0329@naver.com>
2024-02-26 16:06:40 +0900

    feat: 에러가 발생하면 그 지점부터 다시 입력받는 기능 추가

--- src/main/java/controller/LadderGame.java
+++ src/main/java/controller/LadderGame.java
@@ -20,12 +20,39 @@ public class LadderGame {
     }
 
     public void play() {
-        List<String> names = inputView.readNames();
-        int height = inputView.readHeight();
-        Players players = new Players(names);
-        StickGenerator stickGenerator = new RandomStickGenerator();
-        Ladder ladder = new Ladder(height, players.getPlayerSize(), stickGenerator);
+        Players players = readPlayers();
+        int height = readHeight();
 
+        StickGenerator stickGenerator = new RandomStickGenerator();
+        Ladder ladder = makeLadder(height, players.getPlayerSize(), stickGenerator);
         outputView.printLadder(players, ladder);
+    }
+
+    private int readHeight() {
+        try {
+            return inputView.readHeight();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return readHeight();
+        }
+    }
+
+    private Players readPlayers() {
+        try {
+            List<String> names = inputView.readNames();
+            return new Players(names);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return readPlayers();
+        }
+    }
+
+    private Ladder makeLadder(int height, int width, StickGenerator stickGenerator) {
+        try {
+            return new Ladder(height, width, stickGenerator);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return makeLadder(readHeight(), width, stickGenerator);
+        }
     }
 }
