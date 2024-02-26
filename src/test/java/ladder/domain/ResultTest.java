e162ee0ca6eaf9e7fdffd5f09eeebc977a7772ae
mzeong <mjeong@sookmyung.ac.kr>
2024-02-26 15:03:03 +0900

    test(ResultTest): 결과 앞뒤 공백 제거

--- src/test/java/ladder/domain/ResultTest.java
+++ src/test/java/ladder/domain/ResultTest.java
@@ -1,11 +1,28 @@
 package ladder.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ResultTest {
+
+    @Test
+    @DisplayName("결과의 앞뒤 공백은 제거한다.")
+    void trimSpaces() {
+        // given
+        String expectedResult = "꽝";
+
+        // when
+        Result result = new Result("    꽝    ");
+
+        // then
+        assertThat(result)
+                .extracting("value")
+                .asString()
+                .isEqualTo(expectedResult);
+    }
 
     @Test
     @DisplayName("결과가 1글자 미만이면 예외가 발생한다.")
