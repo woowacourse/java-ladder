e9211be0d49a3c9a2100484f4039e192a1aa38ee
fromitive <fromitive@gmail.com>
2024-02-26 15:20:16 +0900

    feat: 사다리 실행 결과들 반환 기능 구현

--- src/main/java/domain/LadderResults.java
+++ src/main/java/domain/LadderResults.java
@@ -6,13 +6,20 @@ public class LadderResults {
     private static final int MIN_LADDER_RESULTS_LENGTH = 2;
     private static final int MAX_LADDER_RESULTS_LENGTH = 10;
 
+    private final List<LadderResult> ladderResults;
+
     public LadderResults(List<LadderResult> ladderResults) {
         validateLadderResultsLength(ladderResults);
+        this.ladderResults = ladderResults;
     }
 
     private void validateLadderResultsLength(List<LadderResult> ladderResults) {
         if (ladderResults.size() < MIN_LADDER_RESULTS_LENGTH || ladderResults.size() > MAX_LADDER_RESULTS_LENGTH) {
             throw new LadderGameException(ExceptionType.INVALID_LADDER_RESULTS_RANGE);
         }
+    }
+
+    public List<LadderResult> getLadderResults() {
+        return ladderResults;
     }
 }
--- src/test/java/domain/LadderResultsTest.java
+++ src/test/java/domain/LadderResultsTest.java
@@ -1,8 +1,10 @@
 package domain;
 
 import java.util.Arrays;
+import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -15,5 +17,22 @@ class LadderResultsTest {
                         .map(LadderResult::new)
                         .toList())).isInstanceOf(LadderGameException.class)
                 .hasMessage(ExceptionType.INVALID_LADDER_RESULTS_RANGE.getMessage());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2", "1,2,3,4,5,6,7,8,9,10"})
+    @DisplayName("사다리 결과 개수가 적절하면 예외를 발생하지 않음")
+    void testLadderResultsCreate(String ladderResults) {
+        Assertions.assertThatCode(() -> new LadderResults(Arrays.stream(ladderResults.split(","))
+                .map(LadderResult::new)
+                .toList())).doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("사다리 결과 개수가 적절하면 해당 개수만큼 반환할 수 있음")
+    void testGetLadderResults() {
+        List<LadderResult> expected = List.of(new LadderResult("꽝"), new LadderResult("당첨"));
+        LadderResults ladderResults = new LadderResults(expected);
+        List<LadderResult> actual = ladderResults.getLadderResults();
     }
 }
