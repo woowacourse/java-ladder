c305828bf8bed9585314edff2872c5e24b7da066
hyxrxn <gpfls981220@gmail.com>
2024-02-26 16:42:54 +0900

    feat: Name 클래스의 금지된 이름 설정

--- src/main/java/domain/Name.java
+++ src/main/java/domain/Name.java
@@ -2,6 +2,8 @@ package domain;
 
 import constant.domain.NameExceptionMessage;
 
+import java.util.Objects;
+
 public class Name {
 
     public static final int MAX_OF_NAME_LENGTH = 5;
@@ -11,6 +13,7 @@ public class Name {
     public Name(String name) {
         validateNoName(name);
         validateNameLength(name);
+        validateForbidName(name);
         this.name = name;
     }
 
@@ -23,6 +26,12 @@ public class Name {
     private void validateNameLength(String name) {
         if (name.length() > MAX_OF_NAME_LENGTH) {
             throw new IllegalArgumentException(NameExceptionMessage.OUT_OF_RANGE_NAME_LENGTH.getExceptionMessage());
+        }
+    }
+
+    private void validateForbidName(String name) {
+        if (Objects.equals(name, "all") || Objects.equals(name, "exit")) {
+            throw new IllegalArgumentException("[ERROR] all 또는 exit는 이름이 될 수 없습니다.");
         }
     }
 
