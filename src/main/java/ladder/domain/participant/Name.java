e8e827b48491635a62999640e1463a67ee549e86
JUHA <84626225+khabh@users.noreply.github.com>
2024-02-26 11:33:39 +0900

    feat: 참가자 이름이 all이 아닌지 검증하는 기능 구현

--- src/main/java/ladder/domain/participant/Name.java
+++ src/main/java/ladder/domain/participant/Name.java
@@ -2,9 +2,11 @@ package ladder.domain.participant;
 
 import ladder.exception.participant.InvalidNameLengthException;
 import ladder.exception.participant.NonAlphabeticNameException;
+import ladder.exception.participant.ProhibitedNameException;
 
 public class Name {
     private static final int MAX_NAME_LENGTH = 5;
+    private static final String PROHIBITED_NAME = "all";
     private static final String NAME_REGEX = "^[a-zA-Z]*$";
 
     private final String value;
@@ -13,6 +15,7 @@ public class Name {
         value = value.trim();
         validateNameLength(value);
         validateIsAlphabetic(value);
+        validateIsNotProhibited(value);
         this.value = value;
     }
 
@@ -25,6 +28,12 @@ public class Name {
     private static void validateIsAlphabetic(final String name) {
         if (!name.matches(NAME_REGEX)) {
             throw new NonAlphabeticNameException();
+        }
+    }
+
+    private static void validateIsNotProhibited(final String name) {
+        if (PROHIBITED_NAME.equals(name)) {
+            throw new ProhibitedNameException();
         }
     }
 
