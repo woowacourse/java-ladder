efa18e5719b238479be1d9e9771b37a4bc9d648a
ashsty <qkralsdk991@naver.com>
2024-02-26 15:41:27 +0900

    refactor : Test 수정
    - 쓰이지 않는 import 삭제
    - ENUM 설정 테스트 삭제

--- src/test/java/model/LineStateTest.java
+++ src/test/java/model/LineStateTest.java
@@ -2,29 +2,11 @@ package model;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.Arrays;
-import java.util.List;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class LineStateTest {
-
-    @DisplayName("LineState는 3가지 상태를 가지고 있는 enum이다.")
-    @Test
-    void createParticipants() {
-        String start = "start";
-        String end = "end";
-        String none = "none";
-
-        List<String> result = Arrays.stream(LineState.values())
-                .map(LineState::getState)
-                .toList();
-
-        assertThat(result)
-                .containsExactlyInAnyOrder(start, end, none);
-    }
 
     @DisplayName("true로 LineState를 생성하면 START를 반환한다")
     @ParameterizedTest
