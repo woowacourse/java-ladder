f7cd1267f9cba4fa13deb6727ac7917b27d1fb51
donghoony <aru0504@naver.com>
2024-02-26 15:12:43 +0900

    feat(LadderIndexConverter): 한 줄이 주어졌을 때, 플레이어의 결과 인덱스를 매핑하는 기능 구현

--- src/main/java/domain/ladder/LadderIndexConverter.java
+++ src/main/java/domain/ladder/LadderIndexConverter.java
@@ -1,6 +1,7 @@
 package domain.ladder;
 
 import dto.RowPatternDto;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -16,12 +17,31 @@ public class LadderIndexConverter {
     }
 
     public int getMappedIndexByPlayerIndex(int playerIndex) {
+        validateIndex(playerIndex);
         return mappedIndex.get(playerIndex);
     }
 
     public void swapByRowPattern(RowPatternDto rowPatternDto) {
+        List<Boolean> rowPattern = rowPatternDto.rowPattern();
+        validateRowPatternSize(rowPattern);
+        IntStream.range(0, rowPattern.size())
+                .filter(rowPattern::get)
+                .forEach(this::swapWithRightIndex);
     }
 
     private void swapWithRightIndex(int index) {
+        Collections.swap(mappedIndex, index, index + 1);
+    }
+
+    private void validateIndex(int playerIndex) {
+        if (playerIndex < 0 || playerIndex >= mappedIndex.size()) {
+            throw new IndexOutOfBoundsException("주어진 인덱스가 매핑 범위를 벗어납니다.");
+        }
+    }
+
+    private void validateRowPatternSize(List<Boolean> rowPattern) {
+        if (rowPattern.size() != mappedIndex.size() - 1) {
+            throw new IndexOutOfBoundsException("주어진 사다리 패턴의 크기가 올바르지 않습니다.");
+        }
     }
 }
