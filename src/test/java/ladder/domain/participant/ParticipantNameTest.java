b88353f29874d04de0fa65fb0268be7eb602e1b1
jongmee <ra123221@gmail.com>
2024-02-26 16:52:22 +0900

    test: 참가자 이름 테스트 클래스 변경

--- src/test/java/ladder/domain/participant/ParticipantNameTest.java
+++ src/test/java/ladder/domain/participant/ParticipantNameTest.java
@@ -6,13 +6,13 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
-public class ParticipantTest {
+public class ParticipantNameTest {
     @ParameterizedTest
     @ValueSource(strings = {"miamia", "potato", ""})
     @DisplayName("참가자 이름이 1~5자가 아니면 예외가 발생한다.")
     void checkNameLengthTest(final String name) {
         // when & then
-        assertThatThrownBy(() -> new Participant(name))
+        assertThatThrownBy(() -> new ParticipantName(name))
                 .isInstanceOf(IllegalArgumentException.class).hasMessage("이름은 1에서 5자 사이로 입력해 주세요.");
     }
 
@@ -21,7 +21,7 @@ public class ParticipantTest {
     @DisplayName("참가자 이름이 영어가 아니면 예외가 발생한다.")
     void checkAlphabeticNameTest(final String name) {
         // when & then
-        assertThatThrownBy(() -> new Participant(name))
+        assertThatThrownBy(() -> new ParticipantName(name))
                 .isInstanceOf(IllegalArgumentException.class).hasMessage("이름은 영어로 입력해 주세요.");
     }
 }
