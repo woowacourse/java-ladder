f5acf4e9f73aa1d523247587cb3f7447a879b118
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2024-02-26 15:17:53 +0900

    feat: Name class에서 이름이 "종료"면 예외 발생

--- src/main/java/domain/Name.java
+++ src/main/java/domain/Name.java
@@ -5,11 +5,13 @@ import exception.domain.NameExceptionMessage;
 public class Name {
 
     public static final int MAX_OF_NAME_LENGTH = 5;
+    public static final String EXIT = "종료";
     private final String name;
 
     public Name(String name) {
         validateNoName(name);
         validateNameLength(name);
+        validateUnavailableName(name);
         this.name = name;
     }
 
@@ -22,6 +24,12 @@ public class Name {
     private void validateNameLength(String name) {
         if (name.length() > MAX_OF_NAME_LENGTH) {
             throw new IllegalArgumentException(NameExceptionMessage.OUT_OF_RANGE_NAME_LENGTH.getExceptionMessage());
+        }
+    }
+
+    private void validateUnavailableName(String name) {
+        if (name.equals(EXIT)) {
+            throw new IllegalArgumentException(NameExceptionMessage.UNAVAILABLE_NAME.getExceptionMessage());
         }
     }
 
--- src/main/java/exception/domain/NameExceptionMessage.java
+++ src/main/java/exception/domain/NameExceptionMessage.java
@@ -5,7 +5,8 @@ import static domain.Name.MAX_OF_NAME_LENGTH;
 public enum NameExceptionMessage {
 
     NO_NAME("[ERROR] 이름이 없습니다."),
-    OUT_OF_RANGE_NAME_LENGTH("[ERROR] 이름의 길이는 " + MAX_OF_NAME_LENGTH + "글자를 초과할 수 없습니다.");
+    OUT_OF_RANGE_NAME_LENGTH("[ERROR] 이름의 길이는 " + MAX_OF_NAME_LENGTH + "글자를 초과할 수 없습니다."),
+    UNAVAILABLE_NAME("[ERROR] 이름에는 종료 키워드를 사용할 수 없습니다.");
 
     private final String exceptionMessage;
 
