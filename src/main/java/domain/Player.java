47d95a5b0f3bb8b254a25a9909a40107ad3a4358
SungKyum Kim <sungkyum1@naver.com>
2024-02-26 20:52:42 +0900

    feat: 주어진 방향으로 참가자의 위치를 이동시키는 기능 구현

--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -1,2 +1,19 @@
-package domain;public class Player {
+package domain;
+
+public class Player {
+    private final Name name;
+    private final Position position;
+
+    public Player(Name name, int position) {
+        this.name = name;
+        this.position = new Position(position);
+    }
+
+    void move(Direction direction) {
+        position.move(direction);
+    }
+
+    public int getPosition() {
+        return position.getPosition();
+    }
 }
