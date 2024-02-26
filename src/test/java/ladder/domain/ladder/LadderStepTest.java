790e128c03ec49d2eee49c311c2c0d0ed470ac3b
JUHA <84626225+khabh@users.noreply.github.com>
2024-02-26 09:10:50 +0900

    test: 현재 Position에서 다음으로 이동할 방향을 반환하는 기능 테스트

--- src/test/java/ladder/domain/ladder/LadderStepTest.java
+++ src/test/java/ladder/domain/ladder/LadderStepTest.java
@@ -1,11 +1,13 @@
 package ladder.domain.ladder;
 
+import ladder.domain.Position;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static ladder.domain.Direction.*;
 import static ladder.domain.ladder.Path.EMPTY;
 import static ladder.domain.ladder.Path.EXIST;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,10 +21,25 @@ class LadderStepTest {
         final List<Path> expectedLadderPaths = List.of(EXIST, EMPTY, EMPTY);
 
         // when
-        final List<Path> actualLadderPaths = new LadderStep(continuousLadderPaths).ladderPaths();
+        final List<Path> actualLadderPaths = new LadderStep(continuousLadderPaths).getLadderPaths();
 
         // then
         assertThat(actualLadderPaths)
                 .containsExactlyElementsOf(expectedLadderPaths);
+    }
+
+    @Test
+    @DisplayName("Position이 현재 Step에서 이동하게 될 방향을 올바르게 반환한다.")
+    void getNextDirectionTest() {
+        // given
+        LadderStep ladderStep = new LadderStep(List.of(EXIST, EMPTY));
+        Position expectedRight = new Position(0);
+        Position expectedLeft = new Position(1);
+        Position expectedNeutral = new Position(2);
+
+        // when & then
+        assertThat(ladderStep.getNextDirection(expectedRight)).isEqualTo(RIGHT);
+        assertThat(ladderStep.getNextDirection(expectedLeft)).isEqualTo(LEFT);
+        assertThat(ladderStep.getNextDirection(expectedNeutral)).isEqualTo(NEUTRAL);
     }
 }
