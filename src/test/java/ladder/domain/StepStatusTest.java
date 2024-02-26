e28f61213e387b602f31abcb4cc5f5851b89da34
Minjoo <m0522inju@gmail.com>
2024-02-26 12:17:49 +0900

    style: import 위치 변경 및 필요 없는 import문 제거

--- src/test/java/ladder/domain/StepStatusTest.java
+++ src/test/java/ladder/domain/StepStatusTest.java
@@ -1,10 +1,10 @@
 package ladder.domain;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
 import static ladder.domain.StepStatus.EXIST;
 import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class StepStatusTest {
 
