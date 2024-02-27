813986a592ac3b750e681084555b467f3d98604e
Hyeon0208 <dmsgktn0208@gmail.com>
2024-02-27 15:57:59 +0900

    test : 플레이어 이름 길이 예외 테스트 추가

--- src/test/java/domain/player/PlayerNameTest.java
+++ src/test/java/domain/player/PlayerNameTest.java
@@ -5,6 +5,7 @@ import domain.player.message.PlayerExceptionMessage;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -13,6 +14,16 @@ public class PlayerNameTest {
     @Nested
     @DisplayName("참가자 이름 공백 테스트")
     class PlayerNameBlankTest {
+
+        @Test
+        @DisplayName("길이가 1 ~ 5 글자가 아니라면 예외가 발생한다")
+        void createPlayerNameFailByLength() {
+            String name = "abcdef";
+
+            Assertions.assertThatThrownBy(() -> new PlayerName(name))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(PlayerExceptionMessage.PLAYER_NAME_LENGTH);
+        }
 
         @ParameterizedTest
         @ValueSource(strings = {"", " "})
