71e2256c0feeb3a7352d478740b8b2fb2d46ca2d
lemone <lejinhy@gmail.com>
2024-02-27 15:18:51 +0900

    feat: 실행 결과의 수가 플레이어 수와 같지 않다면 오류가 발생하는 기능 추가

--- src/main/java/laddergame/domain/ExecutionResult.java
+++ src/main/java/laddergame/domain/ExecutionResult.java
@@ -5,7 +5,18 @@ import java.util.List;
 public class ExecutionResult {
     private final List<String> elements;
 
-    public ExecutionResult(List<String> elements) {
+    public ExecutionResult(List<String> elements, int elementsCount) {
+        validate(elements, elementsCount);
         this.elements = elements;
+    }
+
+    public void validate(List<String> elements, int elementsCount) {
+        checkExecutionResultCount(elements, elementsCount);
+    }
+
+    public void checkExecutionResultCount(List<String> elements, int elementsCount) {
+        if (elements.size() != elementsCount) {
+            throw new IllegalArgumentException();
+        }
     }
 }
--- src/test/java/laddergame/domain/ExecutionResultTest.java
+++ src/test/java/laddergame/domain/ExecutionResultTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DisplayName("실행 결과")
 public class ExecutionResultTest {
@@ -13,9 +14,16 @@ public class ExecutionResultTest {
     @DisplayName("실행 결과 객체를 생성한다.")
     void createExecutionResult() {
         assertThatCode(() -> {
-            new ExecutionResult(List.of("result1", "result2", "result3"));
+            new ExecutionResult(List.of("result1", "result2", "result3"), 3);
         })
         .doesNotThrowAnyException();
     }
 
+    @Test
+    @DisplayName("실행 결과가 플레이어 수만큼 존재하지 않으면 오류가 발생한다.")
+    void findExecutionResultCountIsPlayersCount() {
+        final int playersCount = 4;
+        assertThrows(IllegalArgumentException.class,
+                () -> new ExecutionResult(List.of("result1", "result2", "result3"), playersCount));
+    }
 }
