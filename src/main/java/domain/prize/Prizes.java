ad1f05f6b971bdee8a8a78d14a299d85a5a1c741
Nam Gi Beom <rlqja0523@naver.com>
2024-02-27 16:34:49 +0900

    feat: 상품 일급 컬렉션 추가

---
+++ src/main/java/domain/prize/Prizes.java
@@ -0,0 +1,20 @@
+package domain.prize;
+
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toList;
+
+import java.util.List;
+
+public class Prizes {
+    private final List<Prize> prizes;
+
+    private Prizes(List<Prize> prizes) {
+        this.prizes = prizes;
+    }
+
+    public static Prizes from(List<String> rawPrizes) {
+        return rawPrizes.stream()
+                .map(Prize::new)
+                .collect(collectingAndThen(toList(), Prizes::new));
+    }
+}
