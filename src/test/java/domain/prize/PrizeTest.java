7a6c04e35357c4a75883b8112b254aa9bb66c53f
Nam Gi Beom <rlqja0523@naver.com>
2024-02-27 14:40:40 +0900

    test: 상품명이 한글, 영문자, 숫자로만 구성되어 있는지 테스트

--- src/test/java/domain/prize/PrizeTest.java
+++ src/test/java/domain/prize/PrizeTest.java
@@ -22,4 +22,20 @@ public class PrizeTest {
         assertThatCode(() -> new Prize(name))
                 .doesNotThrowAnyException();
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"상품!", "prize@#$", "!123", "#(!$"})
+    void 상품명이_한글_영문자_숫자로만_구성되어_있지_않으면_예외가_발생한다(String name) {
+        // when & then
+        assertThatThrownBy(() -> new Prize(name))
+                .isExactlyInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"상품", "prize", "3000", "상품1", "prize1"})
+    void 상품명이_한글_영문자_숫자로만_구성되어_있으면_예외가_발생하지_않는다(String name) {
+        // when & then
+        assertThatCode(() -> new Prize(name))
+                .doesNotThrowAnyException();
+    }
 }
