37e55d4f1a029be0f38132c26886a1055a787b65
gyungchan Jo <namejgc@naver.com>
2024-02-27 18:14:51 +0900

    feat(PlayResult): 참여자 이름에 해당하는 결과를 얻는 기능 구현

--- src/main/java/ladder/domain/game/PlayResult.java
+++ src/main/java/ladder/domain/game/PlayResult.java
@@ -8,13 +8,29 @@ import java.util.List;
 import java.util.Map;
 
 public class PlayResult {
-    private final Map<String, String> result = new LinkedHashMap<>();
+    private final Map<String, String> result;
+
+    PlayResult(final Map<String, String> result) {
+        this.result = new LinkedHashMap<>(result);
+    }
 
     public PlayResult(final List<Player> players, final List<String> prizes) {
+        this.result = recordResult(players, prizes);
+    }
+
+    private Map<String, String> recordResult(final List<Player> players, final List<String> prizes) {
+        final Map<String, String> result = new LinkedHashMap<>();
+
         for (final Player player : players) {
             final int position = player.getPosition();
             result.put(player.getName(), prizes.get(position));
         }
+
+        return result;
+    }
+
+    public String findByName(final String name) {
+        return result.get(name);
     }
 
     public Map<String, String> getResult() {
---
+++ src/test/java/ladder/domain/game/PlayResultTest.java
@@ -0,0 +1,22 @@
+package ladder.domain.game;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PlayResultTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {"pobi, 5000", "honux, 3000", "crong, 꽝", "jk, 꽝"})
+    @DisplayName("참여자 이름에 해당하는 결과를 얻을 수 있다.")
+    void findByName(String name, String expected) {
+        PlayResult playResult = new PlayResult(Map.of("pobi", "5000", "crong", "꽝", "honux", "3000", "jk", "꽝"));
+        String actual = playResult.findByName(name);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+}
