32473f6656fe85e0928e3415d511041bdd01cfb7
BurningFalls <sherlocky31415@gmail.com>
2024-02-27 10:54:00 +0900

    refactor: "all" 문자열을 SPECIAL_NAME 상수로 정의

--- src/main/java/ladder/model/Player.java
+++ src/main/java/ladder/model/Player.java
@@ -6,6 +6,7 @@ public class Player {
     private static final int MIN_NAME_LENGTH = 1;
     private static final int MAX_NAME_LENGTH = 5;
     private static final String NAME_PATTERN = "^[a-zA-Z0-9]*$";
+    private static final String SPECIAL_NAME = "all";
 
     private final String name;
 
@@ -33,7 +34,7 @@ public class Player {
     }
 
     public boolean isNameAll() {
-        return name.equals("all");
+        return name.equals(SPECIAL_NAME);
     }
 
     public String getName() {
