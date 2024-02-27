1c662b7648deda10431806794b4cd50b772f9bb3
coli-geonwoo <kkwoo001021@naver.com>
2024-02-27 18:02:10 +0900

    feat(view): 게임 결과 출력기능 구현

--- src/main/java/laddergame/view/ResultView.java
+++ src/main/java/laddergame/view/ResultView.java
@@ -1,5 +1,7 @@
 package laddergame.view;
 
+import laddergame.domain.LadderGame;
+import laddergame.domain.gameelements.people.Name;
 import laddergame.domain.gameelements.people.People;
 import laddergame.domain.gameelements.results.Results;
 import laddergame.domain.ladder.Ladder;
@@ -15,6 +17,16 @@ public class ResultView {
         System.out.println(MessageResolver.resolveElementMessage(people));
         System.out.println(MessageResolver.resolveLadderMessage(ladder));
         System.out.println(MessageResolver.resolveElementMessage(results));
+    }
+
+    public static void printPlayerResult(String playerName, LadderGame ladderGame) {
+        if (playerName.equals("all")) {
+            System.out.println(MessageResolver
+                    .resolveAllPlayerResultMessage(ladderGame.getPlayerGameResult()));
+            return;
+        }
+        System.out.println(MessageResolver
+                .resolvePlayerResultMessage(ladderGame.findPlayerResult(new Name(playerName))));
     }
 
 }
