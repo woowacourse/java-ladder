89c33b43e70cb9afc903cdb7f0c0abc29759d680
이상진 <priceelees@gmail.com>
2024-02-27 15:55:16 +0900

    refactor(Prize): 문자열 형태의 이름으로부터 객체를 얻는 정적 팩토리 메서드 추가

--- src/main/java/domain/Prize.java
+++ src/main/java/domain/Prize.java
@@ -1,31 +1,18 @@
 package domain;
 
-import java.util.Objects;
-
 public class Prize {
 
 	private final Name name;
 
-	public Prize(String name) {
-		this.name = new Name(name);
+	private Prize(Name name) {
+		this.name = name;
+	}
+
+	public static Prize fromName(String name) {
+		return new Prize(new Name(name));
 	}
 
 	public String getName() {
 		return name.value();
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if (this == o)
-			return true;
-		if (o == null || getClass() != o.getClass())
-			return false;
-		Prize prize = (Prize)o;
-		return Objects.equals(name, prize.name);
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(name);
 	}
 }
