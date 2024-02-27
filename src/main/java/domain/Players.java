704bf949b786ee7cf9f6f9675d3a1f6d5e336b0e
SungKyum Kim <sungkyum1@naver.com>
2024-02-27 20:22:11 +0900

    style: stream 가독성 개선

--- src/main/java/domain/Players.java
+++ src/main/java/domain/Players.java
@@ -14,7 +14,9 @@ public class Players {
     }
 
     private List<Player> createPlayers(Names names) {
-        return IntStream.range(0, names.size()).mapToObj(i -> new Player(names.getNames().get(i), i)).toList();
+        return IntStream.range(0, names.size())
+                .mapToObj(i -> new Player(names.getNames().get(i), i))
+                .toList();
     }
 
     public void playGame(Lines lines) {
@@ -22,7 +24,9 @@ public class Players {
     }
 
     public Player findByName(String name) {
-        return players.stream().filter(player -> player.getName().equals(name)).findAny().orElseThrow(() -> new NoSuchElementException("존재하지 않는 이름입니다."));
+        return players.stream()
+                .filter(player -> player.getName().equals(name)).findAny()
+                .orElseThrow(() -> new NoSuchElementException("존재하지 않는 이름입니다."));
     }
 
     public List<Player> getPlayers() {
