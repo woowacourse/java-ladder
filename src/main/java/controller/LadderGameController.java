80c6a04effa4768cddb125d8ada6d52602b1c5a4
이상진 <priceelees@gmail.com>
2024-02-27 15:57:59 +0900

    feat(LadderGameController): 입,출력과 게임 실행을 담당하는 별도의 컨트롤러 구현

---
+++ src/main/java/controller/LadderGameController.java
@@ -0,0 +1,112 @@
+package controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Supplier;
+
+import domain.LadderHeight;
+import domain.Player;
+import domain.Players;
+import domain.Prizes;
+import game.LadderGame;
+import view.InputView;
+import view.OutputView;
+
+public class LadderGameController {
+
+	private final InputView inputView;
+	private final OutputView outputView;
+	private final LadderGame ladderGame;
+
+	public LadderGameController(InputView inputView, OutputView outputView, LadderGame ladderGame) {
+		this.inputView = inputView;
+		this.outputView = outputView;
+		this.ladderGame = ladderGame;
+	}
+
+	public void run() {
+		Players players = retryOnException(this::getPlayers);
+		int playerCount = players.getPlayerCount();
+
+		Prizes prizes = retryOnException(() -> getPrizes(playerCount));
+		LadderHeight height = retryOnException(this::getHeight);
+
+		ladderGame.createRandomLadder(playerCount, height);
+
+		printCurrentLadder(players, prizes);
+		printPlayerResult(players, prizes);
+	}
+
+	private Players getPlayers() {
+		outputView.printReadNames();
+		List<String> names = inputView.readNames();
+
+		List<Player> players = new ArrayList<>();
+		for (int order = 0; order < names.size(); order++) {
+			Player player = new Player(names.get(order), order);
+			players.add(player);
+		}
+
+		return new Players(players);
+	}
+
+	private Prizes getPrizes(int playerCount) {
+		outputView.printReadPrizes();
+		List<String> prizeNames = inputView.readPrizes(playerCount);
+
+		return Prizes.fromNames(prizeNames);
+	}
+
+	private LadderHeight getHeight() {
+		outputView.printReadLadderHeight();
+		int height = inputView.readLadderHeight();
+
+		return new LadderHeight(height);
+	}
+
+	private void printCurrentLadder(Players players, Prizes prizes) {
+		outputView.printResultMessage();
+		outputView.printNames(players.getPlayerNames());
+		outputView.printLadder(ladderGame.getCurrentLadder());
+		outputView.printNames(prizes.getPrizeNames());
+		outputView.printEmptyLine();
+	}
+
+	private void printPlayerResult(Players players, Prizes prizes) {
+		String playerName = retryOnException(() -> getPlayerNameToShowResult(players.getPlayerNames()));
+
+		if (playerName.equals("all")) {
+			printAllPlayersResult(players, prizes);
+			return;
+		}
+		printOnePlayersResult(players, prizes, playerName);
+	}
+
+	private String getPlayerNameToShowResult(List<String> allPlayerNames) {
+		outputView.printReadPlayer();
+		return inputView.readPlayersToShowResult(allPlayerNames);
+	}
+
+	private void printAllPlayersResult(Players players, Prizes prizes) {
+		Map<String, String> results = ladderGame.getAllPlayersPrizeNames(players, prizes);
+		outputView.printAllPlayersResult(results);
+	}
+
+	private void printOnePlayersResult(Players players, Prizes prizes, String playerName) {
+		Player player = players.findPlayerFromName(playerName);
+		String result = ladderGame.getOnePlayersPrizeName(player, prizes);
+		outputView.printOnePlayerResult(result);
+	}
+
+	private <T> T retryOnException(Supplier<T> function) {
+		try {
+			T result = function.get();
+			outputView.printEmptyLine();
+			return result;
+		} catch (IllegalArgumentException e) {
+			outputView.printErrorMessage(e.getMessage());
+			return retryOnException(function);
+		}
+	}
+}
---
+++ src/test/java/controller/LadderGameControllerTest.java
@@ -0,0 +1,149 @@
+package controller;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.time.Duration;
+import java.util.NoSuchElementException;
+import java.util.function.BooleanSupplier;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import game.LadderGame;
+import generator.LadderFloorGenerator;
+import view.InputView;
+import view.OutputView;
+
+public class LadderGameControllerTest {
+
+	private PrintStream standardOut;
+	private OutputStream out;
+
+	@BeforeEach
+	void setUp() {
+		standardOut = System.out;
+		out = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(out));
+	}
+
+	@AfterEach
+	void tearDown() {
+		System.setOut(standardOut);
+		System.out.println(out.toString().trim());
+	}
+
+	@Test
+	@DisplayName("올바른 입력인 경우, 올바르게 작동 후 종료한다.")
+	void validGameTest() {
+		// given
+		command("a,b,c", "d,e,f", "2", "all");
+		// when
+		run();
+		// then
+		assertThat(out.toString())
+			.containsSubsequence(
+				"참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)",
+				"최대 사다리 높이는 몇 개인가요?",
+				"사다리 결과",
+				"a     b     c",
+				"|-----|     |",
+				"|-----|     |",
+				"d     e     f",
+				"결과를 보고 싶은 사람은?",
+				"실행 결과",
+				"a : d",
+				"b : e",
+				"c : f"
+			);
+	}
+
+	@Test
+	@DisplayName("올바르지 않은 이름이 입력되면 재입력을 받는다.")
+	void retryInputOnNamesExceptionTest() {
+		command(
+			"hello,my,name,is,sangdol"
+			, "a:b:c"
+			, "1,2,3"
+			, ""
+		);
+
+		try {
+			assertTimeoutPreemptively(Duration.ofSeconds(1L), this::run);
+		} catch (NoSuchElementException ignored) {
+		}
+	}
+
+	@Test
+	@DisplayName("올바르지 않은 실행 결과가 입력되면 재입력을 받는다.")
+	void retryInputOnPrizesExceptionTest() {
+		command(
+			"a,b,c" // 이름 정상 입력
+			, "a,b,c,d"
+			, "123456, 3, 4, 5"
+			, ""
+			, "12345, ,꽝,꽝"
+		);
+		try {
+			assertTimeoutPreemptively(Duration.ofSeconds(1L), this::run);
+		} catch (NoSuchElementException ignored) {
+		}
+	}
+
+	@Test
+	@DisplayName("올바르지 않은 사다리 높이가 입력되면 재입력을 받는다.")
+	void retryInputOnLadderHeightsExceptionTest() {
+		command(
+			"a,b,c" // 이름 정상 입력
+			, "꽝, 5000, 3000" // 실행 결과 정상 입력
+			, "a"
+			, "-1"
+			, ""
+		);
+
+		try {
+			assertTimeoutPreemptively(Duration.ofSeconds(1L), this::run);
+		} catch (NoSuchElementException ignored) {
+		}
+	}
+
+	@Test
+	@DisplayName("올바르지 않은 유저 이름이 입력되면 재입력을 받는다.")
+	void retryInputOnPlayerNameExceptionTest() {
+		command(
+			"a,b,c" // 이름 정상 입력
+			, "꽝, 5000, 3000" // 실행 결과 정상 입력
+			, "5" // 사다리 정상 입력
+			, "ali"
+			, "d"
+			, ""
+		);
+
+		try {
+			assertTimeoutPreemptively(Duration.ofSeconds(1L), this::run);
+		} catch (NoSuchElementException ignored) {
+		}
+	}
+
+	private void run() {
+		InputView inputView = new InputView();
+		OutputView outputView = new OutputView();
+		BooleanSupplier supplier = () -> true;
+		LadderFloorGenerator generator = new LadderFloorGenerator(supplier);
+		LadderGame ladderGame = new LadderGame(generator);
+		LadderGameController controller = new LadderGameController(inputView, outputView, ladderGame);
+
+		controller.run();
+	}
+
+	private void command(final String... args) {
+		final byte[] buf = String.join(System.lineSeparator(), args).getBytes();
+		System.setIn(new ByteArrayInputStream(buf));
+	}
+}
