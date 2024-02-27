f89cc1c944e5aa77ea83373f8fce94a66ec412bf
Minjoo <m0522inju@gmail.com>
2024-02-27 14:45:11 +0900

    refactor: "all"을 상수화

--- src/main/java/ladder/domain/RequestName.java
+++ src/main/java/ladder/domain/RequestName.java
@@ -3,6 +3,8 @@ package ladder.domain;
 import java.util.Objects;
 
 public class RequestName {
+    private static final String ALL = "all";
+
     private final String requestName;
 
     public RequestName(String requestName, UserNames userNames) {
@@ -11,7 +13,7 @@ public class RequestName {
     }
 
     public void validateRequestName(String requestName, UserNames userNames) {
-        boolean isNotAll = !requestName.equals("all");
+        boolean isNotAll = !requestName.equals(ALL);
         boolean isNotExist = !userNames.isExist(requestName);
         if (isNotAll && isNotExist) {
             throw new IllegalArgumentException("존재하지 않는 참여자입니다.");
@@ -19,7 +21,7 @@ public class RequestName {
     }
 
     public boolean isNotAll() {
-        return !Objects.equals(requestName, "all");
+        return !Objects.equals(requestName, ALL);
     }
 
     public String getRequestName() {
