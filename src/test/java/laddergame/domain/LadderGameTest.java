81b2d4edbd4801fcf8f9c6ac1d13bbe38ed0315b
lemone <lejinhy@gmail.com>
2024-02-27 19:58:41 +0900

    refactor: 메소드명 수정 결과 테스트에 반영

--- src/test/java/laddergame/domain/LadderGameTest.java
+++ src/test/java/laddergame/domain/LadderGameTest.java
@@ -1,12 +1,12 @@
 package laddergame.domain;
 
 import laddergame.util.LinesGenerator;
-import laddergame.util.RandomLinesGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DisplayName("사다리 게임")
@@ -24,10 +24,37 @@ public class LadderGameTest {
             }
         };
         final Ladder ladder = new Ladder(linesGenerator, players.getPlayersCount(), height);
-        final LadderGame ladderGame = new LadderGame(players, ladder);
+        final LadderGame ladderGame = new LadderGame(
+                players,
+                ladder,
+                new ResultItems(List.of("O","O","X","O"), players.getPlayersCount()));
 
-        ladderGame.start();
+        ladderGame.climb();
 
         assertTrue(players.getPlayers().get(3).getPosition().equals(new Position(3, 4)));
+    }
+
+    @Test
+    @DisplayName("플레이어 마다 도착한 위치에 있는 실행 결과를 반환하는지 테스트한다.")
+    void returnPlayersExecutionResult() {
+        final Players players = new Players(List.of("name1", "name2", "name3", "name4"));
+        final Height height = new Height("3");
+        final ResultItems resultItems = new ResultItems(List.of("a","b","c","d"), players.getPlayersCount());
+        LinesGenerator linesGenerator = new LinesGenerator() {
+            @Override
+            public List<Line> generate(int width) {
+                return List.of(Line.EMPTY, Line.BRIDGE, Line.EMPTY);
+            }
+        };
+        final Ladder ladder = new Ladder(linesGenerator, players.getPlayersCount(), height);
+        final LadderGame ladderGame = new LadderGame(players, ladder, resultItems);
+
+        ladderGame.climb();
+        ladderGame.calculatePlayersItem();
+
+        assertEquals(players.getPlayers().get(0).getItem(), resultItems.findByIndex(0));
+        assertEquals(players.getPlayers().get(1).getItem(), resultItems.findByIndex(2));
+        assertEquals(players.getPlayers().get(2).getItem(), resultItems.findByIndex(1));
+        assertEquals(players.getPlayers().get(3).getItem(), resultItems.findByIndex(3));
     }
 }
