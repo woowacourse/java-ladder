f6ecade38dcb5dc6129add88a127b47ca2ff6213
hangillee <skfcb10@naver.com>
2024-02-27 16:51:41 +0900

    feat: 사다리를 모두 이동한 후의 사람 이름과 실행 결과를 매칭하는 로직 구현

--- src/main/java/domain/Results.java
+++ src/main/java/domain/Results.java
@@ -1,6 +1,9 @@
 package domain;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Results {
     private static final String INVALID_RESULT_COUNT = "실행 결과의 개수가 사람 이름의 개수와 일치하지 않습니다.";
@@ -28,6 +31,15 @@ public class Results {
 
     public String resultOf(final int index) {
         return results.get(index).getValue();
+    }
+
+    public Map<String, String> matchNamesWithResults(final List<String> namesAfterMove) {
+        Map<String, String> resultWithName = new HashMap<>();
+        for (int i = 0; i < namesAfterMove.size(); i++) {
+            Result result = results.get(i);
+            resultWithName.put(namesAfterMove.get(i), result.getValue());
+        }
+        return Collections.unmodifiableMap(resultWithName);
     }
 
     public int size() {
