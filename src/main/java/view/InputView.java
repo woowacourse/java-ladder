a0bb8a391158fd4d352d3d46d7b9bf2a84e25301
DobbyMac <kimdobby@g.skku.edu>
2024-02-27 16:42:38 +0900

    refactor: 코드 가독성을 위한 리팩터링 적용
    - 매개변수명 수정
    - View의 생성자 접근제어자 변경

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -7,15 +7,15 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
-    public InputView() {
+    private InputView() {
     }
 
     private static String input() {
         return scanner.next();
     }
 
-    public static List<String> inputNames(String message) {
-        System.out.println(message);
+    public static List<String> inputNames(String userPrompt) {
+        System.out.println(userPrompt);
         String initialInput = input();
         return Arrays.stream(initialInput.split(","))
                      .map(String::trim)
