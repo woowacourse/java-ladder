735697642dbcca08f19ed25bc06ec5fa039a13cf
Eugene Jang <kyunellroll@gmail.com>
2024-02-27 13:58:56 +0900

    feat(Participants): 입력한 이름의 position을 가져오는 기능 구현
    - Name 속 equals, hash 재정의
    - 입력한 이름의 position을 가져오도록 기능 구현
    - 존재하는 이름이 아닐 경우 에러를 받도록 구현

--- src/main/java/ladder/domain/Participants.java
+++ src/main/java/ladder/domain/Participants.java
@@ -23,6 +23,15 @@ public class Participants {
         return names.size();
     }
 
+    public int findNamePosition(Name name) {
+        int index = names.indexOf(name);
+
+        if (index == -1) {
+            throw new IllegalArgumentException("입력한 이름이 존재하지 않습니다.");
+        }
+        return index;
+    }
+
     private List<Name> inviteParticipants(List<String> names) {
         return names.stream()
                 .map(Name::new)
@@ -36,7 +45,7 @@ public class Participants {
 
     private void validateIsNotOverMaximumReception(List<String> names) {
         if (names.size() > MAXIMUM_RECEPTION_COUNT) {
-            throw new IllegalArgumentException("참여자 인원은 최대 "+MAXIMUM_RECEPTION_COUNT+"명까지 가능합니다.");
+            throw new IllegalArgumentException("참여자 인원은 최대 " + MAXIMUM_RECEPTION_COUNT + "명까지 가능합니다.");
         }
     }
 
--- src/test/java/ladder/domain/ParticipantsTest.java
+++ src/test/java/ladder/domain/ParticipantsTest.java
@@ -18,7 +18,7 @@ public class ParticipantsTest {
 
         Assertions.assertThatThrownBy(() -> new Participants(names))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("참여자 인원은 최대 "+MAXIMUM_RECEPTION_COUNT+"명까지 가능합니다.");
+                .hasMessage("참여자 인원은 최대 " + MAXIMUM_RECEPTION_COUNT + "명까지 가능합니다.");
     }
 
     @Test
@@ -29,5 +29,18 @@ public class ParticipantsTest {
         Assertions.assertThatThrownBy(() -> new Participants(names))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("참여자 이름은 중복을 허용하지 않습니다");
+    }
+
+    @Test
+    @DisplayName("이름에 걸맞는 position을 찾을 때 이름이 없는 경우 error")
+    void findNotExistNamePosition() {
+        List<String> names = new ArrayList<>(List.of("aru", "pola", "jazz"));
+        Participants participants = new Participants(names);
+
+        Name name = new Name("gugu");
+
+        Assertions.assertThatThrownBy(() -> participants.findNamePosition(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("입력한 이름이 존재하지 않습니다.");
     }
 }
