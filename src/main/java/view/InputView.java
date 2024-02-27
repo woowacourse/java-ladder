823b4327c0cc24393485a449b113a91fa2b1dfcd
nayonsoso <panda0329@naver.com>
2024-02-27 19:12:57 +0900

    refactor: inputView 함수명과 호출 순서 변경

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -9,22 +9,39 @@ public class InputView {
     private final Scanner scanner = new Scanner(System.in);
 
     public List<String> readNames() {
-        String inputNames = readLine("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
+        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
+        String inputName = scanner.nextLine();
+        validateNotBlank(inputName);
 
-        return Arrays.stream(inputNames.split(",")).toList();
+        return splitByCommaAndTrim(inputName);
     }
 
     public int readHeight() {
-        try {
-            String inputHeight = readLine("최대 사다리 높이는 몇 개인가요?");
-            return Integer.parseInt(inputHeight);
-        } catch (NumberFormatException e) {
-            throw new NumberFormatException("높이는 정수여야 합니다.");
+        System.out.println("최대 사다리 높이는 몇 개인가요?");
+        String inputHeight = scanner.nextLine().trim();
+        validateNotBlank(inputHeight);
+        validateHeightIsInt(inputHeight);
+
+        return Integer.parseInt(inputHeight);
+    }
+
+    private void validateNotBlank(String input) {
+        if (input.isBlank()) {
+            throw new IllegalArgumentException("빈 값은 입력할 수 없습니다.");
         }
     }
 
-    private String readLine(String message) {
-        System.out.println(message);
-        return scanner.nextLine().trim();
+    private List<String> splitByCommaAndTrim(String input) {
+        return Arrays.stream(input.split(",", -1))
+                .map(String::trim)
+                .toList();
+    }
+
+    private void validateHeightIsInt(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException("높이는 정수여야 합니다.");
+        }
     }
 }
