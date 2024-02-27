9693a2a71a2a92614b4d5100cd878c0930f2735f
gyungchan Jo <namejgc@naver.com>
2024-02-27 16:51:03 +0900

    feat(LadderGame): 사다리 게임의 상품을 포함하는 생성자 구현

--- src/main/java/ladder/domain/game/LadderGame.java
+++ src/main/java/ladder/domain/game/LadderGame.java
@@ -2,13 +2,25 @@ package ladder.domain.game;
 
 import ladder.domain.ladder.Ladder;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LadderGame {
     private final Ladder ladder;
     private final Players players;
+    private final List<String> prizes;
 
-    public LadderGame(final Ladder ladder, final Players players) {
+    public LadderGame(final Ladder ladder, final Players players, final List<String> prizes) {
+        validateLadderGame(players.count(), prizes.size());
         this.ladder = ladder;
         this.players = players;
+        this.prizes = new ArrayList<>(prizes);
+    }
+
+    private void validateLadderGame(final int playerCount, final int prizeCount) {
+        if (playerCount != prizeCount) {
+            throw new IllegalArgumentException("사다리 게임의 참여하는 사람의 수와 상품의 수는 같아야합니다.");
+        }
     }
 
     public PlayResult play() {
--- src/test/java/ladder/domain/game/LadderGameTest.java
+++ src/test/java/ladder/domain/game/LadderGameTest.java
@@ -13,11 +13,35 @@ import static org.assertj.core.api.Assertions.*;
 class LadderGameTest {
 
     @Test
-    @DisplayName("사다리 게임 결과를 알 수 있다.")
-    void play() {
+    @DisplayName("사다리 게임에 참여하는 참여자들과 결과에 따른 상품에 대한 정보를 가진다.")
+    void createLadderGame() {
         Players players = new Players(List.of("pobi", "honux", "crong", "jk"));
         Ladder ladder = new Ladder(4, 1, new MockBooleanGenerator(List.of(true, false, true)));
-        LadderGame ladderGame = new LadderGame(ladder, players);
+        List<String> prizes = List.of("꽝", "5000", "꽝", "3000");
+
+        assertThatCode(() -> new LadderGame(ladder, players, prizes))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("참여자들의 수와 상품의 수가 다르면 예외가 발생한다.")
+    void invalidLadderGame() {
+        Players players = new Players(List.of("pobi", "honux", "crong", "jk"));
+        Ladder ladder = new Ladder(4, 1, new MockBooleanGenerator(List.of(true, false, true)));
+        List<String> prizes = List.of("꽝", "5000", "꽝");
+
+        assertThatThrownBy(() -> new LadderGame(ladder, players, prizes))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("사다리 게임 결과를 알 수 있다.")
+    void play() {
+        Ladder ladder = new Ladder(4, 1, new MockBooleanGenerator(List.of(true, false, true)));
+        Players players = new Players(List.of("pobi", "honux", "crong", "jk"));
+        List<String> prizes = List.of("꽝", "5000", "꽝", "3000");
+
+        LadderGame ladderGame = new LadderGame(ladder, players, prizes);
 
         PlayResult playResult = ladderGame.play();
         Map<String, Integer> result = playResult.getResult();
