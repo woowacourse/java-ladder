0bea0efc3941462e80caeafe49915e6d3f5292c9
zangsu <zangsu_@naver.com>
2024-02-27 23:11:05 +0900

    style : 에러 메시지 수정 "반복된 입력" -> "잘못된 입력"

--- src/main/java/LadderApplication.java
+++ src/main/java/LadderApplication.java
@@ -4,7 +4,7 @@ public class LadderApplication {
         try {
             LadderGameController.run();
         } catch (StackOverflowError error) {
-            System.out.println("반복된 입력의 반복으로 프로그램을 종료합니다.");
+            System.out.println("잘못된 입력의 반복으로 프로그램을 종료합니다.");
         }
     }
 }
