a44f3d6d1bee2a5732434f32e139118087776f69
Nam Gi Beom <rlqja0523@naver.com>
2024-02-27 13:44:38 +0900

    test: 사다리를 탄 후 결과를 확인하는 테스트

--- src/test/java/domain/ladder/LadderTest.java
+++ src/test/java/domain/ladder/LadderTest.java
@@ -5,8 +5,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import domain.height.Height;
 import domain.player.Players;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import support.ConnectedLadderRungGenerator;
+import support.FixedLadderRungGenerator;
 
 public class LadderTest {
     private final LadderRungGenerator ladderRungGenerator = new ConnectedLadderRungGenerator();
@@ -22,5 +24,31 @@ public class LadderTest {
 
         // then
         assertThat(ladder.getRows()).hasSize(height.getValue());
+    }
+
+    @Test
+    void 사다리를_탄_후_결과를_반환한다() {
+        /*
+        프린  땡이  포비  토미  네오
+        |-----|     |-----|     |
+        |     |-----|     |     |
+        |-----|     |     |-----|
+        */
+
+        // given
+        Height height = new Height(3);
+        Players players = 참가자들("프린", "땡이", "포비", "토미", "네오");
+        List<LadderRung> ladderRungs = List.of(
+                LadderRung.CONNECTED, LadderRung.DISCONNECTED, LadderRung.CONNECTED, LadderRung.DISCONNECTED,
+                LadderRung.DISCONNECTED, LadderRung.CONNECTED, LadderRung.DISCONNECTED, LadderRung.DISCONNECTED,
+                LadderRung.CONNECTED, LadderRung.DISCONNECTED, LadderRung.DISCONNECTED, LadderRung.CONNECTED
+        );
+
+        // when
+        Ladder ladder = Ladder.create(height, players, new FixedLadderRungGenerator(ladderRungs));
+        List<String> resultNames = ladder.climb(players);
+
+        // then
+        assertThat(resultNames).containsExactly("토미", "땡이", "프린", "네오", "포비");
     }
 }
