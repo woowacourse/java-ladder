40469251facf279fd94bad66968afb13698a5ce0
Choo <choo000407@naver.com>
2024-02-27 13:47:43 +0900

    test: 참여자들에 대한 사다리 실행 결과를 얻는다

--- src/test/java/model/ladder/LadderTest.java
+++ src/test/java/model/ladder/LadderTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 import model.bridge.Bridge;
+import model.ladder.dto.LadderPlayOutcome;
 import model.player.Players;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,5 +24,22 @@ public class LadderTest {
         return codes.stream()
                 .map(code -> Bridge.findBridgeByCode(code).orElse(null))
                 .toList();
+    }
+
+    @DisplayName("모든 참여자에 대한 사다리 실행 결과를 얻는다")
+    @Test
+    void testLadderPlayOutcomeIncludesAllParticipants() {
+        List<String> beforePlayerNames = List.of("pobi", "lala");
+        Players players = Players.from(beforePlayerNames);
+        LadderHeight ladderHeight = new LadderHeight(5);
+        Ladder ladder = Ladder.of(ladderHeight, players, (count) -> createBridges(List.of(1, 0)));
+        List<LadderPlayOutcome> ladderPlayOutcomes = ladder.play();
+
+        List<String> afterPlayerNames = ladderPlayOutcomes.stream()
+                .map(LadderPlayOutcome::getName)
+                .toList();
+
+        assertThat(afterPlayerNames)
+                .containsAll(beforePlayerNames);
     }
 }
