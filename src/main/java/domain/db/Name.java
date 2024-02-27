7e0af1d24c3786de71fedb5035639965f82c1b3d
3juhwan <13selfesteem91@naver.com>
2024-02-27 16:49:26 +0900

    feat: Name에 공백 제거 로직 추가

--- src/main/java/domain/db/Name.java
+++ src/main/java/domain/db/Name.java
@@ -8,13 +8,20 @@ public record Name(String name) {
     private static final Pattern NAME_REGEX_PATTERN = Pattern.compile("^[A-Za-z0-9]+");
 
     public Name(final String name) {
-        this.name = name;
+        this.name = removeBlank(name);
         validateNameLength();
         validateAlphanumericName();
     }
 
+    private String removeBlank(final String name) {
+        return name.replace(" ", "");
+    }
+
     private void validateNameLength() {
-        String errorMessage = String.format("이름의 길이는 %d ~ %d 자 이어야 합니다.", NAME_LENGTH_MIN, NAME_LENGTH_MAX);
+        final String errorMessage = String.format("이름의 길이는 %d ~ %d 자 이어야 합니다.", NAME_LENGTH_MIN, NAME_LENGTH_MAX);
+        if (this.name == null) {
+            throw new IllegalArgumentException(errorMessage);
+        }
         if (NAME_LENGTH_MIN > this.name.length() || this.name.length() > NAME_LENGTH_MAX) {
             throw new IllegalArgumentException(errorMessage);
         }
--- src/test/java/domain/db/NameTest.java
+++ src/test/java/domain/db/NameTest.java
@@ -1,44 +1,34 @@
 package domain.db;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class NameTest {
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(strings = {"a", "abcde", "wq21 3", "1 2 3 4 5 "})
     @DisplayName("정상적인 이름은 예외를 발생하지 않는다")
-    void normal_name_doesnt_throw_exception() {
-        final String nameA = "A";
-        final Name name = new Name(nameA);
-
-        assertThat(name).isEqualTo(new Name(nameA));
+    void normal_name_doesnt_throw_exception(String name) {
+        assertThatCode(() -> new Name(name)).doesNotThrowAnyException();
     }
 
-    @Test
-    @DisplayName("이름의 길이가 1 보다 작다면 예외가 발생한다")
-    void name_length_less_than_one_throw_exception() {
-        assertThatThrownBy(() -> new Name(""))
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "acsbdd", "ds sd d d"})
+    @DisplayName("공백을 제거하고 길이 정책에 어긋나면 예외가 발생한다")
+    void throw_exception_if_conflict_with_length_policy_after_remove_blank(String name) {
+        assertThatThrownBy(() -> new Name(name))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("이름의 길이는 1 ~ 5 자 이어야 합니다.");
-    }
-
-    @Test
-    @DisplayName("이름의 길이가 5 보다 크다면 예외가 발생한다")
-    void name_length_more_than_five_throw_exception() {
-        assertThatThrownBy(() -> new Name("123456"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("이름의 길이는 1 ~ 5 자 이어야 합니다.");
+                .hasMessageContainingAll("이름의 길이는", "자 이어야 합니다.");
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"망크", "+"})
     @DisplayName("이름이 영어와 숫자가 아니라면 예외가 발생한다")
-    void name_not_alphanumeric_throw_exception(final String name) {
+    void name_not_alphanumeric_throw_exception(String name) {
         assertThatThrownBy(() -> new Name(name))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("이름은 영어와 숫자만 허용합니다.");
