f07fa8e6942e9d5a2a777107b33c78415caf7180
lemone <lejinhy@gmail.com>
2024-02-27 13:22:47 +0900

    feat: 실행 결과 입력 추가

--- src/main/java/laddergame/view/InputView.java
+++ src/main/java/laddergame/view/InputView.java
@@ -5,20 +5,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static laddergame.view.OutputView.LINE_SEPARATOR;
 
 public class InputView {
     private static final String IOEXCEPTION_ERROR = "입력 과정 도중 에러가 발생했습니다.";
-    private static final String NAME_DELIMITER = ",";
+    private static final String ELEMENTS_DELIMITER = ",";
 
     public List<String> readPlayersName() {
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         try {
-            return Arrays.stream(bufferedReader.readLine().split(NAME_DELIMITER))
-                    .map(String::trim)
-                    .toList();
+            return splitToSeparator(bufferedReader.readLine());
         } catch (IOException exception) {
             throw new IllegalArgumentException(IOEXCEPTION_ERROR);
         }
@@ -32,5 +31,21 @@ public class InputView {
         } catch (IOException exception) {
             throw new IllegalArgumentException(IOEXCEPTION_ERROR);
         }
+    }
+
+    public List<String> readLadderResult() {
+        System.out.println(LINE_SEPARATOR + "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            return splitToSeparator(bufferedReader.readLine());
+        } catch (IOException exception) {
+            throw new IllegalArgumentException(IOEXCEPTION_ERROR);
+        }
+    }
+
+    private List<String> splitToSeparator(String input) {
+        return Arrays.stream(input.split(ELEMENTS_DELIMITER))
+                .map(String::trim)
+                .toList();
     }
 }
