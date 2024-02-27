abf29e451e0ca2bce5a8c97c1c5a5f08a90fac0f
hyun <osh123chm@gmail.com>
2024-02-27 14:45:41 +0900

    refactor: step 출력 로직 view로 이동

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -9,6 +9,9 @@ public class OutputView {
     private static final String NAME_FORMAT = "%5s";
     private static final String LADDER_BASE = "|";
     private static final String LEFT_LADDER_BASE = "   |";
+    private static final String STEP_BASE = "-";
+    private static final String EMPTY_STEP_BASE = " ";
+    private static final int STEP_LENGTH = 5;
 
     public void printErrorMessage(String message) {
         System.out.println("[ERROR] " + message);
@@ -27,9 +30,16 @@ public class OutputView {
 
     private void printEachLayer(LayerSteps layerSteps) {
         List<String> steps = layerSteps.steps().stream()
-                .map(Step::getOutput)
+                .map(this::printEachStep)
                 .toList();
         String joinedSteps = String.join(LADDER_BASE, steps);
         System.out.println(LEFT_LADDER_BASE + joinedSteps + LADDER_BASE);
+    }
+
+    private String printEachStep(Step step) {
+        if (step.equals(Step.EXIST)) {
+            return STEP_BASE.repeat(STEP_LENGTH);
+        }
+        return EMPTY_STEP_BASE.repeat(STEP_LENGTH);
     }
 }
