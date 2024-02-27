c0241722d1065df2db22b156ee3af9e0171a0de2
coli-geonwoo <kkwoo001021@naver.com>
2024-02-27 21:15:22 +0900

    style(view): static import를 통한 가독성 강화

--- src/main/java/laddergame/view/ResultView.java
+++ src/main/java/laddergame/view/ResultView.java
@@ -5,6 +5,8 @@ import laddergame.domain.gameelements.Element;
 import laddergame.domain.gameelements.Elements;
 import laddergame.domain.ladder.Ladder;
 
+import static laddergame.view.MessageResolver.*;
+
 public class ResultView {
     private static final String LINE_SEPERATOR = System.lineSeparator();
     private static final String CALL_ALL_PLAYER_RESULTS_MESSAGE = "all";
@@ -12,21 +14,22 @@ public class ResultView {
     private ResultView() {
     }
 
-    public static void printLadder(Elements people, Ladder ladder, Elements results) {
-        System.out.println(LINE_SEPERATOR + "실행 결과" + LINE_SEPERATOR);
-        System.out.println(MessageResolver.resolveElementMessage(people));
-        System.out.println(MessageResolver.resolveLadderMessage(ladder));
-        System.out.println(MessageResolver.resolveElementMessage(results));
+    public static void printLadder(Elements upperElements, Ladder ladder, Elements lowerElements) {
+        System.out.println(LINE_SEPERATOR + "사다리 결과" + LINE_SEPERATOR);
+        System.out.println(resolveElementMessage(upperElements));
+        System.out.println(resolveLadderMessage(ladder));
+        System.out.println(resolveElementMessage(lowerElements));
     }
 
     public static void printPlayerResult(String playerName, LadderGame ladderGame) {
+        System.out.println(LINE_SEPERATOR + "실행 결과" + LINE_SEPERATOR);
+
         if (playerName.equals(CALL_ALL_PLAYER_RESULTS_MESSAGE)) {
-            System.out.println(MessageResolver
-                    .resolveAllPlayerResultMessage(ladderGame.getPlayerGameResult()));
+            System.out.println(resolveAllPlayerResultMessage(ladderGame.getPlayerGameResult()));
             return;
         }
-        System.out.println(MessageResolver
-                .resolvePlayerResultMessage(ladderGame.findPlayerResult(new Element(playerName))));
+
+        System.out.println(resolvePlayerResultMessage(ladderGame.findPlayerResult(new Element(playerName))));
     }
 
 }
