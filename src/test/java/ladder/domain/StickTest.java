0373735230b0396832ba99021f27b2384746b5a0
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2024-02-27 09:48:55 +0900

    feat (Stick) : 막대 존재 유무에 따라 플레이어의 위치를 바꾸는 기능 구현

--- src/test/java/ladder/domain/StickTest.java
+++ src/test/java/ladder/domain/StickTest.java
@@ -1,22 +1,48 @@
 package ladder.domain;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import ladder.domain.player.Player;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class StickTest {
 
+    static final Player LEFT = new Player("pobi");
+    static final Player RIGHT = new Player("jk");
+
+    @DisplayName("막대가 해당 자리에 있을 경우, 두 플레이어 위치를 바꾼다")
+    @Test
+    void progressSwitchingTest_WhenIsExist() {
+        Stick stick = Stick.EXISTENCE;
+
+        List<Player> actual = stick.progressSwitching(LEFT, RIGHT);
+
+        assertThat(actual).containsExactly(RIGHT, LEFT);
+    }
+
+    @DisplayName("막대가 해당 자리에 있을 경우, 두 플레이어 위치가 그대로이다")
+    @Test
+    void progressSwitchingTest_WhenIsNotExist() {
+        Stick stick = Stick.NON_EXISTENCE;
+
+        List<Player> actual = stick.progressSwitching(LEFT, RIGHT);
+
+        assertThat(actual).containsExactly(LEFT, RIGHT);
+    }
+
     @DisplayName("막대가 해당자리에 있는지 테스트")
     @Test
     void isExistTest() {
 
-        Assertions.assertThat(Stick.EXISTENCE.isExist()).isTrue();
+        assertThat(Stick.EXISTENCE.isExist()).isTrue();
     }
 
     @DisplayName("막대가 해당자리에 없는지 테스트")
     @Test
     void isNonExistTest() {
 
-        Assertions.assertThat(Stick.NON_EXISTENCE.isExist()).isFalse();
+        assertThat(Stick.NON_EXISTENCE.isExist()).isFalse();
     }
 }
