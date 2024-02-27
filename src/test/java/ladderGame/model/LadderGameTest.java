1b4bcc1f82f7088b2249595028c3331c7efc440f
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-02-27 21:36:40 +0900

    refactor(LadderGameTest) : 중복 실행되는 코드 @BeforeEach로 구분 및 주석으로 사다리 표현

--- src/test/java/ladderGame/model/LadderGameTest.java
+++ src/test/java/ladderGame/model/LadderGameTest.java
@@ -1,5 +1,6 @@
 package ladderGame.model;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -15,17 +16,30 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LadderGameTest {
 
-    @Test
-    @DisplayName("참여자 이름을 통해 사다리 결과를 알려준다.")
-    void findLadderGameResult() {
-        List<Line> lines = Stream.generate(() -> new Line(List.of(ConnectionStatus.CONNECTION, ConnectionStatus.DISCONNECTION, ConnectionStatus.CONNECTION)))
+    private LadderGame ladderGame;
+    @BeforeEach
+    void setUp() {
+        List<Line> lines = Stream.generate(() -> new Line(List.of(ConnectionStatus.CONNECTION, ConnectionStatus.DISCONNECTION)))
                 .limit(4)
                 .toList();
 
         Ladder ladder = new Ladder(new ArrayList<>(lines));
 
-        LadderGame ladderGame = new LadderGame(new Players(List.of("포비", "왼손", "준")), new LadderResults(List.of("꽝", "5000", "3000")), ladder);
+        ladderGame = new LadderGame(new Players(List.of("포비", "왼손", "준")), new LadderResults(List.of("꽝", "5000", "3000")), ladder);
 
+        /*
+        포비   왼손     준
+           |-----|     |
+           |-----|     |
+           |-----|     |
+           |-----|     |
+          꽝  5000  3000
+         */
+    }
+
+    @Test
+    @DisplayName("참여자 이름을 통해 사다리 결과를 알려준다.")
+    void findLadderGameResult() {
         assertAll(
                 () -> assertEquals(ladderGame.findLadderGameResult("포비"), new LadderResult("꽝")),
                 () -> assertEquals(ladderGame.findLadderGameResult("왼손"), new LadderResult("5000")),
@@ -36,14 +50,6 @@ class LadderGameTest {
     @Test
     @DisplayName("모든 참여자의 결과를 알려준다.")
     void findAllLadderGameResults() {
-        List<Line> lines = Stream.generate(() -> new Line(List.of(ConnectionStatus.CONNECTION, ConnectionStatus.DISCONNECTION, ConnectionStatus.CONNECTION)))
-                .limit(4)
-                .toList();
-
-        Ladder ladder = new Ladder(new ArrayList<>(lines));
-
-        LadderGame ladderGame = new LadderGame(new Players(List.of("포비", "왼손", "준")), new LadderResults(List.of("꽝", "5000", "3000")), ladder);
-
         Map<Player, LadderResult> results = ladderGame.findAllLadderGameResults();
         List<String> printResult = List.of("포비 : 꽝", "왼손 : 5000", "준 : 3000");
 
@@ -56,7 +62,7 @@ class LadderGameTest {
     @Test
     @DisplayName("사다리 결과 수가 참여자 수와 동일하지 않을 시 예외가 발생합니다.")
     void validateCounts() {
-        List<Line> lines = Stream.generate(() -> new Line(List.of(ConnectionStatus.CONNECTION, ConnectionStatus.DISCONNECTION, ConnectionStatus.CONNECTION)))
+        List<Line> lines = Stream.generate(() -> new Line(List.of(ConnectionStatus.CONNECTION, ConnectionStatus.DISCONNECTION)))
                 .limit(4)
                 .toList();
 
