1ac34a985444e31844224cb0c444879526fdfd62
Zeus6768 <akaps6768@gmail.com>
2024-02-27 20:06:20 +0900

    feat: 사다리 결과 보상 생성 및 검증

---
+++ src/main/java/ladder/domain/LadderResult.java
@@ -0,0 +1,17 @@
+package ladder.domain;
+
+public record LadderResult(String value) {
+
+    private static final int MIN_LENGTH = 1;
+    private static final int MAX_LENGTH = 5;
+
+    public LadderResult {
+        if (value.isBlank() || value.length() > MAX_LENGTH) {
+            throw new IllegalArgumentException("결과 이름은 %d~%d글자로 입력해주세요: %s".formatted(
+                    MIN_LENGTH,
+                    MAX_LENGTH,
+                    value
+            ));
+        }
+    }
+}
---
+++ src/test/java/ladder/domain/LadderResultTest.java
@@ -0,0 +1,19 @@
+package ladder.domain;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class LadderResultTest {
+
+    @DisplayName("입력한 결과가 1-5자 범위를 벗어날 경우 예외를 발생한다.")
+    @ValueSource(strings = {"", "우아한테크코스"})
+    @ParameterizedTest
+    void constructException(String name) {
+        assertThatCode(() -> new LadderResult(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("결과는 1~5글자로 입력해주세요");
+    }
+}
