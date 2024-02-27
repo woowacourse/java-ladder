f651eaffa5a90e645b34c1b5b625b09dae2247a6
seokmyungham <hellotjraud@gmail.com>
2024-02-27 13:16:10 +0900

    feat(Prizes): 상품의 개수를 검증하는  validatePrizesCount() 추가

--- src/main/java/ladder/domain/Prizes.java
+++ src/main/java/ladder/domain/Prizes.java
@@ -2,5 +2,21 @@ package ladder.domain;
 
 import java.util.List;
 
-public record Prizes(List<String> name) {
+public class Prizes {
+    private final List<String> name;
+
+    public Prizes(List<String> name, int personCount) {
+        validatePrizesCount(name, personCount);
+        this.name = name;
+    }
+
+    private void validatePrizesCount(List<String> name, int personCount) {
+        if (name.size() != personCount) {
+            throw new IllegalArgumentException("상품의 개수는 참여자 인원수와 일치해야 합니다.");
+        }
+    }
+
+    public List<String> name() {
+        return name;
+    }
 }
--- src/test/java/ladder/domain/PrizesTest.java
+++ src/test/java/ladder/domain/PrizesTest.java
@@ -2,6 +2,7 @@ package ladder.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,6 +11,15 @@ public class PrizesTest {
     @DisplayName("상품 생성 테스트")
     @Test
     void createPrizeTest() {
-        Prizes prizes = new Prizes(new ArrayList<>(List.of("꽝", "3000", "꽝", "5000")));
+        Prizes prizes = new Prizes(new ArrayList<>(List.of("꽝", "3000", "꽝", "5000")), 4);
+    }
+
+    @DisplayName("상품의 개수가 참여자 인원수와 일치하지 않을 경우 예외를 발생한다.")
+    @Test
+    void validatePrizesCountTest() {
+        Assertions.assertThatThrownBy(() -> new Prizes(new ArrayList<>(List.of("꽝", "30000", "3000")), 5))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("상품의 개수는 참여자 인원수와 일치해야 합니다.");
+
     }
 }
