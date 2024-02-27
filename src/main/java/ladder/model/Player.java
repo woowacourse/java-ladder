74ea4497e1c6f61d1bc2059bbc2e3716b9055277
BurningFalls <sherlocky31415@gmail.com>
2024-02-27 09:38:04 +0900

    refactor: 메서드 유연성을 위해 에러 메시지 수정

--- src/main/java/ladder/model/Player.java
+++ src/main/java/ladder/model/Player.java
@@ -3,6 +3,7 @@ package ladder.model;
 import java.util.Objects;
 
 public class Player {
+    private static final int MIN_NAME_LENGTH = 1;
     private static final int MAX_NAME_LENGTH = 5;
     private static final String NAME_PATTERN = "^[a-zA-Z0-9]*$";
 
@@ -15,7 +16,8 @@ public class Player {
 
     private void validate(String name) {
         if (isNameLengthNotValid(name)) {
-            throw new IllegalArgumentException("이름의 길이는 1이상 5이하의 숫자여야 합니다.");
+            throw new IllegalArgumentException("이름의 길이는 " + MIN_NAME_LENGTH +
+                    "이상 " + MAX_NAME_LENGTH + "이하의 숫자여야 합니다.");
         }
         if (isNameFormatNotValid(name)) {
             throw new IllegalArgumentException("이름은 영문자와 숫자로 구성되어야 합니다.");
@@ -23,7 +25,7 @@ public class Player {
     }
 
     private boolean isNameLengthNotValid(String name) {
-        return name.isEmpty() || name.length() > MAX_NAME_LENGTH;
+        return (name.length() < MIN_NAME_LENGTH) || (name.length() > MAX_NAME_LENGTH);
     }
 
     private boolean isNameFormatNotValid(String name) {
