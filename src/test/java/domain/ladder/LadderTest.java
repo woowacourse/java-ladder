bf97fd970149ba71fc1a4cd1da0e227b1e140507
Nam Gi Beom <rlqja0523@naver.com>
2024-02-27 16:58:29 +0900

    test: 참가자 수와 상품 수의 개수가 같은지 테스트

--- src/test/java/domain/ladder/LadderTest.java
+++ src/test/java/domain/ladder/LadderTest.java
@@ -3,6 +3,7 @@ package domain.ladder;
 import static fixture.PlayersFixture.참가자들;
 import static fixture.PrizesFixture.상품들;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import domain.height.Height;
 import domain.player.Players;
@@ -13,8 +14,6 @@ import support.ConnectedLadderRungGenerator;
 import support.FixedLadderRungGenerator;
 
 public class LadderTest {
-    private final LadderRungGenerator ladderRungGenerator = new ConnectedLadderRungGenerator();
-
     @Test
     void 주어진_높이에_맞게_사다리를_생성한다() {
         // given
@@ -25,6 +24,28 @@ public class LadderTest {
 
         // then
         assertThat(ladder.getRows()).hasSize(height.getValue());
+    }
+
+    @Test
+    void 참가자_수와_상품_수가_일치하지_않으면_예외가_발생한다() {
+        // given
+        Players players = 참가자들(3);
+        Prizes prizes = 상품들(4);
+
+        // when & then
+        assertThatThrownBy(() -> Ladder.create(new Height(1), players, prizes, new ConnectedLadderRungGenerator()))
+                .isExactlyInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 참가자_수와_상품_수가_일치하면_예외가_발생하지_않는다() {
+        // given
+        Players players = 참가자들(3);
+        Prizes prizes = 상품들(3);
+
+        // when & then
+        assertThatThrownBy(() -> Ladder.create(new Height(1), players, prizes, new ConnectedLadderRungGenerator()))
+                .doesNotThrowAnyException();
     }
 
     @Test
