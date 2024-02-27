fbd0d21fa7cf21a28713221ab4640a1047fc2627
J-I-H-O <jeongjiho0731@gmail.com>
2024-02-27 19:21:10 +0900

    feat: 이름으로 all을 입력할 수 없도록하는 검증 기능 구현

--- src/main/java/domain/player/Name.java
+++ src/main/java/domain/player/Name.java
@@ -3,6 +3,8 @@ package domain.player;
 public class Name {
     private static final int MINIMUM_NAME_LENGTH = 1;
     private static final int MAXIMUM_NAME_LENGTH = 5;
+    private static final String RESERVED_KEYWORD = "all";
+    public static final String RESERVED_EXCEPTION_MESSAGE = "[ERROR] 이름으로 " + RESERVED_KEYWORD + "을 입력할 수 없습니다.";
     private static final String LENGTH_EXCEPTION_MESSAGE = "[ERROR] 이름의 길이는 " + MINIMUM_NAME_LENGTH +
             "보다 크거나, " + MAXIMUM_NAME_LENGTH + "보다 작아야합니다.";
 
@@ -10,12 +12,19 @@ public class Name {
 
     public Name(String name) {
         validateLength(name);
+        validateReserved(name);
         this.name = name;
     }
 
     private void validateLength(final String name) {
         if (name.length() < MINIMUM_NAME_LENGTH || name.length() > MAXIMUM_NAME_LENGTH) {
             throw new IllegalArgumentException(LENGTH_EXCEPTION_MESSAGE);
+        }
+    }
+
+    private void validateReserved(final String name) {
+        if (name.equals(RESERVED_KEYWORD)) {
+            throw new IllegalArgumentException(RESERVED_EXCEPTION_MESSAGE);
         }
     }
 
--- src/test/java/domain/player/NameTest.java
+++ src/test/java/domain/player/NameTest.java
@@ -2,6 +2,7 @@ package domain.player;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -10,6 +11,13 @@ public class NameTest {
     @ValueSource(strings = {"123456", ""})
     void 이름의_길이가_1보다_작거나_5보다_크면_예외가_발생한다(String name) {
         assertThatThrownBy(() -> new Name(name))
+                .isExactlyInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 이름으로_all을_입력하면_예외가_발생한다() {
+        String invalidName = "all";
+        assertThatThrownBy(() -> new Name(invalidName))
                 .isExactlyInstanceOf(IllegalArgumentException.class);
     }
 }
