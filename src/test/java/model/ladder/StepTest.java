76c17df673195d338f4096825d34ffd1efa2227e
MingyeomKim <mingyum119@naver.com>
2024-02-27 21:31:47 +0900

    test(StepTest): 이전의 다리 상태에 따라 현재 다리의 연결을 결정하는 테스트

---
+++ src/test/java/model/ladder/StepTest.java
@@ -0,0 +1,99 @@
+package model.ladder;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class StepTest {
+
+    @DisplayName("이전에 생성된 다리와 겹쳐서 현재 위치의 다리가 연결되지 않는다.")
+    @Test
+    void previousOverlapTest() {
+        // given
+        Step previous = Step.from(StepStatus.CONNECTED);
+        Step expected = Step.from(StepStatus.DISCONNECTED);
+
+        // when
+        Step step = Step.of(previous, new RandomStatusGenerator());
+
+        // then
+        Assertions.assertTrue(step.equals(expected));
+    }
+
+    @Nested
+    @DisplayName("이전에 생성된 다리와 겹치지 않는 경우 테스트")
+    class PreviousDoesNotOverlapTest {
+        @DisplayName("현재 최초로 생성되는 다리이며 현재 위치의 다리를 연결한다.")
+        @Test
+        void firstConnectStepTest() {
+            // given
+            Step previous = Step.from(StepStatus.EMPTY);
+            Step expected = Step.from(StepStatus.CONNECTED);
+
+            // when
+            Step step = Step.of(previous, new AlwaysConnectStatusGenerator());
+
+            // then
+            Assertions.assertTrue(step.equals(expected));
+        }
+
+        @DisplayName("이전에 다리가 생성되지 않으며 현재 위치의 다리를 연결한다.")
+        @Test
+        void connectStepTest() {
+            // given
+            Step previous = Step.from(StepStatus.DISCONNECTED);
+            Step expected = Step.from(StepStatus.CONNECTED);
+
+            // when
+            Step step = Step.of(previous, new AlwaysConnectStatusGenerator());
+
+            // then
+            Assertions.assertTrue(step.equals(expected));
+        }
+
+        @DisplayName("현재 최초로 생성되는 다리이며 현재 위치의 다리를 연결하지 않는다.")
+        @Test
+        void firstDisconnectStepTest() {
+            // given
+            Step previous = Step.from(StepStatus.EMPTY);
+            Step expected = Step.from(StepStatus.DISCONNECTED);
+
+            // when
+            Step step = Step.of(previous, new AlwaysDisconnectStatusGenerator());
+
+            // then
+            Assertions.assertTrue(step.equals(expected));
+        }
+
+        @DisplayName("이전에 다리가 생성되지 않으며 현재 위치의 다리를 연결하지 않는다.")
+        @Test
+        void disconnectStepTest() {
+            // given
+            Step previous = Step.from(StepStatus.DISCONNECTED);
+            Step expected = Step.from(StepStatus.DISCONNECTED);
+
+            // when
+            Step step = Step.of(previous, new AlwaysDisconnectStatusGenerator());
+
+            // then
+            Assertions.assertTrue(step.equals(expected));
+        }
+    }
+
+    static class AlwaysConnectStatusGenerator implements StepStatusGenerator {
+
+        @Override
+        public StepStatus generate() {
+            return StepStatus.CONNECTED;
+        }
+    }
+
+    static class AlwaysDisconnectStatusGenerator implements StepStatusGenerator {
+
+        @Override
+        public StepStatus generate() {
+            return StepStatus.DISCONNECTED;
+        }
+    }
+}
