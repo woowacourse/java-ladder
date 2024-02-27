31d445cb6e91513e7be775d34c023aeae7a6bf44
coli-geonwoo <kkwoo001021@naver.com>
2024-02-27 15:36:21 +0900

    feat: 플레이어 이름 입력기능 추가

--- src/main/java/laddergame/view/InputView.java
+++ src/main/java/laddergame/view/InputView.java
@@ -13,7 +13,7 @@ public class InputView {
     public static List<String> readNames() {
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
         return List.of(scanner.nextLine()
-                        .split(DELIMITER));
+                .split(DELIMITER));
     }
 
     public static List<String> readGameResult() {
@@ -22,11 +22,16 @@ public class InputView {
                 .split(DELIMITER));
     }
 
+    public static String readPlayerName() {
+        System.out.println("결과를 보고 싶은 사람은?");
+        return scanner.nextLine();
+    }
+
     public static int readHeight() {
         System.out.println("최대 사다리 높이는 몇 개인가요?");
         try {
             return Integer.parseInt(scanner.nextLine());
-        }catch(NumberFormatException e){
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException("사다리의 높이는 1이상 100이하의 자연수여야 합니다");
         }
     }
