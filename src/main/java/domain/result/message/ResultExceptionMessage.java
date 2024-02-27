a8ff7329d7928ea0b61e675a93a3fde8c2186da2
Hyeon0208 <dmsgktn0208@gmail.com>
2024-02-27 16:41:17 +0900

    refactor : 실행 결과 입력에 대한 예외 메시지를 관리하는 클래스 변경

--- src/main/java/domain/result/message/ResultExceptionMessage.java
+++ src/main/java/domain/result/message/ResultExceptionMessage.java
@@ -7,7 +7,6 @@ public class ResultExceptionMessage {
 
     public static final String LADDER_RESULT_LENGTH = String.format("실행 결과는 %d ~ %d글자 사이로 입력해야 합니다",
             LadderResult.MIN_RESULT_LENGTH, LadderResult.MAX_RESULT_LENGTH);
-    public static final String LADDER_RESULT_INPUT_FORMAT = String.format("실행 결과는 %s로 구분하여 입력해야합니다",
-            InputView.INPUT_DELIMITER);
     public static final String LADDER_RESULT_BLANK = "실행 결과에 공백을 사용할 수 없습니다";
+    public static final String TOTAL_RESULTS_SIZE = "참가자 수와 동일하게 실행 결과를 입력해야 합니다.";
 }
