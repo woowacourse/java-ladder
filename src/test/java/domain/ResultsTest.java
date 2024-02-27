c38f3feb338bafbdff9a4ab43de99a73e78f6b2d
SCY <helenason@naver.com>
2024-02-27 15:35:25 +0900

    test: 결과들 도메인 관련 테스트 코드 작성

---
+++ src/test/java/domain/ResultsTest.java
@@ -0,0 +1,34 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ResultsTest {
+
+    @Test
+    @DisplayName("결과 객체 생성 성공: 성공적으로 도메인이 생성된다.")
+    void test_ok_createObject() {
+        assertThatCode(() -> new Results(List.of(
+                new Result("1"),
+                new Result("2")),
+                2))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("결과 객체 생성 실패: 참여자 수와 개수가 같지 않으면 에러를 반환한다.")
+    void test_exception_NotEqualCount() {
+        Members members = Members.from("a,b,c,d");
+
+        assertThatCode(() -> new Results(List.of(
+                new Result("1"),
+                new Result("2"),
+                new Result("3")),
+                members.getCount()))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("결과의 수는 참여자의 수와 같아야 합니다.");
+    }
+}
