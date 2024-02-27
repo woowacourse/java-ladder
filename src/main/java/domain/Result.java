bbe67939ff944eda8010f8933c19760b6e6e8c2c
nayonsoso <panda0329@naver.com>
2024-02-27 18:36:21 +0900

    feat: 실행 결과의 value가 1~5자 사이가 아니면 예외 발생 기능 구현

--- src/main/java/domain/Result.java
+++ src/main/java/domain/Result.java
@@ -5,7 +5,14 @@ public class Result {
     private final String value;
 
     public Result(String value) {
+        validateLength(value);
         this.value = value;
+    }
+
+    private void validateLength(String value) {
+        if (value.length() < 1 || 5 < value.length()) {
+            throw new IllegalArgumentException("실행 결과는 1~5자 사이여야 합니다.");
+        }
     }
 
     public String getValue() {
