735b6330e3add04c05d90c005d58e6aa4fe3366a
kelly6bf <kelly6bf@gmail.com>
2024-02-27 16:02:41 +0900

    refactor: LadderGameMachine 수정 - 게임 결과를 입력받는 메서드를 추가함. - 새롭게 생성된 DTO를 OutputView에 전달하도록 로직 수정 - InputView의 로직 변경사항을 적용

--- src/main/java/ladder/controller/LadderGameMachine.java
+++ src/main/java/ladder/controller/LadderGameMachine.java
@@ -1,9 +1,10 @@
 package ladder.controller;
 
+import ladder.domain.GameResults;
+import ladder.domain.Ladder;
 import ladder.domain.LadderHeight;
-import ladder.domain.Lines;
-import ladder.domain.UserNames;
-import ladder.dto.LadderResult;
+import ladder.domain.PlayerNames;
+import ladder.dto.LadderStatus;
 import ladder.util.ConsoleReader;
 import ladder.util.RandomBooleanGenerator;
 import ladder.view.InputView;
@@ -16,22 +17,34 @@ public class LadderGameMachine {
     private static final ConsoleReader CONSOLE = new ConsoleReader();
 
     public void run() {
-        UserNames userNames = initNames();
+        PlayerNames playerNames = initNames();
+        GameResults gameResults = initGameResults();
         LadderHeight ladderHeight = initLadderHeight();
-        LadderResult ladderResult = createLadderResult(
+        Ladder ladder = initLadder(
                 new RandomBooleanGenerator(),
-                ladderHeight.ladderHeight(),
-                userNames);
-        OutputView.printLadderResult(ladderResult);
+                ladderHeight,
+                playerNames
+        );
+        OutputView.printLadderResult(LadderStatus.of(playerNames, ladder));
     }
 
-    private UserNames initNames() {
+    private PlayerNames initNames() {
         try {
-            List<String> input = InputView.readNames(CONSOLE);
-            return UserNames.of(input);
+            List<String> input = InputView.readStringsWithDelimiter(CONSOLE, "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
+            return PlayerNames.of(input);
         } catch (IllegalArgumentException e) {
             OutputView.printErrorMessage(e.getMessage());
             return initNames();
+        }
+    }
+
+    private GameResults initGameResults() {
+        try {
+            List<String> input = InputView.readStringsWithDelimiter(CONSOLE, "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
+            return GameResults.of(input);
+        } catch (IllegalArgumentException e) {
+            OutputView.printErrorMessage(e.getMessage());
+            return initGameResults();
         }
     }
 
@@ -45,8 +58,7 @@ public class LadderGameMachine {
         }
     }
 
-    private LadderResult createLadderResult(final Supplier<Boolean> generator, final int ladderHeight, final UserNames userNames) {
-        Lines lines = Lines.of(generator, ladderHeight, userNames.getUserCount());
-        return LadderResult.of(userNames, lines);
+    private Ladder initLadder(final Supplier<Boolean> generator, final LadderHeight ladderHeight, final PlayerNames playerNames) {
+        return Ladder.of(generator, ladderHeight, playerNames.getPlayerCount());
     }
 }
