4911b06288fbc82b5157b2948c8b196c754d250a
SCY <helenason@naver.com>
2024-02-27 17:24:56 +0900

    feat: 결과 및 타겟 입력받기

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -9,8 +9,18 @@ public class InputView {
         return read();
     }
 
+    public String readResults() {
+        System.out.println("실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
+        return read();
+    }
+
     public String readHeight() {
         System.out.println("최대 사다리 높이는 몇 개인가요?");
+        return read();
+    }
+
+    public String readTargets() {
+        System.out.println("결과를 보고 싶은 사람은?");
         return read();
     }
 
