8be58b778373e97883748a055fa3b258e67ab3b5
slimsha2dy <chy2581@naver.com>
2024-02-27 14:21:17 +0900

    refactor: depth, MAX_DEPTH의 이름을 tryCount, MAX_TRYCOUNT로 수정

--- src/main/java/controller/LadderController.java
+++ src/main/java/controller/LadderController.java
@@ -8,7 +8,7 @@ import view.InputView;
 import view.OutputView;
 
 public class LadderController {
-    private static final int MAX_DEPTH = 5;
+    private static final int MAX_TRYCOUNT = 5;
     private final ConsoleReader consoleReader;
 
     public LadderController() {
@@ -16,31 +16,31 @@ public class LadderController {
     }
 
     public void run() {
-        Players players = nameInput(MAX_DEPTH);
-        Height height = heightInput(MAX_DEPTH);
+        Players players = nameInput(MAX_TRYCOUNT);
+        Height height = heightInput(MAX_TRYCOUNT);
         LadderGame ladderGame = new LadderGame(players, height);
         OutputView.printResult(ladderGame.getResult());
     }
 
-    private Players nameInput(int depth) {
-        if (depth == 0) {
+    private Players nameInput(int tryCount) {
+        if (tryCount == 0) {
             throw new IllegalArgumentException("입력 허용횟수 5회를 초과했습니다.");
         }
         try {
             return new Players(InputView.readNames(consoleReader));
         } catch (IllegalArgumentException e) {
-            return nameInput(depth - 1);
+            return nameInput(tryCount - 1);
         }
     }
 
-    private Height heightInput(int depth) {
-        if (depth == 0) {
+    private Height heightInput(int tryCount) {
+        if (tryCount == 0) {
             throw new IllegalArgumentException("입력 허용횟수 5회를 초과했습니다.");
         }
         try {
             return InputView.readHeight(consoleReader);
         } catch (IllegalArgumentException e) {
-            return heightInput(depth - 1);
+            return heightInput(tryCount - 1);
         }
     }
 }
