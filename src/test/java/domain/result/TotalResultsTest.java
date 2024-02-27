96bb8940e3424fb49ce5635be3ed2bdca7b2aeed
Hyeon0208 <dmsgktn0208@gmail.com>
2024-02-27 16:43:44 +0900

    feat : 총 실행 결과 객체 생성에 대한 성공 테스트 추가

--- src/test/java/domain/result/TotalResultsTest.java
+++ src/test/java/domain/result/TotalResultsTest.java
@@ -1,5 +1,6 @@
 package domain.result;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import domain.player.PlayerName;
@@ -7,33 +8,43 @@ import domain.player.PlayerNames;
 import domain.result.message.ResultExceptionMessage;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class TotalResultsTest {
 
-    @Nested
-    @DisplayName("총 실행 결과 생성 예외 테스트")
-    class TotalResultsExceptionTest {
+    @Test
+    @DisplayName("참가자 수와 실행 결과 수가 동일하지 않으면 예외가 발생한다")
+    void createTotalResultsFailBySize() {
+        // given
+        PlayerName player1 = new PlayerName("aa");
+        PlayerName player2 = new PlayerName("bb");
+        PlayerName player3 = new PlayerName("cc");
+        PlayerNames playerNames = new PlayerNames(List.of(player1, player2, player3));
 
-        @Test
-        @DisplayName("참가자 수와 실행 결과 수가 동일하지 않으면 예외가 발생한다")
-        void createTotalResultsFailBySize() {
-            // given
-            PlayerName player1 = new PlayerName("aa");
-            PlayerName player2 = new PlayerName("bb");
-            PlayerName player3 = new PlayerName("cc");
-            PlayerNames playerNames = new PlayerNames(List.of(player1, player2, player3));
+        LadderResult ladderResult1 = new LadderResult("꽝");
+        LadderResult ladderResult2 = new LadderResult("당첨");
+        List<LadderResult> ladderResults = List.of(ladderResult1, ladderResult2);
 
-            LadderResult ladderResult1 = new LadderResult("꽝");
-            LadderResult ladderResult2 = new LadderResult("당첨");
-            List<LadderResult> ladderResults = List.of(ladderResult1, ladderResult2);
+        // then
+        assertThatThrownBy(() -> new TotalResults(ladderResults, playerNames))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ResultExceptionMessage.TOTAL_RESULTS_SIZE);
+    }
 
-            // then
-            assertThatThrownBy(() -> new TotalResults(ladderResults, playerNames))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessage(ResultExceptionMessage.TOTAL_RESULTS_SIZE);
-        }
+    @Test
+    @DisplayName("참가자 수와 실행 결과 수가 동일하면 정상적으로 생성된다")
+    void createTotalResultsSuccessWithSize() {
+        // given
+        PlayerName player1 = new PlayerName("aa");
+        PlayerName player2 = new PlayerName("bb");
+        PlayerNames playerNames = new PlayerNames(List.of(player1, player2));
 
+        LadderResult ladderResult1 = new LadderResult("꽝");
+        LadderResult ladderResult2 = new LadderResult("당첨");
+        List<LadderResult> ladderResults = List.of(ladderResult1, ladderResult2);
+
+        // then
+        assertThatCode(() -> new TotalResults(ladderResults, playerNames))
+                .doesNotThrowAnyException();
     }
 }
