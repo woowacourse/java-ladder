dd0d65dffea3b5390b4e4e9bb6729c58a2ce248f
fromitive <fromitive@gmail.com>
2024-02-27 14:41:40 +0900

    refactor(LadderGameResult): 필드 감소 및 읽기 전용으로 변경

--- src/main/java/domain/LadderGameResult.java
+++ src/main/java/domain/LadderGameResult.java
@@ -1,33 +1,29 @@
 package domain;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class LadderGameResult {
-
-    Names names;
-    LadderResults ladderResults;
+    private final Map<Name, LadderResult> ladderGameResult;
 
     public LadderGameResult(Names names, LadderResults ladderResults) {
         validateNamesLadderResultLength(names, ladderResults);
-        this.names = names;
-        this.ladderResults = ladderResults;
+        List<Name> swappedNames = names.getSwappedNames();
+        ladderGameResult = IntStream.range(0, swappedNames.size())
+                .boxed()
+                .collect(Collectors.toMap(swappedNames::get, ladderResults.getLadderResults()::get));
+    }
+
+    public Map<Name, LadderResult> getLadderGameResult() {
+        return Collections.unmodifiableMap(ladderGameResult);
     }
 
     private void validateNamesLadderResultLength(Names names, LadderResults ladderResults) {
         if (names.getNameCount() != ladderResults.getLadderResultCount()) {
             throw new LadderGameException(ExceptionType.NOT_ALLOW_DIFFERENT_NAMES_LADDER_RESULTS_LENGTH);
         }
-    }
-
-    public Map<Name, LadderResult> getLadderGameResult() {
-        List<Name> swappedNames = names.getSwappedNames();
-        List<LadderResult> ladderResults = this.ladderResults.getLadderResults();
-
-        return IntStream.range(0, swappedNames.size())
-                .boxed()
-                .collect(Collectors.toMap(swappedNames::get, ladderResults::get));
     }
 }
