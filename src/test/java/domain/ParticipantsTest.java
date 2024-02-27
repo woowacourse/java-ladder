f9e5b42dcce067495be0e0f36d52be55fb47bb9c
cutehumanS2 <cherrycoke@sookmyung.ac.kr>
2024-02-27 15:43:09 +0900

    test: 결과를 조회할 참여자의 인덱스 반환 테스트

--- src/test/java/domain/ParticipantsTest.java
+++ src/test/java/domain/ParticipantsTest.java
@@ -1,5 +1,6 @@
 package domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
@@ -8,11 +9,23 @@ import java.util.List;
 
 class ParticipantsTest {
 
-    @DisplayName("참가자 간 이름이 중복되면 에외가 발생한다.")
+    @DisplayName("참여자 간 이름이 중복되면 에외가 발생한다.")
     @Test
     void occurExceptionIfNameIsDuplicated() {
         assertThatThrownBy(() -> new Participants(List.of("test1", "test2", "test2")))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("참가자의 이름은 중복될 수 없습니다.");
+    }
+
+    @DisplayName("결과를 조회할 참여자의 인덱스를 반환한다.")
+    @Test
+    void returnIndexOfResultName() {
+        Participants participants = new Participants(List.of(
+                "pobi", "honux", "crong", "jk"
+        ));
+        String resultName = "crong";
+
+        int index = participants.findIndexOfParticipant(resultName);
+        assertThat(index).isEqualTo(2);
     }
 }
