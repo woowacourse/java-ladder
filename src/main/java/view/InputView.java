5a5628d3614b0f08752342962cdf566217c510ee
SCY <helenason@naver.com>
2024-02-27 17:26:20 +0900

    refactor: 재사용 변수를 인스턴스 변수로 묶어 사용

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class InputView {
 
+    private final Scanner sc = new Scanner(System.in);
+
     public String readNames() {
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
         return read();
@@ -25,7 +27,6 @@ public class InputView {
     }
 
     private String read() {
-        Scanner sc = new Scanner(System.in);
         return sc.nextLine();
     }
 }
