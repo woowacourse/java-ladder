d4dee7ad96f970d033854fd82b8161ce522e49c0
nayonsoso <panda0329@naver.com>
2024-02-27 18:33:12 +0900

    test: 실행 결과의 value가 1~5자 사이가 아니면 예외 발생 테스트

--- src/test/java/domain/ResultTest.java
+++ src/test/java/domain/ResultTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class ResultTest {
 
@@ -15,5 +16,14 @@ public class ResultTest {
         Result result = new Result(value);
 
         assertThat(result.getValue()).isEqualTo(value);
+    }
+
+    @DisplayName("값이 1~5자 사이가 아니면 에러가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"", "123456"})
+    void checkValueLengthWithException(String value) {
+        assertThatThrownBy(() -> new Result(value))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("실행 결과는 1~5자 사이여야 합니다.");
     }
 }
