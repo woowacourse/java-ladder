3eba480d0c84d339b40986ea230616fff0ec5126
Gyeongho Yang <chchch1213@naver.com>
2024-02-27 16:08:45 +0900

    feat: 게임에 진행에 사용될 사다리 가로 세로 길이 반환 함수 추가

--- src/main/java/ladder/domain/Ladder.java
+++ src/main/java/ladder/domain/Ladder.java
@@ -2,11 +2,21 @@ package ladder.domain;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.BiFunction;
 
 public record Ladder(List<Line> lines) {
     public Ladder(List<Line> lines) {
         this.lines = new ArrayList<>(lines);
+    }
+
+    public int width() {
+        if (lines.isEmpty()) {
+            return 0;
+        }
+        return lines.get(0).scaffolds().size();
+    }
+
+    public int height() {
+        return lines.size();
     }
 
     @Override
