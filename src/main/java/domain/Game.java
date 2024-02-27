1ac6aa6c7f076306149c636c02b6626c487517ad
le2sky <leehaneul990623@gmail.com>
2024-02-27 19:37:50 +0900

    feat: 사용자 사다리 게임 결과 조회 기능 구현

--- src/main/java/domain/Game.java
+++ src/main/java/domain/Game.java
@@ -13,15 +13,34 @@ public class Game {
 
         this.players = new Players(playerNames);
         this.ladder = new Ladder(height, playerNames.size());
-        this.gameResults = gameResults.stream()
-                .map(GameResult::new)
-                .toList();
+        this.gameResults = mapToGameResult(gameResults);
+    }
+
+    Game(List<String> playerNames, List<String> gameResults, Ladder ladder) {
+        validateSameSize(playerNames, gameResults);
+
+        this.players = new Players(playerNames);
+        this.ladder = ladder;
+        this.gameResults = mapToGameResult(gameResults);
     }
 
     private void validateSameSize(List<String> playerNames, List<String> gameResults) {
         if (playerNames.size() != gameResults.size()) {
             throw new IllegalArgumentException("사용자의 수와 실행 결과의 수가 일치하지 않습니다.");
         }
+    }
+
+    private List<GameResult> mapToGameResult(List<String> gameResults) {
+        return gameResults.stream()
+                .map(GameResult::new)
+                .toList();
+    }
+
+    public GameResult showResult(String playerName) {
+        int playerColumn = players.findPlayerColumn(playerName);
+        int resultColumn = ladder.climb(playerColumn);
+
+        return gameResults.get(resultColumn);
     }
 
     public Players getPlayers() {
--- src/test/java/domain/GameTest.java
+++ src/test/java/domain/GameTest.java
@@ -1,5 +1,6 @@
 package domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -21,5 +22,34 @@ class GameTest {
     void checkGameResultSize() {
         assertThatThrownBy(() -> new Game(List.of("pobi", "atom"), List.of("3000"), 3))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("사용자의 위치를 입력하면, 게임 결과를 받아올 수 있다.")
+    @Test
+    void showResult() {
+        Game game = new Game(
+                List.of("pobi", "atom", "mang", "jay"),
+                List.of("3000", "1000", "2000", "꽝"),
+                createLadder());
+
+        GameResult result = game.showResult("atom");
+
+        assertThat(result.getName()).isEqualTo("꽝");
+    }
+
+    private Ladder createLadder() {
+        List<Stick> line1 = List.of(
+                Stick.NOT_FILLED,
+                Stick.FILLED,
+                Stick.NOT_FILLED
+        );
+
+        List<Stick> line2 = List.of(
+                Stick.NOT_FILLED,
+                Stick.NOT_FILLED,
+                Stick.FILLED
+        );
+
+        return new Ladder(List.of(new Line(line1), new Line(line2)));
     }
 }
