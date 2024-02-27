5653ac2fda3b48475f579e5ef60a9916a6fbf0b3
SungKyum Kim <sungkyum1@naver.com>
2024-02-27 14:26:50 +0900

    test: 플레이어 생성, 주어진 사다리로 플레이어를 이동시키는 기능 테스트 작성

--- src/test/java/domain/PlayersTest.java
+++ src/test/java/domain/PlayersTest.java
@@ -1,2 +1,88 @@
-package domain;public class Players {
+package domain;
+
+import domain.lines.Lines;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class PlayersTest {
+    @Test
+    @DisplayName("주어진 이름의 개수만큼 참가자를 생성한다.")
+    void createPlayers() {
+        Names names = new Names(List.of("pobi", "tebah"));
+        Players players = new Players(names);
+
+        Assertions.assertEquals(2, players.getPlayers().size());
+    }
+
+    @Test
+    @DisplayName("주어진 사다리를 이동하여 플레이어의 위치를 결정한다.")
+    void playGame() {
+        int testHeight = 3;
+        int testPersonCount = 2;
+
+
+        BooleanGenerator booleanGenerator = new FixedBooleanGenerator(true);
+        Lines lines = new Lines(testHeight, testPersonCount, booleanGenerator);
+
+        Names names = new Names(List.of("pobi", "tebah"));
+        // 사다리 생성 결과
+        // pobi  tebah
+        //     |-----|
+        //     |-----|
+        //     |-----|
+
+        Players players = new Players(names);
+        players.playGame(lines);
+        List<Player> playersAfterPlay = players.getPlayers();
+
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(1, playersAfterPlay.get(0).getPosition()),
+                () -> Assertions.assertEquals(0, playersAfterPlay.get(1).getPosition())
+        );
+    }
+
+    @Test
+    @DisplayName("주어진 사다리를 이동하여 플레이어의 위치를 결정한다.")
+    void playGame2() {
+        int testHeight = 3;
+        int testPersonCount = 4;
+
+
+        BooleanGenerator booleanGenerator = new FixedBooleanGenerator(true);
+        Lines lines = new Lines(testHeight, testPersonCount, booleanGenerator);
+        // 사다리 생성 결과
+        // pobi  tebah  honux  crong
+        //       |-----|     |-----|
+        //       |-----|     |-----|
+        //       |-----|     |-----|
+        Names names = new Names(List.of("pobi", "tebah", "honux", "crong"));
+
+        Players players = new Players(names);
+        players.playGame(lines);
+        List<Player> playersAfterPlay = players.getPlayers();
+
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(1, playersAfterPlay.get(0).getPosition()),
+                () -> Assertions.assertEquals(0, playersAfterPlay.get(1).getPosition()),
+                () -> Assertions.assertEquals(3, playersAfterPlay.get(2).getPosition()),
+                () -> Assertions.assertEquals(2, playersAfterPlay.get(3).getPosition())
+
+        );
+    }
+
+    static class FixedBooleanGenerator implements BooleanGenerator {
+        private final Boolean value;
+
+        public FixedBooleanGenerator(Boolean value) {
+            this.value = value;
+        }
+
+        @Override
+        public Boolean generate() {
+            return value;
+        }
+    }
 }
