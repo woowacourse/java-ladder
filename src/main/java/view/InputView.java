2b23096aa23de2f52615d7909e4eb00625a36c0b
KimSangHyeok <tkdgur0906@naver.com>
2024-02-27 15:59:12 +0900

    feat : 결과를 보고 싶은 플레이어 입력받는 기능 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -12,6 +12,8 @@ public class InputView {
     private static final String MESSAGE_PARTICIPATED_PLAYERS = "\n참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     private static final String MESSAGE_EXECUTED_RESULT = "\n실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
     private static final String MESSAGE_LADDER_HEIGHT = "\n최대 사다리 높이는 몇 개인가요?";
+    private static final String MESSAGE_RESULT_PLAYER = "\n결과를 보고 싶은 사람은?";
+
     private static final Scanner scanner = new Scanner(System.in);
 
     public static List<Player> readPlayerNames() {
@@ -33,5 +35,10 @@ public class InputView {
     public static int readLadderHeight() throws NumberFormatException {
         System.out.println(MESSAGE_LADDER_HEIGHT);
         return Integer.parseInt(scanner.nextLine());
+    }
+
+    public static String readPlayer() {
+        System.out.println(MESSAGE_RESULT_PLAYER);
+        return scanner.nextLine();
     }
 }
