2211890b2b3aed9b52ea587f8f8125231cee4ace
3juhwan <13selfesteem91@naver.com>
2024-02-28 00:18:29 +0900

    docs: PathMapper 주석 추가

--- src/main/java/domain/game/PathMapper.java
+++ src/main/java/domain/game/PathMapper.java
@@ -10,6 +10,12 @@ import static java.util.stream.IntStream.range;
 public class PathMapper {
     private final Map<Integer, Integer> mapper;
 
+    /**
+     * path = [1, 2, 0] 의 의미
+     * 입구 0번 -> 출구 1번
+     * 입구 1번 -> 출구 2번
+     * 입구 2번 -> 출구 0번
+     */
     public PathMapper(final List<Integer> path) {
         this.mapper = range(0, path.size())
                 .boxed()
