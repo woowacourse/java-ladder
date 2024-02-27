fbc01b5abb5c4351233dee069ab31e7c29047b97
SungKyum Kim <sungkyum1@naver.com>
2024-02-27 16:05:20 +0900

    test: 실행결과 개수 검증 테스트 작성

--- src/test/java/domain/ResultsTest.java
+++ src/test/java/domain/ResultsTest.java
@@ -7,10 +7,10 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-class ResultTest {
+class ResultsTest {
     @Test
     @DisplayName("실행 결과의 개수는 참가자들의 수와 동일해야 한다.")
-    void createResult() {
-        assertThrows(IllegalArgumentException.class, () -> new Result(List.of("꽝", "5000"), 3));
+    void createResults() {
+        assertThrows(IllegalArgumentException.class, () -> new Results(List.of("꽝", "5000"), 3));
     }
 }
