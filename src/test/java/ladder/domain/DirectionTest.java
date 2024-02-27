b2fa0d732c3b3e1ab3a9dcc74f61e5ed06d99c46
JUHA <84626225+khabh@users.noreply.github.com>
2024-02-28 08:45:27 +0900

    test: DirectionTest ParameterizedTest로 수정

--- src/test/java/ladder/domain/DirectionTest.java
+++ src/test/java/ladder/domain/DirectionTest.java
@@ -1,26 +1,32 @@
 package ladder.domain;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static ladder.domain.Direction.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class DirectionTest {
-    @Test
+    @ParameterizedTest
+    @MethodSource("providePositionsForTest")
     @DisplayName("특정 방향으로 움직인 Position 값을 올바르게 반환")
-    void determineNextPositionTest() {
-        // given
-        Position position = new Position(1);
-
+    void determineNextPositionTest(Direction direction, Position initialPosition, Position expectedPosition) {
         // when
-        Position leftPosition = LEFT.determineNextPosition(position);
-        Position rightPosition = RIGHT.determineNextPosition(position);
-        Position neutralPosition = NEUTRAL.determineNextPosition(position);
+        Position actualPosition = direction.determineNextPosition(initialPosition);
 
         // then
-        assertThat(leftPosition).isEqualTo(new Position(0));
-        assertThat(rightPosition).isEqualTo(new Position(2));
-        assertThat(neutralPosition).isEqualTo(position);
+        assertThat(actualPosition).isEqualTo(expectedPosition);
+    }
+
+    private static Stream<Arguments> providePositionsForTest() {
+        return Stream.of(
+                Arguments.of(LEFT, new Position(1), new Position(0)),
+                Arguments.of(RIGHT, new Position(1), new Position(2)),
+                Arguments.of(NEUTRAL, new Position(1), new Position(1))
+        );
     }
 }
