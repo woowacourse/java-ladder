631725d545e63e1b1a87580494917e82223c40f7
yoonjuho <dbswn990@naver.com>
2024-02-27 14:45:12 +0900

    feat(LadderRow): 사다리의 행 내에서 라인에 따라 이동할 수 있다.

--- src/main/java/model/LadderRow.java
+++ src/main/java/model/LadderRow.java
@@ -2,7 +2,6 @@ package model;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class LadderRow {
 
@@ -31,7 +30,34 @@ public class LadderRow {
         return new ArrayList<>(spaces);
     }
 
-    public int move(int i) {
-        return 1;
+    public int move(int index) {
+        // 양끝에 위치한 index의 경우
+        if (index == 0) {
+            return checkRight(index);
+        }
+        if (index == spaces.size()) {
+            return checkLeft(index);
+        }
+        return checkBoth(index);
+    }
+
+    private int checkRight(int index){
+        if(spaces.get(index) == Space.LINE){
+            return index+1;
+        }
+        return index;
+    }
+    private int checkLeft(int index){
+        if(spaces.get(index - 1) == Space.LINE){
+            return index-1;
+        }
+        return index;
+    }
+
+    private int checkBoth(int index){
+        if(checkLeft(index) != index){
+            return checkLeft(index);
+        }
+        return checkRight(index);
     }
 }
