e93f2fc0d4f7e15f8ec80d2fbae6871819358f45
SCY <helenason@naver.com>
2024-02-27 15:07:04 +0900

    feat: 결과 도메인 생성 및 검증

---
+++ src/main/java/domain/Result.java
@@ -0,0 +1,27 @@
+package domain;
+
+public class Result {
+
+    public static final int MIN_RESULT_LENGTH = 1;
+    public static final int MAX_RESULT_LENGTH = 5;
+
+    private final String value;
+
+    public Result(String value) {
+        validateNull(value);
+        validateLength(value);
+        this.value = value;
+    }
+
+    private void validateNull(String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("결과에 null을 입력할 수 없습니다.");
+        }
+    }
+
+    private void validateLength(String value) {
+        if (value.length() < MIN_RESULT_LENGTH || value.length() > MAX_RESULT_LENGTH) {
+            throw new IllegalArgumentException("1~5자의 결과만 허용합니다.");
+        }
+    }
+}
