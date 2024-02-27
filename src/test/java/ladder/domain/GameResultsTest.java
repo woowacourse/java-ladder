6d6312b46ebd2e9bfcf0452a8d2826d3f0b7b40f
kelly6bf <kelly6bf@gmail.com>
2024-02-27 16:10:17 +0900

    test: 게임 결과 문자열 리스트 반환 테스트 로직 작성

--- src/test/java/ladder/domain/GameResultsTest.java
+++ src/test/java/ladder/domain/GameResultsTest.java
@@ -1,10 +1,11 @@
 package ladder.domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class GameResultsTest {
 
@@ -18,6 +19,20 @@ public class GameResultsTest {
         GameResults gameResults = GameResults.of(inputGameResults);
 
         // Then
-        Assertions.assertThat(gameResults).isNotNull();
+        assertThat(gameResults).isNotNull();
+    }
+
+    @DisplayName("게임 결과 문자열 리스트를 반환한다.")
+    @Test
+    void 게임_결과_문자열_리스트_반환() {
+        // Given
+        final List<String> inputGameResults = List.of("꽝", "5000", "꽝", "3000");
+        final GameResults gameResults = GameResults.of(inputGameResults);
+
+        // When
+        List<String> gameResults = gameResults.getGameResults();
+
+        // Then
+        assertThat(gameResults).contains("꽝", "5000", "꽝", "3000");
     }
 }
