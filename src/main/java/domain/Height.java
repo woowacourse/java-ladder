5f022c32ef597c14a80c6c1451c55bef061dcc0a
nayonsoso <panda0329@naver.com>
2024-02-27 20:17:52 +0900

    feat: 높이 조회 기능 구현

--- src/main/java/domain/Height.java
+++ src/main/java/domain/Height.java
@@ -1,4 +1,14 @@
 package domain;
 
 public class Height {
+
+    private final int height;
+
+    public Height(int height) {
+        this.height = height;
+    }
+
+    public int getHeight() {
+        return this.height;
+    }
 }
