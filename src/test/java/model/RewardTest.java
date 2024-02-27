b70e7aa2636dbde49d1f582a8a5cc2d891aba700
yoonjuho <dbswn990@naver.com>
2024-02-27 15:38:52 +0900

    refactor(Reward): 위치에 따라 결과를 알 수 있다.

--- src/test/java/model/RewardTest.java
+++ src/test/java/model/RewardTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class RewardTest {
 
-    @DisplayName("특정 위치에 어떤 결과가 있는지 알 수 있다.")
+    @DisplayName("위치에 따라 결과를 알 수 있다.")
     @Test
     void reward() {
         Reward reward = new Reward(List.of("꽝", "5000", "꽝", "3000"));
