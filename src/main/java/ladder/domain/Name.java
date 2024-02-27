6cf2f556ff4a372c3f090fc774947303bd90add9
mzeong <mjeong@sookmyung.ac.kr>
2024-02-27 22:01:49 +0900

    feat(Name): 이름 값이 동일하면 같은 이름으로 취급

--- src/main/java/ladder/domain/Name.java
+++ src/main/java/ladder/domain/Name.java
@@ -1,10 +1,29 @@
 package ladder.domain;
 
+import java.util.Objects;
+
 public class Name {
 
     private final String value;
 
     public Name(String value) {
         this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Name name = (Name) o;
+        return Objects.equals(value, name.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
     }
 }
