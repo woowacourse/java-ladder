2bbaaba7ac7676e66ebfd4d870878087c8382cf5
zangsu <zangsu_@naver.com>
2024-02-28 14:30:16 +0900

    refactor : Names에 제약 조건 추가
    - 한 번에 하나의 결과만 확인할 수 있기 때문에 중복된 이름을 입력받지 못하도록 변경
    - all 이라는 문자열은 모든 결과 확인에 사용되기 때문에 all 이라는 이름을 사용하지 못하도록 변경

--- src/main/java/domain/player/Names.java
+++ src/main/java/domain/player/Names.java
@@ -20,6 +20,24 @@ public class Names {
         if (names.size() < MIN_PLAYER_SIZE) {
             throw new IllegalArgumentException("참여할 사람은 " + MIN_PLAYER_SIZE + "명 이상이어야 합니다.");
         }
+        if (hasDuplicatedName(names)) {
+            throw new IllegalArgumentException("이름이 중복되어 있습니다.");
+        }
+        if (hasNameAll(names)) {
+            throw new IllegalArgumentException("all 이라는 문자열은 이름으로 사용할 수 없습니다.");
+        }
+    }
+
+    private boolean hasDuplicatedName(List<String> names) {
+        int distinctNameSize = (int) names.stream()
+                .distinct()
+                .count();
+        return distinctNameSize != names.size();
+    }
+
+    private boolean hasNameAll(List<String> names) {
+        return names.stream()
+                .anyMatch(name -> name.equalsIgnoreCase("all"));
     }
 
     public int size() {
