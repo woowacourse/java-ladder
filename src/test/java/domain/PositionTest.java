c22ec472cb5e3d654b35752759c0f24faf415748
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2024-02-28 17:20:11 +0900

    test: 원시값 포장 Position class

---
+++ src/test/java/domain/PositionTest.java
@@ -0,0 +1,24 @@
+package domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PositionTest {
+
+    @Test
+    @DisplayName("위치를 왼쪽으로 옮긴다.")
+    void movePositionLeft() {
+        Position position = new Position(10);
+        position.movePositionLeft();
+        Assertions.assertThat(position.getPosition()).isEqualTo(9);
+    }
+
+    @Test
+    @DisplayName("위치를 오른쪽으로 옮긴다.")
+    void movePositionRight() {
+        Position position = new Position(10);
+        position.movePositionRight();
+        Assertions.assertThat(position.getPosition()).isEqualTo(11);
+    }
+}
