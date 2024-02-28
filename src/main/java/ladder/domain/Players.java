a05ca7f458a0d78da093cd572ffb9970bfb40672
kelly6bf <kelly6bf@gmail.com>
2024-02-28 14:54:52 +0900

    feat: Players 플레이어 조회 메서드 구현

--- src/main/java/ladder/domain/Players.java
+++ src/main/java/ladder/domain/Players.java
@@ -2,6 +2,7 @@ package ladder.domain;
 
 import java.util.List;
 
+import static java.util.Collections.unmodifiableList;
 import static java.util.stream.Collectors.collectingAndThen;
 import static java.util.stream.Collectors.toList;
 import static java.util.stream.IntStream.range;
@@ -18,5 +19,14 @@ public class Players {
         return range(0, playerNameValues.size())
                 .mapToObj(userNameIndex -> Player.of(playerNameValues.get(userNameIndex), userNameIndex + 1, ladderHeight.ladderHeight()))
                 .collect(collectingAndThen(toList(), Players::new));
+    }
+
+    public List<Player> getPlayers() {
+        return unmodifiableList(players);
+    }
+
+    @Override
+    public String toString() {
+        return players.toString();
     }
 }
