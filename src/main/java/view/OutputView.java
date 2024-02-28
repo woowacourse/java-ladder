597eee5104d9e73b61f5542a60bc32ea90b005fb
slimsha2dy <chy2581@naver.com>
2024-02-28 19:38:34 +0900

    feat: 사다리 출력 문구 수정

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class OutputView {
 
     public static void printResult(List<String> results) {
-        System.out.println("실행결과");
+        System.out.println("사다리 결과");
         System.out.println();
         results.forEach(System.out::println);
     }
