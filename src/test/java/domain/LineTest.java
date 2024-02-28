463d96a61e35d5531f6e0536ded0e4597a52f6dd
nayonsoso <panda0329@naver.com>
2024-02-28 14:16:59 +0900

    test: 출발 위치의 오른쪽에 막대가 있으면, 오른쪽으로 이동하는지 테스트

--- src/test/java/domain/LineTest.java
+++ src/test/java/domain/LineTest.java
@@ -3,7 +3,9 @@ package domain;
 import domain.ladder.Line;
 import domain.ladder.Stick;
 import domain.ladder.StickGenerator;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -12,12 +14,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class LineTest {
 
+    private int playerSize;
+    private Line line;
+
+    @BeforeEach
+    void setUp() {
+        playerSize = 3;
+        line = new Line(filledStickGenerator(), playerSize); // FILLED, EMPTY
+    }
+
     @DisplayName("라인은 인원 수 -1 만큼의 막대를 갖는다.")
     @Test
     void lineHasStick() {
-        int playerSize = 3;
-        Line line = new Line(filledStickGenerator(), playerSize);
-
         List<Stick> sticks = line.getSticks();
 
         assertThat(sticks).hasSize(playerSize - 1);
@@ -26,24 +34,23 @@ class LineTest {
     @DisplayName("라인에 연속된 막대는 있을 수 없다.")
     @Test
     void opposite() {
-        int playerSize = 3;
-        Line line = new Line(filledStickGenerator(), playerSize);
-
         List<Stick> sticks = line.getSticks();
 
         assertThat(sticks.get(playerSize - 2)).isEqualTo(Stick.NOT_FILLED);
     }
 
     @DisplayName("사용자의 위치는 라인을 거치며 달라진다.")
-    @Test
-    void climb() {
-        int startPosition = 0;
-        int playerSize = 3;
-        Line line = new Line(filledStickGenerator(), playerSize);
+    @Nested
+    class climb {
 
-        int endPosition = line.climb(startPosition);
-        
-        assertThat(startPosition).isNotEqualTo(endPosition);
+        @DisplayName("출발 위치의 오른쪽에 막대가 있으면, 오른쪽으로 이동한다.")
+        @Test
+        void climb() {
+            int startPosition = 0;
+            int endPosition = line.climb(startPosition);
+
+            assertThat(endPosition).isEqualTo(startPosition + 1);
+        }
     }
 
     private StickGenerator filledStickGenerator() {
