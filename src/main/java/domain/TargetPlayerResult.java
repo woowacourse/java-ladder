609499457708705c34e5a2894442487e801fcb65
nayonsoso <panda0329@naver.com>
2024-02-28 17:46:05 +0900

    feat: 결과를 볼 사용자는 all 또는 기존 사용자들의 이름만 허용하는 기능 구현

--- src/main/java/domain/TargetPlayerResult.java
+++ src/main/java/domain/TargetPlayerResult.java
@@ -1,4 +1,19 @@
 package domain;
 
+import java.util.List;
+
 public class TargetPlayerResult {
+
+    private final String name;
+
+    public TargetPlayerResult(String name, List<String> names) {
+        if (!names.contains(name) && !name.equals("all")) {
+            throw new IllegalArgumentException("all 이나 기존 사용자 이름을 입력해야 합니다.");
+        }
+        this.name = name;
+    }
+
+    public String getTargetPlayerName() {
+        return null;
+    }
 }
