37bfd250c32d9308760ed8b1a7abdd537d75fb1a
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-02-28 17:32:53 +0900

    feat(Height) : Height Domain VO 역활 하게 equals,hashCode 재정의

--- src/main/java/domain/ladder/attribute/Height.java
+++ src/main/java/domain/ladder/attribute/Height.java
@@ -1,15 +1,29 @@
 package domain.ladder.attribute;
 
+import java.util.Objects;
+
 public class Height {
 
     private final int height;
 
-    public Height(String inputNumber){
+    public Height(String inputNumber) {
         validate(inputNumber);
         this.height = Integer.parseInt(inputNumber);
     }
 
-    private void validate(String inputNumber){
+    @Override
+    public boolean equals(final Object object) {
+        if (this == object) return true;
+        if (!(object instanceof final Height height1)) return false;
+        return height == height1.height;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(height);
+    }
+
+    private void validate(String inputNumber) {
         validateIsNumeric(inputNumber);
         validateSize(inputNumber);
     }
@@ -19,11 +33,13 @@ public class Height {
             throw new IllegalArgumentException("높이는 숫자만 입력 가능합니다.");
         }
     }
-    private void validateSize(String inputNumber){
-        if(Integer.valueOf(inputNumber) <= 0){
+
+    private void validateSize(String inputNumber) {
+        if (Integer.valueOf(inputNumber) <= 0) {
             throw new IllegalArgumentException("높이는 1 이상이여야 합니다.");
         }
     }
+
     public int getHeight() {
         return height;
     }
