e043a84988de2134a4004e703789df7950802d7a
nayonsoso <panda0329@naver.com>
2024-02-28 17:41:39 +0900

    test: 결과를 볼 사용자는 all 또는 기존 사용자들의 이름만 허용하는지 검증 테스트

--- src/test/java/domain/TargetPlayerResultTest.java
+++ src/test/java/domain/TargetPlayerResultTest.java
@@ -4,11 +4,17 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 public class TargetPlayerResultTest {
 
-    @DisplayName("생성 테스트")
+    @DisplayName("결과를 볼 사용자는 all 또는 기존 사용자들의 이름만 허용한다.")
     @Test
     void create() {
-        Assertions.assertThatCode(() -> new TargetPlayerResult());
+        String name = "산초";
+        List<String> names = List.of("산초", "아톰");
+
+        Assertions.assertThatCode(() -> new TargetPlayerResult(name, names))
+                .doesNotThrowAnyException();
     }
 }
