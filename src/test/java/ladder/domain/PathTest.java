2de339fd3c3d94b051958e44579314d5295da19d
kelly6bf <kelly6bf@gmail.com>
2024-02-28 15:04:06 +0900

    test: 반대쪽 라인 번호 까지의 거리를 반환하는 테스트 로직 작성

--- src/test/java/ladder/domain/PathTest.java
+++ src/test/java/ladder/domain/PathTest.java
@@ -34,4 +34,18 @@ public class PathTest {
         // Then
         assertThat(hasPath).isTrue();
     }
+
+    @DisplayName("입력된 라인 번호의 반대쪽 라인 번호까지의 거리를 반환한다.")
+    @Test
+    void 반대쪽_라인_번호_거리_반환() {
+        // Given
+        final Path path = Path.of(1, 2);
+        final LineNumber inputLineNumber = new LineNumber(2);
+
+        // When
+        int otherLineNumber = path.getOtherPathDistance(inputLineNumber);
+
+        // Then
+        assertThat(otherLineNumber).isEqualTo(-1);
+    }
 }
