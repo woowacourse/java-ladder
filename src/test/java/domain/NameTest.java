de46b46c15fadd7623637ff93211697888c47a24
linirini <2001yerin@naver.com>
2024-02-28 20:00:53 +0900

    test : 참가자 이름 생성 성공 테스트

--- src/test/java/domain/NameTest.java
+++ src/test/java/domain/NameTest.java
@@ -2,10 +2,22 @@ package domain;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class NameTest {
+
+    @DisplayName("적절한 참가자 이름이 주어지면 잘 생성된다.")
+    @Test
+    void createName() {
+        //given
+        String name = "crong";
+
+        //when & then
+        Assertions.assertThatCode(() -> new Name(name)).doesNotThrowAnyException();
+    }
+
 
     @DisplayName("참가자 이름이 1글자 미만, 5글자를 초과하면 예외를 발생시킨다.")
     @ParameterizedTest
