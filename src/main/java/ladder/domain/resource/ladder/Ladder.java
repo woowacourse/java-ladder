8bdec749cb423f1de362719e679c32af277b55a0
KIMGYUTAE <kiimwf0819@gmail.com>
2024-02-28 15:16:08 +0900

    refactor(Ladder): 비어있는 라인으로 생성하는 경우에 대한 에외 추가 및 테스트코드 작성

--- src/main/java/ladder/domain/resource/ladder/Ladder.java
+++ src/main/java/ladder/domain/resource/ladder/Ladder.java
@@ -8,6 +8,8 @@ public class Ladder {
     private final List<Line> lines;
 
     Ladder(List<Line> lines) {
+        validateNotEmpty(lines);
+        validateLine(lines);
         validateConsistentLineSize(lines);
         this.lines = lines;
     }
@@ -18,6 +20,24 @@ public class Ladder {
 
     public int getWidth() {
         return lines.get(0).getSize();
+    }
+
+    private void validateNotEmpty(List<Line> lines) {
+        if (lines.isEmpty()) {
+            throw new IllegalArgumentException("[ERROR] 라인이 없어 사다리 생성이 불가능합니다.");
+        }
+    }
+
+    private void validateLine(List<Line> lines) {
+        for (Line line : lines) {
+            validateNotEmptyLine(line);
+        }
+    }
+
+    private void validateNotEmptyLine(Line line) {
+        if (line.isEmpty()) {
+            throw new IllegalArgumentException("[ERROR] 방향 정보가 없는 라인으로 사다리 생성이 불가능힙니다.");
+        }
     }
 
     private void validateConsistentLineSize(List<Line> lines) {
--- src/test/java/ladder/domain/resource/ladder/LadderTest.java
+++ src/test/java/ladder/domain/resource/ladder/LadderTest.java
@@ -1,7 +1,9 @@
 package ladder.domain.resource.ladder;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.ArrayList;
 import java.util.List;
 import ladder.domain.resource.line.Line;
 import ladder.domain.resource.line.LineGenerator;
@@ -11,9 +13,37 @@ import org.junit.jupiter.api.Test;
 
 class LadderTest {
 
+    @DisplayName("비어있는 사다리를 만들 경우 예외가 발생한다.")
+    @Test
+    void newLadderByEmpty() {
+        //given
+        List<Line> emptyLines = new ArrayList<>();
+
+        //when, then
+        assertThatThrownBy(() -> new Ladder(emptyLines))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 라인이 없어 사다리 생성이 불가능합니다.");
+    }
+
+    @DisplayName("비어있는 라인으로 사다리를 만들 경우 예외가 발생한다.")
+    @Test
+    void newLadderByEmptyLine() {
+        //given
+        LineGenerator lineGenerator = new LineGeneratorImpl();
+        Line line = lineGenerator.generateLine();
+
+        //when
+        assertThat(line.isEmpty()).isTrue();
+
+        //then
+        assertThatThrownBy(() -> new Ladder(List.of(line)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 방향 정보가 없는 라인으로 사다리 생성이 불가능힙니다.");
+    }
+
     @DisplayName("사다리의 모든 라인의 너비가 동일하지 않은 경우 예외가 발생한다.")
     @Test
-    void newLadderTestByConsistentLineSize() {
+    void newLadderByConsistentLineSize() {
         //given
         LineGenerator lineGenerator = new LineGeneratorImpl();
 
@@ -28,4 +58,35 @@ class LadderTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 사다리의 모든 라인의 너비는 동일해야 합니다.");
     }
+
+    @DisplayName("사다리의 높이(세로)를 반환한다.")
+    @Test
+    void getHeight() {
+        //given
+        int height = 2;
+        LadderGenerator ladderGenerator = new LadderGenerator(new LineGeneratorImpl());
+        Ladder ladder = ladderGenerator.generate(height, 2);
+
+        //when
+        int returnedHeight = ladder.getHeight();
+
+        //then
+        assertThat(returnedHeight).isEqualTo(height);
+    }
+
+    @DisplayName("사다리의 넓이(가로)를 반환한다.")
+    @Test
+    void getWidth() {
+        //given
+        int width = 2;
+        LadderGenerator ladderGenerator = new LadderGenerator(new LineGeneratorImpl());
+        Ladder ladder = ladderGenerator.generate(2, width);
+
+        //when
+        int returnedWidth = ladder.getWidth();
+
+        //then
+        assertThat(returnedWidth).isEqualTo(width);
+    }
 }
+
