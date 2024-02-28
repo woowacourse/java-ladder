5bffe74fcd53f553051e982c65a2e3b7c0879d18
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-02-28 14:28:18 +0900

    feat(GameBoard) : 플레이어 이름을 통해 게임을 진행하는 로직 구현
    - 이에 따른 테스트 코드 추가

--- src/main/java/domain/GameBoard.java
+++ src/main/java/domain/GameBoard.java
@@ -2,9 +2,11 @@ package domain;
 
 import domain.ladder.Ladder;
 import domain.ladder.common.Direction;
+import domain.player.Name;
+import domain.player.Player;
 import domain.player.Players;
 
-import java.util.List;
+import java.util.stream.Stream;
 
 public class GameBoard {
     private final Players players;
@@ -25,15 +27,51 @@ public class GameBoard {
         return ladder.getHeight();
     }
 
-    public Ladder getLadder() {
-        return ladder;
-    }
 
     public Rewards getRewards() {
         return rewards;
     }
 
-    public List<Direction> getDirectionAtHorizontalIndex(Integer index) {
-        return ladder.getDirectionAtHorizontalIndex(index);
+    public Ladder getLadder() {
+        return ladder;
+    }
+
+    public Player findPlayerWithName(Name name) {
+        return players.getPlayerWithName(name);
+    }
+
+    public Result playGameOnePlayer(Player player) {
+        Point startPoint = getPlayerStartPoint(player);
+        Point endPoint = playGameWithStartPoint(startPoint);
+        return new Result(player.getName(), getRewardWithIndex(endPoint.row()));
+    }
+
+    private Point playGameWithStartPoint(final Point startPoint) {
+        return Stream.iterate(startPoint, this::movePoint)
+                     .filter(this::isPointIsEndLine)
+                     .findFirst()
+                     .get();
+    }
+
+    private Point movePoint(Point point) {
+        Direction direction = ladder.getDirectionWithPoint(point);
+        return point.move(direction);
+    }
+
+    private boolean isPointIsEndLine(Point point) {
+        if (point.column() < ladder.getHeight()) {
+            return false;
+        }
+        return true;
+    }
+
+    private Point getPlayerStartPoint(Player player) {
+        return new Point.Builder().column(0)
+                                  .row(players.getPlayerIndex(player))
+                                  .build();
+    }
+
+    private Reward getRewardWithIndex(Integer index) {
+        return rewards.getRewardAt(index);
     }
 }
--- src/test/java/domain/GameBoardTest.java
+++ src/test/java/domain/GameBoardTest.java
@@ -1,17 +1,22 @@
 package domain;
 
 import domain.ladder.Ladder;
+import domain.ladder.common.Direction;
 import domain.ladder.common.Height;
+import domain.player.Name;
 import domain.player.Names;
+import domain.player.Player;
 import domain.player.Players;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+
 import util.RandomDirectionGenerator;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import java.util.List;
+import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import static org.assertj.core.api.Assertions.assertThatCode;
 
 
@@ -25,9 +30,72 @@ public class GameBoardTest {
 
         Ladder ladder = new Ladder(height, players.getPlayerCount(), new RandomDirectionGenerator());
 
-        assertThatCode(() -> new GameBoard(players, ladder, rewards));
-
+        assertThatCode(() -> new GameBoard(players, ladder, rewards))
+                .doesNotThrowAnyException();
     }
+
+    /**
+     * R L
+     * D D
+     * R L
+     */
+    @Test
+    @DisplayName("플레이어 이름을 통해 게임을 진행한다 - pobi 는 꽝에 당첨된다")
+    public void gameOnePlayer_case1() {
+        Players players = 플레이어_생성(Names.from(List.of("pobi", "honux")));
+        Rewards rewards = 보상_목록_생성(List.of("꽝", "5000"));
+        Height height = new Height("3");
+        Ladder ladder = new Ladder(height, players.getPlayerCount(), new FixedDirectionGenerator(List.of(
+                Direction.RIGHT, Direction.DOWN, Direction.RIGHT,
+                Direction.LEFT, Direction.DOWN, Direction.LEFT)));
+        GameBoard gameBoard = new GameBoard(players, ladder, rewards);
+
+        Name name = new Name("pobi");
+        Player player = gameBoard.findPlayerWithName(name);
+
+        Result result = gameBoard.playGameOnePlayer(player);
+        assertResult("꽝", result);
+    }
+
+    @Test
+    @DisplayName("플레이어 이름을 통해 게임을 진행한다. - jk 는 5000원에 당첨된다")
+    public void gameOnePlayer_case2() {
+        Players players = 플레이어_생성(Names.from(List.of("pobi", "honux", "crong", "jk")));
+        Rewards rewards = 보상_목록_생성(List.of("꽝", "5000", "꽝", "3000"));
+        final var gameBoard = 게임보드_생성(players, rewards);
+
+        Name name = new Name("jk");
+        Player player = gameBoard.findPlayerWithName(name);
+
+        Result result = gameBoard.playGameOnePlayer(player);
+        assertResult("5000", result);
+    }
+
+    private void assertResult(String value, Result result) {
+        assertEquals(value, result.reward()
+                                  .getValue());
+    }
+
+    /**
+     * R    L    R   L
+     * D    R    L   D
+     * R    L    D   D
+     * D    R    L   D
+     * R    L    R   L
+     * 꽝  5000  꽝  3000
+     */
+    private static GameBoard 게임보드_생성(final Players players, final Rewards rewards) {
+        Height height = new Height("5");
+
+        Ladder ladder = new Ladder(height, players.getPlayerCount(), new FixedDirectionGenerator(List.of(
+                Direction.RIGHT, Direction.DOWN, Direction.RIGHT, Direction.DOWN, Direction.RIGHT,
+                Direction.LEFT, Direction.RIGHT, Direction.LEFT, Direction.RIGHT, Direction.LEFT,
+                Direction.RIGHT, Direction.LEFT, Direction.DOWN, Direction.LEFT, Direction.RIGHT,
+                Direction.LEFT, Direction.DOWN, Direction.DOWN, Direction.DOWN, Direction.LEFT)));
+        GameBoard gameBoard = new GameBoard(players, ladder, rewards);
+        return gameBoard;
+    }
+
 
     private Players 플레이어_생성(Names names) {
         return new Players(names);
