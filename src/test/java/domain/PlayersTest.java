0b537371d961ebce128aa3a6bebf1a69602cef72
cutehumanS2 <cherrycoke@sookmyung.ac.kr>
2024-02-28 14:41:47 +0900

    test: 불필요한 import 문 제거

--- src/test/java/domain/PlayersTest.java
+++ src/test/java/domain/PlayersTest.java
@@ -5,10 +5,6 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
-import view.InputValidator;
-
 import java.util.List;
 
 class PlayersTest {
