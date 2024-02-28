44453a64e0a9b5ab21d6025f9edbbcd307cfcf47
yoonjuho <dbswn990@naver.com>
2024-02-28 15:03:22 +0900

    feat(OutputView): 요구사항에 추가된 출력을 수행한다.

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,24 +1,24 @@
 package view;
 
-import model.ladder.Height;
-import model.ladder.Ladder;
-import model.ladder.LadderRow;
-import model.ladder.Space;
+import model.ladder.*;
 import model.participant.Participant;
 import model.participant.Participants;
+import model.result.Result;
+import model.result.Results;
 
 import java.util.List;
+import java.util.Map;
 
 public class OutputView {
 
     private static final String RESULT_MESSAGE = "실행결과\n";
     private static final String NAME_FORMAT = "%5s ";
 
-    public void printResult(Height height, Participants participants, Ladder ladder) {
+    public void printResult(LadderGame ladderGame) {
         System.out.println(RESULT_MESSAGE);
-        printParticipantsName(participants);
-        printLadder(height, ladder);
-
+        printParticipantsName(ladderGame.getParticipants());
+        printLadder(ladderGame.getLadder());
+        printDetailResult(ladderGame.getResults());
     }
 
     private void printParticipantsName(Participants participants) {
@@ -29,8 +29,8 @@ public class OutputView {
         System.out.println();
     }
 
-    private void printLadder(Height height, Ladder ladder) {
-        for (int i = 0; i < height.getValue(); i++) {
+    private void printLadder(Ladder ladder) {
+        for (int i = 0; i < ladder.getHeight(); i++) {
             printRow(ladder.getRow(i));
             System.out.println();
         }
@@ -43,5 +43,24 @@ public class OutputView {
             System.out.print(LadderComponent.match(space).getOutput());
         }
         System.out.print(LadderComponent.DIVISION.getOutput());
+    }
+
+    private void printDetailResult(Results results){
+        List<String> result = results.getResults().stream()
+                .map(Result::name)
+                .toList();
+        result.forEach(name -> System.out.printf(NAME_FORMAT, name));
+        System.out.println();
+    }
+    public void printOneResult(Result result){
+        System.out.println("실행 결과");
+        System.out.println(result.name());
+    }
+
+    public void printAllResult(Map<Participant, Result> results){
+        System.out.println("실행 결과");
+        for (Map.Entry<Participant, Result> result :results.entrySet()) {
+            System.out.println(result.getKey().getName() + " : " + result.getValue().name());
+        }
     }
 }
