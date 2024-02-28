db2702c13050a6099b03b38d293ccc034031da88
tackyu <tack970913@gmail.com>
2024-02-28 20:25:41 +0900

    feat: 사다리 결과 목록 생성 구현

---
+++ src/test/java/domain/model/ResultCandidate.java
@@ -0,0 +1,11 @@
+package domain.model;
+
+import java.util.List;
+
+public class ResultCandidate {
+    private final List<String> results;
+
+    public ResultCandidate(List<String> results) {
+        this.results = results;
+    }
+}
