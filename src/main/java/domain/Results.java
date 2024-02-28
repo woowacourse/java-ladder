d2e8627c2bdc6dd9a5c4681cbfce37fdd76c5aaf
SCY <helenason@naver.com>
2024-02-28 21:33:34 +0900

    refactor: Results 클래스 validate 메서드 호출 위치 변경

--- src/main/java/domain/Results.java
+++ src/main/java/domain/Results.java
@@ -6,18 +6,18 @@ public class Results {
 
     private final List<Result> results;
 
-    private Results(List<Result> results, int memberCount) {
-        validateCount(results, memberCount);
+    private Results(List<Result> results) {
         this.results = results;
-    } // TODO: memberCount 주입 과정 어색
-
-    public static Results of(List<String> results, int memberCount) {
-        return new Results(results.stream()
-                .map(Result::new)
-                .toList(), memberCount);
     }
 
-    private void validateCount(List<Result> results, int memberCount) {
+    public static Results of(List<String> results, int memberCount) {
+        validateCount(results, memberCount);
+        return new Results(results.stream()
+                .map(Result::new)
+                .toList());
+    }
+
+    private static void validateCount(List<String> results, int memberCount) {
         if (results.size() != memberCount) {
             throw new IllegalArgumentException("결과의 수는 참여자의 수와 같아야 합니다.");
         }
