1679be62fa93feca3fc8051c73ce8dc1812a2220
hoyeonyy <highoption@naver.com>
2024-02-28 17:26:58 +0900

    feat (Price)  : 검증 추가

--- src/main/java/ladder/domain/Price.java
+++ src/main/java/ladder/domain/Price.java
@@ -4,7 +4,26 @@ public class Price {
     private final String price;
 
     public Price(String price) {
+        validate(price);
+
         this.price = price;
+    }
+
+    private void validate(String priceName) {
+        validateNotEmpty(priceName);
+        validateMaxLength(priceName);
+    }
+
+    private void validateNotEmpty(String priceName) {
+        if (priceName == null || priceName.isBlank()) {
+            throw new IllegalArgumentException("상품 이름은 한글자 이상이어야 합니다.");
+        }
+    }
+
+    private void validateMaxLength(String priceName) {
+        if (priceName.length() > 5) {
+            throw new IllegalArgumentException("상품 이름은 5글자를 넘을 수 없습니다.");
+        }
     }
 
     public String getPrice() {
--- src/test/java/ladder/domain/PriceTest.java
+++ src/test/java/ladder/domain/PriceTest.java
@@ -1,8 +1,14 @@
 package ladder.domain;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class PriceTest {
     @Test
@@ -11,5 +17,25 @@ class PriceTest {
         String priceName = "맥북";
         Price price = new Price(priceName);
         Assertions.assertThat(price.getPrice()).isEqualTo("맥북");
+    }
+
+    @DisplayName("상품 이름은 5글자를 넘을 수 없다")
+    @ParameterizedTest
+    @CsvSource({"steve!", "123 45", "123456"})
+    void validateTest_WhenLengthIsOver5(String priceName) {
+
+        assertThatThrownBy(() -> new Price(priceName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("상품 이름은 5글자를 넘을 수 없습니다.");
+    }
+
+    @DisplayName("상품 이름에 공백 또는 Null 이 오면 안된다.")
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = " ")
+    void validateTest_WhenNameIsEmpty(String priceName) {
+        assertThatThrownBy(() -> new Price(priceName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("상품 이름은 한글자 이상이어야 합니다.");
     }
 }
