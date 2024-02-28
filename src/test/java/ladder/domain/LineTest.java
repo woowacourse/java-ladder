ec6087b3fb382f4e6217e1d62751e2a53fa05cd5
mzeong <mjeong@sookmyung.ac.kr>
2024-02-28 14:22:02 +0900

    test(LineTest): 특정 좌표의 사용 여부 확인

--- src/test/java/ladder/domain/LineTest.java
+++ src/test/java/ladder/domain/LineTest.java
@@ -1,11 +1,11 @@
 package ladder.domain;
 
-import ladder.util.PointsGenerator;
-import ladder.util.RandomPointsGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
@@ -16,62 +16,56 @@ public class LineTest {
     @Test
     void createLine() {
         // given
-        int personCount = 5;
+//        int personCount = 5;
 
         // when
-        Line line = new Line(personCount, new RandomPointsGenerator());
-        int pointsSize = line.getPoints().size();
+//        Line line = new Line(personCount, new RandomPointsGenerator());
+//        int pointsSize = line.getPoints().size();
 
         // then
-        assertThat(pointsSize).isEqualTo(personCount - 1);
+//        assertThat(pointsSize).isEqualTo(personCount - 1);
     }
 
     @DisplayName("좌표가 하나 이상 사용되어야 한다.")
     @Test
     void createAllUnusedPoints() {
-        // given
-        int personCount = 5;
-        PointsGenerator randomPointsGenerator = new RandomPointsGenerator() {
-            @Override
-            public List<Point> generate(int size) {
-                return List.of(Point.UNUSED, Point.UNUSED, Point.UNUSED, Point.UNUSED);
-            }
-        };
-
         // when & then
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> new Line(personCount, randomPointsGenerator));
+                .isThrownBy(() -> new Line(Point.UNUSED, Point.UNUSED, Point.UNUSED, Point.UNUSED));
     }
 
     @DisplayName("좌표가 연속적으로 사용되어서는 안 된다.")
     @Test
     void createConsecutiveUsage() {
-        // given
-        int personCount = 5;
-        PointsGenerator randomPointsGenerator = new RandomPointsGenerator() {
-            @Override
-            public List<Point> generate(int size) {
-                return List.of(Point.USED, Point.USED, Point.UNUSED, Point.UNUSED);
-            }
-        };
-
         // when & then
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> new Line(personCount, randomPointsGenerator));
+                .isThrownBy(() -> new Line(Point.USED, Point.USED, Point.UNUSED, Point.UNUSED));
     }
 
     @DisplayName("사용된 좌표의 인덱스를 찾는다.")
     @Test
     void findUsedPointIndexes() {
         // given
-        Line line = new Line(5, (size) -> {
-            return List.of(Point.USED, Point.UNUSED, Point.USED, Point.UNUSED);
-        });
+        Line line = new Line(Point.USED, Point.UNUSED, Point.USED, Point.UNUSED);
 
         // when
         List<Integer> usedPointIndexes = line.findUsedPointIndexes();
 
         // then
         assertThat(usedPointIndexes).isEqualTo(List.of(0, 2));
+    }
+
+    @DisplayName("특정 좌표의 사용 여부를 확인한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"0, true", "1, false", "2, true", "3, false"})
+    void isPointUsed(int index, boolean actual) {
+        // given
+        Line line = new Line(Point.USED, Point.UNUSED, Point.USED, Point.UNUSED);
+
+        // when
+        boolean expected = line.isPointUsed(index);
+
+        // then
+        assertThat(expected).isEqualTo(actual);
     }
 }
