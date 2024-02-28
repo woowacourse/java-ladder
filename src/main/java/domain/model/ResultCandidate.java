12699f6731adefa18d50a86c177419688f4e2d78
tackyu <tack970913@gmail.com>
2024-02-28 21:04:38 +0900

    feat: 사다리 결과 목록 생성 조건 추가

--- src/main/java/domain/model/ResultCandidate.java
+++ src/main/java/domain/model/ResultCandidate.java
@@ -5,7 +5,13 @@ import java.util.List;
 public class ResultCandidate {
     private final List<String> results;
 
-    public ResultCandidate(List<String> results) {
+    public ResultCandidate(List<String> results,int numberOfPeople) {
+        validateSize(results,numberOfPeople);
         this.results = results;
+    }
+    private void validateSize(List<String> results,int numberOfPeople){
+        if(results.size()!=numberOfPeople) {
+            throw new IllegalArgumentException("결과는 사람 수와 일치해야 합니다.");
+        }
     }
 }
