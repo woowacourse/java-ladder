d561a4cf9dc3403d21a89ea7f7acef27298faabb
Arachneee <jhg2819@naver.com>
2024-02-28 14:11:00 +0900

    test: 불필요 import 제거

--- src/test/java/laddergame/domain/name/NameTest.java
+++ src/test/java/laddergame/domain/name/NameTest.java
@@ -3,7 +3,6 @@ package laddergame.domain.name;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
