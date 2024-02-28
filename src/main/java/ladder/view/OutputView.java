dfcb60d63a48fc0191c8004729d793c2786421e0
Eugene Jang <kyunellroll@gmail.com>
2024-02-29 01:21:44 +0900

    feat(OutputView): 게임의 출력 view 생성
    - 만들어진 사다리와 이름과 상품을 출력하는 method 생성
    - 입력한 결과에 따른 사다리 결과를 출력하는 method 생성

--- src/main/java/ladder/view/OutputView.java
+++ src/main/java/ladder/view/OutputView.java
@@ -10,11 +10,25 @@ public class OutputView {
     private static final String LADDERS_PREFIX = "    |";
     private static final String LADDER_STICK_SYMBOL = "|";
 
-    public void printResult(ResultLadderDto resultLadderDto, List<Name> names) {
+    public void printResult(ResultLadderDto resultLadderDto, List<Name> names, List<String> gamePrizes) {
         List<BuiltLadderDto> builtLadderDtos = resultLadderDto.resultLadder();
 
         printParticipantsNames(names);
         printBuiltLadders(builtLadderDtos);
+        printPrizes(gamePrizes);
+    }
+
+    public void printSingleResult(String prize) {
+        System.out.println("\n실행 결과");
+        System.out.println(prize);
+    }
+
+    public void printAllResult(List<Name> names, List<String> prizes) {
+        System.out.println("\n실행 결과");
+
+        for (int i = 0; i < names.size(); i++) {
+            System.out.println(names.get(i).getName() + " : " + prizes.get(i));
+        }
     }
 
     private void printParticipantsNames(List<Name> names) {
@@ -28,6 +42,13 @@ public class OutputView {
         for (BuiltLadderDto builtLadderDto : builtLadderDtos) {
             System.out.println(LADDERS_PREFIX + String.join(LADDER_STICK_SYMBOL, builtLadderDto.builtLadder()));
         }
+    }
+
+    private void printPrizes(List<String> gamePrizes) {
+        for (String prize : gamePrizes) {
+            System.out.printf("%6s", prize);
+        }
+        System.out.println();
     }
 
     public void printError(IllegalArgumentException e) {
