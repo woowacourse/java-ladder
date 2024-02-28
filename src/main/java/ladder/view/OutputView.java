fd0c56b4db19cd1c53548cbe23f3ff6278a6d156
seokmyungham <hellotjraud@gmail.com>
2024-02-28 17:35:09 +0900

    feat(OutputView): 사다리 게임 실행 결과를 출력하는 메서드 추가

--- src/main/java/ladder/view/OutputView.java
+++ src/main/java/ladder/view/OutputView.java
@@ -1,6 +1,7 @@
 package ladder.view;
 
 import java.util.List;
+import java.util.Map;
 import ladder.domain.dto.FloorResponseDto;
 import ladder.domain.dto.LadderResponseDto;
 import ladder.domain.participant.Name;
@@ -10,7 +11,8 @@ public class OutputView {
     private static final String LADDERS_PREFIX = "    |";
     private static final String LADDER_STICK_SYMBOL = "|";
 
-    public void printResult(LadderResponseDto ladderResponseDto, List<Name> participantNames, List<String> prizeNames) {
+    public void printLadderResult(LadderResponseDto ladderResponseDto, List<Name> participantNames,
+                                  List<String> prizeNames) {
         List<FloorResponseDto> floorResponseDtos = ladderResponseDto.ladderResult();
 
         printParticipantsNames(participantNames);
@@ -19,6 +21,7 @@ public class OutputView {
     }
 
     private void printParticipantsNames(List<Name> names) {
+        System.out.println("\n사다리 결과\n");
         for (Name name : names) {
             System.out.printf("%6s", name.getName());
         }
@@ -36,6 +39,19 @@ public class OutputView {
     private void printPrizes(List<String> prizes) {
         for (String prizeName : prizes) {
             System.out.printf("%-6s", prizeName);
+        }
+        System.out.println();
+    }
+
+    public void printNameMatchResult(Map<Name, String> gameResult, String nameSearch) {
+        System.out.println("\n실행 결과");
+        System.out.println(gameResult.get(new Name(nameSearch)));
+    }
+
+    public void printAllMatchResult(Map<Name, String> gameResult) {
+        System.out.println("\n실행 결과");
+        for (Name name : gameResult.keySet()) {
+            System.out.println(name.getName() + " : " + gameResult.get(name));
         }
     }
 
