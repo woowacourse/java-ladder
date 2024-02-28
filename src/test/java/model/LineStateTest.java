d9e5a1f993f333a5ab726f30cfaa370d72aebb56
ashsty <qkralsdk991@naver.com>
2024-02-28 15:41:54 +0900

    refactor : 마지막 인덱스 분리 처리 로직 추가

--- src/test/java/model/LineStateTest.java
+++ src/test/java/model/LineStateTest.java
@@ -11,16 +11,24 @@ class LineStateTest {
     @DisplayName("true로 LineState를 생성하면 START를 반환한다")
     @ParameterizedTest
     @CsvSource(value = {"true,START", "false,NONE"})
-    void decideLineState(boolean decision, LineState expected) {
-        LineState actual = LineState.decideLineState(decision);
+    void decideFirstLineState(boolean decision, LineState expected) {
+        LineState actual = LineState.decideFirstLineState(decision);
         assertThat(actual).isEqualTo(expected);
     }
 
     @DisplayName("이전 State가 START이면 END를 반환하고 이외에는 boolean 값에 따라 결과를 반환한다")
     @ParameterizedTest
     @CsvSource(value = {"START,true,END", "START,false,END", "NONE,true,START", "END,false,NONE"})
-    void decideLineStateWithBeforeState(LineState beforeState, boolean given, LineState expected) {
-        LineState actual = LineState.decideLineStateWithBeforeState(beforeState, given);
+    void decideMiddleLineState(LineState beforeState, boolean given, LineState expected) {
+        LineState actual = LineState.decideMiddleLineState(beforeState, given);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @DisplayName("이전 State가 START이면 END를 반환하고 이외에는 NONE을 반환한다")
+    @ParameterizedTest
+    @CsvSource(value = {"START,END", "END,NONE", "NONE,NONE"})
+    void decideLastLineState(LineState beforeState, LineState expected) {
+        LineState actual = LineState.decideLastLineState(beforeState);
         assertThat(actual).isEqualTo(expected);
     }
 }
