e8f41b42cc9c29874fd1ea422d346db5dbc23e94
nayonsoso <panda0329@naver.com>
2024-02-28 21:35:52 +0900

    test: 사다리 게임 실행 결과 검증 테스트

--- src/main/java/domain/LadderGame.java
+++ src/main/java/domain/LadderGame.java
@@ -1,12 +1,20 @@
 package domain;
 
 import domain.ladder.Ladder;
+import domain.player.Player;
 import domain.player.Players;
+import domain.result.Result;
 import domain.result.Results;
+
+import java.util.Map;
 
 public class LadderGame {
 
     public LadderGame(Players players, Ladder ladder, Results results) {
 
+    }
+
+    public Map<Player, Result> play() {
+        return null;
     }
 }
--- src/test/java/domain/LadderGameTest.java
+++ src/test/java/domain/LadderGameTest.java
@@ -5,25 +5,36 @@ import domain.ladder.Ladder;
 import domain.ladder.stick.Stick;
 import domain.ladder.stick.StickGenerator;
 import domain.ladder.sticks.SticksGenerator;
+import domain.player.Player;
 import domain.player.Players;
+import domain.result.Result;
 import domain.result.Results;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LadderGameTest {
 
-    @DisplayName("생성 테스트")
+    @DisplayName("사다리 타기를 실행하면, 사용자에 따른 실행 결과가 나온다.")
     @Test
     void create() {
-        Players players = new Players(List.of("산초, 아톰"));
-        Ladder ladder = new Ladder(new Height(1), players.getPlayerSize(), mockSticksGenerator());
-        Results results = new Results(List.of("꽝, 당첨"), players.getPlayerSize());
+        int height = 2;
+        Players players = new Players(List.of("산초", "아톰"));
+        Ladder ladder = new Ladder(new Height(height), players.getPlayerSize(), mockSticksGenerator());
+        Results results = new Results(List.of("꽝", "당첨"), players.getPlayerSize());
+        LadderGame ladderGame = new LadderGame(players, ladder, results);
 
-        Assertions.assertThatCode(() -> new LadderGame(players, ladder, results))
-                .doesNotThrowAnyException();
+        Map<Player, Result> playerResult = ladderGame.play();
+
+        assertAll(
+                () -> assertEquals("꽝", playerResult.get("산초").getValue()),
+                () -> assertEquals("당첨", playerResult.get("아톰").getValue())
+        );
     }
 
     private StickGenerator mockStickGenerator() {
