68494737f8e1e4f8538aa8a9c3c99a7bae3850f3
ehtjsv2 <ehtjsv2@naver.com>
2024-02-28 14:03:52 +0900

    feat: 보상이 5글자 넘을경우 예외처리 구현 및 정상 생성 테스트케이스 추가

--- src/main/java/domain/Reward.java
+++ src/main/java/domain/Reward.java
@@ -1,10 +1,19 @@
 package domain;
 
 public class Reward {
+    private static final int MAXIMUM_REWARD_LENGTH = 5;
+
     private final String reward;
 
     public Reward(String reward) {
+        validateLength(reward);
         this.reward = reward;
+    }
+
+    private void validateLength(String reward) {
+        if (reward.length() > MAXIMUM_REWARD_LENGTH) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public String getReward() {
--- src/test/java/domain/RewardTest.java
+++ src/test/java/domain/RewardTest.java
@@ -3,13 +3,23 @@ package domain;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class RewardTest {
 
     @DisplayName("보상 객체를 정상 생성합니다.")
-    @Test
-    void createReward() {
-        Assertions.assertThatCode(() -> new Reward("0"))
+    @ParameterizedTest
+    @ValueSource(strings = {"꽝", "당첨", "당첨당첨당"})
+    void createReward(String validRewardName) {
+        Assertions.assertThatCode(() -> new Reward(validRewardName))
                 .doesNotThrowAnyException();
+    }
+
+    @DisplayName("보상이 5글자가 넘을 경우 예외처리")
+    @Test
+    void createRewardWithOverLength() {
+        Assertions.assertThatThrownBy(() -> new Reward("당첨당첨당첨"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
