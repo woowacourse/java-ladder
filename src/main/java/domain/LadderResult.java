1805e70937cc15069455e9773492989b666e982b
slimsha2dy <chy2581@naver.com>
2024-02-28 21:09:33 +0900

    feat: "all"을 입력받을 경우 모든 결과를 출력하는 기능 구현

--- src/main/java/domain/LadderResult.java
+++ src/main/java/domain/LadderResult.java
@@ -1,5 +1,7 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -12,8 +14,21 @@ public class LadderResult {
     }
 
     public List<String> getWinning(String player) {
+        if (player.equals("all")) {
+            return getAll();
+        }
         validateContains(player);
         return List.of(results.get(player));
+    }
+
+    private List<String> getAll() {
+        List<String> resultAll = new ArrayList<>();
+        for (Map.Entry<String, String> entry : results.entrySet()) {
+            String player = entry.getKey();
+            String winning = entry.getValue();
+            resultAll.add(player + " : " + winning);
+        }
+        return resultAll;
     }
 
     private void validateContains(String player) {
--- src/test/java/domain/LadderResultTest.java
+++ src/test/java/domain/LadderResultTest.java
@@ -9,13 +9,21 @@ import org.junit.jupiter.api.Test;
 
 public class LadderResultTest {
 
+    private LadderResult ladderResult;
+
+    @BeforeEach
+    void setLadderResult() {
+        ladderResult = new LadderResult(Map.of(
+                "사람1",
+                "보상1",
+                "사람2",
+                "보상2"
+        ));
+    }
+
     @DisplayName("이름을 입력받을 경우 저장된 결과를 반환한다.")
     @Test
     void returnCorrectWinning() {
-        LadderResult ladderResult = new LadderResult(Map.of(
-                "사람1",
-                "보상1"
-        ));
         Assertions.assertThat(ladderResult.getWinning("사람1"))
                 .isEqualTo(List.of("보상1"));
     }
@@ -23,12 +31,18 @@ public class LadderResultTest {
     @DisplayName("저장되지 않은 이름을 입력받을 경우 예외를 발생한다.")
     @Test
     void notSavedNameTest() {
-        LadderResult ladderResult = new LadderResult(Map.of(
-                "사람1",
-                "보상1"
-        ));
-        Assertions.assertThatThrownBy(() -> ladderResult.getWinning("사람2"))
+        Assertions.assertThatThrownBy(() -> ladderResult.getWinning("사람아님"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("입력되지 않은 사용자명입니다.");
+    }
+
+    @DisplayName("'all'을 입력받을 경우 전체 결과를 반환한다.")
+    @Test
+    void returnAllResult() {
+        Assertions.assertThat(ladderResult.getWinning("all"))
+                .isEqualTo(List.of(
+                        "사람1 : 보상1",
+                        "사람2 : 보상2"
+                ));
     }
 }
