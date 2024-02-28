66dd60a139650649c452197e86f6657902f4c484
DobbyMac <kimdobby@g.skku.edu>
2024-02-28 16:06:02 +0900

    feat(GameController): 사용자 결과 검색 기능 적용
    - 입력-검색-출력 로직 구현

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -1,14 +1,17 @@
 package controller;
 
 import domain.GameBoard;
+import domain.Prize;
 import domain.Prizes;
 import domain.ladder.Ladder;
 import domain.ladder.attirbute.Height;
+import domain.player.Name;
 import domain.player.Names;
 import domain.player.Players;
 import util.RandomDirectionGenerator;
 import view.OutputView;
 
+import java.util.Map;
 import java.util.stream.IntStream;
 
 import static util.RetryHelper.retryHelper;
@@ -19,14 +22,15 @@ import static view.UserMessage.*;
 
 public class GameController {
     public void execute() {
-        Players players = retryHelper(() -> new Players(new Names(inputNames())), PLAYER_INPUT_PROMPT);
+        Players players = retryHelper(() -> new Players(new Names(inputNames(PLAYER_INPUT_PROMPT))));
         int numberOfPlayers = players.getPlayerCount();
-        Prizes prizes = retryHelper(() -> new Prizes(inputNames(), numberOfPlayers), PRIZE_INPUT_PROMPT);
-        Height height = retryHelper(() -> new Height(input()), HEIGHT_INPUT_PROMPT);
+        Prizes prizes = retryHelper(() -> new Prizes(inputNames(PRIZE_INPUT_PROMPT), numberOfPlayers));
+        Height height = retryHelper(() -> new Height(input(HEIGHT_INPUT_PROMPT)));
         Ladder ladder = new Ladder(height, numberOfPlayers, new RandomDirectionGenerator());
         GameBoard gameBoard = new GameBoard(players, ladder, prizes);
 
         printGeneratedGameBoard(gameBoard);
+        showGeneratedResult(gameBoard);
     }
 
     private void printGeneratedGameBoard(GameBoard gameBoard) {
@@ -40,5 +44,27 @@ public class GameController {
         OutputView.printObjectNames(gameBoard.getPrizes()
                                              .getValue());
         OutputView.printNewLine();
+    }
+
+    private void showGeneratedResult(GameBoard gameBoard) {
+        boolean repeatFlag = true;
+        while (repeatFlag) {
+            Name targetName = retryHelper(() -> new Name(input(SEARCH_PLAYER_PROMPT)));
+
+            repeatFlag = showResultAndDetermineRepeat(gameBoard, targetName);
+        }
+    }
+
+    private boolean showResultAndDetermineRepeat(GameBoard gameBoard, Name targetName) {
+        OutputView.printNewLine();
+        OutputView.print(GAME_RESULT_HEADER);
+        if (targetName.value()
+                      .equals("all")) {
+            Map<Name, Prize> searchResults = gameBoard.searchAllPlayerResult();
+            searchResults.forEach((name, prize) -> OutputView.printAllResults(name.toString(), prize.toString()));
+            return false;
+        }
+        OutputView.print(gameBoard.searchOnePlayerResult(targetName));
+        return true;
     }
 }
