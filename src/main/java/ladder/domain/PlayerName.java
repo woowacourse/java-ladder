160c18a7d38bf7773a258f0ad112b33cdd63e9b8
kelly6bf <kelly6bf@gmail.com>
2024-02-28 15:22:12 +0900

    refactor: PlayerName hasCode 메서드 추가

--- src/main/java/ladder/domain/PlayerName.java
+++ src/main/java/ladder/domain/PlayerName.java
@@ -35,7 +35,12 @@ public record PlayerName(String value) {
     @Override
     public boolean equals(final Object o) {
         if (this == o) return true;
-        if (!(o instanceof PlayerName playerName)) return false;
-        return Objects.equals(value, playerName.value);
+        if (!(o instanceof final PlayerName that)) return false;
+        return Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
     }
 }
