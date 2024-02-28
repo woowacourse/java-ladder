12827c8d5a3a4830909c367f6b74660e2f72c3de
linirini <2001yerin@naver.com>
2024-02-28 14:49:51 +0900

    feat : 타깃에 대한 실행 결과 출력 기능

--- src/main/java/controller/LadderGame.java
+++ src/main/java/controller/LadderGame.java
@@ -1,9 +1,6 @@
 package controller;
 
-import domain.Height;
-import domain.Ladder;
-import domain.Names;
-import domain.RandomBridgeGenerator;
+import domain.*;
 import view.InputView;
 import view.OutputView;
 
@@ -15,17 +12,31 @@ public class LadderGame {
 
     public void start() {
         final Names names = new Names(inputView.readNames());
+        final Results results = Results.of(inputView.readResults(), names.count());
         final Height height = new Height(inputView.readHeight());
 
         final RandomBridgeGenerator randomBridgeGenerator = RandomBridgeGenerator.getInstance();
         final Ladder ladder = Ladder.createByStrategy(randomBridgeGenerator, height.getValue(), names.count());
 
-        printGameResult(names, ladder);
+        printGameResult(names, ladder, results);
+
+        PlayerResults playerResults = PlayerResults.of(names, ladder, results);
+        while (true) {
+            String target = inputView.readTarget();
+            if (target.equals("all")) {
+                for (final Name name : names.getValues()) {
+                    outputView.printPlayerResult(name.getValue(), playerResults.findResult(name.getValue()).getValue());
+                }
+                break;
+            }
+            outputView.printResult(playerResults.findResult(target).getValue());
+        }
     }
 
-    private void printGameResult(final Names names, final Ladder ladder) {
-        outputView.printResultMessage();
+    private void printGameResult(final Names names, final Ladder ladder, final Results results) {
+        outputView.printLadderResultMessage();
         outputView.printNames(names.getValues());
         outputView.printLadder(ladder.getLines());
+        outputView.printResults(results.getValues());
     }
 }
