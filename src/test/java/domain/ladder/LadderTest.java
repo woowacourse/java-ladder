04f71c75f13eb0fa9a298e356ea6d38cc9a07c34
PgmJun <chltmdwns96@gmail.com>
2024-02-28 18:14:05 +0900

    refactor : 테스트 내부 클래스로 분리

--- src/test/java/domain/ladder/LadderTest.java
+++ src/test/java/domain/ladder/LadderTest.java
@@ -2,10 +2,6 @@ package domain.ladder;
 
 import common.exception.model.ValidationException;
 import domain.bridge.strategy.BridgeGeneratorStub;
-import domain.ladder.Ladder;
-import domain.ladder.LadderBridge;
-import domain.ladder.LadderHeight;
-import domain.ladder.LadderResults;
 import domain.player.PlayerName;
 import domain.player.PlayerNames;
 import org.assertj.core.api.Assertions;
@@ -82,92 +78,96 @@ public class LadderTest {
                                     new PlayerName("h"), new PlayerName("i"), new PlayerName("j"), new PlayerName("k")))
             );
         }
+
+
+        @ParameterizedTest
+        @MethodSource("createLadderFailByHeightArguments")
+        @DisplayName("사다리 높이가 2 미만, 10 초과이면 사람의 수가 2 이상, 10 이하여도 예외가 발생한다")
+        void createLadderFailByHeightAndPointCount(int height, LadderResults results, PlayerNames playerNames) {
+            Assertions.assertThatThrownBy(
+                            () -> Ladder.create(new LadderHeight(height), playerNames, results, new BridgeGeneratorStub()))
+                    .isInstanceOf(ValidationException.class);
+        }
+
+        private static Stream<Arguments> createLadderFailByHeightArguments() {
+            return Stream.of(
+                    Arguments.arguments(1,
+                            new LadderResults(new String[]{"R", "R"}, 2),
+                            new PlayerNames(List.of(new PlayerName("a"), new PlayerName("b")))),
+
+                    Arguments.arguments(11,
+                            new LadderResults(new String[]{"R", "R", "R", "R", "R", "R", "R", "R", "R", "R"}, 10),
+                            new PlayerNames(List.of(new PlayerName("a"), new PlayerName("b"), new PlayerName("c"),
+                                    new PlayerName("d"), new PlayerName("e"), new PlayerName("f"), new PlayerName("g"),
+                                    new PlayerName("h"), new PlayerName("i"), new PlayerName("j"))))
+            );
+        }
     }
 
-    @ParameterizedTest
-    @MethodSource("createLadderFailByHeightArguments")
-    @DisplayName("사다리 높이가 2 미만, 10 초과이면 사람의 수가 2 이상, 10 이하여도 예외가 발생한다")
-    void createLadderFailByHeightAndPointCount(int height, LadderResults results, PlayerNames playerNames) {
-        Assertions.assertThatThrownBy(
-                        () -> Ladder.create(new LadderHeight(height), playerNames, results, new BridgeGeneratorStub()))
-                .isInstanceOf(ValidationException.class);
-    }
+    @Nested
+    @DisplayName("참가자 사다리 결과 테스트")
+    class PlayerLadderResultTest {
+        @ParameterizedTest
+        @MethodSource("getSinglePlayerLadderResultSuccessArguments")
+        @DisplayName("단일 참가자의 결과를 출력한다")
+        void getSinglePlayerLadderResultSuccess(LadderHeight height, LadderResults results, PlayerNames playerNames,
+                                                List<LadderBridge> bridges) {
+            //given
+            BridgeGeneratorStub bridgeGeneratorStub = new BridgeGeneratorStub();
+            bridgeGeneratorStub.setBridges(bridges);
+            Ladder ladder = Ladder.create(height, playerNames, results, bridgeGeneratorStub);
 
-    private static Stream<Arguments> createLadderFailByHeightArguments() {
-        return Stream.of(
-                Arguments.arguments(1,
-                        new LadderResults(new String[]{"R", "R"}, 2),
-                        new PlayerNames(List.of(new PlayerName("a"), new PlayerName("b")))),
+            //when
+            Map<String, String> player1Result = ladder.findSinglePlayerLadderResultValue("pA");
+            Map<String, String> player2Result = ladder.findSinglePlayerLadderResultValue("pB");
+            Map<String, String> player3Result = ladder.findSinglePlayerLadderResultValue("pC");
 
-                Arguments.arguments(11,
-                        new LadderResults(new String[]{"R", "R", "R", "R", "R", "R", "R", "R", "R", "R"}, 10),
-                        new PlayerNames(List.of(new PlayerName("a"), new PlayerName("b"), new PlayerName("c"),
-                                new PlayerName("d"), new PlayerName("e"), new PlayerName("f"), new PlayerName("g"),
-                                new PlayerName("h"), new PlayerName("i"), new PlayerName("j"))))
-        );
-    }
+            //then
+            assertAll(
+                    () -> Assertions.assertThat(player1Result.get("pA")).isEqualTo("O"),
+                    () -> Assertions.assertThat(player2Result.get("pB")).isEqualTo("X"),
+                    () -> Assertions.assertThat(player3Result.get("pC")).isEqualTo("O")
+            );
+        }
 
+        private static Stream<Arguments> getSinglePlayerLadderResultSuccessArguments() {
+            return Stream.of(
+                    Arguments.arguments(new LadderHeight(2),
+                            new LadderResults(new String[]{"O", "X", "O"}, 3),
+                            new PlayerNames(List.of(new PlayerName("pA"), new PlayerName("pB"), new PlayerName("pC"))),
+                            List.of(LadderBridge.BRIDGE, LadderBridge.NONE)
+                    )
+            );
+        }
 
-    @ParameterizedTest
-    @MethodSource("getSinglePlayerLadderResultSuccessArguments")
-    @DisplayName("단일 참가자의 결과를 출력한다")
-    void getSinglePlayerLadderResultSuccess(LadderHeight height, LadderResults results, PlayerNames playerNames,
-                                            List<LadderBridge> bridges) {
-        //given
-        BridgeGeneratorStub bridgeGeneratorStub = new BridgeGeneratorStub();
-        bridgeGeneratorStub.setBridges(bridges);
-        Ladder ladder = Ladder.create(height, playerNames, results, bridgeGeneratorStub);
+        @ParameterizedTest
+        @MethodSource("getAllPlayersLadderResultSuccessByArguments")
+        @DisplayName("전체 참가자의 결과를 조회한다")
+        void test(LadderHeight height, LadderResults results, PlayerNames playerNames, List<LadderBridge> bridges) {
+            //given
+            BridgeGeneratorStub bridgeGeneratorStub = new BridgeGeneratorStub();
+            bridgeGeneratorStub.setBridges(bridges);
+            Ladder ladder = Ladder.create(height, playerNames, results, bridgeGeneratorStub);
 
-        //when
-        Map<String, String> player1Result = ladder.findSinglePlayerLadderResultValue("pA");
-        Map<String, String>  player2Result = ladder.findSinglePlayerLadderResultValue("pB");
-        Map<String, String>  player3Result = ladder.findSinglePlayerLadderResultValue("pC");
+            //when
+            Map<String, String> result = ladder.findAllPlayersLadderResultValue();
 
-        //then
-        assertAll(
-                () -> Assertions.assertThat(player1Result.get("pA")).isEqualTo("O"),
-                () -> Assertions.assertThat(player2Result.get("pB")).isEqualTo("X"),
-                () -> Assertions.assertThat(player3Result.get("pC")).isEqualTo("O")
-        );
-    }
+            //then
+            assertAll(
+                    () -> Assertions.assertThat(result.get("pA")).isEqualTo("O"),
+                    () -> Assertions.assertThat(result.get("pB")).isEqualTo("X"),
+                    () -> Assertions.assertThat(result.get("pC")).isEqualTo("O")
+            );
+        }
 
-    private static Stream<Arguments> getSinglePlayerLadderResultSuccessArguments() {
-        return Stream.of(
-                Arguments.arguments(new LadderHeight(2),
-                        new LadderResults(new String[]{"O", "X", "O"}, 3),
-                        new PlayerNames(List.of(new PlayerName("pA"), new PlayerName("pB"), new PlayerName("pC"))),
-                        List.of(LadderBridge.BRIDGE, LadderBridge.NONE)
-                )
-        );
-    }
-
-    @ParameterizedTest
-    @MethodSource("getAllPlayersLadderResultSuccessByArguments")
-    @DisplayName("전체 참가자의 결과를 조회한다")
-    void test(LadderHeight height, LadderResults results, PlayerNames playerNames, List<LadderBridge> bridges) {
-        //given
-        BridgeGeneratorStub bridgeGeneratorStub = new BridgeGeneratorStub();
-        bridgeGeneratorStub.setBridges(bridges);
-        Ladder ladder = Ladder.create(height, playerNames, results, bridgeGeneratorStub);
-
-        //when
-        Map<String, String> result = ladder.findAllPlayersLadderResultValue();
-
-        //then
-        assertAll(
-                () -> Assertions.assertThat(result.get("pA")).isEqualTo("O"),
-                () -> Assertions.assertThat(result.get("pB")).isEqualTo("X"),
-                () -> Assertions.assertThat(result.get("pC")).isEqualTo("O")
-        );
-    }
-
-    private static Stream<Arguments> getAllPlayersLadderResultSuccessByArguments() {
-        return Stream.of(
-                Arguments.arguments(new LadderHeight(2),
-                        new LadderResults(new String[]{"O", "X", "O"}, 3),
-                        new PlayerNames(List.of(new PlayerName("pA"), new PlayerName("pB"), new PlayerName("pC"))),
-                        List.of(LadderBridge.BRIDGE, LadderBridge.NONE)
-                )
-        );
+        private static Stream<Arguments> getAllPlayersLadderResultSuccessByArguments() {
+            return Stream.of(
+                    Arguments.arguments(new LadderHeight(2),
+                            new LadderResults(new String[]{"O", "X", "O"}, 3),
+                            new PlayerNames(List.of(new PlayerName("pA"), new PlayerName("pB"), new PlayerName("pC"))),
+                            List.of(LadderBridge.BRIDGE, LadderBridge.NONE)
+                    )
+            );
+        }
     }
 }
