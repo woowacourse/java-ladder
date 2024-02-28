09f76c9d19b5b028687c757ddee9fac396133eea
tackyu <tack970913@gmail.com>
2024-02-28 20:23:49 +0900

    test: 사다리 게임 생성 테스트 코드 추가

---
+++ src/test/java/domain/model/LadderGameTest.java
@@ -0,0 +1,23 @@
+package domain.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+
+public class LadderGameTest {
+    @Test
+    @DisplayName("사다리게임을 만든다")
+    void makeLadderGame() {
+        //given
+        Ladder ladder = new Ladder("2", 2);
+        People people=new People(List.of("a","b"));
+        ResultCandidate resultCandidate=new ResultCandidate(List.of("꽝","100"));
+        //when
+        LadderGame ladderGame=new LadderGame(ladder,people);
+        //then
+        assertThatCode(()->new LadderGame(ladder,people)).doesNotThrowAnyException();
+    }
+}
