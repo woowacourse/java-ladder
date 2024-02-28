f992be7f31d575f40edc67355e181820e9f0f62d
nayonsoso <panda0329@naver.com>
2024-02-28 21:28:23 +0900

    refactor: 사다리 게임 생성자에 인자 추가

--- src/main/java/domain/LadderGame.java
+++ src/main/java/domain/LadderGame.java
@@ -1,4 +1,12 @@
 package domain;
 
+import domain.ladder.Ladder;
+import domain.player.Players;
+import domain.result.Results;
+
 public class LadderGame {
+
+    public LadderGame(Players players, Ladder ladder, Results results) {
+
+    }
 }
--- src/test/java/domain/LadderGameTest.java
+++ src/test/java/domain/LadderGameTest.java
@@ -1,15 +1,36 @@
 package domain;
 
+import domain.ladder.Height;
+import domain.ladder.Ladder;
+import domain.ladder.stick.Stick;
+import domain.ladder.stick.StickGenerator;
+import domain.ladder.sticks.SticksGenerator;
+import domain.player.Players;
+import domain.result.Results;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 class LadderGameTest {
 
     @DisplayName("생성 테스트")
     @Test
     void create() {
-        Assertions.assertThatCode(() -> new LadderGame())
+        Players players = new Players(List.of("산초, 아톰"));
+        Ladder ladder = new Ladder(new Height(1), players.getPlayerSize(), mockSticksGenerator());
+        Results results = new Results(List.of("꽝, 당첨"), players.getPlayerSize());
+
+        Assertions.assertThatCode(() -> new LadderGame(players, ladder, results))
                 .doesNotThrowAnyException();
+    }
+
+    private StickGenerator mockStickGenerator() {
+        return () -> Stick.FILLED;
+    }
+
+    private SticksGenerator mockSticksGenerator() {
+        return (mockStickGenerator) -> List.of(Stick.FILLED, Stick.NOT_FILLED);
     }
 }
