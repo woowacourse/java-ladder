c4cc0539ffb42bdd7b8bc013a7f0bc81aeeb0e35
ashsty <qkralsdk991@naver.com>
2024-02-28 14:27:52 +0900

    feat : Prize (실행 결과) 클래스 작성

---
+++ src/test/java/model/PrizeTest.java
@@ -0,0 +1,30 @@
+package model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class PrizeTest {
+    @DisplayName("당첨 프라이즈 이름은 5자를 초과할 수 없다.")
+    @Test
+    void validateNameLength() {
+        String given = "123456";
+        assertThatThrownBy(() -> new Prize(given))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
+    }
+
+    @DisplayName("당첨 프라이즈 이름은 null이거나 공백이면 안된다.")
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {"", "     ","\t","\n"})
+    void validateNameNotNullAndNotBlank(String given) {
+        assertThatThrownBy(() -> new Prize(given))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
+    }
+}
