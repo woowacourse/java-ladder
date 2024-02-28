e31161a7d026231bcfd42ef80616ffc749c12d5d
yechop <channetattoo@gmail.com>
2024-02-28 15:01:16 +0900

    refactor: 사다리를 생성할때 Prizes 검증 구현

--- src/main/java/domain/Ladder.java
+++ src/main/java/domain/Ladder.java
@@ -9,17 +9,26 @@ import java.util.List;
 public class Ladder {
 
     private final List<RowLine> lines;
+    private final Prizes prizes;
 
-    private Ladder(List<RowLine> lines) {
+    private Ladder(List<RowLine> lines, Prizes prizes) {
         this.lines = lines;
+        this.prizes = prizes;
     }
 
-    public static Ladder createFrom(RowLineGenerator rowLineGenerator, Integer personCount, Height height) {
+    public static Ladder createFrom(RowLineGenerator rowLineGenerator, Integer personCount, Height height, Prizes prizes) {
+        validatePersonPrizesCount(personCount, prizes);
         List<RowLine> lines = new ArrayList<>();
         for (int i = 0; i < height.getHeight(); i++) {
             lines.add(rowLineGenerator.generate(personCount));
         }
-        return new Ladder(lines);
+        return new Ladder(lines, prizes);
+    }
+
+    private static void validatePersonPrizesCount(Integer personCount, Prizes prizes) {
+        if (prizes.getPrizeCount() != personCount) {
+            throw new IllegalArgumentException("[ERROR] 상품의 수는 참여자 수와 일치해야 합니다.");
+        }
     }
 
     public List<RowLine> getLines() {
