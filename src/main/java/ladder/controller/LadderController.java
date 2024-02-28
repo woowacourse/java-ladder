d5996528a347e1d37ce2cbb04dd2c007e6e2c332
seokmyungham <hellotjraud@gmail.com>
2024-02-28 17:36:30 +0900

    feat(LadderController): 사람 이름을 입력받고 흐름을 처리하는 메서드 및 로직 추가

--- src/main/java/ladder/controller/LadderController.java
+++ src/main/java/ladder/controller/LadderController.java
@@ -2,6 +2,8 @@ package ladder.controller;
 
 import java.util.List;
 import java.util.function.Supplier;
+import ladder.domain.GameResult;
+import ladder.domain.Prizes;
 import ladder.domain.dto.LadderResponseDto;
 import ladder.domain.ladder.Ladder;
 import ladder.domain.ladder.RungGenerator;
@@ -10,6 +12,8 @@ import ladder.view.InputView;
 import ladder.view.OutputView;
 
 public class LadderController {
+
+    private static final String ALL_RESULT_INPUT_KEYWORD = "all";
 
     private final InputView inputView;
     private final OutputView outputView;
@@ -24,11 +28,24 @@ public class LadderController {
     public void run() {
         Participants participants = repeatUntilValid(this::getParticipants);
         int participantsCount = participants.getCount();
-
+        Prizes prizes = repeatUntilValid(() -> getPrizes(participantsCount));
         Ladder ladder = repeatUntilValid(() -> getLadder(participantsCount));
-        LadderResponseDto ladderResponseDto = ladder.getLadderResult();
+        printLadderResult(participants, prizes, ladder);
 
-        outputView.printResult(ladderResponseDto, participants.getNames());
+        Prizes sortedPrizes = ladder.getSortedPrizesResult(participants, prizes);
+        GameResult gameResult = new GameResult(participants, sortedPrizes);
+        String name = repeatUntilValid(() -> getName(gameResult));
+        printLadderGameResult(gameResult, name);
+    }
+
+
+    private <T> T repeatUntilValid(Supplier<T> function) {
+        try {
+            return function.get();
+        } catch (IllegalArgumentException e) {
+            outputView.printError(e);
+            return repeatUntilValid(function);
+        }
     }
 
     private Participants getParticipants() {
@@ -41,12 +58,33 @@ public class LadderController {
         return new Ladder(height, participantsCount, rungGenerator);
     }
 
-    private <T> T repeatUntilValid(Supplier<T> function) {
-        try {
-            return function.get();
-        } catch (IllegalArgumentException e) {
-            outputView.printError(e);
-            return repeatUntilValid(function);
+    private Prizes getPrizes(int participantsCount) {
+        List<String> prizeNames = inputView.getPrizeNames();
+        return new Prizes(prizeNames, participantsCount);
+    }
+
+    private void printLadderResult(Participants participants, Prizes prizes, Ladder ladder) {
+        LadderResponseDto ladderResponseDto = ladder.getLadderResult();
+        outputView.printLadderResult(ladderResponseDto, participants.getNames(), prizes.getNames());
+    }
+
+    private String getName(GameResult gameResult) {
+        String name = inputView.getName();
+
+        if (name.equals(ALL_RESULT_INPUT_KEYWORD)) {
+            return name;
+        }
+        gameResult.containsName(name);
+        return name;
+    }
+
+    private void printLadderGameResult(GameResult gameResult, String nameSearch) {
+        if (nameSearch.equals(ALL_RESULT_INPUT_KEYWORD)) {
+            outputView.printAllMatchResult(gameResult.getGameResult());
+        }
+
+        if (!nameSearch.equals(ALL_RESULT_INPUT_KEYWORD)) {
+            outputView.printNameMatchResult(gameResult.getGameResult(), nameSearch);
         }
     }
 }
