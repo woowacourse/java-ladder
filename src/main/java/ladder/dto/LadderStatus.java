ea86881c6e82f14fa4a795188e2b2c94677e7921
kelly6bf <kelly6bf@gmail.com>
2024-02-28 16:36:29 +0900

    refactor: LadderStatus of 메서드 로직 수정

--- src/main/java/ladder/dto/LadderStatus.java
+++ src/main/java/ladder/dto/LadderStatus.java
@@ -2,17 +2,20 @@ package ladder.dto;
 
 import ladder.domain.GameResults;
 import ladder.domain.Ladder;
+import ladder.domain.Paths;
 import ladder.domain.PlayerNames;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public record LadderStatus(List<String> playerNames, List<PathStatuses> pathStatuses, List<String> gameResults) {
 
     public static LadderStatus of(PlayerNames playerNames, Ladder ladder, GameResults gameResults) {
-        List<PathStatuses> pathStatuses = ladder.getPaths()
-                .stream()
-                .map(paths -> new PathStatuses(paths.getPathStatuses()))
-                .toList();
+        List<Paths> paths = ladder.getPaths();
+        List<PathStatuses> pathStatuses = new ArrayList<>();
+        for (int i = paths.size() - 1; i >= 0; i--) {
+            pathStatuses.add(new PathStatuses(paths.get(i).getPathStatuses()));
+        }
 
         return new LadderStatus(playerNames.getPlayerNames(), pathStatuses, gameResults.getGameResults());
     }
