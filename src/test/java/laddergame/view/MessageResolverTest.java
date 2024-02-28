3e1621cd3ba7a1e4eed72fde9154e9a5490802b5
coli-geonwoo <kkwoo001021@naver.com>
2024-02-28 14:34:21 +0900

    test: 테스트 케이스 추가

--- src/test/java/laddergame/view/MessageResolverTest.java
+++ src/test/java/laddergame/view/MessageResolverTest.java
@@ -1,6 +1,7 @@
 package laddergame.view;
 
 import laddergame.domain.connectiongenerator.AllFalseConnectionGenerator;
+import laddergame.domain.connectiongenerator.TrueFalseConnectionGenerator;
 import laddergame.domain.gameelements.Elements;
 import laddergame.domain.ladder.Ladder;
 import org.junit.jupiter.api.DisplayName;
@@ -23,9 +24,15 @@ class MessageResolverTest {
     @Test
     @DisplayName("사다리 출력 테스트")
     void makeLadderMessageTest() {
-        Ladder testLadder = new Ladder(1, 3, new AllFalseConnectionGenerator());
-        String expectedLadder = "    |     |     |";
-        assertEquals(expectedLadder
-                , MessageResolver.resolveLadderMessage(testLadder));
+        Ladder testLadder1 = new Ladder(1, 3, new AllFalseConnectionGenerator());
+        Ladder testLadder2 = new Ladder(1, 4, new TrueFalseConnectionGenerator());
+
+        String expectedLadder1 = "    |     |     |";
+        String expectedLadder2 = "    |-----|     |-----|";
+
+        assertEquals(expectedLadder1
+                , MessageResolver.resolveLadderMessage(testLadder1));
+        assertEquals(expectedLadder2
+                , MessageResolver.resolveLadderMessage(testLadder2));
     }
 }
