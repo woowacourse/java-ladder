ed1f1a188af0ca5ef13a5c9d1aefb409d95dcc92
SCY <helenason@naver.com>
2024-02-28 20:25:19 +0900

    fix: 비정상적인 게임 결과 도출하는 메서드 수정

--- src/main/java/domain/Game.java
+++ src/main/java/domain/Game.java
@@ -23,8 +23,15 @@ public class Game {
 
             for (Line line : lines.getLines()) {
                 List<Connection> connections = line.getConnections();
-                index = tryMoveLeft(connections, index);
-                index = tryMoveRight(connections, index);
+                boolean canMoveLeft = canMoveLeft(connections, index);
+                if (canMoveLeft) {
+                    index--;
+                    continue;
+                }
+                boolean canMoveRight = canMoveRight(connections, index);
+                if (canMoveRight) {
+                    index++;
+                }
             }
             Result result = results.getResultByIndex(index);
             gameResult.addGameResult(member, result);
@@ -32,26 +39,26 @@ public class Game {
         return gameResult;
     }
 
-    private int tryMoveLeft(List<Connection> connections, int index) {
+    private boolean canMoveLeft(List<Connection> connections, int index) {
         if (index <= 0) {
-            return index;
+            return false;
         }
         Connection left = connections.get(index - 1);
         if (left.equals(Connection.CONNECTED)) {
-            index--;
+            return true;
         }
-        return index;
+        return false;
     }
 
-    private int tryMoveRight(List<Connection> connections, int index) {
-        if (index >= connections.size() - 1) {
-            return index;
+    private boolean canMoveRight(List<Connection> connections, int index) {
+        if (index > connections.size() - 1) {
+            return false;
         }
         Connection right = connections.get(index);
         if (right.equals(Connection.CONNECTED)) {
-            index++;
+            return true;
         }
-        return index;
+        return false;
     }
 
     public Members getMembers() {
