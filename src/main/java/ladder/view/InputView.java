ce42f01257744a553c8294ef921b1838e3e9299a
Gyeongho Yang <chchch1213@naver.com>
2024-02-28 17:54:40 +0900

    feat: 결과를 보고 싶은 사람 입력 기능 구현

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -11,6 +11,7 @@ public class InputView {
     private static final String ASK_PEOPLE_NAMES = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     private static final String ASK_PRIZE_NAMES = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
     private static final String ASK_LADDER_HEIGHT = "최대 사다리 높이는 몇 개인가요?";
+    private static final String ASK_VIEWER_NAME = "결과를 보고 싶은 사람은?";
 
     private InputView() {
     }
@@ -37,5 +38,13 @@ public class InputView {
 
         OutputView.printNewLine();
         return Arrays.stream(prizeNames.split(PRIZE_NAMES_DELIMITER)).toList();
+    }
+
+    public static String readViewerName() {
+        OutputView.printMessage(ASK_VIEWER_NAME);
+        String viewerName = SCANNER.nextLine();
+
+        OutputView.printNewLine();
+        return viewerName;
     }
 }
