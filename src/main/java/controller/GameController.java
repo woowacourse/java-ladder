97b52baa6e25fde3f6f615712c97ca69989cfce3
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-02-28 14:33:56 +0900

    feat(GameController) : 사다리 게임 진행 로직 반영
    - 기능 구현 완료에 따라 README 갱신
    - Controller 내 함수들 분리

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -1,29 +1,43 @@
 package controller;
 
-import domain.GameBoard;
-import domain.Rewards;
+import domain.*;
 import domain.ladder.Ladder;
 import domain.ladder.common.Height;
+import domain.player.Name;
 import domain.player.Names;
+import domain.player.Player;
 import domain.player.Players;
 import util.RandomDirectionGenerator;
 import view.InputView;
 import view.OutputView;
 
+import java.util.List;
 import java.util.stream.IntStream;
 
 public class GameController {
+    private static final String ALL_COMMAND = "all";
+
     public void execute() {
         Players players = createPlayers();
-
         Rewards rewards = Rewards.from(InputView.inputRewards(), players.getPlayerCount());
-
         Height height = new Height(InputView.inputHeight());
-        
         Ladder ladder = new Ladder(height, players.getPlayerCount(), new RandomDirectionGenerator());
         GameBoard gameBoard = new GameBoard(players, ladder, rewards);
 
-        printResult(gameBoard);
+        printGameBoard(gameBoard);
+        processGameResults(gameBoard);
+    }
+
+    private void processGameResults(GameBoard gameBoard) {
+        while (true) {
+            String playerName = InputView.inputResultPlayer();
+            if (playerName.equals(ALL_COMMAND)) {
+                processAllPlayers(gameBoard);
+                break;
+            }
+            processSinglePlayer(gameBoard, playerName);
+
+        }
     }
 
     private Players createPlayers() {
@@ -31,15 +45,37 @@ public class GameController {
         return new Players(names);
     }
 
-    private void printResult(GameBoard gameBoard) {
-        OutputView.printPlayerNames(gameBoard.getPlayers()
-                                             .getPlayerNames());
+    private void processAllPlayers(GameBoard gameBoard) {
+        List<Result> results = gameBoard.playGameAllPlayer();
+        OutputView.printResult(results);
+    }
 
-        IntStream.range(0, gameBoard.getLadderHeight())
-                 .mapToObj(gameBoard::getDirectionAtHorizontalIndex)
+    private void processSinglePlayer(GameBoard gameBoard, String playerName) {
+        Name name = new Name(playerName);
+        Player player = gameBoard.findPlayerWithName(name);
+        Result result = gameBoard.playGameOnePlayer(player);
+        OutputView.printResult(result);
+    }
+
+
+    private void printGameBoard(GameBoard gameBoard) {
+        printPlayer(gameBoard.getPlayers());
+        printLadder(gameBoard.getLadder(), gameBoard.getLadderHeight());
+        printReward(gameBoard.getRewards());
+
+    }
+
+    private void printPlayer(Players players) {
+        OutputView.printPlayerNames(players.getPlayerNames());
+    }
+
+    private void printLadder(Ladder ladder, int height) {
+        IntStream.range(0, height)
+                 .mapToObj(ladder::getDirectionAtHorizontalIndex)
                  .forEach(OutputView::printDirections);
+    }
 
-        OutputView.printRewards(gameBoard.getRewards()
-                                         .getValue());
+    private void printReward(Rewards rewards) {
+        OutputView.printRewards(rewards.getValue());
     }
 }
