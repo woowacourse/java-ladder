7d36cc8f11dfcdb84a9c1eae8eed13cde39f4e77
ay-eonii <naayen@naver.com>
2024-02-28 09:33:57 +0900

    refactor(LadderGame): LadderGame에서 RandomPointsGenerator 호출

--- src/main/java/ladder/domain/Line.java
+++ src/main/java/ladder/domain/Line.java
@@ -5,34 +5,10 @@ import java.util.List;
 
 public class Line {
 
-    private final PointsGenerator pointsGenerator;
     private final List<Point> points;
 
-    public Line(int personCount, PointsGenerator pointsGenerator) {
-        this.pointsGenerator = pointsGenerator;
-        this.points = createRandomPoints(personCount - 1);
-        ensurePoints();
-    }
-
-    private List<Point> createRandomPoints(int size) {
-        List<Point> points = pointsGenerator.generate(size);
-        if (!points.contains(Point.ON)) {
-            return createRandomPoints(size);
-        }
-        return points;
-    }
-
-    private void ensurePoints() {
-        for (int i = 1; i < points.size(); i++) {
-            makePointEnsure(i);
-        }
-    }
-
-    private void makePointEnsure(int currentIndex) {
-        Point previous = points.get(currentIndex - 1);
-        if (previous == Point.ON) {
-            points.set(currentIndex, Point.OFF);
-        }
+    public Line(List<Point> points) {
+        this.points = points;
     }
 
     public List<Point> getPoints() {
@@ -40,10 +16,10 @@ public class Line {
     }
 
     public int ride(int position) {
-        if (points.get(position) == Point.ON) {
+        if (Point.ON.equals(points.get(position))) {
             return position + 1;
         }
-        if (position > 0 && points.get(position - 1) == Point.ON) {
+        if (position > 0 && Point.ON.equals(points.get(position - 1))) {
             return position - 1;
         }
         return position;
