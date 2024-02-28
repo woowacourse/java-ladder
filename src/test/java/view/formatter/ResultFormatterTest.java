1c6a4abd2f678f593e01c867eced4c0356169609
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-02-28 20:57:48 +0900

    test(ResultFormatterTest): 전체 결과일 경우, :를 기준으로 이름과 결과를 포맷하는 테스트 작성

---
+++ src/test/java/view/formatter/ResultFormatterTest.java
@@ -0,0 +1,26 @@
+package view.formatter;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+class ResultFormatterTest {
+    @Test
+    void format() {
+        Map<String, String> result = new LinkedHashMap<>();
+        result.put("pobi", "꽝");
+        result.put("honux", "3000");
+        result.put("crong", "꽝");
+        result.put("jk", "5000");
+
+        ResultFormatter resultFormatter = new ResultFormatter();
+        assertThat(resultFormatter.format(result)).isEqualTo(
+                "pobi : 꽝\n"
+                        + "honux : 3000\n"
+                        + "crong : 꽝\n"
+                        + "jk : 5000\n"
+        );
+    }
+}
