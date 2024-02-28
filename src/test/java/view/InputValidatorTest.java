16208f5cf029fac880e37e9052b8f311b036a04d
cutehumanS2 <cherrycoke@sookmyung.ac.kr>
2024-02-28 09:26:58 +0900

    test: 결과를 조회할 참여자 이름 입력값 검증 테스트

---
+++ src/test/java/view/InputValidatorTest.java
@@ -0,0 +1,38 @@
+package view;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import java.util.List;
+
+public class InputValidatorTest {
+
+    private static List<String> playerNames;
+
+    @BeforeAll
+    public static void setPlayerNames() {
+        playerNames = List.of("pobi", "honux", "crong", "jk");
+    }
+
+    @DisplayName("참여자 이름이 null이거나 공백이 입력되면 예외가 발생한다.")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void occurExceptionIfPlayerNameIsNullOrBlank(String name) {
+        assertThatThrownBy(() -> InputValidator.validatePlayerName(playerNames, name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("참여자 이름을 입력해 주세요.");
+    }
+
+    @DisplayName("참여자 이름이  참여자 목록에 없는 이름이면 예외가 발생한다.")
+    @Test
+    void occurExceptionIfPlayerNameIsNotExisted() {
+        String name = "name";
+        assertThatThrownBy(() -> InputValidator.validatePlayerName(playerNames, name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("참여자 목록에 없는 이름입니다.");
+    }
+}
