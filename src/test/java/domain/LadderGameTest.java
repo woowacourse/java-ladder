73a9d86d1e93f8dc8bdbfb35b8ad8c375dfd462e
slimsha2dy <chy2581@naver.com>
2024-02-28 21:46:18 +0900

    refactor: 테스트에서 반복되는 초기화 과정을 BeforeEach로 정리

--- src/test/java/domain/LadderGameTest.java
+++ src/test/java/domain/LadderGameTest.java
@@ -2,28 +2,36 @@ package domain;
 
 import java.util.Map;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import util.LadderSequence;
 
 import static util.Connection.CONNECTED;
 import static util.Connection.UNCONNECTED;
 
 public class LadderGameTest {
 
-    @DisplayName("입력받은 이름과 사다리 높이에 따른 결과를 String으로 반환한다.")
-    @Test
-    void getResultTest() {
-        LadderGame ladderGame = new LadderGame(
-                new Players(List.of("1", "2")),
+    private LadderGame ladderGame;
+
+    @BeforeEach
+    void setLadderGame() {
+        ladderGame = new LadderGame(
+                new Players(List.of("a", "b")),
                 new Winnings(List.of("1", "2"), 2),
                 new Height(1),
                 new CustomGenerator(List.of(false, true))
         );
+    }
+
+    @DisplayName("입력받은 이름과 사다리 높이에 따른 결과를 String으로 반환한다.")
+    @Test
+    void getResultTest() {
         Assertions.assertThat(ladderGame.getLadderSequence())
                 .isEqualTo(List.of(
-                        "     1     2",
+                        "     a     b",
                         UNCONNECTED.getBridge() + CONNECTED.getBridge(),
                         "     1     2"
                 ));
@@ -32,12 +40,6 @@ public class LadderGameTest {
     @DisplayName("라인들을 순회하며 연결된 인덱스의 List<Name>을 swap한 결과를 Map으로 반환한다.")
     @Test
     void swapForConnected() {
-        LadderGame ladderGame = new LadderGame(
-                new Players(List.of("a", "b")),
-                new Winnings(List.of("1", "2"), 2),
-                new Height(1),
-                new CustomGenerator(List.of(false, true))
-        );
         Assertions.assertThat(ladderGame.getResult())
                 .isEqualTo(Map.of("a", "2", "b", "1"));
     }
