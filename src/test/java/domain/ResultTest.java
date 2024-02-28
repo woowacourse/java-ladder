22b17c6b537d94aa4017df389cfc2cb637e59ad4
hangillee <skfcb10@naver.com>
2024-02-28 11:40:28 +0900

    refactor: 경계값 테스트 분리를 통해 최소 단위 테스트 작성

--- src/test/java/domain/ResultTest.java
+++ src/test/java/domain/ResultTest.java
@@ -5,25 +5,38 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class ResultTest {
     @Test
-    @DisplayName("실행 결과는 5글자를 넘을 수 없다.")
-    void printResult() {
-        String invalidResult = "500000";
-        String validResult = "50000";
+    @DisplayName("실행 결과는 5글자 이하다.")
+    void isResultUnderMaxLengthLimit() {
+        String result = "50000";
 
-        assertThrows(IllegalArgumentException.class, () -> new Result(invalidResult));
-        assertDoesNotThrow(() -> new Result(validResult));
+        assertDoesNotThrow(() -> new Result(result));
     }
 
     @Test
-    @DisplayName("실행 결과는 1글자 이상이어야 한다.")
-    void isValidResult() {
-        String invalidResult = "";
-        String validResult = "5";
+    @DisplayName("실행 결과가 5글자를 넘으면 예외가 발생한다.")
+    void printResult() {
+        String result = "500000";
 
-        assertThrows(IllegalArgumentException.class, () -> new Result(invalidResult));
-        assertDoesNotThrow(() -> new Result(validResult));
+        assertThrows(IllegalArgumentException.class, () -> new Result(result));
+    }
+
+    @Test
+    @DisplayName("실행 결과는 1글자 이상이다.")
+    void isValidResult() {
+        String result = "5";
+
+        assertDoesNotThrow(() -> new Result(result));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    @DisplayName("실행 결과가 비어있다면 예외가 발생한다.")
+    void isResultNotEmpty(final String result) {
+        assertThrows(IllegalArgumentException.class, () -> new Result(result));
     }
 }
