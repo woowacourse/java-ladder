c83a769c3cec542628f98d2e72ffcad5afed62fa
linirini <2001yerin@naver.com>
2024-02-28 19:50:54 +0900

    feat : 실행 결과 길이 검증 기능

--- src/main/java/domain/Result.java
+++ src/main/java/domain/Result.java
@@ -2,10 +2,20 @@ package domain;
 
 public class Result {
 
+    public static final String INVALID_RESULT_LENGTH = "실행 결과는 1글자 이상 5글자 이하이어야 합니다.";
+    private static final int MINIMUM_RESULT_LENGTH = 1;
+    private static final int MAXIMUM_RESULT_LENGTH = 5;
     private final String value;
 
     public Result(final String value) {
+        validateResultLength(value);
         this.value = value;
+    }
+
+    private void validateResultLength(final String value) {
+        if (value.length() < MINIMUM_RESULT_LENGTH || value.length() > MAXIMUM_RESULT_LENGTH) {
+            throw new IllegalArgumentException(INVALID_RESULT_LENGTH);
+        }
     }
 
     public String getValue() {
---
+++ src/test/java/domain/ResultTest.java
@@ -0,0 +1,33 @@
+package domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+
+class ResultTest {
+
+    @DisplayName("적절한 실행 결과가 주어지면 잘 생성된다.")
+    @Test
+    void createResult() {
+        //given
+        String result = "실행 결과";
+
+        //when & then
+        Assertions.assertThatCode(() -> new Result(result)).doesNotThrowAnyException();
+    }
+
+    @DisplayName("실행 결과가 1글자 미만 5글자 초과이면 예외를 던진다.")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 6})
+    void invalidLengthResult(int count) {
+        //given
+        String result = "r".repeat(count);
+
+        //when & then
+        Assertions.assertThatThrownBy(() -> new Result(result)).isInstanceOf(IllegalArgumentException.class).hasMessage(Result.INVALID_RESULT_LENGTH);
+    }
+
+}
