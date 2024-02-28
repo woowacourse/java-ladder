3012b6af3a3b828b26266baaa14582970a2af193
kelly6bf <kelly6bf@gmail.com>
2024-02-28 16:29:16 +0900

    test: 입력된 라인 번호와 결과가 존재하는 라인 번호가 같은지 체크하는 테스트 로직 작성

--- src/test/java/ladder/domain/GameResultTest.java
+++ src/test/java/ladder/domain/GameResultTest.java
@@ -20,4 +20,18 @@ public class GameResultTest {
         // Then
         assertThat(gameResult).isNotNull();
     }
+
+    @DisplayName("입력된 라인 번호와 결과가 존재하는 라인 번호가 같으면 true를 반환한다.")
+    @Test
+    void 결과_라인_번호와_같은지_체크() {
+        // Given
+        final GameResult gameResult = GameResult.of("꽝", 3);
+        final LineNumber inputLineNumber = new LineNumber(3);
+
+        // When
+        boolean isEqual = gameResult.isEqualLineNumber(inputLineNumber);
+
+        // Then
+        assertThat(isEqual).isTrue();
+    }
 }
