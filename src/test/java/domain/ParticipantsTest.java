caafa8cb99bd3a4721c81063b186d393f5f637ab
eunjungL <dldmswjd322@gmail.com>
2024-02-28 17:01:53 +0900

    test: 참가자 이름 0명 입력 예외 처리 테스트 추가

--- src/test/java/domain/ParticipantsTest.java
+++ src/test/java/domain/ParticipantsTest.java
@@ -1,12 +1,25 @@
 package domain;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 class ParticipantsTest {
+
+    @DisplayName("참가자 이름 0명 입력 예외 처리")
+    @ParameterizedTest
+    @ValueSource(strings = {",,,", ""})
+    void validateNamesLengthTest(String actual) {
+        List<String> names = List.of(actual.split(","));
+
+        assertThatThrownBy(() -> new Participants(names))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 
     @DisplayName("참가자들 이름 중복 에외 처리")
     @Test
