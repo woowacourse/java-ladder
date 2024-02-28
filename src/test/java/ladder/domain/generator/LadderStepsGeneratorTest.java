2a863b0bbd524d29142b330fa9fceb43db61fd46
JUHA <84626225+khabh@users.noreply.github.com>
2024-02-28 12:08:41 +0900

    refactor: LadderStepsGenerator 도메인 테스트 작성

--- src/test/java/ladder/domain/generator/LadderStepsGeneratorTest.java
+++ src/test/java/ladder/domain/generator/LadderStepsGeneratorTest.java
@@ -2,25 +2,26 @@ package ladder.domain.generator;
 
 import ladder.domain.ladder.LadderStep;
 import ladder.domain.ladder.size.LadderSize;
+import ladder.testutil.TestPathAvailabilityGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
-class RandomLadderStepsGeneratorTest {
+class LadderStepsGeneratorTest {
     @Test
     @DisplayName("올바른 크기의 LadderSteps를 생성한다.")
     void generateTest() {
         // given
-        int expectedWidth = 7;
-        int expectedHeight = 5;
+        int expectedWidth = 2;
+        int expectedHeight = 3;
         LadderSize ladderSize = new LadderSize(expectedWidth, expectedHeight);
-        RandomLadderStepsGenerator generator = new RandomLadderStepsGenerator(ladderSize);
+        LadderStepsGenerator ladderStepsGenerator = getLadderStepsGenerator();
 
         // when
-        List<LadderStep> ladderSteps = generator.generate();
+        List<LadderStep> ladderSteps = ladderStepsGenerator.generate(ladderSize);
 
         // then
         assertThat(ladderSteps.size()).isEqualTo(expectedHeight);
@@ -28,5 +29,11 @@ class RandomLadderStepsGeneratorTest {
             int ladderStepWidth = ladderStep.getLadderPaths().size();
             assertThat(ladderStepWidth).isEqualTo(expectedWidth);
         }
+    }
+
+    private static LadderStepsGenerator getLadderStepsGenerator() {
+        List<Boolean> pathAvailabilities = List.of(true, true, false, false, true, false);
+        PathAvailabilityGenerator pathAvailabilityGenerator = new TestPathAvailabilityGenerator(pathAvailabilities);
+        return new LadderStepsGenerator(pathAvailabilityGenerator);
     }
 }
