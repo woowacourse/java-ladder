ee419011185e77a54c2d9ef8b1d6181520cc4b5d
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-02-28 19:01:32 +0900

    test(NameParserTest): 실행 결과 목록이 쉼표로 구분하는 테스트 작성

--- src/test/java/view/parser/NameParserTest.java
+++ src/test/java/view/parser/NameParserTest.java
@@ -10,10 +10,18 @@ import view.parser.NameParser;
 class NameParserTest {
 
     @Test
-    @DisplayName("참여할 사람의 수를 입력받아 라인을 만든다.")
-    void parse() {
+    @DisplayName("참여할 사람의 수를 입력받아 쉼표로 구분한다.")
+    void parsePersonNames() {
         String text = "pobi,honux,crong,jk";
         List<String> names = NameParser.parse(text);
         assertThat(names).containsExactlyInAnyOrder("pobi", "honux", "crong", "jk");
+    }
+
+    @Test
+    @DisplayName("실행 결과 목록이 쉼표로 구분한다.")
+    void parseItemNames() {
+        String text = "꽝,5000,꽝,3000";
+        List<String> names = NameParser.parse(text);
+        assertThat(names).containsExactlyInAnyOrder("꽝", "5000", "꽝", "3000");
     }
 }
