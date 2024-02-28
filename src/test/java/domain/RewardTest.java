f559ee486b4bdc93013c591c3a0a292341eb65af
takoyakimchi <c4nyou@gmail.com>
2024-02-28 11:52:22 +0900

    refactor: unused import 제거

--- src/test/java/domain/RewardTest.java
+++ src/test/java/domain/RewardTest.java
@@ -6,8 +6,6 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EmptySource;
-import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class RewardTest {
--- src/test/java/domain/RewardsTest.java
+++ src/test/java/domain/RewardsTest.java
@@ -8,8 +8,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EmptySource;
-import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class RewardsTest {
