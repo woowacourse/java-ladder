a03675db911f64c11a11275c057c6192d1f74e83
nayonsoso <panda0329@naver.com>
2024-02-28 17:51:43 +0900

    test: 결과를 볼 사용자가 all 또는 기존 사용자들이 아니면 예외 발생 테스트

--- src/test/java/domain/TargetPlayerResultTest.java
+++ src/test/java/domain/TargetPlayerResultTest.java
@@ -17,4 +17,15 @@ public class TargetPlayerResultTest {
         Assertions.assertThatCode(() -> new TargetPlayerResult(name, names))
                 .doesNotThrowAnyException();
     }
+
+    @DisplayName("결과를 볼 사용자는 all 또는 기존 사용자들의 이름이 아니면 예외를 발생시킨다.")
+    @Test
+    void allowAllOrPlayerName() {
+        String name = "산초";
+        List<String> names = List.of("수달", "아톰");
+
+        Assertions.assertThatThrownBy(() -> new TargetPlayerResult(name, names))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("all 이나 기존 사용자 이름을 입력해야 합니다.");
+    }
 }
