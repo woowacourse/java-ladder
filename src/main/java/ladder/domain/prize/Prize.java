18a550946370792a8e71eb536f31436592a25549
masonkimseoul <masonkimseoul@gmail.com>
2024-02-28 09:41:36 +0900

    feat: Prize 이름 공백 포함 검증 기능 구현

--- src/main/java/ladder/domain/prize/Prize.java
+++ src/main/java/ladder/domain/prize/Prize.java
@@ -3,6 +3,7 @@ package ladder.domain.prize;
 public class Prize {
 
     private static final int MAX_NAME_LENGTH = 5;
+    private static final String BLANK = " ";
 
     private final String prizeName;
 
@@ -13,11 +14,18 @@ public class Prize {
 
     private void validatePrizeName(String name) {
         validatePrizeNameLength(name);
+        validateBlankInPrizeName(name);
     }
 
     private void validatePrizeNameLength(String name) {
         if (name.isEmpty() || name.length() > MAX_NAME_LENGTH) {
             throw new IllegalArgumentException("[ERROR] 실행 결과의 이름의 길이는 1~5글자여야 합니다.");
+        }
+    }
+
+    private void validateBlankInPrizeName(String name) {
+        if (name.contains(BLANK)) {
+            throw new IllegalArgumentException("[ERROR] 실행 결과의 이름 내에는 공백을 허용하지 않습니다.");
         }
     }
 }
