1b0cccf8ea7987011f7be4a5900700a4740048cc
mzeong <mjeong@sookmyung.ac.kr>
2024-02-29 22:56:24 +0900

    test(LineTest, PointTest): 방향에 따라 이동

--- src/test/java/ladder/domain/LineTest.java
+++ src/test/java/ladder/domain/LineTest.java
@@ -1,71 +1,82 @@
 package ladder.domain;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import java.util.List;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class LineTest {
 
-    @DisplayName("라인에 사람의 수 - 1만큼 좌표를 생성한다.")
-    @Test
-    void createLine() {
-        // given
+//    @DisplayName("라인에 사람의 수 - 1만큼 좌표를 생성한다.")
+//    @Test
+//    void createLine() {
+//        // given
 //        int personCount = 5;
-
-        // when
+//
+//        // when
 //        Line line = new Line(personCount, new RandomPointsGenerator());
 //        int pointsSize = line.getPoints().size();
-
-        // then
+//
+//        // then
 //        assertThat(pointsSize).isEqualTo(personCount - 1);
-    }
+//    }
+//
+//    @DisplayName("좌표가 하나 이상 사용되어야 한다.")
+//    @Test
+//    void createAllUnusedPoints() {
+//        // when & then
+//        assertThatIllegalArgumentException()
+//                .isThrownBy(() -> new Line(Point.UNUSED, Point.UNUSED, Point.UNUSED, Point.UNUSED));
+//    }
+//
+//    @DisplayName("좌표가 연속적으로 사용되어서는 안 된다.")
+//    @Test
+//    void createConsecutiveUsage() {
+//        // when & then
+//        assertThatIllegalArgumentException()
+//                .isThrownBy(() -> new Line(Point.USED, Point.USED, Point.UNUSED, Point.UNUSED));
+//    }
+//
+//    @DisplayName("사용된 좌표의 인덱스를 찾는다.")
+//    @Test
+//    void findUsedPointIndexes() {
+//        // given
+//        Line line = new Line(Point.USED, Point.UNUSED, Point.USED, Point.UNUSED);
+//
+//        // when
+//        List<Integer> usedPointIndexes = line.findUsedPointIndexes();
+//
+//        // then
+//        assertThat(usedPointIndexes).isEqualTo(List.of(0, 2));
+//    }
+//
+//    @DisplayName("특정 좌표의 사용 여부를 확인한다.")
+//    @ParameterizedTest
+//    @CsvSource(value = {"0, true", "1, false", "2, true", "3, false"})
+//    void isPointUsed(int index, boolean actual) {
+//        // given
+//        Line line = new Line(Point.USED, Point.UNUSED, Point.USED, Point.UNUSED);
+//
+//        // when
+//        boolean expected = line.isPointUsed(index);
+//
+//        // then
+//        assertThat(expected).isEqualTo(actual);
+//    }
 
-    @DisplayName("좌표가 하나 이상 사용되어야 한다.")
-    @Test
-    void createAllUnusedPoints() {
-        // when & then
-        assertThatIllegalArgumentException()
-                .isThrownBy(() -> new Line(Point.UNUSED, Point.UNUSED, Point.UNUSED, Point.UNUSED));
-    }
-
-    @DisplayName("좌표가 연속적으로 사용되어서는 안 된다.")
-    @Test
-    void createConsecutiveUsage() {
-        // when & then
-        assertThatIllegalArgumentException()
-                .isThrownBy(() -> new Line(Point.USED, Point.USED, Point.UNUSED, Point.UNUSED));
-    }
-
-    @DisplayName("사용된 좌표의 인덱스를 찾는다.")
-    @Test
-    void findUsedPointIndexes() {
-        // given
-        Line line = new Line(Point.USED, Point.UNUSED, Point.USED, Point.UNUSED);
-
-        // when
-        List<Integer> usedPointIndexes = line.findUsedPointIndexes();
-
-        // then
-        assertThat(usedPointIndexes).isEqualTo(List.of(0, 2));
-    }
-
-    @DisplayName("특정 좌표의 사용 여부를 확인한다.")
+    @DisplayName("방향에 따라 이동한다.")
     @ParameterizedTest
-    @CsvSource(value = {"0, true", "1, false", "2, true", "3, false"})
-    void isPointUsed(int index, boolean actual) {
+    @CsvSource(value = {"0, 1", "1, 0", "2, 2"})
+    void move(int indexValue, int actual) {
         // given
-        Line line = new Line(Point.USED, Point.UNUSED, Point.USED, Point.UNUSED);
+        Line line = new Line(Direction.RIGHT, Direction.LEFT, Direction.DOWN, Direction.DOWN);
+        Index index = new Index(indexValue);
 
         // when
-        boolean expected = line.isPointUsed(index);
+        Index expected = line.move(index);
 
         // then
-        assertThat(expected).isEqualTo(actual);
+        assertThat(expected.getValue()).isEqualTo(actual);
     }
 }
---
+++ src/test/java/ladder/domain/PointTest.java
@@ -0,0 +1,51 @@
+package ladder.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PointTest {
+
+    @DisplayName("왼쪽으로 이동한다.")
+    @Test
+    void moveLeft() {
+        // given
+        Point point = new Point(Direction.LEFT);
+        Index index = new Index(1);
+
+        // when
+        Index expected = point.move(index);
+
+        // then
+        assertThat(expected.getValue()).isEqualTo(0);
+    }
+
+    @DisplayName("오른쪽으로 이동한다.")
+    @Test
+    void moveRight() {
+        // given
+        Point point = new Point(Direction.RIGHT);
+        Index index = new Index(0);
+
+        // when
+        Index expected = point.move(index);
+
+        // then
+        assertThat(expected.getValue()).isEqualTo(1);
+    }
+
+    @DisplayName("그대로 이동한다.")
+    @Test
+    void moveDown() {
+        // given
+        Point point = new Point(Direction.DOWN);
+        Index index = new Index(1);
+
+        // when
+        Index expected = point.move(index);
+
+        // then
+        assertThat(expected.getValue()).isEqualTo(1);
+    }
+}
