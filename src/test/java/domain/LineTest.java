888a04e376b2fd9112f4398e620a9dc82fc2cc16
SungKyum Kim <sungkyum1@naver.com>
2024-02-29 16:04:23 +0900

    test: 다음 이동방향을 결정하는 기능 테스트 작성

--- src/test/java/domain/LineTest.java
+++ src/test/java/domain/LineTest.java
@@ -2,11 +2,17 @@ package domain;
 
 import domain.line.Line;
 import domain.line.Point;
+import domain.lines.LineGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class LineTest {
@@ -15,4 +21,43 @@ class LineTest {
     void isLineCannotNextToLine() {
         assertThrows(IllegalStateException.class, () -> new Line(List.of(Point.CONNECTED, Point.CONNECTED)));
     }
+
+    @ParameterizedTest
+    @MethodSource("directionPersonProvider")
+    @DisplayName("다음 이동 방향을 결정한다.")
+    void nextPosition(Direction direction, int position) {
+        int personCount = 5;
+        BooleanGenerator generator = new FixedBooleanGenerator(true);
+        LineGenerator lineGenerator = new LineGenerator(personCount, generator);
+        Line line = new Line(lineGenerator.createPoints());
+        // 라인 생성 결과
+        // [CONNECTED, DISCONNECTED, DISCONNECTED, CONNECTED]
+
+        assertEquals(direction, line.nextPosition(position));
+    }
+
+    static Stream<Arguments> directionPersonProvider() {
+        return Stream.of(
+                Arguments.of(Direction.RIGHT, 0),
+                Arguments.of(Direction.LEFT, 1),
+                Arguments.of(Direction.DOWN, 2),
+                Arguments.of(Direction.RIGHT, 3),
+                Arguments.of(Direction.LEFT, 4));
+    }
 }
+
+class FixedBooleanGenerator implements BooleanGenerator {
+    private boolean value;
+
+    public FixedBooleanGenerator(boolean value) {
+        this.value = value;
+    }
+
+    @Override
+    public Boolean generate() {
+        boolean result = value;
+        value = !value;
+        return result;
+    }
+}
+
