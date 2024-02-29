fb2b938bf07cf3a9048847a9e4e5f598aa787058
KB Kim <28584160+jimi567@users.noreply.github.com>
2024-02-29 11:15:13 +0900

    feat : Controller에 추가한 기능 호출
    Prizes, PrizeResults 유효한 값으로 초기화
    view를 통해 입력,
    view를 통해 결과 출력.

--- src/main/java/controller/LadderGameController.java
+++ src/main/java/controller/LadderGameController.java
@@ -2,13 +2,21 @@ package controller;
 
 import domain.Height;
 import domain.Ladder;
+import domain.Player;
 import domain.Players;
+import domain.Prize;
+import domain.PrizeResults;
+import domain.Prizes;
 import domain.booleangenerator.BooleanGenerator;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.function.Supplier;
 import view.InputView;
 import view.OutputView;
 
 public class LadderGameController {
+
+    private static final String GET_ALL_RESULT_OPERATOR = "all";
 
     private final InputView inputView;
     private final OutputView outputView;
@@ -22,18 +30,32 @@ public class LadderGameController {
 
     public void run() {
         Players players = initPlayers();
+        Prizes prizes = initPrizes(players.getSize());
         Height height = initHeight();
         Ladder ladder = initLadder(players.getSize(), height);
-
-        outputView.printResult(ladder.getLinesInformation(), players.getNames());
+        PrizeResults prizeResults = initPrizeResults(players, prizes, ladder);
+        outputView.printResult(ladder.getLinesInformation(), players.getNames(), prizes.getPrizeNames());
+        viewingUntilGetAll(prizeResults);
     }
 
     private Players initPlayers() {
         return repeatUntilValidInput(() -> new Players(inputView.readPlayerNames()));
     }
 
+    private Prizes initPrizes(int playerCount) {
+        return repeatUntilValidInput(() -> Prizes.of(inputView.readPrizeNames(), playerCount));
+    }
+
     private Height initHeight() {
         return repeatUntilValidInput(() -> new Height(inputView.readHeight()));
+    }
+
+    private Ladder initLadder(int playerCount, Height height) {
+        return new Ladder(playerCount, height, booleanGenerator);
+    }
+
+    private PrizeResults initPrizeResults(Players players, Prizes prizes, Ladder ladder) {
+        return repeatUntilValidInput(() -> PrizeResults.of(players, prizes, ladder));
     }
 
     private <R> R repeatUntilValidInput(Supplier<R> supplier) {
@@ -45,7 +67,25 @@ public class LadderGameController {
         }
     }
 
-    private Ladder initLadder(int playerCount, Height height) {
-        return new Ladder(playerCount, height, booleanGenerator);
+    private void viewingUntilGetAll(PrizeResults prizeResults) {
+        String operator;
+        do {
+            operator = repeatUntilValidInput(() -> viewLadderResult(prizeResults));
+        } while (!operator.equals(GET_ALL_RESULT_OPERATOR));
+    }
+
+    private String viewLadderResult(PrizeResults prizeResults) {
+        String operator = repeatUntilValidInput(inputView::readPlayerForResultViewing);
+        Map<String, String> results = getResults(prizeResults.getByOperate(operator));
+        outputView.printPrizeResult(results);
+        return operator;
+    }
+
+    private Map<String, String> getResults(Map<Player, Prize> prizeResults) {
+        Map<String, String> results = new LinkedHashMap<>();
+        for (Player key : prizeResults.keySet()) {
+            results.put(key.getName(), prizeResults.get(key).getPrizeName());
+        }
+        return results;
     }
 }
