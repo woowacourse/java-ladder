429f1976e99403118427c0a7e5caaed0606ccf48
ashsty <qkralsdk991@naver.com>
2024-02-29 11:17:04 +0900

    refactor : Line 생성자에 initialize 기능 포함
    - 메서드로 처리하던 initialize 기능 생성자로 위치 변경
    - 변수명 변경

--- src/main/java/model/Line.java
+++ src/main/java/model/Line.java
@@ -6,29 +6,18 @@ import java.util.List;
 public class Line {
     private final List<LineState> lineStates = new ArrayList<>();
 
-    public Line(final int peopleCount, List<Boolean> makeLadderDecision) {
-        validateCorrespondingInputSize(peopleCount, makeLadderDecision);
-        initializeLineStates(peopleCount, makeLadderDecision);
-    }
-
-    private void initializeLineStates(int peopleCount, List<Boolean> makeLadderDecision) {
-        int index = 0;
-        lineStates.add(LineState.decideLineState(makeLadderDecision.get(index)));
-        index += 1;
-        for (; index < peopleCount; index++) {
-            LineState beforeState = lineStates.get(index - 1);
-            LineState currentState = LineState.decideLineStateWithBeforeState(beforeState, makeLadderDecision.get(index));
+    public Line(List<Boolean> makeLadderOrNot) {
+        int lastMiddleStateIndex = makeLadderOrNot.size() - 1;
+        lineStates.add(LineState.decideFirstLineState(makeLadderOrNot.get(0)));
+        for (int middleIndex = 1; middleIndex <= lastMiddleStateIndex; middleIndex++) {
+            LineState beforeState = lineStates.get(middleIndex - 1);
+            LineState currentState = LineState.decideMiddleLineState(beforeState, makeLadderOrNot.get(middleIndex));
             lineStates.add(currentState);
         }
+        lineStates.add(LineState.decideLastLineState(lineStates.get(lastMiddleStateIndex)));
     }
 
     public List<LineState> getLineStates() {
         return lineStates;
-    }
-
-    private void validateCorrespondingInputSize(int peopleCount, List<Boolean> makeLadderDecision) {
-        if (peopleCount != makeLadderDecision.size()) {
-            throw new IllegalArgumentException("[ERROR] 랜덤 값 생성 개수가 총 참여자 수와 일치하지 않습니다.");
-        }
     }
 }
