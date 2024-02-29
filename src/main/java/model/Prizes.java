5756f2665bdab7138667c14f23de04a3f7573214
fram1998 <koust6u@pusan.ac.kr>
2024-02-29 18:36:09 +0900

    feat: 참가자의 수와 같은 갯수의 경품을 참가하는 검증기능 추가

--- src/main/java/model/Prizes.java
+++ src/main/java/model/Prizes.java
@@ -8,13 +8,20 @@ public class Prizes {
 
     private final List<Prize> prizes;
 
-    public Prizes(String inputPrizesText) {
+    public Prizes(String inputPrizesText,int personCount) {
         prizes =  Arrays.stream(inputPrizesText.split(DELIMITER))
                 .map(Prize::new)
                 .toList();
+        validateLegalLength(personCount);
     }
 
     public Prize findPrizeByIndex(int index) {
         return prizes.get(index);
+    }
+
+    private void validateLegalLength(int personCount) {
+        if (prizes.size() != personCount) {
+            throw new IllegalArgumentException("참가자의 인원 수와 맞게 경품도 입력해주세요.");
+        }
     }
 }
