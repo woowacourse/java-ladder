5df366e2b723df97774927a933e6922a692aea2f
devhoya97 <diajbh123@gmail.com>
2024-02-29 15:57:45 +0900

    refactor: 불변 멤버 변수 final 처리

--- src/main/java/domain/MatchingItems.java
+++ src/main/java/domain/MatchingItems.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class MatchingItems {
 
-    private List<String> items;
+    private final List<String> items;
 
     public MatchingItems(final List<String> items, final int playerCount) {
         validateCount(items.size(), playerCount);
