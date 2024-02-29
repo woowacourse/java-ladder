26e48c91ad319cbd9cfa5dc7665774492ed37846
KIMGYUTAE <kiimwf0819@gmail.com>
2024-02-29 14:38:42 +0900

    refactor(OutputView): 사다리게임 및 게임 결과 출력 기능 추가
    - Formatter를 활용하여 기능 분리

--- src/main/java/ladder/view/OutputView.java
+++ src/main/java/ladder/view/OutputView.java
@@ -1,9 +1,14 @@
 package ladder.view;
 
+import java.util.HashMap;
 import java.util.List;
+import ladder.domain.game.GameResource;
+import ladder.domain.game.GameResult;
 import ladder.domain.resource.direction.Direction;
 import ladder.domain.resource.ladder.Ladder;
 import ladder.domain.resource.line.Line;
+import ladder.domain.resource.prize.Prize;
+import ladder.domain.resource.prize.Prizes;
 import ladder.domain.resource.user.User;
 import ladder.domain.resource.user.Users;
 
@@ -14,55 +19,64 @@ public class OutputView {
     private static final String LINE_HORIZONTAL = "-----";
     private static final int MAX_NAME_LENGTH = 5;
 
+    private final Formatter formatter = new Formatter();
+
     public void printError(String message) {
         System.out.println(message);
     }
 
-    public void printLadderGameResult(Users users, Ladder ladder) {
+    public void printLadderGame(GameResource gameResource) {
         System.out.println("\n실행결과\n");
-        printUserNames(users);
-        printLadder(ladder);
+        printUsers(gameResource.getUsers());
+        printLadder(gameResource.getLadder());
+        printPrizes(gameResource.getPrizes());
     }
 
-    private void printUserNames(Users users) {
-        StringBuilder sb = new StringBuilder();
-        for (User user : users.getUsers()) {
-            String userName = user.getUserName();
-            sb.append(appendNameFormat(userName));
-        }
-        System.out.println(sb);
+    public void printUserResult(Prize prize) {
+        System.out.println("\n실행 결과");
+        System.out.println(prize.getPrizeName());
     }
 
-    private String appendNameFormat(String userName) {
-        if (userName.length() < MAX_NAME_LENGTH) {
-            String blankFormats = BLANK.repeat(4 - userName.length());
-            return blankFormats + userName + BLANK.repeat(2);
+    public void printAllResult(GameResult gameResult) {
+        System.out.println("\n실행 결과");
+
+        HashMap<User, Prize> allResult = gameResult.getAllResult();
+        for (User user : allResult.keySet()) {
+            Prize prize = allResult.get(user);
+            System.out.printf("%s : %s\n", user.getUserName(), prize.getPrizeName());
         }
-        return userName + BLANK;
+    }
+
+    private void printUsers(Users users) {
+        String formattedUserNames = formatter.formatUserNames(users);
+        System.out.println(formattedUserNames);
     }
 
     private void printLadder(Ladder ladder) {
-        List<Line> lines = ladder.getLines();
-        StringBuilder sb = new StringBuilder();
-        for (Line line : lines) {
-            printLine(sb, line.getDirectionsInfo());
-        }
-        System.out.println(sb);
+        String formatLadder = formatter.formatLadder(ladder);
+        System.out.println(formatLadder);
     }
 
-    private void printLine(StringBuilder sb, List<Direction> directionsInfo) {
-        sb.append(BLANK.repeat(4));
-        for (int i = 0; i < directionsInfo.size() - 1; i++) {
-            sb.append(LINE_VERTICAL)
-                    .append(printDirections(directionsInfo.get(i)));
-        }
-        sb.append(LINE_VERTICAL).append("\n");
+    private void printPrizes(Prizes prizes) {
+        String formattedPrizeNames = formatter.formatPrizeNames(prizes);
+        System.out.println(formattedPrizeNames);
     }
 
-    private String printDirections(Direction direction) {
-        if (direction == Direction.RIGHT) {
-            return LINE_HORIZONTAL;
-        }
-        return BLANK.repeat(5);
-    }
+
+//    private void printLine(StringBuilder sb, List<Direction> directionsInfo) {
+//        sb.append(BLANK.repeat(4));
+//        for (int i = 0; i < directionsInfo.size() - 1; i++) {
+//            sb.append(LINE_VERTICAL)
+//                    .append(printDirections(directionsInfo.get(i)));
+//        }
+//        sb.append(LINE_VERTICAL).append("\n");
+//    }
+//
+//    private String printDirections(Direction direction) {
+//        if (direction == Direction.RIGHT) {
+//            return LINE_HORIZONTAL;
+//        }
+//        return BLANK.repeat(5);
+
+//    }
 }
