63aa7249d184cb99e2c810b8092a5691c7476d7d
이상진 <priceelees@gmail.com>
2024-02-29 15:33:39 +0900

    style(OutputView): 메서드명 수정 및 재배치

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -16,27 +16,57 @@ public class OutputView {
 	private static final String NAME_PRINT_FORMAT = "%" + LADDER_WIDTH_UNIT + "s";
 	private static final String COLON_WITH_SPACE = " : ";
 
-	public void printReadNames() {
+	public void printRequestNamesMessage() {
 		System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
 	}
 
-	public void printReadPrizes() {
+	public void printRequestPrizesMessage() {
 		System.out.println("실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
 	}
 
-	public void printReadLadderHeight() {
+	public void printRequestLadderHeightMessage() {
 		System.out.println("최대 사다리 높이는 몇 개인가요?");
 	}
 
-	public void printReadPlayer() {
+	public void printRequestPlayerToGetResultMessage() {
 		System.out.println("결과를 보고 싶은 사람은?");
 	}
 
-	public void printResultMessage() {
+	public void printErrorMessage(String message) {
+		System.out.println("[ERROR] " + message + System.lineSeparator());
+	}
+
+	public void printCurrentLadder(
+		List<String> playerNames,
+		List<FloorConnectionStatusDto> ladderConnectionStatus,
+		List<String> prizeNames
+	) {
+		printLadderResultPrefix();
+		printNames(playerNames);
+		printLadder(ladderConnectionStatus);
+		printNames(prizeNames);
+		printEmptyLine();
+	}
+
+	public void printAllPlayerResults(Map<String, String> result) {
+		printGameResultPrefix();
+		result.forEach((name, prize) -> System.out.println(name + COLON_WITH_SPACE + prize));
+	}
+
+	public void printOnePlayerResult(String name) {
+		printGameResultPrefix();
+		System.out.println(name);
+	}
+
+	public void printEmptyLine() {
+		System.out.println();
+	}
+
+	private void printLadderResultPrefix() {
 		System.out.println("사다리 결과" + System.lineSeparator());
 	}
 
-	public void printNames(List<String> names) {
+	private void printNames(List<String> names) {
 		StringJoiner joiner = new StringJoiner(" ");
 
 		names.stream()
@@ -46,54 +76,36 @@ public class OutputView {
 		System.out.println(joiner);
 	}
 
-	public void printLadder(List<FloorConnectionStatusDto> statuses) {
-		statuses.stream()
+	private String formatName(String name) {
+		return String.format(NAME_PRINT_FORMAT, name);
+	}
+
+	private void printLadder(List<FloorConnectionStatusDto> ladderConnectionStatus) {
+		ladderConnectionStatus.stream()
 			.map(FloorConnectionStatusDto::connections)
-			.forEach(this::printHorizontalLine);
+			.forEach(this::printOneFloor);
 	}
 
-	public void printErrorMessage(String message) {
-		System.out.println("[ERROR] " + message + System.lineSeparator());
-	}
-
-	private void printHorizontalLine(List<Boolean> connections) {
+	private void printOneFloor(List<Boolean> connections) {
 		StringBuilder builder = new StringBuilder();
 
 		builder.append(LADDER_PREFIX_SPACES);
 		connections.forEach(connection ->
 			builder.append(VERTICAL_LADDER_CHARACTER)
-				.append(getHorizontalLadderCharacter(connection))
+				.append(getConnectionCharacter(connection))
 		);
 
 		System.out.println(builder);
 	}
 
-	private String getHorizontalLadderCharacter(Boolean connection) {
+	private String getConnectionCharacter(Boolean connection) {
 		if (connection) {
 			return HORIZONTAL_LADDER_CHARACTER.repeat(LADDER_WIDTH_UNIT);
 		}
 		return EMPTY_LADDER_CHARACTER.repeat(LADDER_WIDTH_UNIT);
 	}
 
-	private String formatName(String name) {
-		return String.format(NAME_PRINT_FORMAT, name);
-	}
-
-	public void printAllPlayersResult(Map<String, String> result) {
-		printGameResult();
-		result.forEach((name, prize) -> System.out.println(name + COLON_WITH_SPACE + prize));
-	}
-
-	public void printOnePlayerResult(String name) {
-		printGameResult();
-		System.out.println(name);
-	}
-
-	private void printGameResult() {
+	private void printGameResultPrefix() {
 		System.out.println("실행 결과");
-	}
-
-	public void printEmptyLine() {
-		System.out.println();
 	}
 }
