6e85beef6840a152f807a512de2af40410089049
KimSangHyeok <tkdgur0906@naver.com>
2024-02-29 16:20:10 +0900

    refactor : 테스트 코드에서 중복된 값 상수처리 취소

--- src/test/java/LadderTest.java
+++ src/test/java/LadderTest.java
@@ -10,29 +10,27 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class LadderTest {
 
-    private static final int HEIGHT = 5;
-    private static final int WIDTH = 4;
     private static final RandomLineGenerator randomLineGenerator = new RandomLineGenerator();
 
     @DisplayName("사다리 객체를 정상적으로 생성한다.")
     @Test
     void createLadder() {
-        assertThat(Ladder.from(HEIGHT, WIDTH, randomLineGenerator).getLines().size()).isEqualTo(HEIGHT);
+        assertThat(Ladder.from(5, 4, randomLineGenerator).getLines().size()).isEqualTo(5);
     }
 
     @DisplayName("주어진 너비에 맞게 사다리의 다리의 개수를 생성한다.")
     @Test
     void makeLinesWithWidth() {
-        Ladder ladder = Ladder.from(HEIGHT, WIDTH, randomLineGenerator);
+        Ladder ladder = Ladder.from(5, 4, randomLineGenerator);
         Line line = ladder.getLines().get(0);
 
-        assertThat(line.getWidth()).isEqualTo(WIDTH);
+        assertThat(line.getWidth()).isEqualTo(4);
     }
 
     @DisplayName("주어진 참여자의 사다리 타기 결과를 인덱스로 반환한다.")
     @Test
     void climb() {
-        Ladder ladder = Ladder.from(HEIGHT, WIDTH, new CustomLineGenerator());
+        Ladder ladder = Ladder.from(5, 4, new CustomLineGenerator());
 
         assertThat(ladder.climb(new Position(1)).getIndex()).isEqualTo(0);
         assertThat(ladder.climb(new Position(2)).getIndex()).isEqualTo(3);
@@ -41,7 +39,7 @@ public class LadderTest {
     @DisplayName("맨 왼쪽에 위치한 참여자의 사다리 타기 결과를 인덱스로 반환한다.")
     @Test
     void climbWithLeftPlayer() {
-        Ladder ladder = Ladder.from(HEIGHT, WIDTH, new CustomLineGenerator());
+        Ladder ladder = Ladder.from(5, 4, new CustomLineGenerator());
 
         assertThat(ladder.climb(new Position(0)).getIndex()).isEqualTo(1);
     }
@@ -49,7 +47,7 @@ public class LadderTest {
     @DisplayName("맨 오른쪽에 위치한 참여자의 사다리 타기 결과를 인덱스로 반환한다.")
     @Test
     void climbWithRightPlayer() {
-        Ladder ladder = Ladder.from(HEIGHT, WIDTH, new CustomLineGenerator());
+        Ladder ladder = Ladder.from(5, 4, new CustomLineGenerator());
 
         assertThat(ladder.climb(new Position(3)).getIndex()).isEqualTo(2);
     }
