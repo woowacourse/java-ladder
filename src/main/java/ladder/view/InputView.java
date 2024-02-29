8211badee360fa7df5b00a2d86f4dcdadca808c2
Eugene Jang <kyunellroll@gmail.com>
2024-02-29 18:39:30 +0900

    style(inputView): 입력을 받기 전 개행 문자 추가

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -22,7 +22,7 @@ public class InputView {
     }
 
     public String getHeight() {
-        System.out.println("최대 사다리 높이는 몇 개인가요?");
+        System.out.println("\n최대 사다리 높이는 몇 개인가요?");
         return scanner.nextLine();
     }
 
