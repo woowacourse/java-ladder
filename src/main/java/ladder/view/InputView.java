9d09a50f4500abe7578d9902872a2fb73cbba083
ay-eonii <naayen@naver.com>
2024-02-29 14:51:25 +0900

    feat(InputView): 실행 결과 입력받기

--- src/main/java/ladder/view/InputView.java
+++ src/main/java/ladder/view/InputView.java
@@ -21,4 +21,15 @@ public class InputView {
         out.println(lineSeparator() + "최대 사다리 높이는 몇 개인가요?");
         return Integer.parseInt(scanner.nextLine());
     }
+
+    public List<String> readResults() {
+        out.println(lineSeparator() + "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
+        String rawNames = scanner.nextLine();
+        return List.of(rawNames.split(DELIMITER));
+    }
+
+    public String readTarget() {
+        out.println(lineSeparator() + "결과를 보고 싶은 사람은?");
+        return scanner.nextLine();
+    }
 }
