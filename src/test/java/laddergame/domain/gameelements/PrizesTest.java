b6477da10195b43bfdbfa5dd93357f27f79456aa
coli-geonwoo <kkwoo001021@naver.com>
2024-02-29 16:59:56 +0900

    test: prizes test추가
    참여자 수와 같지 않은 보상의 수가 주어진다면 Prizes객체가 생성되지 않음을 테스트

---
+++ src/test/java/laddergame/domain/gameelements/PrizesTest.java
@@ -0,0 +1,32 @@
+package laddergame.domain.gameelements;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+class PrizesTest {
+
+    @DisplayName("보상의 수가 참여자의 수와 같지 않으면 Prizes가 생성되지 않는다.")
+    @ParameterizedTest
+    @ValueSource(ints = {2, 1, 5})
+    void notSameLengthBetweenPrizesAndPlayersTest(int playerNumber) {
+        List<String> prizeNames = new ArrayList<>(List.of("꽝", "5000", "꽝", "3000"));
+        assertThatThrownBy(() -> new Prizes(prizeNames, playerNumber))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("보상의 수가 참여자의 수와 같다면 Prizes가 생성된다.")
+    @ParameterizedTest
+    @ValueSource(ints = {4})
+    void sameLengthBetweenPrizesAndPlayersTest(int playerNumber) {
+        assertDoesNotThrow(() ->
+                new Prizes(List.of("꽝", "5000", "꽝", "3000"), playerNumber));
+    }
+
+}
