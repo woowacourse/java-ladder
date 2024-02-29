cd25eafd2f6d57cf26e8d070ff866c2cfdb3bb45
hangillee <skfcb10@naver.com>
2024-02-29 16:29:04 +0900

    feat: 이름 Null 여부 검증 로직 구현 및 if 구문 메소드로 분리

--- src/main/java/domain/Name.java
+++ src/main/java/domain/Name.java
@@ -5,7 +5,7 @@ public class Name {
     private static final int MIN_NAME_LENGTH = 2;
     private static final String INVALID_NAME_LENGTH =
             "이름은 " + MIN_NAME_LENGTH + "자 이상, " + MAX_NAME_LENGTH + "자 이하여야 합니다.";
-    public static final String NAME_CONTAINS_BLANK = "이름은 공백이 포함될 수 없습니다.";
+    public static final String NAME_CONTAINS_BLANK = "이름은 비어있거나 공백을 포함할 수 없습니다.";
     public static final String BLANK = " ";
 
     private final String value;
@@ -16,14 +16,31 @@ public class Name {
     }
 
     private void validateNameLength(final String value) {
+        validateNameIsNotNull(value);
+        validateNameLengthUnderUpperBound(value);
+        validateNameLengthOverLowerBound(value);
+        validateNameIsNotBlank(value);
+    }
+
+    private void validateNameIsNotNull(final String value) {
+        if (value == null) {
+            throw new IllegalArgumentException(INVALID_NAME_LENGTH);
+        }
+    }
+
+    private static void validateNameLengthUnderUpperBound(final String value) {
         if (value.length() > MAX_NAME_LENGTH) {
             throw new IllegalArgumentException(INVALID_NAME_LENGTH);
         }
+    }
 
+    private static void validateNameLengthOverLowerBound(final String value) {
         if (value.length() < MIN_NAME_LENGTH) {
             throw new IllegalArgumentException(INVALID_NAME_LENGTH);
         }
+    }
 
+    private static void validateNameIsNotBlank(final String value) {
         if (value.contains(BLANK)) {
             throw new IllegalArgumentException(NAME_CONTAINS_BLANK);
         }
