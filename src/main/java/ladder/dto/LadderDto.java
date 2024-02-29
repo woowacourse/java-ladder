f8d5d83fe8046c5904857942419ab53914fe02e7
gyungchan Jo <namejgc@naver.com>
2024-02-29 18:52:04 +0900

    refactor: dto로 변환하는 기능 수정

--- src/main/java/ladder/dto/LadderDto.java
+++ src/main/java/ladder/dto/LadderDto.java
@@ -4,15 +4,15 @@ import ladder.domain.ladder.Ladder;
 import ladder.domain.ladder.Line;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public record LadderDto(List<LineDto> lineDtos) {
 
     public static LadderDto from(final Ladder ladder) {
         final List<Line> lines = ladder.getLines();
-        final List<LineDto> lineDtos = lines.stream()
-                .map(LineDto::from)
-                .toList();
 
-        return new LadderDto(lineDtos);
+        return lines.stream()
+                .map(LineDto::from)
+                .collect(Collectors.collectingAndThen(Collectors.toList(), LadderDto::new));
     }
 }
--- src/main/java/ladder/dto/LineDto.java
+++ src/main/java/ladder/dto/LineDto.java
@@ -4,15 +4,15 @@ import ladder.domain.ladder.Line;
 import ladder.domain.ladder.Rung;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public record LineDto(List<Boolean> rungsExist) {
 
     public static LineDto from(final Line line) {
         final List<Rung> rungs = line.getRungs();
-        final List<Boolean> rungExist = rungs.stream()
-                .map(Rung::isExist)
-                .toList();
 
-        return new LineDto(rungExist);
+        return rungs.stream()
+                .map(Rung::isExist)
+                .collect(Collectors.collectingAndThen(Collectors.toList(), LineDto::new));
     }
 }
