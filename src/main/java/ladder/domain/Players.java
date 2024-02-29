35169c68c73b276e244418ada1a34ac7aede59d3
hoyeonyy <highoption@naver.com>
2024-02-29 21:31:00 +0900

    delete (Players) : 테스트 코드 수정

--- src/main/java/ladder/domain/Players.java
+++ src/main/java/ladder/domain/Players.java
@@ -1,7 +1,6 @@
 package ladder.domain;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Players {
@@ -11,15 +10,11 @@ public class Players {
         this.players = playerNames;
     }
 
-    public void changePosition(int present, int after) {
-        Collections.swap(players, present, after);
-    }
-
     public List<String> getPlayerNames() {
         List<String> playerNames = new ArrayList<>();
         for (PlayerName player : players) {
             playerNames.add(player.getName());
         }
-        return Collections.unmodifiableList(playerNames);
+        return playerNames;
     }
 }
--- src/test/java/ladder/domain/PlayersTest.java
+++ src/test/java/ladder/domain/PlayersTest.java
@@ -1,6 +1,5 @@
 package ladder.domain;
 
-import java.util.ArrayList;
 import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -13,24 +12,5 @@ class PlayersTest {
         List<PlayerName> playerNames = List.of(new PlayerName("hogee"), new PlayerName("jazz"));
         Assertions.assertThatCode(() -> new Players(playerNames))
                 .doesNotThrowAnyException();
-    }
-
-    @Test
-    @DisplayName("플레이어 들의 위치를 바꾼다")
-    void changePosition() {
-        List<PlayerName> playerNames = new ArrayList<>();
-        playerNames.add(new PlayerName("hogee"));
-        playerNames.add(new PlayerName("jazz"));
-        Players players = new Players(playerNames);
-
-        List<PlayerName> copyPlayerNames = new ArrayList<>(playerNames);
-        Players copyPlayers = new Players(copyPlayerNames);
-
-        players.changePosition(0, 1);
-
-        String changePlayerName = players.getPlayerNames().get(1);
-        String notChangePlayerName = copyPlayers.getPlayerNames().get(0);
-
-        Assertions.assertThat(changePlayerName).isEqualTo(notChangePlayerName);
     }
 }
