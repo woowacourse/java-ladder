61d7085fd355f24af875def2590153ba5bc689f5
le2sky <leehaneul990623@gmail.com>
2024-02-29 22:33:36 +0900

    refactor: 컨트롤러 로직 개선

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,11 +1,15 @@
 import java.util.List;
-import java.util.Map;
 import domain.Game;
 import domain.GameResult;
+import domain.Height;
+import domain.Ladder;
+import domain.Players;
 import view.InputView;
 import view.OutputView;
 
 class Controller {
+
+    private static final String ALL_RESULT_COMMAND = "all";
 
     private final InputView inputView;
     private final OutputView outputView;
@@ -16,23 +20,47 @@ class Controller {
     }
 
     public void run() {
-        List<String> playerNames = inputView.readNames();
-        List<String> gameResults = inputView.readGameResults();
-        int height = inputView.readHeight();
-        Game game = new Game(playerNames, gameResults, height);
-
+        Game game = createGame();
         outputView.printLadder(game);
+        play(game, inputView.readNameForResult());
+    }
 
-        while (true) {
-            String targetPlayer = inputView.readNameForResult();
+    private Game createGame() {
+        Players players = createPlayers();
+        Height height = createHeight();
+        List<GameResult> gameResults = createGameResult();
+        Ladder ladder = Ladder.createRandomLadder(height, players.size());
 
-            if (targetPlayer.equals("all")) {
-                Map<String, GameResult> result = game.showResultAll();
-                outputView.printResultAll(result);
-            } else {
-                GameResult result = game.showResult(targetPlayer);
-                outputView.printResult(result);
-            }
+        return new Game(players, ladder, gameResults);
+    }
+
+    private Players createPlayers() {
+        List<String> playerNames = inputView.readNames();
+
+        return new Players(playerNames);
+    }
+
+    private Height createHeight() {
+        int inputHeight = inputView.readHeight();
+
+        return new Height(inputHeight);
+    }
+
+    private List<GameResult> createGameResult() {
+        List<String> gameResults = inputView.readGameResults();
+
+        return gameResults.stream()
+                .map(GameResult::new)
+                .toList();
+    }
+
+    private void play(Game game, String targetPlayerName) {
+        if (ALL_RESULT_COMMAND.equals(targetPlayerName)) {
+            outputView.printResultAll(game.showResultAll());
+            return;
         }
+
+        outputView.printResult(game.showResult(targetPlayerName));
+        play(game, inputView.readNameForResult());
     }
 }
