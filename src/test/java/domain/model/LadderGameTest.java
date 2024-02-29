8bfa5ae3bb0d311d77514353853e27b5b5d5df8e
tackyu <tack970913@gmail.com>
2024-02-29 15:30:20 +0900

    test: 사다리 게임 실행 테스트 코드 추가

--- src/test/java/domain/model/LadderGameTest.java
+++ src/test/java/domain/model/LadderGameTest.java
@@ -2,9 +2,13 @@ package domain.model;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.List;
+import java.util.Map;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 
 public class LadderGameTest {
@@ -21,5 +25,24 @@ public class LadderGameTest {
         LadderGame ladderGame=new LadderGame(ladder,people, consequences);
         //then
         assertThatCode(()->new LadderGame(ladder,people, consequences)).doesNotThrowAnyException();
+    }
+    @ParameterizedTest
+    @CsvSource({
+            "a,꽝",
+            "b,100",
+            "c,200"
+    })
+    @DisplayName("게임을 진행하고 결과를 얻는다")
+    void playGameTest(String person,String consequence) {
+        //given
+        Ladder ladder = new Ladder("2", 3,()->true);
+        People people=new People(List.of("a","b","c"));
+        Consequences consequences =new Consequences(List.of("꽝","100","200"),3);
+        LadderGame ladderGame=new LadderGame(ladder,people, consequences);
+        //when
+        ladderGame.play();
+        Map<String,String> result=ladderGame.giveResult();
+        //then
+        assertThat(result.get(person)).isEqualTo(consequence);
     }
 }
