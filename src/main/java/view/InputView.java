4d4e516d404abd1e97c6215ec64a87d343b4fcf9
ashsty <qkralsdk991@naver.com>
2024-02-29 11:27:41 +0900

    refactor : 메서드 이름 수정
    - validateParticipantsNames -> validateLastDelimiter

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -7,9 +7,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
-import model.Height;
-import model.Participant;
-import model.Participants;
+import model.*;
 
 public class InputView {
     private static final String DELIMITER = ",";
@@ -18,7 +16,7 @@ public class InputView {
     public Participants readParticipantNames() {
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
         String input = scanner.nextLine();
-        validateParticipantsNames(input);
+        validateLastDelimiter(input);
         return Arrays.stream(input.split(DELIMITER))
                 .map(Participant::new)
                 .collect(collectingAndThen(toList(), Participants::new));
@@ -50,7 +48,7 @@ public class InputView {
 
     private void validateLastDelimiter(String input) {
         if (input == null || input.isBlank() || input.endsWith(DELIMITER)) {
-            throw new IllegalArgumentException("[ERROR] 입력값은 공백이거나 구분자("+ DELIMITER +")로 끝날 수 없다.");
+            throw new IllegalArgumentException("[ERROR] 입력값은 공백이거나 구분자(" + DELIMITER + ")로 끝날 수 없다.");
         }
     }
 }
