d56d8b29df7204ce21cbb6e10de06061758fd57c
kelly6bf <kelly6bf@gmail.com>
2024-02-29 11:28:41 +0900

    feat: 사용자 이름에 매칭되는 게임 결과를 반환하는 테스트 로직 구현 - 기존 필드로 사용하던 자료구조인 Map을 List로 변경함.

--- src/main/java/domain/game/LadderGameResults.java
+++ src/main/java/domain/game/LadderGameResults.java
@@ -4,13 +4,18 @@ import domain.ladder.Ladder;
 import domain.player.Player;
 import domain.player.Players;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
+
+import static java.util.Collections.unmodifiableList;
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toList;
 
 public class LadderGameResults {
-    private final Map<Player, GameResult> ladderGameResults;
+    private static final String ALL = "all";
 
-    private LadderGameResults(final Map<Player, GameResult> ladderGameResults) {
+    private final List<LadderGameResult> ladderGameResults;
+
+    private LadderGameResults(final List<LadderGameResult> ladderGameResults) {
         this.ladderGameResults = ladderGameResults;
     }
 
@@ -18,16 +23,32 @@ public class LadderGameResults {
         players.getPlayers()
                 .forEach(player -> play(ladder, player));
 
-        Map<Player, GameResult> ladderGameResults = new HashMap<>();
-        players.getPlayers()
-                .forEach(player -> ladderGameResults.put(player, gameResults.findGameResult(player.getCurrentLineNumber())));
-
-        return new LadderGameResults(ladderGameResults);
+        return players.getPlayers()
+                .stream()
+                .map(player -> new LadderGameResult(player.getPlayerName(), gameResults.findGameResult(player.getCurrentLineNumber())))
+                .collect(collectingAndThen(toList(), LadderGameResults::new));
     }
 
     private static void play(Ladder ladder, Player player) {
         while (!player.escapeLadder()) {
             ladder.movePlayer(player);
         }
+    }
+
+    public List<LadderGameResult> findPlayerGameResults(String playerName) {
+        if (playerName.equals(ALL)) {
+            return unmodifiableList(ladderGameResults);
+        }
+
+        return parseGameResultsWithPlayerName(playerName);
+    }
+
+    private List<LadderGameResult> parseGameResultsWithPlayerName(String playerName) {
+        LadderGameResult targetLadderGameResult = ladderGameResults.stream()
+                .filter(ladderGameResult -> ladderGameResult.isPlayerName(playerName))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("유효하지 않은 사용자 이름입니다."));
+
+        return List.of(targetLadderGameResult);
     }
 }
