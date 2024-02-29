0bd371ccd6f9961b4c16a5061f16bd62b019d06e
tackyu <tack970913@gmail.com>
2024-02-29 17:52:36 +0900

    feat: 사다리 게임 실행 구현

--- src/main/java/domain/model/LadderGame.java
+++ src/main/java/domain/model/LadderGame.java
@@ -1,12 +1,30 @@
 package domain.model;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class LadderGame {
     private final Ladder ladder;
     private final People people;
     private final Consequences consequences;
+    private final Result result;
+
     public LadderGame(Ladder ladder, People people, Consequences consequences) {
         this.ladder = ladder;
         this.people = people;
         this.consequences = consequences;
+
+        result = new Result(people, consequences);
+    }
+
+    public void play() {
+        for (int positionOfPerson = 0; positionOfPerson < people.getNumberOfParticipants(); positionOfPerson++) {
+            int positionOfConsequence = ladder.goToConsequence(positionOfPerson, 0);
+            result.make(positionOfPerson, positionOfConsequence);
+        }
+    }
+
+    public Map<String, String> giveResult() {
+        return result.giveResult();
     }
 }
