3881e91db9541cf14dc8e50e927398480f48d98c
Hyeon0208 <dmsgktn0208@gmail.com>
2024-02-29 12:13:24 +0900

    feat : 참가자의 이름과 위치 정보를 갖는 객체 추가

---
+++ src/main/java/domain/player/Player.java
@@ -0,0 +1,25 @@
+package domain.player;
+
+import java.util.Objects;
+
+public class Player {
+    private final PlayerName playerName;
+    private int position;
+
+    public Player(final PlayerName playerName, final int position) {
+        this.playerName = playerName;
+        this.position = position;
+    }
+
+    public void moveTo(int direction) {
+        position += direction;
+    }
+
+    public String getName() {
+        return playerName.getName();
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
---
+++ src/test/java/domain/player/PlayerTest.java
@@ -0,0 +1,30 @@
+package domain.player;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class PlayerTest {
+
+    @Test
+    @DisplayName("플레이어가 사다리를 건널 때 마다 위치가 변경된다")
+    void moveToTest() {
+        // given
+        Player player1 = new Player(new PlayerName("kaki"), 0);
+        Player player2 = new Player(new PlayerName("pobi"), 1);
+
+        // when
+        player1.moveTo(1);
+        player2.moveTo(-1);
+
+        // then
+        assertAll(
+                () -> assertThat(player1.getPosition()).isEqualTo(1),
+                () -> assertThat(player2.getPosition()).isEqualTo(0)
+        );
+    }
+}
