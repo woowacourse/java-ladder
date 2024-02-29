dd188f4d6212792f7418ee16272069f0c572ea79
tackyu <tack970913@gmail.com>
2024-02-29 17:10:24 +0900

    test: 사다리 게임 결과를 만드는 클래스 테스트 코드 작성

---
+++ src/test/java/domain/model/ResultTest.java
@@ -0,0 +1,36 @@
+package domain.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ResultTest {
+    @ParameterizedTest
+    @CsvSource({
+            "a,30",
+            "b,10",
+            "c,20"
+    })
+    @DisplayName("사다리 게임 결과를 담는다")
+    void makeResultTest(String person,String consequence) {
+        //given
+        People people= new People(List.of("a","b","c"));
+        Consequences consequences = new Consequences(List.of("10", "20","30"),3);
+        Result result=new Result(people,consequences);
+
+        //when
+        result.make(0,2);
+        result.make(1,0);
+        result.make(2,1);
+
+        //then
+        Map<String,String> completeResult=result.giveResult();
+        assertThat(completeResult.get(person)).isEqualTo(consequence);
+    }
+}
