c27ad0b26584443c33673b89dd1de301b2682f5b
ay-eonii <naayen@naver.com>
2024-02-29 16:46:00 +0900

    feat(Results): 결과 개수가 이름 개수와 다른 경우 예외 처리

--- src/main/java/ladder/domain/Results.java
+++ src/main/java/ladder/domain/Results.java
@@ -6,8 +6,15 @@ import java.util.List;
 public class Results {
     private final List<String> results;
 
-    public Results(List<String> results) {
+    public Results(List<String> results, int countStandard) {
+        validateCount(results, countStandard);
         this.results = results;
+    }
+
+    private void validateCount(List<String> results, int countStandard) {
+        if (results.size() != countStandard) {
+            throw new IllegalArgumentException("결과 수가 올바르지 않습니다.");
+        }
     }
 
     public String find(int position) {
