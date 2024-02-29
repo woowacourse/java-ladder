d205b962bf7f8ec1806c4670acbc9e683ea5cabd
reddevilmidzy <midzy0228@gmail.com>
2024-02-29 19:38:54 +0900

    refactor: 출력 메시지 상수로 변경

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -9,6 +9,7 @@ public class InputView {
     private static final String PLAYERS_REQUEST_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     private static final String RESULT_REQUEST_MESSAGE = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
     private static final String HEIGHT_REQUEST_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
+    public static final String TARGET_PLAYER_REQUEST_MESSAGE = "결과를 보고 싶은 사람은?";
     private static final String SEPARATOR = ",";
 
     private final Scanner scanner;
@@ -75,7 +76,7 @@ public class InputView {
     }
 
     public String readPlayerNameToCheckPrize() {
-        System.out.println("결과를 보고 싶은 사람은?");
+        System.out.println(TARGET_PLAYER_REQUEST_MESSAGE);
         String rawName = scanner.nextLine();
         validateBlank(rawName);
         System.out.println();
