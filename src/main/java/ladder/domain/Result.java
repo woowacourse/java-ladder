94d1b0c68615ac4353a8ab260942170696bddeb6
ay-eonii <naayen@naver.com>
2024-02-29 17:23:47 +0900

    feat(Result): 결과 길이 예외 처리

---
+++ src/main/java/ladder/domain/Result.java
@@ -0,0 +1,18 @@
+package ladder.domain;
+
+public class Result {
+
+    private static final int RESULT_LENGTH_MAX = 5;
+    private final String value;
+
+    public Result(String value) {
+        validateLength(value);
+        this.value = value;
+    }
+
+    private void validateLength(String value) {
+        if (value.length() > RESULT_LENGTH_MAX) {
+            throw new IllegalArgumentException("결과는 5글자 이내입니다.");
+        }
+    }
+}
---
+++ src/test/java/ladder/domain/ResultTest.java
@@ -0,0 +1,16 @@
+package ladder.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ResultTest {
+
+    @Test
+    @DisplayName("결과가 5글자 이상이라면 예외가 발생한다.")
+    void resultLengthExceptionTest() {
+        Assertions.assertThatThrownBy(
+                () -> new Result("긴결과입니다")
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
+}
