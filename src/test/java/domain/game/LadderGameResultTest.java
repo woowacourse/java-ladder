78c4f42d4d14110c294d69d612fbb07861abda60
kelly6bf <kelly6bf@gmail.com>
2024-02-29 11:20:28 +0900

    test: LadderGameResult 인스턴스를 생성 테스트 로직 작성

---
+++ src/test/java/domain/game/LadderGameResultTest.java
@@ -0,0 +1,24 @@
+package domain.game;
+
+import domain.player.PlayerName;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LadderGameResultTest {
+
+    @DisplayName("사용자 이름과 게임 결과를 입력하면 LadderGameResult 인스턴스를 반환한다.")
+    @Test
+    void LadderGameResult_인스턴스_반환() {
+        // Given
+        final PlayerName playerName = new PlayerName("kelly");
+        final GameResult gameResult = GameResult.of("꽝", 1);
+
+        // When
+        final LadderGameResult ladderGameResult = new LadderGameResult(playerName, gameResult);
+
+        // Then
+        assertThat(ladderGameResult).isNotNull();
+    }
+}
