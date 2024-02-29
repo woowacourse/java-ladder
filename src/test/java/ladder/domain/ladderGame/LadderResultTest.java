ef96c87c44e73cd152f86f0f562e781fd7626d04
Eugene Jang <kyunellroll@gmail.com>
2024-02-29 18:49:40 +0900

    refactor(LadderResultTest): 사용하지 않는 객체 삭제

--- src/test/java/ladder/domain/ladderGame/LadderResultTest.java
+++ src/test/java/ladder/domain/ladderGame/LadderResultTest.java
@@ -7,7 +7,6 @@ import ladder.domain.carpenter.Energy;
 import ladder.domain.dto.MadeLadderDto;
 import ladder.domain.ladder.Height;
 import ladder.domain.participant.Participants;
-import ladder.domain.prize.GamePrizes;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -16,15 +15,12 @@ import org.junit.jupiter.api.Test;
 public class LadderResultTest {
 
     private Participants participants;
-    private GamePrizes gamePrize;
     private Carpenter carpenter;
 
     @BeforeEach
     void setUp() {
         participants = new Participants(new ArrayList<>(List.of("aru", "pola", "jazz")));
         carpenter = new Carpenter(new Height("5"), participants.size(), new Energy(() -> 6));
-
-        gamePrize = new GamePrizes(new ArrayList<>(List.of("꽝", "5000", "꽝")), 3);
     }
 
     @DisplayName("해당하는 position의 상품 position을 return 하는지 확인")
