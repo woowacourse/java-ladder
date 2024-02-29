cab27e7ac8b46126e2f65fdca0c333a19c4ab8f1
mzeong <mjeong@sookmyung.ac.kr>
2024-02-29 22:58:39 +0900

    test(LadderTest): 특정 참여자의 사다리 실행

--- src/test/java/ladder/domain/LadderTest.java
+++ src/test/java/ladder/domain/LadderTest.java
@@ -1,30 +1,17 @@
 package ladder.domain;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LadderTest {
 
-    @Test
-    @DisplayName("입력받은 높이만큼 사다리의 라인을 생성한다.")
-    void createLadder() {
-        // given
-        Height height = new Height("5");
-        People people = new People("pobi,honux,crong,jk");
-
-        // when
-        Ladder ladder = new Ladder(people, height);
-
-        // then
-        assertThat(ladder.getLadder()).hasSize(5);
-    }
-
-    @DisplayName("특정 참여자의 사다리를 실행한다.")
+    @DisplayName("특정 참여자의 사다리를 탄다.")
     @ParameterizedTest
-    @CsvSource(value = {"0, 0", "1, 3", "2, 2", "3, 1"})
-    void execute(int index, int actual) {
+    @CsvSource(value = {"0, 0", "1, 3", "2, 2,", "3, 1"})
+    void climb(int indexValue, int actual) {
         // given
         /*
         |-----|     |-----|
@@ -34,17 +21,19 @@ class LadderTest {
         |-----|     |-----|
          */
         Ladder ladder = new Ladder(
-                new Line(Point.USED, Point.UNUSED, Point.USED, Point.UNUSED),
-                new Line(Point.UNUSED, Point.USED, Point.UNUSED, Point.UNUSED),
-                new Line(Point.USED, Point.UNUSED, Point.UNUSED, Point.UNUSED),
-                new Line(Point.UNUSED, Point.USED, Point.UNUSED, Point.UNUSED),
-                new Line(Point.USED, Point.UNUSED, Point.USED, Point.UNUSED)
+                new Line(Direction.RIGHT, Direction.LEFT, Direction.RIGHT, Direction.LEFT),
+                new Line(Direction.DOWN, Direction.RIGHT, Direction.LEFT, Direction.DOWN),
+                new Line(Direction.RIGHT, Direction.LEFT, Direction.DOWN, Direction.DOWN),
+                new Line(Direction.DOWN, Direction.RIGHT, Direction.LEFT, Direction.DOWN),
+                new Line(Direction.RIGHT, Direction.LEFT, Direction.RIGHT, Direction.LEFT)
         );
 
+        Index index = new Index(indexValue);
+
         // when
-        int expected = ladder.execute(index);
+        Index expected = ladder.climb(index);
 
         // then
-        assertThat(expected).isEqualTo(actual);
+        assertThat(expected.getValue()).isEqualTo(actual);
     }
 }
