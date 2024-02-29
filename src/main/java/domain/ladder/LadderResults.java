9b281d0ab0b4c713b9a7a3cf8eb4c35b841cb58b
PgmJun <chltmdwns96@gmail.com>
2024-03-01 00:32:51 +0900

    refactor : 클래스 변수, 인스턴스 변수 구분

--- src/main/java/domain/ladder/LadderResults.java
+++ src/main/java/domain/ladder/LadderResults.java
@@ -9,6 +9,7 @@ import java.util.List;
 public class LadderResults {
     public static final String NOT_FOUND_ERROR_MESSAGE = "존재하지 않는 사다리 실행 결과입니다";
     public static final String SIZE_ERROR_MESSAGE = "사다리 실행 결과의 수는 참가자의 수와 같아야 합니다";
+
     private final List<LadderResult> values;
 
     public LadderResults(final String[] values, final int playerCount) {
