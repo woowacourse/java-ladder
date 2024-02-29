b56ba3a282f4dcc626b2064d19ca77a99aa20745
donghoony <aru0504@naver.com>
2024-03-01 04:18:32 +0900

    feat(Players): 플레이어 수의 제한 검증 구현

--- src/main/java/player/Players.java
+++ src/main/java/player/Players.java
@@ -4,12 +4,22 @@ import java.util.List;
 
 public class Players {
 
+    private static final int MIN_PLAYER_SIZE = 2;
+    private static final int MAX_PLAYER_SIZE = 10;
+
     private final List<Player> players;
 
     public Players(List<String> playerNames) {
+        validateSize(playerNames);
         this.players = playerNames.stream()
                 .map(Player::new)
                 .toList();
+    }
+
+    private void validateSize(List<String> playerNames) {
+        if (playerNames.size() < MIN_PLAYER_SIZE || playerNames.size() > MAX_PLAYER_SIZE) {
+            throw new IllegalArgumentException("참여자의 수는 2명 이상 10명 이하로 작성해야 합니다.");
+        }
     }
 
     public List<String> getNames() {
--- src/test/java/player/PlayersTest.java
+++ src/test/java/player/PlayersTest.java
@@ -1,11 +1,14 @@
 package player;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class PlayersTest {
 
@@ -26,4 +29,14 @@ class PlayersTest {
         assertThat(actual).containsExactly("aru", "pobi", "woowa");
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"one", "one,two,three,four,five,six,seven,eight,nine,ten,elevn"})
+    @DisplayName("플레이어 수가 범위를 벗어나면 예외를 발생한다.")
+    void invalidSizeTest(String names) {
+        // given
+        List<String> playerNames = List.of(names.split(","));
+        // when, then
+        assertThatThrownBy(() -> new Players(playerNames))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
