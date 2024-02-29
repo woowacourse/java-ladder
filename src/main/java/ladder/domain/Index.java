c3fabab82ee03482ac5779f3614bafe597929cf2
mzeong <mjeong@sookmyung.ac.kr>
2024-02-29 17:41:24 +0900

    feat(Index): 인덱스 데이터의 최소 및 최대 길이 검증

--- src/main/java/ladder/domain/Index.java
+++ src/main/java/ladder/domain/Index.java
@@ -2,12 +2,32 @@ package ladder.domain;
 
 public class Index {
 
+    private static final int MAX_LENGTH = 5;
+
     private final int value;
     private final String data;
 
     public Index(int value, String data) {
+        validateData(data);
         this.value = value;
         this.data = data;
+    }
+
+    private void validateData(String data) {
+        validateNotEmpty(data);
+        validateMaxLength(data);
+    }
+
+    private void validateNotEmpty(String data) {
+        if (data.isEmpty()) {
+            throw new IllegalArgumentException("이름이 1글자 미만입니다.");
+        }
+    }
+
+    private void validateMaxLength(String data) {
+        if (data.length() > MAX_LENGTH) {
+            throw new IllegalArgumentException("이름이 5글자 초과입니다.");
+        }
     }
 
     public Index increase() {
