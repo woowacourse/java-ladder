6c8bc4f24dc286ca1e77812a63386f9ae85945b4
donghoony <aru0504@naver.com>
2024-03-01 04:45:27 +0900

    chore: remove unused import

--- src/test/java/result/ResultsTest.java
+++ src/test/java/result/ResultsTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import player.Players;
 
 class ResultsTest {
 
