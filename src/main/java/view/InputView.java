184de0772117ad39bd61df739f98a216ee3d0e21
ashsty <qkralsdk991@naver.com>
2024-02-29 11:27:12 +0900

    feat : 실행 결과 조회 관련 입력 처리 기능 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -4,6 +4,7 @@ import static java.util.stream.Collectors.collectingAndThen;
 import static java.util.stream.Collectors.toList;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 import model.Height;
@@ -23,6 +24,17 @@ public class InputView {
                 .collect(collectingAndThen(toList(), Participants::new));
     }
 
+    public Prizes readPrizesNames(int peopleCount) {
+        System.out.println("\n실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
+        String input = scanner.nextLine();
+        validateLastDelimiter(input);
+        List<Prize> prizes = Arrays.stream(input.split(DELIMITER))
+                .map(Prize::new)
+                .collect(toList());
+
+        return new Prizes(prizes, peopleCount);
+    }
+
     public Height readLadderHeight() {
         System.out.println();
         System.out.println("최대 사다리 높이는 몇 개인가요?");
@@ -30,7 +42,13 @@ public class InputView {
         return new Height(input);
     }
 
-    private void validateParticipantsNames(String input) {
+    public String readResultSearch() {
+        System.out.println();
+        System.out.println("결과를 보고 싶은 사람은?");
+        return scanner.nextLine();
+    }
+
+    private void validateLastDelimiter(String input) {
         if (input == null || input.isBlank() || input.endsWith(DELIMITER)) {
             throw new IllegalArgumentException("[ERROR] 입력값은 공백이거나 구분자("+ DELIMITER +")로 끝날 수 없다.");
         }
