c0f9967544f66aca8f6f76eda2b295773d6fcc01
hangillee <skfcb10@naver.com>
2024-02-29 16:27:10 +0900

    refactor: Enum의 values()를 활용해 Point 생성 로직 수정

--- src/main/java/domain/Point.java
+++ src/main/java/domain/Point.java
@@ -1,5 +1,7 @@
 package domain;
 
+import java.util.Arrays;
+
 public enum Point {
     MOVABLE(true),
     UNMOVABLE(false);
@@ -15,10 +17,10 @@ public enum Point {
     }
 
     public static Point of(final boolean isMovable) {
-        if (isMovable) {
-            return MOVABLE;
-        }
-        return UNMOVABLE;
+        return Arrays.stream(values())
+                .filter(point -> point.isMovable == isMovable)
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
     }
 
     public static boolean valueOf(final Point point) {
