0578e7127bac565b9bb841605c22137ef7137dc4
hangillee <skfcb10@naver.com>
2024-02-29 15:34:31 +0900

    docs: 리팩토링에 따른 구현 기능 목록 수정

--- src/main/java/controller/LadderController.java
+++ src/main/java/controller/LadderController.java
@@ -1,34 +1,35 @@
 package controller;
 
 import domain.Height;
-import domain.Ladder;
 import domain.Lines;
 import domain.Names;
 import domain.PointGenerator;
 import domain.RandomPointGenerator;
 import domain.Results;
 import java.util.List;
+import java.util.Map;
 import view.InputView;
 import view.ResultView;
 
 public class LadderGenerator {
-    private Ladder ladder;
+    private Names names;
+    private Results results;
 
-    public void generateLadder() {
-        Names names = inputNames();
-        Results results = inputResults(names.size());
+    public void drawLadder() {
+        names = inputNames();
+        results = inputResults(names.size());
         Height height = inputLadderHeight();
 
         PointGenerator generator = new RandomPointGenerator();
         Lines lines = new Lines(height.getValue(), names.size(), generator);
 
-        ladder = new Ladder()
+        LadderDto ladder = new LadderDto()
                 .lines(lines)
                 .names(names)
                 .results(results)
                 .build();
 
-        ResultView.printLadderDrawResult(ladder.toDto());
+        ResultView.printLadderDrawResult(ladder);
     }
 
     private Height inputLadderHeight() {
@@ -46,7 +47,16 @@ public class LadderGenerator {
         return new Names(rawNames);
     }
 
-    public Ladder getLadder() {
-        return ladder;
+    public void printResult(final List<String> namesAfterMove) {
+        String target = InputView.findResult();
+
+        if ("all".equals(target)) {
+            Map<String, String> matchedNamesAndResults = results.matchNamesWithResults(namesAfterMove);
+            ResultView.printMoveResult(names.getAll(), matchedNamesAndResults);
+            return;
+        }
+        String result = results.resultOf(namesAfterMove.indexOf(target));
+
+        ResultView.printMoveResult(result);
     }
 }
