60dbb6229acfb3f2d6c89309f990642b62c91587
Eugene Jang <kyunellroll@gmail.com>
2024-02-29 21:39:27 +0900

    style(dto): Dto에 있는 system.out() 삭제

--- src/main/java/ladder/domain/dto/MadeLadderDto.java
+++ src/main/java/ladder/domain/dto/MadeLadderDto.java
@@ -5,7 +5,6 @@ import java.util.List;
 public record MadeLadderDto(List<MadeLineDto> madeLines) {
 
     public boolean checkLineStepStatus(int linePosition, int stepPosition) {
-        System.out.println(linePosition + " " + stepPosition);
         return madeLines.get(linePosition).findPositionStepStatus(stepPosition);
     }
 }
--- src/main/java/ladder/domain/dto/MadeLineDto.java
+++ src/main/java/ladder/domain/dto/MadeLineDto.java
@@ -6,7 +6,6 @@ import ladder.domain.ladder.Step;
 public record MadeLineDto(List<Step> line) {
 
     public boolean findPositionStepStatus(int stepPosition) {
-        System.out.println(stepPosition);
         return line.get(stepPosition).getBuildStatus();
     }
 }
