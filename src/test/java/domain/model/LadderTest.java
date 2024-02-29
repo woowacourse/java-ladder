af6e7ebaff86c374193e96313141517e110e00a4
tackyu <tack970913@gmail.com>
2024-02-29 15:43:05 +0900

    test: 사다리 타고 내려가는 로직 테스트 코드 추가

---
+++ src/test/java/domain/model/LadderTest.java
@@ -0,0 +1,27 @@
+package domain.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LadderTest {
+    @Test
+    @DisplayName("출발점으로부터 사다리를 타고 도착점을 반환한다.")
+    void findConsequenceTest() {
+        //given
+        Ladder ladder=new Ladder("2",3,()->true);
+        List<Integer> expected= List.of(1,2,3);
+        List<Integer> actual = new ArrayList<>();
+        //when
+        for(int index = 0; index <3; index++){
+            int indexOfConsequence=ladder.goToConsequence(index);
+            actual.add(indexOfConsequence);
+        }
+        //then
+        assertThat(expected).isEqualTo(expected);
+    }
+}
