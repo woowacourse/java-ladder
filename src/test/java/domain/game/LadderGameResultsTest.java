17320d073935fd6400607a6eccefa01b081bf18b
kelly6bf <kelly6bf@gmail.com>
2024-02-29 11:27:54 +0900

    test: 사용자 이름에 매칭되는 게임 결과를 반환하는 테스트 로직 작성

--- src/test/java/domain/game/LadderGameResultsTest.java
+++ src/test/java/domain/game/LadderGameResultsTest.java
@@ -28,4 +28,23 @@ public class LadderGameResultsTest {
         // Then
         assertThat(ladderGameResults).isNotNull();
     }
+
+    @DisplayName("사용자 이름을 입력하면 해당 사용자의 게임 결과를 반환한다.")
+    @Test
+    void 사용자_게임_결과_반환() {
+        // Given
+        final LadderHeight ladderHeight = new LadderHeight(5);
+        final Players players = Players.of(PlayerNames.of(List.of("kelly", "pobi", "ank")), ladderHeight);
+        final Ladder ladder = Ladder.of(() -> true, ladderHeight, 3);
+        final GameResults gameResults = GameResults.of(List.of("꽝", "당첨", "꽝"));
+        final LadderGameResults ladderGameResults = LadderGameResults.of(ladder, players, gameResults);
+        final String findPlayerName = "kelly";
+
+        // When
+        List<LadderGameResult> playerGameResults = ladderGameResults.findPlayerGameResults(findPlayerName);
+
+        // Then
+        assertThat(playerGameResults.get(0).getPlayerName().value()).isEqualTo(findPlayerName);
+        assertThat(playerGameResults.get(0).getGameResult().getGameResultDescriptionValue()).isEqualTo("당첨");
+    }
 }
