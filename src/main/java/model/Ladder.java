4dbee2cb7ccda7bab70589473f483fe719fceba4
ashsty <qkralsdk991@naver.com>
2024-02-29 11:21:57 +0900

    refactor : 메서드명, 변수명 수정
    - generateLine -> generateLadder
    - peopleCount -> totalStateAmount
    - generatedResult = makeLadderOrNot

--- src/main/java/model/Ladder.java
+++ src/main/java/model/Ladder.java
@@ -8,14 +8,14 @@ public class Ladder {
     private final List<Line> lines = new ArrayList<>();
 
     public Ladder(Height height, int peopleCount, RandomGenerator randomGenerator) {
-        generateLine(height, peopleCount, randomGenerator);
+        generateLadder(height, peopleCount, randomGenerator);
     }
 
-    public void generateLine(Height height, int peopleCount, RandomGenerator randomGenerator) {
+    public void generateLadder(Height height, int totalStateAmount, RandomGenerator randomGenerator) {
         int count = 0;
         while (!height.isDesignatedHeight(count)) {
-            List<Boolean> generatedResult = randomGenerator.generate(peopleCount);
-            lines.add(new Line(peopleCount, generatedResult));
+            List<Boolean> makeLadderOrNot = randomGenerator.generate(totalStateAmount - 1);
+            lines.add(new Line(makeLadderOrNot));
             count++;
         }
     }
