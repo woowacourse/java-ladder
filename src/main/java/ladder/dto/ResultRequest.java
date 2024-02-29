29737b1697d71d209e7ecc54c2de21a12a7d83a1
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2024-02-29 15:53:27 +0900

    feat (ResultRequest) : ResultRequest를 통해 프로그램 종료를 요청할 수 있도록 수정

--- src/main/java/ladder/dto/ResultRequest.java
+++ src/main/java/ladder/dto/ResultRequest.java
@@ -3,18 +3,23 @@ package ladder.dto;
 public class ResultRequest {
 
     private static final String NO_NAME = "";
-    public static final ResultRequest ALL = new ResultRequest(NO_NAME, true);
+    public static final ResultRequest ALL = new ResultRequest(NO_NAME, RequestCommand.ALL_RESULT);
+    public static final ResultRequest EXIT_PROGRAM = new ResultRequest(NO_NAME, RequestCommand.EXIT_PROGRAM);
 
     private final String playerName;
-    private final boolean isRequestAll;
+    private final RequestCommand requestCommand;
 
-    private ResultRequest(String playerName, boolean isRequestAll) {
+    public ResultRequest(String playerName, RequestCommand requestCommand) {
         this.playerName = playerName;
-        this.isRequestAll = isRequestAll;
+        this.requestCommand = requestCommand;
     }
 
     public ResultRequest(String playerName) {
-        this(playerName, false);
+        this(playerName, RequestCommand.SINGLE_RESULT);
+    }
+
+    public static ResultRequest from(String playerName) {
+        return new ResultRequest(playerName, RequestCommand.SINGLE_RESULT);
     }
 
     public String getPlayerName() {
@@ -22,6 +27,14 @@ public class ResultRequest {
     }
 
     public boolean isRequestAll() {
-        return isRequestAll;
+        return requestCommand == RequestCommand.ALL_RESULT;
+    }
+
+    public boolean isExitProgram() {
+        return requestCommand == RequestCommand.EXIT_PROGRAM;
+    }
+
+    private enum RequestCommand {
+        SINGLE_RESULT, ALL_RESULT, EXIT_PROGRAM
     }
 }
