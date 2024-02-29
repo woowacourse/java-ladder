c19ba85d94917775b4d1631cd34ee0cec9b34969
mzeong <mjeong@sookmyung.ac.kr>
2024-02-29 17:40:40 +0900

    test(IndexTest): 인덱스 데이터의 최소 및 최대 길이 검증

--- src/test/java/ladder/domain/IndexTest.java
+++ src/test/java/ladder/domain/IndexTest.java
@@ -1,6 +1,7 @@
 package ladder.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -31,5 +32,21 @@ class IndexTest {
 
         // then
         assertThat(expected.getValue()).isEqualTo(0);
+    }
+
+    @DisplayName("인덱스 데이터가 1글자 미만이면 예외가 발생한다.")
+    @Test
+    void createEmptyName() {
+        // when & then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Index(0, ""));
+    }
+
+    @Test
+    @DisplayName("인덱스 데이터가 5글자를 초과하면 예외가 발생한다.")
+    void createExceedMaxLengthName() {
+        // when & then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Index(0, "honux1"));
     }
 }
