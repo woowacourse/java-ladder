5d81bb0bf0d7e7bcbc6eb8c5840d03199f437982
ashsty <qkralsdk991@naver.com>
2024-02-29 11:31:50 +0900

    feat : 사다리 게임 실행 / 실행 결과 출력 기능 추가

--- src/main/java/controller/LadderGameController.java
+++ src/main/java/controller/LadderGameController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 import model.*;
@@ -19,22 +20,43 @@ public class LadderGameController {
 
     public void run() {
         Participants participants = retryUntilSuccess(inputView::readParticipantNames);
+        Prizes prizes = retryUntilSuccess(() -> inputView.readPrizesNames(participants.getSize()));
         Height height = retryUntilSuccess(inputView::readLadderHeight);
 
-        Ladder ladder = createLadder(height, participants);
+        Ladder ladder = new Ladder(height, participants.getSize(), generator);
+        LadderResult ladderResult = new LadderResult(participants, prizes, ladder);
 
-        printResult(participants, ladder);
+        printGame(participants, prizes, ladder);
+        printResult(participants, ladderResult);
     }
 
-    private Ladder createLadder(Height height, Participants participants) {
-        LadderGame ladderGame = new LadderGame(height, participants, generator);
-        return ladderGame.createLadder();
-    }
-
-    private void printResult(Participants participants, Ladder ladder) {
-        outputView.printResultHeader();
+    private void printGame(Participants participants, Prizes prizes, Ladder ladder) {
+        outputView.printLadderHeader();
         outputView.printParticipantsNames(participants);
         outputView.printLadder(ladder);
+        outputView.printPrizesNames(prizes);
+    }
+
+    private void printResult(Participants participants, LadderResult ladderResult) {
+        String target = inputView.readResultSearch();
+        outputView.printResultHeader();
+        if (target.equals("all")) {
+            List<Prize> results = ladderResult.searchAll(participants.getParticipants());
+            outputView.printEntireResult(participants, results);
+            return;
+        }
+        handlingSoloResult(participants, ladderResult, target);
+    }
+
+    private void handlingSoloResult(Participants participants, LadderResult ladderResult, String name) {
+        try {
+            Participant target = participants.checkExistence(name);
+            Prize result = ladderResult.searchOne(target);
+            outputView.printSoloResult(result);
+        } catch (IllegalArgumentException e) {
+            outputView.printErrorMessage(e.getMessage());
+            printResult(participants, ladderResult);
+        }
     }
 
     private <T> T retryUntilSuccess(Supplier<T> supplier) {
