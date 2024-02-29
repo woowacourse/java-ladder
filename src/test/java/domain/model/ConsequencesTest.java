f30e797e2aec01d8326d88ac4945688f4a9502e9
tackyu <tack970913@gmail.com>
2024-02-29 17:40:33 +0900

    test: 순서에 맞는 결과 이름 반환하는 메서드 구현

--- src/test/java/domain/model/ConsequencesTest.java
+++ src/test/java/domain/model/ConsequencesTest.java
@@ -2,9 +2,12 @@ package domain.model;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class ConsequencesTest {
@@ -13,5 +16,21 @@ class ConsequencesTest {
     void checkConsequencesSize() {
         assertThatThrownBy(()->new Consequences(List.of("10","20","30"),2))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+    
+    @ParameterizedTest
+    @CsvSource({
+            "0,100",
+            "1,꽝",
+            "2,200"
+    })
+    @DisplayName("순서에 알맞는 결과를 반환한다")
+    void getConsequenceNameByOrder(int order,String expected) {
+        //given
+        Consequences consequences=new Consequences(List.of("100","꽝","200"),3);
+        //when
+        String actual=consequences.getConsequenceByOrder(order);
+        //then
+        assertThat(expected).isEqualTo(actual);
     }
 }
