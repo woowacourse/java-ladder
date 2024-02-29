fb03e9ab5718a668b103ed269747e5582685ad1f
eunhee <dmsgml9903@gmail.com>
2024-02-29 14:51:01 +0900

    fix(Position): 위치를 감소하는 로직 수정 및 같은 value의 경우 동일하게 취급하도록 구현

--- src/main/java/model/Position.java
+++ src/main/java/model/Position.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Objects;
+
 public class Position {
     private final int value;
 
@@ -12,5 +14,35 @@ public class Position {
             throw new IllegalArgumentException("참가자의 위치는 음수가 될 수 없습니다.");
         }
         this.value = value;
+    }
+
+
+    public Position increment() {
+        return new Position(value + 1);
+    }
+
+    public Position decrement() {
+        return new Position(value - 1);
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Position position = (Position) o;
+        return getValue() == position.getValue();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getValue());
     }
 }
--- src/test/java/model/PositionTest.java
+++ src/test/java/model/PositionTest.java
@@ -4,22 +4,44 @@ import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class PositionTest {
 
+    @DisplayName("음수가 아니면 예외를 던지지 않는다.")
     @Test
-    void PositivePositionNotThrowException() {
+    void notNegativePosition() {
         assertAll(
                 () -> assertThatCode(Position::new).doesNotThrowAnyException(),
                 () -> assertThatCode(() -> new Position(1)).doesNotThrowAnyException()
         );
     }
 
+    @DisplayName("음수면 예외를 던진다.")
     @Test
-    void NotPositivePositionThrowException() {
+    void negativePositionThrowException() {
         Assertions.assertThatThrownBy(() -> new Position(-1))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("참가자의 위치는 음수가 될 수 없습니다.");
+    }
+
+    @DisplayName("같은 값을 가지면 동일하다.")
+    @Test
+    void SameValue() {
+        Assertions.assertThat(new Position(1)).isEqualTo(new Position(1));
+    }
+
+    //TODO: 예외처리: position이 0보다 작다면? List<Line>의 크기보다 크다면?
+    @DisplayName("값을 1 증가시킨다.")
+    @Test
+    void increment() {
+        Assertions.assertThat(new Position(1).increment()).isEqualTo(new Position(2));
+    }
+
+    @DisplayName("값을 1 감소시킨다.")
+    @Test
+    void decrement() {
+        Assertions.assertThat(new Position(1).decrement()).isEqualTo(new Position(0));
     }
 }
