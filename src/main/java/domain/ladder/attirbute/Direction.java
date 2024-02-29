2205f6077a8f400ab063b48fee8c93cfee831352
DobbyMac <kimdobby@g.skku.edu>
2024-03-01 01:01:39 +0900

    refactor: 사다리 출력 포맷팅 기능 롤백
    - Direction Enum Class에서 Symbol 필드 제거
    - OutputView에서 Direction을 받아 직접 Ladder 포맷팅

--- src/main/java/domain/ladder/attirbute/Direction.java
+++ src/main/java/domain/ladder/attirbute/Direction.java
@@ -2,17 +2,8 @@ package domain.ladder.attirbute;
 
 public enum Direction {
 
-    LEFT("---|      "),
-    RIGHT("|---"),
-    DOWN("|      ");
+    LEFT,
+    RIGHT,
+    DOWN;
 
-    private final String symbol;
-
-    Direction(String symbol) {
-        this.symbol = symbol;
-    }
-
-    public String getSymbol() {
-        return symbol;
-    }
 }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -2,23 +2,35 @@ package view;
 
 import domain.Name;
 import domain.ladder.attirbute.Direction;
-
+import java.util.EnumMap;
 import java.util.List;
 
 public class OutputView {
-    private static final String BLANK_SPACE = "      ";
+
     private static final int NAME_SPACE_SIZE = 7;
     private static final String NAME_SPACE_UNIT = " ";
+    private static final String DEFAULT_BLANK_SPACE = "      ";
+    private static final String LADDER_LEG_PIECE_SYMBOL = "|";
+    private static final String LADDER_LEG_BRANCH_SYMBOL = "------";
     private static final String ALL_RESULT_PRINT_FORMAT = "%s : %s";
+    private static final EnumMap<Direction, String> directionSymbols = initiateDirectionSymbolMap();
 
     private OutputView() {
     }
 
+    private static EnumMap<Direction, String> initiateDirectionSymbolMap() {
+        EnumMap<Direction, String> directionSymbolMap = new EnumMap<>(Direction.class);
+        directionSymbolMap.put(Direction.LEFT, DEFAULT_BLANK_SPACE);
+        directionSymbolMap.put(Direction.RIGHT, LADDER_LEG_BRANCH_SYMBOL);
+        directionSymbolMap.put(Direction.DOWN, DEFAULT_BLANK_SPACE);
+        return directionSymbolMap;
+    }
+
     public static void printObjectNames(List<? extends Name> names) {
         names.stream()
-             .map(Name::getValue)
-             .map(OutputView::padString)
-             .forEach(System.out::print);
+                .map(Name::getValue)
+                .map(OutputView::padString)
+                .forEach(System.out::print);
         printNewLine();
     }
 
@@ -26,22 +38,27 @@ public class OutputView {
         return NAME_SPACE_UNIT.repeat(NAME_SPACE_SIZE - name.length()) + name;
     }
 
-    public static void printNewLine() {
-        System.out.print(System.lineSeparator());
+    public static void printDirections(List<Direction> directions) {
+        print(formatLadderResultString(directions));
     }
 
-    public static void printDirections(List<Direction> directions) {
-        StringBuilder resultStringBuilder = new StringBuilder();
-
-        resultStringBuilder.append(BLANK_SPACE);
-        directions.forEach(direction -> resultStringBuilder.append(direction.getSymbol()));
-
-        print(resultStringBuilder.toString());
+    private static String formatLadderResultString(List<Direction> directions) {
+        StringBuilder result = new StringBuilder();
+        result.append(DEFAULT_BLANK_SPACE);
+        directions.forEach(direction -> {
+            result.append(LADDER_LEG_PIECE_SYMBOL);
+            result.append(directionSymbols.get(direction));
+        });
+        return result.toString();
     }
 
     public static void printAllResults(String name, String prize) {
         String resultAndPrize = String.format(ALL_RESULT_PRINT_FORMAT, name, prize);
         print(resultAndPrize);
+    }
+
+    public static void printNewLine() {
+        System.out.print(System.lineSeparator());
     }
 
     public static void print(String toBePrint) {
