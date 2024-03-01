bf8d307b488760699c7e3a5fa954947cc60fc14d
libienz <leekh10195@gmail.com>
2024-03-01 20:42:07 +0900

    feat: 방향 있는 연결 기반 RowLineGenerator 구현

---
+++ src/main/java/domain/RowLineGenerator1.java
@@ -0,0 +1,30 @@
+package domain;
+
+import static domain.Connection.DISCONNECTED;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RowLineGenerator1 {
+    private final ConnectionGenerator1 connectionGenerator1;
+
+    public RowLineGenerator1(ConnectionGenerator1 connectionGenerator1) {
+        this.connectionGenerator1 = connectionGenerator1;
+    }
+
+    public RowLine1 generate(int personCount) {
+        Connection previous = DISCONNECTED;
+        List<Point> points = new ArrayList<>();
+
+        for (int i = 0; i < personCount - 1; i++) {
+            Connection generated = connectionGenerator1.generate(previous);
+            new Point(new ColumnPosition(i), generated);
+            previous = generated;
+        }
+
+        Connection generated = connectionGenerator1.generate(previous);
+        points.add(new Point(new ColumnPosition(personCount - 1), generated));
+
+        return new RowLine1(points);
+    }
+}
