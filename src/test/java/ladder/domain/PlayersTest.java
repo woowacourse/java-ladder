d8392e5bb5bdb9e20113d90ac687c85b20b1af2c
mzeong <mjeong@sookmyung.ac.kr>
2024-03-01 14:23:48 +0900

    test(PlayersTest): 참여자의 인덱스 조회

--- src/test/java/ladder/domain/PlayersTest.java
+++ src/test/java/ladder/domain/PlayersTest.java
@@ -3,6 +3,8 @@ package ladder.domain;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -37,5 +39,30 @@ class PlayersTest {
                 new Player("crong"),
                 new Player("jk")
         );
+    }
+
+    @DisplayName("참여자의 인덱스를 찾는다.")
+    @ParameterizedTest
+    @CsvSource(value = {"pobi, 0", "honux, 1", "crong, 2", "jk, 3"})
+    void findIndexOfPlayer(String name, int actual) {
+        // given
+        Players players = new Players("pobi", "honux", "crong", "jk");
+
+        // when
+        Index expected = players.findIndexOfPlayer(new Player(name));
+
+        // then
+        assertThat(expected.getValue()).isEqualTo(actual);
+    }
+
+    @DisplayName("참여자가 아니라면 예외가 발생한다.")
+    @Test
+    void findIndexOfNonPlayer() {
+        // given
+        Players players = new Players("pobi", "honux", "crong", "jk");
+
+        // when & then
+        assertThatThrownBy(() -> players.findIndexOfPlayer(new Player("pobi1")))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
