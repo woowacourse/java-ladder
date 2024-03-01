05123a082d069fb33f6b8e5ba2a01ef0c811eb4b
tackyu <tack970913@gmail.com>
2024-03-01 18:09:28 +0900

    test: all 이름 금지 추가

--- src/main/java/domain/model/Person.java
+++ src/main/java/domain/model/Person.java
@@ -9,6 +9,7 @@ public class Person {
     public Person(final String name) {
         validateNameLength(name);
         validateNonBlank(name);
+        validateNamedAll(name);
         this.name = name;
     }
 
@@ -16,18 +17,22 @@ public class Person {
         return this.name;
     }
 
-    public void validateNameLength(String name) {
+    private void validateNameLength(String name) {
         if (name.length() > SIZE_LIMIT) {
             throw new IllegalStateException("이름은 5글자 이하입니다.");
         }
     }
 
-    public void validateNonBlank(String name) {
+    private void validateNonBlank(String name) {
         if (name.isBlank()) {
             throw new IllegalArgumentException("공백 이름은 허용하지 않습니다.");
         }
     }
-
+    private void validateNamedAll(String name){
+        if(name.equals("all")){
+            throw new IllegalArgumentException("all이란 이름은 허용하지 않습니다.");
+        }
+    }
     @Override
     public boolean equals(Object obj) {
         if(!(obj instanceof Person)){
