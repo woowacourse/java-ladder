2de28816427aff375397c3ab968e22f037ea550c
robinjoon <robin980108@naver.com>
2024-03-02 00:10:47 +0900

    feat: 상품 이름 입력기능 구현

--- src/main/java/view/GiftsInputView.java
+++ src/main/java/view/GiftsInputView.java
@@ -1,10 +1,36 @@
 package view;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 
 public class GiftsInputView {
+    private static final String SEPARATOR = ",";
+
     public static List<String> getGiftNames(String rawString) {
-        return Collections.emptyList();
+        validateSeparator(rawString);
+        return splitName(rawString);
+    }
+
+    private static void validateSeparator(String giftNames) {
+        validateStarOrEndWithSeparator(giftNames);
+        validateDuplicateSeparator(giftNames);
+    }
+
+    private static void validateStarOrEndWithSeparator(String giftNames) {
+        boolean startsWith = giftNames.startsWith(SEPARATOR);
+        boolean endsWith = giftNames.endsWith(SEPARATOR);
+        if (startsWith || endsWith) {
+            throw new IllegalArgumentException("입력이 구분자로 시작하거나 끝나면 안됩니다.");
+        }
+    }
+
+    private static void validateDuplicateSeparator(String names) {
+        if (names.contains(SEPARATOR + SEPARATOR)) {
+            throw new IllegalArgumentException("입력에 구분자가 연속으로 등장하면 안됩니다.");
+        }
+    }
+
+    private static List<String> splitName(String giftNames) {
+        return Arrays.stream(giftNames.split(SEPARATOR)).toList();
     }
 }
