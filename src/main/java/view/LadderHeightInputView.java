25e65b44f7012f4714b950cd47a9af2bc605b455
robinjoon <robin980108@naver.com>
2024-03-02 00:23:39 +0900

    feat: 사다리 높이 입력 기능 구현

--- src/main/java/view/LadderHeightInputView.java
+++ src/main/java/view/LadderHeightInputView.java
@@ -2,6 +2,10 @@ package view;
 
 public class LadderHeightInputView {
     public static int getLadderHeight(String rawString) {
-        return 0;
+        try {
+            return Integer.parseInt(rawString);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자가 아닙니다.");
+        }
     }
 }
