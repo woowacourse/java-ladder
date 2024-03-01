bbe2d23e4eb9b19f6d65c6e83194169c52de68af
robinjoon <robin980108@naver.com>
2024-03-02 00:31:22 +0900

    feat: 사다리 출력을 위한 문자열 생성 기능 구현

--- src/main/java/view/LadderPrinter.java
+++ src/main/java/view/LadderPrinter.java
@@ -1,9 +1,12 @@
 package view;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LadderPrinter {
     public static String from(List<List<Boolean>> rawLadder) {
-        return null;
+        return rawLadder.stream()
+                .map(LinePrinter::from)
+                .collect(Collectors.joining("\n"));
     }
 }
