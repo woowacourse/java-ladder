b6fa492d125dc6dcf1123704a8992ebe174f3bc2
seunghye218 <seunghye@student.42seoul.kr>
2024-03-01 17:51:51 +0900

    fix: Ladder 생성 가로 길이 유효성 검사 추가

--- src/main/java/domain/Ladder.java
+++ src/main/java/domain/Ladder.java
@@ -1,6 +1,7 @@
 package domain;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.IntStream;
 
 
@@ -9,14 +10,13 @@ public class Ladder {
     private final List<Row> rows;
 
     public Ladder(final int width, final int height) {
-        validationHeight(height);
+        validationSize(width, height);
 
         LadderStrategy strategy = new RandomLadderStrategy();
         this.rows = IntStream.range(0, height)
                 .mapToObj(i -> new Row(width, strategy))
                 .toList();
     }
-
     public int match(int index) {
         validationIndex(index);
 
@@ -31,13 +31,13 @@ public class Ladder {
         return rows;
     }
 
-    private void validationHeight(int height) {
-        if (height < 1) {
-            throw new IllegalArgumentException("높이는 1 이상이어야 한다.");
+    private void validationSize(final int width, final int height) {
+        if (width < 1 || height < 1) {
+            throw new IllegalArgumentException("가로 및 높이는 1 이상이어야 한다.");
         }
     }
 
-    private void validationIndex(int index) {
+    private void validationIndex(final int index) {
         if (index >= rows.size() || index < 0) {
             throw new IllegalArgumentException("위치는 0 이상 가로 길이 미만 이어야 한다.");
         }
--- src/test/java/domain/LadderTest.java
+++ src/test/java/domain/LadderTest.java
@@ -2,7 +2,9 @@ package domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -10,6 +12,15 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 
 class LadderTest {
+
+    @ParameterizedTest
+    @CsvSource({"0,1", "1,0"})
+    @DisplayName("사다리의 가로나 세로 값은 1 이상 이어야 한다")
+    void testCreate(int width, int height) {
+        assertThatThrownBy(() -> new Ladder(width, height))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("가로 및 높이는 1 이상이어야 한다.");
+    }
 
     @RepeatedTest(10)
     @DisplayName("사다리를 내려간 후 위치는 가로 길이 이내이다")
