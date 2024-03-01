0db785ccc73dc4620cd1355449b4dd83edfa574a
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-03-01 22:45:28 +0900

    test(PersonCountTest): 참여 인원에 대한 검증 테스트 추가

---
+++ src/test/java/model/PersonCountTest.java
@@ -0,0 +1,26 @@
+package model;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class PersonCountTest {
+
+    @Test
+    @DisplayName("참여 인원을 생성한다.")
+    void createPersonCount() {
+        assertThatCode(() -> new PersonCount(3));
+    }
+
+    @ParameterizedTest(name = "참여 인원은 최소 2여야 한다.")
+    @ValueSource(ints = {0, -1, 1})
+    void createPersonCountThrowException(final int personCount) {
+        assertThatThrownBy(() -> new PersonCount(personCount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("참여 인원은 최소 2여야 합니다.");
+    }
+}
