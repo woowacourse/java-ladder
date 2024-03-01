7b398e4e885984c77b8ba00c92938cc3b4d3e6d6
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-03-01 16:51:49 +0900

    refactor(Item): name 문자열 포장

--- src/main/java/model/Item.java
+++ src/main/java/model/Item.java
@@ -1,13 +1,13 @@
 package model;
 
 public class Item {
-    private final String name;
+    private final Name name;
 
     public Item(final String name) {
-        this.name = name;
+        this.name = new ItemName(name);
     }
 
     public String getName() {
-        return name;
+        return name.getName();
     }
 }
