3cd244300e159e8dfd5e97dbd83df577fd90272c
tackyu <tack970913@gmail.com>
2024-03-01 14:32:24 +0900

    feat: 개별 결과값 포장 클래스 생성

---
+++ src/main/java/domain/model/Consequence.java
@@ -0,0 +1,18 @@
+package domain.model;
+
+public class Consequence {
+    private final String value;
+
+    public Consequence(String value) {
+        validateSize(value);
+        this.value = value;
+    }
+    private void validateSize(String value){
+        if(value.isBlank()){
+            throw new IllegalArgumentException("결과에 공백값은 허용되지 않습니다.");
+        }
+    }
+    public String getValue() {
+        return value;
+    }
+}
