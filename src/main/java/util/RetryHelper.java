8e843cf59351354ff5299faa255ef879b5903845
robinjoon <robin980108@naver.com>
2024-03-02 02:01:00 +0900

    feat: 일정 횟수까지만 로직이 재시도 되게 하는 기능 구현

--- src/main/java/util/RetryHelper.java
+++ src/main/java/util/RetryHelper.java
@@ -3,10 +3,29 @@ package util;
 import java.util.function.Supplier;
 
 class RetryHelper {
+
+    private final int maxRetryCount;
+    private int retryCount = -1;
+
     public RetryHelper(int maxRetryCount) {
+        this.maxRetryCount = maxRetryCount;
     }
 
     public <E> E retry(Supplier<E> supplier) {
-        return null;
+        E result = null;
+        while (result == null && retryCount < maxRetryCount) {
+            result = useSupplier(supplier);
+        }
+        return result;
+    }
+
+    private <E> E useSupplier(Supplier<E> supplier) {
+        retryCount++;
+        try {
+            return supplier.get();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
     }
 }
