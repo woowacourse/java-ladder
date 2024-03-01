9da3615b909bfcd10a3a76dd92e8411af2d8d193
Nam Gi Beom <rlqja0523@naver.com>
2024-03-01 22:55:09 +0900

    refactor: 변수명 일괄 수정

--- src/main/java/domain/prize/Prize.java
+++ src/main/java/domain/prize/Prize.java
@@ -3,11 +3,11 @@ package domain.prize;
 import java.util.regex.Pattern;
 
 public class Prize {
-    private static final int MAX_PRIZE_NAME_LENGTH = 10;
-    private static final int MIN_PRIZE_NAME_LENGTH = 1;
-    private static final Pattern PRIZE_NAME_PATTERN = Pattern.compile("^[a-zA-Z가-힣0-9]*$"); // todo name name
-    private static final String PRIZE_LENGTH_EXCEPTION_MESSAGE = "[ERROR] 잘못된 상품명: %s - 상품명의 길이는 %d ~ %d 글자여야 합니다.";
-    private static final String PRIZE_PATTERN_EXCEPTION_MESSAGE = "[ERROR] 잘못된 상품명: %s - 상품명은 한글, 영문자, 숫자만 가능합니다.";
+    private static final int MIN_NAME_LENGTH = 1;
+    private static final int MAX_NAME_LENGTH = 10;
+    private static final Pattern PRIZE_NAME_PATTERN = Pattern.compile("^[a-zA-Z가-힣0-9]*$");
+    private static final String NAME_LENGTH_EXCEPTION_MESSAGE = "[ERROR] 잘못된 상품명: %s - 상품명의 길이는 %d ~ %d 글자여야 합니다.";
+    private static final String NAME_PATTERN_EXCEPTION_MESSAGE = "[ERROR] 잘못된 상품명: %s - 상품명은 한글, 영문자, 숫자만 가능합니다.";
 
     private final String name;
 
@@ -18,9 +18,9 @@ public class Prize {
     }
 
     private void validateLength(String name) {
-        if (name.length() < MIN_PRIZE_NAME_LENGTH || MAX_PRIZE_NAME_LENGTH < name.length()) {
+        if (name.length() < MIN_NAME_LENGTH || MAX_NAME_LENGTH < name.length()) {
             throw new IllegalArgumentException(
-                    String.format(PRIZE_LENGTH_EXCEPTION_MESSAGE, name, MIN_PRIZE_NAME_LENGTH, MAX_PRIZE_NAME_LENGTH)
+                    String.format(NAME_LENGTH_EXCEPTION_MESSAGE, name, MIN_NAME_LENGTH, MAX_NAME_LENGTH)
             );
         }
     }
@@ -28,7 +28,7 @@ public class Prize {
     private void validateNamePattern(String name) {
         if (!PRIZE_NAME_PATTERN.matcher(name).matches()) {
             throw new IllegalArgumentException(
-                    String.format(PRIZE_PATTERN_EXCEPTION_MESSAGE, name)
+                    String.format(NAME_PATTERN_EXCEPTION_MESSAGE, name)
             );
         }
     }
