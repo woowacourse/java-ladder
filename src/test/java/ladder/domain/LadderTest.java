4beaf58d415d5ef861ba3d2176e65fcd168c2d64
ay-eonii <naayen@naver.com>
2024-03-01 18:24:34 +0900

    fix(RandomPointsGenerator): 마지막 points 무조건 OFF 생성하도록 수정

--- src/test/java/ladder/domain/LadderTest.java
+++ src/test/java/ladder/domain/LadderTest.java
@@ -12,14 +12,14 @@ class LadderTest {
     private final RandomPointsGenerator pointsGenerator1 = new RandomPointsGenerator() {
         @Override
         public List<Point> generate(int size) {
-            return List.of(Point.ON, Point.OFF, Point.ON);
+            return List.of(Point.ON, Point.OFF, Point.ON, Point.OFF);
         }
     };
 
     private final RandomPointsGenerator pointsGenerator2 = new RandomPointsGenerator() {
         @Override
         public List<Point> generate(int size) {
-            return List.of(Point.OFF, Point.ON, Point.OFF);
+            return List.of(Point.OFF, Point.ON, Point.OFF, Point.OFF);
         }
     };
 
@@ -27,8 +27,8 @@ class LadderTest {
     @DisplayName("사다리를 연속으로 탄다.")
     void rideLadder() {
         // given
-        Line line1 = new Line(pointsGenerator1.generate(3));
-        Line line2 = new Line(pointsGenerator2.generate(3));
+        Line line1 = new Line(pointsGenerator1.generate(4));
+        Line line2 = new Line(pointsGenerator2.generate(4));
 
         Ladder ladder = new Ladder(List.of(line1, line2));
 
