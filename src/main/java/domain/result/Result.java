034fa29d943998d218d9f2dff12b3ef6c19c68d7
dwax1324 <dwax1324@gmail.com>
2024-03-02 05:15:26 +0900

    feat: 결과의 이름이 5글자를 초과하면 예외가 발생한다

--- src/main/java/domain/result/Result.java
+++ src/main/java/domain/result/Result.java
@@ -11,18 +11,20 @@ public class Result {
 
     private void validate(final String name) {
         validateEmptiness(name);
-//        validateLength(name);
+        validateLength(name);
     }
 
     private void validateEmptiness(final String name) {
         if (name.isEmpty() || name.isBlank()) {
-            throw new IllegalArgumentException("결과는 비어있을 수 없습니다");
+            throw new IllegalArgumentException("결과는 비어있을 수 없습니다.");
         }
     }
 
-//    private void validateLength(final String name) {
-//        if (name.length() > MAX_LENGTH)
-//    }
+    private void validateLength(final String name) {
+        if (name.length() > MAX_LENGTH) {
+            throw new IllegalArgumentException(String.format("입력된 값: %s, 결과의 최대 길이는 5입니다.", name));
+        }
+    }
 
     @Override
     public String toString() {
--- src/test/java/domain/result/ResultTest.java
+++ src/test/java/domain/result/ResultTest.java
@@ -1,19 +1,23 @@
 package domain.result;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
-import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class ResultTest {
     @ParameterizedTest
-    @ValueSource(strings = {""," ","    "})
+    @ValueSource(strings = {"", " ", "    "})
     @DisplayName("결과의 이름이 비어 있으면 예외가 발생한다")
-    void emptyName(String name) {
+    void emptyName(final String name) {
+        assertThatCode(() -> new Result(name)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings ={"abcdef","qwrqwrqwrkkk"})
+    @DisplayName("결과의 이름이 5글자를 초과하면 예외가 발생한다")
+    void nameTest(final String name) {
         assertThatCode(() -> new Result(name)).isInstanceOf(IllegalArgumentException.class);
     }
 }
