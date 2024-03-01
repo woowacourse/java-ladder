25c1631cd4e620b3856a8ad2277964a8fe019881
tackyu <tack970913@gmail.com>
2024-03-01 15:04:11 +0900

    feat: 개별 결과값 포장 클래스 생성에 따른 코드 수정

--- src/main/java/domain/model/Consequences.java
+++ src/main/java/domain/model/Consequences.java
@@ -1,20 +1,30 @@
 package domain.model;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Consequences {
-    private final List<String> consequences;
+    private final List<Consequence> consequences;
 
     public Consequences(List<String> consequences, int numberOfPeople) {
-        validateSize(consequences,numberOfPeople);
-        this.consequences = consequences;
+        validateSize(consequences, numberOfPeople);
+        this.consequences = consequences.stream()
+                .map(Consequence::new)
+                .toList();
     }
-    private void validateSize(List<String> consequences,int numberOfPeople){
-        if(consequences.size()!=numberOfPeople) {
+
+    private void validateSize(List<String> consequences, int numberOfPeople) {
+        if (consequences.size() != numberOfPeople) {
             throw new IllegalArgumentException("결과는 사람 수와 일치해야 합니다.");
         }
     }
-    public String getConsequenceByOrder(int order){
-        return consequences.get(order);
+
+    public String getConsequenceByOrder(int order) {
+        Consequence consequence=consequences.get(order);
+        return consequence.getValue();
+    }
+
+    public List<Consequence> getConsequences() {
+        return consequences;
     }
 }
