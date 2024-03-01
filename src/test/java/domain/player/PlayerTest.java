7e360b901b37f0892c005cc7df0a0a817789d266
Nam Gi Beom <rlqja0523@naver.com>
2024-03-01 22:47:17 +0900

    test: 부족한 테스트 케이스 추가 (이름 길이가 올바를 때 예외 발생 x)

--- src/test/java/domain/player/PlayerTest.java
+++ src/test/java/domain/player/PlayerTest.java
@@ -1,5 +1,6 @@
 package domain.player;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.params.ParameterizedTest;
@@ -7,9 +8,18 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 public class PlayerTest {
     @ParameterizedTest
-    @ValueSource(strings = {"1", "123456"})
-    void 이름의_길이가_2보다_작거나_5보다_크면_예외가_발생한다(String name) {
+    @ValueSource(strings = {"프", "프린프린프린"})
+    void 이름의_길이가_범위를_벗어나면_객체를_생성할_때_예외가_발생한다(String name) {
+        // when & then
         assertThatThrownBy(() -> new Player(name))
                 .isExactlyInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"프린", "프린프린프"})
+    void 이름의_길이가_올바르면_객체를_생성할_때_예외가_발생하지_않는다(String name) {
+        // when & then
+        assertThatCode(() -> new Player(name))
+                .doesNotThrowAnyException();
     }
 }
