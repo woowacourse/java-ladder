8c44c732d7d80caebf1928c46de40a3a6d73bc87
robinjoon <robin980108@naver.com>
2024-03-01 21:52:57 +0900

    feat: 참가자 이름이 중복되는지 검증하는 기능 구현

--- src/main/java/domain/Players.java
+++ src/main/java/domain/Players.java
@@ -1,16 +1,28 @@
 package domain;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 class Players {
     private final List<Player> players;
 
     Players(List<Player> players) {
+        validateDuplicateName(players);
         this.players = Collections.unmodifiableList(players);
     }
 
+    private void validateDuplicateName(List<Player> players) {
+        Set<Player> distinctPlayers = new HashSet<>(players);
+        if (distinctPlayers.size() != players.size()) {
+            throw new IllegalStateException("이름이 같은 참가자는 있을 수 없습니다.");
+        }
+    }
+
     static Players of(String... playerNames) {
-        return new Players(null);
+        List<Player> players = Arrays.stream(playerNames).map(Player::new).toList();
+        return new Players(players);
     }
 }
