4e063096e0fb3e711f93757a8dadf4d8987048d9
tackyu <tack970913@gmail.com>
2024-03-01 21:57:35 +0900

    feat: 컨트롤러 생성자 변경에 따른 코드 수정

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,11 +1,9 @@
 import controller.LadderController;
-import view.InputView;
-import view.ResultView;
 
 public class Application {
 
     public static void main(String[] args) {
-        LadderController ladderController=new LadderController(new ResultView(),new InputView());
-        ladderController.make();
+        LadderController ladderController = new LadderController();
+        ladderController.execute();
     }
 }
