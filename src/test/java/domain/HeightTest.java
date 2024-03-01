0aaf95ce933c00a5cc139bb00b953aa980df6bee
eunjungL <dldmswjd322@gmail.com>
2024-03-02 01:18:11 +0900

    test: Height 입력 값 검증 테스트 추가

--- src/test/java/domain/HeightTest.java
+++ src/test/java/domain/HeightTest.java
@@ -4,19 +4,16 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
-import util.TestBooleanGenerator;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class LadderTest {
-
+class HeightTest {
     @DisplayName("사다리 높이가 자연수가 아닐 경우 예외 처리")
     @ParameterizedTest
     @NullAndEmptySource
     @ValueSource(strings = {"abc", "-1", "0"})
     void validateHeightTest(String height) {
-        TestBooleanGenerator testBooleanGenerator = new TestBooleanGenerator(false);
-        assertThatThrownBy(() -> Ladder.of(height, 5, testBooleanGenerator))
+        assertThatThrownBy(() -> new Height(height))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
