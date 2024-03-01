7a94a68a4fbad60e92b79726dfc1ead68eddd4b5
robinjoon <robin980108@naver.com>
2024-03-02 00:04:02 +0900

    feat: 참가자 이름 입력기능 구현

--- src/main/java/view/PlayersInputView.java
+++ src/main/java/view/PlayersInputView.java
@@ -1,10 +1,36 @@
 package view;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 
 public class PlayersInputView {
+    private static final String SEPARATOR = ",";
+
     public static List<String> getPlayerNames(String rawString) {
-        return Collections.emptyList();
+        validateSeparator(rawString);
+        return splitName(rawString);
+    }
+
+    private static void validateSeparator(String playerNames) {
+        validateStarOrEndWithSeparator(playerNames);
+        validateDuplicateSeparator(playerNames);
+    }
+
+    private static void validateStarOrEndWithSeparator(String playerNames) {
+        boolean startsWith = playerNames.startsWith(SEPARATOR);
+        boolean endsWith = playerNames.endsWith(SEPARATOR);
+        if (startsWith || endsWith) {
+            throw new IllegalArgumentException("입력이 구분자로 시작하거나 끝나면 안됩니다.");
+        }
+    }
+
+    private static void validateDuplicateSeparator(String names) {
+        if (names.contains(SEPARATOR + SEPARATOR)) {
+            throw new IllegalArgumentException("입력에 구분자가 연속으로 등장하면 안됩니다.");
+        }
+    }
+
+    private static List<String> splitName(String playerNames) {
+        return Arrays.stream(playerNames.split(SEPARATOR)).toList();
     }
 }
