1a7c3621f1a581af1f09a8672ac9c6094d8ad41e
Nam Gi Beom <rlqja0523@naver.com>
2024-03-01 22:52:27 +0900

    feat: 이름에 한글, 영문자만 존재하는지 검증하는 기능 추가

--- src/main/java/domain/player/Player.java
+++ src/main/java/domain/player/Player.java
@@ -1,21 +1,34 @@
 package domain.player;
 
+import java.util.regex.Pattern;
+
 public class Player {
     private static final int MINIMUM_NAME_LENGTH = 2;
     private static final int MAXIMUM_NAME_LENGTH = 5;
-    private static final String LENGTH_EXCEPTION_MESSAGE = "[ERROR] 잘못된 이름: %s - 이름의 길이는 %d ~ %d 글자여야 합니다.";
+    private static final Pattern PLAYER_NAME_PATTERN = Pattern.compile("^[a-zA-Z가-힣]*$");
+    private static final String NAME_LENGTH_EXCEPTION_MESSAGE = "[ERROR] 잘못된 이름: %s - 이름의 길이는 %d ~ %d 글자여야 합니다.";
+    private static final String NAME_PATTERN_EXCEPTION_MESSAGE = "[ERROR] 잘못된 이름: %s - 이름은 한글, 영문자만 가능합니다.";
 
     private final String name;
 
     public Player(String name) {
         validateLength(name);
+        validateNamePattern(name);
         this.name = name;
     }
 
-    private void validateLength(final String name) {
-        if (name.length() < MINIMUM_NAME_LENGTH || name.length() > MAXIMUM_NAME_LENGTH) {
+    private void validateLength(String name) {
+        if (name.length() < MINIMUM_NAME_LENGTH || MAXIMUM_NAME_LENGTH < name.length()) {
             throw new IllegalArgumentException(
-                    String.format(LENGTH_EXCEPTION_MESSAGE, name, MINIMUM_NAME_LENGTH, MAXIMUM_NAME_LENGTH)
+                    String.format(NAME_LENGTH_EXCEPTION_MESSAGE, name, MINIMUM_NAME_LENGTH, MAXIMUM_NAME_LENGTH)
+            );
+        }
+    }
+
+    private void validateNamePattern(String name) {
+        if (!PLAYER_NAME_PATTERN.matcher(name).matches()) {
+            throw new IllegalArgumentException(
+                    String.format(NAME_PATTERN_EXCEPTION_MESSAGE, name)
             );
         }
     }
