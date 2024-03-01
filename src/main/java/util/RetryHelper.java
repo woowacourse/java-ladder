01a40dffd2fdfb9eb3002c9c38205f0297f56108
robinjoon <robin980108@naver.com>
2024-03-02 02:00:18 +0900

    test: 일정 횟수까지만 로직이 재시도 되는지 검증하는 테스트 추가

---
+++ src/main/java/util/RetryHelper.java
@@ -0,0 +1,12 @@
+package util;
+
+import java.util.function.Supplier;
+
+class RetryHelper {
+    public RetryHelper(int maxRetryCount) {
+    }
+
+    public <E> E retry(Supplier<E> supplier) {
+        return null;
+    }
+}
