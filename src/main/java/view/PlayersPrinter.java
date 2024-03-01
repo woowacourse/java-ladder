87f62171b1eb21f9f101aca8ed35bd34aaee21e3
robinjoon <robin980108@naver.com>
2024-03-02 00:36:18 +0900

    feat: 참가자 출력을 위한 문자열 생성 기능 구현

--- src/main/java/view/PlayersPrinter.java
+++ src/main/java/view/PlayersPrinter.java
@@ -1,9 +1,12 @@
 package view;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PlayersPrinter {
     public static String from(List<String> rawPlayerNames) {
-        return null;
+        return rawPlayerNames.stream()
+                .map(PlayerPrinter::from)
+                .collect(Collectors.joining(" "));
     }
 }
