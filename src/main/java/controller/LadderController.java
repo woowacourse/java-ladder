1be4ba9504cd9a335907b1c698220cefbd661f68
fram1998 <koust6u@pusan.ac.kr>
2024-03-01 19:01:00 +0900

    feat: 게임 종료하는 기능 추가

--- src/main/java/controller/LadderController.java
+++ src/main/java/controller/LadderController.java
@@ -1,31 +1,65 @@
 package controller;
 
-import handler.ExceptionHandler;
+import java.util.Arrays;
 import model.Ladder;
+import model.LadderGame;
 import model.People;
+import model.Prize;
+import model.RewardBoard;
 import utils.ThresholdCheckerImpl;
 import view.InputView;
 import view.ResultView;
 
 public class LadderController {
+    private static final String DELIMITER = ",";
+    private static final String ALL_COMMAND = "all";
+    private static final String EXIT_COMMAND = "exit";
 
     private final ResultView resultView;
     private final InputView inputView;
-    private final ExceptionHandler handler;
+    private LadderGame ladderGame;
 
     public LadderController() {
         this.resultView = new ResultView();
         this.inputView = new InputView();
-        this.handler = new ExceptionHandler();
     }
 
-    public void runLadderGame() {
-        People people = handler.handleInputException(() -> new People(inputView.askParticipants()));
-        Ladder ladder = handler.handleInputException(
-                () -> new Ladder(new ThresholdCheckerImpl(), inputView.askLadderHeight(),
-                        people.getParticipantsSize()));
-
-        resultView.printResult(people, ladder);
+    public void proceedGame() {
+        launchGame();
+        while (true) {
+            String participantName = inputView.askGameResult();
+            if (isExit(participantName)) {
+                break;
+            }
+            getPrize(ladderGame.getPeople(), participantName);
+        }
     }
 
+    private void launchGame() {
+        People people = new People(inputView.askParticipants());
+        String prizesText = inputView.askPrizes();
+        Ladder ladder = new Ladder(new ThresholdCheckerImpl(), inputView.askLadderHeight(),
+                people.getParticipantsSize());
+        ladderGame = new LadderGame(people, ladder, prizesText);
+        resultView.printLadderResult(people, ladder);
+        resultView.printPrizes(Arrays.stream(prizesText.split(DELIMITER)).toList());
+    }
+
+    private void getPrize(People people, String participantName) {
+        RewardBoard rewardBoard = ladderGame.getRewardBoard();
+        if (isAll(participantName)) {
+            resultView.printAllProceedResult(rewardBoard, people);
+            return;
+        }
+        Prize findPrize = rewardBoard.findPrizeByName(participantName);
+        resultView.printProceedResult(findPrize);
+    }
+
+    private boolean isAll(String command) {
+        return command.equals(ALL_COMMAND);
+    }
+
+    private boolean isExit(String command) {
+        return command.equals(EXIT_COMMAND);
+    }
 }
