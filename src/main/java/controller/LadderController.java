42f357aa35984e0a39822534574dbc6904eca9a7
tackyu <tack970913@gmail.com>
2024-03-01 21:57:17 +0900

    feat: 컨트롤러 구현

--- src/main/java/controller/LadderController.java
+++ src/main/java/controller/LadderController.java
@@ -1,42 +1,84 @@
 package controller;
 
-import domain.model.Ladder;
-import domain.model.People;
+import domain.model.*;
 import handler.ExceptionHandler;
 import utils.RuleGeneratorImpl;
 import view.InputView;
 import view.ResultView;
 
 import java.util.List;
+import java.util.Map;
 import java.util.function.Supplier;
 
 public class LadderController {
 
-    private final ResultView resultView;
-    private final InputView inputView;
-    private final ExceptionHandler handler;
+    private final ResultView resultView = new ResultView();
+    private final InputView inputView = new InputView();
+    private final ExceptionHandler handler = new ExceptionHandler();
+    private final People people;
+    private final Consequences consequences;
+    private final Ladder ladder;
 
-    public LadderController(ResultView resultView, InputView inputView) {
-        this.resultView = resultView;
-        this.inputView = inputView;
-        this.handler = new ExceptionHandler();
+    public LadderController() {
+        this.people = registerPeople();
+        this.consequences = registerConsequences();
+        this.ladder = makeLadder();
     }
 
-    public void make() {
+    public void execute() {
+        resultView.printLadderGame(people, ladder, consequences);
+        Result result = playGame();
+        showResult(result);
+    }
+
+    private void showResult(Result result) {
+        String chosen = askChosen();
+        if (chosen.equals("all")) {
+            resultView.printAll(result);
+            return;
+        }
+        resultView.printResult(result, new Person(chosen));
+        showResult(result);
+    }
+
+    private Result playGame() {
+        LadderGame ladderGame = new LadderGame(ladder, people, consequences);
+        ladderGame.play();
+        return ladderGame.giveResult();
+    }
+
+    private String askChosen() {
+        Supplier<String> chosenSupplier = () -> {
+            String chosen = inputView.askChosen();
+            return people.findProperParticipant(chosen);
+        };
+        return registerWithRetry(chosenSupplier);
+    }
+
+    private Ladder makeLadder() {
+        Supplier<Ladder> ladderSupplier = () -> {
+            String height = inputView.askLadderHeight();
+            return new Ladder(height, people.getNumberOfParticipants(), new RuleGeneratorImpl());
+        };
+        return registerWithRetry(ladderSupplier);
+    }
+
+    private People registerPeople() {
         Supplier<People> peopleSupplier = () -> {
             List<String> participants = inputView.askParticipants();
             return new People(participants);
         };
-        People people = registerWithRetry(peopleSupplier);
-
-        Supplier<Ladder> ladderSupplier = () -> {
-            String ladder=inputView.askLadderHeight();
-            return new Ladder(ladder, people.getNumberOfParticipants(),new RuleGeneratorImpl());
-        };
-        Ladder ladder = registerWithRetry(ladderSupplier);
-
-        resultView.printResult(people, ladder);
+        return registerWithRetry(peopleSupplier);
     }
+
+    private Consequences registerConsequences() {
+        Supplier<Consequences> consequencesSupplier = () -> {
+            List<String> consequences = inputView.askConsequences();
+            return new Consequences(consequences, people.getNumberOfParticipants());
+        };
+        return registerWithRetry(consequencesSupplier);
+    }
+
 
     public <T> T registerWithRetry(Supplier<T> callback) {
         return handler.handle(callback);
