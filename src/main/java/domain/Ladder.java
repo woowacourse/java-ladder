cbc96d5827f3a6a63be591f646f05b4524bf8dd3
robinjoon <robin980108@naver.com>
2024-03-01 17:21:39 +0900

    refactor: Ladder 내부 검증 코드 최적화

--- src/main/java/domain/Ladder.java
+++ src/main/java/domain/Ladder.java
@@ -1,26 +1,27 @@
 package domain;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Ladder {
     private final List<Line> lines;
 
     public Ladder(List<Line> lines) {
-        int ladderWeight = lines.get(0).length();
-        boolean[] isConnectedWithNext = new boolean[ladderWeight - 1];
-        for (int index = 0; index < ladderWeight - 1; index++) {
-            for (Line line : lines) {
-                isConnectedWithNext[index] = line.isConnectWithNextPoint(index);
-                if (isConnectedWithNext[index]) {
-                    break;
-                }
-            }
-        }
-        for (int index = 0; index < ladderWeight - 1; index++) {
-            if (!isConnectedWithNext[index]) {
-                throw new IllegalStateException("두 지점 사이에는 반드시 한개 이상의 발판이 있어야 합니다.");
-            }
-        }
+        validateConnectedLadder(lines);
         this.lines = lines;
+    }
+
+    private void validateConnectedLadder(List<Line> lines) {
+        int lastIndex = lines.get(0).length() - 1;
+        boolean isConnectedLadder = IntStream.range(0, lastIndex)
+                .allMatch(index -> isConnectedAtIndex(lines, index));
+        if (!isConnectedLadder) {
+            throw new IllegalStateException("두 지점 사이에는 반드시 한개 이상의 발판이 있어야 합니다.");
+        }
+    }
+
+    private boolean isConnectedAtIndex(List<Line> lines, int index) {
+        return lines.stream()
+                .anyMatch(line -> line.isConnectWithNextPoint(index));
     }
 }
