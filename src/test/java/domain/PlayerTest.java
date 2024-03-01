b7da5b01e8b1c408e333795e79954eded85ca919
robinjoon <robin980108@naver.com>
2024-03-01 21:35:45 +0900

    test: 참가자 이름 구성 문자 검증 테스트 추가

--- src/test/java/domain/PlayerTest.java
+++ src/test/java/domain/PlayerTest.java
@@ -15,4 +15,21 @@ class PlayerTest {
                 .hasMessage("참가자 이름은 1글자 이상 5글자 이하여야 합니다.");
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"!", " ", "가나다", "abc "})
+    @DisplayName("참가자 이름 구성 문자 검증")
+    void validatePlayerNameCharacter(String name) {
+        Assertions.assertThatThrownBy(() -> new Player(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("참가자 이름은 알파벳 대소문자와 숫자만으로 이루어져야 합니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"robin", "12345", "abc12", "12Abc"})
+    @DisplayName("참가자 이름 구성 문자 검증")
+    void normalName(String name) {
+        Assertions.assertThatNoException()
+                .isThrownBy(() -> new Player(name));
+    }
+
 }
