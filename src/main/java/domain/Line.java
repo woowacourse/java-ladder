79544fab7d0086326820b0a162d0dcadd736da1c
robinjoon <robin980108@naver.com>
2024-03-01 15:46:17 +0900

    feat: 연속으로 발판이 나타나는지 검증 기능 구현

--- src/main/java/domain/Line.java
+++ src/main/java/domain/Line.java
@@ -1,12 +1,34 @@
 package domain;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Line {
     private final List<Point> points;
 
     public Line(Boolean... canGoRights) {
-        this.points = new ArrayList<>();
+        List<Point> points = new ArrayList<>();
+        if (canGoRights.length == 0) {
+            throw new IllegalArgumentException();
+        }
+        if (canGoRights[0]) {
+            points.add(new Point(Direction.RIGHT, 0));
+        }
+        for (int index = 1; index < canGoRights.length; index++) {
+            if (canGoRights[index]) {
+                points.add(new Point(Direction.RIGHT, index));
+            }
+            if (canGoRights[index - 1] && canGoRights[index]) {
+                throw new IllegalArgumentException("|-----|-----| 연결 감지!");
+            }
+            if (canGoRights[index - 1] && !canGoRights[index]) {
+                points.add(new Point(Direction.LEFT, index));
+            }
+            if (!canGoRights[index - 1] && !canGoRights[index]) {
+                points.add(new Point(Direction.STRAIGHT, index));
+            }
+        }
+        this.points = Collections.unmodifiableList(points);
     }
 }
