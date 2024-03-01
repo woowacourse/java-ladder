f6a07c210b9377ff446caacece182096bfb5eb9d
eunhee <dmsgml9903@gmail.com>
2024-03-01 17:38:43 +0900

    feat(Participant): 참가자를 이동시키는 로직 구현

--- src/main/java/model/Participant.java
+++ src/main/java/model/Participant.java
@@ -10,10 +10,17 @@ public class Participant {
         this.position = new Position(0);
     }
 
+    Participant(Name name, Position position) {
+        this.name = name;
+        this.position = position;
+    }
+
     public void moveRight() {
+        this.position = position.increment();
     }
 
     public void moveLeft() {
+        this.position = position.decrement();
     }
     public Position getPosition() {
         return position;
--- src/test/java/model/ParticipantTest.java
+++ src/test/java/model/ParticipantTest.java
@@ -17,8 +17,8 @@ public class ParticipantTest {
     @DisplayName("참가자를 왼쪽으로 이동시키면 위치가 1감소한다.")
     @Test
     void moveLeft() {
-        Participant participant = new Participant(new Name("pobi"));
+        Participant participant = new Participant(new Name("pobi"), new Position(1));
         participant.moveLeft();
-        Assertions.assertThat(participant.getPosition()).isEqualTo(new Position(1));
+        Assertions.assertThat(participant.getPosition()).isEqualTo(new Position(0));
     }
 }
