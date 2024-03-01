1e09f4a07e46decc54a85a7a624b8ed3c2f6ad7c
libienz <leekh10195@gmail.com>
2024-03-02 03:15:16 +0900

    test: PointTest 추가

---
+++ src/test/java/domain/PointTest.java
@@ -0,0 +1,36 @@
+package domain;
+
+import static domain.Connection.DISCONNECTION;
+import static domain.Connection.LEFT_CONNECTION;
+import static domain.Connection.RIGHT_CONNECTION;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("사다리 노드 도메인 테스트")
+class PointTest {
+
+    @DisplayName("점에서 오른쪽 이동 지점을 확인할 수 있다")
+    @Test
+    void testNavigateRight() {
+        Point point = new Point(new ColumnPosition(1), RIGHT_CONNECTION);
+        assertThat(point.navigateNextPosition()).isEqualTo(new ColumnPosition(2));
+    }
+
+
+    @DisplayName("점에서 왼쪽 이동 지점을 확인할 수 있다")
+    @Test
+    void testNavigateLeft() {
+        Point point = new Point(new ColumnPosition(1), LEFT_CONNECTION);
+        assertThat(point.navigateNextPosition()).isEqualTo(new ColumnPosition(0));
+    }
+
+
+    @DisplayName("점에서 움직이지 않은 다음 이동 지점을 확인할 수 있다")
+    @Test
+    void testNavigateStay() {
+        Point point = new Point(new ColumnPosition(1), DISCONNECTION);
+        assertThat(point.navigateNextPosition()).isEqualTo(new ColumnPosition(1));
+    }
+}
