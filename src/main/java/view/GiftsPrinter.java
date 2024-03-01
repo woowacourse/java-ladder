ff6d8659284cc9dc679daab95ef70148a58f08da
robinjoon <robin980108@naver.com>
2024-03-02 00:39:43 +0900

    feat: 상품 출력을 위한 문자열 생성 기능 구현

--- src/main/java/view/GiftsPrinter.java
+++ src/main/java/view/GiftsPrinter.java
@@ -1,9 +1,12 @@
 package view;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class GiftsPrinter {
-    public static String from(List<String> rawNames) {
-        return null;
+    public static String from(List<String> rawGiftNames) {
+        return rawGiftNames.stream()
+                .map(GiftPrinter::from)
+                .collect(Collectors.joining(" "));
     }
 }
