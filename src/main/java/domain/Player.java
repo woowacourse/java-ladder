bb4f3db03e49c05d29d4997046d3666fe38c9edd
robinjoon <robin980108@naver.com>
2024-03-01 21:27:13 +0900

    feat: 참가자 이름 길이 검증하는 기능 추가

--- src/main/java/domain/Player.java
+++ src/main/java/domain/Player.java
@@ -2,6 +2,8 @@ package domain;
 
 record Player(String name) {
     Player {
-
+        if (name == null || name.isEmpty() || name.length() > 5) {
+            throw new IllegalArgumentException("참가자 이름은 1글자 이상 5글자 이하여야 합니다.");
+        }
     }
 }
