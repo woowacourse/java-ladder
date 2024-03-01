3796b0cc4a2a2d5b3489d050de53b1ff56ddde67
eunjungL <dldmswjd322@gmail.com>
2024-03-02 01:43:02 +0900

    test: 사다리 게임 진행 테스트 추가

--- src/test/java/domain/LadderTest.java
+++ src/test/java/domain/LadderTest.java
@@ -4,6 +4,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import util.TestBooleanGenerator;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
 class LadderTest {
@@ -16,5 +21,24 @@ class LadderTest {
 
         assertThatCode(() -> Ladder.of(height, 5, testBooleanGenerator))
                 .doesNotThrowAnyException();
+    }
+
+    @DisplayName("사다리 게임 진행")
+    @Test
+    void playLadder() {
+        Participants participants = new Participants(List.of("1", "2", "3", "4"));
+        Prizes prizes = new Prizes(List.of("꽝", "1000", "2000", "3000"));
+        GameResult gameResult = new GameResult();
+
+        Map<Participant, String> expected = new HashMap<>();
+        expected.put(participants.findParticipantByName("1"), prizes.getPrizeByPosition(1));
+        expected.put(participants.findParticipantByName("2"), prizes.getPrizeByPosition(0));
+        expected.put(participants.findParticipantByName("3"), prizes.getPrizeByPosition(3));
+        expected.put(participants.findParticipantByName("4"), prizes.getPrizeByPosition(2));
+
+        Ladder ladder = Ladder.of(new Height("5"), participants.getParticipantsCount(), new TestBooleanGenerator(true));
+
+        ladder.playLadder(gameResult, prizes, participants);
+        assertThat(gameResult.getParticipantsResult()).isEqualTo(expected);
     }
 }
