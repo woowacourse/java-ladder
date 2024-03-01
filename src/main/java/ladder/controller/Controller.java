063c7dd849ebfda4ff9ec36edf9ffe13609e986c
Zeus6768 <akaps6768@gmail.com>
2024-03-01 17:11:24 +0900

    refactor: 컨트롤러 게임 실행 메서드 분리

--- src/main/java/ladder/controller/Controller.java
+++ src/main/java/ladder/controller/Controller.java
@@ -7,12 +7,15 @@ import java.util.Map;
 import ladder.domain.DefaultLadderDirectionSelector;
 import ladder.domain.Height;
 import ladder.domain.Ladder;
+import ladder.domain.LadderBuilder;
+import ladder.domain.LadderDirection;
 import ladder.domain.LadderGame;
 import ladder.domain.LadderResult;
 import ladder.domain.LadderResults;
 import ladder.domain.LadderRow;
 import ladder.domain.Player;
 import ladder.domain.Players;
+import ladder.domain.Width;
 import ladder.view.InputView;
 import ladder.view.ResultView;
 
@@ -28,19 +31,36 @@ public class Controller {
 
     public void run() {
         Players players = inputView.inputNames();
-        LadderResults ladderResults = inputView.inputLadderResults(players);
+        Width<LadderDirection> width = new Width<>(players.count());
+        LadderResults ladderResults = inputView.inputLadderResults(width);
         Height<LadderRow> height = inputView.inputHeight();
-        Ladder ladder = Ladder.of(players, height, new DefaultLadderDirectionSelector());
-        LadderGame ladderGame = new LadderGame(players, ladderResults, ladder);
-        resultView.printLadder(players, ladder, ladderResults);
-        Map<Player, LadderResult> climbResults = ladderGame.play();
+        Ladder ladder = createLadder(width, height);
+        LadderGame ladderGame = LadderGame.of(players, ladderResults, ladder);
+        Map<Player, LadderResult> ladderGameResult = play(ladderGame);
+        printLadderGameResults(ladderGameResult);
+    }
+
+    private Ladder createLadder(final Width<LadderDirection> width, final Height<LadderRow> height) {
+        return LadderBuilder.builder()
+                .width(width)
+                .height(height)
+                .ladderDirectionSelector(new DefaultLadderDirectionSelector())
+                .build();
+    }
+
+    private Map<Player, LadderResult> play(final LadderGame ladderGame) {
+        resultView.printLadderGame(ladderGame);
+        return ladderGame.play();
+    }
+
+    private void printLadderGameResults(final Map<Player, LadderResult> ladderGameResults) {
         while (true) {
-            Player player = inputView.inputPlayerFrom(players);
+            Player player = inputView.inputPlayerFrom(ladderGameResults);
             if (player.equals(ALL)) {
-                resultView.printAllResult(climbResults);
+                resultView.printAllResult(ladderGameResults);
                 break;
             }
-            resultView.printResult(climbResults.get(player));
+            resultView.printResult(ladderGameResults.get(player));
         }
     }
 }
