7cc12100bf487027d27a2ae54338018293c7c11e
kunsanglee <gosmdochee2@gmail.com>
2024-03-01 18:07:46 +0900

    feat: LadderGame 메서드 분리 및 기능 구현

--- src/main/java/LadderGame.java
+++ src/main/java/LadderGame.java
@@ -1,9 +1,13 @@
+import static domain.Player.ALL;
+
 import domain.Height;
 import domain.Ladder;
 import domain.Players;
 import domain.Prizes;
 import domain.generator.BridgeGenerator;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Supplier;
 import view.InputView;
 import view.OutputView;
 
@@ -20,15 +24,65 @@ public class LadderGame {
     }
 
     public void run() {
-        List<String> inputNames = inputView.readPlayerNames();
-        Players players = new Players(inputNames);
-        List<String> inputResults = inputView.readResults();
-        Prizes prizes = new Prizes(players, inputResults);
-        int inputHeight = inputView.readHeight();
-        Height height = new Height(inputHeight);
+        Players players = repeat(this::getPlayers);
+        Prizes prizes = repeat(() -> getPrizes(players));
+        Height height = repeat(this::getHeight);
 
         Ladder ladder = new Ladder(players, height, bridgeGenerator);
 
         outputView.printLadderGame(ladder, players.getNames(), prizes.getPrizes());
+        Map<String, Integer> calculatedResults = ladder.calculate(players);
+
+        repeat(getResultRunnable(calculatedResults, prizes));
+    }
+
+    private Height getHeight() {
+        int inputHeight = inputView.readHeight();
+        return new Height(inputHeight);
+    }
+
+    private Prizes getPrizes(Players players) {
+        List<String> inputResults = inputView.readResults();
+        return new Prizes(players, inputResults);
+    }
+
+    private Players getPlayers() {
+        List<String> inputNames = inputView.readPlayerNames();
+        return new Players(inputNames);
+    }
+
+    private void printResult(Map<String, Integer> calculatedResults, Prizes prizes) {
+        String nameForResult = inputView.readPlayerResult();
+        if (ALL.equals(nameForResult)) {
+            outputView.printResult(calculatedResults, prizes);
+            return;
+        }
+        if (!calculatedResults.containsKey(nameForResult)) {
+            throw new IllegalArgumentException("존재하지 않는 플레이어입니다.");
+        }
+        outputView.printResult(prizes.getPrizeNameOf(calculatedResults.get(nameForResult)));
+        repeat(getResultRunnable(calculatedResults, prizes));
+    }
+
+    private <T> T repeat(Supplier<T> supplier) {
+        try {
+            return supplier.get();
+        } catch (IllegalArgumentException e) {
+            outputView.printError(e.getMessage());
+            return repeat(supplier);
+        }
+    }
+
+    private void repeat(Runnable runnable) {
+        try {
+            runnable.run();
+        } catch (IllegalArgumentException e) {
+            outputView.printError(e.getMessage());
+            repeat(runnable);
+        }
+    }
+
+    private Runnable getResultRunnable(Map<String, Integer> calculatedResults, Prizes prizes) {
+        return () -> printResult(calculatedResults, prizes);
     }
 }
