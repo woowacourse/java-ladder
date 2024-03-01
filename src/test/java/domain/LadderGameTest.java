cbda7c3c91abd9f4b2cbf7f85e4e91af52764894
robinjoon <robin980108@naver.com>
2024-03-01 23:41:18 +0900

    test: 게임 실행 시 없는 참여자 결과 조회 안되는지 검증하는 테스트 추가

--- src/test/java/domain/LadderGameTest.java
+++ src/test/java/domain/LadderGameTest.java
@@ -4,6 +4,7 @@ import domain.LadderGame.LadderGameBuilder;
 import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -13,14 +14,27 @@ class LadderGameTest {
     @CsvSource(value = {"robin,2등", "phobi,1등"})
     @DisplayName("게임 실행 결과 검증")
     void start(String playerName, String giftName) {
-        LadderGame ladderGame = LadderGameBuilder.builder()
+        LadderGame ladderGame = makeTestLadderGame();
+        LadderGameResults actualGameResults = ladderGame.start(playerName);
+        Assertions.assertThat(actualGameResults)
+                .isEqualTo(new LadderGameResults(List.of(new LadderGameResult(playerName, giftName))));
+    }
+
+    private LadderGame makeTestLadderGame() {
+        return LadderGameBuilder.builder()
                 .players("robin", "phobi")
                 .gifts("1등", "2등")
                 .ladderMakeStrategy(() -> new Boolean[]{true, false})
                 .ladderHeight(5)
                 .build();
-        LadderGameResults actualGameResults = ladderGame.start(playerName);
-        Assertions.assertThat(actualGameResults)
-                .isEqualTo(new LadderGameResults(List.of(new LadderGameResult(playerName, giftName))));
+    }
+
+    @Test
+    @DisplayName("게임 실행 시 없는 참여자 결과 조회 안되는지 검증")
+    void validateOperator() {
+        LadderGame ladderGame = makeTestLadderGame();
+        Assertions.assertThatThrownBy(() -> ladderGame.start("none"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("없는 참가자 입니다.");
     }
 }
