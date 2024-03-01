a63c4802bc878497f33a3cb7e3ccf3fa7b74536c
robinjoon <robin980108@naver.com>
2024-03-02 00:15:17 +0900

    refactor: 문자열 자르는 역할 별도 클래스로 분리

--- src/main/java/view/GiftsInputView.java
+++ src/main/java/view/GiftsInputView.java
@@ -1,36 +1,12 @@
 package view;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class GiftsInputView {
     private static final String SEPARATOR = ",";
 
     public static List<String> getGiftNames(String rawString) {
-        validateSeparator(rawString);
-        return splitName(rawString);
-    }
-
-    private static void validateSeparator(String giftNames) {
-        validateStarOrEndWithSeparator(giftNames);
-        validateDuplicateSeparator(giftNames);
-    }
-
-    private static void validateStarOrEndWithSeparator(String giftNames) {
-        boolean startsWith = giftNames.startsWith(SEPARATOR);
-        boolean endsWith = giftNames.endsWith(SEPARATOR);
-        if (startsWith || endsWith) {
-            throw new IllegalArgumentException("입력이 구분자로 시작하거나 끝나면 안됩니다.");
-        }
-    }
-
-    private static void validateDuplicateSeparator(String names) {
-        if (names.contains(SEPARATOR + SEPARATOR)) {
-            throw new IllegalArgumentException("입력에 구분자가 연속으로 등장하면 안됩니다.");
-        }
-    }
-
-    private static List<String> splitName(String giftNames) {
-        return Arrays.stream(giftNames.split(SEPARATOR)).toList();
+        StringSeparator separator = new StringSeparator(SEPARATOR);
+        return separator.splitName(rawString);
     }
 }
