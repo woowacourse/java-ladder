8b60a91dd9d37350068aa1709a9c2c84a8760387
robinjoon <robin980108@naver.com>
2024-03-01 22:03:40 +0900

    test: 참가자 인원수 검증하는 테스트 추가

--- src/test/java/domain/PlayersTest.java
+++ src/test/java/domain/PlayersTest.java
@@ -1,8 +1,11 @@
 package domain;
 
+import java.util.stream.IntStream;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class PlayersTest {
 
@@ -12,6 +15,20 @@ class PlayersTest {
         Assertions.assertThatThrownBy(() -> Players.of("robin", "robin"))
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessage("이름이 같은 참가자는 있을 수 없습니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 11})
+    @DisplayName("참가자 인원 수가 너무 적거나 너무 많은지 확인")
+    void validatePlayersCount(int playersCount) {
+        String[] playerNames = IntStream.rangeClosed(1, playersCount)
+                .mapToObj("a%d"::formatted)
+                .toList()
+                .toArray(new String[playersCount]);
+
+        Assertions.assertThatThrownBy(() -> Players.of(playerNames))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("참가자는 2명 이상 10명 이하여야 합니다.");
     }
 
 }
