b333695f03bffcffa75c9783fea03b5234a82adb
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-03-01 16:58:05 +0900

    test(ItemNameTest): 실행 결과의 이름이 공백일 경우 예외를 던지는 테스트 작성

---
+++ src/main/java/model/ItemName.java
@@ -0,0 +1,11 @@
+package model;
+
+public class ItemName extends Name {
+    public ItemName(final String name) {
+        super(name);
+    }
+
+    @Override
+    protected void validateName(final String name) {
+    }
+}
