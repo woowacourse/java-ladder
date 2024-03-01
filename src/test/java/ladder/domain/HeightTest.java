b260513c0a43560ed2cb075423ef68eff62075e3
mzeong <mjeong@sookmyung.ac.kr>
2024-03-01 12:26:47 +0900

    test(HeightTest): 높이 최소 값 검증 및 높이 생성

--- src/test/java/ladder/domain/HeightTest.java
+++ src/test/java/ladder/domain/HeightTest.java
@@ -2,28 +2,27 @@ package ladder.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class HeightTest {
 
+    @DisplayName("높이가 0 이하면 예외가 발생한다.")
     @Test
-    @DisplayName("높이는 1 이상의 정수이다.")
-    void createHeight() {
+    void validateMinValue() {
         // when & then
-        assertThatCode(() -> new Height(1))
-                .doesNotThrowAnyException();
+        assertThatThrownBy(() -> new Height(0))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @ParameterizedTest
-    @DisplayName("입력된 높이가 0 이하라면 예외가 발생한다.")
-    @ValueSource(ints = {0, -1})
-    void createNegativeHeight(int value) {
-        // when & then
-        assertThatThrownBy(() -> new Height(value))
-                .isInstanceOf(IllegalArgumentException.class);
+    @DisplayName("높이를 생성한다.")
+    @Test
+    void createHeight() {
+        // when
+        Height height = new Height(1);
+
+        // then
+        assertThat(height).extracting("value").isEqualTo(1);
     }
 }
