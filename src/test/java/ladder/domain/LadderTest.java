3fb86bd5308aa37a68342de349fe4ba805d92208
mzeong <mjeong@sookmyung.ac.kr>
2024-03-01 12:56:58 +0900

    test(LadderTest): 사다리 높이 검증

--- src/test/java/ladder/domain/LadderTest.java
+++ src/test/java/ladder/domain/LadderTest.java
@@ -1,12 +1,32 @@
 package ladder.domain;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LadderTest {
+
+    @DisplayName("사다리 높이가 최대 사다리 높이와 같지 않으면 예외가 발생한다.")
+    @Test
+    void validateHeight() {
+        // given
+        List<Line> lines = List.of(
+                new Line(4, Direction.RIGHT, Direction.LEFT, Direction.RIGHT, Direction.LEFT),
+                new Line(4, Direction.DOWN, Direction.RIGHT, Direction.LEFT, Direction.DOWN),
+                new Line(4 ,Direction.RIGHT, Direction.LEFT, Direction.DOWN, Direction.DOWN),
+                new Line(4, Direction.DOWN, Direction.RIGHT, Direction.LEFT, Direction.DOWN),
+                new Line(4, Direction.RIGHT, Direction.LEFT, Direction.RIGHT, Direction.LEFT)
+        );
+
+        // when & then
+        assertThatThrownBy(() -> new Ladder(4, lines))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 
     @DisplayName("특정 참여자의 사다리를 탄다.")
     @ParameterizedTest
@@ -21,11 +41,12 @@ class LadderTest {
         |-----|     |-----|
          */
         Ladder ladder = new Ladder(
-                new Line(Direction.RIGHT, Direction.LEFT, Direction.RIGHT, Direction.LEFT),
-                new Line(Direction.DOWN, Direction.RIGHT, Direction.LEFT, Direction.DOWN),
-                new Line(Direction.RIGHT, Direction.LEFT, Direction.DOWN, Direction.DOWN),
-                new Line(Direction.DOWN, Direction.RIGHT, Direction.LEFT, Direction.DOWN),
-                new Line(Direction.RIGHT, Direction.LEFT, Direction.RIGHT, Direction.LEFT)
+                5,
+                new Line(4, Direction.RIGHT, Direction.LEFT, Direction.RIGHT, Direction.LEFT),
+                new Line(4, Direction.DOWN, Direction.RIGHT, Direction.LEFT, Direction.DOWN),
+                new Line(4, Direction.RIGHT, Direction.LEFT, Direction.DOWN, Direction.DOWN),
+                new Line(4, Direction.DOWN, Direction.RIGHT, Direction.LEFT, Direction.DOWN),
+                new Line(4, Direction.RIGHT, Direction.LEFT, Direction.RIGHT, Direction.LEFT)
         );
 
         Index index = new Index(indexValue);
