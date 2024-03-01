aa120f71849e0b6c8758652ac0620fcd10ffbb3b
leetaehoon <xogns1514@gmail.com>
2024-03-01 16:30:24 +0900

    refactor: Line 생성자 test 삭제

--- src/test/java/domain/ladder/LineTest.java
+++ src/test/java/domain/ladder/LineTest.java
@@ -13,16 +13,6 @@ import static org.assertj.core.api.Assertions.assertThatCode;
 class LineTest {
 
     @Test
-    @DisplayName("가로 길이는 사용자 수 -1 이다.")
-    void createLineWithPersonCount() {
-        //given
-        int personCount = 4;
-        //when
-        //then
-        assertThatCode(() -> Line.of(personCount, new RandomGenerator())).doesNotThrowAnyException();
-    }
-
-    @Test
     @DisplayName("가로 라인은 겹치지 않는다.")
     void createRandomLine() {
         //given
