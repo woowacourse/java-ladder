1710098495edb7d9efcb4c10f338292956abb872
robinjoon <robin980108@naver.com>
2024-03-02 14:53:48 +0900

    test: 참가자 이름 입력 시 중복된 이름 검증하는 테스트, 참가자 이름 수가 너무 많거나 적은지 검증하는 테스트 추가

--- src/test/java/view/PlayersInputViewTest.java
+++ src/test/java/view/PlayersInputViewTest.java
@@ -1,6 +1,8 @@
 package view;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -58,5 +60,25 @@ class PlayersInputViewTest {
         Assertions.assertThatThrownBy(() -> PlayersInputView.getPlayerNames(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("참가자 이름은 all이 될 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("중복된 이름의 참가자가 생성되지 않는지 확인")
+    void validateDuplicateName() {
+        Assertions.assertThatThrownBy(() -> PlayersInputView.getPlayerNames("aa,aa"))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("이름이 같은 참가자는 있을 수 없습니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 11})
+    @DisplayName("참가자 인원 수가 너무 적거나 너무 많은지 확인")
+    void validatePlayersCount(int playersCount) {
+        String input = IntStream.rangeClosed(1, playersCount)
+                .mapToObj("a%d"::formatted)
+                .collect(Collectors.joining(","));
+        Assertions.assertThatThrownBy(() -> PlayersInputView.getPlayerNames(input))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("참가자는 2명 이상 10명 이하여야 합니다.");
     }
 }
