cc393ddc130869750a17dc20eb3b3516b9c6b67f
Nam Gi Beom <rlqja0523@naver.com>
2024-03-02 19:31:45 +0900

    test: 참가자 이름 중 가장 긴 이름 길이 찾기, 인덱스로 참가자 찾기 테스트

--- src/test/java/domain/player/PlayersTest.java
+++ src/test/java/domain/player/PlayersTest.java
@@ -1,5 +1,6 @@
 package domain.player;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
@@ -58,5 +59,48 @@ public class PlayersTest {
 
         // when & then
         assertDoesNotThrow(() -> Players.from(names));
+    }
+
+    @Test
+    void 참가자의_이름_중_가장_긴_이름의_길이를_반환한다() {
+        // given
+        String name1 = "prin";
+        String name2 = "pobi";
+        String maxLengthName = "crong";
+        Players players = Players.from(List.of(name1, name2, maxLengthName));
+
+        // when
+        int result = players.findMaxPlayerNameLength();
+
+        // then
+        assertThat(result).isEqualTo(maxLengthName.length());
+    }
+
+    @Test
+    void 범위를_벗어난_인덱스로_참가자를_찾으면_예외가_발생한다() {
+        // given
+        String name1 = "prin";
+        String name2 = "pobi";
+        String name3 = "crong";
+        Players players = Players.from(List.of(name1, name2, name3));
+
+        // when & then
+        assertThatThrownBy(() -> players.findPlayerByIndex(3))
+                .isExactlyInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 인덱스로_참가자를_찾는다() {
+        // given
+        String name1 = "prin";
+        String name2 = "crong";
+        String targetName = "pobi";
+        Players players = Players.from(List.of(name1, name2, targetName));
+
+        // when
+        Player result = players.findPlayerByIndex(2);
+
+        // then
+        assertThat(result.getName()).isEqualTo(targetName);
     }
 }
