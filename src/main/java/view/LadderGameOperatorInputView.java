9ad6d124b0da8d82ffd79511d9acdefd6acb61f9
robinjoon <robin980108@naver.com>
2024-03-02 15:16:25 +0900

    feat: 사다리 타기 결과를 보고 싶은 참가자를 입력할 때 올바른 입력인지 검증하는 기능 구현

--- src/main/java/view/LadderGameOperatorInputView.java
+++ src/main/java/view/LadderGameOperatorInputView.java
@@ -4,6 +4,13 @@ import java.util.List;
 
 public class LadderGameOperatorInputView {
     public static String getOperator(String rawString, List<String> playerNames) {
+        validateOperator(rawString, playerNames);
         return rawString;
+    }
+
+    private static void validateOperator(String rawString, List<String> playerNames) {
+        if (!playerNames.contains(rawString) && !rawString.equals("all")) {
+            throw new IllegalArgumentException("없는 참가자 입니다.");
+        }
     }
 }
