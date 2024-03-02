57d485214581c61fd2621b6b6c33a4ca4caeeb50
nayonsoso <panda0329@naver.com>
2024-03-03 00:09:49 +0900

    test: 사용자 수와 동일하지 않은 수의 결과에 대한 예외 처리 테스트

--- src/test/java/domain/result/ResultsTest.java
+++ src/test/java/domain/result/ResultsTest.java
@@ -5,21 +5,29 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class ResultsTest {
+class ResultsTest {
 
-    @DisplayName("실행 결과의 수는 사용자의 수와 동일하다.")
+    @DisplayName("사용자의 수와 동일한 수의 실행 결과는 허용한다.")
     @Test
-    void create() {
-        String value = "1";
-        int playerCount = 1;
-        Results results = new Results(List.of(value), playerCount);
+    void checkSizeOfResults() {
+        List<String> resultValues = List.of("1", "2");
+        int playerCount = 2;
 
-        List<String> values = results.getResults().stream()
-                .map(Result::getValue)
-                .toList();
+        assertThatCode(() -> new Results(resultValues, playerCount))
+                .doesNotThrowAnyException();
+    }
 
-        assertThat(values).containsExactly(value);
+    @DisplayName("사용자의 수와 동일하지 않은 수의 실행 결과는 예외를 발생시킨다.")
+    @Test
+    void checkSizeOfResultsFail() {
+        List<String> resultValues = List.of("1", "2");
+        int playerCount = 3;
+
+        assertThatThrownBy(() -> new Results(resultValues, playerCount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("실행 결과의 수는 사용자들의 수와 동일해야 합니다.");
     }
 }
