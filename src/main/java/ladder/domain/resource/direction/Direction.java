b10b77de9ddf4d35926a88f1162e47840755c64d
KIMGYUTAE <kiimwf0819@gmail.com>
2024-03-02 15:08:47 +0900

    refactor(Direction): 불필요한 메서드 제거 및 방향 순서 변경
    - RandomLineGenerator 에서 랜덤 요소를 다루기 때문에 불필요해진 항목 제거
    - 방향 순서 변경

--- src/main/java/ladder/domain/resource/direction/Direction.java
+++ src/main/java/ladder/domain/resource/direction/Direction.java
@@ -1,18 +1,8 @@
 package ladder.domain.resource.direction;
 
-import java.util.Random;
-
 public enum Direction {
 
-    RIGHT,
     LEFT,
-    NEUTRAL;
-
-    private static final Random RANDOM = new Random();
-    private static final Direction[] RIGHT_OR_NEUTRAL = {RIGHT, NEUTRAL};
-    private static final int SIZE = RIGHT_OR_NEUTRAL.length;
-
-    public static Direction getRightOrNeutral() {
-        return RIGHT_OR_NEUTRAL[RANDOM.nextInt(SIZE)];
-    }
+    NEUTRAL,
+    RIGHT
 }
