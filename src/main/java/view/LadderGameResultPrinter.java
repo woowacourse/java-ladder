c07a48740a6eba67c50bb244dd56d0ff84fc521f
fromitive <fromitive@gmail.com>
2024-03-02 21:46:17 +0900

    style: 가독성 개선

--- src/main/java/view/LadderGameResultPrinter.java
+++ src/main/java/view/LadderGameResultPrinter.java
@@ -1,7 +1,9 @@
 package view;
 
 import domain.LadderGameResult;
+import domain.LadderResult;
 import domain.Name;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class LadderGameResultPrinter {
@@ -15,17 +17,18 @@ public class LadderGameResultPrinter {
 
     public void printFromName(String searchName) {
         Name name = new Name(searchName);
+        LadderResult ladderResult = ladderGameResult.getLadderResultFromName(name);
         System.out.println(
-                String.join("\n", LADDER_RESULT_PREFIX,
-                        ladderGameResult.getLadderResultFromName(name).getLadderResult() + "\n"));
+                String.join("\n", LADDER_RESULT_PREFIX, ladderResult.getLadderResult() + "\n"));
     }
 
     public void printAll() {
-        String allNameLadderResult = ladderGameResult.getLadderGameResult()
+        Map<Name, LadderResult> ladderResultMap = this.ladderGameResult.getLadderGameResult();
+        String allNameLadderResult = ladderResultMap
                 .keySet()
                 .stream()
                 .map(name -> NAME_LADDER_RESULT_FORMAT
-                        .formatted(name.getName(), ladderGameResult.getLadderResultFromName(name).getLadderResult()))
+                        .formatted(name.getName(), ladderResultMap.get(name).getLadderResult()))
                 .collect(Collectors.joining("\n"));
         System.out.println(String.join("\n", LADDER_RESULT_PREFIX, allNameLadderResult));
     }
