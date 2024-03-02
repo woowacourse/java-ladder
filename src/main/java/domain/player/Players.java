bac54a9312740f7eb4aafa20573ddaf8e8261e67
Nam Gi Beom <rlqja0523@naver.com>
2024-03-02 19:24:26 +0900

    refactor: 유효성 검증을 먼저 수행하도록 변경

--- src/main/java/domain/player/Players.java
+++ src/main/java/domain/player/Players.java
@@ -1,32 +1,36 @@
 package domain.player;
 
 import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.toList;
 
 import java.util.List;
+import java.util.Map;
 
 public class Players {
     private static final int MINIMUM_NUMBER_OF_PLAYERS = 2;
     private static final int MAXIMUM_NUMBER_OF_PLAYERS = 50;
-    private static final String DUPLICATE_EXCEPTION_MESSAGE = "[ERROR] 중복된 이름이 존재합니다.";
+    private static final int UNIQUE_COUNT = 1;
+    private static final String DUPLICATE_EXCEPTION_MESSAGE = "[ERROR] rejected value: %s - 중복된 값이 존재합니다.";
     private static final String INVALID_PLAYER_COUNT_EXCEPTION_MESSAGE = "[ERROR] 참가자는 %d ~ %d명이어야 합니다.";
 
     private final List<Player> players;
 
     private Players(List<Player> players) {
-        validateNumberOfPlayers(players);
-        validateDuplicateName(players);
         this.players = players;
     }
 
-    public static Players from(List<String> playerNames) { // todo validation 먼저
+    public static Players from(List<String> playerNames) {
+        validateNumberOfPlayers(playerNames);
+        validateDuplicateName(playerNames);
         return playerNames.stream()
                 .map(Player::new)
                 .collect(collectingAndThen(toList(), Players::new));
     }
 
-    private void validateNumberOfPlayers(List<Player> players) {
-        if (players.size() < MINIMUM_NUMBER_OF_PLAYERS || players.size() > MAXIMUM_NUMBER_OF_PLAYERS) {
+    private static void validateNumberOfPlayers(List<String> playerNames) {
+        if (playerNames.size() < MINIMUM_NUMBER_OF_PLAYERS || MAXIMUM_NUMBER_OF_PLAYERS < playerNames.size()) {
             throw new IllegalArgumentException(
                     String.format(INVALID_PLAYER_COUNT_EXCEPTION_MESSAGE, MINIMUM_NUMBER_OF_PLAYERS,
                             MAXIMUM_NUMBER_OF_PLAYERS)
@@ -34,16 +38,20 @@ public class Players {
         }
     }
 
-    private void validateDuplicateName(List<Player> players) {
-        if (getUniqueNameCount(players) != players.size()) {
-            throw new IllegalArgumentException(DUPLICATE_EXCEPTION_MESSAGE);
-        }
+    private static void validateDuplicateName(List<String> playerNames) {
+        Map<String, Long> nameCounts = getNameCounts(playerNames);
+
+        nameCounts.entrySet().stream()
+                .filter(entry -> entry.getValue() > UNIQUE_COUNT)
+                .findFirst()
+                .ifPresent(entry -> {
+                    throw new IllegalArgumentException(String.format(DUPLICATE_EXCEPTION_MESSAGE, entry.getKey()));
+                });
     }
 
-    private long getUniqueNameCount(List<Player> players) {
-        return players.stream()
-                .distinct()
-                .count();
+    private static Map<String, Long> getNameCounts(List<String> playerNames) {
+        return playerNames.stream()
+                .collect(groupingBy(name -> name, counting()));
     }
 
     public int findMaxPlayerNameLength() {
