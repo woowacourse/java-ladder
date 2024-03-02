8b54c442cf290e4c93bbafe2fbf15f99082d25be
yoonjuho <dbswn990@naver.com>
2024-03-02 21:59:44 +0900

    style(Result): 코드 컨벤션 적용

--- src/main/java/model/result/Result.java
+++ src/main/java/model/result/Result.java
@@ -6,7 +6,7 @@ public record Result(String name) {
         this.name = name;
     }
 
-    protected static final String NOT_ALLOWED_NULL_EMPTY_RESULT = "결과의 이름이 없거나 공백으로만 이루어져 있는 경우 결과 생성이 불가합니다.";
+    static final String NOT_ALLOWED_NULL_EMPTY_RESULT = "결과의 이름이 없거나 공백으로만 이루어져 있는 경우 결과 생성이 불가합니다.";
 
     private void validateResultNullAndBlank(String name) {
         if (name == null || name.isBlank()) {
