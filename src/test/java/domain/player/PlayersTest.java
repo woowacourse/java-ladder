835effe5b0adff14649d6a18b345e33174123484
Nam Gi Beom <rlqja0523@naver.com>
2024-03-02 19:45:18 +0900

    refactor: 변수명 변경, 필요없는 변수 제거

--- src/test/java/domain/player/PlayersTest.java
+++ src/test/java/domain/player/PlayersTest.java
@@ -14,38 +14,37 @@ public class PlayersTest {
     @Test
     void 중복된_이름이_존재하면_예외가_발생한다() {
         // given
-        final String duplicateName1 = "prin";
-        final String duplicateName2 = "prin";
-        final String uniqueName = "ddang";
-        final List<String> names = List.of(duplicateName1, duplicateName2, uniqueName);
+        String duplicateName = "prin";
+        String name = "ddang";
+        List<String> playerNames = List.of(duplicateName, duplicateName, name);
 
         // when & then
-        assertThatThrownBy(() -> Players.from(names))
+        assertThatThrownBy(() -> Players.from(playerNames))
                 .isExactlyInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 중복된_이름이_없으면_예외가_발생하지_않는다() {
         // given
-        final String uniqueName1 = "prin";
-        final String uniqueName2 = "ddang";
-        final String uniqueName3 = "pobi";
-        final List<String> names = List.of(uniqueName1, uniqueName2, uniqueName3);
+        String uniqueName1 = "prin";
+        String uniqueName2 = "ddang";
+        String uniqueName3 = "pobi";
+        List<String> playerNames = List.of(uniqueName1, uniqueName2, uniqueName3);
 
         // when & then
-        assertDoesNotThrow(() -> Players.from(names));
+        assertDoesNotThrow(() -> Players.from(playerNames));
     }
 
     @ParameterizedTest
     @ValueSource(ints = {1, 51})
     void 참가자_수가_범위를_벗어나면_예외가_발생한다(int playerCount) {
         // given
-        List<String> names = IntStream.range(0, playerCount)
+        List<String> playerNames = IntStream.range(0, playerCount)
                 .mapToObj(i -> "프린" + i)
                 .toList();
 
         // when & then
-        assertThatThrownBy(() -> Players.from(names))
+        assertThatThrownBy(() -> Players.from(playerNames))
                 .isExactlyInstanceOf(IllegalArgumentException.class);
     }
 
@@ -53,21 +52,19 @@ public class PlayersTest {
     @ValueSource(ints = {2, 50})
     void 참가자_수가_범위_이내이면_예외가_발생하지_않는다(int playerCount) {
         // given
-        List<String> names = IntStream.range(0, playerCount)
+        List<String> playerNames = IntStream.range(0, playerCount)
                 .mapToObj(i -> "프린" + i)
                 .toList();
 
         // when & then
-        assertDoesNotThrow(() -> Players.from(names));
+        assertDoesNotThrow(() -> Players.from(playerNames));
     }
 
     @Test
     void 참가자의_이름_중_가장_긴_이름의_길이를_반환한다() {
         // given
-        String name1 = "prin";
-        String name2 = "pobi";
         String maxLengthName = "crong";
-        Players players = Players.from(List.of(name1, name2, maxLengthName));
+        Players players = Players.from(List.of("jk", "pobi", maxLengthName));
 
         // when
         int result = players.findMaxPlayerNameLength();
@@ -79,10 +76,7 @@ public class PlayersTest {
     @Test
     void 범위를_벗어난_인덱스로_참가자를_찾으면_예외가_발생한다() {
         // given
-        String name1 = "prin";
-        String name2 = "pobi";
-        String name3 = "crong";
-        Players players = Players.from(List.of(name1, name2, name3));
+        Players players = Players.from(List.of("prin", "pobi", "crong"));
 
         // when & then
         assertThatThrownBy(() -> players.findPlayerByIndex(3))
@@ -92,15 +86,13 @@ public class PlayersTest {
     @Test
     void 인덱스로_참가자를_찾는다() {
         // given
-        String name1 = "prin";
-        String name2 = "crong";
-        String targetName = "pobi";
-        Players players = Players.from(List.of(name1, name2, targetName));
+        String targetPlayerName = "pobi";
+        Players players = Players.from(List.of("prin", "crong", targetPlayerName));
 
         // when
         Player result = players.findPlayerByIndex(2);
 
         // then
-        assertThat(result.getName()).isEqualTo(targetName);
+        assertThat(result.getName()).isEqualTo(targetPlayerName);
     }
 }
