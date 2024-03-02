6f0e811e762857f0c80240ee79865f6b60234156
robinjoon <robin980108@naver.com>
2024-03-02 14:34:57 +0900

    test: 사다리 높이 입력 시 너무 길거나 짧은 높이가 입력되었는지 검증하는 테스트 추가

--- src/test/java/view/LadderHeightInputViewTest.java
+++ src/test/java/view/LadderHeightInputViewTest.java
@@ -19,9 +19,18 @@ class LadderHeightInputViewTest {
     @ParameterizedTest
     @ValueSource(strings = {"ㅁ2", "a", " 2", "2 ", "a", "A"})
     @DisplayName("숫자가 아닌게 입력된 경우")
-    void validateLadderHeight(String input) {
+    void validateLadderHeightIsNotNumber(String input) {
         Assertions.assertThatThrownBy(() -> LadderHeightInputView.getLadderHeight(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("숫자가 아닙니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "21"})
+    @DisplayName("너무 길거나 짧은 사다리 높이가 입력된 경우")
+    void validateLadderHeight(String input) {
+        Assertions.assertThatThrownBy(() -> LadderHeightInputView.getLadderHeight(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("사다리 높이는 2 이상 20 이하여야 합니다.");
     }
 }
