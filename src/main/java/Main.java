ad9233da64330d3a0036ec9d68c872910ab9d8dc
robinjoon <robin980108@naver.com>
2024-03-02 15:05:40 +0900

    refactor: main 메서드 분리

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,27 +13,53 @@ import view.PlayersInputView;
 public class Main {
     public static void main(String[] args) {
         RetryHelper retryHelper = new RetryHelper(10);
-        OutputView.print("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
-        List<String> playerNames = retryHelper.retry(() -> PlayersInputView.getPlayerNames(InputView.getInput()));
-        OutputView.print("실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
-        List<String> giftNames = retryHelper.retry(() -> GiftsInputView.getGiftNames(InputView.getInput(),
-                playerNames.size()));
-        OutputView.print("최대 사다리 높이는 몇 개인가요?");
-        Integer ladderHeight = retryHelper.retry(() -> LadderHeightInputView.getLadderHeight(InputView.getInput()));
+        List<String> playerNames = getPlayerNames(retryHelper);
+        List<String> giftNames = getGiftNames(retryHelper, playerNames);
+        Integer ladderHeight = getLadderHeight(retryHelper);
         RandomLineMakeStrategy randomLineMakeStrategy = new RandomLineMakeStrategy(playerNames.size());
-        LadderGame ladderGame = LadderGameBuilder.builder()
-                .players(playerNames.toArray(String[]::new))
-                .gifts(giftNames.toArray(String[]::new))
-                .ladderHeight(ladderHeight)
-                .ladderMakeStrategy(randomLineMakeStrategy::makeLine)
-                .build();
-        OutputView.printPlayers(playerNames);
-        OutputView.printLadder(ladderGame.rawLadder());
-        OutputView.printGifts(giftNames);
+        LadderGame ladderGame = makeLadderGame(playerNames, giftNames, ladderHeight, randomLineMakeStrategy);
+        printLadderGame(playerNames, giftNames, ladderGame);
         LadderGameResults ladderGameResults = retryHelper.retry(() -> {
             String operator = InputView.getInput();
             return ladderGame.start(operator);
         });
         OutputView.printLadderGameResults(ladderGameResults);
+    }
+
+    private static List<String> getPlayerNames(RetryHelper retryHelper) {
+        return retryHelper.retry(() -> {
+            OutputView.print("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
+            return PlayersInputView.getPlayerNames(InputView.getInput());
+        });
+    }
+
+    private static List<String> getGiftNames(RetryHelper retryHelper, List<String> playerNames) {
+        return retryHelper.retry(() -> {
+            OutputView.print("실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
+            return GiftsInputView.getGiftNames(InputView.getInput(), playerNames.size());
+        });
+    }
+
+    private static Integer getLadderHeight(RetryHelper retryHelper) {
+        return retryHelper.retry(() -> {
+            OutputView.print("최대 사다리 높이는 몇 개인가요?");
+            return LadderHeightInputView.getLadderHeight(InputView.getInput());
+        });
+    }
+
+    private static LadderGame makeLadderGame(List<String> playerNames, List<String> giftNames, Integer ladderHeight,
+                                             RandomLineMakeStrategy randomLineMakeStrategy) {
+        return LadderGameBuilder.builder()
+                .players(playerNames.toArray(String[]::new))
+                .gifts(giftNames.toArray(String[]::new))
+                .ladderHeight(ladderHeight)
+                .ladderMakeStrategy(randomLineMakeStrategy::makeLine)
+                .build();
+    }
+
+    private static void printLadderGame(List<String> playerNames, List<String> giftNames, LadderGame ladderGame) {
+        OutputView.printPlayers(playerNames);
+        OutputView.printLadder(ladderGame.rawLadder());
+        OutputView.printGifts(giftNames);
     }
 }
