2e2989ca370ea6790d724dd51e5a3e343ade0ff7
robinjoon <robin980108@naver.com>
2024-03-02 14:56:28 +0900

    feat: 참가자 이름 입력 시 중복된 이름 검증하는 기능, 참가자 이름 수가 너무 많거나 적은지 검증하는 기능 구현

--- src/main/java/view/PlayersInputView.java
+++ src/main/java/view/PlayersInputView.java
@@ -1,6 +1,8 @@
 package view;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -12,12 +14,27 @@ public class PlayersInputView {
     public static List<String> getPlayerNames(String rawString) {
         StringSeparator separator = new StringSeparator(SEPARATOR);
         List<String> splitName = separator.splitName(rawString);
+        validateDuplicateName(splitName);
+        validatePlayersCount(splitName);
         for (String name : splitName) {
             validateNameLength(name);
             validateNameCharacter(name);
             validateNameBlackList(name);
         }
         return splitName;
+    }
+
+    private static void validateDuplicateName(List<String> playersNames) {
+        Set<String> distinctPlayersNames = new HashSet<>(playersNames);
+        if (distinctPlayersNames.size() != playersNames.size()) {
+            throw new IllegalStateException("이름이 같은 참가자는 있을 수 없습니다.");
+        }
+    }
+
+    private static void validatePlayersCount(List<String> playersNames) {
+        if (playersNames == null || playersNames.size() < 2 || playersNames.size() > 10) {
+            throw new IllegalStateException("참가자는 2명 이상 10명 이하여야 합니다.");
+        }
     }
 
     private static void validateNameLength(String name) {
