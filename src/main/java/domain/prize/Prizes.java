fe4f4704c62bb81493d77b57d014a3f387878918
Nam Gi Beom <rlqja0523@naver.com>
2024-03-02 19:36:20 +0900

    refactor: 참가자 수와 같은지 유효성 검증 책임을 prizes로 이동

--- src/main/java/domain/prize/Prizes.java
+++ src/main/java/domain/prize/Prizes.java
@@ -3,19 +3,32 @@ package domain.prize;
 import static java.util.stream.Collectors.collectingAndThen;
 import static java.util.stream.Collectors.toList;
 
+import domain.player.Players;
 import java.util.List;
 
 public class Prizes {
+    private static final String PRIZE_COUNT_EXCEPTION_MESSAGE = "[ERROR] 참가자 수 : %d, 상품 수: %d - 참가자 수와 상품 수는 일치해야 합니다.";
     private final List<Prize> prizes;
 
     private Prizes(List<Prize> prizes) {
         this.prizes = prizes;
     }
 
-    public static Prizes from(List<String> rawPrizes) {
-        return rawPrizes.stream()
+    public static Prizes of(List<String> prizeNames, Players players) {
+        validateSameAsPlayersCount(prizeNames, players);
+        return prizeNames.stream()
                 .map(Prize::new)
                 .collect(collectingAndThen(toList(), Prizes::new));
+    }
+
+    private static void validateSameAsPlayersCount(List<String> prizeNames, Players players) {
+        int prizeCount = prizeNames.size();
+        int playerCount = players.count();
+        if (prizeCount != playerCount) {
+            throw new IllegalArgumentException(
+                    String.format(PRIZE_COUNT_EXCEPTION_MESSAGE, playerCount, prizeCount)
+            );
+        }
     }
 
     public int findMaxPrizeNameLength() {
@@ -25,17 +38,13 @@ public class Prizes {
                 .orElse(0);
     }
 
-    public int count() {
-        return prizes.size();
-    }
-
-    public Prize get(int index) {
-        return prizes.get(index);
-    }
-
-    public List<String> getPrizes() {
+    public List<String> getPrizeNames() {
         return prizes.stream()
                 .map(Prize::getName)
                 .toList();
+    }
+
+    public Prize get(int index) { // todo 메서드, validation
+        return prizes.get(index);
     }
 }
--- src/test/java/fixture/PrizesFixture.java
+++ src/test/java/fixture/PrizesFixture.java
@@ -1,5 +1,6 @@
 package fixture;
 
+import static fixture.PlayersFixture.참가자들;
 import static java.util.stream.Collectors.collectingAndThen;
 import static java.util.stream.Collectors.toList;
 
@@ -11,10 +12,11 @@ public class PrizesFixture {
     public static Prizes 상품들(int count) {
         return IntStream.range(0, count)
                 .mapToObj(i -> "상품" + i)
-                .collect(collectingAndThen(toList(), Prizes::from));
+                .collect(collectingAndThen(toList(), prizeNames -> Prizes.of(prizeNames, 참가자들(count))));
+
     }
 
     public static Prizes 상품들(String... names) {
-        return Prizes.from(Arrays.asList(names));
+        return Prizes.of(Arrays.asList(names), 참가자들(names.length));
     }
 }
