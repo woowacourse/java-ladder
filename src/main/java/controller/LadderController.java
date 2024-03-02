1375b0f2dd72b00e86a0584a11afcae68ee12ea2
MingyeomKim <mingyum119@naver.com>
2024-03-02 14:31:24 +0900

    refactor(LadderController): "all" 입력을 뷰에서 상수로 관리하고 Controller에서 import해서 분기하도록 수정

--- src/main/java/controller/LadderController.java
+++ src/main/java/controller/LadderController.java
@@ -33,12 +33,16 @@ public class LadderController {
 
     // TODO: 이름을 기반으로 Player를 조회하는 역할이 LadderGame인지, Controller인지 고민하기
     private void checkPrize(LadderGame ladderGame) {
-        String playerToCheck = inputView.readPlayerToCheck();
-        while (!playerToCheck.equals("all")) {
-            Prize prize = ladderGame.move(playerToCheck);
+        String player = inputView.readPlayerToCheck();
+        while (checkingOnePlayer(player)) {
+            Prize prize = ladderGame.move(player);
             outputView.printPrizeResult(prize.getName());
-            playerToCheck = inputView.readPlayerToCheck();
+            player = inputView.readPlayerToCheck();
         }
         outputView.printPrizesResult(ladderGame.moveAll());
+    }
+
+    private boolean checkingOnePlayer(final String playerToCheck) {
+        return !playerToCheck.equals(InputView.CHECKING_ALL_MESSAGE);
     }
 }
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,12 +5,13 @@ import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
-
     private static final String PLAYERS_REQUEST_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    private static final String PRIZE_REQUEST_MESSAGE = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
     private static final String HEIGHT_REQUEST_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
+    private static final String PLAYER_TO_CHECK_REQUEST_MESSAGE = "결과를 보고 싶은 사람은?";
     private static final String NAME_SEPARATOR = ",";
     private static final String PRIZE_SEPARATOR = ",";
-
+    public static final String CHECKING_ALL_MESSAGE = "all";
     private final Scanner scanner;
 
     public InputView(Scanner scanner) {
@@ -27,7 +28,7 @@ public class InputView {
 
     public List<String> readPrizes() {
         System.out.println();
-        System.out.println("실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
+        System.out.println(PRIZE_REQUEST_MESSAGE);
         String rawPrizes = scanner.nextLine();
         validateBlank(rawPrizes);
         validateSeparators(rawPrizes, PRIZE_SEPARATOR);
@@ -68,7 +69,7 @@ public class InputView {
 
     public String readPlayerToCheck() {
         System.out.println();
-        System.out.println("결과를 보고 싶은 사람은?");
+        System.out.println(PLAYER_TO_CHECK_REQUEST_MESSAGE);
         return scanner.nextLine();
     }
 }
