4423b705a4730e5c8f4982141234dad7680d1b89
robinjoon <robin980108@naver.com>
2024-03-02 14:50:55 +0900

    feat: 사다리 높이 입력 시 너무 길거나 짧은 높이가 입력되었는지 검증하는 기능 구현

--- src/main/java/view/LadderHeightInputView.java
+++ src/main/java/view/LadderHeightInputView.java
@@ -2,6 +2,14 @@ package view;
 
 public class LadderHeightInputView {
     public static int getLadderHeight(String rawString) {
+        int ladderHeight = parseInteger(rawString);
+        if (ladderHeight < 2 || ladderHeight > 20) {
+            throw new IllegalArgumentException("사다리 높이는 2 이상 20 이하여야 합니다.");
+        }
+        return ladderHeight;
+    }
+
+    private static int parseInteger(String rawString) {
         try {
             return Integer.parseInt(rawString);
         } catch (NumberFormatException e) {
