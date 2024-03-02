86351c0c6bf6eb920cc56370a129b91dce8c43c4
zangsu <zangsu_@naver.com>
2024-03-02 15:44:26 +0900

    refactor : 가독성을 위한 Prize 검증 로직 수정

--- src/main/java/domain/result/Prize.java
+++ src/main/java/domain/result/Prize.java
@@ -3,6 +3,7 @@ package domain.result;
 import java.util.Objects;
 
 public class Prize {
+
     private static final int MAX_RESULT_LENGTH = 5;
 
     private final String resultName;
@@ -13,14 +14,26 @@ public class Prize {
     }
 
     private void validate(String resultName) {
+        validateNotBlank(resultName);
+        validateResultNameLength(resultName);
+        validateStripedResultName(resultName);
+    }
+
+    private void validateNotBlank(String resultName) {
         if (resultName == null || resultName.isBlank()) {
             throw new IllegalArgumentException("빈 값을 결과로 사용할 수 없습니다.");
         }
-        if (!resultName.strip().equals(resultName)) {
-            throw new IllegalArgumentException("앞, 뒤 공백이 존재하는 이름을 결과로 사용할 수 없습니다.");
-        }
+    }
+
+    private void validateResultNameLength(String resultName) {
         if (resultName.length() > MAX_RESULT_LENGTH) {
             throw new IllegalArgumentException("결과는 " + MAX_RESULT_LENGTH + "자 이하로 구성되어야 합니다.");
+        }
+    }
+
+    private void validateStripedResultName(String resultName) {
+        if (!resultName.strip().equals(resultName)) {
+            throw new IllegalArgumentException("앞, 뒤 공백이 존재하는 이름을 결과로 사용할 수 없습니다.");
         }
     }
 
