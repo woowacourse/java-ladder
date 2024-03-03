ab735cd99becfeb62b799072ebbb8a94d45d79a1
hoyeonyy <highoption@naver.com>
2024-03-03 19:30:57 +0900

    refactor (LadderGameTest) : 자바 api를 활용하여 수정

--- src/test/java/ladder/domain/LadderGameTest.java
+++ src/test/java/ladder/domain/LadderGameTest.java
@@ -1,6 +1,5 @@
 package ladder.domain;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.function.BooleanSupplier;
 import ladder.domain.linegenerator.LineGenerator;
@@ -9,16 +8,16 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LadderGameTest {
-    private List<PlayerName> playerNames;
     private Ladder ladder;
 
     @Test
     @DisplayName("게임 결과를 확인한다")
     void playGameTest() {
-        List<PlayerName> playerNames = new ArrayList<>();
-        playerNames.add(new PlayerName("hogee"));
-        playerNames.add(new PlayerName("jazz"));
-        playerNames.add(new PlayerName("pola"));
+        List<PlayerName> playerNames = List.of(
+                new PlayerName("hogee"),
+                new PlayerName("jazz"),
+                new PlayerName("pola")
+        );
 
         ladder = Ladder.makeLadder(new Height(3), 3, new LineGenerator(new AlwaysTrueSupplier()));
 
