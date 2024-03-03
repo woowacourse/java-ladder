888434042a053ecddd03a627a513d296b3fa8e3a
eun-byeol <joeunbyeol98@gmail.com>
2024-03-03 19:37:54 +0900

    test: GameResult 조회 테스트 코드 작성
    - 기존 '참가자와 실행 결과를 연결해 게임 결과 생성한다' 테스트 '존재하는 참가자의 실행 결과 조회 성공' 테스트로 리팩터링
    - '존재하지 않는 참가자를 조회하면 예외 발생' 테스트 추가

--- src/test/java/model/gameresult/GameResultTest.java
+++ src/test/java/model/gameresult/GameResultTest.java
@@ -2,8 +2,8 @@ package model.gameresult;
 
 import static model.bridge.Bridge.CONNECTED;
 import static model.bridge.Bridge.UNCONNECTED;
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import model.ladder.Ladder;
@@ -12,33 +12,41 @@ import model.line.Line;
 import model.player.Players;
 import model.prize.Prizes;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class GameResultTest {
 
-    Players players = Players.of(List.of("jojo", "woo", "lee", "dora"));
-    Prizes prizes = Prizes.of(List.of("꽝", "박수", "칭찬", "박장대소"), players);
+    @DisplayName("존재하는 참가자의 실행 결과 조회 성공")
+    @ParameterizedTest
+    @CsvSource({"jojo,꽝", "woo,칭찬", "lee,박장대소", "dora,박수"})
+    void testValidPlayerNameSearching(String playerName, String prizeName) {
+        GameResult gameResult = prepareGameResult();
 
-    @DisplayName("참가자와 실행 결과를 연결해 게임 결과 생성한다")
-    @Test
-    void testGenerateGameResultFromPlayersAndPrizes() {
-        LadderResult ladderResult = prepareLadderResult();
-        GameResult gameResult = GameResult.of(ladderResult, players, prizes);
-
-        assertAll(
-            () -> assertEquals(gameResult.findPrizeByPlayerName("jojo").getName(), "꽝"),
-            () -> assertEquals(gameResult.findPrizeByPlayerName("woo").getName(), "칭찬"),
-            () -> assertEquals(gameResult.findPrizeByPlayerName("lee").getName(), "박장대소"),
-            () -> assertEquals(gameResult.findPrizeByPlayerName("dora").getName(), "박수")
-        );
+        assertThat(gameResult.findPrizeByPlayerName(playerName).getName())
+            .isEqualTo(prizeName);
     }
 
-    private LadderResult prepareLadderResult() {
+    private GameResult prepareGameResult() {
+        Players players = Players.of(List.of("jojo", "woo", "lee", "dora"));
+        Prizes prizes = Prizes.of(List.of("꽝", "박수", "칭찬", "박장대소"), players);
         Ladder ladder = new Ladder(List.of(
             new Line(List.of(CONNECTED, UNCONNECTED, UNCONNECTED)),
             new Line(List.of(CONNECTED, UNCONNECTED, CONNECTED)),
             new Line(List.of(UNCONNECTED, CONNECTED, UNCONNECTED))
         ));
-        return LadderResult.from(ladder);
+        LadderResult ladderResult = LadderResult.from(ladder);
+        return GameResult.of(ladderResult, players, prizes);
+    }
+
+    @DisplayName("존재하지 않는 참가자를 조회하면 예외 발생")
+    @ParameterizedTest
+    @ValueSource(strings = {"조조", "우", "리", ""})
+    void testInvalidPlayerNameSearching(String playerName) {
+        GameResult gameResult = prepareGameResult();
+
+        assertThatThrownBy(() -> gameResult.findPrizeByPlayerName(playerName))
+            .isInstanceOf(IllegalArgumentException.class);
     }
 }
