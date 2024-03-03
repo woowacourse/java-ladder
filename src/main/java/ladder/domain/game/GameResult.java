e9b07b104c83447e2d66fcaac53b55ec6c241e9f
KIMGYUTAE <kiimwf0819@gmail.com>
2024-03-03 12:04:19 +0900

    refactor(GameResult): 검증 구조 및 메서드명 수정

--- src/main/java/ladder/domain/game/GameResult.java
+++ src/main/java/ladder/domain/game/GameResult.java
@@ -14,47 +14,50 @@ public class GameResult {
     }
 
     public void save(User user, Prize prize) {
-        validateNotEmptyUser(user);
-        validateNotEmptyPrize(prize);
-        validateAlreadyExistsPrize(prize);
+        validateUserToSave(user);
+        validatePrizeToSave(prize);
         result.put(user, prize);
     }
 
     public Prize getUserResult(User user) {
-        validateUserMatch(user);
+        validateUserResultExist(user);
         return result.get(user);
     }
 
     public HashMap<User, Prize> getAllResult() {
-        validateNotEmptyResult();
+        validateResultNotNull();
         return result;
     }
 
-    private void validateNotEmptyUser(User user) {
-        if (user == null) {
-            throw new IllegalArgumentException("[ERROR] 저장하려는 사용자가 존재하지 않습니다.");
+    private void validateUserToSave(User userToSave) {
+        validateResourceNotNull(userToSave, GameResource.RESOURCE_NAME_USERS);
+    }
+
+    private void validatePrizeToSave(Prize prizeToSave) {
+        validateResourceNotNull(prizeToSave, GameResource.RESOURCE_NAME_PRIZES);
+        validatePrizeAlreadyExistsToOtherUser(prizeToSave);
+    }
+
+    private void validateResourceNotNull(Object valueToAdd, String resourceName) {
+        if (valueToAdd == null) {
+            String errorMessage = String.format("[ERROR] 게임 결과에에 저장할 %s가(이) 존재하지 않습니다.", resourceName);
+            throw new IllegalArgumentException(errorMessage);
         }
     }
 
-    private void validateNotEmptyPrize(Prize prize) {
-        if (prize == null) {
-            throw new IllegalArgumentException("[ERROR] 저장하려는 당첨품이 존재하지 않습니다.");
-        }
-    }
-
-    private void validateAlreadyExistsPrize(Prize prize) {
+    private void validatePrizeAlreadyExistsToOtherUser(Prize prize) {
         if (result.containsValue(prize)) {
             throw new IllegalArgumentException("[ERROR] 해당 당첨품은 다른 사용자의 결과로 저장되어 있습니다.");
         }
     }
 
-    private void validateUserMatch(User user) {
+    private void validateUserResultExist(User user) {
         if (!result.containsKey(user)) {
             throw new IllegalArgumentException("[ERROR] 사용자의 결과를 조회할 수 없습니다.");
         }
     }
 
-    private void validateNotEmptyResult() {
+    private void validateResultNotNull() {
         if (result.isEmpty()) {
             throw new IllegalArgumentException("[ERROR] 반환할 결과가 없습니다.");
         }
