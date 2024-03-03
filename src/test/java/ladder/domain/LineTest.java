384dde2dc6543c0e759e1b4e72f30c1e0d4e73af
hoyeonyy <highoption@naver.com>
2024-03-03 18:52:35 +0900

    test (LineTest) : assertAll 로 수정

--- src/test/java/ladder/domain/LineTest.java
+++ src/test/java/ladder/domain/LineTest.java
@@ -1,8 +1,11 @@
 package ladder.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
 import java.util.Collections;
 import java.util.List;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,7 +13,7 @@ class LineTest {
     @Test
     @DisplayName("라인이 비어있으면 예외 발생")
     void validateEmptyLine() {
-        Assertions.assertThatThrownBy(() -> new Line(Collections.emptyList()))
+        assertThatThrownBy(() -> new Line(Collections.emptyList()))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("적어도 가로 라인이 하나이상 있어야 한다.");
     }
@@ -20,7 +23,7 @@ class LineTest {
     void validateOverlappedRowLine() {
         List<Stick> sticks = List.of(Stick.EXISTENCE, Stick.EXISTENCE, Stick.NON_EXISTENCE);
 
-        Assertions.assertThatThrownBy(() -> new Line(sticks))
+        assertThatThrownBy(() -> new Line(sticks))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("가로 라인이 이어지면 안된다.");
     }
@@ -30,8 +33,10 @@ class LineTest {
     void isExistTest() {
         Line line = new Line(List.of(Stick.EXISTENCE, Stick.NON_EXISTENCE, Stick.EXISTENCE));
 
-        Assertions.assertThat(line.isExist(0)).isTrue();
-        Assertions.assertThat(line.isExist(1)).isFalse();
+        assertAll(
+                () -> assertThat(line.isExist(0)).isTrue(),
+                () -> assertThat(line.isExist(1)).isFalse()
+        );
     }
 
     @Test
@@ -39,7 +44,7 @@ class LineTest {
     void sizeTest() {
         Line line = new Line(List.of(Stick.EXISTENCE, Stick.NON_EXISTENCE, Stick.EXISTENCE));
 
-        Assertions.assertThat(line.getWidth()).isEqualTo(3);
+        assertThat(line.getWidth()).isEqualTo(3);
     }
 
     @Test
@@ -49,9 +54,11 @@ class LineTest {
         Line lineAddGap = line.addGap();
         List<Stick> sticks = lineAddGap.getSticks();
 
-        Assertions.assertThat(sticks.get(0).isExist()).isFalse();
-        Assertions.assertThat(sticks.get(sticks.size() - 1).isExist()).isFalse();
-        Assertions.assertThat(sticks.size()).isEqualTo(5);
+        assertAll(
+                () -> assertThat(sticks.get(0).isExist()).isFalse(),
+                () -> assertThat(sticks.get(sticks.size() - 1).isExist()).isFalse(),
+                () -> assertThat(sticks.size()).isEqualTo(5)
+        );
     }
 
     @Test
@@ -60,7 +67,9 @@ class LineTest {
         Line line = new Line(List.of(Stick.EXISTENCE, Stick.NON_EXISTENCE, Stick.EXISTENCE));
         Line lineAddGap = line.addGap();
 
-        Assertions.assertThat(lineAddGap.move(0)).isEqualTo(1);
-        Assertions.assertThat(lineAddGap.move(2)).isEqualTo(3);
+        assertAll(
+                () -> assertThat(lineAddGap.move(0)).isEqualTo(1),
+                () -> assertThat(lineAddGap.move(2)).isEqualTo(3)
+        );
     }
 }
