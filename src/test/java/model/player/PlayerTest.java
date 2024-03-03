2cbada7d163b3891305e1c04ef66f15e2d2fa83b
eun-byeol <joeunbyeol98@gmail.com>
2024-03-03 19:38:39 +0900

    test: CsvSource에서 ValueSource 테스트로 수정

--- src/test/java/model/player/PlayerTest.java
+++ src/test/java/model/player/PlayerTest.java
@@ -5,13 +5,13 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class PlayerTest {
 
     @DisplayName("참여자 이름이 5자 초과면 예외 발생")
     @ParameterizedTest
-    @CsvSource({"pobiii", "dooraaa", "jojojojojojo"})
+    @ValueSource(strings = {"pobiii", "dooraaa", "jojojojojojo"})
     void testInvalidLengthOfPlayerName(String name) {
         assertThatThrownBy(() -> new Player(name))
             .isInstanceOf(IllegalArgumentException.class);
@@ -19,7 +19,7 @@ class PlayerTest {
 
     @DisplayName("참여자 이름이 5자 이하면 객체 생성 성공")
     @ParameterizedTest
-    @CsvSource({"p", "dora", "joj", "doraa"})
+    @ValueSource(strings = {"p", "dora", "joj", "doraa"})
     void testValidLengthOfPlayerName(String name) {
         assertThatCode(() -> new Player(name))
             .doesNotThrowAnyException();
--- src/test/java/model/prize/PrizeTest.java
+++ src/test/java/model/prize/PrizeTest.java
@@ -5,20 +5,20 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class PrizeTest {
 
     @DisplayName("실행 결과가 5자 이하이면 객체 생성 성공")
     @ParameterizedTest
-    @CsvSource({"꽝", "5000", "3000", "식권", "맛있는커피"})
+    @ValueSource(strings = {"꽝", "5000", "3000", "식권", "맛있는커피"})
     void testValidLengthOfPrizeName(String name) {
         assertThatCode(() -> new Prize(name)).doesNotThrowAnyException();
     }
 
     @DisplayName("실행 결과가 5자 초과면 예외 발생")
     @ParameterizedTest
-    @CsvSource({"꽝꽝꽝꽝꽝꽝", "100000", "돼지고기김치찌개"})
+    @ValueSource(strings = {"꽝꽝꽝꽝꽝꽝", "100000", "돼지고기김치찌개"})
     void testInvalidLengthOfPrizeName(String name) {
         assertThatThrownBy(() -> new Prize(name))
             .isInstanceOf(IllegalArgumentException.class);
