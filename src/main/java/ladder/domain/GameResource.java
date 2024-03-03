d078a5b65f558ca457b80fed8d30bfbfcdcda2fc
KIMGYUTAE <kiimwf0819@gmail.com>
2024-03-03 13:24:39 +0900

    refactor(GameResource): 검증 로직 변경
    - 모든 리소스를 한번에 등록하기 때문에 리소스마다 나눠서 검증할 필요 없는 메서드의 변경

--- src/main/java/ladder/domain/GameResource.java
+++ src/main/java/ladder/domain/GameResource.java
@@ -15,9 +15,10 @@ public class GameResource {
     private Ladder ladder;
 
     public void add(Users users, Prizes prizes, Ladder ladder) {
-        validateUsersToAdd(users);
-        validatePrizesToAdd(prizes);
-        validateLadderToAdd(ladder);
+        validateResourceAlreadyExist();
+        validateResourceNotNull(users, RESOURCE_NAME_USERS);
+        validateResourceNotNull(prizes, RESOURCE_NAME_PRIZES);
+        validateResourceNotNull(ladder, RESOURCE_NAME_LADDER);
 
         this.users = users;
         this.prizes = prizes;
@@ -32,32 +33,16 @@ public class GameResource {
         return users.getSize() == prizes.getSize() && prizes.getSize() == ladder.getWidth();
     }
 
-    private void validateUsersToAdd(Users usersToAdd) {
-        validateResourceNotNull(usersToAdd, RESOURCE_NAME_USERS);
-        validateResourceAlreadyExist(this.users, RESOURCE_NAME_USERS);
-    }
-
-    private void validatePrizesToAdd(Prizes prizesToAdd) {
-        validateResourceNotNull(prizesToAdd, RESOURCE_NAME_PRIZES);
-        validateResourceAlreadyExist(this.prizes, RESOURCE_NAME_PRIZES);
-    }
-
-    private void validateLadderToAdd(Ladder ladderToAdd) {
-        validateResourceNotNull(ladderToAdd, RESOURCE_NAME_LADDER);
-        validateResourceAlreadyExist(this.ladder, RESOURCE_NAME_LADDER);
+    private void validateResourceAlreadyExist() {
+        if (allFieldsNotNull()) {
+            throw new IllegalStateException("[ERROR] 게임 리소스가 이미 존재하는 경우 변경할 수 없습니다.");
+        }
     }
 
     private void validateResourceNotNull(Object valueToAdd, String resourceName) {
         if (valueToAdd == null) {
             String errorMessage = String.format("[ERROR] 게임 리소스에 추가할 %s가(이) 존재하지 않습니다.", resourceName);
             throw new IllegalArgumentException(errorMessage);
-        }
-    }
-
-    private void validateResourceAlreadyExist(Object existingValue, String resourceName) {
-        if (existingValue != null) {
-            String errorMessage = String.format("[ERROR] 게임 리소스에 %s가(이) 이미 존재하는 경우 변경할 수 없습니다.", resourceName);
-            throw new IllegalStateException(errorMessage);
         }
     }
 
