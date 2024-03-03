6945593779bb860e6d41094ddefe97a6d4be973b
tackyu <tack970913@gmail.com>
2024-03-03 17:59:38 +0900

    refactor: 사다리게임 클래스의 구조 변경

--- src/main/java/domain/model/ladder/LadderGame.java
+++ src/main/java/domain/model/ladder/LadderGame.java
@@ -1,28 +1,21 @@
 package domain.model.ladder;
 
+import domain.model.consequence.Consequence;
 import domain.model.consequence.Consequences;
 import domain.model.participant.People;
+import domain.model.participant.Person;
 
 public class LadderGame {
-    private final Ladder ladder;
-    private final People people;
-    private final Result result;
 
-    public LadderGame(Ladder ladder, People people, Consequences consequences) {
-        this.ladder = ladder;
-        this.people = people;
-
-        result = new Result(people, consequences);
-    }
-
-    public void play() {
+    public static Result play(Ladder ladder, People people, Consequences consequences) {
+        Result result = new Result();
         for (int positionOfPerson = 0; positionOfPerson < people.getNumberOfParticipants(); positionOfPerson++) {
             int positionOfConsequence = ladder.goToConsequence(positionOfPerson, 0);
-            result.make(positionOfPerson, positionOfConsequence);
+            Person person = people.getNameByOrder(positionOfPerson);
+            Consequence consequence = consequences.getConsequenceByOrder(positionOfConsequence);
+            result.make(person, consequence);
         }
-    }
-
-    public Result giveResult() {
         return result;
     }
+
 }
--- src/test/java/domain/model/LadderGameTest.java
+++ src/test/java/domain/model/LadderGameTest.java
@@ -15,15 +15,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 
 public class LadderGameTest {
-    @Test
-    @DisplayName("사다리게임을 만든다")
-    void makeLadderGame() {
-        Ladder ladder = new Ladder("2", 3, () -> true);
-        People people = new People(List.of("a", "b", "c"));
-        Consequences consequences = new Consequences(List.of("꽝", "100", "200"), 3);
-
-        assertThatCode(() -> new LadderGame(ladder, people, consequences)).doesNotThrowAnyException();
-    }
 
     @Test
     @DisplayName("게임을 진행하고 결과를 얻는다")
@@ -32,10 +23,10 @@ public class LadderGameTest {
         Ladder ladder = new Ladder("2", 3, () -> true);
         People people = new People(List.of("a", "b", "c"));
         Consequences consequences = new Consequences(List.of("꽝", "100", "200"), 3);
-        LadderGame ladderGame = new LadderGame(ladder, people, consequences);
+
         //when
-        ladderGame.play();
-        Result result = ladderGame.giveResult();
+        Result result = LadderGame.play(ladder, people, consequences);
+
         //then
         for (int i = 0; i < 3; i++) {
             Person person = people.getNameByOrder(i);
