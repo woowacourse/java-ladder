57a3700c5c59cb4a92539db199d2b711b4feb6b5
ikjo39 <wer04129@gmail.com>
2024-03-03 17:54:57 +0900

    refactor: 인터페이스 내 어노테이션 추가
    - @FunctionalInterface

--- src/main/java/laddergame/model/generator/BooleansGenerator.java
+++ src/main/java/laddergame/model/generator/BooleansGenerator.java
@@ -4,6 +4,7 @@ import java.util.List;
 import laddergame.model.laddergame.LadderHeight;
 import laddergame.model.participants.Participants;
 
+@FunctionalInterface
 public interface BooleansGenerator {
     List<List<Boolean>> generate(LadderHeight ladderHeight, Participants participants);
 }
