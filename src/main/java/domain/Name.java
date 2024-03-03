bbeeb840c7dc5038a0c0d5d2e1b1c2a0f4bc2e8e
SungKyum Kim <sungkyum1@naver.com>
2024-03-04 01:05:12 +0900

    feat: hashMap 사용을 위한 equals, hashcode 오버라이드

--- src/main/java/domain/Name.java
+++ src/main/java/domain/Name.java
@@ -1,5 +1,7 @@
 package domain;
 
+import java.util.Objects;
+
 public class Name {
     private static final int MAX_NAME_LENGTH = 5;
     private static final int MIN_NAME_LENGTH = 2;
@@ -19,6 +21,19 @@ public class Name {
         if (value.length() < MIN_NAME_LENGTH) {
             throw new IllegalArgumentException("이름의 길이는 2글자 이상만 입력할 수 있습니다.");
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Name n = (Name) o;
+        return name.equals(n.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 
     public String getValue() {
