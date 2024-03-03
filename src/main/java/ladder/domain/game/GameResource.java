6c8cb1d381a60b19d748b7dd684a162cc4563c58
KIMGYUTAE <kiimwf0819@gmail.com>
2024-03-03 12:03:59 +0900

    refactor(GameResource): 검증 구조 및 메서드명 수정

--- src/main/java/ladder/domain/game/GameResource.java
+++ src/main/java/ladder/domain/game/GameResource.java
@@ -7,46 +7,57 @@ import ladder.domain.resource.user.Users;
 public class GameResource {
 
     public static final String RESOURCE_NAME_USERS = "사용자";
-    public static final String RESOURCE_NAME_LADDER = "사다리";
     public static final String RESOURCE_NAME_PRIZES = "당첨품";
-    private static final String SYSTEM_ERROR_ALREADY_EXIST = "[ERROR] 게임 리소스에 %s가(이) 이미 존재하는 경우 변경할 수 없습니다.";
-    private static final String SYSTEM_ERROR_ADD_NULL = "[ERROR] 게임 리소스에 추가할 %s가(이) 존재하지 않습니다.";
-
+    public static final String RESOURCE_NAME_LADDER = "사다리";
 
     private Users users;
-    private Ladder ladder;
     private Prizes prizes;
+    private Ladder ladder;
 
-    public void addUsers(Users users) {
-        validateAlreadyExist(this.users, RESOURCE_NAME_USERS);
-        validateNull(users, RESOURCE_NAME_USERS);
+    public void add(Users users, Prizes prizes, Ladder ladder) {
+        validateUsersToAdd(users);
+        validatePrizesToAdd(prizes);
+        validateLadderToAdd(ladder);
+
         this.users = users;
-    }
-
-    public void addLadder(Ladder ladder) {
-        validateAlreadyExist(this.ladder, RESOURCE_NAME_LADDER);
-        validateNull(ladder, RESOURCE_NAME_LADDER);
+        this.prizes = prizes;
         this.ladder = ladder;
     }
 
-    public void addPrizes(Prizes prizes) {
-        validateAlreadyExist(this.prizes, RESOURCE_NAME_PRIZES);
-        validateNull(prizes, RESOURCE_NAME_PRIZES);
-        this.prizes = prizes;
+    public boolean allFieldsNotNull() {
+        return users != null && prizes != null && ladder != null;
     }
 
+    public boolean allFieldConsistentSize() {
+        return users.getSize() == prizes.getSize() && prizes.getSize() == ladder.getWidth();
+    }
 
-    private void validateAlreadyExist(Object existingValue, String resourceName) {
-        if (existingValue != null) {
-            String errorMessage = String.format(SYSTEM_ERROR_ALREADY_EXIST, resourceName);
-            throw new IllegalStateException(errorMessage);
+    private void validateUsersToAdd(Users usersToAdd) {
+        validateResourceNotNull(usersToAdd, RESOURCE_NAME_USERS);
+        validateResourceAlreadyExist(this.users, RESOURCE_NAME_USERS);
+    }
+
+    private void validatePrizesToAdd(Prizes prizesToAdd) {
+        validateResourceNotNull(prizesToAdd, RESOURCE_NAME_PRIZES);
+        validateResourceAlreadyExist(this.prizes, RESOURCE_NAME_PRIZES);
+    }
+
+    private void validateLadderToAdd(Ladder ladderToAdd) {
+        validateResourceNotNull(ladderToAdd, RESOURCE_NAME_LADDER);
+        validateResourceAlreadyExist(this.ladder, RESOURCE_NAME_LADDER);
+    }
+
+    private void validateResourceNotNull(Object valueToAdd, String resourceName) {
+        if (valueToAdd == null) {
+            String errorMessage = String.format("[ERROR] 게임 리소스에 추가할 %s가(이) 존재하지 않습니다.", resourceName);
+            throw new IllegalArgumentException(errorMessage);
         }
     }
 
-    private void validateNull(Object valueToAdd, String resourceName) {
-        if (valueToAdd == null) {
-            String errorMessage = String.format(SYSTEM_ERROR_ADD_NULL, resourceName);
-            throw new IllegalArgumentException(errorMessage);
+    private void validateResourceAlreadyExist(Object existingValue, String resourceName) {
+        if (existingValue != null) {
+            String errorMessage = String.format("[ERROR] 게임 리소스에 %s가(이) 이미 존재하는 경우 변경할 수 없습니다.", resourceName);
+            throw new IllegalStateException(errorMessage);
         }
     }
 
@@ -54,11 +65,11 @@ public class GameResource {
         return users;
     }
 
-    public Ladder getLadder() {
-        return ladder;
-    }
-
     public Prizes getPrizes() {
         return prizes;
+    }
+
+    public Ladder getLadder() {
+        return ladder;
     }
 }
