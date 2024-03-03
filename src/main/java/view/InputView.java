b57d70d8e9c79c650e83f201d91b30694d903fd7
SCY <helenason@naver.com>
2024-03-03 21:37:26 +0900

    refactor: 구분자 상수화 및 스캐너 생성자 선언

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -6,18 +6,24 @@ import java.util.Scanner;
 
 public class InputView {
 
-    private final Scanner sc = new Scanner(System.in);
+    private static final String DELIMITER = ",";
+
+    private final Scanner sc;
+
+    public InputView() {
+        sc = new Scanner(System.in);
+    }
 
     public List<String> readNames() {
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
         String rawNames = sc.nextLine();
-        return StringParser.splitByDelimiter(rawNames, ",");
+        return StringParser.splitByDelimiter(rawNames, DELIMITER);
     }
 
     public List<String> readResults() {
         System.out.println("실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
         String rawResults = sc.nextLine();
-        return StringParser.splitByDelimiter(rawResults, ",");
+        return StringParser.splitByDelimiter(rawResults, DELIMITER);
     }
 
     public int readHeight() {
