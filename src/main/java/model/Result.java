8d34072f17d87b5dc9f35e2461ae547f966c2a58
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-03-04 16:31:06 +0900

    refactor(Result): 필드 및 생성자 수정

--- src/main/java/model/Result.java
+++ src/main/java/model/Result.java
@@ -1,44 +1,14 @@
 package model;
 
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
 import model.items.Item;
-import model.items.Items;
-import model.people.People;
 import model.people.Person;
 
 public class Result {
-    private final Map<Person, Item> matchedResult;
+    private final Person person;
+    private final Item item;
 
-    protected Result(final Map<Person, Item> matchedResult) {
-        this.matchedResult = matchedResult;
-    }
-
-    public static Result of(final People people, final List<Index> resultIndexes, final Items items) {
-        Map<Person, Item> personAndItemName = new LinkedHashMap<>();
-        Index personIndex = new Index(0);
-        for (Index itemIndex : resultIndexes) {
-            Person person = people.findBy(personIndex);
-            Item item = items.findBy(itemIndex);
-            personAndItemName.put(person, item);
-            personIndex = personIndex.getNext();
-        }
-        return new Result(personAndItemName);
-    }
-
-    public Item findItemByPerson(final Person person) {
-        validatePersonExist(person);
-        return matchedResult.get(person);
-    }
-
-    private void validatePersonExist(final Person person) {
-        if (!matchedResult.containsKey(person)) {
-            throw new IllegalArgumentException("결과를 보려는 사람이 사다리 참여자에 존재하지 않습니다.");
-        }
-    }
-
-    public Map<Person, Item> getMatchedResult() {
-        return matchedResult;
+    public Result(final Person person, final Item item) {
+        this.person = person;
+        this.item = item;
     }
 }
