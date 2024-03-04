33cc35af6032d606600519d751ffb3ffc2443d51
ashsty <qkralsdk991@naver.com>
2024-03-04 16:34:27 +0900

    refactor : 개행 방식 변경

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -24,7 +24,7 @@ public class InputView {
     }
 
     public Prizes readPrizesNames(int peopleCount) {
-        System.out.println("\n실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
+        System.out.println(System.lineSeparator() + "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)");
         String input = scanner.nextLine();
         validateLastDelimiter(input);
         List<Prize> prizes = Arrays.stream(input.split(DELIMITER))
@@ -35,15 +35,13 @@ public class InputView {
     }
 
     public Height readLadderHeight() {
-        System.out.println();
-        System.out.println("최대 사다리 높이는 몇 개인가요?");
+        System.out.println(System.lineSeparator() + "최대 사다리 높이는 몇 개인가요?");
         String input = scanner.nextLine();
         return new Height(input);
     }
 
     public String readResultSearch() {
-        System.out.println();
-        System.out.println("결과를 보고 싶은 사람은?");
+        System.out.println(System.lineSeparator() + "결과를 보고 싶은 사람은?");
         return scanner.nextLine();
     }
 
