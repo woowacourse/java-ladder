12979a4113722e7e2984952d7b771df6d3a08d62
seunghye218 <seunghye@student.42seoul.kr>
2024-03-04 15:54:23 +0900

    refactor: Game 에서 main 분리 및 인스턴스화

--- src/main/java/ladder/Game.java
+++ src/main/java/ladder/Game.java
@@ -11,43 +11,46 @@ import java.io.IOException;
 
 public class Game {
 
-    static final String NAME_SEPARATOR = ",";
+    private static final String NAME_SEPARATOR = ",";
+    private final List<Name> playerNames;
+    private final List<Name> resultNames;
+    private final Ladder ladder;
 
-    public static void main(String[] args) throws IOException {
-        final List<Name> playerNames = getPlayerNames();
-        final List<Name> resultNames = getResultNames(playerNames.size());
-        final int height = getHeight();
-        final Ladder ladder = new Ladder(playerNames.size(), height);
-
-        OutputView.printResult(playerNames, resultNames, ladder);
-        Map<Integer, Integer> allPlayerMatchResult = ladder.getAllMatch();
-        playerMatchResult(playerNames, resultNames, allPlayerMatchResult);
+    public Game() throws IOException {
+        playerNames = getPlayerNames();
+        resultNames = getResultNames();
+        validationResultsSize();
+        ladder = new Ladder(playerNames.size(), getHeight());
     }
 
-    private static List<Name> getPlayerNames() throws IOException {
+    public void run() throws IOException {
+        OutputView.printResult(playerNames, resultNames, ladder);
+
+        displayPlayerMatchResult(ladder.getAllMatch());
+    }
+
+    private List<Name> getPlayerNames() throws IOException {
         OutputView.printf("참여할 사람 이름을 입력하세요. (이름은 쉼표(%s)로 구분하세요)%n", NAME_SEPARATOR);
         return makeNames();
     }
 
-    private static List<Name> getResultNames(final int playerSize) throws IOException {
+    private List<Name> getResultNames() throws IOException {
         OutputView.printf("실행 결과를 입력하세요. (결과는 쉼표(%s)로 구분하세요)\n", NAME_SEPARATOR);
-        final List<Name> resultNames = makeNames();
-        validationResultsSize(playerSize, resultNames.size());
-        return resultNames;
+        return makeNames();
     }
 
-    private static void validationResultsSize(final int nameSize, final int resultSize) {
-        if (nameSize != resultSize) {
+    private void validationResultsSize() {
+        if (playerNames.size() != resultNames.size()) {
             throw new IllegalArgumentException("사람 수와 결과물 수는 같아야 한다.");
         }
     }
 
-    private static int getHeight() throws IOException {
+    private int getHeight() throws IOException {
         OutputView.printf("최대 사다리 높이는 몇 개인가요?\n");
         return InputView.readHeight();
     }
 
-    private static List<Name> makeNames() throws IOException {
+    private List<Name> makeNames() throws IOException {
         final List<String> names = InputView.readNames(NAME_SEPARATOR);
 
         return names.stream()
@@ -55,17 +58,15 @@ public class Game {
                 .toList();
     }
 
-    private static void playerMatchResult(List<Name> playerNames, List<Name> resultNames,
-            Map<Integer, Integer> allPlayerMatchResult) throws IOException {
+    private void displayPlayerMatchResult(Map<Integer, Integer> allPlayerMatchResult) throws IOException {
         while (true) {
             OutputView.printf("\n결과를 보고 싶은 사람은?\n");
-            String rawNameForResult = InputView.readNameForResult();
+            Name nameForResult = new Name(InputView.readNameForResult());
             OutputView.printf("\n실행 결과\n");
-            if ("all".equals(rawNameForResult)) {
+            if ("all".equals(nameForResult.name())) {
                 OutputView.printAllPlayerResult(playerNames, resultNames, allPlayerMatchResult);
                 break;
             }
-            Name nameForResult = new Name(rawNameForResult);
             if (playerNames.contains(nameForResult)) {
                 final int resultIndex = allPlayerMatchResult.get(playerNames.indexOf(nameForResult));
                 final Name result = resultNames.get(resultIndex);
