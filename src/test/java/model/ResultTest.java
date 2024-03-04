b99ed5e9232fb9ff2116e27297bed1bfe81f3438
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-03-04 16:27:41 +0900

    refactor(ResultTest): Person과 Item으로부터 결과를 생성하는 테스트 코드 작성

--- src/test/java/model/ResultTest.java
+++ src/test/java/model/ResultTest.java
@@ -1,9 +1,12 @@
 package model;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.List;
 import java.util.Map;
 import model.items.Item;
+import model.people.People;
 import model.people.Person;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,15 +14,10 @@ import org.junit.jupiter.api.Test;
 class ResultTest {
 
     @Test
-    @DisplayName("결과를 보려는 사람이 사다리 참여자에 없는 경우 예외가 발생한다.")
-    void result() {
-        Map<Person, Item> matchedResult = Map.of(
-                new Person("몰리"), new Item("꽝")
-        );
-
-        Result result = new Result(matchedResult);
-        assertThatThrownBy(() -> result.findItemByPerson(new Person("찰리")))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("결과를 보려는 사람이 사다리 참여자에 존재하지 않습니다.");
+    @DisplayName("결과를 생성한다.")
+    void createResult() {
+        Person person = new Person("찰리");
+        Item item = new Item("꽝");
+        assertThatCode(() -> new Result(person, item));
     }
 }
