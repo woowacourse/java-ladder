e1d5e301e9bfcb298e4a84b974106cbf1991f1c8
ay-eonii <naayen@naver.com>
2024-03-04 17:53:10 +0900

    refactor(Ladder): 방어적 복사 적용

--- src/main/java/ladder/domain/ladder/Ladder.java
+++ src/main/java/ladder/domain/ladder/Ladder.java
@@ -1,5 +1,6 @@
 package ladder.domain.ladder;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -8,7 +9,7 @@ public class Ladder {
     private final List<Line> lines;
 
     public Ladder(List<Line> lines) {
-        this.lines = lines;
+        this.lines = new ArrayList<>(lines);
     }
 
     public List<Line> getLadder() {
