2966f307b79211d13cc910eaf55d513d69248c92
lemone <lejinhy@gmail.com>
2024-03-04 10:20:53 +0900

    chore: gradle 설정 수정

--- gradlew.bat
+++ gradlew.bat
@@ -66,7 +66,7 @@ echo location of your Java installation.
 goto fail
 
 :execute
-@rem Setup the command rung
+@rem Setup the command line
 
 set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 
