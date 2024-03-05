766c2315a5c92e58852a02ad0a938c8c60d41938
Zeus6768 <akaps6768@gmail.com>
2024-03-05 23:29:49 +0900

    refactor: 코드리뷰 반영 - 레코드를 클래스로 변경

--- src/main/java/ladder/domain/ladder/LadderPosition.java
+++ src/main/java/ladder/domain/ladder/LadderPosition.java
@@ -2,9 +2,17 @@ package ladder.domain.ladder;
 
 import ladder.domain.ladder.direction.LadderDirection;
 
-public record LadderPosition(int row, int column) {
+public class LadderPosition {
 
     public static final int START_ROW = 0;
+
+    private final int row;
+    private final int column;
+
+    private LadderPosition(final int row, final int column) {
+        this.row = row;
+        this.column = column;
+    }
 
     public LadderPosition(final int column) {
         this(START_ROW, column);
@@ -12,5 +20,13 @@ public record LadderPosition(int row, int column) {
 
     public LadderPosition next(final LadderDirection direction) {
         return new LadderPosition(row + 1, column + direction.getValue());
+    }
+
+    public int row() {
+        return row;
+    }
+
+    public int column() {
+        return column;
     }
 }
