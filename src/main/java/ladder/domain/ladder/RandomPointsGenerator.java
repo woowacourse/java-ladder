8a0319f9fc08eaae38ad6c4863c413108558f476
ay-eonii <naayen@naver.com>
2024-03-05 10:45:42 +0900

    refactor(RandomPointsGenerator): 불필요한 기본 생성자 제거

--- src/main/java/ladder/domain/ladder/RandomPointsGenerator.java
+++ src/main/java/ladder/domain/ladder/RandomPointsGenerator.java
@@ -8,9 +8,6 @@ import java.util.stream.Stream;
 public class RandomPointsGenerator {
     private final Random random = new Random();
 
-    public RandomPointsGenerator() {
-    }
-
     public List<Point> generate(int size) {
         List<Point> points = Stream.iterate(Point.OFF, this::getPoints)
                 .skip(1)
