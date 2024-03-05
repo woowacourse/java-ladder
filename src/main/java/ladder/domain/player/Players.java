b26b4d58c4f22ea81e5376d8a4b82893c395b707
Zeus6768 <akaps6768@gmail.com>
2024-03-05 23:39:25 +0900

    style: 들여쓰기 2단계를 1단계로 줄이기

--- src/main/java/ladder/domain/player/Players.java
+++ src/main/java/ladder/domain/player/Players.java
@@ -2,32 +2,38 @@ package ladder.domain.player;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 import java.util.function.Consumer;
 
-public record Players(List<Player> players) {
+public class Players {
 
     private static final int MIN_PLAYER_COUNT = 2;
 
-    public Players {
-        validatePlayersCount(players);
-        validateDuplicatedName(players);
+    private final List<Player> players;
+
+    public Players(List<Player> players) {
+        this.players = Objects.requireNonNull(players);
+        validatePlayersCount();
+        validateDuplicatedName();
     }
 
-    private void validatePlayersCount(final List<Player> names) {
-        if (names.size() < MIN_PLAYER_COUNT) {
+    private void validatePlayersCount() {
+        if (players.size() < MIN_PLAYER_COUNT) {
             throw new IllegalArgumentException("참여자는 최소 %d명입니다.".formatted(MIN_PLAYER_COUNT));
         }
     }
 
-    private void validateDuplicatedName(final List<Player> names) {
-        Set<Player> distinctNames = new HashSet<>();
-        names.forEach(name -> {
-            if (distinctNames.contains(name)) {
-                throw new IllegalArgumentException("중복된 이름은 입력할 수 없습니다: %s".formatted(name));
-            }
-            distinctNames.add(name);
-        });
+    private void validateDuplicatedName() {
+        Set<Player> distinctPlayers = new HashSet<>();
+        players.forEach(player -> distinctPlayers.add(requireNotDuplicated(distinctPlayers, player)));
+    }
+
+    private Player requireNotDuplicated(final Set<Player> distinctPlayers, final Player player) {
+        if (distinctPlayers.contains(player)) {
+            throw new IllegalArgumentException("중복된 이름은 입력할 수 없습니다: %s".formatted(player));
+        }
+        return player;
     }
 
     public void forEach(final Consumer<Player> consumer) {
